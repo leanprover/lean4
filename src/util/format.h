03b1ce643e52c118b60d20168d3f98a91b3bc92a
Soonho Kong <soonhok@cs.cmu.edu>
2013-07-23 16:21:04 -0700

    Restructure format, add group and flatten

--- src/util/format.h
+++ src/util/format.h
@@ -5,86 +5,97 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Soonho Kong
 */
 #pragma once
+#include "sexpr.h"
 #include <iostream>
 
 namespace lean {
-class name;
-class mpq;
-class mpz;
-struct format_cell;
-
-enum class format_kind { NIL, INDENT, COMPOSE, CHOICE, LINE, TEXT };
-
 /**
-   \brief Simple LISP-like S-expressions.
-   1. Atoms: nil, string, int, name, mpz or mpq
-   2. Pair: (x . y) where x and y are S-expressions.
+   \brief Format
 
-   S-expressions are used for tracking configuration options, statistics, etc.
+   uses `sexpr` as an internal representation.
 
-   Remark: this datastructure does not allow destructive updates.
+   nil           = nil sexpr
+   text    s     = ("TEXT"    s    )
+   nest    f     = ("NEST"    f    )
+   choice  f1 f2 = ("CHOICE"  f1 f2)
+   compose f1 f2 = ("COMPOSE" f1 f2)
+   line          = ("LINE")
+   nest    n  f  = ("NEST"    n   f)
+
 */
+
 class format {
-    format_cell* m_ptr;
+    sexpr m_value;
 
 public:
-    format():m_ptr(nullptr) {}
-    explicit format(char const * v);
-    explicit format(std::string const & v);
-    explicit format(int v);
-    explicit format(double v);
-    explicit format(name const & v);
-    explicit format(mpz const & v);
-    explicit format(mpq const & v);
-    format(format const & h, format const & t);
-    template<typename T>
-    format(T const & h, format const & t):format(format(h), t) {}
-    template<typename T1, typename T2>
-    format(T1 const & h, T2 const & t):format(format(h), format(t)) {}
-    format(format const & s);
-    format(format && s);
-    template<typename T>
-    format(std::initializer_list<T> const & l):format() {
-        auto it = l.end();
-        while (it != l.begin()) {
-            --it;
-            *this = format(*it, *this);
-        }
+    enum format_kind { NIL, NEST, COMPOSE, CHOICE, LINE, TEXT };
+
+    format():m_value(sexpr()) {}
+    explicit format(sexpr const & v):m_value(v) {}
+    explicit format(char const * v):m_value(v) {}
+    explicit format(std::string const & v):m_value(v) {}
+    explicit format(int v):m_value(v) {}
+    explicit format(double v):m_value(v) {}
+    explicit format(name const & v):m_value(v) {}
+    explicit format(mpz const & v):m_value(v) {}
+    explicit format(mpq const & v):m_value(v) {}
+    format(format const & f1, format const & f2) {
+        m_value = sexpr(sexpr(COMPOSE), sexpr(f1.m_value, f2.m_value));
     }
-    ~format();
+    format(format const & f):m_value(f.m_value) {}
 
-    format_kind kind() const;
+    format_kind kind() const {
+        if(is_nil(m_value)) {
+            return NIL;
+        }
+        if(is_cons(m_value)) {
+            /* CHOICE, COMPOSE, LINE, NEST */
+            return static_cast<format_kind>(to_int(car(m_value)));
+        }
+        return TEXT;
+    }
 
-    friend bool is_nil(format const & s) { return s.m_ptr == nullptr; }
-
-    std::string const & get_string() const;
-
-    unsigned hash() const;
+    unsigned hash() const { return m_value.hash(); }
 
     format & operator=(format const & s);
     format & operator=(format&& s);
     template<typename T>
     format & operator=(T const & v) { return operator=(format(v)); }
 
-    friend void swap(format & a, format & b) { std::swap(a.m_ptr, b.m_ptr); }
+    std::ostream & display(std::ostream & out, sexpr const & s);
+    friend std::ostream & operator<<(std::ostream & out, format const & f);
 
-    // Pointer equality
-    friend bool eqp(format const & a, format const & b) { return a.m_ptr == b.m_ptr; }
+    friend format compose(format const & f1, format const & f2) {
+        return format(f1, f2);
+    }
+    friend format choice(format const & f1, format const & f2) {
+        return format(sexpr(sexpr(CHOICE), sexpr(f1.m_value, f2.m_value)));
+    }
+    friend format nest(int i, format const & f) {
+        return format(sexpr(sexpr(NEST), sexpr(sexpr(i), f.m_value)));
+    }
 
-    friend std::ostream & operator<<(std::ostream & out, format const & s);
+    friend bool is_compose(format const & f)   {
+        return is_cons(f.m_value) && to_int(car(f.m_value)) == format_kind::COMPOSE;
+    }
+    friend bool is_nest(format const & f) {
+        return is_cons(f.m_value) && to_int(car(f.m_value)) == format_kind::NEST;
+    }
+    friend bool is_text(format const & f) {
+        return !is_cons(f.m_value);
+    }
+    friend bool is_line(format const & f) {
+        return is_cons(f.m_value) && to_int(car(f.m_value)) == format_kind::LINE;
+    }
+    friend bool is_choice(format const & f) {
+        return is_cons(f.m_value) && to_int(car(f.m_value)) == format_kind::CHOICE;
+    }
 
+    friend format flatten(format const & f);
 };
 
-inline format compose(format const & head, format const & tail) { return format(head, tail); }
-inline format choice(format const & head, format const & tail) { return format(head, tail); }
-inline format indent(format const & f, unsigned i) { return f; }
-inline format line(format const & f) { return f; }
+inline format line() {
+    return format(sexpr(format::format_kind::LINE));
+}
 
-inline bool is_compose(format const & s)   { return s.kind() != format_kind::COMPOSE; }
-inline bool is_indent(format const & s)   { return s.kind() == format_kind::INDENT; }
-inline bool is_text(format const & s) { return s.kind() == format_kind::TEXT; }
-inline bool is_line(format const & s)    { return s.kind() == format_kind::LINE; }
-inline bool is_choice(format const & s) { return s.kind() == format_kind::CHOICE; }
-
-inline std::string const & to_string(format const & s) { return s.get_string(); }
 }

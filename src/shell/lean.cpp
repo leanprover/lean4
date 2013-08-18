5d609928af5ad3987323df1515c3717c3a1fcda1
Leonardo de Moura <leonardo@microsoft.com>
2013-08-18 16:23:29 -0700

    Add support for reading input files from the command line.
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/shell/lean.cpp
+++ src/shell/lean.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "version.h"
 #include "parser.h"
 using namespace lean;
 
-int main() {
+int main(int argc, char ** argv) {
     std::cout << "Lean (version " << LEAN_VERSION_MAJOR << "." << LEAN_VERSION_MINOR << ")\n";
     frontend f;
-    return parse_commands(f, std::cin) ? 0 : 1;
+    if (argc == 1) {
+        return parse_commands(f, std::cin) ? 0 : 1;
+    } else {
+        bool ok = true;
+        for (int i = 1; i < argc; i++) {
+            std::ifstream in(argv[i]);
+            if (!parse_commands(f, in))
+                ok = false;
+        }
+        return ok ? 0 : 1;
+    }
 }

53e4100e1f35ed0e41c06980d2c5441b9705d3d3
Leonardo de Moura <leonardo@microsoft.com>
2013-08-19 15:58:23 -0700

    Add example with nested quantifiers
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ examples/ex9.lean
@@ -0,0 +1,5 @@
+Variable f : Type -> Bool
+Variable g : Type -> Type -> Bool
+Show forall (a b : Type), exists (c : Type), (g a b) = (f c)
+Check forall (a b : Type), exists (c : Type), (g a b) = (f c)
+Eval forall (a b : Type), exists (c : Type), (g a b) = (f c)

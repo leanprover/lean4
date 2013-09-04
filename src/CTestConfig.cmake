5e603dbf9f7fe400b2c67351cfe30eb1c1d8f11d
Soonho Kong <soonhok@cs.cmu.edu>
2013-09-04 03:41:31 -0700

    Use cdash on cmacslab2 machine due to 10-build per day restriction

--- src/CTestConfig.cmake
+++ src/CTestConfig.cmake
@@ -9,9 +9,14 @@
 set(CTEST_PROJECT_NAME "Lean")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
+#set(CTEST_DROP_METHOD "http")
+#set(CTEST_DROP_SITE "my.cdash.org")
+#set(CTEST_DROP_LOCATION "/submit.php?project=Lean")
+#set(CTEST_DROP_SITE_CDASH TRUE)
+
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Lean")
+set(CTEST_DROP_SITE "cmacslab2.modck.cs.cmu.edu")
+set(CTEST_DROP_LOCATION "/CDash-2-0-2/submit.php?project=Lean")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(UPDATE_COMMAND "git")

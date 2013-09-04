c6db95802c02d479d3607f8cfcb38f21c1e8c48c
Leonardo de Moura <leonardo@microsoft.com>
2013-09-03 18:12:46 -0700

    Make example 1 less symmetric
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- examples/ex1.lean
+++ examples/ex1.lean
@@ -13,14 +13,14 @@ Variable d : N
 Variable e : N
 
 (* Add axioms stating facts about these variables *)
-Axiom H1 : (a = b ∧ b = c) ∨ (a = d ∧ d = c)
+Axiom H1 : (a = b ∧ b = c) ∨ (d = c ∧ a = d)
 Axiom H2 : b = e
 
 (* Proof that (h a b) = (h c e) *)
 Theorem T1 : (h a b) = (h c e) :=
     DisjCases H1
               (λ C1, CongrH (Trans (Conjunct1 C1) (Conjunct2 C1)) H2)
-              (λ C2, CongrH (Trans (Conjunct1 C2) (Conjunct2 C2)) H2)
+              (λ C2, CongrH (Trans (Conjunct2 C2) (Conjunct1 C2)) H2)
 
 (* We can use theorem T1 to prove other theorems *)
 Theorem T2 : (h a (h a b)) = (h a (h c e)) :=

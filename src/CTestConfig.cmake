b04bdae7637dd0c40b485362a6dad985c302c5e0
Soonho Kong <soonhok@cs.cmu.edu>
2013-09-10 10:47:04 -0700

    Use "--trace-children=yes" option for MemCheck(valgrind) to handle leantests shell script

--- src/CTestConfig.cmake
+++ src/CTestConfig.cmake
@@ -9,10 +9,10 @@
 set(CTEST_PROJECT_NAME "Lean")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-#set(CTEST_DROP_METHOD "http")
-#set(CTEST_DROP_SITE "my.cdash.org")
-#set(CTEST_DROP_LOCATION "/submit.php?project=Lean")
-#set(CTEST_DROP_SITE_CDASH TRUE)
+# Specify MEMCHECK Option.
+# Note: We use "--trace-children=yes" to valgrind-ise child processes (follow execve)
+SET (VALGRIND_COMMAND_OPTIONS "-q --tool=memcheck --leak-check=yes --show-reachable=yes --workaround-gcc296-bugs=yes --num-callers=50 --trace-children=yes")
+SET (CTEST_MEMORYCHECK_COMMAND_OPTIONS "-q --tool=memcheck --leak-check=yes --show-reachable=yes --workaround-gcc296-bugs=yes --num-callers=50 --trace-children=yes")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cmacslab2.modck.cs.cmu.edu")

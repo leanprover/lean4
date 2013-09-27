9d8ff0eadb4847bdb8f8b89aeadb39edd19b5d30
Leonardo de Moura <leonardo@microsoft.com>
2013-09-26 19:56:12 -0700

    test(mpz): add unit tests for mpz
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ src/tests/util/numerics/mpz.cpp
@@ -0,0 +1,45 @@
+/*
+Copyright (c) 2013 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Leonardo de Moura
+*/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "util/test.h"
+#include "util/numerics/mpz.h"
+using namespace lean;
+
+static void tst1() {
+    mpz a(-10);
+    lean_assert_eq(a.log2(), 0);
+    lean_assert_eq(a.mlog2(), 3);
+    lean_assert_eq(a.is_power_of_two(), false);
+    lean_assert_eq(mpz(10).mlog2(), 0);
+    mpz r;
+    lean_assert(root(r, mpz(4), 2));
+    lean_assert(r == 2);
+    lean_assert(!root(r, mpz(10), 2));
+    lean_assert(r == 3);
+    lean_assert(mpz(10) % mpz(3) == mpz(1));
+    lean_assert(mpz(10) % mpz(-3) == 1);
+    lean_assert(mpz(-10) % mpz(-3) == 2);
+    lean_assert(mpz(-10) % mpz(3) == 2);
+    mpz big;
+    big = power(mpz(10), 10000);
+    std::ostringstream out;
+    out << big;
+    std::string s = out.str();
+    lean_assert(s.size() == 10001);
+    lean_assert(s[0] == '1');
+    for (unsigned i = 1; i < 10001; i++) {
+        lean_assert(s[i] == '0');
+    }
+}
+
+int main() {
+    tst1();
+    return has_violations() ? 1 : 0;
+}
+

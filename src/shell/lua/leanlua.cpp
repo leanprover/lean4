88ebdbcfb68ceca88d274df7f34a96ceb23ae3dd
Soonho Kong <soonhok@cs.cmu.edu>
2013-11-03 13:25:33 -0500

    fix(shell/lua): move "#include<iostream>"

--- src/shell/lua/leanlua.cpp
+++ src/shell/lua/leanlua.cpp
@@ -4,10 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
-#ifdef LEAN_USE_LUA
-#include <lua.hpp>
 #include <iostream>
 #include <cstdlib>
+
+#ifdef LEAN_USE_LUA
+#include <lua.hpp>
 #include "bindings/lua/name.h"
 
 int main(int argc, char ** argv) {

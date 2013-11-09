554defe89db8cdd0b9aa4b352a66b062e47983dc
Leonardo de Moura <leonardo@microsoft.com>
2013-11-09 12:08:17 -0800

    feat(lua): expose local_context objects in the Lua bindings
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/bindings/lua/leanlua_state.cpp
+++ src/bindings/lua/leanlua_state.cpp
@@ -19,6 +19,7 @@ Author: Leonardo de Moura
 #include "bindings/lua/sexpr.h"
 #include "bindings/lua/format.h"
 #include "bindings/lua/level.h"
+#include "bindings/lua/local_context.h"
 #include "bindings/lua/expr.h"
 #include "bindings/lua/context.h"
 #include "bindings/lua/lean.lua"
@@ -46,6 +47,7 @@ struct leanlua_state::imp {
         lean::open_sexpr(m_state);
         lean::open_format(m_state);
         lean::open_level(m_state);
+        lean::open_local_context(m_state);
         lean::open_expr(m_state);
         lean::open_context(m_state);
         dostring(g_leanlua_extra);

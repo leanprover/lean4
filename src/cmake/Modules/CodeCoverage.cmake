1cf037f11edcbb783e7b322b89e33a94f3f7dc0c
Soonho Kong <soonhok@cs.cmu.edu>
2013-11-09 21:57:25 -0500

    chore(cmake): do not delete coverage.info.cleaned which will be used for coveralls.io

--- src/cmake/Modules/CodeCoverage.cmake
+++ src/cmake/Modules/CodeCoverage.cmake
@@ -94,7 +94,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
                 COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
                 COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' '/usr/*' --output-file ${_outputname}.info.cleaned
                 COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
-                COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
+                COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info
 
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."

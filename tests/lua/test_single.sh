20756c382cab7b978677568e20a984115a7614ea
Soonho Kong <soonhok@cs.cmu.edu>
2013-11-18 18:02:12 -0500

    test(*): split leantests, leanslowtests, leanluatests, leanluadocs into singletons

---
+++ tests/lua/test_single.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "Usage: test.sh [leanlua-executable-path] [file]"
+    exit 1
+fi
+ulimit -s unlimited
+LEANLUA=$1
+f=$2
+echo "-- testing $f"
+if $LEANLUA util.lua $f > $f.produced.out; then
+    echo "-- worked"
+    exit 0
+else
+    echo "ERROR executing $f, produced output is at $f.produced.out"
+    exit 1
+fi

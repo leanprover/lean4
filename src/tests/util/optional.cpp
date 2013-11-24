bcd88cac087956db22f806c059fb3a9fa32c2e59
Leonardo de Moura <leonardo@microsoft.com>
2013-11-23 17:05:46 -0800

    style(util): missing includes
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/tests/util/optional.cpp
+++ src/tests/util/optional.cpp
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #include <iostream>
+#include <string>
 #include "util/test.h"
 #include "util/optional.h"
 using namespace lean;

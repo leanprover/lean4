2972fb9d8c6b9ef188d1a11e34be48776834b10b
Leonardo de Moura <leonardo@microsoft.com>
2013-12-15 22:00:26 -0800

    test(tests/lean): new elaborator tests
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/elab7.lean
@@ -0,0 +1,23 @@
+Variable A : Type U
+Variable B : Type U
+Variable C : Type U
+Variable P : A -> Bool
+Variable F1 : A -> B -> C
+Variable F2 : A -> B -> C
+Variable H : Pi (a : A) (b : B), (F1 a b) == (F2 a b)
+Variable a : A
+Check Eta (F2 a)
+Check Abst (fun a : A,
+              (Trans (Symm (Eta (F1 a)))
+                     (Trans (Abst (fun (b : B), H a b))
+                            (Eta (F2 a)))))
+
+Check Abst (fun a, (Abst (fun b, H a b)))
+
+Theorem T1 : F1 = F2 := Abst (fun a, (Abst (fun b, H a b)))
+Theorem T2 : (fun (x1 : A) (x2 : B), F1 x1 x2) = F2 := Abst (fun a, (Abst (fun b, H a b)))
+Theorem T3 : F1 = (fun (x1 : A) (x2 : B), F2 x1 x2) := Abst (fun a, (Abst (fun b, H a b)))
+Theorem T4 : (fun (x1 : A) (x2 : B), F1 x1 x2) = (fun (x1 : A) (x2 : B), F2 x1 x2) := Abst (fun a, (Abst (fun b, H a b)))
+Show Environment 4
+Set pp::implicit true
+Show Environment 4

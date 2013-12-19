46627289b80a1f7937d946ccd54280412e8e7119
Leonardo de Moura <leonardo@microsoft.com>
2013-12-19 14:47:53 -0800

    fix(kernel/expr): avoid '_' as a binder name, we use '_' as a placeholder in the Lean frontend
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/kernel/expr.h
+++ src/kernel/expr.h
@@ -420,7 +420,7 @@ inline expr mk_eq(expr const & l, expr const & r) { return expr(new expr_eq(l, r
 inline expr Eq(expr const & l, expr const & r) { return mk_eq(l, r); }
 inline expr mk_lambda(name const & n, expr const & t, expr const & e) { return expr(new expr_lambda(n, t, e)); }
 inline expr mk_pi(name const & n, expr const & t, expr const & e) { return expr(new expr_pi(n, t, e)); }
-inline expr mk_arrow(expr const & t, expr const & e) { return mk_pi(name("_"), t, e); }
+inline expr mk_arrow(expr const & t, expr const & e) { return mk_pi(name("a"), t, e); }
 inline expr operator>>(expr const & t, expr const & e) { return mk_arrow(t, e); }
 inline expr mk_let(name const & n, optional<expr> const & t, expr const & v, expr const & e) { return expr(new expr_let(n, t, v, e)); }
 inline expr mk_let(name const & n, expr const & t, expr const & v, expr const & e) { return mk_let(n, some_expr(t), v, e); }

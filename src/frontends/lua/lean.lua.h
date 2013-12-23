f0833b6f46bda58271330dd5ae117c3824dc7b82
Leonardo de Moura <leonardo@microsoft.com>
2013-12-22 21:34:20 -0800

    chore(frontends/lua/lean.lua.h): fix style warnings
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/frontends/lua/lean.lua.h
+++ src/frontends/lua/lean.lua.h
@@ -1,11 +1,16 @@
-static char const * g_extra_code = R"Lua(
-function Consts(s)
-  r = {}
-  i = 1
-  for c in string.gmatch(s, '[^ ,;\t\n]+') do
-     r[i] = Const(c)
-     i = i + 1
-  end
-  return unpack(r)
-end
-)Lua";
+/*
+Copyright (c) 2013 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Leonardo de Moura
+*/
+static char const * g_extra_code =
+    "function Consts(s)\n"
+    "  r = {}\n"
+    "  i = 1\n"
+    "  for c in string.gmatch(s, '[^ ,;\\t\\n]+') do\n"
+    "     r[i] = Const(c)\n"
+    "     i = i + 1\n"
+    "  end\n"
+    "  return unpack(r)\n"
+    "end\n";

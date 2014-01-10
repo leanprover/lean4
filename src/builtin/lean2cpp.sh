5fb718c03a841c54647b5bd9799643c516d2fc6a
Leonardo de Moura <leonardo@microsoft.com>
2014-01-10 10:58:35 -0800

    fix(build): broken dependencies between lean executable and .olean, *_decls.cpp and *_decls.h files
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ src/builtin/lean2cpp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+LEAN=$1         # Lean executable
+SOURCE_DIR=$2   # Where the .lean and .lua auxiliary files are located
+LEAN_FILE=$3    # Lean file to be exported
+DEST=$4         # where to put the CPP file
+ARGS=$5         # extra arguments
+if $LEAN -q $ARGS $LEAN_FILE $SOURCE_DIR/name_conv.lua $SOURCE_DIR/lean2cpp.lean > $DEST.tmp
+then
+    mv $DEST.tmp $DEST
+else
+    echo "Failed to generate $DEST"
+    exit 1
+fi

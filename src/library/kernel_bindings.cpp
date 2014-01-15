28eb980484aff4a3524aae64d844c6b3e3e20f13
Leonardo de Moura <leonardo@microsoft.com>
2014-01-14 18:29:51 -0800

    fix(build): C++ module dependency problem, and style
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/library/kernel_bindings.cpp
+++ src/library/kernel_bindings.cpp
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #include <utility>
+#include <string>
 #include "util/sstream.h"
 #include "util/script_state.h"
 #include "kernel/expr.h"

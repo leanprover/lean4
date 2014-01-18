40b7ed13c2c97133a902a97dbae20d22f71a01f4
Leonardo de Moura <leonardo@microsoft.com>
2014-01-17 19:27:32 -0800

    fix(tests/lean): adjust tests
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/library/equality.cpp
+++ src/library/equality.cpp
@@ -4,8 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
-#include "library/expr_pair.h"
 #include "kernel/kernel.h"
+#include "library/expr_pair.h"
 
 namespace lean {
 bool is_equality(expr const & e) {
@@ -13,13 +13,10 @@ bool is_equality(expr const & e) {
 }
 
 bool is_equality(expr const & e, expr & lhs, expr & rhs) {
-    if (is_eq(e)) {
-        lhs = arg(e, 2);
-        rhs = arg(e, 3);
-        return true;
-    } else if (is_iff(e)) {
-        lhs = arg(e, 1);
-        rhs = arg(e, 2);
+    if (is_eq(e) || is_iff(e)) {
+        unsigned num = num_args(e);
+        lhs = arg(e, num - 2);
+        rhs = arg(e, num - 1);
         return true;
     } else {
         return false;
@@ -27,12 +24,7 @@ bool is_equality(expr const & e, expr & lhs, expr & rhs) {
 }
 
 expr_pair get_equality_args(expr const & e) {
-    if (is_eq(e)) {
-        return mk_pair(arg(e, 2), arg(e, 3));
-    } else if (is_iff(e)) {
-        return mk_pair(arg(e, 1), arg(e, 2));
-    } else {
-        lean_unreachable(); // LCOV_EXCL_LINE
-    }
+    unsigned num = num_args(e);
+    return mk_pair(arg(e, num - 2), arg(e, num - 1));
 }
 }

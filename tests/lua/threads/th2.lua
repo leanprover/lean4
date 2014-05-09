21e3e22017f509af0e1e0f543c1a02ccfec58f7f
Leonardo de Moura <leonardo@microsoft.com>
2014-05-08 18:52:15 -0700

    test(lua/threads): re-activate Lua thread examples
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- tests/lua/threads/th2.lua
+++ tests/lua/threads/th2.lua
@@ -2,21 +2,32 @@ S1 = State()
 S2 = State()
 code = [[
   function f(env, prefix, num, type)
+      local r = list_certified_definition()
       for i = 1, num do
-          env:add_var(prefix .. "_" .. i, type)
+          r = list_certified_definition(type_check(env, mk_var_decl(prefix .. "_" .. i, type)), r)
       end
+      return r
   end
 ]]
 S1:dostring(code)
 S2:dostring(code)
-e = environment()
-e:add_var("N", Type())
+local e = empty_environment()
+e = add_decl(e, mk_var_decl("N", Type))
 code2 = [[
   e, prefix = ...
-  f(e, prefix, 1000, Const("N"))
+  return f(e, prefix, 1000, Const("N"))
 ]]
 T1 = thread(S1, code2, e, "x")
 T2 = thread(S2, code2, e, "y")
-T1:wait()
-T2:wait()
-print(e)
+local r1 = T1:wait()
+local r2 = T2:wait()
+while not r1:is_nil() do
+   e = e:add(r1:car())
+   r1 = r1:cdr()
+end
+while not r2:is_nil() do
+   e = e:add(r2:car())
+   r2 = r2:cdr()
+end
+assert(e:find("x_" .. 10))
+assert(e:find("y_" .. 100))

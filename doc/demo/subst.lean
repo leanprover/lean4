7142c0fed3b20e05a7e2c6fe621aad2d76a27dfc
Leonardo de Moura <leonardo@microsoft.com>
2014-05-17 10:43:30 -0700

    doc(demo): remove Lean 0.1 demo files
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- doc/demo/subst.lean
+++
@@ -1,9 +0,0 @@
-variable q : Nat → Bool
-variable f : Nat → Nat → Nat
-
-theorem T1 (a b : Nat) (H1 : a = b) (H2 : q (f (f a a) (f a a)))  :  q (f (f a b) (f a a))
-:= subst H2 H1
-
-check @subst
-set_option pp::implicit true
-print environment 1

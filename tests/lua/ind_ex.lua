eb6807e1d333b8e7669ab58968bc792730640f41
Leonardo de Moura <leonardo@microsoft.com>
2014-05-19 17:48:35 -0700

    test(lua): add another add_inductive example
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lua/ind_ex.lua
@@ -0,0 +1,16 @@
+local env       = environment()
+local l         = param_univ("l")
+local U_l       = mk_sort(l)
+local inhabited = Const("inhabited", {l})
+local A         = Local("A", U_l)
+local a         = Local("a", A)
+
+env = add_inductive(env,
+                    "inhabited", {l}, 1, mk_arrow(U_l, Bool),
+                    "inhabited_intro", Pi({A, a}, inhabited(A)))
+
+function display_type(env, t)
+   print(tostring(t) .. " : " .. tostring(type_checker(env):check(t)))
+end
+
+display_type(env, Const("inhabited_rec", {1, 1}))

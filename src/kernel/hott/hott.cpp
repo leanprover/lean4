45d473d44eea2236ed3f2df14d2db86fe5b5f348
Leonardo de Moura <leonardo@microsoft.com>
2014-05-21 11:31:34 -0700

    feat(kernel): add mk_hott_environment function for creating HoTT compatible environments
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ src/kernel/hott/hott.cpp
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2014 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Leonardo de Moura
+*/
+#include "kernel/inductive/inductive.h"
+
+namespace lean {
+using inductive::inductive_normalizer_extension;
+
+/** \brief Create a HoTT (Homotopy Type Theory) compatible environment */
+environment mk_hott_environment(unsigned trust_lvl) {
+    return environment(trust_lvl,
+                       false /* Type.{0} is proof relevant */,
+                       true  /* Eta */,
+                       false /* Type.{0} is predicative */,
+                       list<name>(name("Id")) /* Exact equality types are proof irrelevant */,
+                       /* builtin support for inductive datatypes */
+                       std::unique_ptr<normalizer_extension>(new inductive_normalizer_extension()));
+}
+}

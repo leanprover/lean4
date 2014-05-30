cba52b76c78b988949ae37428b19a82a8ebcab2a
Leonardo de Moura <leonardo@microsoft.com>
2014-05-30 16:30:12 -0700

    test(lua): add integer datatype test
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lua/int1.lua
@@ -0,0 +1,19 @@
+local env      = environment()
+local nat      = Const("nat")
+local int      = Const("int")
+
+env = add_inductive(env,
+                    "nat", Type,
+                    "zero", nat,
+                    "succ", mk_arrow(nat, nat))
+
+env = add_inductive(env,
+                    "int", Type,
+                    "pos", mk_arrow(nat, int),
+                    "neg", mk_arrow(nat, int))
+function display_type(env, t)
+   print(tostring(t) .. " : " .. tostring(type_checker(env):check(t)))
+end
+
+display_type(env, Const("nat_rec", {0}))
+display_type(env, Const("int_rec", {0}))

cffbae36678404cbd8ddf64841433ed52540feee
Leonardo de Moura <leonardo@microsoft.com>
2014-06-11 21:07:08 -0700

    test(tests/lean/run): add new test group, where we just execute Lean (and don't check output)
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/t1.lean
@@ -0,0 +1,8 @@
+print raw ((Bool))
+print raw Bool
+print raw fun (x y : Bool), x x
+print raw fun (x y : Bool) {z : Bool}, x y
+print raw λ [x y : Bool] {z : Bool}, x z
+print raw Pi (x y : Bool) {z : Bool}, x
+print raw ∀ (x y : Bool) {z : Bool}, x
+print raw forall {x y : Bool} w {z : Bool}, x

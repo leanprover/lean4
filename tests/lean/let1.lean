873a5c8605162946bd65e1e41e14904e3488b04f
Leonardo de Moura <leonardo@microsoft.com>
2014-06-16 14:11:26 -0700

    test(lean): add 'let' expression test
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/let1.lean
@@ -0,0 +1,23 @@
+-- Correct version
+check let bool : Type.{1}                          := Type.{0},
+          and  (p q : bool) : bool                 := ∀ c : bool, (p → q → c) → c,
+          infixl `∧` 25                           := and,
+          and_intro (p q : bool) (H1 : p) (H2 : q) : p ∧ q
+              := λ (c : bool) (H : p → q → c), H H1 H2,
+          and_elim_left  (p q : bool) (H : p ∧ q) : p
+              := H p (λ (H1 : p) (H2 : q), H1),
+          and_elim_right (p q : bool) (H : p ∧ q) : q
+              := H q (λ (H1 : p) (H2 : q), H2)
+       in and_intro
+
+check let bool : Type.{1}                          := Type.{0},
+          and  (p q : bool) : bool                 := ∀ c : bool, (p → q → c) → c,
+          infixl `∧` 25                           := and,
+          and_intro (p q : bool) (H1 : p) (H2 : q) : q ∧ p
+              := λ (c : bool) (H : p → q → c), H H1 H2,
+          and_elim_left  (p q : bool) (H : p ∧ q) : p
+              := H p (λ (H1 : p) (H2 : q), H1),
+          and_elim_right (p q : bool) (H : p ∧ q) : q
+              := H q (λ (H1 : p) (H2 : q), H2)
+       in and_intro
+

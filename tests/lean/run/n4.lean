3bb53810c5e286cf60c32e4d4dad5ee6f00d618b
Leonardo de Moura <leonardo@microsoft.com>
2014-06-18 09:48:47 -0700

    test(lean/run): add notation test
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/n4.lean
@@ -0,0 +1,20 @@
+definition [inline] Bool : Type.{1} := Type.{0}
+section
+  variable N : Type.{1}
+  variables a b c : N
+  variable and : Bool → Bool → Bool
+  infixr `∧` 35 := and
+  variable le : N → N → Bool
+  variable lt : N → N → Bool
+  precedence `≤`:50
+  precedence `<`:50
+  infixl ≤ := le
+  infixl < := lt
+  check a ≤ b
+  definition T : Bool := a ≤ b
+  check T
+end
+check T
+(*
+print(get_env():find("T"):value())
+*)

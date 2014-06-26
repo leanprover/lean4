a70f8dd98eea7b24bdc5920656053c5e1e92bf6e
Leonardo de Moura <leonardo@microsoft.com>
2014-06-26 10:59:43 -0700

    feat(kernel/inductive): mark parameters, type formers and indices as implicit parameters in the elimination rule
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/e14.lean
@@ -0,0 +1,34 @@
+inductive nat : Type :=
+| zero : nat
+| succ : nat → nat
+
+inductive list (A : Type) : Type :=
+| nil  : list A
+| cons : A → list A → list A
+
+check nil
+check nil.{1}
+check @nil.{1} nat
+check @nil nat
+
+check cons zero nil
+
+inductive vector (A : Type) : nat → Type :=
+| vnil  : vector A zero
+| vcons : forall {n : nat}, A → vector A n → vector A (succ n)
+
+check vcons zero vnil
+variable n : nat
+check vcons n vnil
+
+check vector_rec
+
+definition vector_to_list {A : Type} {n : nat} (v : vector A n) : list A
+:= vector_rec (@nil A) (fun (n : nat) (a : A) (v : vector A n) (l : list A), cons a l) v
+
+coercion vector_to_list
+
+variable f : forall {A : Type}, list A → nat
+
+check f (cons zero nil)
+check f (vcons zero vnil)

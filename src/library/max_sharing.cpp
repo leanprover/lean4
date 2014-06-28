58b6169e8bb5979ebdbfa8e8a26f9e0bf15d3a90
Leonardo de Moura <leonardo@microsoft.com>
2014-06-28 08:31:09 -0700

    fix(library/max_sharing): take binder annotations into account
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/library/max_sharing.cpp
+++ src/library/max_sharing.cpp
@@ -17,7 +17,7 @@ namespace lean {
    shared sub-expressions.
 */
 struct max_sharing_fn::imp {
-    typedef typename std::unordered_set<expr, expr_hash, std::equal_to<expr>> expr_cache;
+    typedef typename std::unordered_set<expr, expr_hash, is_bi_equal_proc> expr_cache;
 
     expr_cache m_cache;
 

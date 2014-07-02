f464775af6ee0baf008c4e3dbd2855bdd304808d
Leonardo de Moura <leonardo@microsoft.com>
2014-07-02 08:36:25 -0700

    fix(frontends/lean/parser): bug when parsing identifiers
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/frontends/lean/parser.cpp
+++ src/frontends/lean/parser.cpp
@@ -832,7 +832,7 @@ expr parser::id_to_expr(name const & id, pos_info const & p) {
         }
         r = save_pos(mk_choice(new_as.size(), new_as.data()), p);
     }
-    if (m_no_undef_id_error)
+    if (!r && m_no_undef_id_error)
         r = save_pos(mk_constant(get_namespace(m_env) + id, ls), p);
     if (!r)
         throw parser_error(sstream() << "unknown identifier '" << id << "'", p);

c1538bfc40f5475a847481fe4c587fc2f05e81f6
Leonardo de Moura <leonardo@microsoft.com>
2014-07-03 09:07:56 -0700

    test(tests/lean/run): add 'apply subst' test
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/tactic15.lean
@@ -0,0 +1,12 @@
+import standard
+using tactic
+
+variable A : Type.{1}
+variable f : A → A → A
+
+theorem tst {a b c : A} (H1 : a = b) (H2 : b = c) : f a (f b b) = f b (f c c)
+:= by apply (subst H1);
+      trace "trying again... ";
+      state;
+      apply (subst H2);
+      apply refl

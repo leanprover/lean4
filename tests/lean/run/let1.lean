92bb0468547ad460215e76adbec1b72bcbe355b6
Leonardo de Moura <leonardo@microsoft.com>
2014-07-10 15:56:15 +0100

    test(tests/lean/run): add nested let-expr test
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/let1.lean
@@ -0,0 +1,10 @@
+import standard
+
+check
+  let f x y := x ∧ y,
+      g x   := f x x,
+      a     := g true
+  in  λ (x : a),
+        let h x y := f x (g y),
+            b     := h
+        in b

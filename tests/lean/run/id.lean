06beff327fd71c8c6875860c76549d2ab142c6ec
Leonardo de Moura <leonardo@microsoft.com>
2014-07-10 14:43:55 +0100

    test(tests/lean/run): add more universe tests
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ tests/lean/run/id.lean
@@ -0,0 +1,12 @@
+import standard
+definition id {A : Type} (a : A) := a
+check id id
+set_option pp.universes true
+check id id
+check id Bool
+check id num.num
+check @id.{0}
+check @id.{1}
+check id num.zero
+check @eq
+check eq eq

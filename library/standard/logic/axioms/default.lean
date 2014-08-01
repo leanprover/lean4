4b05e70762dff560e4c5b06e75d8ee61a0ea8fab
Jeremy Avigad <avigad@cmu.edu>
2014-08-01 11:15:35 -0700

    feat(library/standard/logic/axioms): add import default

---
+++ library/standard/logic/axioms/default.lean
@@ -0,0 +1,8 @@
+----------------------------------------------------------------------------------------------------
+--- Copyright (c) 2014 Microsoft Corporation. All rights reserved.
+--- Released under Apache 2.0 license as described in the file LICENSE.
+--- Author: Jeremy Avigad
+----------------------------------------------------------------------------------------------------
+
+import logic.axioms.classical logic.axioms.funext logic.axioms.hilbert logic.axioms.piext
+import logic.axioms.prop_decidable

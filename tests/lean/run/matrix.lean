42a8fb5965b7ee83ddb183f2acfae6add05f65f3
Leonardo de Moura <leonardo@microsoft.com>
2014-08-25 09:27:19 -0700

    chore(tests/lean/run/matrix): simplify same_dim_irrel proof
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- tests/lean/run/matrix.lean
+++ tests/lean/run/matrix.lean
@@ -5,8 +5,7 @@ variable same_dim {A : Type} : matrix A → matrix A → Prop
 variable add {A : Type} (m1 m2 : matrix A) {H : same_dim m1 m2} : matrix A
 
 theorem same_dim_irrel {A : Type} {m1 m2 : matrix A} {H1 H2 : same_dim m1 m2} : @add A m1 m2 H1 = @add A m1 m2 H2 :=
-have eq : H1 = H2, from rfl,
-subst eq rfl
+rfl
 
 theorem same_dim_eq_args {A : Type} {m1 m2 m1' m2' : matrix A} (H1 : m1 = m1') (H2 : m2 = m2') (H : same_dim m1 m2) : same_dim m1' m2' :=
 subst H1 (subst H2 H)

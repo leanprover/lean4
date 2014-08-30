543fdf840e1c8a8d656f443a20af43c61cac9c12
Soonho Kong <soonhok@cs.cmu.edu>
2014-08-29 20:08:29 -0700

    fix(emacs/lean-mode): add comment-start "--"
    fix #113

--- src/emacs/lean-mode.el
+++ src/emacs/lean-mode.el
@@ -124,6 +124,7 @@ enabled and disabled respectively.")
   :syntax-table lean-syntax-table
   :abbrev-table lean-abbrev-table
   :group 'lean
+  (set (make-local-variable 'comment-start) "--")
   (set (make-local-variable 'comment-start-skip) "[-/]-[ \t]*")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'comment-end-skip) "[ \t]*\\(-/\\|\\s>\\)")

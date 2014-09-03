975841e53bc7d5bf3537da927416922dfd464970
Soonho Kong <soonhok@cs.cmu.edu>
2014-09-03 00:35:50 -0700

    feat(emacs): use lexical scope for lean-info and lean-server

--- src/emacs/lean-info.el
+++ src/emacs/lean-info.el
@@ -1,9 +1,9 @@
+;; -*- lexical-binding: t; -*-
 ;; Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 ;; Released under Apache 2.0 license as described in the file LICENSE.
 ;;
 ;; Author: Soonho Kong
 ;;
-
 (require 'cl-lib)
 (require 'dash)
 (require 'dash-functional)
--- src/emacs/lean-server.el
+++ src/emacs/lean-server.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 ;; Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 ;; Released under Apache 2.0 license as described in the file LICENSE.
 ;;

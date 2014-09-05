d2a4bb8a276348c5a42d7add7cd27e3eeba585a1
Floris van Doorn <fpv@andrew.cmu.edu>
2014-09-05 09:31:27 -0700

    feat(library/data/empty): add false.to_empty and false.rec_type theorems
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- library/data/empty.lean
+++ library/data/empty.lean
@@ -1,11 +1,11 @@
 -- Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 -- Released under Apache 2.0 license as described in the file LICENSE.
--- Author: Jeremy Avigad
-
--- TODO: add notation for negation (in the sense of homotopy type theory)
+-- Author: Jeremy Avigad, Floris van Doorn
 
 -- Empty type
 -- ----------
+
+import logic.core.cast
 
 inductive empty : Type
 
@@ -13,3 +13,11 @@ namespace empty
   theorem elim [protected] (A : Type) (H : empty) : A :=
   rec (λe, A) H
 end empty
+
+namespace false
+  theorem to_empty (H : false) : empty :=
+  cast (false_elim H) true
+
+  theorem rec_type (A : Type) (H : false) : A :=
+  empty.rec (λx,A) (to_empty H)
+end false

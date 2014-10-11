334a4c84d13a3b7f079d246fa2c6824198c64f53
Leonardo de Moura <leonardo@microsoft.com>
2014-10-11 16:17:40 -0700

    refactor(frontends/lean): do not expose unnecessary functions

--- src/frontends/lean/notation_cmd.h
+++ src/frontends/lean/notation_cmd.h
@@ -10,14 +10,6 @@ Author: Leonardo de Moura
 #include "frontends/lean/cmd_table.h"
 namespace lean {
 class parser;
-
-environment precedence_cmd(parser & p);
-environment notation_cmd_core(parser & p, bool overload);
-environment infixl_cmd_core(parser & p, bool overload);
-environment infixr_cmd_core(parser & p, bool overload);
-environment postfix_cmd_core(parser & p, bool overload);
-environment prefix_cmd_core(parser & p, bool overload);
-
 /** \brief Return true iff the current token is a notation declaration */
 bool curr_is_notation_decl(parser & p);
 /** \brief Parse a notation declaration, throws an error if the current token is not a "notation declaration". */

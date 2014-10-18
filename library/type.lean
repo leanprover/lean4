144150d47b41f02d8b63deeea903d963b4749bef
Leonardo de Moura <leonardo@microsoft.com>
2014-10-18 15:15:44 -0700

    fix(library/type): modify declaration order and make sure Type1, Type2 and Type3 notations have precedence over Type' during pretty printing

--- library/type.lean
+++ library/type.lean
@@ -3,8 +3,8 @@
 -- Authors: Leonardo de Moura
 
 notation `Prop`:max  := Type.{0}
+notation `Type'`:max := Type.{_+1}
+notation `Type₊`:max := Type.{_+1}
 notation `Type₁`:max := Type.{1}
 notation `Type₂`:max := Type.{2}
 notation `Type₃`:max := Type.{3}
-notation `Type'`:max := Type.{_+1}
-notation `Type₊`:max := Type.{_+1}

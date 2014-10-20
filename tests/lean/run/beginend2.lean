9b8f60b739e70ff40f5e18411f309fafc72d586e
Leonardo de Moura <leonardo@microsoft.com>
2014-10-20 15:58:11 -0700

    feat(frontends/lean/builtin_exprs): tolerate dangling ',' in begin-end block
    This is useful when debugging proofs.

---
+++ tests/lean/run/beginend2.lean
@@ -0,0 +1,13 @@
+import hott.path tools.tactic
+
+open path tactic
+open path (induction_on)
+
+definition concat_whisker2 {A} {x y z : A} (p p' : x ≈ y) (q q' : y ≈ z) (a : p ≈ p') (b : q ≈ q') :
+  (whiskerR a q) @ (whiskerL p' b) ≈ (whiskerL p b) @ (whiskerR a q') :=
+begin
+  apply (induction_on b),
+  apply (induction_on a),
+  apply ((concat_1p _)^),
+end
+exit

f18229945996f34d069292236f3206d75ecedbf8
Jakob von Raumer <javra@web.de>
2014-10-23 00:30:17 -0400

    fix(library/hott) fix funext.lean to match equivalence notation

--- library/hott/funext.lean
+++ library/hott/funext.lean
@@ -14,7 +14,7 @@ open path
 axiom funext {A : Type} {P : A → Type} (f g : Π x : A, P x) : IsEquiv (@apD10 A P f g)
 
 definition path_forall {A : Type} {P : A → Type} (f g : Π x : A, P x) : f ∼ g → f ≈ g :=
-(funext f g)⁻¹
+  IsEquiv.inv (funext f g)
 
 definition path_forall2 {A B : Type} {P : A → B → Type} (f g : Π x y, P x y) :
   (Πx y, f x y ≈ g x y) → f ≈ g :=

aafdbf57f0bff8dc7f9438a550d91141c98554a6
Leonardo de Moura <leonardo@microsoft.com>
2014-10-25 18:34:41 -0700

    fix(library/data/unit): missing file

---
+++ library/data/unit/thms.lean
@@ -0,0 +1,12 @@
+-- Copyright (c) 2014 Microsoft Corporation. All rights reserved.
+-- Released under Apache 2.0 license as described in the file LICENSE.
+-- Author: Leonardo de Moura
+import data.unit.decl logic.eq
+
+namespace unit
+  protected theorem equal (a b : unit) : a = b :=
+  rec_on a (rec_on b rfl)
+
+  theorem eq_star (a : unit) : a = star :=
+  equal a star
+end unit

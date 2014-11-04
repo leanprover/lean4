0ed046ed806379e27c0326ab5bf9023200e87149
Jakob von Raumer <javra@web.de>
2014-11-04 09:49:07 -0500

    fix(library/hott) fix funext.lean by making funext an instance

--- library/hott/funext.lean
+++ library/hott/funext.lean
@@ -13,8 +13,12 @@ open path
 
 axiom funext {A : Type} {P : A → Type} (f g : Π x : A, P x) : IsEquiv (@apD10 A P f g)
 
+theorem funext_instance [instance] {A : Type} {P : A → Type} (f g : Π x : A, P x) :
+    IsEquiv (@apD10 A P f g) :=
+  @funext A P f g
+
 definition path_forall {A : Type} {P : A → Type} (f g : Π x : A, P x) : f ∼ g → f ≈ g :=
-  @IsEquiv.inv _ _ _ (funext f g)
+  IsEquiv.inv !apD10
 
 definition path_forall2 {A B : Type} {P : A → B → Type} (f g : Π x y, P x y) :
   (Πx y, f x y ≈ g x y) → f ≈ g :=

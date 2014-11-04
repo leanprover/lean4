ea7470375fcf610a36114375e8e4e6023471fe84
Leonardo de Moura <leonardo@microsoft.com>
2014-11-03 18:44:36 -0800

    fix(tests/lean): to reflect changes in the standard library

--- tests/lean/run/print.lean
+++ tests/lean/run/print.lean
@@ -1,10 +1,10 @@
 import data.num logic data.prod data.nat data.int algebra.category.basic
-open num prod int nat category functor
+open num prod int nat category
 
 print instances inhabited
 print raw 3+2
 print options
-print coercions functor
+print coercions Category
 print "-----------"
 print coercions
 print "-----------"

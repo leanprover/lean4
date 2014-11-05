02abc5c2add58d21a755ff4964c75c2eab7b2f83
Jakob von Raumer <javra@web.de>
2014-11-04 20:16:51 -0500

    chore(library/hott) fixed the copyright in equiv_precomp.lean

--- library/hott/equiv_precomp.lean
+++ library/hott/equiv_precomp.lean
@@ -1,6 +1,6 @@
--- Copyright (c) 2014 Microsoft Corporation. All rights reserved.
+-- Copyright (c) 2014 Jakob von Raumer. All rights reserved.
 -- Released under Apache 2.0 license as described in the file LICENSE.
--- Author: Jeremy Avigad, Jakob von Raumer
+-- Author: Jakob von Raumer
 -- Ported from Coq HoTT
 import .equiv .funext
 open path function

126398ccb05190a20987750d7331c180b5550455
Leonardo de Moura <leonardo@microsoft.com>
2014-11-26 11:41:00 -0800

    feat(script): add script for collecting standard library compilation times

---
+++ script/lib_perf.sh
@@ -0,0 +1,13 @@
+# Script for collecting compilation times for the standard library
+# It assumes the lean binary is at the bin directory
+# It assumes the programs time and realpath are available
+TIME=/usr/bin/time
+REALPATH=realpath
+
+MY_PATH="`dirname \"$0\"`"
+LEAN=$MY_PATH/../bin/lean
+LIB=$MY_PATH/../library
+for f in `find $LIB -name '*.lean'`; do
+  rf=`$REALPATH $f`
+  $TIME --format="$rf %e" $LEAN $rf > /dev/null
+done

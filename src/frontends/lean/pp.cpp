8c50048d1be06c0fdbed2fbafeaa30b8fb5dd572
Leonardo de Moura <leonardo@microsoft.com>
2014-12-01 17:15:30 -0800

    chore(frontends/lean/pp): fix style

--- src/frontends/lean/pp.cpp
+++ src/frontends/lean/pp.cpp
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #include <algorithm>
+#include <limits>
 #include "util/flet.h"
 #include "kernel/replace_fn.h"
 #include "kernel/free_vars.h"

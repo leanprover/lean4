3d199d275de47aee0783bbd690f7a3f955159322
Leonardo de Moura <leonardo@microsoft.com>
2014-12-11 17:31:12 -0800

    fix(frontends/lean/token_table): increase binding power of decreasing operator

--- src/frontends/lean/token_table.cpp
+++ src/frontends/lean/token_table.cpp
@@ -11,7 +11,7 @@ Author: Leonardo de Moura
 
 namespace lean {
 static unsigned g_arrow_prec      = 25;
-static unsigned g_decreasing_prec = 25;
+static unsigned g_decreasing_prec = 100;
 static unsigned g_max_prec        = 1024;
 static unsigned g_plus_prec       = 65;
 static unsigned g_cup_prec        = 60;

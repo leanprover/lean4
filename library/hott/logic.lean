25a5e29b7c6e8c9554247c03c9c7258553d16859
Jakob von Raumer <javra@web.de>
2014-12-11 23:16:46 -0500

    chore(hott) delete old hott lib

--- library/hott/logic.lean
+++
@@ -1,9 +0,0 @@
-import data.empty .path
-
-open path
-inductive tdecidable [class] (A : Type) : Type :=
-inl :  A → tdecidable A,
-inr : ~A → tdecidable A
-
-structure decidable_paths [class] (A : Type) :=
-(elim : ∀(x y : A), tdecidable (x ≈ y))

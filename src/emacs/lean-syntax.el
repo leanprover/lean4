f3708715740ae09fdcc1cd8c36a1ddd18c7a1df6
Soonho Kong <soonhok@cs.cmu.edu>
2014-12-30 14:24:22 -0500

    fix(emacs/lean-syntax): highlight axioms
    fix #382

--- src/emacs/lean-syntax.el
+++ src/emacs/lean-syntax.el
@@ -106,7 +106,7 @@
      (,(rx symbol-start (or "λ" "→" "∃" "∀" ":=") symbol-end) . 'font-lock-constant-face )
      ;; universe/inductive/theorem... "names"
      (,(rx word-start
-           (group (or "inductive" "structure" "record" "theorem" "axiom" "lemma" "hypothesis" "definition" "constant"))
+           (group (or "inductive" "structure" "record" "theorem" "axiom" "axioms" "lemma" "hypothesis" "definition" "constant"))
            word-end
            (zero-or-more (or whitespace "(" "{" "["))
            (group (zero-or-more (not (any " \t\n\r{([")))))

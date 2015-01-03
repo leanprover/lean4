92b6c06a21a79af7609657ee3036b5f5ac0f2bba
Leonardo de Moura <leonardo@microsoft.com>
2015-01-02 22:22:20 -0800

    test(tests/lean/run): add basic pattern matching compilation test

---
+++ tests/lean/run/eq3.lean
@@ -0,0 +1,8 @@
+import data.vector
+open nat vector
+
+definition swap {A : Type} : Π {n}, vector A (succ (succ n)) → vector A (succ (succ n)),
+swap (a :: b :: vs) := b :: a :: vs
+
+example (n : nat) (a b : num) (v : vector num n) : swap (a :: b :: v) = b :: a :: v :=
+rfl

7fca862fc3957e4fede404c7fe2c9b90950248d9
Leonardo de Moura <leonardo@microsoft.com>
2015-01-04 17:47:18 -0800

    test(tests/lean/run): define fibonacci using recursive equations

---
+++ tests/lean/run/eq5.lean
@@ -0,0 +1,18 @@
+open nat
+
+definition fib : nat → nat,
+fib 0     := 1,
+fib 1     := 1,
+fib (x+2) := fib x + fib (x+1)
+
+theorem fib0 : fib 0 = 1 :=
+rfl
+
+theorem fib1 : fib 1 = 1 :=
+rfl
+
+theorem fib_succ_succ (a : nat) : fib (a+2) = fib a + fib (a+1) :=
+rfl
+
+example : fib 8 = 34 :=
+rfl

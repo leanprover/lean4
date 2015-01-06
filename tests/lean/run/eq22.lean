6451cad38d89760d644b08d9c5c62c5e0f64c6d8
Leonardo de Moura <leonardo@microsoft.com>
2015-01-05 19:50:07 -0800

    test(tests/lean/run): define list head using recursive equations

---
+++ tests/lean/run/eq22.lean
@@ -0,0 +1,9 @@
+import data.list
+open list
+
+definition head {A : Type} : Π (l : list A), l ≠ nil → A,
+head nil h      := absurd rfl h,
+head (a :: l) _ := a
+
+theorem head_cons {A : Type} (a : A) (l : list A) (h : a :: l ≠ nil) : head (a :: l) h = a :=
+rfl

a24a7f7fa1603ee8b69a590d729f1cf6e8326677
Leonardo de Moura <leonardo@microsoft.com>
2015-01-05 16:52:30 -0800

    test(tests/lean/run) define vector last and unzip functions using recursive equations

---
+++ tests/lean/run/eq19.lean
@@ -0,0 +1,15 @@
+import data.vector data.prod
+open nat vector prod
+
+variables {A B : Type}
+
+definition unzip : Π {n}, vector (A × B) n → vector A n × vector B n,
+unzip nil           := (nil, nil),
+unzip ((a, b) :: t) := (a :: pr₁ (unzip t), b :: pr₂ (unzip t))
+
+theorem unzip_nil : unzip nil = (@nil A, @nil B) :=
+rfl
+
+theorem unzip_cons {n : nat} (a : A) (b : B) (t : vector (A × B) n) :
+  unzip ((a, b) :: t) = (a :: pr₁ (unzip t), b :: pr₂ (unzip t)) :=
+rfl

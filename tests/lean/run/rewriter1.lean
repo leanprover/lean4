0c8eaa38c8c39a512e3b0de25b4682f4f32342aa
Leonardo de Moura <leonardo@microsoft.com>
2015-02-04 15:30:18 -0800

    test(tests/lean/run): add basic rewriter test

---
+++ tests/lean/run/rewriter1.lean
@@ -0,0 +1,9 @@
+import data.nat
+open algebra
+
+theorem test {A : Type} [s : comm_ring A] (a b c : A) : a + b + c = a + c + b :=
+begin
+  rewrite add.assoc [b + _]add.comm -add.assoc,
+end
+
+print definition test

1b73764ad32bada0f2c02acd69a8f4195c67d7f0
Leonardo de Moura <leonardo@microsoft.com>
2015-02-05 10:54:00 -0800

    fix(tests/lean/run/elim2): adjust test to reflect recent changes

--- tests/lean/run/elim2.lean
+++ tests/lean/run/elim2.lean
@@ -4,5 +4,5 @@ constant p : num → num → num → Prop
 axiom H1 : ∃ x y z, p x y z
 axiom H2 : ∀ {x y z : num}, p x y z → p x x x
 theorem tst : ∃ x, p x x x
-:= obtain a b c H [visible], from H1,
+:= obtain a b c H, from H1,
      by (apply exists.intro; apply H2; eassumption)

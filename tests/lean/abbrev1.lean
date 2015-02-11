f9832fb89f67d3e3fd949c3c2fd329995b937b63
Leonardo de Moura <leonardo@microsoft.com>
2015-02-10 18:28:48 -0800

    test(tests/lean/abbrev1): add test for abbreviation command

---
+++ tests/lean/abbrev1.lean
@@ -0,0 +1,19 @@
+open nat
+
+abbreviation foo  : Π (A : Type), nat := λ a, 2 + 3
+
+definition tst := foo nat
+
+set_option pp.abbreviations false
+
+print definition tst
+
+set_option pp.abbreviations true
+
+print definition tst
+
+abbreviation id [parsing-only] {A : Type} (a : A) := a
+
+definition tst1 := id 10
+
+print definition tst1

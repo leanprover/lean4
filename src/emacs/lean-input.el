5c1d7458b5efec619ea63ec11799082e3c7eb5f2
Soonho Kong <soonhok@cs.cmu.edu>
2015-03-26 14:41:26 -0400

    fix(emacs/lean-input): fix input-method.js format
    [skip ci]

--- src/emacs/lean-input.el
+++ src/emacs/lean-input.el
@@ -1112,13 +1112,13 @@ leanprover.github.io/js/input-method.js"
       (let* ((input (substring (car it) 1))
              (outputs (cdr it)))
 
-        (insert (format "{%s:\"" (prin1-to-string input)))
+        (insert (format "%s:\"" (prin1-to-string input)))
 
         (cond ((vectorp outputs)
                (insert (elt outputs 0)))
               (t (insert-char outputs)))
 
-        (insert (format "\"},\n" input))))
+        (insert (format "\",\n" input))))
     (insert "};")))
 
 (defun lean-input-export-translations-to-stdout ()

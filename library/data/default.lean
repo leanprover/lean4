981bf93ce00d24a0784e124df80463cd38b24b63
Jeremy Avigad <avigad@cmu.edu>
2015-05-17 12:19:47 +1000

    feat(library/data/default.lean): add rat and vector

--- library/data/default.lean
+++ library/data/default.lean
@@ -1,9 +1,7 @@
 /-
 Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-
-Module: data.default
 Author: Jeremy Avigad
 -/
-import .empty .unit .bool .num .string .nat .int .fintype
-import .prod .sum .sigma .option .subtype .quotient .list .finset .set
+import .empty .unit .bool .num .string .nat .int .rat .fintype
+import .prod .sum .sigma .option .subtype .quotient .list .vector .finset .set

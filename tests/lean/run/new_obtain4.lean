8aa634378e5e32517153d11816762786e1fef463
Leonardo de Moura <leonardo@microsoft.com>
2015-06-10 17:00:47 -0700

    fix(tests/lean): adjust tests to reflect changes in the standard library

--- tests/lean/run/new_obtain4.lean
+++ tests/lean/run/new_obtain4.lean
@@ -8,7 +8,7 @@ setext (take z,
   iff.intro
     (assume Hz,
       obtain x Hx₁ Hx₂, from Hz,
-      by repeat (apply in_image | assumption | reflexivity))
+      by repeat (apply mem_image | assumption | reflexivity))
     (assume Hz,
       obtain y [x Hz₁ Hz₂] Hy₂, from Hz,
-      by repeat (apply in_image | assumption | esimp [compose] | rewrite Hz₂)))
+      by repeat (apply mem_image | assumption | esimp [compose] | rewrite Hz₂)))

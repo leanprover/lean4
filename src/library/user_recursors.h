88844f62611f5f47f9cf9f6da3cf166874144ffc
Leonardo de Moura <leonardo@microsoft.com>
2015-06-28 12:53:48 -0700

    chore(src/library/user_recursors): add missing include

--- src/library/user_recursors.h
+++ src/library/user_recursors.h
@@ -5,7 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
-
+#include "kernel/environment.h"
 namespace lean {
 /** \brief Information for a user defined recursor */
 class recursor_info {

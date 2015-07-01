14f7e3de9416d1d5ab523cfedac875c5614ddefe
Leonardo de Moura <leonardo@microsoft.com>
2015-06-30 18:15:13 -0700

    fix(library/algebra): missing file

---
+++ library/algebra/priority.lean
@@ -0,0 +1,8 @@
+/-
+Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Leonardo de Moura
+-/
+namespace algebra
+protected definition prio := num.sub std.priority.default 100
+end algebra

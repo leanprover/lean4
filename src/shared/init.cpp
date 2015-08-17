4d3ed6ca43e9f9dae8d671077fb45bf6de33b7de
Leonardo de Moura <leonardo@microsoft.com>
2015-08-17 14:10:26 -0700

    feat(init/init): automatically initialize lean shared library

---
+++ src/shared/init.cpp
@@ -0,0 +1,11 @@
+/*
+Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Leonardo de Moura
+*/
+#include "init/init.h"
+namespace lean {
+// automatic initialization for the shared library
+initializer g_init;
+}

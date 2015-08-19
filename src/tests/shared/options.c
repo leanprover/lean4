c32a95bb132067d5e3b8bb42f4e0cad2bf8741cb
Leonardo de Moura <leonardo@microsoft.com>
2015-08-19 08:13:58 -0700

    chore(tests/shared/options): fix compilation warning

--- src/tests/shared/options.c
+++ src/tests/shared/options.c
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "api/lean.h"

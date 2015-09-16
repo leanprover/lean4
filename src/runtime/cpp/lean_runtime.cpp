46f7123cc8b4ff3c3cc67c719b3ce71bdbd35ef6
Leonardo de Moura <leonardo@microsoft.com>
2015-09-16 07:34:08 -0700

    fix(runtime/cpp): typo

--- src/runtime/cpp/lean_runtime.cpp
+++ src/runtime/cpp/lean_runtime.cpp
@@ -36,7 +36,7 @@ public:
             m_free_list[n] = *(reinterpret_cast<void **>(r));
             return r;
         } else {
-            return malloc(sizeof(obj) + sizeof(void*)*n); // NOLINT
+            return malloc(sizeof(obj_cell) + sizeof(void*)*n); // NOLINT
         }
     }
 

06e35b486393a72b902dcaf32ba9f4398a603b6c
Leonardo de Moura <leonardo@microsoft.com>
2015-10-13 16:33:52 -0700

    fix(library/algebra/numeral): remove redundant definitions

--- library/algebra/numeral.lean
+++ library/algebra/numeral.lean
@@ -3,21 +3,6 @@ open algebra
 
 variable {A : Type}
 
-definition zero [s : has_zero A] : A :=
-has_zero.zero A
-
-definition one [s : has_one A] : A :=
-has_one.one A
-
-definition add [s : has_add A] : A → A → A :=
-has_add.add
-
-definition bit0 [s : has_add A] (a : A) : A :=
-add a a
-
-definition bit1 [s₁ : has_add A] [s₂ : has_one A] (a : A) : A :=
-add (add a a) one
-
 -- variables [s : ring A]
 -- set_option pp.all true
 -- check bit1 (bit0 (one : A))

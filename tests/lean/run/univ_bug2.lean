54c5d2ca994ed8fd29b30009849038ad7012cbb6
Leonardo de Moura <leonardo@microsoft.com>
2015-10-27 15:54:47 -0700

    fix(tests/lean/run/univ_bug2): old test

--- tests/lean/run/univ_bug2.lean
+++ tests/lean/run/univ_bug2.lean
@@ -16,17 +16,8 @@ namespace simplifies_to
 theorem get_eq {T : Type} {t1 t2 : T} (C : simplifies_to t1 t2) : t1 = t2 :=
 simplifies_to.rec (λx, x) C
 
-theorem infer_eq {T : Type} (t1 t2 : T) [C : simplifies_to t1 t2] : t1 = t2 :=
-simplifies_to.rec (λx, x) C
-
 theorem simp_app [instance] (S : Type) (T : Type) (f1 f2 : S → T) (s1 s2 : S)
    [C1 : simplifies_to f1 f2] [C2 : simplifies_to s1 s2] : simplifies_to (f1 s1) (f2 s2) :=
 mk (congr (get_eq C1) (get_eq C2))
-
-theorem test1 (S : Type) (T : Type) (f1 f2 : S → T) (s1 s2 : S) (Hf : f1 = f2) (Hs : s1 = s2) :
-  f1 s1 = f2 s2 :=
-have Rs [visible] : simplifies_to f1 f2, from mk Hf,
-have Cs [visible] : simplifies_to s1 s2, from mk Hs,
-infer_eq _ _
 
 end simplifies_to

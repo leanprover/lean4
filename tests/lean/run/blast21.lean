093a212ed4d1a93264a9e2fb1cfe5c451c50e699
Leonardo de Moura <leonardo@microsoft.com>
2015-11-18 19:20:32 -0800

    test(tests/lean/run/blast21): another blast test

---
+++ tests/lean/run/blast21.lean
@@ -0,0 +1,19 @@
+namespace ex
+
+constant typ : Type₁
+
+constant subtype : typ → typ → Prop
+
+constant subtype_refl : ∀ T, subtype T T
+
+constant subtype_trans : ∀ S T U, subtype S T → subtype T U → subtype S U
+
+attribute subtype_refl subtype_trans [backward]
+
+lemma L1 : ∀ T1 T2 T3 T4 T5 T6, subtype T1 T2 → subtype T2 T3 → subtype T3 T4 → subtype T4 T5 → subtype T5 T6 → subtype T1 T6 :=
+by blast
+
+reveal L1
+print L1
+
+end ex

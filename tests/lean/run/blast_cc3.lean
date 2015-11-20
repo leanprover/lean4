2627eade8b171d1e6316d0ce0ad0a680b8a10b2b
Leonardo de Moura <leonardo@microsoft.com>
2015-11-20 13:53:36 -0800

    test(tests/lean/run/blast_cc3): add test for congruence_closure with dependent types

---
+++ tests/lean/run/blast_cc3.lean
@@ -0,0 +1,15 @@
+open nat
+set_option blast.subst false
+constant f (a b : nat) : a > b → nat
+constant g : nat → nat
+
+definition tst
+        (a₁ a₂ b₁ b₂ c d : nat)
+        (H₁ : a₁ > b₁)
+        (H₂ : a₂ > b₂) :
+        a₁ = c → a₂ = c →
+        b₁ = d → d  = b₂ →
+        g (g (f a₁ b₁ H₁)) = g (g (f a₂ b₂ H₂)) :=
+by blast
+
+print tst

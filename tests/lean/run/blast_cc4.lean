eb3d73873ad84d84e20cce1717ef8eeb134b5c5b
Leonardo de Moura <leonardo@microsoft.com>
2015-11-20 14:01:15 -0800

    test(tests/lean/run/blast_cc4): congruence_closure and type classes

---
+++ tests/lean/run/blast_cc4.lean
@@ -0,0 +1,11 @@
+open nat
+set_option blast.subst false
+
+definition tst
+        (a₁ a₂ b₁ b₂ c d : nat) :
+        a₁ = c → a₂ = c →
+        b₁ = d → d  = b₂ →
+        a₁ + b₁ + a₁ = a₂ + b₂ + c :=
+by blast
+
+print tst

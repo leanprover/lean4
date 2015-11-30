1ab39a518fc0c928f9e089ba14aa3ffcef89518e
Leonardo de Moura <leonardo@microsoft.com>
2015-11-30 11:25:45 -0700

    chore(tests/lean/run/blast_ematch5): disable unnecessary options

--- tests/lean/run/blast_ematch5.lean
+++ tests/lean/run/blast_ematch5.lean
@@ -3,7 +3,13 @@ constant subt : nat → nat → Prop
 lemma subt_trans [forward] {a b c : nat} : subt a b → subt b c → subt a c :=
 sorry
 
-set_option blast.ematch true
+set_option blast.init_depth 20
+set_option blast.inc_depth  100
+set_option blast.ematch   true
+set_option blast.simp     false
+-- TODO(Leo): check if unit propagation is still buggy
+-- If I remove the following option, blast fails
+set_option blast.backward true -- false
 
 example (a b c d : nat) : subt a b → subt b c → subt c d → subt a d :=
 by blast

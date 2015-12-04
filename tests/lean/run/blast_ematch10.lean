5d38a5a5cdcdbdd8a9e3e5df98c04dacac6ef789
Leonardo de Moura <leonardo@microsoft.com>
2015-12-04 14:03:51 -0800

    test(tests/lean/run/blast_ematch10): make sure the test is for the `cc` module

--- tests/lean/run/blast_ematch10.lean
+++ tests/lean/run/blast_ematch10.lean
@@ -1,4 +1,9 @@
 attribute iff [reducible]
+set_option blast.subst false
+set_option blast.simp  false
 
-example (p : nat → Prop) (a b c : nat) : p a → a = b → p b :=
+definition lemma1 (p : nat → Prop) (a b c : nat) : p a → a = b → p b :=
 by blast
+
+set_option pp.beta true
+print lemma1

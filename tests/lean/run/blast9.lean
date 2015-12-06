ae8efb684eb91df84463e61e9f28dc5be341234d
Leonardo de Moura <leonardo@microsoft.com>
2015-12-05 19:26:37 -0800

    fix(tests/lean/run/blast9): missing option

--- tests/lean/run/blast9.lean
+++ tests/lean/run/blast9.lean
@@ -5,6 +5,8 @@ set_option blast.strategy "preprocess"
 example (p : Prop) (a b c : nat) : [a, b, c] = [] → p :=
 by blast
 
+set_option blast.strategy "simple"
+
 lemma l1 (a b c d e f : nat) : [a, b, c] = [d, e, f] → a = d ∧ b = e ∧ c = f :=
 by blast
 

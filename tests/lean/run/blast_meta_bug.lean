af7ffcf5756f8b22867bdd1714a452464a4a2e7b
Leonardo de Moura <leonardo@microsoft.com>
2015-12-07 12:23:01 -0800

    chore(tests/lean/run/blast_meta_bug): use "ematch" strategy in the test

--- tests/lean/run/blast_meta_bug.lean
+++ tests/lean/run/blast_meta_bug.lean
@@ -1,7 +1,7 @@
 constants {A : Type.{1}} (P : A → Prop) (Q : A → Prop)
 definition H : ∀ a, (: P a :) → Exists Q := sorry
 
-set_option blast.ematch true
+set_option blast.strategy "ematch"
 
 example (a : A) : P a → Exists Q :=
 by blast

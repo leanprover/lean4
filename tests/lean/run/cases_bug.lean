912c5254def32faa550079f03e2574e8fcb6faa8
Leonardo de Moura <leonardo@microsoft.com>
2016-01-13 16:37:33 -0800

    fix(tests/lean/run/cases_bug): adjust test to changes in the std lib

--- tests/lean/run/cases_bug.lean
+++ tests/lean/run/cases_bug.lean
@@ -1,4 +1,4 @@
 import logic.cast
 
-theorem cast_heq : ∀ {A B : Type} (H : A = B) (a : A), cast H a == a
+theorem cast_heq₂ : ∀ {A B : Type} (H : A = B) (a : A), cast H a == a
 | A A (eq.refl A) a := heq_of_eq !cast_eq

37fdac4d652bc84c7903fe9a651821d9427cc32d
Leonardo de Moura <leonardo@microsoft.com>
2016-01-13 22:10:15 -0800

    test(tests/lean/run/blast_cc_heq9): add new test

--- tests/lean/run/blast_cc_heq9.lean
+++ tests/lean/run/blast_cc_heq9.lean
@@ -1,8 +1,9 @@
+open subtype
+
 example (f g : Π {A : Type₁}, A → A → A) (h : nat → nat) (a b : nat) :
         h = f a → h b = f a b :=
 by blast
 
-
-example (f g : Π {A : Type₁} (a b : A), A) (h : nat → nat) (a b : nat) :
-        h = f a → h b = f a b :=
+example (f g : Π {A : Type₁} (a b : A), {x : A | x ≠ b}) (h : Π (b : nat), {x : nat | x ≠ b}) (a b₁ b₂ : nat) :
+        h = f a → b₁ = b₂ → h b₁ == f a b₂ :=
 by blast

1048812e2eb262b711d576eda8102486bd2526c2
Leonardo de Moura <leonardo@microsoft.com>
2016-01-13 17:27:25 -0800

    chore(frontends/lean/notation_cmd): cleanup weird coding pattern

--- src/frontends/lean/notation_cmd.cpp
+++ src/frontends/lean/notation_cmd.cpp
@@ -252,9 +252,9 @@ static auto parse_mixfix_notation(parser & p, mixfix_kind k, bool overload, nota
         case mixfix_kind::infixl:
 #if defined(__GNUC__) && !defined(__CLANG__)
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
             return mk_pair(notation_entry(false, to_list(transition(tks, mk_expr_action(*prec), pp_tk)),
                                           mk_app(f, Var(1), Var(0)), overload, priority, grp, parse_only), new_token);
-#endif
         case mixfix_kind::infixr:
             return mk_pair(notation_entry(false, to_list(transition(tks, mk_expr_action(*prec), pp_tk)),
                                           mk_app(f, Var(1), Var(0)), overload, priority, grp, parse_only), new_token);

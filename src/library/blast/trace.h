2868ec9c4340df61b5e6cb38b55e52f090bc26b8
Daniel Selsam <dselsam@users.noreply.github.com>
2016-01-25 13:47:59 -0800

    fix(library/blast/trace): missing pragma

--- src/library/blast/trace.h
+++ src/library/blast/trace.h
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#pragma once
 #include "library/trace.h"
 #include "library/io_state_stream.h"
 #include "library/blast/action_result.h"

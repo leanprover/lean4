fe4fafd95daa9c8184c9c065c8deae42e5da6513
Leonardo de Moura <leonardo@microsoft.com>
2016-06-03 13:07:06 -0700

    chore(library/vm): remove unnecessary includes

--- src/library/vm/vm_nat.cpp
+++ src/library/vm/vm_nat.cpp
@@ -5,7 +5,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #include <iostream>
-#include "library/constants.h"
 #include "library/vm/vm.h"
 #include "library/vm/vm_string.h"
 

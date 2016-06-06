0280281b1cf66df92f815db27eafb766224ec2ba
Leonardo de Moura <leonardo@microsoft.com>
2016-06-06 14:09:39 -0700

    chore(library): remove old tactic definition

--- library/init/default.lean
+++ library/init/default.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 prelude
-import init.datatypes init.reserved_notation init.tactic init.logic
+import init.datatypes init.reserved_notation init.logic
 import init.relation init.wf init.nat init.wf_k init.prod
 import init.bool init.num init.sigma init.measurable init.setoid init.quot
 import init.funext init.function init.subtype init.classical init.simplifier

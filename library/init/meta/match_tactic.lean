d487a59c231732280ff015de1f0dad246aa56c9f
Leonardo de Moura <leonardo@microsoft.com>
2016-06-27 14:49:32 +0100

    chore(library/init/meta/match_tactic): document match_pattern tactic

--- library/init/meta/match_tactic.lean
+++ library/init/meta/match_tactic.lean
@@ -8,9 +8,27 @@ import init.meta.tactic init.function
 
 namespace tactic
 structure pattern :=
-(target : expr) (output : list expr) (nuvars : nat) (nmvars : nat)
+/- Term to match. -/
+(target : expr)
+/- Set of terms that is instantiated for each successful match. -/
+(output : list expr)
+/- Number of (temporary) universe meta-variables in this pattern. -/
+(nuvars : nat)
+/- Number of (temporary) meta-variables in this pattern. -/
+(nmvars : nat)
 
+/- (mk_pattern ls es t o) creates a new pattern with (length ls) universe meta-variables and (length es) meta-variables.
+   In the produced pattern p, we have that
+   - (pattern.target p) is the term t where the universes ls and expressions es have been replaced with temporary meta-variables.
+   - (pattern.output p) is the list o where the universes ls and expressions es have been replaced with temporary meta-variables.
+   - (pattern.nuvars p) = length ls
+   - (pattern.nmvars p) = length es
+
+   The tactic fails if o and the types of es do not contain all universes ls and expressions es. -/
 meta_constant mk_pattern : list level → list expr → expr → list expr → tactic pattern
+/- (mk_pattern_core m p e) matches (pattern.target p) and e using transparency m.
+   If the matching is successful, then return the instantiation of (pattern.output p).
+   The tactic fails if not all (temporary) meta-variables are assigned. -/
 meta_constant match_pattern_core : transparency → pattern → expr → tactic (list expr)
 
 meta_definition match_pattern : pattern → expr → tactic (list expr) :=

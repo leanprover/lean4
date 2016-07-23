b5cddede6bc880be826ad1f266d65f8a97d26646
Leonardo de Moura <leonardo@microsoft.com>
2016-07-23 12:30:41 -0700

    feat(library/init/meta): add mk_measurable_instance tactic

--- library/init/measurable.lean
+++ library/init/measurable.lean
@@ -6,8 +6,8 @@ Authors: Leonardo de Moura
 Types with a nat-valued complexity measure.
 -/
 prelude
-import init.nat
-open nat
+import init.nat init.meta.mk_measurable_instance
+open nat tactic
 
 inductive measurable [class] (A : Type) :=
 mk : (A → nat) → measurable A
@@ -15,26 +15,41 @@ mk : (A → nat) → measurable A
 definition size_of {A : Type} [s : measurable A] : A → nat :=
 measurable.rec_on s (λ f, f)
 
-definition nat.measurable [instance] : measurable nat :=
+definition prop_measurable [instance] (p : Prop) : measurable p :=
+measurable.mk (λ t, zero)
+
+definition nat_measurable [instance] : measurable nat :=
 measurable.mk (λ a, a)
 
-definition option.measurable [instance] (A : Type) [measurable A] : measurable (option A) :=
-measurable.mk (λ a, option.cases_on a zero size_of)
+definition Type_measurable [instance] : measurable Type :=
+measurable.mk (λ t, zero)
 
-definition prod.measurable [instance] (A B : Type) [measurable A] [measurable B] : measurable (prod A B) :=
-measurable.mk (λ p, prod.cases_on p (λ a b, size_of a + size_of b))
-
-definition sum.measurable [instance] (A B : Type) [measurable A] [measurable B] : measurable (sum A B) :=
-measurable.mk (λ s, sum.cases_on s (λ a, size_of a) (λ b, size_of b))
-
-definition bool.measurable [instance] : measurable bool :=
-measurable.mk (λb, zero)
-
-definition Prop.measurable [instance] : measurable Prop :=
-measurable.mk (λp, zero)
-
-definition unit.measurable [instance] : measurable unit :=
-measurable.mk (λu, zero)
-
-definition fn.measurable [instance] (A : Type) (B : A → Type) : measurable (Π x, B x) :=
+definition fn_measurable [instance] (A : Type) (B : A → Type) : measurable (Π x, B x) :=
 measurable.mk (λf, zero)
+
+definition option.measurable [instance] (A : Type) [measurable A] : measurable (option A) :=
+by mk_measurable_instance
+
+definition prod_measurable [instance] (A B : Type) [measurable A] [measurable B] : measurable (prod A B) :=
+by mk_measurable_instance
+
+definition sum_measurable [instance] (A B : Type) [measurable A] [measurable B] : measurable (sum A B) :=
+by mk_measurable_instance
+
+definition list_measurable [instance] (A : Type) [measurable A] : measurable (list A) :=
+by mk_measurable_instance
+
+definition unit_measurable [instance] : measurable unit :=
+by mk_measurable_instance
+
+definition sigma_measurable [instance] (A : Type) (B : A → Type) [measurable A] [∀ x, measurable (B x)] : measurable (Σ x, B x) :=
+by mk_measurable_instance
+
+definition subtype_mesurable [instance] (A : Type) (P : A → Prop) [measurable A] : measurable {x \ P x} :=
+by mk_measurable_instance
+
+definition pos_num_measurable [instance] : measurable pos_num :=
+by mk_measurable_instance
+
+definition num_measurable [instance] : measurable num :=
+by mk_measurable_instance

a05291ed757b7d963f16d8b4837c57374ea510ea
Leonardo de Moura <leonardo@microsoft.com>
2016-07-26 17:59:22 -0700

    chore(library/io_state): fix style

--- src/library/io_state.cpp
+++ src/library/io_state.cpp
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <string>
 #include "kernel/kernel_exception.h"
 #include "library/print.h"
 #include "library/io_state.h"

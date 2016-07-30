68f73851d9740304ae0419eb9f9395114601a8d9
Leonardo de Moura <leonardo@microsoft.com>
2016-07-30 12:02:14 -0700

    test(tests/lean/elab4b): test error messages when pp.full_names is false

---
+++ tests/lean/elab4b.lean
@@ -0,0 +1,13 @@
+definition foo.f {A : Type} {B : Type} (a : A) (b : B) : A := a
+
+definition boo.f (a : nat) (b : nat) (c : nat) := a + b + c
+
+definition bla.f (a b c d : bool) := a
+
+open boo foo bla
+
+#elab f 0 1 2 3
+
+#elab f 0 1
+
+#elab f tt ff

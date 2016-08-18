0304d1ceabaf89223a9741108b783ba78683dd75
Jeremy Avigad <avigad@cmu.edu>
2016-08-18 10:26:10 -0700

    feat(library/init/monad_combinators): add monad combinators

--- library/init/default.lean
+++ library/init/default.lean
@@ -9,6 +9,7 @@ import init.relation init.nat init.prod init.sum init.combinator
 import init.bool init.unit init.num init.sigma init.setoid init.quot
 import init.funext init.function init.subtype init.classical
 import init.monad init.option init.state init.fin init.list init.char init.string init.to_string
+import init.monad_combinators
 import init.timeit init.trace init.unsigned init.ordering init.list_classes init.coe
 import init.wf init.nat_div init.meta init.instances
 import init.wf_k init.sigma_lex init.sizeof

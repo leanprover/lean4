6a880d51a47710ddc8e708b4fc5b9ca731af3de4
Leonardo de Moura <leonardo@microsoft.com>
2016-08-18 10:51:40 -0700

    feat(library/init): define `nat.mod` in the init folder too

--- library/init/nat_div.lean
+++ library/init/nat_div.lean
@@ -7,18 +7,32 @@ prelude
 import init.wf init.nat
 
 namespace nat
-  private definition div_rec_lemma {x y : nat} : 0 < y ∧ y ≤ x → x - y < x :=
-  and.rec (λ ypos ylex, sub_lt (nat.lt_of_lt_of_le ypos ylex) ypos)
 
-  private definition div.F (x : nat) (f : Π x₁, x₁ < x → nat → nat) (y : nat) : nat :=
-  if H : 0 < y ∧ y ≤ x then f (x - y) (div_rec_lemma H) y + 1 else zero
+private definition div_rec_lemma {x y : nat} : 0 < y ∧ y ≤ x → x - y < x :=
+and.rec (λ ypos ylex, sub_lt (nat.lt_of_lt_of_le ypos ylex) ypos)
 
-  protected definition div := well_founded.fix lt_wf div.F
+private definition div.F (x : nat) (f : Π x₁, x₁ < x → nat → nat) (y : nat) : nat :=
+if H : 0 < y ∧ y ≤ x then f (x - y) (div_rec_lemma H) y + 1 else zero
 
-  attribute[instance]
-  definition nat_has_divide : has_div nat :=
-  has_div.mk nat.div
+protected definition div := well_founded.fix lt_wf div.F
 
-  theorem div_def (x y : nat) : div x y = if 0 < y ∧ y ≤ x then div (x - y) y + 1 else 0 :=
-  congr_fun (well_founded.fix_eq lt_wf div.F x) y
+attribute[instance]
+definition nat_has_divide : has_div nat :=
+has_div.mk nat.div
+
+theorem div_def (x y : nat) : div x y = if 0 < y ∧ y ≤ x then div (x - y) y + 1 else 0 :=
+congr_fun (well_founded.fix_eq lt_wf div.F x) y
+
+private definition mod.F (x : nat) (f : Π x₁, x₁ < x → nat → nat) (y : nat) : nat :=
+if H : 0 < y ∧ y ≤ x then f (x - y) (div_rec_lemma H) y else x
+
+protected definition mod := well_founded.fix lt_wf mod.F
+
+attribute [instance]
+definition nat_has_mod : has_mod nat :=
+has_mod.mk nat.mod
+
+theorem mod_def (x y : nat) : mod x y = if 0 < y ∧ y ≤ x then mod (x - y) y else x :=
+congr_fun (well_founded.fix_eq lt_wf mod.F x) y
+
 end nat

9a2c95f35a287755467a76b60d691dc279259c58
Leonardo de Moura <leonardo@microsoft.com>
2016-09-01 14:50:03 -0700

    test(tests/lean): add test for unset_attribute tactic

---
+++ tests/lean/set_attr1.lean
@@ -0,0 +1,17 @@
+set_option new_elaborator true
+open tactic
+
+constant f : nat → nat
+constant foo : ∀ n, f n = n + 1
+constant add_zero : ∀ n, n + 0 = n
+
+definition ex1 (n : nat) : f n + 0 = n + 1 :=
+by do
+  set_basic_attribute `simp `foo,
+  set_basic_attribute `simp `add_zero,
+  simp
+
+definition ex2 (n : nat) : f n + 0 = n + 1 :=
+by do
+  unset_attribute `simp `foo,
+  simp -- should fail since we remove [simp] attribute from `foo`

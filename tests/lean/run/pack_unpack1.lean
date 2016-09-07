57f02dd768f20d6305c8940ec96dfee7007504ec
Leonardo de Moura <leonardo@microsoft.com>
2016-09-07 16:05:05 -0700

    test(tests/lean/run/pack_unpack1): add dependent function example

--- tests/lean/run/pack_unpack1.lean
+++ tests/lean/run/pack_unpack1.lean
@@ -31,6 +31,20 @@ lemma unpack_pack {A : Type} : ∀ (l : list (tree A)), unpack (pack l) = l
   show a :: unpack (pack l) = a :: l, from
   congr_arg (λ x, a :: x) (unpack_pack l)
 
+attribute [inverse]
+lemma pack_unpack {A : Type} : ∀ t : tree_core A tt, pack (unpack t) = t :=
+λ t,
+  @tree_core.rec_on
+    A
+    (λ b, bool.cases_on b (λ t, true) (λ t, pack (unpack t) = t))
+    tt t
+    (λ a, trivial)
+    (λ t ih, trivial)
+    rfl
+    (λ h t ih1 ih2,
+     show cons' h (pack (unpack t)) = cons' h t, from
+     congr_arg (λ x, cons' h x) ih2)
+
 attribute [pattern]
 definition tree.node {A : Type} (l : list (tree A)) : tree A :=
 tree_core.node' (pack l)
@@ -40,12 +54,13 @@ definition tree.leaf {A : Type} : A → tree A :=
 tree_core.leaf'
 
 definition sz {A : Type} : tree A → nat
-| (tree.leaf a) := 0
-| (tree.node l) := list.length l
+| (tree.leaf a) := 1
+| (tree.node l) := list.length l + 1
 
-open tree
+constant P {A : Type} : tree A → Type₁
+constant mk1 {A : Type} (a : A) : P (tree.leaf a)
+constant mk2 {A : Type} (l : list (tree A)) : P (tree.node l)
 
-print sz
-
-example : sz (node [leaf tt, leaf tt, leaf ff]) = 3 :=
-rfl
+definition bla {A : Type} : ∀ n : tree A, P n
+| (tree.leaf a) := mk1 a
+| (tree.node l) := mk2 l

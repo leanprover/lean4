leanprover/lean3

feat(inductive_compiler): support for mutually inductive types

src/library/constructions/has_sizeof.h
@@ -1,0 +1,23 @@
+/*
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Daniel Selsam
+*/
+#pragma once
+#include "kernel/environment.h"
+#include "library/tactic/simplifier/simp_lemmas.h"
+
+namespace lean {
+/** \brief Given an inductive datatype \c n in \c env, add
+    <tt>n.has_sizeof</tt> instance to the environment. */
+environment mk_has_sizeof(environment const & env, name const & ind_name);
+
+name mk_has_sizeof_name(name const & n);
+name simp_sizeof_attribute_name();
+simp_lemmas get_sizeof_simp_lemmas(type_context & tctx);
+environment set_simp_sizeof(environment const & env, name const & n);
+
+void initialize_has_sizeof();
+void finalize_has_sizeof();
+}

tests/lean/run/mutual_inductive.lean
@@ -1,0 +1,201 @@
+namespace X1
+
+mutual_inductive foo, bar
+with foo : Type
+| mk : foo
+with bar : Type
+| mk : bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+end X1
+
+namespace X2
+
+mutual_inductive foo, bar
+with foo : Type
+| mk : bar -> foo
+with bar : Type
+| mk : foo -> bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+end X2
+
+namespace X3
+
+mutual_inductive foo, bar
+with foo : bool -> Type
+| mk : bar -> foo tt
+with bar : Type
+| mk : foo tt -> bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+end X3
+
+namespace X4
+
+mutual_inductive foo, bar, rig
+with foo : bool -> bool -> Type
+| mk : bar tt -> foo tt tt
+with bar : bool -> Type
+| mk : foo tt tt -> bar tt
+with rig : Type
+| mk : foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @rig.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+check @rig.mk.has_sizeof_spec
+end X4
+
+namespace X5
+
+mutual_inductive foo, bar, rig
+with foo : bool -> bool -> Prop
+| mk : bar tt -> foo tt tt
+with bar : bool -> Prop
+| mk : foo tt tt -> bar tt
+with rig : Prop
+| mk : foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X5
+
+namespace X6
+
+mutual_inductive foo, bar, rig (A : Type)
+with foo : bool -> bool -> Prop
+| mk : A -> bar tt -> foo tt tt
+with bar : bool -> Prop
+| mk : A -> foo tt tt -> bar tt
+with rig : Prop
+| mk : A -> foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X6
+
+namespace X7
+
+mutual_inductive foo, bar, rig (A : Type)
+with foo : Pi (b : bool), b = b -> Type
+| mk : A -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type
+| mk : A -> foo tt rfl -> bar tt ff tt
+with rig : Type
+| mk : A -> foo tt rfl -> bar tt ff tt -> rig
+| put : A -> foo tt rfl -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @rig.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+check @rig.mk.has_sizeof_spec
+check @rig.put.has_sizeof_spec
+end X7
+
+namespace X8
+
+mutual_inductive {l₁ l₂} foo, bar, rig (A : Type.{l₁}) (B : Type.{l₂})
+with foo : Pi (b : bool), b = b -> Type.{max l₁ l₂}
+| mk : A -> B -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type.{max l₁ l₂}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt
+with rig : Type.{max l₁ l₂}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X8
+
+namespace X9
+
+mutual_inductive {l₁ l₂ l₃} foo, bar, rig (A : Type.{l₁}) (B : Type.{l₂}) (a : A)
+with foo : Pi (b : bool), b = b -> Type.{max l₁ l₂ l₃}
+| mk : A -> B -> Pi x : A, x = a -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type.{max l₁ l₂ l₃}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt
+with rig : Type.{max l₁ l₂ l₃}
+| mk : A -> B -> (Pi x : A, x = a -> foo tt rfl) -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+
+end X9
+
+namespace X10
+
+mutual_inductive foo, bar, rig
+with foo : Type -> Type
+| mk : bar (foo poly_unit) -> foo (bar poly_unit)
+with bar : Type -> Type
+| mk : foo (bar poly_unit) -> bar (foo poly_unit)
+with rig : Type -> Type
+| mk : foo (bar (rig poly_unit)) -> rig (bar (foo poly_unit)) -> rig (bar (foo poly_unit))
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof
+check @bar.has_sizeof
+check @rig.has_sizeof
+check @foo.mk.has_sizeof_spec
+check @bar.mk.has_sizeof_spec
+check @rig.mk.has_sizeof_spec
+
+end X10

leanprover/lean4

src/library/constructions/has_sizeof.h
@@ -1,0 +1,24 @@
+/*
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Daniel Selsam
+*/
+#pragma once
+#include "kernel/environment.h"
+#include "library/tactic/simplifier/simp_lemmas.h"
+
+namespace lean {
+/** \brief Given an inductive datatype \c n in \c env, add
+    <tt>n.has_sizeof</tt> instance to the environment. */
+environment mk_has_sizeof(environment const & env, name const & ind_name);
+
+name mk_has_sizeof_name(name const & ind_name);
+name mk_sizeof_spec_name(name const & ir_name);
+name simp_sizeof_attribute_name();
+simp_lemmas get_sizeof_simp_lemmas(type_context & tctx);
+environment set_simp_sizeof(environment const & env, name const & n);
+
+void initialize_has_sizeof();
+void finalize_has_sizeof();
+}

tests/lean/run/mutual_inductive.lean
@@ -1,0 +1,201 @@
+namespace X1
+
+mutual_inductive foo, bar
+with foo : Type
+| mk : foo
+with bar : Type
+| mk : bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+end X1
+
+namespace X2
+
+mutual_inductive foo, bar
+with foo : Type
+| mk : bar -> foo
+with bar : Type
+| mk : foo -> bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+end X2
+
+namespace X3
+
+mutual_inductive foo, bar
+with foo : bool -> Type
+| mk : bar -> foo tt
+with bar : Type
+| mk : foo tt -> bar
+
+check @foo
+check @bar
+check @foo.rec
+check @bar.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+end X3
+
+namespace X4
+
+mutual_inductive foo, bar, rig
+with foo : bool -> bool -> Type
+| mk : bar tt -> foo tt tt
+with bar : bool -> Type
+| mk : foo tt tt -> bar tt
+with rig : Type
+| mk : foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @rig.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+check @rig.mk.sizeof_spec
+end X4
+
+namespace X5
+
+mutual_inductive foo, bar, rig
+with foo : bool -> bool -> Prop
+| mk : bar tt -> foo tt tt
+with bar : bool -> Prop
+| mk : foo tt tt -> bar tt
+with rig : Prop
+| mk : foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X5
+
+namespace X6
+
+mutual_inductive foo, bar, rig (A : Type)
+with foo : bool -> bool -> Prop
+| mk : A -> bar tt -> foo tt tt
+with bar : bool -> Prop
+| mk : A -> foo tt tt -> bar tt
+with rig : Prop
+| mk : A -> foo tt tt -> bar tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X6
+
+namespace X7
+
+mutual_inductive foo, bar, rig (A : Type)
+with foo : Pi (b : bool), b = b -> Type
+| mk : A -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type
+| mk : A -> foo tt rfl -> bar tt ff tt
+with rig : Type
+| mk : A -> foo tt rfl -> bar tt ff tt -> rig
+| put : A -> foo tt rfl -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @rig.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+check @rig.mk.sizeof_spec
+check @rig.put.sizeof_spec
+end X7
+
+namespace X8
+
+mutual_inductive {l₁ l₂} foo, bar, rig (A : Type.{l₁}) (B : Type.{l₂})
+with foo : Pi (b : bool), b = b -> Type.{max l₁ l₂}
+| mk : A -> B -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type.{max l₁ l₂}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt
+with rig : Type.{max l₁ l₂}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+end X8
+
+namespace X9
+
+mutual_inductive {l₁ l₂ l₃} foo, bar, rig (A : Type.{l₁}) (B : Type.{l₂}) (a : A)
+with foo : Pi (b : bool), b = b -> Type.{max l₁ l₂ l₃}
+| mk : A -> B -> Pi x : A, x = a -> bar tt ff tt -> foo tt rfl
+with bar : bool -> bool -> bool -> Type.{max l₁ l₂ l₃}
+| mk : A -> B -> foo tt rfl -> bar tt ff tt
+with rig : Type.{max l₁ l₂ l₃}
+| mk : A -> B -> (Pi x : A, x = a -> foo tt rfl) -> bar tt ff tt -> rig
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+
+end X9
+
+namespace X10
+
+mutual_inductive foo, bar, rig
+with foo : Type -> Type
+| mk : bar (foo poly_unit) -> foo (bar poly_unit)
+with bar : Type -> Type
+| mk : foo (bar poly_unit) -> bar (foo poly_unit)
+with rig : Type -> Type
+| mk : foo (bar (rig poly_unit)) -> rig (bar (foo poly_unit)) -> rig (bar (foo poly_unit))
+
+check @foo
+check @bar
+check @rig
+check @foo.rec
+check @bar.rec
+check @rig.rec
+check @foo.has_sizeof_inst
+check @bar.has_sizeof_inst
+check @rig.has_sizeof_inst
+check @foo.mk.sizeof_spec
+check @bar.mk.sizeof_spec
+check @rig.mk.sizeof_spec
+
+end X10
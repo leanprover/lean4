9632f00fbe478277d0ab3c177590f665d42c7237
Leonardo de Moura <leonardo@microsoft.com>
2016-09-13 07:51:04 -0700

    test(tests/lean/run): the lean2 macro `dec_trivial` is tricky to support in the new elaborator
    Here are some workarounds.

---
+++ tests/lean/run/dec_trivial_problem.lean
@@ -0,0 +1,22 @@
+set_option new_elaborator true
+
+definition n : nat := 3
+
+open tactic
+meta_definition dec_triv : tactic unit :=
+do tgt  ← target,
+   inst ← to_expr `(decidable %%tgt) >>= mk_instance,
+   to_expr `(@of_is_true %%tgt %%inst trivial) >>= exact
+
+example : 0 < n :=
+by dec_triv
+
+notation `dec_trivial2` := by dec_triv
+
+example : 0 < n :=
+dec_trivial2
+
+notation `dec_trivial3` := of_is_true (by triv)
+
+example : 0 < n :=
+dec_trivial3

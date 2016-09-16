9e37a8d665423418dd0a9e1c6543eac391ba7edb
Leonardo de Moura <leonardo@microsoft.com>
2016-09-16 09:25:20 -0700

    refactor(library/init/fin): use new elaborator

--- library/init/fin.lean
+++ library/init/fin.lean
@@ -6,33 +6,27 @@ Author: Leonardo de Moura
 prelude
 import init.nat
 open nat
-
+set_option new_elaborator true
 structure fin (n : nat) := (val : nat) (is_lt : val < n)
 
 namespace fin
 
 variable {n : nat}
 
-set_option pp.all true
-
 lemma eq_of_veq : ∀ {i j : fin n}, (val i) = (val j) → i = j
-| (mk iv ilt) (mk jv jlt) := assume (veq : iv = jv),
-  have ∀ e : iv = jv, mk iv ilt = mk jv (eq.subst e ilt), from
-    eq.rec_on veq (λ e : iv = iv, (eq.refl (mk iv (eq.subst e ilt)))),
-  this veq
+| (mk iv ilt₁) (mk .iv ilt₂) rfl := rfl
 
 lemma veq_of_eq : ∀ {i j : fin n}, i = j → (val i) = (val j)
-| (mk iv ilt) (mk jv jlt) := assume Peq,
-  show iv = jv, from fin.no_confusion Peq (λ Pe Pqe, Pe)
-
-lemma eq_iff_veq {i j : fin n} : (val i) = (val j) ↔ i = j :=
-iff.intro eq_of_veq veq_of_eq
+| (mk iv ilt) .(mk iv ilt) rfl := rfl
 
 end fin
 
-open decidable fin
+open fin
 
 attribute [instance]
 protected definition fin.has_decidable_eq (n : nat) : ∀ (i j : fin n), decidable (i = j)
 | (mk ival ilt) (mk jval jlt) :=
-  decidable_of_decidable_of_iff (nat.has_decidable_eq ival jval) eq_iff_veq
+  match nat.has_decidable_eq ival jval with
+  | is_true  h₁ := is_true (eq_of_veq h₁)
+  | is_false h₁ := is_false (λ h₂, absurd (veq_of_eq h₂) h₁)
+  end

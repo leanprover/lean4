bc31e43317db30e1eb403f4c338e20fad625fe93
Leonardo de Moura <leonardo@microsoft.com>
2016-09-18 18:19:04 -0700

    chore(tests/lean/run): delete old tests

--- tests/lean/run/show2.lean
+++
@@ -1,8 +0,0 @@
-open nat
-
-example : ∀ a b : nat, a + b = b + a :=
-show ∀ a b : nat, a + b = b + a
-| 0        0        := rfl
-| 0        (succ b) := sorry -- by rewrite zero_add
-| (succ a) 0        := sorry -- by rewrite zero_add
-| (succ a) (succ b) := sorry -- by rewrite [succ_add, this]

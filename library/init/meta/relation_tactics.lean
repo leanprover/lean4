a2bf24a916e9f28866c016467ca96d1e21515c2c
Leonardo de Moura <leonardo@microsoft.com>
2016-10-05 10:40:55 -0700

    feat(library/init/meta/relation_tactics): add tactic for retrieving lhs and rhs from an registered relation

--- library/init/meta/relation_tactics.lean
+++ library/init/meta/relation_tactics.lean
@@ -9,7 +9,7 @@ import init.meta.tactic init.function
 namespace tactic
 open expr
 
-private meta definition relation_tactic (op_for : environment → name → option name) (tac_name : string) : tactic unit :=
+private meta def relation_tactic (op_for : environment → name → option name) (tac_name : string) : tactic unit :=
 do tgt ← target,
    env ← get_env,
    r   ← return $ get_app_fn tgt,
@@ -18,13 +18,27 @@ do tgt ← target,
    | none        := fail $ tac_name ++ " tactic failed, target is not a relation application with the expected property."
    end
 
-meta definition reflexivity : tactic unit :=
+meta def reflexivity : tactic unit :=
 relation_tactic environment.refl_for "reflexivity"
 
-meta definition symmetry : tactic unit :=
+meta def symmetry : tactic unit :=
 relation_tactic environment.symm_for "symmetry"
 
-meta definition transitivity : tactic unit :=
+meta def transitivity : tactic unit :=
 relation_tactic environment.trans_for "transitivity"
+
+meta def relation_lhs_rhs : expr → tactic (name × expr × expr) :=
+λ e, do
+  (const c _) ← return e^.get_app_fn | failed,
+  env ← get_env,
+  (some (arity, lhs_pos, rhs_pos)) ← return $ env^.relation_info c | failed,
+  args ← return $ get_app_args e,
+  guard (args^.length = arity),
+  (some lhs) ← return $ args^.nth lhs_pos | failed,
+  (some rhs) ← return $ args^.nth rhs_pos | failed,
+  return (c, lhs, rhs)
+
+meta def target_lhs_rhs : tactic (name × expr × expr) :=
+target >>= relation_lhs_rhs
 
 end tactic

1e325c11a1bfc013014e3cf07b4d8e7cb29d42f4
Leonardo de Moura <leonardo@microsoft.com>
2016-10-05 15:01:20 -0700

    feat(library/standard): add system.IO

--- library/standard.lean
+++ library/standard.lean
@@ -2,8 +2,5 @@
 Copyright (c) 2014 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jeremy Avigad
-
-The constructive core of Lean's library.
 -/
-
--- import logic data
+import system.IO

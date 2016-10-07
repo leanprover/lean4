7e62a8e4c792855880f03db0f79e94f864558083
Leonardo de Moura <leonardo@microsoft.com>
2016-10-07 14:09:52 -0700

    chore(library/init/function): cleanup

--- library/init/function.lean
+++ library/init/function.lean
@@ -48,14 +48,14 @@ f x
 @[reducible] def curry : (A × B → C) → A → B → C :=
 λ f a b, f (a, b)
 
-@[reducible] def uncurry : (A → B → C) → (A × B → C) :=
-λ f p, match p with (a, b) := f a b end
+@[reducible] def uncurry : (A → B → C) → A × B → C :=
+λ f ⟨a, b⟩, f a b
 
-theorem curry_uncurry (f : A → B → C) : curry (uncurry f) = f :=
+lemma curry_uncurry (f : A → B → C) : curry (uncurry f) = f :=
 rfl
 
-theorem uncurry_curry (f : A × B → C) : uncurry (curry f) = f :=
-funext (λ p, match p with (a, b) := rfl end)
+lemma uncurry_curry (f : A × B → C) : uncurry (curry f) = f :=
+funext (λ ⟨a, b⟩, rfl)
 
 infixl  ` on `:1         := on_fun
 notation f ` -[` op `]- ` g  := combine f op g
@@ -64,33 +64,29 @@ lemma left_id (f : A → B) : id ∘ f = f := rfl
 
 lemma right_id (f : A → B) : f ∘ id = f := rfl
 
-theorem comp.assoc (f : C → D) (g : B → C) (h : A → B) : (f ∘ g) ∘ h = f ∘ (g ∘ h) := rfl
+lemma comp.assoc (f : C → D) (g : B → C) (h : A → B) : (f ∘ g) ∘ h = f ∘ (g ∘ h) := rfl
 
-theorem comp.left_id (f : A → B) : id ∘ f = f := rfl
+lemma comp.left_id (f : A → B) : id ∘ f = f := rfl
 
-theorem comp.right_id (f : A → B) : f ∘ id = f := rfl
+lemma comp.right_id (f : A → B) : f ∘ id = f := rfl
 
-theorem comp_const_right (f : B → C) (b : B) : f ∘ (const A b) = const A (f b) := rfl
+lemma comp_const_right (f : B → C) (b : B) : f ∘ (const A b) = const A (f b) := rfl
 
 @[reducible] def injective (f : A → B) : Prop := ∀ ⦃a₁ a₂⦄, f a₁ = f a₂ → a₁ = a₂
 
-theorem injective_comp {g : B → C} {f : A → B} (Hg : injective g) (Hf : injective f) :
-  injective (g ∘ f) :=
-take a₁ a₂, assume Heq, Hf (Hg Heq)
+lemma injective_comp {g : B → C} {f : A → B} (hg : injective g) (hf : injective f) : injective (g ∘ f) :=
+take a₁ a₂, assume h, hf (hg h)
 
 @[reducible] def surjective (f : A → B) : Prop := ∀ b, ∃ a, f a = b
 
-theorem surjective_comp {g : B → C} {f : A → B} (Hg : surjective g) (Hf : surjective f) :
-  surjective (g ∘ f) :=
-take (c : C), exists.elim (Hg c) (λ b Hb, exists.elim (Hf b) (λ a Ha,
-  exists.intro a (show g (f a) = c, from (eq.trans (congr_arg g Ha) Hb))))
+lemma surjective_comp {g : B → C} {f : A → B} (hg : surjective g) (hf : surjective f) : surjective (g ∘ f) :=
+λ (c : C), exists.elim (hg c) (λ b hb, exists.elim (hf b) (λ a ha,
+  exists.intro a (show g (f a) = c, from (eq.trans (congr_arg g ha) hb))))
 
 def bijective (f : A → B) := injective f ∧ surjective f
 
-theorem bijective_comp {g : B → C} {f : A → B} (Hg : bijective g) (Hf : bijective f) :
-  bijective (g ∘ f) :=
-and.elim Hg (λ Hginj Hgsurj, and.elim Hf (λ Hfinj Hfsurj,
-  ⟨injective_comp Hginj Hfinj, surjective_comp Hgsurj Hfsurj⟩))
+lemma bijective_comp {g : B → C} {f : A → B} : bijective g → bijective f → bijective (g ∘ f)
+| ⟨h_ginj, h_gsurj⟩ ⟨h_finj, h_fsurj⟩ := ⟨injective_comp h_ginj h_finj, surjective_comp h_gsurj h_fsurj⟩
 
 -- g is a left inverse to f
 def left_inverse (g : B → A) (f : A → B) : Prop := ∀ x, g (f x) = x
@@ -108,44 +104,38 @@ assume h, funext h
 
 def has_right_inverse (f : A → B) : Prop := ∃ finv : B → A, right_inverse finv f
 
-theorem injective_of_left_inverse {g : B → A} {f : A → B} : left_inverse g f → injective f :=
+lemma injective_of_left_inverse {g : B → A} {f : A → B} : left_inverse g f → injective f :=
 assume h, take a b, assume faeqfb,
 have h₁ : a = g (f a),       from eq.symm (h a),
 have h₂ : g (f b) = b,       from h b,
 have h₃ : g (f a) = g (f b), from congr_arg g faeqfb,
 eq.trans h₁ (eq.trans h₃ h₂)
 
-theorem injective_of_has_left_inverse {f : A → B} : has_left_inverse f → injective f :=
+lemma injective_of_has_left_inverse {f : A → B} : has_left_inverse f → injective f :=
 assume h, exists.elim h (λ finv inv, injective_of_left_inverse inv)
 
-theorem right_inverse_of_injective_of_left_inverse {f : A → B} {g : B → A}
+lemma right_inverse_of_injective_of_left_inverse {f : A → B} {g : B → A}
     (injf : injective f) (lfg : left_inverse f g) :
   right_inverse f g :=
 take x,
 have H : f (g (f x)) = f x, from lfg (f x),
 injf H
 
-theorem surjective_of_has_right_inverse {f : A → B} : has_right_inverse f → surjective f :=
-assume h, take b,
-exists.elim h (λ finv inv,
-have h : f (finv b) = b, from calc
-  f (finv b)  = f (finv b)   : rfl
-          ... = b            : inv b,
-⟨finv b, h⟩)
+lemma surjective_of_has_right_inverse {f : A → B} : has_right_inverse f → surjective f
+| ⟨finv, inv⟩ b := ⟨finv b, inv b⟩
 
-theorem left_inverse_of_surjective_of_right_inverse {f : A → B} {g : B → A}
+lemma left_inverse_of_surjective_of_right_inverse {f : A → B} {g : B → A}
     (surjf : surjective f) (rfg : right_inverse f g) :
   left_inverse f g :=
-take y, exists.elim (surjf y) (λ x Hx,
-calc
+take y, exists.elim (surjf y) (λ x Hx, calc
   f (g y) = f (g (f x)) : Hx ▸ rfl
       ... = f x         : eq.symm (rfg x) ▸ rfl
       ... = y           : Hx)
 
-theorem injective_id : injective (@id A) := take a₁ a₂ H, H
+lemma injective_id : injective (@id A) := take a₁ a₂ h, h
 
-theorem surjective_id : surjective (@id A) := take a, ⟨a, rfl⟩
+lemma surjective_id : surjective (@id A) := take a, ⟨a, rfl⟩
 
-theorem bijective_id : bijective (@id A) := ⟨injective_id, surjective_id⟩
+lemma bijective_id : bijective (@id A) := ⟨injective_id, surjective_id⟩
 
 end function

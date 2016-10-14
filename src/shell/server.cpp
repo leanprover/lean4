b9e62a982f3414e5440617a2858372fcb9a83fa4
Leonardo de Moura <leonardo@microsoft.com>
2016-10-13 19:35:50 -0700

    chore(shell/server): fix 'make style'

--- src/shell/server.cpp
+++ src/shell/server.cpp
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Gabriel Ebner
 */
-
+#include <string>
 #include "frontends/lean/parser.h"
 #include "library/module.h"
 #include "shell/server.h"

02a60639238748bc9049007feda6e6f1438abeb1
Sebastian Ullrich <sebasti@nullri.ch>
2016-11-07 11:30:22 +0100

    feat(bin/lean-gdb): support pretty-printing rb_map and rb_tree

--- bin/lean-gdb.py
+++ bin/lean-gdb.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+# Released under Apache 2.0 license as described in the file LICENSE.
+#
+# Authors: Sebastian Ullrich, Gabriel Ebner
+#
+
 import gdb
 import gdb.printing
 
@@ -19,6 +27,43 @@ class LeanNamePrinter:
             return ""
         else:
             return rec(self.val['m_ptr'].dereference())
+
+class LeanExprPrinter:
+    """Print a lean::expr object."""
+
+    expr_kinds = [
+        'lean::expr_var',
+        'lean::expr_sort',
+        'lean::expr_const',
+        'lean::expr_mlocal',
+        'lean::expr_mlocal',
+        'lean::expr_app',
+        'lean::expr_binding',
+        'lean::expr_binding',
+        'lean::expr_let',
+        'lean::expr_macro',
+    ]
+
+    def __init__(self, val):
+        kind = val['m_ptr']['m_kind']
+        subtype = gdb.lookup_type(LeanExprPrinter.expr_kinds[kind])
+        self.val = val['m_ptr'].cast(subtype.pointer()).dereference()
+
+    def to_string(self):
+        return str(self.val.type)
+
+    def children(self):
+        def deep_fields(v):
+            for f in v.type.fields():
+                if f.is_base_class:
+                    for g in deep_fields(v[f]):
+                        yield g
+                elif f.name == 'm_kind':
+                    yield (f.name, v[f].cast(gdb.lookup_type('lean::expr_kind')))
+                else:
+                    yield (f.name, v[f])
+
+        return deep_fields(self.val)
 
 class LeanListPrinter:
     """Print a lean::list object."""
@@ -59,42 +104,43 @@ class LeanBufferPrinter:
     def display_hint(self):
         return 'array'
 
-class LeanExprPrinter:
-    """Print a lean::expr object."""
-
-    expr_kinds = [
-        'lean::expr_var',
-        'lean::expr_sort',
-        'lean::expr_const',
-        'lean::expr_mlocal',
-        'lean::expr_mlocal',
-        'lean::expr_app',
-        'lean::expr_binding',
-        'lean::expr_binding',
-        'lean::expr_let',
-        'lean::expr_macro',
-    ]
+class LeanRBTreePrinter:
+    """Print a lean::rb_tree object."""
 
     def __init__(self, val):
-        kind = val['m_ptr']['m_kind']
-        subtype = gdb.lookup_type(LeanExprPrinter.expr_kinds[kind])
-        self.val = val['m_ptr'].cast(subtype.pointer()).dereference()
-
-    def to_string(self):
-        return str(self.val.type)
+        self.val = val
 
     def children(self):
-        def deep_fields(v):
-            for f in v.type.fields():
-                if f.is_base_class:
-                    for g in deep_fields(v[f]):
-                        yield g
-                elif f.name == 'm_kind':
-                    yield (f.name, v[f].cast(gdb.lookup_type('lean::expr_kind')))
-                else:
-                    yield (f.name, v[f])
+        def rec(node):
+            if node['m_ptr']:
+                cell = node['m_ptr'].dereference()
+                yield from rec(cell['m_left'])
+                yield ('', cell['m_value'])
+                yield from rec(cell['m_right'])
+        return rec(self.val['m_root'])
 
-        return deep_fields(self.val)
+    def to_string(self):
+        return 'lean::rb_tree' # full type is way too verbose
+
+    def display_hint(self):
+        return 'array'
+
+class LeanRBMapPrinter:
+    """Print a lean::rb_map object."""
+
+    def __init__(self, val):
+        self.val = val
+
+    def children(self):
+        for (_, child) in LeanRBTreePrinter(self.val['m_map']).children():
+            yield ('', child['first'])
+            yield ('', child['second'])
+
+    def to_string(self):
+        return 'lean::rb_map' # full type is way too verbose
+
+    def display_hint(self):
+        return 'map'
 
 def build_pretty_printer():
     pp = gdb.printing.RegexpCollectionPrettyPrinter("lean")
@@ -102,6 +148,8 @@ def build_pretty_printer():
     pp.add_printer('expr', '^lean::expr$', LeanExprPrinter)
     pp.add_printer('list', '^lean::list', LeanListPrinter)
     pp.add_printer('buffer', '^lean::buffer', LeanBufferPrinter)
+    pp.add_printer('rb_tree', '^lean::rb_tree', LeanRBTreePrinter)
+    pp.add_printer('rb_map', '^lean::rb_map', LeanRBMapPrinter)
     return pp
 
 gdb.printing.register_pretty_printer(

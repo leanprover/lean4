f7fd8e4384b16e298ade9ac9c5715bb080b1f356
Leonardo de Moura <leonardo@microsoft.com>
2016-11-14 17:33:56 -0800

    chore(library/tactic/vm_monitor): fix style

--- src/library/tactic/vm_monitor.cpp
+++ src/library/tactic/vm_monitor.cpp
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <limits>
 #include "library/vm/vm.h"
 #include "library/vm/vm_option.h"
 #include "library/vm/vm_name.h"

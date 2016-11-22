312689ec7f15c63dbf040858156d1ff9d6a22ca9
Leonardo de Moura <leonardo@microsoft.com>
2016-11-22 13:40:23 -0800

    feat(library/init/nat_lemmas): nat is a decidable_linear_ordered_semiring

--- library/init/nat_lemmas.lean
+++ library/init/nat_lemmas.lean
@@ -149,6 +149,254 @@ namespace nat
    mul_zero       := nat.mul_zero,
    mul_comm       := nat.mul_comm}
 
+  /- properties of inequality -/
+
+  protected lemma le_of_eq {n m : ℕ} (p : n = m) : n ≤ m :=
+  p ▸ le.nat_refl n
+
+  lemma le_succ_iff_true (n : ℕ) : n ≤ succ n ↔ true :=
+  iff_true_intro (le_succ n)
+
+  lemma pred_le_iff_true (n : ℕ) : pred n ≤ n ↔ true :=
+  iff_true_intro (pred_le n)
+
+  lemma le_succ_of_le {n m : ℕ} (h : n ≤ m) : n ≤ succ m :=
+  nat.le_trans h (le_succ m)
+
+  lemma le_of_succ_le {n m : ℕ} (h : succ n ≤ m) : n ≤ m :=
+  nat.le_trans (le_succ n) h
+
+  protected lemma le_of_lt {n m : ℕ} (h : n < m) : n ≤ m :=
+  le_of_succ_le h
+
+  lemma le_succ_of_pred_le {n m : ℕ} : pred n ≤ m → n ≤ succ m :=
+  nat.cases_on n le.step (λ a, succ_le_succ)
+
+  lemma succ_le_zero_iff_false (n : ℕ) : succ n ≤ 0 ↔ false :=
+  iff_false_intro (not_succ_le_zero n)
+
+  lemma succ_le_self_iff_false (n : ℕ) : succ n ≤ n ↔ false :=
+  iff_false_intro (not_succ_le_self n)
+
+  lemma zero_le_iff_true (n : ℕ) : 0 ≤ n ↔ true :=
+  iff_true_intro (zero_le n)
+
+  def lt.step {n m : ℕ} : n < m → n < succ m := le.step
+
+  lemma zero_lt_succ_iff_true (n : ℕ) : 0 < succ n ↔ true :=
+  iff_true_intro (zero_lt_succ n)
+
+  protected lemma lt_trans {n m k : ℕ} (h₁ : n < m) : m < k → n < k :=
+  nat.le_trans (le.step h₁)
+
+  protected lemma lt_of_le_of_lt {n m k : ℕ} (h₁ : n ≤ m) : m < k → n < k :=
+  nat.le_trans (succ_le_succ h₁)
+
+  lemma lt_self_iff_false (n : ℕ) : n < n ↔ false :=
+  iff_false_intro (λ h, absurd h (nat.lt_irrefl n))
+
+  lemma self_lt_succ (n : ℕ) : n < succ n := nat.le_refl (succ n)
+
+  lemma self_lt_succ_iff_true (n : ℕ) : n < succ n ↔ true :=
+  iff_true_intro (self_lt_succ n)
+
+  def lt.base (n : ℕ) : n < succ n := nat.le_refl (succ n)
+
+  lemma le_lt_antisymm {n m : ℕ} (h₁ : n ≤ m) (h₂ : m < n) : false :=
+  nat.lt_irrefl n (nat.lt_of_le_of_lt h₁ h₂)
+
+  protected lemma le_antisymm {n m : ℕ} (h₁ : n ≤ m) : m ≤ n → n = m :=
+  le.cases_on h₁ (λ a, rfl) (λ a b c, absurd (nat.lt_of_le_of_lt b c) (nat.lt_irrefl n))
+
+  instance : weak_order ℕ :=
+    ⟨ @nat.le, @nat.le_refl, @nat.le_trans, @nat.le_antisymm ⟩
+
+  lemma lt_le_antisymm {n m : ℕ} (h₁ : n < m) (h₂ : m ≤ n) : false :=
+  le_lt_antisymm h₂ h₁
+
+  protected lemma nat.lt_asymm {n m : ℕ} (h₁ : n < m) : ¬ m < n :=
+  le_lt_antisymm (nat.le_of_lt h₁)
+
+  lemma lt_zero_iff_false (a : ℕ) : a < 0 ↔ false :=
+  iff_false_intro (not_lt_zero a)
+
+  protected lemma le_of_eq_or_lt {a b : ℕ} (h : a = b ∨ a < b) : a ≤ b :=
+  or.elim h nat.le_of_eq nat.le_of_lt
+
+  lemma succ_lt_succ {a b : ℕ} : a < b → succ a < succ b :=
+  succ_le_succ
+
+  lemma lt_of_succ_lt {a b : ℕ} : succ a < b → a < b :=
+  le_of_succ_le
+
+  lemma lt_of_succ_lt_succ {a b : ℕ} : succ a < succ b → a < b :=
+  le_of_succ_le_succ
+
+  protected lemma lt_or_ge : ∀ (a b : ℕ), a < b ∨ a ≥ b
+  | a 0     := or.inr (zero_le a)
+  | a (b+1) :=
+    match lt_or_ge a b with
+    | or.inl h := or.inl (le_succ_of_le h)
+    | or.inr h :=
+      match nat.eq_or_lt_of_le h with
+      | or.inl h1 := or.inl (h1 ▸ self_lt_succ b)
+      | or.inr h1 := or.inr h1
+      end
+    end
+
+  protected def {u} lt_ge_by_cases {a b : ℕ} {C : Type u} (h₁ : a < b → C) (h₂ : a ≥ b → C) : C :=
+  decidable.by_cases h₁ (λ h, h₂ (or.elim (nat.lt_or_ge a b) (λ a, absurd a h) (λ a, a)))
+
+  protected def {u} lt_by_cases {a b : ℕ} {C : Type u} (h₁ : a < b → C) (h₂ : a = b → C)
+    (h₃ : b < a → C) : C :=
+  nat.lt_ge_by_cases h₁ (λ h₁,
+    nat.lt_ge_by_cases h₃ (λ h, h₂ (nat.le_antisymm h h₁)))
+
+  protected lemma lt_trichotomy (a b : ℕ) : a < b ∨ a = b ∨ b < a :=
+  nat.lt_by_cases (λ h, or.inl h) (λ h, or.inr (or.inl h)) (λ h, or.inr (or.inr h))
+
+  protected lemma eq_or_lt_of_not_lt {a b : ℕ} (hnlt : ¬ a < b) : a = b ∨ b < a :=
+  or.elim (nat.lt_trichotomy a b)
+    (λ hlt, absurd hlt hnlt)
+    (λ h, h)
+
+  lemma lt_of_succ_le {a b : ℕ} (h : succ a ≤ b) : a < b := h
+
+  lemma succ_le_of_lt {a b : ℕ} (h : a < b) : succ a ≤ b := h
+
+  lemma le_add_right : ∀ (n k : ℕ), n ≤ n + k
+  | n 0     := nat.le_refl n
+  | n (k+1) := le_succ_of_le (le_add_right n k)
+
+  lemma le_add_left (n m : ℕ): n ≤ m + n :=
+  nat.add_comm n m ▸ le_add_right n m
+
+  lemma le.elim : ∀ {n m : ℕ}, n ≤ m → ∃ k, n + k = m
+  | n .n        (le.nat_refl .n)  := ⟨0, rfl⟩
+  | n .(succ m) (@le.step .n m h) :=
+    match le.elim h with
+    | ⟨w, hw⟩ := ⟨succ w, hw ▸ add_succ n w⟩
+    end
+
+  lemma le.intro {n m k : ℕ} (h : n + k = m) : n ≤ m :=
+  h ▸ le_add_right n k
+
+  protected lemma add_le_add_left {n m : ℕ} (h : n ≤ m) (k : ℕ) : k + n ≤ k + m :=
+  match le.elim h with
+  | ⟨w, hw⟩ := @le.intro _ _ w begin rw [nat.add_assoc, hw] end
+  end
+
+  protected lemma add_le_add_right {n m : ℕ} (h : n ≤ m) (k : ℕ) : n + k ≤ m + k :=
+  begin rw [nat.add_comm n k, nat.add_comm m k], apply nat.add_le_add_left h end
+
+  protected lemma le_of_add_le_add_left {k n m : ℕ} (h : k + n ≤ k + m) : n ≤ m :=
+  match le.elim h with
+  | ⟨w, hw⟩ := @le.intro _ _ w
+    begin
+      dsimp at hw,
+      rw [nat.add_assoc] at hw,
+      apply nat.add_left_cancel hw
+    end
+  end
+
+  protected lemma lt_of_le_and_ne {m n : ℕ} (h1 : m ≤ n) : m ≠ n → m < n :=
+  or.resolve_right (or.swap (nat.eq_or_lt_of_le h1))
+
+  protected theorem lt_of_add_lt_add_left {k n m : ℕ} (h : k + n < k + m) : n < m :=
+  let h' := nat.le_of_lt h in
+  nat.lt_of_le_and_ne
+    (nat.le_of_add_le_add_left h')
+    (λ heq, nat.lt_irrefl (k + m) begin rw heq at h, assumption end)
+
+  protected lemma add_lt_add_left {n m : ℕ} (h : n < m) (k : ℕ) : k + n < k + m :=
+  lt_of_succ_le (add_succ k n ▸ nat.add_le_add_left (succ_le_of_lt h) k)
+
+  protected lemma add_lt_add_right {n m : ℕ} (h : n < m) (k : ℕ) : n + k < m + k :=
+  nat.add_comm k m ▸ nat.add_comm k n ▸ nat.add_lt_add_left h k
+
+  protected lemma lt_add_of_pos_right {n k : ℕ} (h : k > 0) : n < n + k :=
+  nat.add_lt_add_left h n
+
+  protected lemma zero_lt_one : 0 < (1:nat) :=
+  zero_lt_succ 0
+
+  protected lemma le_total {m n : ℕ} : m ≤ n ∨ n ≤ m :=
+  or.imp_left nat.le_of_lt (nat.lt_or_ge m n)
+
+  protected lemma le_of_lt_or_eq {m n : ℕ} (h : m < n ∨ m = n) : m ≤ n :=
+  nat.le_of_eq_or_lt (or.swap h)
+
+  protected lemma lt_or_eq_of_le {m n : ℕ} (h : m ≤ n) : m < n ∨ m = n :=
+  or.swap (nat.eq_or_lt_of_le h)
+
+  protected lemma le_iff_lt_or_eq (m n : ℕ) : m ≤ n ↔ m < n ∨ m = n :=
+  iff.intro nat.lt_or_eq_of_le nat.le_of_lt_or_eq
+
+  lemma mul_le_mul_left {n m : ℕ} (k : ℕ) (h : n ≤ m) : k * n ≤ k * m :=
+  match le.elim h with
+  | ⟨l, hl⟩ :=
+    have k * n + k * l = k * m, by rw [-left_distrib, hl],
+    le.intro this
+  end
+
+  lemma mul_le_mul_right {n m : ℕ} (k : ℕ) (h : n ≤ m) : n * k ≤ m * k :=
+  mul_comm k m ▸ mul_comm k n ▸ mul_le_mul_left k h
+
+  protected lemma mul_lt_mul_of_pos_left {n m k : ℕ} (h : n < m) (hk : k > 0) : k * n < k * m :=
+  nat.lt_of_lt_of_le (nat.lt_add_of_pos_right hk) (mul_succ k n ▸ nat.mul_le_mul_left k (succ_le_of_lt h))
+
+  protected lemma mul_lt_mul_of_pos_right {n m k : ℕ} (h : n < m) (hk : k > 0) : n * k < m * k :=
+  mul_comm k m ▸ mul_comm k n ▸ nat.mul_lt_mul_of_pos_left h hk
+
+  instance : decidable_linear_ordered_semiring nat :=
+  { nat.comm_semiring with
+    add_left_cancel            := @nat.add_left_cancel,
+    add_right_cancel           := @nat.add_right_cancel,
+    lt                         := nat.lt,
+    le                         := nat.le,
+    le_refl                    := nat.le_refl,
+    le_trans                   := @nat.le_trans,
+    le_antisymm                := @nat.le_antisymm,
+    le_total                   := @nat.le_total,
+    le_iff_lt_or_eq            := @nat.le_iff_lt_or_eq,
+    le_of_lt                   := @nat.le_of_lt,
+    lt_irrefl                  := @nat.lt_irrefl,
+    lt_of_lt_of_le             := @nat.lt_of_lt_of_le,
+    lt_of_le_of_lt             := @nat.lt_of_le_of_lt,
+    lt_of_add_lt_add_left      := @nat.lt_of_add_lt_add_left,
+    add_lt_add_left            := @nat.add_lt_add_left,
+    add_le_add_left            := @nat.add_le_add_left,
+    le_of_add_le_add_left      := @nat.le_of_add_le_add_left,
+    zero_lt_one                := zero_lt_succ 0,
+    mul_le_mul_of_nonneg_left  := (take a b c h₁ h₂, nat.mul_le_mul_left c h₁),
+    mul_le_mul_of_nonneg_right := (take a b c h₁ h₂, nat.mul_le_mul_right c h₁),
+    mul_lt_mul_of_pos_left     := @nat.mul_lt_mul_of_pos_left,
+    mul_lt_mul_of_pos_right    := @nat.mul_lt_mul_of_pos_right,
+    decidable_lt               := nat.decidable_lt }
+
+  /- sub properties -/
+
+  lemma sub_eq_succ_sub_succ (a b : ℕ) : a - b = succ a - succ b :=
+  eq.symm (succ_sub_succ_eq_sub a b)
+
+  lemma zero_sub_eq_zero : ∀ a : ℕ, 0 - a = 0
+  | 0     := rfl
+  | (a+1) := congr_arg pred (zero_sub_eq_zero a)
+
+  lemma zero_eq_zero_sub (a : ℕ) : 0 = 0 - a :=
+  eq.symm (zero_sub_eq_zero a)
+
+  lemma sub_le_iff_true (a b : ℕ) : a - b ≤ a ↔ true :=
+  iff_true_intro (sub_le a b)
+
+  lemma sub_lt_succ (a b : ℕ) : a - b < succ a :=
+  lt_succ_of_le (sub_le a b)
+
+  lemma sub_lt_succ_iff_true (a b : ℕ) : a - b < succ a ↔ true :=
+  iff_true_intro (sub_lt_succ a b)
+
+  /- bit0/bit1 properties -/
+
   protected lemma bit0_succ_eq (n : ℕ) : bit0 (succ n) = succ (succ (bit0 n)) :=
   show succ (succ n + n) = succ (succ (n + n)), from
   congr_arg succ (succ_add n n)
@@ -234,207 +482,6 @@ namespace nat
   protected lemma one_ne_bit1 {n : ℕ} : n ≠ 0 → 1 ≠ bit1 n :=
   λ h, ne.symm (nat.bit1_ne_one h)
 
-  /- properties of inequality -/
-
-  protected lemma le_of_eq {n m : ℕ} (p : n = m) : n ≤ m :=
-  p ▸ le.nat_refl n
-
-  lemma le_succ_iff_true (n : ℕ) : n ≤ succ n ↔ true :=
-  iff_true_intro (le_succ n)
-
-  lemma pred_le_iff_true (n : ℕ) : pred n ≤ n ↔ true :=
-  iff_true_intro (pred_le n)
-
-  lemma le_succ_of_le {n m : ℕ} (h : n ≤ m) : n ≤ succ m :=
-  nat.le_trans h (le_succ m)
-
-  lemma le_of_succ_le {n m : ℕ} (h : succ n ≤ m) : n ≤ m :=
-  nat.le_trans (le_succ n) h
-
-  protected lemma le_of_lt {n m : ℕ} (h : n < m) : n ≤ m :=
-  le_of_succ_le h
-
-  lemma le_succ_of_pred_le {n m : ℕ} : pred n ≤ m → n ≤ succ m :=
-  nat.cases_on n le.step (λ a, succ_le_succ)
-
-  lemma succ_le_zero_iff_false (n : ℕ) : succ n ≤ 0 ↔ false :=
-  iff_false_intro (not_succ_le_zero n)
-
-  lemma succ_le_self_iff_false (n : ℕ) : succ n ≤ n ↔ false :=
-  iff_false_intro (not_succ_le_self n)
-
-  lemma zero_le_iff_true (n : ℕ) : 0 ≤ n ↔ true :=
-  iff_true_intro (zero_le n)
-
-  protected lemma one_le_bit1 (n : ℕ) : 1 ≤ bit1 n :=
-  show 1 ≤ succ (bit0 n), from
-  succ_le_succ (zero_le (bit0 n))
-
-  protected lemma one_le_bit0 : ∀ (n : ℕ), n ≠ 0 → 1 ≤ bit0 n
-  | 0     h := absurd rfl h
-  | (n+1) h :=
-    suffices 1 ≤ succ (succ (bit0 n)), from
-      eq.symm (nat.bit0_succ_eq n) ▸ this,
-    succ_le_succ (zero_le (succ (bit0 n)))
-
-  def lt.step {n m : ℕ} : n < m → n < succ m := le.step
-
-  lemma zero_lt_succ_iff_true (n : ℕ) : 0 < succ n ↔ true :=
-  iff_true_intro (zero_lt_succ n)
-
-  protected lemma lt_trans {n m k : ℕ} (h₁ : n < m) : m < k → n < k :=
-  nat.le_trans (le.step h₁)
-
-  protected lemma lt_of_le_of_lt {n m k : ℕ} (h₁ : n ≤ m) : m < k → n < k :=
-  nat.le_trans (succ_le_succ h₁)
-
-  lemma lt_self_iff_false (n : ℕ) : n < n ↔ false :=
-  iff_false_intro (λ h, absurd h (nat.lt_irrefl n))
-
-  lemma self_lt_succ (n : ℕ) : n < succ n := nat.le_refl (succ n)
-
-  lemma self_lt_succ_iff_true (n : ℕ) : n < succ n ↔ true :=
-  iff_true_intro (self_lt_succ n)
-
-  def lt.base (n : ℕ) : n < succ n := nat.le_refl (succ n)
-
-  lemma le_lt_antisymm {n m : ℕ} (h₁ : n ≤ m) (h₂ : m < n) : false :=
-  nat.lt_irrefl n (nat.lt_of_le_of_lt h₁ h₂)
-
-  protected lemma le_antisymm {n m : ℕ} (h₁ : n ≤ m) : m ≤ n → n = m :=
-  le.cases_on h₁ (λ a, rfl) (λ a b c, absurd (nat.lt_of_le_of_lt b c) (nat.lt_irrefl n))
-
-  instance : weak_order ℕ :=
-    ⟨ @nat.le, @nat.le_refl, @nat.le_trans, @nat.le_antisymm ⟩
-
-  lemma lt_le_antisymm {n m : ℕ} (h₁ : n < m) (h₂ : m ≤ n) : false :=
-  le_lt_antisymm h₂ h₁
-
-  protected lemma nat.lt_asymm {n m : ℕ} (h₁ : n < m) : ¬ m < n :=
-  le_lt_antisymm (nat.le_of_lt h₁)
-
-  lemma lt_zero_iff_false (a : ℕ) : a < 0 ↔ false :=
-  iff_false_intro (not_lt_zero a)
-
-  protected lemma le_of_eq_or_lt {a b : ℕ} (h : a = b ∨ a < b) : a ≤ b :=
-  or.elim h nat.le_of_eq nat.le_of_lt
-
-  lemma succ_lt_succ {a b : ℕ} : a < b → succ a < succ b :=
-  succ_le_succ
-
-  lemma lt_of_succ_lt {a b : ℕ} : succ a < b → a < b :=
-  le_of_succ_le
-
-  lemma lt_of_succ_lt_succ {a b : ℕ} : succ a < succ b → a < b :=
-  le_of_succ_le_succ
-
-  protected lemma lt_or_ge : ∀ (a b : ℕ), a < b ∨ a ≥ b
-  | a 0     := or.inr (zero_le a)
-  | a (b+1) :=
-    match lt_or_ge a b with
-    | or.inl h := or.inl (le_succ_of_le h)
-    | or.inr h :=
-      match nat.eq_or_lt_of_le h with
-      | or.inl h1 := or.inl (h1 ▸ self_lt_succ b)
-      | or.inr h1 := or.inr h1
-      end
-    end
-
-  protected def {u} lt_ge_by_cases {a b : ℕ} {C : Type u} (h₁ : a < b → C) (h₂ : a ≥ b → C) : C :=
-  decidable.by_cases h₁ (λ h, h₂ (or.elim (nat.lt_or_ge a b) (λ a, absurd a h) (λ a, a)))
-
-  protected def {u} lt_by_cases {a b : ℕ} {C : Type u} (h₁ : a < b → C) (h₂ : a = b → C)
-    (h₃ : b < a → C) : C :=
-  nat.lt_ge_by_cases h₁ (λ h₁,
-    nat.lt_ge_by_cases h₃ (λ h, h₂ (nat.le_antisymm h h₁)))
-
-  protected lemma lt_trichotomy (a b : ℕ) : a < b ∨ a = b ∨ b < a :=
-  nat.lt_by_cases (λ h, or.inl h) (λ h, or.inr (or.inl h)) (λ h, or.inr (or.inr h))
-
-  protected lemma eq_or_lt_of_not_lt {a b : ℕ} (hnlt : ¬ a < b) : a = b ∨ b < a :=
-  or.elim (nat.lt_trichotomy a b)
-    (λ hlt, absurd hlt hnlt)
-    (λ h, h)
-
-  lemma lt_of_succ_le {a b : ℕ} (h : succ a ≤ b) : a < b := h
-
-  lemma succ_le_of_lt {a b : ℕ} (h : a < b) : succ a ≤ b := h
-
-  lemma sub_eq_succ_sub_succ (a b : ℕ) : a - b = succ a - succ b :=
-  eq.symm (succ_sub_succ_eq_sub a b)
-
-  lemma zero_sub_eq_zero : ∀ a : ℕ, 0 - a = 0
-  | 0     := rfl
-  | (a+1) := congr_arg pred (zero_sub_eq_zero a)
-
-  lemma zero_eq_zero_sub (a : ℕ) : 0 = 0 - a :=
-  eq.symm (zero_sub_eq_zero a)
-
-  lemma sub_le_iff_true (a b : ℕ) : a - b ≤ a ↔ true :=
-  iff_true_intro (sub_le a b)
-
-  lemma sub_lt_succ (a b : ℕ) : a - b < succ a :=
-  lt_succ_of_le (sub_le a b)
-
-  lemma sub_lt_succ_iff_true (a b : ℕ) : a - b < succ a ↔ true :=
-  iff_true_intro (sub_lt_succ a b)
-
-  lemma le_add_right : ∀ (n k : ℕ), n ≤ n + k
-  | n 0     := nat.le_refl n
-  | n (k+1) := le_succ_of_le (le_add_right n k)
-
-  lemma le_add_left (n m : ℕ): n ≤ m + n :=
-  nat.add_comm n m ▸ le_add_right n m
-
-  lemma le.elim : ∀ {n m : ℕ}, n ≤ m → ∃ k, n + k = m
-  | n .n        (le.nat_refl .n)  := ⟨0, rfl⟩
-  | n .(succ m) (@le.step .n m h) :=
-    match le.elim h with
-    | ⟨w, hw⟩ := ⟨succ w, hw ▸ add_succ n w⟩
-    end
-
-  lemma le.intro {n m k : ℕ} (h : n + k = m) : n ≤ m :=
-  h ▸ le_add_right n k
-
-  protected lemma add_le_add_left {n m : ℕ} (h : n ≤ m) (k : ℕ) : k + n ≤ k + m :=
-  match le.elim h with
-  | ⟨w, hw⟩ := @le.intro _ _ w begin rw [nat.add_assoc, hw] end
-  end
-
-  protected lemma add_le_add_right {n m : ℕ} (h : n ≤ m) (k : ℕ) : n + k ≤ m + k :=
-  begin rw [nat.add_comm n k, nat.add_comm m k], apply nat.add_le_add_left h end
-
-  protected lemma le_of_add_le_add_left {k n m : ℕ} (h : k + n ≤ k + m) : n ≤ m :=
-  match le.elim h with
-  | ⟨w, hw⟩ := @le.intro _ _ w
-    begin
-      dsimp at hw,
-      rw [nat.add_assoc] at hw,
-      apply nat.add_left_cancel hw
-    end
-  end
-
-  protected lemma lt_of_le_and_ne {m n : ℕ} (h1 : m ≤ n) : m ≠ n → m < n :=
-  or.resolve_right (or.swap (nat.eq_or_lt_of_le h1))
-
-  protected theorem lt_of_add_lt_add_left {k n m : ℕ} (h : k + n < k + m) : n < m :=
-  let h' := nat.le_of_lt h in
-  nat.lt_of_le_and_ne
-    (nat.le_of_add_le_add_left h')
-    (λ heq, nat.lt_irrefl (k + m) begin rw heq at h, assumption end)
-
-  protected lemma add_lt_add_left {n m : ℕ} (h : n < m) (k : ℕ) : k + n < k + m :=
-  lt_of_succ_le (add_succ k n ▸ nat.add_le_add_left (succ_le_of_lt h) k)
-
-  protected lemma add_lt_add_right {n m : ℕ} (h : n < m) (k : ℕ) : n + k < m + k :=
-  nat.add_comm k m ▸ nat.add_comm k n ▸ nat.add_lt_add_left h k
-
-  protected lemma lt_add_of_pos_right {n k : ℕ} (h : k > 0) : n < n + k :=
-  nat.add_lt_add_left h n
-
-  protected lemma zero_lt_one : 0 < (1:nat) :=
-  zero_lt_succ 0
-
   protected lemma zero_lt_bit1 (n : nat) : 0 < bit1 n :=
   zero_lt_succ _
 
@@ -463,5 +510,16 @@ namespace nat
       apply succ_lt_succ,
       apply zero_lt_succ
     end
+
+  protected lemma one_le_bit1 (n : ℕ) : 1 ≤ bit1 n :=
+  show 1 ≤ succ (bit0 n), from
+  succ_le_succ (zero_le (bit0 n))
+
+  protected lemma one_le_bit0 : ∀ (n : ℕ), n ≠ 0 → 1 ≤ bit0 n
+  | 0     h := absurd rfl h
+  | (n+1) h :=
+    suffices 1 ≤ succ (succ (bit0 n)), from
+      eq.symm (nat.bit0_succ_eq n) ▸ this,
+    succ_le_succ (zero_le (succ (bit0 n)))
 
 end nat

40d3bc9b435e766a6435aa7daa2275a818f03b03
Leonardo de Moura <leonardo@microsoft.com>
2016-12-01 17:18:15 -0800

    feat(tests/lean/1207): add the other example used in the issue #1207

--- tests/lean/1207.lean
+++ tests/lean/1207.lean
@@ -21,3 +21,10 @@ begin
   assertv H : true := (by foo), -- ERROR
   exact sorry
 end
+
+constant P : Prop
+example (p : P) : true :=
+begin
+  assertv H : P := by do { p ← tactic.get_local `p, tactic.exact p },
+  trivial
+end

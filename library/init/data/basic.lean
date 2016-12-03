e4285bf68470e678d88cdedf3dd154bba5e1a28c
Leonardo de Moura <leonardo@microsoft.com>
2016-12-02 16:29:15 -0800

    refactor(library/init): list classes => instances

--- library/init/data/basic.lean
+++ library/init/data/basic.lean
@@ -4,6 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 prelude
-import init.data.setoid init.data.quot init.data.bool.basic init.data.unit init.data.nat.basic init.data.prod init.data.sum.basic
-import init.data.num.basic init.data.sigma.basic init.data.subtype init.data.fin.basic init.data.list.basic init.data.char.basic init.data.string.basic
-import init.data.option.basic init.data.set init.data.unsigned init.data.ordering init.data.list.classes init.data.to_string
+import init.data.setoid init.data.quot init.data.bool.basic init.data.unit
+import init.data.nat.basic init.data.prod init.data.sum.basic
+import init.data.num.basic init.data.sigma.basic init.data.subtype
+import init.data.fin.basic init.data.list.basic init.data.char.basic
+import init.data.string.basic init.data.option.basic init.data.set
+import init.data.unsigned init.data.ordering init.data.to_string

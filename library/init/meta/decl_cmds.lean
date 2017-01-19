0ad053f0f1b0b3a270847dad50c4b8a5d59bd30c
Leonardo de Moura <leonardo@microsoft.com>
2017-01-18 17:52:11 -0800

    feat(library/init/meta/decl_cmds): add command for copying type and value using replacement

--- library/init/meta/decl_cmds.lean
+++ library/init/meta/decl_cmds.lean
@@ -7,6 +7,17 @@ prelude
 import init.meta.tactic init.meta.rb_map
 open tactic
 
+private meta def apply_replacement (replacements : name_map name) (e : expr) : expr :=
+e^.replace (λ e d,
+  match e with
+  | expr.const n ls :=
+    match replacements^.find n with
+    | some new_n := some (expr.const new_n ls)
+    | none       := none
+    end
+  | _ := none
+  end)
+
 /- Given a set of constant renamings `replacements` and a declaration name `src_decl_name`, create a new
    declaration called `new_decl_name` s.t. its type is the type of `src_decl_name` after applying the
    given constant replacement.
@@ -23,20 +34,19 @@ open tactic
    Then, the command
         run_command copy_decl_updating_type M `f_lemma `g_lemma
    creates the declaration
-        lemma g_lemma : forall a, g a > 0 := ...
--/
+        lemma g_lemma : forall a, g a > 0 := ... -/
 meta def copy_decl_updating_type (replacements : name_map name) (src_decl_name : name) (new_decl_name : name) : command :=
-do env      ← get_env,
-   decl     ← returnex $ env^.get src_decl_name,
-   new_type ← return $ decl^.type^.replace (λ e d,
-              match e with
-              | expr.const n ls :=
-                match replacements^.find n with
-                | some new_n := some (expr.const new_n ls)
-                | none       := none
-                end
-              | _ := none
-              end),
+do env       ← get_env,
+   decl      ← returnex $ env^.get src_decl_name,
+   new_type  ← return $ apply_replacement replacements decl^.type,
    new_value ← return $ expr.const src_decl_name (decl^.univ_params^.for level.param),
+   add_decl (((decl^.to_definition^.update_type new_type)^.update_name new_decl_name)^.update_value new_value),
+   return ()
+
+meta def copy_decl_using (replacements : name_map name) (src_decl_name : name) (new_decl_name : name) : command :=
+do env       ← get_env,
+   decl      ← returnex $ env^.get src_decl_name,
+   new_type  ← return $ apply_replacement replacements decl^.type,
+   new_value ← return $ apply_replacement replacements decl^.value,
    add_decl (((decl^.to_definition^.update_type new_type)^.update_name new_decl_name)^.update_value new_value),
    return ()

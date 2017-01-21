9c9cad6ae8020dd16e15097716072f146811a965
Leonardo de Moura <leonardo@microsoft.com>
2017-01-20 21:48:19 -0800

    test(tests/lean/interactive): add regression test for #1313

---
+++ tests/lean/interactive/1313.lean
@@ -0,0 +1,34 @@
+example (p q : Prop) (a : p) (b : q) : p ∧ q ∧ p :=
+begin
+
+--^ "command": "info"
+end
+
+
+example (p q : Prop) (a : p) (b : q) : p ∧ q ∧ p :=
+begin
+apply and.intro,
+             --^ "command": "info"
+end
+
+example (p q : Prop) (a : p) (b : q) : p ∧ q ∧ p :=
+begin
+apply and.intro,
+               --^ "command": "info"
+end
+
+example (p q : Prop) (a : p) (b : q) : p ∧ q ∧ p :=
+begin
+apply and.intro,
+
+--^ "command": "info"
+end
+
+
+example (p q : Prop) (a : p) (b : q) : p ∧ q ∧ p :=
+begin
+apply and.intro,
+
+--^ "command": "info"
+{exact a},
+end

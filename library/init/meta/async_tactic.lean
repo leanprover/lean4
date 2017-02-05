1eb771f5a1ba9513c5d03058de4187a6c890b204
Leonardo de Moura <leonardo@microsoft.com>
2017-02-05 14:47:43 -0800

    chore(library/init/meta/async_tactic): missing copyright

--- library/init/meta/async_tactic.lean
+++ library/init/meta/async_tactic.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2017 Gabriel Ebner. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gabriel Ebner
+-/
 prelude
 import init.meta.tactic
 import init.meta.interactive

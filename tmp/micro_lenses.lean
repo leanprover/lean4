300f6b23444fd693bc245e43f086147a6bff114c
Leonardo de Moura <leonardo@microsoft.com>
2017-03-10 18:42:15 -0800

    chore(tmp/micro_lenses): update

--- tmp/micro_lenses.lean
+++ tmp/micro_lenses.lean
@@ -1,4 +1,4 @@
-xxxxxxxxxxxxxxxbbuniverses u v w
+universes u v w
 
 structure lens (α : Type u) (β : Type v) :=
 (get    : α → β)
@@ -46,13 +46,13 @@ set_option trace.array.update true
 def f (a : array nat 10 × array bool 5) : array nat 10 × array bool 5 :=
 (idx 2 ∙ snd)^.set ((idx 1 ∙ fst)^.set a 1) ff
 
-vm_eval f (mk_array 10 0, mk_array 5 tt)
+#eval f (mk_array 10 0, mk_array 5 tt)
 
-vm_eval (idx 2 ∙ snd)^.set ((idx 1 ∙ fst)^.set (mk_array 10 0, mk_array 5 tt) 1) ff
+#eval (idx 2 ∙ snd)^.set ((idx 1 ∙ fst)^.set (mk_array 10 0, mk_array 5 tt) 1) ff
 
-vm_eval let p₀ := (mk_array 10 0, mk_array 5 tt),
-            p₁ := (idx 1 ∙ fst)^.set p₀ 1,
-            p₂ := (idx 2 ∙ snd)^.set p₁ ff in
+#eval let p₀ := (mk_array 10 0, mk_array 5 tt),
+          p₁ := (idx 1 ∙ fst)^.set p₀ 1,
+          p₂ := (idx 2 ∙ snd)^.set p₁ ff in
         p₂
 
 example : (fst ∙ nth 1)^.set [(1, 2), (3, 4), (0, 3)] 30 = [(1, 2), (30, 4), (0, 3)] :=
@@ -60,3 +60,34 @@ rfl
 
 example : (snd ∙ nth 1)^.get [(1, 2), (3, 4), (0, 3)] = 4 :=
 rfl
+
+def micro_lens (f : Type u → Type w) [applicative f] (α β : Type u) :=
+(β → f β) → α → β × f α
+
+structure identity (α : Type u) :=
+(val : α)
+
+instance : applicative identity :=
+{pure := λ _ a, ⟨a⟩,
+ seq  := λ _ _ f a, ⟨f^.val a^.val⟩}
+
+def micro_lens.get {α β : Type u} (l : micro_lens identity α β) (a : α) : β :=
+(l (λ b, pure b) a).1
+
+def micro_lens.set {f : Type u → Type w} [applicative f] {α β : Type u} (l : micro_lens f α β) (a : α) (b : f β) : f α :=
+(l (λ _, b) a).2
+
+def micro_lens.modify {f : Type u → Type w} [applicative f] {α β : Type u} (l : micro_lens f α β) (a : α) (b : β → f β) : f α :=
+(l b a).2
+
+def micro_lens.compose {f : Type u → Type w} [applicative f] {α β δ: Type u} (l₁ : micro_lens f β δ) (l₂ : micro_lens f α β) : micro_lens f α δ :=
+λ g a,
+  let (b, r) := l₂ (λ b : β, (l₁ g b).2) a in
+  let     d  := (l₁ g b).1 in
+  (d, r)
+
+def pi₁ {f : Type u → Type w} [applicative f] {α β : Type u} : micro_lens f (α × β) α :=
+λ g ⟨a, b⟩, (a, (λ x, (x, b)) <$> g a)
+
+def pi₂ {f : Type u → Type w} [applicative f] {α β : Type u} : micro_lens f (α × β) β :=
+λ g ⟨a, b⟩, (b, (λ x, (a, x)) <$> g b)

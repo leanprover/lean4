aa209d89e791311a35f5051c454370f003a0a188
Daniel Selsam <daniel.selsam@gmail.com>
2017-03-11 19:08:49 -0800

    fix(library/smt/prove.lean): collect_props was collecting (H : Prop)

--- library/smt/prove.lean
+++ library/smt/prove.lean
@@ -5,11 +5,14 @@ private meta def collect_props : list expr → tactic (list expr)
 | []        := return []
 | (H :: Hs) := do
   Eqs   ← collect_props Hs,
-  Htype ← infer_type H >>= whnf,
+  Htype ← infer_type H >>= infer_type >>= whnf,
   return $ if Htype = ```(Prop) then (H :: Eqs) else Eqs
 
+-- This tactic is just a placeholder, designed to be modified for specific performance experiments
 meta def prove : tactic unit :=
 do local_context >>= collect_props >>= revert_lst,
-   simp
+   trace "SMT state, after reverting propositions:",
+   trace_state,
+   simp { contextual := tt }
 
 end smt

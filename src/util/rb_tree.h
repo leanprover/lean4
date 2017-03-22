59d1b1ee2e86528d40c28e1b3a93e3fd17417055
Leonardo de Moura <leonardo@microsoft.com>
2017-03-22 08:24:17 -0700

    fix(util/rb_tree): avoid warning
    see #1473

--- src/util/rb_tree.h
+++ src/util/rb_tree.h
@@ -71,8 +71,10 @@ class rb_tree : private CMP {
     }
 
     bool check_cmp_result(T const & v1, T const & v2) const {
-        int n1 = CMP::operator()(v1, v2);
-        int n2 = CMP::operator()(v2, v1);
+        DEBUG_CODE(
+            int n1 = CMP::operator()(v1, v2);
+            int n2 = CMP::operator()(v2, v1);
+            );
         lean_assert((n1 < 0  && n2 > 0) || (n1 == 0 && n2 == 0) || (n1 > 0  && n2 < 0));
         return true;
     }

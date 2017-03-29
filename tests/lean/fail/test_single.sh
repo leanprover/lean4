3253a79e778c7b965f68a072fd894cdd459f6fd3
Gabriel Ebner <gebner@gebner.org>
2017-03-29 15:16:37 +0200

    refactor(tests/lean/fail): use test suite runner

--- tests/lean/fail/test_single.sh
+++ tests/lean/fail/test_single.sh
@@ -8,9 +8,18 @@ LEAN=$1
 export LEAN_PATH=../../../library:.
 f=$2
 echo "-- testing $f"
-if "$LEAN" -j 0 "$f"; then
-   echo "failed $f"
-   exit 1
+if [[ -f $f.status ]]; then
+    echo "-- using result from test_all.sh"
+    cat $f.test_suite.out
+    status=$(cat $f.status)
+    rm $f.test_suite.out $f.status
 else
-   echo "-- checked"
+    "$LEAN" -j 0 "$f"
+    status=$?
+fi
+if [ "$status" -eq 1 ]; then
+    echo "-- checked"
+else
+    echo "failed $f"
+    exit 1
 fi

643d68e89a4a26c3f062a25d606f41647763af0d
Sebastian Ullrich <sebasti@nullri.ch>
2017-03-30 13:40:29 +0200

    fix(emacs/lean-type): use correct function for 'sticky' eldoc messages

--- src/emacs/lean-type.el
+++ src/emacs/lean-type.el
@@ -105,7 +105,7 @@
       (when add-to-kill-ring
         (kill-new
          (substring-no-properties info-string)))
-      (message "%s" info-string))))
+      (eldoc-message "%s" info-string))))
 
 (defun lean-eldoc-documentation-function (&optional add-to-kill-ring)
   "Show information of lean expression at point if any"

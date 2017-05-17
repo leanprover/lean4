7cf848cbb22edf3b1a49e08870c783cdf6339991
Leonardo de Moura <leonardo@microsoft.com>
2017-05-17 16:06:45 -0700

    chore(tests/lean/run/wfrec1): cleanup example

--- tests/lean/run/wfrec1.lean
+++ tests/lean/run/wfrec1.lean
@@ -1,16 +1,17 @@
-private definition S := Σ a : nat, nat
-private definition R : S → S → Prop := sigma.skip_left nat (<)
-private definition Rwf : well_founded R :=
+private def S := Σ a : nat, nat
+private def R : S → S → Prop := sigma.skip_left nat (<)
+private def Rwf : well_founded R :=
   sigma.skip_left_wf nat nat.lt_wf
-private definition f_aux : ∀ (p₁ : S), (∀ p₂ : S, R p₂ p₁ → nat) → nat
-| (sigma.mk n (m+1)) F := F (sigma.mk (n+10) (m - n)) (sigma.mk_skip_left _ _ (nat.sub_lt_succ _ _))
-| (sigma.mk n 0)     F := n
 
-definition f (n m : nat) : nat :=
-well_founded.fix Rwf f_aux (sigma.mk n m)
+private def f_aux : ∀ (p₁ : S), (∀ p₂ : S, R p₂ p₁ → nat) → nat
+| ⟨n, m+1⟩ F := F ⟨n+10, m - n⟩ (sigma.mk_skip_left _ _ (nat.sub_lt_succ _ _))
+| ⟨n, 0⟩   F := n
+
+def f (n m : nat) : nat :=
+well_founded.fix Rwf f_aux ⟨n, m⟩
 
 lemma f.eq_1 (n m : nat) : f n (m+1) = f (n+10) (m - n) :=
-well_founded.fix_eq Rwf f_aux (sigma.mk n (m+1))
+well_founded.fix_eq Rwf f_aux ⟨n, m+1⟩
 
 lemma f.eq_2 (n m : nat) : f n 0 = n :=
-well_founded.fix_eq Rwf f_aux (sigma.mk n 0)
+well_founded.fix_eq Rwf f_aux ⟨n, 0⟩

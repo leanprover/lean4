1d7dd20091d8a327b0fe06677577734d9f6cc079
Leonardo de Moura <leonardo@microsoft.com>
2017-05-24 13:33:53 -0700

    feat(library/init/meta/well_founded_tactics): improve default dec_tac
    This is a temporary workaround until we have decision procedures for arithmetic.

--- library/init/meta/well_founded_tactics.lean
+++ library/init/meta/well_founded_tactics.lean
@@ -5,6 +5,17 @@ Authors: Leonardo de Moura
 -/
 prelude
 import init.meta init.data.sigma.lex init.data.nat.lemmas init.data.list.instances
+import init.data.list.qsort
+
+/- TODO(Leo): move this lemma, or delete it after we add algebraic normalizer. -/
+lemma nat.lt_add_of_zero_lt_left (a b : nat) (h : 0 < b) : a < a + b :=
+suffices a + 0 < a + b, by {simp at this, assumption},
+by {apply nat.add_lt_add_left, assumption}
+
+/- TODO(Leo): move this lemma, or delete it after we add algebraic normalizer. -/
+lemma nat.zero_lt_one_add (a : nat) : 0 < 1 + a :=
+suffices 0 < a + 1, by {simp, assumption},
+nat.zero_lt_succ _
 
 namespace well_founded_tactics
 open tactic
@@ -67,10 +78,83 @@ do
 meta def unfold_sizeof : tactic unit :=
 unfold_sizeof_measure >> unfold_sizeof_loop
 
+/- The following section should be removed as soon as we implement the
+   algebraic normalizer. -/
+section simple_dec_tac
+open tactic expr
+
+private meta def collect_add_args : expr → list expr
+| `(%%a + %%b) := collect_add_args a ++ collect_add_args b
+| e            := [e]
+
+private meta def mk_nat_add : list expr → tactic expr
+| []      := to_expr ``(0)
+| [a]     := return a
+| (a::as) := do
+  rs ← mk_nat_add as,
+  to_expr ``(%%a + %%rs)
+
+private meta def mk_nat_add_add : list expr → list expr → tactic expr
+| [] b  := mk_nat_add b
+| a  [] := mk_nat_add a
+| a  b  :=
+  do t ← mk_nat_add a,
+     s ← mk_nat_add b,
+     to_expr ``(%%t + %%s)
+
+private meta def get_add_fn (e : expr) : expr :=
+if is_napp_of e `has_add.add 4 then e.app_fn.app_fn
+else e
+
+private meta def prove_eq_by_perm (a b : expr) : tactic expr :=
+(is_def_eq a b >> to_expr ``(eq.refl %%a))
+<|>
+perm_ac (get_add_fn a) `(nat.add_assoc) `(nat.add_comm) a b
+
+private meta def num_small_lt (a b : expr) : bool :=
+if a = b then ff
+else if is_napp_of a `has_one.one 2 then tt
+else if is_napp_of b `has_one.one 2 then ff
+else a.lt b
+
+private meta def sort_args (args : list expr) : list expr :=
+args.qsort num_small_lt
+
+meta def cancel_nat_add_lt : tactic unit :=
+do `(%%lhs < %%rhs) ← target,
+   ty ← infer_type lhs >>= whnf,
+   guard (ty = `(nat)),
+   let lhs_args := collect_add_args lhs,
+   let rhs_args := collect_add_args rhs,
+   let common   := lhs_args.bag_inter rhs_args,
+   if common = [] then return ()
+   else do
+     let lhs_rest := lhs_args.diff common,
+     let rhs_rest := rhs_args.diff common,
+     new_lhs    ← mk_nat_add_add common (sort_args lhs_rest),
+     new_rhs    ← mk_nat_add_add common (sort_args rhs_rest),
+     lhs_pr     ← prove_eq_by_perm lhs new_lhs,
+     rhs_pr     ← prove_eq_by_perm rhs new_rhs,
+     target_pr  ← to_expr ``(congr (congr_arg (<) %%lhs_pr) %%rhs_pr),
+     new_target ← to_expr ``(%%new_lhs < %%new_rhs),
+     replace_target new_target target_pr,
+     `[apply nat.add_lt_add_left] <|> `[apply nat.lt_add_of_zero_lt_left]
+
+meta def trivial_nat_lt : tactic unit :=
+comp_val
+<|>
+`[apply nat.zero_lt_one_add]
+<|>
+assumption
+<|>
+fail "failed to prove function is descreasing"
+end simple_dec_tac
+
 meta def default_dec_tac : tactic unit :=
+abstract $
 do clear_internals,
    unfold_wf_rel,
-   process_lex (unfold_sizeof >> try assumption >> try `[apply nat.lt_succ_self])
+   process_lex (unfold_sizeof >> cancel_nat_add_lt >> trivial_nat_lt)
 
 end well_founded_tactics
 

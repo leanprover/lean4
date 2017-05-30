cbca84a8ead4b245c8989614ccfdc0d1a6fa2c8a
Leonardo de Moura <leonardo@microsoft.com>
2017-05-30 13:05:37 -0700

    chore(library/init/data/nat/bitwise): fix copyright date

--- library/init/data/nat/bitwise.lean
+++ library/init/data/nat/bitwise.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2014 Microsoft Corporation. All rights reserved.
+Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Mario Carneiro
 -/

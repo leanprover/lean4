81e97de463073c7511f01875075651259eb8c339
Leonardo de Moura <leonardo@microsoft.com>
2017-06-01 19:46:38 -0700

    feat(library/init/meta/interactive): allow metavars in the show_goal tactic

--- library/init/meta/interactive.lean
+++ library/init/meta/interactive.lean
@@ -856,7 +856,7 @@ tactic.done
 private meta def show_goal_aux (p : pexpr) : list expr → list expr → tactic unit
 | []      r := fail "show_goal tactic failed"
 | (g::gs) r := do
-  do {set_goals [g], g_ty ← target, ty ← i_to_expr_strict p, is_def_eq g_ty ty >> set_goals (g :: r.reverse ++ gs) }
+  do {set_goals [g], g_ty ← target, ty ← i_to_expr p, unify g_ty ty >> set_goals (g :: r.reverse ++ gs) }
   <|>
   show_goal_aux gs (g::r)
 
--- tests/lean/run/show_goal.lean
+++ tests/lean/run/show_goal.lean
@@ -24,3 +24,12 @@ begin
   show a + 0 = 0 + a, { simp },
   show 0 + b = b,     { rw [zero_add] }
 end
+
+/- Same example, but the local context of each goal is different -/
+lemma ex4 : (∀ a : nat, a + 0 = 0 + a) ∧ (∀ b : nat, 0 + b = b) ∧ (∀ b c : nat, c + b = b + c) :=
+begin
+  repeat {any_goals {constructor}}, all_goals {intros},
+  show c + b = _,     { apply add_comm },
+  show a + _ = 0 + a, { simp },
+  show _ = b,         { rw [zero_add] }
+end

e6294710db312dd431fb46e8e9e710f2609a879d
Mario Carneiro <di.gama@gmail.com>
2017-07-16 23:29:17 +0100

    refactor(init/classical): simplify classical proofs

--- library/init/classical.lean
+++ library/init/classical.lean
@@ -12,9 +12,9 @@ universes u v
 /- the axiom -/
 axiom choice {α : Sort u} : nonempty α → α
 
-noncomputable theorem indefinite_description {α : Sort u} (p : α → Prop) :
-  (∃ x, p x) → {x // p x} :=
-λ h, choice (let ⟨x, px⟩ := h in ⟨⟨x, px⟩⟩)
+noncomputable theorem indefinite_description {α : Sort u} (p : α → Prop)
+  (h : ∃ x, p x) : {x // p x} :=
+choice $ let ⟨x, px⟩ := h in ⟨⟨x, px⟩⟩
 
 noncomputable def some {α : Sort u} {p : α → Prop} (h : ∃ x, p x) : α :=
 (indefinite_description p h).val
@@ -42,18 +42,17 @@ set_option type_context.unfold_lemmas true
 private lemma u_def : U u := some_spec exU
 private lemma v_def : V v := some_spec exV
 
-private lemma not_uv_or_p : ¬(u = v) ∨ p :=
+private lemma not_uv_or_p : u ≠ v ∨ p :=
 or.elim u_def
   (assume hut : u = true,
     or.elim v_def
       (assume hvf : v = false,
-        have hne : ¬(u = v), from eq.symm hvf ▸ eq.symm hut ▸ true_ne_false,
+        have hne : u ≠ v, from hvf.symm ▸ hut.symm ▸ true_ne_false,
         or.inl hne)
-      (assume hp : p, or.inr hp))
-  (assume hp : p, or.inr hp)
+      or.inr)
+  or.inr
 
-private lemma p_implies_uv : p → u = v :=
-assume hp : p,
+private lemma p_implies_uv (hp : p) : u = v :=
 have hpred : U = V, from
   funext (assume x : Prop,
     have hl : (x = true ∨ p) → (x = false ∨ p), from
@@ -68,33 +67,31 @@ have h₀ : ∀ exU exV,
 show u = v, from h₀ _ _
 
 theorem em : p ∨ ¬p :=
-have h : ¬(u = v) → ¬p, from mt p_implies_uv,
-  or.elim not_uv_or_p
-    (assume hne : ¬(u = v), or.inr (h hne))
-    (assume hp : p, or.inl hp)
+or.elim not_uv_or_p
+  (assume hne : u ≠ v, or.inr (mt p_implies_uv hne))
+  or.inl
 end diaconescu
 
-theorem exists_true_of_nonempty {α : Sort u} (h : nonempty α) : ∃ x : α, true :=
-nonempty.elim h (assume x, ⟨x, trivial⟩)
+theorem exists_true_of_nonempty {α : Sort u} : nonempty α → ∃ x : α, true
+| ⟨x⟩ := ⟨x, trivial⟩
 
 noncomputable def inhabited_of_nonempty {α : Sort u} (h : nonempty α) : inhabited α :=
-⟨(indefinite_description _ (exists_true_of_nonempty h)).val⟩
+⟨choice h⟩
 
 noncomputable def inhabited_of_exists {α : Sort u} {p : α → Prop} (h : ∃ x, p x) :
   inhabited α :=
 inhabited_of_nonempty (exists.elim h (λ w hw, ⟨w⟩))
 
 /- all propositions are decidable -/
-noncomputable def decidable_inhabited (a : Prop) : inhabited (decidable a) :=
-inhabited_of_nonempty
-  (or.elim (em a)
-    (assume ha, ⟨is_true ha⟩)
-    (assume hna, ⟨is_false hna⟩))
-local attribute [instance] decidable_inhabited
-
 noncomputable def prop_decidable (a : Prop) : decidable a :=
-arbitrary (decidable a)
+choice $ or.elim (em a)
+  (assume ha, ⟨is_true ha⟩)
+  (assume hna, ⟨is_false hna⟩)
 local attribute [instance] prop_decidable
+
+noncomputable def decidable_inhabited (a : Prop) : inhabited (decidable a) :=
+⟨prop_decidable a⟩
+local attribute [instance] decidable_inhabited
 
 noncomputable def type_decidable_eq (α : Sort u) : decidable_eq α :=
 λ x y, prop_decidable (x = y)
@@ -106,23 +103,20 @@ match (prop_decidable (nonempty α)) with
 end
 
 noncomputable theorem strong_indefinite_description {α : Sort u} (p : α → Prop)
-  (h : nonempty α) : { x : α // (∃ y : α, p y) → p x} :=
-match (prop_decidable (∃ x : α, p x)) with
-| (is_true hp)  := let xp := indefinite_description _ hp in
-                   ⟨xp.val, λ h', xp.property⟩
-| (is_false hn) := ⟨@inhabited.default _ (inhabited_of_nonempty h), λ h, absurd h hn⟩
-end
+  (h : nonempty α) : {x : α // (∃ y : α, p y) → p x} :=
+if hp : ∃ x : α, p x then
+  let xp := indefinite_description _ hp in
+  ⟨xp.val, λ h', xp.property⟩
+else ⟨choice h, λ h, absurd h hp⟩
 
 /- the Hilbert epsilon function -/
 
 noncomputable def epsilon {α : Sort u} [h : nonempty α] (p : α → Prop) : α :=
 (strong_indefinite_description p h).val
 
-theorem epsilon_spec_aux {α : Sort u} (h : nonempty α) (p : α → Prop) (hex : ∃ y, p y) :
-    p (@epsilon α h p) :=
-have aux : (∃ y, p y) → p ((strong_indefinite_description p h).val),
-  from (strong_indefinite_description p h).property,
-aux hex
+theorem epsilon_spec_aux {α : Sort u} (h : nonempty α) (p : α → Prop) 
+  : (∃ y, p y) → p (@epsilon α h p) :=
+(strong_indefinite_description p h).property
 
 theorem epsilon_spec {α : Sort u} {p : α → Prop} (hex : ∃ y, p y) :
     p (@epsilon α (nonempty_of_exists hex) p) :=
@@ -135,21 +129,16 @@ theorem epsilon_singleton {α : Sort u} (x : α) : @epsilon α ⟨x⟩ (λ y, y 
 
 theorem axiom_of_choice {α : Sort u} {β : α → Sort v} {r : Π x, β x → Prop} (h : ∀ x, ∃ y, r x y) :
   ∃ (f : Π x, β x), ∀ x, r x (f x) :=
-have h : ∀ x, r x (some (h x)), from assume x, some_spec (h x),
-⟨_, h⟩
+⟨_, λ x, some_spec (h x)⟩
 
 theorem skolem {α : Sort u} {b : α → Sort v} {p : Π x, b x → Prop} :
-  (∀ x, ∃ y, p x y) ↔ ∃ (f : Π x, b x) , (∀ x, p x (f x)) :=
-iff.intro
-  (assume h : (∀ x, ∃ y, p x y), axiom_of_choice h)
-  (assume h : (∃ (f : Π x, b x), (∀ x, p x (f x))),
-    assume x, exists.elim h (λ (fw : ∀ x, b x) (hw : ∀ x, p x (fw x)),
-      ⟨fw x, hw x⟩))
+  (∀ x, ∃ y, p x y) ↔ ∃ (f : Π x, b x), ∀ x, p x (f x) :=
+⟨axiom_of_choice, λ ⟨f, hw⟩ x, ⟨f x, hw x⟩⟩
 
 theorem prop_complete (a : Prop) : a = true ∨ a = false :=
 or.elim (em a)
-  (λ t, or.inl (propext (iff.intro (λ h, trivial) (λ h, t))))
-  (λ f, or.inr (propext (iff.intro (λ h, absurd h f) (λ h, false.elim h))))
+  (λ t, or.inl (eq_true_intro t))
+  (λ f, or.inr (eq_false_intro f))
 
 def eq_true_or_eq_false := prop_complete
 
@@ -157,27 +146,22 @@ section aux
 attribute [elab_as_eliminator]
 theorem cases_true_false (p : Prop → Prop) (h1 : p true) (h2 : p false) (a : Prop) : p a :=
 or.elim (prop_complete a)
-  (assume ht : a = true,  eq.symm ht ▸ h1)
-  (assume hf : a = false, eq.symm hf ▸ h2)
+  (assume ht : a = true,  ht.symm ▸ h1)
+  (assume hf : a = false, hf.symm ▸ h2)
 
 theorem cases_on (a : Prop) {p : Prop → Prop} (h1 : p true) (h2 : p false) : p a :=
 cases_true_false p h1 h2 a
 
 -- this supercedes by_cases in decidable
 def by_cases {p q : Prop} (hpq : p → q) (hnpq : ¬p → q) : q :=
-or.elim (em p) (assume hp, hpq hp) (assume hnp, hnpq hnp)
+decidable.by_cases hpq hnpq
 
 -- this supercedes by_contradiction in decidable
 theorem by_contradiction {p : Prop} (h : ¬p → false) : p :=
-by_cases
-  (assume h1 : p, h1)
-  (assume h1 : ¬p, false.rec _ (h h1))
+decidable.by_contradiction h
 
 theorem eq_false_or_eq_true (a : Prop) : a = false ∨ a = true :=
-cases_true_false (λ x, x = false ∨ x = true)
-  (or.inr rfl)
-  (or.inl rfl)
-  a
+(prop_complete a).symm
 end aux
 
 end classical

7d4fe55bffdb972cb352de4cede620d673632d3c
Mario Carneiro <di.gama@gmail.com>
2017-07-20 23:19:02 +0100

    fix(tests/lean/run/choice_anon_ctor): fix test

--- tests/lean/run/choice_anon_ctor.lean
+++ tests/lean/run/choice_anon_ctor.lean
@@ -1,4 +1,9 @@
-import data.list data.vector
+constant vector : Type → nat → Type
+constant vector.nil {α} : vector α 0
+constant vector.cons {α n} : α → vector α n → vector α (nat.succ n)
+
+notation a :: b := vector.cons a b
+notation `[` l:(foldr `, ` (h t, vector.cons h t) vector.nil `]`) := l
 
 structure author :=
 (name : string)

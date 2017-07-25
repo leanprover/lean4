8571860876ff102ee0fc2a52c2ee0aaafe631ad7
Sebastian Ullrich <sebasti@nullri.ch>
2017-07-25 16:43:25 +0200

    chore(emacs/lean-dev): lean-diff-test-file: create missing files

--- src/emacs/lean-dev.el
+++ src/emacs/lean-dev.el
@@ -9,7 +9,7 @@
 (defun lean-diff-test-file ()
   "Use interactive ./test_input.sh on file of current buffer"
   (interactive)
-  (message (shell-command-to-string (format "./test_single.sh \"%s\" \"%s\" yes"
+  (message (shell-command-to-string (format "yes | ./test_single.sh \"%s\" \"%s\" yes"
                                             (lean-get-executable "lean")
                                             (buffer-name)))))
 

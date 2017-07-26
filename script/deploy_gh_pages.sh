99b4c8714d9b040094991f7247e221b9bafcc793
Sebastian Ullrich <sebasti@nullri.ch>
2017-07-26 17:13:34 +0200

    chore(.appveyor.yml,.travis.yml): simplify using glob patterns

--- script/deploy_gh_pages.sh
+++ script/deploy_gh_pages.sh
@@ -9,10 +9,8 @@ rev=$(git rev-parse --short HEAD)
 git clone -b gh-pages "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" gh-pages
 cd gh-pages
 
-# "build/lean-3.2.0-linux.tar.gz" ~> "build/lean-nightly-linux.tar.gz"
-NIGHTLY_TARGET=${TARGET/-*-/-nightly-}
 mkdir -p build
-ln -f ../$NIGHTLY_TARGET $NIGHTLY_TARGET
+ln -f ../build/lean-* build/
 
 git config user.name "Bot Botson"
 git config user.email "bot@bot.bot"

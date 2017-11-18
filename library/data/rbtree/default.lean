65368a0c853c8ec0e135844ad30664bbda39b271
Leonardo de Moura <leonardo@microsoft.com>
2017-11-17 16:12:57 -0800

    refactor(library): rbtree lemmas do not need to be in init folder

--- library/data/rbtree/default.lean
+++ library/data/rbtree/default.lean
@@ -3,8 +3,4 @@ Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
-prelude
-import init.data.rbtree.basic
-
-meta def rbtree.default_lt : tactic unit :=
-`[apply has_lt.lt]
+import data.rbtree.lemmas
--- library/data/rbtree/lemmas.lean
+++ library/data/rbtree/lemmas.lean
@@ -1,11 +1,8 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura, Mario Carneiro
+Authors: Leonardo de Moura
 -/
-prelude
-import init.data.rbtree.basic init.meta init.data.nat
-
 universes u v
 
 /- TODO(Leo): remove after we cleanup stdlib simp lemmas -/

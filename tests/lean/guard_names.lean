f1510a82c724a9f2ab4807b1b3aa70261f5303f2
Leonardo de Moura <leonardo@microsoft.com>
2017-12-10 19:30:43 -0800

    chore(tests/lean): fix tests

--- tests/lean/guard_names.lean
+++ tests/lean/guard_names.lean
@@ -6,7 +6,7 @@ constant foo {α : Type} : tree α → tree α
 
 example {α : Type} (a b : tree α) : foo a = a :=
 begin
-  guard_names { induction a },
+  with_cases { induction a },
   { admit },
   { intros l v r ih_l ih_r, trace_state, admit },
 end

e89eece98cb3b363d939ac083a6ef6b2d8400fc1
Leonardo de Moura <leonardo@microsoft.com>
2017-12-15 17:48:26 -0800

    feat(library/init): add `lean.version : nat × nat × nat`
    closes #1873

--- library/init/default.lean
+++ library/init/default.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 prelude
-import init.core init.logic init.category init.data.basic
+import init.core init.logic init.category init.data.basic init.version
 import init.propext init.cc_lemmas init.funext init.category.combinators init.function init.classical
 import init.util init.coe init.wf init.meta init.meta.well_founded_tactics init.algebra init.data
 import init.native

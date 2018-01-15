c64284a37732114d5349bfa0a92e19d7a4d2c092
Sebastian Ullrich <sebasti@nullri.ch>
2018-01-15 10:51:27 +0100

    perf(init/data/string/ops): make `string.split` linear

--- library/init/data/string/ops.lean
+++ library/init/data/string/ops.lean
@@ -26,23 +26,22 @@ by cases it; cases it_snd; simp [iterator.has_next, iterator.next_to_string, str
 end iterator
 
 -- TODO(Sebastian): generalize to something like https://doc.rust-lang.org/std/primitive.str.html#method.split
--- TODO(Sebastian): run-time quadratic in the number of occurrences because of string copies
-private def split_core (p : char → bool) : iterator → list string
-| it :=
-if h : it.has_next then
+private def split_core (p : char → bool) : iterator → iterator → list string
+| start stop :=
+if h : stop.has_next then
   -- wf hint
-  have it.next_to_string.length - 1 < it.next_to_string.length,
+  have stop.next_to_string.length - 1 < stop.next_to_string.length,
     from nat.sub_lt (iterator.zero_lt_length_next_to_string_of_has_next h) dec_trivial,
-  if p it.curr then
-    let rest := it.next.next_to_string in
-    it.prev_to_string :: split_core rest.mk_iterator
+  if p stop.curr then
+    let rest := stop.next.next_to_string in
+    (start.extract stop).get_or_else "" :: split_core stop.next stop.next
   else
-    split_core it.next
+    split_core start stop.next
 else
-  [it.to_string]
-using_well_founded { rel_tac := λ _ _, `[exact ⟨_, measure_wf (λ it, it.next_to_string.length)⟩] }
+  [start.next_to_string]
+using_well_founded { rel_tac := λ _ _, `[exact ⟨_, measure_wf (λ e, e.2.next_to_string.length)⟩] }
 
 def split (p : char → bool) (s : string) : list string :=
-split_core p s.mk_iterator
+split_core p s.mk_iterator s.mk_iterator
 
 end string

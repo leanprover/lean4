41d1293b389c63056ce5e3f9697995c8d71e79f5
Leonardo de Moura <leonardo@microsoft.com>
2018-04-10 12:33:09 -0700

    chore(*): reduce corelib

--- library/init/default.lean
+++ library/init/default.lean
@@ -5,7 +5,7 @@ Authors: Leonardo de Moura
 -/
 prelude
 import init.core init.logic init.category init.data.basic init.version
-import init.propext init.cc_lemmas init.funext init.category.combinators init.function init.classical
+import init.propext init.funext init.category.combinators init.function init.classical
 import init.util init.coe init.wf init.meta init.meta.well_founded_tactics init.algebra init.data
 
 @[user_attribute]

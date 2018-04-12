e3c1f6c7da4cbe18a8c3d1dfea8c4887d33d2640
Leonardo de Moura <leonardo@microsoft.com>
2018-04-12 09:43:36 -0700

    chore(util/numerics): remove more leftovers from Lean1

--- src/util/numerics/init_module.cpp
+++ src/util/numerics/init_module.cpp
@@ -9,17 +9,14 @@ Author: Leonardo de Moura
 #include "util/memory.h"
 #include "util/numerics/mpz.h"
 #include "util/numerics/mpq.h"
-#include "util/numerics/primes.h"
 
 namespace lean {
 void initialize_numerics_module() {
     initialize_mpz();
     initialize_mpq();
-    initialize_primes();
 }
 
 void finalize_numerics_module() {
-    finalize_primes();
     finalize_mpq();
     finalize_mpz();
 }

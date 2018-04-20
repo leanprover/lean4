ccc433876e63b81c75e2ed181b9b049100de96e4
Leonardo de Moura <leonardo@microsoft.com>
2018-04-19 17:49:16 -0700

    feat(util/lean_obj): develop lean_obj

---
+++ src/util/lean_obj.cpp
@@ -0,0 +1,107 @@
+/*
+Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Leonardo de Moura
+*/
+#include "util/lean_obj.h"
+#include "util/buffer.h"
+
+namespace lean {
+size_t obj_byte_size(lean_obj * o) {
+    switch (get_kind(o)) {
+    case lean_obj_kind::Constructor:     return cnstr_byte_size(o);
+    case lean_obj_kind::Closure:         return closure_byte_size(o);
+    case lean_obj_kind::Array:           return array_byte_size(o);
+    case lean_obj_kind::ScalarArray:     return sarray_byte_size(o);
+    case lean_obj_kind::String:          return string_byte_size(o);
+    case lean_obj_kind::MPZ:             return sizeof(lean_mpz);
+    case lean_obj_kind::External:        lean_unreachable();
+    }
+    lean_unreachable();
+}
+
+size_t obj_header_size(lean_obj * o) {
+    switch (get_kind(o)) {
+    case lean_obj_kind::Constructor:     return sizeof(lean_cnstr);
+    case lean_obj_kind::Closure:         return sizeof(lean_closure);
+    case lean_obj_kind::Array:           return sizeof(lean_array);
+    case lean_obj_kind::ScalarArray:     return sizeof(lean_sarray);
+    case lean_obj_kind::String:          return sizeof(lean_string);
+    case lean_obj_kind::MPZ:             return sizeof(lean_mpz);
+    case lean_obj_kind::External:        lean_unreachable();
+    }
+    lean_unreachable();
+}
+
+/* We use the field m_rc to implement a linked list of lean objects to be deleted.
+   This hack is safe because m_rc has type uintptr_t. */
+
+inline lean_obj * get_next(lean_obj * o) {
+    return *reinterpret_cast<lean_obj**>(o);
+}
+
+inline void set_next(lean_obj * o, lean_obj * n) {
+    *reinterpret_cast<lean_obj**>(o) = n;
+}
+
+inline void push_back(lean_obj * & todo, lean_obj * v) {
+    set_next(v, todo);
+    todo = v;
+}
+
+inline lean_obj * pop_back(lean_obj * & todo) {
+    lean_obj * r = todo;
+    todo = get_next(todo);
+    return r;
+}
+
+inline void dec_ref(lean_obj * o, lean_obj* & todo) {
+    if (!is_scalar(o) && dec_ref_core(o))
+        push_back(todo, o);
+}
+
+void del(lean_obj * o) {
+    lean_obj * todo = nullptr;
+    while (true) {
+        switch (get_kind(o)) {
+        case lean_obj_kind::Constructor: {
+            lean_obj ** it  = cnstr_obj_cptr(o);
+            lean_obj ** end = it + cnstr_num_objs(o);
+            for (; it != end; ++it) dec_ref(*it, todo);
+            free(o);
+            break;
+        }
+        case lean_obj_kind::Closure: {
+            lean_obj ** it  = closure_arg_cptr(o);
+            lean_obj ** end = it + closure_num_fixed(o);
+            for (; it != end; ++it) dec_ref(*it, todo);
+            free(o);
+            break;
+        }
+        case lean_obj_kind::Array: {
+            lean_obj ** it  = array_cptr(o);
+            lean_obj ** end = it + array_size(o);
+            for (; it != end; ++it) dec_ref(*it, todo);
+            free(o);
+            break;
+        }
+        case lean_obj_kind::ScalarArray:
+            free(o); break;
+        case lean_obj_kind::String:
+            free(o); break;
+        case lean_obj_kind::MPZ:
+            dealloc_mpz(o); break;
+        case lean_obj_kind::External:
+            dealloc_external(o); break;
+        }
+        /* We can use a counter to avoid pauses at `del` when many objects
+           are reachable from `o` need to be deleted.
+           The idea is to have a threshold on the maximum number of elements
+           that can be deleted in a single round. */
+        if (todo == nullptr)
+            return;
+        o = pop_back(todo);
+    }
+}
+}
--- src/util/lean_obj.h
+++ src/util/lean_obj.h
@@ -11,61 +11,42 @@ Author: Leonardo de Moura
 #include "util/mpz.h"
 
 namespace lean {
-
 enum class lean_obj_kind {
-    Constructor, BigConstructor,
-    Closure, Array, ScalarArray,
+    Constructor, Closure, Array, ScalarArray,
     String, MPZ, External };
 
+/* The reference counter is a uintptr_t, because at deletion time, we use this field to implement
+   a linked list of objects to be deleted. */
+typedef uintptr_t rc_type;
+
 struct lean_obj {
-    atomic<unsigned> m_rc;
-    unsigned         m_kind:8;
+    atomic<rc_type> m_rc;
+    unsigned        m_kind:16;
     lean_obj(lean_obj_kind k):m_rc(0), m_kind(static_cast<unsigned>(k)) {}
 };
 
-/* We can use regular constructor objects to represent inductive datatypes that
-   have:
-   1) At most 256 constructors
-   2) At most 256 object fields per constructor
-   3) At most 256*4 bytes for scalar/unboxed fields
+/* We can represent inductive datatypes that have:
+   1) At most 2^16 constructors
+   2) At most 2^16 - 1 object fields per constructor
+   3) At most 2^16 - 1 bytes for scalar/unboxed fields
 
    We only need m_scalar_size for implementing sanity checks at runtime.
 
-   Header size: 8 bytes in 32 and 64 bit machines. */
-struct lean_constructor : public lean_obj {
-    unsigned    m_tag:8;
-    unsigned    m_num_objs:8;
-    unsigned    m_scalar_size:8;
-
-    lean_constructor(unsigned tag, unsigned num_objs, unsigned scalar_sz):
+   Header size: 12 bytes in 32 bit machines and 16 bytes in 64 bit machines. */
+struct lean_cnstr : public lean_obj {
+    unsigned    m_tag:16;
+    unsigned    m_num_objs:16;
+    unsigned    m_scalar_size:16;
+    lean_cnstr(unsigned tag, unsigned num_objs, unsigned scalar_sz):
         lean_obj(lean_obj_kind::Constructor), m_tag(tag), m_num_objs(num_objs), m_scalar_size(scalar_sz) {}
 };
 
-/* Similar to lean_constructor, but without the restriction on the field sizes.
-
-   Remark: for the vast majority of inductive datatypes defined in Lean, we are assuming
-   we can represent their values using `lean_constructor` instead of `lean_big_constructor`.
-
-   Header size: 20 bytes in 32 bit machines, and 24 bytes in 64 bit machines. */
-struct lean_big_constructor : public lean_obj {
-    unsigned m_tag;
-    unsigned m_num_objs;
-    unsigned m_scalar_size;
-
-    lean_big_constructor(unsigned tag, unsigned num_objs, unsigned scalar_sz):
-        lean_obj(lean_obj_kind::BigConstructor), m_tag(tag), m_num_objs(num_objs), m_scalar_size(scalar_sz) {}
-};
-
 /* Array of objects.
-
-   Remark: we are assuming that this kind of array has at most 2^32 objects.
-   So, the array would consume 32Gb without counting the objects stored on it.
-
-   Header size: 16 bytes in 32 and 64 bit machines. */
+   Header size: 16 bytes in 32 bit machines and 32 bytes in 64 bit machines. */
 struct lean_array : public lean_obj {
-    unsigned m_size;
-    unsigned m_capacity;
-    lean_array(unsigned sz, unsigned c):
+    size_t   m_size;
+    size_t   m_capacity;
+    lean_array(size_t sz, size_t c):
         lean_obj(lean_obj_kind::Array), m_size(sz), m_capacity(c) {}
 };
 
@@ -73,12 +54,12 @@ struct lean_array : public lean_obj {
    We support arrays with upto 2^64 elements in 64 bit machines.
 
    The field m_elem_size is only needed for implementing sanity checks at runtime.
-   Header size: 24 bytes in 32 and 64 bit machines. */
-struct lean_scalar_array : public lean_obj {
-    unsigned m_elem_size:8;
+   Header size: 16 bytes in 32 bit machines and 32 bytes in 64 bit machines. */
+struct lean_sarray : public lean_obj {
+    unsigned m_elem_size:16; // in bytes
     size_t   m_size;
     size_t   m_capacity;
-    lean_scalar_array(unsigned esz, unsigned sz, unsigned c):
+    lean_sarray(unsigned esz, size_t sz, size_t c):
         lean_obj(lean_obj_kind::ScalarArray), m_elem_size(esz), m_size(sz), m_capacity(c) {}
 };
 
@@ -88,44 +69,43 @@ typedef lean_obj * (*lean_cfun)(lean_obj *);
    arity 1. The `apply` operator performs a cast operation.
    It casts m_fun to a C function of the right arity.
 
-   Header size: 20 bytes in 32 bit machines, 24 bytes in 64 bit machines.
-
-   Remark: we can reduce the header size to 16 bytes in 64 bit machines if
-   we assume function can have at most 255 arguments.
-   We can even support at most 2^12 arguments if we sacrifice performance and
-   use 12-bits for m_arity and m_num_args.
+   Header size: 16 bytes in 32 bit machines and 24 bytes in 64 bit machines.
 
    Note that this structure may also be used to simulate closures built
    from bytecodes. We just store an extra argument: the virtual machine
    function descriptor. We store in m_fun a pointer to a C function
    that extracts the function descriptor and then invokes the VM. */
 struct lean_closure : public lean_obj {
-    unsigned  m_arity;     /* number of arguments expected by m_fun. */
-    unsigned  m_num_fixed; /* number of arguments that have been already fixed. */
+    unsigned  m_arity:16;     // number of arguments expected by m_fun.
+    unsigned  m_num_fixed:16; // number of arguments that have been already fixed.
     lean_cfun m_fun;
     lean_closure(lean_cfun f, unsigned arity, unsigned n):
         lean_obj(lean_obj_kind::Closure), m_arity(arity), m_num_fixed(n), m_fun(f) {}
 };
 
-/* Header size: 20 bytes in 32 bit machines, 32 bytes in 64 bit machines.
+/* Header size: 16 bytes in 32 bit machines, 32 bytes in 64 bit machines.
 
    Remark: in Lean3, we have used std::string, but this adds an extra
    level of indirection. Moreover, most compilers use a reference counter
-   in the implementation. */
+   in the implementation.
+
+   It is unlikely we need support for strings with more than 2^32 bytes in 64-bit machines, but
+   we don't want to keep checking that at runtime. */
 struct lean_string : public lean_obj {
     size_t      m_size;     // in bytes
     size_t      m_capacity; // in bytes
-    size_t      m_length;   // number of unicode scalar values
+    lean_string(size_t sz, size_t c):
+        lean_obj(lean_obj_kind::String), m_size(sz), m_capacity(c) {}
 };
 
 struct lean_mpz : public lean_obj {
     mpz m_value;
+    lean_mpz(mpz const & v):
+        lean_obj(lean_obj_kind::MPZ), m_value(v) {}
 };
 
 /* Base class for wrapping external data.
-   For example, we use it to wrap the Lean environment object.
-
-   Header size: 12 bytes in 32 bit machines, 16 bytes in 64 bit machines. */
+   For example, we use it to wrap the Lean environment object. */
 struct lean_external : public lean_obj {
     virtual void dealloc() {}
     virtual ~lean_external() {}
@@ -135,37 +115,223 @@ inline bool is_scalar(lean_obj * o) { return (reinterpret_cast<uintptr_t>(o) & 1
 inline lean_obj * box(unsigned n) { return reinterpret_cast<lean_obj*>(static_cast<uintptr_t>((n << 1) | 1)); }
 inline unsigned unbox(lean_obj * o) { return reinterpret_cast<uintptr_t>(o) >> 1; }
 
-/* Generic Lean object deallocator.
+/* Generic Lean object delete operation.
 
-   The generic deallocator must be used when we are compiling:
+   The generic delete must be used when we are compiling:
    1- Polymorphic code.
    2- Code using `any` type.
       The `any` type is introduced when we translate Lean expression into the Core language based on System-F.
 
-   We are planning to generate deallocators for non-polymorphic code.
+   We are planning to generate delete operations for non-polymorphic code.
    They can be faster because:
    1- They do not need to test whether nested pointers are boxed scalars or not.
    2- They do not need to test the kind.
    3- They can unfold the loop that decrements the reference counters for nested objects.
 
    \pre !is_scalar(o); */
-void dealloc(lean_obj * o);
+void del(lean_obj * o);
 
-inline unsigned get_rc(lean_obj * o) { return atomic_load_explicit(&(o->m_rc), memory_order_acquire); }
+inline unsigned get_rc(lean_obj * o) { lean_assert(!is_scalar(o)); return atomic_load_explicit(&(o->m_rc), memory_order_acquire); }
 inline bool is_shared(lean_obj * o) { return get_rc(o) > 1; }
-inline void inc_ref(lean_obj * o) { atomic_fetch_add_explicit(&o->m_rc, 1u, memory_order_relaxed); }
-inline void dec_shared_ref(lean_obj * o) { lean_assert(is_shared(o)); atomic_fetch_sub_explicit(&o->m_rc, 1u, memory_order_acq_rel); }
-inline bool dec_ref_core(lean_obj * o) { lean_assert(get_rc(o) > 0); return atomic_fetch_sub_explicit(&o->m_rc, 1u, memory_order_acq_rel) == 1; }
-inline void dec_ref(lean_obj * o) { if (dec_ref_core(o)) dealloc(o); }
+inline void inc_ref(lean_obj * o) { atomic_fetch_add_explicit(&o->m_rc, static_cast<rc_type>(1), memory_order_relaxed); }
+inline void dec_shared_ref(lean_obj * o) { lean_assert(is_shared(o)); atomic_fetch_sub_explicit(&o->m_rc, static_cast<rc_type>(1), memory_order_acq_rel); }
+inline bool dec_ref_core(lean_obj * o) { lean_assert(get_rc(o) > 0); return atomic_fetch_sub_explicit(&o->m_rc, static_cast<rc_type>(1), memory_order_acq_rel) == 1; }
+inline void dec_ref(lean_obj * o) { if (dec_ref_core(o)) del(o); }
+
+/* Testers */
+inline lean_obj_kind get_kind(lean_obj * o) { return static_cast<lean_obj_kind>(o->m_kind); }
+inline bool is_cnstr(lean_obj * o) { return get_kind(o) == lean_obj_kind::Constructor; }
+inline bool is_closure(lean_obj * o) { return get_kind(o) == lean_obj_kind::Closure; }
+inline bool is_array(lean_obj * o) { return get_kind(o) == lean_obj_kind::Array; }
+inline bool is_sarray(lean_obj * o) { return get_kind(o) == lean_obj_kind::ScalarArray; }
+inline bool is_string(lean_obj * o) { return get_kind(o) == lean_obj_kind::String; }
+inline bool is_mpz(lean_obj * o) { return get_kind(o) == lean_obj_kind::MPZ; }
+inline bool is_external(lean_obj * o) { return get_kind(o) == lean_obj_kind::External; }
+
+/* Casting */
+inline lean_cnstr * to_cnstr(lean_obj * o) { lean_assert(is_cnstr(o)); return static_cast<lean_cnstr*>(o); }
+inline lean_closure * to_closure(lean_obj * o) { lean_assert(is_closure(o)); return static_cast<lean_closure*>(o); }
+inline lean_array * to_array(lean_obj * o) { lean_assert(is_array(o)); return static_cast<lean_array*>(o); }
+inline lean_sarray * to_sarray(lean_obj * o) { lean_assert(is_sarray(o)); return static_cast<lean_sarray*>(o); }
+inline lean_string * to_string(lean_obj * o) { lean_assert(is_string(o)); return static_cast<lean_string*>(o); }
+inline lean_mpz * to_mpz(lean_obj * o) { lean_assert(is_mpz(o)); return static_cast<lean_mpz*>(o); }
+inline lean_external * to_external(lean_obj * o) { lean_assert(is_external(o)); return static_cast<lean_external*>(o); }
 
 /* Low-level operations for allocating lean objects.
-
    They do not initialize nested objects and scalar values. */
-inline lean_obj * alloc_constructor(unsigned tag, unsigned num_objs, unsigned scalar_sz) {
-    return new (malloc(sizeof(lean_constructor) + num_objs * sizeof(lean_obj *) + scalar_sz * 4)) lean_constructor(tag, num_objs, scalar_sz);
+
+inline lean_obj * alloc_cnstr(unsigned tag, unsigned num_objs, unsigned scalar_sz) {
+    lean_assert(tag < 65536 && num_objs < 65536 && scalar_sz < 65536);
+    return new (malloc(sizeof(lean_cnstr) + num_objs * sizeof(lean_obj *) + scalar_sz)) lean_cnstr(tag, num_objs, scalar_sz);
 }
 
-inline lean_obj * alloc_big_constructor(unsigned tag, unsigned num_objs, unsigned scalar_sz) {
-    return new (malloc(sizeof(lean_big_constructor) + num_objs * sizeof(lean_obj *) + scalar_sz * 4)) lean_big_constructor(tag, num_objs, scalar_sz);
+inline lean_obj * alloc_array(unsigned size, unsigned capacity) {
+    return new (malloc(sizeof(lean_array) + capacity * sizeof(lean_obj *))) lean_array(size, capacity);
 }
+
+inline lean_obj * alloc_sarray(unsigned elem_size, size_t size, size_t capacity) {
+    return new (malloc(sizeof(lean_sarray) + capacity * elem_size)) lean_sarray(elem_size, size, capacity);
+}
+
+inline lean_obj * alloc_closure(lean_cfun fun, unsigned arity, unsigned num_fixed) {
+    lean_assert(arity > 0);
+    lean_assert(num_fixed < arity);
+    /* We reserve enough space for storing (arity - 1) arguments.
+       So, we may fix extra arguments without allocating extra memory when the closure object is not shared. */
+    return new (malloc(sizeof(lean_closure) + (arity - 1) * sizeof(lean_obj *))) lean_closure(fun, arity, num_fixed);
+}
+
+inline lean_obj * alloc_string(unsigned size, unsigned capacity) {
+    return new (malloc(sizeof(lean_string) + capacity)) lean_string(size, capacity);
+}
+
+inline lean_obj * alloc_mpz(mpz const & m) {
+    return new lean_mpz(m);
+}
+
+/* The memory associated with all Lean objects but mpz and external can be deallocated using `free`. */
+inline void dealloc_mpz(lean_obj * o) { delete to_mpz(o); }
+inline void dealloc_external(lean_obj * o) { delete to_external(o); }
+
+/* Getters */
+inline unsigned cnstr_num_objs(lean_obj * o) { return to_cnstr(o)->m_num_objs; }
+inline unsigned cnstr_scalar_size(lean_obj * o) { return to_cnstr(o)->m_scalar_size; }
+inline size_t cnstr_byte_size(lean_obj * o) { return sizeof(lean_cnstr) + cnstr_num_objs(o)*sizeof(lean_obj*) + cnstr_scalar_size(o); }
+
+inline size_t array_size(lean_obj * o) { return to_array(o)->m_size; }
+inline size_t array_capacity(lean_obj * o) { return to_array(o)->m_capacity; }
+inline size_t array_byte_size(lean_obj * o) { return sizeof(lean_array) + array_capacity(o)*sizeof(lean_obj*); }
+
+inline unsigned sarray_elem_size(lean_obj * o) { return to_sarray(o)->m_elem_size; }
+inline size_t sarray_size(lean_obj * o) { return to_sarray(o)->m_size; }
+inline size_t sarray_capacity(lean_obj * o) { return to_sarray(o)->m_capacity; }
+inline size_t sarray_byte_size(lean_obj * o) { return sizeof(lean_sarray) + sarray_capacity(o)*sarray_elem_size(o); }
+
+inline lean_cfun closure_fun(lean_obj * o) { return to_closure(o)->m_fun; }
+inline unsigned closure_arity(lean_obj * o) { return to_closure(o)->m_arity; }
+inline unsigned closure_num_fixed(lean_obj * o) { return to_closure(o)->m_num_fixed; }
+inline size_t closure_byte_size(lean_obj * o) { return sizeof(lean_closure) + (closure_arity(o) - 1)*sizeof(lean_obj*); }
+
+inline size_t string_size(lean_obj * o) { return to_string(o)->m_size; }
+inline size_t string_capacity(lean_obj * o) { return to_string(o)->m_capacity; }
+inline size_t string_byte_size(lean_obj * o) { return sizeof(lean_string) + string_size(o)* sizeof(lean_obj*); }
+
+inline mpz const & mpz_value(lean_obj * o) { return to_mpz(o)->m_value; }
+
+/* Size of the object in bytes. This function is used for debugging purposes.
+   \pre !is_scalar(o) && !is_external(o) */
+size_t obj_byte_size(lean_obj * o);
+/* Size of the object header in bytes. This function is used for debugging purposes.
+   \pre !is_scalar(o) && !is_external(o) */
+size_t obj_header_size(lean_obj * o);
+
+/* Retrieves data of type `T` stored offset bytes inside of `o` */
+template<typename T>
+inline T obj_data(lean_obj * o, size_t offset) {
+    lean_assert(obj_header_size(o) <= offset);
+    lean_assert(offset + sizeof(T) <= obj_byte_size(o));
+    return *(reinterpret_cast<T *>(reinterpret_cast<char *>(o) + offset));
+}
+
+inline lean_obj * cnstr_obj(lean_obj * o, unsigned i) {
+    lean_assert(i < cnstr_num_objs(o));
+    return obj_data<lean_obj*>(o, sizeof(lean_cnstr) + sizeof(lean_obj*)*i);
+}
+
+inline lean_obj ** cnstr_obj_cptr(lean_obj * o) {
+    lean_assert(is_cnstr(o));
+    return reinterpret_cast<lean_obj**>(reinterpret_cast<char*>(o) + sizeof(lean_cnstr));
+}
+
+inline lean_obj * array_obj(lean_obj * o, size_t i) {
+    lean_assert(i < array_size(o));
+    return obj_data<lean_obj*>(o, sizeof(lean_array) + sizeof(lean_obj*)*i);
+}
+
+inline lean_obj ** array_cptr(lean_obj * o) {
+    lean_assert(is_array(o));
+    return reinterpret_cast<lean_obj**>(reinterpret_cast<char*>(o) + sizeof(lean_array));
+}
+
+template<typename T>
+inline T * sarray_cptr(lean_obj * o) {
+    lean_assert(is_sarray(o));
+    lean_assert(sarray_elem_size(o) == sizeof(T));
+    return reinterpret_cast<T*>(reinterpret_cast<char*>(o) + sizeof(lean_sarray));
+}
+
+inline lean_obj * closure_arg(lean_obj * o, unsigned i) {
+    lean_assert(i < closure_num_fixed(o));
+    return obj_data<lean_obj*>(o, sizeof(lean_closure) + sizeof(lean_obj*)*i);
+}
+
+inline lean_obj ** closure_arg_cptr(lean_obj * o) {
+    lean_assert(is_closure(o));
+    return reinterpret_cast<lean_obj**>(reinterpret_cast<char*>(o) + sizeof(lean_closure));
+}
+
+inline char const * string_cstr(lean_obj * o) {
+    lean_assert(is_string(o));
+    return reinterpret_cast<char *>(o) + sizeof(lean_string);
+}
+
+/* Low level setters.
+   Remark: the set_*_obj procedures do *NOT* update reference counters. */
+
+template<typename T>
+inline void set_obj_data(lean_obj * o, size_t offset, T v) {
+    lean_assert(!is_shared(o));
+    lean_assert(obj_header_size(o) <= offset);
+    lean_assert(offset + sizeof(T) <= obj_byte_size(o));
+    *(reinterpret_cast<T *>(reinterpret_cast<char *>(o) + offset)) = v;
+}
+
+inline void set_cnstr_obj(lean_obj * o, unsigned i, lean_obj * v) {
+    lean_assert(i < cnstr_num_objs(o));
+    set_obj_data(o, sizeof(lean_cnstr) + sizeof(lean_obj*)*i, v);
+}
+
+inline void set_array_size(lean_obj * o, size_t sz) {
+    lean_assert(is_array(o));
+    lean_assert(!is_shared(o));
+    lean_assert(sz <= array_capacity(o));
+    to_array(o)->m_size = sz;
+}
+
+inline void set_array_obj(lean_obj * o, size_t i, lean_obj * v) {
+    lean_assert(i < array_size(o));
+    set_obj_data(o, sizeof(lean_array) + sizeof(lean_obj*)*i, v);
+}
+
+inline void set_sarray_size(lean_obj * o, size_t sz) {
+    lean_assert(is_sarray(o));
+    lean_assert(!is_shared(o));
+    lean_assert(sz <= sarray_capacity(o));
+    to_sarray(o)->m_size = sz;
+}
+
+inline void set_closure_num_fixed(lean_obj * o, unsigned n) {
+    lean_assert(is_closure(o));
+    lean_assert(!is_shared(o));
+    lean_assert(n >= closure_num_fixed(o));
+    lean_assert(n < closure_arity(o));
+    to_closure(o)->m_num_fixed = n;
+}
+
+inline void set_closure_arg(lean_obj * o, unsigned i, lean_obj * a) {
+    lean_assert(i < closure_num_fixed(o));
+    set_obj_data(o, sizeof(lean_closure) + sizeof(lean_obj*)*i, a);
+}
+
+/* Constructors */
+
+inline lean_obj * mk_mpz(mpz const & m) { return alloc_mpz(m); }
+
+lean_obj * mk_string(char const * s);
+lean_obj * mk_string(std::string const & s);
+
+lean_obj * string_shrink_to_fit(lean_obj * s);
+lean_obj * array_shrink_to_fit(lean_obj * a);
+lean_obj * sarray_shrink_to_fit(lean_obj * a);
+lean_obj * shrink_to_fit(lean_obj * o);
 }

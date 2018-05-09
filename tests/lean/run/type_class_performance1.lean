14e19502f07b3c6c14bcad6d37d817cc3979f053
Leonardo de Moura <leonardo@microsoft.com>
2018-05-09 10:24:45 -0700

    chore(tests/lean/run/type_class_performance1): remove `#exit`

--- tests/lean/run/type_class_performance1.lean
+++ tests/lean/run/type_class_performance1.lean
@@ -1,15 +1,8 @@
 #print usize
 
-set_option pp.all true
-set_option trace.class_instances true
-set_option trace.type_context.is_def_eq true
-set_option trace.type_context.is_def_eq_detail true
-
 def foo1 (a b : uint64) : bool :=
 a = b
 
-
-#exit
 def foo2 (a b : uint16) : bool :=
 a = b
 

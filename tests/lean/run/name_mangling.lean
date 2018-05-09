d05e93f7637bb13afeb8d80298d8be3c26499765
Leonardo de Moura <leonardo@microsoft.com>
2018-05-09 10:10:00 -0700

    test(tests/lean/run/name_mangling): add tests for name mangling

---
+++ tests/lean/run/name_mangling.lean
@@ -0,0 +1,19 @@
+import init.lean.name_mangling init.lean.parser.identifier
+open lean lean.parser
+open tactic
+
+meta def check (s : string) : tactic unit :=
+match parse identifier s with
+| except.ok n     :=
+  trace (name.mangle n) >>
+  if name.demangle (name.mangle n) = some n then exact `(true)
+  else trace ("mangling failed at " ++ s) >> exact `(false)
+| except.error ex := trace (ex.to_string s) >> exact `(false)
+
+example : by check "_αβ"                 := trivial
+example : by check "αβ"                  := trivial
+example : by check "nat.add"             := trivial
+example : by check "test.bla.foo"        := trivial
+example : by check "t21'αβ._₁._12"       := trivial
+example : by check "nat"                 := trivial
+example : by check "nat.equations._eqn1" := trivial

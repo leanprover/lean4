b0a40666956bd42e53f574480595d8a6da7c22b7
Leonardo de Moura <leonardo@microsoft.com>
2018-05-14 17:21:55 -0700

    test(tests/ir): add basic script for compiling IR definitions
    The support libraries are currently hard coded.

---
+++ tests/ir/lirc.lean
@@ -0,0 +1,13 @@
+import system.io
+import init.lean.ir.lirc
+open lean.ir io
+
+def main : io unit :=
+do args ← io.cmdline_args,
+   unless (args.length = 1) $
+     io.fail "Error: incorrect number of arguments, expected `lirc file.lean`",
+   let fname := args.head,
+   input ← fs.read_file fname,
+   match lirc input with
+   | except.ok r    := fs.write_file (fname ++ ".cpp") r
+   | except.error e := io.fail (to_string e)

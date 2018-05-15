168e7fa0cd2b811975c446fad3b4a519aefab04d
Leonardo de Moura <leonardo@microsoft.com>
2018-05-14 17:27:25 -0700

    chore(runtime/lean_obj): style

--- src/runtime/lean_obj.cpp
+++ src/runtime/lean_obj.cpp
@@ -5,6 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #include <iostream>
+#include <string>
 #include "runtime/lean_obj.h"
 #include "runtime/utf8.h"
 

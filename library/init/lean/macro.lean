a7760631dc3688218478b77310c6c69e2387dd57
Leonardo de Moura <leonardo@microsoft.com>
2018-05-24 17:39:10 -0700

    chore(library/init/lean/macro): document macros in use

---
+++ library/init/lean/macro.lean
@@ -0,0 +1,43 @@
+prelude
+import init.lean.name
+
+/-
+This is a temporary file to document all macros we have in Lean3.
+Macros will not be part of Lean4, but we need to keep them until
+we can bootstrap Lean4.
+-/
+namespace lean
+
+structure equations_header :=
+(num_fns          : nat)
+(fn_names         : list name)
+(fn_actual_names  : list name)
+(is_private       : bool)
+(is_lemma         : bool)
+(is_meta          : bool)
+(is_noncomputable : bool)
+(aux_lemmas       : bool)
+(prev_errors      : bool)
+(gen_code         : bool)
+
+/-
+Cases missing:
+1- quote
+2- projection
+-/
+
+inductive macro_definition
+| nat_lit         : nat → macro_definition
+| string_lit      : string → macro_definition
+| struct_instance : name → bool → list name → macro_definition
+| field_notation  : name → nat → macro_definition
+| rec_fn          : name → macro_definition
+| annotation      : name → macro_definition
+| delayed_abst    : list name → macro_definition
+| choice          : macro_definition
+| as_pattern      : macro_definition
+| equations       : equations_header → macro_definition
+| equation        : bool → macro_definition
+| no_equation     : bool → macro_definition
+
+end lean

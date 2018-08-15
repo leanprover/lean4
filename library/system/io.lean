e7e98f1134ebbeea089dff79171cc6787a461c2a
Sebastian Ullrich <sebasti@nullri.ch>
2018-08-15 09:36:12 -0700

    refactor(library/vm/vm_io,library/system/io): remove io classes, make errors explicit

--- library/system/io.lean
+++ library/system/io.lean
@@ -1,51 +1,91 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Luke Nelson, Jared Roesch and Leonardo de Moura
+Authors: Luke Nelson, Jared Roesch, Leonardo de Moura, Sebastian Ullrich
 -/
-import system.io_interface
 
-/- The following constants have a builtin implementation -/
-constant io_core : Type → Type → Type 1
+prelude
+import init.control.state init.control.except init.data.string.basic
+import init.meta.tactic
 
-/- Auxiliary definition used in the builtin implementation of monad_io_random_impl -/
--- def io_rand_nat : std_gen → nat → nat → nat × std_gen :=
--- rand_nat
+/-- Like https://hackage.haskell.org/package/ghc-prim-0.5.2.0/docs/GHC-Prim.html#t:RealWorld.
+    Makes sure we never reorder `io` operations. -/
+constant io.real_world : Type
+-- TODO: make opaque
+@[irreducible, derive monad]
+def io : Type → Type := state io.real_world
 
-@[instance] constant monad_io_impl             : monad_io io_core
-@[instance] constant monad_io_terminal_impl    : monad_io_terminal io_core
-@[instance] constant monad_io_file_system_impl : monad_io_file_system io_core
-@[instance] constant monad_io_environment_impl : monad_io_environment io_core
-@[instance] constant monad_io_process_impl     : monad_io_process io_core
+abbreviation monad_io (m : Type → Type) := has_monad_lift_t io m
 
-instance io_core_is_monad (e : Type) : monad (io_core e) :=
-monad_io_is_monad io_core e
+-- TODO: make opaque
+-- In the future, we may want to give more concrete data
+-- like in https://doc.rust-lang.org/std/io/enum.ErrorKind.html
+@[irreducible, derive has_to_string]
+def io.error := string
 
-instance io_core_is_monad_fail : monad_fail (io_core io.error) :=
-monad_io_is_monad_fail io_core
+/-- 'io with errors'. A useful default monad stack to use for operations
+    in the `io` namespace if there is no need for additional layers or
+    a more specific error type than `io.error`. -/
+abbreviation eio := except_t io.error io
 
-instance io_core_is_alternative : alternative (io_core io.error) :=
-monad_io_is_alternative io_core
+/-
+inductive io.process.stdio
+| piped
+| inherit
+| null
 
-@[reducible] def io (α : Type) :=
-io_core io.error α
+structure io.process.spawn_args :=
+/- Command name. -/
+(cmd : string)
+/- Arguments for the process -/
+(args : list string := [])
+/- Configuration for the process' stdin handle. -/
+(stdin := stdio.inherit)
+/- Configuration for the process' stdout handle. -/
+(stdout := stdio.inherit)
+/- Configuration for the process' stderr handle. -/
+(stderr := stdio.inherit)
+/- Working directory for the process. -/
+(cwd : option string := none)
+/- Environment variables for the process. -/
+(env : list (string × option string) := [])
+
+class monad_io_file_system (m : Type → Type → Type 1) [monad_io m] :=
+/- Remark: in Haskell, they also provide  (Maybe TextEncoding) and  NewlineMode -/
+(mk_file_handle : string → io.mode → bool → m io.error (handle m))
+(is_eof         : (handle m) → m io.error bool)
+(flush          : (handle m) → m io.error unit)
+(close          : (handle m) → m io.error unit)
+(read           : (handle m) → nat → m io.error string)
+(write          : (handle m) → string → m io.error unit)
+(get_line       : (handle m) → m io.error string)
+(stdin          : m io.error (handle m))
+(stdout         : m io.error (handle m))
+(stderr         : m io.error (handle m))
+
+class monad_io_environment (m : Type → Type → Type 1) :=
+(get_env : string → m io.error (option string))
+-- we don't provide set_env as it is (thread-)unsafe (at least with glibc)
+(get_cwd : m io.error string)
+(set_cwd : string → m io.error unit)
+
+class monad_io_process (m : Type → Type → Type 1) [monad_io m] :=
+(child  : Type)
+(stdin  : child → (handle m))
+(stdout : child → (handle m))
+(stderr : child → (handle m))
+(spawn  : io.process.spawn_args → m io.error child)
+(wait   : child → m io.error nat)
+-/
 
 namespace io
-/- Remark: the following definitions can be generalized and defined for any (m : Type -> Type -> Type 1)
-   that implements the required type classes. However, the generalized versions are very inconvenient to use,
-   (example: `#eval io.put_str "hello world"` does not work because we don't have enough information to infer `m`.).
--/
-def iterate {e} {α β : Type} (a : α) (f : α → io_core e (sum α β)) : io_core e β :=
-monad_io.iterate e α β a f
+/-
+constant iterate {α β : Type} (a : α) (f : α → io (sum α β)) : io β
 
-def forever {e} (a : io_core e unit) : io_core e unit :=
+def forever (a : io unit) : io unit :=
 iterate () $ λ _, a >> return (sum.inl ())
 
--- TODO(Leo): delete after we merge #1881
-def catch {e₁ e₂ α} (a : io_core e₁ α) (b : e₁ → io_core e₂ α) : io_core e₂ α :=
-monad_io.catch e₁ e₂ α a b
-
-def finally {α e} (a : io_core e α) (cleanup : io_core e unit) : io_core e α := do
+def finally {α e} (a : io α) (cleanup : io unit) : io α := do
 res ← catch (sum.inr <$> a) (return ∘ sum.inl),
 cleanup,
 match res with
@@ -55,43 +95,10 @@ match res with
 protected def fail {α : Type} (s : string) : io α :=
 monad_io.fail io_core _ _ (io.error.other s)
 
-def put_str : string → io unit :=
-monad_io_terminal.put_str io_core
-
-def put_str_ln (s : string) : io unit :=
-put_str s >> put_str "\n"
-
-def get_line : io string :=
-monad_io_terminal.get_line io_core
-
-def cmdline_args : io (list string) :=
-return (monad_io_terminal.cmdline_args io_core)
-
-def print {α} [has_to_string α] (s : α) : io unit :=
-put_str ∘ to_string $ s
-
-def print_ln {α} [has_to_string α] (s : α) : io unit :=
-print s >> put_str "\n"
-
-def handle : Type :=
-monad_io.handle io_core
-
-def mk_file_handle (s : string) (m : mode) (bin : bool := ff) : io handle :=
-monad_io_file_system.mk_file_handle io_core s m bin
-
-def stdin : io handle :=
-monad_io_file_system.stdin io_core
-
-def stderr : io handle :=
-monad_io_file_system.stderr io_core
-
-def stdout : io handle :=
-monad_io_file_system.stdout io_core
-
 namespace env
 
 def get (env_var : string) : io (option string) :=
-monad_io_environment.get_env io_core env_var
+monad_io_environment.get_env ionv_var
 
 /-- get the current working directory -/
 def get_cwd : io string :=
@@ -102,63 +109,119 @@ def set_cwd (cwd : string) : io unit :=
 monad_io_environment.set_cwd io_core cwd
 
 end env
+-/
+
+constant cmdline_args : list string
+
+inductive fs.mode
+| read | write | read_write | append
+constant fs.handle : Type
+
+namespace prim
+open fs
+
+local notation `ioe` α := io (except io.error α)
+
+constant iterate {α β : Type} : α → (α → io (sum α β)) → io β
+
+def iterate_ioe {α β : Type} (a : α) (f : α → ioe (sum α β)) : ioe β :=
+iterate a $ λ r, do
+  r ← f r,
+  match r with
+  | except.ok (sum.inl r) := pure (sum.inl r)
+  | except.ok (sum.inr r) := pure (sum.inr (except.ok r))
+  | except.error e        := pure (sum.inr (except.error e))
+
+constant put_str : string → ioe unit
+constant get_line : ioe string
+constant handle.mk (s : string) (m : mode) (bin : bool := ff) : ioe handle
+constant handle.is_eof : handle → ioe bool
+constant handle.flush : handle → ioe unit
+constant handle.close : handle → ioe unit
+-- TODO: replace `string` with byte buffer
+--constant handle.read : handle → nat → ioe string
+constant handle.write : handle → string → ioe unit
+constant handle.get_line : handle → ioe string
+
+def lift_ioe {m : Type → Type} [monad_io m] [monad_except io.error m] [monad m] {α : Type}
+  (x : ioe α) : m α :=
+monad_lift x >>= monad_except.lift_except
+
+end prim
+
+section
+variables {m : Type → Type} [monad_io m] [monad_except io.error m] [monad m]
+
+def put_str : string → m unit :=
+prim.lift_ioe ∘ prim.put_str
+
+def put_str_ln (s : string) : m unit :=
+put_str s >> put_str "\n"
+
+def print {α} [has_to_string α] (s : α) : m unit :=
+put_str ∘ to_string $ s
+
+def print_ln {α} [has_to_string α] (s : α) : m unit :=
+print s >> put_str "\n"
+end
 
 namespace fs
-def is_eof : handle → io bool :=
-monad_io_file_system.is_eof
+variables {m : Type → Type} [monad_io m] [monad_except io.error m] [monad m]
 
-def flush : handle → io unit :=
-monad_io_file_system.flush
+def handle.mk (s : string) (mode : mode) (bin : bool := ff) : m handle := prim.lift_ioe (prim.handle.mk s mode bin)
+def handle.is_eof : handle → m bool := prim.lift_ioe ∘ prim.handle.is_eof
+def handle.flush : handle → m unit := prim.lift_ioe ∘ prim.handle.flush
+def handle.close : handle → m unit := prim.lift_ioe ∘ prim.handle.flush
+--def handle.read (h : handle) (bytes : nat) : m string := prim.lift_ioe (prim.handle.read h bytes)
+def handle.write (h : handle) (s : string) : m unit := prim.lift_ioe (prim.handle.write h s)
+def handle.get_line : handle → m string := prim.lift_ioe ∘ prim.handle.get_line
 
-def close : handle → io unit :=
-monad_io_file_system.close
-
-def read : handle → nat → io string :=
-monad_io_file_system.read
-
-def write : handle → string → io unit :=
-monad_io_file_system.write
-
-def get_char (h : handle) : io char :=
-do b ← read h 1,
-   if b.is_empty then io.fail "get_char failed"
+/-
+def get_char (h : handle) : m char :=
+do b ← h.read 1,
+   if b.is_empty then fail "get_char failed"
    else return b.mk_iterator.curr
+-/
 
-def get_line : handle → io string :=
-monad_io_file_system.get_line
+def handle.put_char (h : handle) (c : char) : m unit :=
+h.write (to_string c)
 
-def put_char (h : handle) (c : char) : io unit :=
-write h (to_string c)
+def handle.put_str (h : handle) (s : string) : m unit :=
+h.write s
 
-def put_str (h : handle) (s : string) : io unit :=
-write h s
+def handle.put_str_ln (h : handle) (s : string) : m unit :=
+h.put_str s >> h.put_str "\n"
 
-def put_str_ln (h : handle) (s : string) : io unit :=
-put_str h s >> put_str h "\n"
+def handle.read_to_end (h : handle) : m string :=
+prim.lift_ioe $ prim.iterate_ioe "" $ λ r, except_t.run $ do
+  done ← h.is_eof,
+  if done
+  then return (sum.inr r) -- stop
+  else do
+    -- HACK: use less efficient `get_line` while `read` is broken
+    c ← h.get_line,
+    return $ sum.inl (r ++ c) -- continue
 
-def read_to_end (h : handle) : io string :=
-iterate "" $ λ r,
-  do done ← is_eof h,
-    if done
-    then return (sum.inr r) -- stop
-    else do
-      -- HACK: use less efficient `get_line` while `read` is broken
-      c ← get_line h,
-      return $ sum.inl (r ++ c) -- continue
-
-def read_file (fname : string) (bin := ff) : io string :=
-do h ← mk_file_handle fname io.mode.read bin,
-   r ← read_to_end h,
+/-
+def read_file (fname : string) (bin := ff) : m string :=
+do h ← handle.mk fname mode.read bin,
+   r ← h.read_to_end,
    close h,
    return r
+-/
 
-def write_file (fname : string) (data : string) (bin := ff) : io unit :=
-do h ← mk_file_handle fname io.mode.write bin,
-   write h data,
-   close h
+def write_file (fname : string) (data : string) (bin := ff) : m unit :=
+do h ← handle.mk fname mode.write bin,
+   h.write data,
+   h.close
 
 end fs
 
+constant stdin : fs.handle
+constant stderr : fs.handle
+constant stdout : fs.handle
+
+/-
 namespace proc
 def child : Type :=
 monad_io_process.child io_core
@@ -179,7 +242,7 @@ def wait (c : child) : io nat :=
 monad_io_process.wait c
 
 end proc
-
+-/
 end io
 
 meta constant format.print_using : format → options → io unit
@@ -193,6 +256,7 @@ format.print_using (to_fmt a) o
 meta definition pp {α : Type} [has_to_format α] (a : α) : io unit :=
 format.print (to_fmt a)
 
+/-
 /-- Run the external process specified by `args`.
 
     The process will run to completion with its output captured by a pipe, and
@@ -204,6 +268,7 @@ do child ← io.proc.spawn { stdout := io.process.stdio.piped, ..args },
   exitv ← io.proc.wait child,
   if exitv ≠ 0 then io.fail $ "process exited with status " ++ repr exitv else pure (),
   return s
+-/
 
 /--
 This is the "back door" into the `io` monad, allowing IO computation to be performed during tactic execution.
@@ -235,4 +300,4 @@ meta constant tactic.unsafe_run_io {α : Type} : io α → tactic α
    - One single goal of the form `⊢ true`.
    This action is mainly useful for writing tactics that inspect
    the environment. -/
-meta constant io.run_tactic {α : Type} (a : tactic α) : io α
+meta constant io.run_tactic {α : Type} (a : tactic α) : except_t format io α

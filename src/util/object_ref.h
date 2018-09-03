498cfa84fdbf89a52443aff45c02db0954802740
Leonardo de Moura <leonardo@microsoft.com>
2018-09-02 20:19:17 -0700

    fix(util/object_ref): typo

--- src/util/object_ref.h
+++ src/util/object_ref.h
@@ -107,7 +107,7 @@ inline object_ref mk_cnstr(unsigned tag, object_ref const & o1, object_ref const
 }
 
 inline object_ref mk_cnstr(unsigned tag, object_ref const & o1, object_ref const & o2, object_ref const & o3, object_ref const & o4, object_ref const & o5, object_ref const & o6, object_ref const & o7, unsigned scalar_sz = 0) {
-    object * r = alloc_cnstr(tag, 6, scalar_sz);
+    object * r = alloc_cnstr(tag, 7, scalar_sz);
     cnstr_set_obj(r, 0, o1.raw()); inc(o1.raw());
     cnstr_set_obj(r, 1, o2.raw()); inc(o2.raw());
     cnstr_set_obj(r, 2, o3.raw()); inc(o3.raw());

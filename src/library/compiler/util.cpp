d07f115f213450871f1d69a3f15035ff62820688
Leonardo de Moura <leonardo@microsoft.com>
2018-10-23 10:12:26 -0700

    chore(library/compiler/util): fix style

--- src/library/compiler/util.cpp
+++ src/library/compiler/util.cpp
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <algorithm>
 #include <string>
 #include "kernel/type_checker.h"
 #include "kernel/for_each_fn.h"

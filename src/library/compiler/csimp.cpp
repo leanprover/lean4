e21608d45690979511f32c6a9ce23335374d839a
Leonardo de Moura <leonardo@microsoft.com>
2018-10-31 13:25:54 -0700

    chore(library/compiler/csimp): style

--- src/library/compiler/csimp.cpp
+++ src/library/compiler/csimp.cpp
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
 #include "runtime/flet.h"

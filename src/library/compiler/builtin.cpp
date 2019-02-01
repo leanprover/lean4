a1b65acf3d62786107a5b76c6ef98a4b515f3efb
Leonardo de Moura <leonardo@microsoft.com>
2019-01-31 16:57:25 -0800

    fix(library/compiler/builtin): add `lean::` prefix
    TODO: we will need to revise this for the LLVM backend.
    In principle, builtins that are used by LLVM and C++ backends should be
    `extern "C"` in a global namespace.

--- src/library/compiler/builtin.cpp
+++ src/library/compiler/builtin.cpp
@@ -109,71 +109,71 @@ void initialize_builtin() {
     list<bool> bccc{true, false, false, false};
 
     /* nat builtin functions */
-    register_builtin(name({"nat", "add"}), o_o_o, "nat_add", bb);
-    register_builtin(name({"nat", "sub"}), o_o_o, "nat_sub", bb);
-    register_builtin(name({"nat", "mul"}), o_o_o, "nat_mul", bb);
-    register_builtin(name({"nat", "div"}), o_o_o, "nat_div", bb);
-    register_builtin(name({"nat", "mod"}), o_o_o, "nat_mod", bb);
-    register_builtin(name({"nat", "dec_eq"}), o_o_o, "nat_dec_eq", bb);
-    register_builtin(name({"nat", "dec_lt"}), o_o_o, "nat_dec_lt", bb);
-    register_builtin(name({"nat", "dec_le"}), o_o_o, "nat_dec_le", bb);
+    register_builtin(name({"nat", "add"}), o_o_o, "lean::nat_add", bb);
+    register_builtin(name({"nat", "sub"}), o_o_o, "lean::nat_sub", bb);
+    register_builtin(name({"nat", "mul"}), o_o_o, "lean::nat_mul", bb);
+    register_builtin(name({"nat", "div"}), o_o_o, "lean::nat_div", bb);
+    register_builtin(name({"nat", "mod"}), o_o_o, "lean::nat_mod", bb);
+    register_builtin(name({"nat", "dec_eq"}), o_o_o, "lean::nat_dec_eq", bb);
+    register_builtin(name({"nat", "dec_lt"}), o_o_o, "lean::nat_dec_lt", bb);
+    register_builtin(name({"nat", "dec_le"}), o_o_o, "lean::nat_dec_le", bb);
 
     /* int builtin functions */
-    register_builtin(name({"int", "of_nat"}), o_o, "nat2int", b);
-    register_builtin(name({"int", "neg_succ_of_nat"}), o_o, "int_neg_succ_of_nat", b);
-    register_builtin(name({"int", "nat_abs"}), o_o, "nat_abs", b);
-    register_builtin(name({"int", "neg"}), o_o, "int_neg", b);
-    register_builtin(name({"int", "add"}), o_o_o, "int_add", bb);
-    register_builtin(name({"int", "sub"}), o_o_o, "int_sub", bb);
-    register_builtin(name({"int", "mul"}), o_o_o, "int_mul", bb);
-    register_builtin(name({"int", "quot"}), o_o_o, "int_div", bb);
-    register_builtin(name({"int", "rem"}), o_o_o, "int_rem", bb);
-    register_builtin(name({"int", "dec_eq"}), o_o_o, "int_dec_eq", bb);
-    register_builtin(name({"int", "dec_lt"}), o_o_o, "int_dec_lt", bb);
-    register_builtin(name({"int", "dec_le"}), o_o_o, "int_dec_le", bb);
+    register_builtin(name({"int", "of_nat"}), o_o, "lean::nat2int", b);
+    register_builtin(name({"int", "neg_succ_of_nat"}), o_o, "lean::int_neg_succ_of_nat", b);
+    register_builtin(name({"int", "nat_abs"}), o_o, "lean::nat_abs", b);
+    register_builtin(name({"int", "neg"}), o_o, "lean::int_neg", b);
+    register_builtin(name({"int", "add"}), o_o_o, "lean::int_add", bb);
+    register_builtin(name({"int", "sub"}), o_o_o, "lean::int_sub", bb);
+    register_builtin(name({"int", "mul"}), o_o_o, "lean::int_mul", bb);
+    register_builtin(name({"int", "quot"}), o_o_o, "lean::int_div", bb);
+    register_builtin(name({"int", "rem"}), o_o_o, "lean::int_rem", bb);
+    register_builtin(name({"int", "dec_eq"}), o_o_o, "lean::int_dec_eq", bb);
+    register_builtin(name({"int", "dec_lt"}), o_o_o, "lean::int_dec_lt", bb);
+    register_builtin(name({"int", "dec_le"}), o_o_o, "lean::int_dec_le", bb);
 
     /* string builtin functions */
-    register_builtin(name({"string", "mk"}), o_o, "string_mk", c);
-    register_builtin(name({"string", "data"}), o_o, "string_data", c);
-    register_builtin(name({"string", "length"}), o_o, "string_length", b);
-    register_builtin(name({"string", "push"}), o_u32_o, "string_push", cc);
-    register_builtin(name({"string", "append"}), o_o_o, "string_append", cb);
-    register_builtin(name({"string", "mk_iterator"}), o_o, "string_mk_iterator", c);
-    register_builtin(name({"string", "dec_eq"}), o_o_o, "string_dec_eq", bb);
-    register_builtin(name({"string", "dec_lt"}), o_o_o, "string_dec_lt", bb);
-    register_builtin(name({"string", "iterator", "curr"}), o_u32, "string_iterator_curr", b);
-    register_builtin(name({"string", "iterator", "set_curr"}), o_u32_o, "string_iterator_set_curr", cc);
-    register_builtin(name({"string", "iterator", "next"}), o_o, "string_iterator_next", c);
-    register_builtin(name({"string", "iterator", "prev"}), o_o, "string_iterator_prev", c);
-    register_builtin(name({"string", "iterator", "has_next"}), o_u8, "string_iterator_has_next", b);
-    register_builtin(name({"string", "iterator", "has_prev"}), o_u8, "string_iterator_has_prev", b);
-    register_builtin(name({"string", "iterator", "insert"}), o_o_o, "string_iterator_insert", cb);
-    register_builtin(name({"string", "iterator", "remove"}), o_o_o, "string_iterator_remove", cb);
-    register_builtin(name({"string", "iterator", "remaining"}), o_o, "string_iterator_remaining", b);
-    register_builtin(name({"string", "iterator", "offset"}), o_o, "string_iterator_offset", b);
-    register_builtin(name({"string", "iterator", "to_string"}), o_o, "string_iterator_to_string", b);
-    register_builtin(name({"string", "iterator", "to_end"}), o_o, "string_iterator_to_end", c);
-    register_builtin(name({"string", "iterator", "remaining_to_string"}), o_o, "string_iterator_remaining_to_string", b);
-    register_builtin(name({"string", "iterator", "prev_to_string"}), o_o, "string_iterator_prev_to_string", b);
-    register_builtin(name({"string", "iterator", "extract"}), o_o_o, "string_iterator_extract", bb);
-    register_builtin(name({"string", "iterator", "mk"}), o_o_o, "string_iterator_mk", bb);
-    register_builtin(name({"string", "iterator", "fst"}), o_o, "string_iterator_fst", b);
-    register_builtin(name({"string", "iterator", "snd"}), o_o, "string_iterator_snd", b);
+    register_builtin(name({"string", "mk"}), o_o, "lean::string_mk", c);
+    register_builtin(name({"string", "data"}), o_o, "lean::string_data", c);
+    register_builtin(name({"string", "length"}), o_o, "lean::string_length", b);
+    register_builtin(name({"string", "push"}), o_u32_o, "lean::string_push", cc);
+    register_builtin(name({"string", "append"}), o_o_o, "lean::string_append", cb);
+    register_builtin(name({"string", "mk_iterator"}), o_o, "lean::string_mk_iterator", c);
+    register_builtin(name({"string", "dec_eq"}), o_o_o, "lean::string_dec_eq", bb);
+    register_builtin(name({"string", "dec_lt"}), o_o_o, "lean::string_dec_lt", bb);
+    register_builtin(name({"string", "iterator", "curr"}), o_u32, "lean::string_iterator_curr", b);
+    register_builtin(name({"string", "iterator", "set_curr"}), o_u32_o, "lean::string_iterator_set_curr", cc);
+    register_builtin(name({"string", "iterator", "next"}), o_o, "lean::string_iterator_next", c);
+    register_builtin(name({"string", "iterator", "prev"}), o_o, "lean::string_iterator_prev", c);
+    register_builtin(name({"string", "iterator", "has_next"}), o_u8, "lean::string_iterator_has_next", b);
+    register_builtin(name({"string", "iterator", "has_prev"}), o_u8, "lean::string_iterator_has_prev", b);
+    register_builtin(name({"string", "iterator", "insert"}), o_o_o, "lean::string_iterator_insert", cb);
+    register_builtin(name({"string", "iterator", "remove"}), o_o_o, "lean::string_iterator_remove", cb);
+    register_builtin(name({"string", "iterator", "remaining"}), o_o, "lean::string_iterator_remaining", b);
+    register_builtin(name({"string", "iterator", "offset"}), o_o, "lean::string_iterator_offset", b);
+    register_builtin(name({"string", "iterator", "to_string"}), o_o, "lean::string_iterator_to_string", b);
+    register_builtin(name({"string", "iterator", "to_end"}), o_o, "lean::string_iterator_to_end", c);
+    register_builtin(name({"string", "iterator", "remaining_to_string"}), o_o, "lean::string_iterator_remaining_to_string", b);
+    register_builtin(name({"string", "iterator", "prev_to_string"}), o_o, "lean::string_iterator_prev_to_string", b);
+    register_builtin(name({"string", "iterator", "extract"}), o_o_o, "lean::string_iterator_extract", bb);
+    register_builtin(name({"string", "iterator", "mk"}), o_o_o, "lean::string_iterator_mk", bb);
+    register_builtin(name({"string", "iterator", "fst"}), o_o, "lean::string_iterator_fst", b);
+    register_builtin(name({"string", "iterator", "snd"}), o_o, "lean::string_iterator_snd", b);
 
     /* io builtin functions */
-    register_builtin(name({"io", "prim", "put_str"}), o_o_o, "io_prim_put_str", bc);
-    register_builtin(name({"io", "prim", "get_line"}), o_o, "io_prim_get_line", c);
-    register_builtin(name({"io", "prim", "handle", "mk"}), o_u8_u8_o_o, "io_prim_handle_mk", bccc);
-    register_builtin(name({"io", "prim", "handle", "is_eof"}), o_o_o, "io_prim_handle_is_eof", bc);
-    register_builtin(name({"io", "prim", "handle", "flush"}), o_o_o, "io_prim_handle_flush", bc);
-    register_builtin(name({"io", "prim", "handle", "close"}), o_o_o, "io_prim_handle_close", bc);
-    register_builtin(name({"io", "prim", "handle", "get_line"}), o_o_o, "io_prim_handle_get_line", bc);
+    register_builtin(name({"io", "prim", "put_str"}), o_o_o, "lean::io_prim_put_str", bc);
+    register_builtin(name({"io", "prim", "get_line"}), o_o, "lean::io_prim_get_line", c);
+    register_builtin(name({"io", "prim", "handle", "mk"}), o_u8_u8_o_o, "lean::io_prim_handle_mk", bccc);
+    register_builtin(name({"io", "prim", "handle", "is_eof"}), o_o_o, "lean::io_prim_handle_is_eof", bc);
+    register_builtin(name({"io", "prim", "handle", "flush"}), o_o_o, "lean::io_prim_handle_flush", bc);
+    register_builtin(name({"io", "prim", "handle", "close"}), o_o_o, "lean::io_prim_handle_close", bc);
+    register_builtin(name({"io", "prim", "handle", "get_line"}), o_o_o, "lean::io_prim_handle_get_line", bc);
 
     // interface to old elaborator
-    register_builtin(name({"lean", "expr", "local"}), mk_arrow(o, o_o_o_o), "lean_expr_local");
-    register_builtin(name({"lean", "environment", "empty"}), o, "lean_environment_empty");
-    register_builtin(name({"lean", "environment", "contains"}), o_o_o, "lean_environment_contains");
-    register_builtin(name({"lean", "elaborator", "elaborate_command"}), o_o_o_o, "lean_elaborator_elaborate_command");
+    register_builtin(name({"lean", "expr", "local"}), mk_arrow(o, o_o_o_o), "lean::lean_expr_local");
+    register_builtin(name({"lean", "environment", "empty"}), o, "lean::lean_environment_empty");
+    register_builtin(name({"lean", "environment", "contains"}), o_o_o, "lean::lean_environment_contains");
+    register_builtin(name({"lean", "elaborator", "elaborate_command"}), o_o_o_o, "lean::lean_elaborator_elaborate_command");
 }
 
 void finalize_builtin() {

434627ee8e0edfc4a40f282f8afc65f0524f55eb
Leonardo de Moura <leonardo@microsoft.com>
2019-02-06 14:35:13 -0800

    chore(runtime/io): implement `io.prim.put_str` just to be able to test

--- src/runtime/io.cpp
+++ src/runtime/io.cpp
@@ -4,11 +4,29 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include <iostream>
 #include "runtime/io.h"
 namespace lean {
-obj_res io_prim_put_str(b_obj_arg /* s */, obj_arg /* w */) {
-    // not implemented yet
-    lean_unreachable();
+static obj_res const REAL_WORLD = box(0);
+
+obj_res mk_io_result(obj_arg r) {
+    object * res = alloc_cnstr(0, 2, 0);
+    cnstr_set(res, 0, r);
+    cnstr_set(res, 1, REAL_WORLD);
+    return res;
+}
+
+/* `(r : α) → (except ε α × real_world)` */
+obj_res mk_ioe_result(obj_arg r) {
+    object * res = alloc_cnstr(1, 1, 0);
+    cnstr_set(res, 0, r);
+    return mk_io_result(res);
+}
+
+obj_res io_prim_put_str(b_obj_arg s, obj_arg /* w */) {
+    // TODO(Leo): this is a temporary hack for testing
+    std::cout << string_to_std(s);
+    return mk_ioe_result(box(0));
 }
 
 obj_res io_prim_get_line(obj_arg /* w */) {

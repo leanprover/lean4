6c90b1a2ff0ae27d25cab972e8ae586dda745650
Leonardo de Moura <leonardo@microsoft.com>
2019-02-11 10:29:47 -0800

    chore(library/compiler/builtin): style

--- src/library/compiler/builtin.h
+++ src/library/compiler/builtin.h
@@ -11,7 +11,7 @@ namespace lean {
 bool is_native_constant(environment const & env, name const & c);
 bool is_builtin_constant(name const & c);
 optional<name> get_native_constant_cname(environment const & env, name const & c);
-optional <expr> get_native_constant_ll_type(environment const & env, name const & c);
+optional<expr> get_native_constant_ll_type(environment const & env, name const & c);
 optional<unsigned> get_native_constant_arity(environment const & env, name const & c);
 /* Return true if `c` is a native constant, and store in borrowed_args and
    borrowed_res which arguments/results are marked as borrowed. */
@@ -23,6 +23,7 @@ bool get_native_used_args(environment const & env, name const & c, buffer<bool> 
 environment add_native_constant_decl(environment const & env, name const & n, expr const & ll_type, std::string cname,
                                      bool bres, list<bool> const & bargs, list<bool> const & used_args);
 void for_each_native_constant(environment const & env, std::function<void(name const & n)> const & f);
+
 void initialize_builtin();
 void finalize_builtin();
 }

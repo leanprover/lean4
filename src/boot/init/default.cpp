88de217cb73721db8a24db4579513c7a6a498b04
Leonardo de Moura <leonardo@microsoft.com>
2019-02-12 11:57:09 -0800

    chore(library/init): remove `version.lean.in`
    We are currently not using this file. In the future, we should
    reintroduce it, but its functions should be implemented as builtins.
    Thus, every `chore(boot): update` commit will not have to update it.

--- src/boot/init/default.cpp
+++ src/boot/init/default.cpp
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: init.default
-// Imports: init.core init.control.default init.data.basic init.version init.function init.coe init.wf init.data.default init.io
+// Imports: init.core init.control.default init.data.basic init.function init.coe init.wf init.data.default init.io
 #include "runtime/object.h"
 #include "runtime/apply.h"
 #include "runtime/io.h"
@@ -19,7 +19,6 @@ typedef lean::uint32 uint32; typedef lean::uint64 uint64;
 void initialize_init_core();
 void initialize_init_control_default();
 void initialize_init_data_basic();
-void initialize_init_version();
 void initialize_init_function();
 void initialize_init_coe();
 void initialize_init_wf();
@@ -32,7 +31,6 @@ void initialize_init_default() {
  initialize_init_core();
  initialize_init_control_default();
  initialize_init_data_basic();
- initialize_init_version();
  initialize_init_function();
  initialize_init_coe();
  initialize_init_wf();

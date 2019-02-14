1716f1f6c94d5e38394d0e353f2536f718700c4f
Leonardo de Moura <leonardo@microsoft.com>
2019-02-14 08:40:56 -0800

    test(tests/playground/perf): performance test
    On my macbook,
    - with default kind STHeap: 3.15 secs
    - with default kind MTHeap: 3.75 secs
    
    @cc kha

---
+++ tests/playground/perf.lean
@@ -0,0 +1,15 @@
+def g (a : nat) (n : nat) : list nat :=
+let xs := list.repeat a n in
+xs.map (+2)
+
+def h (xs : list nat) : nat :=
+xs.foldl (+) 0
+
+def rep (n : nat) : nat :=
+n.repeat (λ i r, h (g i n)) 0
+
+def act (n : nat) : io unit :=
+io.println' (to_string (rep n))
+
+def main : io uint32 :=
+act 5000 *> pure 0

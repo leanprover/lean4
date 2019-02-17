d100f95469da690f10f5fc64a23c5749a95cc31d
Leonardo de Moura <leonardo@microsoft.com>
2019-02-17 09:50:55 -0800

    feat(runtime/object): make `dbg_trace` thread safe

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -1638,8 +1638,12 @@ void dbg_print_num(object * o) {
     }
 }
 
+static mutex g_dbg_mutex;
+
 object * dbg_trace(obj_arg s, obj_arg fn) {
-    std::cout << string_cstr(s) << "\n";
+    unique_lock<mutex> lock(g_dbg_mutex); {
+        std::cout << string_cstr(s) << std::endl;
+    }
     dec(s);
     return apply_1(fn, box(0));
 }

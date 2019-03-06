af039f420afb255ca55a48085d103cdccd942dc6
Leonardo de Moura <leonardo@microsoft.com>
2019-03-06 05:26:53 -0800

    chore(util): remove dead code

--- src/util/name_map.h
+++ src/util/name_map.h
@@ -9,12 +9,4 @@ Author: Leonardo de Moura
 #include "util/name.h"
 namespace lean {
 template<typename T> using name_map = rb_map<name, T, name_quick_cmp>;
-
-class rename_map : public name_map<name> {
-public:
-    // Similar to name_map::find, but it "follows" the sequence of renames.
-    // Example, if map contains "A->B" and "B->C", then find(A) returns C.
-    // Moreover, if k is not in the map, the result is \c k
-    name const & find(name const & k) const;
-};
 }

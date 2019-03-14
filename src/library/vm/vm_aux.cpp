9afee8591927916032e173f0c8730be9a43bf3bb
Leonardo de Moura <leonardo@microsoft.com>
2019-03-14 16:03:08 -0700

    fix(library/vm/vm_aux): remove old code
    Remark: `vm_timeit` was for the Lean 3 `timeit` primitive which is
    different from the Lean 4 one. It has a different arity, and was
    producing an assertion violation at emit_bytecode.cpp

--- src/library/vm/vm_aux.cpp
+++ src/library/vm/vm_aux.cpp
@@ -14,26 +14,6 @@ Author: Leonardo de Moura
 #include "library/vm/vm_nat.h"
 
 namespace lean {
-vm_obj vm_timeit(vm_obj const &, vm_obj const & s, vm_obj const & fn) {
-    std::string msg = to_string(s);
-    timeit timer(tout().get_stream(), msg.c_str());
-    return invoke(fn, mk_vm_unit());
-}
-
-vm_obj vm_trace(vm_obj const &, vm_obj const & s, vm_obj const & fn) {
-    tout() << to_string(s) << "\n";
-    return invoke(fn, mk_vm_unit());
-}
-
-vm_obj vm_trace_call_stack(vm_obj const &, vm_obj const & fn) {
-    auto & s = get_vm_state();
-    auto out = tout();
-    for (unsigned i = 0; i < s.call_stack_size() - 1; i++) {
-        out << s.call_stack_fn(i) << "\n";
-    }
-    return invoke(fn, mk_vm_unit());
-}
-
 vm_obj vm_sorry() {
     auto & s = get_vm_state();
     throw exception(sstream() << s.call_stack_fn(s.call_stack_size() - 1)
@@ -44,25 +24,9 @@ vm_obj vm_undefined_core(vm_obj const &, vm_obj const & message) {
     throw exception(to_string(message));
 }
 
-vm_obj vm_try_for(vm_obj const &, vm_obj const & n, vm_obj const & thunk) {
-    size_t max  = static_cast<size_t>(force_to_unsigned(n))*1000;
-    scope_heartbeat     scope1(0);
-    scope_max_heartbeat scope2(max);
-    vm_obj unit = mk_vm_unit();
-    if (auto r = get_vm_state().try_invoke_catch(thunk, 1, &unit)) {
-        return mk_vm_some(*r);
-    } else {
-        return mk_vm_none();
-    }
-}
-
 void initialize_vm_aux() {
-    DECLARE_VM_BUILTIN("timeit",           vm_timeit);
-    DECLARE_VM_BUILTIN("trace",            vm_trace);
-    DECLARE_VM_BUILTIN("trace_call_stack", vm_trace_call_stack);
     DECLARE_VM_BUILTIN("sorry",            vm_sorry);
     DECLARE_VM_BUILTIN("undefined_core",   vm_undefined_core);
-    DECLARE_VM_BUILTIN("try_for",          vm_try_for);
 }
 
 void finalize_vm_aux() {

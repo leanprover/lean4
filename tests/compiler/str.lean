3b70e8785b234303db115b864dfae772ca5f30bc
Leonardo de Moura <leonardo@microsoft.com>
2019-03-22 17:26:14 -0700

    chore(tests/compiler/str): fix test

--- tests/compiler/str.lean
+++ tests/compiler/str.lean
@@ -1,14 +1,14 @@
-def showChars : Nat → String → String.utf8Pos → IO Unit
+def showChars : Nat → String → String.Pos → IO Unit
 | 0     _ _   := pure ()
 | (n+1) s idx :=
-  unless (s.utf8AtEnd idx) $
-    IO.println (">> " ++ toString (s.utf8Get idx)) *>
-    showChars n s (s.utf8Next idx)
+  unless (s.atEnd idx) $
+    IO.println (">> " ++ toString (s.get idx)) *>
+    showChars n s (s.next idx)
 
 def main : IO UInt32 :=
-let s₁ := "hello α_world_β" in
-let b  := String.utf8Begin in
-let e  := s₁.utf8ByteSize in
+let s₁             := "hello α_world_β" in
+let b : String.Pos := 0 in
+let e              := s₁.bsize in
 IO.println (s₁.extract b e) *>
 IO.println (s₁.extract (b+2) e) *>
 IO.println (s₁.extract (b+2) (e-1)) *>
@@ -17,5 +17,5 @@ IO.println (s₁.extract (b+7) e) *>
 IO.println (s₁.extract (b+8) e) *>
 IO.println (toString e) *>
 IO.println (repr "   aaa   ".trim) *>
-showChars s₁.utf8ByteSize.toNat s₁ 0  *>
+showChars s₁.length s₁ 0  *>
 pure 0

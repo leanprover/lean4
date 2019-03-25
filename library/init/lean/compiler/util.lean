7f58843d489096daf7b294c29f94201b4734ec40
Leonardo de Moura <leonardo@microsoft.com>
2019-03-25 14:04:38 -0700

    feat(library/init/lean/compiler/util): atMostOnce visitor

--- library/init/lean/compiler/util.lean
+++ library/init/lean/compiler/util.lean
@@ -15,5 +15,44 @@ def objectType : Expr        := Expr.const `_obj []
 def voidType : Expr          := Expr.const `_void []
 def mkLcProof (pred : Expr) := Expr.app (Expr.const `lcProof []) pred
 
+namespace atMostOnce
+
+structure AtMostOnceData :=
+(found result : Bool)
+
+def Visitor := AtMostOnceData → AtMostOnceData
+
+@[inline] def seq (f g : Visitor) : Visitor :=
+λ d, match f d with
+| ⟨found, false⟩ := ⟨found, false⟩
+| other          := g other
+
+@[inline] def skip : Visitor := id
+
+@[inline] def visitFVar (x y : Name) : Visitor
+| d@{result := false, ..}  := d
+| {found := false, result := true} := {found := x == y, result := true}
+| {found := true,  result := true} := {found := true, result := !x == y}
+
+local infix *> := seq
+
+def visit (x : Name) : Expr → Visitor
+| (Expr.fvar y)       := visitFVar y x
+| (Expr.app f a)      := visit a *> visit f
+| (Expr.lam _ _ d b)  := visit d *> visit b
+| (Expr.pi _ _ d b)   := visit d *> visit b
+| (Expr.elet _ t v b) := visit t *> visit v *> visit b
+| (Expr.mdata _ e)    := visit e
+| (Expr.proj _ _ e)   := visit e
+| _                   := skip
+
+end atMostOnce
+
+/-- Return true iff the free variable with id `x` occurs at most once in `e` -/
+@[export lean.at_most_once]
+def atMostOnce (e : Expr) (x : Name) : Bool :=
+let {result := result, ..} := atMostOnce.visit x e {found := false, result := true} in
+result
+
 end Compiler
 end Lean

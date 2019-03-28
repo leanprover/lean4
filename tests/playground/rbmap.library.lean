19f31d483a7803761482c111f55135adf545951e
Leonardo de Moura <leonardo@microsoft.com>
2019-03-28 08:22:52 -0700

    chore(tests/playground/rbmap.library.lean): camelCase

--- tests/playground/rbmap.library.lean
+++ tests/playground/rbmap.library.lean
@@ -1,14 +1,14 @@
-@[reducible] def map : Type := Rbmap Nat Bool (<)
+@[reducible] def Map : Type := RBMap Nat Bool (<)
 
-def mkMapAux : Nat → map → map
+def mkMapAux : Nat → Map → Map
 | 0 m := m
 | (n+1) m := mkMapAux n (m.insert n (n % 10 = 0))
 
 def mkMap (n : Nat) :=
-mkMapAux n (mkRbmap Nat Bool (<))
+mkMapAux n {}
 
 def main (xs : List String) : IO UInt32 :=
 let m := mkMap xs.head.toNat in
-let v := Rbmap.fold (λ (k : Nat) (v : Bool) (r : Nat), if v then r + 1 else r) m 0 in
+let v := RBMap.fold (λ (k : Nat) (v : Bool) (r : Nat), if v then r + 1 else r) m 0 in
 IO.println (toString v) *>
 pure 0

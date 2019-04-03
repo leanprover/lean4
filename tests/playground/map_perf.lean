d6b7c9c1e21b4e4018551edba10212805eae3b5d
Leonardo de Moura <leonardo@microsoft.com>
2019-04-03 07:27:10 -0700

    test(tests/playground/map_perf): RBMap vs HashMap

---
+++ tests/playground/map_perf.lean
@@ -0,0 +1,30 @@
+abbrev Map1 : Type := RBMap Nat Bool (λ a b, a < b)
+abbrev Map2 : Type := HashMap Nat Bool
+
+def mkMap1Aux : Nat → Map1 → Map1
+| 0 m := m
+| (n+1) m := mkMap1Aux n (m.insert n (n % 10 = 0))
+
+def mkMap2Aux : Nat → Map2 → Map2
+| 0 m := m
+| (n+1) m := mkMap2Aux n (m.insert n (n % 10 = 0))
+
+def mkMap1 (n : Nat) :=
+mkMap1Aux n {}
+
+def mkMap2 (n : Nat) :=
+mkMap2Aux n {}
+
+def tst1 (n : Nat) : IO Unit :=
+do let m := mkMap1 n,
+   let v := m.fold (λ (k : Nat) (v : Bool) (r : Nat), if v then r + 1 else r) 0,
+   IO.println ("Result " ++ toString v)
+
+def tst2 (n : Nat) : IO Unit :=
+do let m := mkMap2 n,
+   let v := m.fold 0 (λ (r : Nat) (k : Nat) (v : Bool), if v then r + 1 else r),
+   IO.println ("Result " ++ toString v)
+
+def main (xs : List String) : IO Unit :=
+timeit "tst1" (tst1 xs.head.toNat) *>
+timeit "tst2" (tst2 xs.head.toNat)

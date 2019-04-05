d603baea70e379c5b4a245e0f1ef5f789b60a6b5
Leonardo de Moura <leonardo@microsoft.com>
2019-04-05 16:58:47 -0700

    test(tests/compiler): add new test

---
+++ tests/compiler/strictOrSimp.lean
@@ -0,0 +1,11 @@
+partial def spin : Nat → Bool
+| n := spin (n)
+
+@[inline] def C : Nat := 0
+
+def f (b : Nat) :=
+if strictOr (C == 0) (spin b) then "hello"
+else "world"
+
+def main (xs : List String) : IO Unit :=
+IO.println (f xs.head.toNat)

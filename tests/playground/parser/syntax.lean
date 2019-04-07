97ebe4603b75d9fe7cc28be9bc1b6e33d0785a40
Leonardo de Moura <leonardo@microsoft.com>
2019-04-06 19:36:04 -0700

    test(tests/playground/parser): add `syntax.lean` with flat nodes and arrays

---
+++ tests/playground/parser/syntax.lean
@@ -0,0 +1,223 @@
+/-
+Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Sebastian Ullrich, Leonardo de Moura
+-/
+prelude
+import init.lean.name init.lean.format init.data.array
+
+namespace Lean
+namespace Parser
+
+/-- A hygiene marker introduced by a macro expansion. -/
+@[derive DecidableEq HasToFormat]
+def MacroScope := Nat
+abbrev MacroScopes := List MacroScope
+
+structure SourceInfo :=
+/- Will be inferred after parsing by `Syntax.updateLeading`. During parsing,
+   it is not at all clear what the preceding token was, especially with backtracking. -/
+(leading  : Substring)
+(pos      : String.Pos)
+(trailing : Substring)
+
+/- Node kind generation -/
+
+def mkUniqIdRef : IO (IO.Ref Nat) :=
+IO.mkRef 0
+
+@[init mkUniqIdRef]
+constant nextUniqId : IO.Ref Nat := default _
+
+structure SyntaxNodeKind :=
+(name : Name) (id : Nat)
+
+instance stxKindInh : Inhabited SyntaxNodeKind :=
+⟨{name := default _, id := default _}⟩
+
+instance stxKindBeq : HasBeq SyntaxNodeKind :=
+⟨λ k₁ k₂, k₁.id == k₂.id⟩
+
+def mkNameToKindTable : IO (IO.Ref (NameMap Nat)) :=
+IO.mkRef {}
+
+@[init mkNameToKindTable]
+constant nameToKindTable : IO.Ref (NameMap Nat) := default _
+
+def nextKind (k : Name) : IO SyntaxNodeKind :=
+do m ← nameToKindTable.get,
+   when (m.contains k) (throw $ IO.userError ("Error kind '" ++ toString k ++ "' already exists")),
+   id ← nextUniqId.get,
+   nameToKindTable.set (m.insert k id),
+   nextUniqId.set (id+1),
+   pure { name := k, id := id }
+
+/- Basic node kinds -/
+
+def mkNullKind : IO SyntaxNodeKind := nextKind `null
+@[init mkNullKind] constant nullKind : SyntaxNodeKind := default _
+def mkChoiceKind : IO SyntaxNodeKind := nextKind `choice
+@[init mkChoiceKind] constant choiceKind : SyntaxNodeKind := default _
+def mkOptionSomeKind : IO SyntaxNodeKind := nextKind `some
+@[init mkOptionSomeKind] constant optionSomeKind : SyntaxNodeKind := default _
+def mkOptionNoneKind : IO SyntaxNodeKind := nextKind `none
+@[init mkOptionSomeKind] constant optionNoneKind : SyntaxNodeKind := default _
+def mkManyKind : IO SyntaxNodeKind := nextKind `many
+@[init mkManyKind] constant manyKind : SyntaxNodeKind := default _
+def mkHoleKind : IO SyntaxNodeKind := nextKind `hole
+
+/- Syntax AST -/
+
+inductive Syntax
+| missing
+| node   (kind : SyntaxNodeKind) (args : Array Syntax) (scopes : MacroScopes)
+| atom   (info : Option SourceInfo) (val : String)
+| ident  (info : Option SourceInfo) (rawVal : Substring) (val : Name) (preresolved : List Name) (scopes : MacroScopes)
+
+instance stxInh : Inhabited Syntax :=
+⟨Syntax.missing⟩
+
+def SyntaxNodeKind.fix : SyntaxNodeKind → IO SyntaxNodeKind
+| {name := n, ..} := do
+  m ← nameToKindTable.get,
+  match m.find n with
+  | some id := pure {name := n, id := id}
+  | none    := throw $ IO.userError ("Error unknown Syntax kind '" ++ toString n ++ "'")
+
+partial def Syntax.fixKinds : Syntax → IO Syntax
+| (Syntax.node k args scopes) := do
+  k ← k.fix,
+  args ← args.mmap Syntax.fixKinds,
+  pure (Syntax.node k args scopes)
+| other                       := pure other
+
+inductive IsNode : Syntax → Prop
+| mk (kind : SyntaxNodeKind) (args : Array Syntax) (scopes : MacroScopes) : IsNode (Syntax.node kind args scopes)
+
+def SyntaxNode : Type := {s : Syntax // IsNode s }
+
+def notIsNodeMissing (h : IsNode Syntax.missing) : False                   := match h with end
+def notIsNodeAtom   {info val} (h : IsNode (Syntax.atom info val)) : False := match h with end
+def notIsNodeIdent  {info rawVal val preresolved scopes} (h : IsNode (Syntax.ident info rawVal val preresolved scopes)) : False := match h with end
+
+def unreachIsNodeMissing {α : Type} (h : IsNode Syntax.missing) : α := False.elim (notIsNodeMissing h)
+def unreachIsNodeAtom {α : Type} {info val} (h : IsNode (Syntax.atom info val)) : α := False.elim (notIsNodeAtom h)
+def unreachIsNodeIdent {α : Type} {info rawVal val preresolved scopes} (h : IsNode (Syntax.ident info rawVal val preresolved scopes)) : α :=
+False.elim (match h with end)
+
+@[inline] def withArgs {α : Type} (n : SyntaxNode) (fn : Array Syntax → α) : α :=
+match n with
+| ⟨Syntax.node _ args _, _⟩   := fn args
+| ⟨Syntax.missing, h⟩         := unreachIsNodeMissing h
+| ⟨Syntax.atom _ _, h⟩        := unreachIsNodeAtom h
+| ⟨Syntax.ident _ _ _ _ _, h⟩ := unreachIsNodeIdent h
+
+@[inline] def updateArgs (n : SyntaxNode) (fn : Array Syntax → Array Syntax) : Syntax :=
+match n with
+| ⟨Syntax.node kind args scopes, _⟩ := Syntax.node kind (fn args) scopes
+| ⟨Syntax.missing, h⟩               := unreachIsNodeMissing h
+| ⟨Syntax.atom _ _, h⟩              := unreachIsNodeAtom h
+| ⟨Syntax.ident _ _ _ _ _, h⟩       := unreachIsNodeIdent h
+
+@[inline] def toSyntaxNode {α : Type} (s : Syntax) (base : α) (fn : SyntaxNode → α) : α :=
+match s with
+| Syntax.node kind args scopes := fn ⟨Syntax.node kind args scopes, IsNode.mk kind args scopes⟩
+| other := base
+
+@[inline] def toSyntaxNodeOf {α : Type} (kind : SyntaxNodeKind) (s : Syntax) (base : α) (fn : SyntaxNode → α) : α :=
+match s with
+| Syntax.node k args scopes :=
+  if k == kind then fn ⟨Syntax.node kind args scopes, IsNode.mk kind args scopes⟩
+  else base
+| other := base
+
+-- TODO(Sebastian): exhaustively argue why (if?) this is correct
+-- The basic idea is List concatenation with elimination of adjacent identical scopes
+def MacroScopes.flip : MacroScopes → MacroScopes → MacroScopes
+| ys []      := ys
+| ys (x::xs) := match MacroScopes.flip ys xs with
+  | y::ys := if x == y then ys else x::y::ys
+  | []    := [x]
+
+namespace Syntax
+def flipScopes (scopes : MacroScopes) : Syntax → Syntax
+| (Syntax.ident info rawVal val pre scopes) := Syntax.ident info rawVal val pre (scopes.flip scopes)
+| (Syntax.node kind args scopes)            := Syntax.node kind args (scopes.flip scopes)
+| other := other
+
+local attribute [instance] monadInhabited
+
+@[specialize] partial def mreplace {m : Type → Type} [Monad m] (fn : Syntax → m (Option Syntax)) : Syntax → m Syntax
+| stx@(node kind args scopes) := do
+  o ← fn stx,
+  (match o with
+  | some stx := pure stx
+  | none     := do args ← args.mmap mreplace, pure (node kind args scopes))
+| stx := do o ← fn stx, pure (o.getOrElse stx)
+
+@[inline] def replace {m : Type → Type} [Monad m] (fn : Syntax → m (Option Syntax)) := @mreplace Id _
+
+private def updateInfo : SourceInfo → String.Pos → SourceInfo
+| {leading := {str := s, startPos := sPos, endPos := ePos}, pos := pos, trailing := trailing} last :=
+  {leading := {str := s, startPos := last, endPos := pos}, pos := pos, trailing := trailing}
+
+/- Remark: the State `String.Pos` is the `SourceInfo.trailing.endPos` of the previous token,
+   or the beginning of the String. -/
+@[inline]
+private def updateLeadingAux : Syntax → State String.Pos (Option Syntax)
+| (atom (some info) val) := do
+  last ← get,
+  set info.trailing.endPos,
+  let newInfo := updateInfo info last in
+  pure $ some (atom (some newInfo) val)
+| (ident (some info) rawVal val pre scopes) := do
+  last ← get,
+  set info.trailing.endPos,
+  let newInfo := updateInfo info last in
+  pure $ some (ident (some newInfo) rawVal val pre scopes)
+| _ := pure none
+
+/-- Set `SourceInfo.leading` according to the trailing stop of the preceding token.
+    The Result is a round-tripping Syntax tree IF, in the input Syntax tree,
+    * all leading stops, atom contents, and trailing starts are correct
+    * trailing stops are between the trailing start and the next leading stop.
+
+    Remark: after parsing all `SourceInfo.leading` fields are Empty.
+    The Syntax argument is the output produced by the Parser for `source`.
+    This Function "fixes" the `source.leanding` field.
+
+    Note that, the `SourceInfo.trailing` fields are correct.
+    The implementation of this Function relies on this property. -/
+def updateLeading : Syntax → Syntax :=
+λ stx, Prod.fst <$> (mreplace updateLeadingAux stx).run 0
+
+/-- Retrieve the left-most leaf's info in the Syntax tree. -/
+partial def getHeadInfo : Syntax → Option SourceInfo
+| (atom info _)         := info
+| (ident info _ _ _ _ ) := info
+| (node _ args _)       := args.find getHeadInfo
+| _                     := none
+
+def getPos (stx : Syntax) : Option String.Pos :=
+SourceInfo.pos <$> stx.getHeadInfo
+
+private def reprintLeaf : Option SourceInfo → String → String
+| none        val := val
+| (some info) val := info.leading.toString ++ val ++ info.trailing.toString
+
+partial def reprint : Syntax → Option String
+| (atom info val)           := reprintLeaf info val
+| (ident info rawVal _ _ _) := reprintLeaf info rawVal.toString
+| (node kind args _)        :=
+  if kind == choiceKind then
+    if args.size == 0 then failure
+    else do
+      s ← reprint (args.get 0),
+      args.mfoldlFrom s (λ stx s, do s' ← reprint stx, guard (s == s'), pure s) 1
+  else args.mfoldl "" (λ stx r, do s ← reprint stx, pure $ r ++ s)
+| missing := ""
+
+end Syntax
+
+end Parser
+end Lean

e0821be1323901cd2938e72b18999464cbddbaeb
Leonardo de Moura <leonardo@microsoft.com>
2019-04-29 10:50:10 -0700

    chore(tests/playground/ir): fix test

--- tests/playground/ir.lean
+++ tests/playground/ir.lean
@@ -5,7 +5,7 @@ def tst1 : IO Unit :=
 let fbody : FnBody :=
    FnBody.vdecl `x IRType.uint32 (Expr.lit (LitVal.num 0)) $
    FnBody.ret `x in
-IO.println $ toFormat fbody
+IO.println $ format fbody
 
 def main : IO Unit :=
 tst1

b86d9422b615ddae0a03e516546cea21e21218b9
Leonardo de Moura <leonardo@microsoft.com>
2019-05-27 21:23:38 -0700

    chore(library/compiler/borrowed_annotation): remove old borrow inference code

--- src/library/compiler/borrowed_annotation.h
+++ src/library/compiler/borrowed_annotation.h
@@ -10,8 +10,6 @@ namespace lean {
 expr mk_borrowed(expr const & e);
 bool is_borrowed(expr const & e);
 expr const & get_borrowed_arg(expr const & e);
-bool get_inferred_borrowed_info(environment const & env, name const & fn, buffer<bool> & borrowed_args, bool & borrowed_res);
-environment infer_borrowed_annotations(environment const & env, buffer<comp_decl> const &);
 void initialize_borrowed_annotation();
 void finalize_borrowed_annotation();
 }

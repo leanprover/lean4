9b9465d299857ffa630bffe14f06c328874f1c88
Sebastian Ullrich <sebasti@nullri.ch>
2019-05-29 16:06:06 +0200

    chore(tests/bench): move out from playground/

---
+++ tests/bench/compile.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]; then
+    echo "Usage: compile.sh [file]"
+    exit 1
+fi
+ulimit -s 8192
+BIN_DIR=../../bin
+LEAN=$BIN_DIR/lean
+export LEAN_PATH=../../library:.
+ff=$1
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    # Windows running MSYS2
+    # Replace /c/ with c:, and / with \\
+    ff=$(echo $ff  | sed 's|^/\([a-z]\)/|\1:/|' | sed 's|/|\\\\|g')
+fi
+
+$LEAN --cpp="$ff".cpp "$ff"
+if [ $? -ne 0 ]; then
+    echo "Failed to compile $ff into C++ file"
+    exit 1
+fi
+
+$BIN_DIR/leanc -O3 -o "$ff.out" $ff.cpp
+if [ $? -ne 0 ]; then
+    echo "Failed to compile C++ file $ff.cpp"
+    exit 1
+fi
---
+++ tests/bench/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]; then
+    echo "Usage: run.sh [file] [args]*"
+    exit 1
+fi
+ff=$1
+./compile.sh $ff
+shift 1
+time "./$ff.out" $*

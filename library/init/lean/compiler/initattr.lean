91821529c0e708b6e663f2553180b10e86619765
Leonardo de Moura <leonardo@microsoft.com>
2019-06-18 15:20:17 -0700

    feat(library/init/lean/compiler/initattr): register new `@[init]` attributre using new attribute manager
    This is just the basic skeleton for testing

--- library/init/lean/compiler/initattr.lean
+++ library/init/lean/compiler/initattr.lean
@@ -5,8 +5,21 @@ Authors: Leonardo de Moura
 -/
 prelude
 import init.lean.environment
+import init.lean.attributes
 
 namespace Lean
+
+def mkInitAttr : IO (ParametricAttribute Name) :=
+registerParametricAttribute `myInit "initialization procedure for global references" $ λ env declName stx,
+  match stx with
+  | Syntax.ident _ _ initFnName _ _ :=
+    dbgTrace ("myInit " ++ toString initFnName) $ λ _, Except.ok initFnName
+  | Syntax.missing := Except.error "invalid 'init' attribute, argument is missing"
+  | _ := Except.error "invalid 'init' attribute, unexpected kind of argument"
+
+@[init mkInitAttr]
+constant initAttr : ParametricAttribute Name := default _
+
 @[extern "lean_is_io_unit_init"]
 constant isIOUnitInitFn (env : @& Environment) (fn : @& Name) : Bool := default _
 

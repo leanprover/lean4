6a740723a4113ad8b61b85a3f1423ea1cb6e940c
Leonardo de Moura <leonardo@microsoft.com>
2019-06-24 09:49:59 -0700

    chore(frontends/lean): remove `node!` and `nodeChoice!` macros
    They were used in the parsing framework based on `Parsec`

--- src/frontends/lean/elaborator.h
+++ src/frontends/lean/elaborator.h
@@ -237,8 +237,6 @@ private:
     expr visit_constant(expr const & e, optional<expr> const & expected_type);
     expr visit_lambda(expr const & e, optional<expr> const & expected_type);
     expr visit_pi(expr const & e);
-    expr visit_node_macro(expr const & e, optional<expr> const & expected_type);
-    expr visit_node_choice_macro(expr const & e, bool longest_match, optional<expr> const & expected_type);
     expr visit_mdata(expr const & e, optional<expr> const & expected_type, bool is_app_fn);
     expr visit_app(expr const & e, optional<expr> const & expected_type);
     expr visit_let(expr const & e, optional<expr> const & expected_type);

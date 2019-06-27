235b4c02c1d2c624e26c5ae5c03f5e811418b1a1
Leonardo de Moura <leonardo@microsoft.com>
2019-06-27 09:58:19 -0700

    feat(library/init/lean/compiler/specialize): implement `specExtension` in Lean

--- library/init/lean/compiler/specialize.lean
+++ library/init/lean/compiler/specialize.lean
@@ -48,5 +48,63 @@ hasSpecializeAttrAux env SpecializeAttributeKind.specialize n
 def hasNospecializeAttribute (env : Environment) (n : Name) : Bool :=
 hasSpecializeAttrAux env SpecializeAttributeKind.nospecialize n
 
+inductive SpecArgKind
+| fixed
+| fixedNeutral -- computationally neutral
+| fixedHO      -- higher order
+| fixedInst    -- type class instance
+| other
+
+structure SpecInfo :=
+(mutualDecls : List Name) (argKinds : SpecArgKind)
+
+structure SpecState :=
+(specInfo : SMap Name SpecInfo Name.quickLt := {})
+(cache    : SMap Expr Name Expr.quickLt := {})
+
+inductive SpecEntry
+| info (name : Name) (info : SpecInfo)
+| cache (key : Expr) (fn : Name)
+
+namespace SpecState
+
+instance : Inhabited SpecState := ⟨{}⟩
+
+def addEntry (s : SpecState) (e : SpecEntry) : SpecState :=
+match e with
+| SpecEntry.info name info := { specInfo := s.specInfo.insert name info, .. s }
+| SpecEntry.cache key fn   := { cache    := s.cache.insert key fn, .. s }
+
+def switch : SpecState → SpecState
+| ⟨m₁, m₂⟩ := ⟨m₁.switch, m₂.switch⟩
+
+end SpecState
+
+def mkSpecExtension : IO (SimplePersistentEnvExtension SpecEntry SpecState) :=
+registerSimplePersistentEnvExtension {
+  name          := `specialize,
+  addEntryFn    := SpecState.addEntry,
+  addImportedFn := λ es, (mkStateFromImportedEntries SpecState.addEntry {} es).switch
+}
+
+@[init mkSpecExtension]
+constant specExtension : SimplePersistentEnvExtension SpecEntry SpecState := default _
+
+@[export lean.add_specialization_info_core]
+def addSpecializationInfo (env : Environment) (fn : Name) (info : SpecInfo) : Environment :=
+specExtension.addEntry env (SpecEntry.info fn info)
+
+@[export lean.get_specialization_info_core]
+def getSpecializationInfo (env : Environment) (fn : Name) : Option SpecInfo :=
+(specExtension.getState env).specInfo.find fn
+
+@[export lean.cache_specialization_core]
+def cacheSpecialization (env : Environment) (e : Expr) (fn : Name) : Environment :=
+specExtension.addEntry env (SpecEntry.cache e fn)
+
+@[export lean.get_cached_specialization_core]
+def getCachedSpecialization (env : Environment) (e : Expr) : Option Name :=
+(specExtension.getState env).cache.find e
+
 end Compiler
 end Lean

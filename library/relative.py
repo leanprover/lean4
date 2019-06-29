ca464d25179a8e75ef9f689e177251af83cbd5af
Sebastian Ullrich <sebasti@nullri.ch>
2019-06-29 22:42:14 +0200

    fix(library/relative): `lean --path` returns Windows filenames, but make expects Unix ones
    The joys of cross-platform bootstrapping

--- library/relative.py
+++ library/relative.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2018 Microsoft Corporation. All rights reserved.
 # Released under Apache 2.0 license as described in the file LICENSE.
-# Authors: Simon Hudon
+# Authors: Simon Hudon, Sebastian Ullrich
 
 import sys
 import os
+import re
 
 for x in sys.stdin:
+  # HACK: rewrite Windows path to mingw path
+  x = re.sub(r"^(\w):", lambda m: "/" + m[1].lower(), x).replace('\\', '/').strip()
   print(os.path.relpath(x))

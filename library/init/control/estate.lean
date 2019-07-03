009e8ecd59a417d1ec4be5782d1e750c2a2b6fce
Leonardo de Moura <leonardo@microsoft.com>
2019-07-02 20:45:25 -0700

    chore(library/init/control/estate): style

--- library/init/control/estate.lean
+++ library/init/control/estate.lean
@@ -54,73 +54,73 @@ variables {ε σ α β : Type u}
 
 instance [Inhabited ε] : Inhabited (EState ε σ α) :=
 ⟨fun r => match r with
-      | ⟨Result.ok _ s, _⟩    := Result.error (default ε) s
-      | ⟨Result.error _ _, h⟩ := unreachableError h⟩
+  | ⟨Result.ok _ s, _⟩    := Result.error (default ε) s
+  | ⟨Result.error _ _, h⟩ := unreachableError h⟩
 
 @[inline] protected def pure (a : α) : EState ε σ α :=
 fun r => match r with
-     | ⟨Result.ok _ s, _⟩    := Result.ok a s
-     | ⟨Result.error _ _, h⟩ := unreachableError h
+  | ⟨Result.ok _ s, _⟩    := Result.ok a s
+  | ⟨Result.error _ _, h⟩ := unreachableError h
 
 @[inline] protected def set (s : σ) : EState ε σ PUnit :=
 fun r => match r with
-     | ⟨Result.ok _ _, _⟩    := Result.ok ⟨⟩ s
-     | ⟨Result.error _ _, h⟩ := unreachableError h
+  | ⟨Result.ok _ _, _⟩    := Result.ok ⟨⟩ s
+  | ⟨Result.error _ _, h⟩ := unreachableError h
 
 @[inline] protected def get : EState ε σ σ :=
 fun r => match r with
-     | ⟨Result.ok _ s, _⟩    := Result.ok s s
-     | ⟨Result.error _ _, h⟩ := unreachableError h
+  | ⟨Result.ok _ s, _⟩    := Result.ok s s
+  | ⟨Result.error _ _, h⟩ := unreachableError h
 
 @[inline] protected def modify (f : σ → σ) : EState ε σ PUnit :=
 fun r => match r with
-     | ⟨Result.ok _ s, _⟩    := Result.ok ⟨⟩ (f s)
-     | ⟨Result.error _ _, h⟩ := unreachableError h
+  | ⟨Result.ok _ s, _⟩    := Result.ok ⟨⟩ (f s)
+  | ⟨Result.error _ _, h⟩ := unreachableError h
 
 @[inline] protected def throw (e : ε) : EState ε σ α :=
 fun r => match r with
-     | ⟨Result.ok _ s, _⟩    := Result.error e s
-     | ⟨Result.error _ _, h⟩ := unreachableError h
+  | ⟨Result.ok _ s, _⟩    := Result.error e s
+  | ⟨Result.error _ _, h⟩ := unreachableError h
 
 @[inline] protected def catch (x : EState ε σ α) (handle : ε → EState ε σ α) : EState ε σ α :=
 fun r => match x r with
-     | Result.error e s := handle e (resultOk.mk ⟨⟩ s)
-     | ok               := ok
+  | Result.error e s := handle e (resultOk.mk ⟨⟩ s)
+  | ok               := ok
 
 @[inline] protected def orelse (x₁ x₂ : EState ε σ α) : EState ε σ α :=
 fun r => match x₁ r with
-     | Result.error _ s := x₂ (resultOk.mk ⟨⟩ s)
-     | ok               := ok
+  | Result.error _ s := x₂ (resultOk.mk ⟨⟩ s)
+  | ok               := ok
 
 /-- Alternative orelse operator that allows to select which exception should be used.
     The default is to use the first exception since the standard `orelse` uses the second. -/
 @[inline] protected def orelse' (x₁ x₂ : EState ε σ α) (useFirstEx := true) : EState ε σ α :=
 fun r => match x₁ r with
-     | Result.error e₁ s₁ :=
-       match x₂ (resultOk.mk ⟨⟩ s₁) with
-       | Result.error e₂ s₂ := Result.error (if useFirstEx then e₁ else e₂) s₂
-       | ok                 := ok
-     | ok                 := ok
+  | Result.error e₁ s₁ :=
+    match x₂ (resultOk.mk ⟨⟩ s₁) with
+    | Result.error e₂ s₂ := Result.error (if useFirstEx then e₁ else e₂) s₂
+    | ok                 := ok
+  | ok                 := ok
 
 @[inline] def adaptExcept {ε' : Type u} [HasLift ε ε'] (x : EState ε σ α) : EState ε' σ α :=
 fun r => match x r with
-     | Result.error e s := Result.error (lift e) s
-     | Result.ok a s    := Result.ok a s
+  | Result.error e s := Result.error (lift e) s
+  | Result.ok a s    := Result.ok a s
 
 @[inline] protected def bind (x : EState ε σ α) (f : α → EState ε σ β) : EState ε σ β :=
 fun r => match x r with
-     | Result.ok a s    := f a (resultOk.mk ⟨⟩ s)
-     | Result.error e s := Result.error e s
+  | Result.ok a s    := f a (resultOk.mk ⟨⟩ s)
+  | Result.error e s := Result.error e s
 
 @[inline] protected def map (f : α → β) (x : EState ε σ α) : EState ε σ β :=
 fun r => match x r with
-     | Result.ok a s    := Result.ok (f a) s
-     | Result.error e s := Result.error e s
+  | Result.ok a s    := Result.ok (f a) s
+  | Result.error e s := Result.error e s
 
 @[inline] protected def seqRight (x : EState ε σ α) (y : EState ε σ β) : EState ε σ β :=
 fun r => match x r with
-     | Result.ok _ s    := y (resultOk.mk ⟨⟩ s)
-     | Result.error e s := Result.error e s
+  | Result.ok _ s    := y (resultOk.mk ⟨⟩ s)
+  | Result.error e s := Result.error e s
 
 instance : Monad (EState ε σ) :=
 { bind := @EState.bind _ _, pure := @EState.pure _ _, map := @EState.map _ _, seqRight := @EState.seqRight _ _ }
@@ -132,7 +132,7 @@ instance : MonadState σ (EState ε σ) :=
 { set := @EState.set _ _, get := @EState.get _ _, modify := @EState.modify _ _ }
 
 instance : MonadExcept ε (EState ε σ) :=
-{ throw := @EState.throw _ _, catch := @EState.catch _ _}
+{ throw := @EState.throw _ _, catch := @EState.catch _ _ }
 
 @[inline] def run (x : EState ε σ α) (s : σ) : Result ε σ α :=
 x (resultOk.mk ⟨⟩ s)

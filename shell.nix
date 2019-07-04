65643ee90bb727e2a31040c43ff9d429b36fb5d1
Sebastian Ullrich <sebasti@nullri.ch>
2019-07-04 16:59:57 +0200

    chore(shell.nix): update temci once more

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 let
   lean = pkgs.callPackage ./default.nix {};
-  temci = pkgs.callPackage (builtins.fetchGit { url = https://github.com/parttimenerd/temci.git; rev = "1adbe63e3976693eb82ae1bb17b0b17566473853"; }) {};
+  temci = pkgs.callPackage (builtins.fetchGit { url = https://github.com/parttimenerd/temci.git; rev = "2facd7c78ab35722f34db1d42883ec02f8a0de23"; }) {};
 in pkgs.mkShell rec {
   inputsFrom = [ lean ];
   buildInputs = with pkgs; [ temci clang_7 ccache ninja jemalloc ];

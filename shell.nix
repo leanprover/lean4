f4ea667da6bab8a46a8a00a26a22f62167136fd3
Sebastian Ullrich <sebasti@nullri.ch>
2019-07-04 14:28:35 +0200

    chore(shell.nix): update temci for speedcenter support

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 let
   lean = pkgs.callPackage ./default.nix {};
-  temci = pkgs.callPackage (builtins.fetchGit { url = https://github.com/parttimenerd/temci.git; rev = "66fa7d1eaeb671be32850009304bca5a270b9646"; }) {};
+  temci = pkgs.callPackage (builtins.fetchGit { url = https://github.com/parttimenerd/temci.git; rev = "1adbe63e3976693eb82ae1bb17b0b17566473853"; }) {};
 in pkgs.mkShell rec {
   inputsFrom = [ lean ];
   buildInputs = with pkgs; [ temci clang_7 ccache ninja jemalloc ];

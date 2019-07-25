1f78a5a8e0b7da723a526a62b3fa575d59f05366
Sebastian Ullrich <sebasti@nullri.ch>
2019-07-25 13:00:22 +0200

    chore(shell.nix): no more need for jemalloc, fix debug builds

--- shell.nix
+++ shell.nix
@@ -5,7 +5,9 @@ let
   temci = pkgs.callPackage (builtins.fetchGit { url = https://github.com/parttimenerd/temci.git; rev = "2facd7c78ab35722f34db1d42883ec02f8a0de23"; }) {};
 in pkgs.mkShell rec {
   inputsFrom = [ lean ];
-  buildInputs = with pkgs; [ temci clang_7 ccache ninja jemalloc ];
+  buildInputs = with pkgs; [ temci clang_7 ccache ninja ];
+  # https://github.com/NixOS/nixpkgs/issues/60919
+  hardeningDisable = [ "all" ];
   # TODO: this should not be necessary when leanc starts statically linking binaries
   LD_LIBRARY_PATH = "${pkgs.stdenv.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
 }

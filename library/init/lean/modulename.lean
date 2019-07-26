9bed367cc06751caa974ed2fb2a761e300296422
Leonardo de Moura <leonardo@microsoft.com>
2019-07-26 16:46:03 -0700

    feat(library/init/lean/modulename): add helper functions

--- library/init/lean/modulename.lean
+++ library/init/lean/modulename.lean
@@ -18,6 +18,17 @@ instance : Inhabited ModuleName := ⟨ModuleName.explicit Name.anonymous⟩
 
 instance : HasCoe Name ModuleName := ⟨explicit⟩
 
+def quickLt : ModuleName → ModuleName → Bool
+| (explicit n₁)    (explicit n₂)    := Name.quickLt n₁ n₂
+| (explicit _)     (relative _ _)   := true
+| (relative _ _)   (explicit _)     := false
+| (relative r₁ n₁) (relative r₂ n₂) := r₁ < r₂ || (r₁ == r₂ && Name.quickLt n₁ n₂)
+
+instance : HasToString ModuleName :=
+⟨fun n => match n with
+ | explicit n   => toString n
+ | relative r n => "".pushn '.' r ++ toString n⟩
+
 end ModuleName
 
 end Lean

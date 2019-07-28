10fe453cf62c0bfd5abda2dcda872489afc7f3e0
Leonardo de Moura <leonardo@microsoft.com>
2019-07-28 10:11:35 -0700

    test(tests/lean/run/ubscalar): save UB scalar field test

---
+++ tests/lean/run/ubscalar.lean
@@ -0,0 +1,16 @@
+structure Foo :=
+(flag : Bool   := false)
+(x    : UInt16 := 0)
+(z    : UInt32 := 0)
+(w    : UInt64 := 0)
+(h    : USize  := 0)
+(xs   : List Nat := [])
+
+set_option trace.compiler.ir.init true
+
+def f (s : Foo) : Foo :=
+{ x := s.x + 1, .. s }
+
+def g (flag : Bool) : Foo :=
+let s : Foo := { x := 10, flag := flag };
+f s

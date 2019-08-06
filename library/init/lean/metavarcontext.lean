81854a2d25c6a86c0040c2579638c94cc0060878
Leonardo de Moura <leonardo@microsoft.com>
2019-08-06 10:17:40 -0700

    feat(library/init/lean/metavarcontext): add `MetavarContext`

---
+++ library/init/lean/metavarcontext.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import init.lean.localcontext
+
+namespace Lean
+
+structure MetavarDecl :=
+(userName : Name)
+(lctx     : LocalContext)
+(type     : Expr)
+
+/-
+A delayed assignment for a metavariable `?m`. It represents an assignment of the form
+`?m := (fun fvars => val)`. The local context `lctx` provides the declarations for `fvars`.
+Note that `fvars` may not be defined in the local context for `?m`.
+-/
+structure DelayedMetavarAssignment :=
+(lctx     : LocalContext)
+(fvars    : List Expr)
+(val      : Expr)
+
+structure MetavarContext :=
+(decls       : PHashMap Name MetavarDecl)
+(lAssignment : PHashMap Name Level)
+(eAssignment : PHashMap Name Expr)
+(dAssignment : PHashMap Name DelayedMetavarAssignment)
+
+namespace MetavarContext
+
+end MetavarContext
+end Lean

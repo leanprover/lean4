44bf885829f0b2d6b0b37785951b83866f04a7e3
Leonardo de Moura <leonardo@microsoft.com>
2019-08-15 09:04:01 -0700

    chore(runtime/alloc): simplify header file

--- src/runtime/alloc.cpp
+++ src/runtime/alloc.cpp
@@ -11,6 +11,9 @@ Author: Leonardo de Moura
 #include "runtime/object.h"
 #include "runtime/alloc.h"
 
+#define LEAN_OBJECT_SIZE_DELTA     8
+#define LEAN_MAX_SMALL_OBJECT_SIZE 512
+#define LEAN_PAGE_SIZE             8192        // 8 Kb
 #define LEAN_SEGMENT_SIZE          8*1024*1024 // 8 Mb
 #define LEAN_NUM_SLOTS             (LEAN_MAX_SMALL_OBJECT_SIZE / LEAN_OBJECT_SIZE_DELTA)
 #define LEAN_MAX_TO_EXPORT_OBJS    1024
@@ -40,6 +43,42 @@ struct alloc_stats {
 };
 static alloc_stats g_alloc_stats;
 #endif
+
+struct heap;
+struct page;
+struct page_header {
+    atomic<heap *>   m_heap;
+    page *           m_next;
+    page *           m_prev;
+    void *           m_free_list;
+    unsigned         m_max_free;
+    unsigned         m_num_free;
+    unsigned         m_slot_idx;
+    bool             m_in_page_free_list;
+};
+
+struct page {
+    page_header m_header;
+    char        m_data[LEAN_PAGE_SIZE - sizeof(page_header)];
+    page * get_next() const { return m_header.m_next; }
+    page * get_prev() const { return m_header.m_prev; }
+    void set_next(page * n) { m_header.m_next = n; }
+    void set_prev(page * p) { m_header.m_prev = p; }
+    void set_heap(heap * h) { m_header.m_heap = h; }
+    heap * get_heap() { return m_header.m_heap; }
+    bool has_many_free() const { return m_header.m_num_free > m_header.m_max_free / 4; }
+    bool in_page_free_list() const { return m_header.m_in_page_free_list; }
+    unsigned get_slot_idx() const { return m_header.m_slot_idx; }
+    void push_free_obj(void * o);
+};
+
+inline size_t align(size_t v, size_t a) {
+    return (v / a)*a + a * (v % a != 0);
+}
+
+inline char * align_ptr(char * p, size_t a) {
+    return reinterpret_cast<char*>(align(reinterpret_cast<size_t>(p), a));
+}
 
 struct segment {
     segment *    m_next{nullptr};
@@ -108,6 +147,20 @@ static inline page * get_page_of(void * o) {
 LEAN_THREAD_GLOBAL_PTR(page *, g_curr_pages);
 LEAN_THREAD_PTR(heap, g_heap);
 static heap_manager * g_heap_manager = nullptr;
+
+inline unsigned get_slot_idx(unsigned obj_size) {
+    lean_assert(obj_size > 0);
+    lean_assert(align(obj_size, LEAN_OBJECT_SIZE_DELTA) == obj_size);
+    return obj_size / LEAN_OBJECT_SIZE_DELTA - 1;
+}
+
+inline void set_next_obj(void * obj, void * next) {
+    *reinterpret_cast<void**>(obj) = next;
+}
+
+inline void * get_next_obj(void * obj) {
+    return *reinterpret_cast<void**>(obj);
+}
 
 static inline void page_list_insert(page * & head, page * new_head) {
     if (head)
@@ -372,7 +425,7 @@ void dealloc(void * o, size_t sz) {
     }
 }
 
-void * alloc_small_slow(size_t sz, unsigned slot_idx) {
+static void * alloc_small_slow(size_t sz, unsigned slot_idx) {
     page * p = g_heap->m_curr_page[slot_idx];
     if (g_heap->m_page_free_list[slot_idx] == nullptr) {
         g_heap->import_objs();
@@ -389,10 +442,28 @@ void * alloc_small_slow(size_t sz, unsigned slot_idx) {
     return r;
 }
 
-void * big_alloc(size_t sz) {
-    void * r = malloc(sz);
-    if (r == nullptr) throw std::bad_alloc();
-    return r;
+void * alloc_small(size_t sz) {
+#ifdef LEAN_RUNTIME_STATS
+    return alloc(sz);
+#else
+    sz = allocator::align(sz, LEAN_OBJECT_SIZE_DELTA);
+    if (sz <= LEAN_MAX_SMALL_OBJECT_SIZE) {
+        lean_assert(allocator::g_curr_pages);
+        unsigned slot_idx = allocator::get_slot_idx(sz);
+        allocator::page * p = allocator::g_curr_pages[slot_idx];
+        void * r = p->m_header.m_free_list;
+        if (r != nullptr) {
+            p->m_header.m_free_list = allocator::get_next_obj(r);
+            p->m_header.m_num_free--;
+            return r;
+        }
+        return alloc_small_slow(sz, slot_idx);
+    } else {
+        void * r = malloc(sz);
+        if (r == nullptr) throw std::bad_alloc();
+        return r;
+    }
+#endif
 }
 
 void initialize_alloc() {

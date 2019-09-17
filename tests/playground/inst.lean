6d7041fcb43d4a5d2d26d17b045557d9e8fc034c
Leonardo de Moura <leonardo@microsoft.com>
2019-09-16 18:30:35 -0700

    test(tests/playground/inst): add `instantiate` and `instantiateRev` simple tests

---
+++ tests/playground/inst.lean
@@ -0,0 +1,16 @@
+import init.lean.expr
+open Lean
+
+def main (xs : List String) : IO Unit :=
+do
+let f := mkConst `f;
+let x := Expr.bvar 0;
+let y := Expr.bvar 1;
+let t := Expr.app (Expr.app (Expr.app f x) y) x;
+let a := mkConst `a;
+let b := Expr.app f (mkConst `b);
+IO.println t.dbgToString;
+IO.println (t.instantiate [a, b].toArray).dbgToString;
+IO.println (t.instantiateRev [a, b].toArray).dbgToString;
+IO.println (t.instantiate [a].toArray).dbgToString;
+pure ()

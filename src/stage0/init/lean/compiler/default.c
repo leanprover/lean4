9d2eadc754731d2bcdc064f9aad2ac95323ad7ce
Leonardo de Moura <leonardo@microsoft.com>
2019-10-01 14:08:31 -0700

    chore(library/init/lean/compiler): delete `@[effectful]`

--- src/stage0/init/lean/compiler/default.c
+++ src/stage0/init/lean/compiler/default.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: init.lean.compiler.default
-// Imports: init.lean.compiler.inlineattrs init.lean.compiler.specialize init.lean.compiler.constfolding init.lean.compiler.closedtermcache init.lean.compiler.externattr init.lean.compiler.implementedbyattr init.lean.compiler.effectfulattr init.lean.compiler.neverextractattr init.lean.compiler.ir.default
+// Imports: init.lean.compiler.inlineattrs init.lean.compiler.specialize init.lean.compiler.constfolding init.lean.compiler.closedtermcache init.lean.compiler.externattr init.lean.compiler.implementedbyattr init.lean.compiler.neverextractattr init.lean.compiler.ir.default
 #include "runtime/lean.h"
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -19,7 +19,6 @@ lean_object* initialize_init_lean_compiler_constfolding(lean_object*);
 lean_object* initialize_init_lean_compiler_closedtermcache(lean_object*);
 lean_object* initialize_init_lean_compiler_externattr(lean_object*);
 lean_object* initialize_init_lean_compiler_implementedbyattr(lean_object*);
-lean_object* initialize_init_lean_compiler_effectfulattr(lean_object*);
 lean_object* initialize_init_lean_compiler_neverextractattr(lean_object*);
 lean_object* initialize_init_lean_compiler_ir_default(lean_object*);
 static bool _G_initialized = false;
@@ -38,8 +37,6 @@ if (lean_io_result_is_error(w)) return w;
 w = initialize_init_lean_compiler_externattr(w);
 if (lean_io_result_is_error(w)) return w;
 w = initialize_init_lean_compiler_implementedbyattr(w);
-if (lean_io_result_is_error(w)) return w;
-w = initialize_init_lean_compiler_effectfulattr(w);
 if (lean_io_result_is_error(w)) return w;
 w = initialize_init_lean_compiler_neverextractattr(w);
 if (lean_io_result_is_error(w)) return w;

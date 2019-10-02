c61c4ddcd5351a81bc00b9c59ca3fc1d48a8ccf3
Sebastian Ullrich <sebasti@nullri.ch>
2019-10-02 10:49:52 +0200

    chore(default.nix): update to LLVM 9

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 import (builtins.fetchTarball {
-    name = "nixos-unstable-2019-08-28";
+    name = "nixos-unstable-2019-10-02";
     # Commit hash from `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-    url = https://github.com/nixos/nixpkgs/archive/3f4144c30a6351dd79b177328ec4dea03e2ce45f.tar.gz;
+    url = https://github.com/nixos/nixpkgs/archive/cbf29876cfc2f81186d2e1e233bf992dfcb07fad.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "1qg5n60n3fr6cypihnrjw451fadps5pysj5p0vvfb320mpfvlbjb";
+    sha256 = "1fvb6dkmcfhrwki0v8wqfr3c8mmhhzqdjnzbcdyw201dx8dj4iqn";
   }) {}

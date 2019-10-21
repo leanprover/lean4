c30e9ca551a85fa8f7a0ce080bcc93ce0eb9ccd1
Leonardo de Moura <leonardo@microsoft.com>
2019-10-21 10:51:55 -0700

    chore: add `TypeContext.lean` file

---
+++ library/Init/Lean/TypeContext.lean
@@ -0,0 +1,14 @@
+/-
+Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Init.Lean.Expr
+
+
+/-
+Notes:
+- Track whether a metavariable assignment was used to enable caching.
+- Add TypeContextConfig type class
+-/

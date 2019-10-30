ab88c312b55886173a8e5e22508947c18fbbe3a7
Leonardo de Moura <leonardo@microsoft.com>
2019-10-30 15:51:37 -0700

    chore: `TypeContext` => `TypeInference`

--- library/Init/Lean/TypeInference.lean
+++ library/Init/Lean/TypeInference.lean
@@ -10,6 +10,13 @@ import Init.Lean.Environment
 import Init.Lean.AbstractMetavarContext
 import Init.Lean.Trace
 
+/-
+This module provides three (mutually dependent) goodies:
+1- Weak head normal form computation with support for metavariables and transparency modes.
+2- Definitionally equality checking with support for metavariables (aka unification modulo definitional equality).
+3- Type inference.
+-/
+
 namespace Lean
 inductive TransparencyMode
 | All | Semireducible | Instances | Reducible | None
@@ -20,35 +27,35 @@ structure LocalInstance :=
 
 abbrev LocalInstances := Array LocalInstance
 
-structure UnifierOptions :=
+structure UnifierConfig :=
 (foApprox           : Bool := false)
 (ctxApprox          : Bool := false)
 (quasiPatternApprox : Bool := false)
 
-structure TCOptions :=
+structure TypeInferenceConfig :=
 (opts           : Options          := {})
-(unifierConfig  : UnifierOptions   := {})
+(unifierConfig  : UnifierConfig    := {})
 (transparency   : TransparencyMode := TransparencyMode.Semireducible)
 (smartUnfolding : Bool             := true)
 (useZeta        : Bool             := true)
 
-/- Abstract cache interfact for `TypeContext` functions.
+/- Abstract cache interfact for `TypeInference` functions.
    TODO: add missing methods. -/
-class AbstractTCCache (ϕ : Type) :=
+class AbstractTypeInferenceCache (ϕ : Type) :=
 (getWHNF : ϕ → TransparencyMode → Expr → Option Expr)
 (setWHNF : ϕ → TransparencyMode → Expr → Expr → ϕ)
 
 -- TODO: add special cases
-inductive TCException
-| other : String → TCException
+inductive TypeInferenceException
+| other : String → TypeInferenceException
 
-structure TCContext :=
+structure TypeInferenceContext :=
 (env            : Environment)
 (lctx           : LocalContext)
 (localInstances : LocalInstances)
-(config         : TCOptions := {})
+(config         : TypeInferenceConfig := {})
 
-structure TCState (σ ϕ : Type) :=
+structure TypeInferenceState (σ ϕ : Type) :=
 (mctx           : σ)
 (cache          : ϕ)
 (ngen           : NameGenerator)
@@ -56,35 +63,34 @@ structure TCState (σ ϕ : Type) :=
 (usedAssignment : Bool := false)
 (postponed      : Array (Level × Level) := #[])
 
-instance TCState.backtrackable {σ ϕ} : EState.Backtrackable σ (TCState σ ϕ) :=
+instance TypeInferenceState.backtrackable {σ ϕ} : EState.Backtrackable σ (TypeInferenceState σ ϕ) :=
 { save    := fun s => s.mctx,
   restore := fun s mctx => { mctx := mctx, .. s } }
 
-/-- Abstract Type Context Monad -/
-abbrev ATCM (σ ϕ : Type) := ReaderT TCContext (EState TCException (TCState σ ϕ))
+/-- Type Context Monad -/
+abbrev TypeInferenceM (σ ϕ : Type) := ReaderT TypeInferenceContext (EState TypeInferenceException (TypeInferenceState σ ϕ))
 
-namespace AbstractTypeContext
+namespace TypeInference
 variables {σ ϕ : Type}
 
-private def getOptions : ATCM σ ϕ Options :=
+private def getOptions : TypeInferenceM σ ϕ Options :=
 do ctx ← read; pure ctx.config.opts
 
-private def getTraceState : ATCM σ ϕ TraceState :=
+private def getTraceState : TypeInferenceM σ ϕ TraceState :=
 do s ← get; pure s.traceState
 
-instance tracer : SimpleMonadTracerAdapter (ATCM σ ϕ) :=
+instance tracer : SimpleMonadTracerAdapter (TypeInferenceM σ ϕ) :=
 { getOptions       := getOptions,
   getTraceState    := getTraceState,
   modifyTraceState := fun f => modify $ fun s => { traceState := f s.traceState, .. s } }
 
-end AbstractTypeContext
+end TypeInference
 
-inductive TypeContextNoCache
+inductive TypeInferenceNoCache
 | mk
 
-instance typeContextNoCacheIsAbstractTCCache : AbstractTCCache TypeContextNoCache :=
+instance typeContextNoCacheIsAbstractTCCache : AbstractTypeInferenceCache TypeInferenceNoCache :=
 { getWHNF := fun _ _ _ => none,
-  setWHNF := fun s _ _ _ => s
-}
+  setWHNF := fun s _ _ _ => s }
 
 end Lean

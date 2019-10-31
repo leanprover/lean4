136b2c517121df4f283fe760f896197bd2c1c921
Leonardo de Moura <leonardo@microsoft.com>
2019-10-30 19:29:21 -0700

    fix: save `updateLhs` and `updateRhs` when postponing constraints

--- library/Init/Lean/TypeInference.lean
+++ library/Init/Lean/TypeInference.lean
@@ -55,13 +55,19 @@ structure TypeInferenceContext :=
 (localInstances : LocalInstances)
 (config         : TypeInferenceConfig := {})
 
+structure PostponedEntry :=
+(lhs       : Level)
+(updateLhs : Bool)
+(rhs       : Level)
+(updateRhs : Bool)
+
 structure TypeInferenceState (σ ϕ : Type) :=
 (mctx           : σ)
 (cache          : ϕ)
 (ngen           : NameGenerator)
 (traceState     : TraceState)
 (usedAssignment : Bool := false)
-(postponed      : Array (Level × Level) := #[])
+(postponed      : Array PostponedEntry := #[])
 
 instance TypeInferenceState.backtrackable {σ ϕ} : EState.Backtrackable σ (TypeInferenceState σ ϕ) :=
 { save    := fun s => s.mctx,
@@ -128,50 +134,52 @@ do n ← mkFreshLevelMVar;
    let lhs := mkMaxArgsDiff mvarId v n;
    assignLevel mvarId lhs
 
-private def postponeIsLevelDefEq (u v : Level) : TypeInferenceM σ ϕ Unit :=
-modify $ fun s => { postponed := s.postponed.push (u, v), .. s }
+private def postponeIsLevelDefEq (postpone : Bool) (lhs : Level) (updateLhs : Bool) (rhs : Level) (updateRhs : Bool) : TypeInferenceM σ ϕ Bool :=
+if postpone then do
+  modify $ fun s => { postponed := s.postponed.push { lhs := lhs, updateLhs := updateLhs, rhs := rhs, updateRhs := updateRhs }, .. s };
+  pure true
+else
+  pure false
 
-private partial def isLevelDefEqAux [AbstractMetavarContext σ] (updateLeft updateRight postpone : Bool) : Level → Level → TypeInferenceM σ ϕ Bool
-| Level.succ u, Level.succ v => isLevelDefEqAux u v
-| u, v =>
-  if u == v then
+private partial def isLevelDefEqAux [AbstractMetavarContext σ] (updateLhs updateRhs postpone : Bool) : Level → Level → TypeInferenceM σ ϕ Bool
+| Level.succ lhs, Level.succ rhs => isLevelDefEqAux lhs rhs
+| lhs, rhs =>
+  if lhs == rhs then
     pure true
   else do
-    trace! `type_context.level_is_def_eq (u ++ " =?= " ++ v);
-    u' ← instantiateLevelMVars u;
-    let u' := u'.normalize;
-    v' ← instantiateLevelMVars v;
-    let v' := v'.normalize;
-    if u != u' || v != v' then
-      isLevelDefEqAux u' v'
+    trace! `type_context.level_is_def_eq (lhs ++ " =?= " ++ rhs);
+    lhs' ← instantiateLevelMVars lhs;
+    let lhs' := lhs'.normalize;
+    rhs' ← instantiateLevelMVars rhs;
+    let rhs' := rhs'.normalize;
+    if lhs != lhs' || rhs != rhs' then
+      isLevelDefEqAux lhs' rhs'
     else do
       mctx ← getMCtx;
-      if (!updateLeft  || !hasAssignableLevelMVar mctx u) &&
-         (!updateRight || !hasAssignableLevelMVar mctx v) then
+      if (!updateLhs || !hasAssignableLevelMVar mctx lhs) &&
+         (!updateRhs || !hasAssignableLevelMVar mctx rhs) then
         pure false
-      else if updateLeft && u.isMVar && !isReadOnlyLevelMVar mctx u.mvarId! && !u.occurs v then do
-        assignLevel u.mvarId! v;
+      else if updateLhs && lhs.isMVar && !isReadOnlyLevelMVar mctx lhs.mvarId! && !lhs.occurs rhs then do
+        assignLevel lhs.mvarId! rhs;
         pure true
-      else if auxMVarSupport σ && updateLeft && u.isMVar && !isReadOnlyLevelMVar mctx u.mvarId! && !strictOccursMax u v then do
-        solveSelfMax u.mvarId! v;
+      else if auxMVarSupport σ && updateLhs && lhs.isMVar && !isReadOnlyLevelMVar mctx lhs.mvarId! && !strictOccursMax lhs rhs then do
+        solveSelfMax lhs.mvarId! rhs;
         pure true
-      else if updateRight && v.isMVar && !isReadOnlyLevelMVar mctx v.mvarId! && !v.occurs u then do
-        assignLevel v.mvarId! u;
+      else if updateRhs && rhs.isMVar && !isReadOnlyLevelMVar mctx rhs.mvarId! && !rhs.occurs lhs then do
+        assignLevel rhs.mvarId! lhs;
         pure true
-      else if auxMVarSupport σ && updateRight && v.isMVar && !isReadOnlyLevelMVar mctx v.mvarId! && !strictOccursMax v u then do
-        solveSelfMax v.mvarId! u;
+      else if auxMVarSupport σ && updateRhs && rhs.isMVar && !isReadOnlyLevelMVar mctx rhs.mvarId! && !strictOccursMax rhs lhs then do
+        solveSelfMax rhs.mvarId! lhs;
         pure true
-      else if u.isMVar || v.isMVar then
+      else if lhs.isMVar || rhs.isMVar then
         pure false
       else
-        match u.dec, v.dec with
-        | some u₁, some v₁ => isLevelDefEqAux u₁ v₁
-        | _,       _       =>
-          if postpone then do
-            postponeIsLevelDefEq u v;
-            pure true
-          else
-            pure false
+        if lhs.isSucc || rhs.isSucc then
+          match lhs.dec, rhs.dec with
+          | some lhs', some rhs' => isLevelDefEqAux lhs' rhs'
+          | _,         _         => postponeIsLevelDefEq postpone lhs updateLhs rhs updateRhs
+        else
+          postponeIsLevelDefEq postpone lhs updateLhs rhs updateRhs
 
 end TypeInference
 

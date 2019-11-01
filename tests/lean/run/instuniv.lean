ec71dd256f9f0ada84c383281731ba6e400310dd
Leonardo de Moura <leonardo@microsoft.com>
2019-10-31 21:01:31 -0700

    chore: fix test

--- tests/lean/run/instuniv.lean
+++ tests/lean/run/instuniv.lean
@@ -5,8 +5,8 @@ def tst : IO Unit :=
 do env ← importModules [`Init.Data.Array.Default];
    match env.find `Array.foldl with
    | some info => do
-     IO.println (info.instantiateTypeUnivParams [Level.zero, Level.zero]);
-     IO.println (info.instantiateValueUnivParams [Level.zero, Level.zero]);
+     IO.println (info.instantiateTypeLevelParams [Level.zero, Level.zero]);
+     IO.println (info.instantiateValueLevelParams [Level.zero, Level.zero]);
      pure ()
    | none      => IO.println ("Array.foldl not found");
    pure ()

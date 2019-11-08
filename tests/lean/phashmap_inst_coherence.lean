6d358a5deec6ee44106e1c3e113a321925db9dc5
Leonardo de Moura <leonardo@microsoft.com>
2019-11-08 08:23:10 -0800

    test: add test to demonstrate why it is useful to include instances in the PersistentHashMap type

---
+++ tests/lean/phashmap_inst_coherence.lean
@@ -0,0 +1,11 @@
+import Init.Data.PersistentHashMap
+
+def m : PersistentHashMap Nat Nat :=
+let m : PersistentHashMap Nat Nat := {};
+m.insert 1 1
+
+def natDiffHash : Hashable Nat :=
+⟨fun n => USize.ofNat $ n+10⟩
+
+-- The following example should fail since the `Hashable` instance used to create `m` is not `natDiffHash`
+#eval @PersistentHashMap.find Nat Nat _ natDiffHash m 1

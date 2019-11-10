02b13705cf68f27a141b9224b5470010bf6b2a78
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-10 13:40:48 +0100

    fix: print messages to stderr
    This ensures that errors during dependency resolution in the Makefile actually show up

--- src/library/messages.cpp
+++ src/library/messages.cpp
@@ -34,7 +34,7 @@ std::ostream & operator<<(std::ostream & out, message const & msg) {
 void report_message(message const & msg0) {
     if (!get_global_ios().get_options().get_bool(name {"trace", "as_messages"}, false)) {
         // Print immediately. Still add as a message so that we get the error code correct.
-        get_global_ios().get_regular_stream() << msg0;
+        get_global_ios().get_diagnostic_stream() << msg0;
     }
     lean_assert(global_message_log());
     global_message_log()->add(msg0);

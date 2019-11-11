389f32e1c8bef91d50838c346994c93b57032764
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-11 10:31:39 +0100

    chore: bench/default.nix: fix and improve Lean building

--- tests/bench/default.nix
+++ tests/bench/default.nix
@@ -1,18 +1,12 @@
 { pkgs ? import ./nixpkgs.nix }:
 
 let
-  lean = {}:
-    (pkgs.callPackage ../../default.nix {}).overrideAttrs (attrs: {
-     # pin Lean commit to avoid rebuilds
-     # 2019-11-07
-     src = builtins.fetchGit { url = ../../.; rev = "3b6755dea1acf0a1a3196131dc3bf9cb7d1ea5c8"; };
-    });
+  # pin Lean commit to avoid rebuilds
+  # 2019-11-07
+  lean = import (builtins.fetchGit { url = ../../.; rev = "3b6755dea1acf0a1a3196131dc3bf9cb7d1ea5c8"; }) {};
   # for binarytrees.hs
   ghcPackages = p: [ p.parallel ];
-  ghc = (pkgs.haskellPackages.ghcWithPackages ghcPackages).override {
-    ghc = pkgs.haskell.compiler.ghc881;
-    withLLVM = true;
-  };
+  ghc = pkgs.haskell.packages.ghc881.ghcWithPackages ghcPackages; #.override { withLLVM = true; };
   ocaml = pkgs.ocaml-ng.ocamlPackages_latest.ocaml;
   # note that this will need to be compiled from source
   ocamlFlambda = ocaml.override { flambdaSupport = true; };
@@ -39,29 +33,25 @@ let
 in pkgs.stdenv.mkDerivation rec {
   name = "bench";
   src = pkgs.lib.sourceFilesBySuffices ./. ["Makefile" "leanpkg.path" "temci.yaml" ".py" ".lean" ".hs" ".ml" ".sml"];
-  LEAN_BIN = "${lean {}}/bin";
+  LEAN_BIN = "${lean}/bin";
   #LEAN_GCC_BIN = "${lean { stdenv = pkgs.gcc9Stdenv; }}/bin";
-  LEAN_NO_REUSE_BIN = "${(lean {}).overrideAttrs (attrs: {
+  bootstrapChanges = ''
+    make ''${enableParallelBuilding:+-j''${NIX_BUILD_CORES} -l''${NIX_BUILD_CORES}} SHELL=$SHELL update-stage0
+    make ''${enableParallelBuilding:+-j''${NIX_BUILD_CORES} -l''${NIX_BUILD_CORES}} SHELL=$SHELL clean-olean
+  '';
+  LEAN_NO_REUSE_BIN = "${lean.overrideAttrs (attrs: {
     prePatch = ''
-      substituteInPlace library/init/lean/compiler/ir/default.lean --replace "decls.map Decl.insertResetReuse" "decls"
+      substituteInPlace library/Init/Lean/Compiler/IR/Default.lean --replace "decls.map Decl.insertResetReuse" "decls"
     '';
-    preBuild = ''
-      # bootstrap changes
-      make update-stage0
-      make clean-olean
-    '';
+    preBuild = bootstrapChanges;
   })}/bin";
-  LEAN_NO_BORROW_BIN = "${(lean {}).overrideAttrs (attrs: {
+  LEAN_NO_BORROW_BIN = "${lean.overrideAttrs (attrs: {
     prePatch = ''
-      substituteInPlace library/init/lean/compiler/ir/default.lean --replace "inferBorrow" "pure"
+      substituteInPlace library/Init/Lean/Compiler/IR/Default.lean --replace "inferBorrow" "pure"
     '';
-    preBuild = ''
-      # bootstrap changes
-      make update-stage0
-      make clean-olean
-    '';
+    preBuild = bootstrapChanges;
   })}/bin";
-  LEAN_NO_ST_BIN = "${(lean {}).overrideAttrs (attrs: { prePatch = ''
+  LEAN_NO_ST_BIN = "${lean.overrideAttrs (attrs: { prePatch = ''
     substituteInPlace src/runtime/object.h --replace "c_init_mem_kind = object_memory_kind::STHeap" "c_init_mem_kind = object_memory_kind::MTHeap"
   ''; })}/bin";
   GHC = "${ghc}/bin/ghc";

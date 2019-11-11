01bd502db12b6390fe241c6a76452dd8c694d681
Leonardo de Moura <leonardo@microsoft.com>
2019-11-11 12:07:27 -0800

    test: add more tests for MetaM

--- tests/lean/run/meta1.lean
+++ tests/lean/run/meta1.lean
@@ -8,9 +8,9 @@ do env ← importModules mods;
    | EStateM.Result.ok type s   => IO.println (toString e ++ " : " ++ toString type)
    | EStateM.Result.error err _ => throw (IO.userError (toString err))
 
-def tstWHNF (mods : List Name) (e : Expr) : IO Unit :=
+def tstWHNF (mods : List Name) (e : Expr) (t := TransparencyMode.default) : IO Unit :=
 do env ← importModules mods;
-   match whnf e {} { env := env } with
+   match whnf e { config := { transparency := t } } { env := env } with
    | EStateM.Result.ok type s   => IO.println (toString e ++ " ==> " ++ toString type)
    | EStateM.Result.error err _ => throw (IO.userError (toString err))
 
@@ -49,3 +49,59 @@ let add   := Expr.const `Nat.add [];
 mkApp add #[Expr.lit (Literal.natVal 3), Expr.lit (Literal.natVal 5)]
 
 #eval tstWHNF [`Init.Data.Nat] t5
+#eval tstWHNF [`Init.Data.Nat] t5 TransparencyMode.reducible
+
+set_option pp.all true
+#check @List.cons Nat
+
+def t6 : Expr :=
+let map  := Expr.const `List.map [Level.one, Level.one];
+let nat  := Expr.const `Nat [];
+let add  := Expr.const `Nat.add [];
+let f    := Expr.lam `x BinderInfo.default nat (mkApp add #[Expr.bvar 0, Expr.lit (Literal.natVal 1)]);
+let cons := Expr.app (Expr.const `List.cons [Level.zero]) nat;
+let nil  := Expr.app (Expr.const `List.nil [Level.zero]) nat;
+let one  := Expr.lit (Literal.natVal 1);
+let four := Expr.lit (Literal.natVal 4);
+let xs   := Expr.app (Expr.app cons one) (Expr.app (Expr.app cons four) nil);
+mkApp map #[nat, nat, f, xs]
+
+#eval tstInferType [`Init.Data.List] t6
+#eval tstWHNF [`Init.Data.List] t6
+
+#eval tstInferType [] $ Expr.sort Level.zero
+
+#eval tstInferType [`Init.Data.List] $ Expr.lam `a BinderInfo.implicit (Expr.sort Level.one) (Expr.lam `x BinderInfo.default (Expr.bvar 0) (Expr.lam `xs BinderInfo.default (Expr.app (Expr.const `List [Level.zero]) (Expr.bvar 1)) (Expr.bvar 0)))
+
+def t7 : Expr :=
+let nat  := Expr.const `Nat [];
+let one  := Expr.lit (Literal.natVal 1);
+Expr.letE `x nat one one
+
+#eval tstInferType [`Init.Core] $ t7
+#eval tstWHNF [`Init.Core] $ t7
+
+def t8 : Expr :=
+let nat  := Expr.const `Nat [];
+let one  := Expr.lit (Literal.natVal 1);
+let add  := Expr.const `Nat.add [];
+Expr.letE `x nat one (mkApp add #[one, Expr.bvar 0])
+
+#eval tstInferType [`Init.Core] $ t8
+#eval tstWHNF [`Init.Core] $ t8
+
+def t9 : Expr :=
+let nat  := Expr.const `Nat [];
+Expr.letE `a (Expr.sort Level.one) nat (Expr.forallE `x BinderInfo.default (Expr.bvar 0) (Expr.bvar 1))
+
+#eval tstInferType [`Init.Core] $ t9
+#eval tstWHNF [`Init.Core] $ t9
+
+#eval tstInferType [`Init.Core] $ Expr.lit (Literal.natVal 10)
+#eval tstInferType [`Init.Core] $ Expr.lit (Literal.strVal "hello")
+#eval tstInferType [`Init.Core] $ Expr.mdata {} $ Expr.lit (Literal.natVal 10)
+
+#eval tstInferType [`Init.Lean.Trace] (Expr.proj `Inhabited 0 (Expr.const `Lean.TraceState.Inhabited []))
+#eval tstInferType [`Init.Lean.Trace] (Expr.proj `Lean.TraceState 0 (Expr.proj `Inhabited 0 (Expr.const `Lean.TraceState.Inhabited [])))
+#eval tstWHNF [`Init.Lean.Trace] (Expr.proj `Inhabited 0 (Expr.const `Lean.TraceState.Inhabited []))
+#eval tstWHNF [`Init.Lean.Trace] (Expr.proj `Lean.TraceState 0 (Expr.proj `Inhabited 0 (Expr.const `Lean.TraceState.Inhabited [])))

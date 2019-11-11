9d72fc620eaa7eb570c8a1d0abef45be335440c5
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-11 19:23:48 +0100

    feat: nix: use ccache by default

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
 { pkgs ? import ./nix/nixpkgs.nix, llvmPackages ? pkgs.llvmPackages_9 }:
 
-pkgs.callPackage ./nix/derivation.nix { inherit llvmPackages; }
+# always use clang inside ccache
+let stdenv = pkgs.overrideCC pkgs.stdenv (pkgs.ccacheWrapper.override { cc = llvmPackages.clang; });
+in pkgs.callPackage ./nix/derivation.nix { inherit llvmPackages stdenv; }

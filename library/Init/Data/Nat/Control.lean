37b2e7bf8b910ba68a95262983f479c6d138a44b
Leonardo de Moura <leonardo@microsoft.com>
2019-11-12 15:29:27 -0800

    feat: use `Monad` instead of `Application`
    If the `Applicative` version is needed in the future, we should name
    it using the `A` suffix instead of `M`.

--- library/Init/Data/Nat/Control.lean
+++ library/Init/Data/Nat/Control.lean
@@ -11,18 +11,18 @@ import Init.Data.Nat.Basic
 namespace Nat
 universes u v
 
-@[specialize] def forMAux {m} [Applicative m] (f : Nat → m Unit) (n : Nat) : Nat → m Unit
+@[specialize] def forMAux {m} [Monad m] (f : Nat → m Unit) (n : Nat) : Nat → m Unit
 | 0   => pure ()
-| i+1 => f (n-i-1) *> forMAux i
+| i+1 => do f (n-i-1); forMAux i
 
-@[inline] def forM {m} [Applicative m] (n : Nat) (f : Nat → m Unit) : m Unit :=
+@[inline] def forM {m} [Monad m] (n : Nat) (f : Nat → m Unit) : m Unit :=
 forMAux f n n
 
-@[specialize] def forRevMAux {m} [Applicative m] (f : Nat → m Unit) : Nat → m Unit
+@[specialize] def forRevMAux {m} [Monad m] (f : Nat → m Unit) : Nat → m Unit
 | 0   => pure ()
-| i+1 => f i *> forRevMAux i
+| i+1 => do f i; forRevMAux i
 
-@[inline] def forRevM {m} [Applicative m] (n : Nat) (f : Nat → m Unit) : m Unit :=
+@[inline] def forRevM {m} [Monad m] (n : Nat) (f : Nat → m Unit) : m Unit :=
 forRevMAux f n
 
 @[specialize] def foldMAux {α : Type u} {m : Type u → Type v} [Monad m] (f : Nat → α → m α) (n : Nat) : Nat → α → m α
@@ -38,5 +38,19 @@ foldMAux f n n a
 
 @[inline] def foldRevM {α : Type u} {m : Type u → Type v} [Monad m] (f : Nat → α → m α) (a : α) (n : Nat) : m α :=
 foldRevMAux f n a
+
+@[specialize] def allMAux {m} [Monad m] (p : Nat → m Bool) (n : Nat) : Nat → m Bool
+| 0   => pure true
+| i+1 => condM (p (n-i-1)) (allMAux i) (pure false)
+
+@[inline] def allM {m} [Monad m] (n : Nat) (p : Nat → m Bool) : m Bool :=
+allMAux p n n
+
+@[specialize] def anyMAux {m} [Monad m] (p : Nat → m Bool) (n : Nat) : Nat → m Bool
+| 0   => pure false
+| i+1 => condM (p (n-i-1)) (pure true) (anyMAux i)
+
+@[inline] def anyM {m} [Monad m] (n : Nat) (p : Nat → m Bool) : m Bool :=
+anyMAux p n n
 
 end Nat

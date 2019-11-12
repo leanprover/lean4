ad9466028ae391d94984ac02e17e163bf95ad5bd
Leonardo de Moura <leonardo@microsoft.com>
2019-11-12 12:25:24 -0800

    feat: add `isDefEqEta`

--- library/Init/Lean/Meta/ExprDefEq.lean
+++ library/Init/Lean/Meta/ExprDefEq.lean
@@ -4,11 +4,38 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 prelude
-import Init.Lean.Meta.Basic
+import Init.Lean.Meta.InferType
+import Init.Lean.Meta.LevelDefEq
 
 namespace Lean
 namespace Meta
 
+/--
+  Try to solve `a := (fun x => t) =?= b` by eta-expanding `b`.
+
+  Remark: eta-reduction is not a good alternative even in a system without universe cumulativity like Lean.
+  Example:
+    ```
+    (fun x : A => f ?m) =?= f
+    ```
+    The left-hand side of the constraint above it not eta-reduced because `?m` is a metavariable. -/
+@[specialize] private def isDefEqEta
+  (whnf    : Expr → MetaM Expr)
+  (isDefEq : Expr → Expr → MetaM Bool)
+  (a b : Expr) : MetaM Bool :=
+if a.isLambda && !b.isLambda then do
+  bType ← inferTypeAux whnf b;
+  bType ← usingDefault whnf bType;
+  match bType with
+  | Expr.forallE n bi d b =>
+    let b' := Expr.lam n bi d (Expr.app b (Expr.bvar 0));
+    try $ isDefEq a b'
+  | _ => pure false
+else
+  pure false
+
+
+#exit
 
 end Meta
 end Lean

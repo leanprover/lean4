b8d5193ecc8f0cceecd3e26e2fdcb3cf2db742a9
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-15 18:22:49 +0100

    chore: update Azure and Nix configs

--- shell.nix
+++ shell.nix
@@ -2,12 +2,12 @@
 
 let
   lean = import ./default.nix args;
-  temci = import (builtins.fetchGit { url = http://github.com/parttimenerd/temci.git; rev = "ba1505a7c2de471a5821a2643b34de2d1c1af03e"; }) { inherit pkgs; };
+  temci = import (builtins.fetchGit { url = http://github.com/parttimenerd/temci.git; rev = "4cae802606b871021394e250a280a2154b92f44b"; }) { inherit pkgs; };
 in pkgs.mkShell.override { stdenv = lean.stdenv; } rec {
   inputsFrom = [ lean ];
-  buildInputs = with pkgs; [ temci ccache ninja ];
+  buildInputs = with pkgs; [ temci ];
   # https://github.com/NixOS/nixpkgs/issues/60919
   hardeningDisable = [ "all" ];
-  # TODO: this should not be necessary when leanc starts statically linking binaries
-  LD_LIBRARY_PATH = "${pkgs.stdenv.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
+  # more convenient `ctest` output
+  CTEST_OUTPUT_ON_FAILURE = 1;
 }

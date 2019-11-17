353b511e9b1cfa34dda46e4d085758a3f19cfed5
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-17 20:53:39 +0100

    chore: CI: close ctest's stdin
    Apparently CMake reverted auto-closing stdin for ctest in a recent release because people relied on the old behavior...
    
    Perhaps we should add this to all test scripts instead, but this CI fix is easier

--- script/ci.sh
+++ script/ci.sh
@@ -4,6 +4,7 @@ mkdir build
 cd build
 eval cmake ../src $CMAKE_OPTIONS
 cmake --build .
-# -T to create .xml file
-ctest -j8 --output-on-failure --no-compress-output -T Test
+# -T to create .xml file for CI display
+# `<&-` to close stdin so that assertion dialog doesn't wait for input
+ctest -j8 --output-on-failure --no-compress-output -T Test <&-
 cpack

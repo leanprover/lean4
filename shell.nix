82a0991df6260370b2142abd324a3616b681f9df
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-19 12:41:24 +0100

    chore: switch to Github Actions

--- shell.nix
+++ shell.nix
@@ -2,10 +2,12 @@
 
 let
   lean = import ./default.nix args;
-  temci = import (builtins.fetchGit { url = http://github.com/parttimenerd/temci.git; rev = "4cae802606b871021394e250a280a2154b92f44b"; }) { inherit pkgs; };
+  temci = (import (builtins.fetchGit { url = http://github.com/parttimenerd/temci.git; rev = "4cae802606b871021394e250a280a2154b92f44b"; }) { inherit pkgs; }).override {
+    doCheck = false;
+  };
 in pkgs.mkShell.override { stdenv = lean.stdenv; } rec {
   inputsFrom = [ lean ];
-  buildInputs = with pkgs; [ temci ];
+  buildInputs = with pkgs; [ temci ccache ];
   # https://github.com/NixOS/nixpkgs/issues/60919
   hardeningDisable = [ "all" ];
   # more convenient `ctest` output

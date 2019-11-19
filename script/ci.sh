e8516ea2c04947849810a73befab4f1941165397
Sebastian Ullrich <sebasti@nullri.ch>
2019-11-19 12:41:24 +0100

    chore: switch to Github Actions

--- script/ci.sh
+++ script/ci.sh
@@ -4,7 +4,4 @@ mkdir build
 cd build
 eval cmake ../src $CMAKE_OPTIONS
 cmake --build .
-# -T to create .xml file for CI display
-# `<&-` to close stdin so that assertion dialog doesn't wait for input
-ctest -j8 --output-on-failure --no-compress-output -T Test <&-
 cpack

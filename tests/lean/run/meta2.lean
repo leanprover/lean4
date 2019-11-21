a28cf323b5febb9e93be5f780b33af9a66ca32d2
Leonardo de Moura <leonardo@microsoft.com>
2019-11-21 11:25:27 -0800

    test: test approxDefEq

--- tests/lean/run/meta2.lean
+++ tests/lean/run/meta2.lean
@@ -4,12 +4,14 @@ open Lean.Meta
 
 def dbgOpt : Options :=
 let opt : Options := {};
-let opt := opt.setBool `trace.Meta.debug true;
-let opt := opt.setBool `trace.Meta.isDefEq true;
+let opt := opt.setBool `trace.Meta true;
 opt
 
 def print (msg : MessageData) : MetaM Unit :=
 trace! `Meta.debug msg
+
+def check (x : MetaM Bool) : MetaM Unit :=
+unlessM x $ throw $ Exception.other "check failed"
 
 def run (mods : List Name) (x : MetaM Unit) (opts : Options := dbgOpt) : IO Unit :=
 do env ← importModules $ mods.map $ fun m => {module := m};
@@ -17,14 +19,16 @@ do env ← importModules $ mods.map $ fun m => {module := m};
    | EStateM.Result.ok _ s    => do
      s.traceState.traces.forM $ fun m => IO.println $ format m;
      pure ()
-   | EStateM.Result.error err _ => throw (IO.userError (toString err))
+   | EStateM.Result.error err s => do
+     s.traceState.traces.forM $ fun m => IO.println $ format m;
+     throw (IO.userError (toString err))
 
 def tst1 : MetaM Unit :=
 do print "----- tst1 -----";
    let nat := mkConst `Nat;
    mvar ← mkFreshExprMVar nat;
-   isExprDefEq mvar (mkNatLit 10);
-   isExprDefEq mvar (mkNatLit 10);
+   check $ isExprDefEq mvar (mkNatLit 10);
+   check $ isExprDefEq mvar (mkNatLit 10);
    pure ()
 
 #eval run [`Init.Data.Nat] tst1
@@ -34,8 +38,8 @@ do print "----- tst2 -----";
    let nat  := mkConst `Nat;
    let succ := mkConst `Nat.succ;
    mvar ← mkFreshExprMVar nat;
-   isExprDefEq (mkApp succ mvar) (mkApp succ (mkNatLit 10));
-   isExprDefEq mvar (mkNatLit 10);
+   check $ isExprDefEq (mkApp succ mvar) (mkApp succ (mkNatLit 10));
+   check $ isExprDefEq mvar (mkNatLit 10);
    pure ()
 
 #eval run [`Init.Data.Nat] tst2
@@ -48,7 +52,7 @@ do print "----- tst3 -----";
    mvar ← mkFreshExprMVar (mkForall `x BinderInfo.default nat nat);
    lambdaTelescope t $ fun xs _ => do {
      let x := xs.get! 0;
-     isExprDefEq (mkApp mvar x) (mkAppN add #[x, mkAppN add #[mkNatLit 10, x]]);
+     check $ isExprDefEq (mkApp mvar x) (mkAppN add #[x, mkAppN add #[mkNatLit 10, x]]);
      pure ()
    };
    some v ← getExprMVarAssignment mvar.mvarId! | pure ();
@@ -56,3 +60,22 @@ do print "----- tst3 -----";
    pure ()
 
 #eval run [`Init.Data.Nat] tst3
+
+def tst4 : MetaM Unit :=
+do print "----- tst4 -----";
+   let nat := mkConst `Nat;
+   let add := mkConst `Nat.add;
+   let t   := mkLambda `x BinderInfo.default nat $ mkBVar 0;
+   lambdaTelescope t $ fun xs _ => do {
+     let x := xs.get! 0;
+     mvar ← mkFreshExprMVar (mkForall `x BinderInfo.default nat nat);
+     -- the following `isExprDefEq` fails because `x` is also in the context of `mvar`
+     check $ not <$> isExprDefEq (mkApp mvar x) (mkAppN add #[x, mkAppN add #[mkNatLit 10, x]]);
+     check $ approxDefEq $ isExprDefEq (mkApp mvar x) (mkAppN add #[x, mkAppN add #[mkNatLit 10, x]]);
+     some v ← getExprMVarAssignment mvar.mvarId! | pure ();
+     print v;
+     pure ()
+   };
+   pure ()
+
+#eval run [`Init.Data.Nat] tst4

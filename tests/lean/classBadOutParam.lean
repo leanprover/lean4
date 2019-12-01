ef43a08b91b08b017a2174cb3c0509162412f5de
Leonardo de Moura <leonardo@microsoft.com>
2019-12-01 08:23:28 -0800

    feat: add `checkOutParam` validation
    It implements the approach we have discussed at
    `src/library/type_context.cpp`.

---
+++ tests/lean/classBadOutParam.lean
@@ -0,0 +1,5 @@
+class C1 (x : outParam Nat) (y : { n : Nat // n > x }) (α : Type) := -- should fail
+(val : α)
+
+class C2 (x : outParam Nat) (y : outParam { n : Nat // n > x }) (α : Type) := -- should work
+(val : α)

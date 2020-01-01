d22abd85dd6cfefe3a6125f59c7e5af6ddbfe233
Leonardo de Moura <leonardo@microsoft.com>
2020-01-01 11:02:38 -0800

    test: minimal repro for evalConst crash

--- tests/lean/run/evalconst.lean
+++ tests/lean/run/evalconst.lean
@@ -9,3 +9,13 @@ IO.println $ env.evalConst Nat `x;
 pure ()
 
 #eval tst
+
+def f (x : Nat) := x + 1
+
+unsafe def tst2 : MetaIO Unit := do
+env ← MetaIO.getEnv;
+f ← liftM $ IO.ofExcept $ env.evalConst (Nat → Nat) `f;
+IO.println $ (f 10);
+pure ()
+
+#eval tst2

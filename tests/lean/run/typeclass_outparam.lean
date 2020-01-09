c49e43f69fc8d3bc9cef4abf34fda17e1a0b5651
Leonardo de Moura <leonardo@microsoft.com>
2020-01-09 11:47:48 -0800

    chore: reactivate typeclass test

--- tests/lean/run/typeclass_outparam.lean
+++ tests/lean/run/typeclass_outparam.lean
@@ -1,7 +1,9 @@
-#exit
-
 class OPClass (α : outParam Type) (β : Type) : Type := (u : Unit := ())
 
 instance op₁ : OPClass Nat Nat := {}
+
+new_frontend
+
+set_option pp.all true
 
 #synth OPClass Nat Nat

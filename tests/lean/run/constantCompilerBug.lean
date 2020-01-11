95ed5bd4680755924cb089f3bc7f292d50fd12b6
Leonardo de Moura <leonardo@microsoft.com>
2020-01-10 21:26:09 -0800

    chore: fix test

--- tests/lean/run/constantCompilerBug.lean
+++ tests/lean/run/constantCompilerBug.lean
@@ -6,7 +6,7 @@ open Lean
 open Lean.Parser
 
 def regBlaParserAttribute : IO Unit :=
-registerParserAttribute (mkNameSimple "blaParser") (mkNameSimple "bla")
+registerBuiltinDynamicParserAttribute (mkNameSimple "blaParser") (mkNameSimple "bla")
 
 @[inline] def parser {k : ParserKind} : Parser k :=
 categoryParser (mkNameSimple "bla") 0

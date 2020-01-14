b057f55e5286e5f49ed229f220d0893d722a83d1
Leonardo de Moura <leonardo@microsoft.com>
2020-01-14 14:01:40 -0800

    chore: add missing rules

--- src/Init/Lean/Parser/Syntax.lean
+++ src/Init/Lean/Parser/Syntax.lean
@@ -26,14 +26,16 @@ namespace Syntax
 @[builtinSyntaxParser] def atom      := parser! strLit
 @[builtinSyntaxParser] def num       := parser! nonReservedSymbol "num"
 @[builtinSyntaxParser] def str       := parser! nonReservedSymbol "str"
+@[builtinSyntaxParser] def char      := parser! nonReservedSymbol "char"
+@[builtinSyntaxParser] def ident     := parser! nonReservedSymbol "ident"
 @[builtinSyntaxParser] def try       := parser! nonReservedSymbol "try " >> syntaxParser
 @[builtinSyntaxParser] def lookahead := parser! nonReservedSymbol "lookahead " >> syntaxParser
+@[builtinSyntaxParser] def optional  := parser! nonReservedSymbol "optional " >> syntaxParser
 @[builtinSyntaxParser] def sepBy     := parser! nonReservedSymbol "sepBy " >> syntaxParser >> syntaxParser
 @[builtinSyntaxParser] def sepBy1    := parser! nonReservedSymbol "sepBy1 " >> syntaxParser >> syntaxParser
 
 @[builtinSyntaxParser] def many     := tparser! pushLeading >> symbolAux "*" none
 @[builtinSyntaxParser] def many1    := tparser! pushLeading >> symbolAux "+" none
-@[builtinSyntaxParser] def optional := tparser! pushLeading >> symbolAux "?" none
 @[builtinSyntaxParser] def orelse   := tparser! infixR " <|> " 2
 
 end Syntax

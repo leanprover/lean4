2fbcf4d681228bf2bb048d68869a183f5fc68dce
Leonardo de Moura <leonardo@microsoft.com>
2020-01-15 18:14:06 -0800

    chore: minor

--- src/Init/Lean/Parser/Syntax.lean
+++ src/Init/Lean/Parser/Syntax.lean
@@ -19,7 +19,7 @@ categoryParser `syntax rbp
 namespace Syntax
 def maxPrec := parser! nonReservedSymbol "max" true
 def precedenceLit : Parser := numLit <|> maxPrec
-def «precedence» := parser! " : " >> precedenceLit
+def «precedence» := parser! ":" >> precedenceLit
 @[builtinSyntaxParser] def paren     := parser! "(" >> many1 syntaxParser >> ")"
 @[builtinSyntaxParser] def cat       := parser! ident >> optional (try «precedence»)
 @[builtinSyntaxParser] def atom      := parser! strLit >> optional (try «precedence»)

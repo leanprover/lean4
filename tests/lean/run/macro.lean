2b63aa27d3a7fbe013cc4a170ae222e69525ccfd
Leonardo de Moura <leonardo@microsoft.com>
2020-01-21 09:14:55 -0800

    chore: use new_frontend in the whole test

--- tests/lean/run/macro.lean
+++ tests/lean/run/macro.lean
@@ -1,10 +1,10 @@
+new_frontend
+
 abbrev Set (α : Type) := α → Prop
 axiom setOf {α : Type} : (α → Prop) → Set α
 axiom mem {α : Type} : α → Set α → Prop
 axiom univ {α : Type} : Set α
 axiom Union {α : Type} : Set (Set α) → Set α
-
-new_frontend
 
 syntax term " ∈ ":100 term:99 : term
 

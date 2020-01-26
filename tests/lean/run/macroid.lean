43df63e594a30781a1fad49ca093a592c5aa069d
Leonardo de Moura <leonardo@microsoft.com>
2020-01-25 20:24:11 -0800

    chore: remove unnecessary `|`s from test

--- tests/lean/run/macroid.lean
+++ tests/lean/run/macroid.lean
@@ -1,26 +1,22 @@
 new_frontend
 
 syntax "[" ident "↦" term "]" : term
-macro_rules
-| `([$x ↦ $v]) => `(fun $x => $v)
+macro_rules `([$x ↦ $v]) => `(fun $x => $v)
 
 #check [x ↦ x + 1]
 
 syntax "case!" ident ":" term "with" term "," term : term
-macro_rules
-| `(case! $h : $c with $t, $e) => `((fun $h => cond $h $t $e) $c)
+macro_rules `(case! $h : $c with $t, $e) => `((fun $h => cond $h $t $e) $c)
 
 #check case! h : 0 == 0 with h, not h
 
 syntax "case2!" ident ":" term "with" term "," term : term
-macro_rules
-| `(case2! $h : $c with $t, $e) => `(let $h := $c; cond $h $t $e)
+macro_rules `(case2! $h : $c with $t, $e) => `(let $h := $c; cond $h $t $e)
 
 #check case2! h : 0 == 0 with h, not h
 
 syntax "test" term : term
-macro_rules
-| `(test $x:id) => `(let $x := 0; $x)
+macro_rules `(test $x:id) => `(let $x := 0; $x)
 
 #check fun (x : Nat) => test x
 

b5eb64da3aa9c6954a8fcee4e104c7e72c07dd75
Leonardo de Moura <leonardo@microsoft.com>
2020-02-04 11:24:33 -0800

    fix: add `mkFilePath` and try to fix Windows build

--- tests/lean/run/parseCore.lean
+++ tests/lean/run/parseCore.lean
@@ -2,7 +2,7 @@ import Init.Lean.Parser
 
 def test : IO Unit := do
 env ← Lean.mkEmptyEnvironment;
-Lean.Parser.parseFile env ("../../../src/Init/Core.lean");
+Lean.Parser.parseFile env (System.mkFilePath ["..", "..", "..", "src", "Init", "Core.lean"]);
 IO.println "done"
 
 #eval test

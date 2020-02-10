d37eb896ef6c1fe302ac34f55896edba39c6c9f4
Leonardo de Moura <leonardo@microsoft.com>
2020-02-10 14:40:48 -0800

    doc: performance issue
    cc @dselsam

--- tests/elabissues/equation_compiler_slow_with_many_constructors2.lean
+++ tests/elabissues/equation_compiler_slow_with_many_constructors2.lean
@@ -86,6 +86,47 @@ inductive Proof : Type
 
 namespace Proof
 
+/-
+[Leo] elaboration time is now at 50ms on my machine.
+The problem was not the equation compiler, but time wasted trying
+to synthesize `[MonadFail Except]`.
+
+We used to have the instances
+```
+instance monadFailLift (m n : Type u → Type v) [HasMonadLift m n] [MonadFail m] [Monad n] : MonadFail n :=
+{ fail := fun α s => monadLift (MonadFail.fail s : m α) }
+instance I1 (m : Type → Type) [Monad m] : MonadFail (ExceptT String m)
+```
+The instance `monadFailLift` triggers the performance problem when trying to solve `[MonadFail (Except String)]`.
+It produces the subgoals
+```
+[MonadFail ?m]
+[HasMonadLift ?m (Except String)]
+```
+Then we try to solve `[MonadFail ?m]` using `I1` we can produces an infinite number of solutions.
+The first solution is `?m := ExceptT String (EIO ?ε)` where `EIO ?ε` is the first Monad Lean managed to synthesize when solving I1.
+Then it uses the instance
+```lean
+instance (m : Type → Type) [Monad m] : Monad (OptionT m) :=
+```
+to generate the solutions
+```
+?m := ExceptT String (OptionT (EIO ?ε))
+?m := ExceptT String (OptionT $ OptionT (EIO ?ε))
+?m := ExceptT String (OptionT $ OptionT $ OptionT (EIO ?ε))
+...
+```
+Note that the new type class resolution procedure would not solve this problem. If we wanted to keep this feature, the solution would be to write
+```
+instance monadFailLift (m n : Type u → Type v) [HasMonadLift m n] [MonadFail m] [Monad n] : MonadFail n :=
+```
+as
+```
+instance monadFailLift (m n : Type u → Type v) [MonadFail m] [HasMonadLift m n] [Monad n] : MonadFail n :=
+```
+The subgoal `[HasMonadLift ?m (Except String)]` would fail instantaneously.
+-/
+
 def infer : Proof → Except String (Term × Term)
 | addZero x => pure (x+0, x)
 | zeroAdd y => pure (0 + y, y)

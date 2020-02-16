8e4f3beff334f0574a25c4bab7ee1eeedb52aa64
Leonardo de Moura <leonardo@microsoft.com>
2020-02-15 16:15:37 -0800

    chore: fix test

--- tests/lean/run/ptrAddr.lean
+++ tests/lean/run/ptrAddr.lean
@@ -5,5 +5,5 @@ def y := x
 @[noinline] def mk (v : Nat) := (v, v+1)
 
 #eval withPtrAddr x (fun a => dbgTrace (">> " ++ toString a) $ fun _ => 0) TrustMe
-#eval withPtrEq (fun a b => dbgTrace (">> " ++ toString a ++ " == " ++ toString b) $ fun _ => a == b) TrustMe x y -- should not print message
-#eval withPtrEq (fun a b => dbgTrace (">> " ++ toString a ++ " == " ++ toString b) $ fun _ => a == b) TrustMe x (mk 1) -- should print message
+#eval withPtrEq x y (fun _ => dbgTrace (">> " ++ toString x ++ " == " ++ toString y) $ fun _ => x == y) TrustMe -- should not print message
+#eval withPtrEq x (mk 1) (fun _ => dbgTrace (">> " ++ toString x ++ " == " ++ toString y) $ fun _ => x == y) TrustMe -- should print message

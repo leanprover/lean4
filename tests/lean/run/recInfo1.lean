731a0db0443f246001daee5b0e70ac3011904434
Leonardo de Moura <leonardo@microsoft.com>
2020-02-19 19:08:03 -0800

    test: add `mkRecursorInfo` tests

---
+++ tests/lean/run/recInfo1.lean
@@ -0,0 +1,21 @@
+import Init.Lean.Meta
+
+open Lean
+open Lean.Meta
+
+def print (msg : MessageData) : MetaM Unit :=
+trace! `Meta.debug msg
+
+def showRecInfo (declName : Name) (majorPos? : Option Nat := none) : MetaM Unit := do
+info ← mkRecursorInfo declName majorPos?;
+print (toString info)
+
+set_option trace.Meta true
+set_option trace.Meta.isDefEq false
+
+#eval showRecInfo `Acc.recOn
+#eval showRecInfo `Prod.casesOn
+#eval showRecInfo `List.recOn
+#eval showRecInfo `List.casesOn
+#eval showRecInfo `List.brecOn
+#eval showRecInfo `Iff.elim (some 4)

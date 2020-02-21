15e60e0e05c0691da1d4739f2100f99996d081c1
Leonardo de Moura <leonardo@microsoft.com>
2020-02-21 12:06:49 -0800

    feat: add induction tactic entry point

--- src/Init/Lean/Elab/Tactic/Induction.lean
+++ src/Init/Lean/Elab/Tactic/Induction.lean
@@ -4,7 +4,16 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Sebastian Ullrich
 -/
 prelude
-import Init.Lean.Elab.Term
 import Init.Lean.Elab.Tactic.Basic
-import Init.Lean.Elab.Tactic.ElabTerm
-import Init.Lean.Elab.Tactic.Induction
+
+namespace Lean
+namespace Elab
+namespace Tactic
+
+@[builtinTactic «induction»] def evalInduction : Tactic :=
+fun stx => focus stx $
+  throwError stx ("WIP " ++ stx)
+
+end Tactic
+end Elab
+end Lean

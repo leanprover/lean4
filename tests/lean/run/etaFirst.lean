5bdc54d34473d9fc10dea2cf28a40f6fb584aeb8
Leonardo de Moura <leonardo@microsoft.com>
2020-03-05 08:35:34 -0800

    test: unifier

---
+++ tests/lean/run/etaFirst.lean
@@ -0,0 +1,35 @@
+-- The following fails in the old elaborator
+-- theorem tst1 : (fun a b => Nat.add a b) = Nat.add :=
+-- Eq.refl (fun a b => Nat.add a b)
+
+new_frontend
+
+theorem tst2 : (fun a b => Nat.add a b) = Nat.add :=
+Eq.refl (fun a b => Nat.add a b)
+
+theorem tst3 : (fun a b => Nat.add a b) = Nat.add :=
+rfl
+
+theorem tst4 : Nat.add = (fun a b => Nat.add a b) :=
+rfl
+
+theorem tst5 : Nat.add = (fun (a b : Nat) => a + b) :=
+rfl
+
+theorem tst6 : Nat.add = (· + ·) :=
+rfl
+
+theorem tst7 : (· + ·) = Nat.add :=
+rfl
+
+theorem tst8 : (· + ·) = @HasAdd.add Nat _ :=
+rfl
+
+theorem tst9 : (Nat.add · ·) = @HasAdd.add Nat _ :=
+rfl
+
+axiom p    : (Nat → Nat → Nat) → Prop
+axiom pAdd : p Nat.add
+
+theorem tst10 : p (fun a b => Nat.add a b) :=
+pAdd

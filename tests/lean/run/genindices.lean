dfa392fa17f4a17ffb0ceef0d9fa4f72178ef29f
Leonardo de Moura <leonardo@microsoft.com>
2020-03-04 16:27:01 -0800

    feat: add `generalizeIndices`
    Helper tactic for `cases`

---
+++ tests/lean/run/genindices.lean
@@ -0,0 +1,27 @@
+import Init.Lean
+
+universe u
+
+inductive Pred : ∀ (α : Type u), List α → Type (u+1)
+| foo {α : Type u} (l1 : List α) (l2 : List (Option α)) : Pred (Option α) l2 → Pred α l1
+
+axiom goal : forall (α : Type u) (xs : List (List α)) (h : Pred (List α) xs), xs ≠ [] → xs = xs
+
+open Lean
+open Lean.Meta
+
+def tst1 : MetaM Unit := do
+cinfo ← getConstInfo `goal;
+let type := cinfo.type;
+mvar   ← mkFreshExprMVar type;
+trace! `Elab (MessageData.ofGoal mvar.mvarId!);
+(_, mvarId) ← introN mvar.mvarId! 2;
+(fvarId, mvarId) ← intro1 mvarId;
+trace! `Elab (MessageData.ofGoal mvarId);
+s ← generalizeIndices mvarId fvarId;
+trace! `Elab (MessageData.ofGoal s.mvarId);
+pure ()
+
+set_option trace.Elab true
+
+#eval tst1

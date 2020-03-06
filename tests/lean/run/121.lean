88a62f9f7e832b718d6c71917fbc0ca1a188b04b
Leonardo de Moura <leonardo@microsoft.com>
2020-03-06 12:12:23 -0800

    fix: #121 in the old frontend
    This is temporary hack. We will delete this module in the near future.

--- tests/lean/run/121.lean
+++ tests/lean/run/121.lean
@@ -2,7 +2,7 @@ abbrev DelabM := Id
 abbrev Delab := DelabM Nat
 
 example : DelabM Nat := pure 1  -- works
--- example : Delab := pure 1    -- fails in old frontend
+example : Delab      := pure 1  -- works
 
 new_frontend
 

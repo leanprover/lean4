5770edbffd4a64b976a2cb926591e233caf6543e
Leonardo de Moura <leonardo@microsoft.com>
2020-03-06 16:37:27 -0800

    feat: workaround for the constant approximation issue
    @Kha It seems to cover all the scenarios we discussed earlier today.

---
+++ tests/lean/run/unif_issue2.lean
@@ -0,0 +1,13 @@
+import Init.Lean
+
+new_frontend
+
+open Lean
+
+#check @StateT.run Nat Id Nat (let f := fun (x : Unit) => pure 0; f ()) 0
+
+#check Id.run $ StateT.run (let x := fun _ => pure 0; x ()) 0
+
+#check @StateT.run Nat Id (List Nat) (let x := fun _ => pure [0]; x ()) 0
+
+#check @Unhygienic.run Syntax $ let x := fun _ => pure Syntax.missing; x ()

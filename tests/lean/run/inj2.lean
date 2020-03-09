629f14384ab4364549f128577ad7625bc7fb4ed6
Leonardo de Moura <leonardo@microsoft.com>
2020-03-09 15:12:07 -0700

    test: add dependent elimination tests

--- tests/lean/run/inj2.lean
+++ tests/lean/run/inj2.lean
@@ -4,6 +4,10 @@ inductive Vec2 (α : Type u) (β : Type v) : Nat → Type (max u v)
 | nil  : Vec2 0
 | cons : α → β → forall {n}, Vec2 n → Vec2 (n+1)
 
+inductive Fin2 : Nat → Type
+| zero (n : Nat)              : Fin2 (n+1)
+| succ {n : Nat} (s : Fin2 n) : Fin2 (n+1)
+
 new_frontend
 
 theorem test1 {α β} {n} (a₁ a₂ : α) (b₁ b₂ : β) (v w : Vec2 α β n) (h : Vec2.cons a₁ b₁ v = Vec2.cons a₂ b₂ w) : a₁ = a₂ :=
@@ -22,4 +26,15 @@ theorem test3 {α β} {n} (a₁ a₂ : α) (b₁ b₂ : β) (v w : Vec2 α β n)
 begin
   injection h with _ _ _ h4;
   exact h4
+end
+
+theorem test4 {α} (v : Fin2 0) : α :=
+begin
+  cases v
+end
+
+def test5 {α β} {n} (v : Vec2 α β (n+1)) : α :=
+begin
+  cases v with
+  | cons h1 h2 n tail => h1
 end

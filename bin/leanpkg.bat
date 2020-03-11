10f161353e361f20ac18a7383f46466b149fdfa5
Sebastian Ullrich <sebasti@nullri.ch>
2020-03-11 14:53:24 +0100

    chore: remove leanpkg script

--- bin/leanpkg.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-SET LEANDIR=%~dp0%../
-SET LIBDIR=%LEANDIR%\lib\lean
-IF NOT EXIST %LIBDIR% SET LIBDIR=%LEANDIR%
-SET LEAN_PATH=%LIBDIR%\library;%LIBDIR%\leanpkg
-SET PATH=%LEANDIR%\bin;%PATH%
-
-lean --run %LIBDIR%\leanpkg\leanpkg\main.lean %*

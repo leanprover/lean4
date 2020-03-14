2e6fac285362922b13c58b691a46c2883170485d
Leonardo de Moura <leonardo@microsoft.com>
2020-03-14 11:46:56 -0700

    feat: add support for `reduceBool` and `reduceNat` in the kernel `type_checker`

--- tests/lean/run/kernel1.lean
+++ tests/lean/run/kernel1.lean
@@ -16,3 +16,22 @@ def a3 := 20
 
 #eval checkDefEq `a1 `a2
 #eval checkDefEq `a1 `a3
+
+def v1 := 100000000000 + 100000000000
+def v2 := 200000000000
+def v3 := 200000000001
+def v4 : Bool := 20000000000 > 200000000001
+def v5 := 100000000000 - 100000000000
+
+def c1 := reduceNat v1
+def c2 := reduceNat v2
+def c3 := reduceNat v3
+def c4 := reduceBool v4
+def c5 := reduceNat v5
+
+#eval checkDefEq `c1 `c2
+#eval checkDefEq `c1 `c3
+#eval checkDefEq `c5 `Nat.zero
+#eval checkDefEq `Nat.zero `c5
+
+-- #eval checkDefEq `c4 `Bool.true -- It crashes due to a bug at run_boxed

3c71c4f0ea276e571fd880cea195b3d1a0f52c7c
Sebastian Ullrich <sebasti@nullri.ch>
2020-03-24 13:49:15 +0100

    chore: fix benchmark

--- tests/bench/parser.lean
+++ tests/bench/parser.lean
@@ -3,7 +3,7 @@ import Init.Lean.Parser
 def main : List String → IO Unit
 | [fname, n] => do
   env ← Lean.mkEmptyEnvironment;
-  n.toNat.forM $ fun _ =>
+  n.toNat!.forM $ fun _ =>
     Lean.Parser.parseFile env fname *> pure ();
   pure ()
 | _    => throw $ IO.userError "give file"

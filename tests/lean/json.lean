5749981276c6278411256abf35ea73f4662212ec
Marc Huisinga <mhuisi@protonmail.com>
2020-03-27 18:01:18 +0100

    feat: translate and refactor Gabriel Ebner's JSON parser
    implement the JSON parser from https://github.com/gebner/lean4/tree/server by @gebner using new Lean 4 syntax and fix some minor bugs.

---
+++ tests/lean/json.lean
@@ -0,0 +1,25 @@
+import Init.Lean.Data.Json.Parser
+import Init.Lean.Data.Json.Printer
+
+def test (s : String) : String :=
+match Lean.Json.parse s with
+| Except.ok res    => toString res
+| Except.error err => err
+
+#eval test "null"
+#eval test "false"
+#eval test "true"
+#eval test "123.456e-7"
+#eval test "-0.01e8"
+#eval test "\"\""
+#eval test "\"abc\""
+#eval test "[true, 123, \"foo\", []]"
+#eval test "{\"a\": 1.2, \"b\": \"foo\", \"c\": null, \"d\": {\"foo\": \"bar\"}, \"e\": [{}]}"
+#eval test "[false_]"
+#eval test "["
+#eval test "]"
+#eval test "{"
+#eval test "\""
+#eval test "1."
+#eval test "{foo: 1}"
+#eval test "   "

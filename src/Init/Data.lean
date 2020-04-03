b5f258773b9d5a28c17164e8bd6e689fab93d400
Leonardo de Moura <leonardo@microsoft.com>
2020-04-03 16:49:32 -0700

    feat: add `Float`

--- src/Init/Data.lean
+++ src/Init/Data.lean
@@ -14,6 +14,7 @@ import Init.Data.Array
 import Init.Data.ByteArray
 import Init.Data.Fin
 import Init.Data.UInt
+import Init.Data.Float
 import Init.Data.RBTree
 import Init.Data.RBMap
 import Init.Data.Option

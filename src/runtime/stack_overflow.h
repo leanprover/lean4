7899ab3761f4061f0699e6922102f4abf401f3be
Sebastian Ullrich <sebasti@nullri.ch>
2020-04-30 15:22:43 +0200

    feat: report stack overflows as such on Linux
    Port of the corresponding Rust code (Apache/MIT)

---
+++ src/runtime/stack_overflow.h
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2020 Sebastian Ullrich. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Sebastian Ullrich
+*/
+#pragma once
+
+namespace lean {
+void initialize_stack_overflow();
+void finalize_stack_overflow();
+}

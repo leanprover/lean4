78977697321b2904b6a7f52de4f2de5221a2c406
Leonardo de Moura <leonardo@microsoft.com>
2020-05-22 11:10:47 -0700

    chore: move `BinomialHeap` to `Std`

--- src/Std/Data/BinomialHeap.lean
+++ src/Std/Data/BinomialHeap.lean
@@ -3,5 +3,4 @@ Copyright (c) 2019 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
-prelude
-import Init.Data.BinomialHeap.Basic
+import Std.Data.BinomialHeap.Basic
--- src/Std/Data/BinomialHeap/Basic.lean
+++ src/Std/Data/BinomialHeap/Basic.lean
@@ -3,10 +3,10 @@ Copyright (c) 2019 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
-prelude
 import Init.Data.List
-universes u
 
+namespace Std
+universes u
 namespace BinomialHeapImp
 
 structure HeapNodeAux (α : Type u) (h : Type u) :=
@@ -146,3 +146,4 @@ merge (singleton a) h
 | ⟨b, _⟩ => BinomialHeapImp.toList lt b
 
 end BinomialHeap
+end Std

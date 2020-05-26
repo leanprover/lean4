f7818757efb85b03a271cf10378d69f89700243a
Leonardo de Moura <leonardo@microsoft.com>
2020-05-26 13:50:04 -0700

    chore: add `Lean` package to `CMakeLists.txt`

--- src/Lean.lean
+++ src/Lean.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
@@ -24,3 +24,4 @@ Authors: Leonardo de Moura
 -- import Lean.Structure
 -- import Lean.Delaborator
 -- import Lean.PrettyPrinter
+import Init.Lean

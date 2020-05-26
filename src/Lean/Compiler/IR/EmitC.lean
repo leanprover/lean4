862795e9e9e303d15a3aaaf95a2dd78e2f19bf2d
Leonardo de Moura <leonardo@microsoft.com>
2020-05-26 14:57:29 -0700

    fix: `usesLeanAPI`

--- src/Lean/Compiler/IR/EmitC.lean
+++ src/Lean/Compiler/IR/EmitC.lean
@@ -142,7 +142,7 @@ match d with
 | Decl.fdecl f xs t b => do
   unless (xs.size == 2 || xs.size == 1) (throw "invalid main function, incorrect arity when generating code");
   env ← getEnv;
-  let usesLeanAPI := usesModuleFrom env `Init.Lean;
+  let usesLeanAPI := usesModuleFrom env `Lean;
   if usesLeanAPI then
      emitLn "void lean_initialize();"
   else

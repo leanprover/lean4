c38f4fe837bbde265584497f0fcdb827f2296675
Sebastian Ullrich <sebasti@nullri.ch>
2020-07-08 16:54:11 +0200

    feat: unsafe functions for freeing compacted regions

--- src/library/module.cpp
+++ src/library/module.cpp
@@ -86,16 +86,12 @@ extern "C" object * lean_read_module_data(object * fname, object *) {
             return set_io_error((sstream() << "failed to read file '" << olean_fn << "'").str());
         }
         in.close();
-        /* We don't free compacted_region objects */
         compacted_region * region = new compacted_region(size - header_size, buffer);
-#if defined(__has_feature)
-#if __has_feature(address_sanitizer)
-        // do not report as leak
-        __lsan_ignore_object(region);
-#endif
-#endif
         object * mod = region->read();
-        return set_io_result(mod);
+        object * mod_region = alloc_cnstr(0, 2, 0);
+        cnstr_set(mod_region, 0, mod);
+        cnstr_set(mod_region, 1, box_size_t(reinterpret_cast<size_t>(region)));
+        return set_io_result(mod_region);
     } catch (exception & ex) {
         return set_io_error((sstream() << "failed to read '" << olean_fn << "': " << ex.what()).str());
     }

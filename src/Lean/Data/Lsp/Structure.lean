e137fa780f0393259c9ebdcdcc4db183e4f3bcd1
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2020-07-15 11:28:49 +0200

    feat: WIP snapshots
    Allow interactive editing by only recompiling parts of the file below the edit.

--- src/Lean/Data/Lsp/Structure.lean
+++ src/Lean/Data/Lsp/Structure.lean
@@ -5,10 +5,13 @@ namespace Lean.Lsp
 open Lean
 open Lean.Json
 
--- all Ints in this file are Floats in LSP
+-- all Ints/Nats in this file are Floats in LSP
 
 -- as in http://tools.ietf.org/html/rfc3986
 abbrev DocumentUri := String
+
+-- LSP indexes text with rows and colums
+abbrev DocumentText := Array String
 
 -- character is accepted liberally: actual character := min(line length, character)
 structure Position := (line : Nat) (character : Nat)
@@ -18,6 +21,15 @@ instance positionHasFromJson : HasFromJson Position :=
   line ← j.getObjValAs? Nat "line";
   character ← j.getObjValAs? Nat "character";
   pure ⟨line, character⟩⟩
+
+namespace Position
+
+/-- Computes a linear position from an LSP-style 0-indexed (ln, col) position
+and the text. -/
+def lnColToLinearPos (pos : Position) (text : DocumentText) : String.Pos :=
+text.foldrRange 0 pos.line (fun ln acc => acc + ln.length + 1) pos.character
+
+end Position
 
 -- [start, end)
 structure Range := (start : Position) («end» : Position)
@@ -35,70 +47,6 @@ structure LocationLink :=
 -- must be a subrange of targetRange
 (targetSelectionRange : Range)
 
-inductive DiagnosticSeverity
-| error | warning | information | hint
-
-instance diagnosticSeverityHasFromJson : HasFromJson DiagnosticSeverity :=
-⟨fun j => match j.getNat? with
-  | some 1 => DiagnosticSeverity.error
-  | some 2 => DiagnosticSeverity.warning
-  | some 3 => DiagnosticSeverity.information
-  | some 4 => DiagnosticSeverity.hint
-  | _      => none⟩
-
-instance diagnosticSeverityHasToJson : HasToJson DiagnosticSeverity :=
-⟨fun o => match o with
-  | DiagnosticSeverity.error       => (1 : Nat)
-  | DiagnosticSeverity.warning     => (2 : Nat)
-  | DiagnosticSeverity.information => (3 : Nat)
-  | DiagnosticSeverity.hint        => (4 : Nat)⟩
-
-inductive DiagnosticCode
-| int (i : Int)
-| string (s : String)
-
-instance diagnosticCodeHasToJson : HasToJson DiagnosticCode :=
-⟨fun o => match o with
-  | DiagnosticCode.int i    => i
-  | DiagnosticCode.string s => s⟩
-
-inductive DiagnosticTag
-/- Unused or unnecessary code.
-
-Clients are allowed to render diagnostics with this tag faded out instead of having
-an error squiggle. -/
-| unnecessary
-/- Deprecated or obsolete code.
-
-Clients are allowed to rendered diagnostics with this tag strike through. -/
-| deprecated
-
-/- Represents a related message and source code location for a diagnostic. This should be
-used to point to code locations that cause or are related to a diagnostics, e.g when duplicating
-a symbol in a scope. -/
-structure DiagnosticRelatedInformation :=
-(location : Location)
-(message : String)
-
-structure Diagnostic :=
-/- The range at which the message applies. -/
-(range : Range)
-/- The diagnostic's severity. Can be omitted. If omitted it is up to the
-client to interpret diagnostics as error, warning, info or hint. -/
-(severity? : Option DiagnosticSeverity := none)
-/- The diagnostic's code, which might appear in the user interface. -/
-(code? : Option DiagnosticCode := none)
-/- A human-readable string describing the source of this
-diagnostic, e.g. 'typescript' or 'super lint'. -/
-(source? : Option String := none)
-/- The diagnostic's message. -/
-(message : String)
-/- Additional metadata about the diagnostic.
-@since 3.15.0 -/
-(tags? : Option (Array DiagnosticTag) := none)
-/- An array of related diagnostic information, e.g. when symbol-names within
-a scope collide all definitions can be marked via this property. -/
-(relatedInformation? : Option (Array DiagnosticRelatedInformation) := none)
 
 structure Command :=
 (title : String)
@@ -133,7 +81,7 @@ structure VersionedTextDocumentIdentifier :=
 -- increases after each change, undo and redo
 -- none used when a document is not open and the
 -- disk content is the master
-(version? : Option Int := none)
+(version? : Option Nat := none)
 
 structure TextDocumentEdit :=
 (textDocument : VersionedTextDocumentIdentifier)
@@ -150,7 +98,7 @@ structure TextDocumentItem :=
 -- when handling more than language
 (languageId : String)
 -- increases after each change, undo and redo
-(version : Int)
+(version : Nat)
 (text : String)
 
 -- parameter literal for requests
@@ -205,46 +153,17 @@ instance locationHasFromJson : HasFromJson Location :=
   range ← j.getObjValAs? Range "range";
   pure ⟨uri, range⟩⟩
 
-instance diagnosticCodeHasFromJson : HasFromJson DiagnosticCode :=
-⟨fun j => match j with
-  | num (i : Int) => DiagnosticCode.int i
-  | str s         => DiagnosticCode.string s
-  | _             => none⟩
-
-instance diagnosticTagFromJson : HasFromJson DiagnosticTag :=
-⟨fun j => match j.getNat? with
-  | some 1 => DiagnosticTag.unnecessary
-  | some 2 => DiagnosticTag.deprecated
-  | _      => none⟩
-
-instance diagnosticRelatedInformationHasFromJson : HasFromJson DiagnosticRelatedInformation :=
-⟨fun j => do
-  location ← j.getObjValAs? Location "location";
-  message ← j.getObjValAs? String "message";
-  pure ⟨location, message⟩⟩
-
-instance diagnosticHasFromJson : HasFromJson Diagnostic :=
-⟨fun j => do
-  range ← j.getObjValAs? Range "range";
-  let severity? := j.getObjValAs? DiagnosticSeverity "severity";
-  let code? := j.getObjValAs? DiagnosticCode "code";
-  let source? := j.getObjValAs? String "source";
-  message ← j.getObjValAs? String "message";
-  let tags? := j.getObjValAs? (Array DiagnosticTag) "tags";
-  let relatedInformation? := j.getObjValAs? (Array DiagnosticRelatedInformation) "relatedInformation";
-  pure ⟨range, severity?, code?, source?, message, tags?, relatedInformation?⟩⟩
-
 instance versionedTextDocumentIdentifierHasFromJson : HasFromJson VersionedTextDocumentIdentifier :=
 ⟨fun j => do
   uri ← j.getObjValAs? DocumentUri "uri";
-  let version? := j.getObjValAs? Int "version";
+  let version? := j.getObjValAs? Nat "version";
   pure ⟨uri, version?⟩⟩
 
 instance textDocumentItemHasFromJson : HasFromJson TextDocumentItem :=
 ⟨fun j => do
   uri ← j.getObjValAs? DocumentUri "uri";
   languageId ← j.getObjValAs? String "languageId";
-  version ← j.getObjValAs? Int "version";
+  version ← j.getObjValAs? Nat "version";
   text ← j.getObjValAs? String "text";
   pure ⟨uri, languageId, version, text⟩⟩
 
@@ -275,19 +194,5 @@ instance rangeHasToJson : HasToJson Range :=
 instance locationHasToJson : HasToJson Location :=
 ⟨fun o => mkObj $
   ⟨"uri", toJson o.uri⟩ :: ⟨"range", toJson o.range⟩ :: []⟩
-
-instance diagnosticTagHasToJson : HasToJson DiagnosticTag :=
-⟨fun o => match o with
-  | DiagnosticTag.unnecessary => (1 : Nat)
-  | DiagnosticTag.deprecated  => (2 : Nat)⟩
-
-instance diagnosticRelatedInformationHasToJson : HasToJson DiagnosticRelatedInformation :=
-⟨fun o => mkObj $
-  ⟨"location", toJson o.location⟩ :: ⟨"message", o.message⟩ :: []⟩
-
-instance diagnosticHasToJson : HasToJson Diagnostic :=
-⟨fun o => mkObj $
-  ⟨"range", toJson o.range⟩ :: opt "severity" o.severity? ++ opt "code" o.code? ++
-    opt "source" o.source? ++ ⟨"message", o.message⟩ :: opt "tags" o.tags? ++ []⟩
 
 end Lean.Lsp

e234b9c671251d639cd1c8b8e72b45df3bdc8a28
Leonardo de Moura <leonardo@microsoft.com>
2020-07-28 16:24:16 -0700

    feat: elaborate `#print` command
    Basic `#print` command for helping us to test new frontend

---
+++ tests/lean/run/print_cmd.lean
@@ -0,0 +1,21 @@
+#print Nat
+
+new_frontend
+
+private def foo (x : Nat) : Nat := x + 1
+
+#print "hello"
+#print id
+#print propext
+#print arbitrary
+#print ReaderT.read
+#print Prod
+#print Prod.mk
+#print Nat
+#print Nat.succ
+#print Nat.rec
+#print Nat.casesOn
+#print foo
+#print Quot.mk
+#print Quot.ind
+#print Quot.mk

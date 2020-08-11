bdf1bae754500bf3c5ef449c9c5fef297afa070b
Sebastian Ullrich <sebasti@nullri.ch>
2020-08-11 15:46:16 +0200

    test: parenthesizer no longer dependent on importing `Lean`

--- tests/lean/run/Reparen.lean
+++ tests/lean/run/Reparen.lean
@@ -23,17 +23,17 @@ let (debug, f) : Bool × String := match args with
   | [f]       => (false, f)
   | _         => panic! "usage: file [-d]";
 initSearchPath none;
-withImportModules [{module := `Lean.Parser}] 0 fun env => do
-  stx ← Lean.Parser.parseFile env args.head!;
-  let header := stx.getArg 0;
-  some s ← pure header.reprint | throw $ IO.userError "header reprint failed";
-  IO.print s;
-  let cmds := stx.getArgs.extract 1 stx.getArgs.size;
-  cmds.forM $ fun cmd => do
-    let cmd := unparen cmd;
-    (cmd, _) ← IO.runMeta (PrettyPrinter.parenthesizeCommand cmd)
-      env { opts := KVMap.insert {} `trace.PrettyPrinter.parenthesize debug };
-    some s ← pure cmd.reprint | throw $ IO.userError "cmd reprint failed";
-    IO.print s
+env ← mkEmptyEnvironment;
+stx ← Lean.Parser.parseFile env args.head!;
+let header := stx.getArg 0;
+some s ← pure header.reprint | throw $ IO.userError "header reprint failed";
+IO.print s;
+let cmds := stx.getArgs.extract 1 stx.getArgs.size;
+cmds.forM $ fun cmd => do
+  let cmd := unparen cmd;
+  (cmd, _) ← IO.runMeta (PrettyPrinter.parenthesizeCommand cmd)
+    env { opts := KVMap.insert {} `trace.PrettyPrinter.parenthesize debug };
+  some s ← pure cmd.reprint | throw $ IO.userError "cmd reprint failed";
+  IO.print s
 
 #eval main ["../../../src/Init/Core.lean"]

77bf9f71f8c9d6b9c1b79a4d8d075fb9c1d552ea
Sebastian Ullrich <sebasti@nullri.ch>
2020-08-11 19:32:52 +0200

    feat: close the loop

--- src/Lean/Parser.lean
+++ src/Lean/Parser.lean
@@ -10,3 +10,14 @@ import Lean.Parser.Tactic
 import Lean.Parser.Command
 import Lean.Parser.Module
 import Lean.Parser.Syntax
+
+namespace Lean
+namespace Parser
+
+-- Close the mutual recursion loop; see corresponding `[extern]` in the parenthesizer.
+@[export lean_mk_antiquot_parenthesizer]
+def mkAntiquot.parenthesizer' (name : String) (kind : Option SyntaxNodeKind) (anonymous := true) : PrettyPrinter.Parenthesizer :=
+mkAntiquot.parenthesizer name kind anonymous
+
+end Parser
+end Lean

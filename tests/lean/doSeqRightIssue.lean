b616e8d07ae7cbf7004c6c7299cc7010b4095386
Leonardo de Moura <leonardo@microsoft.com>
2020-08-15 16:07:01 -0700

    test: `do` issue
    This is the example triggered a panic message in the new frontend when
    we were encoding `do a; b` as `a *> b`.
    See 7cdf917c9

---
+++ tests/lean/doSeqRightIssue.lean
@@ -0,0 +1,9 @@
+new_frontend
+
+universes u
+variables {α : Type u}
+variables {β : α → Type v}
+
+theorem ex {p₁ p₂ : Sigma (fun a => β a)} (h₁ : p₁.1 = p₂.1) (h : p₁.2 ≅ p₂.2) : p₁ = p₂ :=
+match p₁, p₂, h₁, h with
+| ⟨_, _⟩, ⟨_, _⟩, rfl, HEq.refl _ => rfl

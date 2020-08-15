4f145c744e2e34430372546660458dd11b154144
Leonardo de Moura <leonardo@microsoft.com>
2020-08-15 16:03:31 -0700

    test: match-expr

--- tests/lean/match3.lean
+++ tests/lean/match3.lean
@@ -10,7 +10,6 @@ match x with
 #eval f 0
 #eval f 30
 
-
 universes u
 
 theorem ex1 {α : Sort u} {a b : α} (h : a ≅ b) : a = b :=
@@ -54,3 +53,10 @@ variables {α : Type u} {p : α → Prop}
 theorem ex8 {a1 a2 : {x // p x}} (h : a1.val = a2.val) : a1 = a2 :=
 match a1, a2, h with
 | ⟨_, _⟩, ⟨_, _⟩, rfl => rfl
+
+universes v
+variables {β : α → Type v}
+
+theorem ex9 {p₁ p₂ : Sigma (fun a => β a)} (h₁ : p₁.1 = p₂.1) (h : p₁.2 ≅ p₂.2) : p₁ = p₂ :=
+match p₁, p₂, h₁, h with
+| ⟨_, _⟩, ⟨_, _⟩, rfl, HEq.refl _ => rfl

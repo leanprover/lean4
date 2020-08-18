647b917b4909161ebda09860836c78808d59dc26
Leonardo de Moura <leonardo@microsoft.com>
2020-08-18 15:14:37 -0700

    test: `HasGetAt` helper class example

--- tests/lean/run/stateRef.lean
+++ tests/lean/run/stateRef.lean
@@ -28,3 +28,27 @@ set (n+1);
 getThe Nat
 
 #eval (f3.run' "test").run' 10
+
+structure Label {β : Type} (v : β) (α : Type) :=
+(val : α)
+
+class HasGetAt {β : Type} (v : β) (α : outParam Type) (m : Type → Type) :=
+(getAt : m α)
+
+instance monadState.hasGetAt (β : Type) (v : β) (α : Type) (m : Type → Type) [Monad m] [MonadState (Label v α) m] : HasGetAt v α m :=
+{ getAt := do a ← getThe (Label v α); pure a.val }
+
+export HasGetAt (getAt)
+
+abbrev M := StateRefT (Label 0 Nat) $ StateRefT (Label 1 Nat) $ StateRefT (Label 2 Nat) IO
+
+def f4 : M Nat := do
+a0 : Nat ← getAt 0;
+a1 ← getAt 1;
+a2 ← getAt 2;
+IO.println $ "state0 " ++ toString a0;
+IO.println $ "state1 " ++ toString a1;
+IO.println $ "state1 " ++ toString a2;
+pure (a0 + a1 + a2)
+
+#eval ((f4.run' ⟨10⟩).run' ⟨20⟩).run' ⟨30⟩

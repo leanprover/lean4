7888919aa902a9410bfbc227e3b5b12101619d7a
Leonardo de Moura <leonardo@microsoft.com>
2020-08-25 12:40:30 -0700

    test: `MonadControl`

---
+++ tests/lean/run/monadControl.lean
@@ -0,0 +1,35 @@
+@[inline] def f {α} (s : String) (x : IO α) : IO α := do
+IO.println "started";
+IO.println s;
+a ← x;
+IO.println ("ended");
+pure a
+
+@[inline] def f'' {α m} [MonadControlT IO m] [Monad m] (msg : String) (x : m α) : m α := do
+controlAt IO fun runInBase => f msg (runInBase x)
+
+abbrev M := StateT Bool $ ExceptT String $ StateT String $ ReaderT Nat $ StateT Nat IO
+
+def tst : M Nat := do
+a ← f'' "hello" do { s ← getThe Nat; ctx ← read; modifyThe Nat fun s => s + ctx; when (s > 10) $ throw "ERROR"; getThe Nat };
+modifyThe Nat Nat.succ;
+pure a
+
+#eval (((tst.run true).run "world").run 1000).run 11
+
+@[inline] def g {α} (s : String) (x : Nat → IO α) : IO α := do
+IO.println "started";
+IO.println s;
+a ← x s.length;
+IO.println ("ended");
+pure a
+
+@[inline] def g' {α m} [MonadControlT IO m] [HasBind m] (msg : String) (x : Nat → m α) : m α := do
+controlAt IO fun runInBase => g msg (fun n => runInBase (x n))
+
+def tst2 : M Nat := do
+a ← g' "hello" fun x => do { s ← getThe Nat; ctx ← read; modifyThe Nat fun s => s + ctx + x; when (s > 10) $ throw "ERROR"; getThe Nat };
+modifyThe Nat Nat.succ;
+pure a
+
+#eval (((tst2.run true).run "world").run 1000).run 10

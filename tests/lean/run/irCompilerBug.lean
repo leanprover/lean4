5a7bf86a42e03866f63a0c7f081b70249225ffd9
Leonardo de Moura <leonardo@microsoft.com>
2020-08-26 08:33:06 -0700

    test: add test for buggy compiler step
    Bug was fixed at da928f8c0

---
+++ tests/lean/run/irCompilerBug.lean
@@ -0,0 +1,44 @@
+structure Payload :=
+(key : Nat)
+(val : Nat)
+
+
+
+@[noinline] def get? (p : Payload) (k : Nat) : Option Nat :=
+if p.key == k then p.val else none
+
+inductive T
+| a (i : Nat)
+| b (i : Nat)
+| c (i : Nat)
+| d (i : Nat)
+
+@[noinline] def foo (p : Payload) : Option T :=
+(do
+  i ← get? p 1;
+  pure (T.a i))
+<|> (do
+  i ← get? p 2;
+  pure (T.b i))
+<|> (do
+  i ← get? p 3;
+  pure (T.c i))
+<|> (do
+  i ← get? p 4;
+  i ← get? p 5;
+  pure (T.d i))
+
+def wrongOutput : String :=
+match foo (Payload.mk 1 20) with
+| some (t : T) =>
+  match t with
+  | T.a i => "1: " ++ toString i
+  | T.b i => "2: " ++ toString i
+  | T.c i => "3: " ++ toString i
+  | T.d i => "4: " ++ toString i
+| none => "5"
+
+#eval wrongOutput
+
+def main (xs : List String) : IO Unit :=
+IO.println wrongOutput

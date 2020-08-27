8746f984fd2391f2e988db38ef692ed42c5d7a66
Sebastian Ullrich <sebasti@nullri.ch>
2020-08-27 12:21:27 +0200

    feat: add ByteArray <-> String conversions

--- src/Init/Data/String/Extra.lean
+++ src/Init/Data/String/Extra.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 -/
 prelude
+import Init.Control.Except
+import Init.Data.ByteArray
 import Init.Util
 
 namespace String
@@ -13,5 +15,14 @@ if s.isNat then
   s.foldl (fun n c => n*10 + (c.toNat - '0'.toNat)) 0
 else
   panic! "Nat expected"
+
+/--
+  Convert a UTF-8 encoded `ByteArray` string to `String`.
+  The result is unspecified if `a` is not properly UTF-8 encoded. -/
+@[extern "lean_string_from_utf8_unchecked"]
+constant fromUTF8Unchecked (a : @& ByteArray) : String := arbitrary _
+
+@[extern "lean_string_to_utf8"]
+constant toUTF8 (a : @& String) : ByteArray := arbitrary _
 
 end String

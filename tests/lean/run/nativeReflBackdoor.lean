50470c328f40dfc258758b1b8b1d271462f759aa
Leonardo de Moura <leonardo@microsoft.com>
2020-08-28 12:09:25 -0700

    test: `nativeRefl!` abuse example

---
+++ tests/lean/run/nativeReflBackdoor.lean
@@ -0,0 +1,36 @@
+new_frontend
+
+/-
+This example demonstratea that when we are using `nativeRefl!`,
+we are also trusting the correctness of `implementedBy` annotations,
+foreign functions (i.e., `[extern]` annotations), etc.
+-/
+def g (b : Bool) := false
+
+/-
+The following `implementedBy` is telling the compiler
+"trust me, `g` does implement `f`"
+which is clearly false in this example.
+-/
+@[implementedBy g]
+def f (b : Bool) := b
+
+theorem fConst (b : Bool) : f b = false :=
+match b with
+| true  =>
+  /- The following `nativeRefl!` is going to use `g` to evaluate `f`
+     because of the `implementedBy` directive. -/
+  have (f true) = false from nativeRefl! (f true);
+  this
+| false => rfl
+
+theorem trueEqFalse : true = false :=
+have h₁ : f true = true  from rfl;
+have h₂ : f true = false from fConst true;
+Eq.trans h₁.symm h₂
+
+/-
+We managed to prove `False` using the unsound annotation `implementedBy` above.
+-/
+theorem unsound : False :=
+Bool.noConfusion trueEqFalse

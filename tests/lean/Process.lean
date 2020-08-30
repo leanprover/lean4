8b79e0f28cb28b52e4dc29487d5386136aa906d1
Sebastian Ullrich <sebasti@nullri.ch>
2020-08-30 10:28:36 +0200

    test: robustly fix test on Windows

--- tests/lean/Process.lean
+++ tests/lean/Process.lean
@@ -1,16 +1,16 @@
 open IO.Process
 
 #eval do
-  child ← spawn { cmd := "bash", args := #["-c", "echo hi!"] };
+  child ← spawn { cmd := "sh", args := #["-c", "echo hi!"] };
   child.wait
 
 #eval do
-  child ← spawn { cmd := "bash", args := #["-c", "echo ho!"], stdout := Stdio.piped };
+  child ← spawn { cmd := "sh", args := #["-c", "echo ho!"], stdout := Stdio.piped };
   child.wait;
   child.stdout.readToEnd
 
 #eval do
-  child ← spawn { cmd := "bash", args := #["-c", "head -n 1"], stdin := Stdio.piped, stdout := Stdio.piped };
+  child ← spawn { cmd := "head", args := #["-n1"], stdin := Stdio.piped, stdout := Stdio.piped };
   child.stdin.putStrLn "hu!";
   child.stdin.flush;
   child.wait;

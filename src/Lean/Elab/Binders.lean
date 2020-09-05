70c42456b9f7e661da699784f3b1c973bb90e544
Leonardo de Moura <leonardo@microsoft.com>
2020-09-04 18:22:56 -0700

    feat: expand `intro`+`matchAlts` macro

--- src/Lean/Elab/Binders.lean
+++ src/Lean/Elab/Binders.lean
@@ -378,33 +378,47 @@ let pats := (alt0.getArg 0).getArgs.getSepElems;
 pats.size
 
 /- Helper function for `expandMatchAltsIntoMatch` -/
-private def expandMatchAltsIntoMatchAux (ref : Syntax) (matchAlts : Syntax) : Nat → Array Syntax → MacroM Syntax
+private def expandMatchAltsIntoMatchAux (ref : Syntax) (matchAlts : Syntax) (matchTactic : Bool) : Nat → Array Syntax → MacroM Syntax
 | 0,   discrs =>
-  pure $ Syntax.node `Lean.Parser.Term.match
+  pure $ Syntax.node (if matchTactic then `Lean.Parser.Tactic.match else `Lean.Parser.Term.match)
     #[mkAtomFrom ref "match ", mkNullNode discrs, mkNullNode, mkAtomFrom ref " with ", mkNullNode, matchAlts]
 | n+1, discrs => withFreshMacroScope do
   x ← `(x);
   let discrs := if discrs.isEmpty then discrs else discrs.push $ mkAtomFrom ref ", ";
   let discrs := discrs.push $ Syntax.node `Lean.Parser.Term.matchDiscr #[mkNullNode, x];
   body ← expandMatchAltsIntoMatchAux n discrs;
-  `(fun $x => $body)
+  if matchTactic then
+    `(tactic| intro $x:term; $body:tactic)
+  else
+    `(fun $x => $body)
 
 /--
   Expand `matchAlts` syntax into a full `match`-expression.
   Example
     ```
-    | 0, true => 1
-    | i, _    => 2
+    | 0, true => alt_1
+    | i, _    => alt_2
     ```
-    expands intro
+    expands intro (for tactic == false)
     ```
     fun x_1 x_2 =>
     match x_1, x_2 with
-    | 0, true => 1
-    | i, _    => 2
-    ``` -/
-def expandMatchAltsIntoMatch (ref : Syntax) (matchAlts : Syntax) : MacroM Syntax :=
-expandMatchAltsIntoMatchAux ref matchAlts (getMatchAltNumPatterns matchAlts) #[]
+    | 0, true => alt_1
+    | i, _    => alt_2
+    ```
+    and (for tactic == true)
+    ```
+    intro x_1; intro x_2;
+    match x_1, x_2 with
+    | 0, true => alt_1
+    | i, _    => alt_2
+    ```
+ -/
+def expandMatchAltsIntoMatch (ref : Syntax) (matchAlts : Syntax) (tactic := false) : MacroM Syntax :=
+expandMatchAltsIntoMatchAux ref matchAlts tactic (getMatchAltNumPatterns matchAlts) #[]
+
+def expandMatchAltsIntoMatchTactic (ref : Syntax) (matchAlts : Syntax) : MacroM Syntax :=
+expandMatchAltsIntoMatchAux ref matchAlts true (getMatchAltNumPatterns matchAlts) #[]
 
 @[builtinTermElab «fun»] def elabFun : TermElab :=
 fun stx expectedType? =>
--- tests/lean/run/match1.lean
+++ tests/lean/run/match1.lean
@@ -11,3 +11,18 @@ theorem ex1 : f [1, 0, 2] = [false, true, false] :=
 rfl
 
 #check f
+
+def g (xs : List Nat) : List Bool :=
+xs.map $ by {
+  intro
+    | 0 => exact true
+    | _ => exact false
+}
+
+theorem ex2 : g [1, 0, 2] = [false, true, false] :=
+rfl
+
+theorem ex3 {p q r : Prop} : p ∨ q → r → (q ∧ r) ∨ (p ∧ r) :=
+by intro
+ | Or.inl hp, h => { apply Or.inr; apply And.intro; assumption; assumption }
+ | Or.inr hq, h => { apply Or.inl; exact ⟨hq, h⟩ }

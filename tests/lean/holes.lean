18a9cb9b437c0ccd6a2bb958fa2fe63e88c0701f
Leonardo de Moura <leonardo@microsoft.com>
2020-09-07 17:02:43 -0700

    feat: improve error message position for binders with implicit types

--- tests/lean/holes.lean
+++ tests/lean/holes.lean
@@ -9,3 +9,11 @@ a
 def f3 (x : Nat) : Nat :=
 let y := g x + g x;
 y + _ + ?hole
+
+def f4 {α β} (a : α) := a
+
+def f5 {α} {β : _} (a : α) := a
+
+def f6 (a : Nat) :=
+let f {α β} (a : α) := a;
+f a

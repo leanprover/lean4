cc520d422cac299217acfe5e7cc7cebf63d96663
Leonardo de Moura <leonardo@microsoft.com>
2020-09-12 07:18:43 -0700

    fix: ensure no unassigned mvars at #eval

---
+++ tests/lean/evalWithMVar.lean
@@ -0,0 +1,13 @@
+new_frontend
+
+def c {α} : Sum α Nat :=
+Sum.inr 10
+
+def Sum.someRight {α β} (s : Sum α β) : Option β :=
+match s with
+| Sum.inl _ => none
+| Sum.inr b => some b
+
+#check Sum.someRight c
+
+#eval Sum.someRight c

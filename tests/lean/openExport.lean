3ebecc8caa624231323aaf7e0ba1fe43a3bb579d
Leonardo de Moura <leonardo@microsoft.com>
2020-09-15 08:58:31 -0700

    test: `open` and `export`

---
+++ tests/lean/openExport.lean
@@ -0,0 +1,35 @@
+new_frontend
+
+def A.x := 10
+
+namespace B
+
+export A (x)
+
+def y := 20
+
+#check x -- works
+#check y -- works
+
+end B
+
+#check A.x -- works
+#check B.x -- works, but fails in old frontend and Lean3 :)
+#check B.y -- works
+#check x -- fails as expected
+#check y -- fails as expected
+open B
+#check x -- works
+#check y -- works
+
+namespace B
+
+#check x -- works
+#check y -- works
+
+def z := 30
+
+#check z -- works
+end B
+
+#check z  -- works, but fails in old frontend and Lean3 :)

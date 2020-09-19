c43450f027a0b26727dec29651392f37051fd7eb
Leonardo de Moura <leonardo@microsoft.com>
2020-09-19 14:59:00 -0700

    feat: macros for the syntax category
    This is the last of a series of commits for adding this feature.

---
+++ tests/lean/run/stxMacro.lean
@@ -0,0 +1,18 @@
+new_frontend
+
+-- Macro for the `syntax` category
+macro "many " x:stx : stx => `(stx| ($x)*)
+
+syntax "sum! " (many term:max) : term
+
+macro_rules
+| `(sum! $x*) => do
+  r ← `(0);
+  x.foldlM (fun r elem => `($r + $elem)) r
+
+#check sum! 1 2 3
+#eval sum! 1 2 3
+#check sum!
+
+theorem ex : sum! 1 2 3 = 6 :=
+rfl

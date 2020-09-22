f4b5ec710f30ca6f8e820e72e15b7fe11d0bfa24
Leonardo de Moura <leonardo@microsoft.com>
2020-09-21 17:11:15 -0700

    fix: fixes #175

---
+++ tests/lean/run/175.lean
@@ -0,0 +1,26 @@
+import Lean
+new_frontend
+
+namespace Foo
+open Lean.Elab.Term
+
+syntax[fooKind] "foo!" term : term
+
+@[termElab fooKind] def elabFoo : TermElab :=
+fun stx expectedType? => elabTerm (stx.getArg 1) expectedType?
+
+#check foo! 10
+
+end Foo
+
+namespace Lean
+namespace Elab
+namespace Tactic
+open Meta
+
+@[builtinTactic clear] def myEvalClear : Tactic := -- this fails in the old-frontend because it eagerly resolves `clear` as `Lean.Meta.clear`.
+fun _ => pure ()
+
+end Tactic
+end Elab
+end Lean

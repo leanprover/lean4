bdf412bf4962859a4d9f0a062c1f7066e4191248
Leonardo de Moura <leonardo@microsoft.com>
2020-09-24 18:01:23 -0700

    test: add old Lean3 tests

---
+++ tests/lean/run/def5.lean
@@ -0,0 +1,10 @@
+new_frontend
+
+def fib : Nat → Nat
+| 0   => 1
+| 1   => 1
+| n+2 => fib n + fib (n+1)
+
+example : fib 0 = 1 := rfl
+example : fib 1 = 1 := rfl
+example (n : Nat) : fib (n+2) = fib n + fib (n+1) := rfl

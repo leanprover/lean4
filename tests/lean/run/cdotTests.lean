b71e5b4648977ff9e4eac48d717900b8c0857c87
Leonardo de Moura <leonardo@microsoft.com>
2020-09-25 11:17:11 -0700

    test: cdot tests

---
+++ tests/lean/run/cdotTests.lean
@@ -0,0 +1,33 @@
+new_frontend
+
+class Inc (α : Type) :=
+(inc : α → α)
+
+export Inc (inc)
+
+instance {α} [Inc α] : Inc (List α) :=
+{ inc := (·.map inc) }
+
+instance : Inc Nat :=
+{ inc := Nat.succ }
+
+#eval inc 10
+#eval inc [1, 2, 3]
+
+theorem ex1 : [(1, "hello"), (2, "world")].map (·.1) = [1, 2] :=
+rfl
+
+theorem ex2 : [(1, "hello"), (2, "world")].map (·.snd) = ["hello", "world"] :=
+rfl
+
+def sum (xs : List Nat) : Nat :=
+(·.2) $ Id.run $ StateT.run (s:=0) do
+  xs.forM fun x => modify (· + x)
+
+#eval sum [1, 2, 3, 4]
+
+theorem ex3 : sum [1, 2, 3] = 6 :=
+rfl
+
+theorem ex4 : sum [1, 2, 3, 4] = 10 :=
+rfl

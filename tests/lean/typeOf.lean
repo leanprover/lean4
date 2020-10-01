4b6d308bc2e2ea07f4ef12e0766173fc9fab47a6
Leonardo de Moura <leonardo@microsoft.com>
2020-10-01 14:56:46 -0700

    feat: add `elabTypeOf` and `elabEnsureTypeOf`

---
+++ tests/lean/typeOf.lean
@@ -0,0 +1,26 @@
+new_frontend
+
+def f1 (x : Nat) (b : Bool) : typeOf! x :=
+let r : typeOf! (x+1) := x+1;
+r + 1
+
+theorem ex1 : f1 1 true = 3 :=
+rfl
+
+def f2 (x : Nat) (b : Bool) : typeOf! x :=
+let r : typeOf! b := x+1; -- error
+r + 1
+
+def f3 (x : Nat) (b : Bool) : typeOf! b :=
+let r (x!1 : typeOf! x) : typeOf! b := x > 1;
+r x
+
+def f4 (x : Nat) : Nat :=
+let y : Nat := x
+let y := ensureTypeOf! y "invalid reassignment" y == 1 -- error
+y + 1
+
+def f5 (x : Nat) : Nat :=
+let y : Nat := x
+let y := ensureTypeOf! y "invalid reassignment" (y+1)
+y + 1

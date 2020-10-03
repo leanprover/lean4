0212ccd3e2fc98ce24e3d1df6d665b7899dbddf5
Sebastian Ullrich <sebasti@nullri.ch>
2020-10-03 19:37:36 +0200

    feat: Nix: compatibility default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
-{ pkgs ? import ./nix/nixpkgs.nix, llvmPackages ? pkgs.llvmPackages_latest }:
-
-pkgs.callPackage ./nix/derivation.nix { inherit llvmPackages; }
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/c75e76f80c57784a6734356315b306140646ee84.tar.gz";
+    sha256 = "071aal00zp2m9knnhddgr2wqzlx6i6qa1263lv1y7bdn2w20h10h"; }
+) {
+  src =  ./.;
+}).defaultNix

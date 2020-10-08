3694936b7d175b03b06b1f9ca371611639467adc
Leonardo de Moura <leonardo@microsoft.com>
2020-10-08 12:11:07 -0700

    test: `doHave` test

--- tests/lean/run/doNotation3.lean
+++ tests/lean/run/doNotation3.lean
@@ -21,3 +21,15 @@ loop as.size (Nat.leRefl _) b
 
 theorem ex : (Id.run $ fold #[1, 2, 3, 4] 0 (pure $ · + ·)) = 10 :=
 rfl
+
+def fold2 {m α β} [Monad m] (as : Array α) (b : β) (f : α → β → m β) : m β :=
+let rec loop (i : Nat) (h : i ≤ as.size) (b : β) : m β := do
+  match i, h with
+  | 0,   h => return b
+  | i+1, h =>
+    have h' : i < as.size          from Nat.ltOfLtOfLe (Nat.ltSuccSelf i) h
+    have as.size - 1 < as.size     from Nat.subLt (zeroLtOfLt h') (decide! (0 < 1))
+    have as.size - 1 - i < as.size from Nat.ltOfLeOfLt (Nat.subLe (as.size - 1) i) this
+    let b ← f (as.get ⟨as.size - 1 - i, this⟩) b
+    loop i (Nat.leOfLt h') b
+loop as.size (Nat.leRefl _) b

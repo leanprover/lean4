736b2bf8ed9edc27b89027d8b03265c516a06509
Leonardo de Moura <leonardo@microsoft.com>
2020-10-10 07:53:52 -0700

    chore: remove kind

--- src/Lean/Util/Trace.lean
+++ src/Lean/Util/Trace.lean
@@ -135,9 +135,9 @@ namespace Lean
 
 macro:max "trace!" id:term:max msg:term : term => `(trace $id fun _ => ($msg : MessageData))
 
-syntax[traceInterp] "trace[" ident "]!" term : term
+syntax "trace[" ident "]!" term : term
 
-macro_rules[traceInterp]
+macro_rules
 | `(trace[$id]! $s) => `(Lean.trace $(quote id.getId) fun _ => ($s : MessageData))
 
 end Lean

5ab15d4e609c297ed5455de9cb726b122a775581
Leonardo de Moura <leonardo@microsoft.com>
2020-10-17 07:25:50 -0700

    chore: move to new frontend

--- src/Lean/Meta/Tactic/Assumption.lean
+++ src/Lean/Meta/Tactic/Assumption.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2019 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -6,25 +7,26 @@ Authors: Leonardo de Moura
 import Lean.Meta.ExprDefEq
 import Lean.Meta.Tactic.Util
 
-namespace Lean
-namespace Meta
+namespace Lean.Meta
 
 def assumptionAux (mvarId : MVarId) : MetaM Bool :=
-withMVarContext mvarId $ do
-  checkNotAssigned mvarId `assumption;
-  mvarType ← getMVarType mvarId;
-  lctx ← getLCtx;
-  h? ← lctx.findDeclRevM? $ fun decl =>
+withMVarContext mvarId do
+  checkNotAssigned mvarId `assumption
+  let mvarType ← getMVarType mvarId
+  let lctx ← getLCtx
+  let h? ← lctx.findDeclRevM? fun decl => do
     if decl.isAuxDecl then
       pure none
+    else if (← isDefEq mvarType decl.type) then
+      pure (some decl.toExpr)
     else
-      condM (isDefEq mvarType decl.type) (pure (some decl.toExpr)) (pure none);
+      pure none
   match h? with
   | some h => do assignExprMVar mvarId h; pure true
   | none   => pure false
 
-def assumption (mvarId : MVarId) : MetaM Unit :=
-unlessM (assumptionAux mvarId) $ throwTacticEx `assumption mvarId ""
+def assumption (mvarId : MVarId) : MetaM Unit := do
+unless (← assumptionAux mvarId) do
+  throwTacticEx `assumption mvarId ""
 
-end Meta
-end Lean
+end Lean.Meta

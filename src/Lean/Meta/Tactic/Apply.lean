c75d0350ec7ccc6254551721a14891cfc0cb9317
Leonardo de Moura <leonardo@microsoft.com>
2020-10-17 07:19:59 -0700

    chore: move to new frontend

--- src/Lean/Meta/Tactic/Apply.lean
+++ src/Lean/Meta/Tactic/Apply.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -9,93 +10,89 @@ import Lean.Meta.SynthInstance
 import Lean.Meta.CollectMVars
 import Lean.Meta.Tactic.Util
 
-namespace Lean
-namespace Meta
+namespace Lean.Meta
 
 /-
   Compute the number of expected arguments and whether the result type is of the form
   (?m ...) where ?m is an unassigned metavariable.
 -/
 private def getExpectedNumArgsAux (e : Expr) : MetaM (Nat × Bool) :=
-withReducible $ forallTelescopeReducing e $ fun xs body =>
+withReducible $ forallTelescopeReducing e fun xs body =>
   pure (xs.size, body.getAppFn.isMVar)
 
 private def getExpectedNumArgs (e : Expr) : MetaM Nat := do
-(numArgs, _) ← getExpectedNumArgsAux e;
+let (numArgs, _) ← getExpectedNumArgsAux e
 pure numArgs
 
 private def throwApplyError {α} (mvarId : MVarId) (eType : Expr) (targetType : Expr) : MetaM α :=
-throwTacticEx `apply mvarId ("failed to unify" ++ indentExpr eType ++ Format.line ++ "with" ++ indentExpr targetType)
+throwTacticEx `apply mvarId msg!"failed to unify{indentExpr eType}\nwith{indentExpr targetType}"
 
 def synthAppInstances (tacticName : Name) (mvarId : MVarId) (newMVars : Array Expr) (binderInfos : Array BinderInfo) : MetaM Unit :=
-newMVars.size.forM $ fun i =>
-  when (binderInfos.get! i).isInstImplicit $ do
-    let mvar := newMVars.get! i;
-    mvarType ← inferType mvar;
-    mvarVal  ← synthInstance mvarType;
-    unlessM (isDefEq mvar mvarVal) $
-      throwTacticEx tacticName mvarId ("failed to assign synthesized instance")
+newMVars.size.forM fun i => do
+  if binderInfos[i].isInstImplicit then
+    let mvar := newMVars[i]
+    let mvarType ← inferType mvar
+    let mvarVal  ← synthInstance mvarType
+    unless (← isDefEq mvar mvarVal) do
+      throwTacticEx tacticName mvarId "failed to assign synthesized instance"
 
 def appendParentTag (mvarId : MVarId) (newMVars : Array Expr) (binderInfos : Array BinderInfo) : MetaM Unit := do
-parentTag ← getMVarTag mvarId;
+let parentTag ← getMVarTag mvarId
 if newMVars.size == 1 then
   -- if there is only one subgoal, we inherit the parent tag
-  setMVarTag (newMVars.get! 0).mvarId! parentTag
-else unless parentTag.isAnonymous $
-  newMVars.size.forM $ fun i =>
-    let newMVarId := (newMVars.get! i).mvarId!;
-    unlessM (isExprMVarAssigned newMVarId) $
-    unless (binderInfos.get! i).isInstImplicit $ do
-      currTag ← getMVarTag newMVarId;
-      setMVarTag newMVarId (appendTag parentTag currTag)
+  setMVarTag newMVars[0].mvarId! parentTag
+else
+  unless parentTag.isAnonymous do
+    newMVars.size.forM fun i => do
+      let newMVarId := newMVars[i].mvarId!
+      unless (← isExprMVarAssigned newMVarId) do
+        unless binderInfos[i].isInstImplicit do
+          let currTag ← getMVarTag newMVarId
+          setMVarTag newMVarId (appendTag parentTag currTag)
 
 def postprocessAppMVars (tacticName : Name) (mvarId : MVarId) (newMVars : Array Expr) (binderInfos : Array BinderInfo) : MetaM Unit := do
-synthAppInstances tacticName mvarId newMVars binderInfos;
+synthAppInstances tacticName mvarId newMVars binderInfos
 -- TODO: default and auto params
 appendParentTag mvarId newMVars binderInfos
 
 private def dependsOnOthers (mvar : Expr) (otherMVars : Array Expr) : MetaM Bool :=
-otherMVars.anyM $ fun otherMVar =>
+otherMVars.anyM fun otherMVar =>
   if mvar == otherMVar then pure false
   else do
-    otherMVarType ← inferType otherMVar;
-    pure $ (otherMVarType.findMVar? $ fun mvarId => mvarId == mvar.mvarId!).isSome
+    let otherMVarType ← inferType otherMVar
+    pure $ (otherMVarType.findMVar? fun mvarId => mvarId == mvar.mvarId!).isSome
 
 private def reorderNonDependentFirst (newMVars : Array Expr) : MetaM (List MVarId) := do
-(nonDeps, deps) ← newMVars.foldlM
-  (fun (acc : Array MVarId × Array MVarId) (mvar : Expr) => do
-    let (nonDeps, deps) := acc;
-    let currMVarId := mvar.mvarId!;
-    condM (dependsOnOthers mvar newMVars)
-      (pure (nonDeps, deps.push currMVarId))
-      (pure (nonDeps.push currMVarId, deps)))
-  (#[], #[]);
+let (nonDeps, deps) ← newMVars.foldlM (init := (#[], #[])) fun (nonDeps, deps) (mvar : Expr) => do
+  let currMVarId := mvar.mvarId!
+  if (← dependsOnOthers mvar newMVars) then
+    pure (nonDeps, deps.push currMVarId)
+  else
+    pure (nonDeps.push currMVarId, deps)
 pure $ nonDeps.toList ++ deps.toList
 
 inductive ApplyNewGoals
 | nonDependentFirst | nonDependentOnly | all
 
 def apply (mvarId : MVarId) (e : Expr) : MetaM (List MVarId) :=
-withMVarContext mvarId $ do
-  checkNotAssigned mvarId `apply;
-  targetType ← getMVarType mvarId;
-  eType      ← inferType e;
-  (numArgs, hasMVarHead) ← getExpectedNumArgsAux eType;
-  numArgs    ← if !hasMVarHead then pure numArgs else do {
-    targetTypeNumArgs ← getExpectedNumArgs targetType;
-    pure (numArgs - targetTypeNumArgs)
-  };
-  (newMVars, binderInfos, eType) ← forallMetaTelescopeReducing eType (some numArgs);
-  unlessM (isDefEq eType targetType) $ throwApplyError mvarId eType targetType;
-  postprocessAppMVars `apply mvarId newMVars binderInfos;
-  e ← instantiateMVars e;
-  assignExprMVar mvarId (mkAppN e newMVars);
-  newMVars ← newMVars.filterM $ fun mvar => not <$> isExprMVarAssigned mvar.mvarId!;
-  otherMVarIds ← getMVarsNoDelayed e;
+withMVarContext mvarId do
+  checkNotAssigned mvarId `apply
+  let targetType ← getMVarType mvarId
+  let eType      ← inferType e
+  let (numArgs, hasMVarHead) ← getExpectedNumArgsAux eType
+  if hasMVarHead then
+    let targetTypeNumArgs ← getExpectedNumArgs targetType
+    numArgs := numArgs - targetTypeNumArgs
+  let (newMVars, binderInfos, eType) ← forallMetaTelescopeReducing eType (some numArgs)
+  unless (← isDefEq eType targetType) do throwApplyError mvarId eType targetType
+  postprocessAppMVars `apply mvarId newMVars binderInfos
+  let e ← instantiateMVars e
+  assignExprMVar mvarId (mkAppN e newMVars)
+  let newMVars ← newMVars.filterM $ fun mvar => not <$> isExprMVarAssigned mvar.mvarId!
+  let otherMVarIds ← getMVarsNoDelayed e
   -- TODO: add option `ApplyNewGoals` and implement other orders
-  newMVarIds ← reorderNonDependentFirst newMVars;
-  let otherMVarIds := otherMVarIds.filter fun mvarId => !newMVarIds.contains mvarId;
+  let newMVarIds ← reorderNonDependentFirst newMVars
+  let otherMVarIds := otherMVarIds.filter fun mvarId => !newMVarIds.contains mvarId
   pure $ newMVarIds ++ otherMVarIds.toList
 
-end Meta
-end Lean
+end Lean.Meta

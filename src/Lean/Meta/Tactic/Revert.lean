072906eb70cacd78eb7ad3251b967b6a1cfa8195
Leonardo de Moura <leonardo@microsoft.com>
2020-10-17 08:18:33 -0700

    chore: move to new frontend

--- src/Lean/Meta/Tactic/Revert.lean
+++ src/Lean/Meta/Tactic/Revert.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -5,20 +6,19 @@ Authors: Leonardo de Moura
 -/
 import Lean.Meta.Tactic.Util
 
-namespace Lean
-namespace Meta
+namespace Lean.Meta
 
-def revert (mvarId : MVarId) (fvars : Array FVarId) (preserveOrder : Bool := false) : MetaM (Array FVarId × MVarId) :=
-if fvars.isEmpty then pure (fvars, mvarId)
-else withMVarContext mvarId $ do
-  tag ← getMVarTag mvarId;
-  checkNotAssigned mvarId `revert;
+def revert (mvarId : MVarId) (fvars : Array FVarId) (preserveOrder : Bool := false) : MetaM (Array FVarId × MVarId) := do
+if fvars.isEmpty then
+  pure (fvars, mvarId)
+else withMVarContext mvarId do
+  let tag ← getMVarTag mvarId
+  checkNotAssigned mvarId `revert
   -- Set metavariable kind to natural to make sure `elimMVarDeps` will assign it.
-  setMVarKind mvarId MetavarKind.natural;
-  e ← finally (elimMVarDeps (fvars.map mkFVar) (mkMVar mvarId) preserveOrder) (setMVarKind mvarId MetavarKind.syntheticOpaque);
-  e.withApp $ fun mvar args => do
-    setMVarTag mvar.mvarId! tag;
+  setMVarKind mvarId MetavarKind.natural
+  let e ← try elimMVarDeps (fvars.map mkFVar) (mkMVar mvarId) preserveOrder finally setMVarKind mvarId MetavarKind.syntheticOpaque
+  e.withApp fun mvar args => do
+    setMVarTag mvar.mvarId! tag
     pure (args.map Expr.fvarId!, mvar.mvarId!)
 
-end Meta
-end Lean
+end Lean.Meta

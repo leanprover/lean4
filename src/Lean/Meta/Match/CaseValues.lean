deb9b570d5d41b7a1b67aa91289e1e62c757b62e
Leonardo de Moura <leonardo@microsoft.com>
2020-10-17 09:14:04 -0700

    chore: move to new frontend

--- src/Lean/Meta/Match/CaseValues.lean
+++ src/Lean/Meta/Match/CaseValues.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -6,8 +7,7 @@ Authors: Leonardo de Moura
 import Lean.Meta.Tactic.Subst
 import Lean.Meta.Tactic.Clear
 
-namespace Lean
-namespace Meta
+namespace Lean.Meta
 
 structure CaseValueSubgoal :=
 (mvarId : MVarId)
@@ -28,37 +28,36 @@ instance CaseValueSubgoal.inhabited : Inhabited CaseValueSubgoal :=
 def caseValueAux (mvarId : MVarId) (fvarId : FVarId) (value : Expr) (hName : Name := `h) (subst : FVarSubst := {})
     : MetaM (CaseValueSubgoal × CaseValueSubgoal) :=
 withMVarContext mvarId $ do
-  tag ← getMVarTag mvarId;
-  checkNotAssigned mvarId `caseValue;
-  target ← getMVarType mvarId;
-  xEqValue ← mkEq (mkFVar fvarId) value;
-  let xNeqValue := mkApp (mkConst `Not) xEqValue;
-  let thenTarget := Lean.mkForall hName BinderInfo.default xEqValue  target;
-  let elseTarget := Lean.mkForall hName BinderInfo.default xNeqValue target;
-  thenMVar ← mkFreshExprSyntheticOpaqueMVar thenTarget tag;
-  elseMVar ← mkFreshExprSyntheticOpaqueMVar elseTarget tag;
-  val ← mkAppOptM `dite #[none, xEqValue, none, thenMVar, elseMVar];
-  assignExprMVar mvarId val;
-  (elseH, elseMVarId) ← intro1P elseMVar.mvarId!;
-  let elseSubgoal := { mvarId := elseMVarId, newH := elseH, subst := subst : CaseValueSubgoal };
-  (thenH, thenMVarId) ← intro1P thenMVar.mvarId!;
-  let symm   := false;
-  let clearH := false;
-  (thenSubst, thenMVarId) ← substCore thenMVarId thenH symm subst clearH;
-  withMVarContext thenMVarId do {
-    trace! `Meta ("subst domain: " ++ toString thenSubst.domain);
-    let thenH := (thenSubst.get thenH).fvarId!;
-    trace! `Meta "searching for decl";
-    decl ← getLocalDecl thenH;
-    trace! `Meta "found decl"
-  };
-  let thenSubgoal := { mvarId := thenMVarId, newH := (thenSubst.get thenH).fvarId!, subst := thenSubst : CaseValueSubgoal };
+  let tag ← getMVarTag mvarId
+  checkNotAssigned mvarId `caseValue
+  let target ← getMVarType mvarId
+  let xEqValue ← mkEq (mkFVar fvarId) value
+  let xNeqValue := mkApp (mkConst `Not) xEqValue
+  let thenTarget := Lean.mkForall hName BinderInfo.default xEqValue  target
+  let elseTarget := Lean.mkForall hName BinderInfo.default xNeqValue target
+  let thenMVar ← mkFreshExprSyntheticOpaqueMVar thenTarget tag
+  let elseMVar ← mkFreshExprSyntheticOpaqueMVar elseTarget tag
+  let val ← mkAppOptM `dite #[none, xEqValue, none, thenMVar, elseMVar]
+  assignExprMVar mvarId val
+  let (elseH, elseMVarId) ← intro1P elseMVar.mvarId!
+  let elseSubgoal := { mvarId := elseMVarId, newH := elseH, subst := subst : CaseValueSubgoal }
+  let (thenH, thenMVarId) ← intro1P thenMVar.mvarId!
+  let symm   := false
+  let clearH := false
+  let (thenSubst, thenMVarId) ← substCore thenMVarId thenH symm subst clearH
+  withMVarContext thenMVarId do
+    trace[Meta]! "subst domain: {thenSubst.domain}"
+    let thenH := (thenSubst.get thenH).fvarId!
+    trace[Meta]! "searching for decl"
+    let decl ← getLocalDecl thenH
+    trace[Meta]! "found decl"
+  let thenSubgoal := { mvarId := thenMVarId, newH := (thenSubst.get thenH).fvarId!, subst := thenSubst : CaseValueSubgoal }
   pure (thenSubgoal, elseSubgoal)
 
 def caseValue (mvarId : MVarId) (fvarId : FVarId) (value : Expr) : MetaM (CaseValueSubgoal × CaseValueSubgoal) := do
-s ← caseValueAux mvarId fvarId value;
-appendTagSuffix s.1.mvarId `thenBranch;
-appendTagSuffix s.2.mvarId `elseBranch;
+let s ← caseValueAux mvarId fvarId value
+appendTagSuffix s.1.mvarId `thenBranch
+appendTagSuffix s.2.mvarId `elseBranch
 pure s
 
 structure CaseValuesSubgoal :=
@@ -68,23 +67,6 @@ structure CaseValuesSubgoal :=
 
 instance CaseValueSubgoals.inhabited : Inhabited CaseValuesSubgoal :=
 ⟨{ mvarId := arbitrary _ }⟩
-
-private def caseValuesAux (hNamePrefix : Name) (fvarId : FVarId) : Nat → MVarId → List Expr → Array FVarId → Array CaseValuesSubgoal → MetaM (Array CaseValuesSubgoal)
-| i, mvarId, [],    hs, subgoals => throwTacticEx `caseValues mvarId "list of values must not be empty"
-| i, mvarId, v::vs, hs, subgoals => do
-  (thenSubgoal, elseSubgoal) ← caseValueAux mvarId fvarId v (hNamePrefix.appendIndexAfter i) {};
-  appendTagSuffix thenSubgoal.mvarId ((`case).appendIndexAfter i);
-  thenMVarId ← hs.foldlM
-    (fun thenMVarId h => match thenSubgoal.subst.get h with
-      | Expr.fvar fvarId _ => tryClear thenMVarId fvarId
-      | _                  => pure thenMVarId)
-    thenSubgoal.mvarId;
-  let subgoals := subgoals.push { mvarId := thenMVarId, newHs := #[thenSubgoal.newH], subst := thenSubgoal.subst };
-  match vs with
-  | [] => do
-    appendTagSuffix elseSubgoal.mvarId ((`case).appendIndexAfter (i+1));
-    pure $ subgoals.push { mvarId := elseSubgoal.mvarId, newHs := hs.push elseSubgoal.newH, subst := {} }
-  | _  => caseValuesAux (i+1) elseSubgoal.mvarId vs (hs.push elseSubgoal.newH) subgoals
 
 /--
   Split goal `... |- C x` into values.size + 1 subgoals
@@ -99,7 +81,22 @@ private def caseValuesAux (hNamePrefix : Name) (fvarId : FVarId) : Nat → MVarI
   Remark: the last subgoal is for the "else" catchall case, and its `subst` is `{}`.
   Remark: the fiels `newHs` has size 1 forall but the last subgoal. -/
 def caseValues (mvarId : MVarId) (fvarId : FVarId) (values : Array Expr) (hNamePrefix := `h) : MetaM (Array CaseValuesSubgoal) :=
-caseValuesAux hNamePrefix fvarId 1 mvarId values.toList #[] #[]
+let rec loop : Nat → MVarId → List Expr → Array FVarId → Array CaseValuesSubgoal → MetaM (Array CaseValuesSubgoal)
+  | i, mvarId, [],    hs, subgoals => throwTacticEx `caseValues mvarId "list of values must not be empty"
+  | i, mvarId, v::vs, hs, subgoals => do
+    let (thenSubgoal, elseSubgoal) ← caseValueAux mvarId fvarId v (hNamePrefix.appendIndexAfter i) {}
+    appendTagSuffix thenSubgoal.mvarId ((`case).appendIndexAfter i)
+    let thenMVarId ← hs.foldlM
+      (fun thenMVarId h => match thenSubgoal.subst.get h with
+        | Expr.fvar fvarId _ => tryClear thenMVarId fvarId
+        | _                  => pure thenMVarId)
+      thenSubgoal.mvarId
+    let subgoals := subgoals.push { mvarId := thenMVarId, newHs := #[thenSubgoal.newH], subst := thenSubgoal.subst }
+    match vs with
+    | [] => do
+      appendTagSuffix elseSubgoal.mvarId ((`case).appendIndexAfter (i+1))
+      pure $ subgoals.push { mvarId := elseSubgoal.mvarId, newHs := hs.push elseSubgoal.newH, subst := {} }
+    | _  => loop (i+1) elseSubgoal.mvarId vs (hs.push elseSubgoal.newH) subgoals
+loop 1 mvarId values.toList #[] #[]
 
-end Meta
-end Lean
+end Lean.Meta

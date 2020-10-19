86ab4d402982e1517291282d0736f8d56920c757
Leonardo de Moura <leonardo@microsoft.com>
2020-10-19 15:38:47 -0700

    chore: move to new frontend

--- src/Lean/Meta/AbstractNestedProofs.lean
+++ src/Lean/Meta/AbstractNestedProofs.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -5,13 +6,15 @@ Authors: Leonardo de Moura
 -/
 import Lean.Meta.Closure
 
-namespace Lean
-namespace Meta
+namespace Lean.Meta
 namespace AbstractNestedProofs
 
-def isNonTrivialProof (e : Expr) : MetaM Bool :=
-condM (not <$> isProof e) (pure false) $ e.withApp fun f args =>
-  pure $ !f.isAtomic || args.any fun arg => !arg.isAtomic
+def isNonTrivialProof (e : Expr) : MetaM Bool := do
+if ! (← isProof e) then
+  pure false
+else
+  e.withApp fun f args =>
+    pure $ !f.isAtomic || args.any fun arg => !arg.isAtomic
 
 structure Context :=
 (baseName : Name)
@@ -22,40 +25,40 @@ structure State :=
 abbrev M := ReaderT Context $ MonadCacheT Expr Expr $ StateRefT State $ MetaM
 
 private def mkAuxLemma (e : Expr) : M Expr := do
-ctx ← read;
-s ← get;
-lemmaName ← mkAuxName (ctx.baseName ++ `proof) s.nextIdx;
-modify fun s => { s with nextIdx := s.nextIdx + 1 };
+let ctx ← read
+let s ← get
+let lemmaName ← mkAuxName (ctx.baseName ++ `proof) s.nextIdx
+modify fun s => { s with nextIdx := s.nextIdx + 1 }
 mkAuxDefinitionFor lemmaName e
 
-partial def visit : Expr → M Expr
-| e =>
-  if e.isAtomic then pure e
-  else do
-    let visitBinders (xs : Array Expr) (k : M Expr) : M Expr := do {
-      localInstances ← getLocalInstances;
-      lctx ← getLCtx;
-      lctx ← xs.foldlM
-        (fun (lctx : LocalContext) x => do
-          let xFVarId := x.fvarId!;
-          localDecl ← getLocalDecl xFVarId;
-          type      ← visit localDecl.type;
-          let localDecl := localDecl.setType type;
-          localDecl ← match localDecl.value? with
-            | some value => do value ← visit value; pure $ localDecl.setValue value
-            | none       => pure localDecl;
-          pure $ lctx.modifyLocalDecl xFVarId fun _ => localDecl)
-        lctx;
-      withLCtx lctx localInstances k
-    };
-    checkCache e fun e => condM (liftM $ isNonTrivialProof e) (mkAuxLemma e) $ match e with
-    | Expr.lam _ _ _ _     => lambdaLetTelescope e fun xs b => visitBinders xs do b ← visit b; mkLambdaFVars xs b
-    | Expr.letE _ _ _ _ _  => lambdaLetTelescope e fun xs b => visitBinders xs do b ← visit b; mkLambdaFVars xs b
-    | Expr.forallE _ _ _ _ => forallTelescope e fun xs b => visitBinders xs do b ← visit b; mkForallFVars xs b
-    | Expr.mdata _ b _     => do b ← visit b; pure $ e.updateMData! b
-    | Expr.proj _ _ b _    => do b ← visit b; pure $ e.updateProj! b
-    | Expr.app _ _ _       => e.withApp fun f args => do args ← args.mapM visit; pure $ mkAppN f args
-    | _                    => pure e
+partial def visit (e : Expr) : M Expr := do
+if e.isAtomic then
+  pure e
+else
+  let visitBinders (xs : Array Expr) (k : M Expr) : M Expr := do
+    let localInstances ← getLocalInstances
+    let lctx ← getLCtx
+    for x in xs do
+      let xFVarId := x.fvarId!
+      let localDecl ← getLocalDecl xFVarId
+      let type      ← visit localDecl.type
+      let localDecl := localDecl.setType type
+      let localDecl ← match localDecl.value? with
+         | some value => do let value ← visit value; pure $ localDecl.setValue value
+         | none       => pure localDecl
+      lctx :=lctx.modifyLocalDecl xFVarId fun _ => localDecl
+    withLCtx lctx localInstances k
+  checkCache e fun e =>
+    if (← isNonTrivialProof e) then
+      mkAuxLemma e
+    else match e with
+      | Expr.lam _ _ _ _     => lambdaLetTelescope e fun xs b => visitBinders xs do mkLambdaFVars xs (← visit b)
+      | Expr.letE _ _ _ _ _  => lambdaLetTelescope e fun xs b => visitBinders xs do mkLambdaFVars xs (← visit b)
+      | Expr.forallE _ _ _ _ => forallTelescope e fun xs b => visitBinders xs do mkForallFVars xs (← visit b)
+      | Expr.mdata _ b _     => do pure $ e.updateMData! (← visit b)
+      | Expr.proj _ _ b _    => do pure $ e.updateProj! (← visit b)
+      | Expr.app _ _ _       => e.withApp fun f args => do pure $ mkAppN f (← args.mapM visit)
+      | _                    => pure e
 
 end AbstractNestedProofs
 
@@ -63,5 +66,4 @@ end AbstractNestedProofs
 def abstractNestedProofs (mainDeclName : Name) (e : Expr) : MetaM Expr :=
 (((AbstractNestedProofs.visit e).run { baseName := mainDeclName }).run).run' { nextIdx := 1 }
 
-end Meta
-end Lean
+end Lean.Meta

e53ff26a911f8e868294a18677f422d219a6e21a
Leonardo de Moura <leonardo@microsoft.com>
2020-10-20 12:18:05 -0700

    chore: move to new frontend

--- src/Lean/Util/FindMVar.lean
+++ src/Lean/Util/FindMVar.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -11,18 +12,22 @@ namespace FindMVar
 
 abbrev Visitor := Option MVarId → Option MVarId
 
-@[inline] def visit (f : Expr → Visitor) (e : Expr) : Visitor :=
-fun s => if s.isSome || !e.hasMVar then s else f e s
+mutual
+partial def visit (p : MVarId → Bool) (e : Expr) : Visitor :=
+fun s => if s.isSome || !e.hasMVar then s else main p e s
 
-@[specialize] partial def main (p : MVarId → Bool) : Expr → Visitor
-| Expr.proj _ _ e _    => visit main e
-| Expr.forallE _ d b _ => visit main b ∘ visit main d
-| Expr.lam _ d b _     => visit main b ∘ visit main d
-| Expr.letE _ t v b _  => visit main b ∘ visit main v ∘ visit main t
-| Expr.app f a _       => visit main a ∘ visit main f
-| Expr.mdata _ b _     => visit main b
+@[specialize]
+partial def main (p : MVarId → Bool) : Expr → Visitor
+| Expr.proj _ _ e _    => visit p e
+| Expr.forallE _ d b _ => visit p b ∘ visit p d
+| Expr.lam _ d b _     => visit p b ∘ visit p d
+| Expr.letE _ t v b _  => visit p b ∘ visit p v ∘ visit p t
+| Expr.app f a _       => visit p a ∘ visit p f
+| Expr.mdata _ b _     => visit p b
 | Expr.mvar mvarId _   => fun s => if s.isNone && p mvarId then some mvarId else s
 | _                    => id
+
+end
 
 end FindMVar
 

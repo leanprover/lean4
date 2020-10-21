4e74e36331ae5b161cda3ecf782ed1b9b8356292
Sebastian Ullrich <sebasti@nullri.ch>
2020-10-21 18:47:05 +0200

    feat: run initializers on import
    Also, refuse to evaluate an `[init]` decl in the same module (since we don't know whether the initialization is
    backtrackable) and always use native symbol of a `[builtinInit]` decl

---
+++ tests/lean/run/evalBuiltinInit.lean
@@ -0,0 +1,6 @@
+#lang lean4
+import Lean
+
+-- option should be ignored when evaluating a `[builtinInit]` decl
+set_option interpreter.prefer_native false
+#eval toString Lean.PrettyPrinter.formatterAttribute.defn.name

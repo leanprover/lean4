8abbf7634d9175bba4ed8cd1d9f9d787117f6903
Leonardo de Moura <leonardo@microsoft.com>
2020-10-21 14:16:41 -0700

    chore: move to new frontend

--- src/Lean/ParserCompiler/Attribute.lean
+++ src/Lean/ParserCompiler/Attribute.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Sebastian Ullrich. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -12,41 +13,41 @@ namespace Lean
 namespace ParserCompiler
 
 structure CombinatorAttribute :=
-(attr : AttributeImpl)
-(ext  : SimplePersistentEnvExtension (Name × Name) (NameMap Name))
+  (attr : AttributeImpl)
+  (ext  : SimplePersistentEnvExtension (Name × Name) (NameMap Name))
 
 -- TODO(Sebastian): We'll probably want priority support here at some point
 
 def registerCombinatorAttribute (name : Name) (descr : String)
     : IO CombinatorAttribute := do
-ext : SimplePersistentEnvExtension (Name × Name) (NameMap Name) ← registerSimplePersistentEnvExtension {
-  name            := name,
-  addImportedFn   := mkStateFromImportedEntries (fun s p => s.insert p.1 p.2) {},
-  addEntryFn      := fun (s : NameMap Name) (p : Name × Name) => s.insert p.1 p.2,
-};
-let attrImpl : AttributeImpl := {
-  name  := name,
-  descr := descr,
-  add   := fun decl args _ => do
-    env ← getEnv;
-    match attrParamSyntaxToIdentifier args with
-    | some parserDeclName => do
-      _ ← getConstInfo parserDeclName;
-      setEnv $ ext.addEntry env (parserDeclName, decl)
-    | none            => throwError $ "invalid [" ++ name ++ "] argument, expected identifier"
-};
-registerBuiltinAttribute attrImpl;
-pure { attr := attrImpl, ext := ext }
+  let ext : SimplePersistentEnvExtension (Name × Name) (NameMap Name) ← registerSimplePersistentEnvExtension {
+    name            := name,
+    addImportedFn   := mkStateFromImportedEntries (fun s p => s.insert p.1 p.2) {},
+    addEntryFn      := fun (s : NameMap Name) (p : Name × Name) => s.insert p.1 p.2,
+  }
+  let attrImpl : AttributeImpl := {
+    name  := name,
+    descr := descr,
+    add   := fun decl args _ => do
+      let env ← getEnv
+      match attrParamSyntaxToIdentifier args with
+      | some parserDeclName => do
+        getConstInfo parserDeclName
+        setEnv $ ext.addEntry env (parserDeclName, decl)
+      | none            => throwError $ "invalid [" ++ name ++ "] argument, expected identifier"
+  }
+  registerBuiltinAttribute attrImpl
+  pure { attr := attrImpl, ext := ext }
 
 namespace CombinatorAttribute
 
 instance : Inhabited CombinatorAttribute := ⟨{attr := arbitrary _, ext := arbitrary _}⟩
 
 def getDeclFor (attr : CombinatorAttribute) (env : Environment) (parserDecl : Name) : Option Name :=
-(attr.ext.getState env).find? parserDecl
+  (attr.ext.getState env).find? parserDecl
 
 def setDeclFor (attr : CombinatorAttribute) (env : Environment) (parserDecl : Name) (decl : Name) : Environment :=
-attr.ext.addEntry env (parserDecl, decl)
+  attr.ext.addEntry env (parserDecl, decl)
 
 end CombinatorAttribute
 

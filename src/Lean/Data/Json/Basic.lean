0c89dca20e761a06eba595d20c4790d29a1332ac
Leonardo de Moura <leonardo@microsoft.com>
2020-10-21 17:09:24 -0700

    chore: move to new frontend

--- src/Lean/Data/Json/Basic.lean
+++ src/Lean/Data/Json/Basic.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2019 Gabriel Ebner. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -5,80 +6,80 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gabriel Ebner, Marc Huisinga
 -/
 import Std.Data.RBTree
-namespace Std end Std -- Hack for old frontend
-
 namespace Lean
 
 -- mantissa * 10^-exponent
 structure JsonNumber :=
-(mantissa : Int) (exponent : Nat)
+  (mantissa : Int) (exponent : Nat)
 
-protected def JsonNumber.decEq : ∀ (a b : JsonNumber), Decidable (a = b)
-| ⟨m1, e1⟩, ⟨m2, e2⟩ =>
-  match decEq m1 m2 with
-  | isTrue hm =>
-    match decEq e1 e2 with
-    | isTrue he  => isTrue (hm ▸ he ▸ rfl)
-    | isFalse he => isFalse (fun h => JsonNumber.noConfusion h (fun hm he2 => he he2))
-  | isFalse hm => isFalse (fun h => JsonNumber.noConfusion h (fun hm2 he => hm hm2))
+protected def JsonNumber.decEq : (a b : JsonNumber) → Decidable (a = b)
+  | ⟨m1, e1⟩, ⟨m2, e2⟩ =>
+    match decEq m1 m2 with
+    | isTrue hm =>
+      match decEq e1 e2 with
+      | isTrue he  => isTrue $ by rw [hm, he]; exact rfl
+      | isFalse he => isFalse (fun h => JsonNumber.noConfusion h (fun hm he2 => he he2))
+    | isFalse hm => isFalse (fun h => JsonNumber.noConfusion h (fun hm2 he => hm hm2))
 
-instance JsonNumber.decidableEq : DecidableEq JsonNumber :=
-JsonNumber.decEq
+instance : DecidableEq JsonNumber := JsonNumber.decEq
 
 namespace JsonNumber
 
 protected def fromNat (n : Nat) : JsonNumber := ⟨n, 0⟩
 protected def fromInt (n : Int) : JsonNumber := ⟨n, 0⟩
 
-instance natToJsonNumber : HasCoe Nat JsonNumber := ⟨JsonNumber.fromNat⟩
-instance intToJsonNumber : HasCoe Int JsonNumber := ⟨JsonNumber.fromInt⟩
+-- TODO: delete
+instance : HasCoe Nat JsonNumber := ⟨JsonNumber.fromNat⟩
+instance : HasCoe Int JsonNumber := ⟨JsonNumber.fromInt⟩
 
-private partial def countDigitsAux : Nat → Nat → Nat | n, digits =>
-if n ≤ 9 then
-  digits
-else
-  countDigitsAux (n/10) (digits+1)
+instance : Coe Nat JsonNumber := ⟨JsonNumber.fromNat⟩
+instance : Coe Int JsonNumber := ⟨JsonNumber.fromInt⟩
+
+private partial def countDigitsAux (n digits : Nat) : Nat :=
+  if n ≤ 9 then
+    digits
+  else
+    countDigitsAux (n/10) (digits+1)
 
 private def countDigits (n : Nat) : Nat :=
-countDigitsAux n 1
+  countDigitsAux n 1
 
 protected def toString : JsonNumber → String
-| ⟨m, 0⟩ => m.repr
-| ⟨m, e⟩ =>
-  let s : Bool := m ≥ 0;
-  let m := m.natAbs;
-  -- if there are too many zeroes after the decimal, we
-  -- use exponents to compress the representation.
-  -- this is mostly done for memory usage reasons:
-  -- the size of the representation would otherwise
-  -- grow exponentially in the value of exponent.
-  let exp := 9 + (countDigits m : Int) - (e : Int);
-  let exp := if exp < 0 then exp else 0;
-  let f := 10 ^ (e - exp.natAbs);
-  let left := m / f;
-  let right := (f : Int) + m % f;
-  let rightUntrimmed := right.repr.mkIterator.next.remainingToString;
-  (if s then "" else "-") ++
-  left.repr ++ "." ++
-  (rightUntrimmed.toSubstring.dropRightWhile (fun c => c = '0')).toString ++
-  (if exp = 0 then "" else "e" ++ exp.repr)
+  | ⟨m, 0⟩ => m.repr
+  | ⟨m, e⟩ =>
+    let s : Bool := m ≥ 0
+    let m := m.natAbs
+    -- if there are too many zeroes after the decimal, we
+    -- use exponents to compress the representation.
+    -- this is mostly done for memory usage reasons:
+    -- the size of the representation would otherwise
+    -- grow exponentially in the value of exponent.
+    let exp := 9 + (countDigits m : Int) - (e : Int)
+    let exp := if exp < 0 then exp else 0
+    let f := 10 ^ (e - exp.natAbs)
+    let left := m / f
+    let right := (f : Int) + m % f
+    let rightUntrimmed := right.repr.mkIterator.next.remainingToString
+    (if s then "" else "-") ++
+    left.repr ++ "." ++
+    (rightUntrimmed.toSubstring.dropRightWhile (fun c => c = '0')).toString ++
+    (if exp = 0 then "" else "e" ++ exp.repr)
 
 -- shift a JsonNumber by a specified amount of places to the left
 protected def shiftl : JsonNumber → Nat → JsonNumber
--- if s ≤ e, then 10 ^ (s - e) = 1, and hence the mantissa remains unchanged.
--- otherwise, the expression pads the mantissa with zeroes
--- to accomodate for the remaining places to shift.
-| ⟨m, e⟩, s => ⟨m * (10 ^ (s - e) : Nat), e - s⟩
+  -- if s ≤ e, then 10 ^ (s - e) = 1, and hence the mantissa remains unchanged.
+  -- otherwise, the expression pads the mantissa with zeroes
+  -- to accomodate for the remaining places to shift.
+  | ⟨m, e⟩, s => ⟨m * (10 ^ (s - e) : Nat), e - s⟩
 
 -- shift a JsonNumber by a specified amount of places to the right
 protected def shiftr : JsonNumber → Nat → JsonNumber
-| ⟨m, e⟩, s => ⟨m, e + s⟩
+  | ⟨m, e⟩, s => ⟨m, e + s⟩
 
-instance jsonNumberToString : HasToString JsonNumber :=
-⟨JsonNumber.toString⟩
+instance : HasToString JsonNumber := ⟨JsonNumber.toString⟩
 
-instance jsonNumberHasRepr : HasRepr JsonNumber :=
-⟨fun ⟨m, e⟩ => "⟨" ++ m.repr ++ "," ++ e.repr ++ "⟩"⟩
+instance : HasRepr JsonNumber :=
+  ⟨fun ⟨m, e⟩ => "⟨" ++ m.repr ++ "," ++ e.repr ++ "⟩"⟩
 
 end JsonNumber
 
@@ -87,74 +88,82 @@ def strLt (a b : String) := Decidable.decide (a < b)
 open Std (RBNode RBNode.leaf)
 
 inductive Json
-| null
-| bool (b : Bool)
-| num (n : JsonNumber)
-| str (s : String)
-| arr (elems : Array Json)
--- uses RBNode instead of RBMap because RBMap is a def
--- and thus currently cannot be used to define a type that
--- is recursive in one of its parameters
-| obj (kvPairs : RBNode String (fun _ => Json))
+  | null
+  | bool (b : Bool)
+  | num (n : JsonNumber)
+  | str (s : String)
+  | arr (elems : Array Json)
+  -- uses RBNode instead of RBMap because RBMap is a def
+  -- and thus currently cannot be used to define a type that
+  -- is recursive in one of its parameters
+  | obj (kvPairs : RBNode String (fun _ => Json))
 
 namespace Json
 
 -- HACK(Marc): temporary ugliness until we can use RBMap for JSON objects
 def mkObj (o : List (String × Json)) : Json :=
-obj (o.foldr (fun ⟨k, v⟩ acc => acc.insert strLt k v) RBNode.leaf)
+  obj (o.foldr (fun ⟨k, v⟩ acc => acc.insert strLt k v) RBNode.leaf)
 
-instance natToJson : HasCoe Nat Json := ⟨fun n => Json.num n⟩
-instance intToJson : HasCoe Int Json := ⟨fun n => Json.num n⟩
-instance stringToJson : HasCoe String Json := ⟨Json.str⟩
-instance boolToJson : HasCoe Bool Json := ⟨Json.bool⟩
+-- TODO delete
+instance : HasCoe Nat Json := ⟨fun n => Json.num n⟩
+instance : HasCoe Int Json := ⟨fun n => Json.num n⟩
+instance : HasCoe String Json := ⟨Json.str⟩
+instance : HasCoe Bool Json := ⟨Json.bool⟩
+
+instance : Coe Nat Json := ⟨fun n => Json.num n⟩
+instance : Coe Int Json := ⟨fun n => Json.num n⟩
+instance : Coe String Json := ⟨Json.str⟩
+instance : Coe Bool Json := ⟨Json.bool⟩
 
 def getObj? : Json → Option (RBNode String (fun _ => Json))
-| obj kvs => kvs
-| _       => none
+  | obj kvs => kvs
+  | _       => none
 
 def getArr? : Json → Option (Array Json)
-| arr a => a
-| _     => none
+  | arr a => a
+  | _     => none
 
 def getStr? : Json → Option String
-| str s => some s
-| _     => none
+  | str s => some s
+  | _     => none
 
 def getNat? : Json → Option Nat
-| (n : Nat) => some n
-| _         => none
+  | (n : Nat) => some n
+  | _         => none
 
 def getInt? : Json → Option Int
-| (i : Int) => some i
-| _         => none
+  | (i : Int) => some i
+  | _         => none
 
 def getBool? : Json → Option Bool
-| (b : Bool) => some b
-| _          => none
+  | (b : Bool) => some b
+  | _          => none
 
 def getNum? : Json → Option JsonNumber
-| num n => n
-| _     => none
+  | num n => n
+  | _     => none
 
 def getObjVal? : Json → String → Option Json
-| obj kvs, k => kvs.find strLt k
-| _      , _ => none
+  | obj kvs, k => kvs.find strLt k
+  | _      , _ => none
 
 def getArrVal? : Json → Nat → Option Json
-| arr a, i => a.get? i
-| _    , _ => none
+  | arr a, i => a.get? i
+  | _    , _ => none
 
 def getObjValD (j : Json) (k : String) : Json :=
-(j.getObjVal? k).getD null
+  (j.getObjVal? k).getD null
 
 inductive Structured
-| arr (elems : Array Json)
-| obj (kvPairs : RBNode String (fun _ => Json))
+  | arr (elems : Array Json)
+  | obj (kvPairs : RBNode String (fun _ => Json))
 
-instance arrayToStructured : HasCoe (Array Json) Structured :=
-⟨Structured.arr⟩
-instance kvPairsToStructured : HasCoe (RBNode String (fun _ => Json)) Structured :=
-⟨Structured.obj⟩
+-- TODO: delete
+instance : HasCoe (Array Json) Structured := ⟨Structured.arr⟩
+instance : HasCoe (RBNode String (fun _ => Json)) Structured := ⟨Structured.obj⟩
+
+instance : Coe (Array Json) Structured := ⟨Structured.arr⟩
+instance : Coe (RBNode String (fun _ => Json)) Structured := ⟨Structured.obj⟩
 
 end Json
 end Lean
--- src/Lean/Data/JsonRpc.lean
+++ src/Lean/Data/JsonRpc.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2020 Marc Huisinga. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -19,86 +20,90 @@ open Json
 open Std (RBNode)
 
 inductive RequestID
-| str (s : String)
-| num (n : JsonNumber)
-| null
+  | str (s : String)
+  | num (n : JsonNumber)
+  | null
 
 /-- Error codes defined by JSON-RPC and LSP. -/
 inductive ErrorCode
-| parseError
-| invalidRequest
-| methodNotFound
-| invalidParams
-| internalError
-| serverErrorStart
-| serverErrorEnd
-| serverNotInitialized
-| unknownErrorCode
--- LSP-specific codes below.
-| requestCancelled
-| contentModified
+  | parseError
+  | invalidRequest
+  | methodNotFound
+  | invalidParams
+  | internalError
+  | serverErrorStart
+  | serverErrorEnd
+  | serverNotInitialized
+  | unknownErrorCode
+  -- LSP-specific codes below.
+  | requestCancelled
+  | contentModified
 
-instance ErrorCode.hasFromJson : HasFromJson ErrorCode :=
-⟨fun j => match j with
-  | num (-32700 : Int) => ErrorCode.parseError
-  | num (-32600 : Int) => ErrorCode.invalidRequest
-  | num (-32601 : Int) => ErrorCode.methodNotFound
-  | num (-32602 : Int) => ErrorCode.invalidParams
-  | num (-32603 : Int) => ErrorCode.internalError
-  | num (-32099 : Int) => ErrorCode.serverErrorStart
-  | num (-32000 : Int) => ErrorCode.serverErrorEnd
-  | num (-32002 : Int) => ErrorCode.serverNotInitialized
-  | num (-32001 : Int) => ErrorCode.unknownErrorCode
-  | num (-32800 : Int) => ErrorCode.requestCancelled
-  | num (-32801 : Int) => ErrorCode.contentModified
-  | _                  => none⟩
+instance : HasFromJson ErrorCode := ⟨fun j =>
+  match j with
+  | num v =>
+    if v == (0-32700 : Int) then ErrorCode.parseError
+    else if v == (0-32600 : Int) then ErrorCode.invalidRequest
+    else if v == (0-32601 : Int) then ErrorCode.methodNotFound
+    else if v == (0-32602 : Int) then ErrorCode.invalidParams
+    else if v == (0-32603 : Int) then ErrorCode.internalError
+    else if v == (0-32099 : Int) then ErrorCode.serverErrorStart
+    else if v == (0-32000 : Int) then ErrorCode.serverErrorEnd
+    else if v == (0-32002 : Int) then ErrorCode.serverNotInitialized
+    else if v == (0-32001 : Int) then ErrorCode.unknownErrorCode
+    else if v == (0-32800 : Int) then ErrorCode.requestCancelled
+    else if v == (0-32801 : Int) then ErrorCode.contentModified
+    else none
+  | _  => none⟩
 
-instance ErrorCode.hasToJson : HasToJson ErrorCode :=
-⟨fun e => match e with
-  | ErrorCode.parseError           => (-32700 : Int)
-  | ErrorCode.invalidRequest       => (-32600 : Int)
-  | ErrorCode.methodNotFound       => (-32601 : Int)
-  | ErrorCode.invalidParams        => (-32602 : Int)
-  | ErrorCode.internalError        => (-32603 : Int)
-  | ErrorCode.serverErrorStart     => (-32099 : Int)
-  | ErrorCode.serverErrorEnd       => (-32000 : Int)
-  | ErrorCode.serverNotInitialized => (-32002 : Int)
-  | ErrorCode.unknownErrorCode     => (-32001 : Int)
-  | ErrorCode.requestCancelled     => (-32800 : Int)
-  | ErrorCode.contentModified      => (-32801 : Int)⟩
+instance : HasToJson ErrorCode := ⟨fun e =>
+  match e with
+  | ErrorCode.parseError           => (0-32700 : Int)
+  | ErrorCode.invalidRequest       => (0-32600 : Int)
+  | ErrorCode.methodNotFound       => (0-32601 : Int)
+  | ErrorCode.invalidParams        => (0-32602 : Int)
+  | ErrorCode.internalError        => (0-32603 : Int)
+  | ErrorCode.serverErrorStart     => (0-32099 : Int)
+  | ErrorCode.serverErrorEnd       => (0-32000 : Int)
+  | ErrorCode.serverNotInitialized => (0-32002 : Int)
+  | ErrorCode.unknownErrorCode     => (0-32001 : Int)
+  | ErrorCode.requestCancelled     => (0-32800 : Int)
+  | ErrorCode.contentModified      => (0-32801 : Int)⟩
 
 /- Uses separate constructors for notifications and errors because client and server
 behavior is expected to be wildly different for both. -/
 inductive Message
-| request (id : RequestID) (method : String) (params? : Option Structured)
-| notification (method : String) (params? : Option Structured)
-| response (id : RequestID) (result : Json)
-| responseError (id : RequestID) (code : ErrorCode) (message : String) (data? : Option Json)
+  | request (id : RequestID) (method : String) (params? : Option Structured)
+  | notification (method : String) (params? : Option Structured)
+  | response (id : RequestID) (result : Json)
+  | responseError (id : RequestID) (code : ErrorCode) (message : String) (data? : Option Json)
 
 def Batch := Array Message
 
 -- data types for reading expected messages
-structure Request (α : Type*) := (id : RequestID) (param : α)
-structure Response (α : Type*) := (id : RequestID) (result : α)
+structure Request (α : Type) := (id : RequestID) (param : α)
+structure Response (α : Type) := (id : RequestID) (result : α)
 structure Error := (id : RequestID) (code : JsonNumber) (message : String) (data? : Option Json)
 
-instance stringToRequestID : HasCoe String RequestID := ⟨RequestID.str⟩
-instance numToRequestID : HasCoe JsonNumber RequestID := ⟨RequestID.num⟩
+instance : HasCoe String RequestID := ⟨RequestID.str⟩
+instance : HasCoe JsonNumber RequestID := ⟨RequestID.num⟩
 
-instance RequestId.hasFromJson : HasFromJson RequestID :=
-⟨fun j => match j with
+instance : Coe String RequestID := ⟨RequestID.str⟩
+instance : Coe JsonNumber RequestID := ⟨RequestID.num⟩
+
+instance : HasFromJson RequestID := ⟨fun j =>
+  match j with
   | str s => RequestID.str s
   | num n => RequestID.num n
   | _     => none⟩
 
-instance RequestID.hasToJson : HasToJson RequestID :=
-⟨fun rid => match rid with
+instance : HasToJson RequestID := ⟨fun rid =>
+  match rid with
   | RequestID.str s => s
   | RequestID.num n => num n
   | RequestID.null  => null⟩
 
-instance Message.hasToJson : HasToJson Message :=
-⟨fun m =>
+instance : HasToJson Message := ⟨fun m =>
   mkObj $ ⟨"jsonrpc", "2.0"⟩ :: match m with
   | Message.request id method params? =>
     [ ⟨"id", toJson id⟩,
@@ -119,34 +124,33 @@ instance Message.hasToJson : HasToJson Message :=
     ]⟩
 
 def aux1 (j : Json) : Option Message := do
-id ← j.getObjValAs? RequestID "id";
-method ← j.getObjValAs? String "method";
-let params? := j.getObjValAs? Structured "params";
-pure (Message.request id method params?)
+  let id ← j.getObjValAs? RequestID "id"
+  let method ← j.getObjValAs? String "method"
+  let params? := j.getObjValAs? Structured "params"
+  pure (Message.request id method params?)
 
 def aux2 (j : Json) : Option Message := do
-method ← j.getObjValAs? String "method";
-let params? := j.getObjValAs? Structured "params";
-pure (Message.notification method params?)
+  let method ← j.getObjValAs? String "method"
+  let params? := j.getObjValAs? Structured "params"
+  pure (Message.notification method params?)
 
 def aux3 (j : Json) : Option Message := do
-id ← j.getObjValAs? RequestID "id";
-result ← j.getObjVal? "result";
-pure (Message.response id result)
+  let id ← j.getObjValAs? RequestID "id"
+  let result ← j.getObjVal? "result"
+  pure (Message.response id result)
 
 def aux4 (j : Json) : Option Message := do
-id ← j.getObjValAs? RequestID "id";
-err ← j.getObjVal? "error";
-code ← err.getObjValAs? ErrorCode "code";
-message ← err.getObjValAs? String "message";
-let data? := err.getObjVal? "data";
-pure (Message.responseError id code message data?)
+  let id ← j.getObjValAs? RequestID "id"
+  let err ← j.getObjVal? "error"
+  let code ← err.getObjValAs? ErrorCode "code"
+  let message ← err.getObjValAs? String "message"
+  let data? := err.getObjVal? "data";
+  pure (Message.responseError id code message data?)
 
 -- HACK: The implementation must be made up of several `auxN`s instead
 -- of one large block because of a bug in the compiler.
-instance Message.hasFromJson : HasFromJson Message :=
-⟨fun j => do
-  "2.0" ← j.getObjVal? "jsonrpc" | none;
+instance Message.hasFromJson : HasFromJson Message := ⟨fun j => do
+  let "2.0" ← j.getObjVal? "jsonrpc" | none
   aux1 j <|> aux2 j <|> aux3 j <|> aux4 j⟩
 
 end JsonRpc
@@ -159,47 +163,47 @@ open Lean.JsonRpc
 open IO
 
 def readMessage (h : FS.Stream) (nBytes : Nat) : IO Message := do
-j ← h.readJson nBytes;
-match fromJson? j with
-| some m => pure m
-| none   => throw (userError ("JSON '" ++ j.compress ++ "' did not have the format of a JSON-RPC message"))
+  let j ← h.readJson nBytes
+  match fromJson? j with
+  | some m => pure m
+  | none   => throw (userError ("JSON '" ++ j.compress ++ "' did not have the format of a JSON-RPC message"))
 
-def readRequestAs (h : FS.Stream) (nBytes : Nat) (expectedMethod : String) (α : Type*) [HasFromJson α] : IO (Request α) := do
-m ← h.readMessage nBytes;
-match m with
-| Message.request id method params? =>
-  if method = expectedMethod then
-    match params? with
-    | some params =>
-      let j := toJson params;
-      match fromJson? j with
-      | some v => pure ⟨id, v⟩
-      | none   => throw (userError ("unexpected param '" ++ j.compress  ++ "' for method '" ++ expectedMethod ++ "'"))
-    | none => throw (userError ("unexpected lack of param for method '" ++ expectedMethod ++ "'"))
-  else
-    throw (userError ("expected method '" ++ expectedMethod ++ "', got method '" ++ method ++ "'"))
-| _ => throw (userError "expected request, got other type of message")
+def readRequestAs (h : FS.Stream) (nBytes : Nat) (expectedMethod : String) (α : Type) [HasFromJson α] : IO (Request α) := do
+  let m ← h.readMessage nBytes
+  match m with
+  | Message.request id method params? =>
+    if method = expectedMethod then
+      match params? with
+      | some params =>
+        let j := toJson params
+        match fromJson? j with
+        | some v => pure ⟨id, v⟩
+        | none   => throw (userError ("unexpected param '" ++ j.compress  ++ "' for method '" ++ expectedMethod ++ "'"))
+      | none => throw (userError ("unexpected lack of param for method '" ++ expectedMethod ++ "'"))
+    else
+      throw (userError ("expected method '" ++ expectedMethod ++ "', got method '" ++ method ++ "'"))
+  | _ => throw (userError "expected request, got other type of message")
 
-def readNotificationAs (h : FS.Stream) (nBytes : Nat) (expectedMethod : String) (α : Type*) [HasFromJson α] : IO α := do
-m ← h.readMessage nBytes;
-match m with
-| Message.notification method params? =>
-  if method = expectedMethod then
-    match params? with
-    | some params =>
-      let j := toJson params;
-      match fromJson? j with
-      | some v => pure v
-      | none   => throw (userError ("unexpected param '" ++ j.compress  ++ "' for method '" ++ expectedMethod ++ "'"))
-    | none => throw (userError ("unexpected lack of param for method '" ++ expectedMethod ++ "'"))
-  else
-    throw (userError ("expected method '" ++ expectedMethod ++ "', got method '" ++ method ++ "'"))
-| _ => throw (userError "expected notification, got other type of message")
+def readNotificationAs (h : FS.Stream) (nBytes : Nat) (expectedMethod : String) (α : Type) [HasFromJson α] : IO α := do
+  let m ← h.readMessage nBytes
+  match m with
+  | Message.notification method params? =>
+    if method = expectedMethod then
+      match params? with
+      | some params =>
+        let j := toJson params;
+        match fromJson? j with
+        | some v => pure v
+        | none   => throw (userError ("unexpected param '" ++ j.compress  ++ "' for method '" ++ expectedMethod ++ "'"))
+      | none => throw (userError ("unexpected lack of param for method '" ++ expectedMethod ++ "'"))
+    else
+      throw (userError ("expected method '" ++ expectedMethod ++ "', got method '" ++ method ++ "'"))
+  | _ => throw (userError "expected notification, got other type of message")
 
 def writeMessage (h : FS.Stream) (m : Message) : IO Unit :=
-h.writeJson (toJson m)
+  h.writeJson (toJson m)
 
-def writeResponse {α : Type*} [HasToJson α] (h : FS.Stream) (id : RequestID) (r : α) : IO Unit :=
-h.writeMessage (Message.response id (toJson r))
+def writeResponse {α} [HasToJson α] (h : FS.Stream) (id : RequestID) (r : α) : IO Unit :=
+  h.writeMessage (Message.response id (toJson r))
 
 end IO.FS.Stream

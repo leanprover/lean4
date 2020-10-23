40578d6d24faad4399ab16ee0cf3f5c36a2e83d5
Leonardo de Moura <leonardo@microsoft.com>
2020-10-23 06:00:51 -0700

    test: add explicit motive test

---
+++ tests/lean/run/explicitMotive.lean
@@ -0,0 +1,10 @@
+#lang lean4
+
+def f1 (ex : Empty) {α : Type} : α :=
+Empty.rec (motive := fun _ => α) ex
+
+def f2 (a b : Nat) (h₁ : b = a) (h₂ : a + b = b) : a + a = b :=
+Eq.rec (motive := fun x _ => a + x = b) h₂ h₁
+
+def f3 (a b : Nat) (h₁ : b = a) (h₂ : a + b = b) : a + a = b :=
+Eq.recOn (motive := fun x _ => a + x = b) h₁ h₂

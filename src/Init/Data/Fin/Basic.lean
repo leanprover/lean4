3757b26dc28fa72dbe0558e67c07dc2bae94fd74
Leonardo de Moura <leonardo@microsoft.com>
2020-10-23 17:30:48 -0700

    chore: move to new frontend

--- src/Init/Data/Fin/Basic.lean
+++ src/Init/Data/Fin/Basic.lean
@@ -1,3 +1,4 @@
+#lang lean4
 /-
 Copyright (c) 2016 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
@@ -9,53 +10,55 @@ import Init.Data.Nat.Bitwise
 import Init.Coe
 
 open Nat
-structure Fin (n : Nat) := (val : Nat) (isLt : val < n)
+structure Fin (n : Nat) :=
+  (val : Nat)
+  (isLt : val < n)
 
 namespace Fin
 
 instance coeToNat {n} : Coe (Fin n) Nat :=
-⟨fun v => v.val⟩
+  ⟨fun v => v.val⟩
 
 protected def lt {n} (a b : Fin n) : Prop :=
-a.val < b.val
+  a.val < b.val
 
 protected def le {n} (a b : Fin n) : Prop :=
-a.val ≤ b.val
+  a.val ≤ b.val
 
 instance {n} : HasLess (Fin n)    := ⟨Fin.lt⟩
 instance {n} : HasLessEq (Fin n)  := ⟨Fin.le⟩
 
 instance decLt {n} (a b : Fin n) :  Decidable (a < b) :=
-Nat.decLt _ _
+  Nat.decLt ..
 
 instance decLe {n} (a b : Fin n) : Decidable (a ≤ b) :=
-Nat.decLe _ _
+  Nat.decLe ..
 
 def elim0.{u} {α : Sort u} : Fin 0 → α
-| ⟨_, h⟩ => absurd h (notLtZero _)
+  | ⟨_, h⟩ => absurd h (notLtZero _)
 
 variable {n : Nat}
 
 def ofNat {n : Nat} (a : Nat) : Fin (succ n) :=
-⟨a % succ n, Nat.modLt _ (Nat.zeroLtSucc _)⟩
+  ⟨a % succ n, Nat.modLt _ (Nat.zeroLtSucc _)⟩
 
 def ofNat' {n : Nat} (a : Nat) (h : n > 0) : Fin n :=
-⟨a % n, Nat.modLt _ h⟩
+  ⟨a % n, Nat.modLt _ h⟩
 
-private theorem mlt {n b : Nat} : ∀ {a}, n > a → b % n < n
-| 0,   h => Nat.modLt _ h
-| a+1, h =>
-  have n > 0 from Nat.ltTrans (Nat.zeroLtSucc _) h;
-  Nat.modLt _ this
+private theorem mlt {b : Nat} : {a : Nat} → a < n → b % n < n
+  | 0,   h => Nat.modLt _ h
+  | a+1, h =>
+    have n > 0 from Nat.ltTrans (Nat.zeroLtSucc _) h;
+    Nat.modLt _ this
 
 protected def add : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(a + b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(a + b) % n, mlt h⟩
 
 protected def mul : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(a * b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(a * b) % n, mlt h⟩
 
 protected def sub : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(a + (n - b)) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(a + (n - b)) % n, mlt h⟩
 
 /-
 Remark: mod/div/modn/land/lor can be defined without using (% n), but
@@ -64,19 +67,19 @@ needed to boostrap Lean.
 -/
 
 protected def mod : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(a % b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(a % b) % n, mlt h⟩
 
 protected def div : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(a / b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(a / b) % n, mlt h⟩
 
 protected def modn : Fin n → Nat → Fin n
-| ⟨a, h⟩, m => ⟨(a % m) % n, mlt h⟩
+  | ⟨a, h⟩, m => ⟨(a % m) % n, mlt h⟩
 
 def land : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(Nat.land a b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(Nat.land a b) % n, mlt h⟩
 
 def lor : Fin n → Fin n → Fin n
-| ⟨a, h⟩, ⟨b, _⟩ => ⟨(Nat.lor a b) % n, mlt h⟩
+  | ⟨a, h⟩, ⟨b, _⟩ => ⟨(Nat.lor a b) % n, mlt h⟩
 
 instance : HasZero (Fin (succ n)) := ⟨⟨0, succPos n⟩⟩
 instance : HasOne (Fin (succ n))  := ⟨ofNat 1⟩
@@ -88,23 +91,23 @@ instance : HasDiv (Fin n)         := ⟨Fin.div⟩
 instance : HasModN (Fin n)        := ⟨Fin.modn⟩
 
 theorem eqOfVeq : ∀ {i j : Fin n}, (val i) = (val j) → i = j
-| ⟨iv, ilt₁⟩, ⟨.(iv), ilt₂⟩, rfl => rfl
+  | ⟨iv, ilt₁⟩, ⟨_, _⟩, rfl => rfl
 
 theorem veqOfEq : ∀ {i j : Fin n}, i = j → (val i) = (val j)
-| ⟨iv, ilt⟩, .(_), rfl => rfl
+  | ⟨iv, ilt⟩, _, rfl => rfl
 
 theorem neOfVne {i j : Fin n} (h : val i ≠ val j) : i ≠ j :=
-fun h' => absurd (veqOfEq h') h
+  fun h' => absurd (veqOfEq h') h
 
 theorem vneOfNe {i j : Fin n} (h : i ≠ j) : val i ≠ val j :=
-fun h' => absurd (eqOfVeq h') h
+  fun h' => absurd (eqOfVeq h') h
 
 theorem modnLt : ∀ {m : Nat} (i : Fin n), m > 0 → (i %ₙ m).val < m
-| m, ⟨a, h⟩, hp =>  Nat.ltOfLeOfLt (modLe _ _) (modLt _ hp)
+  | m, ⟨a, h⟩, hp =>  Nat.ltOfLeOfLt (modLe _ _) (modLt _ hp)
 
 end Fin
 
 open Fin
 
-instance (n : Nat) : DecidableEq (Fin n) :=
-fun i j => decidableOfDecidableOfIff (decEq i.val j.val) ⟨eqOfVeq, veqOfEq⟩
+instance (n : Nat) : DecidableEq (Fin n) := fun i j =>
+  decidableOfDecidableOfIff (decEq i.val j.val) ⟨eqOfVeq, veqOfEq⟩

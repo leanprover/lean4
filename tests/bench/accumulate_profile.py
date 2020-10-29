72fc3f6679bf932774207863deaade6464b21bb2
Sebastian Ullrich <sebasti@nullri.ch>
2020-10-29 10:05:07 +0100

    feat: separate benchmark for profiling the stdlib per-file

--- tests/bench/accumulate_profile.py
+++ tests/bench/accumulate_profile.py
@@ -10,4 +10,7 @@ for m in re.findall("^([^\n\d]+)([\d.]+)(m?)s$", data, re.MULTILINE):
     cats[m[0].strip()] += float(m[1]) * (1e-3 if m[2] else 1)
 
 for cat in sorted(cats.keys()):
-    print(f"{cat!r}: {cats[cat]}")
+    cat2 = cat
+    if len(sys.argv) > 1:
+        cat2 = f"{sys.argv[1]} {cat}"
+    print(f"{cat2!r}: {cats[cat]:f}")

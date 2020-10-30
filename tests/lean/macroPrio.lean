672436bc5f12482def03df5962c41b2825240379
Leonardo de Moura <leonardo@microsoft.com>
2020-10-29 20:33:51 -0700

    feat: allow user to assign parsing priorities in the `macro` and `elab` commands

---
+++ tests/lean/macroPrio.lean
@@ -0,0 +1,27 @@
+import Lean
+
+macro "foo!" x:term:max : term => `($x + 1)
+
+#check foo! 0
+
+theorem ex1 : foo! 2 = 3 :=
+  rfl
+
+macro "foo!" x:term:max : term => `($x * 2)
+
+#check foo! 1 -- ambiguous
+
+-- macro with higher priority
+macro [1] "foo!" x:term:max : term => `($x - 2)
+
+#check foo! 2
+
+theorem ex2 : foo! 2 = 0 :=
+  rfl
+
+-- Define elaborator with even higher priority
+elab [2] "foo!" x:term:max : term <= expectedType =>
+  Lean.Elab.Term.elabTerm x expectedType
+
+theorem ex3 : foo! 3 = 3 :=
+  rfl

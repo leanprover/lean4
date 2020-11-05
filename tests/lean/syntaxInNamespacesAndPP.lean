2ea9d894e7c1509a1eb9e9b0c3b4599d134f5505
Leonardo de Moura <leonardo@microsoft.com>
2020-11-05 15:31:50 -0800

    fix: `SyntaxNodeKind` for `syntax` declarations nested in namespaces
    cc @Kha

---
+++ tests/lean/syntaxInNamespacesAndPP.lean
@@ -0,0 +1,25 @@
+namespace Foo
+
+syntax "foo" term : term
+
+macro_rules
+  | `(foo $x) => x
+
+set_option trace.Elab true in
+#check foo true
+
+end Foo
+
+namespace Bla
+
+syntax[bla] "bla" term : term
+
+macro_rules
+  | `(bla $x) => x
+
+set_option trace.Elab true in
+#check bla true
+
+#print Bla.bla
+
+end Bla

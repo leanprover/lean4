a31d9d3369ba14a5fad3185ad52ca56e1df75f32
Sebastian Ullrich <sebasti@nullri.ch>
2020-11-13 19:57:06 +0100

    chore: remove broken & redundant "new parser Core.lean" benchmark

--- tests/bench/parser.lean
+++
@@ -1,8 +0,0 @@
-import Lean.Parser
-
-def main : List String → IO Unit
-| [fname, n] => do
-  let env ← Lean.mkEmptyEnvironment
-  for _ in [0:n.toNat!] do
-    discard $ Lean.Parser.parseFile env fname
-| _    => throw $ IO.userError "give file"

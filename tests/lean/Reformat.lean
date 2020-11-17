727816e7fd945455c9542a59c168ecea00d0741a
Sebastian Ullrich <sebasti@nullri.ch>
2020-11-17 15:54:47 +0100

    test: bring back Reformat.lean on abbreviated copy of Prelude.lean
    /cc @leodemoura

---
+++ tests/lean/Reformat.lean
@@ -0,0 +1,27 @@
+/-! Parse and reformat file -/
+import Lean.PrettyPrinter
+
+open Lean
+open Lean.Elab
+open Lean.Elab.Term
+open Lean.Format
+
+unsafe def main (args : List String) : IO Unit := do
+let (debug, f) : Bool × String := match args with
+  | [f, "-d"] => (true, f)
+  | [f]       => (false, f)
+  | _         => panic! "usage: file [-d]"
+let env ← mkEmptyEnvironment
+let stx ← Lean.Parser.parseFile env args.head!
+let (f, _) ← (tryFinally (PrettyPrinter.ppModule stx) printTraces).toIO { options := Options.empty.setBool `trace.PrettyPrinter.format debug } { env := env }
+IO.print f
+let stx' ← Lean.Parser.parseModule env args.head! (toString f)
+if stx' != stx then
+  let stx := stx.getArg 1
+  let stx' := stx'.getArg 1
+  stx.getArgs.size.forM fun i => do
+    if stx.getArg i != stx'.getArg i then
+      throw $ IO.userError s!"reparsing failed:\n{stx.getArg i}\n{stx'.getArg i}"
+
+-- abbreviated Prelude.lean, which can be parsed without elaboration
+#eval main ["Reformat/Input.lean"]

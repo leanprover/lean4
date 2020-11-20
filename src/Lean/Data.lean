f78a18db89d30ea0f680a9fa3ca15677031ffa1a
Sebastian Ullrich <sebasti@nullri.ch>
2020-11-21 00:16:26 +0100

    chore: import orphaned files in `Lean`

---
+++ src/Lean/Data.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2020 Sebastian Ullrich. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sebastian Ullrich
+-/
+import Lean.Data.Format
+import Lean.Data.FormatMacro
+import Lean.Data.Json
+import Lean.Data.JsonRpc
+import Lean.Data.KVMap
+import Lean.Data.LBool
+import Lean.Data.LOption
+import Lean.Data.Lsp
+import Lean.Data.Name
+import Lean.Data.Occurrences
+import Lean.Data.OpenDecl
+import Lean.Data.Options
+import Lean.Data.Position
+import Lean.Data.SMap
+import Lean.Data.Trie

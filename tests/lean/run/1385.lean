3eaa4518a0cbd6cc2ab8db52a25a27b7ec88c077
Leonardo de Moura <leonardo@microsoft.com>
2020-11-24 10:43:40 -0800

    test: dependent pattern matching

---
+++ tests/lean/run/1385.lean
@@ -0,0 +1,22 @@
+def S := List Nat
+constant TSpec : PointedType
+def T (s : S) : Type :=  TSpec.type
+instance (s : S) : Inhabited (T s) := {
+  default := TSpec.val
+}
+
+inductive Op : (ishapes : List S) → (oshape : S) → Type
+  | binary : (shape : S) → Op [shape, shape] shape
+  | gemm   : {m n p : Nat} → Op [[m, n], [n, p]] [m, p]
+
+def Op.f : {ishapes : List S} → {oshape : S} → Op ishapes oshape → T oshape
+  | [shape, _],        _,      binary _ => arbitrary _
+  | [[m, n], [_, p]],  [_, _], gemm     => arbitrary _
+
+#print Op.f
+
+def Op.f2 : {ishapes : List S} → {oshape : S} → Op ishapes oshape → T oshape
+  | _,  _, binary _ => arbitrary _
+  | _,  _, gemm     => arbitrary _
+
+#print Op.f2

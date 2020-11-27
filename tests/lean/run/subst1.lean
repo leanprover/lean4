93cc8a6403267857ca7af5d4ab810b4b562a4fa1
Leonardo de Moura <leonardo@microsoft.com>
2020-11-27 09:00:11 -0800

    chore: fix test
    It was working for the wrong reasons

--- tests/lean/run/subst1.lean
+++ tests/lean/run/subst1.lean
@@ -1,4 +1,4 @@
-
+--
 
 set_option trace.Meta.Tactic.subst true
 
@@ -14,12 +14,11 @@ theorem tst2 (x y z : Nat) : y = z → x = z + y → x = z + z := by
   subst h2
   exact rfl
 
-def BV (n : Nat) : Type := Unit
+def BV (n : Nat) : Type := { a : Array Bool // a.size = n }
 
-theorem tst3 (n m : Nat) (v : BV n) (w : BV m) (h1 : n = m) (h2 : forall (v1 v2 : BV n), v1 = v2) : v = cast (congrArg BV h1) w := by
+theorem tst3 (n m : Nat) (v : BV n) (w : BV m) (h1 : n = m) (h2 : forall (v1 v2 : BV n), v1 = v2) : v = cast (congrArg BV h1.symm) w := by
 subst h1
 apply h2
-
 
 theorem tst4 (n m : Nat) (v : BV n) (w : BV m) (h1 : n = m) (h2 : forall (v1 v2 : BV n), v1 = v2) : v = cast (congrArg BV h1.symm) w := by
 subst n

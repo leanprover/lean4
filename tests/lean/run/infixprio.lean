e9069b696593e5f96299df870dc8108c5c586723
Leonardo de Moura <leonardo@microsoft.com>
2020-11-29 08:22:47 -0800

    feat: expand optional priority at `notation` and `mixfix` commands

---
+++ tests/lean/run/infixprio.lean
@@ -0,0 +1,10 @@
+def f (x y : Nat) : Nat :=
+  x + 2*y
+
+-- "+" with priority higher than the builtin "+" notation
+infix:65 [1] "+" => f
+
+#check 1 + 2
+
+theorem ex (x y : Nat) : x + y = f x y :=
+  rfl

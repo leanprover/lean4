d43a65aed0fe24652675a74f4d1498f89d4ef3a1
Leonardo de Moura <leonardo@microsoft.com>
2020-12-05 08:05:40 -0800

    feat: elaboarate `local` `syntax`, `infix` and `notation` commands

---
+++ tests/lean/run/localParsers.lean
@@ -0,0 +1,33 @@
+def f (x y : Nat) := x + 2*y
+
+namespace Foo
+
+local infix:65 [1] "+" => f
+
+theorem ex1 (x y : Nat) : x + y = f x y := rfl
+
+#check 1 + 2
+
+end Foo
+
+#check 1 + 2
+theorem ex2 (x y : Nat) : x + y = HAdd.hAdd x y := rfl
+
+open Foo
+
+theorem ex3 (x y : Nat) : x + y = HAdd.hAdd x y := rfl
+#check 1 + 2
+
+section
+def g (x y : Nat) := 3*(x+y)
+
+local infix:65 [1] "  +  " => g
+
+theorem ex4 (x y : Nat) : x + y = g x y := rfl
+
+#check 1 + 2
+
+end
+
+theorem ex5 (x y : Nat) : x + y = HAdd.hAdd x y := rfl
+#check 1 + 2

54723409c310b76d70a0bc6610dabda8273ea41e
Leonardo de Moura <leonardo@microsoft.com>
2020-12-04 16:16:24 -0800

    test: scoped parsers

---
+++ tests/lean/run/scopedParsers.lean
@@ -0,0 +1,19 @@
+def f (x y : Nat) := x + 2*y
+
+namespace Foo
+
+scoped infix:65 [1] "+" => f
+
+theorem ex1 (x y : Nat) : x + y = f x y := rfl
+
+#check 1 + 2
+
+end Foo
+
+#check 1 + 2
+theorem ex2 (x y : Nat) : x + y = HAdd.hAdd x y := rfl
+
+open Foo
+
+theorem ex3 (x y : Nat) : x + y = f x y := rfl
+#check 1 + 2

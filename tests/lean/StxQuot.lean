6fc03d0f298ff12e75a2ebf6a386f37d4de74bda
Sebastian Ullrich <sebasti@nullri.ch>
2020-12-08 11:53:49 +0100

    feat: quotation scopes in `match_syntax`

--- tests/lean/StxQuot.lean
+++ tests/lean/StxQuot.lean
@@ -38,11 +38,14 @@ end Syntax
 #eval run $ do let a ← `(a.{0}); match_syntax a with `($id:ident) => pure id | _ => pure a
 #eval run $ do let a ← `(match a with | a => 1 | _ => 2); match_syntax a with `(match $e with $eqns:matchAlt*) => pure eqns | _ => pure #[]
 
-#eval run do let a ← some <$> `(a); `({ a := a $[: $(id a)]?})
-#eval run do let a ← pure none; `({ a := a $[: $a]?})
+def f (stx : Syntax) : Unhygienic Syntax := match_syntax stx with
+  | `({ a := a $[: $a]?}) => `({ a := a $[: $(id a)]?})
+  | _ => unreachable!
+#eval run do f (← `({ a := a : a }))
+#eval run do f (← `({ a := a }))
 
 #eval run do
-  let pats := #[← `(a), ← `(a + 1)]
-  let rhss := #[← `(b), ← `(b + 1)]
-  `(match a with $[$pats => $rhss]|*)
+  match_syntax ← `(match a with a => b | a + 1 => b + 1) with
+  | `(match a with $[$pats => $rhss]|*) => `(match a with $[$pats => $rhss]|*)
+  | _ => unreachable!
 end Lean

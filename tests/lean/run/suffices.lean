a10328e7459af77071220f1fe69a3db4d21b4608
Leonardo de Moura <leonardo@microsoft.com>
2020-12-09 17:06:19 -0800

    chore: simplify syntax patterns using `$[...]?`

--- tests/lean/run/suffices.lean
+++ tests/lean/run/suffices.lean
@@ -14,3 +14,13 @@ theorem ex3 (Ha : a) (Hab : a → b) (Hbc : b → c) : c := by
   suffices b by apply Hbc; assumption
   suffices a by apply Hab; assumption
   assumption
+
+theorem ex4 (Ha : a) (Hab : a → b) (Hbc : b → c) : c :=
+  suffices h1 : b from Hbc h1
+  suffices h2 : a from Hab h2
+  Ha
+
+theorem ex5 (Ha : a) (Hab : a → b) (Hbc : b → c) : c := by
+  suffices h1 : b by apply Hbc; assumption
+  suffices h2 : a by apply Hab; exact h2
+  assumption

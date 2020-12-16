fe08b28c7cc6f114808ced3e5527aea0bf5cfe3d
Leonardo de Moura <leonardo@microsoft.com>
2020-12-16 09:32:22 -0800

    feat: add `Array.insertionSort`

---
+++ src/Init/Data/Array/InsertionSort.lean
@@ -0,0 +1,28 @@
+/-
+Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Init.Data.Array
+
+def Array.insertionSort (a : Array α) (lt : α → α → Bool) : Array α :=
+  traverse a 0 a.size
+where
+  traverse (a : Array α) (i : Nat) (fuel : Nat) : Array α :=
+    match fuel with
+    | 0      => a
+    | fuel+1 =>
+      if h : i < a.size then
+        traverse (swapLoop a i h) (i+1) fuel
+      else
+        a
+  swapLoop (a : Array α) (j : Nat) (h : j < a.size) : Array α :=
+    match he:j with
+    | 0    => a
+    | j'+1 =>
+      have h' : j' < a.size by subst j; exact Nat.ltTrans (Nat.ltSuccSelf _) h
+      if lt (a.get ⟨j, h⟩) (a.get ⟨j', h'⟩) then
+        swapLoop (a.swap ⟨j, h⟩ ⟨j', h'⟩) j' (by rw sizeSwapEq; assumption done)
+      else
+        a

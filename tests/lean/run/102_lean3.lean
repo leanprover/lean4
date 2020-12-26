837d6a1529daf4a4c6cc366770cf1e5a10ea9e52
Sebastian Ullrich <sebasti@nullri.ch>
2020-12-26 21:11:57 +0100

    chore: fix test

--- tests/lean/run/102_lean3.lean
+++ tests/lean/run/102_lean3.lean
@@ -2,7 +2,7 @@ inductive time (D : Type) : Type
 | start : time D
 | after : (D → time D) → time D
 
-def foo (C D : Type) : time D → (γ : Type) × γ → C
+def foo (C D : Type) : time D → (γ : Type) × (γ → C)
 | time.start    => ⟨C, id⟩
 | time.after ts => ⟨(∀ (i : D), (foo C D $ ts i).1) × D, λ p => (foo C D $ ts p.2).2 $ p.1 p.2⟩
 

64f7af9da53da44ea44aa717960904ec4195e72a
Leonardo de Moura <leonardo@microsoft.com>
2020-12-29 11:59:46 -0800

    feat: add `SimpM`

--- src/Lean/Meta/Tactic/Simp/Main.lean
+++ src/Lean/Meta/Tactic/Simp/Main.lean
@@ -3,6 +3,9 @@ Copyright (c) 2020 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
-import Lean.Meta.Tactic.Simp.SimpLemmas
 import Lean.Meta.Tactic.Simp.Types
-import Lean.Meta.Tactic.Simp.Main
+
+namespace Lean.Meta.Simp
+
+
+end Lean.Meta.Simp

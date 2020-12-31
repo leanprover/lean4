e2c8b1694bc8d9abf8dff3016a8fd0993132d47e
Leonardo de Moura <leonardo@microsoft.com>
2020-12-31 10:54:55 -0800

    feat: improve `subst`

---
+++ tests/lean/substlet.lean
@@ -0,0 +1,26 @@
+theorem ex1 (n : Nat) : 0 + n = n := by
+  let m := n
+  have h : ∃ k, id k = m := ⟨m, rfl⟩
+  cases h with
+  | intro a e =>
+    traceState
+    subst e
+    traceState
+    apply Nat.zeroAdd
+
+theorem ex2 (n : Nat) : 0 + n = n := by
+  let m := n
+  have h : ∃ k, m = id k := ⟨m, rfl⟩
+  cases h with
+  | intro a e =>
+    traceState
+    subst e
+    traceState
+    apply Nat.zeroAdd
+
+theorem ex3 (n : Nat) (h : n = 0) : 0 + n = 0 := by
+  let m := n + 1
+  let v := m + 1
+  have v = n + 2 from rfl
+  subst v -- error
+  done

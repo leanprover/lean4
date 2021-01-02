b7ad8933777d10f32dd2c99108da6eec9704984a
Sebastian Ullrich <sebasti@nullri.ch>
2021-01-02 22:49:20 +0100

    chore: remove lean4-mode, rename lean4-lsp-mode to lean4-mode
    /cc @leodemoura

--- lean4-mode/lean4-mode.el
+++ lean4-mode/lean4-mode.el
@@ -151,11 +151,6 @@
 
 (defconst lean4-hooks-alist
   '(
-    ;; server
-    ;; (kill-buffer-hook                    . lean4-server-stop)
-    ;; (after-change-functions              . lean4-server-change-hook)
-    ;; (focus-in-hook                       . lean4-server-show-messages)
-    ;; (window-scroll-functions             . lean4-server-window-scroll-function-hook)
     ;; Handle events that may start automatic syntax checks
     (before-save-hook                    . lean4-whitespace-cleanup)
     ;; info windows
@@ -172,23 +167,14 @@ enabled and disabled respectively.")
 
 (defun lean4-mode-setup ()
   "Default lean4-mode setup"
-  ;; server
-  ;;(ignore-errors (lean4-server-ensure-alive))
-  ;;(setq mode-name '("Lean" (:eval (lean4-server-status-string))))
   ;; Right click menu sources
   ;;(setq lean4-right-click-item-functions '(lean4-info-right-click-find-definition
   ;;                                        lean4-hole-right-click))
   ;; Flycheck
-  (lean4-flycheck-turn-on)
   (setq-local flycheck-disabled-checkers '())
   ;; info buffers
   (lean4-ensure-info-buffer lean4-next-error-buffer-name)
   ;(lean4-ensure-info-buffer lean4-show-goal-buffer-name)
-  ;; eldoc
-  ;;(when lean4-eldoc-use
-  ;;  (set (make-local-variable 'eldoc-documentation-function)
-  ;;       'lean4-eldoc-documentation-function)
-  ;;  (eldoc-mode t))
   )
 
 ;; Automode List
@@ -221,32 +207,9 @@ Invokes `lean4-mode-hook'.
     (add-hook hook fn nil 'local))
   (lean4-mode-setup))
 
-(define-derived-mode lean4-lsp-mode prog-mode "Lean 4 LSP"
-  "Major mode for Lean
-\\{lean4-mode-map}
-"
-  :syntax-table lean4-syntax-table
-  :abbrev-table lean4-abbrev-table
-  :group 'lean
-  (set (make-local-variable 'comment-start) "--")
-  (set (make-local-variable 'comment-start-skip) "[-/]-[ \t]*")
-  (set (make-local-variable 'comment-end) "")
-  (set (make-local-variable 'comment-end-skip) "[ \t]*\\(-/\\|\\s>\\)")
-  (set (make-local-variable 'comment-padding) 1)
-  (set (make-local-variable 'comment-use-syntax) t)
-  (set (make-local-variable 'font-lock-defaults) lean4-font-lock-defaults)
-  (set (make-local-variable 'indent-tabs-mode) nil)
-  (set 'compilation-mode-font-lock-keywords '())
-  (set-input-method "Lean")
-  (set (make-local-variable 'lisp-indent-function)
-       'common-lisp-indent-function)
-  (lean4-set-keys)
-  (if (fboundp 'electric-indent-local-mode)
-      (electric-indent-local-mode -1)))
-
 ;; Automatically use lean4-mode for .lean files.
 ;;;###autoload
-(push '("\\.lean$" . lean4-lsp-mode) auto-mode-alist)
+(push '("\\.lean$" . lean4-mode) auto-mode-alist)
 
 ;; Use utf-8 encoding
 ;;;### autoload
@@ -256,18 +219,14 @@ Invokes `lean4-mode-hook'.
 (require 'lsp-mode)
 ;; Ref: https://emacs-lsp.github.io/lsp-mode/page/adding-new-language/
 (add-to-list 'lsp-language-id-configuration
-             '(lean4-lsp-mode . "lean4"))
+             '(lean4-mode . "lean4"))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection `(,(lean4-get-executable lean4-executable-name) "--server"))
-                  :major-modes '(lean4-lsp-mode)
-                  :environment-fn (lambda ()
-                                    '(;; Set to dump LSP communications
-                                      ;("LEAN_SERVER_LOG_DIR" . "my/log/dir")
-                                      ))
+                  :major-modes '(lean4-mode)
                   :server-id 'lean4-lsp))
 
-(add-hook 'lean4-lsp-mode-hook #'lsp)
+(add-hook 'lean4-mode-hook #'lsp)
 
 ;; Flycheck init
 (eval-after-load 'flycheck

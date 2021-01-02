5aab1cadc0f517243a2ee59874071fd6617303fb
Sebastian Ullrich <sebasti@nullri.ch>
2021-01-02 14:09:54 +0100

    chore: remove debug print

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -1027,7 +1027,6 @@ static obj_res task_bind_fn1(obj_arg x, obj_arg f, obj_arg) {
     lean_assert(g_current_task_object->m_imp->m_closure == nullptr);
     obj_res c = mk_closure_2_1(task_bind_fn2, new_task);
     mark_mt(c);
-    std::cerr << "reviving " << g_current_task_object << std::endl;
     g_current_task_object->m_imp->m_closure = c;
     return nullptr; /* notify queue that task did not finish yet. */
 }

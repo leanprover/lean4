403699f5e4a0a91cc0fc3782bfd5210e13f4bdf6
Leonardo de Moura <leonardo@microsoft.com>
2021-01-04 16:06:41 -0800

    fix: `elim_array_cases`

---
+++ tests/compiler/array.lean
@@ -0,0 +1,13 @@
+@[noinline] def f (a : Array Nat) : Nat :=
+  Array.casesOn (motive := fun _ => Nat) a (fun data => data.length)
+
+@[noinline] def g (a : Array Nat) : List Nat :=
+  a.data
+
+@[noinline] def h (a : List Nat) : List Nat :=
+  g (Array.mk a)
+
+def main : IO Unit := do
+  IO.println (f #[2, 3, 4])
+  IO.println (g #[2, 3, 4])
+  IO.println (h [2, 3, 4])

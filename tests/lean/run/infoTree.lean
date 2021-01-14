fb52ec8ef54ce673c009e2c1a6ea7184f02db3be
Leonardo de Moura <leonardo@microsoft.com>
2021-01-14 11:11:54 -0800

    feat: store binder information in the `InfoTree`
    @Vtec234 I am storing the binder information using `TermInfo`.
    If it helps, I can add a custom `Info` constructor.
    Example: `| Info.ofBinderInfo (i : BinderInfo)`.

--- tests/lean/run/infoTree.lean
+++ tests/lean/run/infoTree.lean
@@ -15,9 +15,15 @@ def f (x : Nat) : Nat × Nat :=
   let y := ⟨x, x⟩
   id y
 
-def h : (x : Nat) → x + 0 = x :=
-  fun x => by
+def h : (x y : Nat) → (b : Bool) → x + 0 = x :=
+  fun x y b => by
     simp
     exact rfl
+
+def f2 : (x y : Nat) → (b : Bool) → Nat :=
+  fun x y b =>
+    let (z, w) := (x + y, x - y)
+    let z1 := z + w
+    z + z1
 
 showInfoTrees!

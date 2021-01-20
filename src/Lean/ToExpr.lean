6c63780a8187afa678cfeffce56f6192a1f88b66
Leonardo de Moura <leonardo@microsoft.com>
2021-01-20 11:35:44 -0800

    chore: use double quote
    This commit also fixes a typo at `Option.cons`

--- src/Lean/ToExpr.lean
+++ src/Lean/ToExpr.lean
@@ -16,43 +16,43 @@ export ToExpr (toExpr toTypeExpr)
 
 instance : ToExpr Expr where
   toExpr     := id
-  toTypeExpr := mkConst `Expr
+  toTypeExpr := mkConst ``Expr
 
 instance : ToExpr Nat where
   toExpr     := mkNatLit
-  toTypeExpr := mkConst `Nat
+  toTypeExpr := mkConst ``Nat
 
 instance : ToExpr Bool where
-  toExpr     := fun b => if b then mkConst `Bool.true else mkConst `Bool.false
-  toTypeExpr := mkConst `Bool
+  toExpr     := fun b => if b then mkConst ``Bool.true else mkConst ``Bool.false
+  toTypeExpr := mkConst ``Bool
 
 instance : ToExpr Char where
-  toExpr     := fun c => mkApp (mkConst `Char.ofNat) (toExpr c.toNat)
-  toTypeExpr := mkConst `Char
+  toExpr     := fun c => mkApp (mkConst ``Char.ofNat) (toExpr c.toNat)
+  toTypeExpr := mkConst ``Char
 
 instance : ToExpr String where
   toExpr     := mkStrLit
-  toTypeExpr := mkConst `String
+  toTypeExpr := mkConst ``String
 
 instance : ToExpr Unit where
   toExpr     := fun _ => mkConst `Unit.unit
-  toTypeExpr := mkConst `Unit
+  toTypeExpr := mkConst ``Unit
 
 def Name.toExprAux : Name → Expr
   | Name.anonymous  => mkConst `Lean.Name.anonymous
-  | Name.str p s _  => mkAppB (mkConst `Lean.Name.mkStr) (toExprAux p) (toExpr s)
-  | Name.num p n _  => mkAppB (mkConst `Lean.Name.mkNum) (toExprAux p) (toExpr n)
+  | Name.str p s _  => mkAppB (mkConst ``Lean.Name.mkStr) (toExprAux p) (toExpr s)
+  | Name.num p n _  => mkAppB (mkConst ``Lean.Name.mkNum) (toExprAux p) (toExpr n)
 
 instance : ToExpr Name where
   toExpr     := Name.toExprAux
-  toTypeExpr := mkConst `Name
+  toTypeExpr := mkConst ``Name
 
 instance {α : Type} [ToExpr α] : ToExpr (Option α) :=
   let type := toTypeExpr α
   { toExpr     := fun o => match o with
-      | none   => mkApp (mkConst `Option.none [levelZero]) type
-      | some a => mkApp2 (mkConst `Option.cons [levelZero]) type (toExpr a),
-    toTypeExpr := mkApp (mkConst `Option [levelZero]) type }
+      | none   => mkApp (mkConst ``Option.none [levelZero]) type
+      | some a => mkApp2 (mkConst ``Option.some [levelZero]) type (toExpr a),
+    toTypeExpr := mkApp (mkConst ``Option [levelZero]) type }
 
 def List.toExprAux {α} [ToExpr α] (nilFn : Expr) (consFn : Expr) : List α → Expr
   | []    => nilFn
@@ -60,20 +60,20 @@ def List.toExprAux {α} [ToExpr α] (nilFn : Expr) (consFn : Expr) : List α →
 
 instance {α : Type} [ToExpr α] : ToExpr (List α) :=
   let type := toTypeExpr α
-  let nil  := mkApp (mkConst `List.nil [levelZero]) type
-  let cons := mkApp (mkConst `List.cons [levelZero]) type
+  let nil  := mkApp (mkConst ``List.nil [levelZero]) type
+  let cons := mkApp (mkConst ``List.cons [levelZero]) type
   { toExpr     := List.toExprAux nil cons,
-    toTypeExpr := mkApp (mkConst `List [levelZero]) type }
+    toTypeExpr := mkApp (mkConst ``List [levelZero]) type }
 
 instance {α : Type} [ToExpr α] : ToExpr (Array α) :=
   let type := toTypeExpr α
-  { toExpr     := fun as => mkApp2 (mkConst `List.toArray [levelZero]) type (toExpr as.toList),
-    toTypeExpr := mkApp (mkConst `Array [levelZero]) type }
+  { toExpr     := fun as => mkApp2 (mkConst ``List.toArray [levelZero]) type (toExpr as.toList),
+    toTypeExpr := mkApp (mkConst ``Array [levelZero]) type }
 
 instance {α : Type} {β : Type} [ToExpr α] [ToExpr β] : ToExpr (α × β) :=
   let αType := toTypeExpr α
   let βType := toTypeExpr β
-  { toExpr     := fun ⟨a, b⟩ => mkApp4 (mkConst `Prod.mk [levelZero, levelZero]) αType βType (toExpr a) (toExpr b),
-    toTypeExpr := mkApp2 (mkConst `Prod [levelZero, levelZero]) αType βType }
+  { toExpr     := fun ⟨a, b⟩ => mkApp4 (mkConst ``Prod.mk [levelZero, levelZero]) αType βType (toExpr a) (toExpr b),
+    toTypeExpr := mkApp2 (mkConst ``Prod [levelZero, levelZero]) αType βType }
 
 end Lean

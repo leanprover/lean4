51de663e2c3fcac4b8c565979373f83946d8fa89
Leonardo de Moura <leonardo@microsoft.com>
2021-01-22 14:10:51 -0800

    fix: crash when using `TransparencyMode.instances`

---
+++ tests/lean/run/withReducibleAndInstancesCrash.lean
@@ -0,0 +1,16 @@
+import Lean
+
+@[reducible] def foo (x : Nat) := x + 1
+@[reducible] def bla (x y : Nat) := foo (foo x) + foo (foo x)
+@[reducible] def boo (x y : Nat) := foo (bla (foo x) (foo y) * bla (foo y) (foo x) * bla (foo y) (foo x))
+
+open Lean
+open Lean.Meta
+def tst : MetaM Unit := do
+  let c ← getConstInfo `boo
+  lambdaTelescope c.value! fun xs b => do
+    withReducibleAndInstances do
+      trace[Meta.debug]! "b: {← reduce b}"
+
+set_option trace.Meta.debug true
+#eval tst

f9ebfb466f9728c341a9bf7077a7ac028c411b13
Leonardo de Moura <leonardo@microsoft.com>
2021-01-31 11:18:57 -0800

    fix: missing `lean_inc`

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -1346,6 +1346,7 @@ extern "C" object * lean_int_big_mod(object * a1, object * a2) {
         int i2 = lean_scalar_to_int(a2);
         if (i2 == 0) {
             if (mpz_value(a1) >= 0) {
+                lean_inc(a1);
                 return a1;
             } else {
                 return mpz_to_int(mpz(-1) - mpz_value(a1));

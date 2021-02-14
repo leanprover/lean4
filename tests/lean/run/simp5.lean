3bc5b89ac365dcc097374c324ae46c6effb8e2ad
Leonardo de Moura <leonardo@microsoft.com>
2021-02-14 11:44:10 -0800

    test: add `if p x then .. else ..` example
    cc @Kha

--- tests/lean/run/simp5.lean
+++ tests/lean/run/simp5.lean
@@ -3,7 +3,14 @@ def f {α} (a b : α) := a
 theorem f_Eq {α} (a b : α) : f a b = a :=
   rfl
 
-theorem ex (a b c : α) : f (f a b) c = a := by
+theorem ex1 (a b c : α) : f (f a b) c = a := by
   simp [f_Eq]
 
-#print ex
+#print ex1
+
+@[simp] theorem ifTrue (a b : α) : (if True then a else b) = a := rfl
+
+theorem ex2 (p : Nat → Bool) (x : Nat) (h : p x = true) : (if p x then 1 else 2) = 1 := by
+  simp [h]
+
+#print ex2

2b78de650a36aad284f22368625a98c987fb4095
Leonardo de Moura <leonardo@microsoft.com>
2021-03-04 14:48:49 -0800

    test: experiment

---
+++ tests/playground/nnf.lean
@@ -0,0 +1,33 @@
+open Lean
+
+partial def mkBigAnd (n : Nat) (p : Syntax) : MacroM Syntax :=
+  loop 0 n
+where
+  loop (low high : Nat) : MacroM Syntax := do
+    if low == high then
+      `($p[$(quote low)])
+    else
+      let mid := (low + high)/2
+      `($(← loop low mid) ∧ $(← loop (mid + 1) high))
+
+macro "bigAnd! " n:num p:ident : term => mkBigAnd n.toNat p
+
+partial def mkBigOrNot (n : Nat) (p : Syntax) : MacroM Syntax :=
+  loop 0 n
+where
+  loop (low high : Nat) : MacroM Syntax := do
+    if low == high then
+      `(¬ $p[$(quote low)])
+    else
+      let mid := (low + high)/2
+      `($(← loop low mid) ∨ $(← loop (mid + 1) high))
+
+macro "bigOrNot! " n:num p:ident : term => mkBigOrNot n.toNat p
+
+@[simp] axiom not_and (p q : Prop) : (¬ (p ∧ q)) = (¬ p ∨ ¬ q)
+
+theorem ex (p : Array Prop) : (¬ bigAnd! 500 p) = bigOrNot! 500 p := by
+  simp only [not_and]
+  rfl
+
+-- #print ex

ad68c5cbaaac069790f14e4904aede50659886ec
Leonardo de Moura <leonardo@microsoft.com>
2021-03-06 15:31:23 -0800

    refactor: polymorphic `elabSetOption`

---
+++ src/Lean/Elab/SetOption.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+import Lean.Elab.Log
+
+namespace Lean.Elab
+
+variable [Monad m] [MonadOptions m] [MonadExceptOf Exception m] [MonadRef m]
+variable [AddErrorMessageContext m] [MonadLiftT (EIO Exception) m]
+
+def elabSetOption (id : Syntax) (val : Syntax) : m Options := do
+  let optionName := id.getId.eraseMacroScopes
+  match val.isStrLit? with
+  | some str => setOption optionName (DataValue.ofString str)
+  | none     =>
+  match val.isNatLit? with
+  | some num => setOption optionName (DataValue.ofNat num)
+  | none     =>
+  match val with
+  | Syntax.atom _ "true"  => setOption optionName (DataValue.ofBool true)
+  | Syntax.atom _ "false" => setOption optionName (DataValue.ofBool false)
+  | _ => throwError! "unexpected set_option value {val}"
+where
+  setOption (optionName : Name) (val : DataValue) : m Options := do
+    let decl ← IO.toEIO (fun (ex : IO.Error) => Exception.error (← getRef) ex.toString) (getOptionDecl optionName)
+    unless decl.defValue.sameCtor val do throwError "type mismatch at set_option"
+    return (← getOptions).insert optionName val
+
+end Lean.Elab

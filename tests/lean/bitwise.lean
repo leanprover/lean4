419a6190e8187c886bc142e1188059cfc6f48c60
pcpthm <pcpthm@gmail.com>
2021-03-06 15:11:10 +0900

    fix: bitwise shift overflow of UInt types
    It is an undefined behavior in C when the right operand of a shift operation exceeds the bit-width of the left operand.
    We define the shift operation to be `x << (y % B)` where `B` is the bit-width of the left operand.

--- tests/lean/bitwise.lean
+++ tests/lean/bitwise.lean
@@ -21,6 +21,12 @@
 #eval 0x17 ^^^ (0x10f : UInt8) == 0x18
 #eval (0x12 : UInt8) <<< 4 == 0x120
 #eval (0x12 : UInt8) >>> 4 == 0x1
+#eval (1 : UInt8) <<< 8 == 1
+#eval (1 : UInt8) <<< 32 == 1
+#eval (0x81 : UInt8) <<< 9 == 0x2
+#eval (1 : UInt8) >>> 8 == 1
+#eval (1 : UInt8) >>> 32 == 1
+#eval (0x81 : UInt8) >>> 9 == 0x40
 #eval ~~~(0x12 : UInt8) == 0xed
 
 #eval "UInt16"
@@ -29,6 +35,12 @@
 #eval 0x17 ^^^ (0x1010f : UInt16) == 0x0118
 #eval (0x12 : UInt16) <<< 4 == 0x120
 #eval (0x12 : UInt16) >>> 4 == 0x1
+#eval (1 : UInt16) <<< 16 == 1
+#eval (1 : UInt16) <<< 32 == 1
+#eval (0x8001 : UInt16) <<< 17 == 0x2
+#eval (1 : UInt16) >>> 16 == 1
+#eval (1 : UInt16) >>> 32 == 1
+#eval (0x8001 : UInt16) >>> 17 == 0x4000
 #eval ~~~(0x12 : UInt16) == 0xffed
 
 #eval "UInt32"
@@ -37,6 +49,10 @@
 #eval 0x17 ^^^ (0x1010f : UInt32) == 0x10118
 #eval (0x12 : UInt32) <<< 4 == 0x120
 #eval (0x12 : UInt32) >>> 4 == 0x1
+#eval (1 : UInt32) <<< 32 == 1
+#eval (0x80000001 : UInt32) <<< 33 == 0x2
+#eval (1 : UInt32) >>> 32 == 1
+#eval (0x80000001 : UInt32) >>> 33 == 0x40000000
 #eval ~~~(0x12 : UInt32) == 0xffffffed
 
 #eval "UInt64"
@@ -45,5 +61,18 @@
 #eval 0x17 ^^^ (0x1010f : UInt64) == 0x10118
 #eval (0x12 : UInt64) <<< 4 == 0x120
 #eval (0x12 : UInt64) >>> 4 == 0x1
+#eval (1 : UInt64) <<< 64 == 1
+#eval (0x8000000000000001 : UInt64) <<< 65 == 0x2
+#eval (1 : UInt64) >>> 64 == 1
+#eval (0x8000000000000001 : UInt64) >>> 65 == 0x4000000000000000
 #eval ~~~(0x12 : UInt64) == 0xffffffffffffffed
 
+#eval "USize"
+#eval 0x117 &&& (0x101ff  : USize) == 0x117
+#eval 0x17 ||| (0x1010f : USize) == 0x1011f
+#eval 0x17 ^^^ (0x1010f : USize) == 0x10118
+#eval (0x12 : USize) <<< 4 == 0x120
+#eval (0x12 : USize) >>> 4 == 0x1
+#eval (1 : USize) <<< 64 == 1
+#eval (1 : USize) >>> 64 == 1
+#eval ~~~(0x12 : USize) == (if System.Platform.numBits == 32 then 0xffffffed else 0xffffffffffffffed)

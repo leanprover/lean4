e863376be1552562d0d7ee9c7f13fd5a2d51d9e0
Sebastian Ullrich <sebasti@nullri.ch>
2021-04-05 21:58:57 +0200

    chore: simplify tests

--- tests/lean/interactive/completion.lean
+++ tests/lean/interactive/completion.lean
@@ -3,15 +3,9 @@ structure Foo where
 
 example (f : Foo) : f.
                     --^ textDocument/completion
-example : Nat := 0  -- recover
-
 example (f : Foo) : f.f
                      --^ textDocument/completion
-example : Nat := 0  -- recover
-
 example (f : Foo) : id f |>.
                           --^ textDocument/completion
-example : Nat := 0  -- recover
-
 example (f : Foo) : id f |>.f
                            --^ textDocument/completion
--- tests/lean/tokenErrors.lean
+++ tests/lean/tokenErrors.lean
@@ -1,17 +1,7 @@
 #check 'hi
-example : Nat := 0  -- recover
-
 #check '\y'
-example : Nat := 0  -- recover
-
 #check "hi
-example : Nat := 0  -- recover
-
 #check hi.«
-example : Nat := 0  -- recover
-
 /--
-example : Nat := 0  -- recover
-
 #print Nat
 /-

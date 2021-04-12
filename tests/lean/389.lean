7ae279d04f206e234dedd3f71091b2e6a6e7d09c
Leonardo de Moura <leonardo@microsoft.com>
2021-04-11 19:28:34 -0700

    fix: missing `commitWhenSome?` at `tryPureCoe?`
    fixes #389

---
+++ tests/lean/389.lean
@@ -0,0 +1,7 @@
+structure Foo (A : Sort _) := (foo : A)
+structure Bar (A : Sort _) extends Foo A := (bar : A)
+instance {A} : Coe (Bar A) (Foo A) := {coe := Bar.toFoo}
+def getFoo {A} (F : Foo A) := F.foo
+def bar : Bar Nat := {foo := 0, bar := 1}
+
+#check getFoo bar

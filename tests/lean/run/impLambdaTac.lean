2f37d7e290a9f95a9fc67b9eef25380bb13ecda5
Leonardo de Moura <leonardo@microsoft.com>
2021-04-12 22:54:26 -0700

    feat: elaborate `noImplicitLambda%` notation

---
+++ tests/lean/run/impLambdaTac.lean
@@ -0,0 +1,33 @@
+example (P : Prop) : ∀ {p : P}, P := by
+  exact fun {p} => p
+
+example (P : Prop) : ∀ {p : P}, P := by
+  intro h; exact h
+
+example (P : Prop) : ∀ {p : P}, P := by
+  exact @id _
+
+example (P : Prop) : ∀ {p : P}, P := by
+  exact noImplicitLambda% id
+
+macro "exact'" x:term : tactic => `(exact noImplicitLambda% $x)
+
+example (P : Prop) : ∀ {p : P}, P := by
+  exact' id
+
+example (P : Prop) : ∀ {p : P}, P := by
+  apply id
+
+example (P : Prop) : ∀ p : P, P := by
+  have _ from 1
+  apply id
+
+example (P : Prop) : ∀ {p : P}, P := by
+  refine noImplicitLambda% (have _ from 1; ?_)
+  apply id
+
+#exit
+-- After update stage0, we fix this
+example (P : Prop) : ∀ {p : P}, P := by
+  have _ from 1
+  apply id

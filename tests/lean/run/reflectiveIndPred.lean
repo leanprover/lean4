af391fe812925eff875175249c8c24329e1cf763
Leonardo de Moura <leonardo@microsoft.com>
2021-04-25 20:35:13 -0700

    test: reflective inductive predicate

---
+++ tests/lean/run/reflectiveIndPred.lean
@@ -0,0 +1,12 @@
+inductive Expr where
+  | val : Nat → Expr
+  | add : Expr → Expr → Expr
+  | fn  : (Nat → Expr) → Expr
+
+inductive Pos : Expr → Prop
+  | base : (n : Nat) → Pos (Expr.val n.succ)
+  | add  : Pos e₁ → Pos e₂ → Pos (Expr.add e₁ e₂)
+  | fn   : ((n : Nat) → Pos (f n)) → Pos (Expr.fn f)
+
+#print PosProofBelow
+#print Pos.brecOn

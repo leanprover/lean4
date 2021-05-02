0f8c6ca797c486a4efca1daf8bf5989c815ce71c
Leonardo de Moura <leonardo@microsoft.com>
2021-05-02 16:58:56 -0700

    fix: cache issue at `instantiateBetaRevRange`

---
+++ tests/lean/cacheIssue.lean
@@ -0,0 +1,7 @@
+structure Foo (A B : Type) := f : Unit -> A
+def foo (P) : Foo ((p : P) -> Nat) ({p : P} -> Nat) := ⟨λ _ _ => 0⟩
+def bar (P) : Foo ((p : P) -> Nat) ({p : P} -> Int) := ⟨λ _ _ => 0⟩
+
+#check foo Bool
+#check (foo Bool).f -- (foo Bool).f : Unit → Bool → Nat
+#check (bar Bool).f -- (bar Bool).f : Unit → Bool → Nat

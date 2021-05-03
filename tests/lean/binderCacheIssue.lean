d556ebbdc6544dfaccc3bd4701b2923d210ff2b6
Leonardo de Moura <leonardo@microsoft.com>
2021-05-02 20:04:41 -0700

    test: add test for binder caching issue

---
+++ tests/lean/binderCacheIssue.lean
@@ -0,0 +1,23 @@
+universes u v w
+
+class Funtype (N : Sort u) (O : outParam (Sort v)) (T : outParam (Sort w)) :=
+  pack : O -> N
+  unpack : N -> O
+  apply : N -> T
+
+class LNot {P : Sort u} (L : P -> Prop) :=
+  toFun : (p : P) -> Not (L p)
+
+namespace LNot
+variable {P : Sort u} {L : P -> Prop}
+abbrev applyFun (K : LNot L) {p} := K.toFun p
+abbrev unpackFun (K : LNot L) := K.toFun
+instance isFuntype : Funtype (LNot L)
+  ((p : P) -> Not (L p)) ({p : P} -> Not (L p)) :=
+  {pack := mk, unpack := unpackFun, apply := applyFun}
+end LNot
+
+#check LNot.unpackFun
+#check LNot.isFuntype.unpack
+#check LNot.applyFun
+#check LNot.isFuntype.apply

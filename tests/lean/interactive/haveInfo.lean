2be159515420f5c30c4a3f0ae5c71507ae42ef56
Leonardo de Moura <leonardo@microsoft.com>
2021-05-04 11:26:34 -0700

    fix: preserve `by` toke position information during macro expansion

--- tests/lean/interactive/haveInfo.lean
+++ tests/lean/interactive/haveInfo.lean
@@ -4,3 +4,24 @@ example : False := by
   --^ $/lean/plainGoal
     skip
   admit
+
+example : False := by
+  have True by
+          --^ $/lean/plainGoal
+    skip
+    skip
+  admit
+
+example : False := by
+  have True by
+           --^ $/lean/plainGoal
+    skip
+    skip
+  admit
+
+example : False := by
+  have True by
+    skip
+--^ $/lean/plainGoal
+    skip
+  admit

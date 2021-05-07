5fcd08326ffad6cbb148ed1093c3aa60a70d6420
Leonardo de Moura <leonardo@microsoft.com>
2021-05-07 14:21:37 -0700

    fix: bug at `reduceRec`

---
+++ tests/lean/run/reductionBug.lean
@@ -0,0 +1,19 @@
+abbrev VName := String
+
+inductive Ty where
+  | Bool
+  | Int
+
+def Ctxt := VName → Option Ty
+
+variable (Γ : Ctxt) in
+inductive Expr : Ty → Type where
+  | var (h : Γ x = some τ) : Expr τ
+
+def Expr.constFold : Expr Γ τ → Option Unit
+  | var n   => none
+
+theorem Expr.constFold_sound {e : Expr Γ τ} : constFold e = some v → True := by
+  intro h
+  induction e with
+  | var   => simp only [constFold] at h

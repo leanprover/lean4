65fc11b3b33d47fb030a40cfa873b89fe0094006
Daniel Selsam <daniel.selsam@protonmail.com>
2021-06-10 15:39:49 -0700

    chore: minor cleanup

--- src/Lean/Meta/UnificationHint.lean
+++ src/Lean/Meta/UnificationHint.lean
@@ -139,16 +139,16 @@ where
   trySingletonProjection : MetaM Bool := do
     match t with
     | Expr.proj structName idx x .. =>
-      let fields := getStructureFields (← getEnv) structName
-      if fields.size ≠ 1 then return false
-      let ctor := getStructureCtor (← getEnv) structName
+      if (getStructureFields (← getEnv) structName).size ≠ 1 then return false
       trace[Meta.isDefEq.hint] "trying singleton projection at {t} =?= {s}"
       traceCtx `Meta.isDefEq.hint <| checkpointDefEq do
         let xType ← Meta.whnf (← Meta.instantiateMVars (← Meta.inferType x))
         let Expr.const structName' lvls .. ← pure xType.getAppFn | return false
         if structName != structName' then return false
+        let ctor := getStructureCtor (← getEnv) structName
         let y := mkAppN (mkConst ctor.name lvls) (xType.getAppArgs.push s)
-        if ← Meta.isExprDefEqAux x y then return true else return false
+        if ← Meta.isExprDefEqAux x y then return true 
+        return false
     | _ => return false
 
 builtin_initialize

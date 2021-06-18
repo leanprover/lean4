625d1828c9b1ba4d32240fc7e9282e6d1fdcbba8
Sebastian Ullrich <sebasti@nullri.ch>
2021-06-18 12:09:59 +0200

    test: some syntax docstring tests

--- tests/lean/interactive/hover.lean
+++ tests/lean/interactive/hover.lean
@@ -1,3 +1,5 @@
+import Lean
+
 example : True := by
   apply True.intro
       --^ textDocument/hover
@@ -11,3 +13,75 @@ example (n : Nat) : True := by
   | Nat.zero => _
   --^ textDocument/hover
   | n + 1 => _
+
+
+/-- My tactic -/
+macro "mytac" o:("only"?) e:term : tactic => `(exact $e)
+
+example : True := by
+  mytac only True.intro
+--^ textDocument/hover
+      --^ textDocument/hover
+           --^ textDocument/hover
+
+/-- My way better tactic -/
+macro_rules
+  | `(tactic| mytac $[only]? $e) => `(apply $e)
+
+example : True := by
+  mytac only True.intro
+--^ textDocument/hover
+
+/-- My ultimate tactic -/
+elab_rules : tactic
+  | `(tactic| mytac $[only]? $e) => `(tactic| refine $e) >>= Lean.Elab.Tactic.evalTactic
+
+example : True := by
+  mytac only True.intro
+--^ textDocument/hover
+
+
+/-- My notation -/
+macro "mynota" e:term : term => e
+
+#check mynota 1
+     --^ textDocument/hover
+
+/-- My way better notation -/
+macro_rules
+  | `(mynota $e) => `(2 * $e)
+
+#check mynota 1
+     --^ textDocument/hover
+
+-- macro_rules take precedence over elab_rules for term/command, so use new syntax
+syntax "mynota'" term : term
+
+/-- My ultimate notation -/
+elab_rules : term
+  | `(mynota' $e) => `($e * $e) >>= (Lean.Elab.Term.elabTerm · none)
+
+#check mynota' 1
+     --^ textDocument/hover
+
+
+/-- My command -/
+macro "mycmd" e:term : command => `(def hi := $e)
+
+mycmd 1
+--^ textDocument/hover
+
+/-- My way better command -/
+macro_rules
+  | `(mycmd $e) => `(@[inline] def hi := $e)
+
+mycmd 1
+--^ textDocument/hover
+
+syntax "mycmd'" term : command
+/-- My ultimate command -/
+elab_rules : command
+  | `(mycmd' $e) => `(/-- hi -/ @[inline] def hi := $e) >>= Lean.Elab.Command.elabCommand
+
+mycmd' 1
+--^ textDocument/hover

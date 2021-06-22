c25dcdcfcee68798eb6669fb2544aa17bc963d99
Sebastian Ullrich <sebasti@nullri.ch>
2021-06-22 23:36:25 +0200

    chore: disable LSAN in server tests for now

--- tests/lean/interactive/test_single.sh
+++ tests/lean/interactive/test_single.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 source ../../common.sh
 
+# IO.Process.exit (used by the file worker) seems to be incompatible with LSAN
+# TODO: investigate or work around
+export ASAN_OPTIONS=detect_leaks=0
+
 # these tests don't have to succeed
 exec_capture lean --run run.lean "$f" || true
 diff_produced

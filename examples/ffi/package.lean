4ae14ac8498c88193d82b6d9d358750d253a01b3
tydeu <tydeu@hatpress.net>
2021-07-27 07:24:45 -0400

    refactor: rename 'ext' example to 'ffi'

--- examples/ffi/package.lean
+++ examples/ffi/package.lean
@@ -2,32 +2,32 @@ import Lake.Build
 import Lake.Package
 open Lake System
 
-def extDir : FilePath := "ext"
-def addSrc := extDir / "add.cpp"
+def cDir : FilePath := "c"
+def addSrc := cDir / "add.cpp"
 
 def buildDir := defaultBuildDir
-def addO := buildDir / extDir / "add.o"
-def extLib := buildDir / extDir / "libext.a"
+def addO := buildDir / cDir / "add.o"
+def cLib := buildDir / cDir / "libadd.a"
 
 def fetchAddOTarget : IO ActiveFileTarget := do
   skipIfNewer addO (← getMTime addSrc) <|
     BuildTask.spawn <| compileO addO addSrc (cmd := "c++")
 
-def fetchExtLibTarget : IO ActiveFileTarget := do
+def fetchCLibTarget : IO ActiveFileTarget := do
   let oTarget ← fetchAddOTarget
-  skipIfNewer extLib oTarget.mtime <|
-    oTarget >> compileStaticLib extLib #[addO]
+  skipIfNewer cLib oTarget.mtime <|
+    oTarget >> compileStaticLib cLib #[addO]
 
 def package : PackageConfig := {
-  name := "ext"
+  name := "ffi"
   version := "0.1"
   -- customize layout
   srcDir := "lib"
   moduleRoot := `Add
   binName := "add"
   -- specify path to the lib for linker
-  linkArgs := #[extLib.toString]
+  linkArgs := #[cLib.toString]
   -- specify the lib target as an additional dependency
-  buildMoreDepsTarget := fetchExtLibTarget.map fun target =>
+  buildMoreDepsTarget := fetchCLibTarget.map fun target =>
     LeanTarget.fromMTimeTarget <| target.discardArtifact
 }

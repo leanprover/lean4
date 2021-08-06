3d485bf3756fa610326705b1d4036d05dcdb326f
Leonardo de Moura <leonardo@microsoft.com>
2021-08-06 13:40:54 -0700

    feat: add `moduleDocExt`

--- src/Lean/DocString.lean
+++ src/Lean/DocString.lean
@@ -7,7 +7,7 @@ import Lean.MonadEnv
 
 namespace Lean
 
-builtin_initialize docStringExt : MapDeclarationExtension String ← mkMapDeclarationExtension `docstring
+private builtin_initialize docStringExt : MapDeclarationExtension String ← mkMapDeclarationExtension `docstring
 
 def addDocString [MonadEnv m] (declName : Name) (docString : String) : m Unit :=
   modifyEnv fun env => docStringExt.insert env declName docString
@@ -19,5 +19,21 @@ def addDocString' [Monad m] [MonadEnv m] (declName : Name) (docString? : Option 
 
 def findDocString? [Monad m] [MonadEnv m] (declName : Name) : m (Option String) :=
   return docStringExt.find? (← getEnv) declName
+
+private builtin_initialize moduleDocExt : SimplePersistentEnvExtension String (Std.PersistentArray String) ← registerSimplePersistentEnvExtension {
+  name          := `moduleDocExt
+  addImportedFn := fun _ => {}
+  addEntryFn    := fun s e => s.push e
+  toArrayFn     := fun es => es.toArray
+}
+
+def addMainModuleDoc (env : Environment) (doc : String) : Environment :=
+  moduleDocExt.addEntry env doc
+
+def getMainModuleDoc (env : Environment)  : Std.PersistentArray String :=
+  moduleDocExt.getState env
+
+def getModuleDoc? (env : Environment) (moduleName : Name) : Option (Array String) :=
+  env.getModuleIdx? moduleName |>.map fun modIdx => moduleDocExt.getModuleEntries env modIdx
 
 end Lean

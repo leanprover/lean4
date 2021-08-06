9dc2a4240d53baa04912e8766422d3c65af6cf4c
Leonardo de Moura <leonardo@microsoft.com>
2021-08-06 14:14:22 -0700

    test: add test for `getModuleDoc?`

--- tests/lean/moduleDoc.lean
+++ tests/lean/moduleDoc.lean
@@ -8,6 +8,14 @@ def tst : MetaM Unit := do
   let docs := getMainModuleDoc (← getEnv)
   IO.println <| docs.toList.map repr
 
+def printDoc (moduleName : Name) : MetaM Unit := do
+  match getModuleDoc? (← getEnv) moduleName with
+  | some docs => IO.println <| docs.toList.map repr
+  | _ => throwError "module not found"
+
 /-! Another module doc. -/
 
 #eval tst
+
+#eval printDoc `Lean.Meta.ReduceEval
+#eval printDoc `Lean.Data.Lsp.Communication

8a84532760af06371dfe88b66b11428079acfaa6
Leonardo de Moura <leonardo@microsoft.com>
2021-08-06 19:15:25 -0700

    feat: add `PEmpty`
    closes #537

--- tests/lean/276.lean
+++ tests/lean/276.lean
@@ -1,9 +1,5 @@
 universe u v
 
--- `Type u` version can be defined without this option, but I get the same error
-set_option bootstrap.inductiveCheckResultingUniverse false in
-inductive PEmpty : Sort u
-
 -- `#check` works
 set_option pp.all true in
 #check fun {α : Sort v} => PEmpty.rec (fun _ => α)

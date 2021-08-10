2b71e165510f3bb2a712c8c13d08369b48bc7616
Leonardo de Moura <leonardo@microsoft.com>
2021-08-10 06:31:44 -0700

    test: structure diamond coercion

---
+++ tests/lean/diamond3.lean
@@ -0,0 +1,17 @@
+structure A where
+  x : Nat := 0
+  y : Nat := 0
+  f : Nat → Nat := (. + 10)
+
+structure B extends A where
+  z : Nat
+
+structure C extends A where
+  w : Nat
+
+set_option structureDiamondWarning false
+
+structure D extends B, C where
+  a : Nat
+
+#print D.toC

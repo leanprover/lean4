b012050917eb85448d9ffee5c9134c05d87a51ea
Leonardo de Moura <leonardo@microsoft.com>
2021-08-13 16:57:03 -0700

    chore: remove some macros that use `binop%`
    For example, given `i : Int` and `n : Nat`, we don't want to coerce `n`
    into a `Int` in the term `i ^ n`.

--- src/Init/Notation.lean
+++ src/Init/Notation.lean
@@ -89,10 +89,6 @@ macro_rules | `($x + $y)   => `(binop% HAdd.hAdd $x $y)
 macro_rules | `($x - $y)   => `(binop% HSub.hSub $x $y)
 macro_rules | `($x * $y)   => `(binop% HMul.hMul $x $y)
 macro_rules | `($x / $y)   => `(binop% HDiv.hDiv $x $y)
-macro_rules | `($x % $y)   => `(binop% HMod.hMod $x $y)
-macro_rules | `($x <<< $y) => `(binop% HShiftLeft.hShiftLeft $x $y)
-macro_rules | `($x >>> $y) => `(binop% HShiftRight.hShiftRight $x $y)
-macro_rules | `($x ^ $y)   => `(binop% HPow.hPow $x $y)
 macro_rules | `($x ++ $y)  => `(binop% HAppend.hAppend $x $y)
 
 -- declare ASCII alternatives first so that the latter Unicode unexpander wins

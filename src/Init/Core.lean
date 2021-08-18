8d90872d284de64a290272be86123ffbb98a52b5
Leonardo de Moura <leonardo@microsoft.com>
2021-08-17 20:41:39 -0700

    chore: add `not_not_intro`

--- src/Init/Core.lean
+++ src/Init/Core.lean
@@ -180,6 +180,9 @@ theorem mt {a b : Prop} (h₁ : a → b) (h₂ : ¬b) : ¬a :=
 
 theorem not_false : ¬False := id
 
+theorem not_not_intro {p : Prop} (h : p) : ¬ ¬ p :=
+  fun hn : ¬ p => hn h
+
 -- proof irrelevance is built in
 theorem proofIrrel {a : Prop} (h₁ h₂ : a) : h₁ = h₂ := rfl
 

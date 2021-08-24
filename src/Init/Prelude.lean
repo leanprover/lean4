63b11368cab0ff9e82744d5333e6493d76874b0a
Leonardo de Moura <leonardo@microsoft.com>
2021-08-24 14:11:18 -0700

    chore: add `Or.elim` for tutorial

--- src/Init/Prelude.lean
+++ src/Init/Prelude.lean
@@ -148,6 +148,11 @@ theorem Or.intro_left (b : Prop) (h : a) : Or a b :=
 theorem Or.intro_right (a : Prop) (h : b) : Or a b :=
   Or.inr h
 
+theorem Or.elim {c : Prop} (h : Or a b) (left : a → c) (right : b → c) : c :=
+  match h with
+  | Or.inl h => left h
+  | Or.inr h => right h
+
 inductive Bool : Type where
   | false : Bool
   | true : Bool

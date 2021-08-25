42f1e16f4448a9eef8b835c35d79ec4449b13a06
Leonardo de Moura <leonardo@microsoft.com>
2021-08-25 06:59:46 -0700

    chore: fix `calc` test

--- tests/lean/run/calc.lean
+++ tests/lean/run/calc.lean
@@ -4,7 +4,7 @@ variable (pf12 : t1 = t2) (pf23 : t2 = t3) (pf34 : t3 = t4)
 theorem foo : t1 = t4 :=
   calc
     t1 = t2 := pf12
-     _ = t3 := id
+     _ = t3 := pf23
      _ = t4 := pf34
 
 variable (t5 : Nat)

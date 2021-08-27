03f095ccabc030f8b63004379c5f87594ceae23c
Leonardo de Moura <leonardo@microsoft.com>
2021-08-27 11:48:02 -0700

    fix: `match` + `OfNat` issue
    See https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/simp.20match.20question

---
+++ tests/lean/matchOfNatIssue.lean
@@ -0,0 +1,18 @@
+example (x : Int) (h : x = 2) : Int.div 2 1 = x := by
+  simp [Int.div]
+  traceState
+  simp [h]
+
+example (n : Nat) : Int.div (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n / 0) := by
+  simp [Int.div]
+
+example (n : Nat) : Int.div (Int.ofNat n) 0 = Int.ofNat (n / 0) := by
+  simp [Int.div]
+
+example (n : Nat) : Int.mul (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n * 0) := by
+  simp [Int.mul]
+
+example (n : Nat) : Int.mul (Int.ofNat n) 0 = Int.ofNat (n * 0) := by
+  simp [Int.mul]
+  traceState
+  rfl

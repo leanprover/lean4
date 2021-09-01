22a80a9623a77243cef2db95f79135ddef1bfb04
Leonardo de Moura <leonardo@microsoft.com>
2021-08-31 17:09:00 -0700

    test: `split` tactic tests

--- tests/lean/run/def10.lean
+++ tests/lean/run/def10.lean
@@ -1,18 +1,30 @@
-
-
 def f : Bool → Bool → Nat
-| _, _ => 10
+  | true, true => 0
+  | _, _       => 3
 
-example : f true true = 10 :=
-rfl
+example : f true true = 0 :=
+  rfl
 
 def g : Bool → Bool → Bool → Nat
-| true, _,    true  => 1
-| _,   false, false => 2
-| _,   _,     _     => 3
+  | true, _,    true  => 1
+  | _,   false, false => 2
+  | a,   _,     b     => f a b
 
 theorem ex1 : g true true true = 1 := rfl
 theorem ex2 : g true false true = 1 := rfl
 theorem ex3 : g true false false = 2 := rfl
 theorem ex4 : g false false false = 2 := rfl
 theorem ex5 : g false true true = 3 := rfl
+
+theorem f_eq (h : a = true → b = true → False) : f a b = 3 := by
+  simp [f]
+  split
+  . contradiction
+  . rfl
+
+theorem ex6 : g x y z > 0 := by
+  simp [g]
+  split
+  next => decide
+  next => decide
+  next a b c h₁ h₂ => simp [f_eq h₁]

58e8c5774c9b8642e93d5295b0a2afc3cca11f10
Sebastian Ullrich <sebasti@nullri.ch>
2021-09-08 11:11:52 +0200

    fix: stdlib_flags.h

--- src/stdlib_flags.h
+++ src/stdlib_flags.h
@@ -6,9 +6,9 @@ options get_default_options() {
     // see https://leanprover.github.io/lean4/doc/make/index.html#further-bootstrapping-complications
 #if LEAN_IS_STAGE0 == 1
     // switch to `true` for ABI-breaking changes affecting meta code
-    opts.update("interpreter.prefer_native", false);
-    // switch to `true` for changing built-in parsers
-    opts.update("internal.parseQuotWithCurrentStage", false);
+    opts = opts.update({"interpreter", "prefer_native"}, false);
+    // switch to `true` for changing built-in parsers used in quotations
+    opts = opts.update({"internal", "parseQuotWithCurrentStage"}, false);
 #endif
     return opts;
 }

750c2507da4893d0776fe1e01bf09b2b76e05cbc
Leonardo de Moura <leonardo@microsoft.com>
2021-09-07 18:01:28 -0700

    test: add `*>` laziness test
    This commit also fixes a broken test
    
    Closes #617
    
    The following operators are now lazy: `<|>`, `>>`, `*>`, `<*`, `<*>`

--- tests/lean/run/struct3.lean
+++ tests/lean/run/struct3.lean
@@ -5,18 +5,18 @@ class Bind2 (m : Type u → Type v) where
 
 class Monad2 (m : Type u → Type v) extends Applicative m, Bind2 m : Type (max (u+1) v) where
   map      := fun f x => Bind2.bind x (pure ∘ f)
-  seq      := fun f x => Bind2.bind f fun y => Functor.map y x
-  seqLeft  := fun x y => Bind2.bind x fun a => Bind2.bind y fun _ => pure a
-  seqRight := @fun α β x y => Bind2.bind x fun _ => y -- Recall that `@` disables implicit lambda support
+  seq      := fun f x => Bind2.bind f fun y => Functor.map y (x ())
+  seqLeft  := fun x y => Bind2.bind x fun a => Bind2.bind (y ()) fun _ => pure a
+  seqRight := @fun α β x y => Bind2.bind x fun _ => y () -- Recall that `@` disables implicit lambda support
 
 class Monad3 (m : Type u → Type v) extends Applicative m, Bind2 m : Type (max (u+1) v) where
   map (f x)      := Bind2.bind x (pure ∘ f)
-  seq (f x)      := Bind2.bind f fun y => Functor.map y x
-  seqLeft (x y)  := Bind2.bind x fun a => Bind2.bind y fun _ => pure a
-  seqRight (x y) := Bind2.bind x fun _ => y
+  seq (f x)      := Bind2.bind f fun y => Functor.map y (x ())
+  seqLeft (x y)  := Bind2.bind x fun a => Bind2.bind (y ()) fun _ => pure a
+  seqRight (x y) := Bind2.bind x fun _ => y ()
 
 class Monad4 (m : Type u → Type v) extends Applicative m, Bind2 m : Type (max (u+1) v) where
   map f x      := Bind2.bind x (pure ∘ f)
-  seq f x      := Bind2.bind f fun y => Functor.map y x
-  seqLeft x y  := Bind2.bind x fun a => Bind2.bind y fun _ => pure a
-  seqRight x y := Bind2.bind x fun _ => y
+  seq f x      := Bind2.bind f fun y => Functor.map y (x ())
+  seqLeft x y  := Bind2.bind x fun a => Bind2.bind (y ()) fun _ => pure a
+  seqRight x y := Bind2.bind x fun _ => y ()

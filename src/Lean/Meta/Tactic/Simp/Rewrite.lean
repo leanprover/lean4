19a710ffc9edee134488a4797db4b53692cd846e
Leonardo de Moura <leonardo@microsoft.com>
2021-09-09 19:28:09 -0700

    feat: add `getMatchWithExtra` and improve `tryLemma` at `simp`

--- src/Lean/Meta/Tactic/Simp/Rewrite.lean
+++ src/Lean/Meta/Tactic/Simp/Rewrite.lean
@@ -42,72 +42,84 @@ where
       trace[Meta.Tactic.simp.discharge] "{lemmaName}, failed to synthesize instance{indentExpr type}"
       return false
 
-def tryLemma? (e : Expr) (lemma : SimpLemma) (discharge? : Expr → SimpM (Option Expr)) : SimpM (Option Result) :=
+private def tryLemmaCore (lhs : Expr) (xs : Array Expr) (bis : Array BinderInfo) (val : Expr) (type : Expr) (e : Expr) (lemma : SimpLemma) (numExtraArgs : Nat) (discharge? : Expr → SimpM (Option Expr)) : SimpM (Option Result) := do
+  let rec go (e : Expr) : SimpM (Option Result) := do
+    if (← isDefEq lhs e) then
+      unless (← synthesizeArgs lemma.getName xs bis discharge?) do
+        return none
+      let proof ← instantiateMVars (mkAppN val xs)
+      if ← hasAssignableMVar proof then
+        trace[Meta.Tactic.simp.rewrite] "{lemma}, has unassigned metavariables after unification"
+        return none
+      let rhs   ← instantiateMVars type.appArg!
+      if e == rhs then
+        return none
+      if lemma.perm && !Expr.lt rhs e then
+        trace[Meta.Tactic.simp.rewrite] "{lemma}, perm rejected {e} ==> {rhs}"
+        return none
+      trace[Meta.Tactic.simp.rewrite] "{lemma}, {e} ==> {rhs}"
+      return some { expr := rhs, proof? := proof }
+    else
+      unless lhs.isMVar do
+        -- We do not report unification failures when `lhs` is a metavariable
+        -- Example: `x = ()`
+        -- TODO: reconsider if we want lemmas such as `(x : Unit) → x = ()`
+        trace[Meta.Tactic.simp.unify] "{lemma}, failed to unify {lhs} with {e}"
+      return none
+  /- Check whether we need something more sophisticated here.
+     This simple approach was good enough for Mathlib 3 -/
+  let mut extraArgs := #[]
+  let mut e := e
+  for i in [:numExtraArgs] do
+    extraArgs := extraArgs.push e.appArg!
+    e := e.appFn!
+  match (← go e) with
+  | none => return none
+  | some { expr := eNew, proof? := none } => return some { expr := mkAppN eNew extraArgs }
+  | some { expr := eNew, proof? := some proof } =>
+    let mut proof := proof
+    for extraArg in extraArgs do
+      proof ← mkCongrFun proof extraArg
+    return some { expr := mkAppN eNew extraArgs, proof? := some proof }
+
+def tryLemmaWithExtraArgs? (e : Expr) (lemma : SimpLemma) (numExtraArgs : Nat) (discharge? : Expr → SimpM (Option Expr)) : SimpM (Option Result) :=
   withNewMCtxDepth do
     let val  ← lemma.getValue
     let type ← inferType val
     let (xs, bis, type) ← forallMetaTelescopeReducing type
     let type ← whnf (← instantiateMVars type)
     let lhs := type.appFn!.appArg!
-    let rec go (e : Expr) : SimpM (Option Result) := do
-      if (← isDefEq lhs e) then
-        unless (← synthesizeArgs lemma.getName xs bis discharge?) do
-          return none
-        let proof ← instantiateMVars (mkAppN val xs)
-        if ← hasAssignableMVar proof then
-          trace[Meta.Tactic.simp.rewrite] "{lemma}, has unassigned metavariables after unification"
-          return none
-        let rhs   ← instantiateMVars type.appArg!
-        if e == rhs then
-          return none
-        if lemma.perm && !Expr.lt rhs e then
-          trace[Meta.Tactic.simp.rewrite] "{lemma}, perm rejected {e} ==> {rhs}"
-          return none
-        trace[Meta.Tactic.simp.rewrite] "{lemma}, {e} ==> {rhs}"
-        return some { expr := rhs, proof? := proof }
-      else
-        unless lhs.isMVar do
-          -- We do not report unification failures when `lhs` is a metavariable
-          -- Example: `x = ()`
-          -- TODO: reconsider if we want lemmas such as `(x : Unit) → x = ()`
-          trace[Meta.Tactic.simp.unify] "{lemma}, failed to unify {lhs} with {e}"
-        return none
-    let lhsNumArgs := lhs.getAppNumArgs
-    let eNumArgs := e.getAppNumArgs
-    if eNumArgs == lhsNumArgs then
-      go e
-    else if eNumArgs < lhsNumArgs then
-      return none
-    else
-      /- Check whether we need something more sophisticated here.
-         This simple approach was good enough for Mathlib 3 -/
-      let mut extraArgs := #[]
-      let mut e := e
-      for i in [:eNumArgs - lhsNumArgs] do
-        extraArgs := extraArgs.push e.appArg!
-        e := e.appFn!
-      match (← go e) with
-      | none => return none
-      | some { expr := eNew, proof? := none } => return some { expr := mkAppN eNew extraArgs }
-      | some { expr := eNew, proof? := some proof } =>
-        let mut proof := proof
-        for extraArg in extraArgs do
-          proof ← mkCongrFun proof extraArg
-        return some { expr := mkAppN eNew extraArgs, proof? := some proof }
+    tryLemmaCore lhs xs bis val type e lemma numExtraArgs discharge?
 
+def tryLemma? (e : Expr) (lemma : SimpLemma) (discharge? : Expr → SimpM (Option Expr)) : SimpM (Option Result) := do
+  withNewMCtxDepth do
+    let val  ← lemma.getValue
+    let type ← inferType val
+    let (xs, bis, type) ← forallMetaTelescopeReducing type
+    let type ← whnf (← instantiateMVars type)
+    let lhs := type.appFn!.appArg!
+    match (← tryLemmaCore lhs xs bis val type e lemma 0 discharge?) with
+    | some result => return some result
+    | none =>
+      let lhsNumArgs := lhs.getAppNumArgs
+      let eNumArgs   := e.getAppNumArgs
+      if eNumArgs > lhsNumArgs then
+        tryLemmaCore lhs xs bis val type e lemma (eNumArgs - lhsNumArgs) discharge?
+      else
+        return none
 /-
 Remark: the parameter tag is used for creating trace messages. It is irrelevant otherwise.
 -/
 def rewrite (e : Expr) (s : DiscrTree SimpLemma) (erased : Std.PHashSet Name) (discharge? : Expr → SimpM (Option Expr)) (tag : String) : SimpM Result := do
-  let lemmas ← s.getMatch e (allowExtraArgs := true)
-  if lemmas.isEmpty then
+  let candidates ← s.getMatchWithExtra e
+  if candidates.isEmpty then
     trace[Debug.Meta.Tactic.simp] "no theorems found for {tag}-rewriting {e}"
     return { expr := e }
   else
-    let lemmas := lemmas.insertionSort fun e₁ e₂ => e₁.priority < e₂.priority
-    for lemma in lemmas do
+    let candidates := candidates.insertionSort fun e₁ e₂ => e₁.1.priority < e₂.1.priority
+    for (lemma, numExtraArgs) in candidates do
       unless inErasedSet lemma do
-        if let some result ← tryLemma? e lemma discharge? then
+        if let some result ← tryLemmaWithExtraArgs? e lemma numExtraArgs discharge? then
           return result
     return { expr := e }
 where

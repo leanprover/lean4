127681666a6a842c076963e82df782e933a4b9e7
Leonardo de Moura <leonardo@microsoft.com>
2021-09-10 19:36:27 -0700

    test: more equality theorems using `split`

--- tests/lean/run/eqThm.lean
+++ tests/lean/run/eqThm.lean
@@ -6,11 +6,40 @@ def f : Nat → Bool → Nat
   | x+2, true => f x true
   | x+2, b => f x (not b)
 
+macro "urfl" : tactic => `(set_option smartUnfolding false in rfl)
+
+
 theorem f_main_eq : f x b = f.match_1 (fun _ _ => Nat) x b (fun _ => 1) (fun _ => 2) (fun _ => 3) (fun _ => 4) (fun x => f x true) (fun x b => f x (not b)) := by
-  split
-  anyGoals rfl
-  conv => lhs; delta f; whnf; simpMatch
-  conv => lhs; delta f; whnf; simpMatch
-  set_option smartUnfolding false in rfl
+  split <;> (first | rfl | (conv => lhs; delta f; whnf; simpMatch); try urfl)
 
 #check @f_main_eq
+
+def g : List Nat → List Nat → Nat
+  | [],         y::ys => y
+  | [],         ys    => 0
+  | x1::x2::xs, ys    => g xs ys
+  | x::xs,      y::ys => g xs ys + y
+  | x::xs,      []    => g xs []
+
+theorem g_main_eq (xs ys : List Nat) : g xs ys = g.match_1 (fun _ _ => Nat) xs ys (fun y ys => y) (fun _ => 0) (fun _ _ xs ys => g xs ys) (fun _ xs y ys => g xs ys + y) (fun _ xs => g xs []) := by
+  split <;> (first | rfl | (conv => lhs; delta g; whnf; simpMatch); try urfl)
+
+#check @g_main_eq
+
+def foo (xs : List Nat) : List Nat :=
+  match xs with
+  | []    => []
+  | x::xs =>
+    let y := 2 * x;
+    match xs with
+    | []    => []
+    | x::xs => (y + x) :: foo xs
+
+theorem foo_main_eq (xs : List Nat) : foo xs = foo.match_1 (fun _ => List Nat) xs (fun _ => []) (fun x xs => let y := 2*x; foo.match_1 (fun _ => List Nat) xs (fun _ => []) (fun x xs => (y + x)::foo xs)) := by
+  split
+  . rfl
+  . split
+    . rfl
+    . rfl
+
+#check @foo_main_eq

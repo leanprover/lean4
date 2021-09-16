692ddbafb0f4cf6c3c3268a9cf5cffde98e58d2e
Sebastian Ullrich <sebasti@nullri.ch>
2021-09-16 14:30:24 +0200

    feat: IO.FS.removeDir(All)

--- src/runtime/io.cpp
+++ src/runtime/io.cpp
@@ -609,6 +609,14 @@ extern "C" obj_res lean_io_create_dir(b_obj_arg p, obj_arg) {
     }
 }
 
+extern "C" obj_res lean_io_remove_dir(b_obj_arg p, obj_arg) {
+    if (rmdir(string_cstr(p)) == 0) {
+        return io_result_mk_ok(box(0));
+    } else {
+        return io_result_mk_error(decode_io_error(errno, p));
+    }
+}
+
 extern "C" obj_res lean_io_remove_file(b_obj_arg fname, obj_arg) {
     if (std::remove(string_cstr(fname)) == 0) {
         return io_result_mk_ok(box(0));

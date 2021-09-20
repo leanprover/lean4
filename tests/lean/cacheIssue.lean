9e980b2a78b97981a07c1abce1d69b54dc2d22e3
Leonardo de Moura <leonardo@microsoft.com>
2021-09-20 07:12:40 -0700

    fix: bug at `decAux?`
    We are getting a few "stuck at universe constraint" errors after the
    fix. We may need to add some kind of approximation in the future.

--- tests/lean/cacheIssue.lean
+++ tests/lean/cacheIssue.lean
@@ -1,6 +1,6 @@
 structure Foo (A B : Type) := f : Unit -> A
-def foo (P) : Foo ((p : P) -> Nat) ({p : P} -> Nat) := ⟨λ _ _ => 0⟩
-def bar (P) : Foo ((p : P) -> Nat) ({p : P} -> Int) := ⟨λ _ _ => 0⟩
+def foo (P : Type) : Foo ((p : P) -> Nat) ({p : P} -> Nat) := ⟨λ _ _ => 0⟩
+def bar (P : Type) : Foo ((p : P) -> Nat) ({p : P} -> Int) := ⟨λ _ _ => 0⟩
 
 #check foo Bool
 #check (foo Bool).f -- (foo Bool).f : Unit → Bool → Nat

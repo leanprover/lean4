5a301d8c3b273b32a08643b4bbe3221223d76a47
Leonardo de Moura <leonardo@microsoft.com>
2021-09-21 15:44:21 -0700

    refactor: add `src/Lean/Elab/PreDefinition/WF` directory

---
+++ src/Lean/Elab/PreDefinition/WF/Main.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+import Lean.Elab.PreDefinition.Basic
+namespace Lean
+namespace Elab
+open Meta
+
+def wfRecursion (preDefs : Array PreDefinition) (terminationBy? : Option Syntax) : TermElabM Bool :=
+  throwError "well founded recursion has not been implemented yet"
+
+end Elab
+end Lean

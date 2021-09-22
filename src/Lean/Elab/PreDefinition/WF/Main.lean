ee2bdc1f84b490246b774b10c72cb5d0db075a2d
Leonardo de Moura <leonardo@microsoft.com>
2021-09-21 19:34:15 -0700

    feat: add `WF/PackDomain.lean`

--- src/Lean/Elab/PreDefinition/WF/Main.lean
+++ src/Lean/Elab/PreDefinition/WF/Main.lean
@@ -4,12 +4,23 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 import Lean.Elab.PreDefinition.Basic
-namespace Lean
-namespace Elab
+import Lean.Elab.PreDefinition.WF.PackDomain
+
+namespace Lean.Elab
+open WF
 open Meta
 
-def wfRecursion (preDefs : Array PreDefinition) (terminationBy? : Option Syntax) : TermElabM Bool :=
+def wfRecursion (preDefs : Array PreDefinition) (terminationBy? : Option Syntax) : TermElabM Bool := do
+  withoutModifyingEnv do
+    for preDef in preDefs do
+      addAsAxiom preDef
+    let unaryPreDefs ← packDomain preDefs
+    for preDef in unaryPreDefs do
+      check preDef.value -- TODO: remove
+      trace[Elab.definition.wf] "{preDef.declName}, {preDef.levelParams}, {preDef.value}"
+  -- TODO
   throwError "well founded recursion has not been implemented yet"
 
-end Elab
-end Lean
+builtin_initialize registerTraceClass `Elab.definition.wf
+
+end Lean.Elab

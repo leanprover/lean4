07fb8504a18aa6ea02692469b842aa560bbcae10
Leonardo de Moura <leonardo@microsoft.com>
2021-09-21 20:36:18 -0700

    fix: bug introduced today

--- src/Lean/Elab/PreDefinition/Structural/Main.lean
+++ src/Lean/Elab/PreDefinition/Structural/Main.lean
@@ -57,9 +57,9 @@ private def getFixedPrefix (declName : Name) (xs : Array Expr) (value : Expr) : 
   numFixedRef.get
 
 private def elimRecursion (preDef : PreDefinition) : M (Nat × PreDefinition) := do
-  addAsAxiom preDef
   trace[Elab.definition.structural] "{preDef.declName} := {preDef.value}"
   withoutModifyingEnv do lambdaTelescope preDef.value fun xs value => do
+    addAsAxiom preDef
     let value ← preprocess value preDef.declName
     trace[Elab.definition.structural] "{preDef.declName} {xs} :=\n{value}"
     let numFixed ← getFixedPrefix preDef.declName xs value

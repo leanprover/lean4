6d4360a04d2b58ad97fa793c0379b36569274533
tydeu <tydeu@hatpress.net>
2021-09-22 15:43:07 -0400

    release: 2.1.0

--- Lake/Version.lean
+++ Lake/Version.lean
@@ -10,7 +10,7 @@ namespace Lake
 def version.major := 2
 def version.minor := 1
 def version.patch := 0
-def version.isPre := true
-def versionString := s!"{version.major}.{version.minor}.{version.patch}-pre"
+def version.isPre := false
+def versionString := s!"{version.major}.{version.minor}.{version.patch}"
 def uiVersionString :=
   s!"Lake version {versionString} (Lean version {uiLeanVersionString})"

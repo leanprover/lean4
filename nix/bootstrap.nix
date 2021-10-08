bd3440e543deb2e06eab286adc540194a5968ac3
Sebastian Ullrich <sebasti@nullri.ch>
2021-10-08 19:37:55 +0200

    chore: submodule hints

--- nix/bootstrap.nix
+++ nix/bootstrap.nix
@@ -17,6 +17,10 @@ rec {
   } // args // {
     src = args.realSrc or (lib.sourceByRegex args.src [ "[a-z].*" "CMakeLists\.txt" ]);
     cmakeFlags = (args.cmakeFlags or [ "-DSTAGE=1" "-DPREV_STAGE=./faux-prev-stage" "-DUSE_GITHASH=OFF" ]) ++ (args.extraCMakeFlags or extraCMakeFlags) ++ lib.optional (args.debug or debug) [ "-DCMAKE_BUILD_TYPE=Debug" ];
+    preConfigure = args.preConfigure or "" + ''
+      # ignore absence of submodule
+      sed -i 's!lake/Lake.lean!!' CMakeLists.txt
+    '';
   });
   lean-bin-tools-unwrapped = buildCMake {
     name = "lean-bin-tools";

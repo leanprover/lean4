b4998be270898e8fdb8067b142b4d3844ef8ca12
Gabriel Ebner <gebner@gebner.org>
2021-10-08 19:57:04 +0200

    fix: use eraseMacroScopes on trace classes

---
+++ tests/lean/traceClassScopes.lean
@@ -0,0 +1,19 @@
+import Lean
+
+macro "t" t:interpolatedStr(term) : doElem =>
+  `(Macro.trace[Meta.debug] $t)
+
+macro "tstcmd" : command => do
+  t "hello"
+  `(example : Nat := 1)
+
+set_option trace.Meta.debug true in
+tstcmd
+
+open Lean Meta
+
+macro "r" r:interpolatedStr(term) : doElem =>
+  `(trace[Meta.debug] $r)
+
+set_option trace.Meta.debug true in
+#eval show MetaM _ from do r "world"

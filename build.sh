c6f6eec4c5a7eec42e6feb1d26a1d93cd3550b8c
tydeu <tydeu@hatpress.net>
2021-10-19 11:30:24 -0400

    fix; `leanmake` build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if [ "$OS" = "Windows_NT" ]; then
 else
   LINK_OPTS=-rdynamic
 fi
-leanmake PKG=Lake OLEAN_OUT=build/lib TEMP_OUT=build/ir BIN_NAME=lake LEAN_PATH=./build bin LINK_OPTS=${LINK_OPTS} "$@"
+leanmake PKG=Lake OLEAN_OUT=build/lib TEMP_OUT=build/ir BIN_NAME=lake LEAN_PATH=./build/lib bin LINK_OPTS=${LINK_OPTS} "$@"

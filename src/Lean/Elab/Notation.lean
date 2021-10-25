4c6c2a19e4e26f558375b16f4eb9ca53ee5b8025
Gabriel Ebner <gebner@gebner.org>
2021-10-25 15:02:03 +0200

    feat: predictable naming for notation

--- src/Lean/Elab/Notation.lean
+++ src/Lean/Elab/Notation.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 import Lean.Elab.Syntax
+import Lean.Elab.AuxDef
 
 namespace Lean.Elab.Command
 open Lean.Syntax
@@ -52,12 +53,14 @@ def mkSimpleDelab (attrKind : Syntax) (vars : Array Syntax) (pat qrhs : Syntax) 
     guard <| args.all (Syntax.isIdent ∘ getAntiquotTerm)
     guard <| args.allDiff
     -- replace head constant with (unused) antiquotation so we're not dependent on the exact pretty printing of the head
-    `(@[$attrKind:attrKind appUnexpander $(mkIdent c):ident] def unexpand : Lean.PrettyPrinter.Unexpander := fun
+    `(@[$attrKind:attrKind appUnexpander $(mkIdent c):ident]
+      aux_def unexpand $(mkIdent c) : Lean.PrettyPrinter.Unexpander := fun
        | `($$(_):ident $args*) => `($pat)
        | _                     => throw ())
   | `($c:ident)        =>
     let [(c, [])] ← Macro.resolveGlobalName c.getId | failure
-    `(@[$attrKind:attrKind appUnexpander $(mkIdent c):ident] def unexpand : Lean.PrettyPrinter.Unexpander
+    `(@[$attrKind:attrKind appUnexpander $(mkIdent c):ident]
+      aux_def unexpand $(mkIdent c) : Lean.PrettyPrinter.Unexpander := fun
        | `($$(_):ident) => `($pat)
        | _              => throw ())
   | _                  => failure

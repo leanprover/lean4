80a73dd9032290318a31537c14cfdb939b73b961
Leonardo de Moura <leonardo@microsoft.com>
2021-10-25 12:44:35 -0700

    feat: basic support for definitions at `inductive` declarations

---
+++ tests/lean/run/ind_whnf.lean
@@ -0,0 +1,21 @@
+inductive Expr : id Type
+  | var : Nat → Expr
+  | app : String → List Expr → Expr
+
+partial def Expr.fold (f : Nat → α → α) : Expr → α → α
+  | var n,    a => f n a
+  | app s as, a => as.foldl (init := a) fun a e => fold f e a
+
+def Expr.isVar : Expr → Bool
+  | var _ => true
+  | _     => false
+
+inductive Lst (α : Type u) : id (id (Type u))
+  | nil : Lst α
+  | cons : α → Lst α → Lst α
+
+protected def Lst.append : Lst α → Lst α → Lst α
+  | nil,       bs => bs
+  | cons a as, bs => cons a (Lst.append as bs)
+
+#print Lst

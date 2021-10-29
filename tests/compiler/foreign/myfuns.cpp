6abded466bc340b8d01a8b0b31af2d88a6ef6ee9
Sebastian Ullrich <sebasti@nullri.ch>
2021-10-29 19:09:13 +0200

    test: --load-dynlib

--- tests/compiler/foreign/myfuns.cpp
+++ tests/compiler/foreign/myfuns.cpp
@@ -30,30 +30,30 @@ static inline S const * to_S(b_lean_obj_arg s) {
     return static_cast<S *>(lean_get_external_data(s));
 }
 
-extern "C" lean_object * lean_mk_S(uint32_t x, uint32_t y, b_lean_obj_arg s) {
+extern "C" LEAN_EXPORT lean_object * lean_mk_S(uint32_t x, uint32_t y, b_lean_obj_arg s) {
     return S_to_lean(new S(x, y, lean_string_cstr(s)));
 }
 
-extern "C" uint32_t lean_S_add_x_y(b_lean_obj_arg s) {
+extern "C" LEAN_EXPORT uint32_t lean_S_add_x_y(b_lean_obj_arg s) {
     return to_S(s)->m_x + to_S(s)->m_y;
 }
 
-extern "C" lean_object * lean_S_string(b_lean_obj_arg s) {
+extern "C" LEAN_EXPORT lean_object * lean_S_string(b_lean_obj_arg s) {
     return lean_mk_string(to_S(s)->m_s.c_str());
 }
 
 static S g_s(0, 0, "");
 
-extern "C" lean_object * lean_S_global_append(b_lean_obj_arg str, lean_object /* w */) {
+extern "C" LEAN_EXPORT lean_object * lean_S_global_append(b_lean_obj_arg str, lean_object /* w */) {
     g_s.m_s += lean_string_cstr(str);
     return lean_io_result_mk_ok(lean_box(0));
 }
 
-extern "C" lean_object * lean_S_global_string(lean_object /* w */) {
+extern "C" LEAN_EXPORT lean_object * lean_S_global_string(lean_object /* w */) {
     return lean_io_result_mk_ok(lean_mk_string(g_s.m_s.c_str()));
 }
 
-extern "C" lean_object * lean_S_update_global(b_lean_obj_arg s, lean_object /* w */) {
+extern "C" LEAN_EXPORT lean_object * lean_S_update_global(b_lean_obj_arg s, lean_object /* w */) {
     g_s.m_x = to_S(s)->m_x;
     g_s.m_y = to_S(s)->m_y;
     g_s.m_s = to_S(s)->m_s;

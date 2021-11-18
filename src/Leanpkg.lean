aeccbbe9c22fea1420d5997cb1aaa65f4367ef01
Sebastian Ullrich <sebasti@nullri.ch>
2021-11-18 15:57:26 +0100

    feat: `findSysroot?` & reworked `initSearchPath`

--- src/Leanpkg.lean
+++ src/Leanpkg.lean
@@ -219,7 +219,7 @@ end Leanpkg
 def main (args : List String) : IO UInt32 := do
   try
     Lean.enableInitializersExecution
-    Lean.initSearchPath none  -- HACK
+    Lean.initSearchPath (← Lean.getBuildDir)
     let (cmd, outerArgs, innerArgs) ← Leanpkg.splitCmdlineArgs args
     Leanpkg.main cmd outerArgs innerArgs
   catch e =>

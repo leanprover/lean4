e641ae4eae75d5a1029168adaf063b3979244d46
larsk21 <57503246+larsk21@users.noreply.github.com>
2021-11-26 10:31:01 +0100

    fix: prefix check in set_option completion

--- tests/lean/interactive/completionOption.lean
+++ tests/lean/interactive/completionOption.lean
@@ -1,2 +1,21 @@
-set_option pp.proofs
+
+set_option fo
+           --^ textDocument/completion
+
+set_option format
+               --^ textDocument/completion
+
+set_option format.in
                   --^ textDocument/completion
+
+set_option trace.p
+                --^ textDocument/completion
+
+set_option trace.pp
+                 --^ textDocument/completion
+
+set_option trace.pp.ana
+                     --^ textDocument/completion
+
+set_option trace.pp.analyze
+                         --^ textDocument/completion

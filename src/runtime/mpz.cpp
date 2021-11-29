6396f3b2c1b00feb9694f5bad300051754a9579e
Leonardo de Moura <leonardo@microsoft.com>
2021-11-29 11:11:49 -0800

    chore: simplify `mpz.h`
    Add basic support for `LEAN_USE_GMP=OFF`.

--- src/runtime/mpz.cpp
+++ src/runtime/mpz.cpp
@@ -11,6 +11,28 @@ Author: Leonardo de Moura
 #include "runtime/mpz.h"
 
 namespace lean {
+
+mpz::mpz() {
+    mpz_init(m_val);
+}
+
+mpz::mpz(mpz_t v) {
+    mpz_init(m_val);
+    mpz_set(m_val, v);
+}
+
+mpz::mpz(char const * v) {
+    mpz_init_set_str(m_val, const_cast<char*>(v), 10);
+}
+
+mpz::mpz(unsigned int v) {
+    mpz_init_set_ui(m_val, v);
+}
+
+mpz::mpz(int v) {
+    mpz_init_set_si(m_val, v);
+}
+
 mpz::mpz(uint64 v):
     mpz(static_cast<unsigned>(v)) {
     mpz tmp(static_cast<unsigned>(v >> 32));
@@ -30,12 +52,132 @@ mpz::mpz(int64 v) {
         mpz_neg(m_val, m_val);
 }
 
+mpz::mpz(mpz const & s) {
+    mpz_init_set(m_val, s.m_val);
+}
+
+mpz::mpz(mpz && s):mpz() {
+    mpz_swap(m_val, s.m_val);
+}
+
+mpz::~mpz() {
+    mpz_clear(m_val);
+}
+
+void mpz::set(mpz_t r) const {
+    mpz_set(r, m_val);
+}
+
+void swap(mpz & a, mpz & b) {
+    mpz_swap(a.m_val, b.m_val);
+}
+
+bool mpz::is_int() const {
+    return mpz_fits_sint_p(m_val) != 0;
+}
+
+bool mpz::is_unsigned_int() const {
+    return mpz_fits_uint_p(m_val) != 0;
+}
+
+bool mpz::is_long_int() const {
+    return mpz_fits_slong_p(m_val) != 0;
+}
+
+bool mpz::is_unsigned_long_int() const {
+    return mpz_fits_ulong_p(m_val) != 0;
+}
+
+bool mpz::is_size_t() const {
+    // GMP only features `fits` functions up to `unsigned long`, which is smaller than `size_t` on Windows.
+    // So we directly count the number of mpz words instead.
+    static_assert(sizeof(size_t) == sizeof(mp_limb_t), "GMP word size should be equal to system word size");
+    return is_nonneg() && mpz_size(m_val) <= 1;
+}
+
+long int mpz::get_long_int() const {
+    lean_assert(is_long_int()); return mpz_get_si(m_val);
+}
+
+int mpz::get_int() const {
+    lean_assert(is_int()); return static_cast<int>(get_long_int());
+}
+
+unsigned long int mpz::get_unsigned_long_int() const {
+    lean_assert(is_unsigned_long_int()); return mpz_get_ui(m_val);
+}
+
+unsigned int mpz::get_unsigned_int() const {
+    lean_assert(is_unsigned_int()); return static_cast<unsigned>(get_unsigned_long_int());
+}
+
 size_t mpz::get_size_t() const {
     // GMP only features accessors up to `unsigned long`, which is smaller than `size_t` on Windows.
     // So we directly access the lowest mpz word instead.
     static_assert(sizeof(size_t) == sizeof(mp_limb_t), "GMP word size should be equal system word size");
     // NOTE: mpz_getlimbn returns 0 if the index is out of range (i.e. `m_val == 0`)
     return static_cast<size_t>(mpz_getlimbn(m_val, 0));
+}
+
+mpz & mpz::operator=(mpz const & v) {
+    mpz_set(m_val, v.m_val); return *this;
+}
+
+mpz & mpz::operator=(char const * v) {
+    mpz_set_str(m_val, v, 10); return *this;
+}
+
+mpz & mpz::operator=(unsigned int v) {
+    mpz_set_ui(m_val, v); return *this;
+}
+
+mpz & mpz::operator=(int v) {
+    mpz_set_si(m_val, v); return *this;
+}
+
+int cmp(mpz const & a, mpz const & b) {
+    return mpz_cmp(a.m_val, b.m_val);
+}
+
+int cmp(mpz const & a, unsigned b) {
+    return mpz_cmp_ui(a.m_val, b);
+}
+
+int cmp(mpz const & a, unsigned long b) {
+    return mpz_cmp_ui(a.m_val, b);
+}
+
+int cmp(mpz const & a, int b) {
+    return mpz_cmp_si(a.m_val, b);
+}
+
+mpz & mpz::operator+=(mpz const & o) { mpz_add(m_val, m_val, o.m_val); return *this; }
+
+mpz & mpz::operator+=(unsigned u) { mpz_add_ui(m_val, m_val, u); return *this; }
+
+mpz & mpz::operator+=(int u) { if (u >= 0) mpz_add_ui(m_val, m_val, u); else mpz_sub_ui(m_val, m_val, -u); return *this; }
+
+mpz & mpz::operator-=(mpz const & o) { mpz_sub(m_val, m_val, o.m_val); return *this; }
+
+mpz & mpz::operator-=(unsigned u) { mpz_sub_ui(m_val, m_val, u); return *this; }
+
+mpz & mpz::operator-=(int u) { if (u >= 0) mpz_sub_ui(m_val, m_val, u); else mpz_add_ui(m_val, m_val, -u); return *this; }
+
+mpz & mpz::operator*=(mpz const & o) { mpz_mul(m_val, m_val, o.m_val); return *this; }
+
+mpz & mpz::operator*=(unsigned u) { mpz_mul_ui(m_val, m_val, u); return *this; }
+
+mpz & mpz::operator*=(int u) { mpz_mul_si(m_val, m_val, u); return *this; }
+
+mpz & mpz::operator/=(mpz const & o) { mpz_tdiv_q(m_val, m_val, o.m_val); return *this; }
+mpz & mpz::operator/=(unsigned u) { mpz_tdiv_q_ui(m_val, m_val, u); return *this; }
+
+mpz rem(mpz const & a, mpz const & b) { mpz r; mpz_tdiv_r(r.m_val, a.m_val, b.m_val); return r; }
+
+mpz mpz::pow(unsigned int exp) const {
+    mpz r;
+    mpz_pow_ui(r.m_val, m_val, exp);
+    return r;
 }
 
 size_t mpz::log2() const {
@@ -48,6 +190,41 @@ size_t mpz::log2() const {
 
 mpz operator%(mpz const & a, mpz const & b) {
     return rem(a, b);
+}
+
+mpz & mpz::operator&=(mpz const & o) {
+    mpz_and(m_val, m_val, o.m_val);
+    return *this;
+}
+
+mpz & mpz::operator|=(mpz const & o) {
+    mpz_ior(m_val, m_val, o.m_val);
+    return *this;
+}
+
+mpz & mpz::operator^=(mpz const & o) {
+    mpz_xor(m_val, m_val, o.m_val);
+    return *this;
+}
+
+void mul2k(mpz & a, mpz const & b, unsigned k) {
+    mpz_mul_2exp(a.m_val, b.m_val, k);
+}
+
+void div2k(mpz & a, mpz const & b, unsigned k) {
+    mpz_tdiv_q_2exp(a.m_val, b.m_val, k);
+}
+
+void mod2k(mpz & a, mpz const & b, unsigned k) {
+    mpz_tdiv_r_2exp(a.m_val, b.m_val, k);
+}
+
+void power(mpz & a, mpz const & b, unsigned k) {
+    mpz_pow_ui(a.m_val, b.m_val, k);
+}
+
+void gcd(mpz & g, mpz const & a, mpz const & b) {
+    mpz_gcd(g.m_val, a.m_val, b.m_val);
 }
 
 void display(std::ostream & out, __mpz_struct const * v) {
--- src/runtime/mpz.h
+++ src/runtime/mpz.h
@@ -6,7 +6,11 @@ Author: Leonardo de Moura
 */
 #pragma once
 #include <cstddef>
+#ifdef LEAN_USE_GMP
 #include <gmp.h>
+#else
+#include "runtime/mpn.h"
+#endif
 #include <string>
 #include <iostream>
 #include <limits>
@@ -20,14 +24,22 @@ namespace lean {
 class mpz {
     friend class object_compactor;
     friend class compacted_region;
+#ifdef LEAN_USE_GMP
     mpz_t m_val;
     mpz(__mpz_struct const * v) { mpz_init_set(m_val, v); }
+#else
+    bool      m_sign;
+    unsigned  m_size;
+    mpn_digit m_digits[0];
+#endif
 public:
-    mpz() { mpz_init(m_val); }
-    explicit mpz(mpz_t v) { mpz_init(m_val); mpz_set(m_val, v); }
-    explicit mpz(char const * v) { mpz_init_set_str(m_val, const_cast<char*>(v), 10); }
-    explicit mpz(unsigned int v) { mpz_init_set_ui(m_val, v); }
-    explicit mpz(int v) { mpz_init_set_si(m_val, v); }
+    mpz();
+#ifdef LEAN_USE_GMP
+    explicit mpz(mpz_t v);
+#endif
+    explicit mpz(char const * v);
+    explicit mpz(unsigned int v);
+    explicit mpz(int v);
     explicit mpz(uint64 v);
     explicit mpz(int64 v);
     static mpz of_size_t(size_t v) {
@@ -36,17 +48,31 @@ public:
         else
             return mpz((unsigned) v); // NOLINT
     }
-    mpz(mpz const & s) { mpz_init_set(m_val, s.m_val); }
-    mpz(mpz && s):mpz() { mpz_swap(m_val, s.m_val); }
-    ~mpz() { mpz_clear(m_val); }
+    mpz(mpz const & s);
+    mpz(mpz && s);
+    ~mpz();
 
-    void set(mpz_t r) const { mpz_set(r, m_val); }
+#ifdef LEAN_USE_GMP
+    void set(mpz_t r) const;
+#endif
 
-    friend void swap(mpz & a, mpz & b) { mpz_swap(a.m_val, b.m_val); }
+    friend void swap(mpz & a, mpz & b);
 
-    unsigned hash() const { return static_cast<unsigned>(mpz_get_si(m_val)); }
+    unsigned hash() const {
+#ifdef LEAN_USE_GMP
+        return static_cast<unsigned>(mpz_get_si(m_val));
+#else
+        return m_digits[0];
+#endif
+    }
 
-    int sgn() const { return mpz_sgn(m_val); }
+    int sgn() const {
+#ifdef LEAN_USE_GMP
+        return mpz_sgn(m_val);
+#else
+        return m_sign;
+#endif
+    }
     friend int sgn(mpz const & a) { return a.sgn(); }
     bool is_pos() const { return sgn() > 0; }
     bool is_neg() const { return sgn() < 0; }
@@ -54,39 +80,48 @@ public:
     bool is_nonpos() const { return !is_pos(); }
     bool is_nonneg() const { return !is_neg(); }
 
-    void neg() { mpz_neg(m_val, m_val); }
-    friend mpz neg(mpz a) { a.neg(); return a; }
-
-    void abs() { mpz_abs(m_val, m_val); }
-    friend mpz abs(mpz a) { a.abs(); return a; }
-
-    bool is_int() const { return mpz_fits_sint_p(m_val) != 0; }
-    bool is_unsigned_int() const { return mpz_fits_uint_p(m_val) != 0; }
-    bool is_long_int() const { return mpz_fits_slong_p(m_val) != 0; }
-    bool is_unsigned_long_int() const { return mpz_fits_ulong_p(m_val) != 0; }
-    bool is_size_t() const {
-        // GMP only features `fits` functions up to `unsigned long`, which is smaller than `size_t` on Windows.
-        // So we directly count the number of mpz words instead.
-        static_assert(sizeof(size_t) == sizeof(mp_limb_t), "GMP word size should be equal to system word size");
-        return is_nonneg() && mpz_size(m_val) <= 1;
+    void neg() {
+#ifdef LEAN_USE_GMP
+        mpz_neg(m_val, m_val);
+#else
+        m_sign = !m_sign;
+#endif
     }
 
-    long int get_long_int() const { lean_assert(is_long_int()); return mpz_get_si(m_val); }
-    int get_int() const { lean_assert(is_int()); return static_cast<int>(get_long_int()); }
-    unsigned long int get_unsigned_long_int() const { lean_assert(is_unsigned_long_int()); return mpz_get_ui(m_val); }
-    unsigned int get_unsigned_int() const { lean_assert(is_unsigned_int()); return static_cast<unsigned>(get_unsigned_long_int()); }
+    friend mpz neg(mpz a) { a.neg(); return a; }
+
+    void abs() {
+#ifdef LEAN_USE_GMP
+        mpz_abs(m_val, m_val);
+#else
+        m_sign = false;
+#endif
+    }
+
+    friend mpz abs(mpz a) { a.abs(); return a; }
+
+    bool is_int() const;
+    bool is_unsigned_int() const;
+    bool is_long_int() const;
+    bool is_unsigned_long_int() const;
+    bool is_size_t() const;
+
+    long int get_long_int() const;
+    int get_int() const;
+    unsigned long int get_unsigned_long_int() const;
+    unsigned int get_unsigned_int() const;
     size_t get_size_t() const;
 
-    mpz & operator=(mpz const & v) { mpz_set(m_val, v.m_val); return *this; }
+    mpz & operator=(mpz const & v);
     mpz & operator=(mpz && v) { swap(*this, v); return *this; }
-    mpz & operator=(char const * v) { mpz_set_str(m_val, v, 10); return *this; }
-    mpz & operator=(unsigned int v) { mpz_set_ui(m_val, v); return *this; }
-    mpz & operator=(int v) { mpz_set_si(m_val, v); return *this; }
+    mpz & operator=(char const * v);
+    mpz & operator=(unsigned int v);
+    mpz & operator=(int v);
 
-    friend int cmp(mpz const & a, mpz const & b) { return mpz_cmp(a.m_val, b.m_val); }
-    friend int cmp(mpz const & a, unsigned b) { return mpz_cmp_ui(a.m_val, b); }
-    friend int cmp(mpz const & a, unsigned long b) { return mpz_cmp_ui(a.m_val, b); }
-    friend int cmp(mpz const & a, int b) { return mpz_cmp_si(a.m_val, b); }
+    friend int cmp(mpz const & a, mpz const & b);
+    friend int cmp(mpz const & a, unsigned b);
+    friend int cmp(mpz const & a, unsigned long b);
+    friend int cmp(mpz const & a, int b);
 
     friend bool operator<(mpz const & a, mpz const & b) { return cmp(a, b) < 0; }
     friend bool operator<(mpz const & a, unsigned b) { return cmp(a, b) < 0; }
@@ -130,29 +165,30 @@ public:
     friend bool operator!=(unsigned a, mpz const & b) { return cmp(b, a) != 0; }
     friend bool operator!=(int a, mpz const & b) { return cmp(b, a) != 0; }
 
-    mpz & operator+=(mpz const & o) { mpz_add(m_val, m_val, o.m_val); return *this; }
-    mpz & operator+=(unsigned u) { mpz_add_ui(m_val, m_val, u); return *this; }
+    mpz & operator+=(mpz const & o);
+    mpz & operator+=(unsigned u);
+    mpz & operator+=(int u);
     mpz & operator+=(uint64 u) { return u > std::numeric_limits<unsigned>::max() ? *this += mpz(u) : *this += static_cast<unsigned>(u); }
-    mpz & operator+=(int u) { if (u >= 0) mpz_add_ui(m_val, m_val, u); else mpz_sub_ui(m_val, m_val, -u); return *this; }
 
-    mpz & operator-=(mpz const & o) { mpz_sub(m_val, m_val, o.m_val); return *this; }
-    mpz & operator-=(unsigned u) { mpz_sub_ui(m_val, m_val, u); return *this; }
+    mpz & operator-=(mpz const & o);
+    mpz & operator-=(unsigned u);
+    mpz & operator-=(int u);
     mpz & operator-=(uint64 u) { return u > std::numeric_limits<unsigned>::max() ? *this -= mpz(u) : *this -= static_cast<unsigned>(u); }
-    mpz & operator-=(int u) { if (u >= 0) mpz_sub_ui(m_val, m_val, u); else mpz_add_ui(m_val, m_val, -u); return *this; }
 
-    mpz & operator*=(mpz const & o) { mpz_mul(m_val, m_val, o.m_val); return *this; }
-    mpz & operator*=(unsigned u) { mpz_mul_ui(m_val, m_val, u); return *this; }
+    mpz & operator*=(mpz const & o);
+    mpz & operator*=(unsigned u);
+    mpz & operator*=(int u);
     mpz & operator*=(uint64 u) { return u > std::numeric_limits<unsigned>::max() ? *this *= mpz(u) : *this *= static_cast<unsigned>(u); }
-    mpz & operator*=(int u) { mpz_mul_si(m_val, m_val, u); return *this; }
 
-    mpz & operator/=(mpz const & o) { mpz_tdiv_q(m_val, m_val, o.m_val); return *this; }
-    mpz & operator/=(unsigned u) { mpz_tdiv_q_ui(m_val, m_val, u); return *this; }
+    mpz & operator/=(mpz const & o);
+    mpz & operator/=(unsigned u);
     mpz & operator/=(uint64 u) { return u > std::numeric_limits<unsigned>::max() ? *this /= mpz(u) : *this /= static_cast<unsigned>(u); }
     mpz & operator/=(int u) { return operator/=(mpz(u)); } // TODO(Leo): improve
 
-    friend mpz rem(mpz const & a, mpz const & b) { mpz r; mpz_tdiv_r(r.m_val, a.m_val, b.m_val); return r; }
-    mpz & operator%=(mpz const & o) { mpz r(*this % o); mpz_swap(m_val, r.m_val); return *this; }
-    mpz pow(unsigned int exp) const { mpz r; mpz_pow_ui(r.m_val, m_val, exp); return r; }
+    friend mpz rem(mpz const & a, mpz const & b);
+    mpz & operator%=(mpz const & o);
+
+    mpz pow(unsigned int exp) const;
 
     friend mpz operator+(mpz a, mpz const & b) { return a += b; }
     friend mpz operator+(mpz a, unsigned b)  { return a += b; }
@@ -188,20 +224,20 @@ public:
 
     friend mpz operator%(mpz const & a, mpz const & b);
 
-    mpz & operator&=(mpz const & o) { mpz_and(m_val, m_val, o.m_val); return *this; }
-    mpz & operator|=(mpz const & o) { mpz_ior(m_val, m_val, o.m_val); return *this; }
-    mpz & operator^=(mpz const & o) { mpz_xor(m_val, m_val, o.m_val); return *this; }
+    mpz & operator&=(mpz const & o);
+    mpz & operator|=(mpz const & o);
+    mpz & operator^=(mpz const & o);
 
     friend mpz operator&(mpz a, mpz const & b) { return a &= b; }
     friend mpz operator|(mpz a, mpz const & b) { return a |= b; }
     friend mpz operator^(mpz a, mpz const & b) { return a ^= b; }
 
     // a <- b * 2^k
-    friend void mul2k(mpz & a, mpz const & b, unsigned k) { mpz_mul_2exp(a.m_val, b.m_val, k); }
+    friend void mul2k(mpz & a, mpz const & b, unsigned k);
     // a <- b / 2^k
-    friend void div2k(mpz & a, mpz const & b, unsigned k) { mpz_tdiv_q_2exp(a.m_val, b.m_val, k); }
+    friend void div2k(mpz & a, mpz const & b, unsigned k);
     // a <- b % 2^k
-    friend void mod2k(mpz & a, mpz const & b, unsigned k) { mpz_tdiv_r_2exp(a.m_val, b.m_val, k); }
+    friend void mod2k(mpz & a, mpz const & b, unsigned k);
 
     /**
        \brief Return the position of the most significant bit.
@@ -209,11 +245,11 @@ public:
     */
     size_t log2() const;
 
-    friend void power(mpz & a, mpz const & b, unsigned k) { mpz_pow_ui(a.m_val, b.m_val, k); }
+    friend void power(mpz & a, mpz const & b, unsigned k);
     friend void _power(mpz & a, mpz const & b, unsigned k) { power(a, b, k); }
     friend mpz pow(mpz a, unsigned k) { power(a, a, k); return a; }
 
-    friend void gcd(mpz & g, mpz const & a, mpz const & b) { mpz_gcd(g.m_val, a.m_val, b.m_val); }
+    friend void gcd(mpz & g, mpz const & a, mpz const & b);
     friend mpz gcd(mpz const & a, mpz const & b) { mpz r; gcd(r, a, b); return r; }
 
     friend std::ostream & operator<<(std::ostream & out, mpz const & v);

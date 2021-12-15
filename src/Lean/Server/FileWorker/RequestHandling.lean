34430cd7c383c50907ae86b79a7983560a402b83
Leonardo de Moura <leonardo@microsoft.com>
2021-12-14 17:58:56 -0800

    fix: semantic highlighting for keywords of the form `#<alpha>...`
    closes #703

--- src/Lean/Server/FileWorker/RequestHandling.lean
+++ src/Lean/Server/FileWorker/RequestHandling.lean
@@ -425,7 +425,9 @@ where
           lastPos := ti.stx.getPos?.get!
   highlightKeyword stx := do
     if let Syntax.atom info val := stx then
-      if val.bsize > 0 && val[0].isAlpha then
+      if (val.length > 0 && val[0].isAlpha) ||
+         -- Support for keywords of the form `#<alpha>...`
+         (val.length > 1 && val[0] == '#' && val[1].isAlpha) then
         addToken stx SemanticTokenType.keyword
   addToken stx type := do
     let ⟨beginPos, endPos, text, _⟩ ← read

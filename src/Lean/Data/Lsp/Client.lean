5e7e5a0c6716bbd8e428bf77887462104c090ac2
Joscha <joscha@plugh.de>
2022-01-04 21:17:44 +0100

    style: add copyright headers

--- src/Lean/Data/Lsp/Client.lean
+++ src/Lean/Data/Lsp/Client.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2021 Joscha Mennicken. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Authors: Joscha Mennicken
+-/
+
 import Lean.Data.Lsp.Basic
 import Lean.Data.Json
 
--- src/Lean/Server/References.lean
+++ src/Lean/Server/References.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2021 Joscha Mennicken. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Authors: Joscha Mennicken
+-/
+
 import Init.System.IO
 import Lean.Data.Json
 import Lean.Data.Lsp

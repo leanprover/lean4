6349641d523e647a3c23f10a547cbb09e633d16a
Joscha <joscha@plugh.de>
2022-01-04 18:50:35 +0100

    refactor: move some reference-related types to Lean.Data
    These types are required for worker->watchdog notifications.

--- src/Lean/Data/Lsp.lean
+++ src/Lean/Data/Lsp.lean
@@ -11,6 +11,7 @@ import Lean.Data.Lsp.Communication
 import Lean.Data.Lsp.Diagnostics
 import Lean.Data.Lsp.Extra
 import Lean.Data.Lsp.InitShutdown
+import Lean.Data.Lsp.Internal
 import Lean.Data.Lsp.LanguageFeatures
 import Lean.Data.Lsp.TextSync
 import Lean.Data.Lsp.Utf16

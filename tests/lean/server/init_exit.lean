bfa9299fdff82a8bc7648ec11e3761ecc734dfa8
Sebastian Ullrich <sebasti@nullri.ch>
2022-01-06 19:21:40 +0100

    chore: fix servertest_init_exit

--- tests/lean/server/init_exit.lean
+++ tests/lean/server/init_exit.lean
@@ -7,6 +7,7 @@ def main : IO Unit := do
     hIn.write (←FS.readBinFile "init_vscode_1_47_2.log")
     hIn.flush
     let initResp ← Ipc.readResponseAs 0 InitializeResult
+    let regWatchReq ← Ipc.readRequestAs "client/registerCapability" Json
     Ipc.writeNotification ⟨"initialized", InitializedParams.mk⟩ 
 
     Ipc.writeRequest ⟨1, "shutdown", Json.null⟩

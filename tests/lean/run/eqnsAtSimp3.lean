90b179bea97fe90c88e353262151b88e4010ff0c
Leonardo de Moura <leonardo@microsoft.com>
2022-01-06 13:53:03 -0800

    fix: add equation theorems even if definition supports smart unfolding
    See new test.

--- tests/lean/run/eqnsAtSimp3.lean
+++ tests/lean/run/eqnsAtSimp3.lean
@@ -1,10 +1,3 @@
-import Lean
-
-open Lean
-open Lean.Meta
-def tst (declName : Name) : MetaM Unit := do
-  IO.println (← getEqnsFor? declName)
-
 def f (x y : Nat) : Nat :=
   match x, y with
   | 0,   0 => 1
@@ -12,8 +5,21 @@ def f (x y : Nat) : Nat :=
   | x+1, 5 => 2 * f x 0
   | x+1, y => 2 * f x y
 
-#eval tst ``f
-#check f._eq_1
-#check f._eq_2
-#check f._eq_3
-#check f._eq_4
+theorem ex1 (x : Nat) (y : Nat) (h : y ≠ 5) : ∃ z, f (x+1) y = 2 * z := by
+  simp [f, h]
+  trace_state
+  apply Exists.intro
+  rfl
+
+@[simp] def g (x y : Nat) : Nat :=
+  match x, y with
+  | 0,   0 => 1
+  | 0,   y => y
+  | x+1, 5 => 2 * g x 0
+  | x+1, y => 2 * g x y
+
+theorem ex2 (x : Nat) (y : Nat) (h : y ≠ 5) : ∃ z, g (x+1) y = 2 * z := by
+  simp [h]
+  trace_state
+  apply Exists.intro
+  rfl

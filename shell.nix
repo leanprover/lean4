34cb5c207d90e1e27c6bcd2ed95fd773135c59e7
Sebastian Ullrich <sebasti@nullri.ch>
2022-01-10 14:30:06 +0100

    chore: CI: put llvm-symbolizer in PATH for asan/lsan backtraces

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
 let
   flake = (import ./default.nix);
   flakePkgs = flake.packages.${builtins.currentSystem};
-in { pkgs ? flakePkgs.nixpkgs, pkgsDist ? pkgs, llvmPackages ? null }:
+in { pkgs ? flakePkgs.nixpkgs, pkgsDist ? pkgs }:
 # use `shell` as default
 (attribs: attribs.shell // attribs) rec {
   shell = pkgs.mkShell.override {
-    stdenv = pkgs.overrideCC pkgs.stdenv (if llvmPackages == null
-                                          then flakePkgs.llvmPackages
-                                          else pkgs.${"llvmPackages_${llvmPackages}"}).clang;
+    stdenv = pkgs.overrideCC pkgs.stdenv flakePkgs.llvmPackages.clang;
   } (rec {
-    buildInputs = with pkgs; [ cmake gmp ccache ];
+    buildInputs = with pkgs; [
+      cmake gmp ccache
+      llvmPackages.llvm  # llvm-symbolizer for asan/lsan
+    ];
     # https://github.com/NixOS/nixpkgs/issues/60919
     hardeningDisable = [ "all" ];
     # more convenient `ctest` output

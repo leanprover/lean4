1620987b6ce40c98aaca78393e1ef15b261e6400
Leonardo de Moura <leonardo@microsoft.com>
2022-01-13 09:56:33 -0800

    fix: recursive applications in discriminants

---
+++ tests/lean/run/bubble.lean
@@ -0,0 +1,14 @@
+def List.bubblesort [LT α] [DecidableRel (. < . : α → α → Prop)] (l : List α) : {l' : List α // l.length = l'.length} :=
+  match l with
+  | [] => ⟨[], rfl⟩
+  | x :: xs =>
+    match bubblesort xs with
+    | ⟨[], h⟩ => ⟨[x], by simp[h]⟩
+    | ⟨y :: ys, h⟩ =>
+      if y < x then
+        have : Nat.succ (length ys) < Nat.succ (length xs) := by rw [h, List.length_cons]; apply Nat.lt_succ_self
+        let ⟨zs, he⟩ := bubblesort (x :: ys)
+        ⟨y :: zs, by simp[h, ← he]⟩
+      else
+        ⟨x :: y :: ys, by simp[h]⟩
+termination_by _ => l.length

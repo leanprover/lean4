705962847df61e93265547af9e9bd7cc5e3847d3
tydeu <tydeu@hatpress.net>
2022-01-30 22:49:05 -0500

    fix: glob bugs + cleanup
    closes leanprover/lake#42

--- Lake/Config/Glob.lean
+++ Lake/Config/Glob.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Mac Malone
 -/
 import Lean.Data.Name
+import Lean.Elab.Import
 
 open Lean (Name)
 open System (FilePath)
@@ -27,30 +28,24 @@ def Glob.matches (m : Name) : (self : Glob) → Bool
 | submodules n => n.isPrefixOf m && n != m
 | andSubmodules n => n.isPrefixOf m
 
--- TODO(Mario): Rename Lean.modToFilePath.go to modToDir
-def modToDir (base : FilePath) : Name → FilePath
-  | Name.str p h _ => modToDir base p / h
-  | Name.anonymous => base
-  | Name.num p _ _ => panic! "ill-formed import"
-
 partial def pushSubmodules
-(dir : FilePath) (root : Name) (arr : Array Name) : IO (Array Name) := do
+(rootDir : FilePath) (mod : Name) (arr : Array Name) : IO (Array Name) := do
   let mut arr := arr
-  let dirPath := modToDir dir root
+  let dirPath := Lean.modToFilePath rootDir mod ""
   for entry in (← dirPath.readDir) do
     let path := entry.path
-    if (← path.isDir : Bool) then
-      arr ← pushSubmodules path (root ++ entry.fileName) arr
+    if (← path.isDir) then
+      arr ← pushSubmodules rootDir (mod ++ entry.fileName) arr
     else if path.extension == some "lean" then
       let fileName := FilePath.withExtension entry.fileName "" |>.toString
-      arr := arr.push (root ++ fileName)
+      arr := arr.push (mod ++ fileName)
   return arr
 
-def Glob.pushModules (dir : FilePath) (arr : Array Name)
-: (self : Glob) → IO (Array Name)
+def Glob.pushModules
+(dir : FilePath) (arr : Array Name) : (self : Glob) → IO (Array Name)
 | one n => #[n]
 | submodules n => pushSubmodules dir n arr
 | andSubmodules n => pushSubmodules dir n (arr.push n)
 
-def Glob.getModules (dir : FilePath)  (self : Glob) :=
-  pushModules dir #[]
+def Glob.getModules (dir : FilePath) (self : Glob) : IO (Array Name) :=
+  self.pushModules dir #[]

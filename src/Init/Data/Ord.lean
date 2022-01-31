2ca5d7bd291af70ce6ada73feea111170a28ee16
larsk21 <57503246+larsk21@users.noreply.github.com>
2022-01-31 22:14:53 +0100

    fix: remove explicit Ord Range

--- src/Init/Data/Ord.lean
+++ src/Init/Data/Ord.lean
@@ -12,14 +12,6 @@ inductive Ordering where
   | lt | eq | gt
 deriving Inhabited, BEq
 
-namespace Ordering
-
-def lexOrdering (orderings : List Ordering) : Ordering :=
-  orderings.foldr (init := Ordering.eq) fun ordering result =>
-    if ordering == Ordering.eq then result else ordering
-
-end Ordering
-
 
 class Ord (α : Type u) where
   compare : α → α → Ordering

569b6f99d9b0744fd68ca3dff741732b7eec0224
Gabriel Ebner <gebner@gebner.org>
2022-02-03 15:20:04 +0100

    fix: dependent fields in diamond extensions

---
+++ tests/lean/diamond9.lean
@@ -0,0 +1,17 @@
+class Zero (A : Type u) where zero : A
+instance {A} [Zero A] : OfNat A (nat_lit 0) := ⟨Zero.zero⟩
+
+class AddGroup (A : Type u) extends Zero A where
+  gsmul : Int → A → A
+  gsmul_zero' : ∀ a, gsmul 0 a = 0
+
+class Ring (R : Type u) extends Zero R, AddGroup R
+
+#print Ring.mk
+
+#check {
+  zero := 0
+  gsmul := fun x n => x.toNat * n
+  gsmul_zero' := fun a => Nat.zero_mul _
+  : Ring Nat
+}

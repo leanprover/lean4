3ae455bccfd7406432e72a6a37214528141abd63
Leonardo de Moura <leonardo@microsoft.com>
2022-02-04 17:26:50 -0800

    feat: add `mkCongrSimp?`
    TODO: proof is still missing
    
    see #988

---
+++ tests/lean/run/congrThm.lean
@@ -0,0 +1,12 @@
+import Lean
+
+open Lean
+open Lean.Meta
+
+def test (f : Expr) : MetaM Unit := do
+  let some thm  ← mkCongrSimp? f | unreachable!
+  check thm.type
+  IO.println (← Meta.ppExpr thm.type)
+
+#eval test (mkConst ``decide)
+#eval test (mkConst ``Array.uget [levelZero])

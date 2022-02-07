baa95263b58b89b438434100ae3378098dd30b13
Gabriel Ebner <gebner@gebner.org>
2022-02-07 13:40:39 +0100

    fix: refs to copied subobjects in diamond extension

---
+++ tests/lean/diamond10.lean
@@ -0,0 +1,12 @@
+class Zero (A : Type u) where zero : A
+instance {A} [Zero A] : OfNat A (nat_lit 0) := ⟨Zero.zero⟩
+
+class AddMonoid (A : Type u) extends Add A, Zero A
+class Semiring (R : Type u) extends AddMonoid R
+class SubNegMonoid (A : Type u) extends AddMonoid A, Neg A
+class AddGroup (A : Type u) extends SubNegMonoid A where
+  add_left_neg (a : A) : -a + a = 0
+
+class Ring (R : Type u) extends Semiring R, AddGroup R
+
+#print Ring.mk

38da48c5cfbd982d5e07ae503cecffbb7ad03189
Leonardo de Moura <leonardo@microsoft.com>
2022-02-25 16:35:36 -0800

    refactor: `LinearArith/Basic.lean` => `LinearArith/Solver.lean`

--- src/Lean/Meta/Tactic/LinearArith.lean
+++ src/Lean/Meta/Tactic/LinearArith.lean
@@ -3,5 +3,5 @@ Copyright (c) 2022 Sebastian Ullrich. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sebastian Ullrich
 -/
-import Lean.Meta.Tactic.LinearArith.Basic
+import Lean.Meta.Tactic.LinearArith.Solver
 import Lean.Meta.Tactic.LinearArith.Nat

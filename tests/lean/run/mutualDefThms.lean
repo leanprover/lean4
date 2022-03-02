e091e7fb3088a63445b8d8de22a05240c116ee04
Leonardo de Moura <leonardo@microsoft.com>
2022-03-02 13:57:43 -0800

    test: simplify proofs

--- tests/lean/run/mutualDefThms.lean
+++ tests/lean/run/mutualDefThms.lean
@@ -5,11 +5,11 @@ inductive Foo where
 namespace Foo
 
 mutual
-  def textLengthList : List Foo → Nat
+  @[simp] def textLengthList : List Foo → Nat
     | [] => 0
     | f::fs => textLength f + textLengthList fs
 
-  def textLength : Foo → Nat
+  @[simp] def textLength : Foo → Nat
     | text s => s.length
     | element children => textLengthList children
 end
@@ -18,14 +18,14 @@ def concat (f₁ f₂ : Foo) : Foo :=
   Foo.element [f₁, f₂]
 
 theorem textLength_concat (f₁ f₂ : Foo) : textLength (concat f₁ f₂) = textLength f₁ + textLength f₂ := by
-  simp [concat, textLength, textLengthList]
+  simp [concat]
 
 mutual
-  def flatList : List Foo → List String
+  @[simp] def flatList : List Foo → List String
     | [] => []
     | f :: fs => flat f ++ flatList fs
 
-  def flat : Foo → List String
+  @[simp] def flat : Foo → List String
     | text s => [s]
     | element children => flatList children
 end
@@ -33,27 +33,29 @@ end
 def listStringLen (xs : List String) : Nat :=
   xs.foldl (init := 0) fun sum s => sum + s.length
 
+attribute [simp] List.foldl
+
 theorem foldl_init (s : Nat) (xs : List String) :  (xs.foldl (init := s) fun sum s => sum + s.length) = s + (xs.foldl (init := 0) fun sum s => sum + s.length) := by
   induction xs generalizing s with
-  | nil => simp [List.foldl]
-  | cons x xs ih => simp [List.foldl, ih x.length, ih (s + x.length)]; simp_arith
+  | nil => simp
+  | cons x xs ih => simp_arith [ih x.length, ih (s + x.length)]
 
 theorem listStringLen_append (xs ys : List String) : listStringLen (xs ++ ys) = listStringLen xs + listStringLen ys := by
   simp [listStringLen]
   induction xs with
-  | nil => simp [List.foldl]
-  | cons x xs ih => simp [List.foldl, foldl_init x.length, ih]; simp_arith
+  | nil => simp
+  | cons x xs ih => simp_arith [foldl_init x.length, ih]
 
 mutual
   theorem listStringLen_flat (f : Foo) : listStringLen (flat f) = textLength f := by
     match f with
-    | text s => simp [flat, textLength, listStringLen, List.foldl]
-    | element cs => simp [textLength, flat, listStringLen_flatList cs]
+    | text s => simp [listStringLen]
+    | element cs => simp [listStringLen_flatList cs]
 
   theorem listStringLen_flatList (cs : List Foo) : listStringLen (flatList cs) = textLengthList cs := by
     match cs with
     | [] => simp
-    | f :: fs => simp [flatList, textLengthList, listStringLen_append, listStringLen_flatList fs, listStringLen_flat f]
+    | f :: fs => simp [listStringLen_append, listStringLen_flatList fs, listStringLen_flat f]
 end
 
 end Foo

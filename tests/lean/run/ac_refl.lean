84f72b938936a1286f7c8138e259796c970c4e7a
Daniel Fabian <daniel.fabian@integral-it.ch>
2022-03-04 15:18:36 +0000

    test: add further ac_refl tests.

--- tests/lean/run/ac_refl.lean
+++ tests/lean/run/ac_refl.lean
@@ -21,7 +21,7 @@ theorem max_comm (n m : Nat) : max n m = max m n := by
   case inr h₁ h₂ => simp [Nat.not_lt_eq] at *; apply Nat.le_antisymm <;> assumption
 
 theorem max_idem (n : Nat) : max n n = n := by
-  simp [max]; split <;> rfl
+  simp [max]
 
 theorem Nat.zero_max (n : Nat) : max 0 n = n := by
   simp [max]; rfl
@@ -34,17 +34,17 @@ instance : IsCommutative (α := Nat) max := ⟨max_comm⟩
 instance : IsIdempotent (α := Nat) max := ⟨max_idem⟩
 instance : IsNeutral max 0 := ⟨Nat.zero_max, Nat.max_zero⟩
 
-example (x y z : Nat) : x + y + 0 + z = z + (x + y) := by
-  ac_refl
+example (x y z : Nat) : x + y + 0 + z = z + (x + y) := by ac_refl
 
-example (x y z : Nat) : (x + y) * (0 + z) = (x + y) * z:= by
-  ac_refl
+example (x y z : Nat) : (x + y) * (0 + z) = (x + y) * z:= by ac_refl
 
-example (x y z : Nat) : (x + y) * (0 + z) = 1 * z * (y + 0 + x) := by
-  ac_refl
+example (x y z : Nat) : (x + y) * (0 + z) = 1 * z * (y + 0 + x) := by ac_refl
 
-example (x y z : Nat) : max (0 + (max x (max z (max (0 + 0) ((max 1 0) + 0 + 0) * y)))) y = max (max x y) z := by
-  ac_refl
+example (x y z : Nat) : max (0 + (max x (max z (max (0 + 0) ((max 1 0) + 0 + 0) * y)))) y = max (max x y) z := by ac_refl
 
-example (x y : Nat) : 1 + 0 + 0 = 0 + 1 := by
-  ac_refl
+example (x y : Nat) : 1 + 0 + 0 = 0 + 1 := by ac_refl
+
+example (x y : Nat) : (x + y = 42) = (y + x = 42) := by ac_refl
+
+example (x y : Nat) (P : Prop) : (x + y = 42 → P) = (y + x = 42 → P) := by ac_refl
+

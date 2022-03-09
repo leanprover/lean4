74411aa472c7ff47cd59fcd7ee5c1b3bab133884
Leonardo de Moura <leonardo@microsoft.com>
2022-03-09 12:32:11 -0800

    fix: do not display implicit fields

---
+++ tests/lean/tabulate.lean
@@ -0,0 +1,13 @@
+inductive Vector (α : Type u) : Nat → Type u
+  | nil : Vector α 0
+  | cons : α → Vector α n → Vector α (n+1)
+  deriving Repr
+
+infix:67 " :: " => Vector.cons
+
+def tabulate (f : Fin n → α) : Vector α n :=
+  match n with
+  | 0   => Vector.nil
+  | n+1 => f 0 :: tabulate (f ∘ Fin.succ)
+
+#eval tabulate fun i : Fin 5 => i

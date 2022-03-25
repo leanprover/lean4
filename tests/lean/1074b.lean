7307b02fd7e518eceb80ed96498123c40d729157
Leonardo de Moura <leonardo@microsoft.com>
2022-03-25 15:19:01 -0700

    fix: missing test at `has_trivial_structure`
    see #1074

---
+++ tests/lean/1074b.lean
@@ -0,0 +1,12 @@
+inductive Term
+| id: Term -> Term
+
+inductive Brx: Term -> Prop
+| id: Brx z -> Brx (Term.id n)
+
+def Brx.interp {a} (H: Brx a): Nat :=
+  match a with
+  | Term.id n => by
+    have ⟨Hn, Hz⟩: True ∧ Brx z
+      := by cases H <;> exact ⟨by simp, by assumption⟩;
+    exact Hz.interp

334f4f6c85f69b8997fa2475a37d0f6e7e8da071
Leonardo de Moura <leonardo@microsoft.com>
2022-03-28 07:19:55 -0700

    test: for issue #1079
    The error reported on issue #1079 does not happen in the master branch.
    
    closes #1079

---
+++ tests/lean/1079.lean
@@ -0,0 +1,14 @@
+theorem bad : ∀ (m n : Nat), (if m = n then Ordering.eq else Ordering.gt) = Ordering.lt → False := by
+  intros m n
+  cases (Nat.decEq m n) with -- an error as expected: "alternative `isFalse` has not bee provided"
+  | isTrue h =>
+    set_option trace.Meta.Tactic true in
+    simp [h]
+
+theorem bad' : ∀ (m n : Nat), (if m = n then Ordering.eq else Ordering.gt) = Ordering.lt → False := by
+  intros m n
+  cases (Nat.decEq m n) with
+  | isTrue h =>
+    simp [h]
+  | isFalse h =>
+    simp [h]

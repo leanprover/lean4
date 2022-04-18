bb2df569bcc4caeee991a54ea696c026b0518a51
Leonardo de Moura <leonardo@microsoft.com>
2022-04-18 14:56:22 -0700

    fix: bug at `declare_config_elab`

--- src/Lean/Elab/Tactic/Config.lean
+++ src/Lean/Elab/Tactic/Config.lean
@@ -16,9 +16,11 @@ macro "declare_config_elab" elabName:ident type:ident : command =>
      if optConfig.isNone then
        return { : $type }
      else
-       withoutModifyingState <| withLCtx {} {} <| withSaveInfoContext <| Term.withSynthesize do
+       let c ← withoutModifyingState <| withLCtx {} {} <| withSaveInfoContext <| Term.withSynthesize do
          let c ← Term.elabTermEnsuringType optConfig[0][3] (Lean.mkConst ``$type)
-         eval (← instantiateMVars c)
+         Term.synthesizeSyntheticMVarsNoPostponing
+         instantiateMVars c
+       eval c
   )
 
 end Lean.Elab.Tactic

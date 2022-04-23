ed12b62e28cece4f968a661d1c515d4dc72437d4
Leonardo de Moura <leonardo@microsoft.com>
2022-04-23 12:08:59 -0700

    fix: `InfoTree` was missing information for (pseudo) `match` patterns such as `x + 1`.
    This kind of pattern has to be reduced to a constructor, and the
    `PatternWithRef` information was being lost in the process.

--- tests/lean/interactive/definition.lean
+++ tests/lean/interactive/definition.lean
@@ -4,3 +4,9 @@ example : Foo :=
   let c := Foo.foo
   c
 --^ textDocument/typeDefinition
+
+def f (x : Nat) : Nat :=
+  match x with
+  | 0 => 1
+  | y + 1 => y
+           --^ textDocument/declaration

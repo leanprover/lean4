c2ba34f2dadcabf145523414f35afb9fe14d1c9c
Sebastian Ullrich <sebasti@nullri.ch>
2022-04-28 10:57:43 +0200

    fix: do not show states from tactics indented further than the cursor

--- tests/lean/interactive/infoIssues.lean
+++ tests/lean/interactive/infoIssues.lean
@@ -1,28 +1,30 @@
-#exit
-
+set_option trace.Elab.info true
 example (x y : Nat) (h : x = y) : 0 + x = y + 0 := by
   have := by exact h.symm
-  /-
-    Bad: we get "Goals accomplished" here
-    Should be:
-    ... |- 0 + x = y + 0
-  -/
+--
+--^ $/lean/plainGoal
+-- Good: ..., this : y = x |- 0 + x = y + 0
 
 example (x : Nat) : x = 0 + (0 + (0 + x)) ∧ p := by
   constructor
   · cases x
-    next => rfl                --v Good: we get multiple states here as expected
-    next => repeat rw [Nat.zero_add] /- Good: we get the correct behavior here: Goals accomplished. -/
-  /-
-    Bad: We get "Goals accomplished" here
-    Should be:
-    ... |- p
-  -/
+    next => rfl
+                               -- Good: we get multiple states here as expected
+                               --v $/lean/plainGoal
+    next => repeat rw [Nat.zero_add]
+                                  --^ $/lean/plainGoal
+                                  -- Bad? We still get all states except the one closed by implicit `rfl`
+--^ $/lean/plainGoal
+-- Good: ... |- p
 
 example (x : Nat) : x = 0 + (0 + (0 + x)) ∧ p := by
   constructor
   match x with
   | 0 => rfl
-  | y+1 => repeat rw [Nat.zero_add] /- Good: We get the correct behavior here: Goals accomplished. -/
-       --^ Bad: get the `match` tactic initial state here.
-  -- Bad: we get "Goals accomplished" here
+                                 -- Bad? We still get all states except the one closed by implicit `rfl`
+                                 --v $/lean/plainGoal
+  | y+1 => repeat rw [Nat.zero_add]
+       --^ $/lean/plainGoal
+       -- Bad: get the `match` tactic initial state here.
+--^ $/lean/plainGoal
+-- Good:  ... |- p

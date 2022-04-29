24417ed466b0f22963a7754bdaa8353776fe127f
Leonardo de Moura <leonardo@microsoft.com>
2022-04-29 09:55:45 -0700

    feat: `size`, `get`, `get!`, `getD`, and `getOp` for `Subarray`

--- src/Init/Data/Nat/Basic.lean
+++ src/Init/Data/Nat/Basic.lean
@@ -610,6 +610,15 @@ theorem le_sub_of_add_le {a b c : Nat} (h : a + b ≤ c) : a ≤ c - b := by
     have hd := Nat.sub_eq_of_eq_add hd.symm
     exact hd.symm
 
+theorem add_lt_of_lt_sub {a b c : Nat} (hle : b ≤ c) (h : a < c - b) : a + b < c := by
+  have : a.succ + b ≤ c := add_le_of_le_sub hle h
+  simp [Nat.succ_add] at this
+  exact this
+
+theorem lt_sub_of_add_lt {a b c : Nat} (h : a + b < c) : a < c - b :=
+  have : a.succ + b ≤ c := by simp [Nat.succ_add]; exact h
+  le_sub_of_add_le this
+
 @[simp] protected theorem pred_zero : pred 0 = 0 :=
   rfl
 

4eb2cfec466bfebfdca7633fce24e6b0f5cba68c
Leonardo de Moura <leonardo@microsoft.com>
2022-05-01 07:25:45 -0700

    feat: make sure `case' ... => tac` does not use `done` after `tac`

--- src/Lean/Elab/Tactic/BuiltinTactic.lean
+++ src/Lean/Elab/Tactic/BuiltinTactic.lean
@@ -303,8 +303,7 @@ def renameInaccessibles (mvarId : MVarId) (hs : Array Syntax) : TacticM MVarId :
     assignExprMVar mvarId mvarNew
     return mvarNew.mvarId!
 
-private def evalCaseCore (stx : Syntax) (tag : Syntax) (hs : Array Syntax) (arr : Syntax) (tac : Syntax)
-    (closeOnFailure : Bool) : TacticM Unit := do
+private def getCaseGoals (tag : Syntax) : TacticM (MVarId × List MVarId) := do
   let gs ← getUnsolvedGoals
   let g ←
     if tag.isIdent then
@@ -313,28 +312,30 @@ private def evalCaseCore (stx : Syntax) (tag : Syntax) (hs : Array Syntax) (arr 
       pure g
     else
       getMainGoal
-  let gs := gs.erase g
-  let g ← renameInaccessibles g hs
-  setGoals [g]
-  let savedTag ← getMVarTag g
-  setMVarTag g Name.anonymous
-  try
-    if closeOnFailure then
-      withCaseRef arr tac do
-        closeUsingOrAdmit (withTacticInfoContext stx (evalTactic tac))
-    else
-      evalTactic tac
-  finally
-    setMVarTag g savedTag
-  done
-  setGoals gs
+  return (g, gs.erase g)
 
 @[builtinTactic «case»] def evalCase : Tactic
-  | stx@`(tactic| case $tag $hs* =>%$arr $tac:tacticSeq) => evalCaseCore stx tag hs arr tac (closeOnFailure := true)
+  | stx@`(tactic| case $tag $hs* =>%$arr $tac:tacticSeq) => do
+    let (g, gs) ← getCaseGoals tag
+    let g ← renameInaccessibles g hs
+    setGoals [g]
+    setMVarTag g Name.anonymous
+    withCaseRef arr tac do
+      closeUsingOrAdmit (withTacticInfoContext stx (evalTactic tac))
+    setGoals gs
   | _ => throwUnsupportedSyntax
 
 @[builtinTactic «case'»] def evalCase' : Tactic
-  | stx@`(tactic| case' $tag $hs* =>%$arr $tac:tacticSeq) => evalCaseCore stx tag hs arr tac (closeOnFailure := false)
+  | stx@`(tactic| case' $tag $hs* =>%$arr $tac:tacticSeq) => do
+    let (g, gs) ← getCaseGoals tag
+    let g ← renameInaccessibles g hs
+    let mvarTag ← getMVarTag g
+    setGoals [g]
+    withCaseRef arr tac (evalTactic tac)
+    let gs' ← getUnsolvedGoals
+    if let [g'] := gs' then
+      setMVarTag g' mvarTag
+    setGoals (gs' ++ gs)
   | _ => throwUnsupportedSyntax
 
 @[builtinTactic «renameI»] def evalRenameInaccessibles : Tactic

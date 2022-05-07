af5e13e5345359a93f24c3831feec97d4619dbf0
Leonardo de Moura <leonardo@microsoft.com>
2022-05-07 10:20:40 -0700

    feat: improve `binop%` elaboration function

--- tests/lean/run/hmul2.lean
+++ tests/lean/run/hmul2.lean
@@ -1,5 +1,13 @@
+@[defaultInstance]
 instance [Mul α] : HMul α (Array α) (Array α) where
   hMul a as := as.map (a * ·)
+
+instance [Mul α] : Mul (Array α) where
+  mul as bs := (as.zip bs).map fun (a, b) => a * b
+
+#eval 2 * #[3, 4, 5]
+
+#eval (2:Nat) * #[3, 4, 5]
 
 #check fun x => x * 2
 #check fun y : Int => let  x := 1; x * y
@@ -20,8 +28,6 @@ def f4 (n : Nat) (i : Int) :=
 def f5 (n : Nat) (i : Int) :=
   n + (n + (n * (i, i).1))
 
-#eval (2:Nat) * #[3, 4, 5]
-
 def f6 (n : Nat) (i : Int) :=
   let y := 2 * i
   let x := y * n
@@ -32,3 +38,15 @@ def f7 (n : Nat) (i : Int) :=
 
 def f8 (n : Nat) (i : Int) :=
   n * 2 + (i * 1 * n * 2 * i)
+
+def f9 [Mul α] (a : α) (as bs : Array α) : Array α :=
+  a * as * bs
+
+def f10 (a : Int) (as bs : Array Int) : Array Int :=
+  2 * a * as * bs
+
+def f11 (a : Int) (as bs : Array Int) : Array Int :=
+  3 * a * as * (2 * bs)
+
+def f12 [Mul α] (as bs : Array α) : Array α :=
+  as.foldl (init := bs) fun bs a => a * bs

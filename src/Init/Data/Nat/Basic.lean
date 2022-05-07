155b41937a034d581d7078e58ea639c2828c6ccc
François G. Dorais <fgdorais@gmail.com>
2022-05-07 18:39:37 -0400

    fix: remove unnecessary hypothesis (#1144)

--- src/Init/Data/Nat/Basic.lean
+++ src/Init/Data/Nat/Basic.lean
@@ -610,7 +610,13 @@ theorem le_sub_of_add_le {a b c : Nat} (h : a + b ≤ c) : a ≤ c - b := by
     have hd := Nat.sub_eq_of_eq_add hd.symm
     exact hd.symm
 
-theorem add_lt_of_lt_sub {a b c : Nat} (hle : b ≤ c) (h : a < c - b) : a + b < c := by
+theorem add_lt_of_lt_sub {a b c : Nat} (h : a < c - b) : a + b < c := by
+  have hle : b ≤ c := by
+    apply Nat.ge_of_not_lt
+    intro hgt
+    apply Nat.not_lt_zero a
+    rw [Nat.sub_eq_zero_of_le (Nat.le_of_lt hgt)] at h
+    exact h
   have : a.succ + b ≤ c := add_le_of_le_sub hle h
   simp [Nat.succ_add] at this
   exact this

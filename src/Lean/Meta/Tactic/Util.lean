2fc23a2a2b3d61e917d41de504577156d01ee227
Leonardo de Moura <leonardo@microsoft.com>
2022-05-23 11:50:32 -0700

    feat: make sure we can use `split` to synthesize code

--- src/Lean/Meta/Tactic/Util.lean
+++ src/Lean/Meta/Tactic/Util.lean
@@ -29,9 +29,9 @@ def mkFreshExprSyntheticOpaqueMVar (type : Expr) (tag : Name := Name.anonymous) 
 
 def throwTacticEx {α} (tacticName : Name) (mvarId : MVarId) (msg : MessageData) (ref := Syntax.missing) : MetaM α :=
   if msg.isEmpty then
-    throwError "tactic '{tacticName}' failed\n{MessageData.ofGoal mvarId}"
+    throwError "tactic '{tacticName}' failed\n{mvarId}"
   else
-    throwError "tactic '{tacticName}' failed, {msg}\n{MessageData.ofGoal mvarId}"
+    throwError "tactic '{tacticName}' failed, {msg}\n{mvarId}"
 
 def throwNestedTacticEx {α} (tacticName : Name) (ex : Exception) : MetaM α := do
   throwError "tactic '{tacticName}' failed, nested error:\n{ex.toMessageData}"

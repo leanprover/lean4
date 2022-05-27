3be437cad378e848da063ef16e67b8eca7ff73c0
Leonardo de Moura <leonardo@microsoft.com>
2022-05-26 19:49:33 -0700

    fix: make sure `register_simp_attr` declares an simp-like attribute parser for user `simp` attributes
    closes #1164

--- tests/pkg/user_attr/UserAttr/Tst.lean
+++ tests/pkg/user_attr/UserAttr/Tst.lean
@@ -1,3 +1,4 @@
+import Lean
 import UserAttr.BlaAttr
 
 @[bla] def f (x : Nat) := x + 2
@@ -21,3 +22,29 @@ macro "my_simp" : tactic => `(simp [my_simp])
 
 example : f x = id (x + 2) := by
   my_simp
+
+
+@[simp low, my_simp low]
+theorem expand_mul_add (x y z : Nat) : x * (y + z) = x * y + x * y := sorry
+@[simp high, my_simp high]
+theorem expand_add_mul (x y z : Nat) : (x + y) * z = x * z + y * z := sorry
+@[simp, my_simp]
+theorem lassoc_add (x y z : Nat) : x + (y + z) = x + y + z := sorry
+
+set_option trace.Meta.Tactic.simp.rewrite true
+
+-- Rewrites: expand_mul_add -> expand_mul_add -> lassoc_add
+theorem ex1 (x : Nat) : (x + x) * (x + x) = x * x + x * x + x * x + x * x := by simp only [my_simp]
+
+-- Rewrites: expand_add_mul -> expand_mul_add -> lassoc_add
+theorem ex2 (x : Nat) : (x + x) * (x + x) = x * x + x * x + x * x + x * x := by simp
+
+open Lean
+open Lean.Meta
+def checkProofs : MetaM Unit := do
+  let ConstantInfo.thmInfo info1 ← getConstInfo `ex1 | throwError "unexpected"
+  let ConstantInfo.thmInfo info2 ← getConstInfo `ex2 | throwError "unexpected"
+  unless info1.value == info2.value do
+    throwError "unexpected values"
+
+#eval checkProofs

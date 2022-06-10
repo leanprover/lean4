e6e2c2ab72ab5f51f3ee2ab2b0c461f80dffa530
tydeu <tydeu@hatpress.net>
2022-06-10 15:25:34 -0400

    test: make  `git` example clone local repo

--- examples/git/lakefile.lean
+++ examples/git/lakefile.lean
@@ -4,8 +4,8 @@ open System Lake DSL
 
 package git_hello
 
-require hello from git
-  "https://github.com/leanprover/lake.git"@"master"/"examples"/"hello"
+require hello from
+  git "../.."@"master"/"examples"/"hello"
 
 @[defaultTarget]
 lean_exe git_hello {

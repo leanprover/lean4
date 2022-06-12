4d05803782e6561d961bf2d77f8644278ab6c0da
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-12 14:57:03 -0400

    feat: WF lemma for Nat division

--- src/Init/Data/Nat/Div.lean
+++ src/Init/Data/Nat/Div.lean
@@ -39,6 +39,31 @@ theorem div.inductionOn.{u}
     base x y h
 decreasing_by apply div_rec_lemma; assumption
 
+theorem div_le_self (n k : Nat) : n / k ≤ n := by
+  induction n using Nat.strongInductionOn with
+  | ind n ih =>
+    rw [div_eq]
+    -- Note: manual split to avoid Classical.em which is not yet defined
+    cases (inferInstance : Decidable (0 < k ∧ k ≤ n)) with
+    | isFalse h => simp [h]
+    | isTrue h =>
+      suffices (n - k) / k + 1 ≤ n by simp [h, this]
+      have ⟨hK, hKN⟩ := h
+      have hSub : n - k < n := sub_lt (Nat.lt_of_lt_of_le hK hKN) hK
+      have : (n - k) / k ≤ n - k := ih (n - k) hSub
+      exact succ_le_of_lt (Nat.lt_of_le_of_lt this hSub)
+
+theorem div_lt_self {n k : Nat} (hLtN : 0 < n) (hLtK : 1 < k) : n / k < n := by
+  rw [div_eq]
+  cases (inferInstance : Decidable (0 < k ∧ k ≤ n)) with
+  | isFalse h => simp [hLtN, h]
+  | isTrue h =>
+    suffices (n - k) / k + 1 < n by simp [h, this]
+    have ⟨_, hKN⟩ := h
+    have : (n - k) / k ≤ n - k := div_le_self (n - k) k
+    have := Nat.add_le_of_le_sub hKN this
+    exact Nat.lt_of_lt_of_le (Nat.add_lt_add_left hLtK _) this
+
 @[extern "lean_nat_mod"]
 protected def mod (x y : @& Nat) : Nat :=
   if 0 < y ∧ y ≤ x then

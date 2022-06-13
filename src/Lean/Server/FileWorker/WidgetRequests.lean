16ccb285993697454b3d55fff71a322595ef81f8
E.W.Ayers <edward.ayers@outlook.com>
2022-06-13 10:11:03 -0400

    doc: add docstrings for interactive

--- src/Lean/Server/FileWorker/WidgetRequests.lean
+++ src/Lean/Server/FileWorker/WidgetRequests.lean
@@ -17,6 +17,7 @@ import Lean.Server.FileWorker.RequestHandling
 namespace Lean.Widget
 open Server
 
+/-- Parameters for the `Lean.Widget.ppExprTagged` RPC.-/
 structure PPExprTaggedParams where
   expr : WithRpcRef ExprWithCtx
   explicit : Bool
@@ -43,18 +44,24 @@ builtin_initialize
     (TaggedText MsgEmbed)
     fun ⟨⟨m⟩, i⟩ => RequestM.asTask do msgToInteractive m i (hasWidgets := true)
 
+/-- The information that the infoview uses to render a popup
+for when the user hovers over an expression.
+-/
 structure InfoPopup where
   type : Option CodeWithInfos
+  /-- Show the term with the implicit arguments. -/
   exprExplicit : Option CodeWithInfos
+  /-- Docstring. In markdown. -/
   doc : Option String
   deriving Inhabited, RpcEncoding
 
-builtin_initialize
-  registerBuiltinRpcProcedure
-    `Lean.Widget.InteractiveDiagnostics.infoToInteractive
-    (WithRpcRef InfoWithCtx)
-    InfoPopup
-    fun ⟨i⟩ => RequestM.asTask do
+/-- Given elaborator info for a particular subexpression. Produce the `InfoPopup`.
+
+The intended usage of this is for the infoview to pass the `InfoWithCtx` which
+was stored for a particular `SubexprInfo` tag in a `TaggedText` generated with `ppExprTagged`.
+ -/
+def makePopup : WithRpcRef InfoWithCtx → RequestM (RequestTask InfoPopup)
+    | ⟨i⟩ => RequestM.asTask do
       i.ctx.runMetaM i.info.lctx do
         let type? ← match (← i.info.type?) with
           | some type => some <$> ppExprTagged type
@@ -73,6 +80,13 @@ builtin_initialize
           exprExplicit := exprExplicit?
           doc := ← i.info.docString? : InfoPopup
         }
+
+builtin_initialize
+  registerBuiltinRpcProcedure
+    `Lean.Widget.InteractiveDiagnostics.infoToInteractive
+    (WithRpcRef InfoWithCtx)
+    InfoPopup
+    makePopup
 
 builtin_initialize
   registerBuiltinRpcProcedure

1996d6710bab9ae2783218e44155f1ce13321917
tydeu <tydeu@hatpress.net>
2022-06-14 13:07:42 -0400

    chore: update Lean version

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,11 @@
 import Lake
 open System Lake DSL
 
-package lake where
-  binRoot := `Lake.Main
+package lake
+
+lean_lib Lake
+
+@[defaultTarget]
+lean_exe lake where
+  root := `Lake.Main
   supportInterpreter := true

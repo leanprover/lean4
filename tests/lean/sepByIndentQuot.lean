233a787e651aefac83c602f167dd10bc5701ff03
Gabriel Ebner <gebner@gebner.org>
2022-06-23 21:50:18 +0200

    chore: add test for sepByIndent.formatter

---
+++ tests/lean/sepByIndentQuot.lean
@@ -0,0 +1,16 @@
+import Lean
+open Lean Elab
+
+#eval show TermElabM _ from do
+  let x ← `(x)
+  let xs := #[x,x,x,x]
+  let ys := xs
+  PrettyPrinter.ppTerm <|<-
+    `(frobnicate { $[$xs:ident := $ys:term],* })
+
+#eval show TermElabM _ from do
+  let x ← `(x)
+  let xs := #[x,x,x,x]
+  let ys := xs
+  PrettyPrinter.ppTerm <|<-
+    `(frobnicate { $[$xs:ident := $ys:term]* })

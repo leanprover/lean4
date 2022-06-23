d5142ddeb822103745a947fd24547e09cba719e1
Gabriel Ebner <gebner@gebner.org>
2022-06-23 21:48:36 +0200

    chore: add sepByIndent.formatter

--- src/Lean/Parser/Extra.lean
+++ src/Lean/Parser/Extra.lean
@@ -57,13 +57,32 @@ attribute [runBuiltinParserAttributeHooks]
 @[runBuiltinParserAttributeHooks, inline] def manyIndent (p : Parser) : Parser :=
   withPosition $ many (checkColGe "irrelevant" >> p)
 
-@[runBuiltinParserAttributeHooks, inline] def sepByIndent (p : Parser) (sep : String) (psep : Parser := symbol sep) (allowTrailingSep : Bool := false) : Parser :=
+@[inline] def sepByIndent (p : Parser) (sep : String) (psep : Parser := symbol sep) (allowTrailingSep : Bool := false) : Parser :=
   let p := withAntiquotSpliceAndSuffix `sepBy p (symbol "*")
   withPosition $ sepBy (checkColGe "irrelevant" >> p) sep (psep <|> checkLinebreakBefore >> pushNone) allowTrailingSep
 
-@[runBuiltinParserAttributeHooks, inline] def sepBy1Indent (p : Parser) (sep : String) (psep : Parser := symbol sep) (allowTrailingSep : Bool := false) : Parser :=
+@[inline] def sepBy1Indent (p : Parser) (sep : String) (psep : Parser := symbol sep) (allowTrailingSep : Bool := false) : Parser :=
   let p := withAntiquotSpliceAndSuffix `sepBy p (symbol "*")
   withPosition $ sepBy1 (checkColGe "irrelevant" >> p) sep (psep <|> checkLinebreakBefore >> pushNone) allowTrailingSep
+
+open PrettyPrinter Syntax.MonadTraverser Formatter in
+@[combinatorFormatter Lean.Parser.sepByIndent]
+def sepByIndent.formatter (p : Formatter) (_sep : String) (pSep : Formatter) : Formatter := do
+  let stx ← getCur
+  let hasNewlineSep := stx.getArgs.mapIdx (fun ⟨i, _⟩ n => i % 2 == 1 && n.matchesNull 0) |>.any id
+  visitArgs do
+    for i in (List.range stx.getArgs.size).reverse do
+      if i % 2 == 0 then p else pSep <|> (pushWhitespace "\n" *> goLeft)
+  -- If there is any newline separator, then we need to force a newline at the
+  -- start so that `withPosition` will pick up the right column.
+  if hasNewlineSep then
+    pushWhitespace "\n"
+    -- HACK: allow formatter to put initial brace on previous line in structure instances
+    modify ({ · with mustBeGrouped := false })
+
+@[combinatorFormatter Lean.Parser.sepBy1Indent] def sepBy1Indent.formatter := sepByIndent.formatter
+
+attribute [runBuiltinParserAttributeHooks] sepByIndent sepBy1Indent
 
 @[runBuiltinParserAttributeHooks] abbrev notSymbol (s : String) : Parser :=
   notFollowedBy (symbol s) s

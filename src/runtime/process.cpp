c186a2ec3077ecca050c45c7d641aea4515418a2
Chris Lovett <clovett@microsoft.com>
2022-06-27 20:48:21 -0700

    fix: rename ascii string

--- src/runtime/process.cpp
+++ src/runtime/process.cpp
@@ -142,7 +142,7 @@ static obj_res spawn(string_ref const & proc_name, array_ref<string_ref> const &
         command = buffer;
         if (command.find(' ') != std::string::npos) {
             // path contains spaces!
-            command.insert(ascii.begin(), '"');
+            command.insert(command.begin(), '"');
             command += "\"";
         }
     }

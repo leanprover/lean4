68024b11a4c026e03723ee6fbcde33fec5f158ef
Leonardo de Moura <leonardo@microsoft.com>
2022-07-03 12:26:17 -0700

    fix: `simp_all` bug when goal has duplicate hypotheses

--- tests/lean/run/constProp.lean
+++ tests/lean/run/constProp.lean
@@ -240,11 +240,11 @@ theorem Bigstem.det (h₁ : (σ, s) ⇓ σ₁) (h₂ : (σ, s) ⇓ σ₂) : σ�
   case seq ih₁ ih₂ h₁ h₂ =>
     simp [ih₁ h₁] at ih₂
     simp [ih₂ h₂]
-  case ifTrue ih _ h =>
+  case ifTrue ih h =>
     simp [ih h]
-  case ifFalse ih _ h =>
+  case ifFalse ih h =>
     simp [ih h]
-  case whileTrue ih₁ ih₂ _ h₁ h₂ =>
+  case whileTrue ih₁ ih₂ h₁ h₂ =>
     simp [ih₁ h₁] at ih₂
     simp [ih₂ h₂]
 

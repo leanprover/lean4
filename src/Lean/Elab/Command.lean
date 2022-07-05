6303fb77d2c8d2aab55242827a672a33435e3f0f
Sebastian Ullrich <sebasti@nullri.ch>
2022-07-05 13:14:30 +0200

    fix: expansion info for macro commands
    TODO: investigate that pp error

--- src/Lean/Elab/Command.lean
+++ src/Lean/Elab/Command.lean
@@ -241,7 +241,8 @@ private def elabCommandUsing (s : State) (stx : Syntax) : List (KeyedDeclsAttrib
 
 /- Elaborate `x` with `stx` on the macro stack -/
 def withMacroExpansion {α} (beforeStx afterStx : Syntax) (x : CommandElabM α) : CommandElabM α :=
-  withReader (fun ctx => { ctx with macroStack := { before := beforeStx, after := afterStx } :: ctx.macroStack }) x
+  withInfoContext (mkInfo := pure <| .ofMacroExpansionInfo { stx := beforeStx, output := afterStx, lctx := .empty }) do
+    withReader (fun ctx => { ctx with macroStack := { before := beforeStx, after := afterStx } :: ctx.macroStack }) x
 
 instance : MonadMacroAdapter CommandElabM where
   getCurrMacroScope := getCurrMacroScope

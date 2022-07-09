305630cc23e20549b0f6d20701115ee701910c8e
Leonardo de Moura <leonardo@microsoft.com>
2022-07-09 13:53:15 -0700

    fix: `ElabAppArgs.finalize` bug

---
+++ tests/lean/run/appFinalizeIssue.lean
@@ -0,0 +1,22 @@
+namespace Ex
+
+class GetElem (Cont : Type u) (Idx : Type v) (Elem : outParam (Type w)) where
+  getElem' (xs : Cont) (i : Idx) : Elem
+
+export GetElem (getElem')
+
+instance : GetElem Lean.Syntax Nat Lean.Syntax where
+  getElem' xs i := xs.getArg i
+
+instance : GetElem (Array α) Nat α where
+  getElem' xs i := xs.get ⟨i, sorry⟩
+
+open Lean
+
+def f (stx : Option Syntax) : Syntax :=
+  stx.getD default
+
+def bad (doFor : Syntax) : Syntax :=
+  let doForDecls := (getElem' doFor 1).getArgs
+  let doForDecl := getElem' doForDecls 1
+  f (getElem' doForDecl 1)

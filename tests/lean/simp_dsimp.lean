4b543d5edde05553926aca15e1b556349b0ab708
Leonardo de Moura <leonardo@microsoft.com>
2022-07-10 07:57:41 -0700

    feat: add option for disabling `dsimp` during `simp`

---
+++ tests/lean/simp_dsimp.lean
@@ -0,0 +1,12 @@
+opaque A : Nat → Type
+opaque f (x : Nat) (a : A x) : Nat
+
+example (x : Nat) (a : A (x + 0)) : f (x + 0) a = x := by
+  simp
+  trace_state -- ⊢ f x a = x
+  sorry
+
+example (x : Nat) (a : A (x + 0)) : f (x + 0) a = x := by
+  simp (config := { dsimp := false })
+  trace_state -- ⊢ f (x + 0) a = x
+  sorry

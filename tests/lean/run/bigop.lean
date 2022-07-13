94df25e99ba07e424ab10e877729baf621ec95eb
Leonardo de Moura <leonardo@microsoft.com>
2022-07-13 15:13:31 -0700

    chore: update example

--- tests/lean/run/bigop.lean
+++ tests/lean/run/bigop.lean
@@ -1,18 +1,17 @@
-def Sequence (α : Type) := List α
+abbrev Sequence (α : Type) := List α
 
-def BigBody (β α) :=  α × (β → β → β) × Bool × β
+def bigop (init : β) (seq : Sequence α) (op : β → β → β) (f : α → Bool × β) : β := Id.run do
+  let mut result := init
+  for a in seq do
+    let (ok, b) := f a
+    if ok then
+      result := op result b
+  return result
 
-def applyBig {α β : Type} (body : BigBody β α) (x : β) : β :=
-  let (_, op, b, v) := body;
-  if b then op v x else x
+#eval bigop 0 [1, 2, 4] Add.add fun elem => (elem % 2 == 0, elem * 2)
 
-def reducebig {α β : Type} (idx : β) (r : Sequence α) (body : α → BigBody β α) : β :=
-  r.foldr (applyBig ∘ body) idx
-
-def bigop := @reducebig
-
-partial def iota : Nat → Nat → List Nat
-  | m, 0   => []
+def iota : Nat → Nat → List Nat
+  | _, 0   => []
   | m, n+1 => m :: iota (m+1) n
 
 def index_iota (m n : Nat) := iota m (n - m)
@@ -23,18 +22,20 @@ class Enumerable (α : Type) where
 instance : Enumerable Bool where
   elems := [false, true]
 
--- instance {α β} [Enumerable α] [Enumerable β]: Enumerable (α × β) :=
--- { elems := do let a ← Enumerable.elems α; let b ← Enumerable.elems β; pure (a, b) }
+instance {α β} [Enumerable α] [Enumerable β]: Enumerable (α × β) where
+  elems := Enumerable.elems.bind fun (a : α) => Enumerable.elems.bind fun (b : β) => [(a, b)]
 
-partial def finElemsAux (n : Nat) : (i : Nat) → i < n → List (Fin n)
-  | 0,   h => [⟨0, h⟩]
-  | i+1, h => ⟨i+1, h⟩ :: finElemsAux n i (Nat.lt_of_succ_lt h)
+def finElems (n : Nat) : List (Fin n) :=
+  match n with
+  | 0   => []
+  | n+1 => go (n+1) n (by simp_arith)
+where
+  go (n : Nat) (i : Nat) (h : i < n) : List (Fin n) :=
+   match i with
+   | 0 => [⟨0, h⟩]
+   | i+1 => ⟨i+1, h⟩ :: go n i (Nat.lt_of_succ_lt h)
 
-partial def finElems : (n : Nat) → List (Fin n)
-  | 0     => []
-  | (n+1) => finElemsAux (n+1) n (Nat.lt_succ_self n)
-
-instance {n} : Enumerable (Fin n) where
+instance : Enumerable (Fin n) where
   elems := (finElems n).reverse
 
 instance : OfNat (Fin (Nat.succ n)) m :=
@@ -49,30 +50,30 @@ syntax ident "<-" term "|" term : index
 -- Primitive notation for big operators
 syntax "_big" "[" term "," term "]" "(" index ")" term : term
 
--- We define how to expand `_bigop` with the different kinds of index
+-- We define how to expand `_big` with the different kinds of index
 macro_rules
-| `(_big [$op, $idx] ($i:ident <- $r | $p) $F) => `(bigop $idx $r (fun $i:ident => ($i:ident, $op, $p, $F)))
-| `(_big [$op, $idx] ($i:ident <- $r) $F) => `(bigop $idx $r (fun $i:ident => ($i:ident, $op, true, $F)))
-| `(_big [$op, $idx] ($lower:term ≤ $i:ident < $upper) $F) => `(bigop $idx (index_iota $lower $upper) (fun $i:ident => ($i:ident, $op, true, $F)))
-| `(_big [$op, $idx] ($lower:term ≤ $i:ident < $upper | $p) $F) => `(bigop $idx (index_iota $lower $upper) (fun $i:ident => ($i:ident, $op, $p, $F)))
+| `(_big [$op, $idx] ($i:ident <- $r | $p) $F) => `(bigop $idx $r $op (fun $i:ident => ($p, $F)))
+| `(_big [$op, $idx] ($i:ident <- $r) $F) => `(bigop $idx $r $op (fun $i:ident => (true, $F)))
+| `(_big [$op, $idx] ($lower:term ≤ $i:ident < $upper) $F) => `(bigop $idx (index_iota $lower $upper) $op (fun $i:ident => (true, $F)))
+| `(_big [$op, $idx] ($lower:term ≤ $i:ident < $upper | $p) $F) => `(bigop $idx (index_iota $lower $upper) $op (fun $i:ident => ($p, $F)))
 
--- Define `Sum`
-syntax "Sum" "(" index ")" term : term
-macro_rules | `(Sum ($idx) $F) => `(_big [Add.add, 0] ($idx) $F)
+-- Define `∑ `
+syntax "∑ " "(" index ")" term : term
+macro_rules | `(∑ ($idx) $F) => `(_big [Add.add, 0] ($idx) $F)
 
 -- We can already use `Sum` with the different kinds of index.
-#check Sum (i <- [0, 2, 4] | i != 2) i
-#check Sum (10 ≤ i < 20 | i != 5) i+1
-#check Sum (10 ≤ i < 20) i+1
+#check ∑ (i <- [0, 2, 4] | i != 2) i
+#check ∑ (10 ≤ i < 20 | i != 5) i+1
+#check ∑ (10 ≤ i < 20) i+1
 
--- Define `Prod`
-syntax "Prod" "(" index ")" term : term
-macro_rules | `(Prod ($idx) $F) => `(_big [Mul.mul, 1] ($idx) $F)
+-- Define `∏`
+syntax "∏" "(" index ")" term : term
+macro_rules | `(∏ ($idx) $F) => `(_big [Mul.mul, 1] ($idx) $F)
 
 -- The examples above now also work for `Prod`
-#check Prod (i <- [0, 2, 4] | i != 2) i
-#check Prod (10 ≤ i < 20 | i != 5) i+1
-#check Prod (10 ≤ i < 20) i+1
+#check ∏ (i <- [0, 2, 4] | i != 2) i
+#check ∏ (10 ≤ i < 20 | i != 5) i+1
+#check ∏ (10 ≤ i < 20) i+1
 
 -- We can extend our grammar for the syntax category `index`.
 syntax ident "|" term : index
@@ -80,24 +81,24 @@ syntax ident ":" term : index
 syntax ident ":" term "|" term : index
 -- And new rules
 macro_rules
-| `(_big [$op, $idx] ($i:ident : $type) $F) => `(bigop $idx (Enumerable.elems (α := $type)) (fun $i:ident => ($i:ident, $op, true, $F)))
-| `(_big [$op, $idx] ($i:ident : $type | $p) $F) => `(bigop $idx (Enumerable.elems (α := $type)) (fun $i:ident => ($i:ident, $op, $p, $F)))
-| `(_big [$op, $idx] ($i:ident | $p) $F) => `(bigop $idx (Enumerable.elems) (fun $i:ident => ($i:ident, $op, $p, $F)))
+| `(_big [$op, $idx] ($i:ident : $type) $F) => `(bigop $idx (Enumerable.elems (α := $type)) $op (fun $i:ident => (true, $F)))
+| `(_big [$op, $idx] ($i:ident : $type | $p) $F) => `(bigop $idx (Enumerable.elems (α := $type)) $op (fun $i:ident => ($p, $F)))
+| `(_big [$op, $idx] ($i:ident | $p) $F) => `(bigop $idx (Enumerable.elems) $op (fun $i:ident => ($p, $F)))
 
 -- The new syntax is immediately available for all big operators that we have defined
 def myPred (x : Fin 10) : Bool := true
-#check Sum (i : Fin 10) i+1
-#check Sum (i : Fin 10 | i != 2) i+1
-#check Sum (i | myPred i) i+i
-#check Prod (i : Fin 10) i+1
-#check Prod (i : Fin 10 | i != 2) i+1
-#check Prod (i | myPred i) i+i
+#check ∑ (i : Fin 10) i+1
+#check ∑ (i : Fin 10 | i != 2) i+1
+#check ∑ (i | myPred i) i+i
+#check ∏ (i : Fin 10) i+1
+#check ∏ (i : Fin 10 | i != 2) i+1
+#check ∏ (i | myPred i) i+i
 
 -- We can easily create alternative syntax for any big operator.
-syntax "Σ" index "=>" term : term
-macro_rules | `(Σ $idx => $F) => `(Prod ($idx) $F)
+syntax "Summation" index "=>" term : term
+macro_rules | `(Summation $idx => $F) => `(∑ ($idx) $F)
 
-#check Σ 10 ≤ i < 20 => i+1
+#check Summation 10 ≤ i < 20 => i+1
 
 -- Now, we create command for automating the generation of big operators.
 syntax "def_bigop" str term:max term:max : command

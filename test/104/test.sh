5f9166c621c8d677037c41a42a122f567956dab4
tydeu <tydeu@hatpress.net>
2022-07-15 15:58:13 -0400

    test: try to fix sed script for test 104 for MacOS

--- test/104/test.sh
+++ test/104/test.sh
@@ -10,7 +10,11 @@ TEST_URL=https://example.com/hello.git
 cd test
 $LAKE build
 cat lean_packages/manifest.json
-sed -i "s,\.\.[^\"]*,$TEST_URL," lean_packages/manifest.json
+if [ "`uname`" = Darwin ]; then
+  sed -i '' "s,\\.\\.[^\"]*,$TEST_URL," lean_packages/manifest.json
+else
+  sed -i "s,\\.\\.[^\"]*,$TEST_URL," lean_packages/manifest.json
+fi
 cat lean_packages/manifest.json
 git -C lean_packages/hello remote set-url origin $TEST_URL
 $LAKE build -K url=$TEST_URL

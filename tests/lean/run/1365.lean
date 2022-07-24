c46ef56ac7dd7cce87f4cbe054b3042d6d802d49
Leonardo de Moura <leonardo@microsoft.com>
2022-07-24 13:10:04 -0700

    perf: avoid blowup at `deriving Repr`
    The fix is not perfect. I just avoided inlining in some builtin `Repr` instances.
    The actual problem is at `ElimDeadBranches.lean`.
    
    Closes #1365

---
+++ tests/lean/run/1365.lean
@@ -0,0 +1,28 @@
+structure Foo where
+  a : Option Bool
+  b : Option Bool
+  c : Option Bool
+  d : Option Bool
+  e : Option Bool
+  f : Option Bool
+  g : Option Bool
+  h : Option Bool
+  i : Option Bool
+  j : Option Bool
+  k : Option Bool
+  l : Option Bool
+  m : Option Bool
+  n : Option Bool
+  o : Option Bool
+  p : Option Bool
+  q : Option Bool
+  r : Option Bool
+  s : Option Bool
+  t : Option Bool
+  u : Option Bool
+  v : Option Bool
+  w : Option Bool
+  x : Option Bool
+  y : Option Bool
+  z : Option Bool
+deriving Repr

ff23465a04075796c03270ad6571b9d3c3079182
tydeu <tydeu@hatpress.net>
2022-07-24 21:14:15 -0400

    chore: try to fix meta test script on macOS

--- test/meta/test.sh
+++ test/meta/test.sh
@@ -3,7 +3,7 @@ set -exo pipefail
 
 LAKE=${LAKE:-../../build/bin/lake}
 
-$LAKE update |& grep -m2 -E "lorem|ipsum"
-$LAKE update -Kbaz |& grep -m3 -E "lorem|ipsum|baz"
-$LAKE update -Kenv=foo |& grep -m4 -E "lorem|ipsum|foo|1"
-$LAKE update -Kenv=bar |& grep -m4 -E "lorem|ipsum|bar|2"
+$LAKE update 2>&1 | grep -m2 -E "lorem|ipsum"
+$LAKE update -Kbaz 2>&1 | grep -m3 -E "lorem|ipsum|baz"
+$LAKE update -Kenv=foo 2>&1 | grep -m4 -E "lorem|ipsum|foo|1"
+$LAKE update -Kenv=bar 2>&1 | grep -m4 -E "lorem|ipsum|bar|2"

92d360353caf2734927252f2deaa36db05c835a5
Leonardo de Moura <leonardo@microsoft.com>
2022-07-27 09:53:43 -0700

    feat: elaborate `add_decl_doc`

--- src/Lean/DocString.lean
+++ src/Lean/DocString.lean
@@ -82,4 +82,9 @@ def getMainModuleDoc (env : Environment) : Std.PersistentArray ModuleDoc :=
 def getModuleDoc? (env : Environment) (moduleName : Name) : Option (Array ModuleDoc) :=
   env.getModuleIdx? moduleName |>.map fun modIdx => moduleDocExt.getModuleEntries env modIdx
 
+def getDocStringText [Monad m] [MonadError m] [MonadRef m] (stx : TSyntax `Lean.Parser.Command.docComment) : m String :=
+  match stx.raw[1] with
+  | Syntax.atom _ val => return val.extract 0 (val.endPos - ⟨2⟩)
+  | _                 => throwErrorAt stx "unexpected doc string{indentD stx.raw[1]}"
+
 end Lean

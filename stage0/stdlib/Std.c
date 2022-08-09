f9a73cff9cae27ff2137763813b64c0b750b192b
Gabriel Ebner <gebner@gebner.org>
2022-08-09 21:55:31 +0200

    chore: update stage0

--- stage0/stdlib/Std.c
+++ stage0/stdlib/Std.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: Std
-// Imports: Init Std.Data Std.ShareCommon
+// Imports: Init Std.Data Std.ShareCommon Std.Dynamic
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -16,6 +16,7 @@ extern "C" {
 lean_object* initialize_Init(uint8_t builtin, lean_object*);
 lean_object* initialize_Std_Data(uint8_t builtin, lean_object*);
 lean_object* initialize_Std_ShareCommon(uint8_t builtin, lean_object*);
+lean_object* initialize_Std_Dynamic(uint8_t builtin, lean_object*);
 static bool _G_initialized = false;
 LEAN_EXPORT lean_object* initialize_Std(uint8_t builtin, lean_object* w) {
 lean_object * res;
@@ -28,6 +29,9 @@ res = initialize_Std_Data(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 res = initialize_Std_ShareCommon(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Std_Dynamic(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 return lean_io_result_mk_ok(lean_box(0));

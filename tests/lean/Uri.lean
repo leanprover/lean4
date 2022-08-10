0b36e299c53c70c101ca085c25cc70d89ae4b176
Chris Lovett <clovett@microsoft.com>
2022-08-09 18:06:40 -0700

    test: add a Uri windows drive letter test.

--- tests/lean/Uri.lean
+++ tests/lean/Uri.lean
@@ -22,3 +22,21 @@ open System.Uri
 
 /- trailing truncated escape ignored -/
 #eval unescapeUri "lean%4"
+
+def testWindowsDriveLetterEscaping : String :=
+  if System.Platform.isWindows then
+    let x : System.FilePath := "C:" / "Temp" / "test.lean"
+    let r := toFileUri x.normalize.toString
+    if r == "file:///c%3a/temp/test.lean" then
+      let y := fileUriToPath r
+      if y.normalize.toString == x.normalize.toString  then
+        "testWindowsDriveLetterEscaping ok"
+      else
+        s!"testWindowsDriveLetterEscaping '{x.normalize.toString}' != '{y.normalize.toString}'"
+    else
+      "testWindowsDriveLetterEscaping failed to escape"
+  else
+    -- skip test on other platforms, drive letters are only a windows thing.
+    "testWindowsDriveLetterEscaping ok"
+
+#eval testWindowsDriveLetterEscaping

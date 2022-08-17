4e7c1e1ec8c37461e2f5b706e0944b27ca84f392
E.W.Ayers <edward.ayers@outlook.com>
2022-08-17 09:52:03 +0100

    fix: missing digits in scientific literal should be an error

--- tests/lean/scientific.lean
+++ tests/lean/scientific.lean
@@ -1,5 +1,10 @@
 -- https://github.com/leanprover/lean4/issues/1484
-#eval 1.0e     -- 1.000000
+#eval 1.0e1    -- 10.000000
 #eval 1.0e+1   -- 10.000000
+#eval 1.0e-1   -- 0.1000000
+
+#eval 1.0e     -- error
+#eval 1.0e+    -- error
+#eval 1.0e-    -- error
 #eval 1.0e+ 1  -- error
-#eval 1.0e + 1 -- 2.000000
+#eval 1.0e + 1 -- error

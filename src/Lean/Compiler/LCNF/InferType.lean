9b2887861516d1970e20f1db5f19903cc8498cf5
Leonardo de Moura <leonardo@microsoft.com>
2022-08-24 14:05:25 -0700

    fix: `AltCore.inferType`

--- src/Lean/Compiler/LCNF/InferType.lean
+++ src/Lean/Compiler/LCNF/InferType.lean
@@ -179,10 +179,8 @@ def Code.inferParamType (params : Array Param) (code : Code) : CompilerM Expr :=
   let xs := params.map fun p => .fvar p.fvarId
   InferType.mkForallFVars xs type |>.run {}
 
-def AltCore.inferType (alt : Alt) : CompilerM Expr := do
-  match alt with
-  | .default k => k.inferType
-  | .alt _ params k => k.inferParamType params
+def AltCore.inferType (alt : Alt) : CompilerM Expr :=
+  alt.getCode.inferType
 
 def mkAuxLetDecl (e : Expr) (prefixName := `_x) : CompilerM Expr := do
   if e.isFVar then

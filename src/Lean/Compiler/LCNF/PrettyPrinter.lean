cabcadf9cc0b153261a9919d556650b815509a47
Leonardo de Moura <leonardo@microsoft.com>
2022-08-24 08:41:45 -0700

    feat: add `ppDecl`

--- src/Lean/Compiler/LCNF/PrettyPrinter.lean
+++ src/Lean/Compiler/LCNF/PrettyPrinter.lean
@@ -8,9 +8,9 @@ import Lean.Compiler.LCNF.CompilerM
 
 namespace Lean.Compiler.LCNF
 
-namespace PP
+private abbrev indentD := Std.Format.indentD
 
-abbrev indentD := Std.Format.indentD
+namespace PP
 
 abbrev M := ReaderT LocalContext CompilerM
 
@@ -72,29 +72,38 @@ def ppLetDecl (letDecl : LetDecl) : M Format := do
   else
     return f!"let {letDecl.binderName} := {← ppValue letDecl.value}"
 
-partial def ppCode (c : Code) : CompilerM Format :=
-  withOptions (pp.sanitizeNames.set · false) do
-    go c |>.run (← get).lctx.toLocalContext
-where
-  ppFunDecl (funDecl : FunDecl) : M Format := do
-    return f!"{funDecl.binderName}{← ppParams funDecl.params} :={indentD (← go funDecl.value)}"
+mutual
+  partial def ppFunDecl (funDecl : FunDecl) : M Format := do
+    return f!"{funDecl.binderName}{← ppParams funDecl.params} :={indentD (← ppCode funDecl.value)}"
 
-  ppAlt (alt : Alt) : M Format := do
+  partial def ppAlt (alt : Alt) : M Format := do
     match alt with
-    | .default k => return f!"| _ =>{indentD (← go k)}"
-    | .alt ctorName params k => return f!"| {ctorName}{← ppParams params} =>{indentD (← go k)}"
+    | .default k => return f!"| _ =>{indentD (← ppCode k)}"
+    | .alt ctorName params k => return f!"| {ctorName}{← ppParams params} =>{indentD (← ppCode k)}"
 
-  go (c : Code) : M Format := do
+  partial def ppCode (c : Code) : M Format := do
     match c with
-    | .let decl k => return (← ppLetDecl decl) ++ .line ++ (← go k)
-    | .fun decl k => return f!"fun " ++ (← ppFunDecl decl) ++ .line ++ (← go k)
-    | .jp decl k => return f!"jp " ++ (← ppFunDecl decl) ++ .line ++ (← go k)
+    | .let decl k => return (← ppLetDecl decl) ++ .line ++ (← ppCode k)
+    | .fun decl k => return f!"fun " ++ (← ppFunDecl decl) ++ .line ++ (← ppCode k)
+    | .jp decl k => return f!"jp " ++ (← ppFunDecl decl) ++ .line ++ (← ppCode k)
     | .cases c => return f!"cases {← ppFVar c.discr}{← prefixJoin .line c.alts ppAlt}"
     | .return fvarId => ppFVar fvarId
     | .jmp fvarId args => return f!"goto {← ppFVar fvarId} {← ppArgs args}"
     | .unreach .. => return "⊥"
+end
+
+def run (x : M α) : CompilerM α :=
+  withOptions (pp.sanitizeNames.set · false) do
+    x |>.run (← get).lctx.toLocalContext
 
 end PP
+
+def ppCode (code : Code) : CompilerM Format :=
+  PP.run <| PP.ppCode code
+
+def ppDecl (decl : Decl) : CompilerM Format :=
+  PP.run do
+    return f!"{decl.name}{← PP.ppParams decl.params} :={indentD (← PP.ppCode decl.value)}"
 
 end Lean.Compiler.LCNF
 

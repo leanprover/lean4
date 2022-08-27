c21740e5ed7cf77e35b0c10bfe5ac011af89059b
Chris Lovett <clovett@microsoft.com>
2022-08-27 12:41:45 -0700

    doc: fix references to State to become StateM.

--- doc/monads/states.lean
+++ doc/monads/states.lean
@@ -41,21 +41,21 @@ current player. This would change the board state as well as swap the current pl
 words, we have operations that depend on the current state of the game, but also need to **update
 that state**.
 
-## The State Monad to the Rescue
+## The StateM Monad to the Rescue
 
-This is exactly the situation the State monad deals with. The `State` monad wraps computations in
+This is exactly the situation the `StateM` monad deals with. The `StateM` monad wraps computations in
 the context of reading and modifying a global state object.
 
 It is parameterized by a single type parameter `s`, the state type in use. So just like the `ReaderM`
-has a single type you read from, the `State` has a single type you can both **read from and write
-to**. There are three primary actions you can take within the `State`monad:
+has a single type you read from, the `StateM` has a single type you can both **read from and write
+to**. There are three primary actions you can take within the `StateM`monad:
 
 - **get** - retrieves the state, like Reader.read
 - **set** - updates the state
 - **modifyGet** - retrieves the state, then updates it
 
 There is also a `run` function, similar to `run` on `ReaderM`. Like the `ReaderM` monad, you must
-provide an initial state, in addition to the computation to run. `State` then produces two outputs:
+provide an initial state, in addition to the computation to run. `StateM` then produces two outputs:
 the result of our computation combined with the final updated state.
 
 If you wish to discard the final state and just get the computation's result, you can use
@@ -85,7 +85,8 @@ def chooseRandomMove : StateM GameState TileIndex := do
 
 /-!
 This returns a `TileIndex` and modifies the random number generator stored in our state!
-Notice we have a fun little use of the `Applicative.seqRight` operator `*>` in `findOpen`.
+Notice we have a fun little use of the `Applicative.seqRight` operator `*>` in `findOpen`
+described in [Applicatives](applicatives.lean.md).
 
 Now we can create the function that can make a move:
 -/
@@ -107,9 +108,9 @@ def applyMove (i: TileIndex): StateM GameState Unit := do
 
 /-!
 This updates the board in the `GameState` with the new tile, and then changes the current player,
-providing no output.
+providing no output (`Unit` return type).
 
-So finally, we can combine these functions together with do-syntax, and it actually looks quite
+So finally, we can combine these functions together with `do` notation, and it actually looks quite
 clean! We don't need to worry about the side effects. The different monadic functions handle them.
 Here's a sample of what your function might look like to play one turn of the game. At the end, it
 returns a boolean determining if we've filled all the spaces:
@@ -117,8 +118,7 @@ returns a boolean determining if we've filled all the spaces:
 
 def isGameDone : StateM GameState Bool := do
   let game ← get
-  let openSpots := findOpen game.board
-  return openSpots.length == 0
+  return (findOpen game.board).isEmpty
 
 def nextTurn : StateM GameState Bool := do
   let i ← chooseRandomMove
@@ -126,9 +126,7 @@ def nextTurn : StateM GameState Bool := do
   isGameDone
 
 /-!
-Obviously, there are some more complications for how the game would work in full, but the general
-idea should be clear. Any additional functions could live within the State monad.  To give you
-a quick test harness you can run this test:
+To give you a quick test harness that runs all moves for both playes you can run this:
 -/
 
 instance : ToString TileState where
@@ -170,47 +168,59 @@ def main : IO Unit := do
   printBoard gs.board
 
 #eval main
--- [O, X, X]
 -- [X, X, O]
--- [O, O, O]
+-- [X, O, O]
+-- [O, O, X]
 
 /-!
 
-Lastly, you can see how the State monad adds the input state and output state when you
-look at the reduced Type for `nextTurn`:
+Note that when you run the above code interactively the random number generator always starts in the
+same place.  But if you run `lean --run states.lean` then you will see randomness in the result.
+
+## Implementation
+
+It may be helpful to see how the `StateM` monad adds the input state and output state.  If you look
+at the reduced Type for `nextTurn`:
 -/
 #reduce StateM GameState Bool
 -- GameState → Bool × GameState
 /-!
 
-So a function like `nextTurn` that might have just returned a `Bool` has been modified by the State
-monad such that the initial `GameState` is passed in as a new input argument, and the output value
-has been changed to the pair `Bool × GameState` so that it can return the pure `Bool` and the
+So a function like `nextTurn` that might have just returned a `Bool` has been modified by the
+`State` monad such that the initial `GameState` is passed in as a new input argument, and the output
+value has been changed to the pair `Bool × GameState` so that it can return the pure `Bool` and the
 updated `GameState`.  This is why the call to nextTurn looks like this: `let (_, g) := nextTurn gs`.
-This expression `(_, g)` mean break the pair up into 2 values, we don't care what the first value is
-(so use underscore `_`), but we do need the updated state `g`.
+This expression `(_, g)` conveniently breaks the pair up into 2 values, we don't care what the first
+value is (so we use underscore `_`), but we do need the updated state `g` which we can then assign
+back to our mutable `gs` variable to use next time around this loop.
 
 ## State, IO and other languages
 
-When thinking about Lean, it is often seen as a restriction that we can't have global variables like
-you can with Python. However, hopefully you see now this isn't true. You can have a data type with
-exactly the same functionality as a Python class. You would simple have many functions that can
-modify the global state using the State monad.
+When thinking about Lean, it is often seen as a restriction that we can't have global variables or
+`static` variables like you can with other languages like Python or C++. However, hopefully you see
+now this isn't true. You can have a data type with exactly the same functionality as a Python class.
+You would simply have many functions that can modify some global state using the `StateM` monad.
 
 The difference is in Lean we simply put a label on these types of functions. We don't allow it to
-happen for free anywhere in an uncontrolled fashion. We want to know when side effects can
-potentially happen, because knowing when they can happen makes our code easier to reason about. In a
-Python class, many of the methods won't actually need to modify the global state. But they could,
-which makes it harder to debug them. In Lean we can simply make these pure functions, and the
-compiler will ensure they don't modify any global state.
+happen for free anywhere in an uncontrolled fashion because that results in too many sleepless
+nights debugging nasty code.  We want to know when side effects can potentially happen, because
+knowing when they can happen makes our code easier to reason about. In a Python class, many of the
+methods won't actually need to modify the global state. But they could, which makes it harder to
+debug them. In Lean we can simply make these pure functions, and the compiler will ensure they stay
+pure and cannot modify any global state.
 
 IO is the same way. It's not like we can't perform IO in Lean. Instead, we want to label the areas
 where we can, to increase our certainty about the areas where we don't need to. When we know part of
 our code cannot communicate with the outside world, we can be far more certain of its behavior.
 
+The `StateM` monad is also a more disciplined way of managing side effects. Top level code could
+call a `StateM` function multiple times with different independent initial states, even doing that
+across multiple tasks in parallel and each of these cannot clobber the state belonging to other
+tasks. Monadic code is more reusable than code that uses global variables.
+
 ## Summary
 
-That wraps it up for the State monad! There is one more very useful monad that can be used to
-do exception handling which we'll cover in the [next section](except.lean.md).
+That wraps it up for the `StateM` monad! There is one more very useful monad that can be used to do
+exception handling which we'll cover in the [next section](except.lean.md).
 
 -/

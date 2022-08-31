da9dac5f1fa1f7d75a12a3d65714cff346a0da36
Chris Lovett <clovett@microsoft.com>
2022-08-31 11:19:01 -0700

    doc: fix typo
    Co-authored-by: Sebastian Ullrich <sebasti@nullri.ch>

--- doc/monads/readers.lean
+++ doc/monads/readers.lean
@@ -148,7 +148,7 @@ Lean that will show you the reduced types:
 And you can see here that this type is actually a function!  It's a function that takes an
 `Environment` as input and returns a `String`.
 
-Now, remember in Lean that a function that takes argument of type `Nat` and returns a `String` like
+Now, remember in Lean that a function that takes an argument of type `Nat` and returns a `String` like
 `def f (a : Nat) : String` is the same as this function `def f : Nat → String`.  These are
 definitially equal.  Well this is being used by the `ReaderM` Monad to add an input argument to
 all the functions that use the `ReaderM` monad and this is why `main` is able to start things off by

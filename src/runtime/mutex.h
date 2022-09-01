05c53a08867a7d1bd13a7e1212ba4a55fe0a374d
Gabriel Ebner <gebner@gebner.org>
2022-09-01 13:33:34 +0200

    feat: `Mutex`, `Condvar`

---
+++ src/runtime/mutex.h
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2022 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Gabriel Ebner
+*/
+#pragma once
+
+namespace lean {
+void initialize_mutex();
+void finalize_mutex();
+}

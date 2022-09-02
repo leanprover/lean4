ce6fe647613b4b8200003e830ce80b5ac5243df4
Gabriel Ebner <gebner@gebner.org>
2022-09-02 13:34:38 +0200

    feat: synchronous operations for `Channel`

--- src/Init/Data/Channel.lean
+++ src/Init/Data/Channel.lean
@@ -22,10 +22,10 @@ structure Channel.State (α : Type) where
   deriving Inhabited
 
 /--
-FIFO channel with a receive operation returning a `Task`.
+FIFO channel with unbounded buffer, where `recv?` returns a `Task`.
 
 A channel can be closed.  Once it is closed, all `send`s are ignored, and
-`recvAsync` returns `none` once the queue is empty.
+`recv?` returns `none` once the queue is empty.
 -/
 def Channel (α : Type) : Type := Mutex (Channel.State α)
 
@@ -61,11 +61,13 @@ def Channel.close (ch : Channel α) : BaseIO Unit :=
     set { st with closed := true, consumers := ∅ }
 
 /--
-Receives a message.
+Receives a message, without blocking.
+The returned task waits for the message.
+Every message is only received once.
 
 Returns `none` if the channel is closed and the queue is empty.
 -/
-def Channel.recvAsync (ch : Channel α) : BaseIO (Task (Option α)) :=
+def Channel.recv? (ch : Channel α) : BaseIO (Task (Option α)) :=
   ch.atomically do
     let st ← get
     if let some (a, values) := st.values.dequeue? then
@@ -85,6 +87,49 @@ Note that if this function is called twice, each `forAsync` only gets half the m
 -/
 partial def Channel.forAsync (f : α → BaseIO Unit) (ch : Channel α)
     (prio : Task.Priority := .default) : BaseIO (Task Unit) := do
-  BaseIO.bindTask (prio := prio) (← ch.recvAsync) fun
+  BaseIO.bindTask (prio := prio) (← ch.recv?) fun
     | none => return .pure ()
     | some v => do f v; ch.forAsync f prio
+
+/--
+Receives all currently queued messages from the channel.
+
+Those messages are dequeued and will not be returned by `recv?`.
+-/
+def Channel.recvAllCurrent (ch : Channel α) : BaseIO (Array α) :=
+  ch.atomically do
+    modifyGet fun st => (st.values.toArray, { st with values := ∅ })
+
+/-- Type tag for synchronous (blocking) operations on a `Channel`. -/
+def Channel.Sync := Channel
+
+/--
+Accesses synchronous (blocking) version of channel operations.
+
+For example, `ch.sync.recv?` blocks until the next message,
+and `for msg in ch.sync do ...` iterates synchronously over the channel.
+These functions should only be used in dedicated threads.
+-/
+def Channel.sync (ch : Channel α) : Channel.Sync α := ch
+
+/--
+Synchronously receives a message from the channel.
+
+Every message is only received once.
+Returns `none` if the channel is closed and the queue is empty.
+-/
+def Channel.Sync.recv? (ch : Channel.Sync α) : BaseIO (Option α) := do
+  IO.wait (← Channel.recv? ch)
+
+private partial def Channel.Sync.forIn [Monad m] [MonadLiftT BaseIO m]
+    (ch : Channel.Sync α) (f : α → β → m (ForInStep β)) : β → m β := fun b => do
+  match ← ch.recv? with
+    | some a =>
+      match ← f a b with
+        | .done b => pure b
+        | .yield b => ch.forIn f b
+    | none => pure b
+
+/-- `for msg in ch.sync do ...` receives all messages in the channel until it is closed. -/
+instance [MonadLiftT BaseIO m] : ForIn m (Channel.Sync α) α where
+  forIn ch b f := ch.forIn f b

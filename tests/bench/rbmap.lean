77e42744dd9f49ec8c126b15d684a694b2d64ddc
Sebastian Ullrich <sebasti@nullri.ch>
2022-09-22 16:19:23 +0200

    chore: modernize rbmap benchmarks a bit

--- tests/bench/rbmap.lean
+++ tests/bench/rbmap.lean
@@ -1,79 +1,68 @@
 /-
 Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
+Authors: Leonardo de Moura, Sebastian Ullrich
 -/
-prelude
-import Init.Data.Option.Basic
-import Init.Data.List.BasicAux
-import Init.Data.String
-import Init.System.IO
 
-universe u v w w'
+inductive Color
+  | red | black
 
-inductive color
-| Red | Black
-
-inductive Tree
-| Leaf                                                                           : Tree
-| Node  (color : color) (lchild : Tree) (key : Nat) (val : Bool) (rchild : Tree) : Tree
-
-variable {σ : Type w}
-open color Nat Tree
+inductive Tree where
+  | leaf
+  | node : Color → Tree → Nat → Bool → Tree → Tree
 
 def fold (f : Nat → Bool → σ → σ) : Tree → σ → σ
-| Leaf, b               => b
-| Node _ l k v r,     b => fold f r (f k v (fold f l b))
+  | .leaf,           b => b
+  | .node _ l k v r, b => fold f r (f k v (fold f l b))
 
 @[inline]
 def balance1 : Nat → Bool → Tree → Tree → Tree
-| kv, vv, t, Node _ (Node Red l kx vx r₁) ky vy r₂   => Node Red (Node Black l kx vx r₁) ky vy (Node Black r₂ kv vv t)
-| kv, vv, t, Node _ l₁ ky vy (Node Red l₂ kx vx r)   => Node Red (Node Black l₁ ky vy l₂) kx vx (Node Black r kv vv t)
-| kv, vv, t, Node _ l  ky vy r                       => Node Black (Node Red l ky vy r) kv vv t
-| _,  _,  _,                                       _ => Leaf
+  | kv, vv, t, .node _ (.node .red l kx vx r₁) ky vy r₂   => .node .red (.node .black l kx vx r₁) ky vy (.node .black r₂ kv vv t)
+  | kv, vv, t, .node _ l₁ ky vy (.node .red l₂ kx vx r)   => .node .red (.node .black l₁ ky vy l₂) kx vx (.node .black r kv vv t)
+  | kv, vv, t, .node _ l  ky vy r                         => .node .black (.node .red l ky vy r) kv vv t
+  | _,  _,  _, _                                          => .leaf
 
 @[inline]
 def balance2 : Tree → Nat → Bool → Tree → Tree
-| t, kv, vv, Node _ (Node Red l kx₁ vx₁ r₁) ky vy r₂    => Node Red (Node Black t kv vv l) kx₁ vx₁ (Node Black r₁ ky vy r₂)
-| t, kv, vv, Node _ l₁ ky vy (Node Red l₂ kx₂ vx₂ r₂)   => Node Red (Node Black t kv vv l₁) ky vy (Node Black l₂ kx₂ vx₂ r₂)
-| t, kv, vv, Node _ l ky vy r                           => Node Black t kv vv (Node Red l ky vy r)
-| _, _, _,                                         _    => Leaf
+  | t, kv, vv, .node _ (.node .red l kx₁ vx₁ r₁) ky vy r₂  => .node .red (.node .black t kv vv l) kx₁ vx₁ (.node .black r₁ ky vy r₂)
+  | t, kv, vv, .node _ l₁ ky vy (.node .red l₂ kx₂ vx₂ r₂) => .node .red (.node .black t kv vv l₁) ky vy (.node .black l₂ kx₂ vx₂ r₂)
+  | t, kv, vv, .node _ l ky vy r                           => .node .black t kv vv (.node .red l ky vy r)
+  | _, _,  _,  _                                           => .leaf
 
 def isRed : Tree → Bool
-| Node Red _ _ _ _   => true
-| _                  => false
+  | .node .red .. => true
+  | _             => false
 
-def ins : Tree → Nat → Bool → Tree
-| Leaf,                 kx, vx => Node Red Leaf kx vx Leaf
-| Node Red a ky vy b,   kx, vx =>
-   (if kx < ky then Node Red (ins a kx vx) ky vy b
-    else if kx = ky then Node Red a kx vx b
-    else Node Red a ky vy (ins b kx vx))
-| Node Black a ky vy b,   kx, vx =>
-    if kx < ky then
-      (if isRed a then balance1 ky vy b (ins a kx vx)
-       else Node Black (ins a kx vx) ky vy b)
-    else if kx = ky then Node Black a kx vx b
-    else if isRed b then balance2 a ky vy (ins b kx vx)
-         else Node Black a ky vy (ins b kx vx)
+def ins (kx : Nat) (vx : Bool) : Tree → Tree
+  | .leaf => .node .red .leaf kx vx .leaf
+  | .node .red a ky vy b =>
+    (if kx < ky then .node .red (ins kx vx a) ky vy b
+     else if kx = ky then .node .red a kx vx b
+     else .node .red a ky vy (ins kx vx b))
+  | .node .black a ky vy b =>
+      if kx < ky then
+        (if isRed a then balance1 ky vy b (ins kx vx a)
+         else .node .black (ins kx vx a) ky vy b)
+      else if kx = ky then .node .black a kx vx b
+      else if isRed b then balance2 a ky vy (ins kx vx b)
+      else .node .black a ky vy (ins kx vx b)
 
 def setBlack : Tree → Tree
-| Node _ l k v r   => Node Black l k v r
-| e                => e
+  | .node _ l k v r   => .node .black l k v r
+  | e                 => e
 
-def insert (t : Tree) (k : Nat) (v : Bool) : Tree :=
-if isRed t then setBlack (ins t k v)
-else ins t k v
+def insert (k : Nat) (v : Bool) (t : Tree) : Tree :=
+  if isRed t then setBlack (ins k v t)
+  else ins k v t
 
 def mkMapAux : Nat → Tree → Tree
-| 0, m => m
-| n+1,   m => mkMapAux n (insert m n (n % 10 = 0))
+  | 0,   m => m
+  | n+1, m => mkMapAux n (insert n (n % 10 = 0) m)
 
 def mkMap (n : Nat) :=
-mkMapAux n Leaf
+  mkMapAux n .leaf
 
-def main (xs : List String) : IO UInt32 :=
-let m := mkMap xs.head!.toNat!;
-let v := fold (fun (k : Nat) (v : Bool) (r : Nat) => if v then r + 1 else r) m 0;
-IO.println (toString v) *>
-pure 0
+def main (xs : List String) : IO Unit :=
+  let m := mkMap xs.head!.toNat!
+  let v := fold (fun _ v r => if v then r + 1 else r) m 0
+  IO.println (toString v)

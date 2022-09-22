05dce2ea12ab6121018d8d50a49cff707a3b175f
Chris Lovett <clovett@microsoft.com>
2022-09-22 15:22:24 -0700

    test: add tests for underscore related completion.

---
+++ tests/lean/interactive/completion8.lean
@@ -0,0 +1,8 @@
+#eval _
+     --^ textDocument/completion
+
+#eval Array.append _
+                  --^ textDocument/completion
+
+#eval a_
+      --^ textDocument/completion
---
+++ tests/lean/interactive/completion9.lean
@@ -0,0 +1,7 @@
+def _foo := 123
+
+#eval _
+     --^ textDocument/completion
+
+#eval _f
+      --^ textDocument/completion

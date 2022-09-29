691835037e94d7a14485d59cc070396cb98dbf79
E.W.Ayers <edward.ayers@outlook.com>
2022-09-29 15:26:10 +0100

    feat: code action resolvers

--- tests/lean/interactive/codeaction.lean
+++ tests/lean/interactive/codeaction.lean
@@ -3,16 +3,28 @@ import Lean
 open Lean Server Lsp
 
 @[codeActionProvider]
-def helloProvider : CodeActionProvider := fun (params : CodeActionParams) => do
-  let uri := params.textDocument.uri
-  return RequestTask.pure #[{
+def helloProvider : CodeActionProvider := fun params _snap => do
+  let td := params.textDocument
+  let edit : TextEdit := {
+      range := params.range,
+      newText := "hello!!!"
+    }
+  let ca : CodeAction := {
     title := "hello world",
     kind? := "quickfix",
-    edit? := WorkspaceEdit.ofTextEdit uri {
-      range := params.range,
-      newText := "hello!!!",
+    edit? := WorkspaceEdit.ofTextEdit td.uri edit
+  }
+  let longRunner : CodeAction := {
+    title := "a long-running action",
+    kind? := "refactor",
+  }
+  let lazyResult : IO CodeAction := do
+    let v? ← IO.getEnv "PWD"
+    let v := v?.getD "none"
+    return { longRunner with
+      edit? := WorkspaceEdit.ofTextEdit td.uri { range := params.range, newText := v}
     }
-  }]
+  return #[ca, {eager := longRunner, lazy? := lazyResult}]
 
 theorem asdf : (x : Nat) → x = x := by
   intro x

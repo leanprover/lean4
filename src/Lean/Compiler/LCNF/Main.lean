8ed831101eafe3f7207615a8ecc4048c1db8f0be
Sebastian Ullrich <sebasti@nullri.ch>
2022-10-04 11:03:07 +0200

    chore: benchmark new compiler

--- src/Lean/Compiler/LCNF/Main.lean
+++ src/Lean/Compiler/LCNF/Main.lean
@@ -91,8 +91,9 @@ def showDecl (phase : Phase) (declName : Name) : CoreM Format := do
   ppDecl' decl
 
 @[export lean_lcnf_compile_decls]
-def main (declNames : List Name) : CoreM Unit :=
-  CompilerM.run <| discard <| PassManager.run declNames.toArray
+def main (declNames : List Name) : CoreM Unit := do
+  profileitM Exception "compilation new" (← getOptions) do
+    CompilerM.run <| discard <| PassManager.run declNames.toArray
 
 builtin_initialize
   registerTraceClass `Compiler.init (inherited := true)

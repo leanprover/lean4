ebdbdc10436a4d9d6b66acf78aae7a23f5bd073f
Leonardo de Moura <leonardo@microsoft.com>
2022-10-04 17:41:41 -0700

    chore: temporarily disable `extendJoinPointContext`
    see #1686

--- src/Lean/Compiler/LCNF/Passes.lean
+++ src/Lean/Compiler/LCNF/Passes.lean
@@ -44,7 +44,7 @@ def builtinPassManager : PassManager := {
     findJoinPoints,
     pullFunDecls,
     reduceJpArity,
-    extendJoinPointContext,
+    -- extendJoinPointContext,
     simp { etaPoly := true, inlinePartial := true, implementedBy := true } (occurrence := 1),
     specialize,
     simp (occurrence := 2),

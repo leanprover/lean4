435fdb8ffa872756ae04d8754581b6a876ca3627
Sebastian Ullrich <sebasti@nullri.ch>
2022-10-14 09:59:03 +0200

    fix: copy all crt files to sysroots

--- script/prepare-llvm-linux.sh
+++ script/prepare-llvm-linux.sh
@@ -29,8 +29,8 @@ find stage1 -type f -exec strip --strip-unneeded '{}' \; 2> /dev/null
 # lean.h dependencies
 $CP llvm/lib/clang/*/include/{std*,__std*,limits}.h stage1/include/clang
 # ELF dependencies, must be put there for `--sysroot`
-$CP $GLIBC/lib/crt* llvm/lib/
-$CP $GLIBC/lib/crt* stage1/lib/
+$CP $GLIBC/lib/*crt* llvm/lib/
+$CP $GLIBC/lib/*crt* stage1/lib/
 # host runtime
 (cd llvm-host; $CP --parents lib/clang/*/lib/*/{clang_rt.*.o,libclang_rt.builtins*} ./)
 $CP llvm-host/lib/*/lib{c++,c++abi,unwind}.* $GMP/lib/libgmp.a llvm-host/lib/
@@ -46,7 +46,7 @@ $CP llvm/lib/*/lib{c++,c++abi,unwind}.* $GMP/lib/libgmp.a stage1/lib/
 $CP llvm/lib/*/lib{c++,c++abi,unwind}.* llvm/lib/
 $CP -r llvm/include/*/c++ llvm/include/
 # glibc: use for linking (so Lean programs don't embed newer symbol versions), but not for running (because libc.so, librt.so, and ld.so must be compatible)!
-$CP $GLIBC/lib/{libc_nonshared.a,Scrt1.o} stage1/lib/glibc
+$CP $GLIBC/lib/libc_nonshared.a stage1/lib/glibc
 $CP $GLIBC/lib/Scrt1.o stage1/lib
 for f in $GLIBC/lib/lib{c,dl,m,rt,pthread}-*; do b=$(basename $f); cp $f stage1/lib/glibc/${b%-*}.so; done
 OPTIONS=()

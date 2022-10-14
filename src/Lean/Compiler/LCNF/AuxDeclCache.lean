6ed89a4ceb7861a3027dc3a8b95f9cf38a3c3d30
Leonardo de Moura <leonardo@microsoft.com>
2022-10-13 18:34:57 -0700

    chore: revert cross module lambda lifting cache
    It adds almost 50Mb to the .olean files.

--- src/Lean/Compiler/LCNF/AuxDeclCache.lean
+++ src/Lean/Compiler/LCNF/AuxDeclCache.lean
@@ -9,47 +9,23 @@ import Lean.Compiler.LCNF.Internalize
 
 namespace Lean.Compiler.LCNF
 
-namespace AuxDecl
-abbrev Cache := SMap Decl Name
+abbrev AuxDeclCache := PHashMap Decl Name
 
-structure CacheEntry where
-  key : Decl
-  declName : Name
-  deriving Inhabited
-
-def addEntry (cache : Cache) (e : CacheEntry) : Cache :=
-  cache.insert e.key e.declName
-
-builtin_initialize auxDeclCacheExt : SimplePersistentEnvExtension CacheEntry Cache ←
-  registerSimplePersistentEnvExtension {
-    addEntryFn    := addEntry
-    addImportedFn := fun es => (mkStateFromImportedEntries addEntry {} es).switch
-  }
+builtin_initialize auxDeclCacheExt : EnvExtension AuxDeclCache ← registerEnvExtension (pure {})
 
 inductive CacheAuxDeclResult where
   | new
   | alreadyCached (declName : Name)
 
-def cache (decl : Decl) : CompilerM CacheAuxDeclResult := do
-  if (← getPhase) matches .base then
-    -- We do not cache eager lambda lifting results
-    return .new
-  else
-    let key := { decl with name := .anonymous }
-    let key ← normalizeFVarIds key
-    match auxDeclCacheExt.getState (← getEnv) |>.find? key with
-    | some declName =>
-      return .alreadyCached declName
-    | none =>
-      modifyEnv fun env => auxDeclCacheExt.addEntry env { key, declName := decl.name }
-      return .new
-
-end AuxDecl
-
-open AuxDecl
-
 def cacheAuxDecl (decl : Decl) : CompilerM CacheAuxDeclResult := do
-  cache decl
+  let key := { decl with name := .anonymous }
+  let key ← normalizeFVarIds key
+  match auxDeclCacheExt.getState (← getEnv) |>.find? key with
+  | some declName =>
+    return .alreadyCached declName
+  | none =>
+    modifyEnv fun env => auxDeclCacheExt.modifyState env fun s => s.insert key decl.name
+    return .new
 
 end Lean.Compiler.LCNF
 

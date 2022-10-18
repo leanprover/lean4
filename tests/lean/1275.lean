c8ba66445a48fd6a29557d2491d34f5952775c3c
Sebastian Ullrich <sebasti@nullri.ch>
2022-10-18 10:59:17 +0200

    fix: wrap `&"..." <|> ...` as well

--- tests/lean/1275.lean
+++ tests/lean/1275.lean
@@ -1,7 +1,7 @@
 import Lean open Lean
 
 syntax "👉" (ident <|> "_") : term
-#check `(👉 $_)
+#check fun x => `(👉 $x)
 #eval match Unhygienic.run `(👉 _) with
   | `(👉 $_:ident) => false
   | `(👉 _) => true
@@ -9,4 +9,15 @@ syntax "👉" (ident <|> "_") : term
 #eval match Unhygienic.run `(👉 x) with
   | `(👉 _) => false
   | `(👉 $_:ident) => true
+  | _ => false
+
+syntax "bar" (&"baz" <|> &"boing") : term
+#check fun x => `(bar $x)
+#eval match Unhygienic.run `(bar boing) with
+  | `(bar baz) => false
+  | `(bar boing) => true
+  | _ => false
+#eval match Unhygienic.run `(bar baz) with
+  | `(bar boing) => false
+  | `(bar baz) => true
   | _ => false

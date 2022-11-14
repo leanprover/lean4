1fe4231e57fac802836c29d00c9c7b39d1dc4c67
Alex J. Best <alex.j.best@gmail.com>
2022-11-14 01:28:09 +0100

    add test

---
+++ tests/lean/1821.lean
@@ -0,0 +1,5 @@
+theorem boom : -2147483648 + 2147483648 = 0 := by rfl
+theorem boom' : -2147483648 + 2147483648 = -18446744069414584320 := by native_decide
+theorem boom'' : False := by
+  have := boom'
+  simp [boom] at this

c76215116987dba4750a3b37376cc14dcda9d9fa
Siddharth <siddu.druid@gmail.com>
2022-11-17 18:29:15 -0800

    Update src/Lean/Compiler/IR/LLVMBindings.lean
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>

--- src/Lean/Compiler/IR/LLVMBindings.lean
+++ src/Lean/Compiler/IR/LLVMBindings.lean
@@ -2,7 +2,9 @@
 Copyright (c) 2022 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Siddharth Bhat
+-/
 
+/-!
 This file defines bindings for LLVM. The main mechanisms
 are to:
 - Mirror the values of C enumerations.

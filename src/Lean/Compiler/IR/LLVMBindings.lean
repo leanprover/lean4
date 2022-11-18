576783af33b466f5226c87732dabb5d25fbf7814
Siddharth Bhat <siddu.druid@gmail.com>
2022-11-18 03:46:05 +0000

    fix: use suggested structure API suggested by @Kha, @gebner
    We now use
    
    structure Foo (ctx: Context) where
      private mk ::
      ptr : USize
    
    Also fix the whitespace issue --  'f(var: T)' → 'f (var : T)'

--- src/Lean/Compiler/IR/LLVMBindings.lean
+++ src/Lean/Compiler/IR/LLVMBindings.lean
@@ -28,267 +28,276 @@ def NE : UInt64 := EQ + 1
 def UGT : UInt64 := NE + 1
 end IntPredicate
 
-opaque BasicBlock : Type
-opaque Context : Type
-opaque Module : Type
-opaque Builder : Type
-opaque LLVMType : Type
-opaque Value : Type
-opaque MemoryBuffer : Type
-opaque Target : Type
-opaque TargetMachine : Type
+structure Context where 
+  private mk :: ptr : USize
+structure BasicBlock (ctx : Context)  where 
+  private mk :: ptr : USize
+structure Module (ctx : Context) where 
+  private mk :: ptr : USize
+structure Builder (ctx : Context) where 
+  private mk :: ptr : USize
+structure LLVMType (ctx : Context) where 
+  private mk :: ptr : USize
+structure Value (ctx : Context) where 
+  private mk :: ptr : USize
+structure MemoryBuffer (ctx : Context) where 
+  private mk :: ptr : USize
+structure Target (ctx : Context) where 
+  private mk :: ptr : USize
+structure TargetMachine (ctx : Context) where 
+  private mk :: ptr : USize
 
 -- A raw pointer to a C object, whose Lean representation
 -- is given by α
-structure Ptr (α: Type): Type where
-  ptr: USize
+structure Ptr (α: Type) : Type where
+  ptr : USize
 
 @[extern "lean_llvm_create_context"]
-opaque createContext: IO (Ptr Context)
+opaque createContext : IO (Context)
 
 @[extern "lean_llvm_create_module"]
-opaque createModule (ctx: @&Ptr Context) (name: @&String): IO (Ptr Module)
+opaque createModule (ctx : @&Context) (name : @&String) : IO (Module ctx)
 
 @[extern "lean_llvm_module_to_string"]
-opaque moduleToString (m: @&Ptr Module): IO String
+opaque moduleToString (m : @&Module ctx) : IO String
 
 @[extern "lean_llvm_write_bitcode_to_file"]
-opaque writeBitcodeToFile (m: @&Ptr Module) (path: @&String): IO Unit
+opaque writeBitcodeToFile (m : @&Module ctx) (path : @&String) : IO Unit
 
 @[extern "lean_llvm_add_function"]
-opaque addFunction (m: @&Ptr Module) (name: @&String) (type: @&Ptr LLVMType): IO (Ptr Value)
+opaque addFunction (m : @&Module ctx) (name : @&String) (type : @&LLVMType ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_get_named_function"]
-opaque getNamedFunction (m: @&Ptr Module) (name: @&String): IO (Option (Ptr Value))
+opaque getNamedFunction (m : @&Module ctx) (name : @&String) : IO (Option (Value ctx))
 
 @[extern "lean_llvm_add_global"]
-opaque addGlobal (m: @&Ptr Module) (name: @&String) (type: @&Ptr LLVMType): IO (Ptr Value)
+opaque addGlobal (m : @&Module ctx) (name : @&String) (type : @&LLVMType ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_get_named_global"]
-opaque getNamedGlobal (m: @&Ptr Module) (name: @&String): IO (Option (Ptr Value))
+opaque getNamedGlobal (m : @&Module ctx) (name : @&String) : IO (Option (Value ctx))
 
 @[extern "lean_llvm_build_global_string"]
-opaque buildGlobalString (builder: @&Ptr Builder) (value: @&String) (name: @&String := ""): IO (Ptr Value)
+opaque buildGlobalString (builder : @&Builder ctx) (value : @&String) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_set_initializer"]
-opaque setInitializer (glbl: @&Ptr Value) (val: @&Ptr Value): IO Unit
+opaque setInitializer (glbl : @&Value ctx) (val : @&Value ctx) : IO Unit
 
 @[extern "lean_llvm_function_type"]
-opaque functionType (retty: @&Ptr LLVMType) (args: @&Array (Ptr LLVMType)) (isVarArg: Bool := false): IO (Ptr LLVMType)
+opaque functionType (retty : @&LLVMType ctx) (args : @&Array (LLVMType ctx)) (isVarArg : Bool := false) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_void_type_in_context"]
-opaque voidType (ctx: @&Ptr Context): IO (Ptr LLVMType)
+opaque voidType (ctx : @&Context) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_int_type_in_context"]
-opaque intTypeInContext (ctx: @&Ptr Context) (width: UInt64): IO (Ptr LLVMType)
+opaque intTypeInContext (ctx : @&Context) (width : UInt64) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_float_type_in_context"]
-opaque floatTypeInContext (ctx: @&Ptr Context): IO (Ptr LLVMType)
+opaque floatTypeInContext (ctx : @&Context) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_double_type_in_context"]
-opaque doubleTypeInContext (ctx: @&Ptr Context): IO (Ptr LLVMType)
+opaque doubleTypeInContext (ctx : @&Context) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_pointer_type"]
-opaque pointerType (elemty: @&Ptr LLVMType): IO (Ptr LLVMType)
+opaque pointerType (elemty : @&LLVMType ctx) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_array_type"]
-opaque arrayType (elemty: @&Ptr LLVMType) (nelem: @&UInt64): IO (Ptr LLVMType)
+opaque arrayType (elemty : @&LLVMType ctx) (nelem : @&UInt64) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_const_array"]
-opaque constArray (elemty: @&Ptr LLVMType) (vals: @&Array (Ptr Value)): IO (Ptr LLVMType)
+opaque constArray (elemty : @&LLVMType ctx) (vals : @&Array (Value ctx)) : IO (LLVMType ctx)
 
 -- `constString` provides a `String` as a constant array of element type `i8`
 @[extern "lean_llvm_const_string"]
-opaque constString (ctx: @&Ptr Context) (str: @&String): IO (Ptr Value)
+opaque constString (ctx : @&Context) (str : @&String) : IO (Value ctx)
 
 @[extern "lean_llvm_const_pointer_null"]
-opaque constPointerNull (elemty: @&Ptr LLVMType): IO (Ptr Value)
+opaque constPointerNull (elemty : @&LLVMType ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_get_undef"]
-opaque getUndef (elemty: @&Ptr LLVMType): IO (Ptr Value)
+opaque getUndef (elemty : @&LLVMType ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_create_builder_in_context"]
-opaque createBuilderInContext (ctx: @&Ptr Context): IO (Ptr Builder)
+opaque createBuilderInContext (ctx : @&Context) : IO (Builder ctx)
 
 @[extern "lean_llvm_append_basic_block_in_context"]
-opaque appendBasicBlockInContext (ctx: @&Ptr Context) (fn: @& Ptr Value) (name:  @&String): IO (Ptr BasicBlock)
+opaque appendBasicBlockInContext (ctx : @&Context) (fn : @& Value ctx) (name :  @&String) : IO (BasicBlock ctx)
 
 @[extern "lean_llvm_position_builder_at_end"]
-opaque positionBuilderAtEnd (builder: @&Ptr Builder) (bb: @& Ptr BasicBlock): IO Unit
+opaque positionBuilderAtEnd (builder : @&Builder ctx) (bb : @& BasicBlock ctx) : IO Unit
 
 @[extern "lean_llvm_build_call"]
-opaque buildCall (builder: @&Ptr Builder) (fn: @&Ptr Value) (args: @&Array (Ptr Value)) (name:  @&String := ""): IO (Ptr Value)
+opaque buildCall (builder : @&Builder ctx) (fn : @&Value ctx) (args : @&Array (Value ctx)) (name :  @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_set_tail_call"]
-opaque setTailCall (fn: @&Ptr Value) (istail: Bool): IO Unit
+opaque setTailCall (fn : @&Value ctx) (istail : Bool) : IO Unit
 
 @[extern "lean_llvm_build_cond_br"]
-opaque buildCondBr (builder: @&Ptr Builder) (if_: @&Ptr Value) (thenbb: @&Ptr BasicBlock) (elsebb: @&Ptr BasicBlock): IO (Ptr Value)
+opaque buildCondBr (builder : @&Builder ctx) (if_ : @&Value ctx) (thenbb : @&BasicBlock ctx) (elsebb : @&BasicBlock ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_build_br"]
-opaque buildBr (builder: @&Ptr Builder) (bb: @&Ptr BasicBlock): IO (Ptr Value)
+opaque buildBr (builder : @&Builder ctx) (bb : @&BasicBlock ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_build_alloca"]
-opaque buildAlloca (builder: @&Ptr Builder) (ty: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildAlloca (builder : @&Builder ctx) (ty : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_load"]
-opaque buildLoad (builder: @&Ptr Builder) (val: @&Ptr Value) (name: @&String := ""): IO (Ptr Value)
+opaque buildLoad (builder : @&Builder ctx) (val : @&Value ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_store"]
-opaque buildStore (builder: @&Ptr Builder) (val: @&Ptr Value) (store_loc_ptr: @&Ptr Value): IO Unit
+opaque buildStore (builder : @&Builder ctx) (val : @&Value ctx) (store_loc_ptr : @&Value ctx) : IO Unit
 
 @[extern "lean_llvm_build_ret"]
-opaque buildRet (builder: @&Ptr Builder) (val: @&Ptr Value): IO (Ptr Value)
+opaque buildRet (builder : @&Builder ctx) (val : @&Value ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_build_unreachable"]
-opaque buildUnreachable (builder: @&Ptr Builder): IO (Ptr Value)
+opaque buildUnreachable (builder : @&Builder ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_build_gep"]
-opaque buildGEP (builder: @&Ptr Builder) (base: @&Ptr Value) (ixs: @&Array (Ptr Value)) (name: @&String := ""): IO (Ptr Value)
+opaque buildGEP (builder : @&Builder ctx) (base : @&Value ctx) (ixs : @&Array (Value ctx)) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_inbounds_gep"]
-opaque buildInBoundsGEP (builder: @&Ptr Builder) (base: @&Ptr Value) (ixs: @&Array (Ptr Value)) (name: @&String := ""): IO (Ptr Value)
+opaque buildInBoundsGEP (builder : @&Builder ctx) (base : @&Value ctx) (ixs : @&Array (Value ctx)) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_pointer_cast"]
-opaque buildPointerCast (builder: @&Ptr Builder) (val: @&Ptr Value) (destTy: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildPointerCast (builder : @&Builder ctx) (val : @&Value ctx) (destTy : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_sext"]
-opaque buildSext (builder: @&Ptr Builder) (val: @&Ptr Value) (destTy: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildSext (builder : @&Builder ctx) (val : @&Value ctx) (destTy : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_zext"]
-opaque buildZext (builder: @&Ptr Builder) (val: @&Ptr Value) (destTy: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildZext (builder : @&Builder ctx) (val : @&Value ctx) (destTy : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_sext_or_trunc"]
-opaque buildSextOrTrunc (builder: @&Ptr Builder) (val: @&Ptr Value) (destTy: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildSextOrTrunc (builder : @&Builder ctx) (val : @&Value ctx) (destTy : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_switch"]
-opaque buildSwitch (builder: @&Ptr Builder) (val: @&Ptr Value) (elseBB: @&Ptr BasicBlock) (numCasesHint: @&UInt64): IO (Ptr Value)
+opaque buildSwitch (builder : @&Builder ctx) (val : @&Value ctx) (elseBB : @&BasicBlock ctx) (numCasesHint : @&UInt64) : IO (Value ctx)
 
 @[extern "lean_llvm_build_ptr_to_int"]
-opaque buildPtrToInt (builder: @&Ptr Builder) (ptr: @&Ptr Value) (destTy: @&Ptr LLVMType) (name: @&String := ""): IO (Ptr Value)
+opaque buildPtrToInt (builder : @&Builder ctx) (ptr : @&Value ctx) (destTy : @&LLVMType ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_mul"]
-opaque buildMul (builder: @&Ptr Builder) (x y: @&Ptr Value) (name: @&String): IO (Ptr Value)
+opaque buildMul (builder : @&Builder ctx) (x y : @&Value ctx) (name : @&String) : IO (Value ctx)
 
 @[extern "lean_llvm_build_add"]
-opaque buildAdd (builder: @&Ptr Builder) (x y: @&Ptr Value) (name: @&String): IO (Ptr Value)
+opaque buildAdd (builder : @&Builder ctx) (x y : @&Value ctx) (name : @&String) : IO (Value ctx)
 
 @[extern "lean_llvm_build_sub"]
-opaque buildSub (builder: @&Ptr Builder) (x y: @&Ptr Value) (name: @&String := ""): IO (Ptr Value)
+opaque buildSub (builder : @&Builder ctx) (x y : @&Value ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_not"]
-opaque buildNot (builder: @&Ptr Builder) (x: @&Ptr Value) (name: @&String := ""): IO (Ptr Value)
+opaque buildNot (builder : @&Builder ctx) (x : @&Value ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_build_icmp"]
-opaque buildICmp (builder: @&Ptr Builder) (predicate: UInt64) (x y: @&Ptr Value) (name: @&String := ""): IO (Ptr Value)
+opaque buildICmp (builder : @&Builder ctx) (predicate : UInt64) (x y : @&Value ctx) (name : @&String := "") : IO (Value ctx)
 
 @[extern "lean_llvm_add_case"]
-opaque addCase (switch onVal: @&Ptr Value) (destBB: @&Ptr BasicBlock): IO Unit
+opaque addCase (switch onVal : @&Value ctx) (destBB : @&BasicBlock ctx) : IO Unit
 
 @[extern "lean_llvm_get_insert_block"]
-opaque getInsertBlock (builder: @&Ptr Builder): IO (Ptr BasicBlock)
+opaque getInsertBlock (builder : @&Builder ctx) : IO (BasicBlock ctx)
 
 @[extern "lean_llvm_clear_insertion_position"]
-opaque clearInsertionPosition (builder: @&Ptr Builder): IO Unit
+opaque clearInsertionPosition (builder : @&Builder ctx) : IO Unit
 
 @[extern "lean_llvm_get_basic_block_parent"]
-opaque getBasicBlockParent (bb: @&Ptr BasicBlock): IO (Ptr Value)
+opaque getBasicBlockParent (bb : @&BasicBlock ctx) : IO (Value ctx)
 
 @[extern "lean_llvm_type_of"]
-opaque typeOf (val: @&Ptr Value): IO (Ptr LLVMType)
+opaque typeOf (val : @&Value ctx) : IO (LLVMType ctx)
 
 @[extern "lean_llvm_const_int"]
-opaque constInt (intty: @&Ptr LLVMType) (value: @&UInt64) (signExtend: @Bool := false): IO (Ptr Value)
+opaque constInt (intty : @&LLVMType ctx) (value : @&UInt64) (signExtend : @Bool := false) : IO (Value ctx)
 
 @[extern "lean_llvm_print_module_to_string"]
-opaque printModuletoString (mod: @&Ptr Module): IO (String)
+opaque printModuletoString (mod : @&Module ctx) : IO (String)
 
 @[extern "lean_llvm_print_module_to_file"]
-opaque printModuletoFile (mod: @&Ptr Module) (file: @&String): IO Unit
+opaque printModuletoFile (mod : @&Module ctx) (file : @&String) : IO Unit
 
 @[extern "llvm_count_params"]
-opaque countParams (fn: @&Ptr Function): UInt64 -- will this cause problems..?
+opaque countParams (fn : @&Ptr Function) : UInt64 -- will this cause problems..?
 
 @[extern "llvm_get_param"]
-opaque getParam (fn: @&Ptr Function) (ix: UInt64): IO (Ptr Value)
+opaque getParam (fn : @&Ptr Function) (ix : UInt64) : IO (Value ctx)
 
 @[extern "lean_llvm_create_memory_buffer_with_contents_of_file"]
-opaque createMemoryBufferWithContentsOfFile (path: @&String): IO (Ptr MemoryBuffer)
+opaque createMemoryBufferWithContentsOfFile (path : @&String) : IO (MemoryBuffer ctx)
 
 @[extern "lean_llvm_parse_bitcode"]
-opaque parseBitcode (context: @&Ptr Context) (membuf: @&Ptr MemoryBuffer): IO (Ptr Module)
+opaque parseBitcode (ctx : @&Context) (membuf : @&MemoryBuffer ctx) : IO (Module ctx)
 
 @[extern "lean_llvm_link_modules"]
-opaque linkModules (dest: @Ptr Module) (src: @&Ptr Module): IO Unit
+opaque linkModules (dest : @Module ctx) (src : @&Module ctx) : IO Unit
 
 @[extern "lean_llvm_get_default_target_triple"]
-opaque getDefaultTargetTriple: IO String
+opaque getDefaultTargetTriple : IO String
 
 @[extern "lean_llvm_get_target_from_triple"]
-opaque getTargetFromTriple (triple: @&String): IO (Ptr Target)
+opaque getTargetFromTriple (triple : @&String) : IO (Target ctx)
 
 @[extern "lean_llvm_create_target_machine"]
-opaque createTargetMachine (target: @&Ptr Target) (tripleStr: @&String) (cpu: @&String) (features: @&String): IO (Ptr TargetMachine)
+opaque createTargetMachine (target : @&Target ctx) (tripleStr : @&String) (cpu : @&String) (features : @&String) : IO (TargetMachine ctx)
 
 @[extern "lean_llvm_target_machine_emit_to_file"]
-opaque targetMachineEmitToFile (targetMachine: @&Ptr TargetMachine) (module: @&Ptr Module) (filepath: @&String) (codegenType: @&UInt64): IO Unit
+opaque targetMachineEmitToFile (targetMachine : @&TargetMachine ctx) (module : @&Module ctx) (filepath : @&String) (codegenType : @&UInt64) : IO Unit
 
 
 -- Helper to add a function if it does not exist, and to return the function handle if it does.
-def getOrAddFunction(m: Ptr Module) (name: String) (type: Ptr LLVMType): IO (Ptr Value) :=  do
+def getOrAddFunction(m : Module ctx) (name : String) (type : LLVMType ctx) : IO (Value ctx) :=  do
   match (← getNamedFunction m name) with
   | .some fn => return fn
   | .none => addFunction m name type
 
-def getOrAddGlobal(m: Ptr Module) (name: String) (type: Ptr LLVMType): IO (Ptr Value) :=  do
+def getOrAddGlobal(m : Module ctx) (name : String) (type : LLVMType ctx) : IO (Value ctx) :=  do
   match (← getNamedGlobal m name) with
   | .some fn => return fn
   | .none => addGlobal m name type
 
 
-def i1Type (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i1Type (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   LLVM.intTypeInContext ctx 1
 
-def i8Type (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i8Type (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   LLVM.intTypeInContext ctx 8
 
-def i16Type (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i16Type (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   LLVM.intTypeInContext ctx 16
 
-def i32Type (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i32Type (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   LLVM.intTypeInContext ctx 32
 
-def i64Type (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i64Type (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   LLVM.intTypeInContext ctx 64
 
-def voidPtrType (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def voidPtrType (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   do LLVM.pointerType (← LLVM.intTypeInContext ctx 8)
 
-def i8PtrType (ctx: LLVM.Ptr LLVM.Context): IO (LLVM.Ptr LLVM.LLVMType) :=
+def i8PtrType (ctx : LLVM.Context) : IO (LLVM.LLVMType ctx) :=
   voidPtrType ctx
 
-def constTrue (ctx: Ptr Context): IO (Ptr Value) :=
+def constTrue (ctx : Context) : IO (Value ctx) :=
   do constInt (← i1Type ctx) 1 (signExtend := false)
 
-def constFalse (ctx: Ptr Context): IO (Ptr Value) :=
+def constFalse (ctx : Context) : IO (Value ctx) :=
   do constInt (← i1Type ctx) 0 (signExtend := false)
 
-def constInt' (ctx: Ptr Context) (width: UInt64) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constInt' (ctx : Context) (width : UInt64) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
  do constInt (← LLVM.intTypeInContext ctx width) value signExtend
 
-def constInt1 (ctx: Ptr Context) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constInt1 (ctx : Context) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
   constInt' ctx 1 value signExtend
 
-def constInt8 (ctx: Ptr Context) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constInt8 (ctx : Context) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
   constInt' ctx 8 value signExtend
 
-def constInt32 (ctx: Ptr Context) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constInt32 (ctx : Context) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
   constInt' ctx 32 value signExtend
 
-def constInt64 (ctx: Ptr Context) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constInt64 (ctx : Context) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
   constInt' ctx 64 value signExtend
 
-def constIntUnsigned (ctx: Ptr Context) (value: UInt64) (signExtend: Bool := false): IO (Ptr Value) :=
+def constIntUnsigned (ctx : Context) (value : UInt64) (signExtend : Bool := false) : IO (Value ctx) :=
   constInt' ctx 64 value signExtend
 
 end LLVM

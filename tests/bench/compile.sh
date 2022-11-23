98208224f5cca23ff5da904649920a998fc3dd59
Siddharth Bhat <siddu.druid@gmail.com>
2022-11-23 23:18:03 +0000

    fix: 'tests/bench/compile.sh' should check for LLVM support

--- tests/bench/compile.sh
+++ tests/bench/compile.sh
@@ -2,4 +2,8 @@
 source ../common.sh
 
 compile_lean_c_backend
-compile_lean_llvm_backend
+# Then check the LLVM version
+if lean_has_llvm_support; then
+  echo "running LLVM program..."
+  compile_lean_llvm_backend
+fi

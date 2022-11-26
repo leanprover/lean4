4b3ec3788c322644bb83a36932106b009b84c3aa
Scott Morrison <scott.morrison@gmail.com>
2022-11-27 08:25:42 +1100

    fix two tests

--- tests/lean/run/inj1.lean
+++ tests/lean/run/inj1.lean
@@ -8,20 +8,17 @@ by {
 theorem test2 {α} (a b : α) (as bs : List α) (h : a::as = b::bs) : a = b :=
 by {
   injection h with h1 h2;
-  exact h1
 }
 
 theorem test3 {α} (a b : α) (as bs : List α) (h : (x : List α) → (y : List α) → x = y) : as = bs :=
 have : a::as = b::bs := h (a::as) (b::bs);
 by {
   injection this with h1 h2;
-  exact h2
 }
 
 theorem test4 {α} (a b : α) (as bs : List α) (h : (x : List α) → (y : List α) → x = y) : as = bs :=
 by {
   injection h (a::as) (b::bs) with h1 h2;
-  exact h2
 }
 
 theorem test5 {α} (a : α) (as : List α) (h : a::as = []) : 0 > 1 :=
--- tests/lean/run/injIssue.lean
+++ tests/lean/run/injIssue.lean
@@ -1,4 +1,3 @@
 theorem ex1 (n m : Nat) : some n = some m → n = m := by
   intro h
   injection h with h
-  exact h

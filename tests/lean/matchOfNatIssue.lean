30309dfbef689b2161fe41514547abdd77e84d4b
Scott Morrison <scott.morrison@gmail.com>
2022-12-13 20:10:43 +1100

    fix test

--- tests/lean/matchOfNatIssue.lean
+++ tests/lean/matchOfNatIssue.lean
@@ -1,13 +1,13 @@
-example (x : Int) (h : x = 2) : Int.div 2 1 = x := by
-  simp [Int.div]
+example (x : Int) (h : x = 2) : Int.tdiv 2 1 = x := by
+  simp [Int.tdiv]
   trace_state
   simp [h]
 
-example (n : Nat) : Int.div (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n / 0) := by
-  simp [Int.div]
+example (n : Nat) : Int.tdiv (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n / 0) := by
+  simp [Int.tdiv]
 
-example (n : Nat) : Int.div (Int.ofNat n) 0 = Int.ofNat (n / 0) := by
-  simp [Int.div]
+example (n : Nat) : Int.tdiv (Int.ofNat n) 0 = Int.ofNat (n / 0) := by
+  simp [Int.tdiv]
 
 example (n : Nat) : Int.mul (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n * 0) := by
   simp [Int.mul]

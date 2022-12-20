00bbafa693f646729f6561e13eb889dffa4c188b
Siddharth Bhat <siddu.druid@gmail.com>
2022-12-20 00:05:22 +0000

    fix: make test scripts consistent
    The scripts `tests/{compiler,bench}/test_single.sh` run the C
    and LLVM backends. We make them consistent:
    
    - print "running LLVM" in both scripts.
    - delete out `.out` file in both scripts.
    - add comment describing C and LLVM backends in both scripts.

--- tests/bench/test_single.sh
+++ tests/bench/test_single.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 source ../common.sh
 
+# First check the C version actually works...
 compile_lean_c_backend
 exec_check "./$f.out" $(cat "$f.args")
 diff_produced
 
+# Then check the LLVM version
 if lean_has_llvm_support; then
+    echo "running LLVM program..."
+    rm "./$f.out" || true
     compile_lean_llvm_backend
     exec_check "./$f.out" $(cat "$f.args")
     diff_produced

5b156fbf60db3ce04d04e068a8be9ad71ce0671d
Gabriel Ebner <gebner@gebner.org>
2023-02-05 10:27:03 -0800

    fix: reenable structure eta during tc search
    Fixes #2074.

--- src/Lean/Meta/SynthInstance.lean
+++ src/Lean/Meta/SynthInstance.lean
@@ -24,11 +24,6 @@ register_builtin_option synthInstance.maxSize : Nat := {
   descr := "maximum number of instances used to construct a solution in the type class instance synthesis procedure"
 }
 
-register_builtin_option synthInstance.etaExperiment : Bool := {
-  defValue := false
-  descr := "[DO NOT USE EXCEPT FOR TESTING] enable structure eta for type-classes during type-class search"
-}
-
 namespace SynthInstance
 
 def getMaxHeartbeats (opts : Options) : Nat :=
@@ -664,14 +659,13 @@ def synthInstance? (type : Expr) (maxResultSize? : Option Nat := none) : MetaM (
   withTraceNode `Meta.synthInstance
     (return m!"{exceptOptionEmoji ·} {← instantiateMVars type}") do
   /-
-    We disable eta for structures that are not classes during TC resolution because it allows us to find unintended solutions.
-    See discussion at
-      https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/.60constructor.60.20and.20.60Applicative.60/near/279984801
+  We explicitly enable eta for structures because that is required to make
+  subobject projections and constructor applications commute (and we would get
+  non-defeq diamonds otherwise).
+  https://github.com/leanprover/lean4/issues/2074#issuecomment-1418198304
   -/
-  let etaStruct := if synthInstance.etaExperiment.get (← getOptions) then .all else .notClasses
   withConfig (fun config => { config with isDefEqStuckEx := true, transparency := TransparencyMode.instances,
-                                          foApprox := true, ctxApprox := true, constApprox := false,
-                                          etaStruct }) do
+                                          foApprox := true, ctxApprox := true, constApprox := false }) do
     let type ← instantiateMVars type
     let type ← preprocess type
     let s ← get

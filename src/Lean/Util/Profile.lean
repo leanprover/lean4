d51e404d6a00463f28a3852bf7685e9a117417bd
Sebastian Ullrich <sebasti@nullri.ch>
2023-03-30 14:34:15 +0200

    refactor: move profiling options to Lean

--- src/Lean/Util/Profile.lean
+++ src/Lean/Util/Profile.lean
@@ -7,6 +7,26 @@ import Lean.Data.Options
 
 namespace Lean
 
+register_builtin_option profiler : Bool := {
+  defValue := false
+  group    := "profiler"
+  descr    := "show execution times of various Lean components"
+}
+
+register_builtin_option profiler.threshold : Nat := {
+  defValue := 100
+  group    := "profiler"
+  descr    := "threshold in milliseconds, profiling times under threshold will not be reported individually"
+}
+
+@[export lean_get_profiler]
+private def get_profiler (o : Options) : Bool :=
+  profiler.get o
+
+@[export lean_get_profiler_threshold]
+def profiler.threshold.getSecs (o : Options) : Float :=
+  (profiler.threshold.get o).toFloat / 1000
+
 /-- Print and accumulate run time of `act` when option `profiler` is set to `true`. -/
 @[extern "lean_profileit"]
 def profileit {α : Type} (category : @& String) (opts : @& Options) (fn : Unit → α) (decl := Name.anonymous) : α := fn ()

41a3ebed028a111c3b8ed2711e7797f6eecdfaf6
Sebastian Ullrich <sebasti@nullri.ch>
2023-04-07 12:24:39 +0200

    fix: profiler threshold in C++

--- src/library/profiling.cpp
+++ src/library/profiling.cpp
@@ -14,7 +14,7 @@ bool get_profiler(options const & opts) {
     return lean_get_profiler(opts.to_obj_arg());
 }
 
-extern "C" float lean_get_profiler_threshold(obj_arg opts);
+extern "C" double lean_get_profiler_threshold(obj_arg opts);
 second_duration get_profiling_threshold(options const & opts) {
     double ms = lean_get_profiler_threshold(opts.to_obj_arg());
     return second_duration(ms);

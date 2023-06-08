462d30618472548e42b10d39bca5152602fed935
tydeu <tydeu@hatpress.net>
2023-06-08 05:44:03 -0400

    chore: fix test on non-Windows

--- test/50/test.sh
+++ test/50/test.sh
@@ -7,19 +7,24 @@ cd foo
 
 ${LAKE} build
 echo "# issue 50" | tee ../produced.out
-${LAKE} build -KleanArgs=-DwarningAsError=true |& tee -a ../produced.out
+${LAKE} build -KleanArgs=-DwarningAsError=true | tee -a ../produced.out
 
 ${LAKE} build
 echo "# issue 172" | tee -a ../produced.out
-${LAKE} build -KweakLeanArgs=-DwarningAsError=true |& tee -a ../produced.out
+${LAKE} build -KweakLeanArgs=-DwarningAsError=true | tee -a ../produced.out
 
 ${LAKE} build
 echo "# compile args" | tee -a ../produced.out
-${LAKE} build -KleancArgs=-DBAR |& tee -a ../produced.out
+${LAKE} build -KleancArgs=-DBAR | tee -a ../produced.out
 
 ${LAKE} build
 echo "# link args" | tee -a ../produced.out
-${LAKE} build -KlinkArgs=-Lbuild/lib |& tee -a ../produced.out
+${LAKE} build -KlinkArgs=-Lbuild/lib | tee -a ../produced.out
 
 # test
-diff --strip-trailing-cr ../expected.out ../produced.out
+if [ "$OS" = Windows_NT ]; then
+  sed -i 's/.exe//g' ../produced.out
+  diff --strip-trailing-cr ../expected.out ../produced.out
+else
+  diff ../expected.out ../produced.out
+fi

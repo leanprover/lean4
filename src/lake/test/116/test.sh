544b704a25894e7efa0396588984c712b1e783ff
Sebastian Ullrich <sebasti@nullri.ch>
2023-07-19 13:58:17 +0200

    test: add Lake tests

--- src/lake/test/116/test.sh
+++ src/lake/test/116/test.sh
@@ -14,14 +14,7 @@ INITD_NOT=$'Content-Length: 40\r\n\r\n{"jsonrpc":"2.0","method":"initialized"}'
 OPEN_REQ=$'Content-Length: 145\r\n\r\n{"jsonrpc":"2.0","method":"textDocument/didOpen","params":{"textDocument":{"uri":"file://Test.lean","languageId":"lean4","version":0,"text":""}}}'
 MSGS="$INIT_REQ$INITD_NOT$OPEN_REQ"
 
-set -o pipefail
-
-cp /dev/null lake.in
-tail -f lake.in | $LAKE serve 2>&1 > lake.out &
-pid=$!
-echo -n "$MSGS" >> lake.in
-sleep 3
-kill $pid
+(echo -n "$MSGS"; sleep 3) | $LAKE serve 2>&1 > lake.out || true
 
 echo
 grep -m1 Lake lake.out

ab4cce2f94479ca640454c621a63c0601643f739
F. G. Dorais <fgdorais@gmail.com>
2023-07-21 00:03:47 -0400

    feat: add namespace utility function

--- src/Lean/Data/OpenDecl.lean
+++ src/Lean/Data/OpenDecl.lean
@@ -28,4 +28,13 @@ def rootNamespace := `_root_
 def removeRoot (n : Name) : Name :=
   n.replacePrefix rootNamespace Name.anonymous
 
+/-- Adds namespace prefix unless in root namespace, when it just removes `_root_`.
+```
+addNamespaceUnlessRoot `a.b `c.d = `a.b.c.d
+addNamespaceUnlessRoot `a.b `_root_.c.d = `c.d
+```
+-/
+abbrev addNamespaceUnlessRoot (ns : Name) (n : Name) : Name :=
+  if rootNamespace.isPrefixOf n then removeRoot n else ns ++ n
+
 end Lean

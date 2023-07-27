a75fc1d75632128d9d8b60446cee0f907ea684cc
Wojciech Nawrocki <t-wnawrocki@microsoft.com>
2023-07-27 15:27:24 -0700

    Update src/Lean/Util/Trace.lean
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>

--- src/Lean/Util/Trace.lean
+++ src/Lean/Util/Trace.lean
@@ -193,9 +193,7 @@ def withTraceNode [MonadExcept ε m] [MonadLiftT BaseIO m] (cls : Name) (msg : E
     modifyTraces (oldTraces ++ ·)
     return (← MonadExcept.ofExcept res)
   let ref ← getRef
-  let mut m := match ← observing <| msg res with
-    | .ok m => m
-    | .error _ => m!"<exception thrown while producing trace node message>"
+  let mut m ← try msg res catch _ => m!"<exception thrown while producing trace node message>"
   if profiler.get opts || aboveThresh then
     m := m!"[{secs}s] {m}"
   addTraceNode oldTraces cls ref m collapsed

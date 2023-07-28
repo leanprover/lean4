74e0f09009377d2e85ee2cc4fa704bad47383dd5
Wojciech Nawrocki <t-wnawrocki@microsoft.com>
2023-07-28 08:01:13 -0700

    fix: handle error in withTraceNode message action (#2364)
    * fix: handle error in withTrace message action
    
    * Update src/Lean/Util/Trace.lean
    
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>
    
    * Update Trace.lean
    
    ---------
    
    Co-authored-by: Gabriel Ebner <gebner@gebner.org>

--- src/Lean/Util/Trace.lean
+++ src/Lean/Util/Trace.lean
@@ -193,7 +193,7 @@ def withTraceNode [MonadExcept ε m] [MonadLiftT BaseIO m] (cls : Name) (msg : E
     modifyTraces (oldTraces ++ ·)
     return (← MonadExcept.ofExcept res)
   let ref ← getRef
-  let mut m ← msg res
+  let mut m ← try msg res catch _ => pure m!"<exception thrown while producing trace node message>"
   if profiler.get opts || aboveThresh then
     m := m!"[{secs}s] {m}"
   addTraceNode oldTraces cls ref m collapsed

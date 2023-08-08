bb738796ae13e1fff6dec921b1a26dae3a040498
Sebastian Ullrich <sebasti@nullri.ch>
2023-08-08 14:24:15 +0200

    test: update `parser` benchmark, add to speedcenter suite

--- tests/bench/parser.lean
+++ tests/bench/parser.lean
@@ -1,8 +1,12 @@
 import Lean.Parser.Module
 
+/-!
+  Test parsing only on a .lean file, which necessarily has to be one that does
+  not depend on non-built-in syntax, e.g. `Init.Prelude`. -/
+
 def main : List String → IO Unit
 | [fname, n] => do
   let env ← Lean.mkEmptyEnvironment
   for _ in [0:n.toNat!] do
     discard $ Lean.Parser.testParseFile env fname
-| _    => throw $ IO.userError "give file"
+| _ => throw $ IO.userError "give file and iteration count"

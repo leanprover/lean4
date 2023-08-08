0ae593aef8cb02de214a8c897bd48ca5bc186c8e
tydeu <tydeu@hatpress.net>
2023-08-08 19:05:12 -0400

    test: `lake clean`

--- src/lake/examples/deps/test.sh
+++ src/lake/examples/deps/test.sh
@@ -13,3 +13,15 @@ $LAKE -d foo build --update
 
 # Test print-paths works (i.e., does not error)
 $LAKE -d foo print-paths A B
+
+# Test `lake clean`
+test -d a/build
+test -d b/build
+$LAKE -d foo clean a b
+test ! -d a/build
+test ! -d b/build
+test -d root/build
+test -d foo/build
+$LAKE -d foo clean
+test ! -d root/build
+test ! -d foo/build

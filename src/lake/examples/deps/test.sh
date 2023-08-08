67d89cafeb355c5a9a3d11909e7a13385c4bcc19
tydeu <tydeu@hatpress.net>
2023-08-08 18:54:33 -0400

    fix: `-d` option for `lake print-paths` + test

--- src/lake/examples/deps/test.sh
+++ src/lake/examples/deps/test.sh
@@ -1,10 +1,15 @@
-set -ex
+#!/usr/bin/env bash
+set -exo pipefail
+
+LAKE=${LAKE:-../../build/bin/lake}
 
 ./clean.sh
-./package.sh
+
+$LAKE -d bar build --update
+$LAKE -d foo build --update
+
 ./foo/build/bin/foo
 ./bar/build/bin/bar
 
-cd ./foo
-${LAKE:-../../../build/bin/lake} print-paths A B
-cd ..
+# Test print-paths works (i.e., does not error)
+$LAKE -d foo print-paths A B

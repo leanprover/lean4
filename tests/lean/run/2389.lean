55456ab4fb17f231a7c34cb47c9c22ca8b61c8d8
arthur-adjedj <arthur.adjedj@ens-paris-saclay.fr>
2023-08-10 17:16:51 +0200

    doc : document test #2389

--- tests/lean/run/2389.lean
+++ tests/lean/run/2389.lean
@@ -1,3 +1,9 @@
+/-!
+# Test verifying that nested predicates don't trigger the generation of `below` lemmas
+Since the case of nested predicates is not currently handled by `mkBelow`,
+trying to generate `Bar.below` triggers an error upon defining the inductive type.
+-/
+
 inductive Foo : Prop → Prop
 
 inductive Bar : Prop

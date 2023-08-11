96f04a01b45b9d22b5af865278e6d2f77f98715d
tydeu <tydeu@hatpress.net>
2023-08-11 01:06:12 -0400

    fix: delete `lake.lock` on error & test

--- src/lake/test/lock/test.sh
+++ src/lake/test/lock/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -mexo pipefail
+set -euxo pipefail
 
 LAKE=${LAKE:-../../build/bin/lake}
 
@@ -12,16 +12,26 @@ fi
 ./clean.sh
 
 # Test lock file creation on build
-touch test.log
-$LAKE build Loop 2>&1 > test.log &
-grep -q "Building" < <($TAIL --pid=$$ -f test.log)
-kill %%
+touch test1.log
+$LAKE build Loop 1> test1.log &
+test1_pid=$!
+grep -q "Building" < <($TAIL --pid=$$ -f test1.log)
 test -f build/lake.lock
+kill $test1_pid
+! wait $test1_pid
 
 # Test build waits when lock file present
-$LAKE build Test 2>&1 | tee test.log &
-grep -q "waiting" < <($TAIL --pid=$$ -f test.log)
+touch test2.log
+touch build/lake.lock
+$LAKE build Test 2> test2.log &
+test2_pid=$!
+grep -q "waiting" < <($TAIL --pid=$$ -f test2.log)
 # Test build resumes on lock file removal
 rm build/lake.lock
-fg
-echo "done"
+wait $test2_pid
+# Test build success does not leave lock file
+test ! -f build/lake.lock
+
+# Test build error still deletes lock file
+! $LAKE build Error
+test ! -f build/lake.lock

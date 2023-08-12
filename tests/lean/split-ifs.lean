870bade59312a962daa686bbb15020c40eb82bdf
F. G. Dorais <fgdorais@gmail.com>
2023-08-12 02:59:41 -0400

    chore: attempt to fix tests

--- tests/lean/split-ifs.lean
+++ tests/lean/split-ifs.lean
@@ -1,6 +1,15 @@
 /-! Test that the `split` tactic doesn't use `Classical.em` to split ifs.
 -/
 
-theorem ifthisthenthat (n : Nat) : (if n = 0 then 0 else 0) = 0 := by split <;> rfl
+/- Basic example -/
+theorem ittt1 (n : Nat) : (if n = 0 then 0 else 0) = 0 := by split <;> rfl
 
-#print axioms ifthisthenthat
+#print axioms ittt1
+
+/- Example where the decidabilty instance has loose bound variables
+  and split falls back on classical axioms (courtesy of Mario Carneiro). -/
+theorem ittt2 (c : Prop) :
+    (fun [Decidable c] => if c then 0 else 0) = (fun _ => 0) := by
+  split <;> rfl
+
+#print axioms ittt2

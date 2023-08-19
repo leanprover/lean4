1c813e25b0863e3639824a6af0a9a1d7989d6570
thorimur <68410468+thorimur@users.noreply.github.com>
2023-08-19 02:46:40 -0400

    docs: add module docstrings to test * also deleted superfluous `add_synthetic_goal`

--- tests/lean/refinePreservesNaturalMVars.lean
+++ tests/lean/refinePreservesNaturalMVars.lean
@@ -1,17 +1,27 @@
 import Lean
 
+/-! Ensures that `refine` does not remove pre-existing natural goals from the goal list. -/
+
 open Lean Meta Elab Tactic Term
 
 elab "add_natural_goal" s:ident " : " t:term : tactic => do
   let g ← mkFreshExprMVar (← elabType t) .natural s.getId
   appendGoals [g.mvarId!]
 
-elab "add_synthetic_goal" s:ident " : " t:term : tactic => do
-  let g ← mkFreshExprMVar (← elabType t) .syntheticOpaque s.getId
-  appendGoals [g.mvarId!]
+/-!
 
--- Should not yield (kernel) declaration includes metavariables;
--- instead, should error
+In the following, `refine` would erroneously close each focused goal, leading to a
+`(kernel) declaration has metavariables '_example'` error.
+
+This occurred because `withCollectingNewGoalsFrom` was only erroring on new natural goals (as
+determined by `index`), while simultaneously only passing through non-natural goals to construct
+the resulting goal list. This orphaned old natural metavariables and closed the goal list
+erroneously.
+
+As such, all of the following tests should lead to an `unsolved goals` error, followed by a
+`no goals` error (instead of a successful focus).
+
+-/
 
 example : Bool × Nat := by
   add_natural_goal d : Bool
@@ -27,7 +37,12 @@ example : Bool × Bool := by
   · case d => refine ?e
   · refine ?e
 
--- Should not error:
+/-!
+
+Previously, this would error, as `refine (?d, ?e)` erroneously closed the goal, leading to a
+`no goals` error. Instead, this should succeed.
+
+-/
 
 example : Bool × Bool := by
   add_natural_goal d : Bool

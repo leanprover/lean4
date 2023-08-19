f378fa00af2453db81bc86c8d59d4ee0f7ca90dc
arthur-adjedj <arthur.adjedj@gmail.com>
2023-08-19 13:36:07 +0200

    test: extend `2389.lean`

--- tests/lean/run/2389.lean
+++ tests/lean/run/2389.lean
@@ -4,7 +4,32 @@ Since the case of nested predicates is not currently handled by `mkBelow`,
 trying to generate `Bar.below` triggers an error upon defining the inductive type.
 -/
 
-inductive Foo : Prop → Prop
+inductive Forall (P : α → Prop) : List α → Prop
+  | nil : Forall P []
+  | cons : {x : α} → P x → Forall P l → Forall P (x::l)
 
-inductive Bar : Prop
-  | bar : Foo Bar → Bar
+inductive Tree : Type :=
+  | leaf : Nat → Tree
+  | node : List Tree → Tree
+
+set_option trace.Meta.IndPredBelow true
+
+/--Despite not having `.below` and `.brecOn`,
+the type is still usable thanks to well-founded recursion-/
+inductive OnlyZeros : Tree → Prop :=
+  | leaf : OnlyZeros (.leaf 0)
+  | node (l : List Tree): Forall OnlyZeros l → OnlyZeros (.node l)
+
+/- Fixpoint equivalent of `OnlyZeros`-/
+def onlyZeros : Tree → Prop
+  | .leaf n => n=0
+  | .node [] => True
+  | .node (x::s) => onlyZeros x ∧ onlyZeros (.node s)
+
+/- Pattern-matching on `OnlyZeros` works despite `below` and `brecOn` not being generated-/
+def toFixPoint : OnlyZeros t → onlyZeros t
+  | .leaf => rfl
+  | .node [] _ => True.intro
+  | .node (x::s) (.cons h p) => by
+    rw [onlyZeros] -- necessary because `onlyZeros` isn't structurally recursive
+    exact And.intro (toFixPoint h) (toFixPoint (.node s p))

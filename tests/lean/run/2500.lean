61d124f7bbf48b770d0e0e099d04145a4701bf89
arthur-adjedj <arthur.adjedj@ens-paris-saclay.fr>
2023-09-01 01:30:32 +0200

    fix : make `mk_no_confusion_type` handle delta-reduction when checking the inductive type.

---
+++ tests/lean/run/2500.lean
@@ -0,0 +1,10 @@
+/-!
+# Verify that the `noConfusion` lemma succeeds at being generated, despite the inductive type not being a syntactical telescope
+Fixes https://github.com/leanprover/lean4/issues/2500
+Ensures the `to_telescope` call in `mk_no_confusion_type` (`src/library/constructions/no_confusion.cpp`) takes the global environment into account,
+thus allowing for delta-reduction.
+-/
+
+def family := Type → Type
+
+inductive bad : family

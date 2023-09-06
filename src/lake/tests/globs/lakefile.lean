398c131620372a7164bdf80d313e4f6d38c16607
tydeu <tydeu@hatpress.net>
2023-09-06 09:08:48 -0400

    test: lake: merge `tests/102` into `globs`

--- src/lake/tests/globs/lakefile.lean
+++ src/lake/tests/globs/lakefile.lean
@@ -5,6 +5,9 @@ open Lake DSL
 package test
 
 @[default_target]
-lean_lib Test {
+lean_lib TBA where
+  globs := #[.andSubmodules `TBA]
+
+@[default_target]
+lean_lib Test where
   globs := #[.submodules "Test"]
-}
--- src/lake/tests/globs/test.sh
+++ src/lake/tests/globs/test.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# tests issue 174
-# also tests the issue reported on this Zulip thread:
-# https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/lake.20build.20all/near/370788618
-
 ./clean.sh
+
 LAKE=${LAKE:-../../build/bin/lake}
-$LAKE build -v
+
+# test that directory information is preserved during glob recursion
+# see https://github.com/leanprover/lake/issues/102
+
+$LAKE build TBA
+test -f build/lib/TBA.olean
+test -f build/lib/TBA/Eulerian.olean
+test -f build/lib/TBA/Eulerian/A.olean
+
+# test that globs capture modules with non-Lean names
+# see https://github.com/leanprover/lake/issues/174
+
+# also test that globs capture files in directories without a corresponding `.lean` file
+# see https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/lake.20build.20all/near/370788618
+
+$LAKE build Test
+test -f build/lib/Test/1.olean
+test -f build/lib/Test/Subtest/1.olean
+

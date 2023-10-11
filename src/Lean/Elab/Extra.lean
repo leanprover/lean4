6b48bb04bb56f2c2741f07e1c03361a3ef1a5117
Sebastian Ullrich <sebasti@nullri.ch>
2023-10-11 18:50:41 +0200

    chore: update copyright

--- src/Lean/Elab/Extra.lean
+++ src/Lean/Elab/Extra.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
+Authors: Leonardo de Moura, Kyle Miller, Sebastian Ullrich
 -/
 import Lean.Elab.App
 import Lean.Elab.BuiltinNotation

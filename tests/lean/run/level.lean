39210e748e52474dc3ef7acaf0e3f5c07a0e40e4
Mario Carneiro <di.gama@gmail.com>
2023-10-15 10:54:31 -0400

    fix: Level.geq is missing a case

--- tests/lean/run/level.lean
+++ tests/lean/run/level.lean
@@ -2,7 +2,10 @@ import Lean.Level
 
 open Lean
 
-#guard levelZero == levelZero
-#guard levelZero != mkLevelSucc levelZero
-#guard mkLevelMax (mkLevelSucc levelZero) levelZero != mkLevelSucc levelZero
-#guard mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelMax (mkLevelSucc levelZero) levelZero
+#eval
+  assert! levelZero == levelZero
+  assert! !(levelZero == mkLevelSucc levelZero)
+  assert! !(mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelSucc levelZero)
+  assert! mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelMax (mkLevelSucc levelZero) levelZero
+  assert! Level.geq (.max (.param `u) (.param `v)) (.imax (.param `u) (.param `v))
+  true

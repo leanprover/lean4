614b1712c5e70f5642fd81e795728d9570d9dff1
Mauricio Collares <mauricio@collares.org>
2023-10-20 20:42:44 +0200

    chore: change simp default to decide := false

--- src/Init/SimpLemmas.lean
+++ src/Init/SimpLemmas.lean
@@ -104,6 +104,7 @@ theorem dite_congr {_ : Decidable b} [Decidable c]
 @[simp] theorem implies_true (α : Sort u) : (α → True) = True := eq_true fun _ => trivial
 @[simp] theorem true_implies (p : Prop) : (True → p) = p := propext ⟨(· trivial), (fun _ => ·)⟩
 @[simp] theorem not_false_eq_true : (¬ False) = True := eq_true False.elim
+@[simp] theorem not_true_eq_false : (¬ True) = False := by decide
 
 @[simp] theorem Bool.or_false (b : Bool) : (b || false) = b  := by cases b <;> rfl
 @[simp] theorem Bool.or_true (b : Bool) : (b || true) = true := by cases b <;> rfl

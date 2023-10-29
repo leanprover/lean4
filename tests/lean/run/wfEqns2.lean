e53952f1674af5ac8f6bdfb5e1e28f3f10015ecd
Leonardo de Moura <leomoura@amazon.com>
2023-10-29 10:14:54 -0700

    chore: fix tests

--- tests/lean/run/wfEqns2.lean
+++ tests/lean/run/wfEqns2.lean
@@ -23,7 +23,7 @@ termination_by'
       | PSum.inr n => (n, 1))
     (Prod.lex sizeOfWFRel sizeOfWFRel)
 decreasing_by
-  simp [invImage, InvImage, Prod.lex, sizeOfWFRel, measure, Nat.lt_wfRel, WellFoundedRelation.rel]
+  simp_wf
   first
   | apply Prod.Lex.left
     apply Nat.lt_succ_self

a4c4a4da98de40ff730659a037a231c8f39d517e
tydeu <tydeu@hatpress.net>
2023-10-30 14:29:41 -0400

    fix: lake: consistent order for manifest packages

--- src/lake/tests/order/clean.sh
+++ src/lake/tests/order/clean.sh
@@ -2,3 +2,4 @@ rm -rf build foo/build bar/build
 rm -f lakefile.olean foo/lakefile.olean bar/lakefile.olean
 rm -f lake-manifest.json foo/lake-manifest.json bar/lake-manifest.json
 rm -rf lake-packages
+rm -f lake-manifest-1.json

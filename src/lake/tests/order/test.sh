dcea235b428eecf35f400906221e634912fea429
tydeu <tydeu@hatpress.net>
2023-11-21 14:07:44 -0500

    refactor: reverse pkg/lib search & no exe roots in import

--- src/lake/tests/order/test.sh
+++ src/lake/tests/order/test.sh
@@ -7,12 +7,16 @@ LAKE=${LAKE:-../../.lake/build/bin/lake}
 
 # Tests that the order in which libraries are declared and required
 # is properly preserved and effects which configuration is used for a module.
+# Later packages and libraries in the dependency tree shadow earlier ones.
+# https://github.com/leanprover/lean4/issues/2548
 
 $LAKE update
 $LAKE build +A -v | grep 222000
 $LAKE build +A.B -v | grep 333000
 $LAKE build +A.B.C -v | grep 333000
-$LAKE build +X -v | grep 111000
+$LAKE build +X -v | grep 555000
+$LAKE build +Y -v | grep 666000
+$LAKE exe Y | grep root
 
 # Tests that `lake update` does not reorder packages in the manifest
 # (if there have been no changes to the order in the configuration)

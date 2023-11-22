2f3a1156e7b7cd478118203390f276e7a83a93b6
Scott Morrison <scott.morrison@gmail.com>
2023-11-22 21:15:25 +1100

    fix: most-recently-nightly-tag does not assume a 'nightly' remote

--- script/most-recent-nightly-tag.sh
+++ script/most-recent-nightly-tag.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Name of the remote repository
-remote_name="nightly"
-
 # Prefix for tags to search for
 tag_prefix="nightly-"
 
 # Fetch all tags from the remote repository
-git fetch $remote_name --tags > /dev/null
+git fetch git@github.com:leanprover/lean4-nightly.git --tags > /dev/null
 
 # Get the most recent commit that has a matching tag
 tag_name=$(git tag --merged HEAD --list "${tag_prefix}*" | sort -rV | head -n 1 | sed "s/^$tag_prefix//")

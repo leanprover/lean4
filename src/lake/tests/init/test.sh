984d55c9623a9e24cb76eb39a24f12a0f7662708
Mac Malone <tydeu@hatpress.net>
2023-11-27 11:11:12 -0500

    fix: lake: proper exe targets & pkg generation (#2932)
    Improves executable handling in `lake exe` and `lake init`:
    
    * `lake exe <target>` now parses `target` like a build target (as the
    help text states it should) rather than as a basic name.
    * `lake new foo.bar [std]` now generates executables named `foo-bar`.
    * `lake new foo.bar exe` now properly creates `foo/bar.lean`.

--- src/lake/tests/init/test.sh
+++ src/lake/tests/init/test.sh
@@ -14,68 +14,67 @@ LAKE=${LAKE:-../../.lake/build/bin/lake}
 # Test package name validation (should error)
 # https://github.com/leanprover/lean4/issues/2637
 
-($LAKE new '  ' 2>&1 && false || true) | grep "illegal package name"
-($LAKE new .    2>&1 && false || true) | grep "illegal package name"
-($LAKE new ..   2>&1 && false || true) | grep "illegal package name"
-($LAKE new .... 2>&1 && false || true) | grep "illegal package name"
-($LAKE new a/bc 2>&1 && false || true) | grep "illegal package name"
-($LAKE new a\\b 2>&1 && false || true) | grep "illegal package name"
-($LAKE new init 2>&1 && false || true) | grep "reserved package name"
-($LAKE new Lean 2>&1 && false || true) | grep "reserved package name"
-($LAKE new Lake 2>&1 && false || true) | grep "reserved package name"
-($LAKE new main 2>&1 && false || true) | grep "reserved package name"
+($LAKE new  .    2>&1 && false || true) | grep "illegal package name"
+for cmd in new init; do
+($LAKE $cmd ..   2>&1 && false || true) | grep "illegal package name"
+($LAKE $cmd .... 2>&1 && false || true) | grep "illegal package name"
+($LAKE $cmd '  ' 2>&1 && false || true) | grep "illegal package name"
+($LAKE $cmd a/bc 2>&1 && false || true) | grep "illegal package name"
+($LAKE $cmd a\\b 2>&1 && false || true) | grep "illegal package name"
+($LAKE $cmd init 2>&1 && false || true) | grep "reserved package name"
+($LAKE $cmd Lean 2>&1 && false || true) | grep "reserved package name"
+($LAKE $cmd Lake 2>&1 && false || true) | grep "reserved package name"
+($LAKE $cmd main 2>&1 && false || true) | grep "reserved package name"
+done
 
 # Test `init .`
 
 mkdir hello
 pushd hello
 $LAKE1 init .
-$LAKE1 build
-.lake/build/bin/hello
+$LAKE1 exe hello
 popd
 
 # Test creating packages with uppercase names
 # https://github.com/leanprover/lean4/issues/2540
 
 $LAKE new HelloWorld
-$LAKE -d HelloWorld build
-HelloWorld/.lake/build/bin/helloworld
+$LAKE -d HelloWorld exe helloworld
 
 # Test creating multi-level packages with a `.`
 
 $LAKE new hello.world
-$LAKE -d hello-world build
-hello-world/.lake/build/bin/hello-world
+$LAKE -d hello-world exe hello-world
 test -f hello-world/Hello/World/Basic.lean
+
+$LAKE new hello.exe exe
+$LAKE -d hello-exe exe hello.exe
+test -f hello-exe/hello/exe.lean
 
 # Test creating packages with a `-` (i.e., a non-identifier package name)
 # https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/lake.20new.20lean-data
 
 $LAKE new lean-data
-$LAKE -d lean-data build
-lean-data/.lake/build/bin/lean-data
+$LAKE -d lean-data exe lean-data
 
 # Test creating packages starting with digits (i.e., a non-identifier library name)
 # https://github.com/leanprover/lean4/issues/2865
 
 $LAKE new 123-hello
-$LAKE -d 123-hello build
-123-hello/.lake/build/bin/123-hello
+$LAKE -d 123-hello exe 123-hello
 
 # Test creating packages with keyword names
 # https://github.com/leanprover/lake/issues/128
 
 $LAKE new meta
-$LAKE -d meta build
-meta/.lake/build/bin/meta
+$LAKE -d meta exe meta
 
 # Test `init` with name
 
 mkdir hello_world
 pushd hello_world
 $LAKE1 init hello_world exe
-$LAKE1 build
-./.lake/build/bin/hello_world
+$LAKE1 exe hello_world
 popd
 
 # Test bare `init` on existing package (should error)

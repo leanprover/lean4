79cf2549f22d10eac61709c61129b7c7efd36370
Joachim Breitner <mail@joachim-breitner.de>
2023-11-27 10:36:42 +0100

    Fix test

--- tests/lean/guessLexFailures.lean
+++ tests/lean/guessLexFailures.lean
@@ -29,7 +29,7 @@ decreasing_by decreasing_tactic
 
 
 -- Like above, but now with a `casesOn` alternative with insufficient lambdas
-def Fin_succ_comp (f : (n : Nat) → Fin (n + 1)) : (n m : Nat) → Fin (n + 2) := fun n => Fin.succ (f n)
+def Fin_succ_comp (f : (n : Nat) → Fin (n + 1)) : (n : Nat) → Fin (n + 2) := fun n => Fin.succ (f n)
 def badCasesOn2 (n m : Nat) : Fin (n + 1) :=
    Nat.casesOn (motive := fun n => Fin (n + 1)) n (⟨0,Nat.zero_lt_succ _⟩)
       (Fin_succ_comp (fun n => badCasesOn2 n (.succ m)))

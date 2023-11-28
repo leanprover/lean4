b21627264bcdb0d0b28f7f1a8f435294712b6a33
Scott Morrison <scott.morrison@gmail.com>
2023-11-28 11:41:12 +1100

    test

--- tests/lean/test_extern.lean
+++ tests/lean/test_extern.lean
@@ -7,10 +7,3 @@ test_extern Nat.add 12 37
 test_extern 4 + 5
 
 test_extern ByteArray.copySlice ⟨#[1,2,3]⟩ 1 ⟨#[4, 5, 6, 7, 8, 9, 10, 11, 12, 13]⟩ 0 6
-
-@[extern "lean_nat_add"]
-protected def Nat.not_add : (@& Nat) → (@& Nat) → Nat
-  | a, Nat.zero   => a
-  | a, Nat.succ b => Nat.succ (Nat.add a b)
-
-test_extern Nat.not_add 0 0

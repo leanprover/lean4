10b30bcba514ece1c873e78747d8efb02d96160c
Scott Morrison <scott.morrison@gmail.com>
2023-11-29 11:43:12 +1100

    normalize path separator, apology

--- tests/pkg/test_extern/test.sh
+++ tests/pkg/test_extern/test.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
+# We need a package test because we need multiple files with imports.
+# Currently the other package tests all succeed,
+# but here we need to check for a particular error message.
+# This is just an ad-hoc text mangling script to extract the error message
+# and account for some OS differences.
+# Ideally there would be a more principled testing framework
+# that took care of all this!
 
 rm -rf .lake/build
 
 # Function to process the output
 verify_output() {
-    awk '/error: stdout:/, /error: external command/' | sed '/error: external command/d'
-}
+    # Normalize path separators from backslashes to forward slashes
+    sed 's#\\#/#g' |
+    awk '/error: stdout:/, /error: external command/' |
+    sed '/error: external command/d'}
 
 lake build 2>&1 | verify_output > produced.txt
 

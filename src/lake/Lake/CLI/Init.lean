40bfab6fe2744aa6e49554fe5d6798cdb6aa12b8
Eric Wieser <wieser.eric@gmail.com>
2023-12-05 12:18:08 +0000

    Apply suggestions for error messages
    Co-authored-by: Mac Malone <tydeu@hatpress.net>

--- src/lake/Lake/CLI/Init.lean
+++ src/lake/Lake/CLI/Init.lean
@@ -218,7 +218,7 @@ def initPkg (dir : FilePath) (name : String) (tmp : InitTemplate) (env : Lake.En
 
 def validatePkgName (pkgName : String) : LogIO PUnit := do
   if pkgName.isEmpty || pkgName.all (· == '.') || pkgName.any (· ∈ ['/', '\\']) then
-    error ("illegal package name: " ++ pkgName)
+    error s!"illegal package name '{pkgName}'"
   if pkgName.toLower ∈ ["init", "lean", "lake", "main"] then
     error "reserved package name"
 
@@ -228,7 +228,7 @@ def init (pkgName : String) (tmp : InitTemplate) (env : Lake.Env) (cwd : FilePat
       let path ← IO.FS.realPath cwd
       match path.fileName with
       | some dirName => pure dirName
-      | none => error ("illegal package name: cwd is " ++ path.toString)
+      | none => error s!"illegal package name: could not derive one from '{path}'"
     else
       pure pkgName
   let pkgName := pkgName.trim

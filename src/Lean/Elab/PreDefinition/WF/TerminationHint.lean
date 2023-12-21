cbfd23dfaffb44559c96c97c05dd76ad81687b30
Joachim Breitner <mail@joachim-breitner.de>
2023-12-21 11:17:02 +0100

    Let’s try this

--- src/Lean/Elab/PreDefinition/WF/TerminationHint.lean
+++ src/Lean/Elab/PreDefinition/WF/TerminationHint.lean
@@ -94,6 +94,9 @@ def TerminationHints.checkVars (headerParams : Nat) (hints : TerminationHints) (
 open Parser.Termination
 
 def elabTerminationHints {m} [Monad m] [MonadError m] (stx : TSyntax ``suffix) : m TerminationHints := do
+  -- TODO: Better understand if this is needed
+  if let .missing := stx.raw then
+    return { TerminationHints.none with ref := stx }
   match stx with
   | `(suffix| $[$t?:terminationBy]? $[$d?:decreasingBy]? ) => do
     let termination_by? ← t?.mapM fun t => match t with

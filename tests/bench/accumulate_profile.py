c76277ceb30f9d72e992e009301576de44b1ca39
Sebastian Ullrich <sebasti@nullri.ch>
2023-12-22 10:45:46 +0100

    touch more

--- tests/bench/accumulate_profile.py
+++ tests/bench/accumulate_profile.py
@@ -5,11 +5,10 @@ import re
 import sys
 
 cats = collections.defaultdict(lambda: 0)
-for line in sys.stdin.readlines():
+for line in sys.stdin:
     if m := re.match("(.+?) ([\d.]+)(m?)s", line):
         cats[m[1].strip()] += float(m[2]) * (1e-3 if m[3] else 1)
-    else:
-        print(line, file=sys.stderr)
+    sys.stderr.write(line)
 
 for cat in sorted(cats.keys()):
     cat2 = cat

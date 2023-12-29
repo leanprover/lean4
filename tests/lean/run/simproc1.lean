a2aadee28f9cba171139dccfd10cc4b37efc989c
Leonardo de Moura <leomoura@amazon.com>
2023-12-29 11:40:28 -0800

    feat: simproc declaration vs simproc attribute
    Allow `simproc`s to be declared without setting the `[simproc]`
    attribute. A `simproc` declaration is function + pattern.
    
    Motivation: allow them to be provided as arguments to `simp` **and** `simp only`.
    
    TODO: track their use in `simp`.
    TODO: builtin simprocs

--- tests/lean/run/simproc1.lean
+++ tests/lean/run/simproc1.lean
@@ -4,14 +4,11 @@ import Lean.Meta.Offset
 def foo (x : Nat) : Nat :=
   x + 10
 
-open Lean Meta Simp
-@[simproc foo _]
-def simp_f : Simproc := fun e => do
-  let some n ← evalNat e.appArg! |>.run
-    | return none
+simproc reduce_f (foo _) := fun e => open Lean Meta in do
+  let some n ← evalNat e.appArg! |>.run | return none
   return some (.done { expr := mkNatLit (n+10) })
 
 example : x + foo 2 = 12 + x := by
-  fail_if_success simp (config := { simproc := false })
-  simp (config := { simproc := true })
+  fail_if_success simp (config := { «simproc» := false })
+  simp (config := { «simproc» := true })
   rw [Nat.add_comm]

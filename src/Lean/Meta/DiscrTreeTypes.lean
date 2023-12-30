7ed4d1c43270df528b45241f2a3d72ca80457c23
Leonardo de Moura <leomoura@amazon.com>
2023-12-30 09:10:52 -0800

    feat: add builtin `simproc` support

--- src/Lean/Meta/DiscrTreeTypes.lean
+++ src/Lean/Meta/DiscrTreeTypes.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 import Lean.Expr
+import Lean.ToExpr
 
 namespace Lean.Meta
 
@@ -33,6 +34,17 @@ protected def Key.hash : Key → UInt64
   | .proj s i a  =>  mixHash (hash a) $ mixHash (hash s) (hash i)
 
 instance : Hashable Key := ⟨Key.hash⟩
+
+instance : ToExpr Key where
+  toTypeExpr := mkConst ``Key
+  toExpr k   := match k with
+   | .const n a => mkApp2 (mkConst ``Key.const) (toExpr n) (toExpr a)
+   | .fvar id a => mkApp2 (mkConst ``Key.fvar) (toExpr id) (toExpr a)
+   | .lit l => mkApp (mkConst ``Key.lit) (toExpr l)
+   | .star => mkConst ``Key.star
+   | .other => mkConst ``Key.other
+   | .arrow => mkConst ``Key.arrow
+   | .proj n i a => mkApp3 (mkConst ``Key.proj) (toExpr n) (toExpr i) (toExpr a)
 
 /--
 Discrimination tree trie. See `DiscrTree`.

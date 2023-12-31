c2cbef108e0b964d8f1472da719792bc4bc7088b
Leonardo de Moura <leomoura@amazon.com>
2023-12-31 11:06:09 -0800

    feat: replace `ite` and `dite` shortcircuit theorems with simproc
    Motivation: better `simp` cache behavior. Recall that `simp` cache
    uses `dischargeDepth`.

--- tests/lean/run/simpIfPre.lean
+++ tests/lean/run/simpIfPre.lean
@@ -11,6 +11,7 @@ def myid (x : Nat) := 0 + x
 
 @[simp] theorem myid_eq : myid x = x := by simp [myid]
 
+namespace Ex1
 def f (x : Nat) (y z : Nat) : Nat :=
   if myid x = 0 then y else z
 
@@ -21,3 +22,43 @@ def g (x : Nat) : Nat :=
 
 theorem ex (h : a = 1) : g (a+32) = a := by
   simp [g, f, h]
+end Ex1
+
+namespace Ex2
+def f (x : Nat) (y z : Nat) : Nat :=
+  if myid x > 0 then z else y
+
+def g (x : Nat) : Nat :=
+  match x with
+  | 0 => 1
+  | a+1 => f x (g a + 1) (g a)
+
+theorem ex (h : a = 1) : g (a+32) = a := by
+  simp [g, f, h]
+end Ex2
+
+namespace Ex3
+def f (x : Nat) (y z : Nat) : Nat :=
+  if _ : myid x = 0 then y else z
+
+def g (x : Nat) : Nat :=
+  match x with
+  | 0 => 1
+  | a+1 => f x (g a + 1) (g a)
+
+theorem ex (h : a = 1) : g (a+32) = a := by
+  simp [g, f, h]
+end Ex3
+
+namespace Ex4
+def f (x : Nat) (y z : Nat) : Nat :=
+  if _ : myid x > 0 then z else y
+
+def g (x : Nat) : Nat :=
+  match x with
+  | 0 => 1
+  | a+1 => f x (g a + 1) (g a)
+
+theorem ex (h : a = 1) : g (a+32) = a := by
+  simp [g, f, h]
+end Ex4

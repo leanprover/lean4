4958404f3754944a28531a6dcc6982a85a1479e1
Leonardo de Moura <leomoura@amazon.com>
2024-01-02 14:27:19 -0800

    chore: add another `simproc` test

--- tests/lean/run/simproc2.lean
+++ tests/lean/run/simproc2.lean
@@ -4,3 +4,9 @@ example : foo x = 8 + 2 := by
   fail_if_success simp only
   simp only [Nat.reduceAdd]
   rw [foo]
+
+
+example : foo x = 8 + 2 := by
+  fail_if_success simp [-Nat.reduceAdd]
+  simp only [Nat.reduceAdd]
+  rw [foo]

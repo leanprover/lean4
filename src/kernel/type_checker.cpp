8470b0906620b5170a0d7ae0eba3b500c4f832fc
Joe Hendrix <joe@lean-fro.org>
2024-01-05 11:38:03 -0800

    chore: Helper function for global constants in kernel

--- src/kernel/type_checker.cpp
+++ src/kernel/type_checker.cpp
@@ -1135,48 +1135,39 @@ extern "C" LEAN_EXPORT lean_object * lean_kernel_whnf(lean_object * env, lean_ob
     });
 }
 
+inline static expr * new_persistent_expr_const(name const & n) {
+    expr * e = new expr(mk_const(n));
+    mark_persistent(e->raw());
+    return e;
+}
+
 void initialize_type_checker() {
-    g_dont_care    = new expr(mk_const("dontcare"));
-    mark_persistent(g_dont_care->raw());
     g_kernel_fresh = new name("_kernel_fresh");
     mark_persistent(g_kernel_fresh->raw());
     g_bool_true    = new name{"Bool", "true"};
     mark_persistent(g_bool_true->raw());
-    g_nat_zero     = new expr(mk_constant(name{"Nat", "zero"}));
-    mark_persistent(g_nat_zero->raw());
-    g_nat_succ     = new expr(mk_constant(name{"Nat", "succ"}));
-    mark_persistent(g_nat_succ->raw());
-    g_nat_add      = new expr(mk_constant(name{"Nat", "add"}));
-    mark_persistent(g_nat_add->raw());
-    g_nat_sub      = new expr(mk_constant(name{"Nat", "sub"}));
-    mark_persistent(g_nat_sub->raw());
-    g_nat_mul      = new expr(mk_constant(name{"Nat", "mul"}));
-    mark_persistent(g_nat_mul->raw());
-    g_nat_pow      = new expr(mk_constant(name{"Nat", "pow"}));
-    mark_persistent(g_nat_pow->raw());
-    g_nat_gcd      = new expr(mk_constant(name{"Nat", "gcd"}));
-    mark_persistent(g_nat_gcd->raw());
-    g_nat_div      = new expr(mk_constant(name{"Nat", "div"}));
-    mark_persistent(g_nat_div->raw());
-    g_nat_mod      = new expr(mk_constant(name{"Nat", "mod"}));
-    mark_persistent(g_nat_mod->raw());
-    g_nat_beq      = new expr(mk_constant(name{"Nat", "beq"}));
-    mark_persistent(g_nat_beq->raw());
-    g_nat_ble      = new expr(mk_constant(name{"Nat", "ble"}));
-    mark_persistent(g_nat_ble->raw());
-    g_string_mk    = new expr(mk_constant(name{"String", "mk"}));
-    mark_persistent(g_string_mk->raw());
-    g_lean_reduce_bool = new expr(mk_constant(name{"Lean", "reduceBool"}));
-    mark_persistent(g_lean_reduce_bool->raw());
-    g_lean_reduce_nat  = new expr(mk_constant(name{"Lean", "reduceNat"}));
-    mark_persistent(g_lean_reduce_nat->raw());
+    g_dont_care    = new_persistent_expr_const("dontcare");
+    g_nat_zero     = new_persistent_expr_const({"Nat", "zero"});
+    g_nat_succ     = new_persistent_expr_const({"Nat", "succ"});
+    g_nat_add      = new_persistent_expr_const({"Nat", "add"});
+    g_nat_sub      = new_persistent_expr_const({"Nat", "sub"});
+    g_nat_mul      = new_persistent_expr_const({"Nat", "mul"});
+    g_nat_pow      = new_persistent_expr_const({"Nat", "pow"});
+    g_nat_gcd      = new_persistent_expr_const({"Nat", "gcd"});
+    g_nat_div      = new_persistent_expr_const({"Nat", "div"});
+    g_nat_mod      = new_persistent_expr_const({"Nat", "mod"});
+    g_nat_beq      = new_persistent_expr_const({"Nat", "beq"});
+    g_nat_ble      = new_persistent_expr_const({"Nat", "ble"});
+    g_string_mk    = new_persistent_expr_const({"String", "mk"});
+    g_lean_reduce_bool = new_persistent_expr_const({"Lean", "reduceBool"});
+    g_lean_reduce_nat  = new_persistent_expr_const({"Lean", "reduceNat"});
     register_name_generator_prefix(*g_kernel_fresh);
 }
 
 void finalize_type_checker() {
-    delete g_dont_care;
     delete g_kernel_fresh;
     delete g_bool_true;
+    delete g_dont_care;
     delete g_nat_succ;
     delete g_nat_zero;
     delete g_nat_add;

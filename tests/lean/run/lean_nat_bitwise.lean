c78806aa8d804ce22a63ee78d30dd4cd0b86c38a
Joe Hendrix <joe@lean-fro.org>
2024-01-09 01:01:23 -0800

    chore: fix mistakes in test cases

--- tests/lean/run/lean_nat_bitwise.lean
+++ tests/lean/run/lean_nat_bitwise.lean
@@ -24,14 +24,14 @@ example : 0b1111 ^^^ 0      = 0b1111 := rfl
 example : 0b1111 ^^^ 0b1111 = 0      := rfl
 example : 0b1100 ^^^ 0b1010 = 0b0110 := rfl
 example : 0b0110 ^^^ 0b0101 = 0b0011 := rfl
-example : let x := 2^20; x ||| x = x := rfl
+example : let x := 2^20; x ^^^ x = 0 := rfl
 
 example : 0      >>> 0      = 0 := rfl
 example : 0b1011 >>> 1      = 0b0101 := rfl
 example : 0b1011 >>> 2      = 0b0010 := rfl
-example : 0      >>> 2^20 = 0 := rfl
+example : 0      >>> 2^20   = 0 := rfl
 
 example : 0      <<< 0      = 0 := rfl
 example : 0b1011 <<< 1      = 0b010110 := rfl
 example : 0b1011 <<< 2      = 0b101100 := rfl
-example : 0 <<< 2^20 = 0 := rfl
+example : 0      <<< 2^20   = 0 := rfl

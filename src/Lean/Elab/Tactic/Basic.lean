b8a5a20110a200dccad420bc4e145b6a78c1ec63
mhuisi <mhuisi@protonmail.com>
2024-01-10 15:14:57 +0100

    feat: partial context info

--- src/Lean/Elab/Tactic/Basic.lean
+++ src/Lean/Elab/Tactic/Basic.lean
@@ -51,6 +51,9 @@ instance : Monad TacticM :=
   let i := inferInstanceAs (Monad TacticM);
   { pure := i.pure, bind := i.bind }
 
+instance : MonadParentDecl TacticM where
+  getParentDeclName? := Term.getDeclName?
+
 def getGoals : TacticM (List MVarId) :=
   return (← get).goals
 

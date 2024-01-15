1247eab27d331170165fa7611e6b7143a8dd4d64
Joachim Breitner <mail@joachim-breitner.de>
2024-01-15 17:25:24 +0100

    Cheaper checks

--- tests/lean/motiveNotTypeCorect.lean
+++ tests/lean/motiveNotTypeCorect.lean
@@ -9,11 +9,9 @@ def mwe (t : Nat) (f : Nat -> Nat) (h : f t = t)
 
 def K (x : α) (y : β) : β := y
 example (h : true = false) (A : (b : Bool) → K b Type) (h2 : A false) : A true := by
-  -- the motive is dependent on `true`, but in a non-essential way, so this works fine
-  rw [h]
+  rw [h] -- tactic 'rewrite' failed, motive is dependent
   exact h2
 
-example (h : true = false) (A : (b : Bool) → if b then Prop else Nat) : A true :=
-  by
-    rw [h] -- tactic 'rewrite' failed, motive is dependent
-    exact 0
+example (h : true = false) (A : (b : Bool) → if b then Prop else Nat) : A true := by
+  rw [h] -- tactic 'rewrite' failed, motive is dependent
+  exact 0

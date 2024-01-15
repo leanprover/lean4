aff27c6307d5b54af90da76dcc79a4e1777b1172
Joachim Breitner <mail@joachim-breitner.de>
2024-01-15 15:00:04 +0100

    Add test case

--- tests/lean/motiveNotTypeCorect.lean
+++ tests/lean/motiveNotTypeCorect.lean
@@ -6,3 +6,9 @@ def mwe (t : Nat) (f : Nat -> Nat) (h : f t = t)
   := by
   rw [h] -- tactic 'rewrite' failed, motive is not type correct
   sorry
+
+def K (x : α) (y : β) : β := y
+example (h : true = false) (A : (b : Bool) → K b Type) (h2 : A false) : A true := by
+  -- the motive is dependent on `true`, but in a non-essential way, so this works fine
+  rw [h]
+  exact h2

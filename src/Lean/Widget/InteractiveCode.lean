8886a6e90cd860d4ec02c424fb4dd8965781c76a
Kyle Miller <kmill31415@gmail.com>
2024-02-01 10:59:33 -0800

    feat: have `pp.proofs` use `⋯`
    Also adds `pp.proofs.threshold` option to allow users to pretty print shallow proof terms. By default, only atomic proof terms are pretty printed.

--- src/Lean/Widget/InteractiveCode.lean
+++ src/Lean/Widget/InteractiveCode.lean
@@ -77,12 +77,13 @@ def ppExprTagged (e : Expr) (explicit : Bool := false) : MetaM CodeWithInfos := 
   if pp.raw.get (← getOptions) then
     return .text (toString e)
   let delab := open PrettyPrinter.Delaborator in
-    if explicit then
-      withOptionAtCurrPos pp.tagAppFns.name true do
-      withOptionAtCurrPos pp.explicit.name true do
-        delabApp
-    else
-      delab
+    withOptionAtCurrPos pp.proofs.name true do
+      if explicit then
+        withOptionAtCurrPos pp.tagAppFns.name true do
+        withOptionAtCurrPos pp.explicit.name true do
+          delabApp
+      else
+          delab
   let ⟨fmt, infos⟩ ← PrettyPrinter.ppExprWithInfos e (delab := delab)
   let tt := TaggedText.prettyTagged fmt
   let ctx := {

1446f31f0e44612d5592dacc1b8108bd869aef42
Jon Eugster <eugster.jon@gmail.com>
2024-02-02 13:08:27 +0100

    so these are dummy commits

--- src/Lean/Data/Position.lean
+++ src/Lean/Data/Position.lean
@@ -1,4 +1,4 @@
-/- 
+/-
 Copyright (c) 2018 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Sebastian Ullrich

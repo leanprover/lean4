f68429d3a7da4cf5093f0d65513c74a92d53c26c
Scott Morrison <scott.morrison@gmail.com>
2024-02-09 10:35:17 +1100

    chore: move syntax to Init/Notation, make builtin_term_elab

--- src/Lean/Util/TermUnsafe.lean
+++ src/Lean/Util/TermUnsafe.lean
@@ -28,35 +28,28 @@ def mkAuxName (hint : Name) : TermElabM Name :=
     let name := (← getDeclName?).getD Name.anonymous ++ hint
     pure $ addMacroScope (← getMainModule) name (← getCurrMacroScope)
 
-/--
-`unsafe t : α` is an expression constructor which allows using unsafe declarations inside the
-body of `t : α`, by creating an auxiliary definition containing `t` and using `implementedBy` to
-wrap it in a safe interface. It is required that `α` is nonempty for this to be sound,
-but even beyond that, an `unsafe` block should be carefully inspected for memory safety because
-the compiler is unable to guarantee the safety of the operation.
-
-For example, the `evalExpr` function is unsafe, because the compiler cannot guarantee that when
-you call ```evalExpr Foo ``Foo e``` that the type `Foo` corresponds to the name `Foo`, but in a
-particular use case, we can ensure this, so `unsafe (evalExpr Foo ``Foo e)` is a correct usage.
--/
-elab "unsafe " t:term : term <= expectedType => do
-  let mut t ← elabTerm t expectedType
-  t ← instantiateMVars t
-  if t.hasExprMVar then
-    synthesizeSyntheticMVarsNoPostponing
+@[builtin_term_elab «unsafe», inherit_doc Lean.Parser.Term.unsafe]
+def elabUnsafe : TermElab := fun stx expectedType? =>
+  match stx with
+  | `(unsafe $t) => do
+    let mut t ← elabTerm t expectedType?
     t ← instantiateMVars t
-  if ← logUnassignedUsingErrorInfos (← getMVars t) then throwAbortTerm
-  t ← mkAuxDefinitionFor (← mkAuxName `unsafe) t
-  let Expr.const unsafeFn unsafeLvls .. := t.getAppFn | unreachable!
-  let ConstantInfo.defnInfo unsafeDefn ← getConstInfo unsafeFn | unreachable!
-  let implName ← mkAuxName `impl
-  addDecl <| Declaration.defnDecl {
-    name := implName
-    type := unsafeDefn.type
-    levelParams := unsafeDefn.levelParams
-    value := ← mkOfNonempty unsafeDefn.type
-    hints := ReducibilityHints.opaque
-    safety := DefinitionSafety.safe
-  }
-  setImplementedBy implName unsafeFn
-  pure $ mkAppN (mkConst implName unsafeLvls) t.getAppArgs
+    if t.hasExprMVar then
+      synthesizeSyntheticMVarsNoPostponing
+      t ← instantiateMVars t
+    if ← logUnassignedUsingErrorInfos (← getMVars t) then throwAbortTerm
+    t ← mkAuxDefinitionFor (← mkAuxName `unsafe) t
+    let Expr.const unsafeFn unsafeLvls .. := t.getAppFn | unreachable!
+    let ConstantInfo.defnInfo unsafeDefn ← getConstInfo unsafeFn | unreachable!
+    let implName ← mkAuxName `impl
+    addDecl <| Declaration.defnDecl {
+      name := implName
+      type := unsafeDefn.type
+      levelParams := unsafeDefn.levelParams
+      value := ← mkOfNonempty unsafeDefn.type
+      hints := ReducibilityHints.opaque
+      safety := DefinitionSafety.safe
+    }
+    setImplementedBy implName unsafeFn
+    pure $ mkAppN (mkConst implName unsafeLvls) t.getAppArgs
+  | _ => throwUnsupportedSyntax

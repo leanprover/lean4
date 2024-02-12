0f54bac0008bcbb950e65b09a28246b78b208600
Scott Morrison <scott.morrison@gmail.com>
2024-02-13 00:27:30 +1100

    suggestions from review

--- src/Lean/Elab/Tactic/Repeat.lean
+++ src/Lean/Elab/Tactic/Repeat.lean
@@ -13,9 +13,10 @@ open Lean Elab Tactic
 `iterate n tac` runs `tac` exactly `n` times.
 `iterate tac` runs `tac` repeatedly until failure.
 
-To run multiple tactics, one can do `iterate (tac₁; tac₂; ⋯)` or
+`iterate`'s argument is a tactic sequence,
+so multiple tactics can be run using `iterate n (tac₁; tac₂; ⋯)` or
 ```lean
-iterate
+iterate n
   tac₁
   tac₂
   ⋯
@@ -41,28 +42,28 @@ Returns a boolean indicating whether `f` succeeded at least once, and
 all the remaining goals (i.e. those on which `f` failed).
 -/
 def repeat'Core [Monad m] [MonadExcept ε m] [MonadBacktrack s m] [MonadMCtx m]
-    (f : MVarId → m (List MVarId)) (gs : List MVarId) (maxIters := 100000) :
+    (f : MVarId → m (List MVarId)) (goals : List MVarId) (maxIters := 100000) :
     m (Bool × List MVarId) := do
-  let (progress, acc) ← go maxIters false gs [] #[]
+  let (progress, acc) ← go maxIters false goals [] #[]
   pure (progress, (← acc.filterM fun g => not <$> g.isAssigned).toList)
 where
-  /-- Auxiliary for `repeat'Core`. `repeat'Core.go f maxIters progress gs stk acc` evaluates to
-  essentially `acc.toList ++ repeat' f (gs::stk).join maxIters`: that is, `acc` are goals we will
-  not revisit, and `(gs::stk).join` is the accumulated todo list of subgoals. -/
+  /-- Auxiliary for `repeat'Core`. `repeat'Core.go f maxIters progress goals stk acc` evaluates to
+  essentially `acc.toList ++ repeat' f (goals::stk).join maxIters`: that is, `acc` are goals we will
+  not revisit, and `(goals::stk).join` is the accumulated todo list of subgoals. -/
   go : Nat → Bool → List MVarId → List (List MVarId) → Array MVarId → m (Bool × Array MVarId)
   | _, p, [], [], acc => pure (p, acc)
-  | n, p, [], gs::stk, acc => go n p gs stk acc
-  | n, p, g::gs, stk, acc => do
+  | n, p, [], goals::stk, acc => go n p goals stk acc
+  | n, p, g::goals, stk, acc => do
     if ← g.isAssigned then
-      go n p gs stk acc
+      go n p goals stk acc
     else
       match n with
-      | 0 => pure <| (p, acc.push g ++ gs |> stk.foldl .appendList)
+      | 0 => pure <| (p, acc.push g ++ goals |> stk.foldl .appendList)
       | n+1 =>
         match ← observing? (f g) with
-        | some gs' => go n true gs' (gs::stk) acc
-        | none => go n p gs stk (acc.push g)
-termination_by n p gs stk _ => (n, stk, gs)
+        | some goals' => go n true goals' (goals::stk) acc
+        | none => go n p goals stk (acc.push g)
+termination_by n p goals stk _ => (n, stk, goals)
 
 /--
 `repeat' f` runs `f` on all of the goals to produce a new list of goals,
@@ -71,8 +72,8 @@ or until `maxIters` total calls to `f` have occurred.
 Always succeeds (returning the original goals if `f` fails on all of them).
 -/
 def repeat' [Monad m] [MonadExcept ε m] [MonadBacktrack s m] [MonadMCtx m]
-    (f : MVarId → m (List MVarId)) (gs : List MVarId) (maxIters := 100000) : m (List MVarId) :=
-  repeat'Core f gs maxIters <&> (·.2)
+    (f : MVarId → m (List MVarId)) (goals : List MVarId) (maxIters := 100000) : m (List MVarId) :=
+  repeat'Core f goals maxIters <&> (·.2)
 
 /--
 `repeat1' f` runs `f` on all of the goals to produce a new list of goals,
@@ -81,9 +82,9 @@ or until `maxIters` total calls to `f` have occurred.
 Fails if `f` does not succeed at least once.
 -/
 def repeat1' [Monad m] [MonadError m] [MonadExcept ε m] [MonadBacktrack s m] [MonadMCtx m]
-    (f : MVarId → m (List MVarId)) (gs : List MVarId) (maxIters := 100000) : m (List MVarId) := do
-  let (.true, gs) ← repeat'Core f gs maxIters | throwError "repeat1' made no progress"
-  pure gs
+    (f : MVarId → m (List MVarId)) (goals : List MVarId) (maxIters := 100000) : m (List MVarId) := do
+  let (.true, goals) ← repeat'Core f goals maxIters | throwError "repeat1' made no progress"
+  pure goals
 
 /--
 `repeat' tac` runs `tac` on all of the goals to produce a new list of goals,
@@ -98,9 +99,6 @@ the tactic is applied recursively to the generated subgoals until it eventually 
 -/
 elab "repeat1' " tac:tacticSeq : tactic => do
   setGoals (← repeat1' (evalTacticAtRaw tac) (← getGoals))
-
-/-- `subst_eqs` applies `subst` to all equalities in the context as long as it makes progress. -/
-elab "subst_eqs" : tactic => Elab.Tactic.liftMetaTactic1 (·.substEqs)
 
 /-- `split_ands` applies `And.intro` until it does not make progress. -/
 syntax "split_ands" : tactic

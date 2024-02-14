23d5e6d019bc20344499a48a529b691951ab6e11
Scott Morrison <scott.morrison@gmail.com>
2024-02-14 12:46:09 +1100

    fix tests/lean/run/1549.lean

--- tests/lean/run/1549.lean
+++ tests/lean/run/1549.lean
@@ -1,6 +1,6 @@
 theorem not_mem_nil (a : Nat) : ¬ a ∈ [] := fun x => nomatch x
 
-theorem forall_prop_of_false {p : Prop} {q : p → Prop} (hn : ¬ p) :
+theorem forall_prop_of_false' {p : Prop} {q : p → Prop} (hn : ¬ p) :
   (∀ h' : p, q h') ↔ True := sorry
 
 example (R : Nat → Prop) : (∀ (a' : Nat), a' ∈ [] → R a') := by
@@ -8,9 +8,9 @@ example (R : Nat → Prop) : (∀ (a' : Nat), a' ∈ [] → R a') := by
   exact fun _ => True.intro
 
 
-def Not.elim {α : Sort _} (H1 : ¬a) (H2 : a) : α := absurd H2 H1
-theorem iff_of_true (ha : a) (hb : b) : a ↔ b := ⟨fun _ => hb, fun _ => ha⟩
-theorem iff_true_intro (h : a) : a ↔ True := iff_of_true h ⟨⟩
+def Not.elim' {α : Sort _} (H1 : ¬a) (H2 : a) : α := absurd H2 H1
+theorem iff_of_true' (ha : a) (hb : b) : a ↔ b := ⟨fun _ => hb, fun _ => ha⟩
+theorem iff_true_intro' (h : a) : a ↔ True := iff_of_true h ⟨⟩
 
 example {P : Prop} : ∀ (x : Nat) (_ : x ∈ []), P :=
 by

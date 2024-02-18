97b0d896d19bcf497a5f06507ed57faff544333f
Joe Hendrix <joe@lean-fro.org>
2024-02-18 00:03:57 -0800

    chore: improve bigint tests

--- tests/lean/int_div_mod.lean
+++ tests/lean/int_div_mod.lean
@@ -48,12 +48,12 @@
 #guard (-7 : Int) % -4 == 1
 
 -- Basic big integer tests
-#guard let n : Int := 2^128;  let d : Int :=  3;    d * (n / d) + n % d = n
-#guard let n : Int := 2^128;  let d : Int :=  2^64; d * (n / d) + n % d = n
-#guard let n : Int := -2^128; let d : Int :=  2^64; d * (n / d) + n % d = n
-#guard let n : Int := 2^128;  let d : Int := -2^64; d * (n / d) + n % d = n
-#guard let n : Int := -2^128; let d : Int := -2^64; d * (n / d) + n % d = n
-#guard let n : Int := 2^128+7;  let d : Int :=  2^64; d * (n / d) + n % d = n
-#guard let n : Int := -2^128+3; let d : Int :=  2^64; d * (n / d) + n % d = n
-#guard let n : Int := 2^128+2;  let d : Int := -2^64; d * (n / d) + n % d = n
-#guard let n : Int := -2^128+2; let d : Int := -2^64; d * (n / d) + n % d = n
+#guard let n : Int := 2^128;  let d : Int :=  3;    d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d
+#guard let n : Int := 2^128;  let d : Int :=  2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d
+#guard let n : Int := -2^128; let d : Int :=  2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d
+#guard let n : Int := 2^128;  let d : Int := -2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d.natAbs
+#guard let n : Int := -2^128; let d : Int := -2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d.natAbs
+#guard let n : Int := 2^128+7;  let d : Int :=  2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d
+#guard let n : Int := -2^128+3; let d : Int :=  2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d
+#guard let n : Int := 2^128+2;  let d : Int := -2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d.natAbs
+#guard let n : Int := -2^128+2; let d : Int := -2^64; d * (n / d) + n % d = n ∧ n % d ≥ 0 ∧ n % d < d.natAbs

9c39c10e26650e6d8884637bc5b92df13cbfaa85
Leonardo de Moura <leomoura@amazon.com>
2024-02-18 12:31:26 -0800

    chore: update stage0

--- stage0/stdlib/Lean/Elab/PreDefinition/WF/Eqns.c
+++ stage0/stdlib/Lean/Elab/PreDefinition/WF/Eqns.c
@@ -3515,7 +3515,7 @@ lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 12, x_3);
 lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 13, x_4);
 lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 14, x_3);
 lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 15, x_3);
-lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 16, x_4);
+lean_ctor_set_uint8(x_6, sizeof(void*)*2 + 16, x_3);
 return x_6;
 }
 }

540bf725748a8b65ed2f3df36e083dd08dbab8a0
Scott Morrison <scott.morrison@gmail.com>
2024-02-19 11:27:11 +1100

    fix imports

--- src/Init/Data/Bool.lean
+++ src/Init/Data/Bool.lean
@@ -3,6 +3,8 @@ Copyright (c) 2023 F. G. Dorais. No rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: F. G. Dorais
 -/
+prelude
+import Init.BinderPredicates
 
 /-- Boolean exclusive or -/
 abbrev xor : Bool → Bool → Bool := bne

a7be3bd8b390cea29aa95b18563c269427552723
Scott Morrison <scott.morrison@gmail.com>
2024-02-20 12:07:22 +1100

    chore: add missing copyright headers

--- src/Init/Data/BitVec.lean
+++ src/Init/Data/BitVec.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
 prelude
 import Init.Data.BitVec.Basic
 import Init.Data.BitVec.Bitblast

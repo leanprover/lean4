a7b1bc5353f5223a9e1ce2ecb5be506662ec584c
David Thrane Christiansen <david@davidchristiansen.dk>
2024-02-20 15:21:08 +0100

    chore: improve test documentation formalia

--- tests/lean/parserRecovery.lean
+++ tests/lean/parserRecovery.lean
@@ -1,16 +1,18 @@
 import Lean
 
+/-!
+# Parser error recovery
+
+This test is to make sure that the error recovery feature is working as intended. In particular, it
+checks that certain errors in the head of a declaration (e.g. an invalid name) don't prevent further
+parse errors from being reported.
+-/
+
 open Lean Parser
 
-def nonws : Parser where
-  fn :=
-    andthenFn
-      (andthenFn (satisfyFn (fun c => !c.isWhitespace && c != '#')) (takeWhileFn (!·.isWhitespace)))
-      whitespace
-
-def thing : Parser :=
-  recover ident nonws
-
+/-!
+## Error recovery for Lean commands
+-/
 def "foo" (x : Nat) :  := 5
 
 def 5 : Nat := 3
@@ -50,8 +52,13 @@ inductive Item.{4, 5, 6, 7, a, "foo", b, c} : Nat where
 
 example := ""
 
--- Adding recovery to Lean's term grammar doesn't work well with the primitive tools at hand
--- (extensibility + token tables + backtracking are a real challenge), but for DSLs it can be nice.
+/-!
+## Error recovery for custom syntax
+
+Adding recovery to Lean's term grammar doesn't work well with the primitive tools at hand
+(extensibility + token tables + backtracking are a real challenge), but for DSLs it can be useful.
+This tests that error recovery works for a term syntax extension.
+-/
 
 def altParen : Parser :=
   "{|" >> termParser (prec := 51) >> recover "|}" skip
@@ -65,6 +72,23 @@ macro:50 e:altParen : term => pure ⟨e.raw[1]⟩
 #eval ([ (2 + 3] * ( 3 + ) )
 
 example := ""
+
+/-!
+## Error recovery for custom commands
+
+This command can recover from many parse errors. Make sure that there's no arbitrary small limit by
+testing a few recoveries.
+-/
+
+def nonws : Parser where
+  fn :=
+    andthenFn
+      (andthenFn (satisfyFn (fun c => !c.isWhitespace && c != '#')) (takeWhileFn (!·.isWhitespace)))
+      whitespace
+
+def thing : Parser :=
+  recover ident nonws
+
 
 open Lean Elab Command in
 elab "#go " xs:thing* (Lean.Parser.eoi <|> "#stop") : command => do
@@ -83,6 +107,8 @@ hey
 (     99
   (
 #stop
+
+-- Even though the command had errors, this is still running as expected:
 
 def x := 5
 #eval x

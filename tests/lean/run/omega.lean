989ee537a32c5d526a22cba6c868afbc50ff0dba
Scott Morrison <scott.morrison@gmail.com>
2024-02-21 12:31:51 +1100

    more tests

--- tests/lean/run/omega.lean
+++ tests/lean/run/omega.lean
@@ -382,10 +382,11 @@ example (i : Fin 7) : (i : Nat) < 8 := by omega
 example (x y z i : Nat) (hz : z ≤ 1) : x % 2 ^ i + y % 2 ^ i + z < 2 * 2^ i := by omega
 
 /-! ### BitVec -/
--- Currently these tests require calling `simp` with many lemmas.
+-- Currently these tests require calling `simp` with many lemmas,
+-- and sometimes adding `toNat_lt` as a hypothesis.
 -- We need a good `BitVec` to `Nat` preprocessor.
 
-open Std
+open Std BitVec
 
 example (x y : BitVec 8) (hx : x < 16) (hy : y < 16) : x + y < 31 := by
   simp [BitVec.lt_def] at *
@@ -397,4 +398,9 @@ example (x y z : BitVec 8) (hx : x >>> 1 < 16) (hy : y < 16) (hz : z = x + 2 * y
 
 example (x : BitVec 8) (hx : (x + 1) <<< 1 = 3) : False := by
   simp [BitVec.toNat_eq, Nat.shiftLeft_eq] at *
+  omega
+
+example (x : BitVec 8) (hx : (x + 1) <<< 1 = 4) : x = 1 ∨ x = 129 := by
+  have := toNat_lt x
+  simp [BitVec.toNat_eq, Nat.shiftLeft_eq, BitVec.lt_def] at *
   omega

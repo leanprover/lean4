db92b51bdccfd217cdc4739054df9a6d4fdb23b7
Joe Hendrix <joe@lean-fro.org>
2024-02-21 16:29:37 -0800

    fix: copyright inconsistencies

--- src/Init/Data/Nat/Bitwise/Lemmas.lean
+++ src/Init/Data/Nat/Bitwise/Lemmas.lean
@@ -1,6 +1,5 @@
 /-
-Copyright (c) 2023 by the authors listed in the file AUTHORS and their
-institutional affiliations. All rights reserved.
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joe Hendrix
 -/

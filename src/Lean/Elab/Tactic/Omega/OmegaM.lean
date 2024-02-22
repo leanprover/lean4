2b1a0371c6911b76a5f3b60a7969b3226e776954
Scott Morrison <scott.morrison@gmail.com>
2024-02-22 11:02:37 +1100

    feat: add `bv_omega` tactic

--- src/Lean/Elab/Tactic/Omega/OmegaM.lean
+++ src/Lean/Elab/Tactic/Omega/OmegaM.lean
@@ -169,6 +169,8 @@ def analyzeAtom (e : Expr) : OmegaM (HashSet Expr) := do
         r := r.insert (mkApp (.const ``Int.neg_le_natAbs []) x)
       | _, (``Fin.val, #[n, i]) =>
         r := r.insert (mkApp2 (.const ``Fin.isLt []) n i)
+      | _, (`Std.BitVec.toNat, #[n, x]) =>
+        r := r.insert (mkApp2 (.const `Std.BitVec.toNat_lt []) n x)
       | _, _ => pure ()
     return r
   | (``HDiv.hDiv, #[_, _, _, _, x, k]) => match natCast? k with

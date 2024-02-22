61c22c88d7714d989e47727dbd6b9ec55d2c4a6b
Joe Hendrix <joe@lean-fro.org>
2024-02-21 17:56:48 -0800

    chore: address copyright inconsistencies (#3448)

--- src/Init/Data/BitVec/Basic.lean
+++ src/Init/Data/BitVec/Basic.lean
@@ -1,6 +1,5 @@
 /-
-Copyright (c) 2022 by the authors listed in the file AUTHORS and their
-institutional affiliations. All rights reserved.
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joe Hendrix, Wojciech Nawrocki, Leonardo de Moura, Mario Carneiro, Alex Keizer
 -/

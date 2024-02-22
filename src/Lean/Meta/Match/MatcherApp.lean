b27ab5e25d994cfa3dc761907b5b3e56ca413ef2
Joachim Breitner <mail@joachim-breitner.de>
2024-02-22 17:16:26 +0100

    refactor: module MatcherApp.Transform (#3439)
    PR #3432 will introduce more operations on `MatcherApp`, including somet
    that have more dependencies.
    
    This change prepares by introducing `Lean.Meta.Match.MatcherApp.Basic`
    for the basic definition, and `Lean.Meta.MatcherApp.Transform` for the
    transformations, currently `addArg` and `refineThrough`, but more to
    come.

---
+++ src/Lean/Meta/Match/MatcherApp.lean
@@ -0,0 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura, Joachim Breitner
+-/
+prelude
+import Lean.Meta.Match.MatcherApp.Basic
+import Lean.Meta.Match.MatcherApp.Transform

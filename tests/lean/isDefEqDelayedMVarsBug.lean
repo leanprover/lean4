31346685ae172a0b4038da7a39f91db2269c62c0
thorimur <68410468+thorimur@users.noreply.github.com>
2024-02-23 17:20:02 -0500

    test: more test

--- tests/lean/isDefEqDelayedMVarsBug.lean
+++ tests/lean/isDefEqDelayedMVarsBug.lean
@@ -4,6 +4,7 @@ open Lean Meta Elab Tactic in
 elab "test " stx:term : tactic => withMainContext do
   let e ← elabTerm stx none
   logInfo m!"{← withAssignableSyntheticOpaque <| isDefEq e (← getMainTarget)}"
+  check e
 
 inductive F {α} : α → Prop where | formal : F a
 
@@ -17,4 +18,10 @@ theorem xh : F (fun (x : Nat) (h : F x) => 2) := by
 
 theorem xhi : F (fun (x : Nat) (h : F x) [Decidable (F x)] => 2) := by
   test F (fun x h i => ?e)
+  exact .formal
+
+axiom f {α} (a : α) [Decidable (F a)] : Type
+
+example : F (fun (x : Nat) {h : F x} [Decidable (F x)] (_ : f x) => 2) := by
+  test F (fun x h i _ => ?e)
   exact .formal

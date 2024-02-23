6dbb8550ada6a6ba0a7f83f9be843a88d4e92ebb
thorimur <68410468+thorimur@users.noreply.github.com>
2024-02-23 17:11:59 -0500

    test: more, change `F` to inductive

--- tests/lean/isDefEqDelayedMVarsBug.lean
+++ tests/lean/isDefEqDelayedMVarsBug.lean
@@ -5,12 +5,16 @@ elab "test " stx:term : tactic => withMainContext do
   let e ← elabTerm stx none
   logInfo m!"{← withAssignableSyntheticOpaque <| isDefEq e (← getMainTarget)}"
 
-axiom F {α} : α → Prop
+inductive F {α} : α → Prop where | formal : F a
 
 theorem x : F (fun _ : Nat => 2) := by
   test F (fun _ : Nat => ?e)
-  admit
+  exact .formal
 
 theorem xh : F (fun (x : Nat) (h : F x) => 2) := by
   test F (fun x h => ?e)
-  admit
+  exact .formal
+
+theorem xhi : F (fun (x : Nat) (h : F x) [Decidable (F x)] => 2) := by
+  test F (fun x h i => ?e)
+  exact .formal

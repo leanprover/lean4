59191fa3a312927930d0bcbf3f0cc00166f652a2
thorimur <68410468+thorimur@users.noreply.github.com>
2024-02-23 16:47:13 -0500

    test: logInfo

--- tests/lean/isDefEqDelayedMVarsBug.lean
+++ tests/lean/isDefEqDelayedMVarsBug.lean
@@ -3,7 +3,7 @@ import Lean
 open Lean Meta Elab Tactic in
 elab "test " stx:term : tactic => withMainContext do
   let e ← elabTerm stx none
-  discard <| withAssignableSyntheticOpaque <| isDefEq e (← getMainTarget)
+  logInfo m!"{← withAssignableSyntheticOpaque <| isDefEq e (← getMainTarget)}"
 
 axiom F {α} : α → Prop
 

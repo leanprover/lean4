8bf9d398af37f6defae406ef5a62a8cdcccdb653
Sebastian Ullrich <sebasti@nullri.ch>
2024-02-23 09:06:55 +0100

    chore: CI: flag `Lean` modules not using `prelude` (#3463)
    Co-authored-by: Henrik Böving <hargonix@gmail.com>

--- src/Lean/Elab/Tactic/Omega/Core.lean
+++ src/Lean/Elab/Tactic/Omega/Core.lean
@@ -3,6 +3,8 @@ Copyright (c) 2023 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
+prelude
+import Init.Omega.Constraint
 import Lean.Elab.Tactic.Omega.OmegaM
 import Lean.Elab.Tactic.Omega.MinNatAbs
 

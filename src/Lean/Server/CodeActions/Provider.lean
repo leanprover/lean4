212855c20b415d87780726282deb740f9591d05c
Leonardo de Moura <leomoura@amazon.com>
2024-02-25 10:05:39 -0800

    chore: `code_action_provider` => `builtin_code_action_provider`

--- src/Lean/Server/CodeActions/Provider.lean
+++ src/Lean/Server/CodeActions/Provider.lean
@@ -25,7 +25,7 @@ open Lean Elab Term Server RequestM
 A code action which calls all `@[hole_code_action]` code actions on each hole
 (`?_`, `_`, or `sorry`).
 -/
-@[code_action_provider] def holeCodeActionProvider : CodeActionProvider := fun params snap => do
+@[builtin_code_action_provider] def holeCodeActionProvider : CodeActionProvider := fun params snap => do
   let doc ← readDoc
   let startPos := doc.meta.text.lspPosToUtf8Pos params.range.start
   let endPos := doc.meta.text.lspPosToUtf8Pos params.range.end
@@ -164,7 +164,7 @@ partial def findInfoTree? (kind : SyntaxNodeKind) (tgtRange : String.Range)
 /--
 A code action which calls all `@[command_code_action]` code actions on each command.
 -/
-@[code_action_provider] def cmdCodeActionProvider : CodeActionProvider := fun params snap => do
+@[builtin_code_action_provider] def cmdCodeActionProvider : CodeActionProvider := fun params snap => do
   let doc ← readDoc
   let startPos := doc.meta.text.lspPosToUtf8Pos params.range.start
   let endPos := doc.meta.text.lspPosToUtf8Pos params.range.end

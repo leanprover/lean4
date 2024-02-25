41fcf900bec2fd3e87cb24787f2a4dba16f55dc5
Leonardo de Moura <leomoura@amazon.com>
2024-02-25 11:03:55 -0800

    chore: missing `prelude`

--- src/Lean/Server/CodeActions.lean
+++ src/Lean/Server/CodeActions.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Authors: E.W.Ayers
 -/
+prelude
 import Lean.Server.CodeActions.Attr
 import Lean.Server.CodeActions.Basic
 import Lean.Server.CodeActions.Provider
--- src/Lean/Server/CodeActions/Provider.lean
+++ src/Lean/Server/CodeActions/Provider.lean
@@ -3,6 +3,7 @@ Copyright (c) 2023 Mario Carneiro. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
+prelude
 import Lean.Elab.BuiltinTerm
 import Lean.Elab.BuiltinNotation
 import Lean.Server.InfoUtils

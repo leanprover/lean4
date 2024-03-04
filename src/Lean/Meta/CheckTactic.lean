41c602bcc6b285bc9d0169e00ec5749fbf5c3f98
Joe Hendrix <joe@lean-fro.org>
2024-03-04 14:05:45 -0800

    chore: Add copyright header

--- src/Lean/Meta/CheckTactic.lean
+++ src/Lean/Meta/CheckTactic.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joe Hendrix
+-/
 prelude
 import Lean.Meta.Basic
 

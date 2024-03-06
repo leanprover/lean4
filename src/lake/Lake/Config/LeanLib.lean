ae7d24c0cc140df2dae80e007c7932abe57b5cc1
Sebastian Ullrich <sebasti@nullri.ch>
2024-03-06 14:00:57 +0100

    fix Lake, temporarily

--- src/lake/Lake/Config/LeanLib.lean
+++ src/lake/Lake/Config/LeanLib.lean
@@ -145,7 +145,9 @@ That is, the build type's `leancArgs`, the package's `moreLeancArgs`,
 and then the library's `moreLeancArgs`.
 -/
 @[inline] def leancArgs (self : LeanLib) : Array String :=
-  self.buildType.leancArgs ++ self.pkg.moreLeancArgs ++ self.config.moreLeancArgs
+  self.buildType.leancArgs ++ self.pkg.moreLeancArgs ++ self.config.moreLeancArgs ++
+    -- TEMP
+    #["-DLEAN_EXPORTING"]
 
 /--
 The arguments to weakly pass to `leanc` when compiling the library's Lean-produced C files.

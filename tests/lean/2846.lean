98bb2cc9eb2ebf7fda480f6694afdec8ca03482f
Kyle Miller <kmill31415@gmail.com>
2024-03-06 12:59:37 -0800

    fix tests

--- tests/lean/2846.lean
+++ tests/lean/2846.lean
@@ -39,3 +39,26 @@ This is checking that shadowing is represented correctly (that's not the respons
 def foo' (n n : Nat) (a : Fin ((by clear n; exact n) + 1)) : Fin (n + 1) := 0
 
 #check foo'
+
+/-!
+Named argument after inaccesible name, still stays after the colon.
+But, it does not print using named pi notation since this is not a dependent type.
+-/
+def foo'' : String → (needle : String) → String := fun _ yo => yo
+
+#check foo''
+
+/-!
+Named argument after inaccessible name, still stays after the colon.
+Here, because it's a dependent type the named pi notation shows the name.
+-/
+def bar : String → (n : Nat) → Fin (n+1) := fun _ n => ⟨0, Nat.zero_lt_succ n⟩
+
+#check bar
+
+/-!
+Instance argument is an inaccessible name, and we assume that it is a nameless instance.
+-/
+def bar' [LT α] (x : α) : x < x := sorry
+
+#check bar'

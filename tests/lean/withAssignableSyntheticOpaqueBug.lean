808ee68e8116007aed68d219f6e35091250a2b09
thorimur <68410468+thorimur@users.noreply.github.com>
2024-03-09 17:43:50 -0500

    test: fix `withAssignableSyntheticOpaque` to account for new behavior

--- tests/lean/withAssignableSyntheticOpaqueBug.lean
+++ tests/lean/withAssignableSyntheticOpaqueBug.lean
@@ -10,15 +10,14 @@ def tst : MetaM Unit := do
   lambdaTelescope e fun _ b => do
     let m' := b.getAppFn
     assert! m'.isMVar
-    let some { mvarIdPending .. } ← getDelayedMVarAssignment? m'.mvarId!
-      | IO.println f!"m' is not delayed-assigned"
-    IO.println (mvarIdPending == m.mvarId!)
-    IO.println (← mvarIdPending.isDelayedAssigned)
-    IO.println (← getExprMVarAssignment? m'.mvarId!)
     assert! (← withAssignableSyntheticOpaque <| isDefEq b (mkNatLit 0))
     IO.println (← getExprMVarAssignment? m'.mvarId!)
+    let some m ← getExprMVarAssignment? m.mvarId!
+      | IO.println f!"{m} is not assigned"
+    -- Currently, `m` gets assigned to an intermediate mvar to clear local hypotheses.
+    -- See `expandDelayedAssigned?` in Lean.Meta.ExprDefEq.
+    assert! m.isMVar
     IO.println (← getExprMVarAssignment? m.mvarId!)
-    IO.println (← ppExpr (← instantiateMVars m'))
     IO.println (← ppExpr (← instantiateMVars b))
     IO.println (← ppExpr m)
     return ()

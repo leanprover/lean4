5a53a3ada83f3211acd7768c48eba42c85277d1b
Joachim Breitner <mail@joachim-breitner.de>
2024-03-13 23:41:47 +0100

    More tests to show off

--- tests/lean/guessLexDiff.lean
+++ tests/lean/guessLexDiff.lean
@@ -24,6 +24,56 @@ def henrik1 (xs : Array Nat) (i : Nat) : Bool :=
   else
     true
 
+def merge (xs ys : Array Nat) : Array Nat :=
+  let rec loop (i j : Nat) (acc : Array Nat) : Array Nat :=
+    if _ : i < xs.size then
+      if _ : j < ys.size then
+        if xs[i] < ys[j] then
+          loop (i+1) j (acc.push xs[i])
+        else
+          loop i (j+1) (acc.push ys[j])
+      else
+        loop (i+1) j (acc.push xs[i])
+    else
+      if _ : j < ys.size then
+        loop i (j+1) (acc.push ys[j])
+      else
+        acc
+  loop 0 0 #[]
+
+def distinct (xs : Array Nat) : Bool :=
+  let rec loop (i j : Nat) : Bool :=
+    if _ : i < xs.size then
+      if _ : j < i then
+        if xs[j] = xs[i] then
+          false
+        else
+          loop i (j+1)
+      else
+        loop (i+1) 0
+    else
+      true
+  loop 0 0
+
+-- This examples shows a limitation of our current `decreasing_tactic`.
+-- Guesslex infers
+-- termination_by (Array.size xs - i, i)
+-- but because `decreasing_with` is using
+--     repeat (first | apply Prod.Lex.right | apply Prod.Lex.left)
+-- it cannot solve this goal. But if we leave the Prod.Lex-handling to omega, it works
+def weird (xs : Array Nat) (i : Nat) : Bool :=
+  if _ : i < xs.size then
+    if _ : 0 < i then
+      if xs[i] = 42 then
+        weird xs.pop (i - 1)
+      else
+        weird xs (i+1)
+    else
+      weird xs (i+1)
+  else
+    true
+decreasing_by all_goals simp_wf; omega
+
 /--
 This checks
 * the presentation of complex measures in the table

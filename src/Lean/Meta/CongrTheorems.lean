d1d0a0c3bd93ee38cc8b484bbb060c892f99065a
Scott Morrison <scott.morrison@gmail.com>
2024-03-14 14:26:29 +1100

    prelude

--- src/Lean/Meta/CongrTheorems.lean
+++ src/Lean/Meta/CongrTheorems.lean
@@ -3,5 +3,6 @@ Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
+prelude
 import Lean.Meta.CongrTheorems.Basic
 import Lean.Meta.CongrTheorems.Rich

3b34e1e10298a9d8490e1d98ea5a3d5af8bf0842
Sebastian Ullrich <sebasti@nullri.ch>
2024-03-27 16:20:47 +0100

    consolidate tests

--- tests/lean/interactive/incrementalCombinator.lean
+++ tests/lean/interactive/incrementalCombinator.lean
@@ -5,9 +5,20 @@ def case (h : a ∨ b ∨ c) : True := by
   case inr h =>
     cases h
     case inl =>
-      dbg_trace "0"
-      dbg_trace "1"
-      dbg_trace "2"
-                --^ collectDiagnostics
+      dbg_trace "c 0"
+      dbg_trace "c 1"
+      dbg_trace "c 2"
+                  --^ sync
+                  --^ insert: ".5"
+                  --^ sync
+
+-- RESET
+def case (h : a ∨ b) : True := by
+  cases h
+  . dbg_trace "d 0"
+    dbg_trace "d 1"
+    dbg_trace "d 2"
+                --^ sync
                 --^ insert: ".5"
-                --^ collectDiagnostics
+                --^ sync
+  dbg_trace "d 3"

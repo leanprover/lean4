2d670757145d147b5feb5ae87ebd5b48e1766a03
Scott Morrison <scott.morrison@gmail.com>
2024-03-27 13:58:44 +1100

    feat: change apply_rfl tactic so that it does not operate on =

--- src/Lean/Elab/Tactic/Rfl.lean
+++ src/Lean/Elab/Tactic/Rfl.lean
@@ -10,22 +10,17 @@ import Lean.Elab.Tactic.Basic
 /-!
 # `rfl` tactic extension for reflexive relations
 
-This extends the `rfl` tactic so that it works on any reflexive relation,
+This extends the `rfl` tactic so that it works on reflexive relations other than `=`,
 provided the reflexivity lemma has been marked as `@[refl]`.
 -/
 
 namespace Lean.Elab.Tactic.Rfl
 
-/--
-This tactic applies to a goal whose target has the form `x ~ x`, where `~` is a reflexive
-relation, that is, a relation which has a reflexive lemma tagged with the attribute [refl].
--/
-elab_rules : tactic
-  | `(tactic| rfl) => withMainContext do liftMetaFinishingTactic (·.applyRfl)
-
 @[builtin_tactic Lean.Parser.Tactic.applyRfl] def evalApplyRfl : Tactic := fun stx =>
   match stx with
-  | `(tactic| apply_rfl) => withMainContext do liftMetaFinishingTactic (·.applyRfl)
+  -- We call `applyRfl` with `failOnEq := true`,
+  -- so that this does not do duplicative work when called on an `=` goal from `rfl`.
+  | `(tactic| apply_rfl) => withMainContext do liftMetaFinishingTactic (·.applyRfl true)
   | _ => throwUnsupportedSyntax
 
 end Lean.Elab.Tactic.Rfl

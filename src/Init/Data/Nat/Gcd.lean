9ef8a89e9c8f6da5b595227b3ac4dcdedf6e0c38
Scott Morrison <scott.morrison@gmail.com>
2024-04-10 21:42:03 +1000

    add example with edge cases

--- src/Init/Data/Nat/Gcd.lean
+++ src/Init/Data/Nat/Gcd.lean
@@ -18,6 +18,15 @@ is overridden in both the kernel and the compiler to efficiently
 evaluate using the "bignum" representation (see `Nat`).
 The definition provided here is the logical model
 (and it is soundness-critical that they coincide).
+
+The GCD of two natural numbers is the largest natural number
+that divides both arguments.
+In particular, the GCD of a number and `0` is the number itself.
+```
+example : Nat.gcd 10 15 = 5 := rfl
+example : Nat.gcd 0 5 = 5 := rfl
+example : Nat.gcd 7 0 = 7 := rfl
+```
 -/
 @[extern "lean_nat_gcd"]
 def gcd (m n : @& Nat) : Nat :=

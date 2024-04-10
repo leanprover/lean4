eb22fa774fd311876855f249dd35061e7cc525b5
Joachim Breitner <mail@joachim-breitner.de>
2024-04-10 18:42:09 +0200

    Update src/Init/Control/Basic.lean
    Co-authored-by: Mario Carneiro <di.gama@gmail.com>

--- src/Init/Control/Basic.lean
+++ src/Init/Control/Basic.lean
@@ -22,7 +22,7 @@ export Functor (discard)
 
 /--
 An `Alternative` functor is an `Applicative` functor that has a notion of “emptyness” or “failure”
-and a binary operation `<|>` with a notion of “collecting valures” or “left-most success”.
+and a binary operation `<|>` with a notion of “collecting values” or “left-most success”.
 
 Important instances include
 * `Option`, where `failure := .none` and `<|>` returns the left-most `.some`.

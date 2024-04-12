df42249f771f4e7aa3f1547b9da825914cd0288c
Kyle Miller <kmill31415@gmail.com>
2024-04-12 04:59:20 +0200

    feat: add `#guard_msgs` configuration syntax and update documentation
    Implementation for this configuration syntax will appear after a stage0 update.

--- src/Lean/Elab/GuardMsgs.lean
+++ src/Lean/Elab/GuardMsgs.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
 prelude
+import Lean.Elab.Notation
 import Lean.Server.CodeActions.Attr
 
 /-! `#guard_msgs` command for testing commands

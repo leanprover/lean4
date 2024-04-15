230ae8b1f5607a85a72fe5bdd662aefa7b105d2e
Joachim Breitner <mail@joachim-breitner.de>
2024-04-15 15:10:17 +0200

    Update src/Lean/MetavarContext.lean

--- src/Lean/MetavarContext.lean
+++ src/Lean/MetavarContext.lean
@@ -629,9 +629,6 @@ Example:
 run_meta do
   let mvar1 ← mkFreshExprMVar (mkConst `Nat)
   let e := (mkConst `Nat.succ).app mvar1
-  logInfo e -- Nat.succ ?m.773
-  mvar1.mvarId!.assign (mkNatLit 42)
-  let e' ← instantiateMVars e
   -- e is `Nat.succ ?m.773`, `?m.773` is unassigned
   mvar1.mvarId!.assign (mkNatLit 42)
   -- e is `Nat.succ ?m.773`, `?m.773` is assigned to `42`

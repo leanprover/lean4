09b6b9d5b141f50f1467a28941cf80a3806f053a
Joachim Breitner <mail@joachim-breitner.de>
2024-04-15 15:44:30 +0200

    test refinements

--- tests/compiler/io_as_thunk.lean
+++ tests/compiler/io_as_thunk.lean
@@ -1,6 +1,6 @@
 def p (s: String) : BaseIO String := do
   match ← (IO.println s).toBaseIO with
-  | .ok _ => pure s
+  | .ok _ => pure s!"done with {s}"
   | .error e => pure e.toString
 
 
@@ -9,6 +9,6 @@ def main : IO PUnit := do
   let t1 ← (p "bbb").asThunk
   let t2 ← (p "ccc").asThunk
   let _t3 ← (p "ddd").asThunk
-  IO.print t2.get
-  -- IO.print t1.get
-  -- IO.print t2.get
+  IO.println t2.get
+  IO.println t1.get
+  IO.println t2.get

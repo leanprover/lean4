b3d2936d0944c7efb5f7b7b227b9959218d4817c
Sebastian Ullrich <sebasti@nullri.ch>
2024-04-18 14:58:11 +0200

    evalSepTactics docs

--- src/Lean/Elab/Tactic/BuiltinTactic.lean
+++ src/Lean/Elab/Tactic/BuiltinTactic.lean
@@ -35,62 +35,79 @@ children.
  -/
 partial def evalSepTactics : Tactic := goEven
 where
+  -- `stx[0]` is the next tactic step, if any
   goEven stx := do
     if stx.getNumArgs == 0 then
       return
     let tac := stx[0]
+    /-
+    Each `goEven` step creates three promises under incrementality and reuses their older versions
+    where possible:
+    * `finished` is resolved when `tac` finishes execution; if `tac` is wholly unchanged from the
+      previous version, its state is reused and `tac` execution is skipped.
+    * `inner` is passed to `tac` if it is marked as supporting incrementality and can be used for
+      reporting and partial reuse inside of it; if the tactic is unsupported or `finished` is wholly
+      reused, it is ignored.
+    * `next` is used as the context when invoking `goOdd` and thus eventually used for the next
+      `goEven` step. Thus, the incremental state of a tactic script is ultimately represented as a
+      chain of `next` snapshots. Its reuse is disabled if `tac` or its following separator are
+      changed in any way.
+    -/
     let mut oldInner? := none
     if let some snap := (← readThe Term.Context).tacSnap? then
       if let some old := snap.old? then
-        let oldEvaluated := old.val.get
-        oldInner? := oldEvaluated.next.get? 0 |>.map (⟨oldEvaluated.data.stx, ·⟩)
+        let oldParsed := old.val.get
+        oldInner? := oldParsed.next.get? 0 |>.map (⟨oldParsed.data.stx, ·⟩)
+    -- compare `stx[0]` for `finished`/`next` reuse, focus on remainder of script
     Term.withNarrowedTacticReuse (stx := stx) (fun stx => (stx[0], mkNullNode stx.getArgs[1:])) fun stxs => do
-      if let some snap := (← readThe Term.Context).tacSnap? then
-        let mut reused := false
-        let mut oldNext? := none
-        if let some old := snap.old? then
-          let oldEvaluated := old.val.get
-          if let some state := oldEvaluated.data.finished.get.state? then
-            state.restoreFull
-            reused := true
-            oldNext? := oldEvaluated.next.get? 1 |>.map (⟨old.stx, ·⟩)
+      let some snap := (← readThe Term.Context).tacSnap?
+        | do evalTactic tac; goOdd stxs
+      let mut reused := false
+      let mut oldNext? := none
+      if let some old := snap.old? then
+        -- `tac` must be unchanged given the narrow above; let's reuse `finished`'s state!
+        let oldParsed := old.val.get
+        if let some state := oldParsed.data.finished.get.state? then
+          state.restoreFull
+          reused := true
+          -- only allow `next` reuse in this case
+          oldNext? := oldParsed.next.get? 1 |>.map (⟨old.stx, ·⟩)
 
-        withAlwaysResolvedPromise fun next => do
-          withAlwaysResolvedPromise fun finished => do
-            withAlwaysResolvedPromise fun inner => do
-              snap.new.resolve <| .mk {
-                stx := tac
-                diagnostics := .empty
-                finished := finished.result
-              } #[
-                {
-                  range? := tac.getRange?
-                  task := inner.result },
-                {
-                  range? := stxs |>.getRange?
-                  task := next.result }]
-              unless reused do
-                withTheReader Term.Context ({ · with
-                    tacSnap? := if (← builtinIncrementalTactics.get).contains tac.getKind then
-                      some {
-                        old? := oldInner?
-                        new := inner
-                      } else none }) do
-                  evalTactic tac
-              finished.resolve { state? := (← saveState) }
+      withAlwaysResolvedPromise fun next => do
+        withAlwaysResolvedPromise fun finished => do
+          withAlwaysResolvedPromise fun inner => do
+            snap.new.resolve <| .mk {
+              stx := tac
+              diagnostics := .empty
+              finished := finished.result
+            } #[
+              {
+                range? := tac.getRange?
+                task := inner.result },
+              {
+                range? := stxs |>.getRange?
+                task := next.result }]
+            unless reused do
+              withTheReader Term.Context ({ · with
+                  tacSnap? := if (← builtinIncrementalTactics.get).contains tac.getKind then
+                    some {
+                      old? := oldInner?
+                      new := inner
+                    } else none }) do
+                evalTactic tac
+            finished.resolve { state? := (← saveState) }
 
-          withTheReader Term.Context ({ · with tacSnap? := some {
-            new := next
-            old? := oldNext?
-          } }) do
-            goOdd stxs
-      else
-        evalTactic tac
-        goOdd stxs
+        withTheReader Term.Context ({ · with tacSnap? := some {
+          new := next
+          old? := oldNext?
+        } }) do
+          goOdd stxs
+  -- `stx[0]` is the next separator, if any
   goOdd stx := do
     if stx.getNumArgs == 0 then
       return
     saveTacticInfoForToken stx[0] -- add `TacticInfo` node for `;`
+    -- disable further reuse on separator change as to not reuse wrong `TacticInfo`
     Term.withNarrowedTacticReuse (fun stx => (stx[0], mkNullNode stx.getArgs[1:])) goEven stx
 
 @[builtin_tactic seq1] def evalSeq1 : Tactic := fun stx =>

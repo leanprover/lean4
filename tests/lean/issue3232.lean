bf61b5cb1bed931c5aab20f26c9679dab2038a2b
Joachim Breitner <mail@joachim-breitner.de>
2024-04-18 22:08:55 +0200

    Update tests/lean/issue3232.lean
    Co-authored-by: David Thrane Christiansen <david@davidchristiansen.dk>

--- tests/lean/issue3232.lean
+++ tests/lean/issue3232.lean
@@ -1,6 +1,6 @@
 inductive foo {n : Nat} : Prop
 
--- Check that the error message will print the types with implicit arguments shownx
+-- Check that the error message will print the types with implicit arguments shown
 example (h : @foo 42) : @foo 23 := by
   apply h
 

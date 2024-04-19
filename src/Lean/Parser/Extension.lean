3c36020d13606a0da1df34891f778a595b79cdb9
Mario Carneiro <di.gama@gmail.com>
2024-04-19 08:21:10 -0400

    feat: @[builtin_doc] attribute (part 1) (#3953)
    First part of #3918.

--- src/Lean/Parser/Extension.lean
+++ src/Lean/Parser/Extension.lean
@@ -5,9 +5,8 @@ Authors: Leonardo de Moura, Sebastian Ullrich
 -/
 prelude
 import Lean.Parser.Basic
-import Lean.Compiler.InitAttr
 import Lean.ScopedEnvExtension
-import Lean.DocString
+import Lean.BuiltinDocAttr
 
 /-! Extensible parsing via attributes -/
 
@@ -488,10 +487,7 @@ private def BuiltinParserAttribute.add (attrName : Name) (catName : Name)
   | Expr.const `Lean.Parser.Parser _ =>
     declareLeadingBuiltinParser catName declName prio
   | _ => throwError "unexpected parser type at '{declName}' (`Parser` or `TrailingParser` expected)"
-  if let some doc ← findDocString? (← getEnv) declName (includeBuiltin := false) then
-    declareBuiltin (declName ++ `docString) (mkAppN (mkConst ``addBuiltinDocString) #[toExpr declName, toExpr doc])
-  if let some declRanges ← findDeclarationRanges? declName then
-    declareBuiltin (declName ++ `declRange) (mkAppN (mkConst ``addBuiltinDeclarationRanges) #[toExpr declName, toExpr declRanges])
+  declareBuiltinDocStringAndRanges declName
   runParserAttributeHooks catName declName (builtin := true)
 
 /--

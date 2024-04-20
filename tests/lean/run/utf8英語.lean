62cdb51ed5b9d8487877d5a4adbcd4659d81fc6a
Mario Carneiro <di.gama@gmail.com>
2024-04-20 14:36:37 -0400

    feat: UTF-8 string validation (#3958)
    Previously, there was a function `opaque fromUTF8Unchecked : ByteArray
    -> String` which would convert a list of bytes into a string, but as the
    name implies it does not validate that the string is UTF-8 before doing
    so and as a result it produces unsound results in the compiler (because
    the lean model of `String` indirectly asserts UTF-8 validity). This PR
    replaces that function by
    ```lean
    opaque validateUTF8 (a : @& ByteArray) : Bool
    
    opaque fromUTF8 (a : @& ByteArray) (h : validateUTF8 a) : String
    ```
    so that while the function is still "unchecked", we have a proof witness
    that the string is valid. To recover the original, actually unchecked
    version, use `lcProof` or other unsafe methods to produce the proof
    witness.
    
    Because this was the only `ByteArray -> String` conversion function, it
    was used in several places in an unsound way (e.g. reading untrusted
    input from IO and treating it as UTF-8). These have been replaced by
    `fromUTF8?` or `fromUTF8!` as appropriate.

--- tests/lean/run/utf8英語.lean
+++ tests/lean/run/utf8英語.lean
@@ -4,10 +4,13 @@ def check_eq {α} [BEq α] [Repr α] (tag : String) (expected actual : α) : IO 
       s!"assertion failure \"{tag}\":\n  expected: {repr expected}\n  actual:   {repr actual}"
 
 def DecodeUTF8: IO Unit := do
-  let cs := String.toList "Hello, 英語!"
+  let str := "Hello, 英語!"
+  let cs := String.toList str
   let ns := cs.map Char.toNat
   IO.println cs
   IO.println ns
   check_eq "utf-8 chars" [72, 101, 108, 108, 111, 44, 32, 33521, 35486, 33] ns
+  check_eq "utf-8 bytes" #[72, 101, 108, 108, 111, 44, 32, 232, 139, 177, 232, 170, 158, 33] str.toUTF8.data
+  check_eq "string eq" (some str) (String.fromUTF8? str.toUTF8)
 
 #eval DecodeUTF8

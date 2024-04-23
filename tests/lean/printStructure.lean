87ff38c1e06f2e8bf507f90c92eeb51ef771a120
F. G. Dorais <fgdorais@gmail.com>
2024-04-23 08:47:47 -0400

    chore: update tests

--- tests/lean/printStructure.lean
+++ tests/lean/printStructure.lean
@@ -1,23 +1,47 @@
-structure Prod.{u, v} : Type u → Type v → Type (max u v)
+/-!
+  Test #print command for structures and classes
+-/
+
+/- Structure -/
+/--
+info: structure Prod.{u, v} : Type u → Type v → Type (max u v)
 number of parameters: 2
 constructor:
 Prod.mk : {α : Type u} → {β : Type v} → α → β → α × β
 fields:
 fst : α
 snd : β
-class Inhabited.{u} : Sort u → Sort (max 1 u)
+-/
+#guard_msgs in
+#print Prod
+
+/- Class -/
+/--
+info: class Inhabited.{u} : Sort u → Sort (max 1 u)
 number of parameters: 1
 constructor:
 Inhabited.mk : {α : Sort u} → α → Inhabited α
 fields:
 default : α
-structure Thunk.{u} : Type u → Type u
+-/
+#guard_msgs in
+#print Inhabited
+
+/- Structure with private field -/
+/--
+info: structure Thunk.{u} : Type u → Type u
 number of parameters: 1
 constructor:
 Thunk.mk : {α : Type u} → (Unit → α) → Thunk α
 fields:
 private fn : Unit → α
-class Alternative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
+-/
+#guard_msgs in
+#print Thunk
+
+/- Extended class -/
+/--
+info: class Alternative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
 number of parameters: 1
 constructor:
 Alternative.mk : {f : Type u → Type v} →
@@ -26,7 +50,13 @@ fields:
 toApplicative : Applicative f
 failure : {α : Type u} → f α
 orElse : {α : Type u} → f α → (Unit → f α) → f α
-class Applicative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
+-/
+#guard_msgs in
+#print Alternative
+
+/- Multiply extended class -/
+/--
+info: class Applicative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
 number of parameters: 1
 constructor:
 Applicative.mk : {f : Type u → Type v} →
@@ -38,13 +68,36 @@ toPure : Pure f
 toSeq : Seq f
 toSeqLeft : SeqLeft f
 toSeqRight : SeqRight f
-structure Weird.{u_1, u_2} : Type u_1 → Type u_2 → Type u_1
+-/
+#guard_msgs in
+#print Applicative
+
+/- Structure with unused parameter -/
+
+structure Weird (α β : Type _) where
+  a : α
+
+/--
+info: structure Weird.{u_1, u_2} : Type u_1 → Type u_2 → Type u_1
 number of parameters: 2
 constructor:
 Weird.mk : {α : Type u_1} → {β : Type u_2} → α → Weird α β
 fields:
 a : α
-inductive Fake.{u_1} : Type u_1 → Type u_1
+-/
+#guard_msgs in
+#print Weird
+
+/- Structure-like inductive -/
+
+inductive Fake (α : Type _) where
+  | mk : (x : α) → Fake α
+
+/--
+info: inductive Fake.{u_1} : Type u_1 → Type u_1
 number of parameters: 1
 constructors:
 Fake.mk : {α : Type u_1} → α → Fake α
+-/
+#guard_msgs in
+#print Fake

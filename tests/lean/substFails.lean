3dad892cff6972e9c2e0d9e141f9ea95a0f3e605
Joachim Breitner <mail@joachim-breitner.de>
2024-04-26 14:18:10 +0200

    fix: subst notation (heq ▸ h) should fail if it does't subst
    The subst notation substitues in the expected type, if present, or in
    the type of the argument, if no expected type is known.
    
    If there is an expected type it already fails if it cannot find the
    equations' left hand side or right hand side. But if the expected type
    is not known and the equation's lhs is not present in the second
    argument's type, it will happily do a no-op-substitution.
    
    This is inconsistent and unlikely what the user intended to do, so we
    now print an error message now.
    
    This still only looks for the lhs; search for the rhs as well seems
    prudent, but I’ll leave that for a separate PR, to better diagnose the
    impact on mathlib.

---
+++ tests/lean/substFails.lean
@@ -0,0 +1,16 @@
+def foo := 3
+def bar := 4
+
+def ex1  (heq : foo = bar) (P : Nat → Prop) (h : P foo)         := heq ▸ h
+def ex2  (heq : foo = bar) (P : Nat → Prop) (h : P foo) : P 4   := heq ▸ h -- error
+def ex3  (heq : foo = bar) (P : Nat → Prop) (h : P foo) : P bar := heq ▸ h
+def ex4  (heq : foo = bar) (P : Nat → Prop) (h : P 3)           := heq ▸ h -- error
+def ex5  (heq : foo = bar) (P : Nat → Prop) (h : P 3)   : P 4   := heq ▸ h -- error
+def ex6  (heq : foo = bar) (P : Nat → Prop) (h : P 3)   : P bar := heq ▸ h
+
+def ex7  (heq : bar = foo) (P : Nat → Prop) (h : P foo)         := heq ▸ h -- error
+def ex8  (heq : bar = foo) (P : Nat → Prop) (h : P foo) : P 4   := heq ▸ h -- error
+def ex9  (heq : bar = foo) (P : Nat → Prop) (h : P foo) : P bar := heq ▸ h
+def ex10 (heq : bar = foo) (P : Nat → Prop) (h : P 3)           := heq ▸ h -- error
+def ex11 (heq : bar = foo) (P : Nat → Prop) (h : P 3)   : P 4   := heq ▸ h -- error
+def ex12 (heq : bar = foo) (P : Nat → Prop) (h : P 3)   : P bar := heq ▸ h

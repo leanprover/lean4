72ca410b9643e344fcb2ba99ffaf08b4898bae29
Tobias Grosser <tobias@grosser.es>
2024-05-06 09:22:19 +0100

    feat: add BitVec.[toInt_eq|toInt_ne]

--- src/Init/Data/BitVec/Lemmas.lean
+++ src/Init/Data/BitVec/Lemmas.lean
@@ -245,6 +245,12 @@ theorem eq_of_toInt_eq {i j : BitVec n} : i.toInt = j.toInt → i = j := by
   have _jlt := j.isLt
   split <;> split <;> omega
 
+theorem toInt_eq (x y : BitVec n) : x = y ↔ x.toInt = y.toInt :=
+  Iff.intro (congrArg BitVec.toInt) eq_of_toInt_eq
+
+theorem toInt_ne (x y : BitVec n) : x ≠ y ↔ x.toInt ≠ y.toInt := by
+  rw [Ne, toInt_eq]
+
 @[simp] theorem toNat_ofInt {n : Nat} (i : Int) :
   (BitVec.ofInt n i).toNat = (i % (2^n : Nat)).toNat := by
   unfold BitVec.ofInt

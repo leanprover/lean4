a113b63740c190a75baad2969adff288fcbf926a
Mac Malone <tydeu@hatpress.net>
2024-05-10 17:45:07 -0400

    chore: simplify test code

--- tests/lean/run/taskState.lean
+++ tests/lean/run/taskState.lean
@@ -1,34 +1,25 @@
-import Lean.Elab.ElabRules
-
-open Lean
-
-elab "file_pos%" : term => return toExpr <| (← getFileMap).toPosition (← getRefPos)
-
-def assertBEq
-  [BEq α] [ToString α] (actual expected : α)
-  (filePos : Position := by exact file_pos%)
-: IO Unit :=
+def assertBEq [BEq α] [ToString α] (caption : String) (actual expected : α) : IO Unit := do
   unless actual == expected do
-    throw <| IO.userError <| s!"assert[{filePos.line}:{filePos.column}]: \
-      expected '{expected}', got '{actual}'"
+    throw <| IO.userError <|
+      s!"{caption}: expected '{expected}', got '{actual}'"
 
 def test : IO Unit := do
   let p1 : IO.Promise Unit ← IO.Promise.new -- resolving queues the task
   let p2 : IO.Promise Unit ← IO.Promise.new -- resolved once task is running
   let p3 : IO.Promise Unit ← IO.Promise.new -- resolving finishes the task
   let t ← BaseIO.mapTask (fun () => do p2.resolve (); IO.wait p3.result) p1.result
-  assertBEq (← IO.getTaskState p1.result) .running
-  assertBEq (← IO.getTaskState p2.result) .running
-  assertBEq (← IO.getTaskState p3.result) .running
-  assertBEq (← IO.getTaskState t) .waiting
+  assertBEq "p1" (← IO.getTaskState p1.result) .running
+  assertBEq "p2" (← IO.getTaskState p2.result) .running
+  assertBEq "p3" (← IO.getTaskState p3.result) .running
+  assertBEq "t" (← IO.getTaskState t) .waiting
   p1.resolve ()
-  assertBEq (← IO.getTaskState p1.result) .finished
+  assertBEq "p1" (← IO.getTaskState p1.result) .finished
   IO.wait p2.result
-  assertBEq (← IO.getTaskState p2.result) .finished
-  assertBEq (← IO.getTaskState t) .running
+  assertBEq "p2" (← IO.getTaskState p2.result) .finished
+  assertBEq "t" (← IO.getTaskState t) .running
   p3.resolve ()
-  assertBEq (← IO.getTaskState p3.result) .finished
+  assertBEq "p3" (← IO.getTaskState p3.result) .finished
   IO.wait t
-  assertBEq (← IO.getTaskState t) .finished
+  assertBEq "t" (← IO.getTaskState t) .finished
 
 #eval test

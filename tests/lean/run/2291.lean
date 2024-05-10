a1be9ec8505544801583a41fa3ac1640491958fa
Kyle Miller <kmill31415@gmail.com>
2024-05-10 15:37:34 -0700

    chore: tidying up `Lean.unresolveNameGlobal` (#4091)
    The main loop logic could be simplified, and `if let` could be used to
    make control flow more obvious.
    
    Also adds a check for macro scopes to prevent `unresolveNameGlobal` from
    returning names with macro scopes in the event there's an alias with
    one.
    
    This is a follow up to #3946.

--- tests/lean/run/2291.lean
+++ tests/lean/run/2291.lean
@@ -1,3 +1,5 @@
+import Lean.Elab.Command
+import Lean.Elab.Open
 /-!
 Issue #2291
 
@@ -6,3 +8,23 @@ The following example would cause the pretty printer to panic.
 
 set_option trace.compiler.simp true in
 #eval [0]
+
+
+/-!
+Fixing the above involved changing `Lean.unresolveNameGlobal`.
+Here, we also verify that we do not pretty print using any aliases that have macro scopes.
+-/
+
+open Lean in
+elab "add_bad_alias " n:ident : command => withFreshMacroScope do
+  let declName ← Elab.OpenDecl.resolveNameUsingNamespaces [← getCurrNamespace] n
+  let badName ← MonadQuotation.addMacroScope `bad
+  modify fun s => { s with env := addAlias s.env badName declName }
+
+def f := 1
+
+add_bad_alias f
+
+-- Formerly was info: bad✝ : ℕ
+/-- info: f : Nat -/
+#guard_msgs in #check (f)

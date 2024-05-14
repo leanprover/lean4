f3538dbdfa5595e361141986fc35daf23e225832
Leonardo de Moura <leomoura@amazon.com>
2024-05-14 21:19:38 +0200

    feat: `grind` normalization theorems (#4164)

---
+++ src/Init/Grind.lean
@@ -0,0 +1,7 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Init.Grind.Norm

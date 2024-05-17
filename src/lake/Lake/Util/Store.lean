581f889db7da24b8fff2713eff4f1ec0e1f16c9c
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-05-17 20:58:00 +0100

    chore: fix the `MonadStore` type classes, replacing outParam by semiOutParam

--- src/lake/Lake/Util/Store.lean
+++ src/lake/Lake/Util/Store.lean
@@ -6,14 +6,14 @@ Authors: Mac Malone
 namespace Lake
 
 /-- A monad equipped with a dependently typed key-value store for a particular key. -/
-class MonadStore1 {κ : Type u} (k : κ) (α : outParam $ Type v) (m : Type v → Type w) where
+class MonadStore1 {κ : Type u} (k : κ) (α : semiOutParam $ Type v) (m : Type v → Type w) where
   fetch? : m (Option α)
   store : α → m PUnit
 
 export MonadStore1 (fetch? store)
 
 /-- A monad equipped with a dependently typed key-object store. -/
-class MonadDStore (κ : Type u) (β : outParam $ κ → Type v) (m : Type v → Type w) where
+class MonadDStore (κ : Type u) (β : semiOutParam $ κ → Type v) (m : Type v → Type w) where
   fetch? : (key : κ) → m (Option (β key))
   store : (key : κ) → β key → m PUnit
 
@@ -21,10 +21,21 @@ instance [MonadDStore κ β m] : MonadStore1 k (β k) m where
   fetch? := MonadDStore.fetch? k
   store o := MonadDStore.store k o
 
+instance [MonadLift m n] [MonadDStore κ β m] : MonadDStore κ β n where
+  fetch? k := liftM (m := m) <| fetch? k
+  store k a := liftM (m := m) <| store k a
+
 /-- A monad equipped with a key-object store. -/
 abbrev MonadStore κ α m := MonadDStore κ (fun _ => α) m
 
-instance [MonadLift m n] [MonadDStore κ β m] : MonadDStore κ β n where
+/- In order to make unificiation work, we need to duplicate the dependent instances for `MonadDStore`
+to non-dependent instances for `MonadStore`. -/
+
+instance [MonadStore κ α m] (k : κ) : MonadStore1 k α m where
+  fetch? := MonadDStore.fetch? k (β := fun _ => α)
+  store o := MonadDStore.store k (β := fun _ => α) o
+
+instance [MonadLift m n] [MonadStore κ α m] : MonadStore κ α n where
   fetch? k := liftM (m := m) <| fetch? k
   store k a := liftM (m := m) <| store k a
 
--- src/lake/Lake/Util/StoreInsts.lean
+++ src/lake/Lake/Util/StoreInsts.lean
@@ -13,7 +13,7 @@ namespace Lake
 
 instance [Monad m] [EqOfCmpWrt κ β cmp] : MonadDStore κ β (StateT (DRBMap κ β cmp) m) where
   fetch? k := return (← get).find? k
-  store k a :=  modify (·.insert k a)
+  store k a := modify (·.insert k a)
 
 instance [Monad m] : MonadStore κ α (StateT (RBMap κ α cmp) m) where
   fetch? k := return (← get).find? k
@@ -21,11 +21,11 @@ instance [Monad m] : MonadStore κ α (StateT (RBMap κ α cmp) m) where
 
 instance [Monad m] : MonadStore κ α (StateT (RBArray κ α cmp) m) where
   fetch? k := return (← get).find? k
-  store k a :=  modify (·.insert k a)
+  store k a := modify (·.insert k a)
 
 instance [Monad m] : MonadStore Name α (StateT (NameMap α) m) :=
   inferInstanceAs (MonadStore _ _ (StateT (RBMap ..) _))
 
 @[inline] instance [MonadDStore κ β m] [t : FamilyOut β k α] : MonadStore1 k α m where
-  fetch? := cast (by rw [t.family_key_eq_type]) <| fetch? (m := m) k
-  store a := store k <| cast t.family_key_eq_type.symm a
+  fetch? := cast (by rw [t.family_key_eq_type]) <| MonadDStore.fetch? (β := β) k
+  store a := MonadDStore.store k <| cast t.family_key_eq_type.symm a

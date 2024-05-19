9235be1364bb71d82b187a5af0eea0c7dd3da6fc
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-05-19 22:09:47 +0100

    separate into `MonadStore1Of` and `MonadStore1`

--- src/lake/Lake/Util/Store.lean
+++ src/lake/Lake/Util/Store.lean
@@ -8,11 +8,22 @@ import Lake.Util.Family
 namespace Lake
 
 /-- A monad equipped with a dependently typed key-value store for a particular key. -/
-class MonadStore1 {κ : Type u} (k : κ) (α : semiOutParam $ Type v) (m : Type v → Type w) where
+class MonadStore1Of {κ : Type u} (k : κ) (α : semiOutParam $ Type v) (m : Type v → Type w) where
   fetch? : m (Option α)
   store : α → m PUnit
 
-export MonadStore1 (fetch? store)
+export MonadStore1Of (store)
+
+/-- Similar to `MonadStore1Of`, but `α` is an `outParam` for convenience. -/
+class MonadStore1 {κ : Type u} (k : κ) (α : outParam $ Type v) (m : Type v → Type w) where
+  fetch? : m (Option α)
+  store : α → m PUnit
+
+export MonadStore1 (fetch?)
+
+instance [MonadStore1Of k α m] : MonadStore1 k α m where
+  fetch? := MonadStore1Of.fetch? k
+  store := MonadStore1Of.store k
 
 /-- A monad equipped with a dependently typed key-object store. -/
 class MonadDStore (κ : Type u) (β : semiOutParam $ κ → Type v) (m : Type v → Type w) where
@@ -22,7 +33,7 @@ class MonadDStore (κ : Type u) (β : semiOutParam $ κ → Type v) (m : Type v 
 /-- A monad equipped with a key-object store. -/
 abbrev MonadStore κ α m := MonadDStore κ (fun _ => α) m
 
-@[inline] instance [MonadDStore κ β m] [t : FamilyOut β k α] : MonadStore1 k α m where
+@[inline] instance [MonadDStore κ β m] [t : FamilyOut β k α] : MonadStore1Of k α m where
   fetch? := t.family_key_eq_type ▸ MonadDStore.fetch? k
   store a := MonadDStore.store k <| cast t.family_key_eq_type.symm a
 
@@ -31,7 +42,7 @@ instance [MonadLift m n] [MonadDStore κ β m] : MonadDStore κ β n where
   store k a := liftM (m := m) <| MonadDStore.store k a
 
 @[inline] def fetchOrCreate [Monad m]
-(key : κ) [MonadStore1 key α m] (create : m α) : m α := do
+(key : κ) [MonadStore1Of key α m] (create : m α) : m α := do
   if let some val ← fetch? key then
     return val
   else

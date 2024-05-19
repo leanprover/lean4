6f014ae257e625299e1ea059c9a6cc780f4b2f27
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-05-19 22:24:57 +0100

    update test

--- tests/lean/MonadStore.lean
+++ tests/lean/MonadStore.lean
@@ -4,8 +4,8 @@ open Lake
 
 -- when `MonadStore1` had an outParam, one of the following instances could not be synthesized, as an outParam can only be one value
 
-#synth MonadStore1 `key Int $ CycleT Name
+#synth MonadStore1Of `key Int $ CycleT Name
   (StateT (NameMap Nat) (StateT (NameMap Int) IO))
 
-#synth MonadStore1 `key Nat $ CycleT Name
+#synth MonadStore1Of `key Nat $ CycleT Name
   (StateT (NameMap Nat) (StateT (NameMap Int) IO))

92dcea412825cdab4f6d21aff1799c7a7ee4daf1
Kim Morrison <kim@tqft.net>
2024-05-21 09:14:11 +1000

    add USize lemmas

--- src/Init/Data/UInt/Lemmas.lean
+++ src/Init/Data/UInt/Lemmas.lean
@@ -88,3 +88,21 @@ protected theorem ne_of_val_ne {a b : UInt64} (h : a.val ≠ b.val) : a ≠ b :=
 protected theorem ne_of_lt {a b : UInt64} (h : a < b) : a ≠ b := UInt64.ne_of_val_ne (Fin.ne_of_lt h)
 
 end UInt64
+
+namespace USize
+
+theorem lt_iff_val_lt_val {a b : USize} : a < b ↔ a.val < b.val := Iff.rfl
+@[simp] protected theorem not_le {a b : USize} : ¬ a ≤ b ↔ b < a := Fin.not_le
+@[simp] protected theorem not_lt {a b : USize} : ¬ a < b ↔ b ≤ a := Fin.not_lt
+@[simp] protected theorem le_refl (a : USize) : a ≤ a := by simp [le_def]
+protected theorem lt_irrefl (a : USize) : ¬ a < a := by simp
+protected theorem le_trans {a b c : USize} : a ≤ b → b ≤ c → a ≤ c := Fin.le_trans
+protected theorem lt_trans {a b c : USize} : a < b → b < c → a < c := Fin.lt_trans
+protected theorem le_total (a b : USize) : a ≤ b ∨ b ≤ a := Fin.le_total a.1 b.1
+protected theorem lt_asymm {a b : USize} (h : a < b) : ¬ b < a := Fin.lt_asymm h
+protected theorem val_eq_of_eq {a b : USize} (h : a = b) : a.val = b.val := h ▸ rfl
+protected theorem eq_of_val_eq {a b : USize} (h : a.val = b.val) : a = b := by cases a; cases b; simp at h; simp [h]
+protected theorem ne_of_val_ne {a b : USize} (h : a.val ≠ b.val) : a ≠ b := fun h' => absurd (USize.val_eq_of_eq h') h
+protected theorem ne_of_lt {a b : USize} (h : a < b) : a ≠ b := USize.ne_of_val_ne (Fin.ne_of_lt h)
+
+end USize

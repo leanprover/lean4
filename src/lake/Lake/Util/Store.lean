d9a8c958a64faa35fcc3969fe5194728da09f3f6
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-05-22 00:32:55 +0100

    remove blank line

--- src/lake/Lake/Util/Store.lean
+++ src/lake/Lake/Util/Store.lean
@@ -3,7 +3,6 @@ Copyright (c) 2022 Mac Malone. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mac Malone
 -/
-
 namespace Lake
 
 /-- A monad equipped with a dependently typed key-value store for a particular key. -/

8eee5ff27f38f7b4856391293e32e8697a570414
Leonardo de Moura <leomoura@amazon.com>
2024-05-25 19:16:19 +0200

    fix: do not include internal `match` equational theorems at simp trace (#4274)
    closes #4251

---
+++ tests/lean/run/4251.lean
@@ -0,0 +1,13 @@
+/--
+info: Try this: simp only [ha, Nat.reduceEqDiff, imp_self]
+-/
+#guard_msgs in
+theorem foo₁ (a : Nat) (ha : a = 37) :
+    (match h : a with | 42 => by simp_all | n => n) = 37 := by
+  simp? [ha]
+
+theorem foo₂ (a : Nat) (ha : a = 37) (hb : b = 37)  :
+    (match h : a with | 42 => by simp_all | n => n) = b := by
+  simp only [ha, Nat.reduceEqDiff, imp_self]
+  guard_target =ₛ 37 = b
+  rw [hb]

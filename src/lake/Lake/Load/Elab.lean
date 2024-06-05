ce67d6ef9ea598d5bc7b7dd84985342ca3559db9
Mac Malone <tydeu@hatpress.net>
2024-06-04 20:35:09 -0400

    fix: lake: track trace of cached build logs (#4343)
    Stores the dependency trace for a build in the cached build log and then
    verifies that it matches the trace of the current build before replaying
    the log. Includes test.
    
    Closes #4303.

--- src/lake/Lake/Load/Elab.lean
+++ src/lake/Lake/Load/Elab.lean
@@ -143,9 +143,6 @@ where
     -- IR Extension (for constant evaluation)
     |>.insert ``IR.declMapExt
 
-instance : ToJson Hash := ⟨(toJson ·.val)⟩
-instance : FromJson Hash := ⟨((⟨·⟩) <$> fromJson? ·)⟩
-
 structure ConfigTrace where
   platform : String
   leanHash : String

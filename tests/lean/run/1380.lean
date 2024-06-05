2c3d59ea4da541b6f226bd2363ab5f7e11b183fd
Joachim Breitner <mail@joachim-breitner.de>
2024-06-05 11:19:03 +0200

    Update tests

--- tests/lean/run/1380.lean
+++ tests/lean/run/1380.lean
@@ -3,19 +3,19 @@ variable (n v₁ v₂) (hv₁: v₁ < n + 1) (hv₂: v₂ < n + 1)
 theorem foo (_: ¬ Fin.mk v₂ hv₂ = Fin.mk v₁ hv₁ ): True := trivial
 
 /--
-info: [Meta.Tactic.simp.unify] @eq_self:1000, failed to unify
+info: [Meta.Tactic.simp.unify] eq_self:1000, failed to unify
       ?a = ?a
     with
       ⟨v₂, hv₂⟩ = ⟨v₁, hv₁⟩
-[Meta.Tactic.simp.rewrite] @Fin.mk.injEq:1000, ⟨v₂, hv₂⟩ = ⟨v₁, hv₁⟩ ==> v₂ = v₁
-[Meta.Tactic.simp.unify] @eq_self:1000, failed to unify
+[Meta.Tactic.simp.rewrite] Fin.mk.injEq:1000, ⟨v₂, hv₂⟩ = ⟨v₁, hv₁⟩ ==> v₂ = v₁
+[Meta.Tactic.simp.unify] eq_self:1000, failed to unify
       ?a = ?a
     with
       v₂ = v₁
-[Meta.Tactic.simp.discharge] @Nat.ne_of_gt discharge ✅
+[Meta.Tactic.simp.discharge] Nat.ne_of_gt discharge ✅
       v₁ < v₂
   [Meta.Tactic.simp.rewrite] hv:1000, v₁ < v₂ ==> True
-[Meta.Tactic.simp.rewrite] @Nat.ne_of_gt:1000, v₂ = v₁ ==> False
+[Meta.Tactic.simp.rewrite] Nat.ne_of_gt:1000, v₂ = v₁ ==> False
 -/
 #guard_msgs in
 set_option trace.Meta.Tactic.simp true in

98402c3364e52ddd6e0a191c8a59ab0f69e998f4
Leonardo de Moura <leomoura@amazon.com>
2024-06-11 09:27:41 -0700

    test: add `n = n - 1` test
    It must fail quickly for `n : UInt64`

--- tests/lean/run/4413.lean
+++ tests/lean/run/4413.lean
@@ -11,3 +11,17 @@ def Note.lowerSemitone (n : Note) : Note :=
 theorem Note.self_containsNote_lowerSemitone_self (n : Note) :
     n.containsNote (Note.lowerSemitone n) := by
   simp [Note.containsNote, Note.lowerSemitone]
+
+/--
+error: type mismatch
+  rfl
+has type
+  n = n : Prop
+but is expected to have type
+  n = n - 1 : Prop
+-/
+#guard_msgs in
+set_option maxRecDepth 100 in
+set_option maxHeartbeats 100 in
+example (n : UInt64) : n = n - 1 :=
+  rfl

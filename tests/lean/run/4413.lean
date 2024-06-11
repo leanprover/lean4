9a1cc1361e8c09187a676c3efcab25711143d9c4
Leonardo de Moura <leomoura@amazon.com>
2024-06-11 09:34:57 -0700

    test: additional tests from issue #4413

--- tests/lean/run/4413.lean
+++ tests/lean/run/4413.lean
@@ -25,3 +25,25 @@ set_option maxRecDepth 100 in
 set_option maxHeartbeats 100 in
 example (n : UInt64) : n = n - 1 :=
   rfl
+
+namespace Ex2
+
+def lowerSemitone := fun (n : Note) => Note.mk (n.1 - 0) n.2
+
+set_option maxRecDepth 100 in
+theorem Note.self_containsNote_lowerSemitone_self (n : Note) :
+    0 ≤ (lowerSemitone n).start :=
+  (Nat.zero_le (Note.start n))
+
+end Ex2
+
+namespace Ex3
+
+def lowerSemitone := fun (n : Note) => Note.mk (n.1 + 100) n.2
+
+set_option maxRecDepth 200 in
+theorem Note.self_containsNote_lowerSemitone_self (n : Note) :
+    0 ≤ (lowerSemitone n).start :=
+  (Nat.zero_le (Note.start n))
+
+end Ex3

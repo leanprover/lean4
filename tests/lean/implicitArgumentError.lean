leanprover/lean4

commit da33f498f539c4147e2254f05f8bd403257411c8

feat: show argument name at "don't know how to synthesize placeholder" error messages

src/Lean/Elab/App.lean
@@ -142,23 +142,27 @@ def eraseNamedArg (binderName : Name) : M Unit :=
 /-
   Add a new argument to the result. That is, `f := f arg`, update `fType`.
   This method assumes `fType` is a function type. -/
-private def addNewArg (arg : Expr) : M Unit :=
+private def addNewArg (argName : Name) (arg : Expr) : M Unit := do
   modify fun s => { s with f := mkApp s.f arg, fType := s.fType.bindingBody!.instantiate1 arg }
+  if arg.isMVar then
+    let mvarId := arg.mvarId!
+    if let some mvarErrorInfo ← getMVarErrorInfo? mvarId then
+      registerMVarErrorInfo { mvarErrorInfo with argName? := argName }
 
 /-
   Elaborate the given `Arg` and add it to the result. See `addNewArg`.
   Recall that, `Arg` may be wrapping an already elaborated `Expr`. -/
-private def elabAndAddNewArg (arg : Arg) : M Unit := do
+private def elabAndAddNewArg (argName : Name) (arg : Arg) : M Unit := do
   let s ← get
   let expectedType ← getArgExpectedType
   match arg with
   | Arg.expr val =>
     let arg ← ensureArgType s.f val expectedType
-    addNewArg arg
+    addNewArg argName arg
   | Arg.stx val  =>
     let val ← elabTerm val expectedType
     let arg ← ensureArgType s.f val expectedType
-    addNewArg arg
+    addNewArg argName arg
 
 /- Return true if the given type contains `OptParam` or `AutoParams` -/
 private def hasOptAutoParams (type : Expr) : M Bool := do
@@ -343,36 +347,36 @@ mutual
   /-
     Create a fresh local variable with the current binder name and argument type, add it to `etaArgs` and `f`,
     and then execute the main loop.-/
-  private partial def addEtaArg : M Expr := do
+  private partial def addEtaArg (argName : Name) : M Expr := do
     let n    ← getBindingName
     let type ← getArgExpectedType
     withLocalDeclD n type fun x => do
       modify fun s => { s with etaArgs := s.etaArgs.push x }
-      addNewArg x
+      addNewArg argName x
       main
 
-  private partial def addImplicitArg : M Expr := do
+  private partial def addImplicitArg (argName : Name) : M Expr := do
     let argType ← getArgExpectedType
     let arg ← mkFreshExprMVar argType
     modify fun s => { s with toSetErrorCtx := s.toSetErrorCtx.push arg.mvarId! }
-    addNewArg arg
+    addNewArg argName arg
     main
 
   /-
     Process a `fType` of the form `(x : A) → B x`.
     This method assume `fType` is a function type -/
-  private partial def processExplictArg : M Expr := do
+  private partial def processExplictArg (argName : Name) : M Expr := do
     let s ← get
     match s.args with
     | arg::args =>
       propagateExpectedType arg
       modify fun s => { s with args := args }
-      elabAndAddNewArg arg
+      elabAndAddNewArg argName arg
       main
     | _ =>
       let argType ← getArgExpectedType
       match s.explicit, argType.getOptParamDefault?, argType.getAutoParamTactic? with
-      | false, some defVal, _  => addNewArg defVal; main
+      | false, some defVal, _  => addNewArg argName defVal; main
       | false, _, some (Expr.const tacticDecl _ _) =>
         let env ← getEnv
         let opts ← getOptions
@@ -384,21 +388,21 @@ mutual
           let argType     := argType.getArg! 0 -- `autoParam type := by tactic` ==> `type`
           let argNew := Arg.stx tacticBlock
           propagateExpectedType argNew
-          elabAndAddNewArg argNew
+          elabAndAddNewArg argName argNew
           main
       | false, _, some _ =>
         throwError "invalid autoParam, argument must be a constant"
       | _, _, _ =>
         if !s.namedArgs.isEmpty then
           if (← anyNamedArgDependsOnCurrent) then
-            addImplicitArg
+            addImplicitArg argName
           else
-            addEtaArg
+            addEtaArg argName
         else if !s.explicit then
           if (← fTypeHasOptAutoParams) then
-            addEtaArg
+            addEtaArg argName
           else if (← get).ellipsis then
-            addImplicitArg
+            addImplicitArg argName
           else
             finalize
         else
@@ -407,41 +411,41 @@ mutual
   /-
     Process a `fType` of the form `{x : A} → B x`.
     This method assume `fType` is a function type -/
-  private partial def processImplicitArg : M Expr := do
+  private partial def processImplicitArg (argName : Name) : M Expr := do
     if (← get).explicit then
-      processExplictArg
+      processExplictArg argName
     else
-      addImplicitArg
+      addImplicitArg argName
 
   /-
     Process a `fType` of the form `{{x : A}} → B x`.
     This method assume `fType` is a function type -/
-  private partial def processStrictImplicitArg : M Expr := do
+  private partial def processStrictImplicitArg (argName : Name) : M Expr := do
     if (← get).explicit then
-      processExplictArg
+      processExplictArg argName
     else if (← hasArgsToProcess) then
-      addImplicitArg
+      addImplicitArg argName
     else
       finalize
 
   /-
     Process a `fType` of the form `[x : A] → B x`.
     This method assume `fType` is a function type -/
-  private partial def processInstImplicitArg : M Expr := do
+  private partial def processInstImplicitArg (argName : Name) : M Expr := do
     if (← get).explicit then
       if (← isNextArgHole) then
         /- Recall that if '@' has been used, and the argument is '_', then we still use type class resolution -/
         let arg ← mkFreshExprMVar (← getArgExpectedType) MetavarKind.synthetic
         modify fun s => { s with args := s.args.tail! }
         addInstMVar arg.mvarId!
-        addNewArg arg
+        addNewArg argName arg
         main
       else
-        processExplictArg
+        processExplictArg argName
     else
       let arg ← mkFreshExprMVar (← getArgExpectedType) MetavarKind.synthetic
       addInstMVar arg.mvarId!
-      addNewArg arg
+      addNewArg argName arg
       main
 
   /- Elaborate function application arguments. -/
@@ -456,14 +460,14 @@ mutual
       | some namedArg =>
         propagateExpectedType namedArg.val
         eraseNamedArg binderName
-        elabAndAddNewArg namedArg.val
+        elabAndAddNewArg binderName namedArg.val
         main
       | none          =>
         match binfo with
-        | BinderInfo.implicit       => processImplicitArg
-        | BinderInfo.instImplicit   => processInstImplicitArg
-        | BinderInfo.strictImplicit => processStrictImplicitArg
-        | _                         => processExplictArg
+        | BinderInfo.implicit       => processImplicitArg binderName
+        | BinderInfo.instImplicit   => processInstImplicitArg binderName
+        | BinderInfo.strictImplicit => processStrictImplicitArg binderName
+        | _                         => processExplictArg binderName
     else if (← hasArgsToProcess) then
       synthesizePendingAndNormalizeFunType
       main

src/Lean/Elab/Term.lean
@@ -104,6 +104,7 @@ structure MVarErrorInfo where
   mvarId    : MVarId
   ref       : Syntax
   kind      : MVarErrorKind
+  argName?  : Option Name := none
   deriving Inhabited
 
 structure LetRecToLift where
@@ -379,14 +380,17 @@ def registerSyntheticMVar (stx : Syntax) (mvarId : MVarId) (kind : SyntheticMVar
 def registerSyntheticMVarWithCurrRef (mvarId : MVarId) (kind : SyntheticMVarKind) : TermElabM Unit := do
   registerSyntheticMVar (← getRef) mvarId kind
 
-def registerMVarErrorHoleInfo (mvarId : MVarId) (ref : Syntax) : TermElabM Unit := do
-  modify fun s => { s with mvarErrorInfos := s.mvarErrorInfos.insert mvarId { mvarId := mvarId, ref := ref, kind := MVarErrorKind.hole } }
+def registerMVarErrorInfo (mvarErrorInfo : MVarErrorInfo) : TermElabM Unit :=
+  modify fun s => { s with mvarErrorInfos := s.mvarErrorInfos.insert mvarErrorInfo.mvarId mvarErrorInfo }
+
+def registerMVarErrorHoleInfo (mvarId : MVarId) (ref : Syntax) : TermElabM Unit :=
+  registerMVarErrorInfo { mvarId := mvarId, ref := ref, kind := MVarErrorKind.hole }
 
 def registerMVarErrorImplicitArgInfo (mvarId : MVarId) (ref : Syntax) (app : Expr) : TermElabM Unit := do
-  modify fun s => { s with mvarErrorInfos := s.mvarErrorInfos.insert mvarId { mvarId := mvarId, ref := ref, kind := MVarErrorKind.implicitArg app } }
+  registerMVarErrorInfo { mvarId := mvarId, ref := ref, kind := MVarErrorKind.implicitArg app }
 
 def registerMVarErrorCustomInfo (mvarId : MVarId) (ref : Syntax) (msgData : MessageData) : TermElabM Unit := do
-  modify fun s => { s with mvarErrorInfos := s.mvarErrorInfos.insert mvarId { mvarId := mvarId, ref := ref, kind := MVarErrorKind.custom msgData } }
+  registerMVarErrorInfo { mvarId := mvarId, ref := ref, kind := MVarErrorKind.custom msgData }
 
 def getMVarErrorInfo? (mvarId : MVarId) : TermElabM (Option MVarErrorInfo) := do
   return (← get).mvarErrorInfos.find? mvarId
@@ -411,16 +415,23 @@ def MVarErrorInfo.logError (mvarErrorInfo : MVarErrorInfo) (extraMsg? : Option M
   match mvarErrorInfo.kind with
   | MVarErrorKind.implicitArg app => do
     let app ← instantiateMVars app
-    let msg : MessageData := m!"don't know how to synthesize implicit argument{indentExpr app.setAppPPExplicitForExposingMVars}"
-    let msg := msg ++ Format.line ++ "context:" ++ Format.line ++ MessageData.ofGoal mvarErrorInfo.mvarId
+    let msg := addArgName "don't know how to synthesize implicit argument"
+    let msg := msg ++ m!"{indentExpr app.setAppPPExplicitForExposingMVars}" ++ Format.line ++ "context:" ++ Format.line ++ MessageData.ofGoal mvarErrorInfo.mvarId
     logErrorAt mvarErrorInfo.ref (appendExtra msg)
   | MVarErrorKind.hole => do
-    let msg : MessageData := "don't know how to synthesize placeholder"
+    let msg := addArgName "don't know how to synthesize placeholder" " for argument"
     let msg := msg ++ Format.line ++ "context:" ++ Format.line ++ MessageData.ofGoal mvarErrorInfo.mvarId
     logErrorAt mvarErrorInfo.ref (MessageData.tagged `Elab.synthPlaceholder <| appendExtra msg)
   | MVarErrorKind.custom msg =>
     logErrorAt mvarErrorInfo.ref (appendExtra msg)
 where
+  /-- Append `mvarErrorInfo` argument name (if available) to the message.
+      Remark: if the argument name contains macro scopes we do not append it. -/
+  addArgName (msg : MessageData) (extra : String := "") : MessageData :=
+    match mvarErrorInfo.argName? with
+    | none => msg
+    | some argName => if argName.hasMacroScopes then msg else msg ++ extra ++ m!" '{argName}'"
+
   appendExtra (msg : MessageData) : MessageData :=
     match extraMsg? with
     | none => msg

tests/lean/217.lean.expected.out
@@ -1,6 +1,6 @@
-217.lean:5:28-5:29: error: don't know how to synthesize placeholder
+217.lean:5:28-5:29: error: don't know how to synthesize placeholder for argument 'f'
 context:
 ⊢ CoreM Unit → Name → ConstantInfo → CoreM Unit
-217.lean:5:30-5:31: error: don't know how to synthesize placeholder
+217.lean:5:30-5:31: error: don't know how to synthesize placeholder for argument 'init'
 context:
 ⊢ CoreM Unit

tests/lean/argNameAtPlaceholderError.lean.expected.out
@@ -1,0 +1,12 @@
+argNameAtPlaceholderError.lean:8:13-8:14: error: don't know how to synthesize placeholder for argument 'expectedType?'
+context:
+stx : Syntax
+⊢ Option Expr
+argNameAtPlaceholderError.lean:8:15-8:16: error: don't know how to synthesize placeholder for argument 'catchExPostpone'
+context:
+stx : Syntax
+⊢ optParam Bool true
+argNameAtPlaceholderError.lean:8:11-8:12: error: don't know how to synthesize placeholder for argument 'stx'
+context:
+stx : Syntax
+⊢ Syntax

tests/lean/argNameAtPlaceholderError.lean
@@ -1,0 +1,8 @@
+import Lean
+
+open Lean
+open Lean.Elab
+open Lean.Elab.Term
+
+def f (stx : Syntax) : TermElabM Expr :=
+  elabTerm _ _ _

tests/lean/argNameIfMacroScopes.lean
@@ -1,0 +1,5 @@
+def g : Nat → Nat :=
+  fun _ => 0
+
+def f (x : Nat) : Nat :=
+  g _

tests/lean/argNameIfMacroScopes.lean.expected.out
@@ -1,0 +1,4 @@
+argNameIfMacroScopes.lean:5:4-5:5: error: don't know how to synthesize placeholder
+context:
+x : Nat
+⊢ Nat

commit c96797eb93c538504d53e2208b57b61f63be7b8f

fix: show argument name in implicit argument error (#4426)
When an implicit argument cannot be inferred, the error should show the
name of the argument.

Showing the argument name in the error message for an uninstantiated
metavariable was introduced in da33f498f539c4147e2254f05f8bd403257411c8,
but this implementation causes some argument names to get lost.

src/Lean/Elab/App.lean
@@ -233,9 +233,7 @@ def eraseNamedArg (binderName : Name) : M Unit :=
 private def addNewArg (argName : Name) (arg : Expr) : M Unit := do
   modify fun s => { s with f := mkApp s.f arg, fType := s.fType.bindingBody!.instantiate1 arg }
   if arg.isMVar then
-    let mvarId := arg.mvarId!
-    if let some mvarErrorInfo ← getMVarErrorInfo? mvarId then
-      registerMVarErrorInfo { mvarErrorInfo with argName? := argName }
+    registerMVarArgName arg.mvarId! argName
 
 /--
   Elaborate the given `Arg` and add it to the result. See `addNewArg`.
@@ -833,9 +831,7 @@ private def elabArg (arg : Arg) (argExpectedType : Expr) : M Expr := do
 /-- Save information for producing error messages. -/
 def saveArgInfo (arg : Expr) (binderName : Name) : M Unit := do
   if arg.isMVar then
-    let mvarId := arg.mvarId!
-    if let some mvarErrorInfo ← getMVarErrorInfo? mvarId then
-      registerMVarErrorInfo { mvarErrorInfo with argName? := binderName }
+    registerMVarArgName arg.mvarId! binderName
 
 /-- Create an implicit argument using the given `BinderInfo`. -/
 def mkImplicitArg (argExpectedType : Expr) (bi : BinderInfo) : M Expr := do

src/Lean/Elab/Term.lean
@@ -91,7 +91,6 @@ structure MVarErrorInfo where
   mvarId    : MVarId
   ref       : Syntax
   kind      : MVarErrorKind
-  argName?  : Option Name := none
   deriving Inhabited
 
 /--
@@ -119,7 +118,20 @@ structure State where
   levelNames        : List Name       := []
   syntheticMVars    : MVarIdMap SyntheticMVarDecl := {}
   pendingMVars      : List MVarId := {}
-  mvarErrorInfos    : MVarIdMap MVarErrorInfo := {}
+  /-- List of errors associated to a metavariable that are shown to the user if the metavariable could not be fully instantiated -/
+  mvarErrorInfos    : List MVarErrorInfo := []
+  /--
+    `mvarArgNames` stores the argument names associated to metavariables.
+    These are used in combination with `mvarErrorInfos` for throwing errors about metavariables that could not be fully instantiated.
+    For example when elaborating `List _`, the argument name of the placeholder will be `α`.
+
+    While elaborating an application, `mvarArgNames` is set for each metavariable argument, using the available argument name.
+    This may happen before or after the `mvarErrorInfos` is set for the same metavariable.
+
+    We used to store the argument names in `mvarErrorInfos`, updating the `MVarErrorInfos` to add the argument name when it is available,
+    but this doesn't work if the argument name is available _before_ the `mvarErrorInfos` is set for that metavariable.
+  -/
+  mvarArgNames      : MVarIdMap Name := {}
   letRecsToLift     : List LetRecToLift := []
   deriving Inhabited
 
@@ -626,7 +638,7 @@ def registerSyntheticMVarWithCurrRef (mvarId : MVarId) (kind : SyntheticMVarKind
   registerSyntheticMVar (← getRef) mvarId kind
 
 def registerMVarErrorInfo (mvarErrorInfo : MVarErrorInfo) : TermElabM Unit :=
-  modify fun s => { s with mvarErrorInfos := s.mvarErrorInfos.insert mvarErrorInfo.mvarId mvarErrorInfo }
+  modify fun s => { s with mvarErrorInfos := mvarErrorInfo :: s.mvarErrorInfos }
 
 def registerMVarErrorHoleInfo (mvarId : MVarId) (ref : Syntax) : TermElabM Unit :=
   registerMVarErrorInfo { mvarId, ref, kind := .hole }
@@ -637,13 +649,13 @@ def registerMVarErrorImplicitArgInfo (mvarId : MVarId) (ref : Syntax) (app : Exp
 def registerMVarErrorCustomInfo (mvarId : MVarId) (ref : Syntax) (msgData : MessageData) : TermElabM Unit := do
   registerMVarErrorInfo { mvarId, ref, kind := .custom msgData }
 
-def getMVarErrorInfo? (mvarId : MVarId) : TermElabM (Option MVarErrorInfo) := do
-  return (← get).mvarErrorInfos.find? mvarId
-
 def registerCustomErrorIfMVar (e : Expr) (ref : Syntax) (msgData : MessageData) : TermElabM Unit :=
   match e.getAppFn with
   | Expr.mvar mvarId => registerMVarErrorCustomInfo mvarId ref msgData
   | _ => pure ()
+
+def registerMVarArgName (mvarId : MVarId) (argName : Name) : TermElabM Unit :=
+  modify fun s => { s with mvarArgNames := s.mvarArgNames.insert mvarId argName }
 
 /--
   Auxiliary method for reporting errors of the form "... contains metavariables ...".
@@ -660,22 +672,22 @@ def MVarErrorInfo.logError (mvarErrorInfo : MVarErrorInfo) (extraMsg? : Option M
   match mvarErrorInfo.kind with
   | MVarErrorKind.implicitArg app => do
     let app ← instantiateMVars app
-    let msg := addArgName "don't know how to synthesize implicit argument"
+    let msg ← addArgName "don't know how to synthesize implicit argument"
     let msg := msg ++ m!"{indentExpr app.setAppPPExplicitForExposingMVars}" ++ Format.line ++ "context:" ++ Format.line ++ MessageData.ofGoal mvarErrorInfo.mvarId
     logErrorAt mvarErrorInfo.ref (appendExtra msg)
   | MVarErrorKind.hole => do
-    let msg := addArgName "don't know how to synthesize placeholder" " for argument"
+    let msg ← addArgName "don't know how to synthesize placeholder" " for argument"
     let msg := msg ++ Format.line ++ "context:" ++ Format.line ++ MessageData.ofGoal mvarErrorInfo.mvarId
     logErrorAt mvarErrorInfo.ref (MessageData.tagged `Elab.synthPlaceholder <| appendExtra msg)
   | MVarErrorKind.custom msg =>
     logErrorAt mvarErrorInfo.ref (appendExtra msg)
 where
-  /-- Append `mvarErrorInfo` argument name (if available) to the message.
+  /-- Append the argument name (if available) to the message.
       Remark: if the argument name contains macro scopes we do not append it. -/
-  addArgName (msg : MessageData) (extra : String := "") : MessageData :=
-    match mvarErrorInfo.argName? with
-    | none => msg
-    | some argName => if argName.hasMacroScopes then msg else msg ++ extra ++ m!" '{argName}'"
+  addArgName (msg : MessageData) (extra : String := "") : TermElabM MessageData := do
+    match (← get).mvarArgNames.find? mvarErrorInfo.mvarId with
+    | none => return msg
+    | some argName => return if argName.hasMacroScopes then msg else msg ++ extra ++ m!" '{argName}'"
 
   appendExtra (msg : MessageData) : MessageData :=
     match extraMsg? with
@@ -697,7 +709,7 @@ def logUnassignedUsingErrorInfos (pendingMVarIds : Array MVarId) (extraMsg? : Op
     let mut hasNewErrors := false
     let mut alreadyVisited : MVarIdSet := {}
     let mut errors : Array MVarErrorInfo := #[]
-    for (_, mvarErrorInfo) in (← get).mvarErrorInfos do
+    for mvarErrorInfo in (← get).mvarErrorInfos do
       let mvarId := mvarErrorInfo.mvarId
       unless alreadyVisited.contains mvarId do
         alreadyVisited := alreadyVisited.insert mvarId

tests/lean/1616.lean.expected.out
@@ -1,5 +1,20 @@
+1616.lean:10:24-10:41: error: don't know how to synthesize implicit argument 'z'
+  @Linear ?m ?m (?m :: ?m) (?m :: ?m) c.right
+context:
+c : Cover ?m ?m ?m
+⊢ List ?m
+1616.lean:10:24-10:41: error: don't know how to synthesize implicit argument 'y'
+  @Linear ?m ?m (?m :: ?m) (?m :: ?m) c.right
+context:
+c : Cover ?m ?m ?m
+⊢ List ?m
 1616.lean:10:24-10:41: error: don't know how to synthesize implicit argument
   @Linear ?m ?m (?m :: ?m) (?m :: ?m) c.right
+context:
+c : Cover ?m ?m ?m
+⊢ Type u_1
+1616.lean:10:32-10:40: error: don't know how to synthesize implicit argument 'α'
+  @Cover.right ?m ?m ?m ?m ?m c
 context:
 c : Cover ?m ?m ?m
 ⊢ Type u_1
@@ -8,43 +23,28 @@ c : Cover ?m ?m ?m
 context:
 c : Cover ?m ?m ?m
 ⊢ Type u_1
-1616.lean:9:31-9:38: error: don't know how to synthesize implicit argument
-  @Cover.left ?m ?m ?m ?m ?m c
-context:
-c : Cover ?m ?m ?m
-⊢ Type u_1
-1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument
+1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument 'z'
   @Linear ?m (?m :: ?m) ?m (?m :: ?m) c.left
 context:
 c : Cover ?m ?m ?m
 ⊢ List ?m
-1616.lean:10:24-10:41: error: don't know how to synthesize implicit argument
-  @Linear ?m ?m (?m :: ?m) (?m :: ?m) c.right
+1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument 'x'
+  @Linear ?m (?m :: ?m) ?m (?m :: ?m) c.left
 context:
 c : Cover ?m ?m ?m
 ⊢ List ?m
+1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument
+  @Linear ?m (?m :: ?m) ?m (?m :: ?m) c.left
+context:
+c : Cover ?m ?m ?m
+⊢ Type u_1
+1616.lean:9:31-9:38: error: don't know how to synthesize implicit argument 'α'
+  @Cover.left ?m ?m ?m ?m ?m c
+context:
+c : Cover ?m ?m ?m
+⊢ Type u_1
 1616.lean:9:11-9:19: error: don't know how to synthesize implicit argument
   @Linear ?m ?m ?m ?m c
 context:
 c : Cover ?m ?m ?m
 ⊢ Type u_1
-1616.lean:10:32-10:40: error: don't know how to synthesize implicit argument
-  @Cover.right ?m ?m ?m ?m ?m c
-context:
-c : Cover ?m ?m ?m
-⊢ Type u_1
-1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument
-  @Linear ?m (?m :: ?m) ?m (?m :: ?m) c.left
-context:
-c : Cover ?m ?m ?m
-⊢ Type u_1
-1616.lean:10:24-10:41: error: don't know how to synthesize implicit argument
-  @Linear ?m ?m (?m :: ?m) (?m :: ?m) c.right
-context:
-c : Cover ?m ?m ?m
-⊢ List ?m
-1616.lean:9:23-9:39: error: don't know how to synthesize implicit argument
-  @Linear ?m (?m :: ?m) ?m (?m :: ?m) c.left
-context:
-c : Cover ?m ?m ?m
-⊢ List ?m

tests/lean/217.lean.expected.out
@@ -1,6 +1,6 @@
-217.lean:5:28-5:29: error: don't know how to synthesize placeholder for argument 'f'
-context:
-⊢ CoreM Unit → Name → ConstantInfo → CoreM Unit
 217.lean:5:30-5:31: error: don't know how to synthesize placeholder for argument 'init'
 context:
 ⊢ CoreM Unit
+217.lean:5:28-5:29: error: don't know how to synthesize placeholder for argument 'f'
+context:
+⊢ CoreM Unit → Name → ConstantInfo → CoreM Unit

tests/lean/argNameAtPlaceholderError.lean.expected.out
@@ -1,11 +1,11 @@
-argNameAtPlaceholderError.lean:8:13-8:14: error: don't know how to synthesize placeholder for argument 'expectedType?'
-context:
-stx : Syntax
-⊢ Option Expr
 argNameAtPlaceholderError.lean:8:15-8:16: error: don't know how to synthesize placeholder for argument 'catchExPostpone'
 context:
 stx : Syntax
 ⊢ Bool
+argNameAtPlaceholderError.lean:8:13-8:14: error: don't know how to synthesize placeholder for argument 'expectedType?'
+context:
+stx : Syntax
+⊢ Option Expr
 argNameAtPlaceholderError.lean:8:11-8:12: error: don't know how to synthesize placeholder for argument 'stx'
 context:
 stx : Syntax

tests/lean/evalNone.lean.expected.out
@@ -1,12 +1,12 @@
-evalNone.lean:1:6-1:10: error: don't know how to synthesize implicit argument
+evalNone.lean:1:6-1:10: error: don't know how to synthesize implicit argument 'α'
   @none ?m
 context:
 ⊢ Type ?u
-evalNone.lean:3:6-3:14: error: don't know how to synthesize implicit argument
+evalNone.lean:3:6-3:14: error: don't know how to synthesize implicit argument 'α'
   @List.head? ?m []
 context:
 ⊢ Type ?u
-evalNone.lean:3:6-3:8: error: don't know how to synthesize implicit argument
+evalNone.lean:3:6-3:8: error: don't know how to synthesize implicit argument 'α'
   @List.nil ?m
 context:
 ⊢ Type ?u

tests/lean/evalWithMVar.lean.expected.out
@@ -1,9 +1,9 @@
 c.someRight : Option Nat
-evalWithMVar.lean:13:6-13:21: error: don't know how to synthesize implicit argument
+evalWithMVar.lean:13:6-13:21: error: don't know how to synthesize implicit argument 'α'
   @Sum.someRight ?m Nat c
 context:
 ⊢ Type ?u
-evalWithMVar.lean:13:20-13:21: error: don't know how to synthesize implicit argument
+evalWithMVar.lean:13:20-13:21: error: don't know how to synthesize implicit argument 'α'
   @c ?m
 context:
 ⊢ Type ?u

tests/lean/hidingInaccessibleNames.lean.expected.out
@@ -20,15 +20,15 @@ x✝² : List Nat
 x✝¹ : Nat
 x✝ : x✝² ≠ []
 ⊢ Nat
-hidingInaccessibleNames.lean:8:16-8:17: error: don't know how to synthesize placeholder
-context:
-x✝¹ : Nat
-x✝ : [] ≠ []
-⊢ Nat
 hidingInaccessibleNames.lean:9:19-9:20: error: don't know how to synthesize placeholder
 context:
 a b x✝¹ : Nat
 x✝ : [a, b] ≠ []
+⊢ Nat
+hidingInaccessibleNames.lean:8:16-8:17: error: don't know how to synthesize placeholder
+context:
+x✝¹ : Nat
+x✝ : [] ≠ []
 ⊢ Nat
 case inl
 p q : Prop

tests/lean/holeErrors.lean.expected.out
@@ -1,24 +1,24 @@
-holeErrors.lean:3:11-3:20: error: failed to infer definition type
-holeErrors.lean:3:14-3:20: error: don't know how to synthesize implicit argument
+holeErrors.lean:3:14-3:20: error: don't know how to synthesize implicit argument 'α'
   @id ?m
 context:
 ⊢ Sort u
+holeErrors.lean:3:11-3:20: error: failed to infer definition type
 holeErrors.lean:5:9-5:10: error: failed to infer definition type
 when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
-holeErrors.lean:8:9-8:15: error: don't know how to synthesize implicit argument
+holeErrors.lean:8:9-8:15: error: don't know how to synthesize implicit argument 'α'
   @id ?m
 context:
 ⊢ Sort u
-holeErrors.lean:7:11-9:1: error: failed to infer definition type
 holeErrors.lean:8:4-8:5: error: failed to infer 'let' declaration type
+holeErrors.lean:7:11-9:1: error: failed to infer definition type
 holeErrors.lean:11:11-11:15: error: failed to infer definition type
 holeErrors.lean:11:8-11:9: error: failed to infer binder type
-holeErrors.lean:13:12-13:13: error: failed to infer binder type
 holeErrors.lean:13:15-13:19: error: failed to infer definition type
+holeErrors.lean:13:12-13:13: error: failed to infer binder type
 holeErrors.lean:16:4-16:5: error: failed to infer binder type
 holeErrors.lean:15:7-16:10: error: failed to infer definition type
-holeErrors.lean:19:8-19:9: error: failed to infer 'let rec' declaration type
-holeErrors.lean:19:13-19:19: error: don't know how to synthesize implicit argument
+holeErrors.lean:19:13-19:19: error: don't know how to synthesize implicit argument 'α'
   @id ?m
 context:
 ⊢ Sort u
+holeErrors.lean:19:8-19:9: error: failed to infer 'let rec' declaration type

tests/lean/holes.lean.expected.out
@@ -10,24 +10,24 @@ context:
 x : Nat
 y : Nat := g x + g x
 ⊢ Nat
-holes.lean:10:15-10:18: error: don't know how to synthesize implicit argument
+holes.lean:10:15-10:18: error: don't know how to synthesize implicit argument 'β'
   @g Nat (?m x) x
 context:
 x : Nat
 ⊢ Type
-holes.lean:10:9-10:12: error: don't know how to synthesize implicit argument
+holes.lean:10:9-10:12: error: don't know how to synthesize implicit argument 'β'
   @g Nat (?m x) x
 context:
 x : Nat
 ⊢ Type
 holes.lean:13:10-13:11: error: failed to infer binder type
 holes.lean:15:16-15:17: error: failed to infer binder type
-holes.lean:18:9-18:10: error: failed to infer binder type
-holes.lean:19:0-19:3: error: don't know how to synthesize implicit argument
+holes.lean:19:0-19:3: error: don't know how to synthesize implicit argument 'β'
   @f Nat (?m a) a
 context:
 a : Nat
 f : {α : Type} → {β : ?m a} → α → α := fun {α} {β} a => a
 ⊢ ?m a
+holes.lean:18:9-18:10: error: failed to infer binder type
 holes.lean:21:25-22:4: error: failed to infer definition type
 holes.lean:25:8-25:11: error: failed to infer 'let rec' declaration type

tests/lean/implicitArgumentError.lean
@@ -1,0 +1,17 @@
+/-!
+In the past, when an implicit argument couldn't be synthesized, the name of the argument got lost during elaboration.
+Now it is saved and added to the error message.
+
+In this exaple, that is 'n'.
+-/
+
+def foo {n : Nat} := 2*n
+
+/--
+error: don't know how to synthesize implicit argument 'n'
+  @foo ?m.64
+context:
+⊢ Nat
+-/
+#guard_msgs in
+#eval foo

tests/lean/jason2.lean.expected.out
@@ -1,8 +1,8 @@
-jason2.lean:4:30-4:37: error: don't know how to synthesize implicit argument
+jason2.lean:4:30-4:37: error: don't know how to synthesize implicit argument 'x'
   @Foo.foo ?m
 context:
 ⊢ Nat
-jason2.lean:4:20-4:23: error: don't know how to synthesize implicit argument
+jason2.lean:4:20-4:23: error: don't know how to synthesize implicit argument 'x'
   @Foo ?m
 context:
 ⊢ Nat
55242c5f7230233e6f07b63d64a9eca245a28815
David Thrane Christiansen <david@davidchristiansen.dk>
2024-06-18 17:33:45 +0200

    chore: missing copyright header

--- src/Lean/Elab/Tactic/Doc.lean
+++ src/Lean/Elab/Tactic/Doc.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: David Thrane Christiansen
+-/
 prelude
 import Lean.Elab.Command
 import Lean.Parser.Tactic.Doc

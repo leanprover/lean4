6dee7a13643ee971daf52f23b3fd23f64d076df1
Bolton Bailey <bolton.bailey@gmail.com>
2024-06-19 17:04:00 -0400

    add changes

--- src/Init/Data/Option/Basic.lean
+++ src/Init/Data/Option/Basic.lean
@@ -119,9 +119,12 @@ def merge (fn : α → α → α) : Option α → Option α → Option α
 
 
 /-- An elimination principle for `Option`. It is a nondependent version of `Option.recOn`. -/
-@[simp, inline] protected def elim : Option α → β → (α → β) → β
+@[inline] protected def elim : Option α → β → (α → β) → β
   | some x, _, f => f x
   | none, y, _ => y
+
+@[simp] theorem elim_none (b) (f : α → β) : none.elim b f = b := rfl
+@[simp] theorem elim_some (a) (b) (f : α → β) : (some a).elim b f = f a := rfl
 
 /-- Extracts the value `a` from an option that is known to be `some a` for some `a`. -/
 @[inline] def get {α : Type u} : (o : Option α) → isSome o → α

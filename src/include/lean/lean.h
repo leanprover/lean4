76dbcf5b9e41c8c2f67139428996f1d0faa7b255
qawbecrdtey <40463813+qawbecrdtey@users.noreply.github.com>
2024-06-20 21:10:34 +0900

    typo
    also fixed comments.

--- src/include/lean/lean.h
+++ src/include/lean/lean.h
@@ -1634,16 +1634,16 @@ static inline uint8_t lean_uint8_modn(uint8_t a1, b_lean_obj_arg a2) {
 static inline uint8_t lean_uint8_log2(uint8_t a) {
     static unsigned char const table[8] = {5, 0, 3, 1, 4, 7, 2, 6};
 
-// Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`.
+/* Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`. */
     a |= a >> 1;
     a |= a >> 2;
     a |= a >> 4;
     a |= a >> 8;
 
-// Multiplying the magic number 0x27 to `2 ^ n - 1` for `n = 0` to `7`
-// maps its significant `3` bits from `0` to `7`.
-// `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x07C4ACDD) mod 2 ^ 8` -- table -> `n`.
-    return table[(std::uint8_t)(a * Ox27) >> 5];
+/* Multiplying the magic number 0x27 to `2 ^ n - 1` for `n = 0` to `7` 
+   maps its significant `3` bits from `0` to `7`.
+   `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x07C4ACDD) mod 2 ^ 8` -- table -> `n`. */
+    return table[(std::uint8_t)(a * 0x27) >> 5];
 }
 static inline uint8_t lean_uint8_dec_eq(uint8_t a1, uint8_t a2) { return a1 == a2; }
 static inline uint8_t lean_uint8_dec_lt(uint8_t a1, uint8_t a2) { return a1 < a2; }
@@ -1682,14 +1682,14 @@ static inline uint16_t lean_uint16_modn(uint16_t a1, b_lean_obj_arg a2) {
     }
 }
 static inline uint16_t lean_uint16_log2(uint16_t a) {
-// Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`.
+/* Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`. */
     a |= a >> 1;
     a |= a >> 2;
     a |= a >> 4;
     a |= a >> 8;
 
-// Multiplying the magic number 0xF09B to `2 ^ n - 1` for `n = 0` to `15`
-// maps its significant `4` bits to `(16 - n) mod 16`.
+/* Multiplying the magic number 0xF09B to `2 ^ n - 1` for `n = 0` to `15`
+   maps its significant `4` bits to `(16 - n) mod 16`. */
     return (16 - (uint16_t)(a * 0xF09B)) & 0xF;
 }
 static inline uint8_t lean_uint16_dec_eq(uint16_t a1, uint16_t a2) { return a1 == a2; }
@@ -1739,16 +1739,16 @@ static inline uint32_t lean_uint32_log2(uint32_t a) {
          8, 12, 20, 28, 15, 17, 24,  7,
         19, 27, 23,  6, 26,  5,  4, 31};
 
-// Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`.
+/* Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`. */
     a |= a >> 1;
     a |= a >> 2;
     a |= a >> 4;
     a |= a >> 8;
     a |= a >> 16;
 
-// Multiplying the magic number 0x07C4ACDD to `2 ^ n - 1` for `n = 0` to `31`
-// maps its significant `5` bits from `0` to `31`.
-// `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x07C4ACDD) mod 2 ^ 32` -- table -> `n`.
+/* Multiplying the magic number 0x07C4ACDD to `2 ^ n - 1` for `n = 0` to `31`
+   maps its significant `5` bits from `0` to `31`.
+   `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x07C4ACDD) mod 2 ^ 32` -- table -> `n`. */
     return table[(uint32_t)(a * 0x07C4ACDD) >> 27];
 }
 static inline uint8_t lean_uint32_dec_eq(uint32_t a1, uint32_t a2) { return a1 == a2; }
@@ -1799,7 +1799,7 @@ static inline uint64_t lean_uint64_log2(uint64_t a) {
         45, 25, 31, 35, 16,  9, 12, 44,
         24, 15,  8, 23,  7,  6,  5, 63};
 
-// Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`.
+/* Maps `a` to the smallest `2 ^ n - 1` form not greater than `a`. */
     a |= a >> 1;
     a |= a >> 2;
     a |= a >> 4;
@@ -1807,9 +1807,9 @@ static inline uint64_t lean_uint64_log2(uint64_t a) {
     a |= a >> 16;
     a |= a >> 32;
 
-// Multiplying the magic number 0x03F6EAF2CD271461 to `2 ^ n - 1` for `n = 0` to `63`
-// maps its significant `6` bits from `0` to `63`.
-// `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x03F6EAF2CD271461) mod 2 ^ 64` -- table -> `n`.
+/* Multiplying the magic number 0x03F6EAF2CD271461 to `2 ^ n - 1` for `n = 0` to `63`
+   maps its significant `6` bits from `0` to `63`.
+   `table` satisfies `n` -> `m = 2 ^ n - 1` -> `(m * 0x03F6EAF2CD271461) mod 2 ^ 64` -- table -> `n`. */
     return table[(uint64_t)(a * 0x03F6EAF2CD271461) >> 58];
 }
 static inline uint8_t lean_uint64_dec_eq(uint64_t a1, uint64_t a2) { return a1 == a2; }

51751a3e28ad0c6ba36fbb384a836b0360e42b13
jtristan <trjohnb@amazon.com>
2024-06-27 14:04:09 -0400

    namespace

--- src/Init/Data/Char/Extra.lean
+++ src/Init/Data/Char/Extra.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jean-Baptiste Tristan
 -/
 import Init.Data.Char.UnicodeSkipList
+import Init.Data.Char.Tables
 
 namespace Char
 

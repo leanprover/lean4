9189c894c0c2635cb047c93f7abfedee3f643134
jtristan <trjohnb@amazon.com>
2024-06-27 12:49:19 -0400

    Table regeneration

--- script/unicode/Unicode/Reference.lean
+++ script/unicode/Unicode/Reference.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jean-Baptiste Tristan
+-/
 import Unicode.Unicode
 
 def referenceTable (ucd : List UnicodeData) (property : UnicodeData → Bool) : List Nat :=

7bb77266a781943dd66d9c0a072d14caf2400905
jtristan <trjohnb@amazon.com>
2024-06-28 14:26:14 -0400

    message for generated files

--- src/Init/Data/Char/UnicodeVersion.lean
+++ src/Init/Data/Char/UnicodeVersion.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jean-Baptiste Tristan
 -/
+-- DO NOT EDIT: file generated using the scripts/unicode tool
 prelude
 import Init.Data.Nat.Basic
 

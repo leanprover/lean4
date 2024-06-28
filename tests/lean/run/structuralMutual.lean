6a8fff465d5d145526638356672de83e144f5973
Joachim Breitner <mail@joachim-breitner.de>
2024-06-28 19:40:15 +0200

    Fancy hypothetical example

--- tests/lean/run/structuralMutual.lean
+++ tests/lean/run/structuralMutual.lean
@@ -234,14 +234,42 @@ inductive Tree where
   | leaf
   | node : (Tree × Tree) → Tree
 
+def Tree.below_1 (motive : Tree → Sort u) : Tree → Sort (max 1 u) :=
+  @Tree.below motive (fun _tt => PUnit)
 
-/-- error: its type NestedWithTuple.Tree does not have a recursor -/
+-- Assume we had this construction:
+@[reducible] protected noncomputable def Tree.brecOn.{u}
+  {motive : Tree → Sort u}
+  (t : Tree)
+  (F : (t : Tree) → Tree.below_1 motive t → motive t) :
+  motive t :=
+  (@Tree.rec
+    (fun t => PProd (motive t) (Tree.below_1 motive t))
+    (fun t => PProd (PProd (motive t.1) (Tree.below_1 motive t.1))
+              (PProd (PProd (motive t.2) (Tree.below_1 motive t.2)) PUnit))
+    ⟨F Tree.leaf PUnit.unit,  PUnit.unit⟩
+    (fun a a_ih =>
+      match a with
+      | ⟨a₁, a₂⟩ =>
+        ⟨F (Tree.node ⟨a₁, a₂⟩) ⟨⟨PUnit.unit, a_ih⟩, PUnit.unit⟩,
+                               ⟨⟨PUnit.unit, a_ih⟩, PUnit.unit⟩⟩)
+    (fun _a _a_1 a_ih a_ih_1 => ⟨a_ih, ⟨a_ih_1, PUnit.unit⟩⟩)
+    t).1
+
+-- Then the decrecursifier works just fine:
+
 #guard_msgs in
 def Tree.size : Tree → Nat
   | leaf => 0
   | node (t₁, t₂) => t₁.size + t₂.size
 termination_by structurally t => t
 
+/--
+info: theorem NestedWithTuple.Tree.size.eq_2 : ∀ (t₁ t₂ : Tree), (Tree.node (t₁, t₂)).size = t₁.size + t₂.size :=
+fun t₁ t₂ => Eq.refl (Tree.node (t₁, t₂)).size
+-/
+#guard_msgs in
+#print Tree.size.eq_2
 
 
 end NestedWithTuple

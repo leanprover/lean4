ea2f2d04cd7d4bc90250d37b966ed79787b3b70a
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-06-30 13:29:48 +0100

    fix: a recursor application is stuck if the major contains metavariables

---
+++ tests/lean/run/isDefEqStuckRecursor.lean
@@ -0,0 +1,12 @@
+import Lean
+
+noncomputable def foo (n : Nat) : Nat :=
+  Decidable.rec (fun _ => 10) (fun _ => n) (Classical.propDecidable True)
+
+open Lean Meta
+
+run_meta do
+  let n ← mkFreshExprMVar (Expr.const ``Nat [])
+  let e := mkApp (.const ``foo []) n
+  withConfig (fun cfg => { cfg with isDefEqStuckEx := true }) do
+  guard !(← isDefEq e (.const ``Nat.zero []))

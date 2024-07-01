43e7cab8bdbe654841887f0a850af3d05eb2c1dc
jtristan <trjohnb@amazon.com>
2024-07-01 09:34:07 -0400

    Concrete example

--- src/Init/Data/Char/UnicodeSkipList.lean
+++ src/Init/Data/Char/UnicodeSkipList.lean
@@ -41,6 +41,12 @@ information. The 11 most significants bits are an index into the inner
 array, pointing to the start of a run. The 21 least significant bits
 point to the codepoint one past the run.
 
+As an example, consider some property that would hold for the set of
+codepoints: {100, 101, 103, 104, 105, 108, 109, 2000, 2001, 2002, 2004}
+
+The offsets would be: [100,5,2,2,0,4]
+Before packing into a 32 bit integer, the runs would be: [(0,2000),(5,1114112)]
+
 -/
 
 structure UnicodePropertyTable where

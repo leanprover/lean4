a9955942c43320565d8f458b7e58ae9093d15e20
jtristan <trjohnb@amazon.com>
2024-07-01 09:49:53 -0400

    docstring for hexadecimal functions in String

--- src/Init/Data/String/Extra.lean
+++ src/Init/Data/String/Extra.lean
@@ -229,15 +229,27 @@ where
       have k := Nat.gt_of_not_le <| mt decide_eq_true h
       exact Nat.sub_lt_sub_left k (String.lt_next _ _)
 
+/-- True if `s` is a sequence of hexadecimal digits -/
 def isNatHex (s : String) : Bool :=
   !s.isEmpty && s.all (·.isHexDigit)
 
+/-- If `s` is provably a sequence of hexadecimal digits
+  return its interpretation as a natural number
+-/
 def toNatHex (s : String) (_h : s.isNatHex) : Nat :=
   s.foldl (fun n c =>  n*16 + (if c.isDigit then c.toNat - '0'.toNat else 10 + (c.toNat - 'A'.toNat))) 0
 
+/-- If `s` is a sequence of hexadecimal digits
+  return its interpretation as a natural number
+  and none otherwise
+-/
 def toNatHex? (s : String) : Option Nat :=
   if h : s.isNatHex then s.toNatHex h else none
 
+/-- If `s` is a sequence of hexadecimal digits
+  return its interpretation as a natural number
+  otherwise, panic
+-/
 def toNatHex! (s : String) : Nat :=
   if h : s.isNatHex then s.toNatHex h else panic! "Nat in hexadecimal expected"
 

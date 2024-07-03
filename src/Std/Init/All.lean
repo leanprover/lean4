27fd0ac61b2c2fc55ce7d81f6495b320c36dd343
Markus Himmel <markus@lean-fro.org>
2024-07-03 19:37:12 +0200

    Apply build fix suggested by Sebastian

--- src/Std/Init/All.lean
+++ src/Std/Init/All.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Markus Himmel
+-/
 prelude
 import Std.Init.Array
 import Std.Init.Bif

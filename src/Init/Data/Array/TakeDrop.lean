1843909cb7de459854717980e2d80d56a4700834
Markus Himmel <markus@lean-fro.org>
2024-07-03 05:24:34 +0200

    Update src/Init/Data/Array/TakeDrop.lean

--- src/Init/Data/Array/TakeDrop.lean
+++ src/Init/Data/Array/TakeDrop.lean
@@ -9,7 +9,7 @@ import Init.Data.List.TakeDrop
 
 namespace Array
 
-theorem exists_of_update (self : Array α) (i d h) :
+theorem exists_of_uset (self : Array α) (i d h) :
     ∃ l₁ l₂, self.data = l₁ ++ self[i] :: l₂ ∧ List.length l₁ = i.toNat ∧
       (self.uset i d h).data = l₁ ++ d :: l₂ := by
   simpa [Array.getElem_eq_data_getElem] using List.exists_of_set _

f376ee689d7db2ad5dd6df46cd1fa7b3f2ac6770
Joachim Breitner <mail@joachim-breitner.de>
2024-07-03 11:32:00 +0200

    Add myself as authors a few times

--- src/Lean/Elab/PreDefinition/Structural/Basic.lean
+++ src/Lean/Elab/PreDefinition/Structural/Basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
+Authors: Leonardo de Moura, Joachim Breitner
 -/
 prelude
 import Lean.Meta.Basic

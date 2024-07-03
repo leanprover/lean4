5dddb48f203b8a7abadefa9cdc385c1ff7892fd5
Markus Himmel <markus@lean-fro.org>
2024-07-03 18:41:50 +0200

    Experiment

--- src/Std/A.lean
+++ src/Std/A.lean
@@ -1,8 +1,9 @@
 /-
 Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sebastian Ullrich
+Authors: Markus Himmel
 -/
 prelude
-import Std.A
-import Std.B
+import Init.Prelude
+
+def Std.A : Nat := 1
--- src/Std/B.lean
+++ src/Std/B.lean
@@ -1,8 +1,9 @@
 /-
 Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sebastian Ullrich
+Authors: Markus Himmel
 -/
 prelude
 import Std.A
-import Std.B
+
+def Std.B : Nat := Std.A

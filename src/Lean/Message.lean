f5fd962a25114d0be4c45fce82def56f84a46a50
Leonardo de Moura <leomoura@amazon.com>
2024-07-03 07:12:53 +0200

    feat: safe exponentiation (#4637)
    Summary:
    - Adds configuration option `exponentiation.threshold`
    - An expression `b^n` where `b` and `n` are literals is not reduced by
    `whnf`, `simp`, and `isDefEq` if `n > exponentiation.threshold`.
    
    Motivation: prevents system from becoming irresponsive and/or crashing
    without memory.
    
    TODO: improve support in the kernel. It is using a hard-coded limit for
    now.

--- src/Lean/Message.lean
+++ src/Lean/Message.lean
@@ -359,6 +359,13 @@ structure MessageLog where
   hadErrors : Bool := false
   /-- The list of messages not already reported, in insertion order. -/
   unreported : PersistentArray Message := {}
+  /--
+  Set of message kinds that have been added to the log.
+  For example, we have the kind `unsafe.exponentiation.warning` for warning messages associated with
+  the configuration option `exponentiation.threshold`.
+  We don't produce a warning if the kind is already in the following set.
+  -/
+  reportedKinds : NameSet := {}
   deriving Inhabited
 
 namespace MessageLog

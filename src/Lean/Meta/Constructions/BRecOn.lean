d8747df5bba5607932d3cdd0910aa8effa1493d4
Joachim Breitner <mail@joachim-breitner.de>
2024-07-05 10:55:14 +0200

    Also brecOn

--- src/Lean/Meta/Constructions/BRecOn.lean
+++ src/Lean/Meta/Constructions/BRecOn.lean
@@ -85,9 +85,8 @@ of type
 ```
 α → List α → Sort (max 1 u_1) → Sort (max 1 u_1)
 ```
-The parameter `typeFormers` are the `motive`s.
 -/
-private def buildBelowMinorPremise (rlvl : Level) (typeFormers : Array Expr) (minorType : Expr) : MetaM Expr :=
+private def buildBelowMinorPremise (rlvl : Level) (motives : Array Expr) (minorType : Expr) : MetaM Expr :=
   forallTelescope minorType fun minor_args _ => do go #[] minor_args.toList
 where
   ibelow := rlvl matches .zero
@@ -96,7 +95,7 @@ where
   | arg::args => do
     let argType ← inferType arg
     forallTelescope argType fun arg_args arg_type => do
-      if typeFormers.contains arg_type.getAppFn then
+      if motives.contains arg_type.getAppFn then
         let name ← arg.fvarId!.getUserName
         let type' ← forallTelescope argType fun args _ => mkForallFVars args (.sort rlvl)
         withLocalDeclD name type' fun arg' => do
@@ -143,11 +142,11 @@ private def mkBelowFromRec (recName : Name) (ibelow reflexive : Bool) (nParams :
 
   let decl ← forallTelescope refType fun refArgs _ => do
     assert! refArgs.size > nParams + recVal.numMotives + recVal.numMinors
-    let params      : Array Expr := refArgs[:nParams]
-    let typeFormers : Array Expr := refArgs[nParams:nParams + recVal.numMotives]
-    let minors      : Array Expr := refArgs[nParams + recVal.numMotives:nParams + recVal.numMotives + recVal.numMinors]
-    let indices     : Array Expr := refArgs[nParams + recVal.numMotives + recVal.numMinors:refArgs.size - 1]
-    let major       : Expr       := refArgs[refArgs.size - 1]!
+    let params  : Array Expr := refArgs[:nParams]
+    let motives : Array Expr := refArgs[nParams:nParams + recVal.numMotives]
+    let minors  : Array Expr := refArgs[nParams + recVal.numMotives:nParams + recVal.numMotives + recVal.numMinors]
+    let indices : Array Expr := refArgs[nParams + recVal.numMotives + recVal.numMinors:refArgs.size - 1]
+    let major   : Expr       := refArgs[refArgs.size - 1]!
 
     -- universe parameter names of ibelow/below
     let blvls :=
@@ -177,20 +176,20 @@ private def mkBelowFromRec (recName : Name) (ibelow reflexive : Bool) (nParams :
     -- add parameters
     val := mkAppN val params
     -- add type formers
-    for typeFormer in typeFormers do
-      let arg ← forallTelescope (← inferType typeFormer) fun targs _ =>
+    for motive in motives do
+      let arg ← forallTelescope (← inferType motive) fun targs _ =>
         mkLambdaFVars targs (.sort rlvl)
       val := .app val arg
     -- add minor premises
     for minor in minors do
-      let arg ← buildBelowMinorPremise rlvl typeFormers (← inferType minor)
+      let arg ← buildBelowMinorPremise rlvl motives (← inferType minor)
       val := .app val arg
     -- add indices and major premise
     val := mkAppN val indices
     val := mkApp val major
 
     -- All paramaters of `.rec` besides the `minors` become parameters of `.below`
-    let below_params := params ++ typeFormers ++ indices ++ #[major]
+    let below_params := params ++ motives ++ indices ++ #[major]
     let type ← mkForallFVars below_params (.sort rlvl)
     val ← mkLambdaFVars below_params val
 
@@ -225,9 +224,9 @@ private def mkBelowOrIBelow (indName : Name) (ibelow : Bool) : MetaM Unit := do
     for i in [:numNested] do
       let recName : Name := .str indName s!"rec_{i+1}"
       let belowName : Name := if ibelow then
-          .str indName s!"below_{i+1}"
-      else
           .str indName s!"ibelow_{i+1}"
+      else
+          .str indName s!"below_{i+1}"
       mkBelowFromRec recName ibelow indVal.isReflexive indVal.numParams belowName
 
 def mkBelow (declName : Name) : MetaM Unit := mkBelowOrIBelow declName true
@@ -250,22 +249,21 @@ of type
   PProd (motive tail) (List.below tail) →
   PProd (motive (head :: tail)) (PProd (PProd (motive tail) (List.below tail)) PUnit)
 ```
-The parameter `typeFormers` are the `motive`s.
 -/
-private def buildBRecOnMinorPremise (rlvl : Level) (typeFormers : Array Expr)
+private def buildBRecOnMinorPremise (rlvl : Level) (motives : Array Expr)
     (belows : Array Expr) (fs : Array Expr) (minorType : Expr) : MetaM Expr :=
   forallTelescope minorType fun minor_args minor_type => do
     let rec go (prods : Array Expr) : List Expr → MetaM Expr
       | [] => minor_type.withApp fun minor_type_fn minor_type_args => do
           let b ← mkNProdMk rlvl prods
-          let .some ⟨idx, _⟩ := typeFormers.indexOf? minor_type_fn
-            | throwError m!"Did not find {minor_type} in {typeFormers}"
+          let .some ⟨idx, _⟩ := motives.indexOf? minor_type_fn
+            | throwError m!"Did not find {minor_type} in {motives}"
           mkPProdMk (mkAppN fs[idx]! (minor_type_args.push b)) b
       | arg::args => do
         let argType ← inferType arg
         forallTelescope argType fun arg_args arg_type => do
           arg_type.withApp fun arg_type_fn arg_type_args => do
-            if let .some idx := typeFormers.indexOf? arg_type_fn then
+            if let .some idx := motives.indexOf? arg_type_fn then
               let name ← arg.fvarId!.getUserName
               let type' ← mkForallFVars arg_args
                 (← mkPProd arg_type (mkAppN belows[idx]! arg_type_args) )
@@ -308,81 +306,72 @@ fun {α} {motive} t F_1 => (
   ).1
 ```
 -/
-def mkBRecOnOrBInductionOn (indName : Name) (ind : Bool) : MetaM Unit := do
-  let .inductInfo indVal ← getConstInfo indName | return
-  unless indVal.isRec do return
-  if ← isPropFormerType indVal.type then return
-  let recName := mkRecName indName
+private def mkBRecOnFromRec (recName : Name) (ind reflexive : Bool) (nParams : Nat)
+    (all : Array Name) (brecOnName : Name) : MetaM Unit := do
   let .recInfo recVal ← getConstInfo recName | return
-  unless recVal.numMotives = indVal.all.length do
-    /-
-    The mutual declaration containing `declName` contains nested inductive datatypes.
-    We don't support this kind of declaration here yet. We probably never will :)
-    To support it, we will need to generate an auxiliary `below` for each nested inductive
-    type since their default `below` is not good here. For example, at
-    ```
-    inductive Term
-    | var : String -> Term
-    | app : String -> List Term -> Term
-    ```
-    The `List.below` is not useful since it will not allow us to recurse over the nested terms.
-    We need to generate another one using the auxiliary recursor `Term.rec_1` for `List Term`.
-    -/
-    return
-
   let lvl::lvls := recVal.levelParams.map (Level.param ·)
     | throwError "recursor {recName} has no levelParams"
   let lvlParam := recVal.levelParams.head!
   -- universe parameter names of brecOn/binductionOn
   let blps := if ind then recVal.levelParams.tail!  else recVal.levelParams
-  -- universe arguments of below/ibelow
-  let blvls := if ind then lvls else lvl::lvls
-
-  let .some ⟨idx, _⟩ := indVal.all.toArray.indexOf? indName
-    | throwError m!"Did not find {indName} in {indVal.all}"
-
-  let .some ilvl ← typeFormerTypeLevel indVal.type
-    | throwError "type {indVal.type} of inductive {indVal.name} not a type former?"
-  -- universe level of the resultant type
-  let rlvl : Level :=
-    if ind then
-      0
-    else if indVal.isReflexive then
-      if let .max 1 ilvl' := ilvl then
-        mkLevelMax' (.succ lvl) ilvl'
-      else
-        mkLevelMax' (.succ lvl) ilvl
-    else
-      mkLevelMax' 1 lvl
 
   let refType :=
     if ind then
       recVal.type.instantiateLevelParams [lvlParam] [0]
-    else if indVal.isReflexive then
+    else if reflexive then
       recVal.type.instantiateLevelParams [lvlParam] [lvl.succ]
     else
       recVal.type
 
-  let decl ← forallTelescope refType fun refArgs _ => do
-    assert! refArgs.size == indVal.numParams + recVal.numMotives + recVal.numMinors + indVal.numIndices + 1
-    let params      : Array Expr := refArgs[:indVal.numParams]
-    let typeFormers : Array Expr := refArgs[indVal.numParams:indVal.numParams + recVal.numMotives]
-    let minors      : Array Expr := refArgs[indVal.numParams + recVal.numMotives:indVal.numParams + recVal.numMotives + recVal.numMinors]
-    let remaining   : Array Expr := refArgs[indVal.numParams + recVal.numMotives + recVal.numMinors:]
+  let decl ← forallTelescope refType fun refArgs refBody => do
+    assert! refArgs.size > nParams + recVal.numMotives + recVal.numMinors
+    let params  : Array Expr := refArgs[:nParams]
+    let motives : Array Expr := refArgs[nParams:nParams + recVal.numMotives]
+    let minors  : Array Expr := refArgs[nParams + recVal.numMotives:nParams + recVal.numMotives + recVal.numMinors]
+    let indices : Array Expr := refArgs[nParams + recVal.numMotives + recVal.numMinors:refArgs.size - 1]
+    let major   : Expr       := refArgs[refArgs.size - 1]!
 
-    -- One `below` for each type former (same parameters)
-    let belows := indVal.all.toArray.map fun n =>
-      let belowName := if ind then mkIBelowName n else mkBelowName n
-      mkAppN (.const belowName blvls) (params ++ typeFormers)
+    let some idx := motives.indexOf? refBody.getAppFn
+      | throwError "result type of {refType} is not one of {motives}"
 
-    -- create types of functionals (one for each type former)
+    -- universe parameter of the type fomer.
+    -- same as `typeFormerTypeLevel indVal.type`, but we want to infer it from the
+    -- type of the recursor, to be more robust when facing nested induction
+    let majorTypeType ← inferType (← inferType major)
+    let .some ilvl ← typeFormerTypeLevel majorTypeType
+      | throwError "type of type of major premise {major} not a type former"
+
+    -- universe level of the resultant type
+    let rlvl : Level :=
+      if ind then
+        0
+      else if reflexive then
+        if let .max 1 ilvl' := ilvl then
+          mkLevelMax' (.succ lvl) ilvl'
+        else
+          mkLevelMax' (.succ lvl) ilvl
+      else
+        mkLevelMax' 1 lvl
+
+    -- One `below` for each motive, with the same motive parameters
+    let blvls := if ind then lvls else lvl::lvls
+    let belows := Array.ofFn (n := motives.size) fun ⟨i,_⟩ =>
+      let belowName :=
+        if let some n := all[i]? then
+          if ind then mkIBelowName n else mkBelowName n
+        else
+          if ind then .str all[0]! s!"ibelow_{i-all.size+1}"
+                 else .str all[0]! s!"below_{i-all.size+1}"
+      mkAppN (.const belowName blvls) (params ++ motives)
+
+    -- create types of functionals (one for each motive)
     --   (F_1 : (t : List α) → (f : List.below t) → motive t)
     -- and bring parameters of that type into scope
     let mut fDecls : Array (Name × (Array Expr -> MetaM Expr)) := #[]
-    for typeFormer in typeFormers, below in belows, i in [:typeFormers.size] do
-      let fType ← forallTelescope (← inferType typeFormer) fun targs _ => do
+    for motive in motives, below in belows, i in [:motives.size] do
+      let fType ← forallTelescope (← inferType motive) fun targs _ => do
         withLocalDeclD `f (mkAppN below targs) fun f =>
-          mkForallFVars (targs.push f) (mkAppN typeFormer targs)
+          mkForallFVars (targs.push f) (mkAppN motive targs)
       let fName := .mkSimple s!"F_{i + 1}"
       fDecls := fDecls.push (fName, fun _ => pure fType)
     withLocalDeclsD fDecls fun fs => do
@@ -390,35 +379,57 @@ def mkBRecOnOrBInductionOn (indName : Name) (ind : Bool) : MetaM Unit := do
       -- add parameters
       val := mkAppN val params
       -- add type formers
-      for typeFormer in typeFormers, below in belows do
+      for motive in motives, below in belows do
         -- example: (motive := fun t => PProd (motive t) (@List.below α motive t))
-        let arg ← forallTelescope (← inferType typeFormer) fun targs _ => do
-          let cType := mkAppN typeFormer targs
+        let arg ← forallTelescope (← inferType motive) fun targs _ => do
+          let cType := mkAppN motive targs
           let belowType := mkAppN below targs
           let arg ← mkPProd cType belowType
           mkLambdaFVars targs arg
         val := .app val arg
       -- add minor premises
       for minor in minors do
-        let arg ← buildBRecOnMinorPremise rlvl typeFormers belows fs (← inferType minor)
+        let arg ← buildBRecOnMinorPremise rlvl motives belows fs (← inferType minor)
         val := .app val arg
       -- add indices and major premise
-      val := mkAppN val remaining
+      val := mkAppN val indices
+      val := mkApp val major
       -- project out first component
       val ← mkPProdFst val
 
       -- All paramaters of `.rec` besides the `minors` become parameters of `.bRecOn`, and the `fs`
-      let below_params := params ++ typeFormers ++ remaining ++ fs
-      let type ← mkForallFVars below_params (mkAppN typeFormers[idx]! remaining)
+      let below_params := params ++ motives ++ indices ++ #[major] ++ fs
+      let type ← mkForallFVars below_params (mkAppN motives[idx]! (indices ++ #[major]))
       val ← mkLambdaFVars below_params val
 
-      let name := if ind then mkBInductionOnName indName else mkBRecOnName indName
-      mkDefinitionValInferrringUnsafe name blps type val .abbrev
+      mkDefinitionValInferrringUnsafe brecOnName blps type val .abbrev
 
   addDecl (.defnDecl decl)
   setReducibleAttribute decl.name
   modifyEnv fun env => markAuxRecursor env decl.name
   modifyEnv fun env => addProtected env decl.name
+
+def mkBRecOnOrBInductionOn (indName : Name) (ind : Bool) : MetaM Unit := do
+  let .inductInfo indVal ← getConstInfo indName | return
+  unless indVal.isRec do return
+  if ← isPropFormerType indVal.type then return
+
+  mkBRecOnFromRec (mkRecName indName) ind indVal.isReflexive indVal.numParams indVal.all.toArray
+    (if ind then mkBInductionOnName indName else mkBRecOnName indName)
+
+  -- If this is the first inductive in a mutual group with nested inductives,
+  -- generate the other ones here as well
+  if indVal.all[0]! = indName then
+    let numNested ← numNestedInducts indName
+    for i in [:numNested] do
+      let recName : Name := .str indName s!"rec_{i+1}"
+      let brecOnName : Name := if ind then
+          .str indName s!"binductionOn_{i+1}"
+      else
+          .str indName s!"brecOn_{i+1}"
+      mkBRecOnFromRec recName ind indVal.isReflexive indVal.numParams
+        indVal.all.toArray brecOnName
+
 
 def mkBRecOn (declName : Name) : MetaM Unit := mkBRecOnOrBInductionOn declName false
 def mkBInductionOn (declName : Name) : MetaM Unit := mkBRecOnOrBInductionOn declName true

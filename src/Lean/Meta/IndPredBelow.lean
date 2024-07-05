a060a1cccd0c787131c871c0e97a15e6efc9a83a
Joachim Breitner <mail@joachim-breitner.de>
2024-07-05 10:10:23 +0200

    refactor: Split Constructions module
    for better build paralleization and less rebuilding when editing one of
    these files.

--- src/Lean/Meta/IndPredBelow.lean
+++ src/Lean/Meta/IndPredBelow.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Dany Fabian
 -/
 prelude
-import Lean.Meta.Constructions
+import Lean.Meta.Constructions.CasesOn
 import Lean.Meta.Match.Match
 
 namespace Lean.Meta.IndPredBelow

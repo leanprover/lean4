1729c3449bbea2d3c3ccaead931ece55f60efa29
Joachim Breitner <mail@joachim-breitner.de>
2024-07-08 12:18:37 +0200

    Fix copyright

--- src/Lean/Elab/PreDefinition/Structural/FunPacker.lean
+++ src/Lean/Elab/PreDefinition/Structural/FunPacker.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joachim Breitner
 -/

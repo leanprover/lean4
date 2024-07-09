96620f72f45604a4bce5a533ce435cc250fe8cb6
Kim Morrison <kim@tqft.net>
2024-07-10 07:23:39 +1000

    fix tests

--- tests/lean/run/constructor_as_variable.lean
+++ tests/lean/run/constructor_as_variable.lean
@@ -100,7 +100,8 @@ def ctorSuggestion1 (pair : MyProd) : Nat :=
 /--
 error: invalid pattern, constructor or constant marked with '[match_pattern]' expected
 
-Suggestions: 'List.Sublist.below.cons', 'List.Sublist.cons', 'List.cons'
+Suggestions:
+  'List.Pairwise.below.cons', 'List.Pairwise.cons', 'List.Sublist.below.cons', 'List.Sublist.cons', 'List.cons'
 ---
 warning: Local variable 'nil' resembles constructor 'List.nil' - write '.nil' (with a dot) or 'List.nil' to use the constructor.
 note: this linter can be disabled with `set_option linter.constructorNameAsVariable false`
@@ -119,7 +120,13 @@ inductive StringList : Type where
 /--
 error: invalid pattern, constructor or constant marked with '[match_pattern]' expected
 
-Suggestions: 'List.Sublist.below.cons', 'List.Sublist.cons', 'List.cons', 'StringList.cons'
+Suggestions:
+  'List.Pairwise.below.cons',
+  'List.Pairwise.cons',
+  'List.Sublist.below.cons',
+  'List.Sublist.cons',
+  'List.cons',
+  'StringList.cons'
 ---
 warning: Local variable 'nil' resembles constructor 'List.nil' - write '.nil' (with a dot) or 'List.nil' to use the constructor.
 note: this linter can be disabled with `set_option linter.constructorNameAsVariable false`

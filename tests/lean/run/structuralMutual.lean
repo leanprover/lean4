e32f3e8140f497899190aad557379ba2e7befbfa
Joachim Breitner <mail@joachim-breitner.de>
2024-07-19 12:20:50 +0200

    refactor: IndGroupInst.brecOn (#4787)
    this logic fits nicely within `IndGroupInst`.
    
    Also makes `isAuxRecursorWithSuffix` recognize `brecOn_<n>`.

--- tests/lean/run/structuralMutual.lean
+++ tests/lean/run/structuralMutual.lean
@@ -215,6 +215,8 @@ namespace EvenOdd
 
 -- Mutual structural recursion over a non-mutual inductive type
 
+-- (The functions don't actually implement even/odd, but that isn't the point here.)
+
 mutual
   def Even : Nat → Prop
     | 0 => True
@@ -518,13 +520,13 @@ Too many possible combinations of parameters of type Nattish (or please indicate
 Could not find a decreasing measure.
 The arguments relate at each recursive call as follows:
 (<, ≤, =: relation proved, ? all proofs failed, _: no proof attempted)
-Call from ManyCombinations.f to ManyCombinations.g at 550:15-29:
+Call from ManyCombinations.f to ManyCombinations.g at 552:15-29:
    #1 #2 #3 #4
 #5  ?  ?  ?  ?
 #6  ?  =  ?  ?
 #7  ?  ?  =  ?
 #8  ?  ?  ?  =
-Call from ManyCombinations.g to ManyCombinations.f at 553:15-29:
+Call from ManyCombinations.g to ManyCombinations.f at 555:15-29:
    #5 #6 #7 #8
 #1  _  _  _  _
 #2  _  =  _  _

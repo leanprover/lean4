125644ce1c0876c7ae0b82466118c219adf7460e
Joachim Breitner <mail@joachim-breitner.de>
2024-07-22 09:43:44 +0200

    Add test for well-founded recursive instances

--- tests/lean/run/instanceAttrCheck.lean
+++ tests/lean/run/instanceAttrCheck.lean
@@ -17,3 +17,23 @@ error: type class instance expected
 -/
 #guard_msgs in
 @[instance] def fn : Foo := ⟨⟩
+
+
+-- Checks that the attribute check happens for the right function,
+-- even with mutually well-founded recursive functions
+
+class C (n m : Nat) : Type where
+
+mutual
+@[instance]
+def foo : (n m : Nat) → C n m
+  | 0, _m => C.mk
+  | n+1, m => let _ := bar n m; C.mk
+def bar : (n m : Nat) → C n m
+  | 0, _m => C.mk
+  | n+1, m => let _ := foo n m; C.mk
+end
+
+/-- info: foo 23 23 -/
+#guard_msgs in
+#synth C 23 23

98c243379ea5f5442f71ff4c2706a541ebe749d7
Michael Rothgang <rothgami@math.hu-berlin.de>
2024-07-22 23:10:14 +0200

    One tweak; add new lines.

--- src/Lean/Elab/Command.lean
+++ src/Lean/Elab/Command.lean
@@ -22,14 +22,18 @@ even outside any `section` or `namespace`, and each new pushed `Scope`
 starts as a modified copy of the previous top scope.
 -/
 structure Scope where
-  /-- The component of the `namespace` or `section` that this scope is associated to.
+  /--
+  The component of the `namespace` or `section` that this scope is associated to.
   For example, `section a.b.c` and `namespace a.b.c` each create three scopes with headers
   named `a`, `b`, and `c`.
-  This is used for checking the `end` command. The "base scope" has `""` as its header.  -/
+  This is used for checking the `end` command. The "base scope" has `""` as its header.
+  -/
   header        : String
-  /-- The current state of all set options at this point in the scope. Note that this is the
+  /--
+  The current state of all set options at this point in the scope. Note that this is the
   full current set of options and does *not* simply contain the options set
-  while this scope has been active. -/
+  while this scope has been active.
+  -/
   opts          : Options := {}
   /-- The current namespace. The top-level namespace is represented by `Name.anonymous`. -/
   currNamespace : Name := Name.anonymous
@@ -38,22 +42,27 @@ structure Scope where
   /-- The current list of names for universe level variables to use for new declarations.
   This is managed by the `universe` command. -/
   levelNames    : List Name := []
-  /-- The current list of binders to use for new declarations.
+  /--
+  The current list of binders to use for new declarations.
   This is managed by the `variable` command.
   Each binder is represented in `Syntax` form, and it is re-elaborated
   within each command that uses this information.
 
   This is also used by commands, such as `#check`, to create an initial local context,
-  even if they do not work with binders per se. -/
+  even if they do not work with binders per se.
+  -/
   varDecls      : Array (TSyntax ``Parser.Term.bracketedBinder) := #[]
-  /-- Globally unique internal identifiers for the `varDecls`.
+  /--
+  Globally unique internal identifiers for the `varDecls`.
   There is one identifier per variable introduced by the binders
   (recall that a binder such as `(a b c : Ty)` can produce more than one variable),
   and each identifier is the user-provided variable name with a macro scope.
   This is used by `TermElabM` in `Lean.Elab.Term.Context` to help with processing macros
-  that capture these variables. -/
+  that capture these variables.
+  -/
   varUIds       : Array Name := #[]
-  /-- If true (default: false), all declarations that fail to compile
+  /--
+  If true (default: false), all declarations that fail to compile
   automatically receive the `noncomputable` modifier.
   A scope with this flag set is created by `noncomputable section`.
 
@@ -650,9 +659,11 @@ Interrupt and abort exceptions are caught but not logged.
 private def liftAttrM {α} (x : AttrM α) : CommandElabM α := do
   liftCoreM x
 
-/-- Return the stack of all currently active scopes:
+/--
+Return the stack of all currently active scopes:
 the base scope always comes last; new scopes are prepended in the front.
-In particular, the current scope is always the first element. -/
+In particular, the current scope is always the first element.
+-/
 def getScopes : CommandElabM (List Scope) := do
   pure (← get).scopes
 

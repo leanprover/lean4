7d60d8b5639170dc893b07841892493bf0475af6
Joachim Breitner <mail@joachim-breitner.de>
2024-07-23 17:26:56 +0200

    feat: safer #eval, and #eval! (#4810)
    previously, `#eval` would happily evaluate expressions that contain
    `sorry`, either explicitly or because of failing tactics. In conjunction
    with operations like array access this can lead to the lean process
    crashing, which isn't particularly great.
    
    So how `#eval` will refuse to run code that (transitively) depends on
    the `sorry` axiom (using the same code as `#print axioms`).
    
    If the user really wants to run it, they can use `#eval!`.
    
    Closes #1697

---
+++ tests/lean/run/1697.lean
@@ -0,0 +1,36 @@
+
+/--
+error: tactic 'decide' proved that the proposition
+  False
+is false
+---
+error: cannot evaluate expression that depends on the `sorry` axiom.
+Use `#eval!` to evaluate nevertheless (which may cause lean to crash).
+-/
+#guard_msgs in
+#eval show Nat from False.rec (by decide)
+
+/--
+warning: declaration uses 'sorry'
+---
+error: cannot evaluate expression that depends on the `sorry` axiom.
+Use `#eval!` to evaluate nevertheless (which may cause lean to crash).
+-/
+#guard_msgs in
+#eval #[1,2,3][2]'sorry
+
+/--
+warning: declaration uses 'sorry'
+---
+info: 3
+-/
+#guard_msgs in
+#eval! #[1,2,3][2]'sorry
+
+/--
+warning: declaration uses 'sorry'
+---
+info: 3
+-/
+#guard_msgs in
+#eval! (#[1,2,3].pop)[2]'sorry

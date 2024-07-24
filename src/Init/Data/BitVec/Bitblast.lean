c4b9473c249d36db1f00b33102ce6525034624a2
Siddharth Bhat <siddu.druid@gmail.com>
2024-07-24 10:57:31 +0100

    chore: don't pass explicit params where un-necessary

--- src/Init/Data/BitVec/Bitblast.lean
+++ src/Init/Data/BitVec/Bitblast.lean
@@ -427,6 +427,6 @@ theorem shiftLeft_eq_shiftLeftRec (x : BitVec w₁) (y : BitVec w₂) :
     x <<< y = shiftLeftRec x y (w₂ - 1) := by
   rcases w₂ with rfl | w₂
   · simp [of_length_zero]
-  · simp [shiftLeftRec_eq (x := x) (y := y) (n := w₂)]
+  · simp [shiftLeftRec_eq]
 
 end BitVec

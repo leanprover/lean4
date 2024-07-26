9264505b02e3ce70dc2f4033c9e63d5caa99f100
Joachim Breitner <mail@joachim-breitner.de>
2024-07-26 11:35:15 +0200

    Add termination_by

--- tests/lean/run/1672.lean
+++ tests/lean/run/1672.lean
@@ -1,4 +1,3 @@
-set_option trace.Meta.IndPredBelow true in
 inductive TransClosure (r : α → α → Prop) : α → α → Prop
   | extends : r a b → TransClosure r a b
   | trans_left : r a b → TransClosure r b c → TransClosure r a c
@@ -6,3 +5,4 @@ inductive TransClosure (r : α → α → Prop) : α → α → Prop
 def trans' {a b c} : TransClosure r a b → TransClosure r b c → TransClosure r a c
 | .extends h₁       => .trans_left h₁
 | .trans_left h₁ h₂ => .trans_left h₁ ∘ trans' h₂
+termination_by structural t => t

83ad31622a1362e54a0f81c70d3ee9536a25c042
Joachim Breitner <mail@joachim-breitner.de>
2024-07-26 12:01:42 +0200

    No

--- src/Lean/Elab/PreDefinition/Structural/IndPred.lean
+++ src/Lean/Elab/PreDefinition/Structural/IndPred.lean
@@ -5,7 +5,6 @@ Authors: Dany Fabian
 -/
 prelude
 import Lean.Meta.IndPredBelow
-import Lean.Meta.Tactic.SolveByElim
 import Lean.Elab.PreDefinition.Basic
 import Lean.Elab.PreDefinition.Structural.Basic
 import Lean.Elab.PreDefinition.Structural.RecArgInfo
@@ -41,16 +40,6 @@ private def replaceIndPredRecApp (numFixed : Nat) (funType : Expr) (e : Expr) : 
       throwError "Could not eliminate recursive call {e}"
     instantiateMVars main
 
-def _root_.Lean.LocalDecl.eraseValue : LocalDecl → LocalDecl
-  | .ldecl idx id n t _v _nd k => .cdecl idx id n t .default k
-  | d => d
-
-def withErasedValueImpl (fvarId : FVarId) : MetaM α → MetaM α  :=
-  withReader fun ctx => { ctx with lctx := ctx.lctx.modifyLocalDecl fvarId (·.eraseValue) }
-
-def withErasedValue [Monad n] [MonadControlT MetaM n] (fvarId : FVarId) : n α → n α :=
-  mapMetaM <| withErasedValueImpl fvarId
-
 private partial def replaceIndPredRecApps (recArgInfo : RecArgInfo) (funType : Expr) (motive : Expr) (e : Expr) : M Expr := do
   let rec loop (e : Expr) : M Expr := do
     match e with
@@ -73,19 +62,7 @@ private partial def replaceIndPredRecApps (recArgInfo : RecArgInfo) (funType : E
       let processApp (e : Expr) : M Expr := do
         e.withApp fun f args => do
           if f.isConstOf recArgInfo.fnName then
-            -- let ty ← inferType e
-            let ty := mkAppN funType args[recArgInfo.numFixed:]
-            trace[Elab.definition.structural] "Recursive call {e} with inferred type {←inferType e} and expected type {ty}"
-            unless (← isDefEq ty (← inferType e)) do
-              throwError "Recursive call {e} does not have expected type {ty}"
-            let main ← mkFreshExprSyntheticOpaqueMVar ty
-            let ms ← withoutProofIrrelevance do
-              withErasedValue funType.fvarId! do
-                main.mvarId!.applyRules { backtracking := false, maxDepth := 1 } []
-            if ms.isEmpty then
-              pure main
-            else
-              throwError "could not solve using apply_assumption:{MessageData.ofGoal main.mvarId!}, remaining goals:{ms}"
+            replaceIndPredRecApp recArgInfo.numFixed funType e
           else
             return mkAppN (← loop f) (← args.mapM loop)
       match (← matchMatcherApp? e) with

2bc976f0a30e951b29abe688d3d8a2f12080ac8e
Kim Morrison <kim@tqft.net>
2024-07-30 12:56:00 +1000

    copyright header

--- src/Init/Data/List/Nat.lean
+++ src/Init/Data/List/Nat.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Lean FRO. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kim Morrison
+-/
 prelude
 import Init.Data.List.Nat.Basic
 import Init.Data.List.Nat.Range

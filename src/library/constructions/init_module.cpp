d0a62e90888fe91e28617865165e11f54126826d
Sebastian Ullrich <sebasti@nullri.ch>
2024-08-12 14:13:38 +0200

    fix: Windows needs more `LEAN_EXPORT`s

--- src/library/constructions/init_module.cpp
+++ src/library/constructions/init_module.cpp
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Leonardo de Moura
 */
+#include "library/constructions/init_module.h"
 #include "library/constructions/projection.h"
 #include "library/constructions/util.h"
 

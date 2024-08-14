abfd3be55f42e373a819c170d414486be3f253aa
Sofia Rodrigues <sofia@algebraic.dev>
2024-08-14 18:55:34 -0300

    style: add prelude import

--- src/Std/Time/Zoned/Database/TZdb.lean
+++ src/Std/Time/Zoned/Database/TZdb.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sofia Rodrigues
 -/
+prelude
 import Std.Time.Zoned.Database.Basic
 import Std.Time.Zoned.ZoneRules
 import Std.Time.Zoned.TimeZone

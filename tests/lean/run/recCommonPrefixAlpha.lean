b155daff08c322a3650e056ac50d4d664cdeaf31
Joachim Breitner <mail@joachim-breitner.de>
2024-08-14 14:48:52 +0200

    Simplify test

--- tests/lean/run/recCommonPrefixAlpha.lean
+++ tests/lean/run/recCommonPrefixAlpha.lean
@@ -15,16 +15,6 @@ end Structural
 
 namespace WF
 
--- NB: The proof goal for `f2` mentions `α`, not `β`. Could be fixed in theory if we really care
-
-/--
-info: α : Type
-head✝ : α
-xs : List α
-⊢ (invImage (fun x => PSum.casesOn x (fun a => a) fun a => a) instWellFoundedRelationOfSizeOf).1 (PSum.inl xs)
-    (PSum.inr (head✝ :: xs))
--/
-#guard_msgs in
 mutual
 def f1 (α : Type) : List α → Nat
 | [] => 0
@@ -37,7 +27,8 @@ def f2 (β : Type) : List β → Nat
 | _ :: xs => f1 β xs + 1
 termination_by n => n
 decreasing_by
-  trace_state
+  -- NB: The proof goal for `f2` mentions `α`, not `β`. Could be fixed in theory if we really care
+  guard_hyp α : Type
   decreasing_tactic
 end
 end WF

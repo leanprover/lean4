31b0106ca6a85db3295c99532b73535289506c80
Kim Morrison <kim@tqft.net>
2024-08-16 14:59:26 +1000

    some cleanup

--- src/Init/Data/List/Sort.lean
+++ src/Init/Data/List/Sort.lean
@@ -23,36 +23,35 @@ theorem splitAt_go (n : Nat) (l acc : List α) :
 theorem splitAt_eq (n : Nat) (l : List α) : splitAt n l = (l.take n, l.drop n) := by
   rw [splitAt, splitAt_go, reverse_nil, nil_append]
 
-namespace mergeSort
-
-abbrev Vec (α : Type) (n : Nat) := { l : List α // l.length = n }
-
-def split (l : Vec α n) : Vec α ((n+1)/2) × Vec α (n/2) :=
-  let r := splitAt ((n+1)/2) l.1
-  (⟨r.1, by simp [r, splitAt_eq, l.2]; omega⟩, ⟨r.2, by simp [r, splitAt_eq, l.2]; omega⟩)
-
+/--
+`O(|l| + |r|)`. Merge two lists using `le` as a switch.
+-/
 def merge (le : α → α → Bool) (l₁ l₂ : List α) : List α :=
   go l₁ l₂ []
 where go : List α → List α → List α → List α
-  | [], l₂, acc => acc.reverse ++ l₂
-  | l₁, [], acc => acc.reverse ++ l₁
+  | [], l₂, acc => reverseAux acc l₂
+  | l₁, [], acc => reverseAux acc l₁
   | x :: xs, y :: ys, acc =>
     if le x y then
       go xs (y :: ys) (x :: acc)
     else
       go (x :: xs) ys (y :: acc)
 
-end mergeSort
-
-open mergeSort
+/--
+Split a list in two equal parts. If the length is odd, the first part will be one element longer.
+-/
+def splitInTwo (l : { l : List α // l.length = n }) :
+    { l : List α // l.length = (n+1)/2 } × { l : List α // l.length = n/2 } :=
+  let r := splitAt ((n+1)/2) l.1
+  (⟨r.1, by simp [r, splitAt_eq, l.2]; omega⟩, ⟨r.2, by simp [r, splitAt_eq, l.2]; omega⟩)
 
 def mergeSort (le : α → α → Bool) (l : List α) : List α :=
   run ⟨l, rfl⟩
-where run : {n : Nat} → Vec α n → List α
+where run : {n : Nat} → { l : List α // l.length = n } → List α
   | 0, ⟨[], _⟩ => []
   | 1, ⟨[a], _⟩ => [a]
   | n+2, xs =>
-    let (l, r) := split xs
+    let (l, r) := splitInTwo xs
     merge le (run l) (run r)
 
 #eval mergeSort (· ≤ ·) [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
@@ -63,13 +62,13 @@ abbrev Sorted (r : α → α → Bool) (xs : List α) : Prop := xs.Pairwise (fun
 
 namespace mergeSort
 
-@[simp] theorem split_fst (l : Vec α n) : (split l).1 = ⟨l.1.take ((n+1)/2), by simp [split, splitAt_eq, l.2]; omega⟩ := by
-  simp [split, splitAt_eq]
+@[simp] theorem splitInTwo_fst (l : { l : List α // l.length = n }) : (splitInTwo l).1 = ⟨l.1.take ((n+1)/2), by simp [splitInTwo, splitAt_eq, l.2]; omega⟩ := by
+  simp [splitInTwo, splitAt_eq]
 
-@[simp] theorem split_snd (l : Vec α n) : (split l).2 = ⟨l.1.drop ((n+1)/2), by simp [split, splitAt_eq, l.2]; omega⟩ := by
-  simp [split, splitAt_eq]
+@[simp] theorem splitInTwo_snd (l : { l : List α // l.length = n }) : (splitInTwo l).2 = ⟨l.1.drop ((n+1)/2), by simp [splitInTwo, splitAt_eq, l.2]; omega⟩ := by
+  simp [splitInTwo, splitAt_eq]
 
-theorem split_fst_append_split_snd (l : Vec α n) : (split l).1.1 ++ (split l).2.1 = l.1 := by
+theorem splitInTwo_fst_append_splitInTwo_snd (l : { l : List α // l.length = n }) : (splitInTwo l).1.1 ++ (splitInTwo l).2.1 = l.1 := by
   simp
 
 variable {le : α → α → Bool}
@@ -90,10 +89,10 @@ def naiveMerge (lt : α → α → Bool) : List α → List α → List α
 
 theorem merge_go_eq_naiveMerge : merge.go le l₁ l₂ acc = acc.reverse ++ naiveMerge le l₁ l₂ := by
   induction l₁ generalizing l₂ acc with
-  | nil => simp [merge.go, naiveMerge]
+  | nil => simp [merge.go, naiveMerge, reverseAux_eq]
   | cons x l₁ ih₁ =>
     induction l₂ generalizing acc with
-    | nil => simp [merge.go, naiveMerge]
+    | nil => simp [merge.go, naiveMerge, reverseAux_eq]
     | cons y l₂ ih₂ =>
       simp [merge.go, naiveMerge]
       split <;> simp [ih₁, ih₂]
@@ -105,13 +104,13 @@ def naiveMergeSort (le : α → α → Bool) : List α → List α
   | [] => []
   | [a] => [a]
   | a :: b :: xs =>
-    let lr := split ⟨a :: b :: xs, rfl⟩
+    let lr := splitInTwo ⟨a :: b :: xs, rfl⟩
     have := by simpa using lr.2.2
     have := by simpa using lr.1.2
     naiveMerge le (naiveMergeSort le lr.1) (naiveMergeSort le lr.2)
 termination_by l => l.length
 
-theorem mergeSort_run_eq_naiveMergeSort : {n : Nat} → (l : Vec α n) → mergeSort.run le l = naiveMergeSort le l.1
+theorem mergeSort_run_eq_naiveMergeSort : {n : Nat} → (l : { l : List α // l.length = n }) → mergeSort.run le l = naiveMergeSort le l.1
   | 0, ⟨[], _⟩
   | 1, ⟨[a], _⟩ => by simp [run, naiveMergeSort]
   | n+2, ⟨a :: b :: l, h⟩ => by
@@ -173,25 +172,25 @@ theorem naiveMergeSort_sorted
   | [] => by simp [naiveMergeSort]
   | [a] => by simp [naiveMergeSort]
   | a :: b :: xs => by
-    have : (split ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [split_fst]; omega
-    have : (split ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [split_snd]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [splitInTwo_fst]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [splitInTwo_snd]; omega
     rw [naiveMergeSort]
     apply naiveMerge_sorted @trans @total
     apply naiveMergeSort_sorted trans total
     apply naiveMergeSort_sorted trans total
 termination_by l => l.length
 
-theorem split_fst_sorted (l : Vec α n) (h : Sorted le l) : Sorted le (split l).1 := by
-  rw [split_fst]
+theorem splitInTwo_fst_sorted (l : { l : List α // l.length = n }) (h : Sorted le l) : Sorted le (splitInTwo l).1 := by
+  rw [splitInTwo_fst]
   exact h.take
 
-theorem split_snd_sorted (l : Vec α n) (h : Sorted le l) : Sorted le (split l).2 := by
-  rw [split_snd]
+theorem splitInTwo_snd_sorted (l : { l : List α // l.length = n }) (h : Sorted le l) : Sorted le (splitInTwo l).2 := by
+  rw [splitInTwo_snd]
   exact h.drop
 
-theorem split_fst_le_split_snd {l : Vec α n} (h : Sorted le l) :
-    ∀ a b, a ∈ (split l).1.1 → b ∈ (split l).2.1 → le a b := by
-  rw [split_fst, split_snd]
+theorem splitInTwo_fst_le_splitInTwo_snd {l : { l : List α // l.length = n }} (h : Sorted le l) :
+    ∀ a b, a ∈ (splitInTwo l).1.1 → b ∈ (splitInTwo l).2.1 → le a b := by
+  rw [splitInTwo_fst, splitInTwo_snd]
   intro a b ma mb
   exact h.rel_of_mem_take_of_mem_drop ma mb
 
@@ -210,59 +209,64 @@ theorem naiveMergeSort_of_sorted : ∀ {l : List α} (_ : Sorted le l), naiveMer
   | [], _ => by simp [naiveMergeSort]
   | [a], _ => by simp [naiveMergeSort]
   | a :: b :: xs, h => by
-    have : (split ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [split_fst]; omega
-    have : (split ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [split_snd]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [splitInTwo_fst]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [splitInTwo_snd]; omega
     rw [naiveMergeSort]
-    rw [naiveMergeSort_of_sorted (split_fst_sorted ⟨a :: b :: xs, rfl⟩ h)]
-    rw [naiveMergeSort_of_sorted (split_snd_sorted ⟨a :: b :: xs, rfl⟩ h)]
-    rw [naiveMerge_of_le (split_fst_le_split_snd h)]
-    rw [split_fst_append_split_snd]
+    rw [naiveMergeSort_of_sorted (splitInTwo_fst_sorted ⟨a :: b :: xs, rfl⟩ h)]
+    rw [naiveMergeSort_of_sorted (splitInTwo_snd_sorted ⟨a :: b :: xs, rfl⟩ h)]
+    rw [naiveMerge_of_le (splitInTwo_fst_le_splitInTwo_snd h)]
+    rw [splitInTwo_fst_append_splitInTwo_snd]
 termination_by l => l.length
 
 def stable_le (le : α → α → Bool) (a b : Nat × α) : Bool :=
   if le a.2 b.2 then if le b.2 a.2 then a.1 ≤ b.1 else true else false
 
-theorem split_cons_cons_enumFrom_fst (i : Nat) (l : List α) :
-    (split ⟨(i, a) :: (i+1, b) :: l.enumFrom (i+2), rfl⟩).1.1 =
-      (split ⟨a :: b :: l, rfl⟩).1.1.enumFrom i := by
-  simp_all
+theorem splitInTwo_cons_cons_enumFrom_fst (i : Nat) (l : List α) :
+    (splitInTwo ⟨(i, a) :: (i+1, b) :: l.enumFrom (i+2), rfl⟩).1.1 =
+      (splitInTwo ⟨a :: b :: l, rfl⟩).1.1.enumFrom i := by
+  simp only [length_cons, splitInTwo_fst, enumFrom_length]
   ext1 j
-  rw [getElem?_take]
-  rw [getElem?_enumFrom]
-  rw [getElem?_cons]
-  rw [take_cons (by omega), take_cons (α := α) (by omega), enumFrom_cons]
-  congr 1
-  induction l generalizing i with
-  | nil => simp
-  | cons c l ih =>
-    simp
-    rw [take_cons (by omega), take_cons (α := α) (by omega), enumFrom_cons]
-    congr 1
-    cases l with
-    | nil => simp
-    | cons d l =>
+  rw [getElem?_take, getElem?_enumFrom, getElem?_take]
+  split
+  · rw [getElem?_cons, getElem?_cons, getElem?_cons, getElem?_cons]
+    split
+    · simp; omega
+    · split
+      · simp; omega
+      · simp only [getElem?_enumFrom]
+        congr
+        ext <;> simp; omega
+  · simp
 
-theorem split_cons_cons_enumFrom_snd (i : Nat) (l : List α) :
-    (split ⟨(i, a) :: (i+1, b) :: l.enumFrom (i+2), rfl⟩).2.1 =
-      (split ⟨a :: b :: l, rfl⟩).2.1.enumFrom (i+(l.length+3)/2) := by
-  sorry
+theorem splitInTwo_cons_cons_enumFrom_snd (i : Nat) (l : List α) :
+    (splitInTwo ⟨(i, a) :: (i+1, b) :: l.enumFrom (i+2), rfl⟩).2.1 =
+      (splitInTwo ⟨a :: b :: l, rfl⟩).2.1.enumFrom (i+(l.length+3)/2) := by
+  simp only [length_cons, splitInTwo_snd, enumFrom_length]
+  ext1 j
+  rw [getElem?_drop, getElem?_enumFrom, getElem?_drop]
+  rw [getElem?_cons, getElem?_cons, getElem?_cons, getElem?_cons]
+  split
+  · simp; omega
+  · split
+    · simp; omega
+    · simp only [getElem?_enumFrom]
+      congr
+      ext <;> simp; omega
 
-theorem naiveMerge_stable : ∀ (xs ys) (h : ∀ x y, x ∈ xs → y ∈ ys → x.1 ≤ y.1),
+theorem naiveMerge_stable : ∀ (xs ys) (_ : ∀ x y, x ∈ xs → y ∈ ys → x.1 ≤ y.1),
     (naiveMerge (stable_le le) xs ys).map (·.2) = naiveMerge le (xs.map (·.2)) (ys.map (·.2))
   | [], ys, _ => by simp [naiveMerge]
   | xs, [], _ => by simp [naiveMerge]
   | (i, x) :: xs, (j, y) :: ys, h => by
     simp only [naiveMerge, stable_le, map_cons]
     split <;> rename_i w
-    · rw [if_pos]
-      simp
-      rw [naiveMerge_stable]
-      rw [map_cons]
-      sorry
-      sorry
-    · simp
+    · rw [if_pos (by simp [h _ _ (mem_cons_self ..) (mem_cons_self ..)])]
+      simp only [map_cons, cons.injEq, true_and]
       rw [naiveMerge_stable, map_cons]
-      sorry
+      exact fun x' y' mx my => h x' y' (mem_cons_of_mem (i, x) mx) my
+    · simp only [↓reduceIte, map_cons, cons.injEq, true_and]
+      rw [naiveMerge_stable, map_cons]
+      exact fun x' y' mx my => h x' y' mx (mem_cons_of_mem (j, y) my)
 
 -- TODO: replace this with a proof via `Perm`
 @[simp] theorem mem_naiveMergeSort {a : α} {l : List α} : a ∈ naiveMergeSort le l ↔ a ∈ l := by
@@ -273,11 +277,11 @@ theorem naiveMergeSort_stable : ∀ {i : Nat} {l : List α},
   | _, []
   | _, [a] => by simp [naiveMergeSort]
   | _, a :: b :: xs => by
-    have : (split ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [split_fst]; omega
-    have : (split ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [split_snd]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).1.1.length < xs.length + 1 + 1 := by simp [splitInTwo_fst]; omega
+    have : (splitInTwo ⟨a :: b :: xs, rfl⟩).2.1.length < xs.length + 1 + 1 := by simp [splitInTwo_snd]; omega
     simp only [naiveMergeSort, enumFrom]
-    rw [split_cons_cons_enumFrom_fst]
-    rw [split_cons_cons_enumFrom_snd]
+    rw [splitInTwo_cons_cons_enumFrom_fst]
+    rw [splitInTwo_cons_cons_enumFrom_snd]
     rw [naiveMerge_stable]
     · rw [naiveMergeSort_stable, naiveMergeSort_stable]
     · simp only [mem_naiveMergeSort, Prod.forall]
@@ -289,6 +293,8 @@ theorem naiveMergeSort_stable : ∀ {i : Nat} {l : List α},
 termination_by _ l => l.length
 
 end mergeSort
+
+open mergeSort
 
 theorem mergeSort_sorted
     (trans : ∀ {a b c : α}, le a b → le b c → le a c)
@@ -302,7 +308,7 @@ theorem mergeSort_of_sorted (h : Sorted le l) : mergeSort le l = l := by
   apply naiveMergeSort_of_sorted h
 
 /--
-This merge sort algorithm is "stable",
+This merge sort algorithm is stable,
 in the sense that breaking ties in the ordering function using the position in the list
 has no effect on the output.
 

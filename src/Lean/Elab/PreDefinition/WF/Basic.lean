3be5909c6f41f2e02be7998bad72430940bf2e07
Joachim Breitner <mail@joachim-breitner.de>
2024-08-16 07:57:03 +0200

    refactor: Rename new option to debug.cleanDecreasingByGoal.get
    as suggested by @semorrison

--- src/Lean/Elab/PreDefinition/WF/Basic.lean
+++ src/Lean/Elab/PreDefinition/WF/Basic.lean
@@ -9,7 +9,7 @@ import Lean.Elab.Tactic.Basic
 
 namespace Lean.Elab.WF
 
-register_builtin_option cleanDecreasingByGoal : Bool := {
+register_builtin_option debug.cleanDecreasingByGoal : Bool := {
   defValue := true
   descr    := "Cleans up internal implementation details in the proof goals presented by \
               `decreasing_by`, using the `clean_wf` tactic. Can be disabled for debugging \
@@ -19,7 +19,7 @@ register_builtin_option cleanDecreasingByGoal : Bool := {
 open Lean Elab Tactic
 
 def applyCleanWfTactic : TacticM Unit := do
-  if cleanDecreasingByGoal.get (← getOptions) then
+  if debug.cleanDecreasingByGoal.get (← getOptions) then
     Tactic.evalTactic (← `(tactic| all_goals clean_wf))
 
 end Lean.Elab.WF

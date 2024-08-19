df75a5b743c7070a8479ac4bc8dbab0985465789
Sebastian Ullrich <sebasti@nullri.ch>
2024-08-19 09:39:09 +0200

    fix

--- src/cadical.mk
+++ src/cadical.mk
@@ -1,8 +1,7 @@
-CXX = $(CADICAL_CXX)
-CXX ?= c++
+CADICAL_CXX ?= c++
 
 %.o: src/%.cpp
-	ccache $(CXX) -std=c++11 -O3 -DNDEBUG -DNBUILD $(CXXFLAGS) -c $< -o $@
+	ccache $(CADICAL_CXX) -std=c++11 -O3 -DNDEBUG -DNBUILD $(CXXFLAGS) -c $< -o $@
 
 ../../cadical$(CMAKE_EXECUTABLE_SUFFIX): $(patsubst src/%.cpp,%.o,$(shell ls src/*.cpp | grep -v mobical))
-	$(CXX) -o $@ $^
+	$(CADICAL_CXX) -o $@ $^

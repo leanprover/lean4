87d361d9b641b859719f21a714e44a45a636de0f
Henrik Böving <hargonix@gmail.com>
2024-08-21 15:00:41 +0200

    chore: move LeanSAT logic to Std (#5113)
    As discussed for bootstrapping reasons. The only new files here are
    `Std.Tactic` and `Std.Tactic.BVDecide`. The rest is move +
    renamespacing.

--- src/Lean/Elab/Tactic/BVDecide/LRAT.lean
+++ src/Lean/Elab/Tactic/BVDecide/LRAT.lean
@@ -4,12 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Henrik Böving
 -/
 prelude
-import Lean.Elab.Tactic.BVDecide.LRAT.Actions
-import Lean.Elab.Tactic.BVDecide.LRAT.Checker
 import Lean.Elab.Tactic.BVDecide.LRAT.Trim
 import Lean.Elab.Tactic.BVDecide.LRAT.Parser
 
 /-!
-This directory contains the implementation of the LRAT certificate checking and the LRAT trimming
-algorithm.
+This directory contains the implementation of the LRAT parsing and trimming algorithms.
+They mostly live here because they used datastructures and parsing infrastructure from `Lean`.
+Otherwise they could be put into `Std.Tactic.BVDecide.LRAT`.
 -/

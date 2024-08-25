cee84286e6a6c2716aac8ddcdb552e830f760033
Kim Morrison <scott.morrison@gmail.com>
2024-08-25 14:32:45 +1000

    feat: improving confluence of List simp lemmas (#5151)
    More theorems coming shortly that are easier after these changes, but
    I'll test Mathlib on these simp changes first.

--- src/Init/Data/List/Nat/TakeDrop.lean
+++ src/Init/Data/List/Nat/TakeDrop.lean
@@ -275,7 +275,7 @@ theorem head?_drop (l : List α) (n : Nat) :
 theorem head_drop {l : List α} {n : Nat} (h : l.drop n ≠ []) :
     (l.drop n).head h = l[n]'(by simp_all) := by
   have w : n < l.length := length_lt_of_drop_ne_nil h
-  simpa [head?_eq_head, getElem?_eq_getElem, h, w] using head?_drop l n
+  simpa [getElem?_eq_getElem, h, w] using head?_drop l n
 
 theorem getLast?_drop {l : List α} : (l.drop n).getLast? = if l.length ≤ n then none else l.getLast? := by
   rw [getLast?_eq_getElem?, getElem?_drop]

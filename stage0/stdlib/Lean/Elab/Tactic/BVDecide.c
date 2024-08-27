b37df8e31a07d16dc5b6011e6446741a2a2f7b40
Henrik Böving <hargonix@gmail.com>
2024-08-27 09:33:32 +0200

    chore: update-stage0

--- stage0/stdlib/Lean/Elab/Tactic/BVDecide.c
+++ stage0/stdlib/Lean/Elab/Tactic/BVDecide.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: Lean.Elab.Tactic.BVDecide
-// Imports: Lean.Elab.Tactic.BVDecide.LRAT Lean.Elab.Tactic.BVDecide.External
+// Imports: Lean.Elab.Tactic.BVDecide.LRAT Lean.Elab.Tactic.BVDecide.External Lean.Elab.Tactic.BVDecide.Frontend
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -15,6 +15,7 @@ extern "C" {
 #endif
 lean_object* initialize_Lean_Elab_Tactic_BVDecide_LRAT(uint8_t builtin, lean_object*);
 lean_object* initialize_Lean_Elab_Tactic_BVDecide_External(uint8_t builtin, lean_object*);
+lean_object* initialize_Lean_Elab_Tactic_BVDecide_Frontend(uint8_t builtin, lean_object*);
 static bool _G_initialized = false;
 LEAN_EXPORT lean_object* initialize_Lean_Elab_Tactic_BVDecide(uint8_t builtin, lean_object* w) {
 lean_object * res;
@@ -24,6 +25,9 @@ res = initialize_Lean_Elab_Tactic_BVDecide_LRAT(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 res = initialize_Lean_Elab_Tactic_BVDecide_External(builtin, lean_io_mk_world());
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+res = initialize_Lean_Elab_Tactic_BVDecide_Frontend(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 return lean_io_result_mk_ok(lean_box(0));

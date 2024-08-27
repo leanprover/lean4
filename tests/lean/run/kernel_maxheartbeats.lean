cc86e4f4b78c8eb9a0db32f4739a33d8c17be3a0
Joachim Breitner <mail@joachim-breitner.de>
2024-08-27 12:22:59 +0200

    Fix kernel test differently

--- tests/lean/run/kernel_maxheartbeats.lean
+++ tests/lean/run/kernel_maxheartbeats.lean
@@ -1,16 +1,22 @@
 import Lean
 
-def ack : Nat → Nat → Nat
-  | 0,   y   => y+1
-  | x+1, 0   => ack x 1
-  | x+1, y+1 => ack x (ack (x+1) y)
+noncomputable def ack' (a : Nat × Nat) : Nat :=
+  let wf : WellFoundedRelation (Nat × Nat) := by exact Prod.instWellFoundedRelation
+  WellFounded.fixF
+  (fun p ih => match p with
+  | (0,   y)   => y+1
+  | (x+1, 0)   => ih (x, 1) (by decreasing_tactic)
+  | (x+1, y+1) => ih (x, ih (x+1, y) (by decreasing_tactic)) (by decreasing_tactic)
+  )
+  a
+  (wf.2.apply a)
+
+noncomputable def ack : Nat → Nat → Nat := fun a b => ack' (a,b)
 
 set_option maxHeartbeats 500
 open Lean Meta
 
-/--
-error: (kernel) deterministic timeout
--/
+/-- error: (kernel) deterministic timeout -/
 #guard_msgs in
 run_meta do
   let type ← mkEq (← mkAppM ``ack #[mkNatLit 4, mkNatLit 4]) (mkNatLit 100000)

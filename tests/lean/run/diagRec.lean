08823676e6e7d67f7c7361559e990bbaabfcd35d
Joachim Breitner <mail@joachim-breitner.de>
2024-08-27 11:57:22 +0200

    More tests

--- tests/lean/run/diagRec.lean
+++ tests/lean/run/diagRec.lean
@@ -2,19 +2,18 @@ def fib (n : Nat) :=
   match n with
   | 0 | 1 => 1
   | x+2 => fib x + fib (x+1)
-termination_by n
+-- termination_by n
 
 /--
-info: 89
+info: 377
 ---
-info: [reduction] unfolded declarations (max: 407, num: 3):
-  [reduction] Nat.rec ↦ 407
-  [reduction] Or.rec ↦ 144
-  [reduction] Acc.rec ↦ 108[reduction] unfolded reducible declarations (max: 352, num: 2):
-  [reduction] Nat.casesOn ↦ 352
-  [reduction] Or.casesOn ↦ 144use `set_option diagnostics.threshold <num>` to control threshold for reporting counters
+info: [reduction] unfolded declarations (max: 74, num: 3):
+  [reduction] Nat.rec ↦ 74
+  [reduction] HAdd.hAdd ↦ 22
+  [reduction] Add.add ↦ 12[reduction] unfolded reducible declarations (max: 49, num: 1):
+  [reduction] Nat.casesOn ↦ 49use `set_option diagnostics.threshold <num>` to control threshold for reporting counters
 -/
 #guard_msgs in
 set_option diagnostics true in
-set_option diagnostics.threshold 100 in
-#reduce fib 10
+set_option diagnostics.threshold 10 in
+#reduce fib 13

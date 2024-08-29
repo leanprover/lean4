3d9bf22bcb715c8cedea36d6e59e89fdf5e3f676
Eric Wieser <wieser.eric@gmail.com>
2024-08-29 23:30:11 +0100

    Add test docstring

--- tests/lean/run/4768.lean
+++ tests/lean/run/4768.lean
@@ -1,3 +1,9 @@
+/-!
+Test that `..` tokens do not break nearby `.`s.
+
+Note that this tests specifically for issues with `.` that are not present with `·`.
+-/
+
 example : True ∧ True := by
   constructor
   refine trivial ..

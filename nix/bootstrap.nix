d86141de04114480cde3de5d5024d249bd69c207
Sebastian Ullrich <sebasti@nullri.ch>
2024-09-02 10:09:35 +0200

    fix

--- nix/bootstrap.nix
+++ nix/bootstrap.nix
@@ -102,6 +102,7 @@ lib.warn "The Nix-based build is deprecated" rec {
         name = "LakeMain";
         roots = [{ glob = "one"; mod = "LakeMain"; }];
         executableName = "lake";
+        sharedLibName = "Lake_shared";
         deps = [ Lake ];
         linkFlags = lib.optional stdenv.isLinux "-rdynamic";
         src = src + "/src/lake";
@@ -144,8 +145,7 @@ lib.warn "The Nix-based build is deprecated" rec {
         name = "lean-${desc}";
         buildCommand = ''
           mkdir -p $out/bin $out/lib/lean
-          ln -sf ${leancpp}/lib/lean/* ${lib.concatMapStringsSep " " (l: "${l.modRoot}/* ${l.staticLib}/*") (lib.reverseList stdlib)} ${libInit_shared}/* ${leanshared_1}/* ${leanshared}/* $out/lib/lean/
-          ln -sf ${Lake.sharedLib}/* $out/lib/lean/libLake_shared.so
+          ln -sf ${leancpp}/lib/lean/* ${lib.concatMapStringsSep " " (l: "${l.modRoot}/* ${l.staticLib}/*") (lib.reverseList stdlib)} ${libInit_shared}/* ${leanshared_1}/* ${leanshared}/* ${Lake.sharedLib}/* $out/lib/lean/
           # put everything in a single final derivation so `IO.appDir` references work
           cp ${lean}/bin/lean ${leanc}/bin/leanc ${Lake-Main.executable}/bin/lake $out/bin
           # NOTE: `lndir` will not override existing `bin/leanc`

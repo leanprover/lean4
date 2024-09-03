4f95a796a169d47de152c6ef5be583583b1a9f7a
Alex Keizer <alex@keizer.dev>
2024-09-03 16:18:02 -0500

    fix `extract_congr` lemma to use primed `extractLsb'`

--- tests/lean/run/bv_extract.lean
+++ tests/lean/run/bv_extract.lean
@@ -2,14 +2,14 @@ import Std.Tactic.BVDecide
 
 open BitVec
 
-theorem bv_extract_1 : extractLsb' 32 32 (1#64) = 0#32 := by
+theorem bv_extract_1 (h : x = 1#64) : extractLsb' 32 32 x = 0#32 := by
   bv_decide
 
-theorem bv_extract_2 : extractLsb' 0 32 (1#64) = 1#32 := by
+theorem bv_extract_2 (h : x = 1#64) : extractLsb' 0 32 x = 1#32 := by
   bv_decide
 
-theorem bv_extract_3 : extractLsb 63 32 (1#64) = 0#32 := by
+theorem bv_extract_3 (h : x = 1#64) : extractLsb 63 32 x = 0#32 := by
   bv_decide
 
-theorem bv_extract_4 : extractLsb 31 0 (1#64) = 1#32 := by
+theorem bv_extract_4 (h : x = 1#64) : extractLsb 31 0 x = 1#32 := by
   bv_decide

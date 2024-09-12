a5fc22f232844f13d5377fbfcf5552e0cbfa03f4
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2024-09-12 13:07:47 +0200

    add low prio to `MonadExcept.instOrElse`

--- src/Init/Prelude.lean
+++ src/Init/Prelude.lean
@@ -3035,7 +3035,8 @@ variable {ε : Type u} {m : Type v → Type w}
 @[inline] protected def orElse [MonadExcept ε m] {α : Type v} (t₁ : m α) (t₂ : Unit → m α) : m α :=
   tryCatch t₁ fun _ => t₂ ()
 
-instance [MonadExcept ε m] {α : Type v} : OrElse (m α) where
+/- (priority := low) -/
+instance (priority := 100) [MonadExcept ε m] {α : Type v} : OrElse (m α) where
   orElse := MonadExcept.orElse
 
 end MonadExcept

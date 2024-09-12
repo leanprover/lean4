d1a8229ec4bc4168abf81dfbcd573846a6cf5e93
Kim Morrison <kim@tqft.net>
2024-09-12 21:08:14 +1000

    minor golf

--- src/Init/Data/Int/DivModLemmas.lean
+++ src/Init/Data/Int/DivModLemmas.lean
@@ -311,19 +311,16 @@ theorem ediv_nonneg_of_nonpos_of_nonpos {a b : Int} (Ha : a ≤ 0) (Hb : b ≤ 0
   | ofNat a, b =>
     match Int.le_antisymm Ha (ofNat_zero_le a) with
     | h1 =>
-    rw [h1, zero_ediv,]
-    exact Int.le_refl 0
+      rw [h1, zero_ediv]
+      exact Int.le_refl 0
   | a, ofNat b =>
     match Int.le_antisymm Hb (ofNat_zero_le  b) with
     | h1 =>
-    rw [h1, Int.ediv_zero]
-    exact Int.le_refl 0
+      rw [h1, Int.ediv_zero]
+      exact Int.le_refl 0
   | negSucc a, negSucc b =>
     rw [Int.div_def, ediv]
-    have le_succ {a: Int} : a ≤ a+1 := (le_add_one (Int.le_refl a))
-    have h2: 0 ≤ ((↑b:Int) + 1) := Int.le_trans (ofNat_zero_le b) le_succ
-    have h3: (0:Int) ≤ ↑a / (↑b + 1) := (ediv_nonneg (ofNat_zero_le a) h2)
-    exact Int.le_trans h3 le_succ
+    exact le_add_one (ediv_nonneg (ofNat_zero_le a) (Int.le_trans (ofNat_zero_le b) (le.intro 1 rfl)))
 
 theorem ediv_nonpos {a b : Int} (Ha : 0 ≤ a) (Hb : b ≤ 0) : a / b ≤ 0 :=
   Int.nonpos_of_neg_nonneg <| Int.ediv_neg .. ▸ Int.ediv_nonneg Ha (Int.neg_nonneg_of_nonpos Hb)

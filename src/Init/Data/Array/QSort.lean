8702e68576d5e27283c1d71b4a9da7b2579afbac
lyphyser <lyphyser@gmail.com>
2024-09-15 00:02:30 +0000

    make qpartition take a function and call it instead of returning a pair
    Returning a pair unfortunately result in a heap allocation, while a
    specialized function call doesn't.
    
    However, this unfortunately seems to result in code duplication,
    so the next commit will completely remove qpartition and inline it into
    qsort, which fixes that.

--- src/Init/Data/Array/QSort.lean
+++ src/Init/Data/Array/QSort.lean
@@ -9,8 +9,8 @@ import Init.Data.Nat.Mod
 
 namespace Array
 
-@[specialize] def qpartition (as : Array α) (lt : α → α → Bool) (low high : Nat)
-    (hlh: low ≤ high := by omega) (hhs: high < as.size := by omega): Nat × Array α :=
+@[specialize] def qpartition' (as : Array α) (lt : α → α → Bool) (low high : Nat)
+    (hlh: low ≤ high := by omega) (hhs: high < as.size := by omega) (f: (as': Array α) → as'.size = as.size → (p: Nat) → low ≤ p → p ≤ high → β): β :=
   let s := as.size
   have hs: as.size = s := rfl
 
@@ -34,83 +34,39 @@ namespace Array
 
   let pivot := as[high]'(hs ▸ hhs)
 
-  let rec @[specialize] loop (as : Array α) (i j : Nat) (hij: i ≤ j) (hjh: j ≤ high) (hhs: high < as.size):=
-    let s := as.size
-    have hs: as.size = s := rfl
-
+  let rec @[specialize] loop (as : Array α) (i j : Nat) (hli: low ≤ i) (hij: i ≤ j) (hjh: j ≤ high) (hhs: high < s) (hs: as.size = s):=
     have his: i < s := Nat.lt_of_le_of_lt hij (Nat.lt_of_le_of_lt hjh hhs)
 
-    if hjh : j < high then
-      have hjs: j < s := Nat.lt_trans hjh hhs
+    if hjh' : j < high then
+      have hjs: j < s := Nat.lt_trans hjh' hhs
 
       if lt (as[j]'(hs ▸ hjs)) pivot then
         let as := as.swap ⟨i, hs ▸ his⟩ ⟨j, hs ▸ hjs⟩
         have hs: as.size = s := by simp_all only [as, Array.size_swap]
 
         have hij: i + 1 ≤ j + 1 := Nat.add_le_add_right hij 1
+        have hli: low ≤ i + 1 := Nat.le_add_right_of_le hli
 
-        loop as (i+1) (j+1) hij hjh (hs ▸ hhs)
+        loop as (i+1) (j+1) hli hij hjh' (hs ▸ hhs) hs
       else
         have hij: i ≤ j + 1 := Nat.le_add_right_of_le hij
 
-        loop as i (j+1) hij hjh (hs ▸ hhs)
+        loop as i (j+1) hli hij hjh' (hs ▸ hhs) hs
     else
       let as := as.swap ⟨i, hs ▸ his⟩ ⟨high, hs ▸ hhs⟩
-      (i, as)
+      have hs: as.size = s := by simp_all only [as, Array.size_swap]
+
+      have hih: i ≤ high := Nat.le_trans hij hjh
+
+      f as hs i hli hih
 
   have hll: low ≤ low := Nat.le_refl low
 
-  loop as low low hll hlh (hs ▸ hhs)
+  loop as low low hll hll hlh hhs hs
 
-theorem i_le_fst_qpartition_loop (lt: α → α → Bool) {high: Nat} (pivot: α) {as: Array α} {i: Nat} {j: Nat}
-    (hij: i ≤ j) (hjh: j ≤ high) (hhs: high < as.size):
-    i ≤ (qpartition.loop lt high pivot as i j hij hjh hhs).1 := by
-  unfold qpartition.loop
-  dsimp only
-  split
-  · split
-    · apply Nat.le_of_succ_le
-      apply i_le_fst_qpartition_loop
-    · apply i_le_fst_qpartition_loop
-  · apply Nat.le_refl
-
-theorem fst_qpartition_loop_le_high (lt: α → α → Bool) (high: Nat) (pivot: α) (as: Array α) (i: Nat) (j: Nat)
-    (hij: i ≤ j) (hjh: j ≤ high) (hhs: high < as.size):
-    (qpartition.loop lt high pivot as i j hij hjh hhs).1 ≤ high := by
-  unfold qpartition.loop
-  dsimp only
-  split
-  · split
-    · apply fst_qpartition_loop_le_high
-    · apply fst_qpartition_loop_le_high
-  · exact Nat.le_trans hij hjh
-
-theorem size_snd_qpartition_loop (lt: α → α → Bool) {high: Nat} (pivot: α) {as: Array α} {i: Nat} {j: Nat}
-    (hij: i ≤ j) (hjh: j ≤ high) (hhs: high < as.size):
-    (qpartition.loop lt high pivot as i j hij hjh hhs).2.size = as.size := by
-  unfold qpartition.loop
-  dsimp only
-  split
-  · split
-    · rw [size_snd_qpartition_loop]
-      apply size_swap
-    · apply size_snd_qpartition_loop
-  · apply size_swap
-
-theorem low_le_fst_qpartition (as: Array α) (lt: α → α → Bool) (low high: Nat)
-    (hlh: low ≤ high) (hhs: high < as.size):
-    low ≤ (qpartition as lt low high hlh hhs).1 := by
-  apply i_le_fst_qpartition_loop
-
-theorem fst_qpartition_le_high (as: Array α) (lt: α → α → Bool) (low high: Nat)
-    (hlh: low ≤ high) (hhs: high < as.size):
-    (qpartition as lt low high hlh hhs).1 ≤ high := by
-  apply fst_qpartition_loop_le_high
-
-@[simp] theorem size_snd_qpartition (as: Array α) (lt: α → α → Bool) (low high: Nat)
-    (hlh: low ≤ high) (hhs: high < as.size):
-    (qpartition as lt low high hlh hhs).2.size = as.size := by
-  simp only [qpartition, size_snd_qpartition_loop, size_ite, size_swap, ite_self]
+def qpartition (as : Array α) (lt : α → α → Bool) (low high : Nat)
+    (hlh: low ≤ high := by omega) (hhs: high < as.size := by omega): Nat × Array α :=
+  qpartition' as lt low high hlh hhs (λ as _ p _ _ ↦ (p, as))
 
 @[inline] def qsort (as : Array α) (lt : α → α → Bool) (low := 0) (high := as.size - 1)
     (hlh: low ≤ high := by omega) (hhs: low < high → high < as.size := by omega) : Array α :=
@@ -122,40 +78,26 @@ theorem fst_qpartition_le_high (as: Array α) (lt: α → α → Bool) (low high
     if hlh': low < high then
       have hhs := hhs hlh'
 
-      let p := qpartition as lt low high hlh (hs ▸ hhs)
-      let mid := p.1
-      let as  := p.2
-      have hs: as.size = s := by
-        simp only [as, p, size_snd_qpartition, hs]
+      qpartition' as lt low high hlh (hs ▸ hhs) λ as hs' mid hlm hmh ↦
+        have hs: as.size = s := hs'
 
-      if hmh: mid >= high then ⟨as, hs⟩
-      else
-        have hms: mid < s := by
-          apply Nat.lt_of_le_of_lt ?_ hhs
-          apply fst_qpartition_le_high
+        if hmh': mid >= high then
+          ⟨as, hs⟩
+        else
+          have hms: mid < s := by
+            apply Nat.lt_of_le_of_lt ?_ hhs
+            apply hmh
 
-        have hlm: low ≤ mid := by
-          apply low_le_fst_qpartition
+          have hmh: mid + 1 ≤ high := Nat.succ_le_of_lt (Nat.gt_of_not_le hmh')
 
-        have hmh: mid + 1 ≤ high := Nat.succ_le_of_lt (Nat.gt_of_not_le hmh)
+          let ⟨as, hs'⟩ := sort as low mid hlm (λ _ ↦ hs ▸ hms)
+          have hs: as.size = s := by rw [← hs, hs']
 
-        let ⟨as, hs'⟩ := sort as low mid hlm (λ _ ↦ hs ▸ hms)
-        have hs: as.size = s := by rw [← hs, hs']
+          let ⟨as, hs'⟩ := sort as (mid+1) high hmh (λ _ ↦ hs ▸ hhs)
+          have hs: as.size = s := by rw [← hs, hs']
 
-        let ⟨as, hs'⟩ := sort as (mid+1) high hmh (λ _ ↦ hs ▸ hhs)
-        have hs: as.size = s := by rw [← hs, hs']
-
-        ⟨as, hs⟩
+          ⟨as, hs⟩
     else ⟨as, hs⟩
-  termination_by high - low
-  decreasing_by
-    · apply Nat.sub_lt_sub_right
-      · apply low_le_fst_qpartition
-      · exact hmh
-    · apply Nat.sub_lt_sub_left
-      · exact hlh'
-      · apply Nat.lt_succ_of_le
-        apply low_le_fst_qpartition
 
   (sort as low high hlh hhs).1
 

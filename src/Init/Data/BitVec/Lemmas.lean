013924653b06712ef72318bb445b83c8184ef9cf
Tobias Grosser <tobias@grosser.es>
2024-09-18 21:30:51 +0100

    Fix naming

--- src/Init/Data/BitVec/Lemmas.lean
+++ src/Init/Data/BitVec/Lemmas.lean
@@ -234,13 +234,13 @@ theorem ofBool_eq_iff_eq : ∀ {b b' : Bool}, BitVec.ofBool b = BitVec.ofBool b'
 
 @[simp] theorem not_ofBool : ~~~ (ofBool b) = ofBool (!b) := by cases b <;> rfl
 
-@[simp] theorem and_ofBool : ofBool b &&& ofBool b' = ofBool (b && b') := by
+@[simp] theorem ofBool_and_ofBool : ofBool b &&& ofBool b' = ofBool (b && b') := by
   cases b <;> cases b' <;> rfl
 
-@[simp] theorem or_ofBool : ofBool b ||| ofBool b' = ofBool (b || b') := by
+@[simp] theorem ofBool_or_ofBool : ofBool b ||| ofBool b' = ofBool (b || b') := by
   cases b <;> cases b' <;> rfl
 
-@[simp] theorem xor_ofBool : ofBool b ^^^ ofBool b' = ofBool (b ^^ b') := by
+@[simp] theorem ofBool_of_ofBool : ofBool b ^^^ ofBool b' = ofBool (b ^^ b') := by
   cases b <;> cases b' <;> rfl
 
 @[simp, bv_toNat] theorem toNat_ofFin (x : Fin (2^n)) : (BitVec.ofFin x).toNat = x.val := rfl

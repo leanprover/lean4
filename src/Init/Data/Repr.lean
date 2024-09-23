e551a366a0bbb27d5f853cc8e87cbd381a76ffc0
Kim Morrison <scott.morrison@gmail.com>
2024-09-23 15:24:03 +1000

    feat: theorems about List.toArray (#5403)

--- src/Init/Data/Repr.lean
+++ src/Init/Data/Repr.lean
@@ -163,7 +163,7 @@ protected def _root_.USize.repr (n : @& USize) : String :=
 
 /-- We statically allocate and memoize reprs for small natural numbers. -/
 private def reprArray : Array String := Id.run do
-  List.range 128 |>.map (·.toUSize.repr) |> Array.mk
+  List.range 128 |>.map (·.toUSize.repr) |> List.toArray
 
 private def reprFast (n : Nat) : String :=
   if h : n < 128 then Nat.reprArray.get ⟨n, h⟩ else

b6124039804445a0b8c921edc12533e7bbb73b89
Kim Morrison <scott.morrison@gmail.com>
2024-09-24 15:27:53 +1000

    chore: update copyrights (#5449)

--- src/Lean/Meta/DiscrTree.lean
+++ src/Lean/Meta/DiscrTree.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2019 Microsoft Corporation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura, Jannis Limperg, Scott Morrison
+Authors: Leonardo de Moura, Jannis Limperg, Kim Morrison
 -/
 prelude
 import Lean.Meta.WHNF

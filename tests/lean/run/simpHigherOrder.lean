aef7cc32a4e5b7673887f8fb602877b7eeeae8eb
Joachim Breitner <mail@joachim-breitner.de>
2024-09-26 14:59:24 +0200

    More examples

--- tests/lean/run/simpHigherOrder.lean
+++ tests/lean/run/simpHigherOrder.lean
@@ -49,3 +49,21 @@ theorem add_eq_add_cancel (a x y : Nat) : (a + x = y + a) ↔  (x = y) := by ome
 example (l : List Nat) :
   l.foldr (fun x a => a + x*x) 0 = Nat.sum (l.map (fun x => x * x)) := by
   simp [foldr_to_sum]
+
+
+-- An example with zipWith
+
+theorem zipWith_ignores_right
+  (l₁ : List α) (l₂ : List β) (f : α → β → γ) (g : α → γ)
+  (h : ∀ a b, f a b = g a) :
+  List.zipWith f l₁ l₂ = List.map g (l₁.take l₂.length) := by
+  induction l₁ generalizing l₂ with
+  | nil => simp
+  | cons x xs ih =>
+    cases l₂
+    · simp
+    · simp [List.zipWith, h, ih]
+
+example (l₁ l₂ : List Nat) (hlen: l₁.length = l₂.length):
+  List.zipWith (fun x y => x*x) l₁ l₂ = l₁.map (fun x => x * x) := by
+  simp only [zipWith_ignores_right, hlen.symm, List.take_length]

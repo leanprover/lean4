5eb6c67a78081ac4103167b554654ac1a94d5560
Mac Malone <tydeu@hatpress.net>
2024-10-01 17:18:08 -0400

    feat: lake: selective build cache fetch & display (#5572)
    Lake no longer attempts to fetch Reservoir build caches (barrels) for
    non-Reservoir dependencies, and it will only fetch them for Reservoir
    dependencies in the presence of a known toolchain.
    
    Also, optional build job failures are now only displayed in verbose
    mode.

--- src/lake/Lake/Config/Monad.lean
+++ src/lake/Lake/Config/Monad.lean
@@ -128,9 +128,13 @@ variable [MonadLakeEnv m]
 
 variable [Functor m]
 
-/-- Get the `LAKE_NO_CACHE`/`--no-cache` LAke configuration. -/
+/-- Get the `LAKE_NO_CACHE`/`--no-cache` Lake configuration. -/
 @[inline] def getNoCache [Functor m] [MonadBuild m] : m Bool :=
   (·.noCache) <$> getLakeEnv
+
+/-- Get whether the `LAKE_NO_CACHE`/`--no-cache` Lake configuration is **NOT** set. -/
+@[inline] def getTryCache [Functor m] [MonadBuild m] : m Bool :=
+  (!·.noCache) <$> getLakeEnv
 
 /-- Get the `LAKE_PACKAGE_URL_MAP` for the Lake environment. Empty if none. -/
 @[inline] def getPkgUrlMap : m (NameMap String) :=

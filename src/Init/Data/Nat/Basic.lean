a04b476431ceb10e9fcd299fa3e27a272e537fb2
Kim Morrison <scott.morrison@gmail.com>
2024-10-16 15:42:22 +1100

    chore: remove instBEqNat, which is redundant with instBEqOfDecidableEq but not defeq (#5694)

--- src/Init/Data/Nat/Basic.lean
+++ src/Init/Data/Nat/Basic.lean
@@ -131,7 +131,7 @@ theorem or_exists_add_one : p 0 ∨ (Exists fun n => p (n + 1)) ↔ Exists p :=
 @[simp] theorem blt_eq : (Nat.blt x y = true) = (x < y) := propext <| Iff.intro Nat.le_of_ble_eq_true Nat.ble_eq_true_of_le
 
 instance : LawfulBEq Nat where
-  eq_of_beq h := Nat.eq_of_beq_eq_true h
+  eq_of_beq h := by simpa using h
   rfl := by simp [BEq.beq]
 
 theorem beq_eq_true_eq (a b : Nat) : ((a == b) = true) = (a = b) := by simp

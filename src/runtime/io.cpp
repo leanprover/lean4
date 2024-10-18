400505aa61906510a4afe25467cd751503a439e0
Markus Himmel <markus@lean-fro.org>
2024-10-18 11:49:23 +0200

    Retrigger

--- src/runtime/io.cpp
+++ src/runtime/io.cpp
@@ -5,7 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Sebastian Ullrich
 */
 #if defined(LEAN_WINDOWS)
-#include <icu.h> // for reasons unknown, this must be included before windows.h
+#include <icu.h> // for reasons unknown, this must be included before windows.h.
 #include <windows.h>
 #include <io.h>
 #define NOMINMAX // prevent ntdef.h from defining min/max macros

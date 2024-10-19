a48cb1c9756fc32e2de47c5b5ab2416e9928d7e0
Mac Malone <tydeu@hatpress.net>
2024-10-18 22:04:25 -0400

    test: add stable<rc test

--- src/lake/tests/updateToolchain/test.lean
+++ src/lake/tests/updateToolchain/test.lean
@@ -15,6 +15,7 @@ def test := do
   checkParse "leanprover/lean4-pr-releases:pr-release-101"
   checkParse "leanprover/lean:v4.1.0"
   checkParse "4.12.0"
+  checkLt "4.6.0-rc1" "v4.6.0"
   checkLt "4.12.0" "leanprover/lean4:v4.13.0-rc1"
   checkLt "nightly-2024-09-08" "nightly-2024-10-09"
   checkLt "nightly-2024-09-08" "4.0.0"
@@ -26,6 +27,7 @@ Lake.ToolchainVer.nightly { year := 2024, month := 9, day := 15 }
 Lake.ToolchainVer.pr 101
 Lake.ToolchainVer.other "leanprover/lean:v4.1.0"
 Lake.ToolchainVer.release { toSemVerCore := { major := 4, minor := 12, patch := 0 }, specialDescr := "" }
+true
 true
 true
 false

55a103e20459025de8e5b8755ea188c70fa3b792
Eric Wieser <wieser.eric@gmail.com>
2024-10-22 02:33:46 +0100

    doc: stub for ellipsis notation (#5794)
    This is certainly better than no documentation, though it's not obvious
    to me whether the `_` insertion is greedy, lazy, or somewhere in
    between.

--- src/Lean/Parser/Term.lean
+++ src/Lean/Parser/Term.lean
@@ -819,6 +819,7 @@ We use them to implement `macro_rules` and `elab_rules`
 
 def namedArgument  := leading_parser (withAnonymousAntiquot := false)
   atomic ("(" >> ident >> " := ") >> withoutPosition termParser >> ")"
+/-- In a function application, `..` notation inserts zero or more `_` placeholders. -/
 def ellipsis       := leading_parser (withAnonymousAntiquot := false)
   ".." >> notFollowedBy (checkNoWsBefore >> ".") "`.` immediately after `..`"
 def argument       :=

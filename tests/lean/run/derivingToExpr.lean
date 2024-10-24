b023dac93e8018a1de476217982dda24202239f5
Alex Keizer <alex@keizer.dev>
2024-10-24 11:27:55 -0500

    feat: Upstream derive handler for `ToExpr` from Mathlib
    fix: use prelude in `ToLevel`
    
    fix: ensure `ToLevel.{u}` lives in `Type u` (rather than `Type (u+1)`)
    
    fix: ensure that `ToLevel.{u} : Type`, for all universes `u`
    
    remove old ToLevel pre-merge
    
    use Array.back!
    
    move namespace to Lean.Elab.Deriving.ToExpr
    
    This also ensures that `mkLet` is resolved to `Lean.Elab.Deriving.mkLet` (as intended) instead of `Lean.mkLet`
    
    deriving test cases
    
    The final test case currently fails, exposing a bug when auto implicits are disabled
    
    expand test to actually exercise generated instance
    
    expand tests to include ambient and directly specified universes
    
    expand tests to exercise explicitly provided universes, even when autoImplicits are enabled
    
    fix test by correctly scoping universe
    
    fix: only explicitly declare universe levels that are *not* already present in the ambient context
    
    "fix" test
    
    Turns out I had a typo in the argument list. The problem was not with implicit universes, rather it was with implicit parameters.
    Also adds new testcases to expose this bug properly

---
+++ tests/lean/run/derivingToExpr.lean
@@ -0,0 +1,109 @@
+import Lean.Elab.Deriving.ToExpr
+
+open Lean
+
+deriving instance ToExpr for ULift
+
+-- Test with empty type
+inductive Empty'
+  deriving ToExpr
+
+-- Test without universes
+inductive MonoOption' (α : Type) : Type
+  | some (a : α)
+  | none
+  deriving ToExpr
+
+-- Test with a universe polymporphic type parameter
+inductive Option' (α : Type u)
+  | some (a : α)
+  | none
+  deriving ToExpr
+
+/-- info: instToExprOption'OfToLevel -/
+#guard_msgs in #synth ToExpr (Option' Nat)
+/-- info: instToExprOption'OfToLevel -/
+#guard_msgs in #synth ToExpr (Option' <| ULift.{20} Nat)
+
+/--
+info: Lean.Expr.app
+  (Lean.Expr.app
+    (Lean.Expr.const `Option'.some [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero)))])
+    (Lean.Expr.app
+      (Lean.Expr.const `ULift [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero))), Lean.Level.zero])
+      (Lean.Expr.const `Nat [])))
+  (Lean.Expr.app
+    (Lean.Expr.app
+      (Lean.Expr.const
+        `ULift.up
+        [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero))), Lean.Level.zero])
+      (Lean.Expr.const `Nat []))
+    (Lean.Expr.app
+      (Lean.Expr.app
+        (Lean.Expr.app (Lean.Expr.const `OfNat.ofNat [Lean.Level.zero]) (Lean.Expr.const `Nat []))
+        (Lean.Expr.lit (Lean.Literal.natVal 42)))
+      (Lean.Expr.app (Lean.Expr.const `instOfNatNat []) (Lean.Expr.lit (Lean.Literal.natVal 42)))))
+-/
+#guard_msgs in #eval toExpr (Option'.some <| ULift.up.{3} 42)
+
+-- Test with recursive type
+inductive List' (α : Type u)
+  | cons (a : α) (as : List' α)
+  | nil
+  deriving ToExpr
+
+inductive WithUniverse.{u} (α : Type u)
+  | foo (a : α)
+  deriving ToExpr
+
+universe u in
+inductive WithAmbientUniverse (α : Type u)
+  | foo (a : α)
+  deriving ToExpr
+
+set_option trace.Elab.Deriving.toExpr true in
+-- A test with: an ambient universe `u`, a directly specified universe `v`, and
+-- an implicit universe `w`
+universe u in
+structure WithAmbientUniverseTriple.{v} (α : Type u) (β : Type v) (γ : Type w) where
+  a : α
+  b : β
+  c : γ
+  deriving ToExpr
+
+-- Tests without (universe) auto implicits
+section NoAutoImplicit
+set_option autoImplicit false
+
+-- Test with universe specified directly on the type
+inductive ExplicitList'.{u} (α : Type u)
+  | cons (a : α) (as : List' α)
+  | nil
+  deriving ToExpr
+
+-- Test with ambient (explicit) universe
+universe u in
+inductive AmbientList' (α : Type u)
+  | cons (a : α) (as : List' α)
+  | nil
+  deriving ToExpr
+
+-- Now, test both ambient and directly specified universes
+universe u in
+structure ExplicitAmbientPair.{v} (α : Type u) (β : Type v) where
+  a : α
+  b : β
+  deriving ToExpr
+
+end NoAutoImplicit
+
+-- Test with an implicit parameter to the inductive type
+inductive ImplicitParameter
+  | foo (a : α)
+  deriving ToExpr
+
+-- Test where the type parameter is a variable, with an implicit universe
+variable {α : Type u} in
+inductive VariableParameter : Type
+  | foo (a : α)
+  deriving ToExpr

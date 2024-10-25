6c85232a45bc237ae2a0554ac795f6fb12ee597b
Kyle Miller <kmill31415@gmail.com>
2024-10-25 11:35:05 -0700

    feat: preliminary work for parent projections in `StructureInfo`
    This adds a `parentInfo` field to the `StructureInfo`, which will eventually be populated with the actual parents of a structure. This is work toward #1881.
    
    Requires a stage0 update before the next steps.

--- src/Lean/Structure.lean
+++ src/Lean/Structure.lean
@@ -8,25 +8,61 @@ Helper functions for retrieving structure information.
 prelude
 import Lean.Environment
 import Lean.ProjFns
+import Lean.Exception
 
 namespace Lean
 
+/--
+Data for a structure field.
+These are direct fields of a structure, including "subobject" fields for the embedded parents.
+The full collection of fields is the transitive closure of fields through the subobject fields.
+-/
 structure StructureFieldInfo where
+  /-- The name of the field. This is a single-component name. -/
   fieldName  : Name
+  /-- The projection function associated to the field. -/
   projFn     : Name
-  /-- It is `some parentStructName` if it is a subobject, and `parentStructName` is the name of the parent structure -/
+  /-- If this field is for a subobject (i.e., an embedded parent structure), contains the name of the parent structure. -/
   subobject? : Option Name
+  /-- The binder info for the field from the `structure` definition. -/
   binderInfo : BinderInfo
+  /-- If set, the field is an autoparam (a field declared using `fld := by ...` syntax).
+  The expression evaluates to a tactic `Syntax` object. Generally this is an `Expr.const` expression. -/
   autoParam? : Option Expr := none
   deriving Inhabited, Repr
 
 def StructureFieldInfo.lt (i₁ i₂ : StructureFieldInfo) : Bool :=
   Name.quickLt i₁.fieldName i₂.fieldName
 
-structure StructureInfo where
+/--
+Data for a direct parent of a structure.
+Some structure parents are represented as subobjects (embedded structures),
+and for these the parent projection is a true projection.
+If a structure parent shares a field with a previous parent, it will become an implicit parent,
+and all the fields of the structure parent that do not occur in earlier parents are fields of the new structure
+-/
+structure StructureParentInfo where
+  /-- The name of the parent structure. -/
   structName : Name
-  fieldNames : Array Name := #[] -- sorted by field position in the structure
-  fieldInfo  : Array StructureFieldInfo := #[] -- sorted by `fieldName`
+  /-- Whether this parent structure is represented as a subobject. If so, then there is a `fieldInfo` entry with the same `projFn`. -/
+  subobject  : Bool
+  /-- The projection function associated to the field. For subobjects, this is the same as the `projFn` in its `fieldInfo` entry. -/
+  projFn     : Name
+  deriving Inhabited
+
+/--
+Data about a type created with the `structure` command.
+-/
+structure StructureInfo where
+  /-- The name of the structure. -/
+  structName  : Name
+  /-- The direct fields of a structure, sorted by position in the structure.
+  For example, the `s.3` notation refers to `fieldNames[3 - 1]`. -/
+  fieldNames  : Array Name := #[]
+  /-- Information about the structure fields, sorted by `fieldName`. -/
+  fieldInfo   : Array StructureFieldInfo := #[]
+  /-- (Unimplemented) Information about structure parents. These are in the order they appear in the `extends` clause. -/
+  private parentInfo  : Array StructureParentInfo := #[]
   deriving Inhabited
 
 def StructureInfo.lt (i₁ i₂ : StructureInfo) : Bool :=
@@ -50,17 +86,36 @@ builtin_initialize structureExt : SimplePersistentEnvExtension StructureInfo Str
   toArrayFn     := fun es => es.toArray.qsort StructureInfo.lt
 }
 
+/--
+A descriptor for a structure, for constructing a `StructureInfo` via `Lean.registerStructure`.
+-/
 structure StructureDescr where
-  structName : Name
-  fields     : Array StructureFieldInfo -- Should use the order the field appear in the constructor.
+  /-- The name of the structure. -/
+  structName  : Name
+  /-- The fields should be in the order that they appear in the structure's constructor. -/
+  fields      : Array StructureFieldInfo
   deriving Inhabited
 
+/--
+Declare a new structure to the elaborator.
+Every structure created by `structure` or `class` has such an entry.
+This should be followed up with `setStructureParents` and `setStructureResolutionOrder`.
+-/
 def registerStructure (env : Environment) (e : StructureDescr) : Environment :=
   structureExt.addEntry env {
     structName := e.structName
     fieldNames := e.fields.map fun e => e.fieldName
     fieldInfo  := e.fields.qsort StructureFieldInfo.lt
   }
+
+/--
+Set parent projection info for a structure defined in the current module.
+Throws an error if the structure has not already been registered with `Lean.registerStructure`.
+-/
+def setStructureParents [Monad m] [MonadEnv m] [MonadError m] (structName : Name) (parentInfo : Array StructureParentInfo) : m Unit := do
+  let some info := structureExt.getState (← getEnv) |>.map.find? structName
+    | throwError "cannot set structure parents for '{structName}', structure not defined in current module"
+  modifyEnv fun env => structureExt.addEntry env { info with parentInfo }
 
 def getStructureInfo? (env : Environment) (structName : Name) : Option StructureInfo :=
   match env.getModuleIdxFor? structName with

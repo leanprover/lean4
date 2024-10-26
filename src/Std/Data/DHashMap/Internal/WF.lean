d789318f5be0baba524aa6996bd1c8b03389686d
Markus Himmel <markus@lean-fro.org>
2024-10-26 16:25:49 +0200

    Messy proof of keys_perm_keys_toListModel

--- src/Std/Data/DHashMap/Internal/WF.lean
+++ src/Std/Data/DHashMap/Internal/WF.lean
@@ -59,6 +59,55 @@ theorem isEmpty_eq_isEmpty [BEq α] [Hashable α] {m : Raw α β} (h : Raw.WFImp
   rw [Raw.isEmpty, Bool.eq_iff_iff, List.isEmpty_iff_length_eq_zero, size_eq_length h,
     Nat.beq_eq_true_eq]
 
+-- TODO (Markus): clean up
+theorem toList_perm_toListModel {m : Raw α β} : Perm m.toList (toListModel m.buckets) := by
+  rw [Raw.toList, toListModel, List.flatMap_eq_foldl, Raw.fold, Raw.foldM,
+    Array.foldlM_eq_foldlM_toList, ← List.foldl_eq_foldlM, Id.run]
+  have h₁ : ∀ {l : AssocList α β} {acc : List (Σ a, β a)},
+      l.foldlM (m := Id) (fun acc k v => ⟨k, v⟩ :: acc) acc = l.toList.reverse ++ acc := by
+    intro l acc
+    induction l generalizing acc
+    · simp [AssocList.foldlM]
+    · simp_all [AssocList.foldlM]
+  simp only [h₁]
+  suffices ∀ (l : List (AssocList α β)) (l₁ l₂), Perm l₁ l₂ →
+      Perm (l.foldl (fun acc m => m.toList.reverse ++ acc) l₁)
+        (l.foldl (fun acc m => acc ++ m.toList) l₂) by
+    simpa using this m.buckets.toList [] [] (Perm.refl _)
+  intros l l₁ l₂ h
+  induction l generalizing l₁ l₂
+  · simpa
+  · next l t ih =>
+    simp only [foldl_cons]
+    apply ih
+    refine (reverse_perm _).append_right _ |>.trans perm_append_comm |>.trans ?_
+    exact h.append_right l.toList
+
+-- TODO (Markus): clean up
+theorem keys_perm_keys_toListModel {m : Raw α β} :
+    Perm m.keys (List.keys (toListModel m.buckets)) := by
+  rw [Raw.keys, List.keys_eq_map]
+  refine Perm.trans ?_ (toList_perm_toListModel.map _)
+  simp only [Raw.toList, Raw.fold, Raw.foldM, Array.foldlM_eq_foldlM_toList, ← List.foldl_eq_foldlM,
+    Id.run]
+  generalize m.buckets.toList = l
+  suffices ∀ l', foldl
+      (fun acc l => AssocList.foldlM (m := Id) (fun acc k x => k :: acc) acc l) (l'.map (·.1)) l =
+      map Sigma.fst (foldl
+        (fun acc l => AssocList.foldlM (m := Id) (fun acc k v => ⟨k, v⟩ :: acc) acc l) l' l) from
+    this [] ▸ Perm.refl _
+  intro l'
+  induction l generalizing l' with
+  | nil => simp
+  | cons h t ih =>
+    rw [foldl_cons, foldl_cons, ← ih]
+    congr
+    induction h generalizing l' with
+    | nil => simp [AssocList.foldlM]
+    | cons k v t' ih' =>
+      simp only [AssocList.foldlM, Id.bind_eq]
+      rw [← ih', map_cons]
+
 end Raw
 
 namespace Raw₀

983840149b502e610eaadb06aa61c7cc6cfdeaf2
Henrik Böving <hargonix@gmail.com>
2024-10-27 00:42:17 +0200

    chore: activate test

--- tests/lean/run/bv_substructure.lean
+++ tests/lean/run/bv_substructure.lean
@@ -34,3 +34,8 @@ theorem substructure_unit_6 (a b c: Bool) : (if a then b else c) = (if !a then c
 theorem substructure_unit_7 (a b c: Bool) : (bif a then b else c) = (bif !a then c else b) := by
   bv_decide
   
+theorem substructure_unit_8 (x : BitVec 32) : (if x.getLsbD 0 then !x.getLsbD 0 else x.getLsbD 0) = false := by
+  bv_decide
+
+theorem substructure_unit_9 (x y : BitVec 32) (h : x.getLsbD 0 = false) : (if x.getLsbD 0 then x else y) = y := by
+  bv_decide

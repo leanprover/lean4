032964eec11c7945870f52dbe00ed473a4d1a6df
Kyle Miller <kmill31415@gmail.com>
2024-10-28 09:59:01 -0700

    revert change, add test

--- tests/lean/run/simp-elab-recover.lean
+++ tests/lean/run/simp-elab-recover.lean
@@ -1,5 +1,5 @@
 /-!
-`simp [invaild]` makes progress which is great for interactive UX, but confusing
+`simp [invalid]` makes progress which is great for interactive UX, but confusing
 when inside a tactic combinator. This checks that without the `recover` flag,
 `simp [invalid]` fails.
 -/
@@ -18,6 +18,12 @@ n : Nat
 #guard_msgs in
 example : 0 + n = n := by
   first | simp only [does_not_exist, Nat.zero_add] | skip
+  done
+
+/-- error: unknown identifier 'does_not_exist' -/
+#guard_msgs in
+example : 0 + n = n := by
+  skip <;> simp only [does_not_exist, Nat.zero_add]
   done
 
 /-- error: unknown identifier 'does_not_exist' -/

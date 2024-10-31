92b4402e8c9a182d260b9135c2418aa9bb3a621a
Alex Keizer <alex@keizer.dev>
2024-10-31 14:28:43 -0500

    fix: use prelude in `ToLevel`

--- src/Lean/ToLevel.lean
+++ src/Lean/ToLevel.lean
@@ -3,7 +3,8 @@ Copyright (c) 2023 Kyle Miller. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
-import Lean
+prelude
+import Lean.Expr
 
 /-! # `ToLevel` class
 

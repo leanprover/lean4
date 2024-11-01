4a87ad35bea13442a6ebbecdc2577a570cc08939
Kim Morrison <kim@tqft.net>
2024-11-01 13:07:52 +1100

    prelude

--- src/Lean/SimpLC.lean
+++ src/Lean/SimpLC.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joachim Breitner
 -/
+prelude
 import Lean.SimpLC.Setup
 import Lean.SimpLC.MVarCycles
 import Lean.Elab.Tactic.Meta

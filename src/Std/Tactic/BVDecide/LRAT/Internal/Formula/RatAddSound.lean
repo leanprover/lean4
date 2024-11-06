910b20fb2ccb9e0316db7c7b5ea95324379276f8
Kim Morrison <scott.morrison@gmail.com>
2024-11-06 16:12:25 +1100

    chore: consolidate `decide_True` and `decide_true_eq_true` (#5949)

--- src/Std/Tactic/BVDecide/LRAT/Internal/Formula/RatAddSound.lean
+++ src/Std/Tactic/BVDecide/LRAT/Internal/Formula/RatAddSound.lean
@@ -306,7 +306,7 @@ theorem sat_of_insertRat {n : Nat} (f : DefaultFormula n)
           simp +decide only [hasAssignment, hasPosAssignment, heq]
         have p_entails_i := hf.2.2 i false hasNegAssignment_fi p pf
         simp only [(· ⊨ ·)] at p_entails_i
-        simp only [p_entails_i, decide_True]
+        simp only [p_entails_i, decide_true]
       · next heq =>
         exfalso
         rw [heq] at h3

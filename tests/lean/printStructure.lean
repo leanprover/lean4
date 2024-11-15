28ba0138324d59f81a9f20e00c58e4c5ccfa11c0
Kyle Miller <kmill31415@gmail.com>
2024-11-15 15:34:53 -0800

    feat: have `#print` show precise fields of structures
    This PR improves the `#print` command for structures to show all fields and which parents they were inherited from, hiding internal details like which parents are represented as subobjects. This information is still present in the construtor if needed.
    
    Example output for `#print Monad`:
    ```
    class Monad.{u, v} (m : Type u → Type v) : Type (max (u + 1) v)
    number of parameters: 1
    parents:
      Monad.toApplicative : Applicative m
      Monad.toBind : Bind m
    fields:
      Functor.map : {α β : Type u} → (α → β) → m α → m β
      Functor.mapConst : {α β : Type u} → α → m β → m α
      Pure.pure : {α : Type u} → α → m α
      Seq.seq : {α β : Type u} → m (α → β) → (Unit → m α) → m β
      SeqLeft.seqLeft : {α β : Type u} → m α → (Unit → m β) → m α
      SeqRight.seqRight : {α β : Type u} → m α → (Unit → m β) → m β
      Bind.bind : {α β : Type u} → m α → (α → m β) → m β
    constructor:
      Monad.mk : {m : Type u → Type v} → [toApplicative : Applicative m] → [toBind : Bind m] → Monad m
    resolution order:
      Monad, Applicative, Bind, Functor, Pure, Seq, SeqLeft, SeqRight
    ```
    
    Suggested by Floris van Doorn [on Zulip](https://leanprover.zulipchat.com/#narrow/channel/287929-mathlib4/topic/doc-gen.20ClosureOperator.20issue/near/482503637).

--- tests/lean/printStructure.lean
+++ tests/lean/printStructure.lean
@@ -4,70 +4,88 @@
 
 /- Structure -/
 /--
-info: structure Prod.{u, v} : Type u → Type v → Type (max u v)
+info: structure Prod.{u, v} (α : Type u) (β : Type v) : Type (max u v)
 number of parameters: 2
-constructor:
-Prod.mk : {α : Type u} → {β : Type v} → α → β → α × β
 fields:
-fst : α
-snd : β
+  Prod.fst : α
+  Prod.snd : β
+constructor:
+  Prod.mk : {α : Type u} → {β : Type v} → α → β → α × β
 -/
 #guard_msgs in
 #print Prod
 
 /- Class -/
 /--
-info: class Inhabited.{u} : Sort u → Sort (max 1 u)
+info: class Inhabited.{u} (α : Sort u) : Sort (max 1 u)
 number of parameters: 1
-constructor:
-Inhabited.mk : {α : Sort u} → α → Inhabited α
 fields:
-default : α
+  Inhabited.default : α
+constructor:
+  Inhabited.mk : {α : Sort u} → α → Inhabited α
 -/
 #guard_msgs in
 #print Inhabited
 
 /- Structure with private field -/
 /--
-info: structure Thunk.{u} : Type u → Type u
+info: structure Thunk.{u} (α : Type u) : Type u
 number of parameters: 1
-constructor:
-Thunk.mk : {α : Type u} → (Unit → α) → Thunk α
 fields:
-private fn : Unit → α
+  private _root_.Thunk.fn : Unit → α
+constructor:
+  Thunk.mk : {α : Type u} → (Unit → α) → Thunk α
 -/
 #guard_msgs in
 #print Thunk
 
 /- Extended class -/
 /--
-info: class Alternative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
+info: class Alternative.{u, v} (f : Type u → Type v) : Type (max (u + 1) v)
 number of parameters: 1
-constructor:
-Alternative.mk : {f : Type u → Type v} →
-  [toApplicative : Applicative f] → ({α : Type u} → f α) → ({α : Type u} → f α → (Unit → f α) → f α) → Alternative f
+parents:
+  Alternative.toApplicative : Applicative f
 fields:
-toApplicative : Applicative f
-failure : {α : Type u} → f α
-orElse : {α : Type u} → f α → (Unit → f α) → f α
+  Functor.map : {α β : Type u} → (α → β) → f α → f β
+  Functor.mapConst : {α β : Type u} → α → f β → f α
+  Pure.pure : {α : Type u} → α → f α
+  Seq.seq : {α β : Type u} → f (α → β) → (Unit → f α) → f β
+  SeqLeft.seqLeft : {α β : Type u} → f α → (Unit → f β) → f α
+  SeqRight.seqRight : {α β : Type u} → f α → (Unit → f β) → f β
+  Alternative.failure : {α : Type u} → f α
+  Alternative.orElse : {α : Type u} → f α → (Unit → f α) → f α
+constructor:
+  Alternative.mk : {f : Type u → Type v} →
+    [toApplicative : Applicative f] → ({α : Type u} → f α) → ({α : Type u} → f α → (Unit → f α) → f α) → Alternative f
+resolution order:
+  Alternative, Applicative, Functor, Pure, Seq, SeqLeft, SeqRight
 -/
 #guard_msgs in
 #print Alternative
 
 /- Multiply extended class -/
 /--
-info: class Applicative.{u, v} : (Type u → Type v) → Type (max (u + 1) v)
+info: class Applicative.{u, v} (f : Type u → Type v) : Type (max (u + 1) v)
 number of parameters: 1
-constructor:
-Applicative.mk : {f : Type u → Type v} →
-  [toFunctor : Functor f] →
-    [toPure : Pure f] → [toSeq : Seq f] → [toSeqLeft : SeqLeft f] → [toSeqRight : SeqRight f] → Applicative f
+parents:
+  Applicative.toFunctor : Functor f
+  Applicative.toPure : Pure f
+  Applicative.toSeq : Seq f
+  Applicative.toSeqLeft : SeqLeft f
+  Applicative.toSeqRight : SeqRight f
 fields:
-toFunctor : Functor f
-toPure : Pure f
-toSeq : Seq f
-toSeqLeft : SeqLeft f
-toSeqRight : SeqRight f
+  Functor.map : {α β : Type u} → (α → β) → f α → f β
+  Functor.mapConst : {α β : Type u} → α → f β → f α
+  Pure.pure : {α : Type u} → α → f α
+  Seq.seq : {α β : Type u} → f (α → β) → (Unit → f α) → f β
+  SeqLeft.seqLeft : {α β : Type u} → f α → (Unit → f β) → f α
+  SeqRight.seqRight : {α β : Type u} → f α → (Unit → f β) → f β
+constructor:
+  Applicative.mk : {f : Type u → Type v} →
+    [toFunctor : Functor f] →
+      [toPure : Pure f] → [toSeq : Seq f] → [toSeqLeft : SeqLeft f] → [toSeqRight : SeqRight f] → Applicative f
+resolution order:
+  Applicative, Functor, Pure, Seq, SeqLeft, SeqRight
 -/
 #guard_msgs in
 #print Applicative
@@ -78,12 +96,12 @@ structure Weird (α β : Type _) where
   a : α
 
 /--
-info: structure Weird.{u_1, u_2} : Type u_1 → Type u_2 → Type u_1
+info: structure Weird.{u_1, u_2} (α : Type u_1) (β : Type u_2) : Type u_1
 number of parameters: 2
-constructor:
-Weird.mk : {α : Type u_1} → {β : Type u_2} → α → Weird α β
 fields:
-a : α
+  Weird.a : α
+constructor:
+  Weird.mk : {α : Type u_1} → {β : Type u_2} → α → Weird α β
 -/
 #guard_msgs in
 #print Weird

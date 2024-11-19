bdf971d3c20b1b116febfd3f7a82931d0dcc49af
Kyle Miller <kmill31415@gmail.com>
2024-11-19 12:24:29 -0800

    add test

--- tests/lean/run/level.lean
+++ tests/lean/run/level.lean
@@ -2,10 +2,8 @@ import Lean.Level
 
 open Lean
 
-#eval
-  assert! levelZero == levelZero
-  assert! !(levelZero == mkLevelSucc levelZero)
-  assert! !(mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelSucc levelZero)
-  assert! mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelMax (mkLevelSucc levelZero) levelZero
-  assert! Level.geq (.max (.param `u) (.param `v)) (.imax (.param `u) (.param `v))
-  true
+#guard levelZero == levelZero
+#guard levelZero != mkLevelSucc levelZero
+#guard mkLevelMax (mkLevelSucc levelZero) levelZero != mkLevelSucc levelZero
+#guard mkLevelMax (mkLevelSucc levelZero) levelZero == mkLevelMax (mkLevelSucc levelZero) levelZero
+#guard Level.geq (.max (.param `u) (.param `v)) (.imax (.param `u) (.param `v))

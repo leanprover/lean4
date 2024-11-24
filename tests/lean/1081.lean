a2be4e54113acfc47c0c69c723748874569136d0
Kim Morrison <scott.morrison@gmail.com>
2024-11-25 10:01:32 +1100

    feat: upstream definition of Vector from Batteries (#6197)
    This PR upstreams the definition of `Vector` from Batteries, along with
    the basic functions.

--- tests/lean/1081.lean
+++ tests/lean/1081.lean
@@ -10,28 +10,28 @@ example : f 0 0 = 0 := rfl
 
 example : f 0 (y+1) = y+1 := rfl
 
-inductive Vector (α : Type u) : Nat → Type u where
-  | nil  : Vector α 0
-  | cons : α → Vector α n → Vector α (n+1)
+inductive Vector' (α : Type u) : Nat → Type u where
+  | nil  : Vector' α 0
+  | cons : α → Vector' α n → Vector' α (n+1)
 
-namespace Vector
-  def insert (a: α): Fin (n+1) → Vector α n → Vector α (n+1)
+namespace Vector'
+  def insert (a: α): Fin (n+1) → Vector' α n → Vector' α (n+1)
   | ⟨0  , _⟩,        xs => cons a   xs
   | ⟨i+1, h⟩, cons x xs => cons x $ xs.insert a ⟨i, Nat.lt_of_succ_lt_succ h⟩
 
-  theorem insert_at_0_eq_cons1 (a: α) (v: Vector α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v :=
+  theorem insert_at_0_eq_cons1 (a: α) (v: Vector' α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v :=
     rfl -- Error, it does not hold by reflexivity because the recursion is on v
 
   example (a : α) :  nil.insert a ⟨0, by simp_arith⟩ = cons a nil :=
     rfl
 
-  example (a : α) (b : α) (bs : Vector α n) :  (cons b bs).insert a ⟨0, by simp_arith⟩ = cons a (cons b bs) :=
+  example (a : α) (b : α) (bs : Vector' α n) :  (cons b bs).insert a ⟨0, by simp_arith⟩ = cons a (cons b bs) :=
     rfl
 
-  theorem insert_at_0_eq_cons2 (a: α) (v: Vector α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v := by
+  theorem insert_at_0_eq_cons2 (a: α) (v: Vector' α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v := by
     rw [insert]
 
-  theorem insert_at_0_eq_cons3 (a: α) (v: Vector α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v := by
+  theorem insert_at_0_eq_cons3 (a: α) (v: Vector' α n):  v.insert a ⟨0, Nat.zero_lt_succ n⟩ = cons a v := by
     simp only [insert]
 
-end Vector
+end Vector'
--- tests/lean/run/1024.lean
+++ tests/lean/run/1024.lean
@@ -1,17 +1,17 @@
-inductive Vector (α : Type u): Nat → Type u where
-| nil : Vector α 0
-| cons (head : α) (tail : Vector α n) : Vector α (n+1)
+inductive Vector' (α : Type u): Nat → Type u where
+| nil : Vector' α 0
+| cons (head : α) (tail : Vector' α n) : Vector' α (n+1)
 
-namespace Vector
-  def nth : ∀{n}, Vector α n → Fin n → α
+namespace Vector'
+  def nth : ∀{n}, Vector' α n → Fin n → α
   | n+1, cons x xs, ⟨  0, _⟩ => x
   | n+1, cons x xs, ⟨k+1, h⟩ => xs.nth ⟨k, sorry⟩
 
-  def snoc : ∀{n : Nat} (xs : Vector α n) (x : α), Vector α (n+1)
+  def snoc : ∀{n : Nat} (xs : Vector' α n) (x : α), Vector' α (n+1)
   | _, nil,    x' => cons x' nil
   | _, cons x xs, x' => cons x (snoc xs x')
 
-  theorem nth_snoc_eq (k: Fin (n+1))(v : Vector α n)
+  theorem nth_snoc_eq (k: Fin (n+1))(v : Vector' α n)
     (h: k.val = n):
     (v.snoc x).nth k = x := by
     cases k; rename_i k hk
@@ -19,11 +19,11 @@ namespace Vector
     · simp only [nth]
     · simp! [*]
 
-  theorem nth_snoc_eq_works (k: Fin (n+1))(v : Vector α n)
+  theorem nth_snoc_eq_works (k: Fin (n+1))(v : Vector' α n)
     (h: k.val = n):
     (v.snoc x).nth k = x := by
     cases k; rename_i k hk
     induction v generalizing k <;> subst h
     · simp only [nth]
     · simp[*,nth]
-end Vector
+end Vector'

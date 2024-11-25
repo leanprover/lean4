474e3365a88cb9c5003344145525460fe72e3c0e
Mac Malone <tydeu@hatpress.net>
2024-11-25 17:10:43 -0500

    chore: add environment plugin test

--- tests/pkg/user_plugin/test.sh
+++ tests/pkg/user_plugin/test.sh
@@ -17,26 +17,42 @@ fi
 ./clean.sh
 lake update -q
 
-# Build plugin
+# Build plugins
 lake build
 LIB_DIR=.lake/build/lib
-SHLIB=$LIB_DIR/${LIB_PREFIX}UserPlugin.$SHLIB_EXT
-test -f $SHLIB || {
-  echo "Plugin library not found; $LIB_DIR contains:"
-  ls $LIB_DIR
-  exit 1
+check_plugin () {
+  plugin=$1
+  shlib=$LIB_DIR/${LIB_PREFIX}$plugin.$SHLIB_EXT
+  test -f $shlib || {
+    echo "$plugin library not found; $LIB_DIR contains:"
+    ls $LIB_DIR
+    exit 1
+  }
+  mv $shlib $LIB_DIR/$plugin.$SHLIB_EXT
 }
+check_plugin UserPlugin
+check_plugin UserEnvPlugin
 PLUGIN=$LIB_DIR/UserPlugin.$SHLIB_EXT
-mv $SHLIB $PLUGIN
+ENV_PLUGIN=$LIB_DIR/UserEnvPlugin.$SHLIB_EXT
 
 # Expected test output
 EXPECTED_OUT="Ran builtin initializer"
+ENV_EXPECTED_OUT="Builtin value"
+EXPECTED_ERR="uncaught exception: failed to register environment, extensions can only be registered during initialization"
 
-# Test plugin via `lean`
+# Test plugins at elaboration-time via `lean`
 echo | lean --plugin=$PLUGIN --stdin | diff <(echo "$EXPECTED_OUT") -
+lake env lean --plugin=$ENV_PLUGIN testEnvUse.lean | diff <(echo "$ENV_EXPECTED_OUT") -
 
-# Test plugin via `Lean.loadPlugin`
+# Test plugins at runtime via `Lean.loadPlugin`
 lean --run test.lean $PLUGIN | diff <(echo "$EXPECTED_OUT") -
+lake env lean --run testEnv.lean $ENV_PLUGIN | diff <(echo "$ENV_EXPECTED_OUT") -
+
+# Test failure to load environment plugin without `withImporting`
+lean --run test.lean $ENV_PLUGIN 2>&1 && {
+  echo "Loading environment plugin without importing succeeded unexpectedly."
+  exit 1
+} || true
 
 # Print success
 echo "Tests completed successfully."

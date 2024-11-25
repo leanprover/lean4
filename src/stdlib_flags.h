0f8f040e6d5f20108aa79fdc462db117390e764e
Sebastian Ullrich <sebasti@nullri.ch>
2024-11-25 16:49:30 +0100

    temp: reset default again

--- src/stdlib_flags.h
+++ src/stdlib_flags.h
@@ -9,7 +9,7 @@ options get_default_options() {
     opts = opts.update({"interpreter", "prefer_native"}, false);
     // switch to `false` when enabling `prefer_native` should also affect use
     // of built-in parsers in quotations
-    opts = opts.update({"internal", "parseQuotWithCurrentStage"}, true);
+    opts = opts.update({"internal", "parseQuotWithCurrentStage"}, false);
     // changes to built-in parsers may also require toggling the following option if macros/syntax
     // with custom precheck hooks were affected
     opts = opts.update({"quotPrecheck"}, true);

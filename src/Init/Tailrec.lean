366a43f43a335982b626bcbb3b910d57f94b856e
Joachim Breitner <mail@joachim-breitner.de>
2024-11-25 17:05:28 +0100

    Use type classes, fun_order

--- src/Init/Tailrec.lean
+++ src/Init/Tailrec.lean
@@ -6,72 +6,61 @@ Authors: Joachim Breitner
 
 namespace Lean.Tailrec
 
-universe u
+universe u v
 
--- section utils
+class Order (α : Type u) where
+  /-- The less-defined than relation -/
+  rel : α → α → Prop
+  rel_refl : ∀ {x}, rel x x
+  rel_trans : ∀ {x y z}, rel x y → rel y z → rel x z
+  rel_antisymm : ∀ {x y}, rel x y → rel y x → x = y
 
--- open Nat
+@[inherit_doc] infix:50 " ⊑ "  => Order.rel
 
+section Order
 
--- theorem le_succ_iff {m n : Nat} : m ≤ n.succ ↔ m ≤ n ∨ m = n.succ := by
---   refine ⟨fun hmn ↦ (Nat.lt_or_eq_of_le hmn).imp_left le_of_lt_succ, ?_⟩
---   rintro (hmn | rfl)
---   · exact le_succ_of_le hmn
---   · exact Nat.le_refl _
+variable {α  : Type u} [Order α]
 
--- @[elab_as_elim]
--- def leRec {n} {motive : (m : Nat) → n ≤ m → Sort _}
---     (refl : motive n (Nat.le_refl _))
---     (le_succ_of_le : ∀ ⦃k⦄ (h : n ≤ k), motive k h → motive (k + 1) (le_succ_of_le h)) :
---     ∀ {m} (h : n ≤ m), motive m h
---   | 0, H => Nat.eq_zero_of_le_zero H ▸ refl
---   | m + 1, H =>
---     (le_succ_iff.1 H).by_cases
---       (fun h : n ≤ m ↦ le_succ_of_le h <| leRec refl le_succ_of_le h)
---       (fun h : n = m + 1 ↦ h ▸ refl)
+def monotone (f : α → α) : Prop := ∀ x y, x ⊑ y → f x ⊑ f y
 
--- end utils
+def chain (c : α → Prop) : Prop := ∀ x y , c x → c y → x ⊑ y ∨ y ⊑ x
 
-section fixpoint
+end Order
 
-variable {α : Type u}
+class CCPO (α : Type u) [Order α] where
+  csup : (α → Prop) → α
+  csup_spec {c : α → Prop} (hc : chain c) : csup c ⊑ x ↔ (∀ y, c y → y ⊑ x)
 
-variable (rel : α → α → Prop)
-variable (rel_refl : ∀ {x}, rel x x)
-variable (rel_trans : ∀ {x y z}, rel x y → rel y z → rel x z)
-variable (rel_antisymm : ∀ {x y}, rel x y → rel y x → x = y)
+section CCPO
 
-def monotone (f : α → α) : Prop := ∀ x y, rel x y → rel (f x) (f y)
+open Order CCPO
 
-def chain (c : α → Prop) : Prop := ∀ x y , c x → c y → rel x y ∨ rel y x
+variable {α  : Type u} [Order α] [CCPO α]
 
-variable (csup : (α → Prop) → α)
+variable {c : α → Prop} (hchain : chain c)
 
-variable (csup_spec : ∀ {c} (_ : chain rel c) {x : α},
-  rel (csup c) x ↔ (∀ y, c y → rel y x))
-
-include csup_spec in
-theorem csup_le (hchain : chain rel c) : (∀ y, c y → rel y x) → rel (csup c) x :=
+include hchain in
+theorem csup_le : (∀ y, c y → y ⊑ x) → csup c ⊑ x :=
   (csup_spec hchain).mpr
 
-include rel_refl csup_spec in
-theorem le_csup (hchain : chain rel c) {y} (hy : c y) : rel y (csup c) :=
+include hchain in
+theorem le_csup {y} (hy : c y) : y ⊑ csup c :=
   (csup_spec hchain).mp rel_refl y hy
 
 inductive iterates (f : α → α) : α → Prop where
   | step : iterates f x → iterates f (f x)
-  | sup : chain rel c → (∀ x, c x → iterates f x) → iterates f (csup c)
+  | sup {c : α → Prop } (hc : chain c) (hi : ∀ x, c x → iterates f x) : iterates f (csup c)
 
-def bot := csup (fun _ => False)
+def bot : α := csup (fun _ => False)
 
-include csup_spec in
-theorem bot_le : rel (bot csup) x := by
-  apply csup_le rel csup csup_spec
+notation "⊥" => bot
+
+theorem bot_le (x : α) : ⊥ ⊑ x := by
+  apply csup_le
   · intro x y hx hy; contradiction
   · intro x hx; contradiction
 
-include rel_refl rel_trans csup_spec in
-theorem chain_iterates {f : α → α} (hf : monotone rel f) : chain rel (iterates rel csup f) := by
+theorem chain_iterates {f : α → α} (hf : monotone f) : chain (iterates f) := by
   intros x y hx hy
   induction hx generalizing y
   case step x hx ih =>
@@ -81,14 +70,14 @@ theorem chain_iterates {f : α → α} (hf : monotone rel f) : chain rel (iterat
       · left; apply hf; assumption
       · right; apply hf; assumption
     case sup c hchain hi ih2 =>
-      show rel (f x) (csup c) ∨ rel (csup c) (f x)
-      by_cases h : ∃ z, c z ∧ rel (f x) z
+      show f x ⊑ csup c ∨ csup c ⊑ f x
+      by_cases h : ∃ z, c z ∧ f x ⊑ z
       · left
         obtain ⟨z, hz, hfz⟩ := h
         apply rel_trans hfz
-        apply le_csup rel rel_refl csup csup_spec hchain hz
+        apply le_csup hchain hz
       · right
-        apply csup_le rel csup csup_spec hchain _
+        apply csup_le hchain _
         intro z hz
         rw [not_exists] at h
         specialize h z
@@ -103,9 +92,9 @@ theorem chain_iterates {f : α → α} (hf : monotone rel f) : chain rel (iterat
     · right
       obtain ⟨z, hz, hfz⟩ := h
       apply rel_trans hfz
-      apply le_csup rel rel_refl csup csup_spec hchain hz
+      apply le_csup hchain hz
     · left
-      apply csup_le rel csup csup_spec hchain _
+      apply csup_le hchain _
       intro z hz
       rw [not_exists] at h
       specialize h z
@@ -115,33 +104,32 @@ theorem chain_iterates {f : α → α} (hf : monotone rel f) : chain rel (iterat
       next => assumption
       next => contradiction
 
-include rel_refl rel_trans csup_spec in
-theorem csup_iterate_rel {f : α → α} (hf : monotone rel f) :
-    rel (csup (iterates rel csup f)) (f (csup (iterates rel csup f))) := by
-  apply csup_le rel csup csup_spec (chain_iterates rel rel_refl rel_trans csup csup_spec hf)
-  intro x hx
+def fix (f : α → α) := csup (iterates f)
+
+theorem rel_f_of_iterates {f : α → α} (hf : monotone f) {x : α} (hx : iterates f x) : x ⊑ f x := by
   induction hx
   case step ih =>
     apply hf
-    apply le_csup rel rel_refl csup csup_spec (chain_iterates rel rel_refl rel_trans csup csup_spec hf)
     assumption
   case sup c hchain hi ih =>
-    apply csup_le rel csup csup_spec hchain
+    apply csup_le hchain
     intro y hy
-    apply ih _ hy
+    apply rel_trans (ih y hy)
+    apply hf
+    apply le_csup hchain hy
 
-include rel_refl rel_antisymm rel_trans csup_spec in
-theorem csup_iterate_eq {f : α → α} (hf : monotone rel f) :
-    csup (iterates rel csup f) = f (csup (iterates rel csup f)) := by
+theorem fix_eq {f : α → α} (hf : monotone f) : fix f = f (fix f) := by
   apply rel_antisymm
-  · exact csup_iterate_rel rel rel_refl rel_trans csup csup_spec hf
-  · apply le_csup rel rel_refl csup csup_spec (chain_iterates rel rel_refl rel_trans csup csup_spec hf)
+  · apply rel_f_of_iterates hf
+    apply iterates.sup (chain_iterates hf)
+    exact fun _ h => h
+  · apply le_csup (chain_iterates hf)
     apply iterates.step
-    apply iterates.sup (chain_iterates rel rel_refl rel_trans csup csup_spec hf)
+    apply iterates.sup (chain_iterates hf)
     intro y hy
     exact hy
 
-end fixpoint
+end CCPO
 
 section flat_order
 
@@ -149,68 +137,110 @@ variable {α : Type u}
 
 variable (b : α)
 
-inductive Flat_order (b : α) : (x y : α) → Prop where
-  | bot : Flat_order b b x
-  | refl : Flat_order b x x
+set_option linter.unusedVariables false in
+def FlatOrder (b : α) := α
 
-namespace Flat_order
+inductive FlatOrder.rel (b : α) : (x y : α) → Prop where
+  | bot : rel b b x
+  | refl : rel b x x
 
-theorem trans {x y z : α} (hxy : Flat_order b x y) (hyz : Flat_order b y z) : Flat_order b x z := by
-  cases hxy <;> cases hyz <;> constructor
-
-theorem antisymm {x y : α} (hxy : Flat_order b x y) (hyz : Flat_order b y x) : x = y := by
-  cases hxy <;> cases hyz <;> constructor
-
-
-noncomputable def csup (c : α → Prop) : α :=
-  open Classical in
-  if h : ∃ x, c x ∧ x ≠ b then
-    Classical.choose h
-  else
-    b
+instance FlatOrder.instOrder : Order (FlatOrder b) where
+  rel := rel b
+  rel_refl := .refl
+  rel_trans {x y z : α} (hxy : rel b x y) (hyz : rel b y z) := by
+    cases hxy <;> cases hyz <;> constructor
+  rel_antisymm {x y : α} (hxy : rel b x y) (hyz : rel b y x) : x = y := by
+    cases hxy <;> cases hyz <;> constructor
 
 open Classical in
 private theorem Classical.some_spec₂ {α : Sort _} {p : α → Prop} {h : ∃ a, p a} (q : α → Prop)
     (hpq : ∀ a, p a → q a) : q (choose h) := hpq _ <| choose_spec _
 
-theorem csup_spec {c} (hc : chain (Flat_order b) c) {x : α} :
-    Flat_order b (csup b c) x ↔ (∀ y, c y → Flat_order b y x) := by
-  unfold csup
-  split
-  next hex =>
-    apply Classical.some_spec₂ (q := (Flat_order b · x ↔ (∀ y, c y → Flat_order b y x)))
-    clear hex
-    intro z ⟨hz, hnb⟩
-    constructor
-    · intro h y hy
-      apply trans b _ h; clear h
-      cases hc y z hy hz
-      next => assumption
-      next h =>
-        cases h
+noncomputable def flat_csup (c : FlatOrder b → Prop) : FlatOrder b := by
+  by_cases h : ∃ (x : FlatOrder b), c x ∧ x ≠ b
+  · exact Classical.choose h
+  · exact b
+
+noncomputable instance FlatOrder.instCCPO : CCPO (FlatOrder b) where
+  csup c := flat_csup b c
+  csup_spec := by
+    intro x c hc
+    unfold flat_csup
+    dsimp
+    split
+    next hex =>
+      apply Classical.some_spec₂ (q := (· ⊑ x ↔ (∀ y, c y → y ⊑ x)))
+      clear hex
+      intro z ⟨hz, hnb⟩
+      constructor
+      · intro h y hy
+        apply Order.rel_trans _ h; clear h
+        cases hc y z hy hz
+        next => assumption
+        next h =>
+          cases h
+          · contradiction
+          · constructor
+      · intro h
+        cases h z hz
         · contradiction
         · constructor
-    · intro h
-      cases h z hz
-      · contradiction
-      · constructor
-  next hnotex =>
-    constructor
-    · intro h y hy; clear h
-      suffices y = b by rw [this]; exact Flat_order.bot
-      rw [not_exists] at hnotex
-      specialize hnotex y
-      rw [not_and] at hnotex
-      specialize hnotex hy
-      rw [@Classical.not_not] at hnotex
-      assumption
-    · intro; exact Flat_order.bot
-
-end Flat_order
-
-
-
+    next hnotex =>
+      constructor
+      · intro h y hy; clear h
+        suffices y = b by rw [this]; exact rel.bot
+        rw [not_exists] at hnotex
+        specialize hnotex y
+        rw [not_and] at hnotex
+        specialize hnotex hy
+        rw [@Classical.not_not] at hnotex
+        assumption
+      · intro; exact rel.bot
 
 end flat_order
+
+section fun_order
+
+open Order
+
+variable {α : Type u}
+variable {β : α → Type v}
+
+instance [∀ x, Order (β x)] : Order (∀ x, β x) where
+  rel f g := ∀ x, f x ⊑ g x
+  rel_refl _ := rel_refl
+  rel_trans hf hg x := rel_trans (hf x) (hg x)
+  rel_antisymm hf hg := funext (fun x => rel_antisymm (hf x) (hg x))
+
+theorem chain_apply [∀ x, Order (β x)] {c : (∀ x, β x) → Prop} (hc : chain c) (x : α) :
+    chain (fun y => ∃ f, c f ∧ f x = y) := by
+  intro _ _ ⟨f, hf, hfeq⟩ ⟨g, hg, hgeq⟩
+  subst hfeq; subst hgeq
+  cases hc f g hf hg
+  next h => left; apply h x
+  next h => right; apply h x
+
+def fun_csup  [∀ x, Order (β x)] [∀ x, CCPO (β x)] (c : (∀ x, β x) → Prop) (x : α) :=
+  CCPO.csup (fun y => ∃ f, c f ∧ f x = y)
+
+instance [∀ x, Order (β x)] [∀ x, CCPO (β x)] : CCPO (∀ x, β x) where
+  csup := fun_csup
+  csup_spec := by
+    intro f c hc
+    constructor
+    next =>
+      intro hf g hg x
+      apply rel_trans _ (hf x); clear hf
+      apply le_csup (chain_apply hc x)
+      exact ⟨g, hg, rfl⟩
+    next =>
+      intro h x
+      apply csup_le (chain_apply hc x)
+      intro y ⟨z, hz, hyz⟩
+      subst y
+      apply h z hz
+
+end fun_order
+
 
 end Lean.Tailrec

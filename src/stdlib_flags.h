399e7fa1cfd183a4452bcfbe16e2cd6be0bb3dcc
Sebastian Ullrich <sebasti@nullri.ch>
2024-11-26 11:20:05 +0100

    revert temp changes and document

--- src/stdlib_flags.h
+++ src/stdlib_flags.h
@@ -5,12 +5,15 @@ options get_default_options() {
     options opts;
     // see https://lean-lang.org/lean4/doc/dev/bootstrap.html#further-bootstrapping-complications
 #if LEAN_IS_STAGE0 == 1
-    // switch to `true` for ABI-breaking changes affecting meta code
+    // switch to `true` for ABI-breaking changes affecting meta code;
+    // see also next option!
     opts = opts.update({"interpreter", "prefer_native"}, false);
     // switch to `false` when enabling `prefer_native` should also affect use
-    // of built-in parsers in quotations
-    opts = opts.update({"internal", "parseQuotWithCurrentStage"}, false);
-    // changes to built-in parsers may also require toggling the following option if macros/syntax
+    // of built-in parsers in quotations; this is usually the case, but setting
+    // both to `true` may be necessary for handling non-builtin parsers with
+    // builtin elaborators
+    opts = opts.update({"internal", "parseQuotWithCurrentStage"}, true);
+    // changes to builtin parsers may also require toggling the following option if macros/syntax
     // with custom precheck hooks were affected
     opts = opts.update({"quotPrecheck"}, true);
 

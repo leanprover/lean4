bff3b5ed7b9060f6b1642292d70b26f95dc07a5f
Joachim Breitner <mail@joachim-breitner.de>
2024-11-29 16:59:05 +0100

    Golf

--- tests/lean/run/funind_nonrec.lean
+++ tests/lean/run/funind_nonrec.lean
@@ -7,12 +7,8 @@ info: Option.map.cases.{u_1, u_2} (motive : Prop) {α : Type u_1} {β : Type u_2
 
 example (x : Option Nat) (f : Nat → Nat) : (x.map f).isSome = x.isSome := by
   apply Option.map.cases _ f x
-  case case1 =>
-    intro y hx
-    simp [-Option.isSome_map', hx]
-  case case2 =>
-    intro hx
-    simp [-Option.isSome_map', hx]
+  case case1 => intro y hx; simp [-Option.isSome_map', hx]
+  case case2 => intro hx; simp [-Option.isSome_map', hx]
 
 /--
 info: List.map.cases.{u, v} (motive : Prop) {α : Type u} {β : Type v} (f : α → β) (x✝ : List α) (case1 : x✝ = [] → motive)

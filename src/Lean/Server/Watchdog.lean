leanprover/lean4

commit 3c82f9ae1208dc842d59755d5674aec37eda5ba0

feat: diagnostics for stale dependencies (#3247)
Sends a diagnostic informing the user to run Restart File when a file
dependency is saved.

Based on #3014 because this feature was easier to implement with the new
architecture.

ToDo:
- [x] Adjust vscode-lean4 to display a notification when this diagnostic
appears in a non-annoying way
(https://github.com/leanprover/vscode-lean4/pull/393)
- [x] Use a file watcher to identify changes to files not tracked by VS
Code
- [x] Rebase onto master when #3014 is merged

src/Lean/Server/Watchdog.lean
@@ -141,6 +141,49 @@ end FileWorker
 
 section ServerM
   abbrev FileWorkerMap := RBMap DocumentUri FileWorker compare
+  abbrev ImportMap := RBMap DocumentUri (RBTree DocumentUri compare) compare
+
+  /-- Global import data for all open files managed by this watchdog. -/
+  structure ImportData where
+    /-- For every open file, the files that it imports. -/
+    imports    : ImportMap
+    /-- For every open file, the files that it is imported by. -/
+    importedBy : ImportMap
+
+  /-- Updates `d` with the new set of `imports` for the file `uri`. -/
+  def ImportData.update (d : ImportData) (uri : DocumentUri) (imports : RBTree DocumentUri compare)
+      : ImportData := Id.run do
+    let oldImports     := d.imports.findD uri ∅
+    let removedImports := oldImports.diff imports
+    let addedImports   := imports.diff oldImports
+    let mut importedBy := d.importedBy
+
+    for removedImport in removedImports do
+      let importedByRemovedImport' := importedBy.find! removedImport |>.erase uri
+      if importedByRemovedImport'.isEmpty then
+        importedBy := importedBy.erase removedImport
+      else
+        importedBy := importedBy.insert removedImport importedByRemovedImport'
+
+    for addedImport in addedImports do
+      importedBy :=
+        importedBy.findD addedImport ∅
+          |>.insert uri
+          |> importedBy.insert addedImport
+
+    let imports :=
+      if imports.isEmpty then
+        d.imports.erase uri
+      else
+        d.imports.insert uri imports
+
+    return { imports, importedBy }
+
+  /--
+  Sets the imports of `uri` in `d` to the empty set.
+  -/
+  def ImportData.eraseImportsOf (d : ImportData) (uri : DocumentUri) : ImportData :=
+    d.update uri ∅
 
   structure RequestIDTranslation where
     sourceUri : DocumentUri
@@ -173,18 +216,19 @@ section ServerM
     (translation?, data)
 
   structure ServerContext where
-    hIn               : FS.Stream
-    hOut              : FS.Stream
-    hLog              : FS.Stream
+    hIn                 : FS.Stream
+    hOut                : FS.Stream
+    hLog                : FS.Stream
     /-- Command line arguments. -/
-    args              : List String
-    fileWorkersRef    : IO.Ref FileWorkerMap
+    args                : List String
+    fileWorkersRef      : IO.Ref FileWorkerMap
     /-- We store these to pass them to workers. -/
-    initParams        : InitializeParams
-    workerPath        : System.FilePath
-    srcSearchPath     : System.SearchPath
-    references        : IO.Ref References
-    serverRequestData : IO.Ref ServerRequestData
+    initParams          : InitializeParams
+    workerPath          : System.FilePath
+    srcSearchPath       : System.SearchPath
+    references          : IO.Ref References
+    serverRequestData   : IO.Ref ServerRequestData
+    importData          : IO.Ref ImportData
 
   abbrev ServerM := ReaderT ServerContext IO
 
@@ -201,6 +245,7 @@ section ServerM
 
   def eraseFileWorker (uri : DocumentUri) : ServerM Unit := do
     let s ← read
+    s.importData.modify (·.eraseImportsOf uri)
     s.fileWorkersRef.modify (fun fileWorkers => fileWorkers.erase uri)
     let some module ← s.srcSearchPath.searchModuleNameOfUri uri
       | return
@@ -224,6 +269,15 @@ section ServerM
       | return
     s.references.modify fun refs =>
       refs.finalizeWorkerRefs module params.version params.references
+
+  /--
+  Updates the global import data with the import closure provided by the file worker after it
+  successfully processed its header.
+  -/
+  def handleImportClosure (fw : FileWorker) (params : LeanImportClosureParams) : ServerM Unit := do
+    let s ← read
+    s.importData.modify fun importData =>
+      importData.update fw.doc.uri (.ofList params.importClosure.toList)
 
   /-- Creates a Task which forwards a worker's messages into the output stream until an event
   which must be handled in the main watchdog thread (e.g. an I/O error) happens. -/
@@ -253,6 +307,10 @@ section ServerM
             if let some params := params then
               if let Except.ok params := FromJson.fromJson? <| ToJson.toJson params then
                 handleIleanInfoFinal fw params
+          | Message.notification "$/lean/importClosure" params =>
+            if let some params := params then
+              if let Except.ok params := FromJson.fromJson? <| ToJson.toJson params then
+                handleImportClosure fw params
           | _ => o.writeLspMessage msg
       catch err =>
         -- If writeLspMessage from above errors we will block here, but the main task will
@@ -401,6 +459,18 @@ section ServerM
         fw.stdin.writeLspMessage msg
       catch _ =>
         handleCrash uri initialQueuedMsgs
+
+  /--
+  Sends a notification to the file worker identified by `uri` that its dependency `staleDependency`
+  is out-of-date.
+  -/
+  def notifyAboutStaleDependency (uri : DocumentUri) (staleDependency : DocumentUri)
+      : ServerM Unit :=
+    let notification := Notification.mk "$/lean/staleDependency" {
+      staleDependency := staleDependency
+      : LeanStaleDependencyParams
+    }
+    tryWriteMessage uri notification (queueFailedMessage := false)
 end ServerM
 
 section RequestHandling
@@ -679,28 +749,53 @@ section NotificationHandling
     let notification := Notification.mk "textDocument/didChange" p
     tryWriteMessage doc.uri notification (restartCrashedWorker := true)
 
+  /--
+  When a file is saved, notifies all file workers for files that depend on this file that this
+  specific import is now stale so that the file worker can issue a diagnostic asking users to
+  restart the file.
+  -/
+  def handleDidSave (p : DidSaveTextDocumentParams) : ServerM Unit := do
+    let s ← read
+    let fileWorkers ← s.fileWorkersRef.get
+    let importData  ← s.importData.get
+    let dependents := importData.importedBy.findD p.textDocument.uri ∅
+
+    for ⟨uri, _⟩ in fileWorkers do
+      if ! dependents.contains uri then
+        continue
+      notifyAboutStaleDependency uri p.textDocument.uri
+
   def handleDidClose (p : DidCloseTextDocumentParams) : ServerM Unit :=
     terminateFileWorker p.textDocument.uri
 
   def handleDidChangeWatchedFiles (p : DidChangeWatchedFilesParams) : ServerM Unit := do
+    let importData ← (← read).importData.get
     let references := (← read).references
     let oleanSearchPath ← Lean.searchPathRef.get
     let ileans ← oleanSearchPath.findAllWithExt "ilean"
     for change in p.changes do
-      if let some path := fileUriToPath? change.uri then
-      if let FileChangeType.Deleted := change.type then
-        references.modify (fun r => r.removeIlean path)
-      else if ileans.contains path then
-        try
-          let ilean ← Ilean.load path
-          if let FileChangeType.Changed := change.type then
-            references.modify (fun r => r.removeIlean path |>.addIlean path ilean)
-          else
-            references.modify (fun r => r.addIlean path ilean)
-        catch
-          -- ilean vanished, ignore error
-          | .noFileOrDirectory .. => references.modify (·.removeIlean path)
-          | e => throw e
+      let some path := fileUriToPath? change.uri
+        | continue
+      match path.extension with
+      | "lean" =>
+        let dependents := importData.importedBy.findD change.uri ∅
+        for dependent in dependents do
+          notifyAboutStaleDependency dependent change.uri
+      | "ilean" =>
+        if let FileChangeType.Deleted := change.type then
+          references.modify (fun r => r.removeIlean path)
+        else if ileans.contains path then
+          try
+            let ilean ← Ilean.load path
+            if let FileChangeType.Changed := change.type then
+              references.modify (fun r => r.removeIlean path |>.addIlean path ilean)
+            else
+              references.modify (fun r => r.addIlean path ilean)
+          catch
+            -- ilean vanished, ignore error
+            | .noFileOrDirectory .. => references.modify (·.removeIlean path)
+            | e => throw e
+      | _ => continue
 
   def handleCancelRequest (p : CancelParams) : ServerM Unit := do
     let fileWorkers ← (←read).fileWorkersRef.get
@@ -805,6 +900,8 @@ section MessageHandling
       handle DidChangeTextDocumentParams handleDidChange
     | "textDocument/didClose" =>
       handle DidCloseTextDocumentParams handleDidClose
+    | "textDocument/didSave" =>
+      handle DidSaveTextDocumentParams handleDidSave
     | "workspace/didChangeWatchedFiles" =>
       handle DidChangeWatchedFilesParams handleDidChangeWatchedFiles
     | "$/cancelRequest" =>
@@ -910,7 +1007,7 @@ def mkLeanServerCapabilities : ServerCapabilities := {
     change            := TextDocumentSyncKind.incremental
     willSave          := false
     willSaveWaitUntil := false
-    save?             := none
+    save?             := some { includeText := true }
   }
   -- refine
   completionProvider? := some {
@@ -1001,6 +1098,7 @@ def initAndRunWatchdog (args : List String) (i o e : FS.Stream) : IO Unit := do
     pendingServerRequests := RBMap.empty
     freshServerRequestID  := 0
   }
+  let importData ← IO.mkRef ⟨RBMap.empty, RBMap.empty⟩
   let i ← maybeTee "wdIn.txt" false i
   let o ← maybeTee "wdOut.txt" true o
   let e ← maybeTee "wdErr.txt" true e
@@ -1017,29 +1115,30 @@ def initAndRunWatchdog (args : List String) (i o e : FS.Stream) : IO Unit := do
     }
   }
   o.writeLspRequest {
-    id := RequestID.str "register_ilean_watcher"
+    id := RequestID.str "register_lean_watcher"
     method := "client/registerCapability"
     param := some {
       registrations := #[ {
-        id := "ilean_watcher"
+        id := "lean_watcher"
         method := "workspace/didChangeWatchedFiles"
         registerOptions := some <| toJson {
-          watchers := #[ { globPattern := "**/*.ilean" } ]
+          watchers := #[ { globPattern := "**/*.lean" }, { globPattern := "**/*.ilean" } ]
         : DidChangeWatchedFilesRegistrationOptions }
       } ]
     : RegistrationParams }
   }
   ReaderT.run initAndRunWatchdogAux {
-    hIn            := i
-    hOut           := o
-    hLog           := e
-    args           := args
-    fileWorkersRef := fileWorkersRef
-    initParams     := initRequest.param
+    hIn              := i
+    hOut             := o
+    hLog             := e
+    args             := args
+    fileWorkersRef   := fileWorkersRef
+    initParams       := initRequest.param
     workerPath
     srcSearchPath
     references
     serverRequestData
+    importData
     : ServerContext
   }
 

src/Init/Data/Ord.lean
@@ -6,6 +6,7 @@ Authors: Dany Fabian, Sebastian Ullrich
 
 prelude
 import Init.Data.String
+import Init.Data.Array.Basic
 
 inductive Ordering where
   | lt | eq | gt
@@ -87,10 +88,23 @@ def isGE : Ordering → Bool
 
 end Ordering
 
+/--
+Yields an `Ordering` s.t. `x < y` corresponds to `Ordering.lt` / `Ordering.gt` and
+`x = y` corresponds to `Ordering.eq`.
+-/
 @[inline] def compareOfLessAndEq {α} (x y : α) [LT α] [Decidable (x < y)] [DecidableEq α] : Ordering :=
   if x < y then Ordering.lt
   else if x = y then Ordering.eq
   else Ordering.gt
+
+/--
+Yields an `Ordering` s.t. `x < y` corresponds to `Ordering.lt` / `Ordering.gt` and
+`x == y` corresponds to `Ordering.eq`.
+-/
+@[inline] def compareOfLessAndBEq {α} (x y : α) [LT α] [Decidable (x < y)] [BEq α] : Ordering :=
+  if x < y then .lt
+  else if x == y then .eq
+  else .gt
 
 /--
 Compare `a` and `b` lexicographically by `cmp₁` and `cmp₂`. `a` and `b` are
@@ -146,6 +160,13 @@ instance : Ord USize where
 
 instance : Ord Char where
   compare x y := compareOfLessAndEq x y
+
+instance [Ord α] : Ord (Option α) where
+  compare
+  | none,   none   => .eq
+  | none,   some _ => .lt
+  | some _, none   => .gt
+  | some x, some y => compare x y
 
 /-- The lexicographic order on pairs. -/
 def lexOrd [Ord α] [Ord β] : Ord (α × β) where
@@ -209,5 +230,14 @@ returns 'equal', by `ord₂`.
 -/
 protected def lex' (ord₁ ord₂ : Ord α) : Ord α where
   compare := compareLex ord₁.compare ord₂.compare
+
+/--
+Creates an order which compares elements of an `Array` in lexicographic order.
+-/
+protected def arrayOrd [a : Ord α] : Ord (Array α) where
+  compare x y :=
+    let _ : LT α := a.toLT
+    let _ : BEq α := a.toBEq
+    compareOfLessAndBEq x.toList y.toList
 
 end Ord

src/Lean/Data/Lsp/Basic.lean
@@ -52,7 +52,7 @@ instance : LE Range := leOfOrd
 structure Location where
   uri : DocumentUri
   range : Range
-  deriving Inhabited, BEq, ToJson, FromJson
+  deriving Inhabited, BEq, ToJson, FromJson, Ord
 
 structure LocationLink where
   originSelectionRange? : Option Range

src/Lean/Data/Lsp/Diagnostics.lean
@@ -25,7 +25,7 @@ open Json
 
 inductive DiagnosticSeverity where
   | error | warning | information | hint
-  deriving Inhabited, BEq
+  deriving Inhabited, BEq, Ord
 
 instance : FromJson DiagnosticSeverity := ⟨fun j =>
   match j.getNat? with
@@ -45,7 +45,7 @@ instance : ToJson DiagnosticSeverity := ⟨fun
 inductive DiagnosticCode where
   | int (i : Int)
   | string (s : String)
-  deriving Inhabited, BEq
+  deriving Inhabited, BEq, Ord
 
 instance : FromJson DiagnosticCode := ⟨fun
   | num (i : Int) => return DiagnosticCode.int i
@@ -62,7 +62,7 @@ inductive DiagnosticTag where
   | unnecessary
   /-- Deprecated or obsolete code. Rendered with a strike-through. -/
   | deprecated
-  deriving Inhabited, BEq
+  deriving Inhabited, BEq, Ord
 
 instance : FromJson DiagnosticTag := ⟨fun j =>
   match j.getNat? with
@@ -80,7 +80,7 @@ instance : ToJson DiagnosticTag := ⟨fun
 structure DiagnosticRelatedInformation where
   location : Location
   message : String
-  deriving Inhabited, BEq, ToJson, FromJson
+  deriving Inhabited, BEq, ToJson, FromJson, Ord
 
 /-- Represents a diagnostic, such as a compiler error or warning. Diagnostic objects are only valid in the scope of a resource.
 
@@ -112,6 +112,29 @@ structure DiagnosticWith (α : Type) where
 
 def DiagnosticWith.fullRange (d : DiagnosticWith α) : Range :=
   d.fullRange?.getD d.range
+
+local instance [Ord α] : Ord (Array α) := Ord.arrayOrd
+
+/-- Restriction of `DiagnosticWith` to properties that are displayed to users in the InfoView. -/
+private structure DiagnosticWith.UserVisible (α : Type) where
+  range               : Range
+  fullRange?          : Option Range
+  severity?           : Option DiagnosticSeverity
+  code?               : Option DiagnosticCode
+  source?             : Option String
+  message             : α
+  tags?               : Option (Array DiagnosticTag)
+  relatedInformation? : Option (Array DiagnosticRelatedInformation)
+  deriving Ord
+
+/-- Extracts user-visible properties from the given `DiagnosticWith`. -/
+private def DiagnosticWith.UserVisible.ofDiagnostic (d : DiagnosticWith α)
+    : DiagnosticWith.UserVisible α :=
+  { d with }
+
+/-- Compares `DiagnosticWith` instances modulo non-user-facing properties. -/
+def compareByUserVisible [Ord α] (a b : DiagnosticWith α) : Ordering :=
+  compare (DiagnosticWith.UserVisible.ofDiagnostic a) (DiagnosticWith.UserVisible.ofDiagnostic b)
 
 abbrev Diagnostic := DiagnosticWith String
 

src/Lean/Data/Lsp/Internal.lean
@@ -161,14 +161,33 @@ instance : FromJson ModuleRefs where
     node.foldM (init := HashMap.empty) fun m k v =>
       return m.insert (← RefIdent.fromJson? (← Json.parse k)) (← fromJson? v)
 
-/-- `$/lean/ileanInfoUpdate` and `$/lean/ileanInfoFinal` watchdog<-worker notifications.
-
-Contains the file's definitions and references. -/
+/--
+Used in the `$/lean/ileanInfoUpdate` and `$/lean/ileanInfoFinal` watchdog <- worker notifications.
+Contains the definitions and references of the file managed by a worker.
+-/
 structure LeanIleanInfoParams where
   /-- Version of the file these references are from. -/
   version    : Nat
   /-- All references for the file. -/
   references : ModuleRefs
+  deriving FromJson, ToJson
+
+/--
+Used in the `$/lean/importClosure` watchdog <- worker notification.
+Contains the full import closure of the file managed by a worker.
+-/
+structure LeanImportClosureParams where
+  /-- Full import closure of the file. -/
+  importClosure : Array DocumentUri
+  deriving FromJson, ToJson
+
+/--
+Used in the `$/lean/importClosure` watchdog -> worker notification.
+Informs the worker that one of its dependencies has gone stale and likely needs to be rebuilt.
+-/
+structure LeanStaleDependencyParams where
+  /-- The dependency that is stale. -/
+  staleDependency : DocumentUri
   deriving FromJson, ToJson
 
 end Lean.Lsp

src/Lean/Data/Lsp/TextSync.lean
@@ -38,7 +38,7 @@ structure DidOpenTextDocumentParams where
 
 structure TextDocumentChangeRegistrationOptions where
   documentSelector? : Option DocumentSelector := none
-  syncKind : TextDocumentSyncKind
+  syncKind          : TextDocumentSyncKind
   deriving FromJson
 
 inductive TextDocumentContentChangeEvent where
@@ -61,13 +61,18 @@ instance TextDocumentContentChangeEvent.hasToJson : ToJson TextDocumentContentCh
     | TextDocumentContentChangeEvent.fullChange text => [⟨"text", toJson text⟩]⟩
 
 structure DidChangeTextDocumentParams where
-  textDocument : VersionedTextDocumentIdentifier
+  textDocument   : VersionedTextDocumentIdentifier
   contentChanges : Array TextDocumentContentChangeEvent
+  deriving ToJson, FromJson
+
+structure DidSaveTextDocumentParams where
+  textDocument : TextDocumentIdentifier
+  text?        : Option String
   deriving ToJson, FromJson
 
 -- TODO: missing:
 -- WillSaveTextDocumentParams, TextDocumentSaveReason,
--- TextDocumentSaveRegistrationOptions, DidSaveTextDocumentParams
+-- TextDocumentSaveRegistrationOptions
 
 structure SaveOptions where
   includeText : Bool
@@ -81,11 +86,11 @@ structure DidCloseTextDocumentParams where
 
 /-- NOTE: This is defined twice in the spec. The latter version has more fields. -/
 structure TextDocumentSyncOptions where
-  openClose : Bool
-  change : TextDocumentSyncKind
-  willSave : Bool
+  openClose         : Bool
+  change            : TextDocumentSyncKind
+  willSave          : Bool
   willSaveWaitUntil : Bool
-  save? : Option SaveOptions := none
+  save?             : Option SaveOptions
   deriving ToJson, FromJson
 
 end Lsp

src/Lean/Server/FileSource.lean
@@ -37,6 +37,9 @@ instance : FileSource DidOpenTextDocumentParams :=
 instance : FileSource DidChangeTextDocumentParams :=
   ⟨fun p => fileSource p.textDocument⟩
 
+instance : FileSource DidSaveTextDocumentParams :=
+  ⟨fun p => fileSource p.textDocument⟩
+
 instance : FileSource DidCloseTextDocumentParams :=
   ⟨fun p => fileSource p.textDocument⟩
 

commit 39bffb6fda96c801c752efa16151a6e46833129e

fix: don't walk full project file tree on every file save (#6246)
This PR fixes a performance issue where the Lean language server would
walk the full project file tree every time a file was saved, blocking
the processing of all other requests and notifications and significantly
increasing overall language server latency after saving.

This issue was originally reported at
https://leanprover.zulipchat.com/#narrow/channel/270676-lean4/topic/Compiling.20too.20slow/near/484386515
and uncovered in a lengthy investigation. The performance bug that
causes the Lean language server to walk the full project file tree when
the file watcher for .ilean files is triggered was introduced when the
.ileans were first introduced, whereas the specific issue of file saving
also triggering the walk was introduced by #3247 in 4.8.0 and the use of
the file watcher for .lean files, which would then also trigger the
directory walk. Combining this with VS Code's auto-save feature causes
the language server to walk the full project file tree on every change
of the document.

It somehow hasn't really been much of an issue until now, but we still
do way too much work in the watchdog main loop. I'll look into resolving
that more general issue in the future.

src/Lean/Server/Watchdog.lean
@@ -811,33 +811,35 @@ section NotificationHandling
     terminateFileWorker p.textDocument.uri
 
   def handleDidChangeWatchedFiles (p : DidChangeWatchedFilesParams) : ServerM Unit := do
-    let importData ← (← read).importData.get
-    let references := (← read).references
-    let oleanSearchPath ← Lean.searchPathRef.get
-    let ileans ← oleanSearchPath.findAllWithExt "ilean"
-    for change in p.changes do
-      let some path := fileUriToPath? change.uri
-        | continue
-      match path.extension with
-      | "lean" =>
-        let dependents := importData.importedBy.findD change.uri ∅
+    let changes := p.changes.filterMap fun c => do return (c, ← fileUriToPath? c.uri)
+    let leanChanges := changes.filter fun (_, path) => path.extension == "lean"
+    let ileanChanges := changes.filter fun (_, path) => path.extension == "ilean"
+    if ! leanChanges.isEmpty then
+      let importData ← (← read).importData.get
+      for (c, _) in leanChanges do
+        let dependents := importData.importedBy.findD c.uri ∅
         for dependent in dependents do
-          notifyAboutStaleDependency dependent change.uri
-      | "ilean" =>
-        if let FileChangeType.Deleted := change.type then
+          notifyAboutStaleDependency dependent c.uri
+    if ! ileanChanges.isEmpty then
+      let references := (← read).references
+      let oleanSearchPath ← Lean.searchPathRef.get
+      for (c, path) in ileanChanges do
+        if let FileChangeType.Deleted := c.type then
           references.modify (fun r => r.removeIlean path)
-        else if ileans.contains path then
-          try
-            let ilean ← Ilean.load path
-            if let FileChangeType.Changed := change.type then
-              references.modify (fun r => r.removeIlean path |>.addIlean path ilean)
-            else
-              references.modify (fun r => r.addIlean path ilean)
-          catch
-            -- ilean vanished, ignore error
-            | .noFileOrDirectory .. => references.modify (·.removeIlean path)
-            | e => throw e
-      | _ => continue
+          continue
+        let isIleanInSearchPath := (← searchModuleNameOfFileName path oleanSearchPath).isSome
+        if ! isIleanInSearchPath then
+          continue
+        try
+          let ilean ← Ilean.load path
+          if let FileChangeType.Changed := c.type then
+            references.modify (fun r => r.removeIlean path |>.addIlean path ilean)
+          else
+            references.modify (fun r => r.addIlean path ilean)
+        catch
+          -- ilean vanished, ignore error
+          | .noFileOrDirectory .. => references.modify (·.removeIlean path)
+          | e => throw e
 
   def handleCancelRequest (p : CancelParams) : ServerM Unit := do
     let fileWorkers ← (←read).fileWorkersRef.get
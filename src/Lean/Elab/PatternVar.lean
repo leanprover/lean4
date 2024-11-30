a1c3a36433f74257a2a332b3e0b2a6f6631a9713
Kyle Miller <kmill31415@gmail.com>
2024-11-30 12:27:25 -0800

    feat: parity between structure instance notation and `where` notation (#6165)
    This PR modifies structure instance notation and `where` notation to use
    the same notation for fields. Structure instance notation now admits
    binders, type ascriptions, and equations, and `where` notation admits
    full structure lvals. Examples of these for structure instance notation:
    ```lean
    structure PosFun where
      f : Nat → Nat
      pos : ∀ n, 0 < f n
    
    def p : PosFun :=
      { f n := n + 1
        pos := by simp }
    
    def p' : PosFun :=
      { f | 0 => 1
          | n + 1 => n + 1
        pos := by rintro (_|_) <;> simp }
    ```
    Just like for the structure `where` notation, a field `f x y z : ty :=
    val` expands to `f := fun x y z => (val : ty)`. The type ascription is
    optional.
    
    The PR also is setting things up for future expansion. Pending some
    discussion, in the future structure/`where` notation could have have
    embedded `where` clauses; rather than `{ a := { x := 1, y := z } }` one
    could write `{ a where x := 1; y := z }`.

--- src/Lean/Elab/PatternVar.lean
+++ src/Lean/Elab/PatternVar.lean
@@ -261,11 +261,16 @@ partial def collect (stx : Syntax) : M Syntax := withRef stx <| withFreshMacroSc
   | `({ $[$srcs?,* with]? $fields,* $[..%$ell?]? $[: $ty?]? }) =>
     if let some srcs := srcs? then
       throwErrorAt (mkNullNode srcs) "invalid struct instance pattern, 'with' is not allowed in patterns"
-    let fields ← fields.getElems.mapM fun
-      | `(Parser.Term.structInstField| $lval:structInstLVal := $val) => do
-        let newVal ← collect val
-        `(Parser.Term.structInstField| $lval:structInstLVal := $newVal)
-      | _ => throwInvalidPattern  -- `structInstFieldAbbrev` should be expanded at this point
+    -- TODO(kmill) restore this
+    -- let fields ← fields.getElems.mapM fun
+    --   | `(Parser.Term.structInstField| $lval:structInstLVal := $val) => do
+    --     let newVal ← collect val
+    --     `(Parser.Term.structInstField| $lval:structInstLVal := $newVal)
+    --   | _ => throwInvalidPattern  -- `structInstFieldAbbrev` should be expanded at this point
+    let fields ← fields.getElems.mapM fun field => do
+      let field := field.raw
+      let val ← collect field[1][2][1]
+      pure <| field.setArg 1 <| field[1].setArg 2 <| field[1][2].setArg 1 val
     `({ $[$srcs?,* with]? $fields,* $[..%$ell?]? $[: $ty?]? })
   | _ => throwInvalidPattern
 

38eb44869b7a1df5569ecac6989b7890db667423
Kim Morrison <scott.morrison@gmail.com>
2024-12-03 15:42:17 +1100

    feat: simprocs for other Fin operations (#6295)
    This PR sets up simprocs for all the remaining operations defined in
    `Init.Data.Fin.Basic`

--- src/Lean/Meta/LitValues.lean
+++ src/Lean/Meta/LitValues.lean
@@ -62,7 +62,7 @@ def getStringValue? (e : Expr) : (Option String) :=
   | .lit (.strVal s) => some s
   | _ => none
 
-/-- Return `some ⟨n, v⟩` if `e` is af `OfNat.ofNat` application encoding a `Fin n` with value `v` -/
+/-- Return `some ⟨n, v⟩` if `e` is an `OfNat.ofNat` application encoding a `Fin n` with value `v` -/
 def getFinValue? (e : Expr) : MetaM (Option ((n : Nat) × Fin n)) := OptionT.run do
   let (v, type) ← getOfNatValue? e ``Fin
   let n ← getNatValue? (← whnfD type.appArg!)

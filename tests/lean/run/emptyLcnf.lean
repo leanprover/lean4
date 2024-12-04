6968ec5e92c164663159033c70132598f5b89ab4
Cameron Zwarich <cameron@lean-fro.org>
2024-12-03 16:11:31 -0800

    chore: update expected test outputs
    This makes it easier to distinguish tests that are actually
    failing while we work on the new codegen.

--- tests/lean/run/emptyLcnf.lean
+++ tests/lean/run/emptyLcnf.lean
@@ -7,7 +7,27 @@ def f (x : MyEmpty) : Nat :=
 
 set_option trace.Compiler.result true
 /--
-info: [Compiler.result] size: 0
+info: [Compiler.result] size: 5
+    def _eval._lam_0 _x.1 _x.2 _y.3 _y.4 _y.5 _y.6 _y.7 : EStateM.Result Lean.Exception PUnit PUnit :=
+      let _x.8 := Lean.Compiler.compile _x.1 _y.5 _y.6 _y.7;
+      cases _x.8 : EStateM.Result Lean.Exception PUnit PUnit
+      | EStateM.Result.ok a.9 a.10 =>
+        let _x.11 := EStateM.Result.ok _ _ _ _x.2 a.10;
+        return _x.11
+      | EStateM.Result.error a.12 a.13 =>
+        return _x.8
+[Compiler.result] size: 8
+    def _eval a.1 a.2 a.3 : EStateM.Result Lean.Exception PUnit PUnit :=
+      let _x.4 := "f";
+      let _x.5 := Lean.Name.mkStr1 _x.4;
+      let _x.6 := 1;
+      let _x.7 := Array.mkEmpty ◾ _x.6;
+      let _x.8 := Array.push ◾ _x.7 _x.5;
+      let _x.9 := PUnit.unit;
+      let _f.10 := _eval._lam_0 _x.8 _x.9;
+      let _x.11 := instMonadEvalTOfMonadEval._elam_0._at_.Lean.Elab.Command.elabEvalCoreUnsafe._elam_0._at_.Lean.Elab.Command.elabEvalCoreUnsafe._elam_1._at_.Lean.Elab.Command.elabEvalCoreUnsafe.spec_0.spec_6.spec_6._redArg _f.10 a.1 a.2 a.3;
+      return _x.11
+[Compiler.result] size: 0
     def f._redArg : Nat :=
       ⊥
 [Compiler.result] size: 0 def f x : Nat := ⊥
--- tests/lean/run/erased.lean
+++ tests/lean/run/erased.lean
@@ -20,7 +20,34 @@ set_option pp.funBinderTypes true
 set_option pp.letVarTypes true
 set_option trace.Compiler.result true
 /--
-info: [Compiler.result] size: 1
+info: [Compiler.result] size: 5
+    def _eval._lam_0 (_x.1 : Array
+       Lean.Name) (_x.2 : PUnit) (_y.3 : Lean.Meta.Context) (_y.4 : lcErased) (_y.5 : Lean.Core.Context) (_y.6 : lcErased) (_y.7 : PUnit) : EStateM.Result
+      Lean.Exception PUnit PUnit :=
+      let _x.8 : EStateM.Result Lean.Exception PUnit PUnit := compile _x.1 _y.5 _y.6 _y.7;
+      cases _x.8 : EStateM.Result Lean.Exception PUnit PUnit
+      | EStateM.Result.ok (a.9 : PUnit) (a.10 : PUnit) =>
+        let _x.11 : EStateM.Result Lean.Exception PUnit PUnit := EStateM.Result.ok Lean.Exception PUnit PUnit _x.2 a.10;
+        return _x.11
+      | EStateM.Result.error (a.12 : Lean.Exception) (a.13 : PUnit) =>
+        return _x.8
+[Compiler.result] size: 9
+    def _eval (a.1 : Lean.Elab.Command.Context) (a.2 : lcErased) (a.3 : PUnit) : EStateM.Result Lean.Exception PUnit
+      PUnit :=
+      let _x.4 : String := "Erased";
+      let _x.5 : String := "mk";
+      let _x.6 : Lean.Name := Lean.Name.mkStr2 _x.4 _x.5;
+      let _x.7 : Nat := 1;
+      let _x.8 : Array Lean.Name := Array.mkEmpty ◾ _x.7;
+      let _x.9 : Array Lean.Name := Array.push ◾ _x.8 _x.6;
+      let _x.10 : PUnit := PUnit.unit;
+      let _f.11 : Lean.Meta.Context →
+        lcErased →
+          Lean.Core.Context → lcErased → PUnit → EStateM.Result Lean.Exception PUnit PUnit := _eval._lam_0 _x.9 _x.10;
+      let _x.12 : EStateM.Result Lean.Exception PUnit
+        lcErased := instMonadEvalTOfMonadEval._elam_0._at_.Lean.Elab.Command.elabEvalCoreUnsafe._elam_0._at_.Lean.Elab.Command.elabEvalCoreUnsafe._elam_1._at_.Lean.Elab.Command.elabEvalCoreUnsafe.spec_0.spec_6.spec_6._redArg _f.11 a.1 a.2 a.3;
+      return _x.12
+[Compiler.result] size: 1
     def Erased.mk._redArg : PSigma lcErased lcErased :=
       let _x.1 : PSigma lcErased lcErased := PSigma.mk lcErased ◾ ◾ ◾;
       return _x.1

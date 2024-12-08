23d94d682418cfe90f6f210240add4db04d2d247
Kim Morrison <kim@tqft.net>
2024-12-08 15:54:54 +1100

    fix tests

--- tests/lean/matchOfNatIssue.lean
+++ tests/lean/matchOfNatIssue.lean
@@ -1,13 +1,13 @@
-example (x : Int) (h : x = 2) : Int.div 2 1 = x := by
-  simp [Int.div]
+example (x : Int) (h : x = 2) : Int.ediv 2 1 = x := by
+  simp [Int.ediv]
   trace_state
   simp (config := { decide := true }) [h]
 
-example (n : Nat) : Int.div (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n / 0) := by
-  simp [Int.div]
+example (n : Nat) : Int.ediv (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n / 0) := by
+  simp [Int.ediv]
 
-example (n : Nat) : Int.div (Int.ofNat n) 0 = Int.ofNat (n / 0) := by
-  simp [Int.div]
+example (n : Nat) : Int.ediv (Int.ofNat n) 0 = Int.ofNat (n / 0) := by
+  simp [Int.ediv]
 
 example (n : Nat) : Int.mul (Int.ofNat n) (Int.ofNat 0) = Int.ofNat (n * 0) := by
   simp [Int.mul]

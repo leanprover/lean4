40063c7546301ecca8ebb610f0041c0c73924d07
Tobias Grosser <tobias@grosser.es>
2024-12-08 18:43:26 +0000

    chore: format

--- src/Init/Data/BitVec/Lemmas.lean
+++ src/Init/Data/BitVec/Lemmas.lean
@@ -658,7 +658,7 @@ theorem getMsbD_setWidth {m : Nat} {x : BitVec n} {i : Nat} :
   by_cases h : n ≤ m <;> simp only [h]
   · by_cases h' : (m - n ≤ i)
     <;> simp [h', show (i - (m - n)) = i + n - m by omega]
-  · simp only [show m-n = 0 by omega, getMsbD, getLsbD_setWidth]
+  · simp only [show m - n = 0 by omega, getMsbD, getLsbD_setWidth]
     by_cases h' : i < m
     · simp [show m - 1 - i < m by omega, show i + n - m < n by omega,
         show (n - 1 - (i + n - m)) = m - 1 - i by omega]

884bdd1d853895b35525e03cded442f7cd0fa25e
Leonardo de Moura <leomoura@amazon.com>
2024-12-10 18:25:04 -0800

    test: add basic `Float32` tests

---
+++ tests/lean/run/float32.lean
@@ -0,0 +1,26 @@
+/-- info: 2.100000 -/
+#guard_msgs in
+#eval (2.1 : Float32)
+
+/-- info: 3.200000 -/
+#guard_msgs in
+#eval (2.1 : Float32) + 1.1
+
+/-- info: 0.900000 -/
+#guard_msgs in
+#eval (2.1 : Float32) - 1.2
+
+def test1 : IO Unit := do
+  IO.println ((2 : Float32).sin);
+  IO.println ((2 : Float32).cos);
+  IO.println ((2 : Float32).sqrt);
+  IO.println ((2 : Float32) ^ (100 : Float32));
+
+/--
+info: 0.909297
+-0.416147
+1.414214
+1267650600228229401496703205376.000000
+-/
+#guard_msgs in
+#eval test1

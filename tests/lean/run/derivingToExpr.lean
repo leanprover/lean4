20d4a2c6c6b275748f6eb32b5f31d9b248886749
Alex Keizer <alex@keizer.dev>
2024-12-11 14:53:28 +0000

    expand test to actually exercise generated instance

--- tests/lean/run/derivingToExpr.lean
+++ tests/lean/run/derivingToExpr.lean
@@ -1,12 +1,40 @@
 import Lean.Elab.Deriving.ToExpr
 
-open Lean (ToExpr)
+open Lean
+
+deriving instance ToExpr for ULift
 
 -- Test with a universe polymporphic type parameter
 inductive Option' (α : Type u)
   | some (a : α)
   | none
   deriving ToExpr
+
+/-- info: instToExprOption'OfToLevel -/
+#guard_msgs in #synth ToExpr (Option' Nat)
+/-- info: instToExprOption'OfToLevel -/
+#guard_msgs in #synth ToExpr (Option' <| ULift.{20} Nat)
+
+/--
+info: Lean.Expr.app
+  (Lean.Expr.app
+    (Lean.Expr.const `Option'.some [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero)))])
+    (Lean.Expr.app
+      (Lean.Expr.const `ULift [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero))), Lean.Level.zero])
+      (Lean.Expr.const `Nat [])))
+  (Lean.Expr.app
+    (Lean.Expr.app
+      (Lean.Expr.const
+        `ULift.up
+        [Lean.Level.succ (Lean.Level.succ (Lean.Level.succ (Lean.Level.zero))), Lean.Level.zero])
+      (Lean.Expr.const `Nat []))
+    (Lean.Expr.app
+      (Lean.Expr.app
+        (Lean.Expr.app (Lean.Expr.const `OfNat.ofNat [Lean.Level.zero]) (Lean.Expr.const `Nat []))
+        (Lean.Expr.lit (Lean.Literal.natVal 42)))
+      (Lean.Expr.app (Lean.Expr.const `instOfNatNat []) (Lean.Expr.lit (Lean.Literal.natVal 42)))))
+-/
+#guard_msgs in #eval toExpr (Option'.some <| ULift.up.{3} 42)
 
 -- Test with recursive type
 inductive List' (α : Type u)

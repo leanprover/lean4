24a16a498e0c614abe363fc04f927f311e4c4985
Joachim Breitner <mail@joachim-breitner.de>
2024-12-11 13:41:13 +0100

    Coinductive Predicates

---
+++ tests/lean/run/partial_fixpoint_coinductive_pred.lean
@@ -0,0 +1,38 @@
+-- These can move to Init after a stage0 update
+open Lean.Order in
+attribute [partial_monotone]
+  monotone_ite
+  monotone_dite
+  monotone_bind
+  monotone_mapM
+  monotone_mapFinIdxM
+
+
+/-!
+Johannes Hölzl pointed out that the `partial_fixpoint` machinery can be applied to `Prop` to define
+inductive or (when using the dual order) coinductive predicates.
+
+Without an induction principle this isn't so useful, though.
+-/
+
+open Lean.Order
+
+instance : Order Prop where
+  rel x y := y → x -- NB: Dual
+  rel_refl := fun x => x
+  rel_trans h₁ h₂ := fun x => h₁ (h₂ x)
+  rel_antisymm h₁ h₂ := propext ⟨h₂, h₁⟩
+
+instance : CCPO Prop where
+  csup c := ∀ p, c p → p
+  csup_spec := by
+    intro x c hchain
+    constructor
+    · intro h y hcy hx
+      apply h hx _ hcy
+    · intro h hx y hcy
+      apply h _ hcy hx
+
+def univ (n : Nat) : Prop :=
+  univ (n + 1)
+partial_fixpoint

7afb7a58c85f1f4aa14a906f6e2e52e9aa034cce
Kyle Miller <kmill31415@gmail.com>
2024-12-20 14:04:10 -0800

    finished first pass at full implementation, need to test

--- src/Lean/Elab/Tactic/Lets.lean
+++ src/Lean/Elab/Tactic/Lets.lean
@@ -4,3 +4,40 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kyle Miller
 -/
 prelude
+import Lean.Meta.Tactic.Lets
+import Lean.Elab.Tactic.Location
+
+/-!
+# Tactics to manipulate `let` expressions
+-/
+
+open Lean Meta
+
+namespace Lean.Elab.Tactic
+
+declare_config_elab elabExtractLetsConfig ExtractLetsConfig
+
+@[builtin_tactic extractLets] elab_rules : tactic
+  | `(tactic| extract_lets $cfg:optConfig $ids* $[$ellipsis?:ellipsis]? $[$loc?:location]?) => do
+    let mut config ← elabExtractLetsConfig cfg
+    if ellipsis?.isSome then
+      config := { config with onlyGivenNames := true }
+    let givenNames := (ids.map getNameOfIdent').toList
+    withLocation (expandOptLocation (Lean.mkOptionalNode loc?))
+      (atLocal := fun h => do
+        let fvars ← liftMetaTacticAux fun mvarId => do
+          let (fvars, mvarId) ← mvarId.extractLetsLocalDecl h givenNames config
+          return (fvars, [mvarId])
+        withMainContext do
+          for stx in ids, fvar in fvars do
+            Term.addLocalVarInfo stx (.fvar fvar))
+      (atTarget := do
+        let fvars ← liftMetaTacticAux fun mvarId => do
+          let (fvars, mvarId) ← mvarId.extractLets givenNames config
+          return (fvars, [mvarId])
+        withMainContext do
+          for stx in ids, fvar in fvars do
+            Term.addLocalVarInfo stx (.fvar fvar))
+      (failed := fun _ => throwError "'extract_lets' tactic failed")
+
+end Lean.Elab.Tactic

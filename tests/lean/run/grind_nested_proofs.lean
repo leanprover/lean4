a99d4f8b90f5eec6efb5914792cc3d8c1b526699
Leonardo de Moura <leomoura@amazon.com>
2024-12-23 19:38:47 -0800

    test: congruence and nested proofs in `grind`

--- tests/lean/run/grind_nested_proofs.lean
+++ tests/lean/run/grind_nested_proofs.lean
@@ -8,17 +8,43 @@ elab "grind_test" : tactic => withMainContext do
   Meta.Grind.preprocessAndProbe (← getMainGoal) declName do
     let nodes ← filterENodes fun e => return e.self.isAppOf ``Lean.Grind.nestedProof
     logInfo (nodes.toList.map (·.self))
+    let nodes ← filterENodes fun e => return e.self.isAppOf ``GetElem.getElem
+    let [_, n, _] := nodes.toList | unreachable!
+    logInfo (← getEqc n.self)
 
 set_option grind.debug true
+
+/-
+Recall that array access terms, such as `a[i]`, have nested proofs.
+The following test relies on `grind` `nestedProof` wrapper to
+detect equalities between array access terms.
+-/
 
 /--
 info: [Lean.Grind.nestedProof (i < a.toList.length) (_example.proof_1 i j a b h1 h2),
  Lean.Grind.nestedProof (j < a.toList.length) h1,
  Lean.Grind.nestedProof (j < b.toList.length) h]
 ---
+info: [a[i], b[j], a[j]]
+---
 warning: declaration uses 'sorry'
 -/
 #guard_msgs in
 example (i j : Nat) (a b : Array Nat) (h1 : j < a.size) (h : j < b.size) (h2 : i ≤ j) : a[i] < a[j] + b[j] → i = j → a = b → False := by
+  grind_test
+  sorry
+
+
+/--
+info: [Lean.Grind.nestedProof (i < a.toList.length) (_example.proof_1 i j a b h1 h2),
+ Lean.Grind.nestedProof (j < a.toList.length) h1,
+ Lean.Grind.nestedProof (j < b.toList.length) h]
+---
+info: [a[i], a[j]]
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
+example (i j : Nat) (a b : Array Nat) (h1 : j < a.size) (h : j < b.size) (h2 : i ≤ j) : a[i] < a[j] + b[j] → i = j → False := by
   grind_test
   sorry

24a8561ec4e302f4e0cba07632fddacd6f6e0323
Leonardo de Moura <leomoura@amazon.com>
2024-12-30 04:40:43 +0100

    feat: check pattern coverage in the `grind_pattern` command (#6474)
    This PR adds pattern validation to the `grind_pattern` command. The new
    `checkCoverage` function will also be used to implement the attributes
    `@[grind_eq]`, `@[grind_fwd]`, and `@[grind_bwd]`.

--- tests/lean/run/grind_pattern1.lean
+++ tests/lean/run/grind_pattern1.lean
@@ -26,3 +26,91 @@ error: `foo` is not a theorem, you cannot assign patterns to non-theorems for th
 -/
 #guard_msgs in
 grind_pattern foo => x + x
+
+/--
+error: invalid pattern(s) for `Array.getElem_push_lt`
+  [@Array.push #4 #3 #2]
+the following theorem parameters cannot be instantiated:
+  i : Nat
+  h : i < a.size
+---
+info: [grind.pattern] Array.getElem_push_lt: [@Array.push #4 #3 #2]
+-/
+#guard_msgs in
+grind_pattern Array.getElem_push_lt => (a.push x)
+
+class Foo (α : Type) (β : outParam Type) where
+  a : Unit
+
+class Boo (α : Type) (β : Type) where
+  b : β
+
+def f [Foo α β] [Boo α β] (a : α) : (α × β) :=
+  (a, Boo.b α)
+
+instance [Foo α β] : Foo (List α) (Array β) where
+  a := ()
+
+instance [Boo α β] : Boo (List α) (Array β) where
+  b := #[Boo.b α]
+
+theorem fEq [Foo α β] [Boo α β] (a : List α) : (f a).1 = a := rfl
+
+/-- info: [grind.pattern] fEq: [@f ? ? ? ? #0] -/
+#guard_msgs in
+grind_pattern fEq => f a
+
+theorem fEq2 [Foo α β] [Boo α β] (a : List α) (_h : a.length > 5) : (f a).1 = a := rfl
+
+/-- info: [grind.pattern] fEq2: [@f ? ? ? ? #1] -/
+#guard_msgs in
+grind_pattern fEq2 => f a
+
+def g [Boo α β] (a : α) : (α × β) :=
+  (a, Boo.b α)
+
+theorem gEq [Boo α β] (a : List α) : (g (β := Array β) a).1 = a := rfl
+
+/--
+error: invalid pattern(s) for `gEq`
+  [@g ? ? ? #0]
+the following theorem parameters cannot be instantiated:
+  β : Type
+  inst✝ : Boo α β
+---
+info: [grind.pattern] gEq: [@g ? ? ? #0]
+-/
+#guard_msgs in
+grind_pattern gEq => g a
+
+def plus (a : Nat) (b : Nat) := a + b
+
+theorem hThm1 (h : b > 10) : plus a b + plus a c > 10 := by
+  unfold plus; omega
+
+/--
+error: invalid pattern(s) for `hThm1`
+  [plus #2 #3]
+the following theorem parameters cannot be instantiated:
+  c : Nat
+---
+info: [grind.pattern] hThm1: [plus #2 #3]
+-/
+#guard_msgs in
+grind_pattern hThm1 => plus a b
+
+/--
+error: invalid pattern(s) for `hThm1`
+  [plus #2 #1]
+the following theorem parameters cannot be instantiated:
+  b : Nat
+  h : b > 10
+---
+info: [grind.pattern] hThm1: [plus #2 #1]
+-/
+#guard_msgs in
+grind_pattern hThm1 => plus a c
+
+/-- info: [grind.pattern] hThm1: [plus #2 #1, plus #2 #3] -/
+#guard_msgs in
+grind_pattern hThm1 => plus a c, plus a b

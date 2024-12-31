8dbbcf2652be7064d1bd24a85e219c3732b19d53
Leonardo de Moura <leomoura@amazon.com>
2024-12-30 17:13:53 -0800

    test: ematch

---
+++ tests/lean/run/grind_ematch1.lean
@@ -0,0 +1,26 @@
+set_option trace.grind.ematch.pattern true
+grind_pattern Array.get_set_eq  => a.set i v h
+grind_pattern Array.get_set_ne => (a.set i v)[j]
+
+-- Trace instances of the theorems above found using ematching
+
+set_option trace.grind.ematch.instance true
+
+/--
+info: [grind.ematch.instance] Array.get_set_eq : [α, bs, j, w, Lean.Grind.nestedProof (j < bs.toList.length) h₂]
+[grind.ematch.instance] Array.get_set_eq : [α, as, i, v, Lean.Grind.nestedProof (i < as.toList.length) h₁]
+[grind.ematch.instance] Array.get_set_ne : [α, bs, j, _, i, w, _, _]
+-/
+-- #guard_msgs (info) in
+example (as : Array α)
+        (i : Nat)
+        (h₁ : i < as.size)
+        (h₂ : bs = as.set i v)
+        (_  : ds = bs)
+        (h₂ : j < bs.size)
+        (h₃ : cs = bs.set j w)
+        (h₄ : i ≠ j)
+        (h₅ : i < cs.size)
+        : p ↔ (cs[i] = as[i]) := by
+  fail_if_success grind
+  sorry

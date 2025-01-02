f2e5159d2578a66221992eb07f0758743db2e743
Henrik Böving <hargonix@gmail.com>
2025-01-02 18:34:16 +0100

    doc: memory behavior of repeating timers

--- src/Std/Internal/UV.lean
+++ src/Std/Internal/UV.lean
@@ -67,7 +67,7 @@ This creates a `Timer` in the initial state and doesn't run it yet.
   milliseconds, counting from when it's run.
 - If `repeating` is `true` this constructs a timer that resolves after multiples of `durationMs`
   milliseconds, counting from when it's run. Note that this includes the 0th multiple right after
-  starting the timer.
+  starting the timer. Furthermore a repeating timer will only be freed after `Timer.stop` is called.
 -/
 @[extern "lean_uv_timer_mk"]
 opaque mk (timeout : UInt64) (repeating : Bool) : IO Timer

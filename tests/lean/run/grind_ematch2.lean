5e9a48fa35cdff85238a651ecc5843d6b6e95085
Leonardo de Moura <leomoura@amazon.com>
2025-01-02 17:06:52 +0100

    fix: missing case in `checkParents` (#6508)
    This PR fixes a bug in the sanity checkers for the `grind` tactic. See
    the new test for an example of a case where it was panicking.

---
+++ tests/lean/run/grind_ematch2.lean
@@ -0,0 +1,19 @@
+grind_pattern Array.size_set => Array.set a i v h
+grind_pattern Array.get_set_eq  => a.set i v h
+grind_pattern Array.get_set_ne => (a.set i v hi)[j]
+
+set_option grind.debug true
+set_option trace.grind.ematch.pattern true
+set_option trace.grind.ematch.instance true
+
+example (as bs cs : Array α) (v₁ v₂ : α)
+        (i₁ i₂ j : Nat)
+        (h₁ : i₁ < as.size)
+        (h₂ : bs = as.set i₁ v₁)
+        (h₃ : i₂ < bs.size)
+        (h₃ : cs = bs.set i₂ v₂)
+        (h₄ : i₁ ≠ j ∧ i₂ ≠ j)
+        (h₅ : j < cs.size)
+        (h₆ : j < as.size)
+        : cs[j] = as[j] := by
+  grind

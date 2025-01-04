4a80008fc2e77b56ec442044b87ee3e6fddfe401
Kim Morrison <kim@tqft.net>
2025-01-04 14:57:51 +1100

    fix

--- src/Lean/Linter/MissingDocs.lean
+++ src/Lean/Linter/MissingDocs.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro
 -/
 prelude
+import Lean.Parser.Syntax
 import Lean.Meta.Tactic.Simp.RegisterCommand
 import Lean.Elab.Command
 import Lean.Elab.SetOption

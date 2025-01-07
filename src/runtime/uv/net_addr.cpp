1cca09da5644f05b290b412324cef47b39c79edd
Henrik Böving <hargonix@gmail.com>
2025-01-07 21:02:53 +0100

    feat: finish the C++

--- src/runtime/uv/net_addr.cpp
+++ src/runtime/uv/net_addr.cpp
@@ -17,11 +17,11 @@ namespace lean {
 #ifndef LEAN_EMSCRIPTEN
 
 void lean_ipv4_addr_to_in_addr(b_obj_arg ipv4_addr, struct in_addr* out) {
-    uint8_t ipv4_addr_array[4];
+    out->s_addr = 0;
     for (int i = 0; i < 4; i++) {
-        ipv4_addr_array[i] = (uint8_t)lean_unbox(array_uget(ipv4_addr, i));
+        uint32_t octet = (uint32_t)(uint8_t)lean_unbox(array_uget(ipv4_addr, i));
+        out->s_addr |= octet << (3 - i) * 8;
     }
-    memcpy(&out->s_addr, ipv4_addr_array, 4);
     out->s_addr = htonl(out->s_addr);
 }
 
@@ -34,11 +34,30 @@ void lean_ipv6_addr_to_in6_addr(b_obj_arg ipv6_addr, struct in6_addr* out) {
 }
 
 lean_obj_res lean_in_addr_to_ipv4_addr(const struct in_addr* ipv4_addr) {
+    obj_res ret = alloc_array(0, 4);
+    uint32_t hostaddr = ntohl(ipv4_addr->s_addr);
 
+    for (int i = 0; i < 4; i++) {
+        uint8_t octet = (uint8_t)(hostaddr >> (3 - i) * 8);
+        array_push(ret, lean_box(octet));
+    }
+
+    lean_assert(array_size(ret) == 4);
+    return ret;
 }
 
 lean_obj_res lean_in6_addr_to_ipv6_addr(const struct in6_addr* ipv6_addr) {
+    obj_res ret = alloc_array(0, 8);
 
+    for (int i = 0; i < 16; i += 2) {
+        uint16_t part1 = (uint16_t)ipv6_addr->s6_addr[i];
+        uint16_t part2 = (uint16_t)ipv6_addr->s6_addr[i + 1];
+        uint16_t segment = ntohs((part2 << 8) | part1);
+        array_push(ret, lean_box(segment));
+    }
+
+    lean_assert(array_size(ret) == 8);
+    return ret;
 }
 
 /* Std.Net.IPV4Addr.ofString (s : @&String) : Option IPV4Addr */

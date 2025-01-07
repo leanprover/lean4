958c3a23f464599105c0455073ca72f3d471f850
Sebastian Ullrich <sebasti@nullri.ch>
2025-01-07 08:58:47 +0000

    back out macOS x64

--- script/prepare-llvm-macos.sh
+++ script/prepare-llvm-macos.sh
@@ -53,6 +53,12 @@ else
   echo -n " -DCMAKE_C_COMPILER=$PWD/llvm-host/bin/clang -DLEANC_OPTS='--sysroot $PWD/stage1 -resource-dir $PWD/stage1/lib/clang/15.0.1 ${EXTRA_FLAGS:-}'"
 fi
 echo -n " -DLEANC_INTERNAL_FLAGS='-nostdinc -isystem ROOT/include/clang' -DLEANC_CC=ROOT/bin/clang"
-echo -n " -DLEANC_INTERNAL_LINKER_FLAGS='-L ROOT/lib -L ROOT/lib/libc -fuse-ld=lld --ld-path=ROOT/bin/ld64.lld'"
+if [[ $(uname -a) == "x86_64" ]]; then
+  # `--ld-path` creates some platform_version troubles on macOS x64 but this is not a high-prio platform anymore and
+  # its users are not that likely to have conflicting `lld`s in their `PATH`
+  echo -n " -DLEANC_INTERNAL_LINKER_FLAGS='-L ROOT/lib -L ROOT/lib/libc -fuse-ld=lld'"
+else
+  echo -n " -DLEANC_INTERNAL_LINKER_FLAGS='-L ROOT/lib -L ROOT/lib/libc -fuse-ld=lld --ld-path=ROOT/bin/ld64.lld'"
+endif
 # do not set `LEAN_CC` for tests
 echo -n " -DLEAN_TEST_VARS=''"

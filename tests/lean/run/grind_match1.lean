b32797795aa54069e122ee61163f15800fbad870
Leonardo de Moura <leomoura@amazon.com>
2025-01-07 18:45:10 -0800

    fix: missed split

--- tests/lean/run/grind_match1.lean
+++ tests/lean/run/grind_match1.lean
@@ -24,15 +24,16 @@ info: [grind.assert] (match as, bs with
 [grind.assert] a₁ :: f 0 = as
 [grind.assert] f 0 = a₂ :: f 1
 [grind.assert] ¬d = []
+[grind.assert] Lean.Grind.EqMatch
+      (match a₁ :: a₂ :: f 1, [] with
+      | [], x => bs
+      | head :: head_1 :: tail, [] => []
+      | x :: xs, ys => x :: g xs ys)
+      []
 [grind.split.resolved] match as, bs with
     | [], x => bs
     | head :: head_1 :: tail, [] => []
     | x :: xs, ys => x :: g xs ys
-[grind.assert] (match a₁ :: a₂ :: f 1, [] with
-      | [], x => bs
-      | head :: head_1 :: tail, [] => []
-      | x :: xs, ys => x :: g xs ys) =
-      []
 -/
 #guard_msgs (info) in
 example (f : Nat → List Nat) : g as bs = d → bs = [] → a₁ :: f 0 = as → f 0 = a₂ :: f 1 → d = [] := by

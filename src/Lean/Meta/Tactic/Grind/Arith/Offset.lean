bee5e59ee894ee5dd15ab13dce8cb9da5a02fe44
Leonardo de Moura <leomoura@amazon.com>
2025-01-11 09:07:46 -0800

    chore: move auxiliary functions used to construct proofs

--- src/Lean/Meta/Tactic/Grind/Arith/Offset.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Offset.lean
@@ -6,6 +6,7 @@ Authors: Leonardo de Moura
 prelude
 import Init.Grind.Offset
 import Lean.Meta.Tactic.Grind.Types
+import Lean.Meta.Tactic.Grind.Arith.ProofUtil
 
 namespace Lean.Meta.Grind.Arith.Offset
 /-!
@@ -28,63 +29,6 @@ y ≤ x + k
 The main advantage of this module over a full linear integer arithmetic procedure is
 its ability to efficiently detect all implied equalities and inequalities.
 -/
-
-/-- `Eq.refl true` -/
-def rfl_true : Expr := mkApp2 (mkConst ``Eq.refl [levelOne]) (mkConst ``Bool) (mkConst ``Bool.true)
-
-open Lean.Grind in
-/--
-Assume `pi₁` is `{ w := u, k := k₁, proof := p₁ }` and `pi₂` is `{ w := w, k := k₂, proof := p₂ }`
-`p₁` is the proof for edge `u -(k₁) → w` and `p₂` the proof for edge `w -(k₂)-> v`.
-Then, this function returns a proof for edge `u -(k₁+k₂) -> v`.
--/
-private def mkTrans (nodes : PArray Expr) (pi₁ : ProofInfo) (pi₂ : ProofInfo) (v : NodeId) : ProofInfo :=
-  let { w := u, k := k₁, proof := p₁ } := pi₁
-  let { w, k := k₂, proof := p₂ } := pi₂
-  let u := nodes[u]!
-  let w := nodes[w]!
-  let v := nodes[v]!
-  let p := if k₁ == 0 then
-    if k₂ == 0 then
-      -- u ≤ w, w ≤ v
-      mkApp5 (mkConst ``Nat.le_trans) u w v p₁ p₂
-    else if k₂ < 0 then
-      -- u ≤ v, w ≤ v + k₂
-      mkApp6 (mkConst ``Nat.le_ro) u w v (toExpr k₂.toNat) p₁ p₂
-    else
-      -- u ≤ w, w + k₂ ≤ v
-      mkApp6 (mkConst ``Nat.le_lo) u w v (toExpr k₂.toNat) p₁ p₂
-  else if k₁ > 0 then
-    if k₂ == 0 then
-      mkApp6 (mkConst ``Nat.lo_le) u w v (toExpr k₁.toNat) p₁ p₂
-    else if k₂ > 0 then
-      if k₁ > k₂ then
-        mkApp7 (mkConst ``Nat.lo_lo_1) u w v (toExpr k₁.toNat) (toExpr k₂.toNat) p₁ p₂
-      else
-        mkApp7 (mkConst ``Nat.lo_lo_2) u w v (toExpr k₁.toNat) (toExpr k₂.toNat) p₁ p₂
-    else
-     let k₂ := -k₂
-     let ke₁ := toExpr k₁.toNat
-     let ke₂ := toExpr k₂.toNat
-     if k₁ > k₂ then
-       mkApp8 (mkConst ``Nat.lo_ro_1) u w v ke₁ ke₂ rfl_true p₁ p₂
-     else
-       mkApp7 (mkConst ``Nat.lo_ro_2) u w v ke₁ ke₂ p₁ p₂
-  else
-    let k₁ := -k₁
-    let ke₁ := toExpr k₁.toNat
-    if k₂ == 0 then
-      mkApp6 (mkConst ``Nat.ro_le) u w v ke₁ p₁ p₂
-    else if k₂ > 0 then
-      let ke₂ := toExpr k₂.toNat
-       if k₂ > k₁ then
-         mkApp7 (mkConst ``Nat.ro_lo_1) u w v ke₁ ke₂ p₁ p₂
-       else
-         mkApp8 (mkConst ``Nat.ro_lo_2) u w v ke₁ ke₂ rfl_true p₁ p₂
-    else
-      let ke₂ := toExpr (-k₂).toNat
-      mkApp7 (mkConst ``Nat.ro_ro) u w v ke₁ ke₂ p₁ p₂
-  { w := pi₁.w, k := k₁+k₂, proof := p }
 
 abbrev OffsetM := StateRefT State GoalM
 

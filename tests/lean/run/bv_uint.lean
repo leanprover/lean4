e417c1dc98ebac8edcb3e7994c63780bb3f48db7
Henrik Böving <hargonix@gmail.com>
2025-01-20 10:12:38 +0100

    feat: correctly identify UInt counter examples as non spurious

--- tests/lean/run/bv_uint.lean
+++ tests/lean/run/bv_uint.lean
@@ -1,7 +1,59 @@
 import Std.Tactic.BVDecide
 
+/-! UInt8 -/
 example (a b : UInt8) : a + b = b + a := by
   bv_decide
 
+/--
+error: The prover found a counterexample, consider the following assignment:
+a.toBitVec = 0xff#8
+b.toBitVec = 0xff#8
+-/
+#guard_msgs in
 example (a b : UInt8) : a + b = a := by
+  bv_decide
+
+
+
+/-! UInt16 -/
+example (a b : UInt16) : a + b = b + a := by
+  bv_decide
+
+/--
+error: The prover found a counterexample, consider the following assignment:
+a.toBitVec = 0xffff#16
+b.toBitVec = 0xffff#16
+-/
+#guard_msgs in
+example (a b : UInt16) : a + b = a := by
+  bv_decide
+
+
+
+/-! UInt32 -/
+example (a b : UInt32) : a + b = b + a := by
+  bv_decide
+
+/--
+error: The prover found a counterexample, consider the following assignment:
+a.toBitVec = 0xffffffff#32
+b.toBitVec = 0xffffffff#32
+-/
+#guard_msgs in
+example (a b : UInt32) : a + b = a := by
+  bv_decide
+
+
+
+/-! UInt64 -/
+example (a b : UInt64) : a + b = b + a := by
+  bv_decide
+
+/--
+error: The prover found a counterexample, consider the following assignment:
+a.toBitVec = 0xffffffffffffffff#64
+b.toBitVec = 0xffffffffffffffff#64
+-/
+#guard_msgs in
+example (a b : UInt64) : a + b = a := by
   bv_decide

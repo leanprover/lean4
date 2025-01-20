92e44ffdb006c4402b0bbe009657fff2daa2af34
Kim Morrison <kim@tqft.net>
2025-01-20 14:49:18 +1100

    fix prelude

--- src/Init/Data/Vector/MapIdx.lean
+++ src/Init/Data/Vector/MapIdx.lean
@@ -3,6 +3,7 @@ Copyright (c) 2025 Lean FRO. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kim Morrison
 -/
+prelude
 import Init.Data.Array.MapIdx
 import Init.Data.Vector.Lemmas
 

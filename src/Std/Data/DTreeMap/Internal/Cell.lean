00aadc651b4c384699c46b69ef7695bf9dffcb42
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-01-22 22:51:11 +0100

    add prelude declarations

--- src/Std/Data/DTreeMap/Internal/Cell.lean
+++ src/Std/Data/DTreeMap/Internal/Cell.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Markus Himmel
+-/
+prelude
 import Std.Data.Classes.TransOrd
 import Std.Data.Classes.LawfulEqOrd
 

92da043cf93287cb369a2b90990604218cdb69cf
arthur-adjedj <arthur.adjedj@gmail.com>
2025-01-27 13:36:24 +0000

    chore: use #guard_msgs instead of #check in test

--- tests/lean/run/6789.lean
+++ tests/lean/run/6789.lean
@@ -29,8 +29,21 @@ def Extension.recOn'
 | _, _, .zero => zero
 | _, _, .succ xt A => succ xt A (Extension.recOn' zero succ xt)
 
-#check Extension.recOn'.eq_1
-#check Extension.recOn'.eq_2
+
+/--
+info: equations:
+theorem Extension.recOn'.eq_1.{v} : ∀ {motive : (Γ Δ : Con) → Extension Γ Δ → Sort v}
+  (zero : {Γ : Con} → motive Γ Γ Extension.zero)
+  (succ : {Γ Δ : Con} → (xt : Extension Γ Δ) → (A : Nat) → motive Γ Δ xt → motive Γ (Δ.ext A) (xt.succ A)) (x : Con),
+  Extension.recOn' zero succ Extension.zero = zero
+theorem Extension.recOn'.eq_2.{v} : ∀ {motive : (Γ Δ : Con) → Extension Γ Δ → Sort v}
+  (zero : {Γ : Con} → motive Γ Γ Extension.zero)
+  (succ : {Γ Δ : Con} → (xt : Extension Γ Δ) → (A : Nat) → motive Γ Δ xt → motive Γ (Δ.ext A) (xt.succ A)) (x Δ : Con)
+  (A : Nat) (xt : Extension x Δ),
+  Extension.recOn' zero succ (xt.succ A) = succ xt A (Extension.recOn' (fun {Γ} => zero) (fun {Γ Δ} => succ) xt)
+-/
+#guard_msgs in
+#print equations Extension.recOn'
 
 def Extension.pullback_con
   : (xt : Extension B Δ) → (σ : Op B' B)
@@ -38,5 +51,12 @@ def Extension.pullback_con
 | .zero, σ => B'
 | .succ xt A, σ => .ext (pullback_con xt σ) A
 
-#check Extension.pullback_con.eq_1
-#check Extension.pullback_con.eq_2
+/--
+info: equations:
+theorem Extension.pullback_con.eq_1.{u} : ∀ {Op : Con → Con → Type u} {B B' : Con} (x : Op B' B),
+  Extension.zero.pullback_con x = B'
+theorem Extension.pullback_con.eq_2.{u} : ∀ {Op : Con → Con → Type u} {B B' : Con} (x : Op B' B) (Δ_2 : Con)
+  (xt : Extension B Δ_2) (A : Nat), (xt.succ A).pullback_con x = (xt.pullback_con x).ext A
+-/
+#guard_msgs in
+#print equations Extension.pullback_con

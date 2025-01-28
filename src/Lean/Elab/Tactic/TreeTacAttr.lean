a12244e4d3501b80c657ffd6f2cffc767ed3a1bd
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-01-28 18:24:22 +0100

    cleanup

--- src/Lean/Elab/Tactic/TreeTacAttr.lean
+++ src/Lean/Elab/Tactic/TreeTacAttr.lean
@@ -1,7 +1,12 @@
+/-
+Copyright (c) 2022 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Reichert
+-/
 prelude
 import Lean.Meta.Tactic.Simp
 
-open Lean Elab Tactic Meta
+open Lean.Meta
 
-builtin_initialize treeTacExt : Meta.SimpExtension
-  ← Meta.registerSimpAttr `Std.Internal.tree_tac "simp theorems used by internal DTreeMap lemmas"
+builtin_initialize treeTacExt : SimpExtension
+  ← registerSimpAttr `Std.Internal.tree_tac "simp theorems used by internal DTreeMap lemmas"

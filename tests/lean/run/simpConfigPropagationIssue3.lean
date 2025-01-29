77492031ea6601bdbba425e983eb69ad68df256a
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-01-29 09:45:16 +0100

    remove duplicated lemmas

--- tests/lean/run/simpConfigPropagationIssue3.lean
+++ tests/lean/run/simpConfigPropagationIssue3.lean
@@ -1,12 +1,3 @@
-namespace Ordering
-
-@[simp] theorem swap_swap {o : Ordering} : o.swap.swap = o := by cases o <;> rfl
-
-@[simp] theorem swap_inj {o₁ o₂ : Ordering} : o₁.swap = o₂.swap ↔ o₁ = o₂ :=
-  ⟨fun h => by simpa using congrArg swap h, congrArg _⟩
-
-end Ordering
-
 /-- `OrientedCmp cmp` asserts that `cmp` is determined by the relation `cmp x y = .lt`. -/
 class OrientedCmp (cmp : α → α → Ordering) : Prop where
   /-- The comparator operation is symmetric, in the sense that if `cmp x y` equals `.lt` then
@@ -31,6 +22,6 @@ open OrientedCmp
 
 theorem ge_trans (h₁ : cmp x y ≠ .lt) (h₂ : cmp y z ≠ .lt) : cmp x z ≠ .lt := by
   have := @TransCmp.le_trans _ cmp _ z y x
-  simp [cmp_eq_gt] at *
+  simp? [cmp_eq_gt] at *
   -- `simp` did not fire at `this`
   exact this h₂ h₁

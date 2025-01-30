2fedd7144aff67f65ee95736d6fa8a1f9319ebc4
Leonardo de Moura <leomoura@amazon.com>
2025-01-30 08:00:18 -0800

    feat: normalize `!=` and `==` in `grind` (#6870)
    This PR adds two new normalization steps in `grind` that reduces `a !=
    b` and `a == b` to `decide (¬ a = b)` and `decide (a = b)`,
    respectively.

--- tests/lean/run/grind_bool_prop.lean
+++ tests/lean/run/grind_bool_prop.lean
@@ -17,3 +17,17 @@ example (f : Bool → Nat) : f (!a) = 0 → a = false → f true = 0 := by grind
 example (f : Bool → Nat) : (!a) = c → c = true → f a = 0 → f false = 0 := by grind (splits := 0)
 example (f : Bool → Nat) : (!a) = c → c = false → f a = 0 → f true = 0 := by grind (splits := 0)
 example : (!a) = c → c = a → False := by grind (splits := 0)
+
+example (as bs : List Nat) (f : Prop → Nat) : f (as = bs) = 0 → as = [] → bs = b :: bs' → f False = 0 := by grind (splits := 0)
+example (as bs : List Nat) (f : Bool → Nat) : f (as == bs) = 0 → as = [] → bs = b :: bs' → f false = 0 := by grind (splits := 0)
+example (as bs : List Nat) : (as == bs) = c → c = true → as = bs := by grind (splits := 0)
+example (as bs : List Nat) : (as == bs) = c → c = true → as = cs → bs = cs := by grind (splits := 0)
+example (a b : Nat) : (a == b, c) = d → d = (true, true) → a = b := by grind (splits := 0)
+
+example (as bs : List Nat) (f : Bool → Nat) : f (as != bs) = 0 → as = [] → bs = b :: bs' → f true = 0 := by grind (splits := 0)
+example (as bs : List Nat) : (as != bs) = c → c = true → as ≠ bs := by grind (splits := 0)
+example (a b : Nat) : (a != b, c) = d → d = (false, true) → a = b := by grind (splits := 0)
+example (a b : Bool) : (a ^^ b, c) = d → d = (false, true) → a = b := by grind (splits := 0)
+example (a b : Bool) : (a == b, c) = d → d = (true, true) → a = true → true = b := by grind (splits := 0)
+
+example (h : α = β) (a : α) (b : β) : h ▸ a = b → HEq a b := by grind

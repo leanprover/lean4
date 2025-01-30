ce311f8f6324001bf97acf24b300548d0958ce28
luisacicolini <lc985@cam.ac.uk>
2025-01-30 23:08:06 +0000

    chore: fix typo and fix build

--- src/Init/Data/BitVec/Bitblast.lean
+++ src/Init/Data/BitVec/Bitblast.lean
@@ -1245,7 +1245,7 @@ theorem saddOverflow_eq {w : Nat} (x y : BitVec w) :
   · have := le_toInt (x := x); have := toInt_lt (x := x)
     have := le_toInt (x := y); have := toInt_lt (x := y)
     have := toInd_add_toInt_lt_two_pow (x := x) (y := y)
-    have := neg_two_pow_le_toInd_add_toInt (x := x) (y := y)
+    have := neg_two_pow_le_toInt_add_toInt (x := x) (y := y)
     have bmod_neg_iff {m : Nat} {x : Int} (h2 : -m ≤ x) (h1 : x < m) :
         (x.bmod m) < 0 ↔ (-(m / 2) ≤ x ∧ x < 0) ∨ ((m + 1) / 2 ≤ x) := by
       simp only [Int.bmod_def]

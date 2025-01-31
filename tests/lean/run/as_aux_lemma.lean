19679a3e6479a7836799621219a562b40ac6c75b
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-01-31 08:56:28 +0100

    tests

--- tests/lean/run/as_aux_lemma.lean
+++ tests/lean/run/as_aux_lemma.lean
@@ -1,49 +1,42 @@
+import Lean.Environment
 import Lean.Meta.Basic
 import Lean.Util.NumObjs
-import Lean.Elab.Tactic
-import Init.Tactics
 
-open Lean Meta Elab Tactic Parser.Tactic
+open Lean Meta
 
-elab_rules : tactic
-| `(tactic| as_aux_lemma => $s) =>
-  liftMetaTactic fun mvarId => do
-    let (mvars, _) ← runTactic mvarId s
-    unless mvars.isEmpty do
-      throwError "Cannot abstract term into auxiliary lemma because there are open goals."
-    let e ← instantiateMVars (mkMVar mvarId)
-    let e ← mkAuxTheorem (`Lean.Elab.Tactic.AsAuxLemma ++ (← mkFreshUserName `auxLemma)) (← mvarId.getType) e
-    mvarId.assign e
-    return []
+abbrev testProp := ∀ n : Nat, n = 0 + n
 
-namespace Test
-
-def thm : ∀ n : Nat, n = 0 + n := by as_aux_lemma =>
+def thmUsingTactic : testProp := by as_aux_lemma =>
   intro n
   induction n
   · rfl
   · next ih =>
     rw [← Nat.succ_eq_add_one, Nat.add_succ, ← ih]
 
-end Test
+def thmWithoutTactic : testProp := by
+  intro n
+  induction n
+  · rfl
+  · next ih =>
+    rw [← Nat.succ_eq_add_one, Nat.add_succ, ← ih]
 
 open Lean
 
-def size (ns : Name) : MetaM Nat := do
+def size (name : Name) : MetaM Nat := do
   let env ← getEnv
   let mut totalSize : Nat := 0
-  for (name, info) in env.constants do
-    if ns.isPrefixOf name then
+  for (constantName, info) in env.constants do
+    if name = constantName then
       if let some e := info.value? then
         let numObjs ← e.numObjs
         totalSize := totalSize + numObjs
   return totalSize
 
 /--
-info: theorem smaller than auxiliary lemma: true
+info: as_aux_lemma makes proof term smaller : true
 -/
 #guard_msgs in
 run_meta do
-  let thmSize ← size `Test
-  let auxSize ← size `Lean.Elab.Tactic.AsAuxLemma
-  logInfo m!"theorem smaller than auxiliary lemma: {decide $ thmSize < auxSize}"
+  let sizeUsingTactic ← size `thmUsingTactic
+  let sizeWithoutTactic ← size `thmWithoutTactic
+  logInfo m!"as_aux_lemma makes proof term smaller : {decide <| sizeUsingTactic < sizeWithoutTactic}"

05edc0a3658b4c49bfcf3d507cc830072128ba34
Joachim Breitner <mail@joachim-breitner.de>
2025-02-01 17:59:03 +0100

    More test files

---
+++ tests/lean/run/wfUnfold.lean
@@ -0,0 +1,43 @@
+def fib : Nat → Nat
+  | 0 => 0
+  | 1 => 1
+  | n+2 => fib n + fib (n+1)
+decreasing_by all_goals sorry
+
+/--
+error: tactic 'fail' failed
+⊢ fib 8 + fib (8 + 1) = 55
+-/
+#guard_msgs in
+example : fib 10 = 55 := by
+  unfold fib
+  fail
+
+def ack : Nat → Nat → Nat
+  | 0, m => m + 1
+  | n+1, 0 => ack n 1
+  | n+1, m+1 => ack n (ack (n+1) m)
+decreasing_by all_goals sorry
+
+/--
+error: tactic 'fail' failed
+⊢ ack 1 (ack (1 + 1) 1) = 7
+-/
+#guard_msgs in
+example : ack 2 2 = 7 := by
+  unfold ack
+  fail
+
+/--
+error: tactic 'fail' failed
+α : Type u_1
+as : Array α
+i : Nat
+j : Fin as.size
+⊢ (insertIdx.loop._unary i ⟨as, j⟩).size = as.size
+-/
+#guard_msgs in
+@[simp] private theorem Array.size_insertIdx_loop (as : Array α) (i : Nat) (j : Fin as.size) :
+    (insertIdx.loop i as j).size = as.size := by
+  unfold insertIdx.loop
+  fail

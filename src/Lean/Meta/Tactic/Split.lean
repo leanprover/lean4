32caf9aff12a9daa97f7623eed56261067a18262
Joachim Breitner <mail@joachim-breitner.de>
2025-02-01 17:13:56 +0100

    refactor: simpMatch to not etaStruct
    This PR changes the `simpMatch` function, used inside the equation generator for
    WF-rec functions, to not do eta-expansion.
    
    This makes the process a bit more robust and disciplined, and avoids removing
    match-statements (and introduce projections and dependencies) that we'd rather
    split instead.

--- src/Lean/Meta/Tactic/Split.lean
+++ src/Lean/Meta/Tactic/Split.lean
@@ -17,7 +17,7 @@ def getSimpMatchContext : MetaM Simp.Context := do
    Simp.mkContext
       (simpTheorems   := {})
       (congrTheorems := (← getSimpCongrTheorems))
-      (config        := { Simp.neutralConfig with dsimp := false })
+      (config        := { Simp.neutralConfig with dsimp := false, etaStruct := .none })
 
 def simpMatch (e : Expr) : MetaM Simp.Result := do
   let discharge? ← SplitIf.mkDischarge?

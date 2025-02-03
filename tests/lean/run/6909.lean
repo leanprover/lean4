d974fef8e3464666653c8b40841e51a34819e655
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-02-03 02:22:10 +0000

    add test

---
+++ tests/lean/run/6909.lean
@@ -0,0 +1,16 @@
+abbrev Multiseries : List Nat → Type
+  | [] => Nat
+  | _ :: _ => Int
+
+opaque maxExp (n : Nat) (ns : List Nat) (li : List (Multiseries (n :: ns))) : Nat
+
+@[simp]
+axiom bad_simp_lemma (n : Nat) (ns : List Nat) : maxExp n ns [] = 8
+
+example (n : Nat) (ns : List Nat) : maxExp n ns [] = 8 := by
+  simp -- previously, simp made no progress
+
+/-- error: simp made no progress -/
+#guard_msgs in
+example (n : Nat) (ns : List Nat) : maxExp n ns [] = 8 := by
+  simp -iota -- previously, succeeded

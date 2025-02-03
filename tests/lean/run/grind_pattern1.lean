c1c8afd15ca06c49964d2a7e5a6b444f1fa8f006
Leonardo de Moura <leomoura@amazon.com>
2025-02-02 18:19:21 -0800

    chore: improve `grind` pattern pretty printer

--- tests/lean/run/grind_pattern1.lean
+++ tests/lean/run/grind_pattern1.lean
@@ -1,18 +1,20 @@
 set_option trace.grind.ematch.pattern true
 
 /--
-info: [grind.ematch.pattern] Array.getElem_push_lt: [@getElem ? `[Nat] #4 ? ? (@Array.push ? #3 #2) #1 ?]
+info: [grind.ematch.pattern] Array.getElem_push_lt: [@getElem _ `[Nat] #4 _ _ (@Array.push _ #3 #2) #1 _]
 -/
 #guard_msgs in
 grind_pattern Array.getElem_push_lt => (a.push x)[i]
 
 
-/-- info: [grind.ematch.pattern] List.getElem_attach: [@getElem ? `[Nat] ? ? ? (@List.attach #3 #2) #1 ?] -/
+/--
+info: [grind.ematch.pattern] List.getElem_attach: [@getElem _ `[Nat] _ _ _ (@List.attach #3 #2) #1 _]
+-/
 #guard_msgs in
 grind_pattern List.getElem_attach => xs.attach[i]
 
 /--
-info: [grind.ematch.pattern] List.mem_concat_self: [@Membership.mem #2 ? ? (@HAppend.hAppend ? ? ? ? #1 (@List.cons ? #0 (@List.nil ?))) #0]
+info: [grind.ematch.pattern] List.mem_concat_self: [@Membership.mem #2 _ _ (@HAppend.hAppend _ _ _ _ #1 (@List.cons _ #0 (@List.nil _))) #0]
 -/
 #guard_msgs in
 grind_pattern List.mem_concat_self => a ∈ xs ++ [a]
@@ -52,13 +54,13 @@ instance [Boo α β] : Boo (List α) (Array β) where
 
 theorem fEq [Foo α β] [Boo α β] (a : List α) : (f a).1 = a := rfl
 
-/-- info: [grind.ematch.pattern] fEq: [@f ? ? ? ? #0] -/
+/-- info: [grind.ematch.pattern] fEq: [@f _ _ _ _ #0] -/
 #guard_msgs in
 grind_pattern fEq => f a
 
 theorem fEq2 [Foo α β] [Boo α β] (a : List α) (_h : a.length > 5) : (f a).1 = a := rfl
 
-/-- info: [grind.ematch.pattern] fEq2: [@f ? ? ? ? #1] -/
+/-- info: [grind.ematch.pattern] fEq2: [@f _ _ _ _ #1] -/
 #guard_msgs in
 grind_pattern fEq2 => f a
 
@@ -69,12 +71,12 @@ theorem gEq [Boo α β] (a : List α) : (g (β := Array β) a).1 = a := rfl
 
 /--
 error: invalid pattern(s) for `gEq`
-  [@g ? ? ? #0]
+  [@g _ _ _ #0]
 the following theorem parameters cannot be instantiated:
   β : Type
   inst✝ : Boo α β
 ---
-info: [grind.ematch.pattern] gEq: [@g ? ? ? #0]
+info: [grind.ematch.pattern] gEq: [@g _ _ _ #0]
 -/
 #guard_msgs in
 grind_pattern gEq => g a

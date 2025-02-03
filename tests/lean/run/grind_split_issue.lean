1abac9aca687170a5897de63fbc8402ff0315035
Leonardo de Moura <leomoura@amazon.com>
2025-02-02 20:00:45 -0800

    chore: improve `grind` case-split trace (#6911)

--- tests/lean/run/grind_split_issue.lean
+++ tests/lean/run/grind_split_issue.lean
@@ -23,7 +23,7 @@ h : HEq ⋯ ⋯
   [eqc] Equivalence classes
     [eqc] {s, 0}
   [cases] Case analyses
-    [cases] [2]: X c 0
+    [cases] [1/2]: X c 0
 [grind] Issues
   [issue] #1 other goal(s) were not fully processed due to previous failures, threshold: `(failures := 1)`
 -/

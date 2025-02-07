8c297201e0f806d49fdac48554e911b8a2732c69
Sebastian Ullrich <sebasti@nullri.ch>
2025-02-07 17:12:31 +0100

    chore: re-enable `Elab.async` in the server (#6990)

--- src/Lean/Server/FileWorker.lean
+++ src/Lean/Server/FileWorker.lean
@@ -353,7 +353,7 @@ def setupImports (meta : DocumentMeta) (cmdlineOpts : Options) (chanOut : Std.Ch
   let opts := cmdlineOpts.mergeBy (fun _ _ fileOpt => fileOpt) fileSetupResult.fileOptions
 
   -- default to async elaboration; see also `Elab.async` docs
-  --let opts := Elab.async.setIfNotSet opts true
+  let opts := Elab.async.setIfNotSet opts true
 
   return .ok {
     mainModuleName

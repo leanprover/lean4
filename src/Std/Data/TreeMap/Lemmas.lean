886465e2d571cf2e87e7a940b4546cdb011254ac
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-02-07 09:43:41 +0100

    treemap lemmas

--- src/Std/Data/TreeMap/Lemmas.lean
+++ src/Std/Data/TreeMap/Lemmas.lean
@@ -22,8 +22,21 @@ namespace Std.TreeMap
 
 variable {α : Type u} {β : Type v} {cmp : α → α → Ordering} {t : TreeMap α β cmp}
 
+private theorem ext {t t' : TreeMap α β cmp} : t.inner = t'.inner → t = t' := by
+  cases t; cases t'; rintro rfl; rfl
+
+@[simp]
 theorem isEmpty_empty : (empty : TreeMap α β cmp).isEmpty :=
   DTreeMap.isEmpty_empty
+
+@[simp]
+theorem isEmpty_emptyc : (∅ : TreeMap α β cmp).isEmpty :=
+  DTreeMap.isEmpty_empty
+
+@[simp]
+theorem isEmpty_insert [TransCmp cmp] {k : α} {v : β} :
+    (t.insert k v).isEmpty = false :=
+  DTreeMap.isEmpty_insert
 
 theorem mem_iff_contains {k : α} : k ∈ t ↔ t.contains k :=
   DTreeMap.mem_iff_contains
@@ -35,19 +48,92 @@ theorem contains_congr [TransCmp cmp] {k k' : α} (hab : cmp k k' == .eq) :
 theorem mem_congr [TransCmp cmp] {k k' : α} (hab : cmp k k' == .eq) : k ∈ t ↔ k' ∈ t :=
   DTreeMap.mem_congr hab
 
+@[simp]
 theorem contains_empty {k : α} : (empty : TreeMap α β cmp).contains k = false :=
   DTreeMap.contains_empty
 
-theorem mem_empty {k : α} : k ∉ (empty : TreeMap α β cmp) :=
-  DTreeMap.mem_empty
+@[simp]
+theorem not_mem_empty {k : α} : k ∉ (empty : TreeMap α β cmp) :=
+  DTreeMap.not_mem_empty
 
-theorem isEmpty_insert [TransCmp cmp] {k : α} {v : β} :
-    (t.insert k v).isEmpty = false :=
-  DTreeMap.isEmpty_insert
+@[simp]
+theorem contains_emptyc {k : α} : (∅ : TreeMap α β cmp).contains k = false :=
+  DTreeMap.contains_empty
 
+@[simp]
+theorem not_mem_emptyc {k : α} : k ∉ (∅ : TreeMap α β cmp) :=
+  DTreeMap.not_mem_empty
+
+theorem contains_of_isEmpty [TransCmp cmp] {a : α} :
+    t.isEmpty → t.contains a = false :=
+  DTreeMap.contains_of_isEmpty
+
+theorem not_mem_of_isEmpty [TransCmp cmp] {a : α} :
+    t.isEmpty → a ∉ t :=
+  DTreeMap.not_mem_of_isEmpty
+
+theorem isEmpty_eq_false_iff_exists_contains_eq_true [TransCmp cmp] :
+    t.isEmpty = false ↔ ∃ a, t.contains a = true :=
+  DTreeMap.isEmpty_eq_false_iff_exists_contains_eq_true
+
+theorem isEmpty_eq_false_iff_exists_mem [TransCmp cmp] :
+    t.isEmpty = false ↔ ∃ a, a ∈ t :=
+  DTreeMap.isEmpty_eq_false_iff_exists_mem
+
+theorem isEmpty_iff_forall_contains [TransCmp cmp] :
+    t.isEmpty = true ↔ ∀ a, t.contains a = false :=
+  DTreeMap.isEmpty_iff_forall_contains
+
+theorem isEmpty_iff_forall_not_mem [TransCmp cmp] :
+    t.isEmpty = true ↔ ∀ a, ¬a ∈ t :=
+  DTreeMap.isEmpty_iff_forall_not_mem
+
+@[simp]
+theorem insert_eq_insert {p : α × β} : Insert.insert p t = t.insert p.1 p.2 :=
+  rfl
+
+@[simp]
+theorem singleton_eq_insert {p : α × β} :
+    Singleton.singleton p = (∅ : TreeMap α β cmp).insert p.1 p.2 :=
+  rfl
+
+@[simp]
 theorem contains_insert [h : TransCmp cmp] {k a : α} {v : β} :
     (t.insert k v).contains a = (cmp k a == .eq || t.contains a) :=
   DTreeMap.contains_insert
+
+@[simp]
+theorem mem_insert [TransCmp cmp] {k a : α} {v : β} :
+    a ∈ t.insert k v ↔ cmp k a == .eq ∨ a ∈ t :=
+  DTreeMap.mem_insert
+
+theorem contains_insert_self [TransCmp cmp] {k : α} {v : β} :
+    (t.insert k v).contains k :=
+  DTreeMap.contains_insert_self
+
+theorem mem_insert_self [TransCmp cmp] {k : α} {v : β} :
+    k ∈ t.insert k v :=
+  DTreeMap.mem_insert_self
+
+theorem contains_of_contains_insert [TransCmp cmp] {k a : α} {v : β} :
+    (t.insert k v).contains a → (cmp k a == .eq) = false → t.contains a :=
+  DTreeMap.contains_of_contains_insert
+
+theorem mem_of_mem_insert [TransCmp cmp] {k a : α} {v : β} :
+    a ∈ t.insert k v → (cmp k a == .eq) = false → a ∈ t :=
+  DTreeMap.mem_of_mem_insert
+
+@[simp]
+theorem size_empty : (empty : TreeMap α β cmp).size = 0 :=
+  DTreeMap.size_empty
+
+@[simp]
+theorem size_emptyc : (∅ : TreeMap α β cmp).size = 0 :=
+  DTreeMap.size_empty
+
+theorem isEmpty_eq_size_eq_zero :
+    t.isEmpty = (t.size == 0) :=
+  DTreeMap.isEmpty_eq_size_eq_zero
 
 theorem size_insert [TransCmp cmp] {k : α} {v : β} :
     (t.insert k v).size = if t.contains k then t.size else t.size + 1 :=
@@ -61,17 +147,38 @@ theorem size_insert_le [TransCmp cmp] {k : α} {v : β} :
     (t.insert k v).size ≤ t.size + 1 :=
   DTreeMap.size_insert_le
 
+@[simp]
+theorem erase_empty {k : α} :
+    (empty : TreeMap α β cmp).erase k = empty :=
+  ext <| DTreeMap.erase_empty
+
+@[simp]
+theorem erase_emptyc {k : α} :
+    (∅ : TreeMap α β cmp).erase k = empty :=
+  erase_empty
+
+@[simp]
 theorem isEmpty_erase [TransCmp cmp] {k : α} :
     (t.erase k).isEmpty = (t.isEmpty || (t.size == 1 && t.contains k)) :=
   DTreeMap.isEmpty_erase
 
+@[simp]
 theorem contains_erase [TransCmp cmp] {k a : α} :
     (t.erase k).contains a = (cmp k a != .eq && t.contains a) :=
   DTreeMap.contains_erase
 
+@[simp]
+theorem mem_erase [TransCmp cmp] {k a : α} :
+    a ∈ t.erase k ↔  (cmp k a == .eq) = false ∧ a ∈ t :=
+  DTreeMap.mem_erase
+
 theorem contains_of_contains_erase [TransCmp cmp] {k a : α} :
     (t.erase k).contains a → t.contains a :=
   DTreeMap.contains_of_contains_erase
+
+theorem mem_of_mem_erase [TransCmp cmp] {k a : α} :
+    a ∈ t.erase k → a ∈ t :=
+  DTreeMap.mem_of_mem_erase
 
 theorem size_erase [TransCmp cmp] {k : α} :
     (t.erase k).size = if t.contains k then t.size - 1 else t.size :=
@@ -84,5 +191,61 @@ theorem size_erase_le [TransCmp cmp] {k : α} :
 theorem size_le_size_erase [TransCmp cmp] {k : α} :
     t.size ≤ (t.erase k).size + 1 :=
   DTreeMap.size_le_size_erase
+
+@[simp]
+theorem isEmpty_insertIfNew [TransCmp cmp] {k : α} {v : β} :
+    (t.insertIfNew k v).isEmpty = false :=
+  DTreeMap.isEmpty_insertIfNew
+
+@[simp]
+theorem contains_insertIfNew [TransCmp cmp] {k a : α} {v : β} :
+    (t.insertIfNew k v).contains a = (cmp k a == .eq || t.contains a) :=
+  DTreeMap.contains_insertIfNew
+
+@[simp]
+theorem mem_insertIfNew [TransCmp cmp] {k a : α} {v : β} :
+    a ∈ t.insertIfNew k v ↔ cmp k a == .eq ∨ a ∈ t :=
+  DTreeMap.mem_insertIfNew
+
+@[simp]
+theorem contains_insertIfNew_self [TransCmp cmp] {k : α} {v : β} :
+    (t.insertIfNew k v).contains k :=
+  DTreeMap.contains_insertIfNew_self
+
+theorem mem_insertIfNew_self [TransCmp cmp] {k : α} {v : β} :
+    k ∈ t.insertIfNew k v :=
+  DTreeMap.mem_insertIfNew_self
+
+theorem contains_of_contains_insertIfNew [TransCmp cmp] {k a : α} {v : β} :
+    (t.insertIfNew k v).contains a → (cmp k a == .eq) = false → t.contains a :=
+  DTreeMap.contains_of_contains_insertIfNew
+
+theorem mem_of_mem_insertIfNew [TransCmp cmp] {k a : α} {v : β} :
+    a ∈ t.insertIfNew k v → (cmp k a == .eq) = false → a ∈ t :=
+  DTreeMap.contains_of_contains_insertIfNew
+
+/-- This is a restatement of `contains_of_contains_insertIfNew` that is written to exactly match
+the proof obligation in the statement of `get_insertIfNew`. -/
+theorem contains_of_contains_insertIfNew' [TransCmp cmp] {k a : α} {v : β} :
+    (t.insertIfNew k v).contains a → ¬((cmp k a == .eq) ∧ t.contains k = false) → t.contains a :=
+  DTreeMap.contains_of_contains_insertIfNew'
+
+/-- This is a restatement of `mem_of_mem_insertIfNew` that is written to exactly match
+the proof obligation in the statement of `get_insertIfNew`. -/
+theorem mem_of_mem_insertIfNew' [TransCmp cmp] {k a : α} {v : β} :
+    a ∈ t.insertIfNew k v → ¬((cmp k a == .eq) ∧ ¬k ∈ t) → a ∈ t :=
+  DTreeMap.mem_of_mem_insertIfNew'
+
+theorem size_insertIfNew [TransCmp cmp] {k : α} {v : β} :
+    (t.insertIfNew k v).size = if k ∈ t then t.size else t.size + 1 :=
+  DTreeMap.size_insertIfNew
+
+theorem size_le_size_insertIfNew [TransCmp cmp] {k : α} {v : β} :
+    t.size ≤ (t.insertIfNew k v).size :=
+  DTreeMap.size_le_size_insertIfNew
+
+theorem size_insertIfNew_le [TransCmp cmp] {k : α} {v : β} :
+    (t.insertIfNew k v).size ≤ t.size + 1 :=
+  DTreeMap.size_insertIfNew_le
 
 end Std.TreeMap

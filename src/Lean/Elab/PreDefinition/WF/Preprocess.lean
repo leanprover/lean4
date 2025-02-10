4016a80f66ff93748b51face7f8777f87f756918
Joachim Breitner <mail@joachim-breitner.de>
2025-02-10 17:43:41 +0100

    feat: nested well-founded recursion via automatic preprocessing (#6744)
    This PR extend the preprocessing of well-founded recursive definitions
    to bring assumptions like `h✝ : x ∈ xs` into scope automatically.
    
    This fixes #5471, and follows (roughly) the design written there.
    See the module docs at `src/Lean/Elab/PreDefinition/WF/AutoAttach.lean`
    for details on the implementation.
    
    This only works for higher-order functions that have a suitable setup.
    See for example section “Well-founded recursion preprocessing setup” in
    `src/Init/Data/List/Attach.lean`.
    
    This does not change the `decreasing_tactic`, so in some cases there is
    still the need for a manual termination proof some cases. We expect a
    better termination tactic in the near future.

--- src/Lean/Elab/PreDefinition/WF/Preprocess.lean
+++ src/Lean/Elab/PreDefinition/WF/Preprocess.lean
@@ -1,42 +1,140 @@
 /-
-Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
+Authors: Joachim Breitner
 -/
 prelude
 import Lean.Meta.Transform
-import Lean.Elab.RecAppSyntax
+import Lean.Meta.Match.MatcherApp.Basic
+import Lean.Elab.Tactic.Simp
+
+/-!
+This module implements the preprocessing of function definitions involved in well-founded recursion.
+The goal is to change higher order functions to add more information to the context, e.g. change
+`List.map (fun x => …) xs` to `List.map (fun ⟨x, h⟩ => …) xs.attach`.  This extra information can
+then be used by the termination proof tactic to determine that a recursive call is indeed
+decreasing.
+
+The process proceeds in these steps, to guide the transformation:
+
+1. The parameters of the function are annotated with the `wfParam` gadget.
+
+   We could be more selective here and only annotate those that have a `SizeOf` instance.
+   We cannot (easily) only consider the parameters that appear in the termination measure, as that
+   is not known yet.
+
+
+2. The `wfParam` gadget is pushed around:
+
+   - `f (wfParam x) ==> wfParam (f x)` if `f` is a projection
+
+   - `match (wfParam x) with con y => alt[y] ==> match x with con y => alt[wfParam y]`
+
+     In a match with multiple targets it suffices for any of the targets to be annotated with
+     `wfParam`, and all parameters of the match arms are annotated with `wfParam`. This is an
+     overapproximation.
+
+3. The `wf_preprocess` simpset is applied to do the actual transformation. It typically contains two
+   rules for each higher-order function of interest
+
+   - `(wfParam xs).map f = xs.attach.unattach.map f`
+   - `xs.unattach.map f = xs.map (fun ⟨x, h⟩ => binderNameHint x f (f (wfParam x)))`
+
+   The first rule “activates” the call, the second rule actually performs it. They are separated like
+   this so that for chains of the form `(xs.reverse.filter p).map f` the `.attach` is attached
+   to `xs` and we get the basic `x ∈ xs` in the context of `f`.
+
+   The `binderNameHint` preserves the user-chosen name in `f` if that is a lambda.
+
+   The `wfParam` on the right hand side ensurses that doubly-nested recursion works.
+
+4. All left-over `wfParam` gadgets are removed.
+
+The simplifier is used to perform steps 2 (using simprocs) and 3 (using rewrite rules) together.
+
+-/
+
+open Lean Meta
+
+register_builtin_option wf.preprocess : Bool := {
+  defValue := true
+  descr := "pre-process definitions defined by well-founded recursion with the `wf_preprocess` simp set"
+}
 
 namespace Lean.Elab.WF
-open Meta
 
-private def shouldBetaReduce (e : Expr) (recFnNames : Array Name) : Bool :=
-  if e.isHeadBetaTarget then
-    e.getAppFn.find? (fun e => recFnNames.any (e.isConstOf ·)) |>.isSome
+builtin_initialize wfPreprocessSimpExtension : SimpExtension ←
+  registerSimpAttr `wf_preprocess
+    "simp lemma used in the preprocessing of well-founded recursive function definitions, in \
+    particular to add additional hypotheses to the context. Also see `wfParam`."
+
+private def getSimpContext : MetaM Simp.Context := do
+  let simpTheorems ← wfPreprocessSimpExtension.getTheorems
+  Simp.mkContext
+    (simpTheorems  := #[simpTheorems])
+    (congrTheorems := {})
+    (config        := { Simp.neutralConfig with dsimp := false })
+
+def isWfParam? (e : Expr) : Option Expr :=
+  if e.isAppOfArity ``wfParam 2 then
+    e.appArg!
   else
-    false
+    none
 
-/--
-Preprocesses the expressions to improve the effectiveness of `wfRecursion`.
+def mkWfParam (e : Expr) : MetaM Expr :=
+  mkAppM ``wfParam #[e]
 
-* Floats out the RecApp markers.
-  Example:
-  ```
-  def f : Nat → Nat
-    | 0 => 1
-    | i+1 => (f x) i
-  ```
+/-- `f (wfParam x) ==> wfParam (f x)` if `f` is a projection -/
+builtin_dsimproc paramProj (_) := fun e => do
+  if h : e.isApp then
+    let some a' := isWfParam? (e.appArg h) | return .continue
+    let f := e.getAppFn
+    unless f.isConst do return .continue
+    unless (← isProjectionFn f.constName!) do return .continue
+    let e' ← mkWfParam (.app (e.appFn h) a')
+    return .done e'
+  else
+    return .continue
 
-Unlike `Lean.Elab.Structural.preprocess`, do _not_ beta-reduce, as it could
-remove `let_fun`-lambdas that contain explicit termination proofs.
--/
-def preprocess (e : Expr) : CoreM Expr :=
-  Core.transform e
-    (post := fun e => do
-      if e.isApp && e.getAppFn.isMData then
-        let .mdata m f := e.getAppFn | unreachable!
-        if m.isRecApp then
-          return .done (.mdata m (f.beta e.getAppArgs))
-      return .continue)
+/-- `match (wfParam x) with con y => alt[y] ==> match x with con y => alt[wfParam y] -/
+builtin_dsimproc paramMatcher (_) := fun e => do
+  let some matcherApp ← matchMatcherApp? e (alsoCasesOn := true)  | return .continue
+  unless matcherApp.discrs.any (isWfParam? · |>.isSome) do return .continue
+  let discrs' := matcherApp.discrs.map (fun e => isWfParam? e |>.getD e)
+  let alts' ← matcherApp.alts.mapM fun alt =>
+    lambdaTelescope alt fun xs body => do
+      -- Annotate all xs with `wfParam`
+      let xs' ← xs.mapM (mkWfParam ·)
+      let body' := body.replaceFVars xs xs'
+      mkLambdaFVars xs body'
+  let matcherApp' := { matcherApp with discrs := discrs', alts := alts' }
+  return .continue <| matcherApp'.toExpr
+
+
+def preprocess (e : Expr) : MetaM Simp.Result := do
+  unless wf.preprocess.get (← getOptions) do
+    return { expr := e }
+  lambdaTelescope e fun xs _ => do
+    -- Annotate all xs with `wfParam`
+    let xs' ← xs.mapM mkWfParam
+    let e' := e.beta xs'
+
+    -- Now run the simplifier
+    let simprocs : Simprocs := {}
+    let simprocs ← simprocs.add ``paramProj (post := true)
+    let simprocs ← simprocs.add ``paramMatcher (post := false)
+    let (result, _) ← Meta.simp e' (← getSimpContext) (simprocs := #[simprocs])
+
+    -- Remove left-over markers
+    let e'' ← Core.transform result.expr fun e =>
+      e.withApp fun f as => do
+        if f.isConstOf ``wfParam then
+          if h : as.size ≥ 2 then
+            return .continue (mkAppN as[1] as[2:])
+        return .continue
+    let result := { result with expr := e'' }
+
+    trace[Elab.definition.wf] "Attach-introduction:{indentExpr e}\nto{indentExpr e'}\ncleaned up as{indentExpr e''}"
+    result.addLambdas xs
 
 end Lean.Elab.WF
---
+++ tests/lean/run/wf_preprocess.lean
@@ -0,0 +1,276 @@
+set_option Elab.async false -- for stable output order in #guard_msgs
+
+universe u
+structure Tree (α : Type u) where
+  val : α
+  cs : List (Tree α)
+
+def Tree.isLeaf (t : Tree α) := t.cs.isEmpty
+
+/--
+info: α : Type u_1
+t t' : Tree α
+h✝ : t' ∈ t.cs
+⊢ sizeOf t' < sizeOf t
+-/
+#guard_msgs in
+def Tree.map (f : α → β) (t : Tree α) : Tree β :=
+    ⟨f t.val, t.cs.map (fun t' => t'.map f)⟩
+termination_by t
+decreasing_by trace_state; cases t; decreasing_tactic
+
+/--
+info: equations:
+theorem Tree.map.eq_1.{u_1, u_2} : ∀ {α : Type u_1} {β : Type u_2} (f : α → β) (t : Tree α),
+  Tree.map f t = { val := f t.val, cs := List.map (fun t' => Tree.map f t') t.cs }
+-/
+#guard_msgs in
+#print equations Tree.map
+
+/--
+info: Tree.map.induct.{u_1} {α : Type u_1} (motive : Tree α → Prop)
+  (case1 : ∀ (x : Tree α), (∀ (t' : Tree α), t' ∈ x.cs → motive t') → motive x) (t : Tree α) : motive t
+-/
+#guard_msgs in
+#check Tree.map.induct
+
+/--
+info: α : Type u_1
+t x✝ : Tree α
+h✝ : x✝ ∈ t.cs
+⊢ sizeOf x✝ < sizeOf t
+-/
+#guard_msgs in
+def Tree.pruneRevAndMap (f : α → β) (t : Tree α) : Tree β :=
+    ⟨f t.val, (t.cs.filter (fun t' => not t'.isLeaf)).reverse.map (·.pruneRevAndMap f)⟩
+termination_by t
+decreasing_by trace_state; cases t; decreasing_tactic
+
+/--
+info: equations:
+theorem Tree.pruneRevAndMap.eq_1.{u_1, u_2} : ∀ {α : Type u_1} {β : Type u_2} (f : α → β) (t : Tree α),
+  Tree.pruneRevAndMap f t =
+    { val := f t.val,
+      cs := List.map (fun x => Tree.pruneRevAndMap f x) (List.filter (fun t' => !t'.isLeaf) t.cs).reverse }
+-/
+#guard_msgs in
+#print equations Tree.pruneRevAndMap
+
+/--
+info: Tree.pruneRevAndMap.induct.{u_1} {α : Type u_1} (motive : Tree α → Prop)
+  (case1 : ∀ (x : Tree α), (∀ (x_1 : Tree α), x_1 ∈ x.cs → motive x_1) → motive x) (t : Tree α) : motive t
+-/
+#guard_msgs in
+#check Tree.pruneRevAndMap.induct
+
+/--
+info: α : Type u_1
+v : α
+cs : List (Tree α)
+x✝ : Tree α
+h✝ : x✝ ∈ cs
+⊢ sizeOf x✝ < sizeOf { val := v, cs := cs }
+-/
+#guard_msgs in
+def Tree.pruneRevAndMap' (f : α → β) : Tree α → Tree β
+  | ⟨v,cs⟩ => ⟨f v, (cs.filter (fun t' => not t'.isLeaf)).reverse.map (·.pruneRevAndMap' f)⟩
+termination_by t => t
+decreasing_by trace_state; decreasing_tactic
+
+/--
+info: equations:
+theorem Tree.pruneRevAndMap'.eq_1.{u_1, u_2} : ∀ {α : Type u_1} {β : Type u_2} (f : α → β) (v : α) (cs : List (Tree α)),
+  Tree.pruneRevAndMap' f { val := v, cs := cs } =
+    { val := f v, cs := List.map (fun x => Tree.pruneRevAndMap' f x) (List.filter (fun t' => !t'.isLeaf) cs).reverse }
+-/
+#guard_msgs in
+#print equations Tree.pruneRevAndMap'
+
+/--
+info: Tree.pruneRevAndMap'.induct.{u_1} {α : Type u_1} (motive : Tree α → Prop)
+  (case1 : ∀ (v : α) (cs : List (Tree α)), (∀ (x : Tree α), x ∈ cs → motive x) → motive { val := v, cs := cs })
+  (a✝ : Tree α) : motive a✝
+-/
+#guard_msgs in
+#check Tree.pruneRevAndMap'.induct
+
+structure MTree (α : Type u) where
+  val : α
+  cs : Array (List (MTree α))
+
+-- set_option trace.Elab.definition.wf true in
+/--
+warning: declaration uses 'sorry'
+---
+info: α : Type u_1
+t : MTree α
+x✝¹ : List (MTree α)
+h✝¹ : x✝¹ ∈ t.cs
+x✝ : MTree α
+h✝ : x✝ ∈ x✝¹
+⊢ sizeOf x✝ < sizeOf t
+-/
+#guard_msgs in
+def MTree.map (f : α → β) (t : MTree α) : MTree β :=
+    ⟨f t.val, t.cs.map (·.map (·.map f))⟩
+termination_by t
+decreasing_by trace_state; cases t; simp at *; decreasing_tactic; sorry
+
+/--
+info: equations:
+theorem MTree.map.eq_1.{u_1, u_2} : ∀ {α : Type u_1} {β : Type u_2} (f : α → β) (t : MTree α),
+  MTree.map f t = { val := f t.val, cs := Array.map (fun x => List.map (fun x => MTree.map f x) x) t.cs }
+-/
+#guard_msgs in
+#print equations MTree.map
+
+/--
+info: MTree.map.induct.{u_1} {α : Type u_1} (motive : MTree α → Prop)
+  (case1 : ∀ (x : MTree α), (∀ (x_1 : List (MTree α)), x_1 ∈ x.cs → ∀ (x : MTree α), x ∈ x_1 → motive x) → motive x)
+  (t : MTree α) : motive t
+-/
+#guard_msgs in
+#check MTree.map.induct
+
+/--
+info: α : Type u_1
+t : MTree α
+css : List (MTree α)
+h✝¹ : css ∈ t.cs
+c : MTree α
+h✝ : c ∈ css
+⊢ sizeOf c < sizeOf t
+-/
+#guard_msgs in
+def MTree.size (t : MTree α) : Nat := Id.run do
+  let mut s := 1
+  for css in t.cs do
+    for c in css do
+      s := s + c.size
+  pure s
+termination_by t
+decreasing_by
+  trace_state
+  fail_if_success grind -- eventually, grind should be able to handle this
+  cases t
+  have := Array.sizeOf_lt_of_mem ‹_ ∈ _›
+  have := List.sizeOf_lt_of_mem ‹_ ∈ _›
+  simp only [mk.sizeOf_spec, sizeOf_default, Nat.add_zero, gt_iff_lt] at *
+  omega
+
+/--
+info: equations:
+theorem MTree.size.eq_1.{u_1} : ∀ {α : Type u_1} (t : MTree α),
+  t.size =
+    (let s := 1;
+      do
+      let r ←
+        forIn t.cs s fun css r =>
+            let s := r;
+            do
+            let r ←
+              forIn css s fun c r =>
+                  let s := r;
+                  let s := s + c.size;
+                  do
+                  pure PUnit.unit
+                  pure (ForInStep.yield s)
+            let s : Nat := r
+            pure PUnit.unit
+            pure (ForInStep.yield s)
+      let s : Nat := r
+      pure s).run
+-/
+#guard_msgs in
+#print equations MTree.size
+
+/--
+info: MTree.size.induct.{u_1} {α : Type u_1} (motive : MTree α → Prop)
+  (case1 : ∀ (x : MTree α), (∀ (css : List (MTree α)), css ∈ x.cs → ∀ (c : MTree α), c ∈ css → motive c) → motive x)
+  (t : MTree α) : motive t
+-/
+#guard_msgs in
+#check MTree.size.induct
+
+namespace Ex1
+inductive Expression where
+| var: String → Expression
+| object: List (String × Expression) → Expression
+
+/--
+warning: declaration uses 'sorry'
+---
+info: L : List (String × Expression)
+x : String × Expression
+h✝ : x ∈ L
+⊢ sizeOf x.snd < sizeOf (Expression.object L)
+-/
+#guard_msgs in
+def t (exp: Expression) : List String :=
+  match exp with
+  | Expression.var s => [s]
+  | Expression.object L => List.foldl (fun L1 L2 => L1 ++ L2) [] (L.map (fun x => t x.2))
+termination_by exp
+decreasing_by trace_state; sorry
+
+/--
+info: equations:
+theorem Ex1.t.eq_1 : ∀ (s : String), t (Expression.var s) = [s]
+theorem Ex1.t.eq_2 : ∀ (L : List (String × Expression)),
+  t (Expression.object L) = List.foldl (fun L1 L2 => L1 ++ L2) [] (List.map (fun x => t x.snd) L)
+-/
+#guard_msgs in
+#print equations t
+
+/--
+info: Ex1.t.induct (motive : Expression → Prop) (case1 : ∀ (s : String), motive (Expression.var s))
+  (case2 :
+    ∀ (L : List (String × Expression)), (∀ (x : String × Expression), motive x.snd) → motive (Expression.object L))
+  (exp : Expression) : motive exp
+-/
+#guard_msgs in
+#check t.induct
+
+end Ex1
+
+namespace Ex2
+inductive Expression where
+| var: String → Expression
+| object: List (String × Expression) → Expression
+
+/--
+warning: declaration uses 'sorry'
+---
+info: L : List (String × Expression)
+x : String × Expression
+h✝ : x ∈ L
+⊢ sizeOf x.snd < sizeOf (Expression.object L)
+-/
+#guard_msgs in
+def t (exp: Expression) : List String :=
+  match exp with
+  | Expression.var s => [s]
+  | Expression.object L => L.foldl (fun L1 x => L1 ++ t x.2) []
+termination_by exp
+decreasing_by trace_state; sorry
+
+end Ex2
+
+
+namespace WithOptionOff
+
+set_option wf.preprocess false
+
+/--
+error: tactic 'fail' failed
+α : Type u_1
+t t' : Tree α
+⊢ sizeOf t' < sizeOf t
+-/
+#guard_msgs in
+def map (f : α → β) (t : Tree α) : Tree β :=
+    ⟨f t.val, t.cs.map (fun t' => map f t')⟩
+termination_by t
+decreasing_by fail
+
+end WithOptionOff

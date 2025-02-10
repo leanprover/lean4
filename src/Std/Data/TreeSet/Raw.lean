276fadcfa96382f73324a53ca8c7e720448178ca
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-02-10 14:41:35 +0100

    add myself to the list of authors

--- src/Std/Data/TreeSet/Raw.lean
+++ src/Std/Data/TreeSet/Raw.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus Himmel
+Authors: Markus Himmel, Paul Reichert
 -/
 prelude
 import Std.Data.TreeMap.Raw

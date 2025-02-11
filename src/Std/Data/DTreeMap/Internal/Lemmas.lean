b9ee72ce2e0ac1bbf926403b7d614e2889e62b6c
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-02-11 16:30:56 +0100

    fix copyright notice

--- src/Std/Data/DTreeMap/Internal/Lemmas.lean
+++ src/Std/Data/DTreeMap/Internal/Lemmas.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus Himmel
+Authors: Markus Himmel, Paul Reichert
 -/
 prelude
 import Std.Data.HashMap.Basic

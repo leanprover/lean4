c803f65283b8d64e6b8b2765418e9567e4b659ff
Leonardo de Moura <leomoura@amazon.com>
2025-02-12 14:35:37 -0800

    chore: move

---
+++ src/Lean/Meta/Tactic/Grind/Arith/Offset.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Lean.Meta.Tactic.Arith.Offset.Main
+import Lean.Meta.Tactic.Arith.Offset.Proof
+import Lean.Meta.Tactic.Arith.Offset.Util
+import Lean.Meta.Tactic.Arith.Offset.Types

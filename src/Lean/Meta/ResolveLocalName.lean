88c5463fd5d247020b7e6951d8faedf653fd6d16
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-02-13 13:00:57 -0500

    chore: copy copyright header to new file

--- src/Lean/Meta/ResolveLocalName.lean
+++ src/Lean/Meta/ResolveLocalName.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura, Sebastian Ullrich
+-/
 prelude
 import Lean.Meta.Basic
 

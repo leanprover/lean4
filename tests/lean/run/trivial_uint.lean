7c9454edd245c41c58137e7a5a751db730212f4f
Markus Himmel <markus@lean-fro.org>
2025-02-13 09:45:01 +0100

    feat: `UIntX.ofFin` (#7056)
    This PR adds the `UIntX.ofFin` conversion functions.

--- tests/lean/run/trivial_uint.lean
+++ tests/lean/run/trivial_uint.lean
@@ -13,3 +13,10 @@ example : UInt16.ofBitVec 42 = 42 := by simp
 example : UInt32.ofBitVec 42 = 42 := by simp
 example : UInt64.ofBitVec 42 = 42 := by simp
 example : USize.ofBitVec 42 = 42 := by simp
+
+-- TODO: turn into `by simp` when the relevant theory is in place
+example : UInt8.ofFin 42 = 42 := rfl
+example : UInt16.ofFin 42 = 42 := rfl
+example : UInt32.ofFin 42 = 42 := rfl
+example : UInt64.ofFin 42 = 42 := rfl
+example : USize.ofFin 42 = 42 := rfl

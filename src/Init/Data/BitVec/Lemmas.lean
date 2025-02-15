5854ae5912bee7578eb130c55e0bb95557d9999e
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-02-15 07:49:53 +0000

    chore: docstring format
    Co-authored-by: Tobias Grosser <github@grosser.es>

--- src/Init/Data/BitVec/Lemmas.lean
+++ src/Init/Data/BitVec/Lemmas.lean
@@ -895,10 +895,11 @@ Example (w = 10, start := 7, len := 5):
 let x                       =       9 8 7 6 5 4 3 2 1 0
 let x' = x.extractLsb' 3 4  =   _ _ 9 8 7
                                 |   |
-                                |   x'.getMsbD (i := 2) = x.getMsbD (i := w - (start + len - i) = 10 - (7 + 5 - 2))
-                                |                       = x.getMsbD 0
-                                |                       ✅ start + len - i ≤ w
-                                |                          7 + 5 - 2 = 10 ≤ 10
+                                |   x'.getMsbD (i := 2) =
+                                |   x.getMsbD (i := w - (start + len - i) = 10 - (7 + 5 - 2)) =
+                                |   x.getMsbD 0
+                                |   ✅ start + len - i ≤ w
+                                |        7 + 5 - 2 = 10 ≤ 10
                                 |
                                 x'.getMsbD (i := 0) = x.getMsbD (i := w - (start + len - i) = 10 - (7 + 5 - 0))
                                                     = x.getMsbD (i := w - (start + len - i) = x.getMsbD (i := -2) -- in Nat becomes 0

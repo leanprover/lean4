e3e8a5d4c2fa7de240f87e77c31d229d880b0ff3
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2025-02-15 07:51:16 +0000

    cho
    Co-authored-by: Tobias Grosser <github@grosser.es>

--- src/Init/Data/BitVec/Lemmas.lean
+++ src/Init/Data/BitVec/Lemmas.lean
@@ -902,10 +902,11 @@ let x' = x.extractLsb' 7 5  =   _ _ 9 8 7
                                 |   ✅ start + len - i ≤ w
                                 |        7 + 5 - 2 = 10 ≤ 10
                                 |
-                                x'.getMsbD (i := 0) = x.getMsbD (i := w - (start + len - i) = 10 - (7 + 5 - 0))
-                                                    = x.getMsbD (i := w - (start + len - i) = x.getMsbD (i := -2) -- in Nat becomes 0
-                                                    ❌ start + len - i ≤ w
-                                                      7 + 5 - 0 ≤ w
+                                x'.getMsbD (i := 0) =
+                                x.getMsbD (i := w - (start + len - i) = 10 - (7 + 5 - 0)) =
+                                x.getMsbD (i := w - (start + len - i) = x.getMsbD (i := -2) -- in Nat becomes 0
+                                ❌ start + len - i ≤ w
+                                     7 + 5 - 0 ≤ w
 -/
 @[simp] theorem getMsbD_extractLsb' {start len : Nat} {x : BitVec w} {i : Nat} :
     (extractLsb' start len x).getMsbD i =

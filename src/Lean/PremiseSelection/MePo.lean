15edf24eae58076165560168045da90b84458dfa
Kim Morrison <kim@tqft.net>
2025-02-15 11:05:11 +1100

    fix

--- src/Lean/PremiseSelection/MePo.lean
+++ src/Lean/PremiseSelection/MePo.lean
@@ -21,10 +21,10 @@ open Lean
 
 namespace Lean.PremiseSelection.MePo
 
-def symbolRarity (env : Environment) : NameMap Nat := Id.run do
+def symbolFrequency (env : Environment) : NameMap Nat := Id.run do
   let mut map := {}
   for (_, ci) in env.constants do
-    for n' in ci.getUsedConstantsAsSet do
+    for n' in ci.type.getUsedConstantsAsSet do
       map := map.insert n' (map.findD n' 0 + 1)
   return map
 
@@ -44,7 +44,7 @@ def frequencyScore (frequency : Name → Nat) (relevant candidate : NameSet) : F
 def unweightedScore (relevant candidate : NameSet) : Float := weightedScore (fun _ => 1) relevant candidate
 
 -- The values of p := 0.6 and c := 2.4 are taken from the MePo paper, and need to be tuned.
-def mepo (initialRelevant : NameSet) (accept : ConstantInfo → CoreM Bool) (p : Float := 0.6) (c : Float := 2.4) : CoreM (Array (Name × Float)) := do
+def mepo (initialRelevant : NameSet) (score : NameSet → NameSet → Float) (accept : ConstantInfo → CoreM Bool) (p : Float := 0.6) (c : Float := 2.4) : CoreM (Array (Name × Float)) := do
   let env ← getEnv
   let mut p := p
   let mut candidates := #[]
@@ -52,9 +52,9 @@ def mepo (initialRelevant : NameSet) (accept : ConstantInfo → CoreM Bool) (p :
   let mut accepted : Array (Name × Float) := {}
   for (n, ci) in env.constants do
     if ← accept ci then
-      candidates := candidates.push (n, ci.getUsedConstantsAsSet)
+      candidates := candidates.push (n, ci.type.getUsedConstantsAsSet)
   while candidates.size > 0 do
-    let (newAccepted, candidates') := candidates.map (fun (n, c) => (n, c, unweightedScore relevant c)) |>.partition fun (_, _, s) => p ≤ s
+    let (newAccepted, candidates') := candidates.map (fun (n, c) => (n, c, score relevant c)) |>.partition fun (_, _, s) => p ≤ s
     if newAccepted.isEmpty then return accepted
     accepted := newAccepted.foldl (fun acc (n, _, s) => acc.push (n, s)) accepted
     candidates := candidates'.map fun (n, c, _) => (n, c)
@@ -70,9 +70,16 @@ def _root_.Lean.MVarId.getConstants (g : MVarId) : MetaM NameSet := withContext 
 
 end MePo
 
-def mepo (g : MVarId) (_ : Config) : MetaM (Array Suggestion) := do
+open MePo
+
+def mepo (useRarity : Bool) (g : MVarId) : MetaM (Array Suggestion) := do
   let constants ← g.getConstants
   let env ← getEnv
+  let score := if useRarity then
+    let frequency := symbolFrequency env
+    frequencyScore (frequency.findD · 0)
+  else
+    unweightedScore
   let accept := fun ci => return ! (`Lean).isPrefixOf ci.name && Lean.Meta.allowCompletion env ci.name
-  let suggestions ← MePo.mepo constants accept
+  let suggestions ← MePo.mepo constants score accept
   return suggestions.map fun (n, s) => { name := n, score := s }
--- tests/lean/run/premise_selection_mepo.lean
+++ tests/lean/run/premise_selection_mepo.lean
@@ -1,6 +1,17 @@
 import Lean.PremiseSelection.MePo
 
-set_premise_selector Lean.PremiseSelection.mepo
+set_premise_selector fun g _ => Lean.PremiseSelection.mepo (useRarity := false) g
+
+example (a b : Int) : a + b = b + a := by
+  suggest_premises
+  sorry
+
+#time
+example (x y z : List Int) : x ++ y ++ z = x ++ (y ++ z) := by
+  suggest_premises
+  sorry
+
+set_premise_selector fun g _ => Lean.PremiseSelection.mepo (useRarity := true) g
 
 example (a b : Int) : a + b = b + a := by
   suggest_premises

8bc51eac4412b0fa9f42269a6df584daca313a1f
Leonardo de Moura <leomoura@amazon.com>
2025-02-15 16:07:07 -0800

    feat: `Cutsat/Types.lean`

---
+++ src/Lean/Meta/Tactic/Grind/Arith/Cutsat/Types.lean
@@ -0,0 +1,26 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Init.Data.Int.Linear
+import Lean.Data.PersistentArray
+import Lean.Meta.Tactic.Grind.ENodeKey
+import Lean.Meta.Tactic.Grind.Arith.Util
+
+namespace Lean.Meta.Grind.Arith.Cutsat
+
+open Int.Linear (Var Poly RelCnstr DvdCnstr)
+
+/-- State of the cutsat procedure. -/
+structure State where
+  /-- Mapping from variables to their denotations. -/
+  denote : PArray Expr
+  /--
+  Mapping from variables to divisibility constraints. Recall that we keep the divisibility constraint in solved form.
+  Thus, we have at most one divisibility per variable. -/
+  dvdCnstrs : PArray (Option DvdCnstr) := {}
+  deriving Inhabited
+
+end Lean.Meta.Grind.Arith.Cutsat

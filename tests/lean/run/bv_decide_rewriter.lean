a11b10b8d6abf66df36335fbb3f7b1868e8ac739
luisacicolini <luisacicolini@gmail.com>
2025-02-17 09:33:59 +0000

    chore: fix example and format

--- tests/lean/run/bv_decide_rewriter.lean
+++ tests/lean/run/bv_decide_rewriter.lean
@@ -90,19 +90,15 @@ example (x y : BitVec w) : BitVec.uaddOverflow x y = (x.setWidth (w + 1) + y.set
 example (x y : BitVec w) : BitVec.saddOverflow x y = (x.msb = y.msb ∧ ¬(x + y).msb = x.msb) := by bv_normalize
 example (x y : BitVec 16) : BitVec.umulOverflow x y = (BitVec.twoPow 32 16 ≤ x.zeroExtend (32) * y.zeroExtend (32)) := by bv_normalize
 example (x y : BitVec 16) : BitVec.smulOverflow x y =
-          ((BitVec.signExtend (16 * 2) (BitVec.intMax 16)).slt
-              (BitVec.signExtend (16 * 2) x * BitVec.signExtend (16 * 2) y) ||
-            (BitVec.signExtend (16 * 2) x * BitVec.signExtend (16 * 2) y).slt
-              (BitVec.signExtend (16 * 2) (BitVec.intMin 16)))
-  := by rw [BitVec.smulOverflow_eq]; bv_normalize
+    ((BitVec.signExtend (16 * 2) (BitVec.intMax 16)).slt (BitVec.signExtend (16 * 2) x * BitVec.signExtend (16 * 2) y) ||
+    (BitVec.signExtend (16 * 2) x * BitVec.signExtend (16 * 2) y).slt (BitVec.signExtend (16 * 2) (BitVec.intMin 16))) :=
+  by bv_normalize
 example (x y : BitVec w) : BitVec.umulOverflow x y = (0 < w && BitVec.twoPow (w * 2) w ≤ x.zeroExtend (w * 2) * y.zeroExtend (w * 2)) := by bv_normalize
 example (x y : BitVec w) : BitVec.smulOverflow x y =
-       (decide (w ≠ 0) &&
-            ((BitVec.signExtend (w * 2) (BitVec.intMax w)).slt
-                (BitVec.signExtend (w * 2) x * BitVec.signExtend (w * 2) y) ||
-              (BitVec.signExtend (w * 2) x * BitVec.signExtend (w * 2) y).slt
-                (BitVec.signExtend (w * 2) (BitVec.intMin w))))
-       := by bv_normalize
+    (decide (w ≠ 0) &&
+    ((BitVec.signExtend (w * 2) (BitVec.intMax w)).slt (BitVec.signExtend (w * 2) x * BitVec.signExtend (w * 2) y) ||
+    (BitVec.signExtend (w * 2) x * BitVec.signExtend (w * 2) y).slt (BitVec.signExtend (w * 2) (BitVec.intMin w))))
+  := by bv_normalize
 
 
 

c292ee8cd820bc92f87cba9929c237f8eec0b56d
luisacicolini <luisacicolini@gmail.com>
2025-02-17 23:13:52 +0000

    chore: check simp onlys

--- src/Init/Data/BitVec/Bitblast.lean
+++ src/Init/Data/BitVec/Bitblast.lean
@@ -1258,11 +1258,11 @@ theorem saddOverflow_eq {w : Nat} (x y : BitVec w) :
 theorem umulOverflow_eq {w : Nat} (x y : BitVec w) :
     umulOverflow x y =
       (0 < w && BitVec.twoPow (w * 2) w ≤ x.zeroExtend (w * 2) * y.zeroExtend (w * 2)) := by
-  simp [umulOverflow, ge_iff_le, truncate_eq_setWidth, le_def, toNat_twoPow, toNat_mul,
-    toNat_setWidth, Nat.mul_mod_mod, Nat.mod_mul_mod]
+  simp only [umulOverflow, toNat_twoPow, le_def, toNat_mul, toNat_setWidth, mod_mul_mod]
   rcases w with _|w
-  · simp [of_length_zero, toInt_zero]
-  · simp only [show 0 < w + 1 by omega, decide_true, Bool.true_and, decide_eq_decide]
+  · simp [of_length_zero, toInt_zero, mul_mod_mod]
+  · simp only [ge_iff_le, show 0 < w + 1 by omega, decide_true, mul_mod_mod, Bool.true_and,
+      decide_eq_decide]
     rw [Nat.mod_eq_of_lt BitVec.toNat_mul_toNat_lt, Nat.mod_eq_of_lt]
     have := Nat.pow_lt_pow_of_lt (a := 2) (n := w + 1) (m := (w + 1) * 2)
     omega

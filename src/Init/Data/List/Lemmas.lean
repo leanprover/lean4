8795bad93b46e5d660dfec9c239e2aaf4e075854
Violeta Hernández <vi.hdz.p@gmail.com>
2025-02-17 13:50:41 -0600

    Update Lemmas.lean

--- src/Init/Data/List/Lemmas.lean
+++ src/Init/Data/List/Lemmas.lean
@@ -929,7 +929,7 @@ theorem getElem_zero_eq_head (l : List α) (h : 0 < l.length) :
   | nil => simp at h
   | cons _ _ => simp
 
-theorem head_iff_head?_eq_some {xs : List α} (h) : xs.head h = a ↔ xs.head? = some a := by
+theorem head_eq_iff_head?_eq_some {xs : List α} (h) : xs.head h = a ↔ xs.head? = some a := by
   cases xs with
   | nil => simp at h
   | cons x xs => simp

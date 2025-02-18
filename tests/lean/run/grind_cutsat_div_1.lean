83646352d8492036087fa06b16001cbd67899f43
Leonardo de Moura <leomoura@amazon.com>
2025-02-18 13:19:51 -0800

    test: normalizer

--- tests/lean/run/grind_cutsat_div_1.lean
+++ tests/lean/run/grind_cutsat_div_1.lean
@@ -8,7 +8,7 @@ theorem ex₁ (a : Int) (h₁ : 2 ∣ a) (h₂ : 2 ∣ 2*a + 1 - a) : False := b
 theorem ex₂ (a b : Int) (h₀ : 2 ∣ a + 1) (h₁ : 2 ∣ b + a) (h₂ : 2 ∣ b + 2*a) : False := by
   grind
 
-theorem ex₃ (a b : Int) (_ : 2 ∣ a + 1) (h₁ : 3 ∣ 3*b + 2*a) (h₂ : 2 ∣ 2*b + a + 3) (h₃ : 3 ∣ 3 * b + 2 * a + 1): False := by
+theorem ex₃ (a b : Int) (_ : 2 ∣ a + 1) (h₁ : 3 ∣ a + 3*b + a) (h₂ : 2 ∣ 3*b + a + 3 - b) (h₃ : 3 ∣ 3 * b + 2 * a + 1) : False := by
   grind
 
 #print ex₁

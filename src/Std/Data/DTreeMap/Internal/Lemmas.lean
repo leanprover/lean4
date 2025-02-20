a3a99d387564d67a495f14dbac3c02eebeed44b8
Paul Reichert <datokrat@users.noreply.github.com>
2025-02-20 16:33:41 +0100

    feat: more tree map lemmas about empty, isEmpty, contains, size, insert, erase (#7161)
    This PR adds all missing tree map lemmas about the interactions of the
    functions `empty`, `isEmpty`, `contains`, `size`, `insert(IfNew)` and
    `erase`.
    
    ---------
    
    Co-authored-by: Paul Reichert <6992158+datokrat@users.noreply.github.com>

--- src/Std/Data/DTreeMap/Internal/Lemmas.lean
+++ src/Std/Data/DTreeMap/Internal/Lemmas.lean
@@ -112,4 +112,266 @@ theorem isEmpty_insertIfNew! [TransOrd α] (h : t.WF) {k : α} {v : β k} :
     (t.insertIfNew! k v).isEmpty = false := by
   simp_to_model [insertIfNew!] using List.isEmpty_insertEntryIfNew
 
+theorem contains_empty {a : α} : (empty : Impl α β).contains a = false := by
+  simp [contains, empty]
+
+theorem not_mem_empty {a : α} : a ∉ (empty : Impl α β) := by
+  simp [mem_iff_contains, contains_empty]
+
+theorem contains_of_isEmpty [TransOrd α] (h : t.WF) {a : α} :
+    t.isEmpty → t.contains a = false := by
+  simp_to_model; empty
+
+theorem not_mem_of_isEmpty [TransOrd α] (h : t.WF) {a : α} :
+    t.isEmpty → a ∉ t := by
+  simpa [mem_iff_contains] using contains_of_isEmpty h
+
+theorem isEmpty_eq_false_iff_exists_contains_eq_true [TransOrd α] (h : t.WF) :
+    t.isEmpty = false ↔ ∃ a, t.contains a = true := by
+  simp_to_model using List.isEmpty_eq_false_iff_exists_containsKey
+
+theorem isEmpty_eq_false_iff_exists_mem [TransOrd α] (h : t.WF) :
+    t.isEmpty = false ↔ ∃ a, a ∈ t := by
+  simpa [mem_iff_contains] using isEmpty_eq_false_iff_exists_contains_eq_true h
+
+theorem isEmpty_iff_forall_contains [TransOrd α] (h : t.WF) :
+    t.isEmpty = true ↔ ∀ a, t.contains a = false := by
+  simp_to_model using List.isEmpty_iff_forall_containsKey
+
+theorem isEmpty_iff_forall_not_mem [TransOrd α] (h : t.WF) :
+    t.isEmpty = true ↔ ∀ a, ¬a ∈ t := by
+  simpa [mem_iff_contains] using isEmpty_iff_forall_contains h
+
+theorem contains_insert [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insert k v h.balanced).impl.contains a = (compare k a == .eq || t.contains a) := by
+  simp_to_model [insert] using List.containsKey_insertEntry
+
+theorem contains_insert! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insert! k v).contains a = (compare k a == .eq || t.contains a) := by
+  simp_to_model [insert!] using List.containsKey_insertEntry
+
+theorem mem_insert [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ (t.insert k v h.balanced).impl ↔ compare k a = .eq ∨ a ∈ t := by
+  simp [mem_iff_contains, contains_insert h]
+
+theorem mem_insert! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ t.insert! k v ↔ compare k a = .eq ∨ a ∈ t := by
+  simp [mem_iff_contains, contains_insert! h]
+
+theorem contains_insert_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert k v h.balanced).impl.contains k := by
+  simp_to_model [insert] using List.containsKey_insertEntry_self
+
+theorem contains_insert!_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert! k v).contains k := by
+  simp_to_model [insert!] using List.containsKey_insertEntry_self
+
+theorem mem_insert_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    k ∈ (t.insert k v h.balanced).impl := by
+  simpa [mem_iff_contains] using contains_insert_self h
+
+theorem mem_insert!_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    k ∈ t.insert! k v := by
+  simpa [mem_iff_contains] using contains_insert!_self h
+
+theorem contains_of_contains_insert [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insert k v h.balanced).impl.contains a → compare k a ≠ .eq → t.contains a := by
+  rw [← beq_eq_false_iff_ne]
+  simp_to_model [insert] using List.containsKey_of_containsKey_insertEntry
+
+theorem contains_of_contains_insert! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insert! k v).contains a → compare k a ≠ .eq → t.contains a := by
+  rw [← beq_eq_false_iff_ne]
+  simp_to_model [insert!] using List.containsKey_of_containsKey_insertEntry
+
+theorem mem_of_mem_insert [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ (t.insert k v h.balanced).impl → compare k a ≠ .eq → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_insert h
+
+theorem mem_of_mem_insert! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ t.insert! k v → compare k a ≠ .eq → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_insert! h
+
+theorem size_empty : (empty : Impl α β).size = 0 :=
+  rfl
+
+theorem isEmpty_eq_size_eq_zero (h : t.WF) :
+    letI : BEq Nat := instBEqOfDecidableEq
+    t.isEmpty = (t.size == 0) := by
+  simp_to_model
+  rw [Bool.eq_iff_iff, List.isEmpty_iff_length_eq_zero, Nat.beq_eq_true_eq]
+
+theorem size_insert [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert k v h.balanced).impl.size = if t.contains k then t.size else t.size + 1 := by
+  simp_to_model [insert] using List.length_insertEntry
+
+theorem size_insert! [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert! k v).size = if t.contains k then t.size else t.size + 1 := by
+  simp_to_model [insert!] using List.length_insertEntry
+
+theorem size_le_size_insert [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    t.size ≤ (t.insert k v h.balanced).impl.size := by
+  simp_to_model [insert] using List.length_le_length_insertEntry
+
+theorem size_le_size_insert! [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    t.size ≤ (t.insert! k v).size := by
+  simp_to_model [insert!] using List.length_le_length_insertEntry
+
+theorem size_insert_le [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert k v h.balanced).impl.size ≤ t.size + 1 := by
+  simp_to_model [insert] using List.length_insertEntry_le
+
+theorem size_insert!_le [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insert! k v).size ≤ t.size + 1 := by
+  simp_to_model [insert!] using List.length_insertEntry_le
+
+theorem erase_empty {k : α} :
+    ((empty : Impl α β).erase k balanced_empty).impl = empty :=
+  rfl
+
+theorem erase!_empty {k : α} :
+    (empty : Impl α β).erase! k = empty :=
+  rfl
+
+theorem isEmpty_erase [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase k h.balanced).impl.isEmpty = (t.isEmpty || (t.size = 1 && t.contains k)) := by
+  simp_to_model [erase] using List.isEmpty_eraseKey
+
+theorem isEmpty_erase! [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase! k).isEmpty = (t.isEmpty || (t.size = 1 && t.contains k)) := by
+  simp_to_model [erase!] using List.isEmpty_eraseKey
+
+theorem contains_erase [TransOrd α] (h : t.WF) {k a : α} :
+    (t.erase k h.balanced).impl.contains a = (!(compare k a == .eq) && t.contains a) := by
+  simp_to_model [erase] using List.containsKey_eraseKey
+
+theorem contains_erase! [TransOrd α] (h : t.WF) {k a : α} :
+    (t.erase! k).contains a = (!(compare k a == .eq) && t.contains a) := by
+  simp_to_model [erase!] using List.containsKey_eraseKey
+
+theorem mem_erase [TransOrd α] (h : t.WF) {k a : α} :
+    a ∈ (t.erase k h.balanced).impl ↔ compare k a ≠ .eq ∧ a ∈ t := by
+  simpa only [mem_iff_contains, Bool.and_eq_true, Bool.not_eq_true', beq_eq_false_iff_ne]
+    using Bool.eq_iff_iff.mp <| contains_erase h
+
+theorem mem_erase! [TransOrd α] (h : t.WF) {k a : α} :
+    a ∈ t.erase! k ↔  compare k a ≠ .eq ∧ a ∈ t := by
+  simp [mem_iff_contains, contains_erase! h]
+
+theorem contains_of_contains_erase [TransOrd α] (h : t.WF) {k a : α} :
+    (t.erase k h.balanced).impl.contains a → t.contains a := by
+  simp_to_model [erase] using List.containsKey_of_containsKey_eraseKey
+
+theorem contains_of_contains_erase! [TransOrd α] (h : t.WF) {k a : α} :
+    (t.erase! k).contains a → t.contains a := by
+  simp_to_model [erase!] using List.containsKey_of_containsKey_eraseKey
+
+theorem mem_of_mem_erase [TransOrd α] (h : t.WF) {k a : α} :
+    a ∈ (t.erase k h.balanced).impl → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_erase h
+
+theorem mem_of_mem_erase! [TransOrd α] (h : t.WF) {k a : α} :
+    a ∈ t.erase! k → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_erase! h
+
+theorem size_erase [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase k h.balanced).impl.size = if t.contains k then t.size - 1 else t.size := by
+  simp_to_model [erase] using List.length_eraseKey
+
+theorem size_erase! [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase! k).size = if t.contains k then t.size - 1 else t.size := by
+  simp_to_model [erase!] using List.length_eraseKey
+
+theorem size_erase_le [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase k h.balanced).impl.size ≤ t.size := by
+  simp_to_model [erase] using List.length_eraseKey_le
+
+theorem size_erase!_le [TransOrd α] (h : t.WF) {k : α} :
+    (t.erase! k).size ≤ t.size := by
+  simp_to_model [erase!] using List.length_eraseKey_le
+
+theorem size_le_size_erase [TransOrd α] (h : t.WF) {k : α} :
+    t.size ≤ (t.erase k h.balanced).impl.size + 1 := by
+  simp_to_model [erase] using List.length_le_length_eraseKey
+
+theorem size_le_size_erase! [TransOrd α] (h : t.WF) {k : α} :
+    t.size ≤ (t.erase! k).size + 1 := by
+  simp_to_model [erase!] using List.length_le_length_eraseKey
+
+theorem contains_insertIfNew [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insertIfNew k v h.balanced).impl.contains a = (k == a || t.contains a) := by
+  simp_to_model [insertIfNew] using List.containsKey_insertEntryIfNew
+
+theorem contains_insertIfNew! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insertIfNew! k v).contains a = (k == a || t.contains a) := by
+  simp_to_model [insertIfNew!] using List.containsKey_insertEntryIfNew
+
+theorem mem_insertIfNew [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ (t.insertIfNew k v h.balanced).impl ↔ compare k a = .eq ∨ a ∈ t := by
+  simp [mem_iff_contains, contains_insertIfNew, h, beq_eq]
+
+theorem mem_insertIfNew! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ t.insertIfNew! k v ↔ compare k a = .eq ∨ a ∈ t := by
+  simp [mem_iff_contains, contains_insertIfNew!, h, beq_eq]
+
+theorem contains_insertIfNew_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insertIfNew k v h.balanced).impl.contains k := by
+  simp [contains_insertIfNew, h]
+
+theorem contains_insertIfNew!_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insertIfNew! k v).contains k := by
+  simp [contains_insertIfNew!, h]
+
+theorem mem_insertIfNew_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    k ∈ (t.insertIfNew k v h.balanced).impl := by
+  simp [mem_insertIfNew, h]
+
+theorem mem_insertIfNew!_self [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    k ∈ t.insertIfNew! k v := by
+  simp [mem_insertIfNew!, h]
+
+theorem contains_of_contains_insertIfNew [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insertIfNew k v h.balanced).impl.contains a → compare k a ≠ .eq → t.contains a := by
+  rw [← beq_eq_false_iff_ne]
+  simp_to_model [insertIfNew] using List.containsKey_of_containsKey_insertEntryIfNew
+
+theorem contains_of_contains_insertIfNew! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    (t.insertIfNew! k v).contains a → compare k a ≠ .eq → t.contains a := by
+  rw [← beq_eq_false_iff_ne]
+  simp_to_model [insertIfNew!] using List.containsKey_of_containsKey_insertEntryIfNew
+
+theorem mem_of_mem_insertIfNew [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ (t.insertIfNew k v h.balanced).impl → compare k a ≠ .eq → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_insertIfNew h
+
+theorem mem_of_mem_insertIfNew! [TransOrd α] (h : t.WF) {k a : α} {v : β k} :
+    a ∈ t.insertIfNew! k v → compare k a ≠ .eq → a ∈ t := by
+  simpa [mem_iff_contains] using contains_of_contains_insertIfNew! h
+
+theorem size_insertIfNew [TransOrd α] {k : α} (h : t.WF) {v : β k} :
+    (t.insertIfNew k v h.balanced).impl.size = if k ∈ t then t.size else t.size + 1 := by
+  simp only [mem_iff_contains]
+  simp_to_model [insertIfNew] using List.length_insertEntryIfNew
+
+theorem size_insertIfNew! [TransOrd α] {k : α} (h : t.WF) {v : β k} :
+    (t.insertIfNew! k v).size = if k ∈ t then t.size else t.size + 1 := by
+  simp only [mem_iff_contains]
+  simp_to_model [insertIfNew!] using List.length_insertEntryIfNew
+
+theorem size_le_size_insertIfNew [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    t.size ≤ (t.insertIfNew k v h.balanced).impl.size := by
+  simp_to_model [insertIfNew] using List.length_le_length_insertEntryIfNew
+
+theorem size_le_size_insertIfNew! [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    t.size ≤ (t.insertIfNew! k v).size := by
+  simp_to_model [insertIfNew!] using List.length_le_length_insertEntryIfNew
+
+theorem size_insertIfNew_le [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insertIfNew k v h.balanced).impl.size ≤ t.size + 1 := by
+  simp_to_model [insertIfNew] using List.length_insertEntryIfNew_le
+
+theorem size_insertIfNew!_le [TransOrd α] (h : t.WF) {k : α} {v : β k} :
+    (t.insertIfNew! k v).size ≤ t.size + 1 := by
+  simp_to_model [insertIfNew!] using List.length_insertEntryIfNew_le
+
 end Std.DTreeMap.Internal.Impl
--- src/Std/Data/TreeSet/Lemmas.lean
+++ src/Std/Data/TreeSet/Lemmas.lean
@@ -23,6 +23,9 @@ namespace Std.TreeSet
 
 variable {α : Type u} {cmp : α → α → Ordering} {t : TreeSet α cmp}
 
+private theorem ext {t t' : TreeSet α cmp} : t.inner = t'.inner → t = t' := by
+  cases t; cases t'; rintro rfl; rfl
+
 @[simp]
 theorem isEmpty_emptyc : (∅ : TreeSet α cmp).isEmpty :=
   TreeMap.isEmpty_emptyc
@@ -41,5 +44,132 @@ theorem contains_congr [TransCmp cmp] {k k' : α} (hab : cmp k k' = .eq) :
 
 theorem mem_congr [TransCmp cmp] {k k' : α} (hab : cmp k k' = .eq) : k ∈ t ↔ k' ∈ t :=
   TreeMap.mem_congr hab
+
+@[simp]
+theorem contains_emptyc {k : α} : (∅ : TreeSet α cmp).contains k = false :=
+  TreeMap.contains_emptyc
+
+@[simp]
+theorem not_mem_emptyc {k : α} : k ∉ (∅ : TreeSet α cmp) :=
+  TreeMap.not_mem_emptyc
+
+theorem contains_of_isEmpty [TransCmp cmp] {a : α} :
+    t.isEmpty → t.contains a = false :=
+  DTreeMap.contains_of_isEmpty
+
+theorem not_mem_of_isEmpty [TransCmp cmp] {a : α} :
+    t.isEmpty → a ∉ t :=
+  DTreeMap.not_mem_of_isEmpty
+
+theorem isEmpty_eq_false_iff_exists_contains_eq_true [TransCmp cmp] :
+    t.isEmpty = false ↔ ∃ a, t.contains a = true :=
+  DTreeMap.isEmpty_eq_false_iff_exists_contains_eq_true
+
+theorem isEmpty_eq_false_iff_exists_mem [TransCmp cmp] :
+    t.isEmpty = false ↔ ∃ a, a ∈ t :=
+  DTreeMap.isEmpty_eq_false_iff_exists_mem
+
+theorem isEmpty_iff_forall_contains [TransCmp cmp] :
+    t.isEmpty = true ↔ ∀ a, t.contains a = false :=
+  DTreeMap.isEmpty_iff_forall_contains
+
+theorem isEmpty_iff_forall_not_mem [TransCmp cmp] :
+    t.isEmpty = true ↔ ∀ a, ¬a ∈ t :=
+  DTreeMap.isEmpty_iff_forall_not_mem
+
+@[simp]
+theorem insert_eq_insert {p : α} : Insert.insert p t = t.insert p :=
+  rfl
+
+@[simp]
+theorem singleton_eq_insert {p : α} :
+    Singleton.singleton p = (∅ : TreeSet α cmp).insert p :=
+  rfl
+
+@[simp]
+theorem contains_insert [h : TransCmp cmp] {k a : α} :
+    (t.insert k).contains a = (cmp k a == .eq || t.contains a) :=
+  TreeMap.contains_insertIfNew
+
+@[simp]
+theorem mem_insert [TransCmp cmp] {k a : α} :
+    a ∈ t.insert k ↔ cmp k a = .eq ∨ a ∈ t :=
+  TreeMap.mem_insertIfNew
+
+theorem contains_insert_self [TransCmp cmp] {k : α} :
+    (t.insert k).contains k :=
+  TreeMap.contains_insertIfNew_self
+
+theorem mem_insert_self [TransCmp cmp] {k : α} :
+    k ∈ t.insert k :=
+  TreeMap.mem_insertIfNew_self
+
+theorem contains_of_contains_insert [TransCmp cmp] {k a : α} :
+    (t.insert k).contains a → cmp k a ≠ .eq → t.contains a :=
+  TreeMap.contains_of_contains_insertIfNew
+
+theorem mem_of_mem_insert [TransCmp cmp] {k a : α} :
+    a ∈ t.insert k → cmp k a ≠ .eq → a ∈ t :=
+  TreeMap.mem_of_mem_insertIfNew
+
+@[simp]
+theorem size_emptyc : (∅ : TreeSet α cmp).size = 0 :=
+  TreeMap.size_emptyc
+
+theorem isEmpty_eq_size_eq_zero :
+    t.isEmpty = (t.size == 0) :=
+  TreeMap.isEmpty_eq_size_eq_zero
+
+theorem size_insert [TransCmp cmp] {k : α} :
+    (t.insert k).size = if t.contains k then t.size else t.size + 1 :=
+  TreeMap.size_insertIfNew
+
+theorem size_le_size_insert [TransCmp cmp] {k : α} :
+    t.size ≤ (t.insert k).size :=
+  TreeMap.size_le_size_insertIfNew
+
+theorem size_insert_le [TransCmp cmp] {k : α} :
+    (t.insert k).size ≤ t.size + 1 :=
+  TreeMap.size_insertIfNew_le
+
+@[simp]
+theorem erase_emptyc {k : α} :
+    (empty : TreeSet α cmp).erase k = empty :=
+  ext <| TreeMap.erase_emptyc
+
+@[simp]
+theorem isEmpty_erase [TransCmp cmp] {k : α} :
+    (t.erase k).isEmpty = (t.isEmpty || (t.size == 1 && t.contains k)) :=
+  TreeMap.isEmpty_erase
+
+@[simp]
+theorem contains_erase [TransCmp cmp] {k a : α} :
+    (t.erase k).contains a = (cmp k a != .eq && t.contains a) :=
+  TreeMap.contains_erase
+
+@[simp]
+theorem mem_erase [TransCmp cmp] {k a : α} :
+    a ∈ t.erase k ↔ cmp k a ≠ .eq ∧ a ∈ t :=
+  TreeMap.mem_erase
+
+theorem contains_of_contains_erase [TransCmp cmp] {k a : α} :
+    (t.erase k).contains a → t.contains a :=
+  TreeMap.contains_of_contains_erase
+
+theorem mem_of_mem_erase [TransCmp cmp] {k a : α} :
+    (t.erase k).contains a → t.contains a :=
+  TreeMap.mem_of_mem_erase
+
+theorem size_erase [TransCmp cmp] {k : α} :
+    (t.erase k).size = if t.contains k then t.size - 1 else t.size :=
+  TreeMap.size_erase
+
+theorem size_erase_le [TransCmp cmp] {k : α} :
+    (t.erase k).size ≤ t.size :=
+  TreeMap.size_erase_le
+
+theorem size_le_size_erase [TransCmp cmp] {k : α} :
+    t.size ≤ (t.erase k).size + 1 :=
+  TreeMap.size_le_size_erase
 
 end Std.TreeSet

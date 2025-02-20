leanprover/lean4

commit cc94cff735074f1c0119c48b6de19e55356344f3

feat: fast path for inlay hints (#7149)
This PR adds a fast path to the inlay hint request that makes it re-use
already computed inlay hints from previous requests instead of
re-computing them. This is necessary because for some reason VS Code
emits an inlay hint request for every line you scroll, so we need to be
able to respond to these requests against the same document state
quickly. Otherwise, every single scrolled line would result in a request
that can take a few dozen ms to be responded to in long files, putting
unnecessary pressure on the CPU.
It also filters the result set by the inlay hints that have been
requested.

src/Lean/Server/FileWorker/InlayHints.lean
@@ -99,17 +99,21 @@ def applyEditToHint? (hintMod : Name) (ihi : Elab.InlayHintInfo) (range : String
 
 structure InlayHintState where
   oldInlayHints      : Array Elab.InlayHintInfo
+  oldFinishedSnaps   : Nat
   lastEditTimestamp? : Option Nat
   deriving TypeName, Inhabited
 
 def InlayHintState.init : InlayHintState := {
   oldInlayHints := #[]
+  oldFinishedSnaps := 0
   lastEditTimestamp? := none
 }
 
-def handleInlayHints (_ : InlayHintParams) (s : InlayHintState) :
+def handleInlayHints (p : InlayHintParams) (s : InlayHintState) :
     RequestM (LspResponse (Array InlayHint) × InlayHintState) := do
   let ctx ← read
+  let text := ctx.doc.meta.text
+  let range := text.lspRangeToUtf8Range p.range
   let srcSearchPath := ctx.srcSearchPath
   -- We delay sending inlay hints by 3000ms to avoid inlay hint flickering on the client.
   -- VS Code already has a mechanism for this, but it is not sufficient.
@@ -122,16 +126,24 @@ def handleInlayHints (_ : InlayHintParams) (s : InlayHintState) :
       let timeSinceLastEditMs := timestamp - lastEditTimestamp
       inlayHintEditDelayMs - timeSinceLastEditMs
   let (snaps, _, isComplete) ← ctx.doc.cmdSnaps.getFinishedPrefixWithConsistentLatency editDelayMs.toUInt32 (cancelTk? := ctx.cancelTk.cancellationTask)
-  let finishedRange? : Option String.Range := do
-    return ⟨⟨0⟩, ← List.max? <| snaps.map (fun s => s.endPos)⟩
+  let snaps := snaps.toArray
+  let finishedSnaps := snaps.size
+  let oldFinishedSnaps := s.oldFinishedSnaps
+  -- File processing is monotonic modulo `didChange` notifications.
+  assert! finishedSnaps >= oldFinishedSnaps
+  -- VS Code emits inlay hint requests *every time the user scrolls*. This is reasonably expensive,
+  -- so in addition to re-using old inlay hints from parts of the file that haven't been processed
+  -- yet, we also re-use old inlay hints from parts of the file that have been processed already
+  -- with the current state of the document.
+  let invalidOldInlayHintsRange : String.Range := {
+    start := snaps[oldFinishedSnaps - 1]?.map (·.endPos) |>.getD ⟨0⟩
+    stop := snaps[finishedSnaps - 1]?.map (·.endPos) |>.getD ⟨0⟩
+  }
   let oldInlayHints :=
-    if let some finishedRange := finishedRange? then
-      s.oldInlayHints.filter fun (ihi : Elab.InlayHintInfo) =>
-        ! finishedRange.contains ihi.position
-    else
-      s.oldInlayHints
+    s.oldInlayHints.filter fun (ihi : Elab.InlayHintInfo) =>
+      ! invalidOldInlayHintsRange.contains ihi.position
   let newInlayHints : Array Elab.InlayHintInfo ← (·.2) <$> StateT.run (s := #[]) do
-    for s in snaps do
+    for s in snaps[oldFinishedSnaps:] do
       s.infoTree.visitM' (postNode := fun ci i _ => do
         let .ofCustomInfo i := i
           | return
@@ -139,10 +151,14 @@ def handleInlayHints (_ : InlayHintParams) (s : InlayHintState) :
           | return
         let ih ← ci.runMetaM ih.lctx ih.resolveDeferred
         modify (·.push ih.toInlayHintInfo))
-  let inlayHints := newInlayHints ++ oldInlayHints
-  let lspInlayHints ← inlayHints.mapM (·.toLspInlayHint srcSearchPath ctx.doc.meta.text)
+  let allInlayHints := newInlayHints ++ oldInlayHints
+  let inlayHintsInRange := allInlayHints.filter (range.contains (includeStop := true) ·.position)
+  let lspInlayHints ← inlayHintsInRange.mapM (·.toLspInlayHint srcSearchPath text)
   let r := { response := lspInlayHints, isComplete }
-  let s := { s with oldInlayHints := inlayHints }
+  let s := { s with
+    oldInlayHints := allInlayHints
+    oldFinishedSnaps := finishedSnaps
+  }
   return (r, s)
 
 def handleInlayHintsDidChange (p : DidChangeTextDocumentParams)
@@ -192,6 +208,7 @@ def handleInlayHintsDidChange (p : DidChangeTextDocumentParams)
       some timestamp
   set <| { s with
     oldInlayHints := updatedOldInlayHints
+    oldFinishedSnaps := 0
     lastEditTimestamp?
   }
 

commit 970732ea11515cb6aa96beb3cb3578f6fc8e2cde

fix: inlay hint assertion violation (#7164)
This PR fixes an assertion violation introduced in #7149 where the
monotonic progress assumption was violated by request cancellation.

src/Lean/Server/FileWorker/InlayHints.lean
@@ -126,10 +126,12 @@ def handleInlayHints (p : InlayHintParams) (s : InlayHintState) :
       let timeSinceLastEditMs := timestamp - lastEditTimestamp
       inlayHintEditDelayMs - timeSinceLastEditMs
   let (snaps, _, isComplete) ← ctx.doc.cmdSnaps.getFinishedPrefixWithConsistentLatency editDelayMs.toUInt32 (cancelTk? := ctx.cancelTk.cancellationTask)
+  -- This cancellation check is crucial for the invariant `finishedSnaps >= oldFinishedSnaps` below.
+  RequestM.checkCancelled
   let snaps := snaps.toArray
   let finishedSnaps := snaps.size
   let oldFinishedSnaps := s.oldFinishedSnaps
-  -- File processing is monotonic modulo `didChange` notifications.
+  -- File processing is monotonic modulo `didChange` notifications and cancellation.
   assert! finishedSnaps >= oldFinishedSnaps
   -- VS Code emits inlay hint requests *every time the user scrolls*. This is reasonably expensive,
   -- so in addition to re-using old inlay hints from parts of the file that haven't been processed
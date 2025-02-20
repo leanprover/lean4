6c609028b3b457afeec0d1c91f4acee5fd1c2944
Kim Morrison <scott.morrison@gmail.com>
2025-02-21 10:46:11 +1100

    feat: upgrade Int.tdiv_eq_ediv to an unconditional equivalence (#7163)
    This PR gives an unconditional theorem expressing `Int.tdiv` in terms of
    `Int.ediv`, not just for non-negative arguments.

--- src/Init/Data/Int/DivMod/Basic.lean
+++ src/Init/Data/Int/DivMod/Basic.lean
@@ -112,6 +112,7 @@ instance : Mod Int where
 @[simp, norm_cast] theorem ofNat_ediv (m n : Nat) : (↑(m / n) : Int) = ↑m / ↑n := rfl
 
 theorem ofNat_ediv_ofNat {a b : Nat} : (↑a / ↑b : Int) = (a / b : Nat) := rfl
+@[norm_cast]
 theorem negSucc_ediv_ofNat_succ {a b : Nat} : ((-[a+1]) / ↑(b+1) : Int) = -[a / succ b +1] := rfl
 theorem negSucc_ediv_negSucc {a b : Nat} : ((-[a+1]) / (-[b+1]) : Int) = ((a / (b + 1)) + 1 : Nat) := rfl
 

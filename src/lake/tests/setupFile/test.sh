1ee21c17fc9709d24cd79204ecfe3c690bc1820c
Mac Malone <tydeu@hatpress.net>
2025-02-21 00:07:13 -0500

    feat: use Lake plugin for Lake imports (#7157)
    This PR changes `lake setup-file` to now use Lake as a plugin for files
    which import Lake (or one of its submodules). Thus, the server will now
    load Lake as a plugin when editing a Lake configuration written in Lean.
    This further enables the use of builtin language extensions in Lake.

---
+++ src/lake/tests/setupFile/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+LAKE=${LAKE:-../../.lake/build/bin/lake}
+
+#---
+# Test `setup-file` functionality
+#---
+
+# Test that, by default. no plugins are used.
+$LAKE setup-file bogus Foo | grep -F --color '"pluginPaths":[]'
+
+# Test that a Lake import uses the Lake plugin.
+$LAKE setup-file bogus Lake | (grep -F --color '"pluginPaths":[]' && exit 1 || true)

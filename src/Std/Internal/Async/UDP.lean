6fc9b0c5b6b373653bfadbfaf02386fc46868e8d
Sofia Rodrigues <sofia@algebraic.dev>
2025-02-24 15:04:03 -0300

    feat: add coypright header

--- src/Std/Internal/Async/UDP.lean
+++ src/Std/Internal/Async/UDP.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sofia Rodrigues
+-/
 prelude
 import Std.Time
 import Std.Internal.UV

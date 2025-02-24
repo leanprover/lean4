6d440204184fb472422f48d741e210914994fbc3
Kim Morrison <kim@tqft.net>
2025-02-25 09:48:31 +1100

    one more

--- src/Init/Data/Vector/Attach.lean
+++ src/Init/Data/Vector/Attach.lean
@@ -7,8 +7,8 @@ prelude
 import Init.Data.Vector.Lemmas
 import Init.Data.Array.Attach
 
-set_option linter.listVariables true -- Enforce naming conventions for `List`/`Array`/`Vector` variables.
-set_option linter.indexVariables true -- Enforce naming conventions for index variables.
+-- set_option linter.listVariables true -- Enforce naming conventions for `List`/`Array`/`Vector` variables.
+-- set_option linter.indexVariables true -- Enforce naming conventions for index variables.
 
 namespace Vector
 

50c87edba7fd09b11c622a4491c3cb5547740a34
Leonardo de Moura <leomoura@amazon.com>
2025-02-26 12:49:43 -0800

    chore: remove TODO

--- src/Lean/Meta/Tactic/Grind/Arith/Cutsat/Types.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Cutsat/Types.lean
@@ -148,7 +148,6 @@ structure State where
   /-
   TODO: support for storing
   - Disjuctions: they come from conflict resolution, and disequalities.
-  - Disequalities.
   - Linear integer terms appearing in the main module, and model-based equality propagation.
   -/
   deriving Inhabited

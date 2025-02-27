f90525c05c50c57a1ce2dd9cb86e23ff302e19bc
Cameron Zwarich <cameron@lean-fro.org>
2025-02-26 22:20:14 -0800

    chore: update expected test outputs
    This makes it easier to distinguish tests that are actually
    failing while we work on the new codegen.

--- tests/lean/run/CompilerPullInstances.lean
+++ tests/lean/run/CompilerPullInstances.lean
@@ -19,6 +19,11 @@ trace: [Compiler.test] Starting wrapper test pullInstancesSizeEq for pullInstanc
 [Compiler.test] Wrapper test pullInstancesSizeEq for pullInstances occurrence 0 successful
 [Compiler.test] Starting post condition test pullInstancesFix for pullInstances occurrence 0
 [Compiler.test] Post condition test pullInstancesFix for pullInstances occurrence 0 successful
+---
+info: [Compiler.test] Starting wrapper test pullInstancesSizeEq for pullInstances occurrence 0
+[Compiler.test] Wrapper test pullInstancesSizeEq for pullInstances occurrence 0 successful
+[Compiler.test] Starting post condition test pullInstancesFix for pullInstances occurrence 0
+[Compiler.test] Post condition test pullInstancesFix for pullInstances occurrence 0 successful
 -/
 #guard_msgs in
 run_meta Compiler.compile #[``Lean.Meta.synthInstance, ``Lean.Elab.Term.Do.elabDo]

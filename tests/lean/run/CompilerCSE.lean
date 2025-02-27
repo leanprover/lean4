8d4c363c66e7d8fb4dbfec4d59c130e77b1d51d4
Cameron Zwarich <cameron@lean-fro.org>
2025-02-26 22:20:14 -0800

    chore: update expected test outputs
    This makes it easier to distinguish tests that are actually
    failing while we work on the new codegen.

--- tests/lean/run/CompilerCSE.lean
+++ tests/lean/run/CompilerCSE.lean
@@ -27,6 +27,19 @@ trace: [Compiler.test] Starting wrapper test cseSizeLeq for cse occurrence 0
 [Compiler.test] Wrapper test cseSizeLeq for cse occurrence 2 successful
 [Compiler.test] Starting post condition test cseFix for cse occurrence 2
 [Compiler.test] Post condition test cseFix for cse occurrence 2 successful
+---
+info: [Compiler.test] Starting wrapper test cseSizeLeq for cse occurrence 0
+[Compiler.test] Wrapper test cseSizeLeq for cse occurrence 0 successful
+[Compiler.test] Starting post condition test cseFix for cse occurrence 0
+[Compiler.test] Post condition test cseFix for cse occurrence 0 successful
+[Compiler.test] Starting wrapper test cseSizeLeq for cse occurrence 1
+[Compiler.test] Wrapper test cseSizeLeq for cse occurrence 1 successful
+[Compiler.test] Starting post condition test cseFix for cse occurrence 1
+[Compiler.test] Post condition test cseFix for cse occurrence 1 successful
+[Compiler.test] Starting wrapper test cseSizeLeq for cse occurrence 2
+[Compiler.test] Wrapper test cseSizeLeq for cse occurrence 2 successful
+[Compiler.test] Starting post condition test cseFix for cse occurrence 2
+[Compiler.test] Post condition test cseFix for cse occurrence 2 successful
 -/
 #guard_msgs in
 run_meta Compiler.compile #[``Lean.Meta.synthInstance, ``Lean.Elab.Term.Do.elabDo]

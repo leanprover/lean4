9affc69173bf7c167bf36a64f01f18e2892a9068
Cameron Zwarich <cameron@lean-fro.org>
2025-02-26 22:20:14 -0800

    chore: update expected test outputs
    This makes it easier to distinguish tests that are actually
    failing while we work on the new codegen.

--- tests/lean/run/CompilerFindJoinPoints.lean
+++ tests/lean/run/CompilerFindJoinPoints.lean
@@ -19,6 +19,11 @@ trace: [Compiler.test] Starting wrapper test findJoinPointsSizeLeq for findJoinP
 [Compiler.test] Wrapper test findJoinPointsSizeLeq for findJoinPoints occurrence 0 successful
 [Compiler.test] Starting post condition test findJoinPointsFix for findJoinPoints occurrence 0
 [Compiler.test] Post condition test findJoinPointsFix for findJoinPoints occurrence 0 successful
+---
+info: [Compiler.test] Starting wrapper test findJoinPointsSizeLeq for findJoinPoints occurrence 0
+[Compiler.test] Wrapper test findJoinPointsSizeLeq for findJoinPoints occurrence 0 successful
+[Compiler.test] Starting post condition test findJoinPointsFix for findJoinPoints occurrence 0
+[Compiler.test] Post condition test findJoinPointsFix for findJoinPoints occurrence 0 successful
 -/
 #guard_msgs in
 run_meta Compiler.compile #[``Lean.Meta.synthInstance, ``Lean.Elab.Term.Do.elabDo, ``Lean.MetavarContext.MkBinding.collectForwardDeps]

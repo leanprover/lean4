0736a37caf631cd660199852e6e33fa86538396e
Joachim Breitner <mail@joachim-breitner.de>
2025-03-04 18:19:47 +0100

    Also GuessLex

--- tests/lean/run/issue7332.lean
+++ tests/lean/run/issue7332.lean
@@ -4,5 +4,12 @@ def foo (n : Nat)  : Nat := foo (n - 1)
 decreasing_by
   skip
   match n with
-  | 0 => exact sorry
-  | _ + 1 => exact sorry
+  | 0 => exact testSorry
+  | _ + 1 => exact testSorry
+
+def bar (n : Nat) (m : Nat): Nat := bar (n - 1) (m - 1)
+decreasing_by
+  show _ < _
+  match ‹Nat› with
+  | 0 => exact testSorry
+  | _ + 1 => exact testSorry

e43b1b12dc92463e2ace1f277bdb5c594f468df0
Joachim Breitner <mail@joachim-breitner.de>
2025-03-04 10:34:58 +0100

    Use Term.withoutModifyingElabMetaStateWithInfo

--- tests/lean/run/omegaDischarger.lean
+++ tests/lean/run/omegaDischarger.lean
@@ -3,4 +3,4 @@
 variable {x y: Nat} {p : Nat → Nat → Prop}
 theorem foo (h: ¬ y < x) : p x y := sorry
 
-example (h : x < y): p x y := by simp (discharger := omega) only [foo]
+example (h : x < y): p x y := by simp (discharger := omega)  only [foo]

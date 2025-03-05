8be7dc142b6d4a404de78b0544a4b3f6bf0eba5b
Leonardo de Moura <leomoura@amazon.com>
2025-03-05 15:12:10 -0800

    test: `cooper_right`, `cooper_left`, `cooper_dvd_left`, `cooper_dvd_right`

--- tests/lean/run/grind_cutsat_cooper.lean
+++ tests/lean/run/grind_cutsat_cooper.lean
@@ -25,10 +25,33 @@ info: [grind.cutsat.model] x := 241/154
 #guard_msgs (info) in
 set_option trace.grind.cutsat.model true in
 example (x y : Int) : problem₁ x y → False := by
-  fail_if_success grind +qlia
+  fail_if_success grind +qlia -- Rational counterexamples allowed
   sorry
 
 /-- info: true -/
 #guard_msgs (info) in
 open Std.Internal in
 #eval problem₁ (241/154 : Rat) (1 : Rat)
+
+theorem ex₁ (x y : Int) :
+    27 ≤ 13*x + 11*y → 13*x + 11*y ≤ 30 →
+    -10 ≤ 9*x - 7*y → 9*x - 7*y ≤ 4 → False := by
+  grind
+
+theorem ex₂ (x y : Int) :
+    27 ≤ 11*x + 13*y → 11*x + 13*y ≤ 45 →
+    -10 ≤ 7*x - 9*y → 7*x - 9*y ≤ 4 → False := by
+  grind
+
+theorem ex₃ (x y : Int) :
+    5 ≤ x + y → x + 2*y ≤ 14 → 7 ∣ x → 4 ∣ y → y ≥ 4 → False := by
+  grind
+
+theorem ex₄ (x y : Int) :
+    5 ≤ 2*x + y → 3*x + 2*y ≤ 14 → 7 ∣ x → 4 ∣ y → y ≥ 4 → False := by
+  grind
+
+#print ex₁
+#print ex₂
+#print ex₃
+#print ex₄

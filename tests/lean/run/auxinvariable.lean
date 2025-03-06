4a86d0a7cb8e673a0201e341a05393b99b347386
Joachim Breitner <mail@joachim-breitner.de>
2025-03-06 18:09:39 +0100

    Allow mkAuxName even without a declName, e.g. in `variable` commands

---
+++ tests/lean/run/auxinvariable.lean
@@ -0,0 +1,18 @@
+/-!
+It used to be that aux theorems could only be created within declarations. But with `omega` wanting
+to create an aux theorem, and `omega` being used in, say, `variable` commands, this is not tenable.
+-/
+
+structure Foo (n : Nat) (h : n > 1 := by omega) : Type
+
+set_option pp.proofs true
+
+/-- info: Foo 3 (Decidable.byContradiction fun a => _check.omega_1 a) : Type -/
+#guard_msgs in
+#check Foo 3
+
+variable (x : Foo 2)
+
+/-- info: x : Foo 2 sorry -/
+#guard_msgs in
+#check x

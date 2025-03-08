03d7656a69e238cb925998e1cced8a0dbd26b0ab
Rob23oba <robin.arnez@web.de>
2025-03-08 11:58:42 +0100

    perf: add optimization for one goal in `show` tactic

--- src/Lean/Elab/Tactic/Show.lean
+++ src/Lean/Elab/Tactic/Show.lean
@@ -20,21 +20,30 @@ where
     match l with
     | [] => throwError "tactic 'show' failed, no goal unifies"
     | goal :: goals =>
-      let type ← goal.getType
-      let tag ← goal.getTag
-      let mstate ← getThe Meta.State
-      let info ← getInfoState
-      try
-        let (tgt', mvars) ← goal.withContext
-          (withCollectingNewGoalsFrom (elabChange type newType) tag `show)
-        let goal' ← goal.replaceTargetDefEq tgt'
-        let newGoals := goal' :: prev.reverseAux (mvars ++ goals)
-        setGoals newGoals
-      catch _ =>
-        -- we might otherwise have multiple instances of term elab information
-        setInfoState info
-        set mstate
-        go newType goals (goal :: prev)
+      if goals.isEmpty then
+        -- optimization for last goal
+        try
+          tryGoal newType goal goals prev
+        catch _ =>
+          throwError "tactic 'show' failed, no goal unifies"
+      else
+        let mstate ← getThe Meta.State
+        let info ← getInfoState
+        try
+          tryGoal newType goal goals prev
+        catch _ =>
+          -- we might otherwise have multiple instances of term elab information
+          setInfoState info
+          set mstate
+          go newType goals (goal :: prev)
+  tryGoal (newType : Term) (goal : MVarId) (l : List MVarId) (prev : List MVarId) : TacticM Unit := do
+    let type ← goal.getType
+    let tag ← goal.getTag
+    let (tgt', mvars) ← goal.withContext
+      (withCollectingNewGoalsFrom (elabChange type newType) tag `show)
+    let goal' ← goal.replaceTargetDefEq tgt'
+    let newGoals := goal' :: prev.reverseAux (mvars ++ l)
+    setGoals newGoals
 
 @[builtin_tactic «show»] elab_rules : tactic
   | `(tactic| show $newType:term) => elabShow newType

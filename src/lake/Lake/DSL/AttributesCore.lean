1c0df5b30964fa8e9332aa78ea25f60c34f5f741
Mac Malone <tydeu@hatpress.net>
2025-03-08 20:52:50 -0500

    chore: lake: revert builtin inits, elabs, & macros (#7399)
    This PR reverts the new builtin initializers, elaborators, and macros in
    Lake back to non-builtin.
    
    That is, it reverts the significant change of #7171. This is done to
    potential solve the intermittent test failures Lake has been
    experiencing on `master`, which I suspect may be caused by this change.
    
    (cherry picked from commit ffc7ba0829b6ee2ca1f762dd66f706df992d8e9c)

--- src/lake/Lake/DSL/AttributesCore.lean
+++ src/lake/Lake/DSL/AttributesCore.lean
@@ -9,37 +9,37 @@ import Lake.Util.OrderedTagAttribute
 open Lean
 namespace Lake
 
-builtin_initialize packageAttr : OrderedTagAttribute ←
+initialize packageAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `package "mark a definition as a Lake package configuration"
 
-builtin_initialize packageDepAttr : OrderedTagAttribute ←
+initialize packageDepAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `package_dep "mark a definition as a Lake package dependency"
 
-builtin_initialize postUpdateAttr : OrderedTagAttribute ←
+initialize postUpdateAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `post_update "mark a definition as a Lake package post-update hook"
 
-builtin_initialize scriptAttr : OrderedTagAttribute ←
+initialize scriptAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `script "mark a definition as a Lake script"
 
-builtin_initialize defaultScriptAttr : OrderedTagAttribute ←
+initialize defaultScriptAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `default_script "mark a Lake script as the package's default"
     fun name => do
       unless (← getEnv <&> (scriptAttr.hasTag · name)) do
         throwError "attribute `default_script` can only be used on a `script`"
 
-builtin_initialize leanLibAttr : OrderedTagAttribute ←
+initialize leanLibAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `lean_lib "mark a definition as a Lake Lean library target configuration"
 
-builtin_initialize leanExeAttr : OrderedTagAttribute ←
+initialize leanExeAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `lean_exe "mark a definition as a Lake Lean executable target configuration"
 
-builtin_initialize externLibAttr : OrderedTagAttribute ←
+initialize externLibAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `extern_lib "mark a definition as a Lake external library target"
 
-builtin_initialize targetAttr : OrderedTagAttribute ←
+initialize targetAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `target "mark a definition as a custom Lake target"
 
-builtin_initialize defaultTargetAttr : OrderedTagAttribute ←
+initialize defaultTargetAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `default_target "mark a Lake target as the package's default"
     fun name => do
       let valid ← getEnv <&> fun env =>
@@ -50,7 +50,7 @@ builtin_initialize defaultTargetAttr : OrderedTagAttribute ←
       unless valid do
         throwError "attribute `default_target` can only be used on a target (e.g., `lean_lib`, `lean_exe`)"
 
-builtin_initialize testDriverAttr : OrderedTagAttribute ←
+initialize testDriverAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `test_driver "mark a Lake script, executable, or library as package's test driver"
     fun name => do
       let valid ← getEnv <&> fun env =>
@@ -60,7 +60,7 @@ builtin_initialize testDriverAttr : OrderedTagAttribute ←
       unless valid do
         throwError "attribute `test_driver` can only be used on a `script`, `lean_exe`, or `lean_lib`"
 
-builtin_initialize lintDriverAttr : OrderedTagAttribute ←
+initialize lintDriverAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `lint_driver "mark a Lake script or executable as package's linter"
     fun name => do
       let valid ← getEnv <&> fun env =>
@@ -69,11 +69,11 @@ builtin_initialize lintDriverAttr : OrderedTagAttribute ←
       unless valid do
         throwError "attribute `lint_driver` can only be used on a `script` or `lean_exe`"
 
-builtin_initialize moduleFacetAttr : OrderedTagAttribute ←
+initialize moduleFacetAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `module_facet "mark a definition as a Lake module facet"
 
-builtin_initialize packageFacetAttr : OrderedTagAttribute ←
+initialize packageFacetAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `package_facet "mark a definition as a Lake package facet"
 
-builtin_initialize libraryFacetAttr : OrderedTagAttribute ←
+initialize libraryFacetAttr : OrderedTagAttribute ←
   registerOrderedTagAttribute `library_facet "mark a definition as a Lake library facet"

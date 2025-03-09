0714a7150bd2d8cc3bc64700d51b336d7f3a806b
Henrik Böving <hargonix@gmail.com>
2025-03-09 19:14:30 +0100

    feat: add more multiplication lemmas to bv_normalize (#7407)
    This PR adds rules for `-1#w * a = -a` and `a * -1#w = -a` to
    bv_normalize as seen in Bitwuzla's BV_MUL_SPECIAL_CONST.
    
    This allows us to solve 
    ```lean
    example {a : BitVec 32} : a + -1 * a = 0 := by bv_normalize
    ```
    which would previously time out.

--- tests/lean/run/bv_decide_rewriter.lean
+++ tests/lean/run/bv_decide_rewriter.lean
@@ -536,6 +536,12 @@ example {a b : BitVec 8} {c : Bool} :
 example {a : BitVec 8} : a = a.cast rfl := by
   bv_normalize
 
+-- BitVec.mul_neg
+example {a : BitVec 8} : a * -1 = -a := by bv_normalize
+example {a : BitVec 8} : -1 * a = -a := by bv_normalize
+example {a : BitVec 8} : -1 * a + a = 0 := by bv_normalize
+example {a : BitVec 8} : a + -1 * a = 0 := by bv_normalize
+
 section
 
 example (x y : BitVec 256) : x * y = y * x := by

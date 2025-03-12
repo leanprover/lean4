5cb98e38ed3b0f34a8dadb222f72868f3ce5d1d6
Kim Morrison <kim@tqft.net>
2025-03-12 14:10:14 +1100

    more

--- src/Lean/Util/MonadCache.lean
+++ src/Lean/Util/MonadCache.lean
@@ -68,7 +68,7 @@ instance  : MonadHashMapCacheAdapter α β (MonadCacheT α β m) where
   modifyCache f := (modify f : StateRefT' ..)
 
 @[inline] def run {σ} (x : MonadCacheT α β m σ) : m σ :=
-  x.run' Std.HashMap.empty
+  x.run' ∅
 
 instance : Monad (MonadCacheT α β m) := inferInstanceAs (Monad (StateRefT' _ _ _))
 instance : MonadLift m (MonadCacheT α β m) := inferInstanceAs (MonadLift m (StateRefT' _ _ _))

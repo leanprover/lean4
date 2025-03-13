c7fc5940ba59392c35975116249659a48d1ca090
Wojciech Rozowski <wojciech@lean-fro.org>
2025-03-13 12:03:10 +0100

    Push of what i have

--- tests/lean/run/7458.lean
+++ tests/lean/run/7458.lean
@@ -1,7 +1,25 @@
+-- set_option trace.Elab.definition.structural true
+-- set_option trace.Elab.definition.scc true
+-- set_option trace.Elab.definition.wf true
+-- set_option trace.Elab.definition true
+-- set_option trace.Elab.definition true
+-- The following snippet causes Lean to throw error:
+
 def areAllSame' (arr : Array Nat) (i : Nat := 0) :=
   if _ : i < arr.size then
     if arr[i] = arr[0] then
       areAllSame' arr (i + 1)
+    else
+      false
+  else
+    true
+
+--Removing the optional argument works
+
+def areAllSame (arr : Array Nat) (i : Nat) :=
+  if _ : i < arr.size then
+    if arr[i] = arr[0] then
+      areAllSame arr (i + 1)
     else
       false
   else

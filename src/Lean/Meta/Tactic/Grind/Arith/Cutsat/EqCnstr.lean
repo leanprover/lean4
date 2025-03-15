5d34b1e01da9f2c3c7c1556c89f081285623988e
Leonardo de Moura <leomoura@amazon.com>
2025-03-15 08:21:44 -0700

    refactor: ensure we can support different types in cutsat

--- src/Lean/Meta/Tactic/Grind/Arith/Cutsat/EqCnstr.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Cutsat/EqCnstr.lean
@@ -271,6 +271,16 @@ def processNewDiseqImpl (a b : Expr) : GoalM Unit := do
 private inductive SupportedTermKind where
   | add | mul | num
 
+private def getKindAndType? (e : Expr) : Option (SupportedTermKind × Expr) :=
+  match_expr e with
+  | HAdd.hAdd α _ _ _ _ => some (.add, α)
+  | HMul.hMul α _ _ _ _ _ => some (.mul, α)
+  | OfNat.ofNat α _ _ => some (.num, α)
+  | Neg.neg α _ a =>
+    let_expr OfNat.ofNat _ _ _ := a | none
+    some (.num, α)
+  | _ => none
+
 private def isForbiddenParent (parent? : Option Expr) (k : SupportedTermKind) : Bool := Id.run do
   let some parent := parent? | return false
   let .const declName _ := parent.getAppFn | return false
@@ -280,16 +290,15 @@ private def isForbiddenParent (parent? : Option Expr) (k : SupportedTermKind) : 
   | .mul => return declName == ``HMul.hMul
   | .num => return declName == ``HMul.hMul || declName == ``Eq
 
-private def internalizeCore (e : Expr) (parent? : Option Expr) (k : SupportedTermKind) : GoalM Unit := do
+private def internalizeInt (e : Expr) : GoalM Unit := do
   if (← get').terms.contains { expr := e } then return ()
-  if isForbiddenParent parent? k then return ()
   let p ← toPoly e
   markAsCutsatTerm e
   trace[grind.cutsat.internalize] "{aquote e}:= {← p.pp}"
   modify' fun s => { s with terms := s.terms.insert { expr := e } p }
 
 /--
-Internalizes an integer expression. Here are the different cases that are handled.
+Internalizes an integer (and `Nat`) expression. Here are the different cases that are handled.
 
 - `a + b` when `parent?` is not `+`, `≤`, or `∣`
 - `k * a` when `k` is a numeral and `parent?` is not `+`, `*`, `≤`, `∣`
@@ -298,14 +307,10 @@ Internalizes an integer expression. Here are the different cases that are handle
   back to the congruence closure module. Example: we have `f 5`, `f x`, `x - y = 3`, `y = 2`.
 -/
 def internalize (e : Expr) (parent? : Option Expr) : GoalM Unit := do
-  let k ← if (← isAdd e) then
-    pure .add
-  else if (← isMul e) then
-    pure .mul
-  else if (← getIntValue? e).isSome then
-    pure .num
-  else
-    return ()
-  internalizeCore e parent? k
+  let some (k, type) := getKindAndType? e | return ()
+  if isForbiddenParent parent? k then return ()
+  trace[grind.debug.cutsat.internalize] "{e}"
+  if type.isConstOf ``Int then
+    internalizeInt e
 
 end Lean.Meta.Grind.Arith.Cutsat

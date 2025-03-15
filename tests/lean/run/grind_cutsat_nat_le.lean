96fda6695ce7d14334353e9cc0c27081fc5a8135
Leonardo de Moura <leomoura@amazon.com>
2025-03-14 17:05:54 -0700

    chore: adjust test

--- tests/lean/run/grind_cutsat_nat_le.lean
+++ tests/lean/run/grind_cutsat_nat_le.lean
@@ -3,9 +3,9 @@ set_option grind.warning false
 theorem ex1 (x y z : Nat) : x < y + z → y + 1 < z → z + x < 3*z := by
   grind
 
-theorem ex2 (x y z : Nat) : x < y + z → y + 1 < z → (p ↔ z + x < 3*z) → p := by
+theorem ex2 {p : Prop} (x y z : Nat) : x < y + z → y + 1 < z → (p ↔ z + x < 3*z) → p := by
   grind
 
-open Int Linear Expr OfNat
+open Int.Linear Int.OfNat
 #print ex1
 #print ex2

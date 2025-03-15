7a71f77e7771cc9515615009215f3a5c99b94c25
Leonardo de Moura <leomoura@amazon.com>
2025-03-15 13:55:36 -0700

    test: Nat diseq

--- tests/lean/run/grind_cutsat_nat_eq.lean
+++ tests/lean/run/grind_cutsat_nat_eq.lean
@@ -8,3 +8,6 @@ example (a b c : Nat) : a + b = 0 → a ≤ b + c + a → a ≤ c := by
 
 example (a b : Nat) (_ : 2*a + 3*b = 0) (_ : 2 ∣ 3*b + 1) : False := by
   grind
+
+example (a b c : Nat) : a + 2*b = 0 → b + c + b = 0 → a = c := by
+  grind

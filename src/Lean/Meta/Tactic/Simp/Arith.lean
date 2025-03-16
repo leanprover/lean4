bcc7a33308170184a2b34441f09432ed9cfde1c1
Leonardo de Moura <leomoura@amazon.com>
2025-03-16 15:49:24 -0700

    chore: remove dead code

--- src/Lean/Meta/Tactic/Simp/Arith.lean
+++ src/Lean/Meta/Tactic/Simp/Arith.lean
@@ -14,14 +14,4 @@ def parentIsTarget (parent? : Option Expr) : Bool :=
   | none => false
   | some parent => isLinearTerm parent || isLinearCnstr parent || isDvdCnstr parent
 
-def simp? (e : Expr) (parent? : Option Expr) : MetaM (Option (Expr × Expr)) := do
-  -- TODO: invoke `Int` procedures and add support for arbitrary ordered comm rings
-  if isLinearCnstr e then
-    Nat.simpCnstr? e
-  else if isLinearTerm e && !parentIsTarget parent? then
-    trace[Meta.Tactic.simp] "arith expr: {e}"
-    Nat.simpExpr? e
-  else
-    return none
-
 end Lean.Meta.Simp.Arith

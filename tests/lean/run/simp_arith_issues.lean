e7cde1180b68a675bb42693a0d4018c6a9654856
Leonardo de Moura <leomoura@amazon.com>
2025-03-16 20:11:48 -0700

    fix: `simp +arith` (#7515)
    This PR fixes another bug in `simp +arith`. This bug was affecting
    `grind`. See new test for an example.

--- tests/lean/run/simp_arith_issues.lean
+++ tests/lean/run/simp_arith_issues.lean
@@ -16,3 +16,8 @@ example {a b : Nat} : (if a < b then a else b - 1) ≤ b := by
 
 example {a b : Nat} : b > 0 → (if a < b then a else b - 1) < b := by
   grind
+
+example (a b : Nat) (h : (a + 1) * 8 - 1 = b) : b = 8*a + 7 := by
+  simp +arith at h
+  guard_hyp h : 8*a + 7 = b
+  rw [h]

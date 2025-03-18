e0e20e9f53a1a70ae3bbd7534367d670f5aaf715
luisacicolini <luisacicolini@gmail.com>
2025-03-18 18:17:12 +0000

    feat: neg overflow def

--- src/Init/Data/BitVec/Basic.lean
+++ src/Init/Data/BitVec/Basic.lean
@@ -690,6 +690,14 @@ treating `x` and `y` as 2's complement signed bitvectors.
 def saddOverflow {w : Nat} (x y : BitVec w) : Bool :=
   (x.toInt + y.toInt ≥ 2 ^ (w - 1)) || (x.toInt + y.toInt < - 2 ^ (w - 1))
 
+/-- `negOverflow x` returns `true` if the negation of `x` results in overflow.
+
+  SMT-Lib name: `bvnego`.
+-/
+def negOverflow {w : Nat} (x : BitVec w) : Bool :=
+  x.toInt == - 2 ^ (w - 1)
+
+
 /- ### reverse -/
 
 /-- Reverse the bits in a bitvector. -/

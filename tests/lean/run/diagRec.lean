74a8f9ae8ec781bf6c13a504649c7f0b839c6f8a
Joachim Breitner <mail@joachim-breitner.de>
2025-03-18 08:20:28 +0100

    Update test

--- tests/lean/run/diagRec.lean
+++ tests/lean/run/diagRec.lean
@@ -6,29 +6,19 @@ def fib (n : Nat) :=
 termination_by n
 
 /--
-info: 377
+info: 89
 ---
 info: [diag] Diagnostics
-  [reduction] unfolded declarations (max: 653, num: 10):
-    [reduction] Nat.rec ↦ 653
-    [reduction] Or.rec ↦ 246
-    [reduction] Acc.rec ↦ 147
-    [reduction] Eq.rec ↦ 124
-    [reduction] HAdd.hAdd ↦ 63
-    [reduction] rfl ↦ 57
-    [reduction] Nat.eq_or_lt_of_le ↦ 36
-    [reduction] Add.add ↦ 34
-    [reduction] sizeOf ↦ 25
-    [reduction] Acc.inv ↦ 12
-  [reduction] unfolded reducible declarations (max: 580, num: 5):
-    [reduction] Nat.casesOn ↦ 580
-    [reduction] Or.casesOn ↦ 246
-    [reduction] Eq.ndrec ↦ 112
-    [reduction] Nat.brecOn ↦ 36
-    [reduction] Acc.recOn ↦ 12
+  [reduction] unfolded declarations (max: 407, num: 3):
+    [reduction] Nat.rec ↦ 407
+    [reduction] Or.rec ↦ 144
+    [reduction] Acc.rec ↦ 108
+  [reduction] unfolded reducible declarations (max: 352, num: 2):
+    [reduction] Nat.casesOn ↦ 352
+    [reduction] Or.casesOn ↦ 144
   use `set_option diagnostics.threshold <num>` to control threshold for reporting counters
 -/
 #guard_msgs in
 set_option diagnostics true in
-set_option diagnostics.threshold 10 in
-#reduce fib 13
+set_option diagnostics.threshold 100 in
+#reduce fib 10

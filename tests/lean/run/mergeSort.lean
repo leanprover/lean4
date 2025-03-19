41a2e9af19b88b29070542bcd71ace95cd5d64ac
Joachim Breitner <mail@joachim-breitner.de>
2025-03-19 10:21:04 +0100

    feat: well-founded recursion: opaque well-foundedness proofs (#5182)
    This PR makes functions defined by well-founded recursion use an
    `opaque` well-founded proof by default. This reliably prevents kernel
    reduction of such definitions and proofs, which tends to be
    prohibitively slow (fixes #2171), and which regularly causes
    hard-to-debug kernel type-checking failures. This changes renders
    `unseal` ineffective for such definitions. To avoid the opaque proof,
    annotate the function definition with `@[semireducible]`.

--- tests/lean/run/mergeSort.lean
+++ tests/lean/run/mergeSort.lean
@@ -1,33 +1,26 @@
 open List MergeSort Internal
 
 -- If we omit the comparator, it is filled by the autoparam `fun a b => a ≤ b`
-unseal mergeSort merge in
 example : mergeSort [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9] :=
-  rfl
+  by native_decide
 
-unseal mergeSort merge in
 example : mergeSort [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] (· ≤ ·) = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9] :=
-  rfl
+  by native_decide
 
-unseal mergeSort merge in
 example : mergeSort [3, 100 + 1, 4, 100 + 1, 5, 100 + 9, 2, 10 + 6, 5, 10 + 3, 5] (fun x y => x/10 ≤ y/10) = [3, 4, 5, 2, 5, 5, 16, 13, 101, 101, 109] :=
-  rfl
+  by native_decide
 
-unseal mergeSortTR.run mergeTR.go in
 example : mergeSortTR [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9] :=
-  rfl
+  by native_decide
 
-unseal mergeSortTR.run mergeTR.go in
 example : mergeSortTR [3, 100 + 1, 4, 100 + 1, 5, 100 + 9, 2, 10 + 6, 5, 10 + 3, 5] (fun x y => x/10 ≤ y/10) = [3, 4, 5, 2, 5, 5, 16, 13, 101, 101, 109] :=
-  rfl
+  by native_decide
 
-unseal mergeSortTR₂.run mergeTR.go in
 example : mergeSortTR₂ [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] = [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9] :=
-  rfl
+  by native_decide
 
-unseal mergeSortTR₂.run mergeTR.go in
 example : mergeSortTR₂ [3, 100 + 1, 4, 100 + 1, 5, 100 + 9, 2, 10 + 6, 5, 10 + 3, 5] (fun x y => x/10 ≤ y/10) = [3, 4, 5, 2, 5, 5, 16, 13, 101, 101, 109] :=
-  rfl
+  by native_decide
 
 /-!
 # Behaviour of mergeSort when the comparator is not provided, but typeclasses are missing.

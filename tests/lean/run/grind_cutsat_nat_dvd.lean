2946ba04d5c7d314c2d849742a2b9d71f73ee444
Leonardo de Moura <leomoura@amazon.com>
2025-03-18 17:52:04 -0700

    fix: assert that nonlinear `Nat` terms are nonneg in cutsat (#7561)
    This PR fixes the support for nonlinear `Nat` terms in cutsat. For
    example, cutsat was failing in the following example
    ```lean
    example (i j k l : Nat) : i / j + k + l - k = i / j + l := by grind
    ```
    because we were not adding the fact that `i / j` is non negative when we
    inject the `Nat` expression into `Int`.

--- tests/lean/run/grind_cutsat_nat_dvd.lean
+++ tests/lean/run/grind_cutsat_nat_dvd.lean
@@ -42,3 +42,12 @@ example (x y : Nat) (_ : 2 ≤ x) (_ : x ≤ 3) (_ : 2 ≤ y) (_ : y ≤ 3) :
 example (x y : Nat) (_ : 2 ≤ x) (_ : x ≤ 3) (_ : 2 ≤ y) (_ : y ≤ 3) :
     4 ≤ (y + x) % 8 ∧ (y + x) % 8 ≤ 6 := by
   grind
+
+example (i j k l : Nat) : i / j + k + l - k = i / j + l := by
+  grind
+
+example (i j k l : Nat) : i % j + k + l - k = i % j + l := by
+  grind
+
+example (i j k l : Nat) : i * j + k + l - k = i * j + l := by
+  grind

d57a84971c124baa09559e7d1659babe16a5f40c
Leonardo de Moura <leomoura@amazon.com>
2025-03-22 19:36:51 -0700

    feat: integrate mbtc

--- src/Lean/Meta/Tactic/Grind/Arith/Cutsat/MBTC.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Cutsat/MBTC.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 prelude
+import Lean.Meta.Tactic.Grind.Combinators
+import Lean.Meta.Tactic.Grind.Canon
 import Lean.Meta.Tactic.Grind.MBTC
 import Lean.Meta.Tactic.Grind.Arith.Cutsat.Model
 
@@ -13,15 +15,17 @@ private def hasTheoryVar (e : Expr) : GoalM Bool := do
   return (← getAssignment? (← get) (← getENode e)).isSome
 
 private def isInterpreted (e : Expr) : GoalM Bool := do
-  return isInterpretedTerm e
+  if isInterpretedTerm e then return true
+  let f := e.getAppFn
+  return f.isConstOf ``LE.le || f.isConstOf ``Dvd.dvd
 
 private def eqAssignment (a b : Expr) : GoalM Bool := do
   let some v₁ ← getAssignment? (← get) (← getENode a) | return false
   let some v₂ ← getAssignment? (← get) (← getENode b) | return false
   return v₁ == v₂
 
-def mbtc : GoalM (Array (Expr × Expr)) := do
-  Grind.mbtc {
+def mbtcTac : GrindTactic :=
+  Grind.mbtcTac {
     hasTheoryVar := hasTheoryVar
     isInterpreted := isInterpreted
     eqAssignment := eqAssignment
--- src/Lean/Meta/Tactic/Grind/MBTC.lean
+++ src/Lean/Meta/Tactic/Grind/MBTC.lean
@@ -5,6 +5,8 @@ Authors: Leonardo de Moura
 -/
 prelude
 import Lean.Meta.Tactic.Grind.Types
+import Lean.Meta.Tactic.Grind.Combinators
+import Lean.Meta.Tactic.Grind.Canon
 
 namespace Lean.Meta.Grind
 
@@ -24,7 +26,7 @@ structure MBTC.Context where
   -/
   hasTheoryVar : Expr → GoalM Bool
   /--
-  `eqAssignment a b` returns `true` it the theory variables for `x` and `y` have the same
+  `eqAssignment x y` returns `true` it the theory variables for `x` and `y` have the same
   interpretation/assignment in the target theory. For example, suppose we have the
   constraint `x + y ≤ 0`, and cutsat satified it by assignining both `x` and `y` to
   `0`. Then, `eqAssignment x y` must return `true`.
@@ -40,11 +42,11 @@ private def mkCandidateKey (a b : Expr) : Expr × Expr :=
     (b, a)
 
 /-- Model-based theory combination. -/
-def mbtc (ctx : MBTC.Context) : GoalM (Array (Expr × Expr)) := do
+def mbtc (ctx : MBTC.Context) : GoalM Bool := do
   let mut map : Map := {}
   let mut candidates : Candidates := {}
   for ({ expr := e }, _) in (← get).enodes do
-    if e.isApp then
+    if e.isApp && !e.isEq && !e.isHEq then
     if (← isCongrRoot e) then
     unless (← ctx.isInterpreted e) do
       let f := e.getAppFn
@@ -52,21 +54,41 @@ def mbtc (ctx : MBTC.Context) : GoalM (Array (Expr × Expr)) := do
       for arg in e.getAppArgs do
         let arg ← getRoot arg
         if (← ctx.hasTheoryVar arg) then
+          trace[grind.debug.mbtc] "{arg} @ {f}:{i}"
           if let some others := map[(f, i)]? then
             unless others.any (isSameExpr arg ·) do
               for other in others do
-                unless (← ctx.eqAssignment arg other) do
+                if (← ctx.eqAssignment arg other) then
                   let k := mkCandidateKey arg other
                   candidates := candidates.insert k
               map := map.insert (f, i) (arg :: others)
           else
             map := map.insert (f, i) [arg]
         i := i + 1
+  if candidates.isEmpty then
+    return false
+  if (← get).split.num > (← getConfig).splits then
+    reportIssue "skipping `mbtc`, maximum number of splits has been reached `(splits := {(← getConfig).splits})`"
+    return false
   let result := candidates.toArray.qsort fun (a₁, b₁) (a₂, b₂) =>
     if isSameExpr a₁ a₂ then
       b₁.lt b₂
     else
       a₁.lt a₂
-  return result
+  let eqs ← result.mapM fun (a, b) => do
+    let eq ← mkEq a b
+    trace[grind.mbtc] "{eq}"
+    let eq ← shareCommon (← canon eq)
+    internalize eq (Nat.max (← getGeneration a) (← getGeneration b))
+    return eq
+  modify fun s => { s with split.candidates := s.split.candidates ++ eqs.toList }
+  return true
+
+def mbtcTac (ctx : MBTC.Context) : GrindTactic := fun goal => do
+  let (r, goal) ← GoalM.run goal do mbtc ctx
+  if r then
+    return some [goal]
+  else
+    return none
 
 end Lean.Meta.Grind

leanprover/lean4

commit c066b5cf1c66babd1a2ac8af704ddf0e50f6656c

feat: pretty printing structures, omit default values (#7589)
This PR changes the structure instance notation pretty printer so that
fields are omitted if their value is definitionally equal to the default
value for the field (up to reducible transparancy). Setting
`pp.structureInstances.defaults` to true forces such fields to be pretty
printed anyway.

Closes #1100

src/Lean/PrettyPrinter/Delaborator/Builtins.lean
@@ -201,49 +201,6 @@ def shouldShowMotive (motive : Expr) (opts : Options) : MetaM Bool := do
   <||> (pure (getPPMotivesNonConst opts) <&&> isNonConstFun motive)
 
 /--
-Takes application syntax and converts it into structure instance notation, if possible.
-Assumes that the application is pretty printed in implicit mode.
--/
-def unexpandStructureInstance (stx : Syntax) : Delab := whenPPOption getPPStructureInstances do
-  let env ← getEnv
-  let e ← getExpr
-  let some s ← isConstructorApp? e | failure
-  guard <| isStructure env s.induct
-  /- If implicit arguments should be shown, and the structure has parameters, we should not
-     pretty print using { ... }, because we will not be able to see the parameters. -/
-  let fieldNames := getStructureFields env s.induct
-  let mut fields := #[]
-  guard $ fieldNames.size == stx[1].getNumArgs
-  if hasPPUsingAnonymousConstructorAttribute env s.induct then
-    /- Note that we don't flatten anonymous constructor notation. Only a complete such notation receives TermInfo,
-       and flattening would cause the flattened-in notation to lose its TermInfo.
-       Potentially it would be justified to flatten anonymous constructor notation when the terms are
-       from the same type family (think `Sigma`), but for now users can write a custom delaborator in such instances. -/
-    return ← withTypeAscription (cond := (← withType <| getPPOption getPPStructureInstanceType)) do
-      `(⟨$[$(stx[1].getArgs)],*⟩)
-  let args := e.getAppArgs
-  let fieldVals := args.extract s.numParams args.size
-  for h : idx in [:fieldNames.size] do
-    let fieldName := fieldNames[idx]
-    if (← getPPOption getPPStructureInstancesFlatten) && (Lean.isSubobjectField? env s.induct fieldName).isSome then
-      match stx[1][idx] with
-      | `({ $fields',* $[: $_]?}) =>
-        -- We have found a subobject field that itself is printed with structure instance notation.
-        -- Scavenge its fields.
-        fields := fields ++ fields'.getElems
-        continue
-      | _ => pure ()
-    let fieldId := mkIdent fieldName
-    let fieldPos ← nextExtraPos
-    let fieldId := annotatePos fieldPos fieldId
-    addFieldInfo fieldPos (s.induct ++ fieldName) fieldName fieldId fieldVals[idx]!
-    let field ← `(structInstField|$fieldId:ident := $(stx[1][idx]))
-    fields := fields.push field
-  let tyStx ← withType do
-    if (← getPPOption getPPStructureInstanceType) then delab >>= pure ∘ some else pure none
-  `({ $fields,* $[: $tyStx]? })
-
-/--
 If `e` is an application that is a candidate for using field notation,
 returns the parameter index and the field name to use.
 Checks that there are enough arguments.
@@ -393,25 +350,18 @@ def delabAppImplicitCore (unexpand : Bool) (numArgs : Nat) (delabHead : Delab) (
       appFieldNotationCandidate?
     else
       pure none
-  let (fnStx, args') ←
+  let (fnStx, args) ←
     withBoundedAppFnArgs numArgs
       (do return ((← delabHead), Array.mkEmpty numArgs))
       (fun (fnStx, args) => return (fnStx, args.push (← mkArg paramKinds[args.size]!)))
 
-  -- Strip off optional arguments. We save the original `args'` for structure instance notation
-  let args := args'.popWhile (· matches .optional ..)
+  -- Strip off optional arguments.
+  let args := args.popWhile (· matches .optional ..)
 
   -- App unexpanders
   if ← pure unexpand <&&> getPPOption getPPNotation then
     -- Try using an app unexpander for a prefix of the arguments.
     if let some stx ← (some <$> tryAppUnexpanders fnStx args) <|> pure none then
-      return stx
-
-  -- Structure instance notation
-  if ← pure (unexpand && args'.all (·.canUnexpand)) <&&> getPPOption getPPStructureInstances then
-    -- Try using the structure instance unexpander.
-    let stx := Syntax.mkApp fnStx (args'.filterMap (·.syntax?))
-    if let some stx ← (some <$> unexpandStructureInstance stx) <|> pure none then
       return stx
 
   -- Field notation
@@ -604,6 +554,127 @@ def delabDelayedAssignedMVar : Delab := whenNotPPOption getPPMVarsDelayed do
     -- Only delaborate using decl.mvarIdPending if the delayed mvar is applied to fvars
     guard <| args.all Expr.isFVar
     delabMVarAux decl.mvarIdPending
+
+private partial def collectStructFields
+    (fields : Array (TSyntax ``Parser.Term.structInstField))
+    (fieldValues : NameMap Expr)
+    (s : ConstructorVal) :
+    DelabM (NameMap Expr × Array (TSyntax ``Parser.Term.structInstField)) := do
+  let env ← getEnv
+  -- For default value handling, we need to create a map of type parameter names to expressions.
+  let args := (← getExpr).getAppArgs
+  let paramMap : NameMap Expr ← forallTelescope s.type fun xs _ => do
+    let mut paramMap := {}
+    for i in [:s.numParams] do
+      paramMap := paramMap.insert (← xs[i]!.fvarId!.getUserName) args[i]!
+    return paramMap
+  let fieldNames := getStructureFields env s.induct
+  let (_, fieldValues, fields) ← withBoundedAppFnArgs s.numFields
+    (do return (0, fieldValues, fields))
+    (fun (i, fieldValues, fields) => do
+      let fieldName := fieldNames[i]!
+      let some fieldInfo := Lean.getFieldInfo? env s.induct fieldName | failure
+      let fieldValues := fieldValues.insert fieldName (← getExpr)
+      /- Is it a subobject that should be flattened? -/
+      if let some parentName := fieldInfo.subobject? then
+        if ← getPPOption getPPStructureInstancesFlatten then
+          if let some s' ← isConstructorApp? (← getExpr) then
+            if s'.induct == parentName then
+              let (fieldValues, fields) ← collectStructFields fields fieldValues s'
+              return (i + 1, fieldValues, fields)
+      /- Does it have the default value, and should it be omitted? -/
+      unless ← getPPOption getPPStructureInstancesDefaults do
+        if let some defFn := getDefaultFnForField? (← getEnv) s.induct fieldName then
+          let cinfo ← getConstInfo defFn
+          let defValue := cinfo.instantiateValueLevelParams! (← mkFreshLevelMVarsFor cinfo)
+          if let some defValue ← withNewMCtxDepth <| processDefaultValue paramMap fieldValues defValue then
+            if ← withReducible <| withNewMCtxDepth <| isDefEq defValue (← getExpr) then
+              -- Default value matches, skip the field.
+              return (i + 1, fieldValues, fields)
+      /- Main case: add the field -/
+      let value ←
+        if ← getPPOption getPPAnalysisHole then `(_)
+        else delab
+      let fieldPos ← nextExtraPos
+      let fieldId := annotatePos fieldPos (mkIdent fieldName)
+      addFieldInfo fieldPos fieldInfo.projFn fieldName fieldId (← getExpr)
+      let field ← `(structInstField|$fieldId:ident := $value)
+      return (i + 1, fieldValues, fields.push field))
+  return (fieldValues, fields)
+where
+  processDefaultValue (paramMap : NameMap Expr) (fieldValues : NameMap Expr) : Expr → MetaM (Option Expr)
+  | .lam n d b c => do
+    if c.isExplicit then
+      let some val := fieldValues.find? n | return none
+      if ← isDefEq (← inferType val) d then
+        processDefaultValue paramMap fieldValues (b.instantiate1 val)
+      else
+        return none
+    else
+      let some param := paramMap.find? n | return none
+      if ← isDefEq (← inferType param) d then
+        processDefaultValue paramMap fieldValues (b.instantiate1 param)
+      else
+        return none
+  | e =>
+    let_expr id _ a := e | return some e
+    return some a
+
+/--
+Delaborate structure constructor applications using structure instance notation or anonymous constructor notation.
+-/
+@[builtin_delab app]
+def delabStructureInstance : Delab := do
+  let env ← getEnv
+  let e ← getExpr
+  let some s ← isConstructorApp? e | failure
+  guard <| isStructure env s.induct
+  guard <| ← getPPOption getPPStructureInstances
+  -- Don't use structure instance notation if an unexpander exists
+  guard <| (appUnexpanderAttribute.getValues env s.name).isEmpty
+  unless s.levelParams.isEmpty do
+    guard <| ← withAppFn <| not <$> getPPOption getPPUniverses
+  let explicit ← getPPOption getPPExplicit
+  if explicit then guard <| s.numParams == 0
+  withAppFnArgs
+    (pure ())
+    (fun _ => do guard <| ← not <$> getPPOption getPPAnalysisNamedArg)
+  if hasPPUsingAnonymousConstructorAttribute env s.induct then
+    /-
+    The type has opted-in to anonymous constructor notation.
+    - If a type opts in, then it's either this or application notation. Structure instance notation is never used.
+    - In explicit mode, we require every field be explicit.
+    - We don't flatten anonymous constructor notation. Only a complete such notation receives TermInfo,
+      and flattening would cause the flattened-in notation to lose its TermInfo.
+      Potentially it would be justified to flatten anonymous constructor notation when the terms are
+      from the same type family (think `Sigma`), but for now users can write a custom delaborator in such instances.
+    -/
+    let bis ← forallTelescope s.type fun xs _ => xs.mapM (·.fvarId!.getBinderInfo)
+    if explicit then guard <| bis[s.numParams:].all (·.isExplicit)
+    let (_, args) ← withBoundedAppFnArgs s.numFields
+      (do return (0, #[]))
+      (fun (i, args) => do
+        if ← getPPOption getPPAnalysisHole then
+          return (i + 1, args.push (← `(_)))
+        else if bis[s.numParams + i]!.isExplicit then
+          return (i + 1, args.push (← delab))
+        else
+          return (i + 1, args))
+    withTypeAscription (cond := (← withType <| getPPOption getPPStructureInstanceType)) do
+      `(⟨$[$args],*⟩)
+  else
+    /-
+    Otherwise, we use structure instance notation.
+    If `pp.structureInstances.flatten` is true (and `pp.explicit` is false or the subobject has no parameters)
+    then subobjects are flattened.
+    -/
+    let (_, fields) ← collectStructFields #[] {} s
+    if ← withType <| getPPOption getPPStructureInstanceType then
+      let tyStx ← withType delab
+      `({ $fields,* : $tyStx })
+    else
+      `({ $fields,* })
+
 
 /-- State for `delabAppMatch` and helpers. -/
 structure AppMatchState where

src/Lean/PrettyPrinter/Delaborator/Options.lean
@@ -144,6 +144,11 @@ register_builtin_option pp.structureInstances.flatten : Bool := {
   group    := "pp"
   descr    := "(pretty printer) flatten nested structure instances for parent projections"
 }
+register_builtin_option pp.structureInstances.defaults : Bool := {
+  defValue := false
+  group    := "pp"
+  descr    := "(pretty printer) if false, omit structure instance fields that equal their default values"
+}
 register_builtin_option pp.fieldNotation : Bool := {
   defValue := true
   group    := "pp"
@@ -272,6 +277,7 @@ def getPPFieldNotation (o : Options) : Bool := o.get pp.fieldNotation.name (!get
 def getPPFieldNotationGeneralized (o : Options) : Bool := o.get pp.fieldNotation.generalized.name pp.fieldNotation.generalized.defValue
 def getPPStructureInstances (o : Options) : Bool := o.get pp.structureInstances.name (!getPPAll o)
 def getPPStructureInstancesFlatten (o : Options) : Bool := o.get pp.structureInstances.flatten.name pp.structureInstances.flatten.defValue
+def getPPStructureInstancesDefaults (o : Options) : Bool := o.get pp.structureInstances.defaults.name pp.structureInstances.defaults.defValue
 def getPPStructureInstanceType (o : Options) : Bool := o.get pp.structureInstanceTypes.name (getPPAll o)
 def getPPTagAppFns (o : Options) : Bool := o.get pp.tagAppFns.name (getPPAll o)
 def getPPUniverses (o : Options) : Bool := o.get pp.universes.name (getPPAll o)

tests/lean/diamond9.lean.expected.out
@@ -1,2 +1,2 @@
 constructor Ring.mk.{u} : {R : Type u} → [toZero : Zero R] → (gsmul : Int → R → R) → (∀ (a : R), gsmul 0 a = 0) → Ring R
-Ring.mk (fun x n => x.toNat * n) ⋯ : Ring Nat
+{ zero := 0, gsmul := fun x n => x.toNat * n, gsmul_zero' := ⋯ } : Ring Nat

tests/lean/emptyc.lean.expected.out
@@ -1,4 +1,4 @@
 emptyc.lean:19:0-19:2: error: ambiguous, possible interpretations 
   ∅ : A
   
-  { x := 0 } : A
+  { } : A

tests/lean/run/7353.lean
@@ -11,9 +11,10 @@ set_option pp.explicit true
 
 /--
 info: def foo : Foo :=
-Foo.mk (fun x => @Function.const Type (@Eq Unit Unit.unit Unit.unit) Nat foo.proof_1)
-  (@id ((fun x => @Function.const Type (@Eq Unit Unit.unit Unit.unit) Nat foo.proof_1) Unit.unit)
-    (@OfNat.ofNat Nat 0 (instOfNatNat 0)))
+{ obj := fun x => @Function.const Type (@Eq Unit Unit.unit Unit.unit) Nat foo.proof_1,
+  map :=
+    @id ((fun x => @Function.const Type (@Eq Unit Unit.unit Unit.unit) Nat foo.proof_1) Unit.unit)
+      (@OfNat.ofNat Nat 0 (instOfNatNat 0)) }
 -/
 #guard_msgs in
 #print foo

tests/lean/run/783.lean
@@ -4,6 +4,6 @@ structure MyStruct where
     a : α
     b : β
 
-/-- info: MyStruct.mk 10 true : MyStruct -/
+/-- info: { α := Nat, β := Bool, a := 10, b := true } : MyStruct -/
 #guard_msgs in
 #check { a := 10, b := true : MyStruct }

tests/lean/run/delabStructInst.lean
@@ -5,6 +5,19 @@
 structure A where
   x : Nat
 
+/-!
+Basic example
+-/
+/-- info: { x := 1 } : A -/
+#guard_msgs in #check { x := 1 : A }
+
+/-!
+pp.all
+-/
+set_option pp.all true in
+/-- info: A.mk (@OfNat.ofNat.{0} Nat 1 (instOfNatNat 1)) : A -/
+#guard_msgs in #check { x := 1 : A }
+
 structure B extends A where
   y : Nat
 
@@ -53,3 +66,95 @@ structure D (n : Nat) extends C n where
 
 /-- info: { x := 1, y := 2 } : D 3 -/
 #guard_msgs in #check {x := 1, y := 2 : D 3}
+
+/-!
+Show type
+-/
+set_option pp.structureInstanceTypes true in
+/-- info: { x := 0 : A } : A -/
+#guard_msgs in #check { x := 0 : A }
+
+/-!
+Omit default values
+-/
+
+/-- info: { } : Lean.Meta.Simp.Config -/
+#guard_msgs in #check { : Lean.Meta.Simp.Config }
+
+structure E where
+  n : Nat := 0
+
+/-- info: { } : E -/
+#guard_msgs in #check { : E }
+
+/-- info: { n := 1 } : E -/
+#guard_msgs in #check { n := 1 : E }
+
+set_option pp.structureInstances.defaults true in
+/-- info: { n := 0 } : E -/
+#guard_msgs in #check { : E }
+
+structure F extends E where
+  m : Nat := 1
+
+/-- info: { } : F -/
+#guard_msgs in #check { : F }
+set_option pp.structureInstances.defaults true in
+/-- info: { n := 0, m := 1 } : F -/
+#guard_msgs in #check { : F }
+/-- info: { n := 1 } : F -/
+#guard_msgs in #check { n := 1 : F }
+/-- info: { m := 2 } : F -/
+#guard_msgs in #check { m := 2 : F }
+
+/-!
+Omit default values, with parameter handling
+-/
+
+structure G (n : Nat) where
+  m := n
+
+/-- info: { } : G 3 -/
+#guard_msgs in #check { : G 3 }
+/-- info: { m := 2 } : G 3 -/
+#guard_msgs in #check { m := 2 : G 3 }
+
+
+/-!
+Explicit mode turns off structure instance notation iff there are parameters
+-/
+
+set_option pp.explicit true in
+/-- info: { } : E -/
+#guard_msgs in #check { : E }
+
+set_option pp.explicit true in
+/--
+info: @G.mk (@OfNat.ofNat Nat 3 (instOfNatNat 3))
+  (@OfNat.ofNat Nat 3 (instOfNatNat 3)) : G (@OfNat.ofNat Nat 3 (instOfNatNat 3))
+-/
+#guard_msgs in #check { : G 3 }
+
+/-!
+If universe levels need to be shown, structure instance notation is turned off
+-/
+
+structure U (α : Type _) where
+  x : α
+
+/-- info: { x := 1 } : U Nat -/
+#guard_msgs in #check { x := 1 : U Nat }
+
+set_option pp.universes true in
+/-- info: U.mk.{0} 1 : U.{0} Nat -/
+#guard_msgs in #check { x := 1 : U Nat }
+
+/-!
+Dependence of default value
+-/
+structure H where
+  x : Nat
+  y : Nat := x
+
+/-- info: { x := 1 } : H -/
+#guard_msgs in #check { x := 1 : H }

tests/lean/run/ppUsingAnonymousConstructor.lean
@@ -27,3 +27,7 @@ attribute [pp_using_anonymous_constructor] S
 -/
 /-- info: ⟨2, ⋯⟩ : { n // n < 3 } -/
 #guard_msgs in #check (⟨2, by omega⟩ : {n : Nat // n < 3})
+
+set_option pp.universes true in
+/-- info: Subtype.mk.{1} 2 ⋯ : Subtype.{1} fun n => LT.lt.{0} n 3 -/
+#guard_msgs in #check (⟨2, by omega⟩ : {n : Nat // n < 3})

commit 689f0101e12a39dd40a285a1d6bbe4a4da7f15f4

fix: make pretty printed structure instances hoverable
This PR fixes a bug introduced in #7589, causing pretty printed structure instances to not be hoverable in the Infoview.

This was caused by a choice node being introduced, since `{ $fields,* }` is ambiguous syntax.

src/Lean/PrettyPrinter/Delaborator/Builtins.lean
@@ -671,11 +671,9 @@ def delabStructureInstance : Delab := do
         paramMap := paramMap.insert (← x.fvarId!.getUserName) param
       return paramMap
     let (_, fields) ← collectStructFields s.induct paramMap #[] {} s
-    if ← withType <| getPPOption getPPStructureInstanceType then
-      let tyStx ← withType delab
-      `({ $fields,* : $tyStx })
-    else
-      `({ $fields,* })
+    let tyStx? : Option Term ← withType do
+      if ← getPPOption getPPStructureInstanceType then delab else pure none
+    `({ $fields,* $[: $tyStx?]? })
 
 
 /-- State for `delabAppMatch` and helpers. -/
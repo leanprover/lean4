72d55dedd6d18c61a8bf8e2a2be16a87b1681e01
Kim Morrison <kim@tqft.net>
2025-03-25 15:33:10 +1100

    finRange

--- src/Init/Data/Vector/FinRange.lean
+++ src/Init/Data/Vector/FinRange.lean
@@ -15,20 +15,20 @@ namespace Vector
 /-- `finRange n` is the vector of all elements of `Fin n` in order. -/
 protected def finRange (n : Nat) : Vector (Fin n) n := ofFn fun i => i
 
-@[simp] theorem getElem_finRange (i : Nat) (h : i < n) :
+@[simp] theorem getElem_finRange {i : Nat} (h : i < n) :
     (Vector.finRange n)[i] = ⟨i, h⟩ := by
   simp [Vector.finRange]
 
 @[simp] theorem finRange_zero : Vector.finRange 0 = #v[] := by simp [Vector.finRange]
 
-theorem finRange_succ (n) : Vector.finRange (n+1) =
+theorem finRange_succ {n} : Vector.finRange (n+1) =
     (#v[(0 : Fin (n+1))] ++ (Vector.finRange n).map Fin.succ).cast (by omega) := by
   ext i h
   · simp [getElem_append]
     split <;>
     · simp; omega
 
-theorem finRange_succ_last (n) :
+theorem finRange_succ_last {n} :
     Vector.finRange (n+1) = (Vector.finRange n).map Fin.castSucc ++ #v[Fin.last n] := by
   ext i h
   · simp [getElem_push]
@@ -37,7 +37,7 @@ theorem finRange_succ_last (n) :
     · simp_all
       omega
 
-theorem finRange_reverse (n) : (Vector.finRange n).reverse = (Vector.finRange n).map Fin.rev := by
+theorem finRange_reverse {n} : (Vector.finRange n).reverse = (Vector.finRange n).map Fin.rev := by
   ext i h
   simp
   omega

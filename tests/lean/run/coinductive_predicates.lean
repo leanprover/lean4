da339e579376335ed19a4f332c6308f38ae015c2
Wojciech Rozowski <wojciech@lean-fro.org>
2025-03-26 17:33:43 +0100

    tidy up

--- tests/lean/run/coinductive_predicates.lean
+++ tests/lean/run/coinductive_predicates.lean
@@ -19,14 +19,10 @@ def infinite_chain {α} (step : α → Option α) (x : α) : Prop :=
   ∃ y, step x = some y ∧ (infinite_chain step y)
   greatest_fixpoint
 
-#check infinite_chain.fixpoint_induct
-
 def infinite_chain_fixpoint {α} (step : α → Option α) (x : α) :
   infinite_chain step x = ∃ y, step x = some y ∧ infinite_chain step y := by
     rw [infinite_chain]
 
 theorem infinite_chain.coind {α} (P : α → Prop) (step : α → Option α)
   (h : ∀ (x : α), P x → ∃ y, step x = some y ∧ P y) :
-  ∀ x, P x → infinite_chain step x := by
-    apply infinite_chain.fixpoint_induct
-    apply h
+  ∀ x, P x → infinite_chain step x := infinite_chain.fixpoint_induct _ _ h

d95a2ee35e08d00db5a7c9ac042bb187ec8da7d7
Sofia Rodrigues <sofia@algebraic.dev>
2025-03-27 10:25:23 -0300

    feat: add `FormatConfig` for `GenericFormat` with leap second validation (#7584)
    This PR introduces a structure called `FormatConfig`, which provides
    additional configuration options for `GenericFormat`, such as whether
    leap seconds should be allowed during parsing. By default, this option
    is set to `false`.
    
    This PR also fixes certain flaws to make the implementation less
    permissive by:
    
    - Disallowing the final leap second, such as `2016-12-31T23:59:60Z`,
    when `allowLeapSeconds = false`.
    - Disallowing invalid leap seconds, such as `2017-06-30T23:59:60Z`, when
    `allowLeapSeconds = false`.
    - Disallowing leap-minute time zones, such as
    `2016-12-31T00:00:00+2360`, and out-of-range time zones, such as
    `2016-12-31T00:00:00+2490`.
    
    These changes ensure that Lean aligns with TypeScript's behavior, as
    outlined in this table:
    https://github.com/cedar-policy/cedar-spec/pull/519#issuecomment-2613547897.

--- tests/lean/run/timeFormats.lean
+++ tests/lean/run/timeFormats.lean
@@ -924,3 +924,31 @@ def hFormat : GenericFormat .any := datespec("h hh")
 
 #eval do assert! (not <| hFormat.parseBuilder tuple2Mk "12 32" |>.isOk)
 #eval do assert! (not <| hFormat.parseBuilder tuple2Mk "000001 003" |>.isOk)
+
+/-
+Error tests with some formats.
+-/
+
+/--
+info: zoned("2002-07-14T14:13:12.000000000+23:59")
+-/
+#guard_msgs in
+#eval zoned("2002-07-14T14:13:12+23:59")
+
+/--
+info: Except.error "offset 22: invalid hour offset: 24. Must be between 0 and 23."
+-/
+#guard_msgs in
+#eval ZonedDateTime.fromLeanDateTimeWithZoneString "2002-07-14T14:13:12+24:59"
+
+/--
+info: Except.error "offset 25: invalid minute offset: 60. Must be between 0 and 59."
+-/
+#guard_msgs in
+#eval ZonedDateTime.fromLeanDateTimeWithZoneString "2002-07-14T14:13:12+23:60"
+
+/--
+info: Except.ok (zoned("2002-07-14T14:13:12.000000000Z"))
+-/
+#guard_msgs in
+#eval ZonedDateTime.fromLeanDateTimeWithZoneString "2002-07-14T14:13:12+00:00"

7ddb5b6c5478b90ebdbc9bc7ff033c831200c530
Markus Himmel <markus@lean-fro.org>
2025-03-27 12:41:24 +0100

    Fix

--- src/Init/Data/Int/LemmasAux.lean
+++ src/Init/Data/Int/LemmasAux.lean
@@ -76,10 +76,10 @@ theorem neg_lt_self_iff {n : Int} : -n < n ↔ 0 < n := by
 theorem pos_iff_toNat_pos {n : Int} : 0 < n ↔ 0 < n.toNat := by
   omega
 
-theorem Int.ofNat_toNat_eq_self {a : Int} : a.toNat = a ↔ 0 ≤ a := by omega
-theorem Int.eq_ofNat_toNat {a : Int} : a = a.toNat ↔ 0 ≤ a := by omega
-theorem Int.toNat_le_toNat {n m : Int} (h : n ≤ m) : n.toNat ≤ m.toNat := by omega
-theorem Int.toNat_lt_toNat {n m : Int} (hn : 0 ≤ n) (h : n < m) : n.toNat < m.toNat := by omega
+theorem ofNat_toNat_eq_self {a : Int} : a.toNat = a ↔ 0 ≤ a := by omega
+theorem eq_ofNat_toNat {a : Int} : a = a.toNat ↔ 0 ≤ a := by omega
+theorem toNat_le_toNat {n m : Int} (h : n ≤ m) : n.toNat ≤ m.toNat := by omega
+theorem toNat_lt_toNat {n m : Int} (hn : 0 ≤ n) (h : n < m) : n.toNat < m.toNat := by omega
 
 /-! ### natAbs -/
 

cfb1a6ec7d23622425443302ae0a6196258e4b56
Leonardo de Moura <leomoura@amazon.com>
2025-03-31 13:32:19 -0700

    chore: add reminder

--- tests/lean/run/grind_hcongr.lean
+++ tests/lean/run/grind_hcongr.lean
@@ -1,9 +1,13 @@
+set_option grind.warning false
 def Matrix (m : Type) (n : Type) (α : Type) : Type :=
   m → n → α
+
+-- Remark: the following test used to fail because `grind` was not using
+-- the default reducibility setting when computing the congruence theorem for `M`.
 
 example (o : Type) (m' n' : o → Type) (α : Type)
     (M : (i : o) → Matrix (m' i) (n' i) α)
     (ii : o) (ix : n' ii) (ji : o) (jx : m' ji)
     (j : ii = ji) :
     M ji jx (cast sorry ix) = M ii (cast sorry jx) ix := by
-  grind -- fails with "failed to generate hcongr theorem, insufficient number of arguments"
+  grind

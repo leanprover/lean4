911ca2c535ca95eb7e35f379a269bc73bd3ecb1d
Mac Malone <mac@lean-fro.org>
2025-04-02 18:54:35 +0200

    chore: more verbose `preocmpileArgs` test

--- src/lake/tests/precompileArgs/test.sh
+++ src/lake/tests/precompileArgs/test.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
-set -exo pipefail
-
-LAKE=${LAKE:-../../.lake/build/bin/lake}
-
-if [ "$OS" = Windows_NT ]; then
-LIB_PREFIX=
-SHARED_LIB_EXT=dll
-elif [ "`uname`" = Darwin ]; then
-LIB_PREFIX=lib
-SHARED_LIB_EXT=dylib
-else
-LIB_PREFIX=lib
-SHARED_LIB_EXT=so
-fi
+source ../common.sh
 
 ./clean.sh
 
 # Test that `moreLinkArgs` are included when linking precompiled modules
-($LAKE build -KlinkArgs=-lBaz 2>&1 || true) | grep --color -- "-lBaz"
+test_maybe_err "-lBaz" build -KlinkArgs=-lBaz
 
 # Test that dynlibs are part of the module trace unless `platformIndependent` is set
-$LAKE build -R
+test_run build -R
 echo foo > .lake/build/lib/lean/Foo_Bar.$SHARED_LIB_EXT
-($LAKE build 2>&1 --rehash && exit 1 || true) | grep --color "Building Foo"
+test_err "Building Foo" build --rehash
 rm .lake/build/lib/lean/Foo_Bar.$SHARED_LIB_EXT
-$LAKE build -R -KplatformIndependent=true
+test_run build -R -KplatformIndependent=true
 echo foo > .lake/build/lib/lean/Foo_Bar.$SHARED_LIB_EXT
-$LAKE build --rehash --no-build
+test_run build --rehash --no-build
+
+# cleanup
+rm -f produced.out

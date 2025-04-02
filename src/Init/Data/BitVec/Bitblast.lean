df104d326d8de845d43ac25440dee662528ed705
Siddharth <siddu.druid@gmail.com>
2025-04-02 08:11:34 +0100

    Update src/Init/Data/BitVec/Bitblast.lean

--- src/Init/Data/BitVec/Bitblast.lean
+++ src/Init/Data/BitVec/Bitblast.lean
@@ -1,4 +1,4 @@
-        /-
+/-
 Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Harun Khan, Abdalrhman M Mohamed, Joe Hendrix, Siddharth Bhat

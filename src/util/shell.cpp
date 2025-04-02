bd24ca3093b314b348c63826f7a868e6433b6acc
Sebastian Ullrich <sebasti@nullri.ch>
2025-04-02 12:10:46 +0200

    test: re-elaboration benchmarks (#7784)
    Tests language server memory use by repeatedly re-elaborate a given file

--- src/util/shell.cpp
+++ src/util/shell.cpp
@@ -514,7 +514,7 @@ extern "C" LEAN_EXPORT int lean_main(int argc, char ** argv) {
     buffer<string_ref> forwarded_args;
     buffer<name> error_kinds;
 
-    while (true) {
+    while (!run) {  // stop consuming arguments after `--run`
         int c = getopt_long(argc, argv, g_opt_str, g_long_options, NULL);
         if (c == -1)
             break; // end of command line
@@ -561,6 +561,10 @@ extern "C" LEAN_EXPORT int lean_main(int argc, char ** argv) {
                 break;
             case 'r':
                 run = true;
+                // apparently this call is necessary for `--run` to be permuted in front of any
+                // preceding file name arguments (we shouldn't permute *after* `--run` because those
+                // arguments are not ours)
+                getopt_long(argc, argv, g_opt_str, g_long_options, NULL);
                 break;
             case 'o':
                 olean_fn = optarg;

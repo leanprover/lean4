5f2f010d6611d5f14fbfe1fc138e97ebad1f5f2f
Leonardo de Moura <leomoura@amazon.com>
2025-04-03 15:57:49 -0700

    fix: missing forall normalization rules in `grind` (#7808)
    This PR adds missing forall normalization rules to `grind`.

--- tests/lean/grind/list_problems.lean
+++ tests/lean/grind/list_problems.lean
@@ -2,18 +2,15 @@ theorem getElem?_eq_some_iff {l : List α} : l[i]? = some a ↔ ∃ h : i < l.le
   induction l
   · grind
   · cases i
-    · -- Fails because of the issue:
-      --   [issue] failed to create E-match local theorem for
-      --     ∀ (x : 1 ≤ tail.length), ¬tail[0] = a
-      -- despite having asserted `1 ≤ tail.length `.
+    · -- Better support for implication and dependent implication.
+      -- We need inequality propagation (or case-splits)
       grind
     · -- Similarly
       grind
 
 attribute [grind] List.getElem_append_left List.getElem_append_right
+attribute [grind] List.length_cons List.length_nil
 
-@[simp] theorem getElem_concat_length {l : List α} {a : α} {i : Nat} (h : i = l.length) (w) :
+example {l : List α} {a : α} {i : Nat} (h : i = l.length) (w) :
     (l ++ [a])[i]'w = a := by
-  subst h; grind
--- [issue] failed to create E-match local theorem for
---   ∀ (h₁ : True), (l ++ [a])[l.length] = [a][l.length - l.length]
+  grind -- Similar to issue above.

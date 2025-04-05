dea1edfd6bb80c6aff984732fd6629b5b89feece
Leonardo de Moura <leomoura@amazon.com>
2025-04-05 11:43:11 -0700

    test: model

--- tests/lean/run/grind_mbtc_1.lean
+++ tests/lean/run/grind_mbtc_1.lean
@@ -43,3 +43,24 @@ example (f : Nat → Nat) (x : Nat)
 example (f : Nat → Nat → Nat) (x y : Nat)
     : x ≠ 0 → x ≤ 1 → f x y = 2 → f 1 y = 2 := by
   grind
+
+
+-- `b` must not be `2`. Otherwise, `f (b+1)` and `f 3` must be equal.
+/-- info: [grind.cutsat.model] b := 3 -/
+#guard_msgs (info) in
+set_option trace.grind.cutsat.model true in
+example (f : Int → α) (a b : Int) : b > 1 → f (b + 1) = x → f 3 = y → x = y := by
+  (fail_if_success grind); sorry
+
+-- `b` must not be `2`. Otherwise, `f (b+1)` and `f 3` must be equal.
+/--
+info: [grind.cutsat.model] x := 7
+[grind.cutsat.model] y := 8
+[grind.cutsat.model] b := 3
+[grind.cutsat.model] 「f 3」 := 8
+[grind.cutsat.model] 「f (b + 1)」 := 7
+-/
+#guard_msgs (info) in
+set_option trace.grind.cutsat.model true in
+example (f : Int → Int) (a b : Int) : b > 1 → f (b + 1) = x → f 3 = y → x = y := by
+  (fail_if_success grind); sorry

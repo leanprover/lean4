26af253858c7d3da5107329d0edf3c109bcb0adb
Leonardo de Moura <leomoura@amazon.com>
2025-04-05 20:02:40 -0700

    fix: `shareCommon` at `Cutsat/Norm.lean`

--- src/Lean/Meta/Tactic/Grind/Arith/Cutsat/Norm.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Cutsat/Norm.lean
@@ -16,10 +16,10 @@ and these expressions must be normalized inside of the cutsat module.
 /-- Converts the given integer expression into `Int.Linear.Expr` -/
 partial def toLinearExpr (e : Expr) (generation : Nat := 0) : GoalM Int.Linear.Expr := do
   let toVar (e : Expr) := do
+    let e ← shareCommon e
     if (← alreadyInternalized e) then
       return .var (← mkVar e)
     else
-      let e ← shareCommon e
       internalize e generation
       return .var (← mkVar e)
   let mul (a b : Expr) := do

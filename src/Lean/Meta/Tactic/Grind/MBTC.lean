45591186105aac8d7f0583a46127ccd8d6ec3135
Leonardo de Moura <leomoura@amazon.com>
2025-04-11 12:41:53 -0700

    fix: skip `mbtc` if max number of splits has been reached

--- src/Lean/Meta/Tactic/Grind/MBTC.lean
+++ src/Lean/Meta/Tactic/Grind/MBTC.lean
@@ -44,6 +44,8 @@ private def mkCandidateKey (a b : Expr) : Expr × Expr :=
 /-- Model-based theory combination. -/
 def mbtc (ctx : MBTC.Context) : GoalM Bool := do
   unless (← getConfig).mbtc do return false
+  -- It is pointless to run `mbtc` if maximum number of splits has been reached.
+  if (← checkMaxCaseSplit) then return false
   let mut map : Map := {}
   let mut candidates : Candidates := {}
   for ({ expr := e }, _) in (← get).enodes do

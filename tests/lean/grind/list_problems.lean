4b50adb3d037c28642ec2f4072d88308e3d2f552
Leonardo de Moura <leomoura@amazon.com>
2025-04-11 14:01:55 -0700

    chore: wip

--- tests/lean/grind/list_problems.lean
+++ tests/lean/grind/list_problems.lean
@@ -1,10 +1,19 @@
 reset_grind_attrs%
+set_option grind.warning false
 
-attribute [grind] List.getElem_append_left List.getElem_append_right
-attribute [grind] List.length_cons List.length_nil
+attribute [grind] List.length_cons List.length_nil List.length_append
+
+@[grind] theorem append_aux (as bs : List α) (i : Nat) (h : i < (as ++ bs).length) :
+        (as ++ bs)[i] = if _ : i < as.length then as[i] else bs[i - as.length]'(by grind) := by
+  sorry
+
+attribute [grind] List.nil_append
+attribute [grind] List.eq_nil_of_length_eq_zero
 
 example {l : List α} {a : α} {i : Nat} (h : i = l.length) (w) :
     (l ++ [a])[i]'w = a := by
-  grind -- Similar to issue above.
+--  set_option trace.Meta.isDefEq true in
+  set_option trace.grind.ematch.instance true in
+  grind  -- Similar to issue above.
 
 ---

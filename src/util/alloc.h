32f3fa872ae89e8f97ceb524a6d65444dcff4e5b
Sebastian Ullrich <sebasti@nullri.ch>
2025-04-11 10:56:29 +0200

    perf: use mimalloc in `sharecommon`

--- src/util/alloc.h
+++ src/util/alloc.h
@@ -6,6 +6,8 @@ Authors: Sebastian Ullrich
 */
 #pragma once
 #include <lean/config.h>
+#include <unordered_map>
+#include <unordered_set>
 
 #ifdef LEAN_MIMALLOC
 #include <lean/mimalloc.h>
@@ -25,4 +27,11 @@ template<
     class KeyEqual = std::equal_to<Key>,
     class Allocator = allocator<std::pair<const Key, T>>
 > using unordered_map = std::unordered_map<Key, T, Hash, KeyEqual, Allocator>;
+
+template<
+    class Key,
+    class Hash = std::hash<Key>,
+    class KeyEqual = std::equal_to<Key>,
+    class Allocator = lean_allocator<Key>
+> using unordered_set = std::unordered_set<Key, Hash, KeyEqual, Allocator>;
 };

f6168485381c9fc62c2686aab4e7fd3edc079d9b
Sebastian Ullrich <sebasti@nullri.ch>
2025-04-11 18:07:06 +0200

    doc

--- src/util/alloc.h
+++ src/util/alloc.h
@@ -14,11 +14,18 @@ Authors: Sebastian Ullrich
 #endif
 
 namespace lean {
+
+// We do not override `new` to avoid FFI issues for users but use `lean::allocator`
+// explicitly where using the custom allocator is important.
+
 #ifdef LEAN_MIMALLOC
 template<class T> using allocator = mi_stl_allocator<T>;
 #else
 template<class T> using allocator = std::allocator<T>;
 #endif
+
+// `unordered_map/set` allocates per insert, so specializing to the custom allocator can
+// save significant time for maps with frequent inserts.
 
 template<
     class Key,

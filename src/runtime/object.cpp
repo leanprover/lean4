9bf71fd3b054235fca415b4b4ff19a8b885fb181
Sebastian Ullrich <sebasti@nullri.ch>
2025-04-14 15:48:22 +0200

    fix

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -963,8 +963,8 @@ public:
 
     uint8_t get_task_state(lean_task_object * t) {
         unique_lock<mutex> lock(m_mutex);
-        if (o->m_imp) {
-            if (o->m_imp->m_closure) {
+        if (t->m_imp) {
+            if (t->m_imp->m_closure) {
                 return 0; // waiting (waiting/queued)
             } else {
                 return 1; // running (running/promised)
@@ -1168,9 +1168,10 @@ extern "C" LEAN_EXPORT void lean_io_cancel_core(b_obj_arg t) {
 }
 
 extern "C" LEAN_EXPORT uint8_t lean_io_get_task_state_core(b_obj_arg t) {
+    lean_task_object * o = lean_to_task(t);
     if (!o->m_imp)
         return 2; // finished
-    return g_task_manager->get_task_state(lean_to_task(t));
+    return g_task_manager->get_task_state(o);
 }
 
 extern "C" LEAN_EXPORT b_obj_res lean_io_wait_any_core(b_obj_arg task_list) {

9b86cd73188dac40808dd41216a39edd869a81b7
Mac Malone <mac@lean-fro.org>
2025-04-16 21:03:29 +0200

    fix: lake: external library linking

---
+++ src/lake/tests/externLib/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source ../common.sh
+source ./clean.sh
+
+# Tests the successful compilation of an `extern_lib`
+test_run -d ffi -v exe test
+
+# Tests the successful precompilation of an `extern_lib` (from a dep)
+test_run -v build Test
+
+# Tests the successful compilation of an `extern_lib` from a dep
+test_run -v exe test

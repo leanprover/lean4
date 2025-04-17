7c21a569cf84078282ea7b23d7c97acb1bac1048
Kim Morrison <kim@tqft.net>
2025-04-17 12:28:28 +1000

    fix copyright header

--- tests/lean/run/grind_qsort.lean
+++ tests/lean/run/grind_qsort.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Leonardo de Moura
+Authors: Kim Morrison
 -/
 
 -- FIXME: when `grind` is ready for production use, move this file to `src/Init/Data/Array/QSort/Lemmas.lean`.

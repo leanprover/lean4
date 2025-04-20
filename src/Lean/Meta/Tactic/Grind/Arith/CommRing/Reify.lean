a711e456fa128b1646c1f37e14f8cf87fa01ab25
Leonardo de Moura <leomoura@amazon.com>
2025-04-19 17:44:36 -0700

    feat: add `Reify.lean`

---
+++ src/Lean/Meta/Tactic/Grind/Arith/CommRing/Reify.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+prelude
+import Lean.Meta.Tactic.Grind.Simp
+import Lean.Meta.Tactic.Grind.Arith.CommRing.Util
+
+namespace Lean.Meta.Grind.Arith.CommRing
+
+end  Lean.Meta.Grind.Arith.CommRing

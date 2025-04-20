c571645ee90a7f70a73807ebde5d9f398261f720
Leonardo de Moura <leomoura@amazon.com>
2025-04-19 17:23:14 -0700

    chore: cleanup

--- src/Lean/Meta/Tactic/Grind/Arith/CommRing/Internalize.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/CommRing/Internalize.lean
@@ -9,27 +9,24 @@ import Lean.Meta.Tactic.Grind.Arith.CommRing.RingId
 
 namespace Lean.Meta.Grind.Arith.CommRing
 
-private inductive SupportedTermKind where
-  | add | mul | num | sub | neg | pow
-  deriving BEq
-
-private def getKindAndType? (e : Expr) : Option (SupportedTermKind × Expr) :=
+/-- If `e` is a function application supported by the `CommRing` module, return its type. -/
+private def getType? (e : Expr) : Option Expr :=
   match_expr e with
-  | HAdd.hAdd α _ _ _ _ _ => some (.add, α)
-  | HSub.hSub α _ _ _ _ _ => some (.sub, α)
-  | HMul.hMul α _ _ _ _ _ => some (.mul, α)
+  | HAdd.hAdd α _ _ _ _ _ => some α
+  | HSub.hSub α _ _ _ _ _ => some α
+  | HMul.hMul α _ _ _ _ _ => some α
   | HPow.hPow α β _ _ _ _ =>
     let_expr Nat := β | none
-    some (.pow, α)
-  | Neg.neg α _ a =>
-    let_expr OfNat.ofNat _ _ _ := a | some (.neg, α)
-    some (.num, α)
-  | OfNat.ofNat α _ _ => some (.num, α)
+    some α
+  | Neg.neg α _ _ => some α
+  | OfNat.ofNat α _ _ => some α
+  | NatCast.natCast α _ _ => some α
+  | IntCast.intCast α _ _ => some α
   | _ => none
 
 def internalize (e : Expr) (parent? : Option Expr) : GoalM Unit := do
   unless (← getConfig).ring do return ()
-  let some (kind, type) := getKindAndType? e | return ()
+  let some type := getType? e | return ()
   let some ringId ← getRingId? type | return ()
   trace[grind.ring.internalize] "{e}, {ringId}, {parent?}"
   return ()

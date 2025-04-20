f278336492f3d4c1de38775efc1c9c9f6c1f7503
Leonardo de Moura <leomoura@amazon.com>
2025-04-19 20:19:35 -0700

    chore: helper function

--- src/Lean/Meta/Tactic/Grind/Arith/CommRing/Util.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/CommRing/Util.lean
@@ -34,11 +34,26 @@ def setTermRingId (e : Expr) (ringId : Nat) : GoalM Unit := do
     return ()
   modify' fun s => { s with exprToRingId := s.exprToRingId.insert { expr := e } ringId }
 
+/--
+Converts the given ring expression into a multivariate polynomial.
+If the ring has a nonzero characteristic, it is used during normalization.
+-/
 def toPoly (e : RingExpr) (ringId : Nat) : GoalM Poly := do
   let ring ← getRing ringId
   if let some (_, c) := ring.charInst? then
     if c != 0 then
       return e.toPolyC c
   return e.toPoly
+
+/--
+Returns a context of type `RArray α` containing the variables of the given ring.
+`α` is the type of the ring.
+-/
+def toContextExpr (ringId : Nat) : GoalM Expr := do
+  let ring ← getRing ringId
+  if h : 0 < ring.vars.size then
+    RArray.toExpr ring.type id (RArray.ofFn (ring.vars[·]) h)
+  else
+    RArray.toExpr ring.type id (RArray.leaf (mkApp ring.natCastFn (toExpr 0)))
 
 end Lean.Meta.Grind.Arith.CommRing

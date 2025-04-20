c6f6fc4bfb5c425a5a2876938a4a52ca243ccc14
Leonardo de Moura <leomoura@amazon.com>
2025-04-19 19:08:49 -0700

    feat: `CommRing` reification

--- src/Lean/Meta/Tactic/Grind/Arith/CommRing/Reify.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/CommRing/Reify.lean
@@ -6,42 +6,104 @@ Authors: Leonardo de Moura
 prelude
 import Lean.Meta.Tactic.Grind.Simp
 import Lean.Meta.Tactic.Grind.Arith.CommRing.Util
+import Lean.Meta.Tactic.Grind.Arith.CommRing.Var
 
 namespace Lean.Meta.Grind.Arith.CommRing
 
-partial def reify (e : Expr) (ringId : Nat) (generation : Nat := 0) : GoalM RingExpr := do
-  return .num 0
-/-
-  let toVar (e : Expr) := do
-    let e ← shareCommon e
-    if (← alreadyInternalized e) then
-      return .var (← mkVar e)
-    else
-      internalize e generation
-      return .var (← mkVar e)
-  let mul (a b : Expr) := do
-    match (← getIntValue? a) with
-    | some k => return .mulL k (← toLinearExpr b)
-    | none => match (← getIntValue? b) with
-      | some k => return .mulR (← toLinearExpr a) k
-      | none => toVar e
-  match_expr e with
-  | OfNat.ofNat _ _ _ =>
-    if let some n ← getIntValue? e then return .num n
-    else toVar e
-  | Neg.neg _ i a =>
-    if (← isInstNegInt i) then return .neg (← toLinearExpr a)
-    else toVar e
-  | HAdd.hAdd _ _ _ i a b =>
-    if (← isInstHAddInt i) then return .add (← toLinearExpr a) (← toLinearExpr b)
-    else toVar e
-  | HSub.hSub _ _ _ i a b =>
-    if (← isInstHSubInt i) then return .sub (← toLinearExpr a) (← toLinearExpr b)
-    else toVar e
-  | HMul.hMul _ _ _ i a b =>
-    if (← isInstHMulInt i) then mul a b
-    else toVar e
-  | _ => toVar e
+def isAddInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.addFn.appArg! inst
+def isMulInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.mulFn.appArg! inst
+def isSubInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.subFn.appArg! inst
+def isNegInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.negFn.appArg! inst
+def isPowInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.powFn.appArg! inst
+def isIntCastInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.intCastFn.appArg! inst
+def isNatCastInst (ring : Ring) (inst : Expr) : Bool :=
+  isSameExpr ring.natCastFn.appArg! inst
+
+private def reportAppIssue (e : Expr) : GoalM Unit := do
+  reportIssue! "comm ring term with unexpected instance{indentExpr e}"
+
+/--
+Converts a Lean expression `e` in the `CommRing` with id `ringId` into
+a `CommRing.Expr` object.
 -/
+partial def reify? (e : Expr) (ringId : Nat) : GoalM (Option RingExpr) := do
+  let ring ← getRing ringId
+  let toVar (e : Expr) : GoalM RingExpr := do
+    return .var (← mkVar e ringId)
+  let asVar (e : Expr) : GoalM RingExpr := do
+    reportAppIssue e
+    return .var (← mkVar e ringId)
+  let rec go (e : Expr) : GoalM RingExpr := do
+    match_expr e with
+    | HAdd.hAdd _ _ _ i a b =>
+      if isAddInst ring i then return .add (← go a) (← go b) else asVar e
+    | HMul.hMul _ _ _ i a b =>
+      if isMulInst ring i then return .mul (← go a) (← go b) else asVar e
+    | HSub.hSub _ _ _ i a b =>
+      if isSubInst ring i then return .sub (← go a) (← go b) else asVar e
+    | HPow.hPow _ _ _ i a b =>
+      let some k ← getNatValue? b | toVar e
+      if isPowInst ring i then return .pow (← go a) k else asVar e
+    | Neg.neg _ i a =>
+      if isNegInst ring i then return .neg (← go a) else asVar e
+    | IntCast.intCast _ i e =>
+      if isIntCastInst ring i then
+        let some k ← getIntValue? e | toVar e
+        return .num k
+      else
+        asVar e
+    | NatCast.natCast _ i e =>
+      if isNatCastInst ring i then
+        let some k ← getNatValue? e | toVar e
+        return .num k
+      else
+        asVar e
+    | OfNat.ofNat _ n _ =>
+      let some k ← getNatValue? n | toVar e
+      if (← withDefault <| isDefEq e (mkApp ring.natCastFn n)) then
+        return .num k
+      else
+        asVar e
+    | _ => toVar e
+  let asNone (e : Expr) : GoalM (Option RingExpr) := do
+    reportAppIssue e
+    return none
+  match_expr e with
+  | HAdd.hAdd _ _ _ i a b =>
+    if isAddInst ring i then return some (.add (← go a) (← go b)) else asNone e
+  | HMul.hMul _ _ _ i a b =>
+    if isMulInst ring i then return some (.mul (← go a) (← go b)) else asNone e
+  | HSub.hSub _ _ _ i a b =>
+    if isSubInst ring i then return some (.sub (← go a) (← go b)) else asNone e
+  | HPow.hPow _ _ _ i a b =>
+    let some k ← getNatValue? b | return none
+    if isPowInst ring i then return some (.pow (← go a) k) else asNone e
+  | Neg.neg _ i a =>
+    if isNegInst ring i then return some (.neg (← go a)) else asNone e
+  | IntCast.intCast _ i e =>
+    if isIntCastInst ring i then
+      let some k ← getIntValue? e | return none
+      return some (.num k)
+    else
+      asNone e
+  | NatCast.natCast _ i e =>
+    if isNatCastInst ring i then
+      let some k ← getNatValue? e | return none
+      return some (.num k)
+    else
+      asNone e
+  | OfNat.ofNat _ n _ =>
+    let some k ← getNatValue? n | return none
+    if (← withDefault <| isDefEq e (mkApp ring.natCastFn n)) then
+      return some (.num k)
+    else
+      asNone e
+  | _ => return none
 
 end  Lean.Meta.Grind.Arith.CommRing

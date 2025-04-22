8ce156e55296151674e21f46048536041f0052ed
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-04-22 08:00:46 +0200

    make all (D)Tree(Map|Set) files transitive dependencies of Std.Data

---
+++ src/Std/Data/DTreeMap/Raw.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Reichert
+-/
+import Std.Data.DTreeMap.Raw.Basic
+import Std.Data.DTreeMap.Raw.AdditionalOperations
+import Std.Data.DTreeMap.Raw.Lemmas
+import Std.Data.DTreeMap.Raw.WF
--- src/Std/Data/TreeMap/Raw.lean
+++ src/Std/Data/TreeMap/Raw.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Paul Reichert
 -/
 prelude
-import Std.Data.TreeMap.Basic
-import Std.Data.TreeMap.AdditionalOperations
-import Std.Data.TreeMap.Lemmas
+import Std.Data.TreeMap.Raw.Basic
+import Std.Data.TreeMap.Raw.AdditionalOperations
+import Std.Data.TreeMap.Raw.Lemmas
+import Std.Data.TreeMap.Raw.WF

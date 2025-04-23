33d9778b99f0960d1c5f306f3f7c97fbade37b63
Henrik Böving <hargonix@gmail.com>
2025-04-23 15:38:14 +0200

    i guess not

--- tests/lean/run/async_select_timer.lean
+++ tests/lean/run/async_select_timer.lean
@@ -16,33 +16,14 @@ def test1 : IO (AsyncTask Nat) := do
   let task ← test1
   IO.ofExcept task.get
 
-def test2Helper (dur : Time.Millisecond.Offset) : IO (AsyncTask Nat) := do
+def test2 : IO (AsyncTask Nat) := do
   Selectable.one #[
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 1,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 2,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 3,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 4,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 5,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 6,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 7,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 8,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 9,
-    .case (← Selector.sleep dur) fun _ => return AsyncTask.pure 10,
+    .case (← Selector.sleep 1500) fun _ => return AsyncTask.pure 2,
+    .case (← Selector.sleep 1000) fun _ => return AsyncTask.pure 1,
   ]
 
-def test2 (dur : Time.Millisecond.Offset) : IO Bool := do
-  let r1 ← IO.ofExcept (← test2Helper dur).get
-  let r2 ← IO.ofExcept (← test2Helper dur).get
-  let r3 ← IO.ofExcept (← test2Helper dur).get
-  let r4 ← IO.ofExcept (← test2Helper dur).get
-  let r5 ← IO.ofExcept (← test2Helper dur).get
-  let r6 ← IO.ofExcept (← test2Helper dur).get
-  let r7 ← IO.ofExcept (← test2Helper dur).get
-  let r8 ← IO.ofExcept (← test2Helper dur).get
-  let r9 ← IO.ofExcept (← test2Helper dur).get
-  let r10 ← IO.ofExcept (← test2Helper dur).get
-  return #[r2, r3, r4, r5, r6, r7, r8, r9, r10].any (· != r1)
-
-/-- info: true -/
+/-- info: 1 -/
 #guard_msgs in
-#eval test2 0
+#eval show IO _ from do
+  let task ← test2
+  IO.ofExcept task.get

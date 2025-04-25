fb738a8cbf4926a9009b08aa0a016935a408f58b
Joachim Breitner <mail@joachim-breitner.de>
2025-04-25 18:00:53 +0200

    FunIndInfo fixes

--- tests/lean/run/issue7550.lean
+++ tests/lean/run/issue7550.lean
@@ -40,9 +40,7 @@ hfuel✝ : x✝ < fuel✝.succ
 h✝ : 0 < y ∧ y ≤ x✝
 this✝ : x✝ - y < x✝
 ih1✝ : Bug.divCore (x✝ - y) y fuel✝ ⋯ = 42
-⊢ (let_fun this := ⋯;
-    Bug.divCore (x✝ - y) y fuel✝ ⋯ + 1) =
-    42
+⊢ Bug.divCore (x✝ - y) y fuel✝ ⋯ + 1 = 42
 
 case case3
 x y fuel x✝ fuel✝ : Nat
@@ -68,9 +66,7 @@ hfuel✝ : x✝ < fuel✝.succ
 h✝ : 0 < y ∧ y ≤ x✝
 this✝ : x✝ - y < x✝
 ih1✝ : Bug.divCore (x✝ - y) y fuel✝ ⋯ = 42
-⊢ (let_fun this := ⋯;
-    Bug.divCore (x✝ - y) y fuel✝ ⋯ + 1) =
-    42
+⊢ Bug.divCore (x✝ - y) y fuel✝ ⋯ + 1 = 42
 
 case case3
 x y fuel x✝ fuel✝ : Nat

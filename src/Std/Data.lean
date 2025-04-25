563580dd59aa8252daf4813d5521a33bad56b068
Rob23oba <robin.arnez@web.de>
2025-04-25 22:11:31 +0200

    unorphan

--- src/Std/Data.lean
+++ src/Std/Data.lean
@@ -11,6 +11,7 @@ import Std.Data.DTreeMap
 import Std.Data.TreeMap
 import Std.Data.TreeSet
 import Std.Data.ExtDHashMap
+import Std.Data.ExtHashMap
 
 -- The imports above only import the modules needed to work with the version which bundles
 -- the well-formedness invariant, so we need to additionally import the files that deal with the

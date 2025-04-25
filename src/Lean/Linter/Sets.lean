07cdc77efc3119c01c8897174b8c03d94725c68e
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-04-25 18:13:14 +0200

    feat: implement "linter sets" that can be turned on as a group
    This PR adds a `register_linter_set` command for declaring linter sets. The `getLinterValue` function now checks if the present linter is contained in a set that has been enabled (using the `set_option` command or on the command line).
    
    The implementation stores linter set membership in an environment extension. As a consequence, we need to pass more data to `getLinterValue`: the argument of ype `Options` has been replaced with a `LinterOptions`, which you can access by writing `getLinterOptions` instead of `getOptions`. (The alternative I considered is to modify the `Options` structure. The current approach seems a bit higher-level and lower-impact.)
    
    The logic for checking whether a linter should be enabled now goes in four steps:
    1. If the linter has been explicitly en/disabled, return that.
    2. If `linter.all` has been explicitly set, return that.
    3. If the linter is in any set that has been enabled, return true.
    4. Return the default setting for the linter.
    
    Reasoning:
    * The linter's explicit setting should take precedence.
    * We want to be able to disable all but the explicitly enabled linters with `linter.all`, so it should take precedence over linter sets.
    * We want to progressively enable more linters as they become available, so the check over sets should be *any*.
    * Falling back to the default value last, ensures compatibility with the current way we define linters.
    
    The public-facing API currently does not allow modifying sets: all linters have to be added when the set is declared. This way, there is one place where all the contents of the set are listed.
    
    Linter sets can be declared to contain linters that have not been declared (yet): this allows declaring linter sets low down in the import hierarchy when not all the requested linters are defined yet.

---
+++ src/Lean/Linter/Sets.lean
@@ -0,0 +1,33 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anne Baanen
+-/
+prelude
+import Lean.Elab.Command
+import Lean.Linter.Basic
+
+namespace Lean.Linter
+
+/-- Add a new linter set that contains the given linters. -/
+def insertLinterSet [MonadEnv m] (setName : Name) (linterNames : Array Name) : m Unit :=
+  modifyEnv (linterSetsExt.addEntry · (setName, linterNames))
+
+/-- Declare a new linter set by giving the set of options that will be enabled along with the set. -/
+def registerSet (setName : Name) (ref : Name := by exact decl_name%) : IO (Lean.Option Bool) := do
+  registerOption setName {
+    declName := ref
+    defValue := false
+    group := "linterSet"
+    descr := ""
+  }
+  return { name := setName, defValue := false }
+
+open Lean.Elab.Command in
+elab doc?:(docComment)? "register_linter_set" name:ident " := " decl:ident* : command => do
+  insertLinterSet name.getId <| decl.map (·.getId)
+  let initializer ← `($[$doc?]? initialize $name : Lean.Option Bool ← Lean.Linter.registerSet $(quote name.getId))
+  withMacroExpansion (← getRef) initializer <| elabCommand initializer
+
+
+end Lean.Linter

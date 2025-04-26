e08665dde2106fd2378f1e9081306bbe0cfd19ef
Wojciech Rozowski <wojciech@lean-fro.org>
2025-04-26 13:18:30 +0200

    Fixes with tests

--- tests/lean/run/partial_fixpoint_coinductive_pred.lean
+++ tests/lean/run/partial_fixpoint_coinductive_pred.lean
@@ -43,7 +43,7 @@ coinductive infinite_chain step : α → Prop where
 ```
 -/
 def infinite_chain {α} (step : α → Option α) (x : α) : Prop :=
-  ∃ y, step x = some y ∧ (infinite_chain step y)
+  ∃ y, step x = some y ∧ infinite_chain step y
   partial_fixpoint
 
 theorem infinite_chain.intro {α} (step : α → Option α) (y x : α) :

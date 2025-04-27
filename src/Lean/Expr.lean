9fe21a41a679d525e603aa20186b36ad13ed23dd
Leonardo de Moura <leomoura@amazon.com>
2025-04-27 12:50:52 -0700

    feat: add `mkPropEq`

--- src/Lean/Expr.lean
+++ src/Lean/Expr.lean
@@ -2210,6 +2210,10 @@ private def natEqPred : Expr :=
 def mkNatEq (a b : Expr) : Expr :=
   mkApp2 natEqPred a b
 
+/-- Given `a b : Prop`, return `a = b` -/
+def mkPropEq (a b : Expr) : Expr :=
+  mkApp3 (mkConst ``Eq [levelZero]) (mkSort levelZero) a b
+
 /-! Constants for Int typeclasses. -/
 namespace Int
 

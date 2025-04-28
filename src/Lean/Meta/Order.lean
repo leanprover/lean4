d0aadf12d254ae2779cd4da9abb871a7c585470d
Wojciech Rozowski <wojciech@lean-fro.org>
2025-04-28 21:06:58 +0200

    Add comments to Meta.Order

--- src/Lean/Meta/Order.lean
+++ src/Lean/Meta/Order.lean
@@ -10,9 +10,20 @@ import Lean.Meta.PProdN
 import Lean.Meta.AppBuilder
 import Init.Internal.Order.Basic
 
+/-!
+This module has meta-functions for constructions expressions related to `Lean.Order`.
+In particular some of these functions can handle the `CCPO` and `CompleteLattice`
+structures conveniently uniformly, picking the right one based on the type of the arguments.
+-/
+
 namespace Lean.Meta
 open Order
 
+/--
+Given `inst : CCPO (t[x])` for some FVar `x`, constructs an instance
+of the type `CCPO (∀ x, t[x])`.
+Can handle `CompleteLattice` as well.
+--/
 def mkInstPiOfInstForall (x : Expr) (inst : Expr) : MetaM Expr := do
   if (←inferType inst).isAppOf ``CCPO then
     mkAppOptM ``instCCPOPi #[(← inferType x), none, (← mkLambdaFVars #[x] inst)]
@@ -21,6 +32,12 @@ def mkInstPiOfInstForall (x : Expr) (inst : Expr) : MetaM Expr := do
   else
     throwError "mkInstPiOfInstForall: unexpected type of {inst}"
 
+/--
+Given a function `f : α → α`, an instance `inst : CCPO α`
+and a monotonicity proof `hmono : monotone f`, constructs a least fixpoint of `f`
+with respect to the instance `inst`. The `packedType` is assumed to contain the type `α`.
+Can handle `CompleteLattice` as well.
+-/
 def mkFixOfMonFun (packedType : Expr) (packedInst : Expr) (hmono : Expr) : MetaM Expr := do
   if (←inferType packedInst).isAppOf ``CCPO then
     mkAppOptM ``fix #[packedType, packedInst, none, hmono]
@@ -29,6 +46,12 @@ def mkFixOfMonFun (packedType : Expr) (packedInst : Expr) (hmono : Expr) : MetaM
   else
     throwError "mkFixOfMonFun: unexpected type of {packedInst}"
 
+/--
+Given `packedInst : CCPO α `, returns an underlying instance of the type
+`PartialOrder α`. Can handle `CompleteLattice` as well.
+Takes an optional argument with the type `α`. If the optional arument is `none`,
+it is treated implicitly.
+-/
 def toPartialOrder (packedInst : Expr) (type : Option Expr := .none) := do
   if (←inferType packedInst).isAppOf ``CCPO then
     mkAppOptM ``CCPO.toPartialOrder #[type, packedInst]
@@ -37,12 +60,25 @@ def toPartialOrder (packedInst : Expr) (type : Option Expr := .none) := do
   else
     throwError "getUnderlyingOrder: unexpected type of {packedInst}"
 
+/--
+Given CCPO instances `inst₁ : CCPO α₁` and `inst₂ : CCPO α₂`,
+constructs an instance of the type `CCPO (α₁ × α₂)`.
+-/
 def mkInstCCPOPProd (inst₁ inst₂ : Expr) : MetaM Expr := do
   mkAppOptM ``instCCPOPProd #[none, none, inst₁, inst₂]
 
+/--
+Given CCPO instances `inst₁ : CompleteLattice α₁` and `inst₂ : CompleteLattice α₂`,
+constructs an instance of the type `CompleteLattice (α₁ × α₂)`.
+-/
 def mkInstCompleteLatticePProd (inst₁ inst₂ : Expr) : MetaM Expr := do
   mkAppOptM ``instCompleteLatticePProd #[none, none, inst₁, inst₂]
 
+/--
+Given an array of CCPO instances `insts = #[CCPO α₁, ..., CCPO αₙ]`, constructs an instance
+of the type `CCPO (α₁ × ... × αₙ)`.
+Can handle `CompleteLattice` as well.
+-/
 def mkPackedPPRodInstance (insts : Array Expr) : MetaM Expr := do
   let types ← insts.mapM inferType
   if (types.all (fun t => t.isAppOf ``CCPO)) then

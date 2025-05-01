29a4d814191ddb21596be3a47ec7c5ecac1e5356
Leonardo de Moura <leomoura@amazon.com>
2025-05-01 16:29:05 -0700

    chore: fix test

--- tests/lean/grind/hashmap_list.lean
+++ tests/lean/grind/hashmap_list.lean
@@ -1,5 +1,5 @@
 import Std.Data.HashMap
-
+set_option grind.warning false
 reset_grind_attrs%
 
 open Std
@@ -7,12 +7,6 @@ open Std
 attribute [grind →] List.length_pos_of_mem
 attribute [grind] HashMap.size_insert
 
--- Fails with issue:
--- [issue] type error constructing proof for List.length_pos_of_mem
---     when assigning metavariable ?l with
---       m.insert 1 2
---     has type
---       HashMap Nat Nat : Type
---     but is expected to have type
---       List Nat : Type
-example (m : HashMap Nat Nat) : ((m.insert 1 2).insert 3 4).size ≤ m.size := by grind
+set_option trace.grind.issues true in
+example (m : HashMap Nat Nat) : ((m.insert 1 2).insert 3 4).size ≥ m.size := by
+  grind

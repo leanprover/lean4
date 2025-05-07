933055e1d1fd8e953a249e215ea2d0f3c3be74cf
Leonardo de Moura <leomoura@amazon.com>
2025-05-07 10:04:56 +0200

    test: more tests

--- tests/lean/run/tojson_fromjson_perf_issue.lean
+++ tests/lean/run/tojson_fromjson_perf_issue.lean
@@ -42,3 +42,66 @@ structure Foo where
   a38 : Option Nat
   a39 : Option Nat
   deriving ToJson, FromJson, Repr, BEq
+
+structure Boo where
+  a1 : String × Option Nat
+  a2 : String × Option Nat
+  a3 : String × Option Nat
+  a4 : String × Option Nat
+  a5 : String × Option Nat
+  a6 : String × Option Nat
+  a7 : String × Option Nat
+  a8 : String × Option Nat
+  a9 : String × Option Nat
+  a10 : Array Nat
+  a11 : Array Nat
+  a12 : Array Nat
+  a13 : Array Nat
+  a14 : Array Nat
+  a15 : Array Nat
+  a16 : Array Nat
+  a17 : Array Nat
+  a18 : Array Nat
+  a19 : Array Nat
+  a20 : Array Nat
+  a21 : Array Nat
+  a22 : Array Nat
+  a23 : Array Nat
+  a24 : Array Nat
+  a25 : Array Nat
+  a26 : Array Nat
+  a27 : Array Nat
+  a28 : Array Nat
+  a29 : List Nat
+  a30 : List Nat
+  a31 : List Nat
+  a32 : List Nat
+  a33 : List Nat
+  a34 : List Nat
+  a35 : List Nat
+  a36 : List Nat
+  a37 : List Nat
+  a38 : List Nat
+  a39 : List Nat
+  aa10 : Float × USize × UInt64
+  aa11 : Float × USize × UInt64
+  aa12 : Float × USize × UInt64
+  aa13 : Float × USize × UInt64
+  aa14 : Float × USize × UInt64
+  aa15 : Float × USize × UInt64
+  aa16 : Float × USize × UInt64
+  aa17 : Float × USize × UInt64
+  aa18 : Float × USize × UInt64
+  aa19 : Float × USize × UInt64
+  aa20 : Float × USize × UInt64
+  aa21 : Float × USize × UInt64
+  aa22 : Float × USize × UInt64
+  aa23 : Float × USize × UInt64
+  aa24 : Float × USize × UInt64
+  aa25 : Float × USize × UInt64
+  aa26 : Float × USize × UInt64
+  aa27 : Float × USize × UInt64
+  aa28 : Float × USize × UInt64
+  aa29 : Float × USize × UInt64
+  aa30 : Float × USize × UInt64
+  deriving ToJson, FromJson, Repr, BEq

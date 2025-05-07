764078bffb44dc37af8bbed19d53bacaceca22eb
Leonardo de Moura <leomoura@amazon.com>
2025-05-07 06:22:21 +0100

    test: exponential compilation time
    The option instance is being inlined.

---
+++ tests/lean/run/tojson_fromjson_perf_issue.lean
@@ -0,0 +1,44 @@
+import Lean.Data.Json
+open Lean
+
+structure Foo where
+  a1 : Option Nat
+  a2 : Option Nat
+  a3 : Option Nat
+  a4 : Option Nat
+  a5 : Option Nat
+  a6 : Option Nat
+  a7 : Option Nat
+  a8 : Option Nat
+  a9 : Option Nat
+  a10 : Option Nat
+  a11 : Option Nat
+  a12 : Option Nat
+  a13 : Option Nat
+  a14 : Option Nat
+  a15 : Option Nat
+  a16 : Option Nat
+  a17 : Option Nat
+  a18 : Option Nat
+  a19 : Option Nat
+  a20 : Option Nat
+  a21 : Option Nat
+  a22 : Option Nat
+  a23 : Option Nat
+  a24 : Option Nat
+  a25 : Option Nat
+  a26 : Option Nat
+  a27 : Option Nat
+  a28 : Option Nat
+  a29 : Option Nat
+  a30 : Option Nat
+  a31 : Option Nat
+  a32 : Option Nat
+  a33 : Option Nat
+  a34 : Option Nat
+  a35 : Option Nat
+  a36 : Option Nat
+  a37 : Option Nat
+  a38 : Option Nat
+  a39 : Option Nat
+  deriving ToJson, FromJson, Repr, BEq

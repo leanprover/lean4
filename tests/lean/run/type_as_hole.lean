e811506c8955f983b4094ff4f93ac8cfaae29645
Leonardo de Moura <leomoura@amazon.com>
2025-05-08 13:44:56 +0200

    chore: fix tests

--- tests/lean/run/type_as_hole.lean
+++ tests/lean/run/type_as_hole.lean
@@ -1,6 +1,7 @@
 /--
 error: failed to infer type of `foo`
-note: all holes (e.g., `_`) in the header of a theorem are resolved before the proof is processed; information from the proof cannot be used to infer what these values should be
+
+Note: note: all holes (e.g., `_`) in the header of a theorem are resolved before the proof is processed; information from the proof cannot be used to infer what these values should be
 ---
 error: type of theorem 'foo' is not a proposition
   ?m.2
@@ -11,7 +12,8 @@ theorem foo : _ :=
 
 /--
 error: failed to infer type of example
-note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
+
+Note: note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
 -/
 #guard_msgs (error) in
 example : _ :=
@@ -19,7 +21,8 @@ example : _ :=
 
 /--
 error: failed to infer type of `boo`
-note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
+
+Note: note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
 -/
 #guard_msgs (error) in
 def boo : _ :=
@@ -27,7 +30,8 @@ def boo : _ :=
 
 /--
 error: failed to infer type of instance
-note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
+
+Note: note: when the resulting type of a declaration is explicitly provided, all holes (e.g., `_`) in the header are resolved before the declaration body is processed
 -/
 #guard_msgs (error) in
 instance boo : _ :=

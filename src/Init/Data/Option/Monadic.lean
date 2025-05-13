357828a2386a18e4d8ffe97f08703e4cbd6ffd48
Markus Himmel <markus@lean-fro.org>
2025-05-13 16:10:24 +0200

    feat: `Option` lemmas

--- src/Init/Data/Option/Monadic.lean
+++ src/Init/Data/Option/Monadic.lean
@@ -13,8 +13,8 @@ import Init.Control.Lawful.Basic
 
 namespace Option
 
-@[simp, grind] theorem bindM_none [Monad m] (f : α → m (Option β)) : none.bindM f = pure none := rfl
-@[simp, grind] theorem bindM_some [Monad m] [LawfulMonad m] (a) (f : α → m (Option β)) : (some a).bindM f = f a := by
+@[simp, grind] theorem bindM_none [Pure m] (f : α → m (Option β)) : none.bindM f = pure none := rfl
+@[simp, grind] theorem bindM_some [Pure m] (a) (f : α → m (Option β)) : (some a).bindM f = f a := by
   simp [Option.bindM]
 
 -- We simplify `Option.forM` to `forM`.
@@ -28,6 +28,10 @@ namespace Option
 
 @[simp, grind] theorem forM_map [Monad m] [LawfulMonad m] (o : Option α) (g : α → β) (f : β → m PUnit) :
     forM (o.map g) f = forM o (fun a => f (g a)) := by
+  cases o <;> simp
+
+theorem forM_join [Monad m] [LawfulMonad m] (o : Option (Option α)) (f : α → m PUnit) :
+    forM o.join f = forM o (forM · f) := by
   cases o <;> simp
 
 @[simp, grind] theorem forIn'_none [Monad m] (b : β) (f : (a : α) → a ∈ none → β → m (ForInStep β)) :
@@ -97,6 +101,13 @@ theorem forIn'_eq_pelim [Monad m] [LawfulMonad m]
     forIn' (o.map g) init f = forIn' o init fun a h y => f (g a) (mem_map_of_mem g h) y := by
   cases o <;> simp
 
+theorem forIn'_join [Monad m] [LawfulMonad m] (b : β) (o : Option (Option α))
+    (f : (a : α) → a ∈ o.join → β → m (ForInStep β)) :
+    forIn' o.join b f = forIn' o b (fun o' ho' b => ForInStep.yield <$> forIn' o' b (fun a ha b' => f a (by simp_all [join_eq_some_iff]) b')) := by
+  cases o with
+  | none => simp
+  | some a => simpa using forIn'_congr rfl rfl (by simp)
+
 theorem forIn_eq_elim [Monad m] [LawfulMonad m]
     (o : Option α) (f : (a : α) → β → m (ForInStep β)) (b : β) :
     forIn o b f =
@@ -126,6 +137,11 @@ theorem forIn_eq_elim [Monad m] [LawfulMonad m]
     forIn (o.map g) init f = forIn o init fun a y => f (g a) y := by
   cases o <;> simp
 
+theorem forIn_join [Monad m] [LawfulMonad m]
+    (o : Option (Option α)) (f : α → β → m (ForInStep β)) :
+    forIn o.join init f = forIn o init (fun o' b => ForInStep.yield <$> forIn o' b f) := by
+  cases o <;> simp
+
 @[simp] theorem elimM_pure [Monad m] [LawfulMonad m] (x : Option α) (y : m β) (z : α → m β) :
     Option.elimM (pure x : m (Option α)) y z = x.elim y z := by
   simp [Option.elimM]
@@ -138,11 +154,8 @@ theorem forIn_eq_elim [Monad m] [LawfulMonad m]
     (y : m γ) (z : β → m γ) : Option.elimM (f <$> x) y z = (do Option.elim (f (← x)) y z) := by
   simp [Option.elimM]
 
-@[simp] theorem tryCatch_none (alternative : Unit → Option α) :
-  (tryCatch none alternative) = alternative () := rfl
-
-@[simp] theorem tryCatch_some (a : α) (alternative : Unit → Option α) :
-  (tryCatch (some a) alternative) = some a := rfl
+@[simp] theorem tryCatch_eq_or (o : Option α) (alternative : Unit → Option α) :
+    tryCatch o alternative = o.or (alternative ()) := by cases o <;> rfl
 
 @[simp] theorem throw_eq_none : throw () = (none : Option α) := rfl
 
@@ -150,5 +163,22 @@ theorem forIn_eq_elim [Monad m] [LawfulMonad m]
     none.filterM p = pure none := rfl
 theorem filterM_some [Applicative m] (p : α → m Bool) (a : α) :
     (some a).filterM p = (fun b => if b then some a else none) <$> p a := rfl
+
+theorem sequence_join [Applicative m] [LawfulApplicative m] {o : Option (Option (m α))} :
+    o.join.sequence = join <$> sequence (o.map sequence) := by
+  cases o <;> simp
+
+theorem bindM_join [Pure m] {f : α → m (Option β)} {o : Option (Option α)} :
+    o.join.bindM f = o.bindM (·.bindM f) := by
+  cases o <;> simp
+
+theorem mapM_join [Applicative m] [LawfulApplicative m] {f : α → m β} {o : Option (Option α)} :
+    o.join.mapM f = join <$> o.mapM (Option.mapM f) := by
+  cases o <;> simp
+
+theorem mapM_guard [Applicative m] {x : α} {p : α → Bool} {f : α → m β} :
+    (guard p x).mapM f = if p x then some <$> f x else pure none := by
+  simp only [guard_eq_ite]
+  split <;> simp
 
 end Option

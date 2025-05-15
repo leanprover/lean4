c298f81bc47082c70da4866989f14c17848f54c0
Kim Morrison <kim@tqft.net>
2025-05-15 13:03:11 +1000

    feat: preparation for semirings and noncommutative rings in grind

--- src/Init/Grind/CommRing/Int.lean
+++ src/Init/Grind/CommRing/Int.lean
@@ -19,8 +19,11 @@ instance : CommRing Int where
   mul_assoc := Int.mul_assoc
   mul_comm := Int.mul_comm
   mul_one := Int.mul_one
+  one_mul := Int.one_mul
   left_distrib := Int.mul_add
+  right_distrib := Int.add_mul
   zero_mul := Int.zero_mul
+  mul_zero := Int.mul_zero
   pow_zero _ := rfl
   pow_succ _ _ := rfl
   ofNat_succ _ := rfl

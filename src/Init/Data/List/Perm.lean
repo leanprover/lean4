ab0420e6b57d6ea284b9476cf23dbf867d322667
Kim Morrison <kim@tqft.net>
2025-05-15 22:11:09 +1000

    revert

--- src/Init/Data/List/Perm.lean
+++ src/Init/Data/List/Perm.lean
@@ -393,11 +393,6 @@ theorem cons_perm_iff_perm_erase {a : α} {l₁ l₂ : List α} :
   have : a ∈ l₂ := h.subset mem_cons_self
   exact ⟨this, (h.trans <| perm_cons_erase this).cons_inv⟩
 
-end LawfulBEq
-section DecidableEq
-
-variable [DecidableEq α]
-
 theorem perm_iff_count {l₁ l₂ : List α} : l₁ ~ l₂ ↔ ∀ a, count a l₁ = count a l₂ := by
   refine ⟨Perm.count_eq, fun H => ?_⟩
   induction l₁ generalizing l₂ with
@@ -436,7 +431,7 @@ theorem perm_insert_swap (x y : α) (l : List α) :
   simp [List.insert, xl, yl, xy, Ne.symm xy]
   constructor
 
-end DecidableEq
+end LawfulBEq
 
 theorem Perm.pairwise_iff {R : α → α → Prop} (S : ∀ {x y}, R x y → R y x) :
     ∀ {l₁ l₂ : List α} (_p : l₁ ~ l₂), Pairwise R l₁ ↔ Pairwise R l₂ :=

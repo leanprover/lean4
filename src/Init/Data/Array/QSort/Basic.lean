5a474b6998288466ad4b775e888ff54053c42239
Kim Morrison <kim@tqft.net>
2025-05-15 22:32:15 +1000

    .

--- src/Init/Data/Array/QSort/Basic.lean
+++ src/Init/Data/Array/QSort/Basic.lean
@@ -37,6 +37,10 @@ def qpartition {n} (as : Vector α n) (lt : α → α → Bool) (lo hi : Nat)
   let as  := if lt as[hi]  as[lo] then as.swap lo hi  else as
   let as  := if lt as[mid] as[hi] then as.swap mid hi else as
   let pivot := as[hi]
+  -- During this loop, elements below in `[lo, i)` are less than `pivot`,
+  -- elements in `[i, j)` are greater than or equal to `pivot`,
+  -- elements in `[j, hi)` are unexamined,
+  -- while `as[hi]` is (by definition) the pivot.
   let rec loop (as : Vector α n) (i j : Nat)
       (ilo : lo ≤ i := by omega) (jh : j < n := by omega) (w : i ≤ j := by omega) :=
     if h : j < hi then
@@ -60,8 +64,11 @@ In-place quicksort.
     if h₁ : lo < hi then
       let ⟨⟨mid, hmid⟩, as⟩ := qpartition as lt lo hi
       if h₂ : mid ≥ hi then
+        -- This only occurs when `hi ≤ lo`,
+        -- and thus `as[low:high+1]` is trivially already sorted.
         as
       else
+        -- Otherwise, we recursively sort the two subarrays.
         sort (sort as lo mid) (mid+1) hi
     else as
   if h : as.size = 0 then

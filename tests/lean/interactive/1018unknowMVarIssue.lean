fa0f1e61fa729f22ceb3cbdb45fd2e89b22191b3
Sebastian Ullrich <sebasti@nullri.ch>
2025-05-15 09:24:10 +0200

    chore: change chatty test to interactive test

--- tests/lean/interactive/1018unknowMVarIssue.lean
+++ tests/lean/interactive/1018unknowMVarIssue.lean
@@ -1,10 +1,14 @@
+/-!
+The expected type used to show "unknown metavariable '?_uniq.31828'" because of missing
+backtracking of the info tree.
+-/
+
 inductive Fam2 : Type → Type → Type 1 where
   | any : Fam2 α α
   | nat : Nat → Fam2 Nat Nat
 
-set_option pp.rawOnError true
-set_option trace.Elab.info true
 example (a : α) (x : Fam2 α β) : β :=
   match x with
   | Fam2.any   => _
+                --^ $/lean/plainTermGoal
   | Fam2.nat n => n

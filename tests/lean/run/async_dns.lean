bf2b054bfc979140c0f80832cf2fbb4ded357ffc
Sofia Rodrigues <sofia@algebraic.dev>
2025-05-16 10:59:56 -0300

    fix: dns test

--- tests/lean/run/async_dns.lean
+++ tests/lean/run/async_dns.lean
@@ -2,7 +2,7 @@ import Std.Internal.Async
 import Std.Internal.UV
 import Std.Net.Addr
 
-open Std.Internal.IO.Async
+open Std.Internal.IO Async
 open Std.Net
 
 open Std.Net
@@ -26,6 +26,33 @@ instance : Monad Async where
     let task ← ma.run
     task.bindIO fun a => (f a).run
 
+def baseSelector (asyncWaiter : AsyncTask α) : Selector α :=
+ {
+    tryFn := do
+      if ← IO.hasFinished asyncWaiter then
+        let result ← IO.ofExcept asyncWaiter.get
+        return some result
+      else
+        return none
+    registerFn waiter := do
+      discard <| AsyncTask.mapIO (x := asyncWaiter) fun data => do
+        let lose := return ()
+        let win promise := promise.resolve (.ok data)
+        waiter.race lose win
+    unregisterFn := pure ()
+  }
+
+def race (a : AsyncTask α) (b : AsyncTask β) (map : Except α β → AsyncTask γ) : IO (AsyncTask γ) := do
+  Selectable.one #[
+    .case (baseSelector a) fun a => return map (.error a),
+    .case (baseSelector b) fun b => return map (.ok b),
+  ]
+
+def timeout (a : AsyncTask α) (time : Std.Time.Millisecond.Offset) : IO (AsyncTask α) := do
+  race (← sleep time) a fun
+    | .ok res => Task.pure (.ok res)
+    | .error _ => Task.pure (.error (IO.userError "Timeout."))
+
 -- Await function to simplify AsyncTask handling
 def await (task : IO (AsyncTask α)) : Async α :=
   Async.mk task
@@ -35,7 +62,10 @@ instance : MonadLift IO Async where
 
 /-- Joe is another client. -/
 def runDNS : Async Unit := do
-  assertBEq (← await (DNS.getAddrInfo "google.com" "http")).size 2
+  let infos ← await <| (timeout (← DNS.getAddrInfo "google.com" "http") 10000)
+
+  unless infos.size > 0 do
+    (throw <| IO.userError <| "No DNS results for google.com" : IO _)
 
 def runReverseDNS : Async Unit := do
   let result ← await (DNS.getNameInfo (.v4 ⟨.ofParts 8 8 8 8, 53⟩))

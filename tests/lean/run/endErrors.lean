85f96786b5bbba6c58ff8aa520e163788df1b83d
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-05-16 21:47:49 +0200

    polishing and minor tweaks

--- tests/lean/run/endErrors.lean
+++ tests/lean/run/endErrors.lean
@@ -6,38 +6,43 @@ This file assesses the error messages produced by the `end` command.
 /--
 error: Invalid `end`: There is no current scope to end
 
-Note: scopes are introduced using `namespace` and `section`
+Note: Scopes are introduced using `namespace` and `section`
 -/
 #guard_msgs in
 end
 
 namespace A.B.C
 /--
-error: Missing name at `end`: The current scope is named 'C', but a name was not provided
+error: Missing name after `end`: The current scope is named 'C', but a name was not provided
 
-Hint: To close the innermost scope 'C', specify its name:
+Hint: To end the current scope 'C', specify its name:
   end ̲C̲
 -/
 #guard_msgs in
 end
 
 /--
-error: Name mismatch at `end`: Expected the current scope name
-  B.C
-or some final part thereof, but found
-  A.B
+error: Name 'A.B' does not match the current scope(s) 'A.B.C': Expected 'B.C' or some other suffix of the current scope(s)
 
-Hint: If you meant to end the outer scope(s) 'A.B', you must end the full intervening scopes 'A.B.C':
+Hint: If you meant to end the outer scope(s) 'A.B', you must end all the intervening scopes 'A.B.C':
   end A.B.̲C̲
 -/
 #guard_msgs in
 end A.B
 
+-- Ensure that we don't suggest including `«»` to get to an enclosing scope
+section
+namespace D
+
+/-- error: Name 'A.B' does not match the current scope(s) 'D': Expected 'D' -/
+#guard_msgs in
+end A.B
+
+end D
+end
+
 /--
-error: Name mismatch at `end`: Expected the current scope name
-  C
-but found
-  Irrelevant
+error: Name 'Irrelevant' does not match the current scope(s) 'A.B.C': Expected 'C' or some other suffix of the current scope(s)
 -/
 #guard_msgs in
 end Irrelevant
@@ -45,11 +50,18 @@ end Irrelevant
 section
 
 /--
-error: Unexpected name after `end`: The current section is unnamed, but found the name
-  C
+error: Unexpected name 'C' after `end`: The current section is unnamed`
 
-Hint: Delete the name 'C' to end the current unnamed scope, then close any named scopes using an additional `end` command:
+Hint: Delete the name 'C' to end the current unnamed scope; outer named scopes can then be closed using additional `end` command(s):
   end ̵C̵
 -/
 #guard_msgs in
 end C
+
+end
+
+/--
+error: Invalid name after `end`: The provided name 'D.E.F.G' contains too many components; expected 'A.B.C' or some suffix thereof
+-/
+#guard_msgs in
+end D.E.F.G

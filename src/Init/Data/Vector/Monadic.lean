b0670f1741aedd5554e70fa4a951bb0b02c11ce2
Kim Morrison <kim@tqft.net>
2025-05-17 13:20:55 +1000

    .

--- src/Init/Data/Vector/Monadic.lean
+++ src/Init/Data/Vector/Monadic.lean
@@ -40,7 +40,7 @@ theorem mapM_pure [Monad m] [LawfulMonad m] {xs : Vector α n} (f : α → β) :
 
 @[simp] theorem mapM_map [Monad m] [LawfulMonad m] {f : α → β} {g : β → m γ} {xs : Vector α n} :
     (xs.map f).mapM g = xs.mapM (g ∘ f) := by
-  rcases xs with ⟨xs, rfl⟩
+  apply map_toArray_inj.mp
   simp
 
 @[congr] theorem mapM_congr [Monad m] {xs ys : Vector α n} (w : xs = ys)

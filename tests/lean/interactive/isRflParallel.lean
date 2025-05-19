2913fdbfacb63a3fd1aaf83d89dd77fcc8f3db8d
Joachim Breitner <mail@joachim-breitner.de>
2025-05-19 17:30:28 +0200

    A better test

--- tests/lean/interactive/isRflParallel.lean
+++ tests/lean/interactive/isRflParallel.lean
@@ -1,5 +1,6 @@
 import Lean.Server.Test.Cancel
-open Lean.Server.Test.Cancel
+
+open Lean Meta
 
 /-!
 The following should not deadlock: The `simp` tactic should be able to use `a_eq_b` even before
@@ -9,21 +10,26 @@ that theorem body is evaluated.
 set_option experimental.tactic.simp.useRflAttr true
 
 set_option trace.Elab.block true
-set_option debug.skipKernelTC true
+-- set_option debug.skipKernelTC true
+
+axiom testSorry : α
 
 opaque a : Nat
 opaque b : Nat
 
 theorem a_eq_b : a = b := by
-  wait_for_unblock
-  -- sleep 10000
-  sorry
+  -- wait_for_unblock_async
+  run_tac
+    while true do
+      if (← Server.Test.Cancel.unblockedCancelTk.isSet) then
+        break
+      IO.sleep 30
+  -- sleep 100000
+  exact testSorry
 
-theorem bar : 2 * a = 3 * b := by
-  -- unblock
-  -- simp -dsimp only [a_eq_b]
+theorem bar : 2 * a = 2 * b := by
   congr 1
-  · sorry
-  · apply a_eq_b
-  skip
-  -- unblock
+  -- apply _root_.a_eq_b
+  simp only [_root_.a_eq_b]
+  run_tac
+    Server.Test.Cancel.unblockedCancelTk.set

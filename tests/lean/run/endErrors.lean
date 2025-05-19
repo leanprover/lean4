179e1742c4aeb5567608bc0c2ff970806f16c41c
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-05-19 19:39:33 +0200

    test: update tests

--- tests/lean/run/endErrors.lean
+++ tests/lean/run/endErrors.lean
@@ -13,18 +13,18 @@ end
 
 namespace A.B.C
 /--
-error: Missing name after `end`: The current scope is named 'C', but a name was not provided
+error: Missing name after `end`: The current scope is named `C`, but a name was not provided
 
-Hint: To end the current scope 'C', specify its name:
+Hint: To end the current scope `C`, specify its name:
   end ̲C̲
 -/
 #guard_msgs in
 end
 
 /--
-error: Name 'A.B' does not match the current scope(s) 'A.B.C': Expected 'B.C' or some other suffix of the current scope(s)
+error: Name `A.B` does not match the current scope(s) `A.B.C`: Expected `B.C` or some other suffix of the current scope(s)
 
-Hint: If you meant to end the outer scope(s) 'A.B', you must end all the intervening scopes 'A.B.C':
+Hint: If you meant to end the outer scope(s) `A.B`, you must end all the intervening scopes `A.B.C`:
   end A.B.̲C̲
 -/
 #guard_msgs in
@@ -34,7 +34,7 @@ end A.B
 section
 namespace D
 
-/-- error: Name 'A.B' does not match the current scope(s) 'D': Expected 'D' -/
+/-- error: Name `A.B` does not match the current scope(s) `D`: Expected `D` -/
 #guard_msgs in
 end A.B
 
@@ -50,9 +50,9 @@ end Irrelevant
 section
 
 /--
-error: Unexpected name 'C' after `end`: The current section is unnamed`
+error: Unexpected name `C` after `end`: The current section is unnamed
 
-Hint: Delete the name 'C' to end the current unnamed scope; outer named scopes can then be closed using additional `end` command(s):
+Hint: Delete the name `C` to end the current unnamed scope; outer named scopes can then be closed using additional `end` command(s):
   end ̵C̵
 -/
 #guard_msgs in
@@ -61,7 +61,7 @@ end C
 end
 
 /--
-error: Invalid name after `end`: The provided name 'D.E.F.G' contains too many components; expected 'A.B.C' or some suffix thereof
+error: Invalid name after `end`: The provided name `D.E.F.G` contains too many components; expected `A.B.C` or some suffix thereof
 -/
 #guard_msgs in
 end D.E.F.G

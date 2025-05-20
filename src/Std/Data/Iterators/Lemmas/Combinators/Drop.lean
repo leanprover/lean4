3eaa90d9d7728735d5872cda00e01dd1b46155d2
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-20 17:49:23 +0200

    pure lemmas

---
+++ src/Std/Data/Iterators/Lemmas/Combinators/Drop.lean
@@ -0,0 +1,75 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Reichert
+-/
+prelude
+import Std.Data.Iterators.Combinators.Drop
+import Std.Data.Iterators.Lemmas.Combinators.Monadic.Drop
+import Std.Data.Iterators.Lemmas.Combinators.Take
+import Std.Data.Iterators.Lemmas.Consumers
+
+namespace Std.Iterators
+
+theorem Iter.drop_eq {α β} [Iterator α Id β] {n : Nat}
+    {it : Iter (α := α) β} :
+    it.drop n = (it.toIterM.drop n).toIter :=
+  rfl
+
+theorem Iter.step_drop {α β} [Iterator α Id β] {n : Nat}
+    {it : Iter (α := α) β} :
+    (it.drop n).step = (match it.step with
+    | .yield it' out h =>
+      match n with
+      | 0 => .yield (it'.drop 0) out (.yield h rfl)
+      | k + 1 => .skip (it'.drop k) (.drop h rfl)
+    | .skip it' h => .skip (it'.drop n) (.skip h)
+    | .done h => .done (.done h)) := by
+  simp only [Iter.step, Iter.step, Iter.drop_eq, IterM.step_drop, toIterM_toIter]
+  simp only [Id.pure_eq, Id.bind_eq, Id.run, drop_eq]
+  generalize it.toIterM.step = step
+  obtain ⟨step, h⟩ := step
+  cases step <;> cases n <;>
+    simp [PlausibleIterStep.yield, PlausibleIterStep.skip, PlausibleIterStep.done]
+
+theorem Iter.atIdxSlow?_drop {α β}
+    [Iterator α Id β] [Productive α Id] {k l : Nat}
+    {it : Iter (α := α) β} :
+    (it.drop k).atIdxSlow? l = it.atIdxSlow? (l + k) := by
+  induction k generalizing it <;> induction l generalizing it
+  all_goals
+    induction it using Iter.inductSkips with | step it ih =>
+    rw [atIdxSlow?.eq_def, atIdxSlow?.eq_def, step_drop]
+    cases it.step using PlausibleIterStep.casesOn <;> simp [*]
+
+theorem Iter.toList_drop {α β} [Iterator α Id β] {n : Nat}
+    [Finite α Id] [IteratorCollect α Id] [LawfulIteratorCollect α Id]
+    {it : Iter (α := α) β} :
+    (it.drop n).toList = it.toList.drop n := by
+  ext
+  simp only [getElem?_toList_eq_atIdxSlow?, List.getElem?_drop, atIdxSlow?_drop]
+  rw [Nat.add_comm]
+
+theorem Iter.toListRev_drop {α β} [Iterator α Id β] {n : Nat}
+    [Finite α Id] [IteratorCollect α Id] [LawfulIteratorCollect α Id]
+    {it : Iter (α := α) β} :
+    (it.drop n).toListRev = (it.toList.reverse.take (it.toList.length - n)) := by
+  rw [toListRev_eq, toList_drop, List.reverse_drop]
+
+-- TODO: Init.Data.Array.Lemmas
+theorem _root_.List.drop_toArray {l : List α} {k : Nat} :
+    l.toArray.drop k = (l.drop k).toArray := by
+  induction l generalizing k
+  case nil => simp
+  case cons l' ih =>
+    match k with
+    | 0 => simp
+    | k' + 1 => simp [List.drop_succ_cons, ← ih]
+
+theorem Iter.toArray_drop {α β} [Iterator α Id β] {n : Nat}
+    [Finite α Id] [IteratorCollect α Id] [LawfulIteratorCollect α Id]
+    {it : Iter (α := α) β} :
+    (it.drop n).toArray = it.toArray.drop n := by
+  rw [← toArray_toList, ← toArray_toList, List.drop_toArray, toList_drop]
+
+end Std.Iterators

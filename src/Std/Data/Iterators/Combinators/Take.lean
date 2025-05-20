1d10c25d7ce6356a7fef3b3984c1e84726ab30ec
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-20 10:56:48 +0200

    add lemmas, tests

---
+++ src/Std/Data/Iterators/Combinators/Take.lean
@@ -0,0 +1,16 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Reichert
+-/
+prelude
+import Std.Data.Iterators.Combinators.Monadic.Take
+
+namespace Std.Iterators
+
+@[always_inline, inline]
+def Iter.take {α : Type w} {β : Type w} (n : Nat) (it : Iter (α := α) β) :
+    Iter (α := Take α Id β) β :=
+  it.toIterM.take n |>.toPureIter
+
+end Std.Iterators

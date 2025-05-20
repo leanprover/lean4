ad4279c71e0b43d832225caaa7010302133f49ab
Kim Morrison <kim@tqft.net>
2025-05-20 13:10:04 +1000

    chore: use HMul in Lean.Grind.Module

--- src/Init/Grind/Module/Int.lean
+++ src/Init/Grind/Module/Int.lean
@@ -20,13 +20,13 @@ instance : IntModule Int where
   zero_add := Int.zero_add
   add_comm := Int.add_comm
   add_assoc := Int.add_assoc
-  zero_smul := Int.zero_mul
-  one_smul := Int.one_mul
-  add_smul := Int.add_mul
-  neg_smul := Int.neg_mul
-  smul_zero := Int.mul_zero
-  smul_add := Int.mul_add
-  mul_smul := Int.mul_assoc
+  zero_hmul := Int.zero_mul
+  one_hmul := Int.one_mul
+  add_hmul := Int.add_mul
+  neg_hmul := Int.neg_mul
+  hmul_zero := Int.mul_zero
+  hmul_add := Int.mul_add
+  mul_hmul := Int.mul_assoc
   neg_add_cancel := Int.add_left_neg
   sub_eq_add_neg _ _ := Int.sub_eq_add_neg
 
@@ -40,9 +40,9 @@ instance : IntModule.IsOrdered Int where
   neg_lt_iff := by omega
   add_lt_left := by omega
   add_lt_right := by omega
-  smul_pos k a ha := ⟨fun hk => Int.mul_pos hk ha, fun h => Int.pos_of_mul_pos_left h ha⟩
-  smul_neg k a ha := ⟨fun hk => Int.mul_neg_of_pos_of_neg hk ha, fun h => Int.pos_of_mul_neg_left h ha⟩
-  smul_nonpos k a ha hk := Int.mul_nonpos_of_nonneg_of_nonpos hk ha
-  smul_nonneg k a ha hk := Int.mul_nonneg hk ha
+  hmul_pos k a ha := ⟨fun hk => Int.mul_pos hk ha, fun h => Int.pos_of_mul_pos_left h ha⟩
+  hmul_neg k a ha := ⟨fun hk => Int.mul_neg_of_pos_of_neg hk ha, fun h => Int.pos_of_mul_neg_left h ha⟩
+  hmul_nonpos k a ha hk := Int.mul_nonpos_of_nonneg_of_nonpos hk ha
+  hmul_nonneg k a ha hk := Int.mul_nonneg hk ha
 
 end Lean.Grind

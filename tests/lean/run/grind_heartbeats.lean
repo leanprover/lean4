21846ebdf81a53feb304f4f2097f512de704a0e0
Leonardo de Moura <leomoura@amazon.com>
2025-05-23 15:33:54 -0400

    feat: non-chronological backtracking for `grind` (WIP) (#8440)
    This PR implements non-chronological backtracking for the `grind`
    tactic. This feature ensures that `grind` does not need to process
    irrelevant branches after performing a case-split that is not relevant.
    It is not just about performance, but also the size of the final proof
    term. The new test demonstrates this feature in practice.
    ```lean
    -- In the following test, the first 8 case-splits are irrelevant,
    -- and non-choronological backtracking is used to avoid searching
    -- (2^8 - 1) irrelevant branches
    /--
    trace: 
    [grind.split] p8 ∨ q8, generation: 0
    [grind.split] p7 ∨ q7, generation: 0
    [grind.split] p6 ∨ q6, generation: 0
    [grind.split] p5 ∨ q5, generation: 0
    [grind.split] p4 ∨ q4, generation: 0
    [grind.split] p3 ∨ q3, generation: 0
    [grind.split] p2 ∨ q2, generation: 0
    [grind.split] p1 ∨ q1, generation: 0
    [grind.split] ¬p ∨ ¬q, generation: 0
    -/
    #guard_msgs (trace) in
    set_option trace.grind.split true in
    theorem ex
        : p ∨ q →
          ¬ p ∨ q →
          p ∨ ¬ q →
          ¬ p ∨ ¬ q →
          p1 ∨ q1 →
          p2 ∨ q2 →
          p3 ∨ q3 →
          p4 ∨ q4 →
          p5 ∨ q5 →
          p6 ∨ q6 →
          p7 ∨ q7 →
          p8 ∨ q8 →
          False := by
      grind (splits := 10)
    ```

--- tests/lean/run/grind_heartbeats.lean
+++ tests/lean/run/grind_heartbeats.lean
@@ -12,7 +12,7 @@ macro_rules
   | `(gen! $n:num) => `(op (f $n) (gen! $(Lean.quote (n.getNat - 1))))
 
 /--
-trace: [grind.issues] (deterministic) timeout at `isDefEq`, maximum number of heartbeats (5000) has been reached
+trace: [grind.issues] (deterministic) timeout at `simp`, maximum number of heartbeats (5000) has been reached
     Use `set_option maxHeartbeats <num>` to set the limit.
     ⏎
     Additional diagnostic information may be available using the `set_option diagnostics true` command.
@@ -21,4 +21,5 @@ trace: [grind.issues] (deterministic) timeout at `isDefEq`, maximum number of he
 set_option trace.grind.issues true in
 set_option maxHeartbeats 5000 in
 example : gen! 10 = 0 ∧ True := by
+  set_option trace.Meta.debug true in
   grind (instances := 10000)

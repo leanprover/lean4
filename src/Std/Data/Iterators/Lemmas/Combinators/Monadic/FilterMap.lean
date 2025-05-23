37a7cd73fbfa0728805bd47bad4e3f1858ce8d37
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-23 11:38:09 +0200

    monadic collect lemmas for filterMap

--- src/Std/Data/Iterators/Lemmas/Combinators/Monadic/FilterMap.lean
+++ src/Std/Data/Iterators/Lemmas/Combinators/Monadic/FilterMap.lean
@@ -161,7 +161,7 @@ instance [Monad m] [LawfulMonad n] [LawfulMonad o] [IteratorCollect α m o]
       intro out'
       simp [← ih_yield ‹_›]
       rfl
-    · simp only [bind_pure_comp, pure_bind] --, ← ih_skip ‹_›]
+    · simp only [bind_pure_comp, pure_bind]
       simp only [liftM_pure, pure_bind]
       simp [← ih_skip ‹_›]
       rfl
@@ -169,57 +169,44 @@ instance [Monad m] [LawfulMonad n] [LawfulMonad o] [IteratorCollect α m o]
 
 end Lawful
 
-section Congruence
-
-def IterM.Morphisms.filterMapWithProofCongrRight {α : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
-    {β : Type w} [Iterator α m β] {γ : Type w} {f g : β → PostconditionT m (Option γ)} (h : f = g) :
-    Morphism (FilterMap α f) (FilterMap α g) m := by
-  exact h ▸ Morphism.id _ m
-
--- TODO: upstream from Mathlib.Logic.Basic? Note that this lemma is more general
-theorem Eq.rec_eq_cast {α : Sort _} {a : α} {motive : (a' : α) → a = a' → Sort _} (x : motive a rfl) {a' : α} (h : a = a') :
-    (Eq.rec x h : motive a' h) = cast (by cases h; rfl) x := by
-  cases h; rfl
-
--- TODO: upstream from Mathlib.Logic.Basic?
-theorem eq_cast_iff_heq : a = cast e b ↔ HEq a b := by cases e; simp
-
-private theorem Eq.apply_rec' {α : Type u} {β : α → Type v} {γ : α → Type w} {a a' : α} (h : a = a')
-    (f : ∀ a, β a → γ a) (x : β a) : f a' (h ▸ x) = h ▸ (f a x) := by
-  cases h
-  simp
-
-def IterM.Morphisms.mapToFilterMap (α : Type w) (m : Type w → Type w') [Monad m] [LawfulMonad m]
-    {β : Type w} [Iterator α m β]
-    {γ : Type w} (f : β → γ) :
-    Morphism (β := γ) (Map α (fun x => (pure (f x) : PostconditionT m _)))
-      (FilterMap α (fun x => (pure (some (f x)) : PostconditionT m _)) (γ := γ)) m := by
-  apply IterM.Morphism.copy
-  case f => exact fun it => it.inner.inner.filterMapWithProof _
-  case φ => exact filterMapWithProofCongrRight (α := α) (m := m) (by simp)
-  case h =>
-    ext it
-    simp only [filterMapWithProofCongrRight, Eq.apply_rec' _ fun (a : _) (ψ : Morphism _ _ m) => ψ.map it,
-      Morphism.id]
-    rw [Eq.rec_eq_cast, eq_cast_iff_heq]
-    rw (occs := [2]) [(rfl : it = it.inner.inner.filterMapWithProof _)]
-    apply HEq.congrArg (fun f => it.inner.inner.filterMapWithProof f)
-    simp
-
-end Congruence
-
 section ToList
 
-variable {α : Type w} {m : Type w → Type w'} {β : Type w} {β' : Type w}
-  [Iterator α m β] (it : IterM (α := α) m β) [Monad m]
-
-theorem IterM.toList_filterMap {α : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m] {β : Type w}
-    [Iterator α m β] [IteratorCollect α m] [LawfulIteratorCollect α m] {f : β → Option β'}
-    (it : IterM (α := α) m β) :
+theorem IterM.InternalConsumers.toList_filterMap {α β γ: Type w} {m : Type w → Type w'}
+    [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m]
+    {f : β → Option γ} (it : IterM (α := α) m β) :
     (it.filterMap f).toList = (fun x => x.filterMap f) <$> it.toList := by
-  induction it using IterM.induct
+  induction it using IterM.inductSteps
   rename_i it ihy ihs
-  rw [IterM.toList_of_step, IterM.toList_of_step]
+  rw [IterM.toList_eq_match_step, IterM.toList_eq_match_step]
+  simp
+  rw [step_filterMap]
+  simp only [bind_assoc, IterM.step, map_eq_pure_bind]
+  apply bind_congr
+  intro step
+  split
+  · simp only [List.filterMap_cons, bind_assoc, pure_bind, bind_pure]
+    split
+    · split
+      · simp only [bind_pure_comp, pure_bind]
+        exact ihy ‹_›
+      · simp_all
+    · split
+      · simp_all
+      · simp_all [ihy ‹_›]
+  · simp
+    apply ihs
+    assumption
+  · simp
+
+theorem IterM.toList_filterMap {α β γ : Type w} {m : Type w → Type w'}
+    [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m]
+    {f : β → Option γ} (it : IterM (α := α) m β) :
+    (it.filterMap f).toList = (fun x => x.filterMap f) <$> it.toList := by
+  induction it using IterM.inductSteps
+  rename_i it ihy ihs
+  rw [IterM.toList_eq_match_step, IterM.toList_eq_match_step]
   simp
   rw [step_filterMap]
   simp only [bind_assoc, IterM.step, map_eq_pure_bind]
@@ -241,22 +228,85 @@ theorem IterM.toList_filterMap {α : Type w} {m : Type w → Type w'} [Monad m] 
   · simp
 
 theorem IterM.toList_map {α : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m] {β : Type w}
-    [Iterator α m β] [IteratorCollect α m] [LawfulIteratorCollect α m] {f : β → β'}
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m] {f : β → β'}
     (it : IterM (α := α) m β) :
     (it.map f).toList = (fun x => x.map f) <$> it.toList := by
-  rw [(IterM.Morphisms.mapToFilterMap α m f).toList_eq]
-  change (it.filterMap _).toList = _
-  rw [toList_filterMap]
-  change (fun x => x.filterMap (some ∘ f)) <$> it.toList = _
-  rw [List.filterMap_eq_map]
+  rw [LawfulIteratorCollect.toList_eq, ← List.filterMap_eq_map, ← toList_filterMap]
+  let t := type_of% (it.map f)
+  let t' := type_of% (it.filterMap (some ∘ f))
+  congr
+  · simp [Map]
+  · simp [instIteratorMap, inferInstanceAs]
+    congr
+    simp
+  · refine heq_of_eqRec_eq ?_ rfl
+    congr
+    simp [Map]
+    simp [instIteratorMap, inferInstanceAs]
+    congr
+    simp
+  · simp [Map]
+  · simp [instIteratorMap, inferInstanceAs]
+    congr
+    simp
+  · simp [map, mapWithProof, InternalCombinators.map, filterMap, filterMapWithProof, InternalCombinators.filterMap]
+    congr
+    · simp [Map]
+    · simp
 
 theorem IterM.toList_filter {α : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
-    {β : Type w} [Iterator α m β] [IteratorCollect α m] [LawfulIteratorCollect α m] {f : β → Bool}
-    {it : IterM (α := α) m β} :
+    {β : Type w} [Iterator α m β] [Finite α m] [IteratorCollect α m m] [LawfulIteratorCollect α m m]
+    {f : β → Bool} {it : IterM (α := α) m β} :
     (it.filter f).toList = List.filter f <$> it.toList := by
   simp only [filter, toList_filterMap, ← List.filterMap_eq_filter]
   rfl
 
 end ToList
+
+section ToListRev
+
+theorem IterM.toListRev_filterMap {α β γ : Type w} {m : Type w → Type w'}
+    [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m]
+    {f : β → Option γ} (it : IterM (α := α) m β) :
+    (it.filterMap f).toListRev = (fun x => x.filterMap f) <$> it.toListRev := by
+  simp [toListRev_eq, toList_filterMap]
+
+theorem IterM.toListRev_map {α β γ : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m] {f : β → γ}
+    (it : IterM (α := α) m β) :
+    (it.map f).toListRev = (fun x => x.map f) <$> it.toListRev := by
+  simp [toListRev_eq, toList_map]
+
+theorem IterM.toListRev_filter {α β : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
+    [Iterator α m β] [Finite α m] [IteratorCollect α m m] [LawfulIteratorCollect α m m]
+    {f : β → Bool} {it : IterM (α := α) m β} :
+    (it.filter f).toListRev = List.filter f <$> it.toListRev := by
+  simp [toListRev_eq, toList_filter]
+
+end ToListRev
+
+section ToArray
+
+theorem IterM.toArray_filterMap {α β γ : Type w} {m : Type w → Type w'}
+    [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m]
+    {f : β → Option γ} (it : IterM (α := α) m β) :
+    (it.filterMap f).toArray = (fun x => x.filterMap f) <$> it.toArray := by
+  simp [← toArray_toList, toList_filterMap]
+
+theorem IterM.toArray_map {α β γ : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
+    [Iterator α m β] [IteratorCollect α m m] [LawfulIteratorCollect α m m] [Finite α m] {f : β → γ}
+    (it : IterM (α := α) m β) :
+    (it.map f).toArray = (fun x => x.map f) <$> it.toArray := by
+  simp [← toArray_toList, toList_map]
+
+theorem IterM.toArray_filter {α : Type w} {m : Type w → Type w'} [Monad m] [LawfulMonad m]
+    {β : Type w} [Iterator α m β] [Finite α m] [IteratorCollect α m m] [LawfulIteratorCollect α m m]
+    {f : β → Bool} {it : IterM (α := α) m β} :
+    (it.filter f).toArray = Array.filter f <$> it.toArray := by
+  simp [← toArray_toList, toList_filter]
+
+end ToArray
 
 end Std.Iterators

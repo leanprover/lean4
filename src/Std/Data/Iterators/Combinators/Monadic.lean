36997973d199faaf82f11d93ebdf9d22857d6904
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-27 07:57:01 +0200

    dropWhile

--- src/Std/Data/Iterators/Combinators/Monadic.lean
+++ src/Std/Data/Iterators/Combinators/Monadic.lean
@@ -6,5 +6,6 @@ Authors: Paul Reichert
 prelude
 import Std.Data.Iterators.Combinators.Monadic.Take
 import Std.Data.Iterators.Combinators.Monadic.TakeWhile
+import Std.Data.Iterators.Combinators.Monadic.DropWhile
 import Std.Data.Iterators.Combinators.Monadic.FilterMap
 import Std.Data.Iterators.Combinators.Monadic.Zip

bda7856750e38e7a7232220b9f2b1fcf2f0842d5
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-27 15:24:23 +0200

    docstrings

--- src/Std/Data/Iterators/Producers/Array.lean
+++ src/Std/Data/Iterators/Producers/Array.lean
@@ -14,11 +14,33 @@ This module provides an iterator for arrays that is accessible via `Array.iter`.
 
 namespace Std.Iterators
 
+/--
+Returns a finite iterator for the given array starting at the given index.
+The iterator yields the elements of the array in order and then terminates.
+
+The monadic version of this iterator is `Array.iterFromIdxM`.
+
+**Termination properties:**
+
+* `Finite` instance: always
+* `Productive` instance: always
+-/
 @[always_inline, inline]
 def _root_.Array.iterFromIdx {α : Type w} (l : Array α) (pos : Nat) :
     Iter (α := ArrayIterator α) α :=
   ((l.iterFromIdxM Id pos).toIter : Iter α)
 
+/--
+Returns a finite iterator for the given array.
+The iterator yields the elements of the array in order and then terminates.
+
+The monadic version of this iterator is `Array.iterM`.
+
+**Termination properties:**
+
+* `Finite` instance: always
+* `Productive` instance: always
+-/
 @[always_inline, inline]
 def _root_.Array.iter {α : Type w} (l : Array α) :
     Iter (α := ArrayIterator α) α :=

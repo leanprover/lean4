49efd4812508a8951bee06bdbd4def84cb2e6ca2
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-05-30 15:04:14 +0200

    fix copyright

--- src/Std/Data/Internal/LawfulMonadLiftFunction.lean
+++ src/Std/Data/Internal/LawfulMonadLiftFunction.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2025 Quang Dao. All rights reserved.
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Quang Dao
+Authors: Paul Reichert
 -/
 prelude
 import Init.Control.Basic

90fa5ed139562347212aabbd425cbfa3a5b2cd1e
Leonardo de Moura <leomoura@amazon.com>
2025-05-29 21:06:44 -0700

    chore: update test

--- tests/lean/grind/product_eta.lean
+++ tests/lean/grind/product_eta.lean
@@ -1,19 +1,25 @@
--- In nightly-2025-05-27 this leads to grind internal error "trying to assert equality c = (c.fst, c.snd) with proof Eq.refl c which has type c = c which is not definitionally equal with `reducible` transparency setting"
-
 set_option grind.warning false
 set_option grind.debug true
-
 
 def α : Type := Unit × Unit
 
 def p (_ : α) : Prop := False
+
 /--
-error: tactic 'grind.cases' failed, (non-recursive) inductive type expected at c
-  α
+error: `grind` failed
 case grind
 c : α
 h : p c
 ⊢ False
+[grind] Goal diagnostics
+  [facts] Asserted facts
+    [prop] p c
+  [eqc] True propositions
+    [prop] p c
 -/
 #guard_msgs in
-example : p c → False := by grind
+example : p c → False := by
+  grind
+
+example : p c → False := by
+  grind [p]

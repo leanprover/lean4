f0cc77bf5a7e91f57d77f1d2e956e1860a54cd49
luisacicolini <luisacicolini@gmail.com>
2025-06-02 15:39:21 +0100

    chore: wip

--- src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Impl/Operations/Clz.lean
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Impl/Operations/Clz.lean
@@ -20,14 +20,51 @@ namespace bitblast
 
 variable [Hashable α] [DecidableEq α]
 
-def blastClz (aig : AIG α) (s : AIG.RefVec aig w) : AIG.RefVecEntry α w :=
-  let clz := (1 : Nat)
-  let ⟨refs, hrefs⟩ := s
-  ⟨aig, ⟨sorry, by sorry⟩⟩
+/--
+  Count the number of leading zeroes downward from the 'n'th bit to the '0'th bit.
+-/
+-- def clzAux {w : Nat} (x : BitVec w) (n : Nat) : Nat :=
+--   match n with
+--   | 0 => if x.getLsbD 0 then 0 else 1
+--   | n' + 1 =>
+--     if x.getLsbD n then 0
+--       else 1 + clzAux x n'
+
+
+def blastClz (aig : AIG α) (target : AIG.RefVec aig w) :
+    AIG.RefVecEntry α w :=
+  let ⟨input, distance⟩ := target
+  aig (.emptyWithCapacity w)
+where
+  go (aig : AIG α) (input : AIG.RefVec aig w) (distance : Nat) (curr : Nat) (hcurr : curr ≤ w)
+      (s : AIG.RefVec aig curr) :
+      AIG.RefVecEntry α w :=
+  if hidx : curr < w then
+    if hdist : (distance + curr) < w then
+      let s := s.push (input.get (distance + curr) (by omega))
+      go aig input distance (curr + 1) (by omega) s
+    else
+      let res := aig.mkConstCached false
+      let aig := res.aig
+      let zeroRef := res.ref
+      have hfinal := AIG.LawfulOperator.le_size (f := AIG.mkConstCached) ..
+      let s := s.cast hfinal
+      let input := input.cast hfinal
+      let s := s.push zeroRef
+      go aig input distance (curr + 1) (by omega) s
+  else
+    have hcurr : curr = w := by omega
+    ⟨aig, hcurr ▸ s⟩
+termination_by w - curr
+
+-- def blastClz (aig : AIG α) (s : AIG.RefVec aig w) : AIG.RefVecEntry α w :=
+--   let clz := (1 : Nat)
+--   let ⟨refs, hrefs⟩ := s
+--   ⟨aig, ⟨sorry, by sorry⟩⟩
 
 instance : AIG.LawfulVecOperator α AIG.RefVec blastClz where
-  le_size := by simp [blastClz]
-  decl_eq := by simp [blastClz]
+  le_size := by simp [blastClz]; sorry
+  decl_eq := by simp [blastClz]; sorry
 
 end bitblast
 end BVExpr

5dc043aeefd86f50563f875dd0c0f457c84a30a2
Kim Morrison <kim@tqft.net>
2025-06-04 08:23:49 +1000

    cleanup

--- tests/lean/run/grind_indexmap_pre.lean
+++ tests/lean/run/grind_indexmap_pre.lean
@@ -1,11 +1,18 @@
+-- This is a companion file for `grind_indexmap.lean`,
+-- showing what an outline of this file might look like before any proofs are written.
+
 import Std.Data.HashMap
+
+set_option grind.warning false
 
 open Std
 
 structure IndexMap (α : Type u) (β : Type v) [BEq α] [Hashable α] where
   indices : HashMap α Nat
-  data : Array (α × β)
-  WF : ∀ (a : α) (b : β) (i : Nat), data[i]? = some (a, b) ↔ indices[a]? = some i
+  keys : Array α
+  values : Array β
+  size_keys' : keys.size = values.size := by grind
+  WF : ∀ (i : Nat) (a : α), keys[i]? = some a ↔ indices[a]? = some i := by grind
 
 namespace IndexMap
 
@@ -13,12 +20,12 @@ variable {α : Type u} {β : Type v} [BEq α] [LawfulBEq α] [Hashable α] [Lawf
 variable {m : IndexMap α β} {a : α} {b : β} {i : Nat}
 
 @[inline] def size (m : IndexMap α β) : Nat :=
-  m.data.size
+  m.values.size
 
 def emptyWithCapacity (capacity := 8) : IndexMap α β where
   indices := HashMap.emptyWithCapacity capacity
-  data := Array.emptyWithCapacity capacity
-  WF := sorry
+  keys := Array.emptyWithCapacity capacity
+  values := Array.emptyWithCapacity capacity
 
 instance : EmptyCollection (IndexMap α β) where
   emptyCollection := emptyWithCapacity
@@ -37,32 +44,36 @@ instance {m : IndexMap α β} {a : α} : Decidable (a ∈ m) :=
   inferInstanceAs (Decidable (a ∈ m.indices))
 
 instance : GetElem? (IndexMap α β) α β (fun m a => a ∈ m) where
-  getElem m a h := (m.data[m.indices[a]'h]'sorry).2
-  getElem? m a := m.indices[a]?.pbind fun i h => (m.data[i]'sorry).2
-  getElem! m a := m.indices[a]?.bind (fun i => m.data[i]?) |>.map (·.2) |>.getD default
-
-instance : LawfulGetElem (IndexMap α β) α β (fun m a => a ∈ m) where
-  getElem?_def := sorry
-  getElem!_def := sorry
+  getElem m a h := m.values[m.indices[a]'h]'(by sorry)
+  getElem? m a := m.indices[a]?.bind (fun i => (m.values[i]?))
+  getElem! m a := m.indices[a]?.bind (fun i => (m.values[i]?)) |>.getD default
 
 @[inline] def findIdx? (m : IndexMap α β) (a : α) : Option Nat := m.indices[a]?
 
 @[inline] def findIdx (m : IndexMap α β) (a : α) (h : a ∈ m) : Nat := m.indices[a]
 
-@[inline] def getIdx? (m : IndexMap α β) (i : Nat) : Option β := m.data[i]?.map (·.2)
+@[inline] def getIdx? (m : IndexMap α β) (i : Nat) : Option β := m.values[i]?
 
-@[inline] def getIdx (m : IndexMap α β) (i : Nat) (h : i < m.size) : β := m.data[i].2
+@[inline] def getIdx (m : IndexMap α β) (i : Nat) (h : i < m.size := by get_elem_tactic) : β :=
+  m.values[i]
+
+instance : LawfulGetElem (IndexMap α β) α β (fun m a => a ∈ m) where
+  getElem?_def := sorry
+  getElem!_def := sorry
 
 @[inline] def insert (m : IndexMap α β) (a : α) (b : β) : IndexMap α β :=
   match h : m.indices[a]? with
   | some i =>
-    { m with
-      data := m.data.set i (a, b) sorry
+    { indices := m.indices
+      keys := m.keys.set i a sorry
+      values := m.values.set i b sorry
+      size_keys' := sorry
       WF := sorry }
   | none =>
-    { m with
-      indices := m.indices.insert a m.size
-      data := m.data.push (a, b)
+    { indices := m.indices.insert a m.size
+      keys := m.keys.push a
+      values := m.values.push b
+      size_keys' := sorry
       WF := sorry }
 
 instance : Singleton (α × β) (IndexMap α β) := ⟨fun ⟨a, b⟩ => (∅ : IndexMap α β).insert a b⟩
@@ -78,21 +89,33 @@ If the key is not present, the map is unchanged.
 @[inline] def eraseSwap (m : IndexMap α β) (a : α) : IndexMap α β :=
   match h : m.indices[a]? with
   | some i =>
-    let last := m.data.back sorry
-    { m with
-      indices := (m.indices.erase a).insert last.1 i
-      data := m.data.set i last sorry
-      WF := sorry }
+    if w : i = m.size - 1 then
+      { indices := m.indices.erase a
+        keys := m.keys.pop
+        values := m.values.pop
+        size_keys' := sorry
+        WF := sorry }
+    else
+      let lastKey := m.keys.back sorry
+      let lastValue := m.values.back sorry
+      { indices := (m.indices.erase a).insert lastKey i
+        keys := m.keys.pop.set i lastKey sorry
+        values := m.values.pop.set i lastValue sorry
+        size_keys' := sorry
+        WF := sorry }
   | none => m
 
 /-! ### Verification theorems -/
 
 theorem getIdx_findIdx (m : IndexMap α β) (a : α) (h : a ∈ m) :
-    m.getIdx (m.findIdx a h) sorry = m[a] :=
+    m.getIdx (m.findIdx a h) sorry = m[a] := sorry
+
+theorem mem_insert (m : IndexMap α β) (a a' : α) (b : β) :
+    a' ∈ m.insert a b ↔ a' = a ∨ a' ∈ m := by
   sorry
 
-theorem getElem_insert (m : IndexMap α β) (a a' : α) (b : β) (h) :
-    (m.insert a b)[a']'h = if h' : a' == a then b else m[a']'(sorry) := by
+theorem getElem_insert (m : IndexMap α β) (a a' : α) (b : β) (h : a' ∈ m.insert a b) :
+    (m.insert a b)[a']'h = if h' : a' == a then b else m[a']'sorry := by
   sorry
 
 theorem findIdx_insert_self (m : IndexMap α β) (a : α) (b : β) :

f2e5494eaf36c5d4e1e46ef8d28e0a13ac16ce07
Rob23oba <robin.arnez@web.de>
2025-06-03 15:46:35 +0200

    add test

---
+++ tests/lean/run/8612.lean
@@ -0,0 +1,19 @@
+macro "assert_eq! " t1:term ", " t2:term : doElem =>
+  `(doElem| if $t1 != $t2 then throw (.userError s!"{repr $t1} != {repr $t2}"))
+
+macro "assert_failure! " e:doElem : doElem =>
+  `(doElem| try $e; throw (.userError s!"unexpected success") catch _ => pure ())
+
+def runTests : IO Unit := do
+  unless System.Platform.isWindows do
+    return
+  let out ← IO.Process.run { cmd := "cmd.exe", args := #["/c", "echo", "hi"] }
+  assert_eq! out, "hi\r\n"
+  assert_failure! discard <| IO.Process.run { cmd := "cmd.exe\" echo \"hi" }
+  assert_failure! discard <| IO.Process.run { cmd := "cmd.exe\\" }
+  -- creating a lot of processes should succeed and not leak file handles
+  for _ in [0:500] do
+    discard <| IO.Process.run { cmd := "cmd.exe", args := #["/c", "echo", "hi"] }
+  --assert_failure! discard <| IO.Process.run { cmd := "cmd.exe\" echo \"hi" }
+
+#eval runTests

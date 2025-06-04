9b5ea7c92e325d02a6166850fb0f0a5b845283dd
Siddharth Bhat <siddu.druid@gmail.com>
2025-06-04 13:08:41 +0100

    feat: generalize relabelNat_unsat_iff to allow empty variable type for AIG
    This showed up when reusing the AIG primitives for building model
    checkers. We generalize the proof, by showing that in the case when `α`
    is empty, the environment doesn't matter, since all environments
    `α → Bool` are isomorphic.

--- src/Std/Sat/AIG/RelabelNat.lean
+++ src/Std/Sat/AIG/RelabelNat.lean
@@ -325,10 +325,21 @@ theorem relabelNat'_fst_eq_relabelNat {aig : AIG α} : aig.relabelNat'.fst = aig
 theorem relabelNat_size_eq_size {aig : AIG α} : aig.relabelNat.decls.size = aig.decls.size := by
   simp [relabelNat, relabelNat']
 
-/--
-`relabelNat` preserves unsatisfiablility.
--/
-theorem relabelNat_unsat_iff [Nonempty α] {aig : AIG α} {hidx1} {hidx2} :
+theorem relabel_unsat_iff_of_not_NonEmpty {aigα : AIG α}
+    [DecidableEq β] [Hashable β] {r : α → β} {hidx1} {hidx2}
+    (hαNonempty : ¬ Nonempty α) :
+    (aigα.relabel r).UnsatAt idx invert hidx1 ↔ aigα.UnsatAt idx invert hidx2 := by
+  constructor
+  · intro hα assignα
+    let assignβ : β → Bool := fun b => false
+    specialize hα assignβ
+    have hAssignα : assignα  = assignβ ∘ r := by
+      ext a
+      apply hαNonempty (Nonempty.intro a) |>.elim
+    rw [hAssignα, ← denote_relabel, ← hα]
+  · apply unsat_relabel
+
+theorem relabelNat_unsat_iff_of_NonEmpty [Nonempty α] {aig : AIG α} {hidx1} {hidx2} :
     (aig.relabelNat).UnsatAt idx invert hidx1 ↔ aig.UnsatAt idx invert hidx2 := by
   dsimp only [relabelNat, relabelNat']
   rw [relabel_unsat_iff]
@@ -349,6 +360,15 @@ theorem relabelNat_unsat_iff [Nonempty α] {aig : AIG α} {hidx1} {hidx2} :
     exfalso
     rcases RelabelNat.State.ofAIG_find_some x hx with ⟨n, hn⟩
     simp [hcase] at hn
+
+/--
+`relabelNat` preserves unsatisfiablility.
+-/
+theorem relabelNat_unsat_iff {aig : AIG α} {hidx1} {hidx2} :
+    (aig.relabelNat).UnsatAt idx invert hidx1 ↔ aig.UnsatAt idx invert hidx2 := by
+  by_cases hαNonempty : Nonempty α
+  · apply relabelNat_unsat_iff_of_NonEmpty
+  · apply relabel_unsat_iff_of_not_NonEmpty hαNonempty
 
 namespace Entrypoint
 

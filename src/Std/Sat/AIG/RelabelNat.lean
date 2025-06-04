536cf36cc1d9c50d4a0e59e93d4cc9a598b94006
Siddharth Bhat <siddu.druid@gmail.com>
2025-06-04 13:38:24 +0100

    chore: generalize both Relabel and RelabelNat

--- src/Std/Sat/AIG/RelabelNat.lean
+++ src/Std/Sat/AIG/RelabelNat.lean
@@ -325,20 +325,6 @@ theorem relabelNat'_fst_eq_relabelNat {aig : AIG α} : aig.relabelNat'.fst = aig
 theorem relabelNat_size_eq_size {aig : AIG α} : aig.relabelNat.decls.size = aig.decls.size := by
   simp [relabelNat, relabelNat']
 
-theorem relabel_unsat_iff_of_not_NonEmpty {aigα : AIG α}
-    [DecidableEq β] [Hashable β] {r : α → β} {hidx1} {hidx2}
-    (hαNonempty : ¬ Nonempty α) :
-    (aigα.relabel r).UnsatAt idx invert hidx1 ↔ aigα.UnsatAt idx invert hidx2 := by
-  constructor
-  · intro hα assignα
-    let assignβ : β → Bool := fun b => false
-    specialize hα assignβ
-    have hAssignα : assignα  = assignβ ∘ r := by
-      ext a
-      apply hαNonempty (Nonempty.intro a) |>.elim
-    rw [hAssignα, ← denote_relabel, ← hα]
-  · apply unsat_relabel
-
 theorem relabelNat_unsat_iff_of_NonEmpty [Nonempty α] {aig : AIG α} {hidx1} {hidx2} :
     (aig.relabelNat).UnsatAt idx invert hidx1 ↔ aig.UnsatAt idx invert hidx2 := by
   dsimp only [relabelNat, relabelNat']
@@ -368,7 +354,7 @@ theorem relabelNat_unsat_iff {aig : AIG α} {hidx1} {hidx2} :
     (aig.relabelNat).UnsatAt idx invert hidx1 ↔ aig.UnsatAt idx invert hidx2 := by
   by_cases hαNonempty : Nonempty α
   · apply relabelNat_unsat_iff_of_NonEmpty
-  · apply relabel_unsat_iff_of_not_NonEmpty hαNonempty
+  · apply relabel_unsat_iff_of_not_Nonempty hαNonempty
 
 namespace Entrypoint
 

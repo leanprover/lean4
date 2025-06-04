2be93fa07831043e42dc4f44abeb9fcda86c4ea5
Joachim Breitner <mail@joachim-breitner.de>
2025-06-04 18:37:38 +0200

    Tweaks to not really working test yet

--- tests/lean/interactive/isRflParallel.lean
+++ tests/lean/interactive/isRflParallel.lean
@@ -11,7 +11,9 @@ evaluated.
 -/
 
 -- set_option trace.Elab.block true
--- set_option debug.skipKernelTC true
+set_option debug.skipKernelTC true
+set_option backward.dsimp.useDefEqAttr false
+set_option debug.tactic.simp.checkDefEqAttr false
 
 axiom testSorry : α
 
@@ -19,18 +21,22 @@ opaque a : Nat
 opaque b : Nat
 
 theorem a_eq_b : a = b := by
+  -- Three possible ways to pretend this theorem takes a long time:
+
   -- wait_for_unblock_async
+
   -- run_tac
   --   while true do
   --     if (← Server.Test.Cancel.unblockedCancelTk.isSet) then
   --       break
   --     IO.sleep 30
+
   -- sleep 100000
   exact testSorry
 
 theorem bar : 2 * a = 2 * b := by
   congr 1
-  -- apply _root_.a_eq_b
-  simp only [_root_.a_eq_b]
+  -- apply a_eq_b
+  simp only [a_eq_b]
   run_tac
     Server.Test.Cancel.unblockedCancelTk.set

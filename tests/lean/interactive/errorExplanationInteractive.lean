d9d643906237111c0a0710b0dad8789e24e8e086
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-06-05 18:15:31 +0200

    test: fix interactive explanation test

--- tests/lean/interactive/errorExplanationInteractive.lean
+++ tests/lean/interactive/errorExplanationInteractive.lean
@@ -1,4 +1,6 @@
 import Lean.ErrorExplanation
+import Lean.Exception
+import Lean.Log
 
 /-!
 # Error explanation interactive tests
@@ -7,33 +9,33 @@ Tests completions and hovers for error explanations.
 -/
 
 /-- Example -/
-register_error_explanation Lean.Bar {
+register_error_explanation TestDomain.Bar {
   summary := "Error 0"
   sinceVersion := "4.0.0"
 }
 
 /-- Example -/
-register_error_explanation Lean.Foo1 {
+register_error_explanation TestDomain.Foo1 {
   summary := "Error 1"
   sinceVersion := "4.0.0"
 }
 
 /-- Example -/
-register_error_explanation Lean.Foo2 {
+register_error_explanation TestDomain.Foo2 {
   summary := "Error 2"
   sinceVersion := "4.0.0"
 }
 
-#check throwNamedError Lean
-                         --^ textDocument/completion
-#check throwNamedError Lean.
-                          --^ textDocument/completion
-#check throwNamedError Lean.F
-                           --^ textDocument/completion
-#check throwNamedError Lean.F "test"
-                           --^ textDocument/completion
+#check throwNamedError TestDomain
+                               --^ textDocument/completion
+#check throwNamedError TestDomain.
+                                --^ textDocument/completion
+#check throwNamedError TestDomain.F
+                                 --^ textDocument/completion
+#check throwNamedError TestDomain.F "test"
+                                 --^ textDocument/completion
 
-#check throwNamedError Lean.Foo2
-                          --^ textDocument/hover
-#check throwNamedError Lean.Foo2 "test"
-                          --^ textDocument/hover
+#check throwNamedError TestDomain.Foo2
+                                --^ textDocument/hover
+#check throwNamedError TestDomain.Foo2 "test"
+                                --^ textDocument/hover

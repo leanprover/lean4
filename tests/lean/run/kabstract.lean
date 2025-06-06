c9e0f288adc133f6090b76419c2f77ac956c3f38
Kyle Miller <kmill31415@gmail.com>
2025-06-06 13:07:34 -0700

    add regression test

--- tests/lean/run/kabstract.lean
+++ tests/lean/run/kabstract.lean
@@ -10,7 +10,6 @@ In this example, the issue is that `rw [SPred.and_cons]` tries unifying the `SPr
 and since `σs` is implicit, all the explicit arguments fail.
 Instead, by first unifying the types the `σs`s are solved for immediately.
 -/
-
 namespace Test1
 
 abbrev SPred : List Type → Type
@@ -28,3 +27,23 @@ example {s : Nat} : (SPred.and (fun s => True) (fun s => True) : SPred [Nat]) s 
   exact And.intro trivial trivial
 
 end Test1
+
+/-!
+When unifying types, the reducibility level must be high enough to succeed.
+This example comes from the 1302 test, minimized here as a regression test.
+-/
+namespace Test2
+
+theorem mk_zero_eq (n : Nat) (h) : (⟨0, h⟩ : Fin (n + 1)) = 0 := rfl
+
+-- This was
+/- trace: ⊢ ⟨0, ⋯⟩ = 0 -/
+-- In particular, the theorem wasn't able to apply to the LHS. Now it applies to both sides.
+/-- trace: ⊢ 0 = 0 -/
+#guard_msgs in
+example : (⟨0, by simp⟩ : Fin [1,2,3].length) = (⟨0, by simp⟩ : Fin 3) := by
+  rewrite [mk_zero_eq]
+  trace_state
+  rfl
+
+end Test2

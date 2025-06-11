56b984fbc84ec85879dc38660351563b75d2f770
Kim Morrison <kim@tqft.net>
2025-06-11 18:47:27 +1000

    more

--- tests/lean/run/grind_list_erase.lean
+++ tests/lean/run/grind_list_erase.lean
@@ -10,7 +10,6 @@ theorem length_eraseP_of_mem (al : a ∈ l) (pa : p a) :
     length (l.eraseP p) = length l - 1 := by
   grind
 
--- reverse direction of lemma from library
 theorem eraseP_filterMap' {f : α → Option β} {l : List α} :
     filterMap f (l.eraseP (fun x => match f x with | some y => p y | none => false)) = (filterMap f l).eraseP p := by
   grind
@@ -20,4 +19,13 @@ theorem eraseP_append_left {a : α} (pa : p a) {l₁ : List α} {l₂} : a ∈ l
 
 theorem eraseP_append_right {l₁ : List α} {l₂} (h : ∀ b ∈ l₁, ¬p b) :
     eraseP p (l₁ ++ l₂) = l₁ ++ l₂.eraseP p := by
+  grind
+
+theorem head_eraseP_mem {xs : List α} {p : α → Bool} (h) : (xs.eraseP p).head h ∈ xs := by grind
+
+theorem getLast_eraseP_mem {xs : List α} {p : α → Bool} (h) : (xs.eraseP p).getLast h ∈ xs := by grind
+
+theorem set_getElem_succ_eraseIdx_succ
+    {xs : Array α} {i : Nat} (h : i + 1 < xs.size) :
+    (xs.eraseIdx (i + 1)).set i xs[i + 1] (by grind) = xs.eraseIdx i := by
   grind

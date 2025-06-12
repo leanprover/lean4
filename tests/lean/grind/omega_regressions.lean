f8e7460a3f3708faf0535698028232d5f28f4207
Kim Morrison <kim@tqft.net>
2025-06-12 15:25:52 +1000

    chore: add failing grind tests from Mathlib

---
+++ tests/lean/grind/omega_regressions.lean
@@ -0,0 +1,6 @@
+-- These are test cases extracted from Mathlib, where `omega` works but `grind` does not (yet!)
+
+example (n : Int) (n0 : ¬0 ≤ n) (a : Nat) : n ≠ (a : Int) := by grind
+
+example (p : Int) (n : Nat) (hmp : Int.negSucc (n + 1) + 1 = p)
+  (hnm : Int.negSucc (n + 1 + 1) + 1 = Int.negSucc (n + 1)) : p = Int.negSucc n := by grind

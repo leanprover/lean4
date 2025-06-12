30d5ee12d66abd872b876fbf025e35972d381937
Kim Morrison <kim@tqft.net>
2025-06-12 15:35:04 +1000

    attribute

--- tests/lean/grind/omega_regressions.lean
+++ tests/lean/grind/omega_regressions.lean
@@ -2,5 +2,8 @@
 
 example (n : Int) (n0 : ¬0 ≤ n) (a : Nat) : n ≠ (a : Int) := by grind
 
+-- TODO: add to the library?
+attribute [grind] Int.negSucc_eq
+
 example (p : Int) (n : Nat) (hmp : Int.negSucc (n + 1) + 1 = p)
   (hnm : Int.negSucc (n + 1 + 1) + 1 = Int.negSucc (n + 1)) : p = Int.negSucc n := by grind

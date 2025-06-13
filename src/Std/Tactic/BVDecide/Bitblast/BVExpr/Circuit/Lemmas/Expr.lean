a5b7f02f563dec4b044492d60a799e89a3b1274c
luisacicolini <luisacicolini@gmail.com>
2025-06-13 10:13:15 +0100

    chore: update name

--- src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Expr.lean
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Expr.lean
@@ -428,7 +428,7 @@ theorem go_denote_eq (aig : AIG BVBit) (expr : BVExpr w) (assign : Assignment)
       exact hinv
     · rw [← hres]
       simp only [eval_un, BVUnOp.eval_clz]
-      rw [BitVec.clz_eq_clzAuxRec_of_eq (x := eval assign _) (n := w - 1) (by omega), denote_blastClz]
+      rw [BitVec.clz_eq_clzAuxRec_of_le (x := eval assign _) (n := w - 1) (by omega), denote_blastClz]
       intro idx hidx
       rw [goCache_denote_eq]
       exact hinv

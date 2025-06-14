5a0c83f9ec1bb8d305b61792ebdda9dfc546cc16
Kyle Miller <kmill31415@gmail.com>
2025-06-14 13:41:35 -0700

    feat: add zetaHave/letToHave simp options
    This PR adds the `zetaHave` and `letToHave` options to `simp`. Implementations will appear in future PRs.

--- src/Init/MetaTypes.lean
+++ src/Init/MetaTypes.lean
@@ -161,8 +161,9 @@ structure Config where
   -/
   singlePass        : Bool := false
   /--
-  When `true` (default: `true`), performs zeta reduction of let expressions.
+  When `true` (default: `true`), performs zeta reduction of `let` and `have` expressions.
   That is, `let x := v; e[x]` reduces to `e[v]`.
+  If `zetaHave` is `false` then `have` expressions are not zeta reduced.
   See also `zetaDelta`.
   -/
   zeta              : Bool := true
@@ -226,7 +227,8 @@ structure Config where
   unfoldPartialApp  : Bool := false
   /--
   When `true` (default: `false`), local definitions are unfolded.
-  That is, given a local context containing entry `x : t := e`, the free variable `x` reduces to `e`.
+  That is, given a local context containing `x : t := e`, then the free variable `x` reduces to `e`.
+  Otherwise, `x` must be provided as a `simp` argument.
   -/
   zetaDelta         : Bool := false
   /--
@@ -240,7 +242,7 @@ structure Config where
   -/
   implicitDefEqProofs : Bool := true
   /--
-  When `true` (default : `true`), then simps will remove unused let-declarations:
+  When `true` (default : `true`), then `simp` will remove unused `let` and `have` expressions:
   `let x := v; e` simplifies to `e` when `x` does not occur in `e`.
   -/
   zetaUnused : Bool := true
@@ -249,6 +251,19 @@ structure Config where
   convert them into `simp` exceptions.
   -/
   catchRuntime : Bool := true
+  /--
+  (Unimplemented)
+  When `false` (default: `true`), then disables zeta reduction of `have` expressions.
+  If `zeta` is `false`, then this option has no effect.
+  Unused `have`s are still removed if `zeta` or `zetaUnused` are true.
+  -/
+  zetaHave : Bool := true
+  /--
+  (Unimplemented)
+  When `true` (default : `true`), then `simp` will attempt to transform `let`s into `have`s
+  if they are non-dependent. This only applies when `zeta := false`.
+  -/
+  letToHave : Bool := true
   deriving Inhabited, BEq
 
 -- Configuration object for `simp_all`
@@ -270,6 +285,7 @@ def neutralConfig : Simp.Config := {
   ground            := false
   zetaDelta         := false
   zetaUnused        := false
+  letToHave         := false
 }
 
 structure NormCastConfig extends Simp.Config where

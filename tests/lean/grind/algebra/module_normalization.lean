03e856b8b55a9604eb5b0ff90f990647454c5f4c
Leonardo de Moura <leomoura@amazon.com>
2025-06-14 19:14:46 -0700

    chore: fix tests
    @kim-em fixed this tests

--- tests/lean/grind/algebra/module_normalization.lean
+++ tests/lean/grind/algebra/module_normalization.lean
@@ -25,8 +25,8 @@ example (a : R) : 0 + a = a := by grind
 example (a b c : R) : a + b + c = a + (b + c) := by grind
 example (a : R) : 2 * a = a + a := by grind
 example (a : R) : (-2 : Int) * a = -a - a := by grind
-example (a b : R) : 2 * (b + c) = c + 2 * b + c := by grind
-example (a b c : R) : 2 * (b + c) - 3 * c + b + b = c + 5 * b - 2 * c := by grind
-example (a b c : R) : 2 * (b + c) + (-3 : Int) * c + b + b = c + (5 : Int) * b - 2 • c := by grind
+example (b c : R) : 2 * (b + c) = c + 2 * b + c := by grind
+example (b c : R) : 2 * (b + c) - 3 * c + b + b = c + 5 * b - 2 * c - b := by grind
+example (b c : R) : 2 * (b + c) + (-3 : Int) * c + b + b = c + (5 : Int) * b - 2 • c - b := by grind
 
 end IntModule

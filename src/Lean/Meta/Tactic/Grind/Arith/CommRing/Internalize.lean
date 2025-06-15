5bc5d31fd9002cd617d274909f265574d0be66a3
Leonardo de Moura <leomoura@amazon.com>
2025-06-14 21:49:37 -0400

    fix: `grind` ring + linarith internalization (#8795)
    This PR ensures that auxliary terms are not internalized by the ring and
    linarith modules.

--- src/Lean/Meta/Tactic/Grind/Arith/CommRing/Internalize.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/CommRing/Internalize.lean
@@ -108,6 +108,9 @@ private def internalizeInv (e : Expr) : GoalM Bool := do
 
 def internalize (e : Expr) (parent? : Option Expr) : GoalM Unit := do
   if !(← getConfig).ring && !(← getConfig).ringNull then return ()
+  if isIntModuleVirtualParent parent? then
+    -- `e` is an auxiliary term used to convert `CommRing` to `IntModule`
+    return ()
   if (← internalizeInv e) then return ()
   let some type := getType? e | return ()
   if isForbiddenParent parent? then return ()

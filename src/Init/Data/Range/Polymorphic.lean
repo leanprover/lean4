38d2806cdad309f6aa88093993804fb667cf0a82
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-06-18 15:29:33 +0200

    overview docstring

--- src/Init/Data/Range/Polymorphic.lean
+++ src/Init/Data/Range/Polymorphic.lean
@@ -9,3 +9,25 @@ prelude
 import Init.Data.Range.Polymorphic.Basic
 import Init.Data.Range.Polymorphic.Lemmas
 import Init.Data.Range.Polymorphic.Nat
+
+/-!
+# Polymorphic ranges
+
+Any type that provides certain typeclasses supports range notation: For example, `[2..<5]`
+stands for the numbers at least `2` and smaller than `5`. Such ranges support iteration with
+`for .. in` and can be converted into a list with `PRange.toList`. After importing
+`Std.Data.Iterators`, there will also be `PRange.iter`, which provides an iterator over the
+elements of the range.
+
+In order to support ranges of a certain type `α`, the following instances need to be implemented:
+
+* `LE α` and `DecidableLE α` for inclusive upper and lower bounds
+* `LT α` and `DecidableLT α` for exclusive upper and lower bounds
+* `UpwardEnumerable α` for iteration over the ranges
+* `LawfulUpwardEnumerableUpperBound upperBoundShape α` and
+  `LawfulUpwardEnumerableLowerBound lowerBoundShape α` for iteration
+* `HasFiniteRanges upperBoundShape α` for termination of iteration
+* `Least? α` for iteration over lower-unbounded ranges
+
+An example for how this plays out can be found in `Init.Data.Range.Polymorphic.Nat`.
+-/

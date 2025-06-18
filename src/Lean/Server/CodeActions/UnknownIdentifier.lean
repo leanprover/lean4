leanprover/lean4

commit 336b68ec201e098850824fdc5d205a3eb6cc5ea3

feat: 'unknown identifier' code actions (#7665)
This PR adds support for code actions that resolve 'unknown identifier'
errors by either importing the missing declaration or by changing the
identifier to one from the environment.

<details>
<summary>Demo (Click to open)</summary>


![Demo](https://github.com/user-attachments/assets/ba575860-b76d-4213-8cd7-a5525cd60287)
</details>

Specifically, the following kinds of code actions are added by this PR,
all of which are triggered on 'unknown identifier' errors:
- A code action to import the module containing the identifier at the
text cursor position.
- A code action to change the identifier at the text cursor position to
one from the environment.
- A source action to import the modules for all unambiguous identifiers
in the file.

### Details
When clicking on an identifier with an 'unknown identifier' diagnostic,
after a debounce delay of 1000ms, the language server looks up the
(potentially partial) identifier at the position of the cursor in the
global reference data structure by fuzzy-matching against all
identifiers and collects the 10 closest matching entries. This search
accounts for open namespaces at the position of the cursor, including
the namespace of the type / expected type when using dot notation. The
10 closest matching entries are then offered to the user as code
actions:
- If the suggested identifier is not contained in the environment, a
code action that imports the module that the identifier is contained in
and changes the identifier to the suggested one is offered. The
suggestion is inserted in a "minimal" manner, i.e. by accounting for
open namespaces.
- If the suggested identifier is contained in the environment, a code
action that only changes the identifier to the suggested one is offered.
- If the suggested identifier is not contained in the environment and
the suggested identifier is a perfectly unambiguous match, a source
action to import all unambiguous in the file is offered.

src/Lean/Server/CodeActions/UnknownIdentifier.lean
@@ -1,0 +1,341 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Marc Huisinga
+-/
+prelude
+import Lean.Server.FileWorker.Utils
+import Lean.Data.Lsp.Internal
+import Lean.Server.Requests
+import Lean.Server.Completion.CompletionInfoSelection
+import Lean.Server.CodeActions.Basic
+import Lean.Server.Completion.CompletionUtils
+
+namespace Lean.Server.FileWorker
+
+open Lean.Lsp
+open Lean.Server.Completion
+
+structure UnknownIdentifierInfo where
+  paramsRange : String.Range
+  diagRange   : String.Range
+
+def waitUnknownIdentifierRanges (doc : EditableDocument) (requestedRange : String.Range)
+    : BaseIO (Array String.Range) := do
+  let text := doc.meta.text
+  let msgLog := Language.toSnapshotTree doc.initSnap |>.collectMessagesInRange requestedRange |>.get
+  let mut ranges := #[]
+  for msg in msgLog.reportedPlusUnreported do
+    if ! msg.data.hasTag (· == unknownIdentifierMessageTag) then
+      continue
+    let msgRange : String.Range := ⟨text.ofPosition msg.pos, text.ofPosition <| msg.endPos.getD msg.pos⟩
+    if ! msgRange.overlaps requestedRange
+        (includeFirstStop := true) (includeSecondStop := true) then
+      continue
+    ranges := ranges.push msgRange
+  return ranges
+
+structure Insertion where
+  fullName : Name
+  edit     : TextEdit
+
+structure Query extends LeanModuleQuery where
+  env                : Environment
+  determineInsertion : Name → Insertion
+
+partial def collectOpenNamespaces (currentNamespace : Name) (openDecls : List OpenDecl)
+    : Array OpenNamespace := Id.run do
+  let mut openNamespaces : Array OpenNamespace := #[]
+  let mut currentNamespace := currentNamespace
+  while ! currentNamespace.isAnonymous do
+    openNamespaces := openNamespaces.push <| .allExcept currentNamespace #[]
+    currentNamespace := currentNamespace.getPrefix
+  let openDeclNamespaces := openDecls.map fun
+    | .simple ns except => .allExcept ns except.toArray
+    | .explicit id declName => .renamed declName id
+  openNamespaces := openNamespaces ++ openDeclNamespaces.toArray
+  return openNamespaces
+
+def computeFallbackQuery?
+    (doc : EditableDocument)
+    (requestedRange : String.Range)
+    (unknownIdentifierRange : String.Range)
+    (infoTree : Elab.InfoTree)
+    : Option Query := do
+  let text := doc.meta.text
+  let info? := infoTree.smallestInfo? fun info => Id.run do
+    let some range := info.range?
+      | return false
+    return range.overlaps requestedRange (includeFirstStop := true) (includeSecondStop := true)
+  let some (ctx, _) := info?
+    | none
+  return {
+    identifier := text.source.extract unknownIdentifierRange.start unknownIdentifierRange.stop
+    openNamespaces := collectOpenNamespaces ctx.currNamespace ctx.openDecls
+    env := ctx.env
+    determineInsertion decl :=
+      let minimizedId := minimizeGlobalIdentifierInContext ctx.currNamespace ctx.openDecls decl
+      {
+        fullName := minimizedId
+        edit := {
+          range := text.utf8RangeToLspRange unknownIdentifierRange
+          newText := minimizedId.toString
+        }
+      }
+  }
+
+def computeIdQuery?
+    (doc : EditableDocument)
+    (ctx : Elab.ContextInfo)
+    (stx : Syntax)
+    (id : Name)
+    : Option Query := do
+  let some pos := stx.getPos? (canonicalOnly := true)
+    | none
+  let some tailPos := stx.getTailPos? (canonicalOnly := true)
+    | none
+  return {
+    identifier := id.toString
+    openNamespaces := collectOpenNamespaces ctx.currNamespace ctx.openDecls
+    env := ctx.env
+    determineInsertion decl :=
+      let minimizedId := minimizeGlobalIdentifierInContext ctx.currNamespace ctx.openDecls decl
+      {
+        fullName := minimizedId
+        edit := {
+          range := doc.meta.text.utf8RangeToLspRange ⟨pos, tailPos⟩
+          newText := minimizedId.toString
+        }
+      }
+  }
+
+def computeDotQuery?
+    (doc : EditableDocument)
+    (ctx : Elab.ContextInfo)
+    (ti : Elab.TermInfo)
+    : IO (Option Query) := do
+  let text := doc.meta.text
+  let some pos := ti.stx.getPos? (canonicalOnly := true)
+    | return none
+  let some tailPos := ti.stx.getTailPos? (canonicalOnly := true)
+    | return none
+  let typeNames? : Option (Array Name) ← ctx.runMetaM ti.lctx do
+    try
+      return some <| ← getDotCompletionTypeNames (← Lean.instantiateMVars (← Lean.Meta.inferType ti.expr))
+    catch _ =>
+      return none
+  let some typeNames := typeNames?
+    | return none
+  return some {
+    identifier := text.source.extract pos tailPos
+    openNamespaces := typeNames.map (.allExcept · #[])
+    env := ctx.env
+    determineInsertion decl :=
+      {
+        fullName := decl
+        edit := {
+          range := text.utf8RangeToLspRange ⟨pos, tailPos⟩
+          newText := decl.getString!
+        }
+      }
+  }
+
+def computeDotIdQuery?
+    (doc : EditableDocument)
+    (ctx : Elab.ContextInfo)
+    (stx : Syntax)
+    (id : Name)
+    (lctx : LocalContext)
+    (expectedType? : Option Expr)
+    : IO (Option Query) := do
+  let some pos := stx.getPos? (canonicalOnly := true)
+    | return none
+  let some tailPos := stx.getTailPos? (canonicalOnly := true)
+    | return none
+  let some expectedType := expectedType?
+    | return none
+  let typeNames? : Option (Array Name) ← ctx.runMetaM lctx do
+    let resultTypeFn := (← instantiateMVars expectedType).cleanupAnnotations.getAppFn.cleanupAnnotations
+    let .const .. := resultTypeFn
+      | return none
+    try
+      return some <| ← getDotCompletionTypeNames resultTypeFn
+    catch _ =>
+      return none
+  let some typeNames := typeNames?
+    | return none
+  return some {
+    identifier := id.toString
+    openNamespaces := typeNames.map (.allExcept · #[])
+    env := ctx.env
+    determineInsertion decl :=
+      {
+        fullName := decl
+        edit := {
+          range := doc.meta.text.utf8RangeToLspRange ⟨pos, tailPos⟩
+          newText := decl.getString!
+        }
+      }
+  }
+
+def computeQuery?
+    (doc                    : EditableDocument)
+    (requestedRange         : String.Range)
+    (unknownIdentifierRange : String.Range)
+    : RequestM (Option Query) := do
+  let text := doc.meta.text
+  let some (stx, infoTree) := RequestM.findCmdDataAtPos doc unknownIdentifierRange.stop (includeStop := true) |>.get
+    | return none
+  let completionInfo? : Option ContextualizedCompletionInfo := do
+    let (completionPartitions, _) := findPrioritizedCompletionPartitionsAt text unknownIdentifierRange.stop stx infoTree
+    let highestPrioPartition ← completionPartitions[0]?
+    let (completionInfo, _) ← highestPrioPartition[0]?
+    return completionInfo
+  let some ⟨_, ctx, info⟩ := completionInfo?
+    | return computeFallbackQuery? doc requestedRange unknownIdentifierRange infoTree
+  match info with
+  | .id (stx := stx) (id := id) .. =>
+    return computeIdQuery? doc ctx stx id
+  | .dot (termInfo := ti) .. =>
+    return ← computeDotQuery? doc ctx ti
+  | .dotId stx id lctx expectedType? =>
+    return ← computeDotIdQuery? doc ctx stx id lctx expectedType?
+  | _ => return none
+
+def importAllUnknownIdentifiersProvider : Name := `unknownIdentifiers
+
+def importAllUnknownIdentifiersCodeAction (params : CodeActionParams) (kind : String) : CodeAction := {
+  title := "Import all unambiguous unknown identifiers"
+  kind? := kind
+  data? := some <| toJson {
+    params,
+    providerName := importAllUnknownIdentifiersProvider
+    providerResultIndex := 0
+    : CodeActionResolveData
+  }
+}
+
+def handleUnknownIdentifierCodeAction
+    (id                      : JsonRpc.RequestID)
+    (params                  : CodeActionParams)
+    (requestedRange          : String.Range)
+    (unknownIdentifierRanges : Array String.Range)
+    : RequestM (Array CodeAction) := do
+  let rc ← read
+  let doc := rc.doc
+  let text := doc.meta.text
+  let queries ← unknownIdentifierRanges.filterMapM fun unknownIdentifierRange =>
+    computeQuery? doc requestedRange unknownIdentifierRange
+  if queries.isEmpty then
+    return #[]
+  let responseTask ← RequestM.sendServerRequest LeanQueryModuleParams LeanQueryModuleResponse "$/lean/queryModule" {
+    sourceRequestID := id
+    queries := queries.map (·.toLeanModuleQuery)
+    : LeanQueryModuleParams
+  }
+  let r ← ServerTask.waitAny [
+    responseTask.mapCheap Sum.inl,
+    rc.cancelTk.requestCancellationTask.mapCheap Sum.inr
+  ]
+  let .inl (.success response) := r
+    | RequestM.checkCancelled
+      return #[]
+  let headerStx := doc.initSnap.stx
+  let importInsertionPos : Lsp.Position :=
+    match headerStx.getTailPos? with
+    | some headerTailPos => {
+        line := (text.utf8PosToLspPos headerTailPos |>.line) + 1
+        character := 0
+      }
+    | none => { line := 0, character := 0 }
+  let importInsertionRange : Lsp.Range := ⟨importInsertionPos, importInsertionPos⟩
+  let mut unknownIdentifierCodeActions := #[]
+  let mut hasUnambigiousImportCodeAction := false
+  for q in queries, result in response.queryResults do
+    for ⟨mod, decl, isExactMatch⟩ in result do
+      let isDeclInEnv := q.env.contains decl
+      if ! isDeclInEnv && mod == q.env.mainModule then
+        -- Don't offer any code actions for identifiers defined further down in the same file
+        continue
+      let insertion := q.determineInsertion decl
+      if ! isDeclInEnv then
+        unknownIdentifierCodeActions := unknownIdentifierCodeActions.push {
+          title := s!"Import {insertion.fullName} from {mod}"
+          kind? := "quickfix"
+          edit? := WorkspaceEdit.ofTextDocumentEdit {
+            textDocument := doc.versionedIdentifier
+            edits := #[
+              {
+                range := importInsertionRange
+                newText := s!"import {mod}\n"
+              },
+              insertion.edit
+            ]
+          }
+        }
+        if isExactMatch then
+          hasUnambigiousImportCodeAction := true
+      else
+        unknownIdentifierCodeActions := unknownIdentifierCodeActions.push {
+          title := s!"Change to {insertion.fullName}"
+          kind? := "quickfix"
+          edit? := WorkspaceEdit.ofTextDocumentEdit {
+            textDocument := doc.versionedIdentifier
+            edits := #[insertion.edit]
+          }
+        }
+  if hasUnambigiousImportCodeAction then
+    unknownIdentifierCodeActions := unknownIdentifierCodeActions.push <|
+      importAllUnknownIdentifiersCodeAction params "quickfix"
+  return unknownIdentifierCodeActions
+
+def handleResolveImportAllUnknownIdentifiersCodeAction?
+    (id                      : JsonRpc.RequestID)
+    (action                  : CodeAction)
+    (unknownIdentifierRanges : Array String.Range)
+    : RequestM (Option CodeAction) := do
+  let rc ← read
+  let doc := rc.doc
+  let text := doc.meta.text
+  let queries ← unknownIdentifierRanges.filterMapM fun unknownIdentifierRange =>
+    computeQuery? doc ⟨0, text.source.endPos⟩ unknownIdentifierRange
+  if queries.isEmpty then
+    return none
+  let responseTask ← RequestM.sendServerRequest LeanQueryModuleParams LeanQueryModuleResponse "$/lean/queryModule" {
+    sourceRequestID := id
+    queries := queries.map (·.toLeanModuleQuery)
+    : LeanQueryModuleParams
+  }
+  let .success response := responseTask.get
+    | return none
+  let headerStx := doc.initSnap.stx
+  let importInsertionPos : Lsp.Position :=
+    match headerStx.getTailPos? with
+    | some headerTailPos => {
+        line := (text.utf8PosToLspPos headerTailPos |>.line) + 1
+        character := 0
+      }
+    | none => { line := 0, character := 0 }
+  let importInsertionRange : Lsp.Range := ⟨importInsertionPos, importInsertionPos⟩
+  let mut edits : Array TextEdit := #[]
+  let mut imports : Std.HashSet Name := ∅
+  for q in queries, result in response.queryResults do
+    let some ⟨mod, decl, _⟩ := result.find? fun id =>
+        id.isExactMatch && ! q.env.contains id.decl
+      | continue
+    if mod == q.env.mainModule then
+      continue
+    let insertion := q.determineInsertion decl
+    if ! imports.contains mod then
+      edits := edits.push {
+        range := importInsertionRange
+        newText := s!"import {mod}\n"
+      }
+    edits := edits.push insertion.edit
+    imports := imports.insert mod
+  return some { action with
+    edit? := WorkspaceEdit.ofTextDocumentEdit {
+      textDocument := doc.versionedIdentifier
+      edits
+    }
+  }

src/Lean/Server/Requests.lean
@@ -88,6 +88,18 @@ partial def SnapshotTree.findInfoTreeAtPos (text : FileMap) (tree : SnapshotTree
         | return (none, .proceed (foldChildren := true))
       return (infoTree, .done)
 
+partial def SnapshotTree.collectMessagesInRange (tree : SnapshotTree)
+    (requestedRange : String.Range) : ServerTask MessageLog :=
+  tree.foldSnaps (init := .empty) fun snap log => Id.run do
+    let some stx := snap.stx?
+      | return .pure (log, .proceed (foldChildren := true))
+    let some range := stx.getRangeWithTrailing? (canonicalOnly := true)
+      | return .pure (log, .proceed (foldChildren := true))
+    if ! range.overlaps requestedRange (includeFirstStop := true) (includeSecondStop := true) then
+      return .pure (log, .proceed (foldChildren := false))
+    return snap.task.asServerTask.mapCheap fun tree => Id.run do
+      return (log ++ tree.element.diagnostics.msgLog, .proceed (foldChildren := true))
+
 end Lean.Language
 
 namespace Lean.Server
@@ -109,13 +121,16 @@ def methodNotFound (method : String) : RequestError :=
     message := s!"No request handler found for '{method}'" }
 
 def invalidParams (message : String) : RequestError :=
-  {code := ErrorCode.invalidParams, message}
+  { code := ErrorCode.invalidParams, message }
 
 def internalError (message : String) : RequestError :=
   { code := ErrorCode.internalError, message }
 
 def requestCancelled : RequestError :=
   { code := ErrorCode.requestCancelled, message := "" }
+
+def rpcNeedsReconnect : RequestError :=
+  { code := ErrorCode.rpcNeedsReconnect, message := "Outdated RPC session" }
 
 def ofException (e : Lean.Exception) : IO RequestError :=
   return internalError (← e.toMessageData.toString)
@@ -132,17 +147,27 @@ end RequestError
 
 def parseRequestParams (paramType : Type) [FromJson paramType] (params : Json)
     : Except RequestError paramType :=
-  fromJson? params |>.mapError fun inner =>
-    { code := JsonRpc.ErrorCode.parseError
-      message := s!"Cannot parse request params: {params.compress}\n{inner}" }
+  fromJson? params |>.mapError fun inner => {
+    code := JsonRpc.ErrorCode.invalidParams
+    message := s!"Cannot parse request params: {params.compress}\n{inner}"
+  }
+
+inductive ServerRequestResponse (α : Type) where
+  | success (response : α)
+  | failure (code : JsonRpc.ErrorCode) (message : String)
+  deriving Inhabited
+
+abbrev ServerRequestEmitter := (method : String) → (param : Json)
+  → BaseIO (ServerTask (ServerRequestResponse Json))
 
 structure RequestContext where
-  rpcSessions       : RBMap UInt64 (IO.Ref FileWorker.RpcSession) compare
-  srcSearchPathTask : ServerTask SearchPath
-  doc               : FileWorker.EditableDocument
-  hLog              : IO.FS.Stream
-  initParams        : Lsp.InitializeParams
-  cancelTk          : RequestCancellationToken
+  rpcSessions          : RBMap UInt64 (IO.Ref FileWorker.RpcSession) compare
+  srcSearchPathTask    : ServerTask SearchPath
+  doc                  : FileWorker.EditableDocument
+  hLog                 : IO.FS.Stream
+  initParams           : Lsp.InitializeParams
+  cancelTk             : RequestCancellationToken
+  serverRequestEmitter : ServerRequestEmitter
 
 def RequestContext.srcSearchPath (rc : RequestContext) : SearchPath :=
   rc.srcSearchPathTask.get
@@ -151,6 +176,9 @@ abbrev RequestTask α := ServerTask (Except RequestError α)
 abbrev RequestT m := ReaderT RequestContext <| ExceptT RequestError m
 /-- Workers execute request handlers in this monad. -/
 abbrev RequestM := ReaderT RequestContext <| EIO RequestError
+
+def RequestM.run (act : RequestM α) (rc : RequestContext) : EIO RequestError α :=
+  act rc
 
 abbrev RequestTask.pure (a : α) : RequestTask α := ServerTask.pure (.ok a)
 
@@ -209,10 +237,48 @@ def bindTaskCostly (t : ServerTask α) (f : α → RequestM (RequestTask β)) : 
   let rc ← readThe RequestContext
   ServerTask.EIO.bindTaskCostly t (f · rc)
 
+def mapRequestTaskCheap (t : RequestTask α) (f : α → RequestM β) : RequestM (RequestTask β) := do
+  mapTaskCheap (t := t) fun
+    | .error e => throw e
+    | .ok r => f r
+
+def mapRequestTaskCostly (t : RequestTask α) (f : α → RequestM β) : RequestM (RequestTask β) := do
+  mapTaskCostly (t := t) fun
+    | .error e => throw e
+    | .ok r => f r
+
+def bindRequestTaskCheap (t : RequestTask α) (f : α → RequestM (RequestTask β)) : RequestM (RequestTask β) := do
+  bindTaskCheap (t := t) fun
+    | .error e => throw e
+    | .ok r => f r
+
+def bindRequestTaskCostly (t : RequestTask α) (f : α → RequestM (RequestTask β)) : RequestM (RequestTask β) := do
+  bindTaskCostly (t := t) fun
+    | .error e => throw e
+    | .ok r => f r
+
+def parseRequestParams (paramType : Type) [FromJson paramType] (params : Json)
+    : RequestM paramType :=
+  EIO.ofExcept <| Server.parseRequestParams paramType params
+
 def checkCancelled : RequestM Unit := do
   let rc ← readThe RequestContext
   if ← rc.cancelTk.wasCancelledByCancelRequest then
     throw .requestCancelled
+
+def sendServerRequest
+    paramType [ToJson paramType] responseType [FromJson responseType] [Inhabited responseType]
+    (method : String)
+    (param  : paramType)
+    : RequestM (ServerTask (ServerRequestResponse responseType)) := do
+  let ctx ← read
+  let task ← ctx.serverRequestEmitter method (toJson param)
+  return task.mapCheap fun
+    | ServerRequestResponse.success response =>
+      match fromJson? response with
+      | .ok (response : responseType) => ServerRequestResponse.success response
+      | .error err => ServerRequestResponse.failure .parseError s!"Cannot parse server request response: {response.compress}\n{err}"
+    | ServerRequestResponse.failure code msg => ServerRequestResponse.failure code msg
 
 def waitFindSnapAux (notFoundX : RequestM α) (x : Snapshot → RequestM α)
     : Except IO.Error (Option Snapshot) → RequestM α
@@ -385,7 +451,7 @@ def registerLspRequestHandler (method : String)
   let fileSource := fun j =>
     parseRequestParams paramType j |>.map Lsp.fileSource
   let handle := fun j => do
-    let params ← liftExcept <| parseRequestParams paramType j
+    let params ← RequestM.parseRequestParams paramType j
     let t ← handler params
     pure <| t.mapCheap <| Except.map ToJson.toJson
 
@@ -412,7 +478,7 @@ def chainLspRequestHandler (method : String)
       let t ← oldHandler.handle j
       let t := t.mapCheap fun x => x.bind fun j => FromJson.fromJson? j |>.mapError fun e =>
         .internalError s!"Failed to parse original LSP response for `{method}` when chaining: {e}"
-      let params ← liftExcept <| parseRequestParams paramType j
+      let params ← RequestM.parseRequestParams paramType j
       let t ← handler params t
       pure <| t.mapCheap <| Except.map ToJson.toJson
 
@@ -493,7 +559,7 @@ private def overrideStatefulLspRequestHandler
   let stateRef ← IO.mkRef initState
 
   let pureHandle : Json → Dynamic → RequestM (LspResponse Json × Dynamic) := fun param state => do
-    let param ← liftExcept <| parseRequestParams paramType param
+    let param ← RequestM.parseRequestParams paramType param
     let state ← getState! method state stateType
     let (r, state') ← handler param state
     return ({ r with response := toJson r.response }, Dynamic.mk state')

src/Lean/Data/Lsp/Diagnostics.lean
@@ -145,33 +145,11 @@ structure DiagnosticWith (α : Type) where
   /-- An array of related diagnostic information, e.g. when symbol-names within a scope collide all definitions can be marked via this property. -/
   relatedInformation? : Option (Array DiagnosticRelatedInformation) := none
   /-- A data entry field that is preserved between a `textDocument/publishDiagnostics` notification and `textDocument/codeAction` request. -/
-  data?: Option Json := none
+  data? : Option Json := none
   deriving Inhabited, BEq, ToJson, FromJson
 
 def DiagnosticWith.fullRange (d : DiagnosticWith α) : Range :=
   d.fullRange?.getD d.range
-
-attribute [local instance] Ord.arrayOrd in
-/-- Restriction of `DiagnosticWith` to properties that are displayed to users in the InfoView. -/
-private structure DiagnosticWith.UserVisible (α : Type) where
-  range               : Range
-  fullRange?          : Option Range
-  severity?           : Option DiagnosticSeverity
-  code?               : Option DiagnosticCode
-  source?             : Option String
-  message             : α
-  tags?               : Option (Array DiagnosticTag)
-  relatedInformation? : Option (Array DiagnosticRelatedInformation)
-  deriving Ord
-
-/-- Extracts user-visible properties from the given `DiagnosticWith`. -/
-private def DiagnosticWith.UserVisible.ofDiagnostic (d : DiagnosticWith α)
-    : DiagnosticWith.UserVisible α :=
-  { d with }
-
-/-- Compares `DiagnosticWith` instances modulo non-user-facing properties. -/
-def compareByUserVisible [Ord α] (a b : DiagnosticWith α) : Ordering :=
-  compare (DiagnosticWith.UserVisible.ofDiagnostic a) (DiagnosticWith.UserVisible.ofDiagnostic b)
 
 abbrev Diagnostic := DiagnosticWith String
 

src/Lean/Data/Lsp/Internal.lean
@@ -7,6 +7,7 @@ Authors: Joscha Mennicken
 prelude
 import Lean.Expr
 import Lean.Data.Lsp.Basic
+import Lean.Data.JsonRpc
 import Std.Data.TreeMap
 
 set_option linter.missingDocs true -- keep it documented
@@ -200,5 +201,63 @@ structure LeanStaleDependencyParams where
   /-- The dependency that is stale. -/
   staleDependency : DocumentUri
   deriving FromJson, ToJson
+
+/-- LSP type for `Lean.OpenDecl`. -/
+inductive OpenNamespace
+  /-- All declarations in `«namespace»` are opened, except for `exceptions`. -/
+  | allExcept («namespace» : Name) (exceptions : Array Name)
+  /-- The declaration `«from»` is renamed to `to`. -/
+  | renamed («from» : Name) (to : Name)
+  deriving FromJson, ToJson
+
+/-- Query in the `$/lean/queryModule` watchdog <- worker request. -/
+structure LeanModuleQuery where
+  /-- Identifier (potentially partial) to query. -/
+  identifier : String
+  /--
+  Namespaces that are open at the position of `identifier`.
+  Used for accurately matching declarations against `identifier` in context.
+  -/
+  openNamespaces : Array OpenNamespace
+  deriving FromJson, ToJson
+
+/--
+Used in the `$/lean/queryModule` watchdog <- worker request, which is used by the worker to
+extract information from the .ilean information in the watchdog.
+-/
+structure LeanQueryModuleParams where
+  /--
+  The request ID in the context of which this worker -> watchdog request was emitted.
+  Used for cancelling this request in the watchdog.
+  -/
+  sourceRequestID : JsonRpc.RequestID
+  /-- Module queries for extracting .ilean information in the watchdog. -/
+  queries : Array LeanModuleQuery
+  deriving FromJson, ToJson
+
+/-- Result entry of a module query. -/
+structure LeanIdentifier where
+  /-- Module that `decl` is defined in. -/
+  module : Name
+  /-- Full name of the declaration that matches the query. -/
+  decl : Name
+  /-- Whether this `decl` matched the query exactly. -/
+  isExactMatch : Bool
+  deriving FromJson, ToJson
+
+/--
+Result for a single module query.
+Identifiers in the response are sorted descendingly by how well they match the query.
+-/
+abbrev LeanQueriedModule := Array LeanIdentifier
+
+/-- Response for the `$/lean/queryModule` watchdog <- worker request. -/
+structure LeanQueryModuleResponse where
+  /--
+  Results for each query in `LeanQueryModuleParams`.
+  Positions correspond to `queries` in the parameter of the request.
+  -/
+  queryResults : Array LeanQueriedModule
+  deriving FromJson, ToJson, Inhabited
 
 end Lean.Lsp

commit aea8e11d4ba585f74bbe5cd569b2f007751b451c

fix: restore code action incrementality (#8868)
This PR ensures that code actions do not have to wait for the full file
to elaborate. This regression was accidentally introduced in #7665.

src/Lean/Server/CodeActions/UnknownIdentifier.lean
@@ -25,7 +25,7 @@ def waitUnknownIdentifierRanges (doc : EditableDocument) (requestedRange : Strin
   let text := doc.meta.text
   let some parsedSnap := RequestM.findCmdParsedSnap doc requestedRange.start |>.get
     | return #[]
-  let msgLog := Language.toSnapshotTree parsedSnap |>.collectMessagesInRange requestedRange |>.get
+  let msgLog := Language.toSnapshotTree parsedSnap.elabSnap |>.collectMessagesInRange requestedRange |>.get
   let mut ranges := #[]
   for msg in msgLog.unreported do
     if ! msg.data.hasTag (· == unknownIdentifierMessageTag) then

src/Lean/Server/FileWorker/RequestHandling.lean
@@ -269,7 +269,7 @@ def findGoalsAt? (doc : EditableDocument) (hoverPos : String.Pos) : ServerTask (
   let text := doc.meta.text
   findCmdParsedSnap doc hoverPos |>.bindCostly fun
     | some cmdParsed =>
-      let t := toSnapshotTree cmdParsed |>.foldSnaps [] fun snap oldGoals => Id.run do
+      let t := toSnapshotTree cmdParsed.elabSnap |>.foldSnaps [] fun snap oldGoals => Id.run do
         let some stx := snap.stx?
           | return .pure (oldGoals, .proceed (foldChildren := false))
         let some (pos, tailPos, trailingPos) := getPositions stx

src/Lean/Server/Requests.lean
@@ -394,7 +394,7 @@ def findCmdDataAtPos
     (includeStop : Bool)
     : ServerTask (Option (Syntax × Elab.InfoTree)) :=
   findCmdParsedSnap doc hoverPos |>.bindCheap fun
-    | some cmdParsed => toSnapshotTree cmdParsed |>.findInfoTreeAtPos doc.meta.text hoverPos includeStop |>.bindCheap fun
+    | some cmdParsed => toSnapshotTree cmdParsed.elabSnap |>.findInfoTreeAtPos doc.meta.text hoverPos includeStop |>.bindCheap fun
       | some infoTree => .pure <| some (cmdParsed.stx, infoTree)
       | none          => cmdParsed.elabSnap.infoTreeSnap.task.asServerTask.mapCheap fun s =>
         assert! s.infoTree?.isSome
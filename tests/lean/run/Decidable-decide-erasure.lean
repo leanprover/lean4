50e66cd2f4adbe0c8adb2785fc7fce229e2964d6
Rob23oba <robin.arnez@web.de>
2025-06-20 21:42:25 +0200

    restore tests

--- tests/lean/run/Decidable-decide-erasure.lean
+++ tests/lean/run/Decidable-decide-erasure.lean
@@ -6,6 +6,9 @@ open Lean.Compiler.LCNF
 def f (a : Nat) : Bool :=
   decide (a = 1)
 
+-- This is only required until the new code generator is enabled.
+run_meta Lean.Compiler.compile #[``f]
+
 def countCalls : Probe Decl Nat :=
   Probe.getLetValues >=>
   Probe.filter (fun e => return e matches .const `Decidable.decide ..) >=>
@@ -13,7 +16,7 @@ def countCalls : Probe Decl Nat :=
 
 #eval do
   let numCalls <- Probe.runOnDeclsNamed #[`f] (phase := .base) <| countCalls
-  assert! numCalls == #[0]
+  assert! numCalls == #[1]
 
 #eval do
   let numCalls <- Probe.runOnDeclsNamed #[`f] (phase := .mono) <| countCalls

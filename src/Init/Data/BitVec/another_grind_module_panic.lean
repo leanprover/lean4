25613a5b9f67a5f8c89872b3ce9fc30df3e40362
Kim Morrison <kim@tqft.net>
2025-06-20 11:06:16 +1000

    rename

--- src/Init/Data/BitVec/another_grind_module_panic.lean
+++ src/Init/Data/BitVec/another_grind_module_panic.lean
@@ -11,11 +11,11 @@ import Init.Grind
 
 namespace BitVec
 
-theorem getElem_of_getLsbD_eq_true {x : BitVec w} {i : Nat} (h : x.getLsbD i = true) :
+theorem getElem_of_getLsbD_eq_true' {x : BitVec w} {i : Nat} (h : x.getLsbD i = true) :
     (x[i]'sorry = true) = True := by
   sorry
 
-theorem getLsbD_eq_getMsbD (x : BitVec w) (i : Nat) : x.getLsbD i = (decide (i < w) && x.getMsbD (w - 1 - i)) := by
+theorem getLsbD_eq_getMsbD' (x : BitVec w) (i : Nat) : x.getLsbD i = (decide (i < w) && x.getMsbD (w - 1 - i)) := by
   rw [getMsbD]
   by_cases h₁ : i < w <;> by_cases h₂ : w - 1 - i < w <;> simp only [h₁, h₂]
   · -- FIXME `grind` panics here

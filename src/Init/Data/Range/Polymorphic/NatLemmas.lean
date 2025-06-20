4742a3007d808cfca76a32bf8423f0d44d882a39
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-06-20 16:48:58 +0200

    add a nat lemma

--- src/Init/Data/Range/Polymorphic/NatLemmas.lean
+++ src/Init/Data/Range/Polymorphic/NatLemmas.lean
@@ -11,13 +11,13 @@ import Init.Data.Range.Polymorphic.Lemmas
 
 namespace Std.PRange.Nat
 
--- -- idea: show that toList = range'
--- @[simp] theorem forIn'_eq_forIn'_range' [Monad m] (r : PRange ⟨.closed, .open⟩ Nat)
---     (init : β) (f : (a : Nat) → a ∈ r → β → m (ForInStep β)) :
---     forIn' r init f =
---       forIn' (List.range' r.lower r.size 1) init (fun a h => f a (mem_of_mem_range' h)) := by
+theorem succ_eq {n : Nat} : UpwardEnumerable.succ n = n + 1 :=
+  rfl
 
--- theorem ClosedClosed.toList_succ_succ  {m n : Nat} :
---     ((m+1)..(n+1)).toList = (m..n).toList.map (· + 1) := sorry
+theorem ClosedOpen.toList_succ_succ  {m n : Nat} :
+    (PRange.mk (shape := ⟨.closed, .open⟩) (m+1) (n+1)).toList =
+      (PRange.mk (shape := ⟨.closed, .open⟩) m n).toList.map (· + 1) := by
+  simp only [← succ_eq]
+  rw [Std.PRange.ClosedOpen.toList_succ_succ_eq_map]
 
 end Std.PRange.Nat

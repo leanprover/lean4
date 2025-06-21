478fbea70ec77b66793ec4f4aa0ebfebb315165e
Leonardo de Moura <leomoura@amazon.com>
2025-06-21 09:05:09 +0900

    fix: grind modifiers

--- src/Init/Grind/Tactics.lean
+++ src/Init/Grind/Tactics.lean
@@ -71,8 +71,8 @@ syntax grindEq     := "=" (grindGen)?
 syntax grindEqBoth := atomic("_" "=" "_") (grindGen)?
 syntax grindEqRhs  := atomic("=" "_") (grindGen)?
 syntax grindEqBwd  := atomic("←" "=") <|> atomic("<-" "=")
-syntax grindBwd    := ("←" <|> "<-") (grindGen)?
-syntax grindFwd    := "→" <|> "->"
+syntax grindBwd    := ("← " <|> "←" <|> "<-") (grindGen)? -- TODO remove "← " after update stage 0
+syntax grindFwd    := "→ " <|> "→" <|> "->" -- TODO remove "→ " after update stage 0
 syntax grindRL     := "⇐" <|> "<="
 syntax grindLR     := "⇒" <|> "=>"
 syntax grindUsr    := &"usr"

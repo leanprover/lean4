794230e633b5a1cc1d9b742fb8a8df417dee0e45
Kim Morrison <kim@tqft.net>
2025-06-25 14:30:38 +1000

    feat: add ToInt typeclasses for grind

--- src/Init/GrindInstances/ToInt.lean
+++ src/Init/GrindInstances/ToInt.lean
@@ -7,6 +7,7 @@ module
 
 prelude
 import all Init.Grind.ToInt
+import Init.Grind.Module.Basic
 import Init.Data.Int.DivMod.Basic
 import Init.Data.Int.Lemmas
 import Init.Data.Int.Order
@@ -26,6 +27,9 @@ instance : ToInt Int none none where
 
 @[simp] theorem toInt_int (x : Int) : ToInt.toInt x = x := rfl
 
+instance : ToInt.Zero Int none none where
+  toInt_zero := by simp
+
 instance : ToInt.Add Int none none where
   toInt_add := by simp
 
@@ -35,8 +39,17 @@ instance : ToInt.Neg Int none none where
 instance : ToInt.Sub Int none none where
   toInt_sub x y := by simp
 
+instance : ToInt.Mul Int none none where
+  toInt_mul x y := by simp
+
+instance : ToInt.Pow Int none none where
+  toInt_pow x n := by simp
+
 instance : ToInt.Mod Int none none where
   toInt_mod x y := by simp
+
+instance : ToInt.Div Int none none where
+  toInt_div x y := by simp
 
 instance : ToInt.LE Int none none where
   le_iff x y := by simp
@@ -52,11 +65,23 @@ instance : ToInt Nat (some 0) none where
 
 @[simp] theorem toInt_nat (x : Nat) : ToInt.toInt x = (x : Int) := rfl
 
+instance : ToInt.Zero Nat (some 0) none where
+  toInt_zero := by simp
+
 instance : ToInt.Add Nat (some 0) none where
   toInt_add := by simp
 
+instance : ToInt.Mul Nat (some 0) none where
+  toInt_mul x y := by simp
+
+instance : ToInt.Pow Nat (some 0) none where
+  toInt_pow x n := by simp
+
 instance : ToInt.Mod Nat (some 0) none where
   toInt_mod x y := by simp
+
+instance : ToInt.Div Nat (some 0) none where
+  toInt_div x y := by simp
 
 instance : ToInt.LE Nat (some 0) none where
   le_iff x y := by simp
@@ -74,17 +99,26 @@ instance : ToInt (Fin n) (some 0) (some n) where
 
 @[simp] theorem toInt_fin (x : Fin n) : ToInt.toInt x = (x.val : Int) := rfl
 
+instance [NeZero n] : ToInt.Zero (Fin n) (some 0) (some n) where
+  toInt_zero := rfl
+
 instance : ToInt.Add (Fin n) (some 0) (some n) where
   toInt_add x y := by rfl
 
-instance [NeZero n] : ToInt.Zero (Fin n) (some 0) (some n) where
-  toInt_zero := by rfl
-
 -- The `ToInt.Neg` and `ToInt.Sub` instances are generated automatically from the `IntModule (Fin n)` instance.
+-- See `Init.GrindInstances.Ring.Fin`.
+
+instance : ToInt.Mul (Fin n) (some 0) (some n) where
+  toInt_mul x y := by rfl
+
+-- The `IoInt.Pow` instance is defined in `Init.GrindInstances.Ring.Fin`,
+-- since the power operation is only defined there.
 
 instance : ToInt.Mod (Fin n) (some 0) (some n) where
-  toInt_mod x y := by
-    simp only [toInt_fin, Fin.mod_val, Int.natCast_emod]
+  toInt_mod _ _ := rfl
+
+instance : ToInt.Div (Fin n) (some 0) (some n) where
+  toInt_div _ _ := rfl
 
 instance : ToInt.LE (Fin n) (some 0) (some n) where
   le_iff x y := by simpa using Fin.le_def
@@ -100,14 +134,20 @@ instance : ToInt UInt8 (some 0) (some (2^8)) where
 
 @[simp] theorem toInt_uint8 (x : UInt8) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add UInt8 (some 0) (some (2^8)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero UInt8 (some 0) (some (2^8)) where
   toInt_zero := by simp
 
+instance : ToInt.Add UInt8 (some 0) (some (2^8)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul UInt8 (some 0) (some (2^8)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod UInt8 (some 0) (some (2^8)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div UInt8 (some 0) (some (2^8)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE UInt8 (some 0) (some (2^8)) where
   le_iff x y := by simpa using UInt8.le_iff_toBitVec_le
@@ -123,14 +163,20 @@ instance : ToInt UInt16 (some 0) (some (2^16)) where
 
 @[simp] theorem toInt_uint16 (x : UInt16) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add UInt16 (some 0) (some (2^16)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero UInt16 (some 0) (some (2^16)) where
   toInt_zero := by simp
 
+instance : ToInt.Add UInt16 (some 0) (some (2^16)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul UInt16 (some 0) (some (2^16)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod UInt16 (some 0) (some (2^16)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div UInt16 (some 0) (some (2^16)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE UInt16 (some 0) (some (2^16)) where
   le_iff x y := by simpa using UInt16.le_iff_toBitVec_le
@@ -146,14 +192,20 @@ instance : ToInt UInt32 (some 0) (some (2^32)) where
 
 @[simp] theorem toInt_uint32 (x : UInt32) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add UInt32 (some 0) (some (2^32)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero UInt32 (some 0) (some (2^32)) where
   toInt_zero := by simp
 
+instance : ToInt.Add UInt32 (some 0) (some (2^32)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul UInt32 (some 0) (some (2^32)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod UInt32 (some 0) (some (2^32)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div UInt32 (some 0) (some (2^32)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE UInt32 (some 0) (some (2^32)) where
   le_iff x y := by simpa using UInt32.le_iff_toBitVec_le
@@ -169,14 +221,20 @@ instance : ToInt UInt64 (some 0) (some (2^64)) where
 
 @[simp] theorem toInt_uint64 (x : UInt64) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add UInt64 (some 0) (some (2^64)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero UInt64 (some 0) (some (2^64)) where
   toInt_zero := by simp
 
+instance : ToInt.Add UInt64 (some 0) (some (2^64)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul UInt64 (some 0) (some (2^64)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod UInt64 (some 0) (some (2^64)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div UInt64 (some 0) (some (2^64)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE UInt64 (some 0) (some (2^64)) where
   le_iff x y := by simpa using UInt64.le_iff_toBitVec_le
@@ -196,14 +254,20 @@ instance : ToInt USize (some 0) (some (2^System.Platform.numBits)) where
 
 @[simp] theorem toInt_usize (x : USize) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add USize (some 0) (some (2^System.Platform.numBits)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero USize (some 0) (some (2^System.Platform.numBits)) where
   toInt_zero := by simp
 
+instance : ToInt.Add USize (some 0) (some (2^System.Platform.numBits)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul USize (some 0) (some (2^System.Platform.numBits)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod USize (some 0) (some (2^System.Platform.numBits)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div USize (some 0) (some (2^System.Platform.numBits)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE USize (some 0) (some (2^System.Platform.numBits)) where
   le_iff x y := by simpa using USize.le_iff_toBitVec_le
@@ -219,17 +283,21 @@ instance : ToInt Int8 (some (-2^7)) (some (2^7)) where
 
 @[simp] theorem toInt_int8 (x : Int8) : ToInt.toInt x = (x.toInt : Int) := rfl
 
-instance : ToInt.Add Int8 (some (-2^7)) (some (2^7)) where
-  toInt_add x y := by
-    simp [Int.bmod_eq_emod]
-    split <;> · simp; omega
-
 instance : ToInt.Zero Int8 (some (-2^7)) (some (2^7)) where
   toInt_zero := by
     --  simp -- FIXME: succeeds, but generates a `(kernel) application type mismatch` error!
     change (0 : Int8).toInt = _
     rw [Int8.toInt_zero]
-    decide
+
+instance : ToInt.Add Int8 (some (-2^7)) (some (2^7)) where
+  toInt_add x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
+
+instance : ToInt.Mul Int8 (some (-2^7)) (some (2^7)) where
+  toInt_mul x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
 
 -- Note that we can not define `ToInt.Mod` instances for `Int8`,
 -- because the condition does not hold unless `0 ≤ x.toInt ∨ y.toInt ∣ x.toInt ∨ y = 0`.
@@ -248,17 +316,21 @@ instance : ToInt Int16 (some (-2^15)) (some (2^15)) where
 
 @[simp] theorem toInt_int16 (x : Int16) : ToInt.toInt x = (x.toInt : Int) := rfl
 
-instance : ToInt.Add Int16 (some (-2^15)) (some (2^15)) where
-  toInt_add x y := by
-    simp [Int.bmod_eq_emod]
-    split <;> · simp; omega
-
 instance : ToInt.Zero Int16 (some (-2^15)) (some (2^15)) where
   toInt_zero := by
     -- simp -- FIXME: succeeds, but generates a `(kernel) application type mismatch` error!
     change (0 : Int16).toInt = _
     rw [Int16.toInt_zero]
-    decide
+
+instance : ToInt.Add Int16 (some (-2^15)) (some (2^15)) where
+  toInt_add x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
+
+instance : ToInt.Mul Int16 (some (-2^15)) (some (2^15)) where
+  toInt_mul x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
 
 instance : ToInt.LE Int16 (some (-2^15)) (some (2^15)) where
   le_iff x y := by simpa using Int16.le_iff_toInt_le
@@ -274,17 +346,21 @@ instance : ToInt Int32 (some (-2^31)) (some (2^31)) where
 
 @[simp] theorem toInt_int32 (x : Int32) : ToInt.toInt x = (x.toInt : Int) := rfl
 
-instance : ToInt.Add Int32 (some (-2^31)) (some (2^31)) where
-  toInt_add x y := by
-    simp [Int.bmod_eq_emod]
-    split <;> · simp; omega
-
 instance : ToInt.Zero Int32 (some (-2^31)) (some (2^31)) where
   toInt_zero := by
     -- simp -- FIXME: succeeds, but generates a `(kernel) application type mismatch` error!
     change (0 : Int32).toInt = _
     rw [Int32.toInt_zero]
-    decide
+
+instance : ToInt.Add Int32 (some (-2^31)) (some (2^31)) where
+  toInt_add x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
+
+instance : ToInt.Mul Int32 (some (-2^31)) (some (2^31)) where
+  toInt_mul x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
 
 instance : ToInt.LE Int32 (some (-2^31)) (some (2^31)) where
   le_iff x y := by simpa using Int32.le_iff_toInt_le
@@ -300,17 +376,21 @@ instance : ToInt Int64 (some (-2^63)) (some (2^63)) where
 
 @[simp] theorem toInt_int64 (x : Int64) : ToInt.toInt x = (x.toInt : Int) := rfl
 
-instance : ToInt.Add Int64 (some (-2^63)) (some (2^63)) where
-  toInt_add x y := by
-    simp [Int.bmod_eq_emod]
-    split <;> · simp; omega
-
 instance : ToInt.Zero Int64 (some (-2^63)) (some (2^63)) where
   toInt_zero := by
     -- simp -- FIXME: succeeds, but generates a `(kernel) application type mismatch` error!
     change (0 : Int64).toInt = _
     rw [Int64.toInt_zero]
-    decide
+
+instance : ToInt.Add Int64 (some (-2^63)) (some (2^63)) where
+  toInt_add x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
+
+instance : ToInt.Mul Int64 (some (-2^63)) (some (2^63)) where
+  toInt_mul x y := by
+    simp [Int.bmod_eq_emod]
+    split <;> · simp; omega
 
 instance : ToInt.LE Int64 (some (-2^63)) (some (2^63)) where
   le_iff x y := by simpa using Int64.le_iff_toInt_le
@@ -329,14 +409,20 @@ instance : ToInt (BitVec v) (some 0) (some (2^v)) where
 
 @[simp] theorem toInt_bitVec (x : BitVec v) : ToInt.toInt x = (x.toNat : Int) := rfl
 
-instance : ToInt.Add (BitVec v) (some 0) (some (2^v)) where
-  toInt_add x y := by simp
-
 instance : ToInt.Zero (BitVec v) (some 0) (some (2^v)) where
   toInt_zero := by simp
 
+instance : ToInt.Add (BitVec v) (some 0) (some (2^v)) where
+  toInt_add x y := by simp
+
+instance : ToInt.Mul (BitVec v) (some 0) (some (2^v)) where
+  toInt_mul x y := by simp
+
 instance : ToInt.Mod (BitVec v) (some 0) (some (2^v)) where
   toInt_mod x y := by simp
+
+instance : ToInt.Div (BitVec v) (some 0) (some (2^v)) where
+  toInt_div x y := by simp
 
 instance : ToInt.LE (BitVec v) (some 0) (some (2^v)) where
   le_iff x y := by simpa using BitVec.le_def
@@ -352,6 +438,10 @@ instance : ToInt ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.
 
 @[simp] theorem toInt_isize (x : ISize) : ToInt.toInt x = x.toInt := rfl
 
+instance : ToInt.Zero ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.Platform.numBits-1))) where
+  toInt_zero := by
+    rw [toInt_isize, ISize.toInt_zero]
+
 instance : ToInt.Add ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.Platform.numBits-1))) where
   toInt_add x y := by
     rw [toInt_isize, ISize.toInt_add, ToInt.wrap_eq_bmod (Int.pow_nonneg (by decide))]
@@ -364,11 +454,17 @@ instance : ToInt.Add ISize (some (-2^(System.Platform.numBits-1))) (some (2^(Sys
       simp
     simp [p₁, p₂]
 
-instance : ToInt.Zero ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.Platform.numBits-1))) where
-  toInt_zero := by
-    rw [toInt_isize]
-    rw [ISize.toInt_zero, ToInt.wrap_eq_bmod (Int.pow_nonneg (by decide))]
-    simp
+instance : ToInt.Mul ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.Platform.numBits-1))) where
+  toInt_mul x y := by
+    rw [toInt_isize, ISize.toInt_mul, ToInt.wrap_eq_bmod (Int.pow_nonneg (by decide))]
+    have p₁ : (2 : Int) * 2 ^ (System.Platform.numBits - 1) = 2 ^ System.Platform.numBits := by
+      have := System.Platform.numBits_pos
+      have : System.Platform.numBits - 1 + 1 = System.Platform.numBits := by omega
+      simp [← Int.pow_succ', this]
+    have p₂ : ((2 : Int) ^ System.Platform.numBits).toNat = 2 ^ System.Platform.numBits := by
+      rw [Int.toNat_pow_of_nonneg (by decide)]
+      simp
+    simp [p₁, p₂]
 
 instance instToIntLEISize : ToInt.LE ISize (some (-2^(System.Platform.numBits-1))) (some (2^(System.Platform.numBits-1))) where
   le_iff x y := by simpa using ISize.le_iff_toInt_le

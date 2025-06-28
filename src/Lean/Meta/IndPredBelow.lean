9681eb0c0f81439e53e4e71573d5e380adcf4ebd
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-06-28 08:02:29 +0200

    failing snapshot

--- src/Lean/Meta/IndPredBelow.lean
+++ src/Lean/Meta/IndPredBelow.lean
@@ -493,7 +493,7 @@ where
       -- `belowIndices` is a mapping from non-`below` to the `below` version of each field.
       let mut belowFieldOpts := .replicate belowCtor.numFields none
       let fields := fields.toArray
-      for fieldIdx in [:fields.size] do
+      for fieldIdx in *...fields.size do
         belowFieldOpts := belowFieldOpts.set! belowIndices[fieldIdx]! (some fields[fieldIdx]!)
 
       let belowParams := params.toArray.push belowMotive
@@ -592,6 +592,28 @@ def mkBelow (declName : Name) : MetaM Unit := do
       trace[Meta.IndPredBelow] "added {ctx.belowNames}"
       ctx.belowNames.forM Lean.mkCasesOn
       for i in [:ctx.typeInfos.size] do
+        try
+          let decl ← IndPredBelow.mkBrecOnDecl ctx i
+          -- disable async TC so we can catch its exceptions
+          withOptions (Elab.async.set · false) do
+            addDecl decl
+        catch e => trace[Meta.IndPredBelow] "failed to prove brecOn for {ctx.belowNames[i]!}\n{e.toMessageData}"
+    else trace[Meta.IndPredBelow] "Nested or not recursive"
+  else trace[Meta.IndPredBelow] "Not inductive predicate"
+
+/-- Generates the auxiliary lemmas `below` and `brecOn` for a recursive inductive predicate.
+The current generator doesn't support nested predicates, but pattern-matching on them still works
+thanks to well-founded recursion. -/
+def mkBelow' (declName : Name) : MetaM Unit := do
+  if (← isInductivePredicate declName) then
+    let x ← getConstInfoInduct declName
+    if x.isRec && !x.isNested then
+      let ctx ← IndPredBelow.mkContext declName
+      let decl ← IndPredBelow.mkBelowDecl ctx
+      addDecl decl
+      trace[Meta.IndPredBelow] "added {ctx.belowNames}"
+      ctx.belowNames.forM Lean.mkCasesOn
+      for i in *...ctx.typeInfos.size do
         try
           let decl ← IndPredBelow.mkBrecOnDecl ctx i
           -- disable async TC so we can catch its exceptions
--- tests/lean/run/compilerTest1.lean
+++ tests/lean/run/compilerTest1.lean
@@ -1,3 +1,29 @@
 import Lean
 
-run_meta Lean.Compiler.compile #[``Lean.Elab.Structural.structuralRecursion, ``Lean.Elab.Command.elabInductives, ``Lean.Environment.displayStats, ``Lean.Meta.IndPredBelow.mkBelow, ``unexpandExists]
+import Lean.Meta.Constructions.CasesOn
+import Lean.Meta.Match.Match
+import Lean.Meta.Tactic.SolveByElim
+
+namespace Lean.Meta.IndPredBelow
+open Match
+
+-- def mkBelow'' (declName : Name) : MetaM Unit := do
+--   if (← isInductivePredicate declName) then
+--     let x ← getConstInfoInduct declName
+--     if x.isRec && !x.isNested then
+--       let ctx ← IndPredBelow.mkContext declName
+--       let decl ← IndPredBelow.mkBelowDecl ctx
+--       addDecl decl
+--       trace[Meta.IndPredBelow] "added {ctx.belowNames}"
+--       ctx.belowNames.forM Lean.mkCasesOn
+--       for i in *...ctx.typeInfos.size do
+--         try
+--           let decl ← IndPredBelow.mkBrecOnDecl ctx i
+--           -- disable async TC so we can catch its exceptions
+--           withOptions (Elab.async.set · false) do
+--             addDecl decl
+--         catch e => trace[Meta.IndPredBelow] "failed to prove brecOn for {ctx.belowNames[i]!}\n{e.toMessageData}"
+--     else trace[Meta.IndPredBelow] "Nested or not recursive"
+--   else trace[Meta.IndPredBelow] "Not inductive predicate"
+
+run_meta Lean.Compiler.compile #[``Lean.Elab.Structural.structuralRecursion, ``Lean.Elab.Command.elabInductives, ``Lean.Environment.displayStats, ``Lean.Meta.IndPredBelow.mkBelow', ``unexpandExists]

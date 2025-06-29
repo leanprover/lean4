224de51063345c22672f6e7bd0271964034a5ca7
Sebastian Ullrich <sebasti@nullri.ch>
2025-06-29 11:18:36 +0200

    refactor: `module`-ize `Std.Classes`

--- src/Std/Classes/Ord/Basic.lean
+++ src/Std/Classes/Ord/Basic.lean
@@ -3,8 +3,12 @@ Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Himmel, Paul Reichert, Robin Arnez
 -/
+module
+
 prelude
-import Init.Data.Ord
+public import Init.Data.Ord
+
+public section
 
 /-!
 # Type classes related to `Ord`

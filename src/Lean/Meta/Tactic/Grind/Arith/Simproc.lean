1429b082318afa0ea150e2074753873fd42d5fec
Leonardo de Moura <leomoura@amazon.com>
2025-07-01 20:27:24 -0700

    feat: improve simproc

--- src/Lean/Meta/Tactic/Grind/Arith/Simproc.lean
+++ src/Lean/Meta/Tactic/Grind/Arith/Simproc.lean
@@ -10,20 +10,42 @@ import Lean.Meta.Tactic.Simp.Simproc
 
 namespace Lean.Meta.Grind.Arith
 
-/-- Applies `a^(m+n) = a^m * a^n` -/
+private def mkSemiringThm (declName : Name) (α : Expr) : MetaM (Option Expr) := do
+  let some u ← getDecLevel? α | return none
+  let semiring := mkApp (mkConst ``Grind.Semiring [u]) α
+  let .some semiringInst ← trySynthInstance semiring | return none
+  return mkApp2 (mkConst declName [u]) α semiringInst
+
+/--
+Applies `a^(m+n) = a^m * a^n`, `a^0 = 1`, `a^1 = a`.
+
+We do normalize `a^0` and `a^1` when converting expressions into polynomials,
+but we need to normalize them here when for other preprocessing steps such as
+`a / b = a*b⁻¹`. If `b` is of the form `c^1`, it will be treated as an
+atom in the comm ring module.
+-/
 builtin_simproc_decl expandPowAdd (_ ^ _) := fun e => do
   let_expr HPow.hPow α nat α' _ a k := e | return .continue
-  let_expr HAdd.hAdd _ _ _ _ m n := k | return .continue
   let_expr Nat ← nat | return .continue
-  unless (← isDefEq α α') do return .continue
-  let u ← getLevel α
-  let some u ← decLevel? u | return .continue
-  let semiring := mkApp (mkConst ``Grind.Semiring [u]) α
-  let .some semiringInst ← trySynthInstance semiring | return .continue
-  let pwFn := e.appFn!.appFn!
-  let r ← mkMul (mkApp2 pwFn a m) (mkApp2 pwFn a n)
-  let h := mkApp5 (mkConst ``Grind.Semiring.pow_add [u]) α semiringInst a m n
-  return .visit { expr := r, proof? := some h }
+  if let some k ← getNatValue? k then
+    if k == 0 then
+      unless (← isDefEq α α') do return .continue
+      let some h ← mkSemiringThm ``Grind.Semiring.pow_zero α | return .continue
+      let r ← mkNumeral α 1
+      return .done { expr := r, proof? := some (mkApp h a) }
+    else if k == 1 then
+      unless (← isDefEq α α') do return .continue
+      let some h ← mkSemiringThm ``Grind.Semiring.pow_one α | return .continue
+      return .done { expr := a, proof? := some (mkApp h a) }
+    else
+      return .continue
+  else
+    let_expr HAdd.hAdd _ _ _ _ m n := k | return .continue
+    unless (← isDefEq α α') do return .continue
+    let some h ← mkSemiringThm ``Grind.Semiring.pow_add α | return .continue
+    let pwFn := e.appFn!.appFn!
+    let r ← mkMul (mkApp2 pwFn a m) (mkApp2 pwFn a n)
+    return .visit { expr := r, proof? := some (mkApp3 h a m n) }
 
 def addSimproc (s : Simprocs) : CoreM Simprocs := do
   s.add ``expandPowAdd (post := true)

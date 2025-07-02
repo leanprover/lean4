50ebee39e2a0fa09b1b1bb9e983384c6d7df124c
Leni Aniva <v@leni.sh>
2025-07-01 17:49:44 -0700

    feat: Add prohibitUnsafe flag

--- src/Lean/Elab/Eval.lean
+++ src/Lean/Elab/Eval.lean
@@ -11,6 +11,7 @@ namespace Lean.Elab.Term
 open Meta
 
 unsafe def evalTerm (α) (type : Expr) (value : Syntax) (safety := DefinitionSafety.safe) : TermElabM α := withoutModifyingEnv do
+  throwErrorIfUnsafe
   let v ← elabTermEnsuringType value type
   synthesizeSyntheticMVarsNoPostponing
   let v ← instantiateMVars v

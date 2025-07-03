leanprover/lean4

commit 9f06aff83490efa07f9c9f096e8c89266afc9351

feat: optimized division without remainder for `Int` and `Nat` (#8089)
This PR adds optimized division functions for `Int` and `Nat` when the
arguments are known to be divisible (such as when normalizing
rationals). These are backed by the gmp functions `mpz_divexact` and
`mpz_divexact_ui`. See also leanprover-community/batteries#1202.

src/runtime/mpz.cpp
@@ -160,6 +160,12 @@ mpz & mpz::operator*=(unsigned u) { mpz_mul_ui(m_val, m_val, u); return *this; }
 
 mpz & mpz::operator*=(int u) { mpz_mul_si(m_val, m_val, u); return *this; }
 
+mpz mpz::divexact(mpz const & n, mpz const & d) {
+    mpz q;
+    mpz_divexact(q.m_val, n.m_val, d.m_val);
+    return q;
+}
+
 mpz mpz::ediv(mpz const & n, mpz const & d) {
     mpz q;
     mpz_t r;
@@ -755,6 +761,10 @@ mpz & mpz::operator/=(unsigned u) {
 
 mpz & mpz::operator%=(mpz const & o) {
     return rem(o.m_size, o.m_digits);
+}
+
+mpz divexact(mpz const & n, mpz const & d) {
+    return n / d;
 }
 
 mpz mpz::ediv(mpz const & n, mpz const & d) {

src/Init/Data/Int/DivMod/Basic.lean
@@ -125,6 +125,28 @@ theorem ofNat_ediv_negSucc {a b : Nat} : (ofNat a / (-[b+1])) = -(a / (b + 1) : 
 theorem negSucc_emod_ofNat {a b : Nat} : -[a+1] % (b : Int) = subNatNat b (succ (a % b)) := rfl
 theorem negSucc_emod_negSucc {a b : Nat} : -[a+1] % -[b+1] = subNatNat (b + 1) (succ (a % (b + 1))) := rfl
 
+/--
+Division of two divisible integers. Division by `0` returns `0`.
+
+This operation uses an optimized implementation, specialized for two divisible integers.
+
+This function is overridden at runtime with an efficient implementation. This definition is
+the logical model.
+
+Examples:
+ * `Int.divExact 21 3 (by decide) = 7`
+ * `Int.divExact 21 (-3) (by decide) = -7`
+ * `Int.divExact (-15) 5 (by decide) = -3`
+ * `Int.divExact 0 22 (by decide) = 0`
+ * `Int.divExact 0 0 (by decide) = 0`
+-/
+@[extern "lean_int_div_exact"]
+protected def divExact (x y : @& Int) (h : y ∣ x) : Int :=
+  x / y
+
+@[simp]
+theorem divExact_eq_ediv {x y : Int} (h : y ∣ x) : x.divExact y h = x / y := rfl
+
 /-! ### T-rounding division -/
 
 /--

src/Init/Data/Int/DivMod/Lemmas.lean
@@ -222,6 +222,9 @@ theorem ediv_eq_tdiv {a b : Int} :
     a / b = a.tdiv b - if 0 ≤ a ∨ b ∣ a then 0 else sign b := by
   simp [tdiv_eq_ediv]
 
+theorem divExact_eq_tdiv {a b : Int} (h : b ∣ a) : a.divExact b h = a.tdiv b := by
+  simp [ediv_eq_tdiv, h]
+
 theorem fdiv_eq_ediv_of_nonneg : ∀ (a : Int) {b : Int}, 0 ≤ b → fdiv a b = a / b
   | 0, _, _ | -[_+1], 0, _ => by simp
   | succ _, ofNat _, _ | -[_+1], succ _, _ => rfl
@@ -251,6 +254,9 @@ theorem fdiv_eq_ediv {a b : Int} :
 theorem ediv_eq_fdiv {a b : Int} :
     a / b = a.fdiv b + if 0 ≤ b ∨ b ∣ a then 0 else 1 := by
   simp [fdiv_eq_ediv]
+
+theorem divExact_eq_fdiv {a b : Int} (h : b ∣ a) : a.divExact b h = a.fdiv b := by
+  simp [ediv_eq_fdiv, h]
 
 theorem fdiv_eq_tdiv_of_nonneg {a b : Int} (Ha : 0 ≤ a) (Hb : 0 ≤ b) : fdiv a b = tdiv a b :=
   tdiv_eq_ediv_of_nonneg Ha ▸ fdiv_eq_ediv_of_nonneg _ Hb
@@ -2657,6 +2663,16 @@ theorem bmod_eq_zero_of_dvd : {a : Nat} → {b : Int} → (h : (a : Int) ∣ b) 
 
 theorem dvd_iff_bmod_eq_zero {a : Nat} {b : Int} : (a : Int) ∣ b ↔ b.bmod a = 0 :=
   ⟨bmod_eq_zero_of_dvd, dvd_of_bmod_eq_zero⟩
+
+theorem divExact_eq_bdiv {a : Int} {b : Nat} (h : (b : Int) ∣ a) : a.divExact b h = a.bdiv b := by
+  have := bdiv_add_bmod a b
+  rw [bmod_eq_zero_of_dvd h, Int.add_zero] at this
+  rw [divExact_eq_ediv]
+  conv => lhs; rw [← this]
+  by_cases h' : (b : Int) = 0
+  · cases h'
+    simp_all
+  · rw [mul_ediv_cancel_left _ h']
 
 theorem le_bmod {x : Int} {m : Nat} (h : 0 < m) : - (m/2) ≤ Int.bmod x m := by
   dsimp [bmod]

src/Init/Data/Nat/Div/Basic.lean
@@ -130,6 +130,26 @@ theorem div_lt_self {n k : Nat} (hLtN : 0 < n) (hLtK : 1 < k) : n / k < n := by
     exact Nat.lt_of_lt_of_le (Nat.add_lt_add_left hLtK _) this
 
 /--
+Division of two divisible natural numbers. Division by `0` returns `0`.
+
+This operation uses an optimized implementation, specialized for two divisible natural numbers.
+
+This function is overridden at runtime with an efficient implementation. This definition is
+the logical model.
+
+Examples:
+ * `Nat.divExact 21 3 (by decide) = 7`
+ * `Nat.divExact 0 22 (by decide) = 0`
+ * `Nat.divExact 0 0 (by decide) = 0`
+-/
+@[extern "lean_nat_div_exact"]
+protected def divExact (x y : @& Nat) (h : y ∣ x) : Nat :=
+  x / y
+
+@[simp]
+theorem divExact_eq_div {x y : Nat} (h : y ∣ x) : x.divExact y h = x / y := rfl
+
+/--
 The modulo operator, which computes the remainder when dividing one natural number by another.
 Usually accessed via the `%` operator. When the divisor is `0`, the result is the dividend rather
 than an error.

src/include/lean/lean.h
@@ -1242,6 +1242,7 @@ LEAN_EXPORT lean_object * lean_nat_big_sub(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_nat_big_mul(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_nat_overflow_mul(size_t a1, size_t a2);
 LEAN_EXPORT lean_object * lean_nat_big_div(lean_object * a1, lean_object * a2);
+LEAN_EXPORT lean_object * lean_nat_big_div_exact(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_nat_big_mod(lean_object * a1, lean_object * a2);
 LEAN_EXPORT bool lean_nat_big_eq(lean_object * a1, lean_object * a2);
 LEAN_EXPORT bool lean_nat_big_le(lean_object * a1, lean_object * a2);
@@ -1322,6 +1323,20 @@ static inline lean_obj_res lean_nat_div(b_lean_obj_arg a1, b_lean_obj_arg a2) {
             return lean_box(n1 / n2);
     } else {
         return lean_nat_big_div(a1, a2);
+    }
+}
+
+// assumes that a1 % a2 = 0
+static inline lean_obj_res lean_nat_div_exact(b_lean_obj_arg a1, b_lean_obj_arg a2) {
+    if (LEAN_LIKELY(lean_is_scalar(a1) && lean_is_scalar(a2))) {
+        size_t n1 = lean_unbox(a1);
+        size_t n2 = lean_unbox(a2);
+        if (n2 == 0)
+            return lean_box(0);
+        else
+            return lean_box(n1 / n2);
+    } else {
+        return lean_nat_big_div_exact(a1, a2);
     }
 }
 
@@ -1423,6 +1438,7 @@ LEAN_EXPORT lean_object * lean_int_big_add(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_sub(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_mul(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_div(lean_object * a1, lean_object * a2);
+LEAN_EXPORT lean_object * lean_int_big_div_exact(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_mod(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_ediv(lean_object * a1, lean_object * a2);
 LEAN_EXPORT lean_object * lean_int_big_emod(lean_object * a1, lean_object * a2);
@@ -1540,6 +1556,30 @@ static inline lean_obj_res lean_int_div(b_lean_obj_arg a1, b_lean_obj_arg a2) {
         }
     } else {
         return lean_int_big_div(a1, a2);
+    }
+}
+
+static inline lean_obj_res lean_int_div_exact(b_lean_obj_arg a1, b_lean_obj_arg a2) {
+    if (LEAN_LIKELY(lean_is_scalar(a1) && lean_is_scalar(a2))) {
+        if (sizeof(void*) == 8) {
+            /* 64-bit version, we use 64-bit numbers to avoid overflow when v1 == LEAN_MIN_SMALL_INT. */
+            int64_t v1 = lean_scalar_to_int(a1);
+            int64_t v2 = lean_scalar_to_int(a2);
+            if (v2 == 0)
+                return lean_box(0);
+            else
+                return lean_int64_to_int(v1 / v2);
+        } else {
+            /* 32-bit version */
+            int v1 = lean_scalar_to_int(a1);
+            int v2 = lean_scalar_to_int(a2);
+            if (v2 == 0)
+                return lean_box(0);
+            else
+                return lean_int_to_int(v1 / v2);
+        }
+    } else {
+        return lean_int_big_div_exact(a1, a2);
     }
 }
 

src/runtime/mpz.h
@@ -245,6 +245,8 @@ public:
 
     friend mpz operator%(mpz a, mpz const & b) { return a %= b; }
 
+    static mpz divexact(mpz const & n, mpz const & d);
+
     static mpz ediv(mpz const & n, mpz const & d);
     static mpz ediv(int n, mpz const & d) { return ediv(mpz(n), d); }
     static mpz ediv(mpz const& n, int d) { return ediv(n, mpz(d)); }

src/runtime/object.cpp
@@ -1345,6 +1345,22 @@ extern "C" LEAN_EXPORT object * lean_nat_big_div(object * a1, object * a2) {
     }
 }
 
+extern "C" LEAN_EXPORT object * lean_nat_big_div_exact(object * a1, object * a2) {
+    lean_assert(!lean_is_scalar(a1) || !lean_is_scalar(a2));
+    if (lean_is_scalar(a1)) {
+        lean_assert(a1 == lean_box(0));
+        lean_assert(mpz_value(a2) != 0);
+        return lean_box(0);
+    } else if (lean_is_scalar(a2)) {
+        usize n2 = lean_unbox(a2);
+        lean_assert(n2 != 0);
+        return mpz_to_nat(mpz::divexact(mpz_value(a1), mpz::of_size_t(n2)));
+    } else {
+        lean_assert(mpz_value(a2) != 0);
+        return mpz_to_nat(mpz::divexact(mpz_value(a1), mpz_value(a2)));
+    }
+}
+
 extern "C" LEAN_EXPORT object * lean_nat_big_mod(object * a1, object * a2) {
     lean_assert(!lean_is_scalar(a1) || !lean_is_scalar(a2));
     if (lean_is_scalar(a1)) {
@@ -1590,6 +1606,24 @@ extern "C" LEAN_EXPORT object * lean_int_big_div(object * a1, object * a2) {
             return mpz_to_int(mpz_value(a1) / d);
     } else {
         return mpz_to_int(mpz_value(a1) / mpz_value(a2));
+    }
+}
+
+extern "C" LEAN_EXPORT object * lean_int_big_div_exact(object * a1, object * a2) {
+    lean_assert(!lean_is_scalar(a1) || !lean_is_scalar(a2));
+    if (lean_is_scalar(a1)) {
+        // a1 is scalar, a2 isn't but a2 divides a1
+        // two possibilities:
+        // 1. a1 = 0 -> return 0 or
+        // 2. a1 = LEAN_MIN_SMALL_INT and a2 = LEAN_MAX_SMALL_INT + 1 = -a1 -> return -1
+        int n = lean_scalar_to_int(a1);
+        return n == 0 ? a1 : lean_box(static_cast<unsigned>(-1));
+    } else if (lean_is_scalar(a2)) {
+        int d = lean_scalar_to_int(a2);
+        lean_assert(d != 0);
+        return mpz_to_int(mpz::divexact(mpz_value(a1), mpz(d)));
+    } else {
+        return mpz_to_int(mpz::divexact(mpz_value(a1), mpz_value(a2)));
     }
 }
 

tests/lean/run/divExact.lean
@@ -1,0 +1,25 @@
+#guard Nat.divExact 4 2 (by decide) == 2
+#guard Nat.divExact 0 0 (by decide) == 0
+#guard Nat.divExact 0 2 (by decide) == 0
+#guard Nat.divExact 0 (2 ^ 100) (by decide) == 0
+#guard Nat.divExact (2 ^ 100) (2 ^ 100) (by decide) == 1
+#guard Nat.divExact (2 ^ 100) 4 (by decide) == 2 ^ 98
+#guard Nat.divExact (3 ^ 200) (3 ^ 100) (by decide) == 3 ^ 100
+#guard Nat.divExact (2 ^ 100) (2 ^ 60) (by decide) == 2 ^ 40
+
+#guard Int.divExact 4 2 (by decide) == 2
+#guard Int.divExact 0 0 (by decide) == 0
+#guard Int.divExact 0 2 (by decide) == 0
+#guard Int.divExact 0 (2 ^ 100 : Nat) (by decide) == 0
+#guard Int.divExact (2 ^ 100 : Nat) (2 ^ 100 : Nat) (by decide) == 1
+#guard Int.divExact (2 ^ 100 : Nat) 4 (by decide) == 2 ^ 98
+#guard Int.divExact (3 ^ 200 : Nat) (3 ^ 100 : Nat) (by decide) == 3 ^ 100
+
+#guard Int.divExact 4 (-2) (by decide) == -2
+#guard Int.divExact (-8) (-2) (by decide) == 4
+#guard Int.divExact 0 (-2) (by decide) == 0
+#guard Int.divExact 0 (-(2 ^ 100 : Nat)) (by decide) == 0
+#guard Int.divExact (2 ^ 100 : Nat) (-(2 ^ 100 : Nat)) (by decide) == -1
+#guard Int.divExact (-(3 ^ 200 : Nat)) (-(3 ^ 100 : Nat)) (by decide) == 3 ^ 100
+#guard Int.divExact (-(3 ^ 100 : Nat)) 3 (by decide) == -3 ^ 99
+#guard Int.divExact (-(2 ^ 31 : Nat)) (2 ^ 31 : Nat) (by decide) == -1

commit 0106ca3becc3bca72ee4dab9ea3198989b1ed7f5

fix: undefined symbol without `LEAN_USE_GMP` (#9106)
This PR fixes `undefined symbol: lean::mpz::divexact(lean::mpz const&,
lean::mpz const&)` when building without `LEAN_USE_GMP`

This fixes a regression in #8089

src/runtime/mpz.cpp
@@ -763,7 +763,7 @@ mpz & mpz::operator%=(mpz const & o) {
     return rem(o.m_size, o.m_digits);
 }
 
-mpz divexact(mpz const & n, mpz const & d) {
+mpz mpz::divexact(mpz const & n, mpz const & d) {
     return n / d;
 }
 
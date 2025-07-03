1b765a0d9464f32a8eb3b43e9e6e624704839d58
Leonardo de Moura <leomoura@amazon.com>
2025-07-03 14:51:37 -0700

    chore: reduce LE.le and LT.lt priorities

--- src/Init/Grind/Tactics.lean
+++ src/Init/Grind/Tactics.lean
@@ -419,6 +419,5 @@ Sets symbol priorities for the E-matching pattern inference procedure used in `g
 attribute [grind symbol 0] Eq HEq Iff And Or Not
 -- The following symbols are only used as the root pattern symbol if there isn't another option
 attribute [grind symbol low] HAdd.hAdd HSub.hSub HMul.hMul Dvd.dvd HDiv.hDiv HMod.hMod
--- TODO: we want to mark LT.lt LE.le as `low` too, but we have theorems such as `some a ≤ some b ↔ a ≤ b` where
--- we don't want the multi-pattern `some a, some b` being selected.
+attribute [grind symbol low] LT.lt LE.le
 end Lean.Parser.Tactic

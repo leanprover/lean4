a05311d1ec7bc1005a5d9565111a9e376e370f78
Cameron Zwarich <cameron@lean-fro.org>
2025-07-08 12:49:27 -0700

    chore: split IR types out into their own header file (#9264)

--- src/library/compiler/ir.h
+++ src/library/compiler/ir.h
@@ -8,30 +8,9 @@ Author: Leonardo de Moura
 #include <string>
 #include "library/elab_environment.h"
 #include "library/compiler/util.h"
+#include "library/ir_types.h"
 namespace lean {
 namespace ir {
-/*
-inductive IRType
-| float | uint8 | uint16 | uint32 | uint64 | usize
-| irrelevant | object | tobject
-| float32
-| struct (leanTypeName : Option Name) (types : Array IRType) : IRType
-| union (leanTypeName : Name) (types : Array IRType) : IRType
-
-Remark: we don't create struct/union types from C++.
-*/
-enum class type { Float, UInt8, UInt16, UInt32, UInt64, USize, Irrelevant, Object, TObject, Float32, Struct, Union };
-
-typedef nat        var_id;
-typedef nat        jp_id;
-typedef name       fun_id;
-typedef object_ref arg;
-typedef object_ref expr;
-typedef object_ref param;
-typedef object_ref fn_body;
-typedef object_ref alt;
-typedef object_ref decl;
-
 typedef object_ref decl;
 std::string decl_to_string(decl const & d);
 void test(decl const & d);

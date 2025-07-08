76d3b0e02298b23f99042b8ff5ced943301449ff
Leonardo de Moura <leomoura@amazon.com>
2025-07-08 13:26:01 -0700

    feat: mark nested `Decidable` terms

--- src/Lean/Meta/Tactic/Grind/MarkNestedSubsingletons.lean
+++ src/Lean/Meta/Tactic/Grind/MarkNestedSubsingletons.lean
@@ -16,6 +16,19 @@ namespace Lean.Meta.Grind
 
 private abbrev M := StateRefT (Std.HashMap ExprPtr Expr) MetaM
 
+def isMarkedSubsingletonConst (e : Expr) : Bool := Id.run do
+  let .const declName _ := e | false
+  return declName == ``Grind.nestedProof || declName == ``Grind.nestedDecidable
+
+def isMarkedSubsingletonApp (e : Expr) : Bool :=
+  isMarkedSubsingletonConst e.getAppFn
+
+/-- Returns `some p` if `e` is of the form `Decidable p` -/
+private def isDecidable (e : Expr) : MetaM (Option Expr) := do
+  match_expr (← whnfCore e) with
+  | Decidable p => return some p
+  | _ => return none
+
 /--
 Wrap nested proofs and decidable instances in `e` with `Lean.Grind.nestedProof` and `Lean.Grind.nestedDecidable`-applications.
 Recall that the congruence closure module has special support for them.
@@ -26,21 +39,25 @@ partial def markNestedSubsingletons (e : Expr) : MetaM Expr := do
   visit e |>.run' {}
 where
   visit (e : Expr) : M Expr := do
-    if (← isProof e) then
-      if e.isAppOf ``Lean.Grind.nestedProof then
-        return e -- `e` is already marked
-      if let some r := (← get).get? { expr := e } then
-        return r
-      let e' ← markNestedProof e
+    if isMarkedSubsingletonApp e then
+      return e -- `e` is already marked
+    -- check whether result is cached
+    if let some r := (← get).get? { expr := e } then
+      return r
+    -- check whether `e` is a proposition or `Decidable`
+    let type ← inferType e
+    if (← isProp type) then
+      let e' := mkApp2 (mkConst ``Grind.nestedProof) (← preprocess type) e
+      modify fun s => s.insert { expr := e } e'
+      return e'
+    if let some p ← isDecidable type then
+      let e' := mkApp2 (mkConst ``Grind.nestedDecidable) (← preprocess p) e
       modify fun s => s.insert { expr := e } e'
       return e'
     -- Remark: we have to process `Expr.proj` since we only
     -- fold projections later during term internalization
     unless e.isApp || e.isForall || e.isProj || e.isMData do
       return e
-    -- Check whether it is cached
-    if let some r := (← get).get? { expr := e } then
-      return r
     let e' ← match e with
       | .app .. => e.withApp fun f args => do
         let mut modified := false
@@ -85,18 +102,18 @@ where
     let e ← foldProjs e
     normalizeLevels e
 
-  markNestedProof (e : Expr) : M Expr := do
-    let prop ← inferType e
-    let prop ← preprocess prop
-    return mkApp2 (mkConst ``Lean.Grind.nestedProof) prop e
+def markNestedProof (e : Expr) : M Expr := do
+  let prop ← inferType e
+  let prop ← markNestedSubsingletons.preprocess prop
+  return mkApp2 (mkConst ``Grind.nestedProof) prop e
 
 /--
 Given a proof `e`, mark it with `Lean.Grind.nestedProof`
 -/
 def markProof (e : Expr) : MetaM Expr := do
-  if e.isAppOf ``Lean.Grind.nestedProof then
+  if e.isAppOf ``Grind.nestedProof then
     return e -- `e` is already marked
   else
-    unsafe markNestedSubsingletons.markNestedProof e |>.run' {}
+    markNestedProof e |>.run' {}
 
 end Lean.Meta.Grind

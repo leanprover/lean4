8cb5d5ba91bd7ff9f6506312aadf7cf791f37f85
Leonardo de Moura <leomoura@amazon.com>
2025-07-08 12:46:06 -0700

    refactor: cleanup `MarkNestedProofs.lean`

--- src/Lean/Meta/Tactic/Grind/MarkNestedProofs.lean
+++ src/Lean/Meta/Tactic/Grind/MarkNestedProofs.lean
@@ -9,61 +9,48 @@ import Lean.Util.PtrSet
 import Lean.Meta.Transform
 import Lean.Meta.Basic
 import Lean.Meta.InferType
+import Lean.Meta.Tactic.Grind.ExprPtr
 import Lean.Meta.Tactic.Grind.Util
 
 namespace Lean.Meta.Grind
 
-private unsafe def markNestedProofImpl (e : Expr) (visit : Expr → StateRefT (PtrMap Expr Expr) MetaM Expr)
-    : StateRefT (PtrMap Expr Expr) MetaM Expr := do
-  let prop ← inferType e
-  /-
-  We must unfold reducible constants occurring in `prop` because the congruence closure
-  module in `grind` assumes they have been expanded.
-  See `grind_mark_nested_proofs_bug.lean` for an example.
-  TODO: We may have to normalize `prop` too.
-  -/
-  /- We must also apply beta-reduction to improve the effectiveness of the congruence closure procedure. -/
-  let prop ← Core.betaReduce prop
-  let prop ← unfoldReducible prop
-  /- We must mask proofs occurring in `prop` too. -/
-  let prop ← visit prop
-  let prop ← eraseIrrelevantMData prop
-  /- We must fold kernel projections like it is done in the preprocessor. -/
-  let prop ← foldProjs prop
-  let prop ← normalizeLevels prop
-  return mkApp2 (mkConst ``Lean.Grind.nestedProof) prop e
+private abbrev M := StateRefT (Std.HashMap ExprPtr Expr) MetaM
 
-unsafe def markNestedProofsImpl (e : Expr) : MetaM Expr := do
-  visit e |>.run' mkPtrMap
+/--
+Wrap nested proofs `e` with `Lean.Grind.nestedProof`-applications.
+Recall that the congruence closure module has special support for `Lean.Grind.nestedProof`.
+-/
+partial def markNestedProofs (e : Expr) : MetaM Expr := do
+  visit e |>.run' {}
 where
-  visit (e : Expr) : StateRefT (PtrMap Expr Expr) MetaM Expr := do
+  visit (e : Expr) : M Expr := do
     if (← isProof e) then
       if e.isAppOf ``Lean.Grind.nestedProof then
         return e -- `e` is already marked
-      if let some r := (← get).find? e then
+      if let some r := (← get).get? { expr := e } then
         return r
-      let e' ← markNestedProofImpl e visit
-      modify fun s => s.insert e e'
+      let e' ← markNestedProof e
+      modify fun s => s.insert { expr := e } e'
       return e'
     -- Remark: we have to process `Expr.proj` since we only
     -- fold projections later during term internalization
     unless e.isApp || e.isForall || e.isProj || e.isMData do
       return e
     -- Check whether it is cached
-    if let some r := (← get).find? e then
+    if let some r := (← get).get? { expr := e } then
       return r
     let e' ← match e with
       | .app .. => e.withApp fun f args => do
         let mut modified := false
-        let mut args := args
-        for i in *...args.size do
-          let arg := args[i]!
+        let mut args := args.toVector
+        for h : i in *...args.size do
+          let arg := args[i]
           let arg' ← visit arg
-          unless ptrEq arg arg' do
-            args := args.set! i arg'
+          unless isSameExpr arg arg' do
+            args := args.set i arg'
             modified := true
         if modified then
-          pure <| mkAppN f args
+          pure <| mkAppN f args.toArray
         else
           pure e
       | .proj _ _ b =>
@@ -76,15 +63,30 @@ where
         let b' ← if b.hasLooseBVars then pure b else visit b
         pure <| e.updateForallE! d' b'
       | _ => unreachable!
-    modify fun s => s.insert e e'
+    modify fun s => s.insert { expr := e } e'
     return e'
 
-/--
-Wrap nested proofs `e` with `Lean.Grind.nestedProof`-applications.
-Recall that the congruence closure module has special support for `Lean.Grind.nestedProof`.
--/
-def markNestedProofs (e : Expr) : MetaM Expr :=
-  unsafe markNestedProofsImpl e
+  preprocess (e : Expr) : M Expr := do
+    /-
+    We must unfold reducible constants occurring in `prop` because the congruence closure
+    module in `grind` assumes they have been expanded.
+    See `grind_mark_nested_proofs_bug.lean` for an example.
+    TODO: We may have to normalize `prop` too.
+    -/
+    /- We must also apply beta-reduction to improve the effectiveness of the congruence closure procedure. -/
+    let e ← Core.betaReduce e
+    let e ← unfoldReducible e
+    /- We must mask proofs occurring in `prop` too. -/
+    let e ← visit e
+    let e ← eraseIrrelevantMData e
+    /- We must fold kernel projections like it is done in the preprocessor. -/
+    let e ← foldProjs e
+    normalizeLevels e
+
+  markNestedProof (e : Expr) : M Expr := do
+    let prop ← inferType e
+    let prop ← preprocess prop
+    return mkApp2 (mkConst ``Lean.Grind.nestedProof) prop e
 
 /--
 Given a proof `e`, mark it with `Lean.Grind.nestedProof`
@@ -93,6 +95,6 @@ def markProof (e : Expr) : MetaM Expr := do
   if e.isAppOf ``Lean.Grind.nestedProof then
     return e -- `e` is already marked
   else
-    unsafe markNestedProofImpl e markNestedProofsImpl.visit |>.run' mkPtrMap
+    unsafe markNestedProofs.markNestedProof e |>.run' {}
 
 end Lean.Meta.Grind

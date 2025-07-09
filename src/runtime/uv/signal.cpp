00590c727498951a70b5403c059bdd33618e26dd
Sofia Rodrigues <sofia@algebraic.dev>
2025-07-09 12:36:15 -0300

    fix: change header
    Co-authored-by: Markus Himmel <markus@lean-fro.org>

--- src/runtime/uv/signal.cpp
+++ src/runtime/uv/signal.cpp
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Sofia Rodrigues
 */
 
-#include "runtime/uv/udp.h"
+#include "runtime/uv/signal.h"
 
 namespace lean {
 #ifndef LEAN_EMSCRIPTEN

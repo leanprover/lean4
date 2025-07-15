5ae6e38b24308cab1d7f3524bf7365f0fdd54346
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-07-15 20:59:43 +0200

    feat: add additional hints and reorganize

--- src/Lean/Elab/Tactic/Split.lean
+++ src/Lean/Elab/Tactic/Split.lean
@@ -13,44 +13,21 @@ namespace Lean.Elab.Tactic
 open Meta
 
 @[builtin_tactic Lean.Parser.Tactic.split] def evalSplit : Tactic := fun stx => do
-  let traceHint : MessageData := .ofLazyM do
-    if ! (← getBoolOption `trace.split.failure) then
-      return .hint' m!"Use `set_option trace.split.failure true` to display additional diagnostic information."
-    else return .nil
   if let `(tactic|split $t:term $[at $loc]?) := stx then
-    withMainContext do
-      let mut error := m!"Tactic `split` failed: Specifying a term to split is not supported yet"
-      if t.raw.isIdent then
-        let name := t.raw.getId
-        let lctx ← getLCtx
-        if name.isStr && loc.isNone then
-          if let some decl := lctx.findFromUserName? name then
-            let e := decl.toExpr
-            let tgtHint ← MessageData.hint
-              m!"If you intended to apply `split` at the hypothesis `{e}`, use `split at {e}`:"
-              #[{ suggestion := (← `(tactic|split at $(mkIdent name):ident)) }]
-            error := error ++ tgtHint
-      throwErrorAt stx error
+    throwTermUnsupported t loc
   let loc := expandOptLocation stx[2]
   match loc with
   | Location.targets hyps simplifyTarget =>
     if (hyps.size > 0 && simplifyTarget) || hyps.size > 1 then
-      let goalStr := if simplifyTarget then "the goal and " else ""
-      let hypsStr :=
-        (if hyps.size == 1 then m!"hypothesis" else m!"hypotheses") ++
-        MessageData.andList (hyps.toList.map (m!"`{·}`"))
-      let hint := .hint' m!"Specify a single target to split, or use `split at *` to split the first target that can be automatically split"
-      throwErrorAt stx[2] m!"Tactic `split` failed: Specifying multiple targets ({goalStr ++ hypsStr}) is not supported" ++ hint
+      throwMultipleLocationsAt stx[2] simplifyTarget hyps
     if simplifyTarget then
       liftMetaTactic fun mvarId => do
-       let some mvarIds ← splitTarget? mvarId
-        | Meta.throwTacticEx `split mvarId <| m!"Could not split an `if` or `match` expression in the goal" ++ traceHint
+       let some mvarIds ← splitTarget? mvarId | throwCouldNotSplitGoal mvarId
         return mvarIds
     else
       let fvarId ← getFVarId hyps[0]!
       liftMetaTactic fun mvarId => do
-        let some mvarIds ← splitLocalDecl? mvarId fvarId
-          | Meta.throwTacticEx `split mvarId <| m!"Could not split an `if` or `match` expression in the type of `{Expr.fvar fvarId}`" ++ traceHint
+        let some mvarIds ← splitLocalDecl? mvarId fvarId | throwCouldNotSplitHyp fvarId mvarId
         return mvarIds
   | Location.wildcard =>
     liftMetaTactic fun mvarId => do
@@ -59,17 +36,73 @@ open Meta
         if let some mvarIds ← splitLocalDecl? mvarId fvarId then
           return mvarIds
       let some mvarIds ← splitTarget? mvarId
-        | let splitAtNoteText := "It may still be possible to manually split a hypothesis using `split at`"
-          if fvarIds.size > 0 then
-            let fvarMsgs := fvarIds.toList.map (m!"`{Expr.fvar ·}`")
-            let fvarMsgs := MessageData.joinSep fvarMsgs ", "
-            let splitAtNoteText := s!"`split at *` does not attempt to split at non-propositional hypotheses or those on which other hypotheses depend. {splitAtNoteText}."
-            Meta.throwTacticEx `split mvarId <|
-              m!"Could not split the goal or any of the following hypotheses: {fvarMsgs}." ++ .note splitAtNoteText ++ traceHint
-          else
-            Meta.throwTacticEx `split mvarId <|
-              m!"Could not split the goal, and no hypotheses that could be automatically split were found." ++ splitAtNoteText ++ traceHint
-
+        | throwCouldNotSplitWildcard fvarIds mvarId
       return mvarIds
+where
+  traceHint : MessageData := .ofLazyM do
+    if ! (← getBoolOption `trace.split.failure) then
+      return .hint' m!"Use `set_option trace.split.failure true` to display additional diagnostic information"
+    else return .nil
+
+  throwTermUnsupported (t : Term) (loc : Option Syntax) := do
+    withMainContext do
+      let mut error := m!"Tactic `split` failed: Specifying a term to split is not supported yet"
+      if t.raw.isIdent then
+        let name := t.raw.getId
+        let lctx ← getLCtx
+        if name.isStr && loc.isNone then
+          if let some decl := lctx.findFromUserName? name then
+            let e := decl.toExpr
+            let hint ← MessageData.hint
+              m!"To apply `split` at the hypothesis `{e}`, use `split at {e}`:"
+              #[{ suggestion := (← `(tactic|split at $(mkIdent name):ident)) }]
+            error := error ++ hint
+      throwErrorAt t error
+
+  throwMultipleLocationsAt (stx : Syntax) (simplifyTarget : Bool) (hyps : Array Syntax) {α} : MetaM α := do
+    let goalStr := if simplifyTarget then "the goal and " else ""
+    let hypsStr :=
+      (if hyps.size == 1 then m!"hypothesis" else m!"hypotheses") ++
+      MessageData.andList (hyps.toList.map (m!"`{·}`"))
+    throwErrorAt stx m!"Tactic `split` failed: Specifying multiple targets ({goalStr ++ hypsStr}) is not supported"
+      ++ .hint' m!"Specify a single target to split, or use `split at *` to split the first target that can be automatically split"
+
+  throwCouldNotSplitGoal (mvarId : MVarId) := do
+    Meta.throwTacticEx `split mvarId <| m!"Could not split an `if` or `match` expression in the goal"
+      ++ (← mkCasesHint <$> mvarId.getType) ++ traceHint
+
+  throwCouldNotSplitHyp (fvarId : FVarId) (mvarId: MVarId) := do
+    let fvarType ← fvarId.getType
+    Meta.throwTacticEx `split mvarId <| m!"Could not split an `if` or `match` expression in \
+      the type{inlineExpr fvarType}of `{Expr.fvar fvarId}`" ++ mkCasesHint fvarType ++ traceHint
+
+  throwCouldNotSplitWildcard {α} (fvarIds : Array FVarId) (mvarId : MVarId) : MetaM α := do
+    let note := .note "`split at *` does not attempt to split at non-propositional hypotheses \
+      or those on which other hypotheses depend. It may still be possible to manually split a \
+      hypothesis using `split at`"
+    if fvarIds.size > 0 then
+      let fvarMsgs := fvarIds.toList.map (m!"`{Expr.fvar ·}`")
+      let fvarMsgs := MessageData.joinSep fvarMsgs ", "
+      Meta.throwTacticEx `split mvarId <|
+        m!"Could not split the goal or any of the following hypotheses: {fvarMsgs}" ++ note ++ traceHint
+    else
+      Meta.throwTacticEx `split mvarId <|
+        m!"Could not split the goal, and no hypotheses that could be automatically split were found" ++ note ++ traceHint
+
+  mkCasesHint (type : Expr) : MessageData := MessageData.ofLazyM do
+    let isFnStructureConst (t : Expr) : MetaM Bool :=
+      if let .const c _ := t.getAppFn then
+        (isStructureLike · c) <$> getEnv
+      else
+        pure false
+    let kind? ←
+      if type.isAppOf ``And then pure <| some "conjunction"
+      else if type.isAppOf ``Prod then pure <| some "pair"
+      else if (← isFnStructureConst type.getAppFn) then pure <| some "structure"
+      else pure <| none
+    if let some kind := kind? then
+      return .hint' m!"To destruct a {kind}, use the `cases` tactic"
+    else
+      return .nil
 
 end Lean.Elab.Tactic
--- tests/lean/run/splitErrors.lean
+++ tests/lean/run/splitErrors.lean
@@ -1,7 +1,87 @@
+/-! # `split` tactic errors
+
+Assesses error messages and hints produced by the `split` tactic.
+-/
+
+/--
+error: Tactic `split` failed: Specifying a term to split is not supported yet
+
+Hint: To apply `split` at the hypothesis `h`, use `split at h`:
+  split a̲t̲ ̲h
+-/
+#guard_msgs in
 example : False := by
   have h : if True then True else True := trivial
   split h
 
+/-- error: Tactic `split` failed: Specifying a term to split is not supported yet -/
+#guard_msgs in
+example (x y : Bool) : (if x && y then 1 else 2) > (if x || y then 0 else 1) := by
+  split x && y
+
+/--
+error: tactic 'split' failed, Could not split an `if` or `match` expression in the goal
+
+Hint: Use `set_option trace.split.failure true` to display additional diagnostic information
+⊢ False
+-/
+#guard_msgs in
+example : False := by
+  split
+
+/--
+error: tactic 'split' failed, Could not split the goal or any of the following hypotheses: `h1`
+
+Note: `split at *` does not attempt to split at non-propositional hypotheses or those on which other hypotheses depend. It may still be possible to manually split a hypothesis using `split at`
+
+Hint: Use `set_option trace.split.failure true` to display additional diagnostic information
+h1 : True
+⊢ False
+-/
+#guard_msgs in
+example : False := by
+  have h1 : True := trivial
+  split at *
+
+/--
+error: tactic 'split' failed, Could not split an `if` or `match` expression in the type `p ∧ q` of `h`
+
+Hint: To destruct a conjunction, use the `cases` tactic
+
+Hint: Use `set_option trace.split.failure true` to display additional diagnostic information
+p q : Prop
+h : p ∧ q
+⊢ p
+-/
+#guard_msgs in
+example {p q : Prop} (h : p ∧ q) : p := by
+  split at h
+
+/--
+error: tactic 'split' failed, Could not split an `if` or `match` expression in the type `Equivalence R` of `h`
+
+Hint: To destruct a structure, use the `cases` tactic
+
+Hint: Use `set_option trace.split.failure true` to display additional diagnostic information
+α : Sort u_1
+R : α → α → Prop
+h : Equivalence R
+⊢ False
+-/
+#guard_msgs in
+example {R : α → α → Prop} (h : Equivalence R) : False := by
+  split at h
+
+/--
+error: tactic 'split' failed, Could not split the goal or any of the following hypotheses: `t`, `use_dep`
+
+Note: `split at *` does not attempt to split at non-propositional hypotheses or those on which other hypotheses depend. It may still be possible to manually split a hypothesis using `split at`
+t : True
+dep : if 1 ≥ 0 then True else False
+use_dep : dep = dep
+⊢ False
+-/
+#guard_msgs in
 example : False := by
   have t : True := trivial
   have dep : if 1 ≥ 0 then True else False := by simp
@@ -9,9 +89,13 @@ example : False := by
   set_option trace.split.failure true in
   split at *
 
--- example (n : Nat) : n.casesOn True (fun _ => True) = True := by
-example (n : Nat) (h : (match h : (decide $ n = n) with | true => of_decide_eq_true h | false => Eq.refl n) = .refl n) : True := by
--- example (n : Nat) (h : (if n = n then true else false)) : True := by
+/--
+error: tactic 'split' failed, Could not split an `if` or `match` expression in the type `Nat` of `x`
+x : Nat := if true = true then 3 else 4
+⊢ False
+-/
+#guard_msgs in
+example : False := by
   let x := if true then 3 else 4
   set_option trace.split.failure true in
   split at x

8d288ec04b2ffcba2e422d3bc17a194d465df73a
Sebastian Ullrich <sebasti@nullri.ch>
2025-07-15 21:29:57 +0200

    fix: discover templates to export recursively

--- src/Lean/Compiler/LCNF/Visibility.lean
+++ src/Lean/Compiler/LCNF/Visibility.lean
@@ -41,11 +41,10 @@ private partial def setClosureTransparent (root : Name) : CompilerM Unit := do
 -- TODO: refine? balance run time vs export size
 private def isBodyRelevant (decl : Decl) : CompilerM Bool := do
   -- let opts := (← getOptions)
-  decl.isTemplateLike <||>
-    pure (`_at_ ∈ decl.name.components)
+  decl.isTemplateLike
     -- <||> decl.value.isCodeAndM (pure <| ·.sizeLe (compiler.small.get opts))
 
-def inferVisibilities (decls : Array Decl) : CompilerM Unit := do
+partial def inferVisibilities (decls : Array Decl) : CompilerM Unit := do
   if !(← getEnv).header.isModule then
     return
   let locals := decls.foldl (init := {}) fun l d => l.insert d.name d
@@ -54,19 +53,25 @@ def inferVisibilities (decls : Array Decl) : CompilerM Unit := do
       trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as transparent because it is tagged with `meta`"
       modifyEnv (bumpDeclVisibility · decl.name .transparent)
       setClosureTransparent locals decl.name
+    else if `_at_ ∈ decl.name.components then  -- TODO: don't?
+      trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as transparent because it is a specialization"
+      markOpaque locals decl
     else if (← getEnv).setExporting true |>.contains decl.name then
-      if (← isBodyRelevant decl) then
-        trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as transparent because it is already opaque and its body looks relevant"
-        modifyEnv (bumpDeclVisibility · decl.name .transparent)
-        decl.value.forCodeM fun code =>
-          for ref in collectUsedDecls code do
-            if getDeclVisibility (← getEnv) ref != .private then
-              continue
-            trace[Compiler.LCNF.inferVisibility] m!"Marking {ref} as opaque because it is in template closure"
-            modifyEnv (bumpDeclVisibility · ref .opaque)
-      else
-        trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as opaque because it is a public def"
-        modifyEnv (bumpDeclVisibility · decl.name .opaque)
+      trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as opaque because it is a public def"
+      markOpaque locals decl
+where
+  markOpaque (locals : NameMap Decl) (decl : Decl) : CompilerM Unit := do
+    if (← isBodyRelevant decl) then
+      trace[Compiler.LCNF.inferVisibility] m!"Marking {decl.name} as transparent because it is opaque and its body looks relevant"
+      modifyEnv (bumpDeclVisibility · decl.name .transparent)
+      decl.value.forCodeM fun code =>
+        for ref in collectUsedDecls code do
+          if let some refDecl ← pure (locals.find? ref) <|> getDecl? ref then
+            if getDeclVisibility (← getEnv) ref == .private then
+              trace[Compiler.LCNF.inferVisibility] m!"Marking {ref} as opaque because it is used by transparent {decl.name}"
+              markOpaque locals refDecl
+    else
+      modifyEnv (bumpDeclVisibility · decl.name .opaque)
 
 builtin_initialize
   registerTraceClass `Compiler.LCNF.inferVisibility

b2ab2112f3885d87cf86cbf19e2ebe7bf426280b
Sebastian Ullrich <sebasti@nullri.ch>
2025-07-16 16:14:07 +0200

    fix?

--- script/bench.sh
+++ script/bench.sh
@@ -9,7 +9,7 @@ export PATH=$PWD/build/release/stage2/bin:$PATH
 
 # The extra opts used to be passed to the Makefile during benchmarking only but with Lake it is
 # easier to configure them statically.
-cmake -B build/release/stage2 -S src -DLEAN_EXTRA_MAKE_OPTS="-Dprofiler=true -Dprofiler.threshold=9999999 --stats" 1>&2
+cmake -B build/release/stage2 -S src -DLEAN_EXTRA_LAKEFILE_TOML='weakLeanArgs=["-Dprofiler=true", "-Dprofiler.threshold=9999999", "--stats"]' 1>&2
 
 cd tests/bench
 timeout -s KILL 1h time temci exec --config speedcenter.yaml --in speedcenter.exec.velcom.yaml 1>&2

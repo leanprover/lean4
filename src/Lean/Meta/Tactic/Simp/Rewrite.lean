68c8d6608ded9046a3f72bba13184686d301cb6c
Leonardo de Moura <leomoura@amazon.com>
2025-07-15 19:13:52 -0700

    perf: avoid `inferType` at `simpArith`
    This PR ensures `simpArith` does not have to invoke `inferType` a
    performance bottleneck.

--- src/Lean/Meta/Tactic/Simp/Rewrite.lean
+++ src/Lean/Meta/Tactic/Simp/Rewrite.lean
@@ -289,11 +289,6 @@ where
   catch _ =>
     return .continue
 
-private def isNatExpr (e : Expr) : MetaM Bool := do
-  let type ← inferType e
-  let_expr Nat ← type | return false
-  return true
-
 def simpArith (e : Expr) : SimpM Step := do
   unless (← getConfig).arith do
     return .continue
@@ -305,18 +300,19 @@ def simpArith (e : Expr) : SimpM Step := do
     if let some (e', h) ← Arith.Int.simpEq? e then
       return .visit { expr := e', proof? := h }
     return .continue
-  let isNat ← isNatExpr e
-  if Arith.isLinearTerm e isNat then
-    if Arith.parentIsTarget (← getContext).parent? isNat then
+  if let some α := Arith.isLinearTerm? e then
+    if Arith.parentIsTarget (← getContext).parent? then
       -- We mark `cache := false` to ensure we do not miss simplifications.
       return .continue (some { expr := e, cache := false })
-    if isNat then
+    match_expr α with
+    | Nat =>
       let some (e', h) ← Arith.Nat.simpExpr? e | pure ()
       return .visit { expr := e', proof? := h }
-    else
+    | Int =>
       let some (e', h) ← Arith.Int.simpExpr? e | pure ()
       return .visit { expr := e', proof? := h }
-    return .continue
+    | _ =>
+      return .continue
   if Arith.isDvdCnstr e then
     let some (e', h) ← Arith.Int.simpDvd? e | pure ()
     return .visit { expr := e', proof? := h }

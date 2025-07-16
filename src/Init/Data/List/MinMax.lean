892bc617589fb692ded58377b0a1be96744be089
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-07-16 12:00:12 +0200

    max lemmas

--- src/Init/Data/List/MinMax.lean
+++ src/Init/Data/List/MinMax.lean
@@ -81,6 +81,7 @@ theorem min?_mem_legacy [Min α] (min_eq_or : ∀ a b : α, min a b = a ∨ min 
   haveI : MinEqOr α := ⟨min_eq_or⟩
   apply min?_mem
 
+-- TODO: update this
 -- See also `Init.Data.List.Nat.Basic` for specialisations of the next two results to `Nat`.
 
 theorem le_min?_iff [Min α] [LE α] [OrderData α] [LawfulOrderInf α] [LawfulOrderLE α] :
@@ -183,16 +184,27 @@ theorem min?_eq_some_iff_legacy [Min α] [LE α]
   haveI : LawfulOrderMin (Subtype (· ∈ xs)) := ⟨⟩
   exact min?_eq_some_iff_subtype (α := α) (xs := xs) (a := a)
 
-theorem min?_replicate [Min α] {n : Nat} {a : α} (w : min a a = a) :
+theorem min?_replicate_general [Min α] {n : Nat} {a : α} (w : min a a = a) :
     (replicate n a).min? = if n = 0 then none else some a := by
   induction n with
   | zero => rfl
   | succ n ih => cases n <;> simp_all [replicate_succ, min?_cons']
 
-@[simp] theorem min?_replicate_of_pos [Min α] {n : Nat} {a : α} (w : min a a = a) (h : 0 < n) :
-    (replicate n a).min? = some a := by
-  simp [min?_replicate, Nat.ne_of_gt h, w]
+theorem min?_replicate [Min α] [Std.IdempotentOp (min : α → α → α)] {n : Nat} {a : α} :
+    (replicate n a).min? = if n = 0 then none else some a :=
+  min?_replicate_general (Std.IdempotentOp.idempotent _)
 
+@[simp] theorem min?_replicate_of_pos_general [Min α] {n : Nat} {a : α} (w : min a a = a) (h : 0 < n) :
+    (replicate n a).min? = some a := by
+  simp [min?_replicate_general, Nat.ne_of_gt h, w]
+
+@[simp] theorem min?_replicate_of_pos [Min α] [MinEqOr α] {n : Nat} {a : α} (h : 0 < n) :
+    (replicate n a).min? = some a := by
+  simp [min?_replicate, Nat.ne_of_gt h]
+
+/--
+Requirements are satisfied for `[OrderData α] [Min α] [LinearOrder α] [LawfulOrderMin α]`
+-/
 theorem foldl_min [Min α] [Std.IdempotentOp (min : α → α → α)] [Std.Associative (min : α → α → α)]
     {l : List α} {a : α} : l.foldl (init := a) min = min a (l.min?.getD a) := by
   cases l <;> simp [min?, foldl_assoc, Std.IdempotentOp.idempotent]
@@ -216,54 +228,163 @@ theorem isSome_max?_of_mem {l : List α} [Max α] {a : α} (h : a ∈ l) :
     l.max?.isSome := by
   cases l <;> simp_all [max?_cons']
 
-theorem max?_mem [Max α] (min_eq_or : ∀ a b : α, max a b = a ∨ max a b = b) :
-    {xs : List α} → xs.max? = some a → a ∈ xs
-  | nil => by simp
-  | cons x xs => by
-    rw [max?]; rintro ⟨⟩
-    induction xs generalizing x with simp at *
-    | cons y xs ih =>
-      rcases ih (max x y) with h | h <;> simp [h]
-      simp [← or_assoc, min_eq_or x y]
+theorem max?_eq_head? {α : Type u} [Max α] {l : List α}
+    (h : l.Pairwise (fun a b => max a b = a)) : l.max? = l.head? := by
+  cases l with
+  | nil => rfl
+  | cons x l =>
+    rw [head?_cons, max?_cons', Option.some.injEq]
+    induction l generalizing x with
+    | nil => simp
+    | cons y l ih =>
+      have hx : max x y = x := rel_of_pairwise_cons h mem_cons_self
+      rw [foldl_cons, ih _ (hx.symm ▸ h.sublist (by simp)), hx]
 
+theorem max?_mem [Max α] [MaxEqOr α] :
+    {xs : List α} → xs.max? = some a → a ∈ xs := by
+  intro xs
+  match xs with
+  | nil => simp
+  | x :: xs =>
+    simp only [max?_cons', Option.some.injEq, mem_cons]
+    intro eq
+    induction xs generalizing x with
+    | nil =>
+      simp at eq
+      simp [eq]
+    | cons y xs ind =>
+      simp at eq
+      have p := ind _ eq
+      cases p with
+      | inl p =>
+        cases MaxEqOr.max_eq_or x y with | _ q => simp [p, q]
+      | inr p => simp [p, mem_cons]
+
+theorem max?_mem_legacy [Max α] (max_eq_or : ∀ a b : α, max a b = a ∨ max a b = b) :
+    {xs : List α} → xs.max? = some a → a ∈ xs := by
+  haveI : MaxEqOr α := ⟨max_eq_or⟩
+  apply max?_mem
+
+-- TODO: update this
 -- See also `Init.Data.List.Nat.Basic` for specialisations of the next two results to `Nat`.
 
-theorem max?_le_iff [Max α] [LE α]
-    (max_le_iff : ∀ a b c : α, max b c ≤ a ↔ b ≤ a ∧ c ≤ a) :
-    {xs : List α} → xs.max? = some a → ∀ {x}, a ≤ x ↔ ∀ b ∈ xs, b ≤ x
+theorem max?_le_iff [Max α] [LE α] [OrderData α] [LawfulOrderSup α] [LawfulOrderLE α] :
+    {xs : List α} → xs.max? = some a → ∀ {x}, a ≤ x ↔ ∀ b, b ∈ xs → b ≤ x
   | nil => by simp
   | cons x xs => by
-    rw [max?]; rintro ⟨⟩ y
+    rw [max?]
+    intro eq y
+    simp only [Option.some.injEq] at eq
     induction xs generalizing x with
-    | nil => simp
-    | cons y xs ih => simp [ih, max_le_iff, and_assoc]
+    | nil =>
+      simp at eq
+      simp [eq]
+    | cons z xs ih =>
+      simp at eq
+      simp [ih _ eq, max_le_iff, and_assoc]
 
--- This could be refactored by designing appropriate typeclasses to replace `le_refl`, `max_eq_or`,
--- and `le_min_iff`.
-theorem max?_eq_some_iff [Max α] [LE α] [anti : Std.Antisymm (· ≤ · : α → α → Prop)]
-    (le_refl : ∀ a : α, a ≤ a)
-    (max_eq_or : ∀ a b : α, max a b = a ∨ max a b = b)
-    (max_le_iff : ∀ a b c : α, max b c ≤ a ↔ b ≤ a ∧ c ≤ a) {xs : List α} :
-    xs.max? = some a ↔ a ∈ xs ∧ ∀ b ∈ xs, b ≤ a := by
-  refine ⟨fun h => ⟨max?_mem max_eq_or h, (max?_le_iff max_le_iff h).1 (le_refl _)⟩, ?_⟩
+theorem max?_le_iff_legacy [Max α] [LE α]
+    (max_le_iff : ∀ a b c : α, max a b ≤ c ↔ a ≤ c ∧ b ≤ c) :
+    {xs : List α} → xs.max? = some a → ∀ {x}, a ≤ x ↔ ∀ b, b ∈ xs → b ≤ x := by
+  letI : OrderData α := .ofLE α
+  haveI : LawfulOrderSup α := .ofLE max_le_iff
+  apply max?_le_iff
+
+theorem max?_eq_some_iff [Max α] [LE α] {xs : List α} [OrderData α] [LinearOrder (α)]
+    [LawfulOrderMax α] [LawfulOrderLE α] : xs.max? = some a ↔ a ∈ xs ∧ ∀ b, b ∈ xs → b ≤ a := by
+  refine ⟨fun h => ⟨max?_mem h, (max?_le_iff h).1 (le_refl _)⟩, ?_⟩
   intro ⟨h₁, h₂⟩
   cases xs with
   | nil => simp at h₁
   | cons x xs =>
-    exact congrArg some <| anti.1 _ _
-      (h₂ _ (max?_mem max_eq_or (xs := x::xs) rfl))
-      ((max?_le_iff max_le_iff (xs := x::xs) rfl).1 (le_refl _) _ h₁)
+    rw [List.max?]
+    exact congrArg some <| le_antisymm
+      (h₂ _ (max?_mem (xs := x :: xs) rfl))
+      ((max?_le_iff (xs := x :: xs) rfl).1 (le_refl _) _ h₁)
 
-theorem max?_replicate [Max α] {n : Nat} {a : α} (w : max a a = a) :
+private theorem max?_attach [Max α] [MaxEqOr α] {xs : List α} :
+    xs.attach.max? = (xs.max?.pmap (fun m hm => ⟨m, max?_mem hm⟩) (fun _ => id)) := by
+  cases xs with
+  | nil => simp
+  | cons x xs =>
+    simp only [max?, attach_cons, Option.some.injEq, Option.pmap_some]
+    rw [foldl_map]
+    simp only [Subtype.ext_iff]
+    rw [← foldl_attach (l := xs)]
+    apply Eq.trans (foldl_hom (f := Subtype.val) ?_).symm
+    · rfl
+    · intros; rfl
+
+private theorem max?_eq_max?_attach [Max α] [MaxEqOr α] {xs : List α} :
+    xs.max? = (xs.attach.max?.map Subtype.val) := by
+  simp [max?_attach, Option.map_pmap]
+
+private theorem max?_eq_some_iff_subtype [Max α] [LE α] {xs : List α} [OrderData α]
+    [MaxEqOr α] [LinearOrder (Subtype (· ∈ xs))]
+    [LawfulOrderMax (Subtype (· ∈ xs))] [LawfulOrderLE (Subtype (· ∈ xs))] :
+    xs.max? = some a ↔ a ∈ xs ∧ ∀ b, b ∈ xs → b ≤ a := by
+  have := fun a => max?_eq_some_iff (xs := xs.attach) (a := a)
+  rw [max?_eq_max?_attach]
+  simp [max?_eq_some_iff]
+  constructor
+  · rintro ⟨ha, h⟩
+    exact ⟨ha, h⟩
+  · rintro ⟨ha, h⟩
+    exact ⟨ha, h⟩
+
+theorem max?_eq_some_iff_legacy [Max α] [LE α] [anti : Std.Antisymm (· ≤ · : α → α → Prop)]
+    (le_refl : ∀ a : α, a ≤ a)
+    (max_eq_or : ∀ a b : α, max a b = a ∨ max a b = b)
+    (max_le_iff : ∀ a b c : α, max b c ≤ a ↔ b ≤ a ∧ c ≤ a) {xs : List α} :
+    xs.max? = some a ↔ a ∈ xs ∧ ∀ b ∈ xs, b ≤ a := by
+  letI : OrderData α := .ofLE α
+  haveI : MaxEqOr α := ⟨max_eq_or⟩
+  haveI : LinearOrder α := by
+    refine .ofLE ?_ ?_ ?_ ?_
+    · exact fun a => le_refl a
+    · exact anti.antisymm
+    · intro a b c hab hbc
+      have : max a b = b := by
+        apply anti.antisymm _ _
+        · refine MaxEqOr.elim (P := (· ≤ b)) hab (le_refl _)
+        · exact ((max_le_iff _ _ _).mp (le_refl _)).2
+      specialize max_le_iff c a b
+      rw [this] at max_le_iff
+      exact (max_le_iff.mp hbc).1
+    · exact fun a b => by
+        cases max_eq_or a b
+        case inl h => exact Or.inr ((h ▸ max_le_iff a a b).mp (le_refl _)).2
+        case inr h => exact Or.inl ((h ▸ max_le_iff b a b).mp (le_refl _)).1
+  haveI : LawfulOrderSup α := by
+    refine ⟨fun a b c => ?_⟩
+    simpa [OrderData.IsLE, LawfulOrderLE.le_iff] using max_le_iff c a b
+  haveI : MaxEqOr (Subtype (· ∈ xs)) := by
+    refine ⟨fun a b => ?_⟩
+    simpa [Max.max, Subtype.ext_iff] using max_eq_or a b
+  haveI : LawfulOrderMax α := ⟨⟩
+  apply max?_eq_some_iff
+
+theorem max?_replicate_general [Max α] {n : Nat} {a : α} (w : max a a = a) :
     (replicate n a).max? = if n = 0 then none else some a := by
   induction n with
   | zero => rfl
   | succ n ih => cases n <;> simp_all [replicate_succ, max?_cons']
 
-@[simp] theorem max?_replicate_of_pos [Max α] {n : Nat} {a : α} (w : max a a = a) (h : 0 < n) :
-    (replicate n a).max? = some a := by
-  simp [max?_replicate, Nat.ne_of_gt h, w]
+theorem max?_replicate [Max α] [Std.IdempotentOp (max : α → α → α)] {n : Nat} {a : α} :
+    (replicate n a).max? = if n = 0 then none else some a :=
+  max?_replicate_general (Std.IdempotentOp.idempotent _)
 
+@[simp] theorem max?_replicate_of_pos_general [Max α] {n : Nat} {a : α} (w : max a a = a) (h : 0 < n) :
+    (replicate n a).max? = some a := by
+  simp [max?_replicate_general, Nat.ne_of_gt h, w]
+
+@[simp] theorem max?_replicate_of_pos [Max α] [MaxEqOr α] {n : Nat} {a : α} (h : 0 < n) :
+    (replicate n a).max? = some a := by
+  simp [max?_replicate, Nat.ne_of_gt h]
+
+/--
+Requirements are satisfied for `[OrderData α] [Max α] [LinearOrder α] [LawfulOrderMax α]`
+-/
 theorem foldl_max [Max α] [Std.IdempotentOp (max : α → α → α)] [Std.Associative (max : α → α → α)]
     {l : List α} {a : α} : l.foldl (init := a) max = max a (l.max?.getD a) := by
   cases l <;> simp [max?, foldl_assoc, Std.IdempotentOp.idempotent]

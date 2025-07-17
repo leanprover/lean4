12b51cfefc5063f5c0685f9a2574f09467e217ba
luisacicolini <luisacicolini@gmail.com>
2025-07-17 10:46:52 +0100

    chore: missing ite

--- src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Impl/Operations/PopCnt.lean
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Impl/Operations/PopCnt.lean
@@ -25,40 +25,95 @@ namespace bitblast
 def blastPopCnt (aig : AIG α) (x : AIG.RefVec aig w) :
     AIG.RefVecEntry α w :=
   let acc := blastConst aig (w := w) 0
-  go aig x 0 acc
+  go aig x w acc
 where
   go (aig : AIG α) (x : AIG.RefVec aig w) (fuel : Nat) (acc : AIG.RefVec aig w) :=
-    if hf : fuel < w then
+    if hf : 0 < fuel then
       let one : AIG.RefVec aig w := blastConst aig (w := w) 1
+
+      -- ite x = 0, acc, acc
+
+
       -- acc := acc + 1
       let res : AIG.RefVecEntry α w := blastAdd aig ⟨acc, one⟩
       let aig := res.aig
-      let acc : AIG.RefVec aig w := res.vec
+      let add : AIG.RefVec aig w := res.vec
       have := AIG.LawfulVecOperator.le_size (f := blastAdd) ..
       let one : AIG.RefVec aig w := one.cast this
       let x := x.cast this
+
+      -- ite x recCall acc
+
       -- sub := x - 1
       let res : AIG.RefVecEntry α w := blastSub aig ⟨x, one⟩
       let aig := res.aig
       let sub : AIG.RefVec aig w := res.vec
       have := AIG.LawfulVecOperator.le_size (f := blastSub) ..
       let x := x.cast this
-      let acc := acc.cast this
+      let add : AIG.RefVec aig w := acc.cast this
+
       -- x :=& sub
       let res : AIG.RefVecEntry α w := AIG.RefVec.zip aig ⟨x, sub⟩ AIG.mkAndCached
       let aig := res.aig
       let x : AIG.RefVec aig w  := res.vec
       have := AIG.RefVec.zip_le_size ..
       let acc := acc.cast this
-      go aig x (fuel + 1) acc
+
+      go aig x (fuel - 1) acc
     else
       ⟨aig, acc⟩
-  termination_by (w - fuel)
+  termination_by fuel
 
 namespace blastPopCnt
 
 end blastPopCnt
 
+theorem blastPopCnt.go_le_size (aig : AIG α) (fuel : Nat) (acc : AIG.RefVec aig w)
+    (xc : AIG.RefVec aig w) :
+    aig.decls.size ≤ (go aig xc fuel acc).aig.decls.size := by
+  unfold go
+  dsimp only
+  split
+  · simp [AIG.RefVec.zip_le_size]
+    sorry
+  · simp
+  -- dsimp only
+  -- split
+  -- · refine Nat.le_trans ?_ (by apply go_le_size)x
+  --   sorry
+  -- · simp
+termination_by fuel
+
+theorem blastPopCnt.go_decl_eq (aig : AIG α) (fuel : Nat) (acc : AIG.RefVec aig w)
+    (xc : AIG.RefVec aig w) :
+    ∀ (idx : Nat) h1 h2,
+        (go aig xc fuel acc).aig.decls[idx]'h1 = aig.decls[idx]'h2 := by
+  generalize hgo : go aig xc fuel acc = res
+  unfold go at hgo
+  dsimp only at hgo
+  split at hgo
+  · rw [← hgo]
+    intros
+    simp [AIG.RefVec.zip_le_size]
+    rw [AIG.LawfulVecOperator.decl_eq (f := AIG.RefVec.zip)]
+    sorry
+    -- rw [blastPopCnt.go_decl_eq, AIG.LawfulVecOperator.decl_eq (f := AIG.RefVec.ite)]
+    -- apply AIG.LawfulVecOperator.lt_size_of_lt_aig_size (f := AIG.RefVec.ite)
+    -- assumption
+  · simp [← hgo]
+termination_by w - curr
+
+instance : AIG.LawfulVecOperator α AIG.RefVec blastPopCnt where
+  le_size := by
+    intros
+    unfold blastPopCnt
+    dsimp only
+    apply blastPopCnt.go_le_size
+  decl_eq := by
+    intros
+    unfold blastPopCnt
+    dsimp only
+    apply blastPopCnt.go_decl_eq
 
 end bitblast
 end BVExpr

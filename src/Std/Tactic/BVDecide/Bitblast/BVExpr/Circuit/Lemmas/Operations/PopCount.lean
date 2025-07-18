fe0ab0b23dc53d493e116b68056f42dcd17c2ee7
luisacicolini <luisacicolini@gmail.com>
2025-07-18 10:35:44 +0100

    chore: fix circuit

---
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Operations/PopCount.lean
@@ -0,0 +1,65 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Luisa Cicolini, Siddharth Bhat, Henrik Böving
+-/
+prelude
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Basic
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Impl.Operations.PopCount
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Const
+
+/-!
+This module contains the verification of the bitblaster for `BitVec.popCountRec` from
+`Impl.Operations.popCount`. We prove that the accumulator of the `go` function
+at step`n` represents the portion of the `ite` nodes in the AIG constructed for
+bits `0` until `n`.
+-/
+
+namespace Std.Tactic.BVDecide
+
+open Std.Sat
+open Std.Sat.AIG
+
+variable [Hashable α] [DecidableEq α]
+
+namespace BVExpr
+
+namespace bitblast
+namespace blastPopCount
+
+example (x : Nat) (hx : 0 < x) : ∃ y, x = y + 1 := by
+  exact Nat.exists_eq_add_one.mpr hx
+
+theorem go_denote_eq {w : Nat} (aig : AIG α)
+    (acc : AIG.RefVec aig w) (xc : AIG.RefVec aig w) (x : BitVec w) (assign : α → Bool)
+    (hx : ∀ (idx : Nat) (hidx : idx < w), ⟦aig, xc.get idx hidx, assign⟧ = x.getLsbD idx)
+    (hacc : ∀ (idx : Nat) (hidx : idx < w),
+      if curr = 0 then ⟦aig, acc.get idx hidx, assign⟧ = (BitVec.ofNat w w).getLsbD idx
+      else ⟦aig, acc.get idx hidx, assign⟧ = (BitVec.clzAuxRec x (curr - 1)).getLsbD idx)
+    :
+    ∀ (idx : Nat) (hidx : idx < w),
+        ⟦
+          (go aig xc curr acc).aig,
+          (go aig xc curr acc).vec.get idx hidx,
+          assign
+        ⟧
+          =
+        (BitVec.popCountRec x 0 w).getLsbD idx := by
+    sorry
+
+end blastPopCount
+
+@[simp]
+theorem denote_blastPopCount (aig : AIG α) (xc : RefVec aig w) (x : BitVec w) (assign : α → Bool)
+      (hx : ∀ (idx : Nat) (hidx : idx < w), ⟦aig, xc.get idx hidx, assign⟧ = x.getLsbD idx)
+      :
+      ∀ (idx : Nat) (hidx : idx < w),
+        ⟦(blastPopCount aig xc).aig, (blastPopCount aig xc).vec.get idx hidx, assign⟧
+          =
+        (BitVec.popCountRec x 0 w).getLsbD idx := by
+  sorry
+
+end bitblast
+end BVExpr
+
+end Std.Tactic.BVDecide

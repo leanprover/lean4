9aca332fd9fce65a8e6d5072f67cab4c0057f192
luisacicolini <luisacicolini@gmail.com>
2025-07-18 10:35:44 +0100

    chore: fix circuit

---
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Impl/Operations/PopCount.lean
@@ -0,0 +1,123 @@
+/-
+Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Luisa Cicolini, Siddharth Bhat, Henrik Böving
+-/
+prelude
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Impl.Const
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Impl.Operations.Sub
+import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Impl.Operations.Eq
+import Std.Sat.AIG.If
+
+/-!
+This module contains the implementation of a bitblaster for `BitVec.clz`.
+-/
+
+namespace Std.Tactic.BVDecide
+
+open Std.Sat
+
+variable [Hashable α] [DecidableEq α]
+
+namespace BVExpr
+namespace bitblast
+
+def blastPopCount (aig : AIG α) (x : AIG.RefVec aig w) :
+    AIG.RefVecEntry α w :=
+  let wconst : AIG.RefVec aig w := blastConst aig (w := w) w
+  go aig x w 0 wconst
+where
+  go (aig : AIG α) (x : AIG.RefVec aig w) (fuel : Nat) (curr : Nat) (acc : AIG.RefVec aig w) :=
+    if hf : 0 < fuel then
+      -- create curr constant node
+      let currConst : AIG.RefVec aig w := blastConst aig (w := w) curr
+      let zero : AIG.RefVec aig w := blastConst aig (w := w) 0
+      -- create node x = 0
+      let res := BVPred.mkEq aig ⟨x, zero⟩
+      let aig := res.aig
+      let cond := res.ref
+      have := AIG.LawfulOperator.le_size (f := BVPred.mkEq) ..
+      let currConst := currConst.cast this
+      let x := x.cast this
+      let acc := acc.cast this
+      -- ite (x & x - 1) then acc else cnt
+      let res := AIG.RefVec.ite aig ⟨cond, currConst, acc⟩
+      let aig := res.aig
+      let acc := res.vec
+      have := AIG.LawfulVecOperator.le_size (f := AIG.RefVec.ite) ..
+      let x := x.cast this
+      -- sub := x - 1
+      let one : AIG.RefVec aig w := blastConst aig (w := w) 1
+      let res : AIG.RefVecEntry α w := blastSub aig ⟨x, one⟩
+      let aig := res.aig
+      let sub : AIG.RefVec aig w := res.vec
+      have := AIG.LawfulVecOperator.le_size (f := blastSub) ..
+      let x := x.cast this
+      let acc := acc.cast this
+      -- x :=& sub
+      let res : AIG.RefVecEntry α w := AIG.RefVec.zip aig ⟨x, sub⟩ AIG.mkAndCached
+      let aig := res.aig
+      let x := res.vec
+      have := AIG.RefVec.zip_le_size ..
+      let acc := acc.cast this
+      go aig x (fuel - 1) (curr + 1) acc
+    else
+      ⟨aig, acc⟩
+  termination_by fuel
+
+namespace blastPopCount
+
+end blastPopCount
+
+theorem blastPopCount.go_le_size (aig : AIG α) (fuel : Nat) (curr : Nat) (acc : AIG.RefVec aig w)
+    (xc : AIG.RefVec aig w) :
+    aig.decls.size ≤ (go aig xc fuel curr acc).aig.decls.size := by
+  unfold go
+  sorry
+  -- dsimp only
+  -- split
+  -- · simp [AIG.RefVec.zip_le_size]
+  --   sorry
+  -- · simp
+  -- dsimp only
+  -- split
+  -- · refine Nat.le_trans ?_ (by apply go_le_size)x
+  --   sorry
+  -- · simp
+-- termination_by fuel
+
+theorem blastPopCount.go_decl_eq (aig : AIG α) (fuel : Nat) (curr : Nat) (acc : AIG.RefVec aig w)
+    (xc : AIG.RefVec aig w) :
+    ∀ (idx : Nat) h1 h2,
+        (go aig xc fuel curr acc).aig.decls[idx]'h1 = aig.decls[idx]'h2 := by sorry
+--   generalize hgo : go aig xc fuel acc = res
+--   unfold go at hgo
+--   dsimp only at hgo
+--   split at hgo
+--   · rw [← hgo]
+--     intros
+--     simp [AIG.RefVec.zip_le_size]
+--     rw [AIG.LawfulVecOperator.decl_eq (f := AIG.RefVec.zip)]
+--     sorry
+--     -- rw [blastPopCount.go_decl_eq, AIG.LawfulVecOperator.decl_eq (f := AIG.RefVec.ite)]
+--     -- apply AIG.LawfulVecOperator.lt_size_of_lt_aig_size (f := AIG.RefVec.ite)
+--     -- assumption
+--   · simp [← hgo]
+-- termination_by w - curr
+
+instance : AIG.LawfulVecOperator α AIG.RefVec blastPopCount where
+  le_size := by
+    intros
+    unfold blastPopCount
+    dsimp only
+    apply blastPopCount.go_le_size
+  decl_eq := by
+    intros
+    unfold blastPopCount
+    dsimp only
+    apply blastPopCount.go_decl_eq
+
+end bitblast
+end BVExpr
+
+end Std.Tactic.BVDecide

de5b89070da6e8a91199ddf1817d2a6e7790accf
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-07-21 19:09:17 +0200

    test: improve documentation and exclusion handling for test 9366

--- tests/lean/run/9366.lean
+++ tests/lean/run/9366.lean
@@ -17,20 +17,54 @@ structure A.D where
 
 open A
 
+/-
+The following tests simulate, for each class `Cls` with a registered deriving handler, the three
+following declarations:
+
+```
+structure B where
+  deriving Cls
+
+structure C where
+  x : Nat
+  deriving Cls
+
+inductive D where
+  | mk₁ : Bool → D
+  | mk₂ : Bool → D
+  deriving Cls
+```
+
+The purpose of the three distinct declarations is to account for the fact that many deriving
+handlers have different logic for structures, singletons, and/or sums. If a class cannot be derived
+for one or more of these declarations, add it to the `exclusions` map below, indicating the tests
+from which it should be excluded.
+-/
+
+inductive ExclusionKind
+  | singleton | struct | sum
+  deriving BEq, Hashable
+
+def exclusions : Std.HashMap Lean.Name (Std.HashSet ExclusionKind) := .ofList [
+  (``SizeOf, { .singleton, .struct, .sum })
+]
+
 open Lean Meta Elab Command in
 set_option hygiene false in
 #eval show CommandElabM Unit from do
   let go : StateRefT (Array (Name × PersistentArray Message)) CommandElabM Unit := do
     let derivingHandlers ← derivingHandlersRef.get
-    -- `SizeOf` panics if you try to re-derive it
     let derivingHandlers := derivingHandlers.filter (fun nm _ => nm != ``SizeOf)
     for (cls, _) in derivingHandlers do
       withoutModifyingEnv do
+        let hasExcl (kind : ExclusionKind) := (·.contains kind) <$> exclusions[cls]? |>.getD false
         let s ← getThe Command.State
-        -- Many handlers have different logic for enums, singletons, and/or sums
-        Command.elabCommand (← `(structure B where deriving $(mkIdent cls):ident))
-        Command.elabCommand (← `(structure C where x : Nat deriving $(mkIdent cls):ident))
-        Command.elabCommand (← `(inductive D where | mk₁ : Bool → D | mk₂ : Bool → D deriving $(mkIdent cls):ident))
+        unless hasExcl .singleton do
+          Command.elabCommand (← `(structure B where deriving $(mkIdent cls):ident))
+        unless hasExcl .struct do
+          Command.elabCommand (← `(structure C where x : Nat deriving $(mkIdent cls):ident))
+        unless hasExcl .sum do
+          Command.elabCommand (← `(inductive D where | mk₁ : Bool → D | mk₂ : Bool → D deriving $(mkIdent cls):ident))
         let msgs := (← getThe Command.State).messages.unreported
         set s
         if msgs.any (·.severity == .error) then

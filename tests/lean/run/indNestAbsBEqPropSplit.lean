2fa089240bcbeb2e7976ae7438fb85b0a4770735
Joachim Breitner <mail@joachim-breitner.de>
2025-07-24 18:29:21 +0200

    Experimenting with TreeMap

--- tests/lean/run/indNestAbsBEqPropSplit.lean
+++ tests/lean/run/indNestAbsBEqPropSplit.lean
@@ -1,3 +1,5 @@
+import Std
+
 opaque T : (α : Type u) → [BEq α] → Type u
 
 @[instance] axiom instBeqT [BEq α] : BEq (T α)
@@ -65,12 +67,12 @@ instance [PropSplit α β P] : PropSplit (List α) (List β) (List.Forall P) whe
 
 -- Trivial Instance for pure data
 
--- instance (priority := low) instPropSplitDefault : PropSplit α α (fun _ => True) where
---   data x := x
---   prop _ := True.intro
---   join x _ := x
---   split_join _ _ := rfl
---   join_split _ := rfl
+instance (priority := low) instPropSplitDefault : PropSplit α α (fun _ => True) where
+  data x := x
+  prop _ := True.intro
+  join x _ := x
+  split_join _ _ := rfl
+  join_split _ := rfl
 
 
 
@@ -85,6 +87,8 @@ def instSubtypeSimple {P : α → Prop} : PropSplit (Subtype P) α P where
   join_split _ := rfl
 
 -- Not so simple instance for subtypes
+-- (TODO: This probably does not actually work with nested propositions, due to the
+-- existential around `P`)
 
 instance {Q : α → Prop} [PropSplit α β P] :
     PropSplit (Subtype Q) β (fun y => ∃ (h : P y), Q (PropSplit.join y h)) where
@@ -95,7 +99,7 @@ instance {Q : α → Prop} [PropSplit α β P] :
   join_split _ := by simp [PropSplit.join_split]
 
 axiom T.WF [BEq α] : T.Raw α → Prop
-inductive T.Raw.Forall (p : α → Prop) : T.Raw α → Prop
+inductive T.Raw.Forall [BEq α] (p : α → Prop) : T.Raw α → Prop
   | mk x xs : p x → xs.Forall p → T.Raw.Forall p (T.Raw.mk x xs)
 
 @[instance]
@@ -419,7 +423,7 @@ error: (kernel) invalid nested inductive datatype 'Eq', nested inductive datatyp
 -/
 #guard_msgs in
 inductive VTree' : (n : Nat) → Type u where
-  | node : Vector (VTree' n) (n + 1) → VTree' n
+  | node : Vector (VTree' n) (n - 1) → VTree' n
 
 /--
 error: (kernel) invalid nested inductive datatype 'Eq', nested inductive datatypes parameters cannot contain local variables.
@@ -623,3 +627,369 @@ theorem VTree.rec_2'_eq2 :
     rfl
 
 end rec
+
+-- Now trying DTreeMap.
+
+-- The dependent case is too hard. If `β` depends on `α` we cannot reasonable
+-- erase `α`. We can probably support erasing `β` only if it is dependent.
+
+-- #print Std.DTreeMap.Internal.Impl
+
+-- Like Std.DTreeMap.Raw but without the ord constraint
+structure Std.TreeMap.Raw' α β where
+  inner : Std.DTreeMap.Internal.Impl α (fun _ => β)
+
+inductive Std.DTreeMap.Internal.Impl.Prop {α : Type u} {β : Type v} (P₁ : α → Prop) (P₂ : β → Prop) :
+    (x : Std.DTreeMap.Internal.Impl α (fun _ => β)) → Prop where
+  | inner (size : Nat) (k : α) (v : β) (l r : Impl α (fun _ => β)) :
+    P₁ k → P₂ v → Impl.Prop P₁ P₂ l → Impl.Prop P₁ P₂ r →
+    Impl.Prop P₁ P₂ (Impl.inner size k v l r)
+  | leaf :
+    Impl.Prop P₁ P₂ Impl.leaf
+
+
+inductive Std.TreeMap.Prop {α : Type u} {β : Type v} [Ord α] (P₁ : α → Prop) (P₂ : β → Prop) :
+    (x : Std.TreeMap.Raw' α β) → Prop where
+  | mk {inner : Std.DTreeMap.Internal.Impl α (fun _ => β)}
+       (wf : Std.DTreeMap.Internal.Impl.WF inner)
+       (pinner : Std.DTreeMap.Internal.Impl.Prop P₁ P₂ inner)
+    : Std.TreeMap.Prop P₁ P₂ (Std.TreeMap.Raw'.mk inner)
+
+def Std.DTreeMap.Internal.Impl.data
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  : Std.DTreeMap.Internal.Impl α (fun _ => β) → Std.DTreeMap.Internal.Impl α' (fun _ => β')
+  | .inner size k v l r =>
+    Std.DTreeMap.Internal.Impl.inner
+      size (PropSplit.data k) (PropSplit.data v)
+      (Std.DTreeMap.Internal.Impl.data P₁ P₂ l)
+      (Std.DTreeMap.Internal.Impl.data P₁ P₂ r)
+  | .leaf =>
+    Std.DTreeMap.Internal.Impl.leaf
+
+-- TODO: This is where we need the preservation of WF by order-preserving `PropSplit.data`
+theorem Std.DTreeMap.Internal.Impl.wf_data
+  [Ord α]
+  [Ord α']
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  (x : Std.DTreeMap.Internal.Impl α (fun _ => β))
+  (wf : Std.DTreeMap.Internal.Impl.WF x) :
+  (Std.DTreeMap.Internal.Impl.data P₁ P₂ x).WF := sorry
+
+def Std.DTreeMap.Internal.Impl.prop
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  : (x : Std.DTreeMap.Internal.Impl α (fun _ => β)) → Std.DTreeMap.Internal.Impl.Prop P₁ P₂ (x.data P₁ P₂)
+  | .inner size k v l r =>
+    Std.DTreeMap.Internal.Impl.Prop.inner
+      size
+      (PropSplit.data k) (PropSplit.data v)
+      (Std.DTreeMap.Internal.Impl.data P₁ P₂ l)
+      (Std.DTreeMap.Internal.Impl.data P₁ P₂ r)
+      (PropSplit.prop k) (PropSplit.prop v)
+      (Std.DTreeMap.Internal.Impl.prop P₁ P₂ l)
+      (Std.DTreeMap.Internal.Impl.prop P₁ P₂ r)
+  | .leaf =>
+    .leaf
+
+def Std.DTreeMap.Internal.Impl.join
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  : (x : Std.DTreeMap.Internal.Impl α' (fun _ => β')) →
+    Std.DTreeMap.Internal.Impl.Prop P₁ P₂ x →
+    Std.DTreeMap.Internal.Impl α (fun _ => β)
+  | .inner size k v l r, h =>
+    Std.DTreeMap.Internal.Impl.inner
+      size
+      (PropSplit.join k (match h with | .inner _ _ _ _ _ h _ _ _ => h))
+      (PropSplit.join v (match h with | .inner _ _ _ _ _ _ h _ _ => h))
+      (Std.DTreeMap.Internal.Impl.join P₁ P₂ l (match h with | .inner _ _ _ _ _ _ _ h _ => h))
+      (Std.DTreeMap.Internal.Impl.join P₁ P₂ r (match h with | .inner _ _ _ _ _ _ _ _ h => h))
+  | .leaf, _ =>
+    Std.DTreeMap.Internal.Impl.leaf
+
+-- TODO: This is where we need the preservation of WF by order-preserving `PropSplit.data`
+theorem Std.DTreeMap.Internal.Impl.wf_join
+  [Ord α]
+  [Ord α']
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  (x : Std.DTreeMap.Internal.Impl α' (fun _ => β'))
+  (h : Std.DTreeMap.Internal.Impl.Prop P₁ P₂ x)
+  (wf : Std.DTreeMap.Internal.Impl.WF x) :
+  (Std.DTreeMap.Internal.Impl.join (α := α) (β := β) P₁ P₂ x h).WF := sorry
+
+theorem Std.DTreeMap.Internal.Impl.split_join
+  [Ord α']
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  :
+  (x : Std.DTreeMap.Internal.Impl α' (fun _ => β')) →
+  (h : Std.DTreeMap.Internal.Impl.Prop P₁ P₂ x) →
+  (Std.DTreeMap.Internal.Impl.join (α := α) (β := β) P₁ P₂ x h).data P₁ P₂ = x
+  | .inner size k v l r, h =>
+    by
+      simp only [Std.DTreeMap.Internal.Impl.join, Std.DTreeMap.Internal.Impl.data]
+      congr
+      all_goals simp [PropSplit.split_join, Std.DTreeMap.Internal.Impl.split_join]
+  | .leaf, _ => rfl
+
+theorem Std.DTreeMap.Internal.Impl.join_split
+  [Ord α']
+  (P₁ : α' → Prop) (P₂ : β' → Prop)
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  :
+  (x : Std.DTreeMap.Internal.Impl α (fun _ => β)) →
+  Std.DTreeMap.Internal.Impl.join (α := α) (β := β) P₁ P₂ (x.data P₁ P₂) (x.prop P₁ P₂) = x
+  | .inner size k v l r =>
+    by
+      simp only [Std.DTreeMap.Internal.Impl.join, Std.DTreeMap.Internal.Impl.data]
+      congr
+      all_goals simp [PropSplit.join_split, Std.DTreeMap.Internal.Impl.join_split]
+  | .leaf => rfl
+
+instance [Ord α']
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  : PropSplit
+    (Std.DTreeMap.Internal.Impl α (fun _ => β))
+    (Std.DTreeMap.Internal.Impl α' (fun _ => β'))
+    (Std.DTreeMap.Internal.Impl.Prop P₁ P₂) where
+  data x := x.data P₁ P₂
+  prop x := x.prop P₁ P₂
+  join x h := x.join P₁ P₂ h
+  split_join := Std.DTreeMap.Internal.Impl.split_join _ _
+  join_split := Std.DTreeMap.Internal.Impl.join_split _ _
+
+instance [Ord α']
+  [PropSplit α α' P₁]
+  [PropSplit β β' P₂]
+  : PropSplit
+    (Std.TreeMap α β cmp)
+    (Std.TreeMap.Raw' α' β')
+    (Std.TreeMap.Prop P₁ P₂) where
+  data x := Std.TreeMap.Raw'.mk (PropSplit.data x.inner.inner)
+  prop x := Std.TreeMap.Prop.mk
+    (letI : Ord α := ⟨cmp⟩; Std.DTreeMap.Internal.Impl.wf_data _ _ _ x.inner.wf)
+    (PropSplit.prop x.inner.inner)
+  join x h := match x with
+    | .mk x => Std.TreeMap.mk (Std.DTreeMap.mk (PropSplit.join x (match h with | .mk _ h => h)) (letI : Ord α := ⟨cmp⟩; Std.DTreeMap.Internal.Impl.wf_join _ _ _ (match h with | .mk _ h => h) (match h with | .mk h _ => h) ))
+  split_join x h := by simp [PropSplit.split_join]
+  join_split x := by simp [PropSplit.join_split]
+
+-- Now let's use it
+
+inductive SExpr.Raw where
+  | leaf : SExpr.Raw
+  | app : SExpr.Raw → Std.TreeMap.Raw' SExpr.Raw Unit → SExpr.Raw
+
+instance : Ord SExpr.Raw where
+  compare _ _ := .eq -- TODO: We don't have Ord on Std.TreeMap, so little hope to derive it on SExpr?
+
+attribute [pp_nodot] SExpr.Raw.app
+
+inductive SExpr.WF : SExpr.Raw → Prop where
+  | leaf : SExpr.WF .leaf
+  | app {f es} :
+    (wf_f : SExpr.WF f) →
+    (wf_es : Std.TreeMap.Prop SExpr.WF (fun _ => True) es) →
+    WF (.app f es)
+
+def SExpr := Subtype SExpr.WF
+
+instance : PropSplit SExpr SExpr.Raw SExpr.WF where
+  data := Subtype.val
+  prop := Subtype.property
+  join x h := ⟨x, h⟩
+  split_join _ _ := rfl
+  join_split _ := rfl
+
+instance instOrdSExpr : Ord SExpr where
+  compare e₁ e₂ := compare e₁.val e₂.val
+
+-- Fake constructor
+noncomputable def SExpr.leaf : SExpr where
+  val := .leaf
+  property := SExpr.WF.leaf
+
+noncomputable def SExpr.app (f : SExpr) (es : Std.TreeMap SExpr Unit) : SExpr where
+  val := SExpr.Raw.app (PropSplit.data f) (PropSplit.data es)
+  property := SExpr.WF.app (PropSplit.prop f) (PropSplit.prop es)
+
+-- Fake recursor
+
+section rec
+
+/--
+info: recursor SExpr.Raw.rec.{u} : {motive_1 : SExpr.Raw → Sort u} →
+  {motive_2 : Std.TreeMap.Raw' SExpr.Raw Unit → Sort u} →
+    {motive_3 : (Std.DTreeMap.Internal.Impl SExpr.Raw fun x => Unit) → Sort u} →
+      motive_1 SExpr.Raw.leaf →
+        ((a : SExpr.Raw) →
+            (a_1 : Std.TreeMap.Raw' SExpr.Raw Unit) → motive_1 a → motive_2 a_1 → motive_1 (SExpr.Raw.app a a_1)) →
+          ((inner : Std.DTreeMap.Internal.Impl SExpr.Raw fun x => Unit) →
+              motive_3 inner → motive_2 { inner := inner }) →
+            ((size : Nat) →
+                (k : SExpr.Raw) →
+                  (v : (fun x => Unit) k) →
+                    (l r : Std.DTreeMap.Internal.Impl SExpr.Raw fun x => Unit) →
+                      motive_1 k → motive_3 l → motive_3 r → motive_3 (Std.DTreeMap.Internal.Impl.inner size k v l r)) →
+              motive_3 Std.DTreeMap.Internal.Impl.leaf → (t : SExpr.Raw) → motive_1 t
+-/
+#guard_msgs in
+#print SExpr.Raw.rec
+
+variable
+  {motive_1 : SExpr → Sort u}
+  {motive_2 : Std.TreeMap SExpr Unit → Sort u}
+  {motive_3 : Std.DTreeMap.Internal.Impl SExpr (fun _ => Unit) → Sort u}
+  (leaf : motive_1 SExpr.leaf)
+  (app : (a : SExpr) → (a_1 : Std.TreeMap SExpr Unit) → motive_1 a → motive_2 a_1 → motive_1 (SExpr.app a a_1))
+  (mk : (inner : Std.DTreeMap.Internal.Impl SExpr (fun _ => Unit)) → (h : Std.DTreeMap.Internal.Impl.WF inner) → motive_3 inner →
+    motive_2 (.mk (.mk inner h)))
+  (node : (size : Nat) → (k : SExpr) → (v : Unit) →
+    (l r : Std.DTreeMap.Internal.Impl SExpr (fun _ => Unit)) →
+    motive_1 k → motive_3 l → motive_3 r → motive_3 (Std.DTreeMap.Internal.Impl.inner size k v l r))
+  (tleaf : motive_3 Std.DTreeMap.Internal.Impl.leaf)
+
+noncomputable def SExpr.rec' : (t : SExpr) → motive_1 t :=
+  let go : (x : SExpr.Raw) → (h : SExpr.WF x) → motive_1 (PropSplit.join x h) :=
+    @SExpr.Raw.rec
+      (fun x => ∀ h, motive_1 (PropSplit.join x h))
+      (fun x => ∀ h, motive_2 (PropSplit.join x h))
+      (fun x => ∀ (h : Std.DTreeMap.Internal.Impl.Prop _ _ x), motive_3 (PropSplit.join x h))
+      (fun h => leaf)
+      (fun f es ih1 ih2 h => by
+        simpa [SExpr.app, PropSplit.split_join] using
+          app (PropSplit.join f (match h with | SExpr.WF.app h _ => h))
+              (PropSplit.join es (match h with | SExpr.WF.app _ h => h))
+              (ih1 (match h with | SExpr.WF.app h _ => h))
+              (ih2 (match h with | SExpr.WF.app _ h => h)))
+      (fun inner ih h => by
+        simpa [PropSplit.split_join] using
+          mk
+            (PropSplit.join inner (match h with | .mk _ h => h))
+            _
+            (ih _)
+      )
+      (fun size k v l r pk pl pr h => by
+        simpa [PropSplit.split_join] using
+          node size
+            (PropSplit.join k (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ h _ _ _ => h))
+            v
+            (PropSplit.join l (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ h _ => h))
+            (PropSplit.join r (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ _ h => h))
+            (pk _)
+            (pl _)
+            (pr _))
+      (fun h => tleaf)
+  fun x => by simpa [PropSplit.join_split] using go (PropSplit.data x) (PropSplit.prop x)
+
+noncomputable def SExpr.rec_1' : (x : Std.TreeMap SExpr Unit) → motive_2 x :=
+  let go : (x : Std.TreeMap.Raw' SExpr.Raw Unit) → (h : _) → motive_2 (PropSplit.join x h) :=
+    @SExpr.Raw.rec_1
+      (fun x => ∀ h, motive_1 (PropSplit.join x h))
+      (fun x => ∀ h, motive_2 (PropSplit.join x h))
+      (fun x => ∀ (h : Std.DTreeMap.Internal.Impl.Prop _ _ x), motive_3 (PropSplit.join x h))
+      (fun h => leaf)
+      (fun f es ih1 ih2 h => by
+        simpa [SExpr.app, PropSplit.split_join] using
+          app (PropSplit.join f (match h with | SExpr.WF.app h _ => h))
+              (PropSplit.join es (match h with | SExpr.WF.app _ h => h))
+              (ih1 (match h with | SExpr.WF.app h _ => h))
+              (ih2 (match h with | SExpr.WF.app _ h => h)))
+      (fun inner ih h => by
+        simpa [PropSplit.split_join] using
+          mk
+            (PropSplit.join inner (match h with | .mk _ h => h))
+            _
+            (ih _)
+      )
+      (fun size k v l r pk pl pr h => by
+        simpa [PropSplit.split_join] using
+          node size
+            (PropSplit.join k (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ h _ _ _ => h))
+            v
+            (PropSplit.join l (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ h _ => h))
+            (PropSplit.join r (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ _ h => h))
+            (pk _)
+            (pl _)
+            (pr _))
+      (fun h => tleaf)
+  fun x => by simpa [PropSplit.join_split] using go (PropSplit.data x) (PropSplit.prop x)
+
+noncomputable def SExpr.rec_2' : (x : _) → motive_3 x :=
+  let go : (x : _) → (h : _) → motive_3 (PropSplit.join x h) :=
+    @SExpr.Raw.rec_2
+      (fun x => ∀ h, motive_1 (PropSplit.join x h))
+      (fun x => ∀ h, motive_2 (PropSplit.join x h))
+      (fun x => ∀ (h : Std.DTreeMap.Internal.Impl.Prop _ _ x), motive_3 (PropSplit.join x h))
+      (fun h => leaf)
+      (fun f es ih1 ih2 h => by
+        simpa [SExpr.app, PropSplit.split_join] using
+          app (PropSplit.join f (match h with | SExpr.WF.app h _ => h))
+              (PropSplit.join es (match h with | SExpr.WF.app _ h => h))
+              (ih1 (match h with | SExpr.WF.app h _ => h))
+              (ih2 (match h with | SExpr.WF.app _ h => h)))
+      (fun inner ih h => by
+        simpa [PropSplit.split_join] using
+          mk
+            (PropSplit.join inner (match h with | .mk _ h => h))
+            _
+            (ih _)
+      )
+      (fun size k v l r pk pl pr h => by
+        simpa [PropSplit.split_join] using
+          node size
+            (PropSplit.join k (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ h _ _ _ => h))
+            v
+            (PropSplit.join l (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ h _ => h))
+            (PropSplit.join r (match h with | Std.DTreeMap.Internal.Impl.Prop.inner _ _ _ _ _ _ _ _ h => h))
+            (pk _)
+            (pl _)
+            (pr _))
+      (fun h => tleaf)
+  fun x => by simpa [PropSplit.join_split] using go (PropSplit.data x) (PropSplit.prop x)
+
+theorem SExpr.rec'_eq1 :
+    SExpr.rec' leaf app mk node tleaf SExpr.leaf = leaf := by
+  rfl
+
+theorem SExpr.rec'_eq2 :
+    SExpr.rec' leaf app mk node tleaf (SExpr.app f es) =
+      app f es (SExpr.rec' leaf app mk node tleaf f)
+               (SExpr.rec_1' leaf app mk node tleaf es)
+      := by
+  unfold SExpr.rec' SExpr.rec_1' SExpr.app
+  simp [PropSplit.data]
+  simp only [cast_eq_iff_heq]
+  apply heq_motive_f_congr2_2
+  · exact PropSplit.join_split f
+  · exact PropSplit.join_split es
+  · rfl
+  · simp only [heq_cast_iff_heq, heq_eq_eq]
+
+theorem SExpr.rec_1'_eq1 :
+    SExpr.rec_1' leaf app mk node tleaf (.mk (.mk inner h)) =
+      mk inner h
+        (SExpr.rec_2' leaf app mk node tleaf inner)
+      := by
+  unfold SExpr.rec_1' SExpr.rec_2'
+  simp [PropSplit.data]
+  simp only [cast_eq_iff_heq]
+  apply heq_motive_f_congr1_1_1
+    (ih := fun x _ => motive_3 x)
+  · exact PropSplit.join_split inner
+  · apply HEq_Prop
+    congr
+    simp [PropSplit.join, Std.DTreeMap.Internal.Impl.join_split]
+  · simp only [heq_cast_iff_heq]
+    rfl

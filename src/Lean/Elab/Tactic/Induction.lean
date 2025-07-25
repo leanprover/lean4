leanprover/lean4

commit dd848292822f7a724773cdbe7baf96467c991b8c

feat: allow omission of `=> ?_` in induction/cases tactics (#7830)
This PR modifies the syntax of `induction`, `cases`, and other tactics
that use `Lean.Parser.Tactic.inductionAlts`. If a case omits `=> ...`
then it is assumed to be `=> ?_`. Example:
```lean
example (p : Nat × Nat) : p.1 = p.1 := by
  cases p with | _ p1 p2
  /-
  case mk
  p1 p2 : Nat
  ⊢ (p1, p2).fst = (p1, p2).fst
  -/
```
This works with multiple cases as well. Example:
```lean
example (n : Nat) : n + 1 = 1 + n := by
  induction n with | zero | succ n ih
  /-
  case zero
  ⊢ 0 + 1 = 1 + 0
  
  case succ
  n : Nat
  ih : n + 1 = 1 + n
  ⊢ n + 1 + 1 = 1 + (n + 1)
  -/
```
The `induction n with | zero | succ n ih` is short for `induction n with
| zero | succ n ih => ?_`, which is short for `induction n with | zero
=> ?_ | succ n ih => ?_`. Note that a consequence of parsing is that
only the last alternative can omit `=>`. Any `=>`-free alternatives
before an alternative with `=>` will be a part of that alternative.

Rationale:
- In the future we may require `tacticSeq` to be indented. For
one-constructor types, this lets the rest of the tactic sequence not
need indentation.
- This is a semi-structured alternative to the `cases'`/`induction'`
tactics in mathlib.

src/Lean/Elab/Tactic/Induction.lean
@@ -26,8 +26,10 @@ open Meta
   Given an `inductionAlt` of the form
   ```
   syntax inductionAltLHS := "| " (group("@"? ident) <|> hole) (ident <|> hole)*
-  syntax inductionAlt  := ppDedent(ppLine) inductionAltLHS+ " => " (hole <|> syntheticHole <|> tacticSeq)
+  syntax inductionAlt  := ppDedent(ppLine) inductionAltLHS+ (" => " (hole <|> syntheticHole <|> tacticSeq))?
   ```
+  We assume that the syntax has been expanded. There is exactly one `inductionAltLHS`,
+  and `" => " (hole <|> syntheticHole <|> tacticSeq)` is present
 -/
 private def getAltLhses (alt : Syntax) : Syntax :=
   alt[0]
@@ -50,23 +52,11 @@ private def getAltVars (alt : Syntax) : Array Syntax :=
   let lhs := getFirstAltLhs alt
   lhs[2].getArgs
 private def hasAltRHS (alt : Syntax) : Bool :=
-  if !alt[1].isOfKind nullKind then
-    -- TODO(kmill) Bootstrapping workaround. Delete case in #7830.
-    true
-  else
-    alt[1].getNumArgs > 0
+  alt[1].getNumArgs > 0
 private def getAltRHS (alt : Syntax) : Syntax :=
-  if !alt[1].isOfKind nullKind then
-    -- TODO(kmill) Bootstrapping workaround. Delete case in #7830.
-    alt[2]
-  else
-    alt[1][1]
+  alt[1][1]
 private def getAltDArrow (alt : Syntax) : Syntax :=
-  if !alt[1].isOfKind nullKind then
-    -- TODO(kmill) Bootstrapping workaround. Delete case in #7830.
-    alt[1]
-  else
-    alt[1][0]
+  alt[1][0]
 
 -- Return true if `stx` is a term occurring in the RHS of the induction/cases tactic
 def isHoleRHS (rhs : Syntax) : Bool :=
@@ -78,26 +68,23 @@ def evalAlt (mvarId : MVarId) (alt : Syntax) (addInfo : TermElabM Unit) : Tactic
   else
     let rhs := getAltRHS alt
     withCaseRef (getAltDArrow alt) rhs do
-      if isHoleRHS rhs then
-        addInfo
-        let goals ← getGoals
+      let goals ← getGoals
+      setGoals []
+      try
         setGoals [mvarId]
-        mvarId.withContext <| withTacticInfoContext (mkNullNode #[getAltLhses alt, rhs]) do
-          let mvarDecl ← mvarId.getDecl
-          let val ← elabTermEnsuringType rhs mvarDecl.type
-          mvarId.assign val
-          let gs' ← getMVarsNoDelayed val
-          tagUntaggedGoals mvarDecl.userName `induction gs'.toList
-          setGoals <| goals ++ gs'.toList
-      else
-        let goals ← getGoals
-        try
-          setGoals [mvarId]
-          closeUsingOrAdmit <|
-            withTacticInfoContext (mkNullNode #[getAltLhses alt, getAltDArrow alt]) <|
-              (addInfo *> evalTactic rhs)
-        finally
-          setGoals goals
+        withTacticInfoContext (mkNullNode #[getAltLhses alt, getAltDArrow alt]) do
+          addInfo
+          if isHoleRHS rhs then
+            mvarId.withContext do
+              let mvarDecl ← mvarId.getDecl
+              -- Elaborate ensuring that `_` is interpreted as `?_`.
+              let (val, gs') ← elabTermWithHoles rhs mvarDecl.type `induction (parentTag? := mvarDecl.userName) (allowNaturalHoles := true)
+              mvarId.assign val
+              setGoals gs'
+          else
+            closeUsingOrAdmit <| evalTactic rhs
+      finally
+        pushGoals goals
 
 /-!
   Helper method for creating an user-defined eliminator/recursor application.
@@ -451,7 +438,8 @@ where
     -- all previous alternatives have to be unchanged for reuse
     Term.withNarrowedArgTacticReuse (stx := mkNullNode altStxs) (argIdx := altStxIdx) fun altStx => do
     -- everything up to rhs has to be unchanged for reuse
-    Term.withNarrowedArgTacticReuse (stx := altStx) (argIdx := 2) fun _rhs => do
+    Term.withNarrowedArgTacticReuse (stx := altStx) (argIdx := 1) fun rhs? => do
+    Term.withNarrowedArgTacticReuse (stx := rhs?) (argIdx := 1) fun _rhs => do
     -- disable reuse if rhs is run multiple times
     Term.withoutTacticIncrementality (altMVarIds.length != 1 || isWildcard altStx) do
       for altMVarId' in altMVarIds do
@@ -555,8 +543,7 @@ Returns true if the `Lean.Parser.Tactic.inductionAlt` either has more than one a
 or has no RHS.
 -/
 private def shouldExpandAlt (alt : Syntax) : Bool :=
-  -- TODO(kmill) enable second case in #7830
-  alt[0].getNumArgs > 1 || (false && 1 < alt.getNumArgs && alt[1].getNumArgs == 0)
+  alt[0].getNumArgs > 1 || (1 < alt.getNumArgs && alt[1].getNumArgs == 0)
 
 /--
 Returns `some #[alt_1, ..., alt_n]` if `alt` has multiple LHSs or if `alt` has no RHS.
@@ -564,13 +551,12 @@ If there is no RHS, it is filled in with a hole.
 -/
 private def expandAlt? (alt : Syntax) : Option (Array Syntax) := Id.run do
   if shouldExpandAlt alt then
-    let alt :=
-      -- TODO(kmill) enable case in #7830
-      if false && 1 < alt.getNumArgs && alt[1].getNumArgs == 0 then
-        alt.setArg 1 <| mkNullNode #[mkAtomFrom alt "=>", mkHole alt]
+    some <| alt[0].getArgs.map fun lhs =>
+      let alt := alt.setArg 0 (mkNullNode #[lhs])
+      if 1 < alt.getNumArgs && alt[1].getNumArgs == 0 then
+        alt.setArg 1 <| mkNullNode #[mkAtomFrom lhs "=>", mkHole lhs]
       else
         alt
-    some <| alt[0].getArgs.map fun lhs => alt.setArg 0 (mkNullNode #[lhs])
   else
     none
 
@@ -579,8 +565,8 @@ Given `inductionAlts` of the form
 ```
 syntax inductionAlts := "with " (tactic)? withPosition( (colGe inductionAlt)*)
 ```
-Return `some inductionAlts'` if one of the alternatives have multiple LHSs, in the new `inductionAlts'`
-all alternatives have a single LHS.
+Return `some inductionAlts'` if one of the alternatives has multiple LHSs or no RHS.
+In the new `inductionAlts'` all alternatives have a single LHS.
 
 Remark: the `RHS` of alternatives with multi LHSs is copied.
 -/

src/Init/Tactics.lean
@@ -819,12 +819,12 @@ The left hand side of an induction arm, `| foo a b c` or `| @foo a b c`
 where `foo` is a constructor of the inductive type and `a b c` are the arguments
 to the constructor.
 -/
-syntax inductionAltLHS := "| " (("@"? ident) <|> hole) (ident <|> hole)*
+syntax inductionAltLHS := withPosition("| " (("@"? ident) <|> hole) (colGt (ident <|> hole))*)
 /--
 In induction alternative, which can have 1 or more cases on the left
 and `_`, `?_`, or a tactic sequence after the `=>`.
 -/
-syntax inductionAlt  := ppDedent(ppLine) inductionAltLHS+ " => " (hole <|> syntheticHole <|> tacticSeq)
+syntax inductionAlt  := ppDedent(ppLine) inductionAltLHS+ (" => " (hole <|> syntheticHole <|> tacticSeq))?
 /--
 After `with`, there is an optional tactic that runs on all branches, and
 then a list of alternatives.

tests/lean/run/induction1.lean
@@ -142,7 +142,7 @@ h : (xs ++ ys ++ zs).length = n + 1
 example {α : Type} (xs ys zs : List α) : (xs ++ ys) ++ zs = xs ++ (ys ++ zs) := by
   induction h : ((xs ++ ys) ++ zs).length generalizing xs with
   | zero =>
-    simp only [List.length_append, Nat.add_eq_zero_iff, List.length_eq_zero] at h
+    simp only [List.length_append, Nat.add_eq_zero_iff, List.length_eq_zero_iff] at h
     obtain ⟨⟨rfl, rfl⟩, rfl⟩ := h
     rfl
   | succ n ih =>
@@ -211,4 +211,44 @@ example (n : Nat) : n + 1 = 1 + n := by
   induction _ : n + 1
   trace_state
   omega
+  omega
+
+/-!
+Having no `=>` clause is short for `=> ?_`.
+-/
+/--
+info: case mk
+p1 p2 : Nat
+⊢ (p1, p2).fst = (p1, p2).fst
+-/
+#guard_msgs in
+example (p : Nat × Nat) : p.1 = p.1 := by
+  cases p with | _ p1 p2
+  trace_state
+  rfl
+
+/-!
+Can have multiple trailing `=>`-free goals. This is short for
+```
+induction n with | zero | succ n ih => ?_
+```
+which is short for
+```
+induction n with | zero => ?_ | succ n ih => ?_
+```
+-/
+/--
+info: case zero
+⊢ 0 + 1 = 1 + 0
+
+case succ
+n : Nat
+ih : n + 1 = 1 + n
+⊢ n + 1 + 1 = 1 + (n + 1)
+-/
+#guard_msgs in
+example (n : Nat) : n + 1 = 1 + n := by
+  induction n with | zero | succ n ih
+  trace_state
+  rfl
   omega

commit be6ba0366918b38a99427f2419786153d46e8a81

fix: erroneous "no goals" in empty tactic list in `induction`
This PR fixes a bug introduced in #7830 where if the cursor is at the indicated position
```lean
example (as bs : List Nat) : (as.append bs).length = as.length + bs.length := by
  induction as with
  | nil => -- cursor
  | cons b bs ih =>
```
then the Infoview would show "no goals".

src/Lean/Elab/Tactic/Induction.lean
@@ -74,20 +74,22 @@ def evalAlt (mvarId : MVarId) (alt : Syntax) (addInfo : TermElabM Unit) : Tactic
     let rhs := getAltRHS alt
     withCaseRef (getAltDArrow alt) rhs do
       let goals ← getGoals
-      setGoals []
       try
         setGoals [mvarId]
-        withTacticInfoContext (mkNullNode #[getAltLhses alt, getAltDArrow alt]) do
-          addInfo
-          if isHoleRHS rhs then
-            mvarId.withContext do
-              let mvarDecl ← mvarId.getDecl
-              -- Elaborate ensuring that `_` is interpreted as `?_`.
-              let (val, gs') ← elabTermWithHoles rhs mvarDecl.type `induction (parentTag? := mvarDecl.userName) (allowNaturalHoles := true)
-              mvarId.assign val
-              setGoals gs'
-          else
-            closeUsingOrAdmit <| evalTactic rhs
+        let withInfo (m : TacticM Unit) :=
+          withTacticInfoContext (mkNullNode #[getAltLhses alt, getAltDArrow alt]) do
+            addInfo; m
+        if isHoleRHS rhs then
+          withInfo <| mvarId.withContext do
+            let mvarDecl ← mvarId.getDecl
+            -- Elaborate ensuring that `_` is interpreted as `?_`.
+            let (val, gs') ← elabTermWithHoles rhs mvarDecl.type `induction (parentTag? := mvarDecl.userName) (allowNaturalHoles := true)
+            mvarId.assign val
+            setGoals gs'
+        else
+          -- Admit remaining goals outside the TacticInfo so that the infoview
+          -- can't report "no goals" in incomplete proofs.
+          closeUsingOrAdmit <| withInfo <| evalTactic rhs
       finally
         pushGoals goals
 
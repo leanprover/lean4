0c83c5a3d1a7cfa95ea25ed3edddaed2bb1d7256
Marcus Rossel <marcusrossel@icloud.com>
2025-07-25 09:43:39 +0200

    Add test for global constants

--- tests/lean/run/grind_local_hyps.lean
+++ tests/lean/run/grind_local_hyps.lean
@@ -1,6 +1,19 @@
 /-! This test ensures that local declarations explicitly passed to `grind` produce an appropriate
     error message, instead of just `unknown constant '...'`. -/
 
+-- Checks that (invalid) identifiers which are not local declarations are still elaborated as global
+-- constants.
+/-- error: unknown constant 'h' -/
+#guard_msgs in
+example : 0 = 0 := by
+  grind [h]
+
+-- Checks that local hypotheses do not shadow global constants.
+def P := 0 = 0
+example : 0 = 0 := by
+  have P : 0 = 0 := rfl
+  grind [P]
+
 /-- error: redundant parameter `h`, `grind` uses local hypotheses automatically -/
 #guard_msgs in
 example : 0 = 0 := by

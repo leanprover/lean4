4ff5e275ef86a675387689a382b03f24f8b48499
Sofia Rodrigues <sofia@algebraic.dev>
2025-07-25 13:40:48 -0300

    feat: add same obj comparison

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -2420,14 +2420,18 @@ extern "C" LEAN_EXPORT obj_res lean_byte_array_mk(obj_arg a) {
 }
 
 extern "C" LEAN_EXPORT bool lean_byte_array_beq(obj_arg a, obj_arg b) {
-  size_t size_a = lean_sarray_size(a);
-  size_t size_b = lean_sarray_size(b);
+    if (a == b) {
+        return 1;
+    }
 
-  if (size_a != size_b) {
-    return 0;
-  }
+    size_t size_a = lean_sarray_size(a);
+    size_t size_b = lean_sarray_size(b);
 
-  return memcmp(lean_sarray_cptr(a), lean_sarray_cptr(b), size_a) == 0;
+    if (size_a != size_b) {
+        return 0;
+    }
+
+    return memcmp(lean_sarray_cptr(a), lean_sarray_cptr(b), size_a) == 0;
 }
 
 extern "C" LEAN_EXPORT obj_res lean_byte_array_data(obj_arg a) {

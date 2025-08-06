bd9e1124122b75bf7d37fc06824ae704ba4fd5d4
Cameron Zwarich <cameron@lean-fro.org>
2025-08-05 21:49:25 -0700

    perf: ignore constructor params in mono phase `simp` optimizations

--- src/Lean/Compiler/LCNF/Simp/DiscrM.lean
+++ src/Lean/Compiler/LCNF/Simp/DiscrM.lean
@@ -95,23 +95,36 @@ where
     let ctorVal ← getConstInfoCtor ctorName
     let fieldArgs := ctorFields.map (Arg.fvar ·.fvarId)
     let ctx ← read
-    if let some (us, params) ← getIndInfo? (← getType discr) then
-      let ctorArgs := params ++ fieldArgs
+    match (← getPhase) with
+    | .base =>
+      if let some (us, params) ← getIndInfo? (← getType discr) then
+        let ctorArgs := params ++ fieldArgs
+        let ctorInfo := .ctor ctorVal ctorArgs
+        let ctor := LetValue.const ctorVal.name us ctorArgs
+        return { ctx with discrCtorMap := ctx.discrCtorMap.insert discr ctorInfo, ctorDiscrMap := ctx.ctorDiscrMap.insert ctor.toExpr discr }
+      else
+        -- For the discrCtor map, the constructor parameters are irrelevant for optimizations that use this information
+        let ctorInfo := .ctor ctorVal (.replicate ctorVal.numParams Arg.erased ++ fieldArgs)
+        return { ctx with discrCtorMap := ctx.discrCtorMap.insert discr ctorInfo }
+    | .mono =>
+      let ctorArgs := .replicate ctorVal.numParams Arg.erased ++ fieldArgs
       let ctorInfo := .ctor ctorVal ctorArgs
-      let ctor := LetValue.const ctorVal.name us ctorArgs
+      let ctor := LetValue.const ctorVal.name [] ctorArgs
       return { ctx with discrCtorMap := ctx.discrCtorMap.insert discr ctorInfo, ctorDiscrMap := ctx.ctorDiscrMap.insert ctor.toExpr discr }
-    else
-      -- For the discrCtor map, the constructor parameters are irrelevant for optimizations that use this information
-      let ctorInfo := .ctor ctorVal (.replicate ctorVal.numParams Arg.erased ++ fieldArgs)
-      return { ctx with discrCtorMap := ctx.discrCtorMap.insert discr ctorInfo }
+
 
 @[inline, inherit_doc withDiscrCtorImp] def withDiscrCtor [MonadFunctorT DiscrM m] (discr : FVarId) (ctorName : Name) (ctorFields : Array Param) : m α → m α :=
   monadMap (m := DiscrM) <| withDiscrCtorImp discr ctorName ctorFields
 
 def simpCtorDiscrCore? (e : Expr) : DiscrM (Option FVarId) := do
   let some discr := (← read).ctorDiscrMap.find? e | return none
-  unless eqvTypes (← getType discr) (← inferType e) do return none
-  return some <| discr
+  match (← getPhase) with
+  | .base =>
+    if eqvTypes (← getType discr) (← inferType e) then
+      return some discr
+    else
+      return none
+  | .mono => return some discr
 
 end Simp
 end Lean.Compiler.LCNF

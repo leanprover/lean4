leanprover/lean4

commit 68a249d23dfc8de727c0d2b637a68636973dfa6b

perf: `normalizeLevels` in `grind` (#9814)
This PR skips the `normalizeLevels` preprocessing step in `grind` when
it is not needed.

src/Lean/Level.lean
@@ -288,7 +288,7 @@ def normLtAux : Level → Nat → Level → Nat → Bool
 def normLt (l₁ l₂ : Level) : Bool :=
   normLtAux l₁ 0 l₂ 0
 
-private def isAlreadyNormalizedCheap : Level → Bool
+def isAlreadyNormalizedCheap : Level → Bool
   | zero    => true
   | param _ => true
   | mvar _  => true

src/Lean/Meta/Tactic/Grind/Util.lean
@@ -182,10 +182,18 @@ def foldProjs (e : Expr) : MetaM Expr := do
       return .done e
   Meta.transform e (post := post)
 
+/-- Quick filter for checking whether we can skip `normalizeLevels`. -/
+private def levelsAlreadyNormalized (e : Expr) : Bool :=
+  Option.isNone <| e.find? fun
+    | .const _ us => us.any (! ·.isAlreadyNormalizedCheap)
+    | .sort u => !u.isAlreadyNormalizedCheap
+    | _ => false
+
 /--
 Normalizes universe levels in constants and sorts.
 -/
 def normalizeLevels (e : Expr) : CoreM Expr := do
+  if levelsAlreadyNormalized e then return e
   let pre (e : Expr) := do
     match e with
     | .sort u => return .done <| e.updateSort! u.normalize

commit 7b6809e11937dcf6ea1ae022c807c99615899fb8

chore: add (failing) grind test for a regression from #9814

tests/lean/grind/match.lean
@@ -1,0 +1,13 @@
+
+abbrev F : Fin 3 → Nat
+| 0 => 0
+| 1 => 1
+| 2 => 2
+
+abbrev Pairwise (r : α → α → Prop) :=
+  ∀ ⦃i j⦄, i ≠ j → r i j
+
+abbrev onFun (f : β → β → φ) (g : α → β) : α → α → φ := fun x y => f (g x) (g y)
+
+example : Pairwise (onFun (fun a b => a + b < 10) fun x ↦ F x) := by
+  grind
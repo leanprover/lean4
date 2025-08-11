2f6db6ab2856e1b0920dca97451cf8b183d1264d
luisacicolini <luisacicolini@gmail.com>
2025-08-11 15:08:28 +0100

    chore: add testing sripts for repro

---
+++ test.lean
@@ -0,0 +1,20 @@
+import Std.Tactic.BVDecide
+
+def test : IO Unit :=
+  have w := 8
+  for xx in [0 : 2^w] do
+    have x := BitVec.ofNat w xx
+    let mut pop := 0
+    for bit in [0 : w] do
+      if x.getLsbD bit = true then pop := pop + 1
+
+    IO.print f!"sum of set bits is {pop} = {x.popCountParSum}"
+    if pop ≠ x.popCount then
+      IO.println "FAIL"
+    else
+      IO.println " OK"
+
+
+    IO.println ""
+
+#eval test

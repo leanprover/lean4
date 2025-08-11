2f902abeb25c84eda701c922fad65d072862b61d
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-08-11 15:14:22 +0200

    restore after rebasing

--- src/Init/Data/Order/FactoriesExtra.lean
+++ src/Init/Data/Order/FactoriesExtra.lean
@@ -62,7 +62,7 @@ public class LinearPreorderPackage (α : Type u) extends
 
 public structure Packages.LinearPreorderOfLEArgs (α : Type u) extends
     PreorderOfLEArgs α where
-  ord [i : OrderData α] (hi : i = orderData := by rfl) : Ord α := by
+  ord [i : LE α] [DecidableLE α] (hi : i = le := by rfl) : Ord α := by
     first
       | infer_instance
       | exact fun _ => Instances.instOrdOfDecidableLE
@@ -71,13 +71,13 @@ public structure Packages.LinearPreorderOfLEArgs (α : Type u) extends
       | exact Total.total
       | fail "Failed to automatically prove that the `LE` instance is total. You can either ensure that a `Total` instance is available or manually provide the `le_total` field."
   le_refl := (by intro a; simpa using le_total a a)
-  compare_isLE : letI := orderData; letI := ord; ∀ a b : α, (compare a b).isLE ↔ a ≤ b := by
+  compare_isLE : letI := ord; ∀ a b : α, (compare a b).isLE ↔ a ≤ b := by
     first
       | open scoped Classical in
         simpa only [Instances.instOrdOfDecidableLE, Ord.compare] using fun a b => Instances.compare_isLE
         done
       | fail "Failed to automatically prove that `(compare a b).isLE` is equivalent to `a ≤ b`."
-  compare_isGE (le_total := le_total) : letI := orderData; letI := ord; ∀ a b : α, (compare a b).isGE ↔ b ≤ a := by
+  compare_isGE (le_total := le_total) : letI := ord; ∀ a b : α, (compare a b).isGE ↔ b ≤ a := by
     first
       | open scoped Classical in
         simpa only [Instances.instOrdOfDecidableLE, Ord.compare] using
@@ -89,10 +89,10 @@ public structure Packages.LinearPreorderOfLEArgs (α : Type u) extends
 public def LinearPreorderPackage.ofLE (α : Type u)
     (args : Packages.LinearPreorderOfLEArgs α := by exact {}) : LinearPreorderPackage α where
   toPreorderPackage := .ofLE α args.toPreorderOfLEArgs
-  toOrd := letI := args.le; letI := args.orderData; args.ord
-  le_total := by simpa [args.lawful_orderData] using args.le_total
-  compare_isLE := by simpa [args.lawful_orderData] using args.compare_isLE
-  compare_isGE := by simpa [args.lawful_orderData] using args.compare_isGE
+  toOrd := letI := args.le; letI := args.decidableLE; args.ord
+  le_total := args.le_total
+  compare_isLE := args.compare_isLE
+  compare_isGE := args.compare_isGE
 
 namespace Instances
 
@@ -134,51 +134,51 @@ public class LinearOrderPackage (α : Type u) extends
 
 public structure Packages.LinearOrderOfLEArgs (α : Type u) extends
     LinearPreorderOfLEArgs α, PartialOrderOfLEArgs α where
-  min [i : OrderData α] (hi : i = orderData := by rfl) : Min α := by
+  min [i : LE α] [DecidableLE α] (hi : i = le := by rfl) : Min α := by
     first
       | infer_instance
       | exact fun _ => Instances.instMinOfDecidableLE
-  max [i : OrderData α] (hi : i = orderData := by rfl) : Max α := by
+  max [i : LE α] [DecidableLE α] (hi : i = le := by rfl) : Max α := by
     first
       | infer_instance
       | exact fun _ => Instances.instMaxOfDecidableLE
   min_eq :
-      letI := le; letI := orderData; letI := min
+      letI := le; letI := min
       ∀ a b : α, Min.min a b = if a ≤ b then a else b := by
     first
       | infer_instance
       | exact fun _ _ => Instances.min_eq
   max_eq :
-      letI := le; letI := orderData; letI := max
+      letI := le; letI := max
       ∀ a b : α, Max.max a b = if a ≤ b then b else a := by
     first
       | infer_instance
       | exact fun _ _ => Instances.max_eq
 
-public theorem IsLinearPreorder.lawfulOrderMin_of_min_eq {α : Type u} [OrderData α] [LE α]
-    [DecidableLE α] [Min α] [IsLinearPreorder α] [LawfulOrderLE α]
+public theorem IsLinearPreorder.lawfulOrderMin_of_min_eq {α : Type u} [LE α]
+    [DecidableLE α] [Min α] [IsLinearPreorder α]
     (min_eq : ∀ a b : α, min a b = if a ≤ b then a else b) :
     LawfulOrderMin α where
   min_eq_or a b := by
     rw [min_eq]
     split <;> simp
   le_min_iff a b c := by
-    simp only [← LawfulOrderLE.le_iff, min_eq]
+    simp only [min_eq]
     split <;> rename_i hbc
     · simp only [iff_self_and]
       exact fun hab => le_trans hab hbc
     · simp only [iff_and_self]
       exact fun hac => le_trans hac (by simpa [hbc] using Std.le_total (a := b) (b := c))
 
-public theorem IsLinearPreorder.lawfulOrderMax_of_max_eq {α : Type u} [OrderData α] [LE α]
-    [DecidableLE α] [Max α] [IsLinearPreorder α] [LawfulOrderLE α]
+public theorem IsLinearPreorder.lawfulOrderMax_of_max_eq {α : Type u} [LE α]
+    [DecidableLE α] [Max α] [IsLinearPreorder α]
     (max_eq : ∀ a b : α, max a b = if a ≤ b then b else a) :
     LawfulOrderMax α where
   max_eq_or a b := by
     rw [max_eq]
     split <;> simp
   max_le_iff a b c := by
-    simp only [← LawfulOrderLE.le_iff, max_eq]
+    simp only [max_eq]
     split <;> rename_i hab
     · simp only [iff_and_self]
       exact fun hbc => le_trans hab hbc
@@ -190,8 +190,8 @@ public def LinearOrderPackage.ofLE (α : Type u)
     (args : Packages.LinearOrderOfLEArgs α := by exact {}) : LinearOrderPackage α where
   toLinearPreorderPackage := .ofLE α args.toLinearPreorderOfLEArgs
   le_antisymm := (PartialOrderPackage.ofLE α args.toPartialOrderOfLEArgs).le_antisymm
-  toMin := letI := args.le; letI := args.orderData; args.min
-  toMax := letI := args.le; letI := args.orderData; args.max
+  toMin := letI := args.le; letI := args.decidableLE; args.min
+  toMax := letI := args.le; letI := args.decidableLE; args.max
   toLawfulOrderMin :=
     letI := LinearPreorderPackage.ofLE α args.toLinearPreorderOfLEArgs
     letI := args.decidableLE; letI := args.min

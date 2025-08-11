d2aef045d8ec0717aaeb2f719924bafb13739c65
luisacicolini <luisacicolini@gmail.com>
2025-08-11 16:32:49 +0100

    chore: blastExtractAndExtend_denote_eq

--- src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Operations/PopCount.lean
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Operations/PopCount.lean
@@ -11,6 +11,8 @@ public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Impl.Operations.PopCou
 public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Const
 public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Operations.Sub
 public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Operations.Eq
+public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Operations.ZeroExtend
+public import Std.Tactic.BVDecide.Bitblast.BVExpr.Circuit.Lemmas.Operations.Extract
 
 @[expose] public section
 
@@ -33,38 +35,59 @@ namespace BVExpr
 namespace bitblast
 namespace blastPopCount
 
-theorem go_denote_eq {w : Nat} (aig : AIG α) (h : curr ≤ w)
-    (acc : AIG.RefVec aig w) (xc : AIG.RefVec aig w) (x : BitVec w) (assign : α → Bool)
-    (hx : ∀ (idx : Nat) (hidx : idx < w), ⟦aig, xc.get idx hidx, assign⟧ = x.getLsbD idx)
-    (parSum : RefVecVec aig w inputNodes) (hw : NeZero w) (hw' : inputNodes ≤ w) (hw'' : 0 < inputNodes)
-    (hacc : ∀ (idx : Nat) (hidx : idx < w),
-      if curr = 0 then ⟦aig, acc.get idx hidx, assign⟧ = (BitVec.ofNat w 0).getLsbD idx
-      else ⟦aig, acc.get idx hidx, assign⟧ = (BitVec.popCountAuxRec x 0#w curr).getLsbD idx)
-    :
-    ∀ (idx : Nat) (hidx : idx < w),
-        ⟦
-          (go aig w inputNodes parSum hw hw' hw'').aig,
-          (go aig w inputNodes parSum hw hw' hw'').vec.get idx hidx,
-          assign
-        ⟧
-          =
-        (BitVec.popCountAuxRec x 0 w).getLsbD idx := by sorry
---     intro idx hidx
---     generalize hgo: go aig xc curr acc = res
---     unfold go at hgo
---     split at hgo
---     · case isTrue h =>
---       simp at hgo
---       split at hacc
---       · simp [show curr = 0 by omega] at *
---         intros
---         sorry
---       · sorry
---     · case isFalse h =>
---       sorry
-      -- rw [← hgo]
-      -- simp [show ¬ curr = 0 by omega] at hacc
-      -- simp [hacc]
+theorem blastExtractAndExtend_denote_eq (aig : AIG α) (xc : AIG.RefVec aig w) (x : BitVec w) (start : Nat)
+    (hx : ∀ (idx : Nat) (hidx : idx < w), ⟦aig, xc.get idx hidx, assign⟧ = x.getLsbD idx) :
+  ∀ (idx : Nat) (hidx : idx < w),
+    ⟦
+      (blastExtractAndExtend aig xc start).aig,
+      (blastExtractAndExtend aig xc start).vec.get idx hidx,
+      assign
+     ⟧ = (BitVec.zeroExtend w (BitVec.extractLsb start start x)).getLsbD idx := by
+  intros idx hidx
+  generalize hext: blastExtractAndExtend aig xc start = res
+  unfold blastExtractAndExtend at hext
+  dsimp at hext
+  rw [← hext]
+  simp [denote_blastZeroExtend]
+  split
+  · by_cases hidx0 : idx = 0
+    · simp [hidx0]
+      simp [show 0 < w by omega]
+      apply hx
+    · simp [hidx0]
+  · intros
+    simp [show w ≤ start + idx by omega]
+
+
+theorem blastExtractAndExtendPopulate_denote_eq
+  -- blastExtractAndExtendPopulate inputs
+  (aig : AIG α) (start : Nat) (xc : AIG.RefVec aig w) (x : BitVec w) (acc : RefVecVec aig w start) (hw : start ≤ w)
+  -- denotation
+  (initVec : RefVecVec aig w 0 := {vec : Vector (AIG.RefVec aig w) 0 := Vector.emptyWithCapacity 0})
+  (hx : ∀ (idx : Nat) (hidx : idx < w), ⟦aig, xc.get idx hidx, assign⟧ = x.getLsbD idx)
+  (assign : α → Bool) :
+    ∀ (idx1 : Nat) (hidx1 : idx1 < w),
+      ∀ (idx2 : Nat) (hidx2 : idx2 < w),
+      ⟦
+        (blastExtractAndExtendPopulate aig 0 xc initVec (by omega)).aig,
+        ((blastExtractAndExtendPopulate aig 0 xc initVec (by omega)).vec.vec.get ⟨idx1,hidx1⟩).get idx2 hidx2,
+        assign
+      ⟧ =
+      ((BitVec.extractAndExtendPopulateAux 0 x [] (by omega) (by simp)).val.get ⟨idx1, by
+        have := (BitVec.extractAndExtendPopulateAux 0 x [] (by omega) (by simp)).property
+        simp [this]
+        omega
+        ⟩).getLsbD idx2 := by
+  intros idx1 hidx1 idx2 hidx2
+  generalize hext: blastExtractAndExtendPopulate aig 0 xc initVec (by omega) = res
+  unfold blastExtractAndExtendPopulate at hext
+  dsimp at hext
+  split at hext
+  · case isTrue h =>
+    rw [← hext]
+    dsimp
+    sorry
+  · case isFalse h => omega
 
 end blastPopCount
 

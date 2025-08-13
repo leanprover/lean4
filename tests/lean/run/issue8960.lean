2f9ce4d95c1cc0cbc762104c252767c91176bc89
Joachim Breitner <mail@joachim-breitner.de>
2025-08-13 22:35:07 +0200

    Update tests

--- tests/lean/run/issue8960.lean
+++ tests/lean/run/issue8960.lean
@@ -20,12 +20,11 @@ structure TestThingy (α : Type u) where
   invariant : data ≠ []
 
 /--
-error: (kernel) application type mismatch
-  data ≠ []
-argument has type
-  List Maze'
-but function has type
-  _nested.List_2 → Prop
+error: In argument #2 of constructor Maze'.mk:
+  Invalid occurrence of inductive type `Maze'`, parameter #1 of `TestThingy` is not positive.
+  ⏎
+  Note: That parameter is not positive:
+    Non-positive occurrence of parameter `α` in type of TestThingy.mk
 -/
 #guard_msgs in
 structure Maze' where

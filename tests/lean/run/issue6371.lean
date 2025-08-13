b55e6b7026500d5f62de456a7bc87c27dc5cff2d
Joachim Breitner <mail@joachim-breitner.de>
2025-08-13 22:32:18 +0200

    Move file

--- tests/lean/run/issue6371.lean
+++ tests/lean/run/issue6371.lean
@@ -1,7 +1,10 @@
 set_option inductive.autoPromoteIndices false in
 inductive Foo (A : Type) : (A -> A) → Type where
 
-/-- error: (kernel) unknown constant 'Bar' -/
+/--
+error: In argument #1 of constructor Bar.bar:
+  Invalid occurrence of inductive type `Bar`, must not occur in index of `Foo`
+-/
 #guard_msgs in
 inductive Bar where
   | bar : Foo Bar (fun x => x) → Bar

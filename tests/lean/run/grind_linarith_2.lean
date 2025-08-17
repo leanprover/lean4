a2cae1a7439e2a6c9b57be753ba3b2c15662379a
Leonardo de Moura <leomoura@amazon.com>
2025-08-16 22:15:31 -0700

    chore: fix test

--- tests/lean/run/grind_linarith_2.lean
+++ tests/lean/run/grind_linarith_2.lean
@@ -7,14 +7,14 @@ example [IntModule α] [LE α] [LT α] [Preorder α] [OrderedAdd α] (a b : α)
 
 /--
 trace: [grind.debug.proof] Classical.byContradiction fun h =>
-      let ctx := RArray.branch 1 (RArray.leaf One.one) (RArray.branch 2 (RArray.leaf a) (RArray.leaf b));
-      let rctx := RArray.branch 1 (RArray.leaf a) (RArray.leaf b);
-      let re_1 := (CommRing.Expr.var 0).add (CommRing.Expr.var 1);
-      let re_2 := (CommRing.Expr.var 1).add (CommRing.Expr.var 0);
-      let rp_1 := CommRing.Poly.num 0;
+      let ctx := RArray.leaf One.one;
+      let p_1 := Poly.nil;
       let e_1 := Expr.zero;
       let e_2 := Expr.intMul 0 (Expr.var 0);
-      let p_1 := Poly.nil;
+      let rctx := RArray.branch 1 (RArray.leaf a) (RArray.leaf b);
+      let rp_1 := CommRing.Poly.num 0;
+      let re_1 := (CommRing.Expr.var 0).add (CommRing.Expr.var 1);
+      let re_2 := (CommRing.Expr.var 1).add (CommRing.Expr.var 0);
       diseq_unsat ctx
         (diseq_norm ctx e_2 e_1 p_1 (eagerReduce (Eq.refl true))
           (CommRing.diseq_norm rctx re_1 re_2 rp_1 (eagerReduce (Eq.refl true)) h))

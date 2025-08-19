24cafcd65dabf3ddc4f4573372932352deb2c38a
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-08-19 15:43:29 +0200

    feat: package factories for order typeclasses (#9797)
    This PR provides the means to quickly provide all the order instances
    associated with some high-level order structure (preorder, partial
    order, linear preorder, linear order). This can be done via the factory
    functions `PreorderPackage.ofLE`, `PartialOrderPackage.ofLE`,
    `LinearPreorderPackage.ofLE` and `LinearOrderPackage.ofLE`.

--- src/Init/Data/Order/LemmasExtra.lean
+++ src/Init/Data/Order/LemmasExtra.lean
@@ -145,6 +145,16 @@ public instance LawfulOrderMax.of_ord (α : Type u) [Ord α] [Max α] [LE α] [L
   toLawfulOrderSup := .of_ord α compare_max_isLE_iff
   max_eq_or := max_eq_or
 
+public theorem min_eq_if_isLE_compare {α : Type u} [Ord α] [LE α] {_ : Min α}
+    [LawfulOrderOrd α] [LawfulOrderLeftLeaningMin α] {a b : α} :
+    min a b = if (compare a b).isLE then a else b := by
+  open Classical in simp [min_eq_if, isLE_compare]
+
+public theorem max_eq_if_isGE_compare {α : Type u} [Ord α] [LE α] {_ : Max α}
+    [LawfulOrderOrd α] [LawfulOrderLeftLeaningMax α]
+    {a b : α} : max a b = if (compare a b).isGE then a else b := by
+  open Classical in simp [max_eq_if, isGE_compare]
+
 end Std
 
 namespace Classical.Order

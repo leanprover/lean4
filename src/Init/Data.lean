de493d761dc077bd36f74133fb458e3a65c89bb4
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-08-19 11:58:24 +1000

    feat: upstream definition of Rat from Batteries (#9957)
    This PR upstreams the definition of Rat from Batteries, for use in our
    planned interval arithmetic tactic.
    
    ---------
    
    Co-authored-by: Sebastian Ullrich <sebasti@nullri.ch>

--- src/Init/Data.lean
+++ src/Init/Data.lean
@@ -50,5 +50,6 @@ public import Init.Data.Iterators
 public import Init.Data.Range.Polymorphic
 public import Init.Data.Slice
 public import Init.Data.Order
+public import Init.Data.Rat
 
 public section

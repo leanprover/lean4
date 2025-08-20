09a14c42c4b851afbbfffc00c19f1ccb25b43cc8
Rob23oba <robin.arnez@web.de>
2025-08-20 02:48:28 +0200

    add test

---
+++ tests/lean/run/indPredBelow.lean
@@ -0,0 +1,227 @@
+/-!
+# Tests for `brecOn` on recursive inductive predicates
+-/
+
+/-!
+Transitive closure (see issue 1672)
+-/
+
+inductive TransClosure (r : α → α → Prop) : α → α → Prop
+  | extends : r a b → TransClosure r a b
+  | trans_left : r a b → TransClosure r b c → TransClosure r a c
+
+/--
+info: TransClosure.brecOn.{u_1} {α : Sort u_1} {r : α → α → Prop} {motive : (a a_1 : α) → TransClosure r a a_1 → Prop}
+  {a✝ a✝¹ : α} (t : TransClosure r a✝ a✝¹)
+  (F_1 : ∀ (a a_1 : α) (t : TransClosure r a a_1), TransClosure.below t → motive a a_1 t) : motive a✝ a✝¹ t
+-/
+#guard_msgs in
+#check TransClosure.brecOn
+
+theorem TransClosure.recTest (h : TransClosure r a b) : True :=
+  match h with
+  | .extends _ => trivial
+  | .trans_left _ h => h.recTest
+termination_by structural h
+
+/-!
+Symmetric closure (see issue 4540)
+-/
+
+inductive SymmGen (r : α → α → Prop) : α → α → Prop
+  | rel : ∀ x y, r x y → SymmGen r x y
+  | symm : ∀ x y, SymmGen r x y → SymmGen r y x
+
+/--
+info: SymmGen.brecOn.{u_1} {α : Sort u_1} {r : α → α → Prop} {motive : (a a_1 : α) → SymmGen r a a_1 → Prop} {a✝ a✝¹ : α}
+  (t : SymmGen r a✝ a✝¹) (F_1 : ∀ (a a_1 : α) (t : SymmGen r a a_1), SymmGen.below t → motive a a_1 t) : motive a✝ a✝¹ t
+-/
+#guard_msgs in
+#check SymmGen.brecOn
+
+theorem SymmGen.recTest (h : SymmGen r a b) : True :=
+  match h with
+  | .rel _ _ _ => trivial
+  | .symm _ _ h => h.recTest
+termination_by structural h
+
+/-!
+Basic typing relation
+-/
+
+inductive Ty : Type where
+  | unit : Ty
+  | arr  : Ty → Ty → Ty
+
+inductive Term : Type where
+  | var : Nat → Term
+  | unit : Term
+  | abs : Term → Term
+  | app : Term → Term → Term
+
+inductive Wt : List Ty → Term → Ty → Prop where
+  | var {Γ x} : (h : x < Γ.length) → Wt Γ (.var x) Γ[x]
+  | unit {Γ} : Wt Γ .unit .unit
+  | abs {Γ b A B} : Wt (A :: Γ) b B → Wt Γ (.abs b) (.arr A B)
+  | app {Γ b a A B} : Wt Γ b (.arr A B) → Wt Γ a A → Wt Γ (.app b a) B
+
+/--
+info: Wt.brecOn {motive : (a : List Ty) → (a_1 : Term) → (a_2 : Ty) → Wt a a_1 a_2 → Prop} {a✝ : List Ty} {a✝¹ : Term}
+  {a✝² : Ty} (t : Wt a✝ a✝¹ a✝²)
+  (F_1 : ∀ (a : List Ty) (a_1 : Term) (a_2 : Ty) (t : Wt a a_1 a_2), Wt.below t → motive a a_1 a_2 t) :
+  motive a✝ a✝¹ a✝² t
+-/
+#guard_msgs in
+#check Wt.brecOn
+
+theorem Wt.recTest (h : Wt Γ a A) : True :=
+  match h with
+  | .var _ => trivial
+  | .unit => trivial
+  | .abs h => h.recTest
+  | .app h h' => h.recTest
+termination_by structural h
+
+/-!
+Mutual inductives
+-/
+
+mutual
+
+inductive EvenDist : Nat → Nat → Prop where
+  | zero : EvenDist 0 0
+  | succ_left (h : OddDist a b) : EvenDist a.succ b
+  | succ_right (h : OddDist a b) : EvenDist a b.succ
+
+inductive OddDist : Nat → Nat → Prop where
+  | succ_left (h : EvenDist a b) : OddDist a.succ b
+  | succ_right (h : EvenDist a b) : OddDist a b.succ
+
+end
+
+/--
+info: EvenDist.brecOn {motive_1 : (a a_1 : Nat) → EvenDist a a_1 → Prop} {motive_2 : (a a_1 : Nat) → OddDist a a_1 → Prop}
+  {a✝ a✝¹ : Nat} (t : EvenDist a✝ a✝¹) (F_1 : ∀ (a a_1 : Nat) (t : EvenDist a a_1), t.below → motive_1 a a_1 t)
+  (F_2 : ∀ (a a_1 : Nat) (t : OddDist a a_1), t.below → motive_2 a a_1 t) : motive_1 a✝ a✝¹ t
+-/
+#guard_msgs in
+#check EvenDist.brecOn
+
+/--
+info: OddDist.brecOn {motive_1 : (a a_1 : Nat) → EvenDist a a_1 → Prop} {motive_2 : (a a_1 : Nat) → OddDist a a_1 → Prop}
+  {a✝ a✝¹ : Nat} (t : OddDist a✝ a✝¹) (F_1 : ∀ (a a_1 : Nat) (t : EvenDist a a_1), t.below → motive_1 a a_1 t)
+  (F_2 : ∀ (a a_1 : Nat) (t : OddDist a a_1), t.below → motive_2 a a_1 t) : motive_2 a✝ a✝¹ t
+-/
+#guard_msgs in
+#check OddDist.brecOn
+
+/- mutual recursion not supported yet -/
+
+/--
+error: failed to infer structural recursion:
+Cannot use parameters h of OddDist.recTest and h of EvenDist.recTest:
+  structural mutual recursion over inductive predicates is not supported
+-/
+#guard_msgs in
+mutual
+
+theorem OddDist.recTest {a b} (h : OddDist b a) : True :=
+  match h with
+  | .succ_left h => h.recTest
+  | .succ_right h => h.recTest
+termination_by structural h
+
+theorem EvenDist.recTest (h : EvenDist a b) : True :=
+  match h with
+  | .zero => trivial
+  | .succ_left h => h.recTest
+  | .succ_right h => h.recTest
+termination_by structural h
+
+end
+
+/-!
+Nested inductives
+-/
+
+inductive Test (p : Nat → Prop) : Nat → Prop
+  | zero : Test p 0
+  | one (h : Test p 3 ∧ p 2) : Test p 1
+
+-- Note: recursion doesn't work yet
+/--
+error: failed to infer structural recursion:
+Cannot use parameter h:
+  Application type mismatch: The argument
+    n
+  has type
+    Nat
+  but is expected to have type
+    Test p 3 ∧ p 2 → Prop
+  in the application
+    @brecOn p (fun {n} h => funType h) n
+-/
+#guard_msgs in
+theorem Test.recTest (h : Test p n) : True :=
+  match h with
+  | .zero => trivial
+  | .one ⟨h, h'⟩ => recTest h
+termination_by structural h
+
+/--
+info: Test.brecOn {p : Nat → Prop} {motive_1 : (a : Nat) → Test p a → Prop} {motive_2 : Test p 3 ∧ p 2 → Prop} {a✝ : Nat}
+  (t : Test p a✝) (F_1 : ∀ (a : Nat) (t : Test p a), t.below → motive_1 a t)
+  (F_2 : ∀ (t : Test p 3 ∧ p 2), Test.below_1 t → motive_2 t) : motive_1 a✝ t
+-/
+#guard_msgs in
+#check Test.brecOn
+
+/--
+info: Test.brecOn_1 {p : Nat → Prop} {motive_1 : (a : Nat) → Test p a → Prop} {motive_2 : Test p 3 ∧ p 2 → Prop}
+  (t : Test p 3 ∧ p 2) (F_1 : ∀ (a : Nat) (t : Test p a), t.below → motive_1 a t)
+  (F_2 : ∀ (t : Test p 3 ∧ p 2), Test.below_1 t → motive_2 t) : motive_2 t
+-/
+#guard_msgs in
+#check Test.brecOn_1
+
+/-!
+Reflexive inductives
+-/
+
+inductive Test2 (p : Nat → Prop) : Nat → Prop
+  | zero : Test2 p 0
+  | abc (h : ∀ n, p (a + n) → Test2 p n) : Test2 p a
+
+/--
+info: Test2.brecOn {p : Nat → Prop} {motive : (a : Nat) → Test2 p a → Prop} {a✝ : Nat} (t : Test2 p a✝)
+  (F_1 : ∀ (a : Nat) (t : Test2 p a), Test2.below t → motive a t) : motive a✝ t
+-/
+#guard_msgs in
+#check Test2.brecOn
+
+theorem Test2.recTest (h : Test2 (fun _ => True) n) : True :=
+  match h with
+  | .zero => trivial
+  | .abc h => recTest (h 0 trivial)
+termination_by structural h
+
+/-!
+Nested inductives with indices
+-/
+
+set_option inductive.autoPromoteIndices false in
+inductive And2 (p : Prop) : (q : Prop) → Prop where
+  | intro (left : p) (right : b) : And2 p b
+
+inductive Test' (p : Nat → Prop) : Nat → Prop where
+  | zero : Test' p 0
+  | succ (h : And2 (Test' p 2) (p 3)) : Test' p 1
+
+/--
+info: Test'.brecOn {p : Nat → Prop} {motive_1 : (a : Nat) → Test' p a → Prop}
+  {motive_2 : (q : Prop) → And2 (Test' p 2) q → Prop} {a✝ : Nat} (t : Test' p a✝)
+  (F_1 : ∀ (a : Nat) (t : Test' p a), t.below → motive_1 a t)
+  (F_2 : ∀ (q : Prop) (t : And2 (Test' p 2) q), Test'.below_1 t → motive_2 q t) : motive_1 a✝ t
+-/
+#guard_msgs in
+#check Test'.brecOn

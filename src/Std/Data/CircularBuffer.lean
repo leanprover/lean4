269d96c39a9f477920ea6aa87e53fb341452157c
Sofia Rodrigues <sofia@algebraic.dev>
2025-08-21 10:13:02 -0300

    refactor: remove circular buffer

--- src/Std/Data/CircularBuffer.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Circular Buffer
--/
-module
-
-prelude
-public import Std.Data.CircularBuffer.Mutable

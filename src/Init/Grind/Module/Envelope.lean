5daf65ec56b7d41fa809138550e338802e2a5bd7
Leonardo de Moura <leomoura@amazon.com>
2025-08-22 13:36:05 -0700

    feat: add helper theorems for `NatModule` (#10069)
    This PR adds helper theorems to support `NatModule` in `grind linarith`.

--- src/Init/Grind/Module/Envelope.lean
+++ src/Init/Grind/Module/Envelope.lean
@@ -222,6 +222,12 @@ attribute [instance] ofNatModule
 theorem toQ_add (a b : α) : toQ (a + b) = toQ a + toQ b := by
   simp; apply Quot.sound; simp
 
+theorem toQ_zero : toQ (0 : α) = 0 := by
+  simp; apply Quot.sound; simp
+
+theorem toQ_smul (n : Nat) (a : α) : toQ (n • a) = (↑n : Int) • toQ a := by
+  simp; apply Quot.sound; simp; exists 0
+
 /-!
 Helper definitions and theorems for proving `toQ` is injective when
 `CommSemiring` has the right_cancel property

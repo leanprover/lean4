069f519549b594c53804c642bff363c136ebd1e4
Joachim Breitner <mail@joachim-breitner.de>
2025-08-25 16:55:56 +0200

    Save file

--- tests/lean/run/ctorElim.lean
+++ tests/lean/run/ctorElim.lean
@@ -21,9 +21,8 @@ info: @[reducible] protected def Vec.cons.elim.{u} : {α : Type} →
 
 /--
 info: @[defeq] theorem Vec.cons.elim.eq.{u} : ∀ {α : Type} {motive : (a : Nat) → Vec α a → Sort u} {n : Nat} (a : α)
-  (a_1 : Vec α n) (h : (Vec.cons a a_1).ctorIdx = 1)
-  (cons : {n : Nat} → (a : α) → (a_2 : Vec α n) → motive (n + 1) (Vec.cons a a_2)),
-  Vec.cons.elim (Vec.cons a a_1) h cons = cons a a_1
+  (a_1 : Vec α n) (cons : {n : Nat} → (a : α) → (a_2 : Vec α n) → motive (n + 1) (Vec.cons a a_2)),
+  Vec.cons.elim (Vec.cons a a_1) ⋯ cons = cons a a_1
 -/
 #guard_msgs in
 #print sig Vec.cons.elim.eq

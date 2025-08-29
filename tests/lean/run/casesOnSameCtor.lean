a31fe55ef755111c1c5ff02648e06b65d9f0f597
Joachim Breitner <mail@joachim-breitner.de>
2025-08-29 11:43:44 +0200

    Update test

--- tests/lean/run/casesOnSameCtor.lean
+++ tests/lean/run/casesOnSameCtor.lean
@@ -12,7 +12,7 @@ run_meta mkCasesOnSameCtorHet `Vec.casesOn2Het ``Vec
 run_meta mkCasesOnSameCtor `Vec.casesOn2 ``Vec ``Vec.casesOn2Het
 
 /--
-info: Vec.casesOn2.{u_1, u} {α : Type u} (motive : {a : Nat} → Vec α a → Vec α a → Sort u_1) {a✝ : Nat} (t t✝ : Vec α a✝)
+info: Vec.casesOn2.{u_1, u} {α : Type u} {motive : {a : Nat} → Vec α a → Vec α a → Sort u_1} {a✝ : Nat} (t t✝ : Vec α a✝)
   (h : t.ctorIdx = t✝.ctorIdx) (nil : motive nil nil)
   (cons : (a : α) → {n : Nat} → (a_1 : Vec α n) → (a_2 : α) → (a_3 : Vec α n) → motive (cons a a_1) (cons a_2 a_3)) :
   motive t t✝
@@ -30,7 +30,7 @@ run_meta mkCasesOnSameCtorHet `List.casesOn2Het ``List
 run_meta mkCasesOnSameCtor `List.casesOn2 ``List ``List.casesOn2Het
 
 /--
-info: List.casesOn2.{u_1, u} {α : Type u} (motive : List α → List α → Sort u_1) (t t✝ : List α) (h : t.ctorIdx = t✝.ctorIdx)
+info: List.casesOn2.{u_1, u} {α : Type u} {motive : List α → List α → Sort u_1} (t t✝ : List α) (h : t.ctorIdx = t✝.ctorIdx)
   (nil : motive [] [])
   (cons : (head : α) → (tail : List α) → (head_1 : α) → (tail_1 : List α) → motive (head :: tail) (head_1 :: tail_1)) :
   motive t t✝

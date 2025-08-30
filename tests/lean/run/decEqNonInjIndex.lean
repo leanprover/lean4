1e421f0be05e84ca571e546247bd967fa35a0f61
Joachim Breitner <mail@joachim-breitner.de>
2025-08-30 16:59:08 +0200

    Update test

--- tests/lean/run/decEqNonInjIndex.lean
+++ tests/lean/run/decEqNonInjIndex.lean
@@ -19,15 +19,8 @@ the dependent pattern matcher can solve the following kinds of equations
 - <term> = <term> where the terms are definitionally equal
 - <constructor> = <constructor>, examples: List.cons x xs = List.cons y ys, and List.cons x xs = List.nil
 ---
-error: Tactic `cases` failed with a nested error:
-Dependent elimination failed: Failed to solve equation
-  f n✝¹ = f n✝
-at case `T.mk1` after processing
-  _, (T.mk1 _ _), _
-the dependent pattern matcher can solve the following kinds of equations
-- <var> = <term> and <term> = <var>
-- <term> = <term> where the terms are definitionally equal
-- <constructor> = <constructor>, examples: List.cons x xs = List.cons y ys, and List.cons x xs = List.nil
+error: Dependent elimination failed: Failed to solve equation
+  f n✝ = f n
 -/
 #guard_msgs(pass trace, all) in
 inductive T : (n : Nat) → Type where

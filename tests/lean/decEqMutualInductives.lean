9402b77f11cb992ca6755bee4f29fce3a313bde4
Joachim Breitner <mail@joachim-breitner.de>
2025-09-01 16:31:26 +0200

    More fixing

--- tests/lean/decEqMutualInductives.lean
+++ tests/lean/decEqMutualInductives.lean
@@ -3,6 +3,9 @@
 -- Print the generated derivations
 set_option trace.Elab.Deriving.decEq true
 
+namespace A
+set_option deriving.decEq.linear_construction_threshold 1000
+
 mutual
 inductive Tree : Type where
   | node : ListTree → Tree
@@ -25,3 +28,33 @@ inductive Foo₂ : Type where
 inductive Foo₃ : Type where
   | foo₃ : Foo₁ → Foo₃
 end
+
+end A
+
+namespace B
+set_option deriving.decEq.linear_construction_threshold 0
+
+mutual
+inductive Tree : Type where
+  | node : ListTree → Tree
+
+inductive ListTree : Type where
+  | nil : ListTree
+  | cons : Tree → ListTree → ListTree
+  deriving DecidableEq
+end
+
+mutual
+inductive Foo₁ : Type where
+  | foo₁₁ : Foo₁
+  | foo₁₂ : Foo₂ → Foo₁
+deriving DecidableEq
+
+inductive Foo₂ : Type where
+  | foo₂ : Foo₃ → Foo₂
+
+inductive Foo₃ : Type where
+  | foo₃ : Foo₁ → Foo₃
+end
+
+end B
--- tests/lean/run/decEqNonInjIndex.lean
+++ tests/lean/run/decEqNonInjIndex.lean
@@ -5,8 +5,7 @@ non-injective indices, and just how bad the error messages are.
 
 opaque f : Nat → Nat
 
--- set_option trace.Elab.Deriving.decEq true
--- set_option trace.Elab.Deriving.beq true
+set_option deriving.decEq.linear_construction_threshold 0
 
 /--
 error: Tactic `cases` failed with a nested error:
@@ -26,4 +25,34 @@ error: Dependent elimination failed: Failed to solve equation
 inductive T : (n : Nat) → Type where
   | mk1 : Fin n → T (f n)
   | mk2 : Fin (2*n) → T (f n)
+deriving BEq, DecidableEq
+
+
+set_option deriving.decEq.linear_construction_threshold 10000
+
+/--
+error: Tactic `cases` failed with a nested error:
+Dependent elimination failed: Failed to solve equation
+  f n✝¹ = f n✝
+at case `T'.mk1` after processing
+  _, (T'.mk1 _ _), _
+the dependent pattern matcher can solve the following kinds of equations
+- <var> = <term> and <term> = <var>
+- <term> = <term> where the terms are definitionally equal
+- <constructor> = <constructor>, examples: List.cons x xs = List.cons y ys, and List.cons x xs = List.nil
+---
+error: Tactic `cases` failed with a nested error:
+Dependent elimination failed: Failed to solve equation
+  f n✝¹ = f n✝
+at case `T'.mk1` after processing
+  _, (T'.mk1 _ _), _
+the dependent pattern matcher can solve the following kinds of equations
+- <var> = <term> and <term> = <var>
+- <term> = <term> where the terms are definitionally equal
+- <constructor> = <constructor>, examples: List.cons x xs = List.cons y ys, and List.cons x xs = List.nil
+-/
+#guard_msgs(pass trace, all) in
+inductive T' : (n : Nat) → Type where
+  | mk1 : Fin n → T' (f n)
+  | mk2 : Fin (2*n) → T' (f n)
 deriving BEq, DecidableEq

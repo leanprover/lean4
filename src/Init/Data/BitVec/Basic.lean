6c35fccecafb21e72d1b80ba5f4fdfc2877ddb99
luisacicolini <luisacicolini@gmail.com>
2025-09-02 21:30:10 +0100

    chore: tested correctness of bitvec defs

--- src/Init/Data/BitVec/Basic.lean
+++ src/Init/Data/BitVec/Basic.lean
@@ -941,7 +941,6 @@ def parPrefixSum
     hcast▸parSum
 
 
-
 /-- We express `popCount` as the result of parallel prefix sum. -/
 def popCountParSum {x : BitVec w} : BitVec w :=
   if hw : 1 < w then
@@ -963,8 +962,5 @@ termination_by (w - n)
 
 /-- Count the number of bits with value one in a bitvec -/
 def popCount {w : Nat} (x : BitVec w) : BitVec w := popCountAuxRec x 0 0
-
--- theorem correctness (x r n : BitVec w) :
---   x.popCount =
 
 end BitVec

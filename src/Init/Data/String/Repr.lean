cdb628fded9344f3e5730fad2bc548382449c16c
Markus Himmel <markus@lean-fro.org>
2025-09-03 18:55:50 +0200

    Fix

--- src/Init/Data/String/Repr.lean
+++ src/Init/Data/String/Repr.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Leonardo de Moura, Mario Carneiro
+-/
 module
 
 prelude

dfbfca472bcf9ca2c93f553508ff4aa0b6e5d450
Leonardo de Moura <leomoura@amazon.com>
2025-09-05 14:59:30 -0700

    refactor: add LinearM

---
+++ src/Lean/Meta/Tactic/Grind/Arith/Linear/LinearM.lean
@@ -0,0 +1,84 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+module
+prelude
+public import Lean.Meta.Tactic.Grind.Types
+public import Lean.Meta.Tactic.Grind.Arith.CommRing.RingM
+public section
+namespace Lean.Meta.Grind.Arith.Linear
+
+def get' : GoalM State := do
+  return (← get).arith.linear
+
+@[inline] def modify' (f : State → State) : GoalM Unit := do
+  modify fun s => { s with arith.linear := f s.arith.linear }
+
+structure LinearM.Context where
+  structId : Nat
+
+class MonadGetStruct (m : Type → Type) where
+  getStruct : m Struct
+
+export MonadGetStruct (getStruct)
+
+@[always_inline]
+instance (m n) [MonadLift m n] [MonadGetStruct m] : MonadGetStruct n where
+  getStruct    := liftM (getStruct : m Struct)
+
+/-- We don't want to keep carrying the `StructId` around. -/
+abbrev LinearM := ReaderT LinearM.Context GoalM
+
+abbrev LinearM.run (structId : Nat) (x : LinearM α) : GoalM α :=
+  x { structId }
+
+abbrev getStructId : LinearM Nat :=
+  return (← read).structId
+
+protected def LinearM.getStruct : LinearM Struct := do
+  let s ← get'
+  let structId ← getStructId
+  if h : structId < s.structs.size then
+    return s.structs[structId]
+  else
+    throwError "`grind` internal error, invalid structure id"
+
+instance : MonadGetStruct LinearM where
+  getStruct := LinearM.getStruct
+
+open CommRing
+
+def getRingCore? (ringId? : Option Nat) : GoalM (Option Ring) := do
+  let some ringId := ringId? | return none
+  RingM.run ringId do return some (← getRing)
+
+def throwNotRing : LinearM α :=
+  throwError "`grind linarith` internal error, structure is not a ring"
+
+def throwNotCommRing : LinearM α :=
+  throwError "`grind linarith` internal error, structure is not a commutative ring"
+
+def getRing? : LinearM (Option Ring) := do
+  getRingCore? (← getStruct).ringId?
+
+def getRing : LinearM Ring := do
+  let some ring ← getRing?
+    | throwNotCommRing
+  return ring
+
+instance : MonadRing LinearM where
+  getRing := Linear.getRing
+  modifyRing f := do
+    let some ringId := (← getStruct).ringId? | throwNotCommRing
+    RingM.run ringId do modifyRing f
+  canonExpr e := do shareCommon (← canon e)
+  synthInstance? e := Grind.synthInstance? e
+
+def withRingM (x : RingM α) : LinearM α := do
+  let some ringId := (← getStruct).ringId?
+    | throwNotCommRing
+  RingM.run ringId x
+
+end Lean.Meta.Grind.Arith.Linear

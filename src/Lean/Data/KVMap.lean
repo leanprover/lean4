1fa974127dff0e8b7d9fce44843567c77d265251
Eric Wieser <wieser.eric@gmail.com>
2025-09-06 09:27:15 +0000

    fix: move the monad argument for ForIn, ForIn', and ForM

--- src/Lean/Data/KVMap.lean
+++ src/Lean/Data/KVMap.lean
@@ -183,7 +183,7 @@ def updateSyntax (m : KVMap) (k : Name) (f : Syntax → Syntax) : KVMap :=
   (kv : KVMap) (init : δ) (f : Name × DataValue → δ → m (ForInStep δ)) : m δ :=
   forIn kv.entries init f
 
-instance : ForIn m KVMap (Name × DataValue) where
+instance [Monad m] : ForIn m KVMap (Name × DataValue) where
   forIn := KVMap.forIn
 
 def subsetAux : List (Name × DataValue) → KVMap → Bool

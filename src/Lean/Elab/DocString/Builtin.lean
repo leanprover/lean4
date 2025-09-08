61333db6a232ba332daf1b89df84d98e0469358b
David Thrane Christiansen <david@lean-fro.org>
2025-09-08 17:11:48 +0200

    chore: missing header

--- src/Lean/Elab/DocString/Builtin.lean
+++ src/Lean/Elab/DocString/Builtin.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: David Thrane Christiansen
+-/
+
 module
 prelude
 public import Lean.Elab.DocString

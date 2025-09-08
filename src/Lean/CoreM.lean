b7952eeb5f8675faa4473b54177e71f3ef791663
Sebastian Ullrich <sebasti@nullri.ch>
2025-09-08 11:23:53 +0200

    fix config eval, now Init.Data.Format.Basic doesn't like me

--- src/Lean/CoreM.lean
+++ src/Lean/CoreM.lean
@@ -695,6 +695,8 @@ builtin_initialize postponedCompileDeclsExt : SimplePersistentEnvExtension Name 
     addEntryFn    := fun s n => s.insert n
     toArrayFn     := fun es => es.toArray
     asyncMode     := .sync
+    replay?       := some <| SimplePersistentEnvExtension.replayOfFilter
+      (!·.contains ·) (·.insert)
   }
 
 -- Forward declaration
@@ -744,8 +746,8 @@ where doCompile := do
       if logErrors then
         throw e
 
-def compileDecl (decl : Declaration) (logErrors := true) : CoreM Unit := do
-  compileDecls (Compiler.getDeclNamesForCodeGen decl) logErrors
+def compileDecl (decl : Declaration) (logErrors mayPostpone := true) : CoreM Unit := do
+  compileDecls (Compiler.getDeclNamesForCodeGen decl) logErrors mayPostpone
 
 def getDiag (opts : Options) : Bool :=
   diagnostics.get opts

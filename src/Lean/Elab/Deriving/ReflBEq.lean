96b367770d3fe5ce26a3cfb0d4c2d184e781dfa5
Joachim Breitner <mail@joachim-breitner.de>
2025-09-12 10:34:49 +0200

    Move simproc into proper location

--- src/Lean/Elab/Deriving/ReflBEq.lean
+++ src/Lean/Elab/Deriving/ReflBEq.lean
@@ -6,55 +6,17 @@ Authors: Joachim Breitner
 module
 
 prelude
-public import Lean.Data.Options
-public import Lean.Meta.Tactic.Simp.Types
-import Init.Simproc
-import Lean.Meta.Transform
 import Lean.Elab.Deriving.Basic
 import Lean.Elab.Deriving.Util
-import Lean.Meta.Eqns
-import Lean.Meta.SameCtorUtils
-import Lean.Meta.MethodSpecs
-import Lean.Meta.Tactic.Simp.Simproc
-import Lean.Meta.Tactic.Simp.Rewrite
 
 namespace Lean.Elab.Deriving.ReflBEq
 open Lean.Parser.Term
 open Meta
 
--- TODO: Should this be moved to MethodSpecs?
-public section
-builtin_simproc_decl simpBEq (_ == _) := fun e => do
-  let_expr BEq.beq _ inst lhs rhs ← e | return .continue
-  unless inst.getAppFn.isConst do return .continue
-  let some _ ← isConstructorApp? lhs | return .continue
-  let some _ ← isConstructorApp? rhs | return .continue
-  let some thm ← getMethodSpecTheorem inst.getAppFn.constName! "beq" | return .continue
-  let simpThms ← mkSimpTheoremFromConst thm
-  assert! simpThms.size = 1
-  let some r ← Simp.tryTheorem? e simpThms[0]! | return .continue
-  return .visit r
-end
-
 open TSyntax.Compat in
 open Parser.Tactic in
 def mkReflBEqInstanceCmds (declName : Name) : TermElabM (Array Syntax) := do
   let indVal ← getConstInfoInduct declName
-
-  -- let specThms ← forallTelescopeReducing indVal.type fun xs _ => do
-  --   let t := mkAppN (← mkConstWithLevelParams declName) xs
-  --   let beqInstType ← mkAppM ``BEq #[t]
-  --   let beqInst ← synthInstance beqInstType
-  --   unless beqInst.getAppFn.isConst do
-  --     throwError "cannot derive ReflBEq for instance {beqInst}"
-  --   let beqInstConst := beqInst.getAppFn.constName!
-  --   let some thms ← getMethodSpecTheorems beqInstConst "beq"
-  --     | throwError "instance {.ofConstName beqInstConst} has no method spec theorems. Try `@[method_specs]`"
-  --   pure thms
-
-  -- let mut simpArgs : TSyntaxArray ``simpArg := #[← `(simpArg | *)]
-  -- for thm in specThms do
-  --   simpArgs := simpArgs.push (← `(simpArg | $(mkCIdent thm)))
 
   let argNames     ← mkInductArgNames indVal
   let binders      ← mkImplicitBinders argNames
@@ -67,7 +29,7 @@ def mkReflBEqInstanceCmds (declName : Name) : TermElabM (Array Syntax) := do
     intro x
     induction x
     all_goals
-      first | simp [ *, $(mkCIdent ``simpBEq):ident ]
+      first | simp [ *, reduceBEq ]
             | fail "Failed to prove ReflBEq instance")
   let cmds := #[instCmd]
   trace[Elab.Deriving.reflBEq] "\n{cmds}"
--- tests/lean/run/derivingReflBEq.lean
+++ tests/lean/run/derivingReflBEq.lean
@@ -7,4 +7,82 @@ deriving BEq, ReflBEq
 #guard_msgs in
 #print sig instReflBEqL
 
--- TODO: More test, error messages
+inductive Vec (α : Type u) : Nat → Type u where
+  | nil  : Vec α 0
+  | cons : ∀ {n}, α → Vec α n → Vec α (n+1)
+deriving BEq, ReflBEq
+
+/--
+info: theorem instReflBEqVec.{u_1} : ∀ {α : Type u_1} {a : Nat} [inst : BEq α] [ReflBEq α], ReflBEq (Vec α a)
+-/
+#guard_msgs in
+#print sig instReflBEqVec
+
+
+inductive Enum
+  | mk1 | mk2 | mk3
+deriving BEq, ReflBEq
+
+/-- info: theorem instReflBEqEnum : ReflBEq Enum -/
+#guard_msgs in
+#print sig instReflBEqEnum
+
+-- The following type has `Eq.rec`’s in its `BEq` implementation,
+-- but `simp` seems to handle that just fine
+
+inductive WithHEq where
+  | mk : ∀ n, Fin n → WithHEq
+deriving BEq, ReflBEq
+
+/--
+info: instBEqWithHEq.beq_spec (x✝ x✝¹ : WithHEq) :
+  (x✝ == x✝¹) =
+    if h : (x✝.1 == x✝¹.1) = true then
+      Eq.rec (motive := fun a t => x✝¹.1 = a → ⋯ ≍ t → Bool)
+        (fun h_1 =>
+          Eq.rec (motive := fun x x_1 => Fin x → (x✝.1 == x) = true → (x_2 : x✝.1 = x) → x_2 ≍ ⋯ → Bool)
+            (fun b h x h => x✝.2 == b) ⋯ x✝¹.2 ⋯ ⋯)
+        ⋯ ⋯ ⋯
+    else false
+-/
+#guard_msgs in
+#check instBEqWithHEq.beq_spec
+
+/-- info: instReflBEqWithHEq : ReflBEq WithHEq -/
+#guard_msgs in
+#check instReflBEqWithHEq
+
+-- No `BEq` derived? Not a great error message yet.
+
+/--
+error: failed to synthesize
+  BEq Foo
+
+Hint: Additional diagnostic information may be available using the `set_option diagnostics true` command.
+---
+error: Tactic `constructor` failed: target is not an inductive datatype
+
+⊢ sorry
+-/
+#guard_msgs in
+structure Foo where
+  deriving ReflBEq
+
+/--
+@ +5:16...23
+error: The `induction` tactic does not support the type `Tree` because it is mutually inductive
+
+Hint: Consider using the `cases` tactic instead
+-/
+#guard_msgs (positions := true) in
+mutual
+inductive Tree (α : Type u) where
+  | node : TreeList α → Tree α
+  | leaf : α → Tree α
+  deriving BEq, ReflBEq
+
+inductive TreeList (α : Type u) where
+  | nil : TreeList α
+  | cons : Tree α → TreeList α → TreeList α
+  deriving BEq
+end

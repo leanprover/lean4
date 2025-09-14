6b97e4165045177f8bef6831e516ccee241a55b4
Leonardo de Moura <leomoura@amazon.com>
2025-09-14 14:04:40 -0700

    feat: sanity check for instances in `grind ring` (#10380)
    This PR implements sanity checks in the `grind ring` module to ensure
    the instances synthesized by type class resolution are definitionally
    equal to the corresponding ones in the `grind` core classes. The
    definitional equality test is performed with reduction restricted to
    reducible definitions and instances.

--- src/Lean/Meta/Tactic/Grind/Types.lean
+++ src/Lean/Meta/Tactic/Grind/Types.lean
@@ -28,6 +28,10 @@ def congrPlaceholderProof := mkConst (Name.mkSimple "[congruence]")
 def isDefEqD (t s : Expr) : MetaM Bool :=
   withDefault <| isDefEq t s
 
+/-- Similar to `isDefEq`, but ensures that only reducible definitions and instances can be reduced. -/
+def isDefEqI (t s : Expr) : MetaM Bool :=
+  withReducibleAndInstances <| isDefEq t s
+
 /--
 Returns `true` if `e` is `True`, `False`, or a literal value.
 See `Lean.Meta.LitValues` for supported literals.

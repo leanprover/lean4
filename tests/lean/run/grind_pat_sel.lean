6d470e964bfa2420061e90606a19f0d9ff9fe0d5
Leonardo de Moura <leomoura@amazon.com>
2025-09-16 16:45:41 -0700

    test:

---
+++ tests/lean/run/grind_pat_sel.lean
@@ -0,0 +1,63 @@
+-- Enable new pattern selection algorithm
+set_option backward.grind.inferPattern false
+set_option warn.sorry false
+
+opaque f : Nat → Nat
+opaque g : Nat → Nat
+
+/-- info: fg₁: [g #0] -/
+#guard_msgs in
+@[grind!? ←] axiom fg₁ : f (g x) = x
+
+set_option trace.Meta.debug true
+
+/-- info: fg₂: [f (g #0)] -/
+#guard_msgs in
+@[grind? ←] axiom fg₂ : f (g x) = x
+
+/-- error: redundant modifier `!` in `grind` attribute -/
+#guard_msgs in
+@[grind! =] axiom fg₃ : f (g x) = x
+
+/-- error: redundant modifier `!` in `grind` attribute -/
+#guard_msgs in
+@[grind! =_] axiom fg₄ : f (g x) = x
+
+/--
+error: invalid pattern, (non-forbidden) application expected
+  #0
+-/
+#guard_msgs (error) in
+@[grind =_] theorem fg₅ : f (g x) = x := sorry
+
+opaque p : Nat → Prop
+
+/-- info: pf₁: [f #3, f #2] -/
+#guard_msgs in
+@[grind!? →] axiom pf₁ : p (f x) → p (f y) → f x = f y
+
+/-- info: pf₂: [p (f #3), p (f #2)] -/
+#guard_msgs in
+@[grind? →] axiom pf₂ : p (f x) → p (f y) → f x = f y
+
+/-- info: pf₃: [p (f #3), f (f #2)] -/
+#guard_msgs in
+@[grind? →] axiom pf₃ : p (f x) → f (f y) = y → f x = f y
+
+opaque r : Nat → Nat → Nat
+
+/-- info: pr₁: [p (f #2), r #2 (f #1)] -/
+#guard_msgs in
+@[grind? =>] axiom pr₁ : p (f x) → r x (f y) = y
+
+/-- info: pr₂: [f #2, f #1] -/
+#guard_msgs in
+@[grind!? =>] axiom pr₂ : p (f x) → r x (f y) = y
+
+/-- info: pr₃: [r #2 (f #1)] -/
+#guard_msgs in
+@[grind!? <=] axiom pr₃ : p (f x) → r x (f y) = y
+
+/-- info: pr₄: [r #1 (f #1), p (f #2)] -/
+#guard_msgs in
+@[grind? <=] axiom pr₄ : p (f x) → r y (f y) = y

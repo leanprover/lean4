07a6c689ecb21608fad00e8a46c246b23a1adc38
Kim Morrison <kim@tqft.net>
2025-09-18 22:32:31 +1000

    list and option

--- tests/lean/run/grind_array_attach.lean
+++ tests/lean/run/grind_array_attach.lean
@@ -25,3 +25,20 @@ reprove pmap_reverse reverse_pmap attachWith_reverse reverse_attachWith attach_r
 reprove back?_pmap back?_attachWith back?_attach by grind
 
 end Vector
+
+namespace List
+
+-- `grind` is less capable on List by default, because the theorems are set up to use induction and `cons`,
+-- rathering than extensionality via indices. Here we just use extensionality.
+attribute [local grind ←=] List.ext_getElem
+
+reprove pmap_nil pmap_cons attach_nil attachWith_nil by grind
+reprove map_pmap pmap_map attach_cons attachWith_cons pmap_eq_map_attach pmap_eq_attachWith by grind
+reprove attach_map_val attach_map_subtype_val attachWith_map_val attachWith_map_subtype_val by grind
+reprove pmap_attach pmap_attachWith by grind
+reprove attach_map attachWith_map map_attachWith map_attachWith_eq_pmap map_attach_eq_pmap by grind
+reprove pmap_pmap pmap_append pmap_append' attach_append attachWith_append by grind
+reprove pmap_reverse reverse_pmap attachWith_reverse reverse_attachWith attach_reverse reverse_attach by grind
+reprove getLast?_pmap getLast?_attachWith getLast?_attach by grind
+
+end List

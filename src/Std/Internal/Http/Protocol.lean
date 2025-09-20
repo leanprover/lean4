c89e865a415154f98aed30e54b46dc0f28b51aa8
Sofia Rodrigues <sofia@algebraic.dev>
2025-09-20 17:51:57 -0300

    fix: remove orphaned module

--- src/Std/Internal/Http/Protocol.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sofia Rodrigues
--/
-module
-
-prelude
-public import Std.Internal.Http.Protocol.H1

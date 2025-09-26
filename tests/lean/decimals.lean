ce03344184e9f0a8f1d9f6a9d8cd6e09db8b2943
Rob Simmons <rob@lean-fro.org>
2025-09-25 20:59:03 -0400

    Add back mistakenly removed tests

---
+++ tests/lean/decimals.lean
@@ -0,0 +1,31 @@
+#check 1.2
+#check 1.2 + 2.3
+#check 1.0
+#eval 1.2 + 2.3
+#check 1.
+#check 3.1416
+
+theorem ex : 31416e-4 = 3.1416 :=
+  rfl
+
+#eval 3.4e-100 * 1e98
+
+#eval 12.3e90 * 1E-90
+#eval 3.00e-100 * 1e100
+#eval 3.00e-100 * 1.e100
+#eval 3.00e-100 * 1.0e100
+
+#eval 1e10
+#eval 1e50
+#eval 1e80
+#eval 1e100
+#eval 1e200
+#eval 1e300
+#eval 1e400
+
+#eval 1 / 1e-1
+#eval 1 / 1e-2
+#eval 1 / 1e-10
+#eval 1 / 1e-100
+#eval 1 / 1e-200
+#eval 1 / 1e-400

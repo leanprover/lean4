8aa32bc663ac8eed840521115b0b3d688a9c120f
Markus Himmel <markus@lean-fro.org>
2025-09-30 16:05:19 +0200

    Undo accidental changes

--- tests/lean/run/simpSInt.lean
+++ tests/lean/run/simpSInt.lean
@@ -1,5 +1,3 @@
-module
-
 #check_simp (-64 : Int8).toBitVec ~> 192#8
 #check_simp (-64 : Int16).toBitVec ~> 65472#16
 #check_simp (-64 : Int32).toBitVec ~> 4294967232#32
--- tests/lean/run/simprocUInt.lean
+++ tests/lean/run/simprocUInt.lean
@@ -1,5 +1,3 @@
-module
-
 section
 
 variable (x : Nat)

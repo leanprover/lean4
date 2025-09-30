f667ae25d381312e53eb52fb8e83b9d0c4e66da4
Sebastian Ullrich <sebasti@nullri.ch>
2025-09-30 22:35:12 +0200

    chore: fix `maxErrors` error location

--- src/Lean/Language/Basic.lean
+++ src/Lean/Language/Basic.lean
@@ -316,9 +316,7 @@ private def reportMessages (msgLog : MessageLog) (opts : Options)
     let numErrors := numErrors + (if msg.severity matches .error then 1 else 0)
     let maxErrorsReached := maxErrors.get opts != 0 && numErrors > maxErrors.get opts
     let msg : Message :=
-      if maxErrorsReached then {
-        fileName := ""
-        pos := ⟨0, 0⟩
+      if maxErrorsReached then { msg with
         data := s!"maximum number of errors ({maxErrors.get opts}; from option `maxErrors`) reached, exiting"
         severity := .error
       } else if let some severity := severityOverrides.find? msg.kind then

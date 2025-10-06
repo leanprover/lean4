fbfb0757cabab732f8e00dc47fa57e6bb14d905a
Leonardo de Moura <leomoura@amazon.com>
2025-10-05 18:08:26 -0700

    feat: `grind` interactive mode basic tactics (#10677)
    This PR implements the basic tactics for the new `grind` interactive
    mode. While many additional `grind` tactics will be added later, the
    foundational framework is already operational. The following `grind`
    tactics are currently implemented: `skip`, `done`, `finish`, `lia`, and
    `ring`.
    This PR also removes the notion of `grind` fallback procedure since it
    is subsumed by the new framework. Examples:
    ```lean
    example (x y : Nat) : x ≥ y + 1 → x > 0 := by
      grind => skip; lia; done
    
    open Lean Grind
    
    example [CommRing α] (a b c : α)
      : a + b + c = 3 →
        a^2 + b^2 + c^2 = 5 →
        a^3 + b^3 + c^3 = 7 →
        a^4 + b^4 + c^4 = 9 := by
      grind => ring
    ```

--- src/Init/Grind/Interactive.lean
+++ src/Init/Grind/Interactive.lean
@@ -6,7 +6,6 @@ Authors: Leonardo de Moura
 module
 prelude
 public import Init.Tactics
-public meta import Init.Meta
 public section
 namespace Lean.Parser.Tactic.Grind
 
@@ -18,12 +17,19 @@ syntax grindSeq1Indented := sepBy1IndentSemicolon(grind)
 syntax grindSeqBracketed := "{" withoutPosition(sepByIndentSemicolon(grind)) "}"
 syntax grindSeq := grindSeqBracketed <|> grindSeq1Indented
 
+/-- `(grindSeq)` runs the `grindSeq` in sequence on the current list of targets.
+This is pure grouping with no added effects. -/
+syntax (name := paren) "(" withoutPosition(grindSeq) ")" : grind
+
 /-- `skip` does nothing. -/
 syntax (name := skip) "skip" : grind
 /-- `lia` linear integer arithmetic. -/
 syntax (name := lia) "lia" : grind
 /-- `ring` (commutative) rings and fields. -/
 syntax (name := ring) "ring" : grind
+/-- `done` succeeds iff there are no remaining goals. -/
+syntax (name := done) "done" : grind
+
 /-- `finish` tries to close the current goal using `grind`'s default strategy -/
 syntax (name := finish) "finish" : grind
 
@@ -31,8 +37,5 @@ syntax (name := «have») "have" letDecl : grind
 
 /-- Executes the given tactic block to close the current goal. -/
 syntax (name := nestedTacticCore) "tactic" " => " tacticSeq : grind
-
-/-- `grind` interactive mode -/
-syntax (name := grind) "grind" " => " grindSeq : tactic
 
 end Lean.Parser.Tactic.Grind

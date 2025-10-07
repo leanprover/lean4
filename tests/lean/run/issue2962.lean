9712de708633f12f9102cbfb5c335b5443e671d0
Joachim Breitner <mail@joachim-breitner.de>
2025-10-07 14:34:46 +0200

    More test cases

--- tests/lean/run/issue2962.lean
+++ tests/lean/run/issue2962.lean
@@ -49,3 +49,57 @@ info: equations:
 -/
 #guard_msgs in
 #print equations replace2
+
+
+-- Now also mutual
+
+mutual
+inductive N1 where
+  | zero : N1
+  | succ : N2 → N1
+inductive N2 where
+  | zero : N2
+  | succ : N1 → N2
+end
+
+mutual
+def replaceMut1 (f : N1 → Option N1) (g : N2 → Option N2) (t : N1) : N1 :=
+  match f t with
+  | some u => u
+  | none =>
+    match t with
+    | .zero => .zero
+    | .succ t' => .succ (replaceMut2 f g t')
+def replaceMut2 (f : N1 → Option N1) (g : N2 → Option N2) (t : N2) : N2 :=
+  match g t with
+  | some u => u
+  | none =>
+    match t with
+    | .zero => .zero
+    | .succ t' => .succ (replaceMut1 f g t')
+end
+
+/--
+info: theorem replaceMut1.eq_def : ∀ (f : N1 → Option N1) (g : N2 → Option N2) (t : N1),
+  replaceMut1 f g t =
+    match f t with
+    | some u => u
+    | none =>
+      match t with
+      | N1.zero => N1.zero
+      | N1.succ t' => N1.succ (replaceMut2 f g t')
+-/
+#guard_msgs in
+#print sig replaceMut1.eq_def
+/--
+info: theorem replaceMut2.eq_def : ∀ (f : N1 → Option N1) (g : N2 → Option N2) (t : N2),
+  replaceMut2 f g t =
+    match g t with
+    | some u => u
+    | none =>
+      match t with
+      | N2.zero => N2.zero
+      | N2.succ t' => N2.succ (replaceMut1 f g t')
+-/
+#guard_msgs in
+#print sig replaceMut2.eq_def

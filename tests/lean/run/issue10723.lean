b9ee9241133a7f2ae7e906ae3eecbb4495c96c8f
Joachim Breitner <mail@joachim-breitner.de>
2025-10-09 15:33:55 +0200

    Not quite correct yet

--- tests/lean/run/issue10723.lean
+++ tests/lean/run/issue10723.lean
@@ -33,3 +33,16 @@ where
   | .node _ _ xs =>
     xs.all isEmpty
   | _ => false
+
+private partial def onlyIdent'' : Syntax → Bool
+  | .node _ _ args =>
+    let nonEmpty := args.filter (!isEmpty () ·)
+    if h : nonEmpty.size = 1 then onlyIdent nonEmpty[0]
+    else false
+  | .ident .. => true
+  | _ => false
+where
+  isEmpty : Unit → Syntax → Bool
+  | .node _ _ xs =>
+    xs.all isEmpty
+  | _ => false

307d39df387a20bc60ed5b89018dd56ece3c0bfb
Rob23oba <robin.arnez@web.de>
2025-10-10 19:37:53 +0200

    don't fuel, wf

--- src/Lean/Compiler/NameMangling.lean
+++ src/Lean/Compiler/NameMangling.lean
@@ -28,25 +28,44 @@ where finally
   simp only [Nat.reducePow, Nat.add_one_sub_one] at this
   simp [i, UInt32.lt_iff_toNat_lt, this]; omega
 
-def mangleAux : Nat → (s : String) → s.ValidPos → String → String
-  | 0,   _, _,   r => r
-  | i+1, s, pos, r =>
-    if h : pos = s.endValidPos then r else
-    let c := pos.get h
-    let pos := pos.next h
-    if c.isAlpha || c.isDigit then
-      mangleAux i s pos (r.push c)
-    else if c = '_' then
-      mangleAux i s pos (r ++ "__")
-    else if c.toNat < 0x100 then
-      mangleAux i s pos (pushHex 2 c.val (r ++ "_x"))
-    else if c.toNat < 0x10000 then
-      mangleAux i s pos (pushHex 4 c.val (r ++ "_u"))
-    else
-      mangleAux i s pos (pushHex 8 c.val (r ++ "_u"))
+def ValidPos.remainingBytes (pos : String.ValidPos s) : Nat :=
+  s.utf8ByteSize - pos.offset.byteIdx
+
+theorem ValidPos.remainingBytes_next_lt_of_lt {p p' : String.ValidPos s} (h' : p' < p) :
+    p.remainingBytes < p'.remainingBytes := by
+  simp only [ValidPos.lt_iff, Pos.Raw.lt_iff] at h' ⊢
+  simp only [remainingBytes]
+  have : p.offset.byteIdx ≤ s.utf8ByteSize := p.isValid.le_endPos
+  omega
+
+theorem ValidPos.lt_next {p : String.ValidPos s} (h) : p < p.next h := by
+  simp only [next, lt_iff, Slice.Pos.ofset_ofSlice, Pos.Raw.lt_iff, Slice.Pos.byteIdx_offset_next,
+    offset_toSlice, Nat.lt_add_right_iff_pos]
+  exact Char.utf8Size_pos _
+
+theorem ValidPos.remainingBytes_next_lt (pos : String.ValidPos s) (h) :
+    (pos.next h).remainingBytes < pos.remainingBytes :=
+  remainingBytes_next_lt_of_lt (pos.lt_next h)
+
+def mangleAux (s : String) (pos : s.ValidPos) (r : String) : String :=
+  if h : pos = s.endValidPos then r else
+  let c := pos.get h
+  let pos := pos.next h
+  if c.isAlpha || c.isDigit then
+    mangleAux s pos (r.push c)
+  else if c = '_' then
+    mangleAux s pos (r ++ "__")
+  else if c.toNat < 0x100 then
+    mangleAux s pos (pushHex 2 c.val (r ++ "_x"))
+  else if c.toNat < 0x10000 then
+    mangleAux s pos (pushHex 4 c.val (r ++ "_u"))
+  else
+    mangleAux s pos (pushHex 8 c.val (r ++ "_u"))
+termination_by pos.remainingBytes
+decreasing_by all_goals apply ValidPos.remainingBytes_next_lt
 
 public def mangle (s : String) : String :=
-  mangleAux s.length s s.startValidPos ""
+  mangleAux s s.startValidPos ""
 
 end String
 
@@ -98,26 +117,26 @@ def parseLowerHex : (n : Nat) → (s : String) → (p : s.ValidPos) →
     if ch.isDigit then parseLowerHex k s pos h' (n <<< 4 ||| (ch.val - 48).toNat)
     else parseLowerHex k s pos h' (n <<< 4 ||| (ch.val - 87).toNat)
 
-def checkDisambiguation : Nat → (s : String) → s.ValidPos → Bool
-  | 0, _, _ => true
-  | k + 1, s, p =>
-    if h : _ then
-      let b := p.get h
-      if b = '_' then
-        checkDisambiguation k s (p.next h)
-      else if b = 'x' then
-        checkLowerHex 2 s (p.next h)
-      else if b = 'u' then
-        checkLowerHex 4 s (p.next h)
-      else if b = 'U' then
-        checkLowerHex 8 s (p.next h)
-      else if b.val >= 48 && b.val <= 57 then
-        true
-      else false
-    else true
+def checkDisambiguation (s : String) (p : s.ValidPos) : Bool :=
+  if h : _ then
+    let b := p.get h
+    if b = '_' then
+      checkDisambiguation s (p.next h)
+    else if b = 'x' then
+      checkLowerHex 2 s (p.next h)
+    else if b = 'u' then
+      checkLowerHex 4 s (p.next h)
+    else if b = 'U' then
+      checkLowerHex 8 s (p.next h)
+    else if b.val >= 48 && b.val <= 57 then
+      true
+    else false
+  else true
+termination_by p.remainingBytes
+decreasing_by apply p.remainingBytes_next_lt
 
 def needDisambiguation (prev : String) (next : String) : Bool :=
-  prev.endsWith "__" || checkDisambiguation next.utf8ByteSize next next.startValidPos
+  prev.endsWith "__" || checkDisambiguation next next.startValidPos
 
 def Name.mangleAux : Name → String
   | Name.anonymous => ""
@@ -125,7 +144,7 @@ def Name.mangleAux : Name → String
     let m := String.mangle s
     match p with
     | Name.anonymous =>
-      if checkDisambiguation m.utf8ByteSize m m.startValidPos then "00" ++ m else m
+      if checkDisambiguation m m.startValidPos then "00" ++ m else m
     | p              =>
       let m1 := mangleAux p
       m1 ++ (if needDisambiguation m1 m then "_00" else "_") ++ m
@@ -145,64 +164,82 @@ public def mkModuleInitializationFunctionName (moduleName : Name) : String :=
 
 -- assumes `s` has been generated `Name.mangle n ""`
 def Name.unmangleAux (s : String) (p : s.ValidPos) (res : Name)
-    (acc : String) (ucount : Nat) : Nat → Name
-  | 0 => res.str (acc.pushn '_' (ucount / 2))
-  | k + 1 =>
-    if h : p = s.endValidPos then res.str (acc.pushn '_' (ucount / 2)) else
+    (acc : String) (ucount : Nat) : Name :=
+  if h : p = s.endValidPos then res.str (acc.pushn '_' (ucount / 2)) else
+  let ch := p.get h
+  let p := p.next h
+  if ch = '_' then unmangleAux s p res acc (ucount + 1) else
+  if ucount % 2 = 0 then
+    unmangleAux s p res (acc.pushn '_' (ucount / 2) |>.push ch) 0
+  else if ch.isDigit then
+    let res := res.str (acc.pushn '_' (ucount / 2))
+    if h : ch = '0' ∧ ∃ h : p ≠ s.endValidPos, p.get h = '0' then
+      unmangleAux s (p.next h.2.1) res "" 0
+    else
+      decodeNum s p res (ch.val - 48).toNat
+  else if h : ch = 'x' ∧ checkLowerHex 2 s p then
+    let acc := acc.pushn '_' (ucount / 2)
+    unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 2 s p h.2 0))) 0
+  else if h : ch = 'u' ∧ checkLowerHex 4 s p then
+    let acc := acc.pushn '_' (ucount / 2)
+    unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 4 s p h.2 0))) 0
+  else if h : ch = 'U' ∧ checkLowerHex 8 s p then
+    let acc := acc.pushn '_' (ucount / 2)
+    unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 8 s p h.2 0))) 0
+  else
+    unmangleAux s p (res.str acc) ("".pushn '_' (ucount / 2) |>.push ch) 0
+termination_by p.remainingBytes
+decreasing_by
+  · apply String.ValidPos.remainingBytes_next_lt
+  · apply String.ValidPos.remainingBytes_next_lt
+  · apply String.ValidPos.remainingBytes_next_lt_of_lt
+        (Nat.lt_trans (String.ValidPos.lt_next _) (String.ValidPos.lt_next _))
+  · apply String.ValidPos.remainingBytes_next_lt
+  · apply String.ValidPos.remainingBytes_next_lt_of_lt
+      (Nat.lt_trans (String.ValidPos.lt_next _) (Nat.lt_add_of_pos_right (by decide)))
+  · apply String.ValidPos.remainingBytes_next_lt_of_lt
+      (Nat.lt_trans (String.ValidPos.lt_next _) (Nat.lt_add_of_pos_right (by decide)))
+  · apply String.ValidPos.remainingBytes_next_lt_of_lt
+      (Nat.lt_trans (String.ValidPos.lt_next _) (Nat.lt_add_of_pos_right (by decide)))
+  · apply String.ValidPos.remainingBytes_next_lt
+where
+  decodeNum (s : String) (p : s.ValidPos) (res : Name) (n : Nat) : Name :=
+    if h : p = s.endValidPos then res.num n else
     let ch := p.get h
     let p := p.next h
-    if ch = '_' then unmangleAux s p res acc (ucount + 1) k else
-    if ucount % 2 = 0 then
-      unmangleAux s p res (acc.pushn '_' (ucount / 2) |>.push ch) 0 k
-    else if ch.isDigit then
-      let res := res.str (acc.pushn '_' (ucount / 2))
-      if h : ch = '0' ∧ ∃ h : p ≠ s.endValidPos, p.get h = '0' then
-        unmangleAux s (p.next h.2.1) res "" 0 k
-      else
-        decodeNum s p res (ch.val - 48).toNat k
-    else if h : ch = 'x' ∧ checkLowerHex 2 s p then
-      let acc := acc.pushn '_' (ucount / 2)
-      unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 2 s p h.2 0))) 0 k
-    else if h : ch = 'u' ∧ checkLowerHex 4 s p then
-      let acc := acc.pushn '_' (ucount / 2)
-      unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 4 s p h.2 0))) 0 k
-    else if h : ch = 'U' ∧ checkLowerHex 8 s p then
-      let acc := acc.pushn '_' (ucount / 2)
-      unmangleAux s ⟨_, valid_of_checkLowerHex h.2⟩ res (acc.push (Char.ofNat (parseLowerHex 8 s p h.2 0))) 0 k
-    else
-      unmangleAux s p (res.str acc) ("".pushn '_' (ucount / 2) |>.push ch) 0 k
-where
-  decodeNum (s : String) (p : s.ValidPos) (res : Name) (n : Nat) : Nat → Name
-    | 0 => res.num n
-    | k + 1 =>
-      if h : p = s.endValidPos then res.num n else
-      let ch := p.get h
-      let p := p.next h
-      if ch.isDigit then
-        decodeNum s p res (n * 10 + (ch.val - 48).toNat) k
-      else -- assume ch = '_'
-        let res := res.num n
-        if h : p = s.endValidPos then res else
-        nameStart s (p.next h) res k -- assume s.get' p h = '_'
-  nameStart (s : String) (p : s.ValidPos) (res : Name) : Nat → Name
-    | 0 => res
-    | k + 1 =>
+    if ch.isDigit then
+      decodeNum s p res (n * 10 + (ch.val - 48).toNat)
+    else -- assume ch = '_'
+      let res := res.num n
       if h : p = s.endValidPos then res else
-      let ch := p.get h
-      let p := p.next h
-      if ch.isDigit then
-        if h : ch = '0' ∧ ∃ h : p ≠ s.endValidPos, p.get h = '0' then
-          unmangleAux s (p.next h.2.1) res "" 0 k
-        else
-          decodeNum s p res (ch.val - 48).toNat k
-      else if ch = '_' then
-        unmangleAux s p res "" 1 k
+      nameStart s (p.next h) res -- assume s.get' p h = '_'
+  termination_by p.remainingBytes
+  decreasing_by
+    · apply String.ValidPos.remainingBytes_next_lt
+    · apply String.ValidPos.remainingBytes_next_lt_of_lt
+        (Nat.lt_trans (String.ValidPos.lt_next _) (String.ValidPos.lt_next _))
+  nameStart (s : String) (p : s.ValidPos) (res : Name) : Name :=
+    if h : p = s.endValidPos then res else
+    let ch := p.get h
+    let p := p.next h
+    if ch.isDigit then
+      if h : ch = '0' ∧ ∃ h : p ≠ s.endValidPos, p.get h = '0' then
+        unmangleAux s (p.next h.2.1) res "" 0
       else
-        unmangleAux s p res (String.singleton ch) 0 k
+        decodeNum s p res (ch.val - 48).toNat
+    else if ch = '_' then
+      unmangleAux s p res "" 1
+    else
+      unmangleAux s p res (String.singleton ch) 0
+  termination_by p.remainingBytes
+  decreasing_by
+    · apply String.ValidPos.remainingBytes_next_lt_of_lt
+        (Nat.lt_trans (String.ValidPos.lt_next _) (String.ValidPos.lt_next _))
+    all_goals apply String.ValidPos.remainingBytes_next_lt
 
 /-- Assuming `s` has been produced by `Name.mangle _ ""`, return the original name. -/
 public def Name.unmangle (s : String) : Name :=
-  unmangleAux.nameStart s s.startValidPos .anonymous s.length
+  unmangleAux.nameStart s s.startValidPos .anonymous
 
 /--
 Returns the unmangled version of `s`, if it's the result of `Name.mangle _ ""`. Otherwise returns

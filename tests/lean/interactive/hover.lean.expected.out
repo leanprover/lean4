leanprover/lean4

commit 3e2124bb489dd98025427015ae5b3c5a17d14aad

feat: docstrings with Verso syntax (#10307)
This PR upstreams the Verso parser and adds preliminary support for
Verso in docstrings. This will allow the compiler to check examples and
cross-references in documentation.

After a `stage0` update, a follow-up PR will add the appropriate
attributes that allow the feature to be used. The parser tests from
Verso also remain to be upstreamed, and user-facing documentation will
be added once the feature has been used on more internals.

src/Lean/Elab/MutualInductive.lean
@@ -19,7 +19,9 @@ public import Lean.Elab.DefView
 public import Lean.Elab.DeclUtil
 public import Lean.Elab.Deriving.Basic
 public import Lean.Elab.DeclarationRange
+public import Lean.Parser.Command
 import Lean.Elab.ComputedFields
+import Lean.DocString.Extension
 import Lean.Meta.Constructions.CtorIdx
 import Lean.Meta.Constructions.CtorElim
 import Lean.Meta.IndPredBelow
@@ -110,6 +112,8 @@ structure InductiveView where
   ctors           : Array CtorView
   computedFields  : Array ComputedFieldView
   derivingClasses : Array DerivingClassView
+  /-- The declaration docstring, and whether it's Verso -/
+  docString?      : Option (TSyntax ``Lean.Parser.Command.docComment × Bool)
   deriving Inhabited
 
 /-- Elaborated header for an inductive type before fvars for each inductive are added to the local context. -/
@@ -1071,6 +1075,18 @@ private def elabInductiveViewsPostprocessing (views : Array InductiveView) (res 
     for view in views do withRef view.declId <| Term.applyAttributesAt view.declName view.modifiers.attrs .afterTypeChecking
     for elab' in finalizers do elab'.finalize
   applyDerivingHandlers views
+  -- Docstrings are added during postprocessing to allow them to have checked references to
+  -- the type and its constructors, but before attributes to enable e.g. `@[inherit_doc X]`
+  runTermElabM fun _ => Term.withDeclName view0.declName do withRef ref do
+    for view in views do
+      withRef view.declId do
+        if let some (doc, verso) := view.docString? then
+          addDocStringOf verso view.declName view.binders doc
+      for ctor in view.ctors do
+        withRef ctor.declId do
+          if let some (doc, verso) := ctor.modifiers.docString? then
+            addDocStringOf verso ctor.declName ctor.binders doc
+
   runTermElabM fun _ => Term.withDeclName view0.declName do withRef ref do
     for view in views do withRef view.declId <| Term.applyAttributesAt view.declName view.modifiers.attrs .afterCompilation
 

tests/lean/interactive/hover.lean.expected.out
@@ -112,7 +112,8 @@
 {"range":
  {"start": {"line": 73, "character": 18}, "end": {"line": 73, "character": 25}},
  "contents":
- {"value": "```lean\nmyInfix : Lean.TrailingParserDescr\n```",
+ {"value":
+  "```lean\nmyInfix : Lean.TrailingParserDescr\n```\n***\nAddition of natural numbers, typically used via the `+` operator.\n\nThis function is overridden in both the kernel and the compiler to efficiently evaluate using the\narbitrary-precision arithmetic library. The definition provided here is the logical model.\n",
   "kind": "markdown"}}
 {"textDocument": {"uri": "file:///hover.lean"},
  "position": {"line": 73, "character": 39}}

commit 18d7eba3eef6f20f5436d74ae84472de2d7f14c9

fix: hovers and docstrings for (co)inductive types
This PR fixes a regression introduced by #10307, where hovering the
name of an inductive type or constructor in its own declaration didn't
show the docstring. In the process, a bug in docstring handling for
coinductive types was discovered and also fixed. Tests are added to
prevent the regression from repeating in the future.

src/Lean/Elab/MutualInductive.lean
@@ -1123,7 +1123,6 @@ Term.withoutSavingRecAppSyntax do
 private def mkInductiveDecl (vars : Array Expr) (elabs : Array InductiveElabStep1) : TermElabM FinalizeContext :=
   withElaboratedHeaders vars elabs fun vars elabs rs scopeLevelNames => do
     let res ← mkInductiveDeclCore addAndFinalizeInductiveDecl vars elabs rs scopeLevelNames
-    addTermInfoViews <| elabs.map (·.view)
     return res
 
 private def mkAuxConstructions (declNames : Array Name) : TermElabM Unit := do
@@ -1275,9 +1274,11 @@ private def elabInductiveViewsPostprocessing (views : Array InductiveView) (res 
       unless (views.any (·.isCoinductive)) do
         Term.applyAttributesAt view.declName view.modifiers.attrs .afterCompilation
 
+  -- Term info is added here so that docstrings are maximally available in the environment for hovers
+  runTermElabM fun _ => Term.withDeclName view0.declName <| withRef ref <| addTermInfoViews views
+
 private def elabInductiveViewsPostprocessingCoinductive (views : Array InductiveView)
      : CommandElabM Unit := do
-  let views := views.map updateViewRemovingFunctorName
   let view0 := views[0]!
   let ref := view0.ref
 
@@ -1298,6 +1299,9 @@ private def elabInductiveViewsPostprocessingCoinductive (views : Array Inductive
     for view in views do withRef view.declId <|
       unless (views.any (·.isCoinductive)) do
         Term.applyAttributesAt view.declName view.modifiers.attrs .afterCompilation
+
+  -- Term info is added here so that docstrings are maximally available in the environment for hovers
+  runTermElabM fun _ => Term.withDeclName view0.declName <| withRef ref <| addTermInfoViews views
 
 def InductiveViewToCoinductiveElab (e : InductiveElabStep1) : CoinductiveElabData where
   declId := e.view.declId
@@ -1321,7 +1325,6 @@ def elabInductives (inductives : Array (Modifiers × Syntax)) : CommandElabM Uni
       elabFlatInductiveViews vars flatElabs
       discard <| flatElabs.mapM fun e => MetaM.run' do mkSumOfProducts e.view.declName
       elabCoinductive (flatElabs.map InductiveViewToCoinductiveElab)
-      addTermInfoViews <| elabs.map (·.view)
     elabInductiveViewsPostprocessingCoinductive (elabs.map (·.view))
   else
     let res ← runTermElabM fun vars => do

tests/lean/interactive/hover.lean
@@ -306,3 +306,43 @@ since it needs the environment with the generated `_eval.match_1` matcher.
 -/
 #eval (default : Nat) matches .succ ..
                       --^ textDocument/hover
+
+/--
+These are docs
+-/
+structure S where
+        --^ textDocument/hover
+  /-- So are these -/
+  mk ::
+--^ textDocument/hover
+  /-- And these -/
+  x : Nat
+--^ textDocument/hover
+
+#check { x := 5 : S }
+       --^ textDocument/hover
+                --^ textDocument/hover
+
+/-- Docs -/
+inductive S' where
+        --^ textDocument/hover
+  /-- More docs -/
+  | mk (x : Nat)
+  --^ textDocument/hover
+
+#check (.mk 5 : S')
+       --^ textDocument/hover
+              --^ textDocument/hover
+
+/-- An infinite sequence -/
+coinductive InfSeq (r : α → α → Prop) : α → Prop where
+          --^ textDocument/hover
+  /-- Take a step -/
+  | step : r a b → InfSeq r b → InfSeq r a
+     --^ textDocument/hover
+
+#check InfSeq
+     --^ textDocument/hover
+
+#check InfSeq.step
+     --^ textDocument/hover

tests/lean/interactive/hover.lean.expected.out
@@ -657,3 +657,102 @@
  {"start": {"line": 306, "character": 6},
   "end": {"line": 306, "character": 38}},
  "contents": {"value": "```lean\nBool\n```", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 312, "character": 10}}
+{"range":
+ {"start": {"line": 312, "character": 10},
+  "end": {"line": 312, "character": 11}},
+ "contents":
+ {"value": "```lean\nS : Type\n```\n***\nThese are docs\n", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 315, "character": 2}}
+{"range":
+ {"start": {"line": 315, "character": 2}, "end": {"line": 315, "character": 4}},
+ "contents":
+ {"value": "```lean\nS.mk (x : ℕ) : S\n```\n***\nSo are these ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 318, "character": 2}}
+{"range":
+ {"start": {"line": 318, "character": 2}, "end": {"line": 318, "character": 3}},
+ "contents":
+ {"value": "```lean\nS.x (self : S) : ℕ\n```\n***\nAnd these ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 321, "character": 9}}
+{"range":
+ {"start": {"line": 321, "character": 9},
+  "end": {"line": 321, "character": 10}},
+ "contents":
+ {"value": "```lean\nx : ℕ\n```\n***\nAnd these ", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 321, "character": 18}}
+{"range":
+ {"start": {"line": 321, "character": 18},
+  "end": {"line": 321, "character": 19}},
+ "contents":
+ {"value": "```lean\nS : Type\n```\n***\nThese are docs\n", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 326, "character": 10}}
+{"range":
+ {"start": {"line": 326, "character": 10},
+  "end": {"line": 326, "character": 12}},
+ "contents":
+ {"value": "```lean\nS' : Type\n```\n***\nDocs ", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 329, "character": 4}}
+{"range":
+ {"start": {"line": 329, "character": 4}, "end": {"line": 329, "character": 6}},
+ "contents":
+ {"value": "```lean\nS'.mk (x : ℕ) : S'\n```\n***\nMore docs ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 332, "character": 9}}
+{"range":
+ {"start": {"line": 332, "character": 8},
+  "end": {"line": 332, "character": 11}},
+ "contents":
+ {"value": "```lean\nS'.mk (x : ℕ) : S'\n```\n***\nMore docs ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 332, "character": 16}}
+{"range":
+ {"start": {"line": 332, "character": 16},
+  "end": {"line": 332, "character": 18}},
+ "contents":
+ {"value": "```lean\nS' : Type\n```\n***\nDocs ", "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 337, "character": 12}}
+{"range":
+ {"start": {"line": 337, "character": 12},
+  "end": {"line": 337, "character": 18}},
+ "contents":
+ {"value":
+  "```lean\nInfSeq.{u_1} {α : Sort u_1} (r : α → α → Prop) : α → Prop\n```\n***\nAn infinite sequence ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 340, "character": 7}}
+{"range":
+ {"start": {"line": 340, "character": 4}, "end": {"line": 340, "character": 8}},
+ "contents":
+ {"value":
+  "```lean\nInfSeq.step.{u_1} {α : Sort u_1} (r : α → α → Prop) {a b : α} : r a b → InfSeq r b → InfSeq r a\n```\n***\nTake a step ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 343, "character": 7}}
+{"range":
+ {"start": {"line": 343, "character": 7},
+  "end": {"line": 343, "character": 13}},
+ "contents":
+ {"value":
+  "```lean\nInfSeq.{u_1} {α : Sort u_1} (r : α → α → Prop) : α → Prop\n```\n***\nAn infinite sequence ",
+  "kind": "markdown"}}
+{"textDocument": {"uri": "file:///hover.lean"},
+ "position": {"line": 346, "character": 7}}
+{"range":
+ {"start": {"line": 346, "character": 7},
+  "end": {"line": 346, "character": 18}},
+ "contents":
+ {"value":
+  "```lean\nInfSeq.step.{u_1} {α : Sort u_1} (r : α → α → Prop) {a b : α} : r a b → InfSeq r b → InfSeq r a\n```\n***\nTake a step ",
+  "kind": "markdown"}}
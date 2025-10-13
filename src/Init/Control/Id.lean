fed3e94305739f2bcccd867371646e1e61a88b94
Sebastian Graf <sgraf1337@gmail.com>
2025-10-13 11:46:21 +0200

    WIP
    Full working duplicable continuations
    
    simplifications and comments
    
    Nested do blocks
    
    WIP
    
    WIP
    
    feat: add `OptionCpsT`
    
    feat: use strict implicit binder for `ExceptCpsT`
    
    rudimentary stateless for loop
    
    state for loop
    
    break and continue
    
    Some testing with early return from a loop
    
    feat: eta-expand for oversaturating arguments while specializing (#10924)
    
    Revert "feat: eta-expand for oversaturating arguments while specializing (#10924)"
    
    This reverts commit 3bda93242ee7ae3dbcc497c313839770e7cec1cf.
    
    Nested for loops with early return, break and continue
    
    extract some useful functions from `for` elaboration
    
    documentation and refactorings
    
    refactor
    
    `try`/`catch` + let muts, `for ... in ... invariant ... do`
    
    Finished `invariant` syntax, found fix for `let` inlining problem
    
    invariant syntax for state vars
    
    simplify DoElemCont, smaller refactorings
    
    Checkpoint
    
    refactor
    
    refactor
    
    WIP
    
    WIP
    
    WIP
    
    Amen
    
    Fix invariant stuff
    
    Despair in the face of metavar assignment
    
    Found a fix (phew!). Abandoned the `eraseProxyDefs` idea because it makes MVar assignment fail
    
    chore: add `kind` argument to `withLocalDeclsDND`
    
    chore: add `kind` argument to `withLocalDeclsDND`
    
    stuff
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    elaboration framework works; now porting elaborators to Core
    
    WIP

--- src/Init/Control/Id.lean
+++ src/Init/Control/Id.lean
@@ -68,3 +68,9 @@ instance {m : Type u → Type v} [Pure m] : MonadLiftT Id m where
   monadLift x := pure x.run
 
 end Id
+
+def ForInNew.toArray {ρ α} [ForInNew Id ρ α] (r : ρ) : Array α :=
+  Id.run (forIn r (Array.mkEmpty 0) (fun a k arr => k (arr.push a)) pure)
+
+def ForInNew.toList {ρ α} [ForInNew Id ρ α] (r : ρ) : List α :=
+  toArray r |>.toList

61fa50d1bac99c2009efa6a2fa4f58e16a17385c
Sebastian Graf <sgraf1337@gmail.com>
2025-10-13 11:46:21 +0200

    WIP
    Full working duplicable continuations
    
    simplifications and comments
    
    Nested do blocks
    
    WIP
    
    WIP
    
    feat: add `OptionCpsT`
    
    feat: use strict implicit binder for `ExceptCpsT`
    
    rudimentary stateless for loop
    
    state for loop
    
    break and continue
    
    Some testing with early return from a loop
    
    feat: eta-expand for oversaturating arguments while specializing (#10924)
    
    Revert "feat: eta-expand for oversaturating arguments while specializing (#10924)"
    
    This reverts commit 3bda93242ee7ae3dbcc497c313839770e7cec1cf.
    
    Nested for loops with early return, break and continue
    
    extract some useful functions from `for` elaboration
    
    documentation and refactorings
    
    refactor
    
    `try`/`catch` + let muts, `for ... in ... invariant ... do`
    
    Finished `invariant` syntax, found fix for `let` inlining problem
    
    invariant syntax for state vars
    
    simplify DoElemCont, smaller refactorings
    
    Checkpoint
    
    refactor
    
    refactor
    
    WIP
    
    WIP
    
    WIP
    
    Amen
    
    Fix invariant stuff
    
    Despair in the face of metavar assignment
    
    Found a fix (phew!). Abandoned the `eraseProxyDefs` idea because it makes MVar assignment fail
    
    chore: add `kind` argument to `withLocalDeclsDND`
    
    chore: add `kind` argument to `withLocalDeclsDND`
    
    stuff
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    elaboration framework works; now porting elaborators to Core
    
    WIP

---
+++ src/Init/Control/Do.lean
@@ -0,0 +1,59 @@
+/-
+Copyright (c) 2025 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sebastian Graf
+-/
+module
+
+prelude
+public import Init.Control.Except
+public import Init.Control.Option
+
+public section
+
+/-!
+This module provides specialized wrappers around `ExceptT` to support the `do` elaborator.
+
+Specifically, the types here are used to tunnel early `return`, `break` and `continue` through
+non-algebraic higher-order effect combinators such as `tryCatch`.
+-/
+
+/-- A wrapper around `ExceptT` signifying early return. -/
+@[expose]
+abbrev EarlyReturnT (ρ m α) := ExceptT ρ m α
+
+/-- Exit a computation by returning a value `r : ρ` early. -/
+@[always_inline, inline, expose]
+abbrev EarlyReturnT.return {ρ m α} [Monad m] (r : ρ) : EarlyReturnT ρ m α :=
+  throw r
+
+/-- A specialization of `Except.casesOn`. -/
+@[always_inline, inline, expose]
+abbrev EarlyReturn.runK {ρ α : Type u} {β : Type v} (x : Except ρ α) (ret : ρ → β) (pure : α → β) : β :=
+  x.casesOn ret pure
+
+/-- A wrapper around `OptionT` signifying `break` in a loop. -/
+@[expose]
+abbrev BreakT := OptionT
+
+/-- Exit a loop body via `break`. -/
+@[always_inline, inline, expose]
+abbrev BreakT.break {m : Type w → Type x} [Monad m] : BreakT m α := failure
+
+/-- A specialization of `Option.casesOn`. -/
+@[always_inline, inline, expose]
+abbrev Break.runK {α : Type u} {β : Type v} (x : Option α) (breakK : Unit → β) (successK : α → β) : β :=
+  x.casesOn breakK (fun a _ => successK a) ()
+
+/-- A wrapper around `OptionT` signifying `continue` in a loop. -/
+@[expose]
+abbrev ContinueT := OptionT
+
+/-- Exit a loop body via `continue`. -/
+@[always_inline, inline, expose]
+abbrev ContinueT.continue {m : Type w → Type x} [Monad m] : ContinueT m α := failure
+
+/-- A specialization of `Option.casesOn`. -/
+@[always_inline, inline, expose]
+abbrev Continue.runK {α : Type u} {β : Type v} (x : Option α) (continueK : Unit → β) (successK : α → β) : β :=
+  x.casesOn continueK (fun a _ => successK a) ()

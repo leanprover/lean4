fb6192e2f6708770478a2e4b1e81b825ca5a0e0a
Wojciech Rozowski <wojciech@lean-fro.org>
2025-10-14 18:13:51 +0100

    push intermediate work

--- src/Std/Data/DTreeMap/Slice.lean
+++ src/Std/Data/DTreeMap/Slice.lean
@@ -16,11 +16,14 @@ public import Std.Data.DTreeMap
 namespace Std.DTreeMap
 open Std.Iterators
 
-
 -- Zipper-based iterator for tree maps
 inductive MapIterator (α : Type u) (β : α → Type v) where
   | done
   | cons (k : α) (v : β k) (tree : Internal.Impl α β) (next : MapIterator α β)
+
+def MapIterator.toList : MapIterator α β → List ((a : α) × β a)
+| .done => []
+| .cons k v tree next => ⟨k,v⟩ :: tree.toList ++ next.toList
 
 def treeSize : Internal.Impl α β → Nat
 | .leaf => 0
@@ -34,17 +37,23 @@ def MapIterator.prependMap : Internal.Impl α β → MapIterator α β → MapIt
   | .leaf, it => it
   | .inner _ k v l r, it => prependMap l (.cons k v r it)
 
+def prependMap_to_list (t : Internal.Impl α β) (it : MapIterator α β) : (MapIterator.prependMap t it).toList = t.toList ++ it.toList := by
+  induction t generalizing it
+  case leaf =>
+    simp [MapIterator.prependMap, Internal.Impl.toList_eq_toListModel]
+  case inner _ k v l r l_ih r_ih =>
+    simp only [MapIterator.prependMap]
+    specialize l_ih (MapIterator.cons k v r it)
+    rw [l_ih]
+    simp [MapIterator.toList, Internal.Impl.toList_eq_toListModel]
+
 variable (α : Type u) (β : α → Type v)
 theorem prependMap_size  (t : Internal.Impl α β) (it : MapIterator α β) : (MapIterator.prependMap t it).size = treeSize t + it.size := by
   fun_induction MapIterator.prependMap
   case case1 =>
-   simp [treeSize]
+   simp only [treeSize, Nat.zero_add]
   case case2 size k v l r it ih =>
-    rw [ih]
-    simp [treeSize, MapIterator.size]
-    rw [←Nat.add_assoc]
-    sorry
-
+    simp only [ih, MapIterator.size, treeSize, ← Nat.add_assoc, Nat.add_comm]
 
 structure RxcIterator (cmp : α → α → Ordering) where
   iter : MapIterator α β
@@ -97,6 +106,21 @@ def RxC_finite : FinitenessRelation (RxcIterator α β cmp) Id where
           simp at h'
           simp only [h2, ← h'.1, prependMap_size, MapIterator.size, Nat.add_lt_add_iff_right,
             Nat.lt_add_left_iff_pos, Nat.lt_add_one]
+
+instance instFinite [TransCmp cmp] : Finite (RxcIterator α β cmp) Id :=
+  .of_finitenessRelation (RxC_finite α β)
+
+instance : IteratorCollect (RxcIterator α β cmp) Id Id := .defaultImplementation
+
+instance : IteratorCollectPartial (RxcIterator α β cmp) Id Id := .defaultImplementation
+
+def test : Internal.Impl Nat fun _ => Nat :=
+  .ofList [⟨1, 2⟩, ⟨5, 4⟩, ⟨3, 1⟩, ⟨2, 8⟩, ⟨7, 9⟩, ⟨10, 5⟩]
+
+def iterLE (t : Internal.Impl α β) (comp : α → α → Ordering) (bound : α) : IterM (α := RxcIterator α β comp) Id ((a : α) × β a) :=
+  ⟨{iter := MapIterator.prependMap t .done, upper := bound }⟩
+
+#eval (iterLE _ _ test compare 5).allowNontermination.toList
 
 section Rxc
 end Rxc

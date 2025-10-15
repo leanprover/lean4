6fd84dc1768077662e59fcf41f52315782192258
Sebastian Ullrich <sebasti@nullri.ch>
2025-10-15 14:23:47 +0200

    fix tests

--- tests/lean/run/emptyLcnf.lean
+++ tests/lean/run/emptyLcnf.lean
@@ -12,7 +12,8 @@ trace: [Compiler.result] size: 0
       ⊥
 ---
 trace: [Compiler.result] size: 5
-    def _eval._lam_0 _x.1 _x.2 _y.3 _y.4 _y.5 _y.6 _y.7 _y.8 _y.9 : EStateM.Result Lean.Exception lcRealWorld PUnit :=
+    def _private.lean.run.emptyLcnf.0._eval._lam_0 _x.1 _x.2 _y.3 _y.4 _y.5 _y.6 _y.7 _y.8 _y.9 : EStateM.Result
+      Lean.Exception lcRealWorld PUnit :=
       let _x.10 := Lean.Compiler.compile _x.1 _y.7 _y.8 _y.9;
       cases _x.10 : EStateM.Result Lean.Exception lcRealWorld PUnit
       | EStateM.Result.ok a.11 a.12 =>
@@ -21,34 +22,34 @@ trace: [Compiler.result] size: 5
       | EStateM.Result.error a.14 a.15 =>
         return _x.10
 [Compiler.result] size: 1
-    def _eval._closed_0 : String :=
+    def _private.lean.run.emptyLcnf.0._eval._closed_0 : String :=
       let _x.1 := "f";
       return _x.1
 [Compiler.result] size: 2
-    def _eval._closed_1 : Lean.Name :=
-      let _x.1 := _eval._closed_0;
+    def _private.lean.run.emptyLcnf.0._eval._closed_1 : Lean.Name :=
+      let _x.1 := _eval._closed_0.2;
       let _x.2 := Lean.Name.mkStr1 _x.1;
       return _x.2
 [Compiler.result] size: 2
-    def _eval._closed_2 : Array Lean.Name :=
+    def _private.lean.run.emptyLcnf.0._eval._closed_2 : Array Lean.Name :=
       let _x.1 := 1;
       let _x.2 := Array.mkEmpty ◾ _x.1;
       return _x.2
 [Compiler.result] size: 3
-    def _eval._closed_3 : Array Lean.Name :=
-      let _x.1 := _eval._closed_1;
-      let _x.2 := _eval._closed_2;
+    def _private.lean.run.emptyLcnf.0._eval._closed_3 : Array Lean.Name :=
+      let _x.1 := _eval._closed_1.2;
+      let _x.2 := _eval._closed_2.2;
       let _x.3 := Array.push ◾ _x.2 _x.1;
       return _x.3
 [Compiler.result] size: 8
-    def _eval a.1 a.2 a.3 : EStateM.Result Lean.Exception lcRealWorld PUnit :=
-      let _x.4 := _eval._closed_0;
-      let _x.5 := _eval._closed_1;
+    def _private.lean.run.emptyLcnf.0._eval a.1 a.2 a.3 : EStateM.Result Lean.Exception lcRealWorld PUnit :=
+      let _x.4 := _eval._closed_0.2;
+      let _x.5 := _eval._closed_1.2;
       let _x.6 := 1;
-      let _x.7 := _eval._closed_2;
-      let _x.8 := _eval._closed_3;
+      let _x.7 := _eval._closed_2.2;
+      let _x.8 := _eval._closed_3.2;
       let _x.9 := PUnit.unit;
-      let _f.10 := _eval._lam_0 _x.8 _x.9;
+      let _f.10 := _eval._lam_0.2 _x.8 _x.9;
       let _x.11 := Lean.Elab.Command.liftTermElabM._redArg _f.10 a.1 a.2 a.3;
       return _x.11
 -/

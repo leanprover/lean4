87eab5c0bee44d8c8ea863aba7b8569cfa9708dc
Mac Malone <mac@lean-fro.org>
2025-10-23 05:35:33 +0200

    chore: fix typos

--- tests/pkg/def_clash/test.sh
+++ tests/pkg/def_clash/test.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# Tests the behavior of importing transitively importing multiple modules
+# Tests the behavior of transitively importing multiple modules
 # that define the same symbol.
 
 # Currently, if two packages define the same Lean symbol (e.g., `foo`)
 # they cannot be in the same transitive import graph, even if the no module
-# can see both instances of ths symbol.
+# can see both instances of the symbol.
 
 # In the example in this directory, `fooA` and `fooB` both define `foo`.
-# `useA` privately imports and uses `fooA` and `useB` private imports and uses
-# `fooB`. When `test` imports `useA` and `useB`, the linker will complain even
-# though the Lean file does not see both `foo` definitions.
+# `useA` privately imports and uses `fooA`, and `useB` private imports and uses
+# `fooB`. When `TestUse` imports `useA` and `useB`, the linker will complain
+# even though the Lean file does not see both `foo` definitions.
 
 # See also https://github.com/leanprover/lean4/issues/222
 
@@ -19,7 +19,7 @@ source ../../lake/tests/common.sh
 ./clean.sh
 
 # Test the behavior when multiple copies of the same definition (`foo`)
-# are seen by Lean (e.g., via importing two modules which define them)
+# are seen by Lean (e.g., via importing two modules which define them).
 test_err "environment already contains 'foo'" build TestFoo
 
 # Test the behavior when multiple copies of the same definition (`foo`) exist

fc10fc397be47bd549bcb6809cc76762badf3c2a
Leonardo de Moura <leomoura@amazon.com>
2025-10-26 13:55:19 -0700

    feat: add `Init/Grind/Config.lean`

--- src/Init/Grind/Interactive.lean
+++ src/Init/Grind/Interactive.lean
@@ -16,6 +16,16 @@ when selecting patterns.
 -/
 syntax grindLemmaMin := ppGroup("!" (Attr.grindMod ppSpace)? ident)
 
+/-!
+`grind` tactic and related tactics.
+-/
+syntax grindErase    := "-" ident
+/--
+The `!` modifier instructs `grind` to consider only minimal indexable subexpressions
+when selecting patterns.
+-/
+syntax grindParam    := grindErase <|> grindLemma <|> grindLemmaMin
+
 namespace Grind
 declare_syntax_cat grind_filter (behavior := both)
 

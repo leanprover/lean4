065094f87a1a14fde48a10e29027f6304b52eeca
Leonardo de Moura <leomoura@amazon.com>
2025-10-27 15:09:25 -0400

    feat: `set_config` for setting `grind` configuration options
    This PR adds the `set_config` tactic for setting `grind` configuration
    options. It uses the same syntax used for setting configuration
    options in the `grind` main tactic.

--- src/Lean/Elab/Tactic/Grind/Config.lean
+++ src/Lean/Elab/Tactic/Grind/Config.lean
@@ -13,4 +13,20 @@ namespace Lean.Elab.Tactic.Grind
 /-- Sets a field of the `grind` configuration object. -/
 declare_config_getter setConfigField Grind.Config
 
+def elabConfigItems (init : Grind.Config) (items : Array (TSyntax `Lean.Parser.Tactic.configItem))
+    : CoreM Grind.Config := do
+  let mut config := init
+  for item in items do
+    match item with
+    | `(Lean.Parser.Tactic.configItem| ($fieldName:ident := true))
+    | `(Lean.Parser.Tactic.configItem| +$fieldName:ident) =>
+      config ← setConfigField config fieldName.getId true
+    | `(Lean.Parser.Tactic.configItem| ($fieldName:ident := false))
+    | `(Lean.Parser.Tactic.configItem| -$fieldName:ident) =>
+      config ← setConfigField config fieldName.getId false
+    | `(Lean.Parser.Tactic.configItem| ($fieldName:ident := $val:num)) =>
+      config ← setConfigField config fieldName.getId (.ofNat val.getNat)
+    | _ => throwErrorAt item "unexpected configuration option"
+  return config
+
 end Lean.Elab.Tactic.Grind

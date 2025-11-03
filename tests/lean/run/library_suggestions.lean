81481fd0a2476597b33ed0ac52a43f38f308a7cd
Kim Morrison <kim@tqft.net>
2025-11-03 03:55:34 +0100

    feat: make `set_library_suggestions` persistent

--- tests/lean/run/library_suggestions.lean
+++ tests/lean/run/library_suggestions.lean
@@ -14,14 +14,6 @@ error: Failed to elaborate Nat as a `MVarId → Config → MetaM (Array Suggesti
 #guard_msgs in
 set_library_suggestions Nat
 
-/--
-error: No library suggestions engine registered. (Note that Lean does not provide a default library suggestions engine, these must be provided by a downstream library, and configured using `set_library_suggestions`.)
--/
-#guard_msgs in
-example : True := by
-  suggestions
-  trivial
-
 set_library_suggestions (fun _ _ => pure #[])
 
 /-- info: Library suggestions: [] -/

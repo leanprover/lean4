0a93219d66cf40abf6e01a32c9c18a030310d6ee
Mac Malone <mac@lean-fro.org>
2025-11-03 02:13:49 +0100

    chore: test touchup

--- tests/lake/tests/semver/online-test.sh
+++ tests/lake/tests/semver/online-test.sh
@@ -7,7 +7,7 @@ source ../common.sh
 test_out 'using version `2.0.0`' -R -KdepVer="=2.0.0" update --keep-toolchain
 
 # Test an approximate version
-test_out 'using version `2.1.0`' -R -KdepVer="2.*.*" update --keep-toolchain
+test_out 'using version `2.1.0`' -R -KdepVer="~2" update --keep-toolchain
 
 # Test a complex version
 test_out 'using version `2.0.0`' -R -KdepVer="1.*.* || 2.x.x, !=2.1.0" update --keep-toolchain

c61283222faa3a3a2269d6d1faf3099ce6f31f36
Henrik Böving <hargonix@gmail.com>
2025-11-04 16:36:57 +0100

    perf: detect unnecessary repeated checks in more situations

--- tests/lean/run/double_match.lean
+++ tests/lean/run/double_match.lean
@@ -21,3 +21,30 @@ def foo (f : Nat → Option Nat) (x : Nat) : Option Nat :=
       none
   else
     none
+
+/--
+trace: [Compiler.saveMono] size: 9
+    def bar a i xs : Nat :=
+      let _x.1 := Nat.decLt a i;
+      cases _x.1 : Nat
+      | Bool.false =>
+        let _x.2 := 0;
+        return _x.2
+      | Bool.true =>
+        let stuff := 12;
+        let _x.3 := 0;
+        let _x.4 := Array.get!Internal ◾ _x.3 xs a;
+        let _x.5 := Nat.add _x.4 stuff;
+        return _x.5
+-/
+#guard_msgs in
+set_option trace.Compiler.saveMono true in
+def bar (a : Nat) (i : Nat) (xs : Array Nat) :=
+  if a < i then
+    let stuff := 12
+    if a < i then
+      xs[a]! + stuff
+    else
+      stuff
+  else
+    0

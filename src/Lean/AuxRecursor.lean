0cb79868f4b80a8018a7737bf53e54760443f906
Joachim Breitner <mail@joachim-breitner.de>
2025-11-05 16:49:11 +0100

    feat: sparse casesOn constructions (#11072)
    This PR adds “sparse casesOn” constructions. They are similar to
    `.casesOn`, but have arms only for some constructors and a catch-all
    (providing `t.ctorIdx ≠ 42` assumptions). The compiler has native
    support for these constructors and now (because of the similarity) also
    the per-constructor elimination principles.

--- src/Lean/AuxRecursor.lean
+++ src/Lean/AuxRecursor.lean
@@ -23,7 +23,7 @@ def mkRecOnName (indDeclName : Name) : Name   := Name.mkStr indDeclName recOnSuf
 def mkBRecOnName (indDeclName : Name) : Name  := Name.mkStr indDeclName brecOnSuffix
 def mkBelowName (indDeclName : Name) : Name := Name.mkStr indDeclName belowSuffix
 
-builtin_initialize auxRecExt : TagDeclarationExtension ← mkTagDeclarationExtension
+builtin_initialize auxRecExt : TagDeclarationExtension ← mkTagDeclarationExtension (asyncMode := .local)
 
 def markAuxRecursor (env : Environment) (declName : Name) : Environment :=
   auxRecExt.tag env declName
@@ -49,6 +49,19 @@ def isRecOnRecursor (env : Environment) (declName : Name) : Bool :=
 
 def isBRecOnRecursor (env : Environment) (declName : Name) : Bool :=
   isAuxRecursorWithSuffix env declName brecOnSuffix
+
+private builtin_initialize sparseCasesOnExt : TagDeclarationExtension ← mkTagDeclarationExtension (asyncMode := .local)
+
+def markSparseCasesOn (env : Environment) (declName : Name) : Environment :=
+  sparseCasesOnExt.tag env declName
+
+/-- Is this a constructor elimination or a sparse casesOn? -/
+public def isSparseCasesOn (env : Environment) (declName : Name) : Bool :=
+  sparseCasesOnExt.isTagged env declName
+
+/-- Is this a `.casesOn`, a constructor elimination or a sparse casesOn? -/
+public def isCasesOnLike (env : Environment) (declName : Name) : Bool :=
+  isCasesOnRecursor env declName || isSparseCasesOn env declName
 
 builtin_initialize noConfusionExt : TagDeclarationExtension ← mkTagDeclarationExtension
 
---
+++ tests/lean/run/sparseCasesOn.lean
@@ -0,0 +1,46 @@
+import Lean
+
+
+/-- info: test._sparseCasesOn_1 -/
+#guard_msgs in
+run_meta
+  Lean.withDeclNameForAuxNaming `test do
+    let name ← Lean.Meta.mkSparseCasesOn ``Lean.Expr #[``Lean.Expr.fvar, ``Lean.Expr.sort]
+    Lean.logInfo m!"{name}"
+
+/--
+info: test._sparseCasesOn_1.{u} {motive : Lean.Expr → Sort u} (t : Lean.Expr)
+  (fvar : (fvarId : Lean.FVarId) → motive (Lean.Expr.fvar fvarId))
+  (sort : (u : Lean.Level) → motive (Lean.Expr.sort u)) : (t.ctorIdx ≠ 1 → t.ctorIdx ≠ 3 → motive t) → motive t
+-/
+#guard_msgs in
+#check test._sparseCasesOn_1
+
+/-- error: mkSparseCasesOn: unexpected number of universe parameters in `Or.casesOn` -/
+#guard_msgs in
+run_meta
+  Lean.withDeclNameForAuxNaming `test do
+    let name ← Lean.Meta.mkSparseCasesOn ``Or #[``Or.inl]
+    Lean.logInfo m!"{name}"
+
+
+/- Check that the compiler is producin good code -/
+
+set_option trace.Compiler.saveBase true
+
+/--
+trace: [Compiler.saveBase] size: 7
+    def testDecl motive t fvar sort x.1 : motive lcAny :=
+      cases t : motive lcAny
+      | Lean.Expr.fvar fvarId =>
+        let _x.2 := fvar fvarId;
+        return _x.2
+      | Lean.Expr.sort u =>
+        let _x.3 := sort u;
+        return _x.3
+      | _ =>
+        let _x.4 := x.1 _ _;
+        return _x.4
+-/
+#guard_msgs in
+def testDecl := @test._sparseCasesOn_1

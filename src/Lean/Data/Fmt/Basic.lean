c9fc3748186727e2fc9d82e36c60cda0bddf575b
Marc Huisinga <mhuisi@protonmail.com>
2025-11-06 16:20:23 +0100

    wip

--- src/Lean/Data/Fmt/Basic.lean
+++ src/Lean/Data/Fmt/Basic.lean
@@ -13,6 +13,7 @@ public import Init.Data.Array
 public import Std.Data.HashMap.Basic
 public import Init.Data.Hashable
 public import Std.Data.HashSet.Basic
+public import Std.Data.Iterators
 
 public section
 
@@ -102,11 +103,18 @@ def Measure.concat (m1 m2 : Measure τ) : Measure τ where
     m1.output
     m2.output
 
+def Measure.print (m : Measure τ) : String :=
+  let (_, printed) := m.output.run ""
+  printed
+
 inductive TaintedMeasure (τ : Type) where
   | mergeTainted (tm1 tm2 : TaintedMeasure τ) (maxNewlineCount? : Option Nat)
-  | taintedConcat (tm : TaintedMeasure τ) (doc : CoreDoc) (indentation : Nat) (fullness : FullnessState) (maxNewlineCount? : Option Nat)
-  | concatTainted (m : Measure τ) (tm : TaintedMeasure τ) (maxNewlineCount? : Option Nat)
+  | taintedConcat (tm1 : TaintedMeasure τ) (doc2 : CoreDoc) (indentation : Nat) (fullness : FullnessState) (maxNewlineCount? : Option Nat)
+  | concatTainted (m1 : Measure τ) (tm2 : TaintedMeasure τ) (maxNewlineCount? : Option Nat)
   | resolveTainted (doc : CoreDoc) (columnPos : Nat) (indentation : Nat) (fullness : FullnessState) (maxNewlineCount? : Option Nat)
+
+def TaintedMeasure.ptr (tm : TaintedMeasure τ) : USize :=
+  unsafe ptrAddrUnsafe tm
 
 def TaintedMeasure.maxNewlineCount? : TaintedMeasure τ → Option Nat
   | .mergeTainted (maxNewlineCount? := n) .. => n
@@ -123,12 +131,46 @@ def TaintedMeasure.merge (tm1 tm2 : TaintedMeasure τ) (prunable : Bool) : Taint
   if prunable then
     tm1
   else
-    -- The Racket formatter uses `tm1.maxNewlineCount?` here instead.
-    .mergeTainted tm1 tm2 (.merge (max · ·) tm1.maxNewlineCount? tm2.maxNewlineCount?)
+    -- TODO: Is this a good newline approximation?
+    .mergeTainted tm1 tm2 tm1.maxNewlineCount?
+
+abbrev MeasureSet.Set (τ : Type) := Array (Measure τ)
+
+def MeasureSet.Set.merge (ms1 ms2 : MeasureSet.Set τ) : MeasureSet.Set τ := Id.run do
+  let mut i1 := 0
+  let mut i2 := 0
+  let mut r := #[]
+  while h : i1 < ms1.size ∧ i2 < ms2.size do
+    let m1 := ms1[i1]
+    let m2 := ms2[i2]
+    if m1.dominates m2 then
+      i2 := i2 + 1
+    else if m2.dominates m1 then
+      i1 := i1 + 1
+    else if m1.lastLineLength > m2.lastLineLength then
+      r := r.push m1
+      i1 := i1 + 1
+    else
+      r := r.push m2
+      i2 := i2 + 1
+  r := r ++ ms1[i1...*]
+  r := r ++ ms2[i2...*]
+  return r
+
+def MeasureSet.Set.dedup (ms : MeasureSet.Set τ) : MeasureSet.Set τ := Id.run do
+  let mut deduped := #[]
+  for previous in ms, current in ms[1:] do
+    -- TODO: Why was this sound again?
+    if current.cost <= previous.cost then
+      continue
+    deduped := deduped.push previous
+  if let some last := ms[ms.size - 1]? then
+    deduped := deduped.push last
+  return deduped
 
 inductive MeasureSet (τ : Type) where
   | tainted (tm : TaintedMeasure τ)
-  | set (ms : Array (Measure τ))
+  | set (ms : MeasureSet.Set τ)
   deriving Inhabited
 
 def MeasureSet.merge (ms1 ms2 : MeasureSet τ) (prunable : Bool) : MeasureSet τ :=
@@ -144,28 +186,7 @@ def MeasureSet.merge (ms1 ms2 : MeasureSet τ) (prunable : Bool) : MeasureSet τ
   | .tainted _, _ =>
     ms2
   | .set ms1, .set ms2 =>
-    .set (mergeSets ms1 ms2)
-where
-  mergeSets (ms1 ms2 : Array (Measure τ)) : Array (Measure τ) := Id.run do
-    let mut i1 := 0
-    let mut i2 := 0
-    let mut r := #[]
-    while h : i1 < ms1.size ∧ i2 < ms2.size do
-      let m1 := ms1[i1]
-      let m2 := ms2[i2]
-      if m1.dominates m2 then
-        i2 := i2 + 1
-      else if m2.dominates m1 then
-        i1 := i1 + 1
-      else if m1.lastLineLength > m2.lastLineLength then
-        r := r.push m1
-        i1 := i1 + 1
-      else
-        r := r.push m2
-        i2 := i2 + 1
-    r := r ++ ms1[i1...*]
-    r := r ++ ms2[i2...*]
-    return r
+    .set (ms1.merge ms2)
 
 structure SetCacheKey where
   docPtr : USize
@@ -180,10 +201,14 @@ structure FailureCacheKey where
   deriving BEq, Hashable
 
 structure ResolutionState (τ : Type) where
-  setCache : HashMap SetCacheKey (MeasureSet τ)
-  failureCache : HashSet FailureCacheKey
+  setCache : HashMap SetCacheKey (MeasureSet τ) := {}
+  resolvedTaintedCache : HashMap USize (Option (Measure τ)) := {}
+  failureCache : HashSet FailureCacheKey := {}
 
 abbrev ResolverM (τ : Type) := StateM (ResolutionState τ)
+
+def ResolverM.run (f : ResolverM τ α) : α :=
+  StateT.run' f {}
 
 def getCachedSet? (doc : CoreDoc) (columnPos indentation : Nat) (fullness : FullnessState) :
     ResolverM τ (Option (MeasureSet τ)) := do
@@ -194,7 +219,7 @@ def getCachedSet? (doc : CoreDoc) (columnPos indentation : Nat) (fullness : Full
     fullness
   }
 
-def cacheSet (doc : CoreDoc) (columnPos indentation : Nat) (fullness : FullnessState)
+def setCachedSet (doc : CoreDoc) (columnPos indentation : Nat) (fullness : FullnessState)
     (set : MeasureSet τ) : ResolverM τ Unit :=
   modify fun state => { state with
     setCache := state.setCache.insert {
@@ -205,6 +230,22 @@ def cacheSet (doc : CoreDoc) (columnPos indentation : Nat) (fullness : FullnessS
       } set
   }
 
+inductive CacheResult (α : Type)
+  | miss
+  | hit (cached : α)
+
+def getCachedResolvedTainted? (tm : TaintedMeasure τ) :
+    ResolverM τ (CacheResult (Option (Measure τ))) := do
+  match (← get).resolvedTaintedCache.get? tm.ptr with
+  | none => return .miss
+  | some cached? => return .hit cached?
+
+def setCachedResolvedTainted (tm : TaintedMeasure τ) (m? : Option (Measure τ)) :
+    ResolverM τ Unit :=
+  modify fun state => { state with
+    resolvedTaintedCache := state.resolvedTaintedCache.insert tm.ptr m?
+  }
+
 def isFailing (doc : CoreDoc) (fullness : FullnessState) : ResolverM τ Bool := do
   let isCachedFailure := (← get).failureCache.contains {
     docPtr := doc.ptr
@@ -212,11 +253,19 @@ def isFailing (doc : CoreDoc) (fullness : FullnessState) : ResolverM τ Bool := 
   }
   return isCachedFailure || doc.isFailure fullness
 
+def setCachedFailing (doc : CoreDoc) (fullness : FullnessState) : ResolverM τ Unit :=
+  modify fun state => { state with
+    failureCache := state.failureCache.insert {
+      docPtr := doc.ptr
+      fullness
+    }
+  }
+
 @[expose] def Resolver (τ : Type) :=
   (doc : CoreDoc) → (columnPos indentation : Nat) → (fullness : FullnessState) →
     ResolverM τ (MeasureSet τ)
 
-def memoize (f : Resolver τ) : Resolver τ := fun doc columnPos indentation fullness => do
+def Resolver.memoize (f : Resolver τ) : Resolver τ := fun doc columnPos indentation fullness => do
   if ← isFailing doc fullness then
     -- TODO: Set failing, unlike Racket impl?
     return .set #[]
@@ -225,12 +274,12 @@ def memoize (f : Resolver τ) : Resolver τ := fun doc columnPos indentation ful
   if let some cachedSet ← getCachedSet? doc columnPos indentation fullness then
     return cachedSet
   let r ← f doc columnPos indentation fullness
-  cacheSet doc columnPos indentation fullness r
+  setCachedSet doc columnPos indentation fullness r
   return r
 
 mutual
 
-partial def resolveCore : Resolver τ := fun doc columnPos indentation fullness => do
+partial def MeasureSet.resolveCore : Resolver τ := fun doc columnPos indentation fullness => do
   match doc with
   | .failure =>
     return .set #[]
@@ -283,24 +332,12 @@ where
         | .tainted tm2 =>
           return .tainted (.concatTainted m1 tm2 doc.maxNewlineCount?)
         | .set ms2 =>
-          let mut m1Result := #[]
-          let mut best? : Option (Measure τ) := none
-          for m2 in ms2 do
-            let current := Measure.concat m1 m2
-            let some best := best?
-              | best? := some current
-                continue
-            if current.cost <= best.cost then
-              best? := some current
-            else
-              best? := some current
-              m1Result := m1Result.push best
-          if let some best := best? then
-            m1Result := m1Result.push best
-          result := MeasureSet.merge (.set m1Result) result (prunable := true)
+          let m1Result : MeasureSet.Set τ := ms2.map m1.concat
+          let m1Result := m1Result.dedup
+          result := MeasureSet.merge result (.set m1Result) (prunable := true)
       return result
 
-partial def resolve : Resolver τ := memoize fun doc columnPos indentation fullness => do
+partial def MeasureSet.resolve : Resolver τ := Resolver.memoize fun doc columnPos indentation fullness => do
   let columnPos' :=
     if let .text s := doc then
       columnPos + s.length
@@ -311,3 +348,66 @@ partial def resolve : Resolver τ := memoize fun doc columnPos indentation fulln
   return ← resolveCore doc columnPos indentation fullness
 
 end
+
+@[expose] def TaintedResolver (τ : Type) :=
+    (tm : TaintedMeasure τ) → ResolverM τ (Option (Measure τ))
+
+def TaintedResolver.memoize (f : TaintedResolver τ) : TaintedResolver τ := fun tm => do
+  let cachedResolvedTainted? ← getCachedResolvedTainted? tm
+  if let .hit m := cachedResolvedTainted? then
+    return m
+  let m? ← f tm
+  return m?
+
+mutual
+
+partial def TaintedMeasure.resolve? : TaintedResolver τ := TaintedResolver.memoize fun tm => do
+  match tm with
+  | .mergeTainted tm1 tm2 _ =>
+    let some m1 ← tm1.resolve?
+      | let m2? ← tm2.resolve?
+        return m2?
+    return some m1
+  | .taintedConcat tm doc indentation fullness _ =>
+    let some m1 ← tm.resolve?
+      | return none
+    let ms2 ← MeasureSet.resolve doc m1.lastLineLength indentation fullness
+    let m2? ← ms2.extractAtMostOne?
+    return m2?
+  | .concatTainted m1 tm2 _ =>
+    let some m2 ← tm2.resolve?
+      | return none
+    return some <| m1.concat m2
+  | .resolveTainted doc columnPos indentation fullness _ =>
+    -- TODO: Why resolveCore instead of resolve?
+    let ms ← MeasureSet.resolveCore doc columnPos indentation fullness
+    let m? ← ms.extractAtMostOne?
+    if m?.isNone then
+      setCachedFailing doc fullness
+    return m?
+
+partial def MeasureSet.extractAtMostOne? (ms : MeasureSet τ) : ResolverM τ (Option (Measure τ)) := do
+  match ms with
+  | .tainted tm =>
+    tm.resolve?
+  | .set ms =>
+    return ms[0]?
+
+end
+
+def resolve? (doc : CoreDoc) (offset : Nat) : Option (Measure τ) := ResolverM.run do
+  let ms1 ← MeasureSet.resolve doc offset 0 {
+    isFullBefore := false
+    isFullAfter := false
+  }
+  let ms2 ← MeasureSet.resolve doc offset 0 {
+    isFullBefore := false
+    isFullAfter := true
+  }
+  let ms := ms1.merge ms2 (prunable := false)
+  ms.extractAtMostOne?
+
+def format? (τ : Type) [Add τ] [LE τ] [DecidableLE τ] [Cost τ]
+    (doc : CoreDoc) (offset : Nat) : Option String := do
+  let m ← resolve? (τ := τ) doc offset
+  return m.print

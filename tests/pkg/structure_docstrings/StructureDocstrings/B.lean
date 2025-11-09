c3486f253ffc92ac2336f9358c5e93b769f0e30f
Kim Morrison <kim@tqft.net>
2025-11-09 23:42:39 +0100

    fix

--- tests/pkg/structure_docstrings/StructureDocstrings/B.lean
+++ tests/pkg/structure_docstrings/StructureDocstrings/B.lean
@@ -1,17 +1,7 @@
 module
 
-import Lean.DocString
-import Lean.Meta.Basic
 public import StructureDocstrings.A
 
 public section
 
 class GroupWithZero (G : Type) extends Monoid G, DivInvMonoid G where
-
-/-- info: The power operation: `a ^ n = a * ··· * a`; `a ^ (-n) = a⁻¹ * ··· a⁻¹` (`n` times) -/
-#guard_msgs in
-open Lean in
-run_meta do
-  let env ← getEnv
-  let some r ← Lean.findDocString? env `GroupWithZero.zpow | failure
-  logInfo r

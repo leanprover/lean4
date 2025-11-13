73601936f5be9166fc6855ba5f11e25353878194
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2025-11-13 10:38:46 +0100

    fix merge conflict

--- tests/lean/run/grind_11130.lean
+++ tests/lean/run/grind_11130.lean
@@ -10,34 +10,7 @@ theorem monotone_iff_forall_lt : Monotone f ↔ ∀ ⦃a b⦄, a < b → f a ≤
 
 end Mathlib.Order.Monotone.Defs
 
-/--
-error: `grind` failed
-case grind.1.1.1.1.1.1.1.1.1
-h : ¬Monotone fun n => n ^ n
-w : Nat
-h_2 : ¬∀ ⦃b : Nat⦄, w + 1 ≤ b → w ^ w ≤ b ^ b
-w_1 : Nat
-h_4 : ¬(w + 1 ≤ w_1 → w ^ w ≤ w_1 ^ w_1)
-h_5 :
-  ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1 =
-    (((w + 1) ^ (w + 1)) ^ (w + 1) ^ (w + 1)) ^ ((w + 1) ^ (w + 1)) ^ (w + 1) ^ (w + 1)
-h_6 :
-  ((w ^ w) ^ w ^ w) ^ (w ^ w) ^ w ^ w =
-    (((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1) ^ ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1
-h_7 : (w ^ w) ^ w ^ w = ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1
-h_8 : (w ^ w) ^ w ^ w = ((w_1 + 1) ^ (w_1 + 1)) ^ (w_1 + 1) ^ (w_1 + 1)
-h_9 : w ^ w = ((w + 1) ^ (w + 1)) ^ (w + 1) ^ (w + 1)
-h_10 : (w_1 ^ w_1 + 1) ^ w_1 ^ w_1 * (w_1 ^ w_1 + 1) = ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1
-h_11 :
-  (w ^ w + 1) ^ w ^ w * (w ^ w + 1) =
-    (((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1) ^ ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1
-h_12 : (w_1 ^ w_1) ^ w_1 ^ w_1 + 1 = (((w + 1) ^ (w + 1)) ^ (w + 1) ^ (w + 1)) ^ ((w + 1) ^ (w + 1)) ^ (w + 1) ^ (w + 1)
-h_13 :
-  (w ^ w) ^ w ^ w + 1 =
-    (((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1) ^ ((w_1 ^ w_1) ^ w_1 ^ w_1) ^ (w_1 ^ w_1) ^ w_1 ^ w_1
-⊢ False
--/
-#guard_msgs in
+#guard_msgs (drop error) in
 theorem pow_self_mono : Monotone fun n : Nat ↦ n ^ n := by
   grind -verbose [
     monotone_iff_forall_lt,

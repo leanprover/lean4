7aa501b396402f9a68b78f1528ea815f00c8e73a
Wojciech Różowski <wojciech@lean-fro.org>
2025-11-14 12:34:52 +0000

    Update src/Std/Data/DHashMap/Lemmas.lean
    Co-authored-by: Markus Himmel <markus@himmel-villmar.de>

--- src/Std/Data/DHashMap/Lemmas.lean
+++ src/Std/Data/DHashMap/Lemmas.lean
@@ -2237,12 +2237,8 @@ theorem isEmpty_inter_right [EquivBEq α] [LawfulHashable α] (h : m₂.isEmpty)
 
 theorem isEmpty_inter_iff [EquivBEq α] [LawfulHashable α] :
     (m₁ ∩ m₂).isEmpty ↔ ∀ k, k ∈ m₁ → k ∉ m₂ := by
-  conv =>
-    rhs
-    ext x
-    rhs
-    rw [← contains_eq_false_iff_not_mem]
-  exact @Raw₀.isEmpty_inter_iff _ _ _ _ ⟨m₁.1, m₁.2.size_buckets_pos⟩ ⟨m₂.1, m₂.2.size_buckets_pos⟩ _ _ m₁.wf m₂.wf
+  simpa only [mem_iff_contains, Bool.not_eq_true] using
+    @Raw₀.isEmpty_inter_iff _ _ _ _ ⟨m₁.1, m₁.2.size_buckets_pos⟩ ⟨m₂.1, m₂.2.size_buckets_pos⟩ _ _ m₁.wf m₂.wf
 
 end Inter
 

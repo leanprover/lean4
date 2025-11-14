6d34b268f78412af725f45f1e987bf6ed724cf87
Rob Simmons <rob@lean-fro.org>
2025-11-14 10:35:01 -0500

    Spelling

--- src/Lean/Elab/SyntheticMVars.lean
+++ src/Lean/Elab/SyntheticMVars.lean
@@ -244,7 +244,7 @@ def explainStuckTypeclassProblem (typeclassProblem : Expr) : TermElabM (Option M
       args := arg :: args
     | _ => return .none -- Precluded by loop guard
 
-  -- Find the typeclass construstor and look up it's classifying sort
+  -- Find the typeclass constructor and look up its classifying sort
   let .const name _ := ty
     | return .none -- Typeclass problem has unexpected structure; fall back to default error
   let .some defn := (← getEnv).findConstVal? name

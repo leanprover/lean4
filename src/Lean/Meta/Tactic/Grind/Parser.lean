c112bc848651dc52510ff9a2eddc4ef55e42bcd7
Leonardo de Moura <leomoura@amazon.com>
2025-11-14 15:58:48 -0800

    feat: add `grind_pattern` constraint syntax
    This PR adds syntax for specifying constraints in the `grind_pattern`
    command.

--- src/Lean/Meta/Tactic/Grind/Parser.lean
+++ src/Lean/Meta/Tactic/Grind/Parser.lean
@@ -8,11 +8,16 @@ prelude
 public import Lean.Parser.Command
 public section
 namespace Lean.Parser.Command
+
+def grindPatternCnstr : Parser := leading_parser withPosition (ident >> " =/= " >> checkColGe "right-hand-side to start in a column greater than or equal to the corresponding to the constraint" >> termParser)
+
+def grindPatternCnstrs : Parser := leading_parser "where " >> many1Indent (ppLine >> grindPatternCnstr >> optional ";")
+
 /-!
 Builtin parsers for `grind` related commands
 -/
 @[builtin_command_parser] def grindPattern := leading_parser
-  Term.attrKind >> "grind_pattern " >>  ident >> darrow >> sepBy1 termParser ","
+  Term.attrKind >> "grind_pattern " >>  ident >> darrow >> sepBy1 termParser "," >> optional grindPatternCnstrs
 
 @[builtin_command_parser] def initGrindNorm := leading_parser
   "init_grind_norm " >> many ident >> "| " >> many ident

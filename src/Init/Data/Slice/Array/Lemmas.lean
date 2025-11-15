ba8042557efdf590dc620ac400b2ab2c21048f5e
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-11-15 17:28:52 +0100

    wip

--- src/Init/Data/Slice/Array/Lemmas.lean
+++ src/Init/Data/Slice/Array/Lemmas.lean
@@ -13,6 +13,7 @@ public import Init.Data.Slice.Array.Iterator
 import all Init.Data.Slice.Array.Iterator
 import all Init.Data.Slice.Operations
 import all Init.Data.Range.Polymorphic.Iterators
+public import Init.Data.Range.Polymorphic.Lemmas
 import all Init.Data.Range.Polymorphic.Lemmas
 public import Init.Data.Slice.Lemmas
 public import Init.Data.Iterators.Lemmas
@@ -35,12 +36,11 @@ theorem internalIter_eq {α : Type u} {s : Subarray α} :
 theorem toList_internalIter {α : Type u} {s : Subarray α} :
     (Internal.iter s).toList =
       ((s.start...s.stop).toList
-        |>.attachWith (· < s.array.size)
-          (fun out h => h
-              |> Rco.mem_toList_iff_mem.mp
-              |> Rco.lt_upper_of_mem
-              |> (Nat.lt_of_lt_of_le · s.stop_le_array_size))
-        |>.map fun i => s.array[i.1]) := by
+        |>.attach
+        |>.map fun i => s.array[i.1]'(i.property
+            |> Rco.mem_toList_iff_mem.mp
+            |> Rco.lt_upper_of_mem
+            |> (Nat.lt_of_lt_of_le · s.stop_le_array_size))) := by
   rw [internalIter_eq, Iter.toList_map, Iter.toList_uLift, Iter.toList_attachWith]
   simp [Rco.toList]
 
@@ -68,5 +68,12 @@ public theorem forIn_toArray {α : Type u} {s : Subarray α}
     {f : α → γ → m (ForInStep γ)} :
     ForIn.forIn s.toArray init f = ForIn.forIn s init f :=
   Slice.forIn_toArray
+
+public theorem rccSlice_eq {xs : Subarray α} {lo hi : Nat} :
+    xs[lo...=hi] = xs.array[(xs.start + lo)...=(xs.start + min (hi + 1) xs.size)] := by
+  simp [Std.Rcc.Sliceable.mkSlice, Std.Rcc.HasRcoIntersection.intersection,
+    ]
+
+public theorem rccSlice_rccSlice_array {xs : Array α} :
 
 end Subarray

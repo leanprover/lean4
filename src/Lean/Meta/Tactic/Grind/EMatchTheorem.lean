b1ab14b35c2703c8c31d3616da2faa246c32238a
Leonardo de Moura <leomoura@amazon.com>
2025-11-14 18:25:23 -0800

    feat: elaborate `grind_pattern` constraints

--- src/Lean/Meta/Tactic/Grind/EMatchTheorem.lean
+++ src/Lean/Meta/Tactic/Grind/EMatchTheorem.lean
@@ -350,8 +350,15 @@ The `lhs` is one of the bound variables, and the `rhs` an abstract term that mus
 equal to a term `t` assigned to `lhs`.
 -/
 structure EMatchTheoremConstraint where
-  bvarIdx : Nat
-  rhs     : AbstractMVarsResult
+  /-- `lhs` -/
+  bvarIdx    : Nat
+  /-- Abstracted universe level param names in the `rhs` -/
+  levelNames : Array Name
+  /-- Number of abstracted metavariable in the `rhs` -/
+  numMVars   : Nat
+  /-- The actual `rhs`. -/
+  rhs        : Expr
+  deriving Inhabited, Repr
 
 /-- A theorem for heuristic instantiation based on E-matching. -/
 structure EMatchTheorem where
@@ -786,7 +793,7 @@ Creates an E-matching theorem for a theorem with proof `proof`, `numParams` para
 Pattern variables are represented using de Bruijn indices.
 -/
 def mkEMatchTheoremCore (origin : Origin) (levelParams : Array Name) (numParams : Nat) (proof : Expr)
-    (patterns : List Expr) (kind : EMatchTheoremKind) (showInfo := false) (minIndexable : Bool := false) : MetaM EMatchTheorem := do
+    (patterns : List Expr) (kind : EMatchTheoremKind) (cnstrs : List EMatchTheoremConstraint := []) (showInfo := false) (minIndexable : Bool := false) : MetaM EMatchTheorem := do
   -- the patterns have already been selected, there is no point in using priorities here
   let (patterns, symbols, bvarFound) ← NormalizePattern.main patterns (← getGlobalSymbolPriorities) (minPrio := 1)
   if symbols.isEmpty then
@@ -798,7 +805,7 @@ def mkEMatchTheoremCore (origin : Origin) (levelParams : Array Name) (numParams 
   logPatternWhen showInfo origin patterns
   return {
     proof, patterns, numParams, symbols
-    levelParams, origin, kind, minIndexable
+    levelParams, origin, kind, minIndexable, cnstrs
   }
 
 private def getProofFor (declName : Name) : MetaM Expr := do
@@ -815,8 +822,8 @@ Creates an E-matching theorem for `declName` with `numParams` parameters, and th
 Pattern variables are represented using de Bruijn indices.
 -/
 def mkEMatchTheorem (declName : Name) (numParams : Nat) (patterns : List Expr)
-    (kind : EMatchTheoremKind) (minIndexable : Bool) : MetaM EMatchTheorem := do
-  mkEMatchTheoremCore (.decl declName) #[] numParams (← getProofFor declName) patterns kind (minIndexable := minIndexable)
+    (kind : EMatchTheoremKind) (minIndexable : Bool) (cnstrs : List EMatchTheoremConstraint) : MetaM EMatchTheorem := do
+  mkEMatchTheoremCore (.decl declName) #[] numParams (← getProofFor declName) patterns kind cnstrs (minIndexable := minIndexable)
 
 /--
 Given a theorem with proof `proof` and type of the form `∀ (a_1 ... a_n), lhs = rhs`,
@@ -864,8 +871,8 @@ Adds an E-matching theorem to the environment.
 See `mkEMatchTheorem`.
 -/
 def addEMatchTheorem (declName : Name) (numParams : Nat) (patterns : List Expr) (kind : EMatchTheoremKind)
-    (minIndexable : Bool) (attrKind := AttributeKind.global) : MetaM Unit := do
-  ematchTheoremsExt.add (← mkEMatchTheorem declName numParams patterns kind (minIndexable := minIndexable)) attrKind
+    (minIndexable : Bool) (attrKind := AttributeKind.global) (cnstrs : List EMatchTheoremConstraint) : MetaM Unit := do
+  ematchTheoremsExt.add (← mkEMatchTheorem declName numParams patterns kind cnstrs (minIndexable := minIndexable)) attrKind
 
 /--
 Adds an E-matching equality theorem to the environment.

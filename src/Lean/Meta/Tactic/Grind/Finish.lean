a8dcd82fe7d5acf2633aff70ca0229d25848c994
Leonardo de Moura <leomoura@amazon.com>
2025-11-16 15:23:01 -0800

    feat: improve `finish`

--- src/Lean/Meta/Tactic/Grind/Finish.lean
+++ src/Lean/Meta/Tactic/Grind/Finish.lean
@@ -8,13 +8,14 @@ prelude
 public import Lean.Meta.Tactic.Grind.Action
 import Lean.Meta.Tactic.Grind.EMatchAction
 import Lean.Meta.Tactic.Grind.Split
+import Lean.Meta.Tactic.Grind.Intro
 namespace Lean.Meta.Grind.Action
 
 public abbrev maxIterationsDefault := 10000 -- **TODO**: Add option
 
 public def mkFinish (maxIterations : Nat := maxIterationsDefault) : IO Action := do
   let solvers ← Solvers.mkAction
-  let step : Action := Action.done <|> solvers <|> Action.instantiate <|> Action.splitNext <|> Action.mbtc
-  return Action.checkTactic (warnOnly := true) >> step.loop maxIterations
+  let step : Action := solvers <|> instantiate <|> splitNext <|> mbtc
+  return checkTactic (warnOnly := true) >> intros 0 >> assertAll >> step.loop maxIterations
 
 end Lean.Meta.Grind.Action

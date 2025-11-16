ed34ee0cd5afd955349c7766c65496bc47a2e05a
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-16 21:11:56 +0100

    chore: make `declMetaExt` persistent for `shake` (#11201)

--- src/Lean/Compiler/MetaAttr.lean
+++ src/Lean/Compiler/MetaAttr.lean
@@ -29,22 +29,64 @@ private builtin_initialize notMetaExt : EnvExtension NameSet ←
     (replay? := some fun _ _ newEntries s => newEntries.foldl (·.insert) s)
 
 /-- Marks in the environment extension that the given declaration has been declared by the user as `meta`. -/
-def addMeta (env : Environment) (declName : Name) : Environment :=
+def markMeta (env : Environment) (declName : Name) : Environment :=
   metaExt.tag env declName
 
 /--
 Marks the given declaration as not being annotated with `meta` even if it could have been by the
 user.
 -/
-def addNotMeta (env : Environment) (declName : Name) : Environment :=
+def markNotMeta (env : Environment) (declName : Name) : Environment :=
   if declName.isAnonymous then  -- avoid panic from `modifyState` on partial input
     env
   else
     notMetaExt.modifyState (asyncDecl := declName) env (·.insert declName)
 
 /-- Returns true iff the user has declared the given declaration as `meta`. -/
-def isMeta (env : Environment) (declName : Name) : Bool :=
+def isMarkedMeta (env : Environment) (declName : Name) : Bool :=
   metaExt.isTagged env declName
+
+/--
+Set of IR decls that should be made available to any importer. This is a superset of `metaExt`,
+which is managed by the elaborator and has a different async mode. More precisely, it contains the
+closure of `metaExt` as well as further derived decls such as `_boxed` versions. We store this set
+primarily to filter exports in `declMapExt`; we persist it in `.olean.private` for the benefit of
+`shake`.
+-/
+private builtin_initialize declMetaExt : SimplePersistentEnvExtension Name NameSet ←
+  registerSimplePersistentEnvExtension {
+    addImportedFn := fun _ => {}
+    addEntryFn := fun s n => s.insert n
+    asyncMode := .sync
+    replay? := some <| SimplePersistentEnvExtension.replayOfFilter (!·.contains ·) (·.insert ·)
+    exportEntriesFnEx? := some fun env s entries => fun
+      | .private =>
+        let decls := entries.foldl (init := #[]) fun decls decl => decls.push decl
+        decls.qsort Name.quickLt
+      | _ => #[]
+  }
+
+/-- Whether a declaration should be exported for interpretation. -/
+def isDeclMeta (env : Environment) (declName : Name) : Bool :=
+  if !env.header.isModule then
+    true
+  else
+    -- The interpreter may call the boxed variant even if the IR does not directly reference it, so
+    -- use same visibility as base decl.
+    -- Note that boxed decls are created after the `inferVisibility` pass.
+    let inferFor := match declName with
+      | .str n "_boxed" => n
+      | n               => n
+    match env.getModuleIdxFor? declName with
+    | some idx => declMetaExt.getModuleEntries env idx |>.binSearchContains inferFor Name.quickLt
+    | none => declMetaExt.getState env |>.contains inferFor
+
+/-- Marks a declaration to be exported for interpretation. -/
+def setDeclMeta (env : Environment) (declName : Name) : Environment :=
+  if isDeclMeta env declName then
+    env
+  else
+    declMetaExt.addEntry env declName
 
 /--
 Returns the IR phases of the given declaration that should be considered accessible. Does not take
@@ -55,12 +97,12 @@ def getIRPhases (env : Environment) (declName : Name) : IRPhases := Id.run do
     return .all
   match env.getModuleIdxFor? declName with
   | some idx =>
-    if isMeta env declName then
+    if isMarkedMeta env declName then
       .comptime
     else
       env.header.modules[idx]?.map (·.irPhases) |>.get!
   | none =>
-    if isMeta env declName then
+    if isMarkedMeta env declName then
       .comptime
     else if notMetaExt.getState env |>.contains declName then
       .runtime

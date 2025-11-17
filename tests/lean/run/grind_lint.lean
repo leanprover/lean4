8b575dcbf2a3b907044df2233a61b61003eeeb45
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-11-17 16:30:01 +1100

    chore: fixing grind annotations using #grind_lint (#11206)
    Slightly more extensive version of #11205, for which I want separate CI.

--- tests/lean/run/grind_lint.lean
+++ tests/lean/run/grind_lint.lean
@@ -5,17 +5,15 @@
 
 -- In progress:
 
-#grind_lint check (min := 20) in List
-
 -- TODO: Not sure what to do here, see https://lean-fro.zulipchat.com/#narrow/channel/503415-grind/topic/.60.23grind_lint.60.20command/near/556730710
 #grind_lint inspect List.getLast?_concat
-#grind_lint mute List.getLast?_concat
+#grind_lint skip List.getLast?_concat
 
 -- TODO: We should consider changing the grind annotation for `List.getElem?_eq_none`
 -- so it only fires if we've already proved the hypothesis holds. (i.e. the new gadget)
 -- Other than that, everything looks sane here:
 #grind_lint inspect List.getLast?_pmap
-#grind_lint mute List.getLast?_pmap
+#grind_lint skip List.getLast?_pmap
 
 -- TODO: We should try to remove these attributes; if that's okay we can remove these mutes.
 attribute [-grind] List.Sublist.getLast_mem List.Sublist.head_mem
@@ -29,19 +27,18 @@ attribute [-grind] List.Sublist.getLast_mem List.Sublist.head_mem
 -- `List.replicate_sublist_iff` is reasonable at 30.
 #guard_msgs in
 #grind_lint inspect (min := 30) List.replicate_sublist_iff
-#grind_lint mute List.replicate_sublist_iff
+#grind_lint skip List.replicate_sublist_iff
 
 -- `List.Sublist.append` is reasonable at 25.
 #guard_msgs in
 #grind_lint inspect (min := 25) List.Sublist.append
-#grind_lint mute List.Sublist.append
+#grind_lint skip List.Sublist.append
 
 -- `List.Sublist.middle` is reasonable at 25.
 #guard_msgs in
 #grind_lint inspect (min := 25) List.Sublist.middle
-#grind_lint mute List.Sublist.middle
+#grind_lint skip List.Sublist.middle
 
--- FIXME: I think I've muted all of these, but they are still being reported.
 #grind_lint check (min := 20) in List
 
 -- TODO:

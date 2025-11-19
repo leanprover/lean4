e51a7e8a0727b03aff47124138d182e3aa096308
Leonardo de Moura <leomoura@amazon.com>
2025-11-18 19:20:21 -0800

    test:
    https://leanprover.zulipchat.com/#narrow/channel/270676-lean4/topic/Grind.20aggressively.20includes.20local.20hypotheses.2E/near/554887715

---
+++ tests/lean/run/grind_unnecessary_hypothesis.lean
@@ -0,0 +1,51 @@
+/-
+This is an example posted by Wrenna Robson on Zulip.
+-/
+
+-- Reference
+def idxToCount [BEq α] (xs : List α) (i : Nat) (hi : i < xs.length) : Nat := go xs i hi 0 where
+  @[specialize] go : (xs : List α) → (i : Nat) → i < xs.length → Nat → Nat
+  | _ :: _, 0, _, acc => acc
+  | x :: xs, i + 1, hi, acc =>
+    haveI hi := Nat.lt_of_succ_lt_succ hi
+    bif x == xs[i] then go xs i hi (acc + 1) else go xs i hi acc
+
+-- `go` does not depend on the outer `xs` as expected.
+/--
+info: idxToCount.go.{u_1} {α : Type u_1} [BEq α] (xs : List α) (i : Nat) : i < xs.length → Nat → Nat
+-/
+#guard_msgs in
+#check idxToCount.go
+
+
+def idxToCount' [BEq α] (xs : List α) (i : Nat) (hi : i < xs.length) : Nat := go xs i hi 0 where
+  @[specialize] go : (xs : List α) → (i : Nat) → i < xs.length → Nat → Nat
+  | _ :: _, 0, _, acc => acc
+  | x :: xs, i + 1, hi, acc =>
+    bif x == xs[i]'(by grind) then go xs i (by grind) (acc + 1) else go xs i (by grind) acc
+
+/-
+`grind +revert` was the default behavior until v4.25.1
+Thus, `go` used to depend on the outer `xs`. This is not the case anymore.
+-/
+/--
+info: idxToCount'.go.{u_1} {α : Type u_1} [BEq α] (xs : List α) (i : Nat) : i < xs.length → Nat → Nat
+-/
+#guard_msgs in
+#check idxToCount'.go
+
+/-
+We can reproduce the behavior of v4.25.1 using `grind +revert`.
+-/
+def idxToCount'' [BEq α] (xs : List α) (i : Nat) (hi : i < xs.length) : Nat := go xs i hi 0 where
+  @[specialize] go : (xs : List α) → (i : Nat) → i < xs.length → Nat → Nat
+  | _ :: _, 0, _, acc => acc
+  | x :: xs, i + 1, hi, acc =>
+    bif x == xs[i]'(by grind +revert) then go xs i (by grind +revert) (acc + 1) else go xs i (by grind +revert) acc
+
+/--
+info: idxToCount''.go.{u_1} {α : Type u_1} [BEq α] (xs : List α) (i : Nat) (hi : i < xs.length) (xs✝ : List α) (i✝ : Nat) :
+  i✝ < xs✝.length → Nat → Nat
+-/
+#guard_msgs in
+#check idxToCount''.go

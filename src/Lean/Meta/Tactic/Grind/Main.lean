db4206f2a93d7fc027708c4dcb9af6525c611604
Leonardo de Moura <leomoura@amazon.com>
2025-11-21 20:28:53 -0800

    fix: instantiate metavariables in hypotheses in `grind` (#11315)
    This PR fixes an issue affecting `grind -revert`. In this mode, assigned
    metavariables in hypotheses were not being instantiated. This issue was
    affecting two files in Mathlib.

--- src/Lean/Meta/Tactic/Grind/Main.lean
+++ src/Lean/Meta/Tactic/Grind/Main.lean
@@ -270,6 +270,11 @@ See issue #11806 for a motivating example.
 def withProtectedMCtx [Monad m] [MonadControlT MetaM m] [MonadLiftT MetaM m]
     [MonadExcept Exception m] [MonadRuntimeException m]
     (abstractProof : Bool) (mvarId : MVarId) (k : MVarId → m α) : m α := do
+  /-
+  **Note**: `instantiateGoalMVars` here also instantiates mvars occurring in hypotheses.
+  This is particularly important when using `grind -revert`.
+  -/
+  let mvarId ← mvarId.instantiateGoalMVars
   let mvarId ← mvarId.abstractMVars
   let mvarId ← mvarId.clearImplDetails
   tryCatchRuntimeEx (main mvarId) fun ex => do

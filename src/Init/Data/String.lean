85d7f3321c751b300c85dbcf164c2ef32e28cea9
Markus Himmel <markus@lean-fro.org>
2025-11-25 16:48:41 +0100

    feat: `String.Slice.toInt?` (#11358)
    This PR adds `String.Slice.toInt?` and variants.
    
    Closes #11275.

--- src/Init/Data/String.lean
+++ src/Init/Data/String.lean
@@ -13,7 +13,6 @@ public import Init.Data.String.Defs
 public import Init.Data.String.Extra
 public import Init.Data.String.Iterator
 public import Init.Data.String.Lemmas
-public import Init.Data.String.Repr
 public import Init.Data.String.Bootstrap
 public import Init.Data.String.Slice
 public import Init.Data.String.Pattern

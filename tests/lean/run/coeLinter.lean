2949f5e8917114bcbc2287bc604d95496a25c096
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-12-02 18:08:15 +0100

    fixes and cleanups

--- tests/lean/run/coeLinter.lean
+++ tests/lean/run/coeLinter.lean
@@ -1,23 +1,26 @@
-import Lean
-import Lean.Linter.Coe
-open Lean
-
-#check Lean.Linter.Coe.shouldWarnOnDeprecatedCoercions
-
-#check coercionsBannedInCore
-
-set_option linter.deprecatedCoercions true
+module
 
 structure X
 structure Y
 
-def convert (_ : X) : Y := ⟨⟩
-
-@[deprecated convert (since := "")]
+@[deprecated "" (since := "")]
 instance mycoe : Coe X Y where coe _ := ⟨⟩
 
+/-- warning: This term uses the coercion `optionCoe`, which is banned in Lean's core library. -/
+#guard_msgs in
 def f : Option String := "hi"
 
+/--
+warning: This term uses the coercion `instCoeSubarrayArray`, which is banned in Lean's core library.
+-/
+#guard_msgs in
 def g : Array Nat := #[1, 2, 3][*...*]
 
+/-- warning: This term uses the deprecated coercion `mycoe`. -/
+#guard_msgs in
 def h (foo : X) : Y := foo
+
+set_option linter.deprecatedCoercions false
+
+#guard_msgs in
+def h' (foo : X) : Y := foo

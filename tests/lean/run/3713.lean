2882fdfa0ec4577ef20f561ffdd6a5a93a0e582e
Sebastian Graf <sgraf1337@gmail.com>
2025-12-03 17:45:35 +0000

    updated test outputs

--- tests/lean/run/3713.lean
+++ tests/lean/run/3713.lean
@@ -5,7 +5,7 @@ def somethingBad : MetaM Nat := do
   IO.println "oh no"
   return 1
 
-/-- error: invalid use of `(<- ...)`, must be nested inside a 'do' expression -/
+/-- error: Nested action `← somethingBad` must be nested inside a `do` expression. -/
 #guard_msgs in
 #eval show MetaM Unit from do
   let t := if false then ← somethingBad else 9
@@ -13,7 +13,7 @@ def somethingBad : MetaM Nat := do
 def foo : MetaM Bool :=
   return false
 
-/-- error: invalid use of `(<- ...)`, must be nested inside a 'do' expression -/
+/-- error: Nested action `← somethingBad` must be nested inside a `do` expression. -/
 #guard_msgs in
 #eval show MetaM Unit from do
   let t := if (← foo) then ← somethingBad else 9

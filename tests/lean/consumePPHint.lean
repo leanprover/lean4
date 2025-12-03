ed7a6e9a700c6cc3c23ea68ccca8ff7074c868bf
thorimur <thomasmurrills@gmail.com>
2025-12-03 14:36:43 -0500

    chore: remove `let_fun`

--- tests/lean/consumePPHint.lean
+++ tests/lean/consumePPHint.lean
@@ -7,7 +7,7 @@ opaque q : Nat → Prop
 
 theorem p_of_q : q x → p x := sorry
 
-theorem pletfun : p (have x := 0; x + 1) := by
+theorem phave : p (have x := 0; x + 1) := by
   -- ⊢ p (have x := 0; x + 1)
   apply p_of_q
   trace_state -- `have` should not be consumed.

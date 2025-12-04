7f0535e83c4ff652c17bd2562683a6f58530724f
Sebastian Graf <sgraf1337@gmail.com>
2025-12-04 17:25:43 +0000

    fix 9581

--- tests/lean/run/9581.lean
+++ tests/lean/run/9581.lean
@@ -13,7 +13,6 @@ theorem F_spec :
    ⦃⌜True⌝⦄
    F
    ⦃⇓ _ => ⌜1 < 2⌝⦄ := by
-  set_option trace.Elab.Tactic.Do.vcgen true in
   mvcgen [F]
   case inv1 => exact fun _ _ => ⌜1 < 2⌝
   -- it would be nice if we had a tactic wrapper around `case inv => exact ...` that does `mleave`
@@ -36,4 +35,3 @@ theorem F_spec_using_with :
   invariants
   | inv1 => fun _ _ => ⌜1 < 2⌝
   with omega
-  | vc2 spec _ => intro _; mvcgen [spec]

5fbf91eb82ce95faa956d55ed6a14a68aa2d862e
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-12-04 12:07:21 +0100

    fix tests

--- tests/lean/run/coeLinter.lean
+++ tests/lean/run/coeLinter.lean
@@ -1,13 +1,14 @@
 module
 
+-- by default, linters are disabled when running tests
+set_option linter.deprecatedCoercions true
+
 structure X
 structure Y
 
 @[deprecated "" (since := "")]
 instance mycoe : Coe X Y where coe _ := ⟨⟩
 
-/-- warning: This term uses the coercion `optionCoe`, which is banned in Lean's core library. -/
-#guard_msgs in
 def f : Option String := "hi"
 
 /--
@@ -16,8 +17,7 @@ warning: This term uses the coercion `instCoeSubarrayArray`, which is banned in 
 #guard_msgs in
 def g : Array Nat := #[1, 2, 3][*...*]
 
-/-- warning: This term uses the deprecated coercion `mycoe`. -/
-#guard_msgs in
+set_option trace.Elab.info true
 def h (foo : X) : Y := foo
 
 set_option linter.deprecatedCoercions false

b0a12cb49ffd23fd08c4206856623dda521135d6
Leonardo de Moura <leomoura@amazon.com>
2025-12-04 19:49:01 -0800

    feat: mark `Nat` power and divisibility theorems for `grind` (#11519)
    This PR marks `Nat` power and divisibility theorems for `grind`. We use
    the new `grind_pattern` constraints to control theorem instantiation.
    Examples:
    
    ```lean
    example {x m n : Nat} (h : x = 4 ^ (m + 1) * n) : x % 4 = 0 := by
      grind
    
    example (a m n o p : Nat) : a ∣ n → a ∣ m * n * o * p := by
      grind
    
    example {a b x m n : Nat}
        : n > 0 → x = b * 4^m * a → a = 9^n → m > 0 → x % 6 = 0 := by
      grind
    
    example {a n : Nat}
        : m > 4 → a = 2^(m^n) → a % 2 = 0 := by
      grind
    ```
    
    Closes #11515
    
    Remark: We are adding support for installing extra theorems to `lia`
    (aka `cutsat`). The example at #11515 can already be solved by `grind`
    with this PR, but we still need to add the new theorems to the set for
    `lia`.
    
    cc @kim-em

--- tests/lean/run/grind_match_cond_issue.lean
+++ tests/lean/run/grind_match_cond_issue.lean
@@ -1,13 +1,10 @@
 /--
 error: `grind` failed
-case grind.2.2.1.1
+case grind.1
 n m a : Nat
 ih : ∀ {a : Nat}, a ^ 2 = 4 ^ m * n → False
 h : a ^ 2 = 4 ^ (m + 1) * n
-h_1 : ¬4 * 4 ^ m = 4 ^ m
-h_2 : ¬4 * 4 ^ m = 4 ^ m
-h_3 : n = 4 ^ m
-h_4 : ↑a = 4
+h_1 : ↑a = 4
 ⊢ False
 -/
 #guard_msgs in

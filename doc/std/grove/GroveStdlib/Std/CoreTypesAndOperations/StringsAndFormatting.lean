fe18c08da42f616cad018bd8bf8bebf74eb28699
Markus Himmel <markus@lean-fro.org>
2025-12-08 17:49:37 +0100

    doc: grove: update and add `String` data (#11551)
    This PR bumps Grove to the latest revision and starts adding data about
    the `String` library.
    
    Just a small start, more to come.

--- doc/std/grove/GroveStdlib/Std/CoreTypesAndOperations/StringsAndFormatting.lean
+++ doc/std/grove/GroveStdlib/Std/CoreTypesAndOperations/StringsAndFormatting.lean
@@ -11,9 +11,28 @@ namespace GroveStdlib.Std.CoreTypesAndOperations
 
 namespace StringsAndFormatting
 
+def highLevelStringTypes : List Lean.Name :=
+  [`String, `String.Slice, `String.Pos, `String.Slice.Pos]
+
+def sliceProducing : AssociationTable (β := Alias Lean.Name) .declaration
+    [`String, `String.Slice,
+     Alias.mk `String.Pos "string-pos-forwards" "String.Pos (forwards)",
+     Alias.mk `String.Pos "string-pos-backwards" "String.Pos (backwards)",
+     Alias.mk `String.Pos "string-pos-noproof" "String.Pos (no proof)",
+     Alias.mk `String.Slice.Pos "string-slice-pos-forwards" "String.Slice.Pos (forwards)",
+     Alias.mk `String.Slice.Pos "string-slice-pos-backwards" "String.Slice.Pos (backwards)",
+     Alias.mk `String.Slice.Pos "string-slice-pos-noproof" "String.Slice.Pos (no proof)"] where
+  id := "slice-producing"
+  title := "String functions returning slices"
+  description := "Operations on strings and string slices that themselves return a new string slice."
+  dataSources n := DataSource.definitionsInNamespace n.inner
+
 end StringsAndFormatting
 
+open StringsAndFormatting
+
 def stringsAndFormatting : Node :=
-  .section "strings-and-formatting" "Strings and formatting" #[]
+  .section "strings-and-formatting" "Strings and formatting"
+    #[.associationTable sliceProducing]
 
 end GroveStdlib.Std.CoreTypesAndOperations

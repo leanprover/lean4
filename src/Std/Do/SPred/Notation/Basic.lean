f0f7ec2681d7cf988976426cc2a1d0753795c3dd
David Thrane Christiansen <david@lean-fro.org>
2025-12-08 14:17:12 +0100

    doc: docstring review for Std.Do
    This PR reviews the docstrings for Std.Do that will appear in the Lean
    reference manual and adds those that were missing.

--- src/Std/Do/SPred/Notation/Basic.lean
+++ src/Std/Do/SPred/Notation/Basic.lean
@@ -11,6 +11,8 @@ import Init.NotationExtra
 
 public section
 
+set_option linter.missingDocs true
+
 namespace Std.Do
 
 open Lean Macro Parser PrettyPrinter
@@ -18,7 +20,17 @@ open Lean Macro Parser PrettyPrinter
 -- define `spred` embedding in `term`.
 -- An explicit `spred` marker avoids exponential blowup in terms
 -- that do not opt into the extended syntax.
+/--
+An embedding of the special syntax for `SPred` into ordinary terms that provides alternative
+interpretations of logical connectives and quantifiers.
+
+Within `spred(...)`, `term(...)` escapes to the ordinary Lean interpretation of this syntax.
+-/
 scoped syntax:max "spred(" term ")" : term
+/--
+Escapes from a surrounding `spred(...)` term, returning to the usual interpretations of quantifiers
+and connectives.
+-/
 scoped syntax:max "term(" term ")" : term
 
 -- allow fallback to `term`
@@ -26,14 +38,14 @@ macro_rules
   | `(spred(term($t))) => pure t
   | `(spred($t))       => pure t
 
--- push `spred` inside some `term` constructs
+-- pushes `spred` inside some `term` constructs
 macro_rules
   | `(spred(($P)))                  => ``((spred($P)))
   | `(spred(fun $xs* => $b))        => ``(fun $xs* => spred($b))
   | `(spred(if $c then $t else $e)) => ``(if $c then spred($t) else spred($e))
   | `(spred(($P : $t)))             => ``((spred($P) : $t))
 
-/-- Remove an `spred` layer from a `term` syntax object. -/
+/-- Removes an `spred` layer from a `term` syntax object. -/
 -- inverts the rules above.
 partial def SPred.Notation.unpack [Monad m] [MonadRef m] [MonadQuotation m] : Term → m Term
   | `(spred($P))             => do `($P)

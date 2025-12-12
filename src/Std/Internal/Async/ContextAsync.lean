1800a079b6de42a3278a450e2a93e044ad46598a
Sofia Rodrigues <sofia@algebraic.dev>
2025-12-12 13:21:22 -0300

    fix: function names

--- src/Std/Internal/Async/ContextAsync.lean
+++ src/Std/Internal/Async/ContextAsync.lean
@@ -167,6 +167,7 @@ def concurrentlyAll (xs : Array (ContextAsync α))
 
   let result ← tasks.mapM await
   return result
+
 /--
 Launches a `ContextAsync` computation in the background, discarding its result.
 

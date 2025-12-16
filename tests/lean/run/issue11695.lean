8a5a64284afb04c82b82add28766fe7239d3c3e4
Joachim Breitner <mail@joachim-breitner.de>
2025-12-16 16:17:35 +0000

    Failed to break my code

--- tests/lean/run/issue11695.lean
+++ tests/lean/run/issue11695.lean
@@ -9,3 +9,16 @@ def dependent : (n : Nat) → (m : Fin n) → Nat
  | .succ 0, 0 => 0
  | .succ (.succ n), 0 => 1
  | .succ (.succ n), ⟨.succ m, h⟩ => 2
+
+
+inductive Vec (α : Type) : Nat → Type where
+  | nil  : Vec α 0
+  | cons : α → Vec α n → Vec α (n + 1)
+
+def f1 : {n : Nat} → (v : Vec α n) → Nat
+  | 0, _          => 0
+  | _, .cons x xs => 1
+
+def f2 : {n : Nat} → (v : Vec α n) → Nat
+  | _, Vec.nil   => 0
+  | .succ n, _   => 1

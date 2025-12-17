ac70b2c4c951be439549a07c4e29cb4ea98d1cc0
Rob Simmons <rob@lean-fro.org>
2025-12-17 09:10:50 -0500

    feat: focused error messages for named examples

--- tests/lean/multiConstantError.lean
+++ tests/lean/multiConstantError.lean
@@ -1,3 +1,22 @@
+opaque a b : Nat
+
 opaque a b c : Nat
 
+opaque a b c d : Nat
+
+opaque a b c d e: Nat
+
+opaque a b (c : _) : Nat
+
 opaque a α β : β → Bool
+set_option pp.rawOnError true
+
+example x : True := sorry
+
+example x y : True := sorry
+
+example x y z : True := sorry
+
+example α β γ : β → True := sorry
+
+example x y (c : True) : True := sorry

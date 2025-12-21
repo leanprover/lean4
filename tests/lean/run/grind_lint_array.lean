f9bf680a64e21c18336385d4114800c0cf66937d
Kim Morrison <kim@tqft.net>
2025-12-22 07:52:06 +1100

    update exceptions

--- tests/lean/run/grind_lint_array.lean
+++ tests/lean/run/grind_lint_array.lean
@@ -4,9 +4,12 @@ import Lean.Elab.Tactic.Grind.LintExceptions
 /-! Check Array namespace: -/
 
 -- These go slightly over 20, but seem reasonable.
-#grind_lint skip Array.count_singleton
-#grind_lint skip Array.foldl_empty
-#grind_lint skip Array.foldr_empty
+#guard_msgs in
+#grind_lint inspect (min := 22) Array.count_singleton
+#guard_msgs in
+#grind_lint inspect (min := 22) Array.foldl_empty
+#guard_msgs in
+#grind_lint inspect (min := 22) Array.foldr_empty
 
 #guard_msgs in
 #grind_lint check (min := 20) in Array

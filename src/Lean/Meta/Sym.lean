73e45cd27157bada52746546d75f53f7abd9ff56
Leonardo de Moura <leomoura@amazon.com>
2025-12-25 11:52:04 -0800

    feat: add `replaceS`, `lowerLooseBVarsS`, and `liftLooseBVarsS`

--- src/Lean/Meta/Sym.lean
+++ src/Lean/Meta/Sym.lean
@@ -9,6 +9,8 @@ public import Lean.Meta.Sym.SymM
 public import Lean.Meta.Sym.Main
 public import Lean.Meta.Sym.Util
 public import Lean.Meta.Sym.MaxFVar
+public import Lean.Meta.Sym.ReplaceS
+public import Lean.Meta.Sym.LooseBVarsS
 
 /-!
 # Symbolic simulation support.
---
+++ src/Lean/Meta/Sym/LooseBVarsS.lean
@@ -0,0 +1,48 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+module
+prelude
+public import Lean.Meta.Sym.SymM
+import Lean.Meta.Sym.ReplaceS
+namespace Lean.Meta.Sym
+
+/--
+Lowers the loose bound variables `>= s` in `e` by `d`.
+That is, a loose bound variable `bvar i` with `i >= s` is mapped to `bvar (i-d)`.
+It assumes the input is maximally shared, and ensures the output is too.
+Remark: if `s < d`, then the result is `e`.
+-/
+def lowerLooseBVarsS (e : Expr) (s d : Nat) : SymM Expr := do
+  replaceS e fun e offset => do
+    let s₁ := s + offset
+    if s₁ >= e.looseBVarRange then
+      return some e -- `e` does not contain bound variables with idx >= s₁
+    else match e with
+    | .bvar idx =>
+      if idx >= s₁ then
+        return some (← Grind.mkBVarS (idx - d))
+      else
+        return none
+    | _ => return none
+
+/--
+Lifts loose bound variables `>= s` in `e` by `d`.
+It assumes the input is maximally shared, and ensures the output is too.
+-/
+def liftLooseBVarsS (e : Expr) (s d : Nat) : SymM Expr := do
+  replaceS e fun e offset => do
+    let s₁ := s + offset
+    if s₁ >= e.looseBVarRange then
+      return some e -- `e` does not contain bound variables with idx >= s₁
+    else match e with
+    | .bvar idx =>
+      if idx >= s₁ then
+        return some (← Grind.mkBVarS (idx + d))
+      else
+        return none
+    | _ => return none
+
+end Lean.Meta.Sym

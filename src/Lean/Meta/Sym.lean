3b87f84e9355477d5625ad9b4ffde97d849cac16
Leonardo de Moura <leomoura@amazon.com>
2025-12-28 09:50:38 -0800

    feat: add `instantiateMVarsS`

--- src/Lean/Meta/Sym.lean
+++ src/Lean/Meta/Sym.lean
@@ -14,6 +14,7 @@ public import Lean.Meta.Sym.LooseBVarsS
 public import Lean.Meta.Sym.InstantiateS
 public import Lean.Meta.Sym.IsClass
 public import Lean.Meta.Sym.Intro
+public import Lean.Meta.Sym.InstantiateMVarsS
 public import Lean.Meta.Sym.Pattern
 
 /-!

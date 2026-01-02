b6f0ad65ca1f7fbff10d0d72760f41e802658cfc
Leonardo de Moura <leomoura@amazon.com>
2026-01-02 10:38:50 -0800

    refactor: move `DiscrTreeTypes`

--- src/Lean/Meta/DiscrTree.lean
+++ src/Lean/Meta/DiscrTree.lean
@@ -4,13 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura, Jannis Limperg, Kim Morrison
 -/
 module
-
 prelude
 public import Lean.Meta.WHNF
-public import Lean.Meta.DiscrTreeTypes
-
+public import Lean.Meta.DiscrTree.Types
 public section
-
 namespace Lean.Meta.DiscrTree
 /-!
   (Imperfect) discrimination trees.

c54088d97f93555aec6fce0a6673e8d228cd4ca8
Rob23oba <robin.arnez@web.de>
2026-01-03 00:57:43 +0100

    oopieS

--- src/Lean/Compiler/IR/Boxing.lean
+++ src/Lean/Compiler/IR/Boxing.lean
@@ -12,7 +12,7 @@ public import Lean.Compiler.IR.CompilerM
 public import Lean.Compiler.IR.ElimDeadVars
 public import Lean.Compiler.IR.ToIRType
 public import Lean.Data.AssocList
-import Lean.Compiler.IR.InitAttrS
+import Lean.Compiler.IR.InitAttr
 
 public section
 

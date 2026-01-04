609d99e8607f76ea0b62e325a70e4938abe86760
Leonardo de Moura <leomoura@amazon.com>
2026-01-04 10:51:18 -0800

    chore: include free variables (#11894)
    This PR includes free variable in a `simp` benchmark to stress the
    default `simp` matching procedure.

--- tests/bench/sym/meta_simp_1.lean
+++ tests/bench/sym/meta_simp_1.lean
@@ -28,7 +28,7 @@ def simp (e : Expr) : MetaM (Simp.Result × Float) := Sym.SymM.run' do
 def mkTransitivityChain (n : Nat) : MetaM Expr := do
   withLocalDeclD `x (mkConst ``Nat) fun x => do
     let zero := mkNatLit 0
-    let mut e := zero
+    let mut e := x
     for _ in [:n] do
       e := mkApp (mkConst ``f) (mkNatAdd zero e)
     mkForallFVars #[x] e

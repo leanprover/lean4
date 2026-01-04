80f73b8b79bc796d21fd0efdfb2fc3bf57d7624b
Rob23oba <robin.arnez@web.de>
2026-01-04 21:17:32 +0100

    restore tests

--- tests/compiler/link_lake.lean
+++ tests/compiler/link_lake.lean
@@ -1,7 +1,6 @@
 -- this should be sufficient to trigger linking against Lake's initializer symbols
 
--- temporarily disabled, this works after a stage0 update
---import Lake
+import Lake
 
 def main : IO Unit :=
   return

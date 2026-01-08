45755e629b8830aadca8b435898280a54709ed40
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-01-08 11:44:27 +0100

    add List authors to author list

--- src/Init/Data/Array/MinMax.lean
+++ src/Init/Data/Array/MinMax.lean
@@ -1,7 +1,8 @@
 /-
 Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Paul Reichert
+Authors: Parikshit Khanna, Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Mario Carneiro,
+  Paul Reichert
 -/
 module
 

4a6f53271bf27d371d535250b46729b418a12980
Alok Singh <alokbeniwal@gmail.com>
2026-01-12 00:33:50 -0800

    chore: backtick Decidable in rewrite message

--- src/Lean/Meta/Tactic/Rewrite.lean
+++ src/Lean/Meta/Tactic/Rewrite.lean
@@ -68,7 +68,7 @@ def _root_.Lean.MVarId.rewrite (mvarId : MVarId) (e : Expr) (heq : Expr)
               \n\n\
               Possible solutions: use rewrite's `occs` configuration option to limit which occurrences are rewritten, \
               or use `simp` or `conv` mode, which have strategies for certain kinds of dependencies \
-              (these tactics can handle proofs and '{.ofConstName ``Decidable}' instances whose types depend on the rewritten term, \
+              (these tactics can handle proofs and `{.ofConstName ``Decidable}` instances whose types depend on the rewritten term, \
               and `simp` can apply user-defined `@[congr]` theorems as well)."
           unless (← withLocalDeclD `_a α fun a => do isDefEq (← inferType (eAbst.instantiate1 a)) eType) do
             -- NB: using motive.arrow? would disallow motives where the dependency

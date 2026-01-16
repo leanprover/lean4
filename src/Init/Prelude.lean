679d2c49bfcb41a34711fdaf7b5f7cee17c88e6f
Joachim Breitner <mail@joachim-breitner.de>
2026-01-16 16:59:19 +0000

    refactor: move String.ofList to the Prelude
    This PR moves `String.ofList` to `Init.Prelude`. It is a function that
    the Lean kernel expects to be present and has special support for (when
    reducing string literals). By moving this to `Init.Prelude`, all
    declarations that are special to the kernel are in that single module.

--- src/Init/Prelude.lean
+++ src/Init/Prelude.lean
@@ -3482,6 +3482,18 @@ attribute [extern "lean_string_to_utf8"] String.toByteArray
 attribute [extern "lean_string_from_utf8_unchecked"] String.ofByteArray
 
 /--
+Creates a string that contains the characters in a list, in order.
+
+Examples:
+ * `['L', '∃', '∀', 'N'].asString = "L∃∀N"`
+ * `[].asString = ""`
+ * `['a', 'a', 'a'].asString = "aaa"`
+-/
+@[extern "lean_string_mk"]
+def String.ofList (data : List Char) : String :=
+  ⟨List.utf8Encode data,.intro data rfl⟩
+
+/--
 Decides whether two strings are equal. Normally used via the `DecidableEq String` instance and the
 `=` operator.
 

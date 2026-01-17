298f319a0ec7abbd74f590e1f75d4c3dc3c94c0c
Leonardo de Moura <leomoura@amazon.com>
2026-01-16 18:59:32 -0800

    chore: add `Sym/Lemmas.lean`

---
+++ src/Init/Sym/Lemmas.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2026 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura
+-/
+module
+prelude
+public import Init.Data.Nat.Basic
+public import Init.Data.Int.Basic
+namespace Lean.Sym
+
+end Lean.Sym

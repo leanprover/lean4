5457a227baa0e81f0efc2c1b50748af15dfaffd8
Leonardo de Moura <leomoura@amazon.com>
2026-01-17 18:54:30 -0800

    feat: conditional rewriting in `Sym.simp` (#12033)
    This PR adds support for conditional rewriting rules to `Sym.simp`.

--- src/Lean/Meta/Sym/Simp/Rewrite.lean
+++ src/Lean/Meta/Sym/Simp/Rewrite.lean
@@ -9,6 +9,7 @@ public import Lean.Meta.Sym.Simp.SimpM
 public import Lean.Meta.Sym.Simp.Simproc
 public import Lean.Meta.Sym.Simp.Theorems
 public import Lean.Meta.Sym.Simp.App
+public import Lean.Meta.Sym.Simp.Discharger
 import Lean.Meta.Sym.InstantiateS
 import Lean.Meta.Sym.Simp.DiscrTree
 namespace Lean.Meta.Sym.Simp
@@ -28,8 +29,18 @@ def mkValue (expr : Expr) (pattern : Pattern) (result : MatchUnifyResult) : Expr
 /--
 Tries to rewrite `e` using the given theorem.
 -/
-public def Theorem.rewrite (thm : Theorem) (e : Expr) : SimpM Result := do
+public def Theorem.rewrite (thm : Theorem) (e : Expr) (d : Discharger := dischargeNone) : SimpM Result := do
   if let some result ← thm.pattern.match? e then
+    -- **Note**: Potential optimization: check whether pattern covers all variables.
+    for arg in result.args do
+      let .mvar mvarId := arg | pure ()
+      unless (← mvarId.isAssigned) do
+        let decl ← mvarId.getDecl
+        if let some val ← d decl.type then
+          mvarId.assign val
+        else
+          -- **Note**: Failed to discharge hypothesis.
+          return .rfl
     let proof := mkValue thm.expr thm.pattern result
     let rhs   := thm.rhs.instantiateLevelParams thm.pattern.levelParams result.us
     let rhs   ← shareCommonInc rhs
@@ -41,12 +52,12 @@ public def Theorem.rewrite (thm : Theorem) (e : Expr) : SimpM Result := do
   else
     return .rfl
 
-public def Theorems.rewrite (thms : Theorems) : Simproc := fun e => do
+public def Theorems.rewrite (thms : Theorems) (d : Discharger := dischargeNone) : Simproc := fun e => do
   for (thm, numExtra) in thms.getMatchWithExtra e do
     let result ← if numExtra == 0 then
-      thm.rewrite e
+      thm.rewrite e d
     else
-      simpOverApplied e numExtra thm.rewrite
+      simpOverApplied e numExtra (thm.rewrite · d)
     if !result.isRfl then
       return result
   return .rfl

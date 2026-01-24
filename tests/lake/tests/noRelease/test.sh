leanprover/lean4

commit 648e1b18771691dc05a7a979d03b462925648839

fix: lake: `--no-build` failure w/ optional release fetch (#12086)
This PR fixes a bug where a `lake build --no-build` would exit with code
`3` if the optional job to fetch a GitHub or Reservoir release for a
package failed (even if nothing else needed rebuilding).

src/lake/Lake/Build/Run.lean
@@ -50,14 +50,14 @@ private structure MonitorContext where
   /-- How often to poll jobs (in milliseconds). -/
   updateFrequency : Nat
 
-@[inline] def MonitorContext.logger (ctx : MonitorContext) : MonadLog IO :=
+@[inline] def MonitorContext.logger (ctx : MonitorContext) : MonadLog BaseIO :=
   .stream ctx.out ctx.outLv ctx.useAnsi
 
 /-- State of the Lake build monitor. -/
 private structure MonitorState where
   jobNo : Nat := 0
   totalJobs : Nat := 0
-  didBuild : Bool := false
+  wantsRebuild : Bool := false
   failures : Array String
   resetCtrl : String
   lastUpdate : Nat
@@ -114,16 +114,17 @@ private def renderProgress (running unfinished : Array OpaqueJob) (h : 0 < unfin
     flush
 
 private def reportJob (job : OpaqueJob) : MonitorM PUnit := do
-  let {jobNo, totalJobs, didBuild, ..} ← get
+  let {jobNo, totalJobs, ..} ← get
   let {failLv, outLv, showOptional, out, useAnsi, showProgress, minAction, showTime, ..} ← read
   let {task, caption, optional, ..} := job
-  let {log, action, buildTime, ..} := task.get.state
+  let {log, action, wantsRebuild, buildTime, ..} := task.get.state
   let maxLv := log.maxLv
   let failed := strictAnd log.hasEntries (maxLv ≥ failLv)
-   if !didBuild && action = .build then
-    modify fun s => {s with didBuild := true}
   if failed && !optional then
-    modify fun s => {s with failures := s.failures.push caption}
+    modify fun s => {s with
+      failures := s.failures.push caption
+      wantsRebuild := s.wantsRebuild || wantsRebuild
+    }
   let hasOutput := failed || (log.hasEntries && maxLv ≥ outLv)
   let showJob :=
     (!optional || showOptional) &&
@@ -195,7 +196,7 @@ end Monitor
 
 /-- **For internal use only.** -/
 public structure MonitorResult where
-  didBuild : Bool
+  wantsRebuild : Bool
   failures : Array String
   numJobs : Nat
 
@@ -233,11 +234,11 @@ def monitorJobs'
   return {
     failures := s.failures
     numJobs := s.totalJobs
-    didBuild := s.didBuild
+    wantsRebuild := s.wantsRebuild
   }
 
 /-- The job monitor function. An auxiliary definition for `runFetchM`. -/
-@[inline, deprecated "Deprecated without replacement" (since := "2025-01-08")]
+@[inline, deprecated "Deprecated without replacement." (since := "2025-01-08")]
 public def monitorJobs
   (initJobs : Array OpaqueJob)
   (jobs : IO.Ref (Array OpaqueJob))
@@ -259,9 +260,9 @@ public def monitorJobs
 public def noBuildCode : ExitCode := 3
 
 def Workspace.saveOutputs
-  [logger : MonadLog IO] (ws : Workspace)
+  [logger : MonadLog BaseIO] (ws : Workspace)
   (out : IO.FS.Stream) (outputsFile : FilePath) (isVerbose : Bool)
-: IO Unit := do
+: BaseIO Unit := do
   unless ws.isRootArtifactCacheEnabled do
     logWarning s!"{ws.root.prettyName}: \
       the artifact cache is not enabled for this package, so the artifacts described \
@@ -313,13 +314,20 @@ def monitorJob (ctx : MonitorContext) (job : Job α) : BaseIO (BuildResult α) :
   else
     return {toMonitorResult := result, out := .error "build failed"}
 
-def monitorFetchM
-  (mctx : MonitorContext) (bctx : BuildContext) (build : FetchM α)
+def mkBuildContext' (ws : Workspace) (cfg : BuildConfig) (jobs : JobQueue) : BuildContext where
+  opaqueWs := ws
+  toBuildConfig := cfg
+  registeredJobs := jobs
+  leanTrace := .ofHash (pureHash ws.lakeEnv.leanGithash)
+    s!"Lean {Lean.versionStringCore}, commit {ws.lakeEnv.leanGithash}"
+
+def Workspace.startBuild
+  (ws : Workspace)  (cfg : BuildConfig) (jobs : JobQueue) (build : FetchM α)
   (caption := "job computation")
-: BaseIO (BuildResult α) := do
+: BaseIO (Job α) := do
+  let bctx := mkBuildContext' ws cfg jobs
   let compute := Job.async build (caption := caption)
-  let job ← compute.run.run'.run bctx |>.run nilTrace
-  monitorJob mctx job
+  compute.run.run'.run bctx |>.run nilTrace
 
 def Workspace.finalizeBuild
   (ws : Workspace) (cfg : BuildConfig) (ctx : MonitorContext) (result : BuildResult α)
@@ -327,17 +335,10 @@ def Workspace.finalizeBuild
   reportResult cfg ctx.out result
   if let some outputsFile := cfg.outputsFile? then
     ws.saveOutputs (logger := ctx.logger) ctx.out outputsFile (cfg.verbosity matches .verbose)
-  if cfg.noBuild && result.didBuild then
+  if cfg.noBuild && result.wantsRebuild then
     IO.Process.exit noBuildCode.toUInt8
   else
     IO.ofExcept result.out
-
-def mkBuildContext' (ws : Workspace) (cfg : BuildConfig) (jobs : JobQueue) : BuildContext where
-  opaqueWs := ws
-  toBuildConfig := cfg
-  registeredJobs := jobs
-  leanTrace := .ofHash (pureHash ws.lakeEnv.leanGithash)
-    s!"Lean {Lean.versionStringCore}, commit {ws.lakeEnv.leanGithash}"
 
 /--
 Run a build function in the Workspace's context using the provided configuration.
@@ -349,16 +350,13 @@ public def Workspace.runFetchM
 : IO α := do
   let jobs ← mkJobQueue
   let mctx ← mkMonitorContext cfg jobs
-  let bctx := mkBuildContext' ws cfg jobs
-  let result ← monitorFetchM mctx bctx build caption
+  let job ← ws.startBuild cfg jobs build caption
+  let result ← monitorJob mctx job
   ws.finalizeBuild cfg mctx result
 
-def monitorBuild
-  (mctx : MonitorContext) (bctx : BuildContext) (build : FetchM (Job α))
-  (caption := "job computation")
-: BaseIO (BuildResult α) := do
-  let result ← monitorFetchM mctx bctx build caption
-   match result.out with
+def monitorBuild (mctx : MonitorContext) (job : Job (Job α)) : BaseIO (BuildResult α) := do
+  let result ← monitorJob mctx job
+  match result.out with
   | .ok job =>
     if let some a ← job.wait? then
       return {result with out := .ok a}
@@ -374,24 +372,24 @@ Returns whether a build is needed to validate `build`. Does not report on the at
 
 This is equivalent to checking whether `lake build --no-build` exits with code 0.
 -/
-@[inline] public def Workspace.checkNoBuild
+public def Workspace.checkNoBuild
   (ws : Workspace) (build : FetchM (Job α))
 : BaseIO Bool := do
   let jobs ← mkJobQueue
   let cfg := {noBuild := true}
   let mctx ← mkMonitorContext cfg jobs
-  let bctx := mkBuildContext' ws cfg jobs
-  let result ← monitorBuild mctx bctx build
-  return result.isOk && !result.didBuild
+  let job ← ws.startBuild cfg jobs build
+  let result ← monitorBuild mctx job
+  return result.isOk -- `isOk` means no failures, and thus no `--no-build` failures
 
 /-- Run a build function in the Workspace's context and await the result. -/
-@[inline] public def Workspace.runBuild
+public def Workspace.runBuild
   (ws : Workspace) (build : FetchM (Job α)) (cfg : BuildConfig := {})
 : IO α := do
   let jobs ← mkJobQueue
   let mctx ← mkMonitorContext cfg jobs
-  let bctx := mkBuildContext' ws cfg jobs
-  let result ← monitorBuild mctx bctx build
+  let job ← ws.startBuild cfg jobs build
+  let result ← monitorBuild mctx job
   ws.finalizeBuild cfg mctx result
 
 /-- Produce a build job in the Lake monad's workspace and await the result. -/

tests/lake/tests/noRelease/test.sh
@@ -23,12 +23,17 @@ git tag release
 INIT_REV=`git rev-parse release`
 git commit --allow-empty -m "second commit"
 popd
+set +x
 
 # Clone dependency
-$LAKE update
+test_run update
+
+# Test that an indirect fetch does not cause `--no-build` to fail
+echo "# TEST: --no-build fetch"
+test_run build dep:extraDep -v --no-build
+
 # Remove the release tag from the local copy
-git -C .lake/packages/dep tag -d release
-set +x
+test_cmd git -C .lake/packages/dep tag -d release
 
 # Test that a direct invocation of `lake build *:release` fails
 echo "# TEST: Direct fetch"

src/lake/Lake/Build/Common.lean
@@ -304,13 +304,13 @@ and log are saved to `traceFile`, if the build completes without a fatal error
   (depTrace : BuildTrace) (traceFile : FilePath) (build : JobM α)
   (action : JobAction := .build)
 : JobM α := do
+  updateAction action
   let noBuildTraceFile := traceFile.addExtension "nobuild"
   if (← getNoBuild) then
-    updateAction .build
+    modify ({· with wantsRebuild := true})
     writeBuildTrace noBuildTraceFile depTrace Json.null {}
     error s!"target is out-of-date and needs to be rebuilt"
   else
-    updateAction action
     let startTime ← IO.monoMsNow
     try
       let iniPos ← getLogPos

src/lake/Lake/Build/Job/Basic.lean
@@ -63,6 +63,8 @@ public structure JobState where
   log : Log := {}
   /-- Tracks whether this job performed any significant build action. -/
   action : JobAction := .unknown
+  /-- Whether this job failed due to a request to rebuild for `--no-build`. -/
+  wantsRebuild : Bool := false
   /-- Current trace of a build job. -/
   trace : BuildTrace := .nil
   /-- How long the job spent building (in milliseconds). -/
@@ -72,6 +74,7 @@ public structure JobState where
 public def JobState.merge (a b : JobState) : JobState where
   log := a.log ++ b.log
   action := a.action.merge b.action
+  wantsRebuild := a.wantsRebuild || b.wantsRebuild
   trace := mixTrace a.trace b.trace
   buildTime := a.buildTime + b.buildTime
 

commit 6f409e0eeabba761758c7f8576bfd5311129d7f7

fix: lake: `--no-build` exit code w/ release fetch (#12142)
This PR fixes a bug introduced in #12086 where a `lake build :release
--no-build` would exit with code 1 rather than the `--no-build ` code 3.
Now both the bug from #12086 and this bug are fixed.

src/lake/Lake/Build/Run.lean
@@ -120,11 +120,10 @@ private def reportJob (job : OpaqueJob) : MonitorM PUnit := do
   let {log, action, wantsRebuild, buildTime, ..} := task.get.state
   let maxLv := log.maxLv
   let failed := strictAnd log.hasEntries (maxLv ≥ failLv)
+  if wantsRebuild then
+    modify fun s => if s.wantsRebuild then s else {s with wantsRebuild := true}
   if failed && !optional then
-    modify fun s => {s with
-      failures := s.failures.push caption
-      wantsRebuild := s.wantsRebuild || wantsRebuild
-    }
+    modify fun s => {s with failures := s.failures.push caption}
   let hasOutput := failed || (log.hasEntries && maxLv ≥ outLv)
   let showJob :=
     (!optional || showOptional) &&
@@ -335,10 +334,14 @@ def Workspace.finalizeBuild
   reportResult cfg ctx.out result
   if let some outputsFile := cfg.outputsFile? then
     ws.saveOutputs (logger := ctx.logger) ctx.out outputsFile (cfg.verbosity matches .verbose)
-  if cfg.noBuild && result.wantsRebuild then
-    IO.Process.exit noBuildCode.toUInt8
-  else
-    IO.ofExcept result.out
+  match result.out with
+  | .ok a =>
+    return a
+  | .error e =>
+    if cfg.noBuild && result.wantsRebuild then
+      IO.Process.exit noBuildCode.toUInt8
+    else
+      throw (IO.userError e)
 
 /--
 Run a build function in the Workspace's context using the provided configuration.

tests/lake/tests/noRelease/test.sh
@@ -3,6 +3,8 @@ source ../common.sh
 
 ./clean.sh
 
+NO_BUILD_CODE=3
+
 # ---
 # Test Lake's behavior when failing to fetch a cloud release
 # ---
@@ -29,8 +31,12 @@ set +x
 test_run update
 
 # Test that an indirect fetch does not cause `--no-build` to fail
-echo "# TEST: --no-build fetch"
+echo "# TEST: --no-build success"
 test_run build dep:extraDep -v --no-build
+
+# Test that a direct fetch causes `--no-build` to fail with the proper exit code
+echo "# TEST: --no-build failure"
+test_status $NO_BUILD_CODE build dep:release -v --no-build
 
 # Remove the release tag from the local copy
 test_cmd git -C .lake/packages/dep tag -d release
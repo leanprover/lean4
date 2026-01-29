d7b9fe583a498f5c821a7c590175e54a21dc5bc3
Sebastian Ullrich <sebasti@nullri.ch>
2026-01-29 13:35:24 +0000

    add and fix module test

--- tests/pkg/initialize/Initialize/Basic.lean
+++ tests/pkg/initialize/Initialize/Basic.lean
@@ -1,8 +1,10 @@
-initialize initNat : Nat ← pure 42
+module
 
-initialize ref : IO.Ref (Array String) ← IO.mkRef #[]
+public initialize initNat : Nat ← pure 42
 
-private initialize ref3 : Unit ← ref.modify (·.push "ref3")
-initialize ref2 : Unit ← ref.modify (·.push "ref2")
-initialize ref1 : Unit ← ref.modify (·.push "ref1")
-private initialize ref4 : Unit ← ref.modify (·.push "ref4")
+public initialize ref : IO.Ref (Array String) ← IO.mkRef #[]
+
+initialize ref3 : Unit ← ref.modify (·.push "ref3")
+public initialize ref2 : Unit ← ref.modify (·.push "ref2")
+public initialize ref1 : Unit ← ref.modify (·.push "ref1")
+initialize ref4 : Unit ← ref.modify (·.push "ref4")

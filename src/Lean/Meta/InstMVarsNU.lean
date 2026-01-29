946773f0da2edd06c543e5fbc70e3c7a1c55ead3
Joachim Breitner <mail@joachim-breitner.de>
2026-01-29 15:28:12 +0000

    Please CI

--- src/Lean/Meta/InstMVarsNU.lean
+++ src/Lean/Meta/InstMVarsNU.lean
@@ -1,10 +1,13 @@
+/-
+Copyright (c) 2026 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joachim Breitner
+-/
 
 module
 prelude
 
 public import Lean.Meta.Basic
-
-open Lean Meta
 
 namespace Lean.Meta
 

9d57b541cae795fda880e28fc302198a8e36dae4
Sebastian Ullrich <sebasti@nullri.ch>
2026-02-02 09:13:11 +0000

    annotate Init.lean

--- src/Init.lean
+++ src/Init.lean
@@ -42,7 +42,7 @@ public import Init.While
 public import Init.Syntax
 public import Init.Internal
 public import Init.Try
-public meta import Init.Try  -- make sure `Try.Config` can be evaluated anywhere
+public meta import Init.Try  -- shake: keep (make sure `Try.Config` can be evaluated anywhere)
 public import Init.BinderNameHint
 public import Init.Task
 public import Init.MethodSpecsSimp

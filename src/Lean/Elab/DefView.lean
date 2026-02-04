5bfa0a7a8f0b12bd9a4716da2d1a1a2a7ad96619
Sebastian Ullrich <sebasti@nullri.ch>
2026-02-04 12:27:03 +0000

    feat: instances should always be non-recursive

--- src/Lean/Elab/DefView.lean
+++ src/Lean/Elab/DefView.lean
@@ -173,6 +173,11 @@ def mkDefViewOfInstance (modifiers : Modifiers) (stx : Syntax) : CommandElabM De
   let prio            ← liftMacroM <| expandOptNamedPrio stx[2]
   let attrStx         ← `(attr| instance $(quote prio):num)
   let modifiers       := modifiers.addAttr { kind := attrKind, name := `instance, stx := attrStx }
+  if modifiers.recKind matches .partial then
+    throwError "Invalid use of `partial`, instances are always non-recursive. Consider splitting out a recursive `def`."
+  else if modifiers.recKind matches .nonrec then
+    logWarning "Unnecessary use of `nonrec`, instances are always non-recursive."
+  let modifiers := { modifiers with recKind := .nonrec }
   let (binders, type) := expandDeclSig stx[4]
   let declId ← match stx[3].getOptional? with
     | some declId =>

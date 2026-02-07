da62a81e5e2e65cf21783a6277fbe8dbd3ad073b
Sebastian Ullrich <sebasti@nullri.ch>
2026-02-07 16:19:15 +0100

    feat: shake: track simpset/grindset uses (#12375)
    This PR extends shake with tracking of attribute names passed to
    `simp`/`grind`.
    
    On the way there, it also fixes `register_simp/grind_attr` uses outside
    `public meta section` as well as go-to-definition on declaration-level
    uses of the created attributes (tactic-level goto would be a separate
    todo).

--- src/Lean/Meta/Tactic/Simp/RegisterCommand.lean
+++ src/Lean/Meta/Tactic/Simp/RegisterCommand.lean
@@ -23,10 +23,10 @@ macro (name := _root_.Lean.Parser.Command.registerSimpAttr) doc:(docComment)?
   let procIdParser := mkIdentFrom procId (`Parser.Attr ++ procId.getId)
   let procDescr := quote s!"simproc set for {procId.getId.toString}"
   -- TODO: better docComment for simprocs
-  `($[$doc:docComment]? initialize ext : SimpExtension ← registerSimpAttr $(quote id.getId) $descr $(quote id.getId)
+  `($[$doc:docComment]? public meta initialize ext : SimpExtension ← registerSimpAttr $(quote id.getId) $descr
     $[$doc:docComment]? syntax (name := $idParser:ident) $(quote str):str (Parser.Tactic.simpPre <|> Parser.Tactic.simpPost)? patternIgnore("← " <|> "<- ")? (prio)? : attr
     /-- Simplification procedure -/
-    initialize extProc : SimprocExtension ← registerSimprocAttr $(quote procId.getId) $procDescr none $(quote procId.getId)
+    public meta initialize extProc : SimprocExtension ← registerSimprocAttr $(quote procId.getId) $procDescr none
     /-- Simplification procedure -/
     syntax (name := $procIdParser:ident) $(quote procStr):str (Parser.Tactic.simpPre <|> Parser.Tactic.simpPost)? : attr)
 

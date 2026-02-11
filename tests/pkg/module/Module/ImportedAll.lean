f20cae3729e4428c13883b906ec63e2c28dc4995
Joachim Breitner <mail@joachim-breitner.de>
2026-02-11 12:49:10 +0100

    fix: no defeq equations for irreducible definitions (#12429)
    This PR sets the `irreducible` attribute before generating the equations
    for recursive definitions. This prevents these equations to be marked as
    `defeq`, which could lead to `simp` generation proofs that do not type
    check at default transparency.
    
    This issue is surfacing more easily since well-founded recursion on
    `Nat` is implemented with a dedicated fix point operator (#7965). Before
    that, `WellFounded.fix` was used, which is inherently not reducing, so
    we did get the desired result even without the explicit reducibility
    setting.
    
    Fixes #12398.

--- tests/pkg/module/Module/ImportedAll.lean
+++ tests/pkg/module/Module/ImportedAll.lean
@@ -105,7 +105,7 @@ info: theorem f_wfrec.induct_unfolding : ∀ (motive : Nat → Nat → Nat → P
 -/
 #guard_msgs(pass trace, all) in #print sig f_wfrec.induct_unfolding
 
-/-- info: @[defeq] theorem f_exp_wfrec.eq_1 : ∀ (x : Nat), f_exp_wfrec 0 x = x -/
+/-- info: theorem f_exp_wfrec.eq_1 : ∀ (x : Nat), f_exp_wfrec 0 x = x -/
 #guard_msgs in #print sig f_exp_wfrec.eq_1
 
 /--

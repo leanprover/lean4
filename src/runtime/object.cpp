f171f666e5fdf94520ea984dc45184170cf591de
Henrik Böving <hargonix@gmail.com>
2026-02-11 00:35:40 +0000

    chore: update types

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -2768,82 +2768,82 @@ void unlock_simple_atomic(std::atomic<int>& lock) {
     lock.notify_one();
 }
 
-extern "C" LEAN_EXPORT lean_object* lean_obj_once_cold(lean_object** loc, lean_once_cell_t* tok, lean_object* (*init)(void)) {
+extern "C" LEAN_EXPORT lean_object* lean_obj_once_cold(lean_object** loc, lean_once_cell_t* tok, lean_object* (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
         lean_mark_persistent(*loc);
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT uint8_t lean_uint8_once_cold(uint8_t* loc, lean_once_cell_t* tok, uint8_t (*init)(void)) {
+extern "C" LEAN_EXPORT uint8_t lean_uint8_once_cold(uint8_t* loc, lean_once_cell_t* tok, uint8_t (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT uint16_t lean_uint16_once_cold(uint16_t* loc, lean_once_cell_t* tok, uint16_t (*init)(void)) {
+extern "C" LEAN_EXPORT uint16_t lean_uint16_once_cold(uint16_t* loc, lean_once_cell_t* tok, uint16_t (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT uint32_t lean_uint32_once_cold(uint32_t* loc, lean_once_cell_t* tok, uint32_t (*init)(void)) {
+extern "C" LEAN_EXPORT uint32_t lean_uint32_once_cold(uint32_t* loc, lean_once_cell_t* tok, uint32_t (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT uint64_t lean_uint64_once_cold(uint64_t* loc, lean_once_cell_t* tok, uint64_t (*init)(void)) {
+extern "C" LEAN_EXPORT uint64_t lean_uint64_once_cold(uint64_t* loc, lean_once_cell_t* tok, uint64_t (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT size_t lean_usize_once_cold(size_t* loc, lean_once_cell_t* tok, size_t (*init)(void)) {
+extern "C" LEAN_EXPORT size_t lean_usize_once_cold(size_t* loc, lean_once_cell_t* tok, size_t (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT float lean_float32_once_cold(float* loc, lean_once_cell_t* tok, float (*init)(void)) {
+extern "C" LEAN_EXPORT float lean_float32_once_cold(float* loc, lean_once_cell_t* tok, float (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;
 }
 
-extern "C" LEAN_EXPORT double lean_float_once_cold(double* loc, lean_once_cell_t* tok, double (*init)(void)) {
+extern "C" LEAN_EXPORT double lean_float_once_cold(double* loc, lean_once_cell_t* tok, double (*init)()) {
     lock_simple_atomic(tok->lock);
-    if (tok->state != 1) {
+    if (tok->state.load() != 1) {
         *loc = init();
-        tok->state = 1;
+        tok->state.store(1);
     }
     unlock_simple_atomic(tok->lock);
     return *loc;

f084ce14974f9de67e3031ac3b563aaf5590b6a3
Sebastian Graf <sgraf1337@gmail.com>
2026-02-16 14:17:00 +0100

    test: share benchmark driver for Sym mvcgen; don't measure unfolding (#12501)
    This PR shares the driver code from the Sym-based mvcgen benchmarks. It
    also moves the `simp only [loop, step]` call out of the measured
    section, so that we measure purely the overhead of VC generation.
    
    The new benchmark results are as follows. All measurements for n=1000:
    
    ```
    baseline_add_sub_cancel:   719.318425 ms, kernel: 382.708178 ms
    vcgen_add_sub_cancel:      306.883079 ms, kernel: 455.050825 ms
    vcgen_deep_add_sub_cancel: 543.350543 ms, kernel: 896.926298 ms
    vcgen_get_throw_set:       669.566541 ms, kernel: 60754.202714 ms
    ```
    
    Note that `vcgen_add_sub_cancel` sped up by 100% because we no longer
    measure unfolding `loop` and `step`. The baseline didn't speed up as
    much because it unfolded in the same `Sym.simp` call that also does
    other rewrites, so there was no `simp` pass that could be eliminated.

--- tests/bench/mvcgen/sym/vcgen_get_throw_set.lean
+++ tests/bench/mvcgen/sym/vcgen_get_throw_set.lean
@@ -5,6 +5,7 @@ Authors: Sebastian Graf
 -/
 import Lean
 import VCGen
+import Driver
 
 open Lean Parser Meta Elab Tactic Sym Std Do SpecAttr
 
@@ -26,46 +27,6 @@ def loop (n : Nat) : ExceptT String (StateM Nat) Unit := do
 
 def Goal (n : Nat) : Prop := ⦃fun s => ⌜s = 0⌝⦄ loop n ⦃⇓_ s => ⌜s = n⌝⦄
 
-/-- Helper function for executing a tactic `k` for solving `Goal n`. -/
-def driver (n : Nat) (check := true) (k : MVarId → MetaM Unit) : MetaM Unit := do
-  let some goal ← unfoldDefinition? (mkApp (mkConst ``Goal) (mkNatLit n)) | throwError "UNFOLD FAILED!"
-  let mvar ← mkFreshExprMVar goal
-  let startTime ← IO.monoNanosNow
-  k mvar.mvarId!
-  let endTime ← IO.monoNanosNow
-  let ms := (endTime - startTime).toFloat / 1000000.0
-  if check then
-    let startTime ← IO.monoNanosNow
-    checkWithKernel (← instantiateExprMVars mvar)
-    let endTime ← IO.monoNanosNow
-    let kernelMs := (endTime - startTime).toFloat / 1000000.0
-    IO.println s!"goal_{n}: {ms} ms, kernel: {kernelMs} ms"
-  else
-    IO.println s!"goal_{n}: {ms} ms"
-
-/-
-A tactic for solving goal `Goal n`
--/
-macro "solve" : tactic => `(tactic| {
-  simp only [loop, step]
-  mvcgen'
-  all_goals sorry
-  -- all_goals grind
-})
-
-/--
-Solves a goal of the form `Goal n` using the `solve` tactic.
--/
-def solveUsingMeta (n : Nat) (check := true) : MetaM Unit := do
-  driver n check fun mvarId => do
-    let ([], _) ← Lean.Elab.runTactic mvarId (← `(tactic| solve)).raw {} {} | throwError "FAILED!"
-
-def runBenchUsingMeta (sizes : List Nat) : MetaM Unit := do
-  IO.println "=== VCGen tests ==="
-  IO.println ""
-  for n in sizes do
-    solveUsingMeta n
-
 set_option maxRecDepth 10000
 set_option maxHeartbeats 10000000
 
@@ -76,5 +37,6 @@ example : Goal 20 := by
   all_goals sorry
 -/
 
-#eval runBenchUsingMeta [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
--- #eval runBenchUsingMeta [1000]
+#eval runBenchUsingTactic ``Goal [``loop, ``step] `(tactic| mvcgen' <;> sorry)
+  [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+  -- [1000]

910c71954ecadebe61eb4946adf1c14f36609a1b
Sofia Rodrigues <sofia@algebraic.dev>
2026-02-17 21:07:48 -0300

    feat: reason phrase

--- src/Std/Internal/Http/Data/Response.lean
+++ src/Std/Internal/Http/Data/Response.lean
@@ -37,6 +37,11 @@ structure Response.Head where
   status : Status := .ok
 
   /--
+  Optional reason-phrase override for the status line.
+  -/
+  reasonPhrase : Option String := none
+
+  /--
   The HTTP protocol version used in the response, e.g. `HTTP/1.1`.
   -/
   version : Version := .v11
@@ -80,9 +85,10 @@ namespace Response
 
 instance : ToString Head where
   toString r :=
+    let reasonPhrase := r.reasonPhrase.getD r.status.reasonPhrase
     toString r.version ++ " " ++
     toString r.status.toCode ++ " " ++
-    toString r.status ++ "\r\n"
+    reasonPhrase ++ "\r\n"
 
 open Internal in
 instance : Encode .v11 Head where

b888f032b2d003912b4a48b64da131000c293bcb
fiforeach <249703130+fiforeach@users.noreply.github.com>
2026-02-19 17:32:39 +0100

    Fix and improve test

--- tests/lean/run/12559.lean
+++ tests/lean/run/12559.lean
@@ -1,4 +1,11 @@
--- The `linter.all` option also controls the `linter.unusedSimpArgs` linter.
+/-!
+  # Make `linter.all` option also control the `linter.unusedSimpArgs` linter
+
+  https://github.com/leanprover/lean4/issues/12559 -/
+
+section
+
+set_option linter.unusedSimpArgs true
 
 /--
 warning: This simp argument is unused:
@@ -10,9 +17,14 @@ Hint: Omit it from the simp argument list.
 Note: This linter can be disabled with `set_option linter.unusedSimpArgs false`
 -/
 #guard_msgs in
-set_option linter.all true in
 example : True := by simp [False]
 
+end
+
+section
+
+set_option linter.all true
+
 /--
 warning: This simp argument is unused:
   False
@@ -23,8 +35,9 @@ Hint: Omit it from the simp argument list.
 Note: This linter can be disabled with `set_option linter.unusedSimpArgs false`
 -/
 #guard_msgs in
-set_option linter.unusedSimpArgs true in
 example : True := by simp [False]
 
 set_option linter.all false in
 example : True := by simp [False]
+
+end

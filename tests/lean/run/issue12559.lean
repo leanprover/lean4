7ca6366f36ebe990cae17c3a07c53de1a0e1a5e4
fiforeach <249703130+fiforeach@users.noreply.github.com>
2026-02-19 11:38:22 +0100

    Improve test

--- tests/lean/run/issue12559.lean
+++ tests/lean/run/issue12559.lean
@@ -1,3 +1,15 @@
--- There should be no warning for `linter.unusedSimpArgs`
+/--
+warning: This simp argument is unused:
+  False
+
+Hint: Omit it from the simp argument list.
+  simp ̵[̵F̵a̵l̵s̵e̵]̵
+
+Note: This linter can be disabled with `set_option linter.unusedSimpArgs false`
+-/
+#guard_msgs in
+example : True := by simp [False]
+
+-- The `linter.all` option also controls the `linter.unusedSimpArgs` linter.
 set_option linter.all false in
 example : True := by simp [False]

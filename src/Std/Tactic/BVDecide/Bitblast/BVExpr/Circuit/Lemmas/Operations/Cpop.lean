a3617dffc9f6c8a7608dc5767206c4fc7523b924
luisacicolini <luisacicolini@gmail.com>
2026-02-19 18:02:27 +0000

    chore: credit of this massive thing

--- src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Operations/Cpop.lean
+++ src/Std/Tactic/BVDecide/Bitblast/BVExpr/Circuit/Lemmas/Operations/Cpop.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Luisa Cicolini
+Authors: Luisa Cicolini, Siddharth Bhat, Henrik Böving
 -/
 module
 

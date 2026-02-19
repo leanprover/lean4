1004f9cc8b386b1c461524febec3e4a7817d7413
fiforeach <249703130+fiforeach@users.noreply.github.com>
2026-02-19 11:58:03 +0100

    Fix test
    Tests in `tests/lean/run` run with `-Dlinter.all=false` to reduce noise.

--- tests/lean/run/issue12559.lean
+++ tests/lean/run/issue12559.lean
@@ -8,6 +8,7 @@ Hint: Omit it from the simp argument list.
 Note: This linter can be disabled with `set_option linter.unusedSimpArgs false`
 -/
 #guard_msgs in
+set_option linter.all true in
 example : True := by simp [False]
 
 -- The `linter.all` option also controls the `linter.unusedSimpArgs` linter.

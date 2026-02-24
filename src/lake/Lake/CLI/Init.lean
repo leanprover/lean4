72c82c7541183c98a176e2768569335a6284d70e
Shreyas <shreyasss94@gmail.com>
2026-02-24 20:09:21 +0100

    fix silly mistake

--- src/lake/Lake/CLI/Init.lean
+++ src/lake/Lake/CLI/Init.lean
@@ -230,7 +230,7 @@ weak.linter.mathlibStandardSet = true
 
 [[require]]
 name = \"cslib\"
-scope = \"leanprover\"
+scope = \"leanprover-community\"
 rev = {repr rev}
 
 [[lean_lib]]
@@ -239,9 +239,7 @@ name = {repr libRoot}
 
 def readmeFileContents (pkgName : String) := s!"# {pkgName}"
 
-def mathReadmeFileContents (pkgName : String) := s!"# {pkgName}"
-
-def csReadmeFileContents (pkgName : String) := s!"# {pkgName}"
+def mathReadmeFileContents (pkgName : String) := s!"# {pkgName}
 
 ## GitHub configuration
 
@@ -255,6 +253,21 @@ To set up your new GitHub repository, follow these steps:
 
 After following the steps above, you can remove this section from the README file.
 "
+def csReadmeFileContents (pkgName : String) := s!"# {pkgName}
+
+## GitHub configuration
+
+To set up your new GitHub repository, follow these steps:
+
+* Under your repository name, click **Settings**.
+* In the **Actions** section of the sidebar, click \"General\".
+* Check the box **Allow GitHub Actions to create and approve pull requests**.
+* Click the **Pages** section of the settings sidebar.
+* In the **Source** dropdown menu, select \"GitHub Actions\".
+
+After following the steps above, you can remove this section from the README file.
+"
+
 
 def leanActionWorkflowContents :=
 "name: Lean Action CI

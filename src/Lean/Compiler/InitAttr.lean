114113b9026fbe3d4ab3ea4a190147aecc4b0a5a
Sebastian Ullrich <sebasti@nullri.ch>
2026-02-24 17:08:13 +0000

    catch missing initializer enabling early

--- src/Lean/Compiler/InitAttr.lean
+++ src/Lean/Compiler/InitAttr.lean
@@ -160,44 +160,45 @@ def declareBuiltin (forDecl : Name) (value : Expr) : CoreM Unit :=
 
 @[export lean_run_init_attrs]
 private unsafe def runInitAttrs (env : Environment) (opts : Options) : IO Unit := do
-  if (← isInitializerExecutionEnabled) then
-    -- **Note**: `ModuleIdx` is not an abbreviation, and we don't have instances for it.
-    -- Thus, we use `(modIdx : Nat)`
-    for mod in env.header.modules, (modIdx : Nat) in 0...* do
-      let initRuntime := Elab.inServer.get opts || mod.irPhases != .runtime
+  if !(← isInitializerExecutionEnabled) then
+    throw <| IO.userError "`enableInitializerExecution` must be run before calling `importModules (loadExts := true)`"
+  -- **Note**: `ModuleIdx` is not an abbreviation, and we don't have instances for it.
+  -- Thus, we use `(modIdx : Nat)`
+  for mod in env.header.modules, (modIdx : Nat) in 0...* do
+    let initRuntime := Elab.inServer.get opts || mod.irPhases != .runtime
 
-      -- any native Lean code reachable by the interpreter (i.e. from shared
-      -- libraries with their corresponding module in the Environment) must
-      -- first be initialized
-      let pkg? := env.getModulePackageByIdx? modIdx
-      if env.header.isModule && /- TODO: remove after reboostrap -/ false then
-        let initializedRuntime ← pure initRuntime <&&> runModInit (phases := .runtime) mod.module pkg?
-        let initializedComptime ← runModInit (phases := .comptime) mod.module pkg?
-        if initializedRuntime || initializedComptime then
-          continue
-      else
-        if (← runModInit (phases := .all) mod.module pkg?) then
-          continue
-
-      -- As `[init]` decls can have global side effects, ensure we run them at most once,
-      -- just like the compiled code does.
-      if (← interpretedModInits.get).contains mod.module then
+    -- any native Lean code reachable by the interpreter (i.e. from shared
+    -- libraries with their corresponding module in the Environment) must
+    -- first be initialized
+    let pkg? := env.getModulePackageByIdx? modIdx
+    if env.header.isModule && /- TODO: remove after reboostrap -/ false then
+      let initializedRuntime ← pure initRuntime <&&> runModInit (phases := .runtime) mod.module pkg?
+      let initializedComptime ← runModInit (phases := .comptime) mod.module pkg?
+      if initializedRuntime || initializedComptime then
         continue
-      interpretedModInits.modify (·.insert mod.module)
-      let modEntries := regularInitAttr.ext.getModuleEntries env modIdx
-      -- `getModuleIREntries` is identical to `getModuleEntries` if we loaded only one of
-      -- .olean (from `meta initialize`)/.ir (`initialize` via transitive `meta import`)
-      -- so deduplicate (these lists should be very short).
-      -- If we have both, we should not need to worry about their relative ordering as `meta` and
-      -- non-`meta` initialize should not have interdependencies.
-      let modEntries := modEntries ++ (regularInitAttr.ext.getModuleIREntries env modIdx).filter (!modEntries.contains ·)
-      for (decl, initDecl) in modEntries do
-        if !initRuntime && getIRPhases env decl == .runtime then
-          continue
-        if initDecl.isAnonymous then
-          let initFn ← IO.ofExcept <| env.evalConst (IO Unit) opts decl
-          initFn
-        else
-          runInit env opts decl initDecl
+    else
+      if (← runModInit (phases := .all) mod.module pkg?) then
+        continue
+
+    -- As `[init]` decls can have global side effects, ensure we run them at most once,
+    -- just like the compiled code does.
+    if (← interpretedModInits.get).contains mod.module then
+      continue
+    interpretedModInits.modify (·.insert mod.module)
+    let modEntries := regularInitAttr.ext.getModuleEntries env modIdx
+    -- `getModuleIREntries` is identical to `getModuleEntries` if we loaded only one of
+    -- .olean (from `meta initialize`)/.ir (`initialize` via transitive `meta import`)
+    -- so deduplicate (these lists should be very short).
+    -- If we have both, we should not need to worry about their relative ordering as `meta` and
+    -- non-`meta` initialize should not have interdependencies.
+    let modEntries := modEntries ++ (regularInitAttr.ext.getModuleIREntries env modIdx).filter (!modEntries.contains ·)
+    for (decl, initDecl) in modEntries do
+      if !initRuntime && getIRPhases env decl == .runtime then
+        continue
+      if initDecl.isAnonymous then
+        let initFn ← IO.ofExcept <| env.evalConst (IO Unit) opts decl
+        initFn
+      else
+        runInit env opts decl initDecl
 
 end Lean

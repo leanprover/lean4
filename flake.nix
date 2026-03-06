9b79c9200cdbc67fb58481d00542568a25a5e8db
fiforeach <249703130+fiforeach@users.noreply.github.com>
2026-03-06 09:55:51 +0100

    Add meld to Nix flake

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
             llvmPackages.bintools  # wrapped lld
             llvmPackages.llvm  # llvm-symbolizer for asan/lsan
             gdb
+            meld
             python314
             tree  # for CI
           ];

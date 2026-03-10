a5d35889d0c2bc3bb9ef4d81ea370b8e6786cb47
Henrik Böving <hargonix@gmail.com>
2026-03-10 10:16:49 +0000

    chore: delete unused files

--- src/Lean/Compiler/IR.lean
+++ src/Lean/Compiler/IR.lean
@@ -13,7 +13,6 @@ public import Lean.Compiler.IR.CompilerM
 public import Lean.Compiler.IR.NormIds
 public import Lean.Compiler.IR.Checker
 public import Lean.Compiler.IR.UnboxResult
-public import Lean.Compiler.IR.EmitC
 public import Lean.Compiler.IR.Sorry
 public import Lean.Compiler.IR.ToIR
 public import Lean.Compiler.IR.ToIRType

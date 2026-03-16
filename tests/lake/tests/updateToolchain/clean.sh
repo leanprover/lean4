3e981ac55d632678d022f219c2f5e19095dd851f
Mac Malone <mac@lean-fro.org>
2026-03-16 19:28:37 +0000

    feat: lake: `fixedToolchain` package configuration

--- tests/lake/tests/updateToolchain/clean.sh
+++ tests/lake/tests/updateToolchain/clean.sh
@@ -1,1 +1,3 @@
-rm -f produced.out lean-toolchain a/lean-toolchain b/lean-toolchain
+rm -f produced.out
+rm -f lean-toolchain a/lean-toolchain b/lean-toolchain
+rm -f lake-manifest.json a/lake-manifest.json b/lake-manifest.json

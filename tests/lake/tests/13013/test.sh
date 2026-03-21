80042b27ae9d1e071de373a0713a75ce65716ddb
Mac Malone <mac@lean-fro.org>
2026-03-21 22:19:40 +0000

    chore: clarify test design

--- tests/lake/tests/13013/test.sh
+++ tests/lake/tests/13013/test.sh
@@ -5,8 +5,11 @@ source ../common.sh
 # This test covers issue 13013
 # https://github.com/leanprover/lean4/issues/13013
 
+# Lake (and Lean) rely on module names being unique within a package,
+# so this test verifies that Lake produces a clean error on such a configuration,
+# rather than supporting it.
+
 # Verify that sharing root module names across executables produces an error.
-# Lake expects module names to be unique within a package.
 test_err "shared.toml:9:0: test: executable 'bar' has the same root module 'Main' as executable 'foo'" \
   -f shared.toml build
 test_err "test: executable 'bar' has the same root module 'Main' as executable 'foo'" \

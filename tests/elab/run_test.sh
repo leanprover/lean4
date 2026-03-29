8b2e07b7cfb076698442498646f9560ab40fb352
Sebastian Ullrich <sebasti@nullri.ch>
2026-03-29 15:21:20 +0000

    chore: clean up old test artifacts

--- tests/elab/run_test.sh
+++ tests/elab/run_test.sh
@@ -3,8 +3,9 @@ run_before "$1"
 
 # `--root` to infer same private names as in the server
 # Elab.inServer to allow for arbitrary `#eval`
+# compiler.postponeCompile to immediate trace output
 capture_only "$1" \
-  lean --root=.. -DprintMessageEndPos=true -Dlinter.all=false -DElab.inServer=true "${TEST_LEAN_ARGS[@]}" "$1"
+  lean --root=.. -DprintMessageEndPos=true -Dlinter.all=false -DElab.inServer=true -Dcompiler.postponeCompile=true "${TEST_LEAN_ARGS[@]}" "$1"
 normalize_mvar_suffixes
 normalize_reference_urls
 normalize_measurements

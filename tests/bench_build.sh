ca66d58e0f1fc2d64e48187ee3081afdac7efb5e
Henrik Böving <hargonix@gmail.com>
2026-05-05 09:21:37 +0000

    fixup scripts

--- tests/bench_build.sh
+++ tests/bench_build.sh
@@ -5,8 +5,6 @@
 # See also the https://github.com/leanprover/radar readme.
 
 LLVM_RELEASE=19.1.2
-LLVM_BASE_DIR="$RADAR_CACHE/llvm/$LLVM_RELEASE"
-LLVM_DIR="$LLVM_BASE_DIR/lean-llvm"
 LLVM_TARBALL="$RADAR_CACHE/llvm/$LLVM_RELEASE.tar.zst"
 
 rm -rf "$RADAR_CACHE/llvm"
@@ -14,20 +12,14 @@ rm -rf "$RADAR_CACHE/llvm"
 if [ ! -f "$LLVM_TARBALL" ]; then
     mkdir -p "$RADAR_CACHE/llvm"
     curl --location -o "$LLVM_TARBALL" "https://github.com/leanprover/lean-llvm/releases/download/$LLVM_RELEASE/lean-llvm-x86_64-linux-gnu.tar.zst"
-    mkdir -p "$LLVM_BASE_DIR"
-    tar -I zstd -xf "$LLVM_TARBALL" -C "$LLVM_BASE_DIR"
 fi
 
-export LD_LIBRARY_PATH="$LLVM_DIR/lib/x86_64-unknown-linux-gnu:$LD_LIBRARY_PATH"
-export PATH="$LLVM_DIR/bin:$PATH"
-
-$LLVM_DIR/bin/clang --help
-$LLVM_DIR/bin/clang++ --help
-
-cmake --preset release -DWFAIL=OFF \
-  -DCMAKE_C_COMPILER="$LLVM_DIR/bin/clang" \
-  -DCMAKE_CXX_COMPILER="$LLVM_DIR/bin/clang++" \
-  -DLLVM_CONFIG="$LLVM_DIR/bin/llvm-config"
+mkdir -p build/release
+cd build/release
+eval cmake ../.. \
+    --preset release $(../.././script/prepare-llvm-linux.sh /tmp/llvm/19.1.2.tar.zst) \
+    -DWFAIL=OFF
+cd ../..
 make -C build/release -j"$(nproc)" bench-part1
 mv tests/part1.measurements.jsonl "$RADAR_OUT"
 

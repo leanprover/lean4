e44ee619984d7b226e57c4c17277f48a9f58bba8
Francisco Giordano <fg@frang.io>
2026-05-27 13:52:57 -0300

    feat: simp lemma for `seqRight`

--- src/Init/Control/Lawful/Basic.lean
+++ src/Init/Control/Lawful/Basic.lean
@@ -110,6 +110,9 @@ attribute [simp] map_pure seq_pure
 @[simp] theorem pure_id_seq [Applicative f] [LawfulApplicative f] (x : f α) : pure id <*> x = x := by
   simp [pure_seq]
 
+@[simp] theorem pure_seqRight [Applicative f] [LawfulApplicative f] (x : f α) (y : f β) : pure x *> y = y := by
+  simp [seqRight_eq]
+
 /--
 Lawful monads are those that satisfy a certain behavioral specification. While all instances of
 `Monad` should satisfy these laws, not all implementations are required to prove this.

0db02729eb880065d7ddb42a9c61c3fe2db87c38
Henrik Böving <hargonix@gmail.com>
2026-06-01 18:38:08 +0000

    choer: fix comment

--- src/Lean/Elab/Tactic/BVDecide.lean
+++ src/Lean/Elab/Tactic/BVDecide.lean
@@ -78,6 +78,6 @@ For the second kind more steps are involved:
 5. Add simplification lemmas for the primitive to `bv_normalize` in `Std.Tactic.BVDecide.Normalize`.
    If there are multiple ways to write the primitive (e.g. with TC based notation and without) you
    should normalize for one notation here.
-6. Add the reflection code to `Lean.Elab.Tactic.BVDecide.Frontend.BVDecide`
+6. Add the reflection code to `Lean.Meta.Tactic.BVDecide.Reflect`
 7. Add a test!
 -/

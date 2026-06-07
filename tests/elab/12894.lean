ca68590c5bf95202f00b476d12bf4a9a5b022f45
Parcly Taxel <reddeloostw@gmail.com>
2026-06-07 18:22:20 +0800

    doc

--- tests/elab/12894.lean
+++ tests/elab/12894.lean
@@ -1,3 +1,6 @@
+/-! Test that the `zero` parameter in `Nat.caseStrongRecOn`, which was made computable
+in https://github.com/leanprover/lean4/pull/12894, is not eagerly evaluated. -/
+
 def foo (n : Nat) : Nat := Nat.caseStrongRecOn n (panic!"I shouldn't be run") fun n _ ↦ n
 
 /-- info: 1 -/

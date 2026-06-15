a16b28a9926b21422ef8ee01291f9d69fd62458a
Julia Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-06-15 09:38:25 +0000

    chore: upstream `Nat.sqrt`

---
+++ src/Init/Data/Nat/Sqrt.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Leonardo de Moura, Jeremy Avigad, Mario Carneiro
+-/
+module
+
+prelude
+import Init.Data.Nat.Sqrt.Basic
+import Init.Data.Nat.Sqrt.Lemmas

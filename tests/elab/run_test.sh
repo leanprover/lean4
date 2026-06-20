1755a59880ac4b9961e0077df4db1ee062368a3b
Joscha <joscha@plugh.de>
2026-06-20 12:32:23 +0200

    bring back normalize_measurements

--- tests/elab/run_test.sh
+++ tests/elab/run_test.sh
@@ -9,6 +9,7 @@ capture_only "$1" \
   lean --root=.. -DprintMessageEndPos=true -Dlinter.all=false -DElab.inServer=true -Dcompiler.postponeCompile=false "${TEST_LEAN_ARGS[@]}" "$1"
 normalize_mvar_suffixes
 normalize_reference_urls
+normalize_measurements
 check_out_file
 check_exit_is_success
 

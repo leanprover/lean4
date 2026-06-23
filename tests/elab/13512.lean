d51974bcf8b4f3a61356912f029a632ad008e8f3
Théophile Wallez <theophile@wallez.net>
2026-06-23 16:58:44 +0200

    add more comments

--- tests/elab/13512.lean
+++ tests/elab/13512.lean
@@ -1,5 +1,12 @@
 module
 
+/-!
+  Test that matching on the output of an implicit reducible function does not reduces the function at default transparency.
+  Previous version of Lean (e.g. v4.30) would perform such a reduction.
+-/
+
+section ImplicitReducibleFunction
+
 @[implicit_reducible]
 def f (n: Nat): Nat × Nat :=
   (n, 2*n)
@@ -11,6 +18,9 @@ trace: n : Nat
 warning: declaration uses `sorry`
 -/
 #guard_msgs in
+-- Test with dsimp only.
+-- Lean v4.30 would leave the goal `2 * n = n + n`
+-- (hence unfolding the definition of `f`)
 theorem test_dsimp_f (n: Nat):
   let (n1, n2) := f n
   n2 = n1 + n1
@@ -19,11 +29,20 @@ theorem test_dsimp_f (n: Nat):
   trace_state
   sorry
 
+-- Test with grind internalizer.
+-- Lean v4.30 would fail, because `grind` preprocessing
+-- would leave the goal `n = (f n).fst ∧ 2*n = (f n).snd`
+-- which cannot be proved without inspecting the definition of `f`
 theorem test_grind_f (n: Nat):
   let (n1, n2) := f n
   n1 = (f n).fst ∧ n2 = (f n).snd
 := by
   grind
+
+section ImplicitReducibleFunction
+
+-- same tests, but using a typeclass instance instead of @[implicit_reducible] annotation.
+section TypeclassInstanceFunction
 
 class C (α: Type) where
   g: α → α × α
@@ -51,3 +70,5 @@ theorem test_grind_g (n: Nat):
   n1 = (C.g n).fst ∧ n2 = (C.g n).snd
 := by
   grind
+
+end TypeclassInstanceFunction

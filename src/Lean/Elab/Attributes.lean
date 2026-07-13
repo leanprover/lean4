3d5116fe98ba6defe7a09c6347269fe10dc07db5
Alex Vzorov <alex.vzorov@gmail.com>
2026-07-13 13:20:34 +0800

    detect if the attribute has been declared

--- src/Lean/Elab/Attributes.lean
+++ src/Lean/Elab/Attributes.lean
@@ -60,7 +60,11 @@ def elabAttr [Monad m] [MonadEnv m] [MonadResolveName m] [MonadError m] [MonadMa
       | .str _ s => pure <| Name.mkSimple s
       | _ => throwErrorAt attr  "Unknown attribute"
     match getAttributeImpl (← getEnv) attrName with
-      | .error _ => throwError "Unknown attribute `[{attrName}]`. Note, that attributes are impossible to use in the same file they are declared in: consider extracting the declaration into a separate file."
+      | .error _ => 
+          if attr.getKind == ``Parser.Attr.simple then
+            throwError "Unknown attribute `[{attrName}]`."
+          else
+            throwError "Unknown attribute `[{attrName}]`. Note, that attributes are impossible to use in the same file they are declared in: consider extracting the declaration into a separate file."
       | .ok impl =>
       if regularInitAttr.getParam? (← getEnv) impl.ref |>.isSome then  -- skip `builtin_initialize` attributes
         -- Reject attribute uses where the implementation's module has been loaded for IR only as

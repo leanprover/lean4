4fbe5e4eb3b85f1c36dfb2210b4e47de3b6b5ae3
Thomas CHAUMENY <thomas@lipsum.dev>
2026-07-16 20:04:53 +0200

    refactor: remove funext dependency from Classical.em, drop Quot.sound axiom

--- src/Init/Classical.lean
+++ src/Init/Classical.lean
@@ -32,34 +32,27 @@ noncomputable def choose {α : Sort u} {p : α → Prop} (h : ∃ x, p x) : α :
 theorem choose_spec {α : Sort u} {p : α → Prop} (h : ∃ x, p x) : p (choose h) :=
   (indefiniteDescription p h).property
 
-/-- **Diaconescu's theorem**: excluded middle from choice, Function extensionality and propositional extensionality. -/
+/-- **Diaconescu's theorem**: excluded middle from choice and propositional extensionality. -/
 theorem em (p : Prop) : p ∨ ¬p :=
-  let U (x : Prop) : Prop := x = True ∨ p
-  let V (x : Prop) : Prop := x = False ∨ p
-  have exU : ∃ x, U x := ⟨True, Or.inl rfl⟩
-  have exV : ∃ x, V x := ⟨False, Or.inl rfl⟩
-  let u : Prop := choose exU
-  let v : Prop := choose exV
-  have u_def : U u := choose_spec exU
-  have v_def : V v := choose_spec exV
+  let U := (p, True)
+  let V := (True, p)
+  let f : Prop × Prop → Bool → Prop :=
+    fun p b => bif b then p.2 else p.1
+  have exU : ∃ x, f U x := ⟨true, trivial⟩
+  have exV : ∃ x, f V x := ⟨false, trivial⟩
+  let u : Bool := choose exU
+  let v : Bool := choose exV
+  have u_def : f U u := choose_spec exU
+  have v_def : f V v := choose_spec exV
   have not_uv_or_p : u ≠ v ∨ p :=
-    match u_def, v_def with
-    | Or.inr h, _ => Or.inr h
-    | _, Or.inr h => Or.inr h
-    | Or.inl hut, Or.inl hvf =>
-      have hne : u ≠ v := by simp [hvf, hut]
-      Or.inl hne
+    match u, v with
+    | false, _ => Or.inr u_def
+    | _, true => Or.inr v_def
+    | true, false => Or.inl Bool.noConfusion
   have p_implies_uv : p → u = v :=
     fun hp =>
-    have hpred : U = V :=
-      funext fun x =>
-        have hl : (x = True ∨ p) → (x = False ∨ p) :=
-          fun _ => Or.inr hp
-        have hr : (x = False ∨ p) → (x = True ∨ p) :=
-          fun _ => Or.inr hp
-        show (x = True ∨ p) = (x = False ∨ p) from
-          propext (Iff.intro hl hr)
-    have h₀ : ∀ exU exV, @choose _ U exU = @choose _ V exV := by
+    have hpred : U = V := by simp [hp, U, V]
+    have h₀ : ∀ exU exV, choose exU = choose exV := by
       rw [hpred]; intros; rfl
     show u = v from h₀ _ _
   match not_uv_or_p with

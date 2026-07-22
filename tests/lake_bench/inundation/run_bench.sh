8498b627f7f3a07625b5f319e027858ad41bd23f
Mac Malone <mac@lean-fro.org>
2026-07-22 18:23:10 -0400

    chore: rename benchmarks

--- tests/lake_bench/inundation/run_bench.sh
+++ tests/lake_bench/inundation/run_bench.sh
@@ -5,42 +5,42 @@ echo "Running $PREFIX"
 rm -rf .lake lake-manifest.json test
 lake -R run mkBuild
 
-echo "Running $PREFIX/build no-op"
+echo "Running $PREFIX/build/no-op"
 lake -R clean
 lake build
-"$TEST_DIR/measure.py" -t "$PREFIX/build no-op" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/build/no-op" -d -a -- \
   lake build
 
-echo "Running $PREFIX/build clean"
+echo "Running $PREFIX/build/clean"
 lake -R clean
-"$TEST_DIR/measure.py" -t "$PREFIX/build clean" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/build/clean" -d -a -- \
   lake build
 
-echo "Running $PREFIX/build precompile no-op"
+echo "Running $PREFIX/build/precompile+no-op"
 lake -R -K precompile=true clean
 lake build
-"$TEST_DIR/measure.py" -t "$PREFIX/build precompile no-op" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/build/precompile+no-op" -d -a -- \
   lake build
 
-echo "Running $PREFIX/build precompile clean"
+echo "Running $PREFIX/build/precompile+clean"
 lake -R -K precompile=true clean
-"$TEST_DIR/measure.py" -t "$PREFIX/build precompile clean" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/build/precompile+clean" -d -a -- \
   lake build
 
-echo "Running $PREFIX/config elab"
+echo "Running $PREFIX/config/elab"
 lake -R run nop
-"$TEST_DIR/measure.py" -t "$PREFIX/config elab" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/config/elab" -d -a -- \
   lake -R run nop
 
-echo "Running $PREFIX/config import"
+echo "Running $PREFIX/config/import"
 lake -R run nop
-"$TEST_DIR/measure.py" -t "$PREFIX/config import" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/config/import" -d -a -- \
   lake run nop
 
-echo "Running $PREFIX/config tree"
+echo "Running $PREFIX/config/tree"
 lake -R run mkTree
 lake -d test/tree update
-"$TEST_DIR/measure.py" -t "$PREFIX/config tree" -d -a -- \
+"$TEST_DIR/measure.py" -t "$PREFIX/config/tree" -d -a -- \
   lake -d test/tree run nop
 
 echo "Running $PREFIX/env"

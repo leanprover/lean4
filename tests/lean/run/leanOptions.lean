61d8df87c248659bc4fa9968c9005aee8db23671
Sankalp Thakur <31366524+sankalpsthakur@users.noreply.github.com>
2026-08-02 06:09:21 +0530

    test: move Lean option guards into elab suite

--- tests/lean/run/leanOptions.lean
+++
@@ -1,6 +0,0 @@
-import Lean.Util.LeanOptions
-
-open Lean
-
-#guard (LeanOptionValue.ofString "two words").asCliFlagValue == "two words"
-#guard (LeanOption.mk `example (.ofString "two words")).asCliArg == "-Dexample=two words"

b83484198c774b42fea4f3151811a5a5ab389cf6
Sankalp Thakur <31366524+sankalpsthakur@users.noreply.github.com>
2026-08-02 05:36:06 +0530

    fix: render Json panic value without ToString

--- src/Lean/Data/Json/Basic.lean
+++ src/Lean/Data/Json/Basic.lean
@@ -184,7 +184,7 @@ inductive Json where
   | str (s : String)
   | arr (elems : Array Json)
   | obj (kvPairs : Std.TreeMap.Raw String Json)
-  deriving Inhabited
+  deriving Inhabited, Repr
 
 namespace Json
 
@@ -282,7 +282,7 @@ def getObjValD (j : Json) (k : String) : Json :=
 
 def setObjVal! : Json → String → Json → Json
   | obj kvs, k, v => obj <| kvs.insert k v
-  | j      , _, _ => panic! s!"Json.setObjVal!: not an object: {j}"
+  | j      , _, _ => panic! s!"Json.setObjVal!: not an object: {reprStr j}"
 
 /-- Assuming both inputs `o₁, o₂` are json objects, will compute `{...o₁, ...o₂}`.
 If `o₁` is not a json object, `o₂` will be returned.

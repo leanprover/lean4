3b6f68fe25142bcb7b5a846ac6c4555618e177da
Rob23oba <robin.arnez@web.de>
2026-08-07 21:55:00 +0200

    clarify and improve

--- src/runtime/object.cpp
+++ src/runtime/object.cpp
@@ -2383,16 +2383,19 @@ extern "C" LEAN_EXPORT uint8 lean_string_is_valid_pos(b_obj_arg s, b_obj_arg i0)
 extern "C" LEAN_EXPORT obj_res lean_string_utf8_extract_fast(b_obj_arg s, b_obj_arg b0, b_obj_arg e0) {
     usize b = lean_unbox(b0);
     usize e = lean_unbox(e0);
+    lean_assert(b <= lean_string_size(s) - 1);
+    lean_assert(e <= lean_string_size(s) - 1);
     if (b >= e) return lean_mk_string_unchecked("", 0, 0);
     char const * str = lean_string_cstr(s);
     return lean_mk_string_from_bytes_unchecked(str + b, e - b);
 }
 
 extern "C" LEAN_EXPORT obj_res lean_string_utf8_extract(b_obj_arg s, b_obj_arg b0, b_obj_arg e0) {
-    // Clamp b0 and e0 to usize values: Non-scalar values are out of bounds here (see lean_string_utf8_get)
-    // Values that are out of bounds all behave the same, so clamping is enough here
-    usize b = lean_is_scalar(b0) ? lean_unbox(b0) : LEAN_MAX_SMALL_NAT;
-    usize e = lean_is_scalar(e0) ? lean_unbox(e0) : LEAN_MAX_SMALL_NAT;
+    /* Replace non-scalar values with SIZE_MAX:
+    Non-scalar values are out of bounds here (see comment at string_utf8_get),
+    including SIZE_MAX, and values that are out of bounds all behave the same */
+    usize b = lean_is_scalar(b0) ? lean_unbox(b0) : SIZE_MAX;
+    usize e = lean_is_scalar(e0) ? lean_unbox(e0) : SIZE_MAX;
     char const * str = lean_string_cstr(s);
     usize sz = lean_string_size(s) - 1;
     if (b >= e || b >= sz) return lean_mk_string_unchecked("", 0, 0);

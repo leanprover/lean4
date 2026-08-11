73ec7c716f898adc7658b489ee2a3bdc9a29686f
kernelpanic888 <48477233+kernelpanic888@users.noreply.github.com>
2026-08-11 02:18:19 +0200

    fix: use implemented_by for Fin.foldl runtime

--- src/Init/Data/Fin/Fold.lean
+++ src/Init/Data/Fin/Fold.lean
@@ -262,11 +262,13 @@ private theorem foldlTR_succ (f : α → Fin (n+1) → α) (x) :
     foldlTR (n+1) f x = foldlTR n (fun x i => f x i.succ) (f x 0) :=
   foldl_loop ..
 
-@[csimp] theorem foldl_eq_foldlTR : @foldl = @foldlTR := by
+theorem foldl_eq_foldlTR : @foldl = @foldlTR := by
   funext α n f x
   induction n generalizing x with
   | zero => exact foldlTR_zero .. |>.symm
   | succ n ih => rw [foldl, foldlTR_succ, ih]
+
+attribute [implemented_by foldlTR] foldl
 
 @[simp] theorem foldl_zero (f : α → Fin 0 → α) (x) : foldl 0 f x = x :=
   rfl

ec84267d855df8b5db9efcd846e054f2cc622e30
Rob23oba <robin.arnez@web.de>
2026-08-14 19:26:04 +0200

    remove now redundant test

--- tests/elab_fail/276.lean
+++
@@ -1,9 +0,0 @@
-universe u v
-
--- `#check` works
-set_option pp.all true in
-#check fun {α : Sort v} => PEmpty.rec (fun _ => α)
-
--- but `def` doesn't work
--- error: code generator does not support recursor 'PEmpty.rec' yet
-def PEmpty.elim' {α : Sort v} := PEmpty.rec (fun _ => α)

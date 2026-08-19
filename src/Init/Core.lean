fed71644c8a40a54ebde33f488e665fb350c2673
Rob23oba <robin.arnez@web.de>
2026-08-19 16:04:09 +0200

    add test + fix instance

--- src/Init/Core.lean
+++ src/Init/Core.lean
@@ -1610,9 +1610,7 @@ theorem Eq.propIntro {a b : Prop} (h₁ : a → b) (h₂ : b → a) : a = b :=
 
 -- Eq for Prop is now decidable if the equivalent Iff is decidable
 instance {p q : Prop} [d : Decidable (p ↔ q)] : Decidable (p = q) :=
-  match d with
-  | isTrue h => isTrue (propext h)
-  | isFalse h => isFalse fun heq => h (heq ▸ Iff.rfl)
+  decidable_of_decidable_of_iff propext_iff.symm
 
 /-- Helper theorem for proving injectivity theorems -/
 theorem Lean.injEq_helper {P Q R : Prop} :

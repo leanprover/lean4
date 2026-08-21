acb983f5c2677e30caa9a14af69e3e7dcfd5d21a
Kyle Miller <kmill31415@gmail.com>
2026-08-21 11:22:46 -0700

    add interactive test showing terminfo on exception

--- tests/server_interactive/structGoTo.lean
+++ tests/server_interactive/structGoTo.lean
@@ -17,3 +17,18 @@ structure S where
 -/
 example (s : S) := s.b
                    --^ textDocument/definition
+
+/-!
+The field terminfo is still available even if there is an exception during elaboration.
+Field redeclaration currently throws an exception, preventing the step where the projection
+terminfos are added (and preventing adding the structure to the environment).
+-/
+structure SInvalid where
+  a : Nat
+--^ textDocument/hover
+  a : Nat
+--^ collectDiagnostics
+
+-- Check that the structure is never added to the environment:
+#check SInvalid
+      --^ collectDiagnostics

000b26acd38a60e88640cae9b1d48f495999d9ea
Sofia Rodrigues <sofia@algebraic.dev>
2026-08-25 11:20:30 -0300

    feat: add handlers

--- src/Std/Http/Client.lean
+++ src/Std/Http/Client.lean
@@ -6,7 +6,11 @@ Authors: Sofia Rodrigues
 module
 
 prelude
+public import Std.Http.Client.Authenticator
 public import Std.Http.Client.Config
+public import Std.Http.Client.CookieHandler
+public import Std.Http.Client.Error
+public import Std.Http.Client.Proxy
 
 public section
 

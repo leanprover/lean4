1acb0320aa7d425ec1c40ccf8a7de019ea0ac0f7
Julia Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-09-04 10:05:09 +0000

    Tweak script and bump

--- doc/std/grove/grove-local.sh
+++ doc/std/grove/grove-local.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 lake exe grove-stdlib --full metadata.json
 cd .lake/packages/grove/frontend
@@ -6,7 +7,5 @@ npm install
 cp ../../../../metadata.json public/metadata.json
 if [ -f "../../../../invalidated.json" ]; then
     cp ../../../../invalidated.json public/invalidated.json
-    GROVE_DATA_LOCATION=public/metadata.json GROVE_UPSTREAM_INVALIDATED_FACTS_LOCATION=public/invalidated.json npm run dev
-else
-    GROVE_DATA_LOCATION=public/metadata.json npm run dev
 fi
+npm run dev

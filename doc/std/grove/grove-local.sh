a2ec82aa77e3fc3c2c8c5dd244c52da26ebc23c4
Julia Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-09-09 13:38:39 +0000

    grove stuff

--- doc/std/grove/grove-local.sh
+++ doc/std/grove/grove-local.sh
@@ -3,7 +3,6 @@ set -e
 
 lake exe grove-stdlib --full metadata.json
 cd .lake/packages/grove/frontend
-npm install
 cp ../../../../metadata.json public/metadata.json
 if [ -f "../../../../invalidated.json" ]; then
     cp ../../../../invalidated.json public/invalidated.json
